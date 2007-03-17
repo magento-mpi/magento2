5de17794ef5a43e81754baf7c8896915ae1db255
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-03-17 02:56:30 +0000

    Theme switch

---
+++ app/code/core/Mage/Core/Block/Text/Tag/Css/Admin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Base html block
+ *
+ * @copyright  Varien, 2007
+ * @version    1.0 
+ * @author     Dmitriy Soroka <dmitriy@varien.com>
+ */
+
+class Mage_Core_Block_Text_Tag_Css_Admin extends Mage_Core_Block_Text_Tag_Css
+{
+    function __construct()
+    {
+        parent::__construct();
+        $theme = empty($_COOKIE['admtheme']) ? 'default' : $_COOKIE['admtheme'];
+        $this->setAttribute('theme', $theme);
+    }
+
+    function setHref($href, $locate='skin')
+    {
+        $url = Mage::getBaseUrl($locate).$href.$this->getAttribute('theme').'.css';
+        return $this->setTagParam('href', $url);
+    }
+    
+}// Class Mage_Core_Block_List END
