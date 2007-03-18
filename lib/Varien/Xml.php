0735d5790d693094b3c36812e29baaedf6018c87
Boris Gurvich <noreply@magento.com>
2007-03-18 23:26:45 +0000

    created layout update xmls

--- lib/Varien/Xml.php
+++ lib/Varien/Xml.php
@@ -59,11 +59,75 @@ class Varien_Xml extends SimpleXMLElement
         
         return $this;
     }
-
-    function extendChildByName($source, $overwrite=false)
+/*
+    function extendChildByNode($source, $overwrite=false, $mergeBy='name')
     {
-        $mergeBy = 'name';
+        // this will be our new target node
+        $targetChild = null;
         
+        // name of the source node
+        $sourceName = $source->getName();
+        
+        // here we have children of our source node
+        $sourceChildren = $source->children();
+
+        if (!$sourceChildren) {
+            // handle string node
+            if (isset($this->$sourceName)) {
+                if ($overwrite) {
+                    unset($this->$sourceName);
+                } else {
+                    return $this;
+                }
+            }
+            $targetChild = $this->addChild($sourceName, (string)$source);
+            foreach ($source->attributes() as $key=>$value) {
+                $targetChild->addAttribute($key, $value);
+            }
+            return $this;
+        }
+        
+        if (isset($this->$sourceName)) {
+            // search for target child with same name subnode as node's name
+            if (isset($source->$mergeBy)) {
+                foreach ($this->$sourceName as $targetNode) {
+                    if (!isset($targetNode->$mergeBy)) {
+                        Zend::exception("Can't merge identified node with non identified");
+                    }
+                    if ((string)$source->$mergeBy==(string)$targetNode->$mergeBy) {
+                        $targetChild = $targetNode;
+                        break;
+                    }
+                }
+            } else {
+                $existsWithId = false;
+                foreach ($this->$sourceName as $targetNode) {
+                    if (isset($targetNode->$mergeBy)) {
+                        Zend::exception("Can't merge identified node with non identified");
+                    }
+                }
+                $targetChild = $this->$sourceName;
+            }
+        }
+        
+        if (is_null($targetChild)) {
+            // if child target is not found create new and descend
+            $targetChild = $this->addChild($sourceName);
+            foreach ($source->attributes() as $key=>$value) {
+                $targetChild->addAttribute($key, $value);
+            }
+        }
+        
+        // finally add our source node children to resulting new target node
+        foreach ($sourceChildren as $childKey=>$childNode) {
+            $targetChild->extendChildByNode($childNode, $overwrite, $mergeBy);
+        }        
+        
+        return $this;
+    }
+    
+    function extendChildByAttribute($source, $overwrite=false, $mergeBy='name')
+    {
         // this will be our new target node
         $targetChild = null;
         
@@ -122,10 +186,10 @@ class Varien_Xml extends SimpleXMLElement
         
         // finally add our source node children to resulting new target node
         foreach ($sourceChildren as $childKey=>$childNode) {
-            $targetChild->extendChild($childNode);
+            $targetChild->extendChildByAttribute($childNode, $overwrite, $mergeBy);
         }        
         
         return $this;
     }
-    
+*/
 }
