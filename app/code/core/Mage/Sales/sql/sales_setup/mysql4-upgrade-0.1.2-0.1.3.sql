2d7f983620ac5cb79db240b89ac0fccdf3bfad2d
Boris Gurvich <noreply@magento.com>
2007-04-15 03:12:26 +0000

    added orders and invoices db tables

--- app/code/core/Mage/Sales/sql/sales_setup/mysql4-upgrade-0.1.2-0.1.3.sql
+++ app/code/core/Mage/Sales/sql/sales_setup/mysql4-upgrade-0.1.2-0.1.3.sql
@@ -37,7 +37,7 @@ drop table if exists sales_quote_attribute_decimal;
 CREATE TABLE `sales_quote` (
   `quote_id` int(11) unsigned NOT NULL auto_increment,
   PRIMARY KEY  (`quote_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Quotes info';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Quote header';
 
 CREATE TABLE `sales_quote_attribute_text` (
   `quote_id` int(11) unsigned NOT NULL default '0',
@@ -97,6 +97,136 @@ CREATE TABLE `sales_quote_attribute_datetime` (
   KEY `key` (`attribute_code`,`attribute_value`),
   CONSTRAINT `FK_sales_quote_attribute_datetime` FOREIGN KEY (`quote_id`) REFERENCES `sales_quote` (`quote_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Quote attributes DATETIME';
+
+
+CREATE TABLE `sales_order` (
+  `order_id` int(11) unsigned NOT NULL auto_increment,
+  PRIMARY KEY  (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Order header';
+
+CREATE TABLE `sales_order_attribute_text` (
+  `order_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','payment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` text NOT NULL,
+  KEY `document_id` (`order_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  CONSTRAINT `FK_sales_order_attribute_text` FOREIGN KEY (`order_id`) REFERENCES `sales_order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Order attributes TEXT';
+
+CREATE TABLE `sales_order_attribute_int` (
+  `order_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','payment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` int(11) NOT NULL default '0',
+  KEY `document_id` (`order_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  KEY `key` (`attribute_code`,`attribute_value`),
+  CONSTRAINT `FK_sales_order_attribute_int` FOREIGN KEY (`order_id`) REFERENCES `sales_order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Order attributes INT';
+
+CREATE TABLE `sales_order_attribute_decimal` (
+  `order_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','payment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` decimal(12,4) NOT NULL default '0.0000',
+  KEY `document_id` (`order_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  KEY `key` (`attribute_code`,`attribute_value`),
+  CONSTRAINT `FK_sales_order_attribute_decimal` FOREIGN KEY (`order_id`) REFERENCES `sales_order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Order attributes DECIMAL';
+
+CREATE TABLE `sales_order_attribute_varchar` (
+  `order_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','payment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` varchar(255) NOT NULL default '',
+  KEY `document_id` (`order_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  KEY `key` (`attribute_code`,`attribute_value`),
+  CONSTRAINT `FK_sales_order_attribute_varchar` FOREIGN KEY (`order_id`) REFERENCES `sales_order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Order attributes VARCHAR';
+
+CREATE TABLE `sales_order_attribute_datetime` (
+  `order_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','payment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` datetime NOT NULL default '0000-00-00 00:00:00',
+  KEY `document_id` (`order_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  KEY `key` (`attribute_code`,`attribute_value`),
+  CONSTRAINT `FK_sales_order_attribute_datetime` FOREIGN KEY (`order_id`) REFERENCES `sales_order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Order attributes DATETIME';
+
+
+CREATE TABLE `sales_invoice` (
+  `invoice_id` int(11) unsigned NOT NULL auto_increment,
+  PRIMARY KEY  (`invoice_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Invoice header';
+
+CREATE TABLE `sales_invoice_attribute_text` (
+  `invoice_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','transaction','shipment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` text NOT NULL,
+  KEY `document_id` (`invoice_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  CONSTRAINT `FK_sales_invoice_attribute_text` FOREIGN KEY (`invoice_id`) REFERENCES `sales_invoice` (`invoice_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Invoice attributes TEXT';
+
+CREATE TABLE `sales_invoice_attribute_int` (
+  `invoice_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','transaction','shipment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` int(11) NOT NULL default '0',
+  KEY `document_id` (`invoice_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  KEY `key` (`attribute_code`,`attribute_value`),
+  CONSTRAINT `FK_sales_invoice_attribute_int` FOREIGN KEY (`invoice_id`) REFERENCES `sales_invoice` (`invoice_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Invoice attributes INT';
+
+CREATE TABLE `sales_invoice_attribute_decimal` (
+  `invoice_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','transaction','shipment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` decimal(12,4) NOT NULL default '0.0000',
+  KEY `document_id` (`invoice_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  KEY `key` (`attribute_code`,`attribute_value`),
+  CONSTRAINT `FK_sales_invoice_attribute_decimal` FOREIGN KEY (`invoice_id`) REFERENCES `sales_invoice` (`invoice_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Invoice attributes DECIMAL';
+
+CREATE TABLE `sales_invoice_attribute_varchar` (
+  `invoice_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','transaction','shipment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` varchar(255) NOT NULL default '',
+  KEY `document_id` (`invoice_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  KEY `key` (`attribute_code`,`attribute_value`),
+  CONSTRAINT `FK_sales_invoice_attribute_varchar` FOREIGN KEY (`invoice_id`) REFERENCES `sales_invoice` (`invoice_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Invoice attributes VARCHAR';
+
+CREATE TABLE `sales_invoice_attribute_datetime` (
+  `invoice_id` int(11) unsigned NOT NULL default '0',
+  `entity_type` enum('self','item','address','transaction','shipment') NOT NULL default 'self',
+  `entity_id` int(11) NOT NULL default '0',
+  `attribute_code` varchar(32) NOT NULL default '',
+  `attribute_value` datetime NOT NULL default '0000-00-00 00:00:00',
+  KEY `document_id` (`invoice_id`,`entity_id`),
+  KEY `entity_id` (`entity_id`,`attribute_code`),
+  KEY `key` (`attribute_code`,`attribute_value`),
+  CONSTRAINT `FK_sales_invoice_attribute_datetime` FOREIGN KEY (`invoice_id`) REFERENCES `sales_invoice` (`invoice_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Invoice attributes DATETIME';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
