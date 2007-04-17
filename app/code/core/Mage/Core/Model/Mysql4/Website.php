d7c0cb27e45a5a346f9b4cc656d24f2523fc1da2
Boris Gurvich <noreply@magento.com>
2007-04-17 23:00:36 +0000

    website resource

--- app/code/core/Mage/Core/Model/Mysql4/Website.php
+++ app/code/core/Mage/Core/Model/Mysql4/Website.php
@@ -2,5 +2,24 @@
 
 class Mage_Core_Model_Mysql4_Website
 {
+    protected $_read;
+    protected $_write;
+    protected $_websiteTable;
     
+    public function __construct()
+    {
+        $this->_read = Mage::registry('resources')->getConnection('core_read');
+        $this->_write = Mage::registry('resources')->getConnection('core_write');
+        $this->_websiteTable = Mage::registry('resources')->getTableName('core_resource', 'website');
+    }
+    
+    public function getWebsiteById($id)
+    {
+        return $this->_read->fetchRow("select * from ".$this->_websiteTable." where website_id=?", $id);
+    }
+    
+    public function getWebsiteByCode($code)
+    {
+        return $this->_read->fetchRow("select * from ".$this->_websiteTable." where website_code=?", $code);
+    }
 }
