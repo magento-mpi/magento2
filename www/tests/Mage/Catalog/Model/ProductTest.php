0b47c9d5423b4767326822178d7eaa236b6e0c96
Andrey Korolyov <noreply@magento.com>
2007-04-26 01:42:59 +0000

    add test

--- www/tests/Mage/Catalog/Model/ProductTest.php
+++ www/tests/Mage/Catalog/Model/ProductTest.php
@@ -2,10 +2,19 @@
 
 class Mage_Catalog_Model_ProductTest extends PHPUnit_Framework_TestCase
 {
+    protected $fixture;
+ 
+    protected function setUp()
+    {
+        // Create a fixture.
+        $this->fixture = Mage::getModel('catalog', 'product');
+    }
+
+
     public function testLoad()
     {
-        $product = Mage::getModel('catalog', 'product')->load(500);
-        $this->assertEquals(500, $product->getId());
+        $product = $this->fixture->load(500);
+        $this->assertEquals(500, $this->fixture->getId());
     }
     
     public function testSave()
