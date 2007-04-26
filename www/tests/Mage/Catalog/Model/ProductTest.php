3cf5261d978654e1da328be99abe300126e2f84b
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-04-26 01:24:32 +0000

    Load product test

--- www/tests/Mage/Catalog/Model/ProductTest.php
+++ www/tests/Mage/Catalog/Model/ProductTest.php
@@ -4,10 +4,8 @@ class Mage_Catalog_Model_ProductTest extends PHPUnit_Framework_TestCase
 {
     public function testLoad()
     {
-        // Create the Array fixture.
-        $fixture = array();
- 
+        $product = Mage::getModel('catalog', 'product')->load(500);
         // Assert that the size of the Array fixture is 0.
-        $this->assertEquals(0, sizeof($fixture));
+        //$this->assertEquals(0, sizeof($fixture));
     }
 }
