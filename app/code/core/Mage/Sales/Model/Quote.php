f1b87c12ed8d0e37241f005539e75cef00583078
Boris Gurvich <noreply@magento.com>
2007-04-27 21:46:14 +0000

    minor fixes

--- app/code/core/Mage/Sales/Model/Quote.php
+++ app/code/core/Mage/Sales/Model/Quote.php
@@ -108,16 +108,15 @@ class Mage_Sales_Model_Quote extends Mage_Sales_Model_Document
             foreach ($this->getEntitiesByType('item') as $item) {
                 if ($item->getProductId()==$product->getProductId()) {
                     $item->setQty($item->getQty()+$product->getQty());
-                    $item->setPrice($product->getTierPrice($item->getQty()));
-                    $this->collectTotals();
-                    return $this;
+                    break;
                 }
             }
+        } else {
+            $item = Mage::getModel('sales', 'quote_entity_item');
+            $item->setEntityType('item')->addData($product->getData());
+            $this->addEntity($item);
         }
-        $item = Mage::getModel('sales', 'quote_entity_item');
-        $item->setEntityType('item')->addData($product->getData());
         $item->setPrice($product->getTierPrice($item->getQty()));
-        $this->addEntity($item);
         $this->collectTotals();
         return $this;
     }
