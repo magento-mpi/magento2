4ccfc2ca0f122701371a7488fdc21b093f8177ee
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-05-03 23:19:45 +0000

    Customer forgot password

--- app/code/core/Mage/Customer/controllers/AccountController.php
+++ app/code/core/Mage/Customer/controllers/AccountController.php
@@ -179,6 +179,7 @@ class Mage_Customer_AccountController extends Mage_Core_Controller_Front_Action
                     $mailer = Mage::getModel('customer', 'email')
                         ->setTemplate('email/forgot_password.phtml')
                         ->setCustomer($customer)
+                        ->setSubject('New password')
                         ->send();
                     
                     Mage::getSingleton('customer', 'session')
