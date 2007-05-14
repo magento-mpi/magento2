be534463b6bebcf2ca061a86121fde113fc71390
Andrey Korolyov <noreply@magento.com>
2007-05-14 23:42:34 +0000

    new JsonForm class

--- www/admin/js/magenta/core/panel/form.js
+++ www/admin/js/magenta/core/panel/form.js
@@ -47,89 +47,16 @@ Ext.extend(Mage.core.PanelForm, Mage.core.Panel, {
     
     _buildForm : function() {
         var i;
+
         this.frm = new Mage.JsonForm({
-            method : 'POST',
-            url : this.form.config.url,
-            fileUpload : false
+            method : this.form.config.method,
+            name : this.form.config.name,
+            action : this.form.config.action,
+            fileUpload : this.form.config.fileupload,
+            metaData : this.form.elements
         });
         this._buildTemplate(this.form.config.id + '_El');        
-        console.log(this.form);
-        for(i=0; i < this.form.elements.length; i++) {
-          this._makeElement(this.frm, this.form.elements[i]);
-        }
+
         this.frm.render(this.form.config.id + '_El');
-    },
-    
-    _makeElement : function(form, element) {
-        var i;
-        switch(element.config.type) {
-            case 'fieldset' :
-                form.fieldset(element.config);
-                for(i=0; i < element.elements.length; i++) {
-                    this._makeElement(form, element.elements[i]);
-                }
-                form.end;
-                return true;
-                break;
-            case 'column' :
-                form.column(element.config);
-                for(i=0; i < element.elements.length; i++) {
-                    this._makeElement(form, element.elements[i]);
-                }
-                form.end;
-                return true;
-                break;                
-            default :
-                form.add(this._makeField(element))
-                return true;
-                break;                
-        }
-    },
-    
-    _makeField : function(field) {
-            var config = {
-                fieldLabel : field.config.label,
-                name : field.config.name,
-                allowBlank : true,
-                value : field.config.value
-            };
-            switch (field.config.ext_type) {
-                case 'checkbox' :
-                    return new Ext.form.Checkbox(config);                
-                case 'combobox' :
-                    var RecordDef = Ext.data.Record.create([{name: 'value'},{name: 'label'}]);                    
-                    var myReader = new Ext.data.JsonReader({root: 'values'}, RecordDef);                    
-                    var store = new Ext.data.Store({
-                       	reader : myReader,
-                       	proxy : new Ext.data.MemoryProxy(field.config)
-                    });
-                    store.load();
-                    config.store = store;
-                    config.displayField = 'label';
-                    config.valueField = 'value';
-                    config.mode = 'local';
-                    config.typeAhead = true;
-                    config.triggerAction = 'all';
-                    config.forceSelection = true;
-                    var combo = new Ext.form.ComboBox(config);
-                    combo.setValue(field.config.value);
-                    return combo;
-                case 'datefield' :
-                    return new Ext.form.DateField(config);                
-                case 'numberfield' :
-                    return new Ext.form.NumberField(config);                
-                case 'radio' :
-                    return new Ext.form.Radio(config);                
-                case 'textarea' :
-                    return new Ext.form.TextArea(config);                
-                case 'textfield' :
-                    return new Ext.form.TextField(config);
-                case 'file' : 
-                    config.inputType = 'file';
-                    return new Ext.form.Field(config);
-            }
-            throw 'This field type:"'+field.ext_type+'" not supported';
-        
     }
-     
 })
--- www/admin/js/magenta/jsonform.js
+++ www/admin/js/magenta/jsonform.js
@@ -5,9 +5,87 @@ Mage.JsonForm = function(config) {
 Ext.extend(Mage.JsonForm, Ext.form.Form, {
     render : function(ct) {
         if (this.metaData) {
-            console.log(metaData);
+            this._buildMetaData();
         }
         Mage.JsonForm.superclass.render.call(this, ct);
+    },
+    
+    _buildMetaData : function() {
+        for(i=0; i < this.metaData.length; i++) {
+          this._makeElement(this, this.metaData[i]);
+        }
+    },
+    
+    _makeElement : function(form, element) {
+        var i;
+        switch(element.config.type) {
+            case 'fieldset' :
+                form.fieldset(element.config);
+                for(i=0; i < element.elements.length; i++) {
+                    this._makeElement(form, element.elements[i]);
+                }
+                form.end;
+                return true;
+                break;
+            case 'column' :
+                form.column(element.config);
+                for(i=0; i < element.elements.length; i++) {
+                    this._makeElement(form, element.elements[i]);
+                }
+                form.end;
+                return true;
+                break;                
+            default :
+                form.add(this._makeField(element))
+                return true;
+                break;                
+        }
+    },
+    
+    _makeField : function(field) {
+            var config = {
+                fieldLabel : field.config.label,
+                name : field.config.name,
+                allowBlank : true,
+                value : field.config.value
+            };
+            switch (field.config.ext_type) {
+                case 'checkbox' :
+                    return new Ext.form.Checkbox(config);                
+                case 'combobox' :
+                    var RecordDef = Ext.data.Record.create([{name: 'value'},{name: 'label'}]);                    
+                    var myReader = new Ext.data.JsonReader({root: 'values'}, RecordDef);                    
+                    var store = new Ext.data.Store({
+                       	reader : myReader,
+                       	proxy : new Ext.data.MemoryProxy(field.config)
+                    });
+                    store.load();
+                    config.store = store;
+                    config.displayField = 'label';
+                    config.valueField = 'value';
+                    config.mode = 'local';
+                    config.typeAhead = true;
+                    config.triggerAction = 'all';
+                    config.forceSelection = true;
+                    var combo = new Ext.form.ComboBox(config);
+                    combo.setValue(field.config.value);
+                    return combo;
+                case 'datefield' :
+                    return new Ext.form.DateField(config);                
+                case 'numberfield' :
+                    return new Ext.form.NumberField(config);                
+                case 'radio' :
+                    return new Ext.form.Radio(config);                
+                case 'textarea' :
+                    return new Ext.form.TextArea(config);                
+                case 'textfield' :
+                    return new Ext.form.TextField(config);
+                case 'file' : 
+                    config.inputType = 'file';
+                    return new Ext.form.Field(config);
+            }
+            throw 'This field type:"'+field.ext_type+'" not supported';
+        
     }
 })
 
