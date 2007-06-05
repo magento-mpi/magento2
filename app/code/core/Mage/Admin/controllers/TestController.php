62bcd2ec5765f6a235b57a0bcbdc8b238dbf0abc
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-06-05 19:09:58 +0000

    test customer wizard json

--- app/code/core/Mage/Admin/controllers/TestController.php
+++ app/code/core/Mage/Admin/controllers/TestController.php
@@ -12,9 +12,41 @@ class Mage_Admin_TestController extends Mage_Core_Controller_Zend_Action
 {
     public function wizardAction()
     {
-        $arr = array(
-            'key' => 'value'
-        );
-        $this->getResponse()->setBody(Zend_Json::encode($arr));
+        $step = $this->getRequest()->getParam('step');
+        
+        switch ($step) {
+            case 1:
+                $customer = Mage::getModel('customer', 'customer');
+                $form = new Mage_Admin_Block_Customer_Form($customer);
+
+                $tab = array(
+                    'name'  => 'general',
+                    'title' => __('Account Information'),
+                    'type'  => 'form',
+                    'form'  => $form->toArray()
+                );
+                break;
+            case 2:
+                $address = Mage::getModel('customer', 'address');
+                $form = new Mage_Admin_Block_Customer_Address_Form($address);
+
+                $tab = array(
+                    'name'  => 'general',
+                    'title' => __('Customer address'),
+                    'type'  => 'form',
+                    'form'  => $form->toArray()
+                );
+                break;
+            default:
+                $tab['title'] = __('New Customer');
+                $tab['name']  = 'default';
+                $tab['type']  = 'view';
+                $tab['url']   = Mage::getBaseUrl();
+                break;
+        }
+        
+        $cardStruct['title'] = __('New Customer');
+        $cardStruct['tabs'][] = $tab;
+        $this->getResponse()->setBody(Zend_Json::encode($cardStruct));
     }
 }
