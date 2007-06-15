4797293f3e265f33a7c3a1d45dc561c0e06650d8
Boris Gurvich <noreply@magento.com>
2007-06-15 09:20:51 +0000

    changed location.href

--- app/code/core/Mage/Adminhtml/controllers/IndexController.php
+++ app/code/core/Mage/Adminhtml/controllers/IndexController.php
@@ -1,33 +1,33 @@
 <?php
 
-class Mage_Adminhtml_IndexController extends Mage_Core_Controller_Front_Action 
+class Mage_Adminhtml_IndexController extends Mage_Core_Controller_Front_Action
 {
     protected function _outTemplate($tplName)
     {
         $block = $this->getLayout()->createBlock('core/template')->setTemplate("adminhtml/$tplName.phtml");
         $this->getResponse()->setBody($block->toHtml());
     }
-    
+
     public function indexAction()
     {
         $this->_outTemplate('frameset');
     }
-    
+
     public function jsFrameAction()
     {
         $this->_outTemplate('js');
     }
-    
+
     public function waitFrameAction()
     {
         $this->_outTemplate('wait');
     }
-    
+
     public function layoutFrameAction()
     {
         $this->_outTemplate('layout');
     }
-    
+
     public function exampleAction()
     {
         $this->_outTemplate('example');
