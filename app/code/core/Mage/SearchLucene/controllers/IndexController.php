06634dd00c07aae906b378c640872803c0f86e6c
Andrey Korolyov <noreply@magento.com>
2007-06-16 21:11:14 +0000

    rename

---
+++ app/code/core/Mage/SearchLucene/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Page Index Controller
+ *
+ * @copyright  Varien, 2007
+ * @version    1.0
+ * @author	   Andrey Korolyov <andrey@varien.com>
+ */
+
+class Mage_Search_IndexController extends Mage_Core_Controller_Front_Action
+{
+    function indexAction()
+    {
+
+    }
+}// Class IndexController END
