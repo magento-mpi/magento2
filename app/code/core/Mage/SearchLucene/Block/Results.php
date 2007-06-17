08bab6b6baa28d43a72a001f9ec4db1b7eac363d
Andrey Korolyov <noreply@magento.com>
2007-06-17 01:19:39 +0000

    Zend_Search_Lucene test module

---
+++ app/code/core/Mage/SearchLucene/Block/Results.php
@@ -0,0 +1,25 @@
+<?php
+class Mage_SearchLucene_Block_Results extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getResults(Zend_Controller_Request_Http $request)
+    {
+        $this->setTemplate('searchlucene/result.phtml');
+        $query = $request->getParam('q', false);
+        $queryEscaped = htmlspecialchars($query);
+
+        Mage::registry('action')->getLayout()->getBlock('head.title')->setContents('Search results for: '.$queryEscaped);
+
+        $var = Mage::getBaseDir('var');
+        $index_dir = $var . DS . 'search' . DS . 'index';
+        $index = Zend_Search_Lucene::open($index_dir);
+        $hits = $index->find($query);
+
+        $this->assign('hits', $hits);
+        $this->assign('query', $queryEscaped);
+    }
+}
--- app/code/core/Mage/SearchLucene/controllers/IndexController.php
+++ app/code/core/Mage/SearchLucene/controllers/IndexController.php
@@ -9,8 +9,107 @@
 
 class Mage_SearchLucene_IndexController extends Mage_Core_Controller_Front_Action
 {
+
+
     function indexAction()
     {
+        $var = Mage::getBaseDir('var');
+        $index_dir = $var . DS . 'search' . DS . 'index';
+        $index = Zend_Search_Lucene::open($index_dir);
+        $indexSize = $index->count();
+        $documents = $index->numDocs();
+        echo "Index size : " . $indexSize ."<br>";
+        echo "Documents : " . $documents ."<br>";
+    }
 
+
+    function searchAction()
+    {
+
+        $this->loadLayout();
+
+        $searchQuery = $this->getRequest()->getParam('q', false);
+        if ($searchQuery) {
+            $this->getLayout()->getBlock('search.form.mini')->assign('query', $searchQuery);
+
+            $searchResBlock = $this->getLayout()->createBlock('searchlucene/results', 'searchlucene.results');
+            $searchResBlock->getResults($this->getRequest());
+
+            $this->getLayout()->getBlock('content')->append($searchResBlock);
+        }
+        $this->renderLayout();
+    }
+
+
+
+    function buildindexAction()
+    {
+        set_time_limit(0);
+        $var = Mage::getBaseDir('var');
+        $index_dir = $var . DS . 'search' . DS . 'index';
+
+
+        $index = Zend_Search_Lucene::open($index_dir);
+
+        $collection = Mage::getModel('catalog_resource/product_collection');
+        $collection->addAttributeToSelect('name')
+            ->addAttributeToSelect('description')
+            ->addAttributeToSelect('sku')
+            ->addAttributeToSelect('manufacturer')
+            ->addAttributeToSelect('shoe_type');
+
+        $collection->load();
+        $types = Mage::getModel('catalog/product_attribute')
+            ->loadByCode('shoe_type')
+            ->getSource()
+            ->getArrOptions();
+
+        $type_data = array();
+        foreach($types as $typ) {
+            $type_data[$typ['value']] = $typ['label'];
+        }
+
+        $manufacturers = Mage::getModel('catalog/product_attribute')
+            ->loadByCode('manufacturer')
+            ->getSource()
+            ->getArrOptions();
+
+        $man_data = array();
+        foreach($manufacturers as $man) {
+            $man_data[$man['value']] = $man['label'];
+        }
+
+        foreach($collection as $product) {
+            $doc = new Zend_Search_Lucene_Document();
+            $doc->addField(Zend_Search_Lucene_Field::Text('url', $product->getProductUrl(), 'utf-8'));
+            $doc->addField(Zend_Search_Lucene_Field::Text('type', 'product', 'utf-8'));
+            $doc->addField(Zend_Search_Lucene_Field::Keyword('name', $product->getName(), 'utf-8'));
+            $doc->addField(Zend_Search_Lucene_Field::Keyword('sku', $product->getSku(), 'utf-8'));
+            if (!empty($man_data[$product->getManufacturer()])) {
+                $doc->addField(Zend_Search_Lucene_Field::Keyword('manufacturer', $man_data[$product->getManufacturer()], 'utf-8'));
+            }
+            if (!empty($type_data[$product->getShoe_type()])) {
+                $doc->addField(Zend_Search_Lucene_Field::Keyword('shoe_type', $type_data[$product->getShoe_type()], 'utf-8'));
+            }
+            $doc->addField(Zend_Search_Lucene_Field::UnStored('description', $product->getDescription(), 'utf-8'));
+            $index->addDocument($doc);
+        }
+    }
+
+
+    function createAction()
+    {
+        $var = Mage::getBaseDir('var');
+        $index_dir = $var . DS . 'search' . DS . 'index';
+        if (!is_dir($index_dir)) {
+            $res = mkdir($index_dir, 0777, true);
+            if (!$res) {
+                Mage::log("Can't create index directory : ". $index_dir, Zend_Log::CRIT);
+                throw  new Exception("Can't create index directory : ". $index_dir);
+            }
+        } else {
+            Mage::log("Index directory " .$index_dir. " is created", Zend_Log::NOTICE);
+        }
+        Zend_Search_Lucene::create($index_dir);
     }
 }// Class IndexController END
