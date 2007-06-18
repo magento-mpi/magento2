8634cb22c3b20c8b961b6409f3ba22f101552cb8
Alex Stadnitskiy <noreply@magento.com>
2007-06-18 20:13:30 +0000

    Log db upgrade fix

--- app/code/core/Mage/Log/sql/log_setup/mysql4-upgrade-0.1.1-0.1.2.sql
+++ app/code/core/Mage/Log/sql/log_setup/mysql4-upgrade-0.1.1-0.1.2.sql
@@ -7,24 +7,22 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
 
 
 
-ALTER TABLE `log_visitor` DROP `url_history`;
+ALTER TABLE `log_visitor` DROP `url_history` ;
 ALTER TABLE `log_visitor` DROP `request_uri`;
 
 ALTER TABLE `log_visitor` ADD `last_url_id` BIGINT UNSIGNED NOT NULL ;
 
 
-ALTER TABLE `log_visitor` 
-    CHANGE `server_addr` `server_addr` BIGINT NULL,
-    CHANGE `remote_addr` `remote_addr` BIGINT NULL 
+ALTER TABLE `log_visitor` CHANGE `server_addr` `server_addr` BIGINT NULL,
+    CHANGE `remote_addr` `remote_addr` BIGINT NULL;
 
-ALTER TABLE `log_visitor` 
-    CHANGE `first_visit_at` `first_visit_at` TIMESTAMP NULL ,
+ALTER TABLE `log_visitor` CHANGE `first_visit_at` `first_visit_at` TIMESTAMP NULL ,
     CHANGE `last_visit_at` `last_visit_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL;
 
 
 CREATE TABLE `log_url_history` (
 `url_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT ,
-`session_id` VARCHAR( 32 ) NOT NULL ,
+`session_id` VARCHAR( 255 ) NOT NULL ,
 `url_value` VARCHAR( 255 ) NOT NULL ,
 INDEX ( `url_id` )
 ) ENGINE = innodb;
