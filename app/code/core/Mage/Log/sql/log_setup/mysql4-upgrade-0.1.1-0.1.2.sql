ae15e5a2678de3c9784fe25993aa2d62fb831c8b
Alex Stadnitskiy <noreply@magento.com>
2007-06-18 20:24:16 +0000

    Log db upgrade fix

--- app/code/core/Mage/Log/sql/log_setup/mysql4-upgrade-0.1.1-0.1.2.sql
+++ app/code/core/Mage/Log/sql/log_setup/mysql4-upgrade-0.1.1-0.1.2.sql
@@ -6,6 +6,27 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
 
 
+DROP TABLE IF EXISTS `log_visitor`;
+CREATE TABLE `log_visitor` (
+  `session_id` varchar(255) NOT NULL default '',
+  `first_visit_at` datetime NOT NULL default '0000-00-00 00:00:00',
+  `last_visit_at` datetime NOT NULL default '0000-00-00 00:00:00',
+  `server_addr` varchar(64) NOT NULL default '',
+  `remote_addr` varchar(64) NOT NULL default '',
+  `http_referer` text,
+  `http_secure` tinyint(4) NOT NULL default '0',
+  `http_host` varchar(255) NOT NULL default '',
+  `http_user_agent` varchar(255) NOT NULL default '',
+  `http_accept_language` varchar(255) NOT NULL default '',
+  `http_accept_charset` varchar(255) NOT NULL default '',
+  `request_uri` text NOT NULL,
+  `website_id` int(10) unsigned NOT NULL default '0',
+  `customer_id` int(10) unsigned NOT NULL default '0',
+  `quote_id` int(10) unsigned NOT NULL default '0',
+  `url_history` text NOT NULL,
+  PRIMARY KEY  (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 ALTER TABLE `log_visitor` DROP `url_history` ;
 ALTER TABLE `log_visitor` DROP `request_uri`;
