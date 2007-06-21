008994155e22c4bb80cba2a3790555391490f9fa
Boris Gurvich <noreply@magento.com>
2007-06-21 23:17:27 +0000

    fixed multi connection to mysql

--- app/code/core/Mage/Core/Model/Resource.php
+++ app/code/core/Mage/Core/Model/Resource.php
@@ -35,17 +35,29 @@ class Mage_Core_Model_Resource
      */
     public function getConnection($name)
     {
-        if (!isset($this->_connections[$name])) {
-            $conn = Mage::getConfig()->getResourceConnectionConfig($name);
-            if ($conn && $conn->is('active', 1)) {
-                $typeObj = $this->getConnectionTypeInstance((string)$conn->type);
-                $this->_connections[$name] = $typeObj->getConnection($conn);
-            }
-            else {
-                return false;
-            }
+        if (isset($this->_connections[$name])) {
+            return $this->_connections[$name];
         }
-        return $this->_connections[$name];
+        
+        $connConfig = Mage::getConfig()->getResourceConnectionConfig($name);
+        if (!$connConfig || !$connConfig->is('active', 1)) {
+            return false;
+        }
+        $origName = $connConfig->getName();
+
+        if (isset($this->_connections[$origName])) {
+            return $this->_connections[$origName];
+        }
+        
+        $typeInstance = $this->getConnectionTypeInstance((string)$connConfig->type);
+        $conn = $typeInstance->getConnection($connConfig);
+        
+        $this->_connections[$name] = $conn;
+        if ($origName!==$name) {
+            $this->_connections[$origName] = $conn;
+        }
+        
+        return $conn;
     }
     
     /**
