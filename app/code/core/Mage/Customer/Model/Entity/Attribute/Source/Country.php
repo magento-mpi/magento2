a4a8f15ab57af6f009e15bd892eec3afc466729b
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-07-03 20:12:14 +0000

    countries icons

--- app/code/core/Mage/Customer/Model/Entity/Attribute/Source/Country.php
+++ app/code/core/Mage/Customer/Model/Entity/Attribute/Source/Country.php
@@ -14,6 +14,9 @@ class Mage_Customer_Model_Entity_Attribute_Source_Country extends Mage_Eav_Model
     {
         if (!$this->_options) {
             $this->_options = Mage::getResourceModel('directory/country_collection')->load()->toOptionArray();
+            foreach ($this->_options as $option) {
+            	$option->setStyle('background-image:url('.Mage::getBaseUrl().'skins/default/images/icons/flags/'.strtolower($option->getTitle()).'.gif)');
+            }
         }
         return $this->_options;
     }
