8f2719916c472b2cfcb644a99dd5f8940fe8e553
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-07-07 10:31:14 +0000

    grid filters

---
+++ app/code/core/Mage/Adminhtml/Block/Widget/Grid/Column/Filter/Abstract.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Grid colum filter block
+ *
+ * @package     Mage
+ * @subpackage  Adminhtml
+ * @copyright   Varien (c) 2007 (http://www.varien.com)
+ * @license     http://www.opensource.org/licenses/osl-3.0.php
+ * @author      Dmitriy Soroka <dmitriy@varien.com>
+ */
+class Mage_Adminhtml_Block_Widget_Grid_Column_Filter_Abstract extends Mage_Core_Block_Abstract implements Mage_Adminhtml_Block_Widget_Grid_Column_Filter_Interface
+{
+    protected $_column;
+    
+    public function setColumn($column)
+    {
+        $this->_column = $column;
+        return $this;
+    }
+    
+    public function getColumn()
+    {
+        return $this->_column;
+    }
+    
+    public function getHtml()
+    {
+        return '';
+    }
+}
