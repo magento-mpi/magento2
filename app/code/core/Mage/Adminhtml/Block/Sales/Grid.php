f0d4627056683cadc5636d509e928ccd92811206
Michael Bessolov <michael.bessolov@magento.com>
2007-07-09 20:03:37 +0000

    orders grid

--- app/code/core/Mage/Adminhtml/Block/Sales/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Grid.php
@@ -14,45 +14,54 @@ class Mage_Adminhtml_Block_Sales_Grid extends Mage_Adminhtml_Block_Widget_Grid
     public function __construct()
     {
         parent::__construct();
-        $this->setId('salesGrid');
+        $this->setId('ordersGrid');
+        $this->setDefaultSort('id');
     }
 
-    protected function _initCollection()
+    protected function _prepareCollection()
     {
         $collection = Mage::getResourceModel('sales/order_collection')
-			->addAttributeSelect('self/created_at')
-			->addAttributeSelect('self/grand_total')
-			->addAttributeSelect('self/created_at')
-			->addAttributeSelect('self/status')
-			->addAttributeSelect('self/currency_code')
-//			->addAttributeSelect('address/address_type')
-			// ->addAttributeFilter('address/address_type', 'shipping')
-			// ->addAttributeSelect('address/firstname')
-			// ->addAttributeSelect('address/lastname')
+            ->addAttributeToSelect('real_order_id')
+            ->addAttributeToSelect('created_at')
+            ->addAttributeToSelect('grand_total')
+            ->addAttributeToSelect('currency_code')
+            ->addAttributeToSelect('status')
+            ->joinAttribute('shipping_entity_id', 'order_address/entity_id', 'parent_id')
+            ->joinAttribute('shipping_address_type', 'order_address/address_type', 'shipping_entity_id')
+            ->addAttributeToFilter('shipping_address_type', 'shipping')
+            ->joinAttribute('shipping_firstname', 'order_address/firstname', 'shipping_entity_id')
+            ->joinAttribute('shipping_lastname', 'order_address/lastname', 'shipping_entity_id')
+            ->joinAttribute('payment_entity_id', 'order_payment/entity_id', 'parent_id')
+            ->joinAttribute('payment_method', 'order_payment/method','payment_entity_id')
         ;
         $this->setCollection($collection);
+        return parent::_prepareCollection();
     }
 
-    protected function _beforeToHtml()
+    protected function _prepareColumns()
     {
+        // Order Number, Date, Shipped To, Total, Status
         $this->addColumn('id', array(
             'header' => __('id'),
             'width' => 5,
             'align' => 'center',
             'sortable' => true,
-            'index' => 'order_id'
+            'index' => 'entity_id'
         ));
-
-        // Order Number, Date, Shipped To, Total, Status
-
         $this->addColumn('created_at', array(
             'header'    => __('Created At'),
             'index'     => 'created_at',
             'type'      => 'date'
         ));
-        $this->addColumn('firstname', array(
+        $this->addColumn('shipped_to', array(
             'header' => __('Shipped To'),
-            'index' => 'firstname',
+            'index' => array('shipping_firstname','shipping_lastname'),
+            'type' => 'concat',
+            'separator' => ' ',
+        ));
+        $this->addColumn('payment_method', array(
+            'header' => __('Payment Method'),
+            'index' => 'payment_method',
         ));
         $this->addColumn('grand_total', array(
             'header' => __('Total'),
@@ -66,12 +75,19 @@ class Mage_Adminhtml_Block_Sales_Grid extends Mage_Adminhtml_Block_Widget_Grid
         $this->addColumn('action', array(
             'header' => __('action'),
             'align' => 'center',
-            'format' => '<a href="'.Mage::getUrl('*/*/edit/id/$order_id').'">'.__('edit').'</a>',
+            'format' => '<a href="'.Mage::getUrl('*/*/edit/id/$entity_id').'">'.__('edit').'</a>',
             'index' => 'order_id',
             'sortable' => false,
+            'filter' => false,
         ));
 
-        $this->_initCollection();
-        return parent::_beforeToHtml();
+        $this->setColumnFilter('id')
+            ->setColumnFilter('created_at')
+            ->setColumnFilter('firstname')
+            ->setColumnFilter('grand_total')
+            ->setColumnFilter('status')
+        ;
+
+        return parent::_prepareColumns();
     }
 }
