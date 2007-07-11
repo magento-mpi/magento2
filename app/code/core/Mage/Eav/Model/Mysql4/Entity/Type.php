137b65ae47189fe4ce528ab9bb2e43339d4324b0
Boris Gurvich <noreply@magento.com>
2007-07-11 16:55:35 +0000

    eav for abstract model

--- app/code/core/Mage/Eav/Model/Mysql4/Entity/Type.php
+++ app/code/core/Mage/Eav/Model/Mysql4/Entity/Type.php
@@ -4,8 +4,7 @@ class Mage_Eav_Model_Mysql4_Entity_Type extends Mage_Core_Model_Resource_Abstrac
 {
     protected function _construct()
     {
-        $this->setResourceModel('eav');
-        $this->setMainTable('entity_type');
+        $this->_init('eav/entity_type');
     }
     
     public function loadByName($object, $name)
