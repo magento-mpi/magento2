92ad03cb946c94e0fc2c0d7d7389917e6f34eef7
Michael Bessolov <michael.bessolov@magento.com>
2007-07-16 01:53:42 +0000

    Mage_Adminhtml_Controller_Action: return $this in _setActiveMenu(), _addBreadcrumb(), _addContent(), _addLeft()

--- app/code/core/Mage/Adminhtml/Controller/Action.php
+++ app/code/core/Mage/Adminhtml/Controller/Action.php
@@ -5,20 +5,24 @@ class Mage_Adminhtml_Controller_Action extends Mage_Core_Controller_Varien_Actio
     protected function _setActiveMenu($menuPath)
     {
         $this->getLayout()->getBlock('menu')->setActive($menuPath);
+        return $this;
     }
-    
+
     protected function _addBreadcrumb($label, $title, $link=null)
     {
         $this->getLayout()->getBlock('breadcrumbs')->addLink($label, $title, $link);
+        return $this;
     }
-    
+
     protected function _addContent(Mage_Core_Block_Abstract $block)
     {
         $this->getLayout()->getBlock('content')->append($block);
+        return $this;
     }
-    
+
     protected function _addLeft(Mage_Core_Block_Abstract $block)
     {
         $this->getLayout()->getBlock('left')->append($block);
+        return $this;
     }
 }
