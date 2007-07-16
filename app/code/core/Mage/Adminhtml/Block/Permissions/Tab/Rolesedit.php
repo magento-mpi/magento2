6b3e4492a79a4795704d69f769289032edf71f81
Alex Stadnitskiy <noreply@magento.com>
2007-07-16 19:04:53 +0000

    Minor changes

--- app/code/core/Mage/Adminhtml/Block/Permissions/Tab/Rolesedit.php
+++ app/code/core/Mage/Adminhtml/Block/Permissions/Tab/Rolesedit.php
@@ -2,30 +2,35 @@
 class Mage_Adminhtml_Block_Permissions_Tab_Rolesedit extends Mage_Adminhtml_Block_Widget_Form {
     public function __construct() {
         parent::__construct();
-        
+
         $rid = Mage::registry('controller')->getRequest()->getParam('rid', false);
-        
+
         $resources = Mage::getModel("permissions/roles")->getResourcesList();
+
         $rules_set = Mage::getResourceModel("permissions/rules_collection")
         				->getByRoles($rid)
-        				->load();        				
-        
+        				->load();
+
         //normalizing resources names and converting values to keys
+
+        /*
         $values = array();
         foreach ($resources as & $resource) {
         	$resource = strtolower($resource);
         	$values[] = array();
-        }        
-        $resources = array_combine($resources, $values);//just simple stupid, but working perfect :)
-        
+        }
+        */
+
+        #$resources = array_combine($resources, $values);//just simple stupid, but working perfect :)
+
         //adding additional data to resources array
         foreach ($rules_set->getItems() as $item) {
         	if (array_key_exists(strtolower($item->getResource_id()), $resources)) {
         		$resources[$item->getResource_id()] = array('privileges' => explode(",", $item->getPrivileges()));
         	}
         }
-        
+
         $this->setTemplate('adminhtml/permissions/rolesedit.phtml')
-        	->assign('resources', $resources);        	
+        	->assign('resources', $resources);
     }
 }
--- app/code/core/Mage/Permissions/Model/Roles.php
+++ app/code/core/Mage/Permissions/Model/Roles.php
@@ -3,43 +3,65 @@ class Mage_Permissions_Model_Roles extends Varien_Object {
 	public function getResource() {
         return Mage::getResourceSingleton('permissions/roles');
     }
-    
+
     public function load($roleId) {
         $this->setData($this->getResource()->load($roleId));
         return $this;
     }
-    
+
     public function save() {
         $this->getResource()->save($this);
-        return $this;        
+        return $this;
     }
-    
+
     public function update() {
         $this->getResource()->update($this);
-        return $this;        
+        return $this;
     }
-    
+
     public function delete() {
         $this->getResource()->delete($this);
         return $this;
     }
-    
+
     public function getCollection() {
         return Mage::getResourceModel('permissions/roles_collection');
     }
-    
+
     public function getResourcesList() {
+        return $this->_buildResourceArray();
+        /*
     	$root = Mage::getBaseDir();
     	$file = $root."/etc/config-compiled.xml";
     	$xml = simplexml_load_string(file_get_contents($file));
-    	
+
     	$resources = array();
     	foreach ($xml->modules->children() as $module) {
     		$resources[] = substr($module->getName(), 5);
     	}
-    	
+
     	sort($resources);
     	return $resources;
+    	*/
+    }
+
+    protected function _buildResourceArray(Varien_Simplexml_Element $parent=null, $path='', $level=0)
+    {
+        static $result;
+
+        if (is_null($parent)) {
+            $parent = Mage::getSingleton('adminhtml/config')->getNode('admin/menu');
+        }
+
+        foreach ($parent->children() as $childName=>$child) {
+            $result[$path.$childName.'/']['name'] = __((string)$child->title);
+            $result[$path.$childName.'/']['level'] = $level;
+            if ($child->children) {
+                $this->_buildResourceArray($child->children, $path.$childName.'/', $level+1);
+            }
+        }
+
+        return $result;
     }
 }
 ?>
