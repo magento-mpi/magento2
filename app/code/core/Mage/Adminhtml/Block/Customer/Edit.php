12798d173d7400fd5134e1a585eac4f78b53470d
Alex Stadnitskiy <noreply@magento.com>
2007-07-18 18:16:44 +0000

    fixed #0000103

--- app/code/core/Mage/Adminhtml/Block/Customer/Edit.php
+++ app/code/core/Mage/Adminhtml/Block/Customer/Edit.php
@@ -8,7 +8,7 @@
  * @license     http://www.opensource.org/licenses/osl-3.0.php
  * @author      Dmitriy Soroka <dmitriy@varien.com>
  */
-class Mage_Adminhtml_Block_Customer_Edit extends Mage_Adminhtml_Block_Widget 
+class Mage_Adminhtml_Block_Customer_Edit extends Mage_Adminhtml_Block_Widget
 {
     public function __construct()
     {
@@ -16,17 +16,33 @@ class Mage_Adminhtml_Block_Customer_Edit extends Mage_Adminhtml_Block_Widget
         $this->setTemplate('adminhtml/customer/edit.phtml');
         $this->setId('customerEdit');
     }
-    
+
     protected function _initChildren()
     {
-        $this->setChild('saveButton', 
+        $this->setChild('backButton',
+            $this->getLayout()->createBlock('adminhtml/widget_button')
+                ->setData(array(
+                    'label'     => __('Back'),
+                    'onclick'   => 'window.location.href=\''.Mage::getUrl('*/*/').'\''
+                ))
+        );
+
+        $this->setChild('cancelButton',
+            $this->getLayout()->createBlock('adminhtml/widget_button')
+                ->setData(array(
+                    'label'     => __('Cancel'),
+                    'onclick'   => 'window.location.href=\''.Mage::getUrl('*/*/').'\''
+                ))
+        );
+
+        $this->setChild('saveButton',
             $this->getLayout()->createBlock('adminhtml/widget_button')
                 ->setData(array(
                     'label'     => __('Save Customer'),
                     'onclick'   => 'customerForm.submit()'
                 ))
         );
-        $this->setChild('deleteButton', 
+        $this->setChild('deleteButton',
             $this->getLayout()->createBlock('adminhtml/widget_button')
                 ->setData(array(
                     'label'     => __('Delete Customer'),
@@ -34,17 +50,27 @@ class Mage_Adminhtml_Block_Customer_Edit extends Mage_Adminhtml_Block_Widget
                 ))
         );
     }
-    
+
+    public function getBackButtonHtml()
+    {
+        return $this->getChildHtml('backButton');
+    }
+
+    public function getCancelButtonHtml()
+    {
+        return $this->getChildHtml('cancelButton');
+    }
+
     public function getSaveButtonHtml()
     {
         return $this->getChildHtml('saveButton');
     }
-    
+
     public function getDeleteButtonHtml()
     {
         return $this->getChildHtml('deleteButton');
     }
-    
+
     public function getSaveUrl()
     {
         return $this->getUrl('*/*/save', array('_current'=>true));
@@ -54,12 +80,12 @@ class Mage_Adminhtml_Block_Customer_Edit extends Mage_Adminhtml_Block_Widget
     {
         return Mage::registry('customer')->getId();
     }
-    
+
     public function getDeleteUrl()
     {
         return $this->getUrl('*/*/delete', array('_current'=>true));
     }
-    
+
     public function getHeader()
     {
         if (Mage::registry('customer')->getId()) {
