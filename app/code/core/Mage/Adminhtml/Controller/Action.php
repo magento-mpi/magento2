3517a57a3570f01e8fda889a2403a9d5bc43ea40
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-07-19 18:05:31 +0000

    global messages display in admin

--- app/code/core/Mage/Adminhtml/Controller/Action.php
+++ app/code/core/Mage/Adminhtml/Controller/Action.php
@@ -25,4 +25,11 @@ class Mage_Adminhtml_Controller_Action extends Mage_Core_Controller_Varien_Actio
         $this->getLayout()->getBlock('left')->append($block);
         return $this;
     }
+    
+    function loadLayout($ids=null, $key='', $generateBlocks=true)
+    {
+        parent::loadLayout($ids, $key, $generateBlocks);
+        $this->_initLayoutMessages('adminhtml/session');
+        return $this;
+    }
 }
