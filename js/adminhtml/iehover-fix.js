fa2e71fbcc2eb64973a07bb441583a9b9def915c
Boris Gurvich <noreply@magento.com>
2007-07-22 06:06:01 +0000

    cleanup css

--- js/adminhtml/iehover-fix.js
+++ js/adminhtml/iehover-fix.js
@@ -1,6 +1,6 @@
 
 ieHover = function() {
-	var ieULs = ($'nav').getElementsByTagName('ul');
+	var ieULs = $('nav').getElementsByTagName('ul');
 	for (j=0; j<ieULs.length; j++) {
 		ieULs[j].innerHTML = ('<iframe src="about:blank" scrolling="no" frameborder="0"></iframe>' + ieULs[j].innerHTML);
 		var ieMat = ieULs[j].firstChild;
