291f6a8b08cf2537bbce664801d633b2ea3808ca
Boris Gurvich <noreply@magento.com>
2007-07-22 17:09:18 +0000

    frontend design skin

--- app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.1-0.5.2.sql
+++ app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.1-0.5.2.sql
@@ -3,9 +3,10 @@
  */
 
 replace into core_config_data (scope, scope_id, path, `value`) values
-('general', 0, 'system/filesystem/design', '{{app_dir}}/design')
-,('general', 0, 'design/package/name', 'default')
-,('general', 0, 'design/package/area', 'frontend')
-,('general', 0, 'design/package/theme', 'default')
-,('general', 0, 'web/url/skin', '{{base_path}}skin/')
+('default', 0, 'system/filesystem/design', '{{app_dir}}/design')
+,('default', 0, 'system/filesystem/skin', '{{base_dir}}/skin')
+,('default', 0, 'design/package/name', 'default')
+,('default', 0, 'design/package/area', 'frontend')
+,('default', 0, 'design/package/theme', 'default')
+,('default', 0, 'web/url/skin', '{{base_path}}skin/')
 ;
