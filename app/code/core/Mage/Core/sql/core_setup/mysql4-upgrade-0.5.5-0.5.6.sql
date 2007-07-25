5df99c8798ac1a19072fb95a5558e16182e9351e
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-07-25 10:57:18 +0000

    catalog DB

---
+++ app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.5-0.5.6.sql
@@ -0,0 +1,53 @@
+/*
+SQLyog Enterprise - MySQL GUI v5.13
+Host - 4.1.22 : Database - magento_dmitriy
+*********************************************************************
+Server version : 4.1.22
+*/
+
+SET NAMES utf8;
+
+SET SQL_MODE='';
+
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+
+/*Table structure for table `core_store` */
+
+DROP TABLE IF EXISTS `core_store`;
+
+CREATE TABLE `core_store` (
+  `store_id` smallint(5) unsigned NOT NULL auto_increment,
+  `code` varchar(32) NOT NULL default '',
+  `language_code` varchar(2) default NULL,
+  `website_id` smallint(5) unsigned default '0',
+  `name` varchar(32) NOT NULL default '',
+  PRIMARY KEY  (`store_id`),
+  UNIQUE KEY `code` (`code`),
+  KEY `FK_STORE_LANGUAGE` (`language_code`),
+  KEY `FK_STORE_WEBSITE` (`website_id`),
+  CONSTRAINT `FK_STORE_LANGUAGE` FOREIGN KEY (`language_code`) REFERENCES `core_language` (`language_code`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `FK_STORE_WEBSITE` FOREIGN KEY (`website_id`) REFERENCES `core_website` (`website_id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores';
+
+/*Data for the table `core_store` */
+
+insert into `core_store` (`store_id`,`code`,`language_code`,`website_id`,`name`) values (0,'default','en',0,'Default'),(1,'base','en',1,'English Store'),(2,'russian','ru',1,'Russian Store'),(3,'gifts','en',2,'Gifts store'),(4,'vipgifts','en',2,'VIP Gifts');
+
+/*Table structure for table `core_website` */
+
+DROP TABLE IF EXISTS `core_website`;
+
+CREATE TABLE `core_website` (
+  `website_id` smallint(5) unsigned NOT NULL auto_increment,
+  `code` varchar(32) NOT NULL default '',
+  `name` varchar(64) NOT NULL default '',
+  PRIMARY KEY  (`website_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Websites';
+
+/*Data for the table `core_website` */
+
+insert into `core_website` (`website_id`,`code`,`name`) values (0,'default','Default'),(1,'base','Main Website'),(2,'gift','Gifts Website');
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
