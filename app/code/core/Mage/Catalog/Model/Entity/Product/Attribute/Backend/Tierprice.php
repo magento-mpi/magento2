eaa7c9d9958b54d00addb01e999be9e306a768c3
Ivan Chepurnyi <noreply@magento.com>
2007-07-25 14:36:34 +0000

    Tier prices

---
+++ app/code/core/Mage/Catalog/Model/Entity/Product/Attribute/Backend/Tierprice.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Catalog product tier price backend attribute model
+ *
+ * @package    Mage
+ * @subpackage Catalog
+ * @copyright  Varien (c) 2007 (http://www.varien.com)
+ * @license    http://www.opensource.org/licenses/osl-3.0.php
+ * @author	   Ivan Chepurnyi <mitch@varien.com>
+ */
+
+class Mage_Catalog_Model_Entity_Product_Attribute_Backend_Tierprice extends Mage_Eav_Model_Entity_Attribute_Backend_Abstract
+{
+	/**
+	 * DB connections list
+	 *
+	 * @var array
+	 */
+	protected $_connections = array();
+		
+	public function getMainTable() 
+	{
+		if (empty($this->_table)) {
+			$this->_table = Mage::getSingleton('core/resource')->getTableName('catalog/product_attribute_tier_price');
+		}
+		
+		return $this->_table;
+	}
+	
+	public function afterLoad($object)
+    {
+    	if ($object->getUseDataSharing()) {
+            $storeId = $object->getData('store_id');
+        } else {
+            $storeId = $object->getStoreId();
+        }
+        
+        $attributeId   = $this->getAttribute()->getId();
+        $entityId	   = $object->getId();
+        $entityIdField = $this->getEntityIdField();
+     	
+        $select = $this->getConnection('read')->select()
+        	->from($this->getMainTable(), array('qty AS price_qty', 'value AS price'))
+        	->where('store_id = ?', $storeId)
+        	->where($entityIdField . ' = ?', $entityId)
+        	->where('attribute_id = ?', $attributeId);
+        	
+        $object->setData($this->getAttribute()->getName(), $this->getConnection('read')->fetchAll($select));
+    }
+    
+    public function beforeSave($object)
+    {
+    
+    }
+    
+    public function afterSave($object) 
+    {
+    	if ($object->getUseDataSharing()) {
+            $storeId = $object->getData('store_id');
+        } else {
+            $storeId = $object->getStoreId();
+        }
+        
+        $attributeId   = $this->getAttribute()->getId();
+        $entityId	   = $object->getId();
+        $entityTypeId  = $object->getTypeId();
+        $entityIdField = $this->getEntityIdField();
+        
+        $connection = $this->getConnection('write');
+        
+    	$condition = array(
+    		$connection->quoteInto('store_id = ?', $storeId),
+    		$connection->quoteInto($entityIdField . ' = ?', $entityId),
+    		$connection->quoteInto('attribute_id = ?', $attributeId)
+    	);
+    	    	 
+    	$connection->delete($this->getMainTable(), $condition);
+    	
+    	$tierPrices = $object->getData($this->getAttribute()->getName());
+    	
+    	if (!is_array($tierPrices)) {
+    		return;
+    	}
+    	
+    	$finalValue = array();
+    	
+    	foreach ($tierPrices as $tierPrice) {
+    		if (!isset($tierPrice['price_qty']) || !isset($tierPrice['price'])) {
+    			continue;
+    		}
+    		
+    		$data = array();
+    		$data[$entityIdField] 	= $entityId;
+    		$data['attribute_id'] 	= $attributeId;
+    		$data['store_id']	  	= $storeId;
+    		$data['qty']		  	= $tierPrice['price_qty'];
+    		$data['value']		  	= $tierPrice['price'];
+    		$data['entity_type_id'] = $entityTypeId;
+    		
+    		$connection->insert($this->getMainTable(), $data);
+    	}
+    }
+    
+    public function afterDelete($object) 
+    {
+    	if ($object->getUseDataSharing()) {
+            $storeId = $object->getData('store_id');
+        } else {
+            $storeId = $object->getStoreId();
+        }
+        
+        $attributeId   = $this->getAttribute()->getId();
+        $entityId	   = $object->getId();
+        $entityTypeId  = $object->getTypeId();
+        $entityIdField = $this->getEntityIdField();
+        
+        $connection = $this->getConnection('write');
+        
+    	$condition = array(
+    		$connection->quoteInto('store_id = ?', $storeId),
+    		$connection->quoteInto($entityIdField . ' = ?', $entityId),
+    		$connection->quoteInto('attribute_id = ?', $attributeId)
+    	);
+    	    	 
+    	$connection->delete($this->getMainTable(), $condition);    	
+    }
+    
+    /**
+     * Return DB connection
+     *
+     * @param	string		$type
+     * @return	Zend_Db_Adapter_Abstract
+     */    
+    public function getConnection($type)
+    {
+    	if (!isset($this->_connections[$type])) {
+    		$this->_connections[$type] = Mage::getSingleton('core/resource')->getConnection('catalog_' . $type);
+    	}
+    	
+    	return $this->_connections[$type];
+    }
+    
+}// Class Mage_Catalog_Model_Entity_Product_Attribute_Backend_Tierprice END
---
+++ app/code/core/Mage/Catalog/Model/Entity/Product/Attribute/Frontend/Tierprice.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Catalog product attribute tier price frontend model
+ *
+ * @package    Mage
+ * @subpackage Catalog
+ * @copyright  Varien (c) 2007 (http://www.varien.com)
+ * @license    http://www.opensource.org/licenses/osl-3.0.php
+ * @author	   Ivan Chepurnyi <mitch@varien.com>
+ */
+
+class Mage_Catalog_Model_Entity_Product_Attribute_Frontend_Tierprice extends Mage_Eav_Model_Entity_Attribute_Frontend_Abstract
+{
+	
+}// Class Mage_Catalog_Model_Entity_Product_Attribute_Frontend_Tierprice END
