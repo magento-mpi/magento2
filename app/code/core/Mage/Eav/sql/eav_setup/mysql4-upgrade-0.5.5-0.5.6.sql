47eb58f03f3cafbffd2fcf32a6005483282bd1d4
Alex Stadnitskiy <noreply@magento.com>
2007-07-27 18:38:31 +0000

    Attributes changes

---
+++ app/code/core/Mage/Eav/sql/eav_setup/mysql4-upgrade-0.5.5-0.5.6.sql
@@ -0,0 +1,19 @@
+/*
+SQLyog Enterprise - MySQL GUI v5.13
+Host - 4.1.22 : Database - magento_dmitriy
+*********************************************************************
+Server version : 4.1.22
+*/
+
+SET NAMES utf8;
+
+SET SQL_MODE='';
+
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+
+ALTER TABLE `eav_attribute` ADD `attribute_code` VARCHAR( 255 ) NOT NULL AFTER `attribute_name`;
+UPDATE `eav_attribute` SET `attribute_code` = `attribute_name` WHERE `attribute_code` = '';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
