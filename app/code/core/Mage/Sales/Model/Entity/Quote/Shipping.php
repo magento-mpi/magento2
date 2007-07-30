25a1a3b995897f28a7ac0cf7f041f6b5b5c7df73
Boris Gurvich <noreply@magento.com>
2007-07-30 17:28:53 +0000

    quotes

--- app/code/core/Mage/Sales/Model/Entity/Quote/Shipping.php
+++ app/code/core/Mage/Sales/Model/Entity/Quote/Shipping.php
@@ -8,12 +8,12 @@
  * @license     http://www.opensource.org/licenses/osl-3.0.php
  * @author      Moshe Gurvich <moshe@varien.com>
  */
-class Mage_Sales_Model_Entity_Quote extends Mage_Eav_Model_Entity_Abstract
+class Mage_Sales_Model_Entity_Quote_Shipping extends Mage_Eav_Model_Entity_Abstract
 {
     public function __construct()
     {
         $resource = Mage::getSingleton('core/resource');
-	    $this->setType('quote')->setConnection(
+	    $this->setType('quote_shipping')->setConnection(
             $resource->getConnection('sales_read'),
             $resource->getConnection('sales_write')
         );
