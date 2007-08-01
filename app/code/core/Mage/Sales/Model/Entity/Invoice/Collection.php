10718d7c97c1470c98061807be0fa74646420a45
Boris Gurvich <noreply@magento.com>
2007-08-01 17:18:41 +0000

    sales models

---
+++ app/code/core/Mage/Sales/Model/Entity/Invoice/Collection.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Invoices collection
+ *
+ * @package    Mage
+ * @subpackage Sales
+ * @copyright  Varien (c) 2007 (http://www.varien.com)
+ * @author     Moshe Gurvich <moshe@varien.com>
+ */
+
+class Mage_Sales_Model_Entity_Invoice_Collection extends Mage_Eav_Model_Entity_Collection_Abstract
+{
+    public function __construct()
+    {
+        $this->setEntity(Mage::getResourceSingleton('sales/invoice'));
+        $this->setObject('sales/invoice');
+    }
+}
