ef18f52007331eef519590b7c7a8f6023d969da7
Boris Gurvich <noreply@magento.com>
2007-08-01 19:36:58 +0000

    sales models

--- app/code/core/Mage/Sales/Model/Entity/Order/Status/Collection.php
+++ app/code/core/Mage/Sales/Model/Entity/Order/Status/Collection.php
@@ -7,7 +7,7 @@
  * @copyright  Varien (c) 2007 (http://www.varien.com)
  * @author      Michael Bessolov <michael@varien.com>
  */
-class Mage_Customer_Model_Entity_Order_Status_Collection extends Mage_Eav_Model_Entity_Collection_Abstract
+class Mage_Sales_Model_Entity_Order_Status_Collection extends Mage_Eav_Model_Entity_Collection_Abstract
 {
     public function __construct()
     {
