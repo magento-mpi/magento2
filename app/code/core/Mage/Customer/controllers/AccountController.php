4fa4c3f38ccbca64b1cf23b9e420c78953295ca0
Dmitriy Vasilenko <dmitriy.vasilenko@magento.com>
2007-08-07 17:45:22 +0000

    small pass error

--- app/code/core/Mage/Customer/controllers/AccountController.php
+++ app/code/core/Mage/Customer/controllers/AccountController.php
@@ -63,17 +63,11 @@ class Mage_Customer_AccountController extends Mage_Core_Controller_Varien_Action
             $login = $this->getRequest()->getPost('login');
             if (!empty($login)) {
                 extract($login);
-                if (!empty($username) && !empty($password)) {
-                    if (strlen($password) < 6)
-                    {
-                        $session->addError('Please enter 6 or more characters.');
+                if (!empty($username) && !empty($password)) {                
+                    if (!$session->login($username, $password)) {
+                        // _('invalid login or password')
+                        $session->addError('Invalid login or password');
                         Mage::getSingleton('customer/session')->setLoginData($username);
-                    } else {
-                        if (!$session->login($username, $password)) {
-                            // _('invalid login or password')
-                            $session->addError('Invalid login or password');
-                            Mage::getSingleton('customer/session')->setLoginData($username);
-                        }
                     }
                 }
             }
