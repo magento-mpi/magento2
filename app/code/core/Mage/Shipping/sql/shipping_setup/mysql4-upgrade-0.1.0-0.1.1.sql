d8d85a3848ea976d396079706e20558a7fcb46cf
Boris Gurvich <noreply@magento.com>
2007-08-08 18:06:08 +0000

    pricerules

--- app/code/core/Mage/Shipping/sql/shipping_setup/mysql4-upgrade-0.1.0-0.1.1.sql
+++ app/code/core/Mage/Shipping/sql/shipping_setup/mysql4-upgrade-0.1.0-0.1.1.sql
@@ -3,5 +3,5 @@ replace into core_config_data (`path`, `value`) values
 ('carriers/dhl/model', 'usa/shipping_carrier_dhl'),
 ('carriers/usps/model', 'usa/shipping_carrier_usps'),
 ('carriers/fedex/model', 'usa/shipping_carrier_fedex'),
-('carriers/pickup/model', 'usa/shipping_carrier_pickup'),
-('carriers/rablerate/model', 'usa/shipping_carrier_tablerate');
+('carriers/pickup/model', 'shipping/carrier_pickup'),
+('carriers/rablerate/model', 'shipping/carrier_tablerate');
