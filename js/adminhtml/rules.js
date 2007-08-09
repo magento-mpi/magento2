14ebf8cd655d4913c2972b8c1e0252e8863c6ecf
Boris Gurvich <noreply@magento.com>
2007-08-09 00:40:46 +0000

    rules

--- js/adminhtml/rules.js
+++ js/adminhtml/rules.js
@@ -5,46 +5,47 @@ VarienRulesForm.prototype = {
         
     	var params = $$('.rule-param'), i, label, elem, cont;
     	for (var i=0; i<params.length; i++) {
-    		cont = params[i];
+    		container = params[i];
+    		container.rulesObject = this;
     		label = Element.down(params[i], '.label');
     		elem = Element.down(params[i], '.element').down();
-    		Event.observe(label, 'click', this.showParamInputField.bind(cont));
-    		Event.observe(elem, 'change', this.hideParamInputField.bind(cont));
-    		Event.observe(elem, 'blur', this.hideParamInputField.bind(cont));
+    		Event.observe(label, 'click', this.showParamInputField.bind(this, container));
+    		Event.observe(elem, 'change', this.hideParamInputField.bind(this, container));
+    		Event.observe(elem, 'blur', this.hideParamInputField.bind(this, container));
     	}
     },
     
-    showParamInputField: function (event) {
-    	Element.addClassName(this, 'rule-param-edit');
-    	Element.down(this, '.element').down().focus();
+    showParamInputField: function (container, event) {
+    	Element.addClassName(container, 'rule-param-edit');
+    	Element.down(container, '.element').down().focus();
     },
     
-    hideParamInputField: function (event) {
-    	Element.removeClassName(this, 'rule-param-edit');
-    	var label = Element.down(this, '.label'), elem;
+    hideParamInputField: function (container, event) {
+    	Element.removeClassName(container, 'rule-param-edit');
+    	var label = Element.down(container, '.label'), elem;
     
-    	if (!this.hasClassName('rule-param-new-child')) {
-        	elem = Element.down(this, 'select');
+    	if (!container.hasClassName('rule-param-new-child')) {
+        	elem = Element.down(container, 'select');
         	if (elem) {
         		label.innerHTML = elem.options[elem.selectedIndex].text;
         	}
     
-        	elem = Element.down(this, 'input.input-text');
+        	elem = Element.down(container, 'input.input-text');
         	if (elem) {
         		label.innerHTML = elem.value;
         	}
     	} else {
-    	    elem = Element.down(this, 'select');
+    	    elem = Element.down(container, 'select');
         	
     	    if (elem.value) {
-            	this.addNewChild(elem);
+    	        this.addRuleNewChild(elem);
     	    }
     
         	elem.value = '';
     	}
     },
     
-    addNewChild: function (event) {
+    addRuleNewChild: function (elem) {
         var parent_id = elem.id.replace(/^.*:(.*):.*$/, '$1');
         var children_ul = $(elem.id.replace(/[^:]*$/, 'children'));
         var children_inputs = Selector.findChildElements(children_ul, $A('input[type=hidden]'));
@@ -60,9 +61,15 @@ VarienRulesForm.prototype = {
         }
         var new_type = elem.value;
         
-        /*Ajax.request(this.newChildUrl, {
-        
-        });*/
+        Ajax.Request(self.newChildUrl, {
+            method: 'get',
+            onSuccess: function (transport) {
+                console.log('success');
+            },
+            onFailure: function (transport) {
+                console.log('failure');
+            }
+        });
         console.log(new_id, new_type);
     }
 }
