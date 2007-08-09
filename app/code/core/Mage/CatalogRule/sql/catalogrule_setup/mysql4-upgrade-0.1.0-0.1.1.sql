d2046721912eead51bbafd0c8326dd4054698bb8
Boris Gurvich <noreply@magento.com>
2007-08-09 10:19:58 +0000

    minor fixes

--- app/code/core/Mage/CatalogRule/sql/catalogrule_setup/mysql4-upgrade-0.1.0-0.1.1.sql
+++ app/code/core/Mage/CatalogRule/sql/catalogrule_setup/mysql4-upgrade-0.1.0-0.1.1.sql
@@ -1,2 +1,4 @@
-alter table `catalogrule` 
-    ,add column `stop_rules_processing` tinyint (1) DEFAULT '1' NOT NULL  after `actions_serialized`;
+alter table `catalogrule`
+    ,add column `stop_rules_processing` tinyint (1) DEFAULT '1' NOT NULL
+    ,MODIFY COLUMN `from_date` DATE ,
+    ,MODIFY COLUMN `to_date` DATE ;
