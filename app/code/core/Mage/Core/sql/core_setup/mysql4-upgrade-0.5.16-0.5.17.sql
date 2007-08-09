2b64b0ec41fe9601607a83c6f4b4024387012a15
Sergei Lysak <noreply@magento.com>
2007-08-09 19:11:43 +0000

    FedEx related DB update

---
+++ app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.16-0.5.17.sql
@@ -0,0 +1,6 @@
+replace  into `core_config_data`(`scope`,`scope_id`,`path`,`value`,`old_value`,`inherit`) values 
+('default',0,'carriers/fedex/active','1','0',0),
+('default',0,'carriers/fedex/gateway_url','https://gateway.fedex.com/GatewayDC','',0),
+('default',0,'carriers/fedex/title','Federal Express','',0),
+('default',0,'carriers/fedex/packaging','YOURPACKAGING','',0),
+('default',0,'carriers/fedex/dropoff','REGULARPICKUP','',0); 
