87491a2953168c9b147f43270583ec04b4cde7b0
Boris Gurvich <noreply@magento.com>
2007-08-13 19:44:33 +0000

    transactional emails config

--- app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.24-0.5.25.php
+++ app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.24-0.5.25.php
@@ -16,11 +16,11 @@ $this->addConfigField('trans_email/ident_support', 'Customer support');
 $this->addConfigField('trans_email/ident_support/name', 'Sender name', array(), 'Customer support');
 $this->addConfigField('trans_email/ident_support/email', 'Sender email', array(), 'support@magento.varien.com');
 
-$this->addConfigField('trans_email/ident_custom1', 'Custom identity 1');
+$this->addConfigField('trans_email/ident_custom1', 'Custom email 1');
 $this->addConfigField('trans_email/ident_custom1/name', 'Sender name', array(), 'Custom 1');
 $this->addConfigField('trans_email/ident_custom1/email', 'Sender email', array(), 'custom1@magento.varien.com');
 
-$this->addConfigField('trans_email/ident_custom2', 'Custom identity 2');
+$this->addConfigField('trans_email/ident_custom2', 'Custom email 2');
 $this->addConfigField('trans_email/ident_custom2/name', 'Sender name', array(), 'Custom 2');
 $this->addConfigField('trans_email/ident_custom2/email', 'Sender email', array(), 'custom2@magento.varien.com');
 
@@ -28,17 +28,17 @@ $identity = array('frontend_type'=>'select', 'source_model'=>'adminhtml/system_c
 $template = array('frontend_type'=>'select', 'source_model'=>'adminhtml/system_config_source_email_template');
 
 $this->addConfigField('trans_email/trans_new_account', 'Transactional email - New Account');
-$this->addConfigField('trans_email/trans_new_account/identity', 'Identity', $identity);
+$this->addConfigField('trans_email/trans_new_account/identity', 'Sender', $identity);
 $this->addConfigField('trans_email/trans_new_account/template', 'Template', $template);
 
 $this->addConfigField('trans_email/trans_new_order', 'Transactional email - New Order');
-$this->addConfigField('trans_email/trans_new_order/identity', 'Identity', $identity);
+$this->addConfigField('trans_email/trans_new_order/identity', 'Sender', $identity);
 $this->addConfigField('trans_email/trans_new_order/template', 'Template', $template);
 
 $this->addConfigField('trans_email/trans_new_password', 'Transactional email - New Password');
-$this->addConfigField('trans_email/trans_new_password/identity', 'Identity', $identity);
+$this->addConfigField('trans_email/trans_new_password/identity', 'Sender', $identity);
 $this->addConfigField('trans_email/trans_new_password/template', 'Template', $template);
 
 $this->addConfigField('trans_email/trans_order_update', 'Transactional email - Order Update');
-$this->addConfigField('trans_email/trans_order_update/identity', 'Identity', $identity);
+$this->addConfigField('trans_email/trans_order_update/identity', 'Sender', $identity);
 $this->addConfigField('trans_email/trans_order_update/template', 'Template', $template);
