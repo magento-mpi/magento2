2e1af3094e315dff2180f18fbb443506ea6e6e31
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-08-17 15:33:03 +0000

    fix tax class dropdown in product admin

--- app/code/core/Mage/Tax/Model/Class/Source/Product.php
+++ app/code/core/Mage/Tax/Model/Class/Source/Product.php
@@ -7,7 +7,10 @@ class Mage_Tax_Model_Class_Source_Product extends Mage_Eav_Model_Entity_Attribut
 		if (!$this->_options) {
 			$this->_options = Mage::getResourceModel('tax/class_collection')
         		->addFieldToFilter('class_type', 'PRODUCT')
-        		->load()->toOptionArray();
+        		->load()
+        		->toOptionArray();
+        		
+            array_unshift($this->_options, array('value'=>'', 'label'=>''));
 		}
 		return $this->_options;
 	}
