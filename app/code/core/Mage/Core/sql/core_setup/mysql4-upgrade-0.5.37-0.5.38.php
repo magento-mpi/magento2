c6116ea44db792fb227c9769f7f24c09f1ee73f0
Boris Gurvich <noreply@magento.com>
2007-08-17 15:32:13 +0000

    minor config field update

--- app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.37-0.5.38.php
+++ app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.37-0.5.38.php
@@ -17,3 +17,10 @@ $this->addConfigField('customer/newsletter', 'Newsletter options');
 $this->addConfigField('customer/newsletter/confirm', 'Request new subscription confirmation', array(
 	'frontend_type'=>'select', 'source_model'=>'adminhtml/system_config_source_yesno'
 ));
+
+$conn->multi_query(<<<EOT
+
+update core_config_field set frontend_type='select', source_model='adminhtml/system_config_source_web_protocol' where path like 'web/%/protocol';
+
+EOT
+);
