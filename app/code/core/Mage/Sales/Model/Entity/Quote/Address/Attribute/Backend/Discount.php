19b716ca8c80074697b9dc17d20daa06ca0a046a
Michael Bessolov <michael.bessolov@magento.com>
2007-08-19 02:16:49 +0000

    get/set storeId fix

--- app/code/core/Mage/Sales/Model/Entity/Quote/Address/Attribute/Backend/Discount.php
+++ app/code/core/Mage/Sales/Model/Entity/Quote/Address/Attribute/Backend/Discount.php
@@ -8,19 +8,19 @@ class Mage_Sales_Model_Entity_Quote_Address_Attribute_Backend_Discount
         $validator = Mage::getModel('salesrule/validator')
         	->setCouponCode($address->getQuote()->getCouponCode())
         	->setCustomerGroupId($address->getQuote()->getCustomerGroupId())
-        	->setStoreId($address->getQuote()->setStoreId());
-        	
+        	->setStoreId($address->getQuote()->getStoreId());
+
         $address->setDiscountAmount(0);
-        
+
         $totalDiscountAmount = 0;
         foreach ($address->getAllItems() as $item) {
         	$validator->process($item);
         	$totalDiscountAmount += $item->getDiscountAmount();
         }
-        
+
         $address->setCouponCode($validator->getConfirmedCouponCode());
         $address->setDiscountAmount($totalDiscountAmount);
-            
+
         $address->setGrandTotal($address->getGrandTotal() - $address->getDiscountAmount());
 
         return $this;
