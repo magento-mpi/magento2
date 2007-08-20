ce6cf4475379634099ff58c71b31b670808a9033
Sergei Lysak <noreply@magento.com>
2007-08-20 11:49:23 +0000

    table rates DB update

---
+++ app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.42-0.5.43.sql
@@ -0,0 +1,3 @@
+replace into `core_config_field`(`level`,`path`,`frontend_label`,`frontend_type`,`frontend_class`,`frontend_model`,`backend_model`,`source_model`,`sort_order`,`show_in_default`,`show_in_website`,`show_in_store`,`module_name`) values 
+(3,'carriers/tablerate/import','Import','import','','','adminhtml/system_config_backend_shipping_tablerate','',6,0,1,0,'');
+
