f8d52c4adb5a6825aea969a823bb66306d49a398
Sergei Lysak <noreply@magento.com>
2007-08-21 16:17:58 +0000

    shipping table rate DB fix

--- app/code/core/Mage/Shipping/sql/shipping_setup/mysql4-upgrade-0.1.2-0.1.3.sql
+++
@@ -1,4 +0,0 @@
-alter table `shipping_tablerate` add column `website_id` int(11) not null after `pk`, drop key `dest_country`, add unique key `dest_country` (`website_id`, `dest_country_id`, `dest_region_id`, `condition_name`, `condition_value`);
-
-alter table `shipping_tablerate` drop key `dest_country`, add unique key `dest_country` (`website_id`, `dest_country_id`, `dest_region_id`, `dest_zip`, `condition_name`, `condition_value`);
-
---
+++ app/code/core/Mage/Shipping/sql/shipping_setup/mysql4-upgrade-0.1.3-0.1.4.sql
@@ -0,0 +1,31 @@
+SET NAMES utf8;
+
+SET SQL_MODE='';
+
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+
+/*Table structure for table `shiptable_data` */
+
+DROP TABLE IF EXISTS `shipping_tablerate`;
+CREATE TABLE `shipping_tablerate` (
+  `pk` int(10) unsigned NOT NULL auto_increment,
+  `website_id` int(11) NOT NULL,
+  `dest_country_id` int(10) NOT NULL default '0',
+  `dest_region_id` int(10) NOT NULL default '0',
+  `dest_zip` varchar(10) NOT NULL default '',
+  `condition_name` varchar(20) NOT NULL default '',
+  `condition_value` decimal(12,4) NOT NULL default '0.0000',
+  `price` decimal(12,4) NOT NULL default '0.0000',
+  `cost` decimal(12,4) NOT NULL default '0.0000',
+  PRIMARY KEY  (`pk`),
+  UNIQUE KEY `dest_country` (`website_id`, `dest_country_id`, `dest_region_id`, `dest_zip`, `condition_name`, `condition_value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `shiptable_data` */
+
+insert into `shipping_tablerate` (`pk`,`dest_country_id`,`dest_region_id`,`dest_zip`,`condition_name`,`condition_value`,`price`,`cost`) values (1,223,1,'','package_weight',100.0000,10.0000,5.0000),(2,223,1,'','package_weight',1000.0000,20.0000,10.0000);
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+
