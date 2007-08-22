d560bfcc6cbfbc5de0cb407df8296c15fbe83123
Alex Stadnitskiy <noreply@magento.com>
2007-08-22 20:57:11 +0000

    fixes #0000248

--- app/code/core/Mage/Wishlist/Block/Customer/Sidebar.php
+++ app/code/core/Mage/Wishlist/Block/Customer/Sidebar.php
@@ -38,18 +38,27 @@ class Mage_Wishlist_Block_Customer_Sidebar extends Mage_Core_Block_Template
 		return $this->_wishlist;
 	}
 
+	public function toHtml()
+	{
+        if( sizeof($this->getWishlistItems()->getItems()) > 0 ){
+        	return parent::toHtml();
+        } else {
+            return '';
+        }
+	}
+
 	public function getCanDisplayWishlist()
 	{
 		return Mage::getSingleton('customer/session')->isLoggedIn();
 	}
-    
+
 	public function getRemoveItemUrl($item)
 	{
 	    return $this->getUrl('wishlist/index/remove',array('item'=>$item->getWishlistItemId()));
 	}
-	
+
 	public function getAddToCartItemUrl($item)
 	{
 	    return $this->getUrl('wishlist/index/cart',array('item'=>$item->getId()));
-	}	
+	}
 }// Class Mage_Wishlist_Block_Customer_Sidebar END
