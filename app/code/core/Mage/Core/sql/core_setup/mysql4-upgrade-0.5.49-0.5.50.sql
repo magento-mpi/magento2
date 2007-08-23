c10e92a48f167df503387963b99ecd1a3a10188a
Sergei Lysak <noreply@magento.com>
2007-08-23 14:12:00 +0000

    last shipping fixes and improvements - mainly js

---
+++ app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.5.49-0.5.50.sql
@@ -0,0 +1,5 @@
+UPDATE `core_config_field` SET `frontend_class` = 'countries' WHERE `path` ='shipping/origin/country_id';
+UPDATE `core_config_field` SET `frontend_class` = 'free-method' WHERE `path` ='carriers/dhl/free_method';
+UPDATE `core_config_field` SET `frontend_class` = 'free-method' WHERE `path` ='carriers/fedex/free_method';
+UPDATE `core_config_field` SET `frontend_class` = 'free-method' WHERE `path` ='carriers/ups/free_method';
+UPDATE `core_config_field` SET `frontend_class` = 'free-method' WHERE `path` ='carriers/usps/free_method';
