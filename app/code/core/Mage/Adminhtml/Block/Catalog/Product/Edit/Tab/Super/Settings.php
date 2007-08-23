8a56a35bd51293dbef7f87bd1860b80ad16666da
Michael Bessolov <michael.bessolov@magento.com>
2007-08-23 16:43:30 +0000

    super -> configurable

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Super/Settings.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Super/Settings.php
@@ -8,11 +8,11 @@
  * @license     http://www.opensource.org/licenses/osl-3.0.php
  * @author      Ivan Chepurnyi <mitch@varien.com>
  */
-class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Settings extends Mage_Adminhtml_Block_Widget_Form 
-{		
+class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Settings extends Mage_Adminhtml_Block_Widget_Form
+{
     protected function _initChildren()
     {
-        $this->setChild('continue_button', 
+        $this->setChild('continue_button',
             $this->getLayout()->createBlock('adminhtml/widget_button')
                 ->setData(array(
                     'label'     => __('Continue'),
@@ -21,20 +21,20 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Settings extends Mage_
 					))
 				);
     }
-    
+
     protected function _prepareForm()
 	{
 		$form = new Varien_Data_Form();
-		$fieldset = $form->addFieldset('settings', array('legend'=>__('Create Super Product Settings')));
-		
+		$fieldset = $form->addFieldset('settings', array('legend'=>__('Select Configurable Attributes ')));
+
 		$entityType = Mage::registry('product')->getResource()->getConfig();
-		
+
 		$fieldset->addField('attributes', 'hidden', array(
 		            'name'  => 'attribute_validate',
 		            'value' => '',
 		            'class'	=> 'validate-super-product-attributes'
 				));
-		
+
 		$product = Mage::registry('product');
 		$attributes = $product->getAttributes();
 		foreach($attributes as $attribute) {
@@ -48,16 +48,16 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Settings extends Mage_
 				));
 			}
 		}
-		
-		
-				
+
+
+
 		$fieldset->addField('continue_button', 'note', array(
             'text' => $this->getChildHtml('continue_button'),
 		));
-		
+
 		$this->setForm($form);
 	}
-	
+
 	public function getContinueUrl()
 	{
 	    return Mage::getUrl('*/*/new', array('_current'=>true));
--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tabs.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tabs.php
@@ -123,7 +123,7 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tabs extends Mage_Adminhtml_Bloc
         }
         elseif ($setId) {
         	$this->addTab('super_settings', array(
-                'label'     => __('Super Product Settings'),
+                'label'     => __('Configurable Product Settings'),
                 'content'   => $this->getLayout()->createBlock('adminhtml/catalog_product_edit_tab_super_settings')->toHtml(),
                 'active'    => true
             ));
