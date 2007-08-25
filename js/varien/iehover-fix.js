fd5bd3a33f84832a0e854234b73ca68ab3be0fbb
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-08-25 21:31:07 +0000

    fix js error in compare list

--- js/varien/iehover-fix.js
+++ js/varien/iehover-fix.js
@@ -1,15 +1,18 @@
 
 ieHover = function() {
-	var ieULs = $('nav').getElementsByTagName('ul'), iframe, li;
-	for (var j=0; j<ieULs.length; j++) {
-		iframe = document.createElement('IFRAME');
-		iframe.src = "about:blank";
-		iframe.scrolling = 'no';
-		iframe.frameBorder = 0;
-		iframe.style.width = ieULs[j].offsetWidth+"px";
-		iframe.style.height = ieULs[j].offsetHeight+"px";
-		ieULs[j].insertBefore(iframe, ieULs[j].firstChild);
-		ieULs[j].style.zIndex="1";
+	var ieULs, iframe, li;
+	if($('nav')){
+	    ieULs = $('nav').getElementsByTagName('ul');
+    	for (var j=0; j<ieULs.length; j++) {
+    		iframe = document.createElement('IFRAME');
+    		iframe.src = "about:blank";
+    		iframe.scrolling = 'no';
+    		iframe.frameBorder = 0;
+    		iframe.style.width = ieULs[j].offsetWidth+"px";
+    		iframe.style.height = ieULs[j].offsetHeight+"px";
+    		ieULs[j].insertBefore(iframe, ieULs[j].firstChild);
+    		ieULs[j].style.zIndex="1";
+    	}
 	}
 }
 
