76157c7ea28a3c5a5f3cfcbfe333e6e6ff49d45f
Alex Stadnitskiy <noreply@magento.com>
2007-08-27 13:58:26 +0000

    fixes #0000547

--- js/prototype/validation.js
+++ js/prototype/validation.js
@@ -311,6 +311,9 @@ Validation.addAllThese([
     ['validate-zip', 'Please enter a valid zip code. For example 90602 or 90602-1234.', function(v) {
             return Validation.get('IsEmpty').test(v) || /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(v);
             }],
+    ['validate-zip-international', 'Please enter a valid zip code.', function(v) {
+            return Validation.get('IsEmpty').test(v) || /(^[A-z0-9]{2,10}([\s]{0,1}|[\-]{0,1})[A-z0-9]{2,10}$)/.test(v);
+            }],
     ['validate-date-au', 'Please use this date format: dd/mm/yyyy. For example 17/03/2006 for the 17th of March, 2006.', function(v) {
                 if(Validation.get('IsEmpty').test(v)) return true;
                 var regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
