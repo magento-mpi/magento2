0b56c4f16056f60800b97e8cacb36feb48fd6146
Boris Gurvich <noreply@magento.com>
2007-08-27 01:25:54 +0000

    wishlist sql updates

--- app/code/core/Mage/Wishlist/sql/wishlist_setup/mysql4-upgrade-0.5.1-0.5.2.php
+++ app/code/core/Mage/Wishlist/sql/wishlist_setup/mysql4-upgrade-0.5.1-0.5.2.php
@@ -1,4 +1,5 @@
 <?php
+
 $this->addConfigField('wishlist/email', 'Share options');
 
 $this->addConfigField('wishlist/email/email_identity', 'Email Sender', array(
@@ -13,7 +14,11 @@ $this->addConfigField('wishlist/email/email_template', 'Email Template', array(
 
 $this->run(
 <<<EOT
+DELETE FROM `core_email_template` WHERE `template_code`='Share Wishlist';
 INSERT INTO `core_email_template` (`template_id`, `template_code`, `template_text`, `template_type`, `template_subject`, `template_sender_name`, `template_sender_email`, `added_at`, `modified_at`) VALUES 
 (NULL, 'Share Wishlist', '{{var message}}<br>\r\n\r\n{{var items}}<br>\r\n\r\n<a href="{{var addAllLink}}">Add all items to cart</a><br>\r\n<a href="{{var viewOnSiteLink}}">View this items on site</a>', 2, 'Share Wishlist Subject', NULL, NULL, '2007-08-25 19:27:49', '2007-08-26 15:58:43');
 EOT
 );
+
+$template = $conn->raw_fetchRow("select template_id from `core_email_template` where template_code='Share Wishlist'");
+$this->setConfigData('wishlist/email/email_template', $template['template_id']);
