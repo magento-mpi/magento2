159a7ab36fbde891da92e30d2c7ebf72cfa555ef
Boris Gurvich <noreply@magento.com>
2007-08-28 23:06:02 +0000

    category image swap

---
+++ fr/index.php
@@ -0,0 +1,8 @@
+<?php
+if (version_compare(phpversion(), '5.2.0', '<')===true) {
+	echo "<h1>Invalid PHP version</h1><p>Magento supports only PHP 5.2.0 or newer.</p>";
+	exit;
+}
+
+require '../app/Mage.php';
+Mage::run('french');
