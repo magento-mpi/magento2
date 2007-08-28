e865fc387bdb13cc82c5830baeb81416489b17e3
Boris Gurvich <noreply@magento.com>
2007-08-28 01:44:00 +0000

    directory: address format is same for all countries

---
+++ app/code/core/Mage/Directory/sql/directory_setup/mysql4-upgrade-0.5.7-0.5.8.sql
@@ -0,0 +1,9 @@
+update directory_country set address_template_plain='{{firstname}} {{lastname}}
+{{company}}
+{{street1}}
+{{street2}}
+{{city}}, {{region}} {{postcode}}', address_template_html='{{firstname}} {{lastname}}<br/>
+{{company}}<br/>
+{{street}}<br/>
+{{city}}, {{region}} {{postcode}}<br/>
+T: {{telephone}}';
