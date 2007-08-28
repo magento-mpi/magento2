0722d89093fbcd50b93d33ea577c52003fd0bb92
Boris Gurvich <noreply@magento.com>
2007-08-28 23:53:45 +0000

    virtual blocks includes

--- app/code/core/Mage/Core/Model/Email/Template/Filter.php
+++ app/code/core/Mage/Core/Model/Email/Template/Filter.php
@@ -1,45 +1,62 @@
 <?php
 
-class Mage_Core_Model_Email_Template_Filter extends Varien_Filter_Template 
+class Mage_Core_Model_Email_Template_Filter extends Varien_Filter_Template
 {
 	public function blockDirective($construction)
 	{
 		$blockParameters = $this->_getIncludeParameters($construction[2]);
-		
-		$type = $blockParameters['type'];
-		
-		$block = Mage::registry('action')->getLayout()->createBlock($type);
-		
-		if (!empty($blockParameters['template'])) {
-			$block->setTemplate($blockParameters['template']);
+		$layout = Mage::registry('action')->getLayout();
+
+		if (isset($blockParameters['type'])) {
+    		$type = $blockParameters['type'];
+
+    		$block = $layout->createBlock($type);
+    		if (!$block) {
+    		    return '';
+    		}
+    		if (!empty($blockParameters['template'])) {
+    			$block->setTemplate($blockParameters['template']);
+    		}
+
+    		$block->addData($blockParameters);
+
+    		return $block->toHtml();
 		}
-		
-		if (!$block) {
-			return '';
+
+		if (isset($blockParameters['id'])) {
+		    $block = $layout->createBlock('cms/block');
+
+		    if (!$block) {
+		        return '';
+		    }
+
+		    $block
+		        ->setBlockId($blockParameters['id'])
+		        ->setBlockParams($blockParameters);
+
+		    return $block->toHtml();
 		}
-		
-		$block->addData($blockParameters);
-		
-		return $block->toHtml();
+
+        return '';
 	}
-	
+
 	protected function _getBlockParameters($value)
 	{
         $tokenizer = new Varien_Filter_Template_Tokenizer_Parameter();
         $tokenizer->setString($value);
-        
+
         return $tokenizer->tokenize();
 	}
 
 	public function skinDirective($construction)
 	{
 		$params = $this->_getIncludeParameters($construction[2]);
-		
+
 		$url = Mage::getDesign()->getSkinUrl($params['url']);
-		
+
 		return $url;
 	}
-	
+
 	public function storeDirective($construction)
 	{
     	$params = $this->_getIncludeParameters($construction[2]);
@@ -48,7 +65,7 @@ class Mage_Core_Model_Email_Template_Filter extends Varien_Filter_Template
     	unset($params['url']);
 
     	$url = Mage::getUrl($path, $params);
-		
+
     	return $url;
 	}
 
