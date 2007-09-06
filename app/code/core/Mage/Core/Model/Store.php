ae8b364093cd9bb7f5841fe798c9b6e81bb9578a
Boris Gurvich <noreply@magento.com>
2007-09-06 18:50:18 +0000

    fixed ://:/install bug

--- app/code/core/Mage/Core/Model/Store.php
+++ app/code/core/Mage/Core/Model/Store.php
@@ -232,7 +232,7 @@ class Mage_Core_Model_Store extends Mage_Core_Model_Abstract
         }
 
         $url = $protocol.'://'.$host;
-        $url .= ('http'===$protocol && 80==$port || 'https'===$protocol && 443==$port) ? '' : ':'.$port;
+        $url .= ('http'===$protocol && 80==$port || 'https'===$protocol && 443==$port) ? '' : ($port ? ':'.$port : '');
 
         return $url;
     }
@@ -244,17 +244,13 @@ class Mage_Core_Model_Store extends Mage_Core_Model_Abstract
         if (false!==$config) {
             if (empty($params['_type'])) {
                 $basePath = $config['base_path'];
-    #echo '1: '.$basePath.'<hr>';
             } else {
                 $basePath = $this->getConfig('web/url/'.$params['_type']);
-    #echo '2: '.$basePath.'<hr>';
             }
         } else {
             $basePath = dirname($_SERVER['SCRIPT_NAME']).'/';
         }
 
-        #$basePath = preg_replace('#/([a-z0-9_-]+)/../#', '/', $basePath); // fix this
-#print_r($basePath);
         return empty($basePath) ? '/' : $basePath;
     }
 
