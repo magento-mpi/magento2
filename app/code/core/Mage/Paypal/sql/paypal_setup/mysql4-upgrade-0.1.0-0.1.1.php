bca9ca1b09fda36251c44cd8fce9fefc3d3f2fdb
Boris Gurvich <noreply@magento.com>
2007-09-17 17:51:53 +0000

    paypal cleanup

--- app/code/core/Mage/Paypal/sql/paypal_setup/mysql4-upgrade-0.1.0-0.1.1.php
+++ app/code/core/Mage/Paypal/sql/paypal_setup/mysql4-upgrade-0.1.0-0.1.1.php
@@ -1,5 +1,6 @@
 <?php
 
 $this->run("
-delete from `core_config_data` where path like 'payment/paypal/%';
+delete from `core_config_field` where `path` like 'payment/paypal/%';
+delete from `core_config_data` where `path` like 'payment/paypal/%';
 ");
