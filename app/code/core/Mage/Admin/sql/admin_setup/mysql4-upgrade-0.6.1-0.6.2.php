efe280cae519611b22dff1e1bf9a5f5e9f52a4b5
Magento Core <noreply@magento.com>
2007-09-26 21:42:41 +0000

    bugfix for old acl resourcess

---
+++ app/code/core/Mage/Admin/sql/admin_setup/mysql4-upgrade-0.6.1-0.6.2.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category   Mage
+ * @package    Mage_Admin
+ * @copyright  Copyright (c) 2004-2007 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+$this->startSetup()->run(" 
+
+DELETE FROM `admin_rule`;
+INSERT INTO `admin_rule` (`rule_id`, `role_id`, `resource_id`, `privileges`, `assert_id`, `role_type`, `permission`) VALUES (1,1,'admin','',0,'G','allow');
+
+")->endSetup();
