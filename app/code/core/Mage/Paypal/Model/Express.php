82a906dbf22d034b188af999feda70b0acf341e6
Boris Gurvich <noreply@magento.com>
2007-10-02 00:38:22 +0000

    fixed paypal express mark

--- app/code/core/Mage/Paypal/Model/Express.php
+++ app/code/core/Mage/Paypal/Model/Express.php
@@ -106,9 +106,9 @@ class Mage_Paypal_Model_Express extends Mage_Paypal_Model_Abstract
 
     public function returnFromPaypal()
     {
+        $this->_getExpressCheckoutDetails();
         switch ($this->getApi()->getUserAction()) {
             case Mage_Paypal_Model_Api_Nvp::USER_ACTION_CONTINUE:
-                $this->_getExpressCheckoutDetails();
                 $this->getApi()->setRedirectUrl(Mage::getUrl('paypal/express/review'));
                 break;
 
