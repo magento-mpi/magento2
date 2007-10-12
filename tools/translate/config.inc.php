9e69ff664022919faaf7ff294a4444c37e78984e
Michael Bessolov <michael.bessolov@magento.com>
2007-10-12 14:09:02 +0000

    Completed translation tool config

--- tools/translate/config.inc.php
+++ tools/translate/config.inc.php
@@ -11,10 +11,140 @@ $CONFIG['translates'] = array(
         'app/design/adminhtml/default/default/template/',
     ),
 
+    'Mage_Backup' => array(
+        'app/code/core/Mage/Backup/',
+    ),
+
     'Mage_Catalog' => array(
         'app/code/core/Mage/Catalog/',
         'app/design/frontend/default/default/template/catalog/',
-        '',
+    ),
+
+    'Mage_CatalogRule' => array(
+        'app/code/core/Mage/CatalogRule/',
+    ),
+
+    'Mage_CatalogSearch' => array(
+        'app/code/core/Mage/CatalogSearch/',
+        'app/design/frontend/default/default/template/catalogsearch/',
+    ),
+
+    'Mage_Checkout' => array(
+        'app/code/core/Mage/Checkout/',
+        'app/design/frontend/default/default/template/checkout/',
+    ),
+
+    'Mage_Cms' => array(
+        'app/code/core/Mage/Cms/',
+        'app/design/frontend/default/default/template/cms/',
+    ),
+
+    'Mage_Core' => array(
+        'app/code/core/Mage/Core/',
+        'app/design/frontend/default/default/template/core/',
+    ),
+
+    'Mage_Customer' => array(
+        'app/code/core/Mage/Customer/',
+        'app/design/frontend/default/default/template/customer/',
+    ),
+
+    'Mage_Datafeed' => array(
+        'app/code/core/Mage/Datafeed/',
+        'app/design/frontend/default/default/template/datafeed/',
+    ),
+
+    'Mage_Directory' => array(
+        'app/code/core/Mage/Directory/',
+        'app/design/frontend/default/default/template/directory/',
+    ),
+
+    'Mage_Eav' => array(
+        'app/code/core/Mage/Eav/',
+    ),
+
+    'Mage_Giftcert' => array(
+        'app/code/core/Mage/Giftcert/',
+    ),
+
+    'Mage_GoogleCheckout' => array(
+        'app/code/core/Mage/GoogleCheckout/',
+    ),
+
+    'Mage_Install' => array(
+        'app/code/core/Mage/Install/',
+        'app/design/install/default/default/template/',
+    ),
+
+    'Mage_Newsletter' => array(
+        'app/code/core/Mage/Newsletter/',
+        'app/design/frontend/default/default/template/newsletter/',
+    ),
+
+    'Mage_Page' => array(
+        'app/code/core/Mage/Page/',
+        'app/design/frontend/default/default/template/page/',
+    ),
+
+    'Mage_Payment' => array(
+        'app/code/core/Mage/Payment/',
+        'app/design/frontend/default/default/template/payment/',
+    ),
+
+    'Mage_Paypal' => array(
+        'app/code/core/Mage/Paypal/',
+        'app/design/frontend/default/default/template/paypal/',
+    ),
+
+    'Mage_Poll' => array(
+        'app/code/core/Mage/Poll/',
+        'app/design/frontend/default/default/template/poll/',
+    ),
+
+    'Mage_Rating' => array(
+        'app/code/core/Mage/Rating/',
+        'app/design/frontend/default/default/template/rating/',
+    ),
+
+    'Mage_Review' => array(
+        'app/code/core/Mage/Review/',
+        'app/design/frontend/default/default/template/review/',
+    ),
+
+    'Mage_Rule' => array(
+        'app/code/core/Mage/Rule/',
+    ),
+
+    'Mage_Sales' => array(
+        'app/code/core/Mage/Sales/',
+        'app/design/frontend/default/default/template/sales/',
+    ),
+
+    'Mage_SearchLucene' => array(
+        'app/code/core/Mage/SearchLucene/',
+        'app/design/frontend/default/default/template/searchlucene/',
+    ),
+
+    'Mage_Shipping' => array(
+        'app/code/core/Mage/Shipping/',
+    ),
+
+    'Mage_Tag' => array(
+        'app/code/core/Mage/Tag/',
+        'app/design/frontend/default/default/template/tag/',
+    ),
+
+    'Mage_Tax' => array(
+        'app/code/core/Mage/Tax/',
+    ),
+
+    'Mage_Usa' => array(
+        'app/code/core/Mage/Usa/',
+    ),
+
+    'Mage_Wishlist' => array(
+        'app/code/core/Mage/Wishlist/',
+        'app/design/frontend/default/default/template/wishlist/',
     ),
 
 );
