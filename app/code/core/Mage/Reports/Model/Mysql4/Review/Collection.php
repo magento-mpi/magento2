95c95956d56b3c91986cd3de516edbb4a73b1715
Michael Bessolov <michael.bessolov@magento.com>
2007-10-12 22:05:35 +0000

    Products reviews report fix

--- app/code/core/Mage/Reports/Model/Mysql4/Review/Collection.php
+++ app/code/core/Mage/Reports/Model/Mysql4/Review/Collection.php
@@ -25,29 +25,29 @@
  * @package    Mage_Reports
  * @author     Dmytro Vasylenko  <dimav@varien.com>
  */
- 
+
 class Mage_Reports_Model_Mysql4_Review_Collection extends Mage_Review_Model_Mysql4_Review_Collection
 {
     protected function _construct()
     {
         $this->_init('review/review');
     }
-    
+
     public function addProductFilter($productId)
     {
         $this->_sqlSelect
-            ->where($this->_reviewTable.'.entity_pk_value = ?', $productId);
-            
+            ->where('main_table.entity_pk_value = ?', $productId);
+
         return $this;
     }
-        
+
     public function resetSelect()
     {
         parent::resetSelect();
         $this->_joinFields();
         return $this;
     }
-    
+
     public function getSelectCountSql()
     {
         $countSelect = clone $this->_sqlSelect;
@@ -56,12 +56,12 @@ class Mage_Reports_Model_Mysql4_Review_Collection extends Mage_Review_Model_Mysq
         $countSelect->reset(Zend_Db_Select::LIMIT_OFFSET);
 
         $sql = $countSelect->__toString();
-        
-        $sql = preg_replace('/^select\s+.+?\s+from\s+/is', 'select count('.$this->_reviewTable.'.review_id) from ', $sql);
+
+        $sql = preg_replace('/^select\s+.+?\s+from\s+/is', 'select count(main_table.review_id) from ', $sql);
 
         return $sql;
     }
-    
+
     public function setOrder($attribute, $dir='desc')
     {
         $fields = array(
@@ -70,15 +70,15 @@ class Mage_Reports_Model_Mysql4_Review_Collection extends Mage_Review_Model_Mysq
             'detail',
             'created_at'
         );
-        
+
         if (in_array($attribute, $fields)) {
                 $this->_sqlSelect->order($attribute . ' ' . $dir);
         } else {
-                parent::setOrder($attribute, $dir);    
+                parent::setOrder($attribute, $dir);
         }
-        
+
         return $this;
     }
-    
+
 }
 ?>
