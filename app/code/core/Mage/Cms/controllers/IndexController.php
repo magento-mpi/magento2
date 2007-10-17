b0cf4b53b755ce0bfcc7138f3d882bebe4402010
Boris Gurvich <noreply@magento.com>
2007-10-17 22:51:09 +0000

    added 404 http status for not found page

--- app/code/core/Mage/Cms/controllers/IndexController.php
+++ app/code/core/Mage/Cms/controllers/IndexController.php
@@ -30,6 +30,10 @@ class Mage_Cms_IndexController extends Mage_Core_Controller_Front_Action
 
     public function noRouteAction($coreRoute = null)
     {
+        #header('HTTP/1.1 404 Not Found');
+        $this->getResponse()->setHeader('HTTP/1.1','404 Not Found');
+        $this->getResponse()->setHeader('Status','404 File not found');
+        
         $pageId = Mage::getStoreConfig('web/default/cms_no_route');
         if (!Mage::helper('cms/page')->renderPage($this, $pageId)) {
             $this->_forward('noRoute');   
