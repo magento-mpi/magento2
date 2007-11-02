76b1910a478ceb5ff4a3c692beaa2bb1a77d7c1d
Boris Gurvich <noreply@magento.com>
2007-11-02 20:31:22 +0000

    import/export db updates

--- app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.6.22-0.6.23.php
+++ app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.6.22-0.6.23.php
@@ -2,6 +2,18 @@
 
 $this->startSetup()->run("
 
-alter table core_convert_profile add column gui_data text;
+drop table if exists `core_convert_profile`;
+CREATE TABLE `core_convert_profile` (
+  `profile_id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL default '',
+  `created_at` datetime NOT NULL default '0000-00-00 00:00:00',
+  `updated_at` datetime NOT NULL default '0000-00-00 00:00:00',
+  `actions_xml` text,
+  `gui_data` text,
+  `direction` enum('import','export') default NULL,
+  `entity_type` varchar(64) NOT NULL default '',
+  `store_id` smallint(5) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`profile_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ")->endSetup();
