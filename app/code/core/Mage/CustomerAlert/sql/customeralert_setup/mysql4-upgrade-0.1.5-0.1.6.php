2ac8aa975ebbb253f8309087519d1666acb74ee7
Magento Core <noreply@magento.com>
2007-12-04 00:16:14 +0000

    customer product alert

---
+++ app/code/core/Mage/CustomerAlert/sql/customeralert_setup/mysql4-upgrade-0.1.5-0.1.6.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category   Mage
+ * @package    Mage_Core
+ * @copyright  Copyright (c) 2004-2007 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+$this->run("
+    ALTER TABLE `customer_product_alert_check` ADD status INT(2) UNSIGNED NOT NULL DEFAULT '0';
+    Update `core_email_template` Set template_type='2' Where template_code='Product alert for back in stock';
+    Update `core_email_template` Set template_type='2' Where template_code='Product alert for changed price';
+");
+?>
