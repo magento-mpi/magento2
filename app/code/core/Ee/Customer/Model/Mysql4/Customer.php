e76ec98dcf1f9533614a049dfabdcd4eac8fcc4f
Alex Stadnitskiy <noreply@magento.com>
2007-12-11 18:26:28 +0000

    ee

--- app/code/core/Ee/Customer/Model/Mysql4/Customer.php
+++ app/code/core/Ee/Customer/Model/Mysql4/Customer.php
@@ -17,19 +17,73 @@
  * @copyright  Copyright (c) 2004-2007 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
- 
+
 /**
  * Ee customer resource model
  *
  * @category   Ee
  * @package    Ee_Customer
- * @author     Dmitriy Soroka <dmitriy@varien.com> 
+ * @author     Dmitriy Soroka <dmitriy@varien.com>
  */
 
-class Ee_Customer_Model_Mysql4_Customer
+class Ee_Customer_Model_Mysql4_Customer extends Mage_Core_Model_Mysql4_Abstract
 {
-    public function __construct() 
+    protected $_memberTable;
+
+    public function _construct()
     {
-        
+        $this->_init('ee_customer/members', 'member_id');
+
+        $resource = Mage::getSingleton('core/resource');
+        $this->_memberTable = $resource->getTableName('customer/members');
     }
+
+    public function getEntityIdField()
+    {
+        return 'member_id';
+    }
+
+    public function loadByEmail(Mage_Customer_Model_Customer $customer, $email, $testOnly=false)
+    {
+        $read = $this->_getReadAdapter();
+        $select = $read->select()
+            ->from($this->_memberTable)
+            ->where($read->quoteInto('email = ?', $email));
+
+        $customer->setData($read->fetchRow($select));
+        return $this;
+    }
+
+    public function loadByUsername(Mage_Customer_Model_Customer $customer, $username, $testOnly=false)
+    {
+        $read = $this->_getReadAdapter();
+        $select = $read->select()
+            ->from($this->_memberTable)
+            ->where($read->quoteInto('username = ?', $username));
+
+        $customer->setData($read->fetchRow($select));
+        return $this;
+    }
+
+    public function authenticate(Mage_Customer_Model_Customer $customer, $login, $password)
+    {
+        $this->loadByUsername($customer, $login);
+        echo "<pre>DEBUG:\n";
+        print_r($customer);
+        echo "</pre>";
+        die();
+    }
+
+    public function changePassword(Mage_Customer_Model_Customer $customer, $newPassword, $checkCurrent=true)
+    {
+        $customer->setPassword($newPassword);
+        # FIXME;
+        return $this;
+    }
+
+/*    public function __call($m, $a)
+    {
+        print "Method $m called:\n";
+        var_dump($a);
+    }*/
 }
