f0b68aef3a5e3adb42e721169d1dbb1816117509
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-12-20 12:37:29 +0000

    small changes with translate

--- app/code/core/Mage/Adminhtml/controllers/Catalog/Product/Action/AttributeController.php
+++ app/code/core/Mage/Adminhtml/controllers/Catalog/Product/Action/AttributeController.php
@@ -27,22 +27,27 @@
  */
 class Mage_Adminhtml_Catalog_Product_Action_AttributeController extends Mage_Adminhtml_Controller_Action
 {
+    protected function _construct()
+    {
+        $this->setUsedModuleName('Mage_Catalog');
+    }
+    
     public function editAction()
     {
         if(!is_array($this->_getHelper()->getProductIds())) {
-            $this->_getSession()->addError($this->_getCatalogHelper()->__('Please select products for attributes update'));
+            $this->_getSession()->addError($this->__('Please select products for attributes update'));
             $this->_redirect('*/catalog_product/index', array('_current'=>true));
             return;
         }
 
         if($countNotInStore = count($this->_getHelper()->getProductsNotInStoreIds())) {
-            $this->_getSession()->addWarning($this->_getCatalogHelper()->__('There is %d product(s) that will be not updated for selected store', $countNotInStore));
+            $this->_getSession()->addWarning($this->__('There is %d product(s) that will be not updated for selected store', $countNotInStore));
         }
 
         $this->loadLayout();
         $this->_addLeft(
                 $this->getLayout()->createBlock('adminhtml/store_switcher')
-                    ->setDefaultStoreName($this->_getCatalogHelper()->__('Default Values'))
+                    ->setDefaultStoreName($this->__('Default Values'))
                     ->setSwitchUrl(Mage::getUrl('*/*/*', array('_current'=>true, 'store'=>null)))
         );
         $this->_addLeft($this->getLayout()->createBlock('adminhtml/catalog_product_edit_action_attribute_tabs', 'attributes_tabs'));
@@ -53,7 +58,7 @@ class Mage_Adminhtml_Catalog_Product_Action_AttributeController extends Mage_Adm
     public function saveAction()
     {
         if(!is_array($this->_getHelper()->getProductIds())) {
-            $this->_getSession()->addError($this->_getCatalogHelper()->__('Please select products for attributes update'));
+            $this->_getSession()->addError($this->__('Please select products for attributes update'));
             $this->_redirect('*/catalog_product/index', array('_current'=>true));
             return;
         }
@@ -86,14 +91,14 @@ class Mage_Adminhtml_Catalog_Product_Action_AttributeController extends Mage_Adm
                 $product->save();
             }
 
-            $this->_getSession()->addSuccess($this->_getCatalogHelper()->__('Attributes of %d product(s) has been successfully updated',
+            $this->_getSession()->addSuccess($this->__('Attributes of %d product(s) has been successfully updated',
                                              count($this->_getHelper()->getProducts())-count($productsNotInStore)));
         }
         catch (Mage_Core_Exception $e) {
             $this->_getSession()->addError($e->getMessage());
         }
         catch (Exception $e) {
-            $this->_getSession()->addError($this->_getCatalogHelper()->__('There was an error while updating product(s) attributes'));
+            $this->_getSession()->addError($this->__('There was an error while updating product(s) attributes'));
         }
 
         $this->_redirect('*/catalog_product/', array('store'=>$this->getRequest()->getParam('store', 0)));
