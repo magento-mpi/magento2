707fceeb43350cae407b6e6ce5d426e8c6d0e14e
Ivan Chepurnyi <noreply@magento.com>
2007-12-20 20:12:38 +0000

    Gift messages fix

--- app/code/core/Mage/GiftMessage/Model/Entity/Attribute/Backend/Boolean/Config.php
+++ app/code/core/Mage/GiftMessage/Model/Entity/Attribute/Backend/Boolean/Config.php
@@ -29,8 +29,16 @@ class Mage_GiftMessage_Model_Entity_Attribute_Backend_Boolean_Config extends Mag
 {
     public function afterLoad($object)
     {
-    	if($object->getData($this->getAttribute()->getAttributeCode())===null) {
+    	if(!$object->hasData($this->getAttribute()->getAttributeCode())) {
     	    $object->setData($this->getAttribute()->getAttributeCode(), $this->getDefaultValue());
     	}
+    }
+
+    public function beforeSave($object)
+    {
+        if($object->hasData($this->getAttribute()->getAttributeCode())
+            && $object->getData($this->getAttribute()->getAttributeCode()) == $this->getDefaultValue()) {
+            $object->unsData($this->getAttribute()->getAttributeCode());
+        }
     }
 } // Class Mage_GiftMessage_Model_Entity_Attribute_Backend_Boolean_Config End
