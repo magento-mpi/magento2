a3806efa2bf6c695dba687444e7546f2b8838d14
Ivan Chepurnyi <noreply@magento.com>
2007-12-24 13:08:40 +0000

    Configurable products fix

--- app/code/core/Mage/Catalog/Block/Product/View/Super/Config.php
+++ app/code/core/Mage/Catalog/Block/Product/View/Super/Config.php
@@ -25,45 +25,46 @@
  * @package    Mage_Catalog
  * @author      Ivan Chepurnyi <mitch@varien.com>
  */
- class Mage_Catalog_Block_Product_View_Super_Config extends Mage_Core_Block_Template 
+ class Mage_Catalog_Block_Product_View_Super_Config extends Mage_Core_Block_Template
  {
     public function __construct()
     {
         parent::__construct();
         $this->setTemplate('catalog/product/view/super/config.phtml');
     }
-    
+
     public function getAllowAttributes()
     {
         $collection = $this->getProduct()->getSuperLinkCollection();
         Mage::getSingleton('catalog/product_status')->addSaleableFilterToCollection($collection);
+        $collection->setStoreFilterByProduct($this->getProduct());
         return $this->getProduct()->getSuperAttributes(false, true);
     }
-    
+
     public function getAllowProducts()
     {
         return $this->getProduct()->getSuperLinks();
     }
-    
+
     public function getJsonConfig()
     {
         $attributes = array();
         $options = array();
         $store = Mage::app()->getStore();
-        
+
         foreach ($this->getAllowProducts() as $productId => $productAttributes) {
         	foreach ($productAttributes as $attribute) {
         	    if (!isset($options[$attribute['attribute_id']])) {
         	        $options[$attribute['attribute_id']] = array();
         	    }
-        	    
+
         	    if (!isset($options[$attribute['attribute_id']][$attribute['value_index']])) {
         	        $options[$attribute['attribute_id']][$attribute['value_index']] = array();
         	    }
         	    $options[$attribute['attribute_id']][$attribute['value_index']][] = $productId;
         	}
         }
-        
+
         foreach ($this->getAllowAttributes() as $attribute) {
             $attributeId = $attribute['attribute_id'];
         	$info = array(
@@ -72,9 +73,14 @@
         	   'label'     => $attribute['label'],
         	   'options'   => array()
         	);
-        	
+
         	foreach ($attribute['values'] as $value) {
         		//$info['options'][$value['value_index']] = array(
+
+        		if(!$this->_validateAttributeValue($attributeId, $value, $options)) {
+        		    continue;
+        		}
+
         		$info['options'][] = array(
         		    'id'    => $value['value_index'],
                     'label' => $value['label'],
@@ -82,10 +88,12 @@
                     'products'   => isset($options[$attributeId][$value['value_index']]) ? $options[$attributeId][$value['value_index']] : array(),
         		);
         	}
-        	
-        	$attributes[$attribute['attribute_id']] = $info;
+
+        	if($this->_validateAttributeInfo($info)) {
+        	   $attributes[$attribute['attribute_id']] = $info;
+        	}
         }
-        
+
         $config = array(
             'attributes'=> $attributes,
             'template'  => str_replace('%s', '#{price}', $store->getCurrentCurrency()->getOutputFormat()),
@@ -93,10 +101,42 @@
             'productId' => $this->getProduct()->getId(),
             'chooseText'=> Mage::helper('catalog')->__('Choose option...'),
         );
-        
+
         return Zend_Json::encode($config);
     }
-    
+
+    /**
+     * Validating of super product option value
+     *
+     * @param array $attribute
+     * @param array $value
+     * @param array $options
+     * @return boolean
+     */
+    protected function _validateAttributeValue($attributeId, &$value, &$options)
+    {
+        if(isset($options[$attributeId][$value['value_index']])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Validation of super product option
+     *
+     * @param array $info
+     * @return boolean
+     */
+    protected function _validateAttributeInfo(&$info)
+    {
+        if(count($info['options']) > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
     protected function _preparePrice($price, $isPercent=false)
     {
         if ($isPercent) {
@@ -106,7 +146,7 @@
         $price = Zend_Locale_Format::toNumber($price, array('number_format'=>'##0.00'));
         return str_replace(',', '.', $price);
     }
-    
+
     /**
      * REtrieve current product
      *
@@ -116,7 +156,7 @@
     {
         return Mage::registry('product');
     }
-    
+
  	/*public function getAttributes()
  	{
  		if($this->getRequest()->getParam('super_attribute') && is_array($this->getRequest()->getParam('super_attribute'))) {
@@ -127,37 +167,37 @@
  				}
  			}
  		}
- 		
+
  		return Mage::registry('product')->getSuperAttributes(false, true);
  	}*/
- 	
+
  	public function canDisplayContainer()
  	{
  		return !(bool)$this->getRequest()->getParam('ajax', false);
  	}
- 	
+
  	public function getPricingValue($value)
     {
     	$value = Mage::registry('product')->getPricingValue($value);
     	$numberSign = $value >= 0 ? '+' : '-';
     	return ' ' . $numberSign . ' ' . Mage::app()->getStore()->formatPrice(abs($value));
     }
-    
-    public function isSelectedOption($value, $attribute) 
+
+    public function isSelectedOption($value, $attribute)
     {
     	$selected = $this->getRequest()->getParam('super_attribute', array());
     	if(is_array($selected) && isset($selected[$attribute['attribute_id']]) && $selected[$attribute['attribute_id']]==$value['value_index']) {
     		return true;
     	}
-    	
+
     	return false;
     }
-    
+
     public function getUpdateUrl()
     {
-    	return $this->getUrl('*/*/superConfig', array('_current'=>true));    	
+    	return $this->getUrl('*/*/superConfig', array('_current'=>true));
     }
-    
+
     public function getUpdatePriceUrl()
     {
     	return $this->getUrl('*/*/price', array('_current'=>true));
--- app/code/core/Mage/Catalog/Model/Entity/Product/Super/Link/Collection.php
+++ app/code/core/Mage/Catalog/Model/Entity/Product/Super/Link/Collection.php
@@ -29,34 +29,34 @@
 class Mage_Catalog_Model_Entity_Product_Super_Link_Collection extends Mage_Catalog_Model_Entity_Product_Collection
 {
 	protected $_isLoaded  = false;
-	
+
 	public function __construct()
 	{
 		$this->setEntity(Mage::getResourceSingleton('catalog/product'))
            	->setObject('catalog/product_super_link');
 	}
-	
+
 	protected function _joinLink()
 	{
 		$this->joinField('link_id', 'catalog/product_super_link', 'link_id', 'product_id=entity_id')
 			->joinField('parent_id', 'catalog/product_super_link', 'parent_id', 'link_id=link_id')
 			->joinField('product_id', 'catalog/product_super_link', 'product_id', 'link_id=link_id');
-		
+
 		return $this;
 	}
-	
+
 	public function resetSelect()
 	{
 		$result = parent::resetSelect();
 		$this->_joinLink();
 		return $result;
 	}
-	
+
 	public function getIsLoaded()
 	{
 		return $this->_isLoaded;
 	}
-	
+
 	public function load($printQuery=false, $logQuery=false)
 	{
 		$oldStoreId = $this->getEntity()->getStoreId();
@@ -70,19 +70,31 @@ class Mage_Catalog_Model_Entity_Product_Super_Link_Collection extends Mage_Catal
 		}
 		return $this;
 	}
-	
+
 	public function useProductItem()
     {
     	$this->setObject('catalog/product');
     	return $this;
     }
-    
+
     public function setProductFilter($product)
     {
     	$this->addFieldToFilter('parent_id', (int) $product->getId());
     	return $this;
     }
-    
-    
-    
+
+    public function setStoreFilterByProduct($product)
+    {
+        if(!isset($this->_joinFields['store_id'])) {
+        	$this->joinField(
+        	   'link_store_id',
+        	   'catalog/product_store',
+        	   'store_id' ,
+        	   'product_id=entity_id',
+        	   array('store_id'=>$product->getStoreId())
+        	);
+        }
+    	return $this;
+    }
+
 }// Class Mage_Catalog_Model_Entity_Product_Super_Link_Collection END
