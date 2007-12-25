df2140e5a42dada07128ea573c46df906e5b7c1b
Michael Bessolov <michael.bessolov@magento.com>
2007-12-25 17:18:20 +0000

    Mage_Core 0.7.0 setup Install broken // upgrade mysql4-upgrade-0.6.26-0.7.0.php

--- app/code/core/Mage/Core/sql/core_setup/mysql4-upgrade-0.6.3-0.6.4.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$this->addConfigField('web/url/use_relative', 'Use relative paths in links', array(
-    'frontend_type'=>'select',
-    'source_model'=>'adminhtml/system_config_source_yesno'
-));
