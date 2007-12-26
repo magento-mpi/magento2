a8816c6a55e05e6afc5972c476f6e58cfdac48e5
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-12-26 22:06:53 +0000

    change pool install

--- app/code/core/Mage/Poll/sql/poll_setup/mysql4-install-0.7.0.sql
+++ app/code/core/Mage/Poll/sql/poll_setup/mysql4-install-0.7.0.sql
@@ -30,11 +30,11 @@
 DROP TABLE IF EXISTS `poll`;
 
 CREATE TABLE `poll` (
-  `poll_id` int(11) NOT NULL auto_increment,
+  `poll_id` int(10) unsigned NOT NULL auto_increment,
   `poll_title` varchar(255) NOT NULL default '',
   `votes_count` int(10) unsigned NOT NULL default '0',
   `store_id` smallint(6) NOT NULL default '0',
-  `date_posted` datetime NOT NULL default CURRENT_TIME,
+  `date_posted` datetime NOT NULL default '0000-00-00 00:00:00',
   `date_closed` datetime default NULL,
   `active` smallint(6) NOT NULL default '1',
   `closed` tinyint(1) NOT NULL default '0',
@@ -44,15 +44,15 @@ CREATE TABLE `poll` (
 
 /*Data for the table `poll` */
 
-insert  into `poll`(`poll_id`,`poll_title`,`votes_count`,`store_id`,`date_posted`,`date_closed`,`active`,`closed`,`answers_display`) values (1,'What is your favorite color',0,1,NOW(),NULL,1,0,NULL);
+insert  into `poll`(`poll_id`,`poll_title`,`votes_count`,`store_id`,`date_posted`,`date_closed`,`active`,`closed`,`answers_display`) values (1,'What is your favorite color',5,1, NOW(),NULL,1,0,NULL);
 
 /*Table structure for table `poll_answer` */
 
 DROP TABLE IF EXISTS `poll_answer`;
 
 CREATE TABLE `poll_answer` (
-  `answer_id` int(11) NOT NULL auto_increment,
-  `poll_id` int(11) NOT NULL default '0',
+  `answer_id` int(10) unsigned NOT NULL auto_increment,
+  `poll_id` int(10) unsigned NOT NULL default '0',
   `answer_title` varchar(255) NOT NULL default '',
   `votes_count` int(10) unsigned NOT NULL default '0',
   `answer_order` smallint(6) NOT NULL default '0',
@@ -63,7 +63,21 @@ CREATE TABLE `poll_answer` (
 
 /*Data for the table `poll_answer` */
 
-insert  into `poll_answer`(`answer_id`,`poll_id`,`answer_title`,`votes_count`,`answer_order`) values (5,1,'Green',0,0),(6,1,'Red',0,0),(7,1,'Black',0,0),(8,1,'Magenta',0,0);
+insert  into `poll_answer`(`answer_id`,`poll_id`,`answer_title`,`votes_count`,`answer_order`) values (1,1,'Green',4,0),(2,1,'Red',1,0),(3,1,'Black',0,0),(4,1,'Magenta',0,0);
+
+/*Table structure for table `poll_store` */
+
+DROP TABLE IF EXISTS `poll_store`;
+
+CREATE TABLE `poll_store` (
+  `poll_id` int(10) unsigned NOT NULL default '0',
+  `store_id` smallint(5) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`poll_id`,`store_id`),
+  CONSTRAINT `FK_POLL_STORE` FOREIGN KEY (`poll_id`) REFERENCES `poll` (`poll_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `poll_store` */
+INSERT INTO `poll_store` (`poll_id`,`store_id`) VALUES(1,1);
 
 /*Table structure for table `poll_vote` */
 
@@ -71,17 +85,15 @@ DROP TABLE IF EXISTS `poll_vote`;
 
 CREATE TABLE `poll_vote` (
   `vote_id` int(10) unsigned NOT NULL auto_increment,
-  `poll_id` int(11) NOT NULL default '0',
-  `poll_answer_id` int(11) NOT NULL default '0',
+  `poll_id` int(10) unsigned NOT NULL default '0',
+  `poll_answer_id` int(10) unsigned NOT NULL default '0',
   `ip_address` bigint(20) default NULL,
   `customer_id` int(11) default NULL,
-  `vote_time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `vote_time` timestamp NOT NULL,
   PRIMARY KEY  (`vote_id`),
   KEY `FK_POLL_ANSWER` (`poll_answer_id`),
   CONSTRAINT `FK_POLL_ANSWER` FOREIGN KEY (`poll_answer_id`) REFERENCES `poll_answer` (`answer_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `poll_vote` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
