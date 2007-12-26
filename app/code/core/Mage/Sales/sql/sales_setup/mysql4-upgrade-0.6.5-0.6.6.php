c8e49e841d587a35181c226e1d49484d5430a0f7
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-12-26 12:54:47 +0000

    update order db for new status logic

---
+++ app/code/core/Mage/Sales/sql/sales_setup/mysql4-upgrade-0.6.5-0.6.6.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category   Mage
+ * @package    Mage_Sales
+ * @copyright  Copyright (c) 2004-2007 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+$installer = $this;
+
+/* @var $installer Mage_Sales_Model_Entity_Setup */
+$installer->removeAttribute('order', 'order_status_id');
+$installer->addAttribute('order', 'state', array('type'=>'varchar', 'visible'=>false));
+$installer->addAttribute('order', 'status', array('type'=>'varchar', 'visible'=>false));
+$installer->startSetup();
+$installer->run("DROP TABLE IF EXISTS `sales_order_status`;");
+$installer->endSetup();
