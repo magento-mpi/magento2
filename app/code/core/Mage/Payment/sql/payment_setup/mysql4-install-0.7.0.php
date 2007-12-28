5dd49e92c1abcc3ff6dabac9fd4eeb884d4476b2
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-12-28 14:54:49 +0000

    fix payment module install

--- app/code/core/Mage/Payment/sql/payment_setup/mysql4-install-0.7.0.php
+++ app/code/core/Mage/Payment/sql/payment_setup/mysql4-install-0.7.0.php
@@ -17,24 +17,4 @@
  * @copyright  Copyright (c) 2004-2007 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-$installer = $this;
-/* @var $installer Mage_Core_Model_Resource_Setup */
-
-$installer->setConfigData('payment/ccsave/active', '1');
-$installer->setConfigData('payment/ccsave/cctypes', 'VI,MC');
-$installer->setConfigData('payment/ccsave/model', 'payment/method_ccsave');
-$installer->setConfigData('payment/ccsave/order_status', 'processing');
-$installer->setConfigData('payment/ccsave/sort_order', '1');
-$installer->setConfigData('payment/ccsave/title', 'Credit Card');
-
-$installer->setConfigData('payment/checkmo/active', '1');
-$installer->setConfigData('payment/checkmo/model', 'payment/method_checkmo');
-$installer->setConfigData('payment/checkmo/order_status', 'processing');
-$installer->setConfigData('payment/checkmo/sort_order', '2');
-$installer->setConfigData('payment/checkmo/title', 'Check / Money order');
-
-$installer->setConfigData('payment/purchaseorder/active', '1');
-$installer->setConfigData('payment/purchaseorder/model', 'payment/method_purchaseorder');
-$installer->setConfigData('payment/purchaseorder/order_status', 'processing');
-$installer->setConfigData('payment/purchaseorder/sort_order', '3');
-$installer->setConfigData('payment/purchaseorder/title', 'Purchase Order');
+$this->installModuleSystemDefaults();
