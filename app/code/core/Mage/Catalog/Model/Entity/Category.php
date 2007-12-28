e19f1980247ce75927f5b8784176d6e465e6f2fd
Dmitriy Soroka <dmitriy.soroka@magento.com>
2007-12-28 19:03:50 +0000

    fix category saving problem

--- app/code/core/Mage/Catalog/Model/Entity/Category.php
+++ app/code/core/Mage/Catalog/Model/Entity/Category.php
@@ -33,15 +33,15 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
      * @var Varien_Data_Tree_Db
      */
     protected $_tree;
-    
+
     /**
      * Catalog products table name
      *
      * @var string
      */
     protected $_categoryProductTable;
-    
-    public function __construct() 
+
+    public function __construct()
     {
         $resource = Mage::getSingleton('core/resource');
         $this->setType('catalog_category')
@@ -51,7 +51,7 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
             );
         $this->_categoryProductTable = Mage::getSingleton('core/resource')->getTableName('catalog/category_product');
     }
-    
+
     /**
      * Retrieve category tree object
      *
@@ -65,23 +65,23 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
         }
         return $this->_tree;
     }
-    
+
     protected function _afterDelete(Varien_Object $object){
         parent::_afterDelete($object);
         $node = $this->_getTree()->getNodeById($object->getId());
         $path = $this->_getTree()->getPath($object->getId());
-        
+
         $this->_getTree()->removeNode($node);
         $this->_updateCategoryPath($object, $path);
         return $this;
     }
-    
+
     protected function _beforeSave(Varien_Object $object)
     {
         parent::_beforeSave($object);
         if ($object->getParentId()) {
             $parentNode = $this->_getTree()->getNodeById($object->getParentId());
-            
+
             if (!$object->getId()) {
                 $node = $this->_getTree()->appendChild(array(), $parentNode);
                 $object->setId($node->getId());
@@ -89,18 +89,18 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
         }
         return $this;
     }
-    
+
     protected function _afterSave(Varien_Object $object)
     {
         parent::_afterSave($object);
         //$this->_saveInStores($object);
-        
+
         $this->_saveCategoryProducts($object)
             ->_updateCategoryPath($object, $this->_getTree()->getPath($object->getId()));
-            
+
         return $this;
     }
-    
+
     protected function _saveInStores(Varien_Object $object)
     {
         if (!$object->getMultistoreSaveFlag()) {
@@ -116,19 +116,19 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
         }
         return $this;
     }
-    
+
     protected function _saveCategoryProducts($category)
     {
         $products = $category->getPostedProducts();
         if (!is_null($products)) {
             $oldProducts = $category->getProductsPosition();
             if (!empty($oldProducts)) {
-                $this->getWriteConnection()->delete($this->_categoryProductTable, 
+                $this->getWriteConnection()->delete($this->_categoryProductTable,
                     $this->getWriteConnection()->quoteInto('product_id in(?)', array_keys($oldProducts)) . ' AND ' .
                     $this->getWriteConnection()->quoteInto('category_id=?', $category->getId())
                 );
             }
-            
+
             foreach ($products as $productId => $productPosition) {
                 if (!intval($productId)) {
                     continue;
@@ -143,11 +143,11 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
         }
         return $this;
     }
-    
+
     protected function _updateCategoryPath($category, $path)
     {
         foreach ($path as $pathItem) {
-            if ($category->getId() != $pathItem->getId()) {
+            if ($pathItem->getPid()>1 && $category->getId() != $pathItem->getId()) {
                 $category = Mage::getModel('catalog/category')
                     ->load($pathItem->getId())
                     ->save();
@@ -155,18 +155,18 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
         }
         return $this;
     }
-    
+
     protected function _insertAttribute($object, $attribute, $value, $storeIds = array())
     {
         return parent::_insertAttribute($object, $attribute, $value, $object->getStoreIds());
     }
-    
+
     public function getStoreIds($category)
     {
         if (!$category->getId()) {
             return array();
         }
-        
+
         $nodePath = $this->_getTree()
             ->getNodeById($category->getId())
                 ->getPath();
@@ -174,7 +174,7 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
         foreach ($nodePath as $node) {
         	$nodes[] = $node->getId();
         }
-        
+
         $stores = array_keys(Mage::getConfig()->getStoresByPath('catalog/category/root_id', $nodes));
         $entityStoreId = $this->getStoreId();
         if (!in_array($entityStoreId, $stores)) {
@@ -185,7 +185,7 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
         }
         return $stores;
     }
-    
+
     /**
      * Retrieve category product id's
      *
@@ -195,51 +195,51 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
     public function getProductsPosition($category)
     {
         $collection = Mage::getResourceModel('catalog/product_collection')
-            ->joinField('store_id', 
-                'catalog/product_store', 
-                'store_id', 
-                'product_id=entity_id', 
+            ->joinField('store_id',
+                'catalog/product_store',
+                'store_id',
+                'product_id=entity_id',
                 '{{table}}.store_id='.(int) $category->getStoreId())
-            ->joinField('category_id', 
-                'catalog/category_product', 
-                'category_id', 
-                'product_id=entity_id', 
+            ->joinField('category_id',
+                'catalog/category_product',
+                'category_id',
+                'product_id=entity_id',
                 null)
-            ->joinField('position', 
-                'catalog/category_product', 
-                'position', 
-                'product_id=entity_id', 
+            ->joinField('position',
+                'catalog/category_product',
+                'position',
+                'product_id=entity_id',
                 '{{table}}.category_id='.(int) $category->getId(),
                 'left')
             ->addFieldToFilter('category_id', $category->getId())
             ->load();
-        
+
         $products = array();
         foreach ($collection as $product) {
         	$products[$product->getId()] = $product->getPosition();
         }
         return $products;
     }
-    
+
     public function move(Mage_Catalog_Model_Category $category, $newParentId)
     {
         $oldStoreId = $category->getStoreId();
         $parent = Mage::getModel('catalog/category')
             ->setStoreId($category->getStoreId())
             ->load($category->getParentId());
-            
+
         $newParent = Mage::getModel('catalog/category')
             ->setStoreId($category->getStoreId())
             ->load($newParentId);
-            
+
         $oldParentStores = $parent->getStoreIds();
         $newParentStores = $newParent->getStoreIds();
-        
+
         $category->setParentId($newParentId)
             ->save();
         $parent->save();
         $newParent->save();
-        
+
         // Add to new stores
         $addToStores = array_diff($newParentStores, $oldParentStores);
         foreach ($addToStores as $storeId) {
@@ -247,7 +247,7 @@ class Mage_Catalog_Model_Entity_Category extends Mage_Eav_Model_Entity_Abstract
         	$newCategory->setStoreId($storeId)
         	   ->save();
             $children = $category->getAllChildren();
-            
+
             if ($children && $arrChildren = explode(',', $children)) {
                 foreach ($arrChildren as $childId) {
                     if ($childId == $category->getId()) {
