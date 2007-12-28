e7600042c8d31ed5a0e8a1f87036007cb625163f
Michael Bessolov <michael.bessolov@magento.com>
2007-12-28 14:25:05 +0000

    cleanup

--- app/code/core/Mage/Catalog/controllers/IndexController.php
+++ app/code/core/Mage/Catalog/controllers/IndexController.php
@@ -18,10 +18,8 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-
-
-
-class Mage_Catalog_IndexController extends Mage_Core_Controller_Front_Action {
+class Mage_Catalog_IndexController extends Mage_Core_Controller_Front_Action
+{
 
     /**
      * Index action
@@ -29,7 +27,7 @@ class Mage_Catalog_IndexController extends Mage_Core_Controller_Front_Action {
      * Display categories home page
      *
      */
-    function indexAction()
+    public function indexAction()
     {
         $this->loadLayout();
 
@@ -39,70 +37,5 @@ class Mage_Catalog_IndexController extends Mage_Core_Controller_Front_Action {
         $this->renderLayout();
     }
 
-    function testAction()
-    {
-        /*
-    	$content = Mage::getModel('catalogexcel/export')->getWorkbookXml();
-
-    	$fileName = 'catalog.xml';
-        header('HTTP/1.1 200 OK');
-        header('Content-Disposition: attachment; filename='.$fileName);
-        header('Last-Modified: '.date('r'));
-        header("Accept-Ranges: bytes");
-        header("Content-Length: ".strlen($content));
-        header("Content-type: application/octet-stream");
-        echo $content;
-        */
-        #Mage::getSingleton('catalog/convert')->run('export_catalog_to_http');
-        Mage::getSingleton('catalog/convert')->run('export_products_to_file');
-        #Mage::getSingleton('catalog/convert')->run('import_catalog');
-
-    	exit;
-    }
-
-    function importAction()
-    {
-        $fileName = Mage::getBaseDir('var').DS.'import'.DS.'simplybirkenstock.txt';
-
-        $fieldMap = array(
-            0=>'old_id',
-            1=>'sku',
-            2=>'small_image',
-            3=>'image',
-            4=>'price',
-            5=>'weight',
-            6=>'status',
-            7=>'manufacturer',
-            8=>'shoe_type',
-            9=>'is_clearance',
-            10=>'is_shoe',
-            11=>'product_type',
-            12=>'name',
-            13=>'description',
-        );
-
-        echo "<pre>";
-
-        $import = new Mage_Catalog_Import();
-        $import->loadCsv($fileName, $fieldMap);
-        $import->convert()->save();
-    }
-
-    protected function _isAllowed()
-    {
-        print "Action: ".$this->getRequest()->getActionName() . "<br/>";
-    	/*switch ($this->getRequest()->getActionName()) {
-            case 'pending':
-                return Mage::getSingleton('admin/session')->isAllowed('catalog/tag/pending');
-                break;
-            case 'all':
-                return Mage::getSingleton('admin/session')->isAllowed('catalog/tag/all');
-                break;
-            default:
-                return Mage::getSingleton('admin/session')->isAllowed('catalog/tag');
-                break;
-        }
-        */
-    }   
 }
 
