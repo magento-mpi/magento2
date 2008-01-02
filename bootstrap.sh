7b740c6d1ab5dc395fcdea75ef01bf8e7014a426
Magento Core <noreply@magento.com>
2008-01-02 18:13:38 +0000

    add all .htaccess

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 MAGE_SVN_ROOT=http://svn.magentocommerce.com/svn/magento/base/magento/trunk
 MAGE_LOCAL_ROOT="."
 
-for ENTRY in index.php pear LICENSE.txt favicon.ico .htaccess lib app/etc app/Mage.php app/code/core/Mage/Core app/code/core/Mage/Install js/prototype js/mage js/varien js/spacer.gif js/blank.html app/design/install skin/install
+for ENTRY in index.php pear LICENSE.txt favicon.ico lib app/etc app/Mage.php app/code/core/Mage/Core app/code/core/Mage/Install js/prototype js/mage js/varien js/spacer.gif js/blank.html app/design/install skin/install */.htaccess
 do
   #echo $ENTRY
   svn export --force $MAGE_SVN_ROOT/$ENTRY $MAGE_LOCAL_ROOT/$ENTRY
