aa01b398fb07902d1c61d86c74122f59fb0ccea8
Magento Core <noreply@magento.com>
2008-01-03 19:08:58 +0000

    fixed bug with empty states after import tax rates

--- app/code/core/Mage/Adminhtml/controllers/Tax/RateController.php
+++ app/code/core/Mage/Adminhtml/controllers/Tax/RateController.php
@@ -249,7 +249,7 @@ class Mage_Adminhtml_Tax_RateController extends Mage_Adminhtml_Controller_Action
         $csvFields  = array(
             0   => Mage::helper('tax')->__('State'),
             1   => Mage::helper('tax')->__('County'),
-            2   => Mage::helper('tax')->__('Zip/Postal Code')
+            2   => Mage::helper('tax')->__('Zip/Post Code')
         );
 
         $rateTypeI = 3;
@@ -280,10 +280,11 @@ class Mage_Adminhtml_Tax_RateController extends Mage_Adminhtml_Controller_Action
                 }
 
                 $rateData  = array(
-                    'tax_county_id' => $regions[$v[0]],
-                    'tax_region_id' => 0,
-                    'tax_postcode'  => $v[2]
+                    'tax_county_id' => 0,
+                    'tax_region_id' => $regions[$v[0]],
+                    'tax_postcode'  => (empty($v[2]) || $v[2]=='*') ? null : $v[2]
                 );
+                
                 $rateModel = Mage::getModel('tax/rate')
                     ->setData($rateData);
                 foreach ($rateTypes as $i => $typeId) {
@@ -314,7 +315,7 @@ class Mage_Adminhtml_Tax_RateController extends Mage_Adminhtml_Controller_Action
         }
 
         /** start csv content and set template */
-        $content    = '"'.Mage::helper('tax')->__('State').'","'.Mage::helper('tax')->__('County').'","'.Mage::helper('tax')->__('Zip/Postal Code').'"';
+        $content    = '"'.Mage::helper('tax')->__('State').'","'.Mage::helper('tax')->__('County').'","'.Mage::helper('tax')->__('Zip/Post Code').'"';
         $template   = '"{{region_name}}","","{{tax_postcode}}"';
         foreach ($rateTypes as $k => $v) {
             $content   .= ',"'.$v.'"';
