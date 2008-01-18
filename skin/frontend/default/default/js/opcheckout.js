f16c2ec1c3cc3c79c325da0f0e26883198ed7c22
Dmitriy Vasilenko <dmitriy.vasilenko@magento.com>
2008-01-18 17:32:41 +0000

    Checkmo: Additional Data changed to Payment Information(textarea)

--- skin/frontend/default/default/js/opcheckout.js
+++ skin/frontend/default/default/js/opcheckout.js
@@ -546,18 +546,13 @@ Payment.prototype = {
         if (this.currentMethod && $('payment_form_'+this.currentMethod)) {
             var form = $('payment_form_'+this.currentMethod);
             form.style.display = 'none';
-            var elements = form.getElementsByTagName('input');
+            var elements = form.getElementsBySelector('input', 'select', 'textarea');
             for (var i=0; i<elements.length; i++) elements[i].disabled = true;
-            var elements = form.getElementsByTagName('select');
-            for (var i=0; i<elements.length; i++) elements[i].disabled = true;
-
         }
         if ($('payment_form_'+method)){
             var form = $('payment_form_'+method);
             form.style.display = '';
-            var elements = form.getElementsByTagName('input');
-            for (var i=0; i<elements.length; i++) elements[i].disabled = false;
-            var elements = form.getElementsByTagName('select');
+            var elements = form.getElementsBySelector('input', 'select', 'textarea');
             for (var i=0; i<elements.length; i++) elements[i].disabled = false;
             this.currentMethod = method;
         }
