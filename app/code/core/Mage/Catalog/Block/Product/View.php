c46cd22dbd2afc3c4df5bb381f17043090dc522b
Dmitriy Vasilenko <dmitriy.vasilenko@magento.com>
2008-01-18 17:42:33 +0000

    super attributes deleted from additional info

--- app/code/core/Mage/Catalog/Block/Product/View.php
+++ app/code/core/Mage/Catalog/Block/Product/View.php
@@ -95,6 +95,9 @@ class Mage_Catalog_Block_Product_View extends Mage_Catalog_Block_Product_Abstrac
         //die;
         $attributes = $product->getAttributes();
         foreach ($attributes as $attribute) {
+            if ($product->getSuperAttributesIds() && in_array($attribute->getAttributeId(), $product->getSuperAttributesIds())) {
+                continue;
+            }
             if ($attribute->getIsVisibleOnFront() && $attribute->getIsUserDefined()) {
                 $value = $attribute->getFrontend()->getValue($product);
                 if (strlen($value)) {
