a7331d4962dcd963ff12b3a8c0fd93d85042f357
Boris Gurvich <noreply@magento.com>
2008-01-24 01:48:09 +0000

    body class contains dashes only

--- app/code/core/Mage/Page/Block/Html.php
+++ app/code/core/Mage/Page/Block/Html.php
@@ -74,6 +74,7 @@ class Mage_Page_Block_Html extends Mage_Core_Block_Template
 
     public function addBodyClass($className)
     {
+        $className = preg_replace('#[^a-zA-Z0-9]+#', '-', $className);
         $this->setBodyClass($this->getBodyClass().' '.$className);
         return $this;
     }
