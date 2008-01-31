efaa08ca25cd16b4bcfe284eddfd2d14eb937343
Alex Boyko <noreply@magento.com>
2008-01-31 19:53:06 +0000

    product event fix

--- app/code/local/Mage/Catalog/Model/Entity/Product.php
+++ app/code/local/Mage/Catalog/Model/Entity/Product.php
@@ -67,7 +67,7 @@ class Mage_Catalog_Model_Entity_Product extends Mage_Catalog_Model_Entity_Abstra
 
     protected function _afterSave(Varien_Object $object)
     {
-        Mage::dispatchEvent('catalog_product_save_after', array('product'=>$object));
+//        Mage::dispatchEvent('catalog_product_save_after', array('product'=>$object));
         parent::_afterSave($object);
 
         $this->_saveBundle($object)
