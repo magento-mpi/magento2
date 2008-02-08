6e8b92110ab0b30950014d8e4d874981efa64df9
Boris Gurvich <noreply@magento.com>
2008-02-08 19:52:51 +0000

    Fixed ignoring empty config values for encryption

--- app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Encrypted.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Encrypted.php
@@ -4,14 +4,14 @@ class Mage_Adminhtml_Model_System_Config_Backend_Encrypted extends Mage_Core_Mod
 {
     protected function _afterLoad()
     {
-        if ($decrypted = Mage::helper('core')->decrypt($this->getValue())) {
+        if ($this->getValue() && $decrypted = Mage::helper('core')->decrypt($this->getValue())) {
             $this->setValue($decrypted);
         }
     }
 
     protected function _beforeSave()
     {
-        if ($encrypted = Mage::helper('core')->encrypt($this->getValue())) {
+        if ($this->getValue() && $encrypted = Mage::helper('core')->encrypt($this->getValue())) {
             $this->setValue($encrypted);
         }
     }
