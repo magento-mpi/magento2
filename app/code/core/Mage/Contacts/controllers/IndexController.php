68f115acc47e5b35800450df1a37a0ae7fd4d5c7
Alex Stadnitskiy <noreply@magento.com>
2008-02-12 12:16:33 +0000

    fixes #0002937

--- app/code/core/Mage/Contacts/controllers/IndexController.php
+++ app/code/core/Mage/Contacts/controllers/IndexController.php
@@ -31,6 +31,16 @@ class Mage_Contacts_IndexController extends Mage_Core_Controller_Front_Action
     const XML_PATH_EMAIL_RECIPIENT  = 'contacts/email/recipient_email';
     const XML_PATH_EMAIL_SENDER     = 'contacts/email/sender_email_identity';
     const XML_PATH_EMAIL_TEMPLATE   = 'contacts/email/email_template';
+    const XML_PATH_ENABLED          = 'contacts/contacts/enabled';
+
+    public function preDispatch()
+    {
+        parent::preDispatch();
+
+        if( !Mage::getStoreConfig(self::XML_PATH_ENABLED) ) {
+            $this->norouteAction();
+        }
+    }
 
     public function indexAction()
     {
