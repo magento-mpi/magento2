569460d988e650120bb7a8c178560fc3f10981bf
Alex Stadnitskiy <noreply@magento.com>
2008-02-12 14:39:42 +0000

    reverted Identity

--- app/code/core/Mage/Adminhtml/Model/System/Config/Source/Email/Identity.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Source/Email/Identity.php
@@ -22,23 +22,23 @@
 class Mage_Adminhtml_Model_System_Config_Source_Email_Identity
 {
     protected $_options = null;
-
-    const XML_IDENT_PATH = 'trans_email';
-
     public function toOptionArray()
     {
         if (is_null($this->_options)) {
             $this->_options = array();
-            $config = Mage::getStoreConfig(self::XML_IDENT_PATH);
-
-            foreach ($config as $key => $node) {
-                $label      = $node['name'];
-                $this->_options[] = array(
-                    'value' => preg_replace('#^ident_(.*)$#', '$1', $key),
+            $config = Mage::getSingleton('adminhtml/config')->getSection('trans_email')->groups->children();
+            foreach ($config as $node) {
+                $nodeName   = $node->getName();
+                $label      = (string) $node->label;
+                $sortOrder  = (int) $node->sort_order;
+                $this->_options[$sortOrder] = array(
+                    'value' => preg_replace('#^ident_(.*)$#', '$1', $nodeName),
                     'label' => Mage::helper('adminhtml')->__($label)
                 );
             }
+            ksort($this->_options);
         }
+
         return $this->_options;
     }
 }
