793102bf889199030a15b5f9737278e8612d9f6c
Alex Boyko <noreply@magento.com>
2008-02-21 19:08:28 +0000

    catalog fix

--- app/code/local/Mage/Catalog/Model/Resource/Eav/Mysql4/Category/Collection.php
+++ app/code/local/Mage/Catalog/Model/Resource/Eav/Mysql4/Category/Collection.php
@@ -78,7 +78,7 @@ class Mage_Catalog_Model_Resource_Eav_Mysql4_Category_Collection extends Mage_Ca
     public function getProductStoreId()
     {
         if (is_null($this->_productStoreId)) {
-            $this->_productStoreId = $this->getEntity()->getStoreId();
+            $this->_productStoreId = 0;
         }
         return $this->_productStoreId;
     }
@@ -108,7 +108,8 @@ class Mage_Catalog_Model_Resource_Eav_Mysql4_Category_Collection extends Mage_Ca
     {
         $anchor     = array();
         $regular    = array();
-        foreach ($this as $item) {
+
+        foreach ($this->_items as $item) {
             if ($item->getIsAnchor()) {
                 $anchor[$item->getId()] = $item;
             }
@@ -120,15 +121,13 @@ class Mage_Catalog_Model_Resource_Eav_Mysql4_Category_Collection extends Mage_Ca
         // Retrieve regular categories product counts
         $regularIds = array_keys($regular);
         if (!empty($regularIds)) {
-            $select = $this->_read->select();
-            $select->from($this->_productTable,
-                    array('category_id', new Zend_Db_Expr('COUNT('.$this->_productTable.'.product_id)'))
+            $select = $this->_conn->select();
+            $select->from(array('main_table'=>$this->_productTable),
+                    array('category_id', new Zend_Db_Expr('COUNT(main_table.product_id)'))
                 )
-                //->join($this->_storeTable, $this->_storeTable.'.product_id='.$this->_productTable.'.product_id')
-                ->where($this->_read->quoteInto($this->_productTable.'.category_id IN(?)', $regularIds))
-                //->where($this->_read->quoteInto($this->_storeTable.'.store_id=?', $this->getProductStoreId()))
-                ->group($this->_productTable.'.category_id');
-            $counts = $this->_read->fetchPairs($select);
+                ->where($this->_conn->quoteInto('main_table.category_id IN(?)', $regularIds))
+                ->group('main_table.category_id');
+            $counts = $this->_conn->fetchPairs($select);
             foreach ($regular as $item) {
                 if (isset($counts[$item->getId()])) {
                     $item->setProductCount($counts[$item->getId()]);
@@ -138,19 +137,15 @@ class Mage_Catalog_Model_Resource_Eav_Mysql4_Category_Collection extends Mage_Ca
                 }
             }
         }
+
         // Retrieve Anchor categories product counts
         foreach ($anchor as $item) {
             if ($allChildren = $item->getAllChildren()) {
-                $select = $this->_read->select();
-                $select->from($this->_productTable, new Zend_Db_Expr('COUNT( DISTINCT '.$this->_productTable.'.product_id)'))
-                    //->join($this->_storeTable, $this->_storeTable.'.product_id='.$this->_productTable.'.product_id')
-                    ->where($this->_read->quoteInto($this->_productTable.'.category_id IN(?)', explode(',', $item->getAllChildren())))
-                    //->where($this->_read->quoteInto($this->_storeTable.'.store_id=?', $this->getProductStoreId()))
-                    //->group($this->_storeTable.'.store_id')
-                    ->group($this->_productTable.'.category_id')
-                    ;
-
-                $item->setProductCount((int) $this->_read->fetchOne($select));
+                $select = $this->_conn->select();
+                $select->from(array('main_table'=>$this->_productTable), new Zend_Db_Expr('COUNT( DISTINCT main_table.product_id)'))
+                    ->where($this->_conn->quoteInto('main_table.category_id IN(?)', explode(',', $item->getAllChildren())))
+                    ->group('main_table.category_id');
+                $item->setProductCount((int) $this->_conn->fetchOne($select));
             }
             else {
                 $item->setProductCount(0);
