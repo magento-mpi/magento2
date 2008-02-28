ca8cf18454723ba55f367d22324ec3ac351d8a4c
Lindy Kyaw <lindy@magento.com>
2008-02-28 20:01:38 +0000

    adding default gateway url for usps

--- app/code/core/Mage/Rss/Block/Catalog/New.php
+++ app/code/core/Mage/Rss/Block/Catalog/New.php
@@ -57,9 +57,9 @@ getFinalPrice() - used in shopping cart calculations
         $todayDate = $product->getResource()->formatDate(time());
 
         $products = $product->setStoreId($storeId)->getCollection()
-            //->addAttributeToFilter('news_from_date', array('date'=>true, 'to'=> $todayDate))
-            //->addAttributeToFilter(array(array('attribute'=>'news_to_date', 'date'=>true, 'from'=>$todayDate), array('attribute'=>'news_to_date', 'is' => new Zend_Db_Expr('null'))),'','left')
-            //->addAttributeToSort('news_from_date','desc')
+            ->addAttributeToFilter('news_from_date', array('date'=>true, 'to'=> $todayDate))
+            ->addAttributeToFilter(array(array('attribute'=>'news_to_date', 'date'=>true, 'from'=>$todayDate), array('attribute'=>'news_to_date', 'is' => new Zend_Db_Expr('null'))),'','left')
+            ->addAttributeToSort('news_from_date','desc')
             //->addAttributeToSelect(array('name', 'short_description', 'description', 'price', 'thumbnail'), 'inner')
             //->addAttributeToSelect(array('special_price', 'special_from_date', 'special_to_date'), 'left')
         ;
