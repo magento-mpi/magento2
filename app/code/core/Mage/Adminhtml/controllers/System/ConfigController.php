40a2ea81cf4e6175bb5f361718b69d49cd795b1d
Michael Bessolov <michael.bessolov@magento.com>
2008-02-28 21:53:25 +0000

    fixing problem fix non-secure urls for regions uploader in shipping configuration

--- app/code/core/Mage/Adminhtml/controllers/System/ConfigController.php
+++ app/code/core/Mage/Adminhtml/controllers/System/ConfigController.php
@@ -78,9 +78,9 @@ class Mage_Adminhtml_System_ConfigController extends Mage_Adminhtml_Controller_A
             ->append($this->getLayout()->createBlock('adminhtml/system_config_tabs')->initTabs());
 
         $this->_addContent($this->getLayout()->createBlock('adminhtml/system_config_edit')->initForm());
-        $this->_addJs($this->getLayout()->createBlock('core/template')->setTemplate('system/shipping/ups.phtml'));
-        $this->_addJs($this->getLayout()->createBlock('core/template')->setTemplate('system/config/js.phtml'));
-        $this->_addJs($this->getLayout()->createBlock('core/template')->setTemplate('system/shipping/applicable_country.phtml'));
+        $this->_addJs($this->getLayout()->createBlock('adminhtml/template')->setTemplate('system/shipping/ups.phtml'));
+        $this->_addJs($this->getLayout()->createBlock('adminhtml/template')->setTemplate('system/config/js.phtml'));
+        $this->_addJs($this->getLayout()->createBlock('adminhtml/template')->setTemplate('system/shipping/applicable_country.phtml'));
         $this->renderLayout();
     }
 
