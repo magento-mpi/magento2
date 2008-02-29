12de73ca4c9f62243d365a0fd8f2d2093e57110a
Victor Tihonchuk <noreply@magento.com>
2008-02-29 17:40:45 +0000

    delete method _printError from Core_App model

--- index.php
+++ index.php
@@ -25,10 +25,9 @@ if (version_compare(phpversion(), '5.2.0', '<')===true) {
 
 try {
     require_once 'app/Mage.php';
+umask(0);
+Mage::run('default');
 }
 catch (Exception $e) {
     echo '<div style="font:12px/1.35em arial, helvetica, sans-serif;"><div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;"><h3 style="margin:0; font-size:1.7em; font-weight:normal; text-transform:none; text-align:left; color:#2f2f2f;">Configuration error.</h3></div><p>'.$e->getMessage().'</p></div>';
 }
-
-umask(0);
-Mage::run('default');
