419a58c542a6b63e79d8daba27dc3c4d6c69f649
Lindy Kyaw <lindy@magento.com>
2008-02-29 18:14:09 +0000

    category rss feed

--- app/code/core/Mage/Rss/Block/Catalog/Category.php
+++ app/code/core/Mage/Rss/Block/Catalog/Category.php
@@ -38,24 +38,83 @@ class Mage_Rss_Block_Catalog_Category extends Mage_Rss_Block_Abstract
 
     protected function _toHtml()
     {
-echo "category";
         $categoryId = $this->getRequest()->getParam('cid');
         $storeId = $this->_getStoreId();
+        $rssObj = Mage::getModel('rss/rss');
         if ($categoryId) {
             $category = Mage::getModel('catalog/category')->load($categoryId);
-print_r($category->getData());
             if ($category && $category->getId()) {
-                $category->getProductCollection()->setStoreId($storeId);
                 $layer = Mage::getSingleton('catalog/layer')->setStore($storeId);
-                $collection = $layer->setCurrentCategory($category)
+                $newurl = $category->getCategoryUrl();
+                $title = $category->getName();
+                $data = array('title' => $title,
+                        'description' => $title,
+                        'link'        => $newurl,
+                        'charset'     => 'UTF-8',
+                        );
+//echo "<pre>";
+//print_r($data);
+                $rssObj->_addHeader($data);
+
+                $_collection = $category->getCollection();
+                $_collection->addAttributeToSelect('url_key')
+                    ->addAttributeToSelect('name')
+                    ->addAttributeToSelect('is_anchor')
+                    ->addAttributeToFilter('is_active',1)
+                    ->addIdFilter($category->getChildren())
+                    ->load()
+                ;
+                $productCollection = Mage::getResourceModel('catalog/product_collection');
+                $layer->prepareProductCollection($productCollection);
+                $productCollection->addCountToCategories($_collection);
+
+                if ($_collection->count()) {
+                    foreach ($_collection as $_category){
+                         $data = array(
+                                    'title'         => $_category->getName(),
+                                    'link'          => $_category->getCategoryUrl(),
+                                    'description'   => $this->helper('rss')->__('Total Products: %s', $_category->getProductCount()),
+                                    );
+
+                        $rssObj->_addEntry($data);
+                    }
+                }
+                $category->getProductCollection()->setStoreId($storeId);
+                /*
+                only load latest 50 products
+                */
+                $_productCollection = $layer->setCurrentCategory($category)
                     ->getProductCollection()
                     ->addAttributeToSort('created_at','desc')
+                    ->setCurPage(1)
+                    ->setPageSize(50)
                 ;
-echo "<hr>".$collection->getSelect();
+//echo "<hr>".$_productCollection->getSelect();
+                if ($_productCollection->getSize()>0) {
+                    foreach ($_productCollection as $_product) {
+                        $final_price = $_product->getFinalPrice();
+                        $description = '<table><tr>'.
+                            '<td><a href="'.$_product->getProductUrl().'"><img src="'.$_product->getThumbnailUrl()
+                            .'" border="0" align="left" height="75" width="75"></a></td>'.
+                            '<td  style="text-decoration:none;">'.$_product->getDescription().
+                            '<p> Price:'.Mage::helper('core')->currency($_product->getPrice()).
+                            ($_product->getPrice() != $final_price  ? ' Special Price:'. Mage::helper('core')->currency($final_price) : '').
+                            '</p>'.
+                            '</td>'.
+                            '</tr></table>'
+                        ;
+                        $data = array(
+                                    'title'         => $_product->getName(),
+                                    'link'          => $_product->getProductUrl(),
+                                    'description'   => $description,
+                                    );
+//print_r($data);
+                        $rssObj->_addEntry($data);
+                    }
+                }
             }
-
         }
-
+        return $rssObj->createRssXml();
 
     }
 }
