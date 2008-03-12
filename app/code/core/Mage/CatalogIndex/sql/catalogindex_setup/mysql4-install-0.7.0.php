47c8532fc024eb75630ed971ac7fb43e7231aae1
Victor Tihonchuk <noreply@magento.com>
2008-03-12 14:51:14 +0000

    fix catalog index setup

--- app/code/core/Mage/CatalogIndex/sql/catalogindex_setup/mysql4-install-0.7.0.php
+++ app/code/core/Mage/CatalogIndex/sql/catalogindex_setup/mysql4-install-0.7.0.php
@@ -24,66 +24,37 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-CREATE TABLE `{$this->getTable('catalogindex_eav')}` (
-`index_id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`store_id` SMALLINT( 5 ) UNSIGNED NOT NULL,
-`entity_id` INT( 10 ) UNSIGNED NOT NULL,
-`attribute_id` SMALLINT( 5 ) UNSIGNED NOT NULL ,
-`value` INT( 11 ) NOT NULL,
-
-INDEX(`value`),
-
-KEY `FK_CATALOGINDEX_EAV_ENTITY` (`entity_id`),
-KEY `FK_CATALOGINDEX_EAV_ATTRIBUTE` (`attribute_id`),
-KEY `FK_CATALOGINDEX_EAV_STORE` (`store_id`),
-
-CONSTRAINT `FK_CATALOGINDEX_EAV_ENTITY` FOREIGN KEY (`entity_id`) REFERENCES
- {$this->getTable('catalog_product_entity')} (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-
-CONSTRAINT `FK_CATALOGINDEX_EAV_ATTRIBUTE` FOREIGN KEY (`attribute_id`)
- REFERENCES {$this->getTable('eav_attribute')} (`attribute_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-
-CONSTRAINT `FK_CATALOGINDEX_EAV_STORE` FOREIGN KEY (`store_id`)
- REFERENCES {$this->getTable('core_store')} (`store_id`) ON DELETE CASCADE ON UPDATE CASCADE
-
+DROP TABLE IF EXISTS `{$installer->getTable('catalogindex_eav')}`;
+CREATE TABLE `{$installer->getTable('catalogindex_eav')}` (
+  `index_id` int(10) unsigned NOT NULL auto_increment,
+  `store_id` smallint(5) unsigned NOT NULL default '0',
+  `entity_id` int(10) unsigned NOT NULL default '0',
+  `attribute_id` smallint(5) unsigned NOT NULL default '0',
+  `value` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`index_id`),
+  KEY `IDX_VALUE` (`value`),
+  CONSTRAINT `FK_CATALOGINDEX_EAV_ENTITY` FOREIGN KEY (`entity_id`) REFERENCES `{$installer->getTable('catalog_product_entity')}` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_CATALOGINDEX_EAV_ATTRIBUTE` FOREIGN KEY (`attribute_id`) REFERENCES `{$installer->getTable('eav_attribute')}` (`attribute_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_CATALOGINDEX_EAV_STORE` FOREIGN KEY (`store_id`) REFERENCES `{$installer->getTable('core_store')}` (`store_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-
-
-
-CREATE TABLE `{$this->getTable('catalogindex_price')}` (
-`index_id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`store_id` SMALLINT( 5 ) UNSIGNED NOT NULL,
-`entity_id` INT( 10 ) UNSIGNED NOT NULL,
-`attribute_id` SMALLINT( 5 ) UNSIGNED NOT NULL ,
-`customer_group_id` SMALLINT( 3 ) UNSIGNED NOT NULL,
-`qty` DECIMAL( 12,4 ) UNSIGNED NOT NULL,
-`value` DECIMAL( 12,4 ) NOT NULL,
-
-INDEX(`value`),
-INDEX(`qty`),
-
-KEY `FK_CATALOGINDEX_PRICE_ENTITY` (`entity_id`),
-KEY `FK_CATALOGINDEX_PRICE_ATTRIBUTE` (`attribute_id`),
-KEY `FK_CATALOGINDEX_PRICE_STORE` (`store_id`),
-KEY `FK_CATALOGINDEX_PRICE_CUSTOMER_GROUP` (`customer_group_id`),
-
-CONSTRAINT `FK_CATALOGINDEX_PRICE_ENTITY` FOREIGN KEY (`entity_id`) REFERENCES
- {$this->getTable('catalog_product_entity')} (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-
-CONSTRAINT `FK_CATALOGINDEX_PRICE_ATTRIBUTE` FOREIGN KEY (`attribute_id`)
- REFERENCES {$this->getTable('eav_attribute')} (`attribute_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-
-CONSTRAINT `FK_CATALOGINDEX_PRICE_STORE` FOREIGN KEY (`store_id`)
- REFERENCES {$this->getTable('core_store')} (`store_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-
-CONSTRAINT `FK_CATALOGINDEX_PRICE_CUSTOMER_GROUP` FOREIGN KEY (`customer_group_id`)
- REFERENCES {$this->getTable('customer_group')} (`customer_group_id`) ON DELETE CASCADE ON UPDATE CASCADE
-
+DROP TABLE IF EXISTS `{$installer->getTable('catalogindex_price')}`;
+CREATE TABLE `{$installer->getTable('catalogindex_price')}` (
+  `index_id` int(10) unsigned NOT NULL auto_increment,
+  `store_id` smallint(5) unsigned NOT NULL default '0',
+  `entity_id` int(10) unsigned NOT NULL default '0',
+  `attribute_id` smallint(5) unsigned NOT NULL default '0',
+  `customer_group_id` smallint(3) unsigned NOT NULL default '0',
+  `qty` decimal(12,4) unsigned NOT NULL default '0.0000',
+  `value` decimal(12,4) NOT NULL default '0.0000',
+  PRIMARY KEY  (`index_id`),
+  KEY `IDX_VALUE` (`value`),
+  KEY `IDX_QTY` (`qty`),
+  CONSTRAINT `FK_CATALOGINDEX_PRICE_ENTITY` FOREIGN KEY (`entity_id`) REFERENCES `{$installer->getTable('catalog_product_entity')}` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_CATALOGINDEX_PRICE_ATTRIBUTE` FOREIGN KEY (`attribute_id`) REFERENCES `{$installer->getTable('eav_attribute')}` (`attribute_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_CATALOGINDEX_PRICE_STORE` FOREIGN KEY (`store_id`) REFERENCES `{$installer->getTable('core_store')}` (`store_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_CATALOGINDEX_PRICE_CUSTOMER_GROUP` FOREIGN KEY (`customer_group_id`) REFERENCES `{$installer->getTable('customer_group')}` (`customer_group_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 ");
 
 $installer->endSetup();
