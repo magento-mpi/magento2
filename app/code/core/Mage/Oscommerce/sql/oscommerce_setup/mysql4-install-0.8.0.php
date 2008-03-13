e17f64cfb9f8ec04b3592c4c29d157f14aada2bb
Vincent Maung <vincent@magento.com>
2008-03-13 17:22:43 +0000

    Added new module for OsCommerce

---
+++ app/code/core/Mage/Oscommerce/sql/oscommerce_setup/mysql4-install-0.8.0.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category   Mage
+ * @package    Mage_DImportOsc
+ * @copyright  Copyright (c) 2004-2007 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+$installer = $this;
+/* @var $installer Mage_Core_Model_Resource_Setup */
+
+$installer->startSetup();
+
+$installer->run("
+
+DROP TABLE IF EXISTS `{$this->getTable('oscommerce_import')}`;
+
+CREATE TABLE `{$this->getTable('oscommerce_import')}` (
+  `import_id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  `host` varchar(255) NOT NULL,
+  `port` int(5) NOT NULL,
+  `db_name` varchar(255) default NULL,
+  `db_user` varchar(255) default NULL,
+  `db_password` varchar(255) default NULL,
+  `db_type` varchar(32) default NULL,
+  PRIMARY KEY  (`import_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+");
+
+$installer->endSetup();
