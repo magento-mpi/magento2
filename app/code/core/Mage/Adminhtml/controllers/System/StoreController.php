9284a08ccf2e930837b7d722fc289e5e931dcee7
Alex Boyko <noreply@magento.com>
2008-03-18 14:02:09 +0000

    cache cleanup on store edit/add/delete

--- app/code/core/Mage/Adminhtml/controllers/System/StoreController.php
+++ app/code/core/Mage/Adminhtml/controllers/System/StoreController.php
@@ -165,13 +165,16 @@ class Mage_Adminhtml_System_StoreController extends Mage_Adminhtml_Controller_Ac
                         break;
 
                     case 'store':
+                        $eventName = 'store_edit';
                         $storeModel = Mage::getModel('core/store')->setData($postData['store']);
                         if ($postData['store']['store_id'] == '') {
                             $storeModel->setId(null);
+                            $eventName = 'store_add';
                         }
                         $groupModel = Mage::getModel('core/store_group')->load($storeModel->getGroupId());
                         $storeModel->setWebsiteId($groupModel->getWebsiteId());
                         $storeModel->save();
+                        Mage::dispatchEvent($eventName, array('store'=>$storeModel));
                         $session->addSuccess(Mage::helper('core')->__('Store View was successfully saved'));
                         break;
                     default:
@@ -410,6 +413,9 @@ class Mage_Adminhtml_System_StoreController extends Mage_Adminhtml_Controller_Ac
 
         try {
             $model->delete();
+
+            Mage::dispatchEvent('store_delete', array('store'=>$model));
+
             $session->addSuccess(Mage::helper('core')->__('Store View was successfully deleted.'));
             $this->_redirect('*/*/');
             return ;
