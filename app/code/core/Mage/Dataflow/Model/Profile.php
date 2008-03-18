07cd4a94645d17a3ab6f0a3c602c66450298105a
Ruslan Voitenko <ruslan.voytenko@magento.com>
2008-03-18 21:09:01 +0000

    "Fixed #2064: \"No validation in Actions XML field\""

--- app/code/core/Mage/Dataflow/Model/Profile.php
+++ app/code/core/Mage/Dataflow/Model/Profile.php
@@ -50,7 +50,7 @@ class Mage_Dataflow_Model_Profile extends Mage_Core_Model_Abstract
         parent::_beforeSave();
 
         $actionsXML = $this->getData('actions_xml');
-        if (0 < strlen($actionsXML) && false === simplexml_load_string($actionsXML, null, LIBXML_NOERROR)) {
+        if (0 < strlen($actionsXML) || false === simplexml_load_string('<data>'.$actionsXML.'</data>', null, LIBXML_NOERROR)) {
             Mage::throwException(Mage::helper("dataflow")->__("Actions XML is not valid."));
         }
 
