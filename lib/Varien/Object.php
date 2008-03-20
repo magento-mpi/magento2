969ebf4b759592f6631f41b6f547af9a18aaf37b
Dmitriy Soroka <dmitriy.soroka@magento.com>
2008-03-20 20:29:25 +0000

    revert __set __get

--- lib/Varien/Object.php
+++ lib/Varien/Object.php
@@ -498,6 +498,32 @@ class Varien_Object
     }
 
     /**
+     * Attribute getter (deprecated)
+     *
+     * @param string $var
+     * @return mixed
+     */
+
+    public function __get($var)
+    {
+        $var = $this->_underscore($var);
+        return $this->getData($var);
+    }
+
+    /**
+     * Attribute setter (deprecated)
+     *
+     * @param string $var
+     * @param mixed $value
+     */
+    public function __set($var, $value)
+    {
+        $this->_isChanged = true;
+        $var = $this->_underscore($var);
+        $this->setData($var, $value);
+    }
+
+    /**
      * checks whether the object is empty
      *
      * @return boolean
