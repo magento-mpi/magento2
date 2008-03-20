091a87d58e9bc783e3a7a18bbf2014a7362b92d6
Dmitriy Volik <dmitriy.volik@magento.com>
2008-03-20 09:08:47 +0000

    fixed #3490: Admin: 'Catalog' tab isn't highlighted for some sub functionnalities

--- app/code/core/Mage/Adminhtml/controllers/UrlrewriteController.php
+++ app/code/core/Mage/Adminhtml/controllers/UrlrewriteController.php
@@ -26,7 +26,7 @@ class Mage_Adminhtml_UrlrewriteController extends Mage_Adminhtml_Controller_Acti
     protected function _initAction()
     {
         $this->loadLayout();
-        $this->_setActiveMenu('adminhtml/urlrewrite');
+        $this->_setActiveMenu('catalog/urlrewrite');
         return $this;
     }
 
@@ -101,11 +101,10 @@ class Mage_Adminhtml_UrlrewriteController extends Mage_Adminhtml_Controller_Acti
     public function newAction()
     {
         $this->loadLayout();
-        $this->_setActiveMenu('adminhtml/urlrewrite');
 
         $this->getLayout()->getBlock('head')->setCanLoadExtJs(true);
 
-        $this->_addContent($this->getLayout()->createBlock('adminhtml/urlrewrite_add'));
+        $this->_initAction()->_addContent($this->getLayout()->createBlock('adminhtml/urlrewrite_add'));
 
         $this->renderLayout();
     }
