8ac8a641da5de1b815189447eb4fe009379764ca
Ruslan Voitenko <ruslan.voytenko@magento.com>
2008-03-23 18:46:47 +0000

    added item 'My Tags' to navigation menu for logged customer

--- app/code/core/Mage/Tag/controllers/CustomerController.php
+++ app/code/core/Mage/Tag/controllers/CustomerController.php
@@ -39,6 +39,11 @@ class Mage_Tag_CustomerController extends Mage_Core_Controller_Front_Action
 
         $this->loadLayout();
         $this->_initLayoutMessages('tag/session');
+        
+        if ($navigationBlock = $this->getLayout()->getBlock('customer_account_navigation')) {
+            $navigationBlock->setActive('tag/customer');
+        }
+        
         $this->renderLayout();
     }
 
@@ -52,6 +57,11 @@ class Mage_Tag_CustomerController extends Mage_Core_Controller_Front_Action
 
         $this->loadLayout();
         $this->_initLayoutMessages('tag/session');
+        
+        if ($navigationBlock = $this->getLayout()->getBlock('customer_account_navigation')) {
+            $navigationBlock->setActive('tag/customer');
+        }
+        
         $this->renderLayout();
     }
 
@@ -84,6 +94,10 @@ class Mage_Tag_CustomerController extends Mage_Core_Controller_Front_Action
         }
 
         $this->_initLayoutMessages('customer/session');
+        
+        if ($navigationBlock = $this->getLayout()->getBlock('customer_account_navigation')) {
+            $navigationBlock->setActive('tag/customer');
+        }
 
         $this->renderLayout();
     }
