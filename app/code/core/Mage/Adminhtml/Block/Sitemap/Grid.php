6bc1620a2ae8ca5f81f07c69975d6e794d9f6c6b
Victor Tihonchuk <noreply@magento.com>
2008-03-24 19:41:38 +0000

    Fix issue 0003678: Google Sitemap - incorrcet 'Last Time Generated' after adding

--- app/code/core/Mage/Adminhtml/Block/Sitemap/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Sitemap/Grid.php
@@ -71,8 +71,7 @@ class Mage_Adminhtml_Block_Sitemap_Grid extends Mage_Adminhtml_Block_Widget_Grid
         $this->addColumn('sitemap_time', array(
             'header'    => Mage::helper('sitemap')->__('Last Time Generated'),
             'width'     => '150px',
-            'index'     => 'sitemap_time',
-            'renderer'  => 'adminhtml/sitemap_grid_renderer_time',
+            'index'     => 'datetime',
         ));
 
 
