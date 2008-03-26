70ecdfd2ecd8fddb73d8e0b3b36d13918ca0778d
Andrey Babich <noreply@magento.com>
2008-03-26 14:58:39 +0000

    Fixed #3143: Incorrect viewing of the PDF Packingslip

--- app/code/core/Mage/Sales/Model/Order/Pdf/Invoice.php
+++ app/code/core/Mage/Sales/Model/Order/Pdf/Invoice.php
@@ -72,7 +72,7 @@ class Mage_Sales_Model_Order_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Abst
 
             /* Add body */
             foreach ($invoice->getAllItems() as $item){
-                $shift = 10;
+                $shift = array();
                 if ($this->y<15) {
                     /* Add new table head */
                     $page = $pdf->newPage(Zend_Pdf_Page::SIZE_A4);
@@ -99,18 +99,57 @@ class Mage_Sales_Model_Order_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Abst
                 /* Add products */
                 $page->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 7);
                 $page->drawText($item->getQty()*1, 35, $this->y);
-                $page->drawText($item->getName(), 60, $this->y);
-                foreach (explode('</li>', $item->getDescription()) as $description){
-                    $page->drawText(strip_tags($description), 65, $this->y-$shift);
-                    $shift += 10;
+
+                //$page->drawText($item->getName(), 60, $this->y);
+                /* in case Product name is longer than 80 chars - it is written in a few lines */
+                if (strlen($item->getName()) > 80) {
+                    $drawTextValue = explode(" ", $item->getName());
+                    $drawTextParts = array();
+                    $i = 0;
+                    foreach ($drawTextValue as $drawTextPart) {
+                        if (!empty($drawTextParts{$i}) &&
+                            (strlen($drawTextParts{$i}) + strlen($drawTextPart)) < 80 ) {
+                            $drawTextParts{$i} .= ' '. $drawTextPart;
+                        } else {
+                            $i++;
+                            $drawTextParts{$i} = $drawTextPart;
+                        }
+                    }
+                    $shift{0} = 0;
+                    foreach ($drawTextParts as $drawTextPart) {
+                        $page->drawText($drawTextPart, 60, $this->y-$shift{0}, 'UTF-8');
+                        $shift{0} += 10;
+                    }
+
+                } else {
+                    $page->drawText($item->getName(), 60, $this->y);
                 }
-                $page->drawText($item->getSku(), 380, $this->y);
+
+                $shift{1} = 0;
+                foreach (explode('</li>', $item->getDescription()) as $description){
+                    $page->drawText(strip_tags($description), 65, $this->y-$shift{1});
+                    $shift{1} += 10;
+                }
+
+                //$page->drawText($item->getSku(), 380, $this->y);
+                /* in case Product SKU is longer than 36 chars - it is written in a few lines */
+                if (strlen($item->getSku()) > 36) {
+                    $drawTextValue = str_split($item->getSku(), 36);
+                    $shift{2} = 0;
+                    foreach ($drawTextValue as $drawTextPart) {
+                        $page->drawText($drawTextPart, 380, $this->y-$shift{2}, 'UTF-8');
+                        $shift{2} += 10;
+                    }
+
+                } else {
+                    $page->drawText($item->getSku(), 380, $this->y);
+                }
 
                 $font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_BOLD);
                 $row_total = $order->formatPriceTxt($item->getRowTotal()+$item->getTaxAmount()-$item->getDiscountAmount());
 
                 $page->drawText($row_total, 565-$this->widthForStringUsingFontSize($row_total, $font, 7), $this->y, 'UTF-8');
-                $this->y -=$shift;
+                $this->y -= max($shift)+10;
             }
 
             /* Add totals */
