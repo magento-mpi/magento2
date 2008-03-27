506417cebf9838813b74bfdb2653b5e590eb417c
Dmitriy Soroka <dmitriy.soroka@magento.com>
2008-03-27 23:57:29 +0000

    fix not valid carriers for quote address

--- app/code/core/Mage/Sales/Model/Quote/Address.php
+++ app/code/core/Mage/Sales/Model/Quote/Address.php
@@ -341,7 +341,7 @@ class Mage_Sales_Model_Quote_Address extends Mage_Customer_Model_Address_Abstrac
     {
         $rates = array();
         foreach ($this->getShippingRatesCollection() as $rate) {
-            if (!$rate->isDeleted()) {
+            if (!$rate->isDeleted() && $rate->getCarrierInstance()) {
                 if (!isset($rates[$rate->getCarrier()])) {
                     $rates[$rate->getCarrier()] = array();
                 }
