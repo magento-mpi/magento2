0dd2702ecc486bada589b8f9c7f7dd88a55c31e4
Dmitriy Soroka <dmitriy.soroka@magento.com>
2008-03-27 10:00:51 +0000

    fix big prices bug

--- app/code/core/Mage/Directory/Model/Currency/Filter.php
+++ app/code/core/Mage/Directory/Model/Currency/Filter.php
@@ -66,6 +66,7 @@ class Mage_Directory_Model_Currency_Filter implements Zend_Filter_Interface
         $value = floatval($value);
         $value = Mage::app()->getStore()->roundPrice($this->_rate*$value);
         //$value = round($value, 2);
+        $value = sprintf("%f", $value);
         return $this->_currency->toCurrency($value);
     }
 }
