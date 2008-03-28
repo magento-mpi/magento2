c16f75d43e317d257d190d7306ac69b199113e8d
Dmitriy Soroka <dmitriy.soroka@magento.com>
2008-03-28 13:00:24 +0000

    fix html escae for search term

--- app/code/core/Mage/Adminhtml/Block/Dashboard/Searches/Renderer/Searchquery.php
+++ app/code/core/Mage/Adminhtml/Block/Dashboard/Searches/Renderer/Searchquery.php
@@ -31,7 +31,10 @@ class Mage_Adminhtml_Block_Dashboard_Searches_Renderer_Searchquery extends Mage_
     {
         $value = $row->getData($this->getColumn()->getIndex());
         if (strlen($value) > 30) {
-            $value = '<span title="'. $value .'">'. $this->htmlEscape(substr($value, 0, 30)).'...</span>';
+            $value = '<span title="'. $this->htmlEscape($value) .'">'. $this->htmlEscape(substr($value, 0, 30)).'...</span>';
+        }
+        else {
+            $value = $this->htmlEscape($value);
         }
         return $value;
     }
