5e310f6144c6255bf48bfb526d0ff2b92bb12bcf
Dmitriy Soroka <dmitriy.soroka@magento.com>
2008-03-29 20:06:03 +0000

    fix product grid in admin

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Grid.php
@@ -67,7 +67,7 @@ class Mage_Adminhtml_Block_Catalog_Product_Grid extends Mage_Adminhtml_Block_Wid
             $collection->joinAttribute('custom_name', 'catalog_product/name', 'entity_id', null, 'inner', $store->getId());
             $collection->joinAttribute('status', 'catalog_product/status', 'entity_id', null, 'inner', $store->getId());
             $collection->joinAttribute('visibility', 'catalog_product/visibility', 'entity_id', null, 'inner', $store->getId());
-            $collection->joinAttribute('price', 'catalog_product/price', 'entity_id', null, 'inner', $store->getId());
+            $collection->joinAttribute('price', 'catalog_product/price', 'entity_id', null, 'left', $store->getId());
         }
         else {
             $collection->addAttributeToSelect('price');
