0636cb8405452cf8de80e4a0cce9a6e10e63ef24
Boris Gurvich <noreply@magento.com>
2008-03-30 10:09:53 +0000

    Product tax class is optional now

--- app/code/core/Mage/Tax/Model/Class/Source/Product.php
+++ app/code/core/Mage/Tax/Model/Class/Source/Product.php
@@ -32,7 +32,7 @@ class Mage_Tax_Model_Class_Source_Product extends Mage_Eav_Model_Entity_Attribut
 
 		$options = $this->_options;
         if ($withEmpty) {
-            array_unshift($options, array('value'=>'', 'label'=>Mage::helper('tax')->__('-- Please Select --')));
+            array_unshift($options, array('value'=>'', 'label'=>Mage::helper('tax')->__('NONE')));
         }
         return $options;
 	}
