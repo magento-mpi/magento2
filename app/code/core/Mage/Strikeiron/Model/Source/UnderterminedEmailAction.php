56d96782c00215aaf11e3cf351c05e4cc7d2adc2
Lindy Kyaw <lindy@magento.com>
2008-04-02 22:21:46 +0000

    email constant moved to email service

--- app/code/core/Mage/Strikeiron/Model/Source/UnderterminedEmailAction.php
+++ app/code/core/Mage/Strikeiron/Model/Source/UnderterminedEmailAction.php
@@ -28,9 +28,9 @@ class Mage_Strikeiron_Model_Source_UnderterminedEmailAction
     public function toOptionArray()
     {
         return array(
-            array('value' => Mage_Strikeiron_Model_Strikeiron::EMAIL_UNDETERMINED_ACCEPT, 'label' => Mage::helper('strikeiron')->__('Accept')),
-            array('value' => Mage_Strikeiron_Model_Strikeiron::EMAIL_UNDETERMINED_REJECT, 'label' => Mage::helper('strikeiron')->__('Reject')),
-            array('value' => Mage_Strikeiron_Model_Strikeiron::EMAIL_UNDETERMINED_CONFIRM, 'label' => Mage::helper('strikeiron')->__('Pop up warning and accept')),
+            array('value' => Mage_Strikeiron_Model_Service_EmailVerification::EMAIL_UNDETERMINED_ACCEPT, 'label' => Mage::helper('strikeiron')->__('Accept')),
+            array('value' => Mage_Strikeiron_Model_Service_EmailVerification::EMAIL_UNDETERMINED_REJECT, 'label' => Mage::helper('strikeiron')->__('Reject')),
+            array('value' => Mage_Strikeiron_Model_Service_EmailVerification::EMAIL_UNDETERMINED_CONFIRM, 'label' => Mage::helper('strikeiron')->__('Pop up warning and accept')),
         );
     }
 }
