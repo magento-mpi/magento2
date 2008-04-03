17509acbc6adea18ab189a7954d1b326be7c0aa3
Lindy Kyaw <lindy@magento.com>
2008-04-03 19:17:58 +0000

    paypal standard for secure

--- app/code/core/Mage/Paypal/controllers/StandardController.php
+++ app/code/core/Mage/Paypal/controllers/StandardController.php
@@ -97,9 +97,9 @@ class Mage_Paypal_StandardController extends Mage_Core_Controller_Front_Action
             $order->sendNewOrderEmail();
         }
 
-        Mage::getSingleton('checkout/session')->unsQuoteId();
+        //Mage::getSingleton('checkout/session')->unsQuoteId();
 
-        $this->_redirect('checkout/onepage/success');
+        $this->_redirect('checkout/onepage/success', array('_secure'=>true));
     }
 
     /**
