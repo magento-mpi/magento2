208d612ad1339f8ff64ccdbf7d498580c5c7710e
Dmitriy Soroka <dmitriy.soroka@magento.com>
2008-04-25 11:15:57 +0000

    fix tax update for customer

--- app/code/core/Mage/Customer/sql/customer_setup/mysql4-upgrade-0.8.2-0.8.3.php
+++ app/code/core/Mage/Customer/sql/customer_setup/mysql4-upgrade-0.8.2-0.8.3.php
@@ -18,23 +18,3 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-$installer = $this;
-/* @var $installer Mage_Customer_Model_Entity_Setup  */
-
-$installer->startSetup();
-
-$customerTaxClassIds = $installer->getConnection()->fetchCol(
-    "SELECT class_id FROM {$installer->getTable('tax_class')}
-        WHERE class_type = 'CUSTOMER'
-        ORDER BY class_id ASC"
-);
-
-if (count($customerTaxClassIds) > 0) {
-    $installer->run(
-        "UPDATE {$installer->getTable('customer_group')}
-            SET tax_class_id = {$customerTaxClassIds[0]}
-            WHERE tax_class_id NOT IN (".implode(',', $customerTaxClassIds).")"
-    );
-}
-
-$installer->endSetup();
