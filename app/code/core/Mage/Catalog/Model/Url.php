afacbfd97b20e513a098ea853ca81c61fa2fe168
Victor Tihonchuk <noreply@magento.com>
2008-05-05 13:05:35 +0000

    Fix issue 0004692: Product's details pages not available on second WebSite :: Refresh Catalog Rewrites

--- app/code/core/Mage/Catalog/Model/Url.php
+++ app/code/core/Mage/Catalog/Model/Url.php
@@ -23,7 +23,7 @@
  *
  * @category   Mage
  * @package    Mage_Catalog
- * @author      Magento Core Team <core@magentocommerce.com>
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Catalog_Model_Url
 {
