b5ad416f4d9b4e0ec193a36ce0e40a68cbf703dc
Alex Boyko <noreply@magento.com>
2008-05-05 16:29:31 +0000

    fixed 'is_active' attribute in oscommerce module

--- app/code/core/Mage/Oscommerce/Model/Mysql4/Catalog/Url.php
+++ app/code/core/Mage/Oscommerce/Model/Mysql4/Catalog/Url.php
@@ -1,9 +1,10 @@
 <?php
 
-class Mage_Oscommerce_Model_Mysql4_Catalog_Url extends Mage_Catalog_Model_Resource_Eav_Mysql4_Url 
+class Mage_Oscommerce_Model_Mysql4_Catalog_Url extends Mage_Catalog_Model_Resource_Eav_Mysql4_Url
 {
     protected function _getCategories($categoryIds, $storeId = null, $path = null)
     {
+        $isActiveAttribute = Mage::getModel('eav/entity_attribute')->loadByCode('catalog_category', 'is_active');
         $categories = array();
 
         if (!is_array($categoryIds)) {
@@ -11,14 +12,17 @@ class Mage_Oscommerce_Model_Mysql4_Catalog_Url extends Mage_Catalog_Model_Resour
         }
 
         $select = $this->_getWriteAdapter()->select()
-            ->from($this->getTable('catalog/category'), array('entity_id', 'parent_id', 'is_active', 'path'));
+            ->from(array('main_table'=>$this->getTable('catalog/category')), array('main_table.entity_id', 'main_table.parent_id', 'is_active'=>'IFNULL(c.value, d.value)', 'main_table.path'));
         if (is_null($path)) {
-            $select->where('entity_id IN(?)', $categoryIds);
+            $select->where('main_table.entity_id IN(?)', $categoryIds);
         }
         else {
-            $select->where('path LIKE ?', $path . '%')
-                ->order('path');
+            $select->where('main_table.path LIKE ?', $path . '%')
+                ->order('main_table.path');
         }
+        $table = $this->getTable('catalog/category') . '_int';
+        $select->joinLeft(array('d'=>$table), "d.attribute_id = '{$isActiveAttribute->getId()}' AND d.store_id = 0 AND d.entity_id = main_table.entity_id", array())
+            ->joinLeft(array('c'=>$table), "c.attribute_id = '{$isActiveAttribute->getId()}' AND c.store_id = '{$storeId}' AND c.entity_id = main_table.entity_id", array());
 
         if (!is_null($storeId)) {
             $rootCategoryPath = $this->getStores($storeId)->getRootCategoryPath();
