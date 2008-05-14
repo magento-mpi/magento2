072e2633b1790570ac297e03e874ea21a3644222
Alex Boyko <noreply@magento.com>
2008-05-14 14:49:08 +0000

    fixed free shipping for flat rate

--- app/code/core/Mage/Sales/Model/Quote/Address.php
+++ app/code/core/Mage/Sales/Model/Quote/Address.php
@@ -436,6 +436,7 @@ class Mage_Sales_Model_Quote_Address extends Mage_Customer_Model_Address_Abstrac
         }
 
         $request = Mage::getModel('shipping/rate_request');
+        $request->setAllItems($this->getAllItems());
         $request->setDestCountryId($this->getCountryId());
         $request->setDestRegionId($this->getRegionId());
         $request->setDestRegionCode($this->getRegionCode());
@@ -461,7 +462,6 @@ class Mage_Sales_Model_Quote_Address extends Mage_Customer_Model_Address_Abstrac
         $request->setStoreId($this->getQuote()->getStore()->getId());
         $request->setWebsiteId($this->getQuote()->getStore()->getWebsiteId());
         $request->setFreeShipping($this->getFreeShipping());
-
         /**
          * Currencies need to convert in free shipping
          */
