ca21b493845e9464d3feb7995f383a2adabb11d4
Dmitriy Volik <dmitriy.volik@magento.com>
2008-05-28 15:43:01 +0000

    #4882: Missing word in contact form success message

--- app/code/core/Mage/Contacts/controllers/IndexController.php
+++ app/code/core/Mage/Contacts/controllers/IndexController.php
@@ -75,7 +75,7 @@ class Mage_Contacts_IndexController extends Mage_Core_Controller_Front_Action
                     );
                 $translate->setTranslateInline(true);
 
-                Mage::getSingleton('customer/session')->addSuccess(Mage::helper('contacts')->__('Your inquiry was submitted and will be responded as soon as possible. Thank you for contacting us.'));
+                Mage::getSingleton('customer/session')->addSuccess(Mage::helper('contacts')->__('Your inquiry was submitted and will be responded to as soon as possible. Thank you for contacting us.'));
                 $this->_redirect('*/*/');
 
                 return;
