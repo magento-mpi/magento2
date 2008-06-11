6fc1748b1b94157239879abce767463d057a4595
Victor Tihonchuk <noreply@magento.com>
2008-06-11 18:15:49 +0000

    Bundle attribute update

--- app/code/core/Mage/Bundle/sql/bundle_setup/mysql4-install-0.1.0.php
+++ app/code/core/Mage/Bundle/sql/bundle_setup/mysql4-install-0.1.0.php
@@ -150,10 +150,14 @@ $installer->addAttribute('catalog_product', 'price_view', array(
         'is_configurable'   => false
     ));
 
-$priceApplyTo = split(',', $installer->getAttribute('catalog_product', 'price', 'apply_to'));
-if (!in_array('bundle', $priceApplyTo)) {
-    $priceApplyTo[] = 'bundle';
-    $installer->updateAttribute('catalog_product', 'price', 'apply_to', join(',', $priceApplyTo));
+$fieldList = array('price','special_price','special_from_date','special_to_date',
+    'minimal_price','cost','tier_price','weight');
+foreach ($fieldList as $field) {
+    $applyTo = split(',', $installer->getAttribute('catalog_product', $field, 'apply_to'));
+    if (!in_array('bundle', $applyTo)) {
+        $applyTo[] = 'bundle';
+        $installer->updateAttribute('catalog_product', $field, 'apply_to', join(',', $applyTo));
+    }
 }
 
 $installer->endSetup();
