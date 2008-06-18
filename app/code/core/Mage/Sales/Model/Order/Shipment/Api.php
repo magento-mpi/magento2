e9dcef010d124565b58a162897050fa8e2e84d1e
Ivan Chepurnyi <noreply@magento.com>
2008-06-18 18:05:36 +0000

    Sales API

---
+++ app/code/core/Mage/Sales/Model/Order/Shipment/Api.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category   Mage
+ * @package    Mage_Sales
+ * @copyright  Copyright (c) 2004-2007 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Sales order shippment API
+ *
+ * @category   Mage
+ * @package    Mage_Sales
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Sales_Model_Order_Shipment_Api extends Mage_Sales_Model_Api_Resource
+{
+    public function __construct()
+    {
+        $this->_attributesMap['shipment'] = array(
+            'shippment_id' => 'entity_id'
+        );
+
+        $this->_attributesMap['shipment_item'] = array(
+            'item_id'    => 'entity_id'
+        );
+
+        $this->_attributesMap['shipment_comment'] = array(
+            'comment_id' => 'entity_id'
+        );
+
+        $this->_attributesMap['shipment_track'] = array(
+            'track_id'   => 'entity_id'
+        );
+    }
+
+    public function items($filters)
+    {
+        //TODO: add full name logic
+        $collection = Mage::getResourceModel('sales/order_shipment_collection')
+            ->addAttributeToSelect('increment_id')
+            ->addAttributeToSelect('created_at')
+            ->addAttributeToSelect('total_qty')
+            ->joinAttribute('shipping_firstname', 'order_address/firstname', 'shipping_address_id', null, 'left')
+            ->joinAttribute('shipping_lastname', 'order_address/lastname', 'shipping_address_id', null, 'left')
+            ->joinAttribute('order_increment_id', 'order/increment_id', 'order_id', null, 'left')
+            ->joinAttribute('order_created_at', 'order/created_at', 'order_id', null, 'left');
+
+        if (is_array($filters)) {
+            try {
+                foreach ($filters as $field => $value) {
+                    if (isset($this->_filtersMap[$field])) {
+                        $field = $this->_filtersMap[$field];
+                    }
+
+                    $collection->addFieldToFilter($field, $value);
+                }
+            } catch (Mage_Core_Exception $e) {
+                $this->_fault('filters_invalid', $e->getMessage());
+            }
+        }
+
+        $result = array();
+
+        foreach ($collection as $order) {
+            $result[] = $this->_getAttributes($order, 'shipment');
+        }
+
+        return $result;
+    }
+
+    public function create($orderIncrementId, $itemsQty, $comment = null, $email = false, $includeComment = false)
+    {
+
+    }
+} // Class Mage_Sales_Model_Order_Shipment_Api End
