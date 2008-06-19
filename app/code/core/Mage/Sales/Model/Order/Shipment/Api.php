2af054b544d00cdcb33497d5b8c2f5c5de2b45a7
Ivan Chepurnyi <noreply@magento.com>
2008-06-19 18:12:33 +0000

    Webservices Sales API

--- app/code/core/Mage/Sales/Model/Order/Shipment/Api.php
+++ app/code/core/Mage/Sales/Model/Order/Shipment/Api.php
@@ -83,6 +83,169 @@ class Mage_Sales_Model_Order_Shipment_Api extends Mage_Sales_Model_Api_Resource
 
     public function create($orderIncrementId, $itemsQty, $comment = null, $email = false, $includeComment = false)
     {
+        $order = Mage::getModel('sales/order')->loadByIncrementId($orderIncrementId);
 
+        /**
+          * Check order existing
+          */
+        if (!$order->getId()) {
+             $this->_fault('order_not_exists');
+        }
+
+        /**
+         * Check shipment create availability
+         */
+        if (!$order->canShip()) {
+             $this->_fault('data_invalid', Mage::helper('sales')->__('Can not do shipment for order.'));
+        }
+
+        $convertor   = Mage::getModel('sales/convert_order');
+        $shipment    = $convertor->toShipment($order);
+
+        foreach ($order->getAllItems() as $orderItem) {
+            if (!$orderItem->getQtyToShip()) {
+                continue;
+            }
+            if ($orderItem->getIsVirtual()) {
+                continue;
+            }
+            $item = $convertor->itemToShipmentItem($orderItem);
+            if (isset($itemsQty[$orderItem->getId()])) {
+                $qty = $itemsQty[$orderItem->getId()];
+            }
+            else {
+                $qty = $orderItem->getQtyToShip();
+            }
+            $item->setQty($qty);
+        	$shipment->addItem($item);
+        }
+
+        $shipment->getOrder()->setIsInProcess(true);
+
+        try {
+            $transactionSave = Mage::getModel('core/resource_transaction')
+                ->addObject($shipment)
+                ->addObject($shipment->getOrder())
+                ->save();
+        } catch (Mage_Core_Exception $e) {
+            $this->_fault('data_invalid', $e->getMessage());
+        }
+
+        return $shipment->getIncrementId();
     }
+
+    public function addTrack($shipmentIncrementId, $carrier, $title, $trackNumber)
+    {
+        $shipment = Mage::getModel('sales/order_shipment')->load($shipmentIncrementId);
+
+        /* @var $shipment Mage_Sales_Model_Order_Shipment */
+
+        if (!$shipment->getId()) {
+            $this->_fault('not_exists');
+        }
+
+        $carriers = $this->_getCarriers($shipment);
+
+        if (!isset($carriers[$carrier])) {
+            $this->_fault('data_invalid', Mage::helper('sales')->__('Invalid carrier specified.'));
+        }
+
+        $data = array(
+            'carrier' => $carrier,
+            'title'   => $title,
+            'number'  => $trackNumber
+        );
+
+        $track = Mage::getModel('sales/order_shipment_track')
+                	   ->addData($data);
+
+        $shipment->addTrack($track);
+
+        try {
+            $shipment->save();
+        } catch (Mage_Core_Exception $e) {
+            $this->_fault('data_invalid', $e->getMessage());
+        }
+
+        return true;
+    }
+
+    public function removeTrack($shipmentIncrementId, $trackId)
+    {
+        $shipment = Mage::getModel('sales/order_shipment')->load($shipmentIncrementId);
+
+        /* @var $shipment Mage_Sales_Model_Order_Shipment */
+
+        if (!$shipment->getId()) {
+            $this->_fault('not_exists');
+        }
+
+        if(!$track = $shipment->getTrackById($trackId)) {
+            $this->_fault('track_not_exists');
+        }
+
+        try {
+            $track->delete();
+        } catch (Mage_Core_Exception $e) {
+            $this->_fault('track_not_deleted', $e->getMessage());
+        }
+
+        return true;
+    }
+
+    public function infoTrack($shipmentIncrementId, $trackId)
+    {
+         $shipment = Mage::getModel('sales/order_shipment')->load($shipmentIncrementId);
+
+        /* @var $shipment Mage_Sales_Model_Order_Shipment */
+
+        if (!$shipment->getId()) {
+            $this->_fault('not_exists');
+        }
+
+        if(!$track = $shipment->getTrackById($trackId)) {
+            $this->_fault('track_not_exists');
+        }
+
+        /* @var $track Mage_Sales_Model_Order_Shipment_Track */
+        $info = $track->getNumberDetail();
+
+        if (is_object($info)) {
+            $info = $info->toArray();
+        }
+
+        return $info;
+    }
+
+    public function getCarriers($orderIncrementId)
+    {
+        $order = Mage::getModel('sales/order')->loadByIncrementId($orderIncrementId);
+
+        /**
+          * Check order existing
+          */
+        if (!$order->getId()) {
+             $this->_fault('order_not_exists');
+        }
+
+        return $this->_getCarriers($order);
+    }
+
+    protected function _getCarriers($object)
+    {
+        $carriers = array();
+        $carrierInstances = Mage::getSingleton('shipping/config')->getAllCarriers(
+            $object->getStoreId()
+        );
+
+        $carriers['custom'] = Mage::helper('sales')->__('Custom Value');
+        foreach ($carrierInstances as $code => $carrier) {
+            if ($carrier->isTrackingAvailable()) {
+                $carriers[$code] = $carrier->getConfigData('title');
+            }
+        }
+
+        return $carriers;
+    }
+
 } // Class Mage_Sales_Model_Order_Shipment_Api End
