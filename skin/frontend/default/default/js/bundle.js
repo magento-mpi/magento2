3b59a1218ac4402d23d111e8633b58bfeb3e1f43
Dmitriy Vasilenko <dmitriy.vasilenko@magento.com>
2008-06-30 21:12:40 +0000

    fixed bundle templates & js

--- skin/frontend/default/default/js/bundle.js
+++ skin/frontend/default/default/js/bundle.js
@@ -65,8 +65,12 @@ Product.Bundle.prototype = {
             return 0;
         }
 
-        if (this.config.options[optionId].selections[selectionId].customQty == 1 && !this.config['options'][optionId].isMulti) {
-            qty = $('bundle-option-' + optionId + '-qty-input').value;
+        if (this.config.options[optionId].selections[selectionId].customQty == 1 && this.config['options'][optionId].isMulti) {
+            if ($('bundle-option-' + optionId + '-qty-input')) {
+                qty = $('bundle-option-' + optionId + '-qty-input').value;
+            } else {
+                qty = 1;
+            }
         } else {
             qty = this.config.options[optionId].selections[selectionId].qty;
         }
