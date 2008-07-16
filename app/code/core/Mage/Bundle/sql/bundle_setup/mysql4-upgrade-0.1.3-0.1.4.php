21bf6919ab760ec4fff0ab47a13ee68f30d67815
Michael Bessolov <michael.bessolov@magento.com>
2008-07-16 10:27:02 +0000

    fixed catalog upgrade for bundles during installation

--- app/code/core/Mage/Bundle/sql/bundle_setup/mysql4-upgrade-0.1.3-0.1.4.php
+++ app/code/core/Mage/Bundle/sql/bundle_setup/mysql4-upgrade-0.1.3-0.1.4.php
@@ -13,7 +13,7 @@
  * to license@magentocommerce.com so we can send you a copy immediately.
  *
  * @category   Mage
- * @package    Mage_Catalog
+ * @package    Mage_Bundle
  * @copyright  Copyright (c) 2004-2007 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -23,22 +23,9 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-    ALTER TABLE `{$installer->getTable('catalog/product_entity')}` ADD `has_options` SMALLINT(1) NOT NULL DEFAULT '0';
-");
-
-$installer->addAttribute('catalog_product', 'has_options', array(
-    'type' => 'static',
-    'visible'=>false,
-    'default' => false
-));
-$installer->run("
     UPDATE `{$installer->getTable('catalog/product_entity')}` SET `has_options` = '1'
     WHERE (entity_id IN (
-        SELECT product_id FROM `{$installer->getTable('catalog/product_option')}` GROUP BY product_id
-    ));
-    UPDATE `{$installer->getTable('catalog/product_entity')}` SET `has_options` = '1'
-    WHERE (entity_id IN (
-        SELECT product_id FROM `{$installer->getTable('catalog/product_super_attribute')}` GROUP BY product_id
+        SELECT parent_product_id FROM `{$installer->getTable('catalog/product_bundle_selection')}` GROUP BY parent_product_id
     ));
 ");
 
