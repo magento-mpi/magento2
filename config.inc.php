5ac3fcda74dc9cea8ac435622c64313ccaf8e276
Victor Tihonchuk <noreply@magento.com>
2008-07-24 12:26:51 +0000

    Fixed #0005994: Translatable texts from layouts don't go to translation files Add AdminNotification module and layout files to translate config

--- config.inc.php
+++ config.inc.php
@@ -33,251 +33,234 @@ $CONFIG['translates'] = array(
         'app/design/adminhtml/default/default/layout/',
         'app/design/adminhtml/default/default/template/',
     ),
-
+    'Mage_AdminNotification' => array(
+        'app/code/core/Mage/AdminNotification/',
+    ),
     'Mage_Backup' => array(
         'app/code/core/Mage/Backup/',
     ),
-
     'Mage_Bundle' => array(
         'app/code/core/Mage/Bundle/',
         'app/design/frontend/default/default/template/bundle/',
+        'app/design/frontend/default/default/layout/bundle.xml',
         'app/design/adminhtml/default/default/template/bundle/',
     ),
-
     'Mage_Catalog' => array(
         'app/code/core/Mage/Catalog/',
         'app/design/frontend/default/default/template/catalog/',
+        'app/design/frontend/default/default/layout/catalog.xml',
     ),
-
     'Mage_CatalogInventory' => array(
         'app/code/core/Mage/CatalogInventory/',
     ),
-
     'Mage_CatalogRule' => array(
         'app/code/core/Mage/CatalogRule/',
     ),
-
     'Mage_CatalogSearch' => array(
         'app/code/core/Mage/CatalogSearch/',
-        'app/design/frontend/default/default/template/catalogsearch/',
+        'app/design/frontend/de fault/default/template/catalogsearch/',
+        'app/design/frontend/default/default/layout/catalogsearch.xml',
     ),
-
     'Mage_Checkout' => array(
         'app/code/core/Mage/Checkout/',
         'app/design/frontend/default/default/template/checkout/',
+        'app/design/frontend/default/default/layout/checkout.xml',
     ),
-
     'Mage_Chronopay' => array(
         'app/code/core/Mage/Chronopay/',
         'app/design/frontend/default/default/template/chronopay/',
+        'app/design/frontend/default/default/layout/chronopay.xml',
     ),
-
     'Mage_Cms' => array(
         'app/code/core/Mage/Cms/',
         'app/design/frontend/default/default/template/cms/',
+        'app/design/frontend/default/default/layout/cms.xml',
     ),
-
     'Mage_Contacts' => array(
         'app/code/core/Mage/Contacts/',
         'app/design/frontend/default/default/template/contacts/',
+        'app/design/frontend/default/default/layout/contacts.xml',
     ),
-
     'Mage_Core' => array(
         'app/code/core/Mage/Core/',
         'app/design/frontend/default/default/template/core/',
+        'app/design/frontend/default/default/layout/core.xml',
     ),
-
     'Mage_Cron' => array(
         'app/code/core/Mage/Cron/',
     ),
-
     'Mage_Customer' => array(
         'app/code/core/Mage/Customer/',
         'app/design/frontend/default/default/template/customer/',
+        'app/design/frontend/default/default/layout/customer.xml',
     ),
-
     'Mage_Cybermut' => array(
         'app/code/core/Mage/Cybermut/',
         'app/design/frontend/default/default/template/cybermut/',
-        'app/design/adminhtml/default/default/template/cybermut/',
+        'app/design/frontend/default/default/layout/cybermut.xml',
     ),
-
     'Mage_Cybersource' => array(
         'app/code/core/Mage/Cybersource/',
         'app/design/frontend/default/default/template/cybersource/',
     ),
-
     'Mage_Dataflow' => array(
         'app/code/core/Mage/Dataflow/',
     ),
-
     'Mage_Directory' => array(
         'app/code/core/Mage/Directory/',
         'app/design/frontend/default/default/template/directory/',
+        'app/design/frontend/default/default/layout/directory.xml',
     ),
-
     'Mage_Eav' => array(
         'app/code/core/Mage/Eav/',
     ),
-
     'Mage_Eway' => array(
         'app/code/core/Mage/Eway/',
         'app/design/frontend/default/default/template/eway/',
+        'app/design/frontend/default/default/layout/eway.xml',
         'app/design/adminhtml/default/default/template/eway/',
     ),
-
     'Mage_Flo2Cash' => array(
         'app/code/core/Mage/Flo2Cash/',
         'app/design/frontend/default/default/template/flo2cash/',
         'app/design/adminhtml/default/default/template/flo2cash/',
     ),
-
     'Mage_GiftMessage' => array(
         'app/code/core/Mage/GiftMessage/',
         'app/design/frontend/default/default/template/giftmessage/',
+        'app/design/frontend/default/default/layout/giftmessage.xml',
     ),
-
     'Mage_GoogleAnalytics' => array(
         'app/code/core/Mage/GoogleAnalytics/',
+        'app/design/frontend/default/default/layout/googleanalytics.xml',
     ),
-
     'Mage_GoogleCheckout' => array(
         'app/code/core/Mage/GoogleCheckout/',
+        'app/design/frontend/default/default/layout/googlecheckout.xml',
     ),
-
     'Mage_Ideal' => array(
         'app/code/core/Mage/Ideal/',
         'app/design/frontend/default/default/template/ideal/',
+        'app/design/frontend/default/default/layout/ideal.xml',
         'app/design/adminhtml/default/default/template/ideal/',
     ),
-
     'Mage_Install' => array(
         'app/code/core/Mage/Install/',
         'app/design/install/default/default/layout/',
         'app/design/install/default/default/template/',
     ),
-
     'Mage_Log' => array(
         'app/code/core/Mage/Log/',
     ),
-
     'Mage_Newsletter' => array(
         'app/code/core/Mage/Newsletter/',
         'app/design/frontend/default/default/template/newsletter/',
+        'app/design/frontend/default/default/layout/newsletter.xml',
     ),
-
     'Mage_Page' => array(
         'app/code/core/Mage/Page/',
         'app/design/frontend/default/default/template/page/',
+        'app/design/frontend/default/default/layout/page.xml',
     ),
-
     'Mage_Paybox' => array(
         'app/code/core/Mage/Paybox/',
         'app/design/frontend/default/default/template/paybox/',
+        'app/design/frontend/default/default/layout/paybox.xml',
         'app/design/adminhtml/default/default/template/paybox/',
     ),
-
     'Mage_Paygate' => array(
         'app/code/core/Mage/Paygate/',
     ),
-
     'Mage_Payment' => array(
         'app/code/core/Mage/Payment/',
         'app/design/frontend/default/default/template/payment/',
     ),
-
     'Mage_Paypal' => array(
         'app/code/core/Mage/Paypal/',
         'app/design/frontend/default/default/template/paypaluk/',
+        'app/design/frontend/default/default/layout/paypaluk.xml',
     ),
-
     'Mage_PaypalUk' => array(
         'app/code/core/Mage/Paypal/',
         'app/design/frontend/default/default/template/paypal/',
+        'app/design/frontend/default/default/layout/paypal.xml',
     ),
-
     'Mage_Poll' => array(
         'app/code/core/Mage/Poll/',
         'app/design/frontend/default/default/template/poll/',
+        'app/design/frontend/default/default/layout/poll.xml',
     ),
-
     'Mage_ProductAlert' => array(
         'app/code/core/Mage/ProductAlert/',
         'app/design/frontend/default/default/template/email/productalert/',
         'app/design/frontend/default/default/template/productalert/',
+        'app/design/frontend/default/default/layout/productalert.xml',
     ),
-
     'Mage_Protx' => array(
         'app/code/core/Mage/Protx/',
-        'app/design/frontend/default/default/template/protx',
+        'app/design/frontend/default/default/template/protx/',
+        'app/design/frontend/default/default/layout/protx.xml',
     ),
-
     'Mage_Rating' => array(
         'app/code/core/Mage/Rating/',
         'app/design/frontend/default/default/template/rating/',
     ),
-
     'Mage_Reports' => array(
         'app/code/core/Mage/Reports/',
         'app/design/frontend/default/default/template/reports/',
+        'app/design/frontend/default/default/layout/reports.xml',
     ),
-
     'Mage_Review' => array(
         'app/code/core/Mage/Review/',
         'app/design/frontend/default/default/template/review/',
+        'app/design/frontend/default/default/layout/review.xml',
     ),
-
     'Mage_Rss' => array(
         'app/code/core/Mage/Rss/',
         'app/design/frontend/default/default/template/rss/',
+        'app/design/frontend/default/default/layout/rss.xml',
     ),
-
     'Mage_Rule' => array(
         'app/code/core/Mage/Rule/',
     ),
-
     'Mage_Sales' => array(
         'app/code/core/Mage/Sales/',
         'app/design/frontend/default/default/template/email/order/',
         'app/design/frontend/default/default/template/sales/',
+        'app/design/frontend/default/default/layout/sales.xml',
     ),
-
     'Mage_SalesRule' => array(
         'app/code/core/Mage/SalesRule/',
     ),
-
     'Mage_Sendfriend' => array(
         'app/code/core/Mage/Sendfriend/',
         'app/design/frontend/default/default/template/sendfriend/',
+        'app/design/frontend/default/default/layout/sendfriend.xml',
     ),
-
     'Mage_Shipping' => array(
         'app/code/core/Mage/Shipping/',
         'app/design/frontend/default/default/template/shipping/',
+        'app/design/frontend/default/default/layout/shipping.xml',
     ),
-
     'Mage_Sitemap' => array(
         'app/code/core/Mage/Sitemap/',
     ),
-
     'Mage_Tag' => array(
         'app/code/core/Mage/Tag/',
         'app/design/frontend/default/default/template/tag/',
+        'app/design/frontend/default/default/layout/tag.xml',
     ),
-
     'Mage_Tax' => array(
         'app/code/core/Mage/Tax/',
     ),
-
     'Mage_Usa' => array(
         'app/code/core/Mage/Usa/',
     ),
-
     'Mage_Wishlist' => array(
         'app/code/core/Mage/Wishlist/',
         'app/design/frontend/default/default/template/wishlist/',
+        'app/design/frontend/default/default/layout/wishlist.xml',
     ),
-
     'translate' => array(
         'app/design/frontend/default/default/template/callouts/',
     ),
@@ -286,6 +269,7 @@ $CONFIG['translates'] = array(
 
 $CONFIG['helpers']  = array(
     'adminhtml'         => 'Mage_Adminhtml',
+    'adminnotification' => 'Mage_AdminNotification',
     'backup'            => 'Mage_Backup',
     'bundle'            => 'Mage_Bundle',
     'catalog'           => 'Mage_Catalog',
