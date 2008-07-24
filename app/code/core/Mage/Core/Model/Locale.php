33ccd6ee8e9ece0148f0bf6b35a0c6f9418e9333
Alex Boyko <noreply@magento.com>
2008-07-24 16:15:38 +0000

    fixed exception in core/locale when requested currency in not found

--- app/code/core/Mage/Core/Model/Locale.php
+++ app/code/core/Mage/Core/Model/Locale.php
@@ -426,9 +426,19 @@ class Mage_Core_Model_Locale
     {
         Varien_Profiler::start('locale/currency');
         if (!isset(self::$_currencyCache[$this->getLocaleCode()][$currency])) {
-            self::$_currencyCache[$this->getLocaleCode()][$currency] =
-                //new Zend_Currency($currency, $this->getLocale());
-                new Mage_Core_Model_Locale_Currency($currency, $this->getLocale());
+            try {
+                $currencyObject = new Mage_Core_Model_Locale_Currency($currency, $this->getLocale());
+            } catch (Exception $e) {
+                $currencyObject = new Mage_Core_Model_Locale_Currency($this->getCurrency(), $this->getLocale());
+                $options = array(
+                        'name'      => $currency,
+                        'currency'  => $currency,
+                        'symbol'    => $currency
+                );
+                $currencyObject->setFormat($options);
+            }
+
+            self::$_currencyCache[$this->getLocaleCode()][$currency] = $currencyObject;
         }
         Varien_Profiler::stop('locale/currency');
         return self::$_currencyCache[$this->getLocaleCode()][$currency];
