da434ff806f4571cb74f417bb298fa06d33549c7
Michael Bessolov <michael.bessolov@magento.com>
2008-07-24 06:07:27 +0000

    Minor fixes:  - No highlight for not-loaded AJAX tabs  - Changed sort order of theme fields in configuration  - Commented out DROP TABLE for checkout agreements  - Moved semicolon into translation in frontend store switcher

--- app/code/core/Mage/Checkout/sql/checkout_setup/mysql4-upgrade-0.9.0-0.9.1.php
+++ app/code/core/Mage/Checkout/sql/checkout_setup/mysql4-upgrade-0.9.0-0.9.1.php
@@ -24,7 +24,7 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-DROP TABLE IF EXISTS `{$installer->getTable('checkout_agreement')}`;
+// DROP TABLE IF EXISTS `{$installer->getTable('checkout_agreement')}`;
 CREATE TABLE `{$installer->getTable('checkout_agreement')}` (
    `agreement_id` int(10) unsigned NOT NULL auto_increment,
    `name` varchar(255) NOT NULL default '',
