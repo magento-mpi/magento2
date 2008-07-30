0dda90a1ab68efc2fe9469cd95a4103b59eab993
Anton Makarenko <anton.makarenko@magento.com>
2008-07-30 16:51:27 +0000

    Added Mage_Tag_Controllers_IndexTest

--- tests/Mage/Tag/Controllers/AllTests.php
+++ tests/Mage/Tag/Controllers/AllTests.php
@@ -5,6 +5,7 @@ class Mage_Tag_Controllers_AllTests
     {
         $suite = new PHPUnit_Framework_TestSuite('Mage/Tag/Controllers/AllTests');
         $suite->addTestSuite('Mage_Tag_Controllers_ProductTest');
+        $suite->addTestSuite('Mage_Tag_Controllers_IndexTest');
         return $suite;
     }
 }
--- tests/Mage/Tag/Controllers/IndexTest.php
+++ tests/Mage/Tag/Controllers/IndexTest.php
@@ -2,12 +2,12 @@
 if (!defined('_IS_INCLUDED')) {
     require dirname(__FILE__) . '/../../../PHPUnitTestInit.php';
     $suite = new PHPUnit_Framework_TestSuite();
-    $suite->addTestSuite('Mage_Tag_Controllers_ProductTest');
+    $suite->addTestSuite('Mage_Tag_Controllers_IndexTest');
     PHPUnit_TextUI_TestRunner::run($suite);
     exit;
 }
 
-class Mage_Tag_Controllers_ProductTest extends PHPUnit_Framework_TestCase
+class Mage_Tag_Controllers_IndexTest extends PHPUnit_Framework_TestCase
 {
     protected $_product;
     protected $_tag;
@@ -65,29 +65,74 @@ class Mage_Tag_Controllers_ProductTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Check if added product tag is at the page
+     * Check if tag is added by saveAction
      *
      */
-    public function testListAction()
+    public function testSaveAction()
     {
         ob_start();
         try {
+            // create unique tag name
+            for ($i = 0; ; $i++) {
+                $tagName = uniqid();
+                $tag = Mage::getModel('tag/tag')
+                    ->loadByName($tagName);
+                if (0 == $tag->getId()) {
+                    break;
+                }
+                if ($i > 10) {
+                    $this->fail('Failed to generate unique random tag.');
+                }
+            }
+            // create a success url
+            $successUrl = uniqid();
+            // log in customer
+            $session = Mage::getSingleton('customer/session')
+                ->setCustomerAsLoggedIn($this->_customer);
+
+            // dispatch controller
+            $_GET['tagName'] = $tagName;
             Mage::app()->getFrontController()->getRequest()
                 ->setModuleName('tag')
-                ->setControllerName('product')
-                ->setActionName('list')
-                ->setParam('tagId', $this->_tag->getId());
+                ->setControllerName('index')
+                ->setActionName('save')
+                ->setParam('product', $this->_product->getId())
+                ->setParam(Mage_Core_Controller_Front_Action::PARAM_NAME_URL_ENCODED, $successUrl)
+            ;
             Mage::app()->getFrontController()->dispatch();
 
-            $this->assertThat(
-                Mage::getSingleton('core/layout')->getBlock('tag_products'),
-                new PHPUnit_Framework_Constraint_IsInstanceOf(Mage::getConfig()->getBlockClassName('tag/product_result'))
+            // check if our new tag has been saved
+            $tag = Mage::getModel('tag/tag')
+                ->loadByName($tagName);
+            $this->assertTrue(0 != $tag->getId(), sprintf('Failed to save tag "%s".', $tagName));
+
+            // check if our new tag has pending status
+            $this->assertTrue($tag->getPendingStatus() == $tag->getStatus(), 'New tag has wrong status. Expected pending.');
+
+            // check if our new tag is related to product/customer/store
+            $relation = Mage::getModel('tag/tag_relation')->loadByTagCustomer(
+                $this->_product->getId(), $tag->getId(), $this->_customer->getId(), $this->_customer->getStoreId()
             );
+            $this->assertTrue(0 != $relation->getId(), 'Tag saved, but relation is not.');
+            $this->assertTrue(
+                (0 != $relation->getTagId()) && (0 != $relation->getCustomerId()) && (0 != $relation->getProductId()),
+                'Tag saved, but relation is corrupt. Expected aggregated relation.'
+            );
+
+            // dispose of tag
+            $tag->delete();
+            // logoff customer
+            $session->logout();
+
             $contents = ob_get_clean();
-            $this->assertContains($this->_tag->getName(), $contents);
-            $this->assertContains($this->_product->getName(), $contents);
         }
         catch (Exception $e) {
+            if ($tag && $tag->getId()) {
+                $tag->delete();
+            }
+            if ($session && $session->isLoggedIn()) {
+                $session->logout();
+            }
             ob_get_clean();
             throw $e;
         }
