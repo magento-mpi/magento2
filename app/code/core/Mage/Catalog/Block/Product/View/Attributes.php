755832236cbfe5242efcf60c166b5f1caafbdac9
Ruslan Voitenko <ruslan.voytenko@magento.com>
2008-08-13 13:07:39 +0000

    Fixed#6253: Exclude attribute from get all attributes

--- app/code/core/Mage/Catalog/Block/Product/View/Attributes.php
+++ app/code/core/Mage/Catalog/Block/Product/View/Attributes.php
@@ -38,13 +38,20 @@ class Mage_Catalog_Block_Product_View_Attributes extends Mage_Core_Block_Templat
         return $this->_product;
     }
 
-    public function getAdditionalData()
+    /**
+     * $excludeAttr is optional array of attribute codes to
+     * exclude them from additional data array
+     *
+     * @param array $excludeAttr
+     * @return array
+     */
+    public function getAdditionalData(array $excludeAttr = array())
     {
         $data = array();
         $product = $this->getProduct();
         $attributes = $product->getAttributes();
         foreach ($attributes as $attribute) {
-            if ($attribute->getIsVisibleOnFront() && $attribute->getIsUserDefined()) {
+            if ($attribute->getIsVisibleOnFront() && $attribute->getIsUserDefined() && !in_array($attribute->getAttributeCode(), $excludeAttr)) {
 
                 $value = $attribute->getFrontend()->getValue($product);
                 if (strlen($value) && $product->hasData($attribute->getAttributeCode())) {
