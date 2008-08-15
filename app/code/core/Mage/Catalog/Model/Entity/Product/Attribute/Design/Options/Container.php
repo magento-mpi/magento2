c42acbdbc3703cf0b8ae0329f305351d02f72bc4
Ruslan Voitenko <ruslan.voytenko@magento.com>
2008-08-15 14:55:59 +0000

    Fixed#6415: CSV Product Import not picking up existing custom options

--- app/code/core/Mage/Catalog/Model/Entity/Product/Attribute/Design/Options/Container.php
+++ app/code/core/Mage/Catalog/Model/Entity/Product/Attribute/Design/Options/Container.php
@@ -33,4 +33,27 @@ class Mage_Catalog_Model_Entity_Product_Attribute_Design_Options_Container exten
     {
         $this->_configNodePath = 'global/catalog/product/design/options_container';
     }
+
+    /**
+     * Get a text for option value
+     *
+     * @param string|integer $value
+     * @return string
+     */
+    public function getOptionText($value)
+    {
+        $options = $this->getAllOptions();
+        if (sizeof($options) > 0) {
+            foreach ($options as $option) {
+                if (isset($option['value']) && $option['value'] == $value) {
+                    return $option['label'];
+                }
+            }
+        }
+        if (isset($options[$value])) {
+            return $option[$value];
+        }
+        return false;
+    }
+
 }
