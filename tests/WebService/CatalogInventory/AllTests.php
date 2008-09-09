4e2b0d8fbbfbcf957d1782fbb51691e95fd2fcfa
Dmitriy Volik <dmitriy.volik@magento.com>
2008-09-09 16:32:36 +0000

    CatalogInventory test cases

---
+++ tests/WebService/CatalogInventory/AllTests.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Mage
+ * @package    WebService_CatalogInventory_AllTests
+ * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class WebService_CatalogInventory_AllTests
+{
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('WebService/CatalogInventory/AllTests');
+        $suite->addTestSuite('WebService_CatalogInventory_StockItemTest');
+        return $suite;
+    }
+}
---
+++ tests/WebService/CatalogInventory/StockItemTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Mage
+ * @package    WebService_CatalogInventory_StockItemTest
+ * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class WebService_CatalogInventory_StockItemTest extends WebService_TestCase_Abstract
+{
+    /**
+     * tests cataloginventory_stock_item.list
+     */
+    public function testItems(WebService_Connector_Interface $connector)
+    {
+        //$result = $connector->call('customer_group.list');
+    }
+    /**
+     * tests cataloginventory_stock_item.update
+     */
+    public function testUpdate(WebService_Connector_Interface $connector)
+    {
+        //$result = $connector->call('customer_group.list');
+    }
+}
