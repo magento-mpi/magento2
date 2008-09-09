93281a4ce006423fba1efff3c48ceef657a80d59
Anton Makarenko <anton.makarenko@magento.com>
2008-09-09 16:56:35 +0000

    Updated includes in CatalogInventory webservice tests

--- tests/WebService/CatalogInventory/StockItemTest.php
+++ tests/WebService/CatalogInventory/StockItemTest.php
@@ -24,6 +24,11 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+if (!defined('_IS_INCLUDED')) {
+    require dirname(__FILE__) . '/../../PHPUnitTestInit.php';
+    PHPUnitTestInit::runMe(__FILE__);
+}
+
 class WebService_CatalogInventory_StockItemTest extends WebService_TestCase_Abstract
 {
     /**
