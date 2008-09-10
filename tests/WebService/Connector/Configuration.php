0b5542ed522c9bc6470f2d279b3dfbbf85d928fb
Anton Makarenko <anton.makarenko@magento.com>
2008-09-10 10:49:15 +0000

    Implemented Webserivice tests connector configuration

--- tests/WebService/Connector/Configuration.php
+++ tests/WebService/Connector/Configuration.php
@@ -21,23 +21,78 @@
 
 class WebService_Connector_Configuration
 {
-    public static  function getApiLogin()
+    private static $_credentials = -1;
+
+    private static function _init()
     {
-        return '';
+        if (-1 === self::$_credentials) {
+            Mage::app();
+
+            // check if url available
+            $url = Mage::getStoreConfig('web/unsecure/base_url', 0);
+            if (empty($url) || '{{base_url}}' === $url) {
+                throw new Exception('Base URL must be specified in Magento.');
+            }
+
+            // create api role with full access
+            $role = Mage::getModel("api/roles")
+                    ->setId(false)
+                    ->setName(uniqid())
+                    ->setPid(false)
+                    ->setRoleType('G')
+                    ->save();
+            Mage::getModel("api/rules")
+                ->setRoleId($role->getId())
+                ->setResources(array('all'))
+                ->saveRel();
+
+            // create api user with pseudo-random id
+            $id = 'u' . preg_replace('/[^0-9]/', '', microtime());
+            $roles = array($role->getId());
+            $user = Mage::getModel('api/user')->setData(array (
+                'username'             => $id,
+                'firstname'            => $id,
+                'lastname'             => $id,
+                'email'                => "{$id}@example.com",
+                'api_key'              => $id,
+                'api_key_confirmation' => $id,
+                'is_active'            => 1,
+                'user_roles'           => '',
+                'page'                 => '1',
+                'limit'                => '20',
+                'assigned_user_role'   => '',
+                'role_name'            => '',
+                'roles'                => $roles,
+            ))->save();
+            $user->setRoleIds($roles)
+                 ->setRoleUserId($user->getUserId())
+                 ->saveRelations();
+
+            self::$_credentials = new WebService_Connector_Credentials($role, $user, $id, $url);
+        }
     }
 
-    public static  function getApiPassword()
+    public static function getApiLogin()
     {
-        return '';
+        self::_init();
+        return self::$_credentials->getApiKey();
     }
 
-    public static  function getRpcApiUrl()
+    public static function getApiPassword()
     {
-        return '';
+        self::_init();
+        return self::$_credentials->getApiConfirmation();
+    }
+
+    public static function getRpcApiUrl()
+    {
+        self::_init();
+        return self::$_credentials->getBaseUrl() . 'api/xmlrpc/';
     }
 
     public static function getSoapApiUrl()
     {
-        return '';
+        self::_init();
+        return self::$_credentials->getBaseUrl() . 'api/soap/?wsdl';
     }
 }
