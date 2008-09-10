5419455d58d1a01f82f3266ac1804117a9895fe7
Ruslan Voitenko <ruslan.voytenko@magento.com>
2008-09-10 14:33:24 +0000

    Test for customer address

--- tests/WebService/Customer/AddressTest.php
+++ tests/WebService/Customer/AddressTest.php
@@ -28,3 +28,58 @@ if (!defined('_IS_INCLUDED')) {
     require dirname(__FILE__) . '/../../PHPUnitTestInit.php';
     PHPUnitTestInit::runMe(__FILE__);
 }
+
+class WebService_Customer_AddressTest extends WebService_TestCase_Abstract
+{
+
+    /**
+     * @dataProvider connectorProvider
+     */
+    public function testList(WebService_Connector_Interface $connector)
+    {
+        $newCustomer = array(
+            'firstname'  => 'First',
+            'lastname'   => 'Last',
+            'email'      => 'test_'.uniqid().'@example.com',
+            'password'   => 'password',
+            'store_id'   => 0,
+            'website_id' => 0
+        );
+        $newCustomerId = $connector->call('customer.create', array($newCustomer));
+        $newCustomerAddress = array(
+            'firstname'  => 'First',
+            'lastname'   => 'Last',
+            'country_id' => 'USA',
+            'region_id'  => '43',
+            'region'     => 'New York',
+            'city'       => 'New York',
+            'street'     => array('Bla bla','bla bla'),
+            'telephone'  => '5555-555',
+            'postcode'   => 10021,
+
+            'is_default_billing'  => true,
+            'is_default_shipping' => true
+        );
+        $newCustomerAddressId = $connector->call('customer_address.create', array($newCustomerId, $newCustomerAddress));
+        $addressList = $connector->call('customer_address.list', $newCustomerId);
+
+        $addressFirstname = 'Changedfirst';
+        $customerAddressUpdate = array(
+            'firstname' => $addressFirstname
+        );
+        $connector->call('customer_address.update', array($newCustomerAddressId, $customerAddressUpdate));
+        $updatedAddressList = $connector->call('customer_address.list', $newCustomerId);
+
+        $customerInfo = $connector->call('customer_address.info', $newCustomerAddressId);
+
+        $connector->call('customer_address.delete', $newCustomerAddressId);
+        $connector->call('customer.delete', $newCustomerId);
+
+        $this->assertType('array', $addressList);
+        $this->assertTrue(isset($addressList[0]));
+        $this->assertTrue($addressList[0]['customer_id'] == $newCustomerId);
+        $this->assertTrue($updatedAddressList[0]['firstname'] == $addressFirstname);
+        $this->assertTrue(isset($customerInfo['customer_id']));
+        $this->assertTrue($customerInfo['customer_id'] == $newCustomerId);
+    }
+}
