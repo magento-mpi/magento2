72108392f4af0a548c57c549a306e8a3d456afae
Ruslan Voitenko <ruslan.voytenko@magento.com>
2008-11-07 14:58:48 +0000

    Fixed #7103: Cannot add more than 5 custom options for my simple item

--- app/code/core/Mage/Catalog/Model/Product/Option.php
+++ app/code/core/Mage/Catalog/Model/Product/Option.php
@@ -220,7 +220,7 @@ class Mage_Catalog_Model_Product_Option extends Mage_Core_Model_Abstract
             }
             $isEdit = (bool)$this->getId()? true:false;
 
-            if ($this->getData('is_delete') == '1') {// better strlen()
+            if ($this->getData('is_delete') == '1') {
                 if ($isEdit) {
                     $this->getValueInstance()->deleteValue($this->getId());
                     $this->deletePrices($this->getId());
@@ -228,7 +228,7 @@ class Mage_Catalog_Model_Product_Option extends Mage_Core_Model_Abstract
                     $this->delete();
                 }
             } else {
-                if ($this->getData('previous_type') != '') {//better strlen()
+                if ($this->getData('previous_type') != '') {
                     $previousType = $this->getData('previous_type');
                     //if previous option has dfferent group from one is came now need to remove all data of previous group
                     if ($this->getGroupByType($previousType) != $this->getGroupByType($this->getData('type'))) {
@@ -274,7 +274,7 @@ class Mage_Catalog_Model_Product_Option extends Mage_Core_Model_Abstract
 
             $this->getValueInstance()->setOption($this)
                 ->saveValues();
-        } else {
+        } elseif ($this->getGroupByType($this->getType()) == self::OPTION_GROUP_SELECT) {
             Mage::throwException(Mage::helper('catalog')->__('Select type options required values rows.'));
         }
 
