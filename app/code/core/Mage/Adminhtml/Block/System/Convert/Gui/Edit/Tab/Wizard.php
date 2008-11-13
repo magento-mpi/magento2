50eccf58450a28b0ecf666cf35cc515c0b528be4
Anton Makarenko <anton.makarenko@magento.com>
2008-11-13 13:46:59 +0000

    Fixed #8280: Enclosed Values

--- app/code/core/Mage/Adminhtml/Block/System/Convert/Gui/Edit/Tab/Wizard.php
+++ app/code/core/Mage/Adminhtml/Block/System/Convert/Gui/Edit/Tab/Wizard.php
@@ -78,7 +78,7 @@ class Mage_Adminhtml_Block_System_Convert_Gui_Edit_Tab_Wizard extends Mage_Admin
     public function getValue($key, $default='')
     {
         $value = $this->getData($key);
-        return htmlspecialchars(strlen($value) > 0 ? $value : $default);
+        return $this->htmlEscape(strlen($value) > 0 ? $value : $default);
     }
 
     public function getSelected($key, $value)
@@ -126,8 +126,6 @@ class Mage_Adminhtml_Block_System_Convert_Gui_Edit_Tab_Wizard extends Mage_Admin
 
     public function getProductAttributeSetFilterOptions()
     {
-    	
-    	
         $options = Mage::getResourceModel('eav/entity_attribute_set_collection')
             ->setEntityTypeFilter(Mage::getModel('catalog/product')->getResource()->getTypeId())
             ->load()
