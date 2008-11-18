ec6957efaadf3d70135871ad1a71306f1948860f
Victor Tihonchuk <noreply@magento.com>
2008-11-18 14:26:02 +0000

    Fixed Share Wishlist Email show disabled products

--- app/code/core/Mage/Wishlist/Block/Share/Email/Items.php
+++ app/code/core/Mage/Wishlist/Block/Share/Email/Items.php
@@ -55,7 +55,10 @@ class Mage_Wishlist_Block_Share_Email_Items extends Mage_Core_Block_Template
                 ->addAttributeToSelect('image')
                 ->addAttributeToSelect('small_image')
                 //->addAttributeToFilter('store_id', array('in'=>Mage::registry('wishlist')->getSharedStoreIds()))
-                ->addStoreFilter()
+                ->addStoreFilter();
+            Mage::getSingleton('catalog/product_visibility')
+                ->addVisibleInSiteFilterToCollection(Mage::registry('wishlist')->getProductCollection());
+            Mage::registry('wishlist')->getProductCollection()
                 ->load();
 
             $this->_wishlistLoaded = true;
