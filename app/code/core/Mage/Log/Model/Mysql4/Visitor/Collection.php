e2df454d7c5e12e4fac94faa07aae58ce5c768b4
Victor Tihonchuk <noreply@magento.com>
2008-11-26 19:54:00 +0000

    Fixed #8903: SQL error when sorting Online Customers by Type attribute

--- app/code/core/Mage/Log/Model/Mysql4/Visitor/Collection.php
+++ app/code/core/Mage/Log/Model/Mysql4/Visitor/Collection.php
@@ -149,6 +149,7 @@ class Mage_Log_Model_Mysql4_Visitor_Collection extends Varien_Data_Collection_Db
         $email  = $customersCollection->getAttribute('email');
 
         $this->_select
+            ->from('', array('type' => 'IF(customer_id, 1, 0)'))
             ->joinLeft(
                 array('customer_lastname_table'=>$lastname->getBackend()->getTable()),
                 'customer_lastname_table.entity_id=customer_table.customer_id
