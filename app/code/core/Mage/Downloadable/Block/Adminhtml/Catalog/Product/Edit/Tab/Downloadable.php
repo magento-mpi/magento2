538e06113e7ae1f7f5da28e22022e8b7e5c12a2a
Ruslan Voitenko <ruslan.voytenko@magento.com>
2008-12-02 19:21:57 +0000

    Added field of attribute to downloadable tab in admin

--- app/code/core/Mage/Downloadable/Block/Adminhtml/Catalog/Product/Edit/Tab/Downloadable.php
+++ app/code/core/Mage/Downloadable/Block/Adminhtml/Catalog/Product/Edit/Tab/Downloadable.php
@@ -82,7 +82,7 @@ class Mage_Downloadable_Block_Adminhtml_Catalog_Product_Edit_Tab_Downloadable ex
 
         $accordion->addItem('links', array(
             'title'   => Mage::helper('adminhtml')->__('Links'),
-            'content' => 'assume this is the primary content', // $this->getLayout()->createBlock('...')->toHtml(),
+            'content' => $this->getLayout()->createBlock('downloadable/adminhtml_catalog_product_edit_tab_downloadable_links')->toHtml(), // $this->getLayout()->createBlock('...')->toHtml(),
             'open'    => true
         ));
 
