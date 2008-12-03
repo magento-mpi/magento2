4434cd5b4520935e102e7d9707498d4e429a8bb4
Vitaliy Korotun <vitaliy.korotun@magento.com>
2008-12-03 09:15:55 +0000

    downloadable product (remove unnecessary changes from Catalog module)

--- app/code/core/Mage/Catalog/Model/Product/Type.php
+++ app/code/core/Mage/Catalog/Model/Product/Type.php
@@ -41,7 +41,6 @@ class Mage_Catalog_Model_Product_Type
     const TYPE_CONFIGURABLE = 'configurable';
     const TYPE_GROUPED      = 'grouped';
     const TYPE_VIRTUAL      = 'virtual';
-    const TYPE_DOWNLOADABLE = 'downloadable';
 
     const DEFAULT_TYPE      = 'simple';
     const DEFAULT_TYPE_MODEL    = 'catalog/product_type_simple';
