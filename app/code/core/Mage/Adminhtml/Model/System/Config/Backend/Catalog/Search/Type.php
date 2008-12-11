b93803969d4998bceb6a4183c3ee8893ead5c32a
Victor Tihonchuk <noreply@magento.com>
2008-12-11 18:55:43 +0000

    Fixed search type backend model

--- app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Catalog/Search/Type.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Catalog/Search/Type.php
@@ -32,7 +32,7 @@
  * @package    Mage_Adminhtml
  * @author     Magento Core Team <core@magentocommerce.com>
  */
-class Mage_Adminhtml_Model_System_Config_Catalog_Search_Type extends Mage_Core_Model_Config_Data
+class Mage_Adminhtml_Model_System_Config_Backend_Catalog_Search_Type extends Mage_Core_Model_Config_Data
 {
     /**
      * After change Catalog Search Type process
