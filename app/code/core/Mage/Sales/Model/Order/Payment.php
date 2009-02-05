e16543928cd965879f86a3f986c89cc5241edb16
Dmitriy Volik <dmitriy.volik@magento.com>
2009-02-05 17:10:30 +0000

    Zero Subtotal automatically invoice creating

--- app/code/core/Mage/Sales/Model/Order/Payment.php
+++ app/code/core/Mage/Sales/Model/Order/Payment.php
@@ -90,6 +90,16 @@ class Mage_Sales_Model_Order_Payment extends Mage_Payment_Model_Info
     }
 
     /**
+     * Check whether invoice is created automatically or not
+     *
+     * @return boolean
+     */
+    public function canCreateInvoice()
+    {
+        return $this->getMethodInstance()->getConfigData('auto_invoice_active');
+    }
+
+    /**
      * Place payment information
      *
      * This method are colling when order will be place
@@ -148,6 +158,16 @@ class Mage_Sales_Model_Order_Payment extends Mage_Payment_Model_Info
                         break;
                 }
             }
+        }
+
+        if ($this->canCreateInvoice()) {
+            $invoice = $this->getOrder()->prepareInvoice();
+            $invoice->register();
+            if ($methodInstance->canCapture()) {
+                $invoice->capture();
+            }
+            $this->getOrder()->setIsInProcess(true);
+            $this->getOrder()->addRelatedObject($invoice);
         }
 
         $orderIsNotified = null;
