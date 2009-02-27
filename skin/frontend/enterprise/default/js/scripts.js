5a5cfd348e1b800f49aa082a2ae0ef1dc0bf09b6
Evgeniy Kolesov <evgeniy.kolesov@magento.com>
2009-02-27 13:16:40 +0000

    Magento Enteprise Theme - mini-cart

--- skin/frontend/enterprise/default/js/scripts.js
+++ skin/frontend/enterprise/default/js/scripts.js
@@ -20,3 +20,31 @@
  * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
  * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
+
+function topCart(elC) {
+	var elC = $(elC);
+	var elC=elC;
+	var el=elC.parentNode;
+	var check=0;
+	var iTime = 2000;
+
+	el.onmouseout = function() {
+		tI=setTimeout("hideElement()",iTime);	
+	};
+	
+	el.onmouseover = function() {
+		if	(check==0)	{
+			elC.zIndex=999;
+			Effect.SlideDown(elC.id, { duration: 0.5 });			
+			check=1;
+			}
+		if (tI) {clearTimeout(tI);}	
+	};	
+
+	hideElement = function() {
+		Effect.SlideUp(elC.id, { duration: 0.5 });
+		if (tI) {clearTimeout(tI);}
+		elC.zIndex=1;	
+		check=0;
+	}	
+}
