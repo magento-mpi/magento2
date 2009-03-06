eb4fad5cf486f52f3cb54c3f7c19d1d4f9c99589
Dmitriy Vasilenko <dmitriy.vasilenko@magento.com>
2009-03-06 16:02:38 +0000

    fixing unique keys in catalog/product tables

--- app/code/core/Mage/Catalog/sql/catalog_setup/mysql4-upgrade-0.7.60-0.7.61.php
+++ app/code/core/Mage/Catalog/sql/catalog_setup/mysql4-upgrade-0.7.60-0.7.61.php
@@ -29,9 +29,60 @@ $installer = $this;
 
 $tableCatalogProduct = $this->getTable('catalog/product');
 
-$installer->getConnection()->addKey("{$tableCatalogProduct}_datetime", 'IDX_ATTRIBUTE_VALUE', array('entity_id', 'attribute_id', 'store_id'), 'unique');
-$installer->getConnection()->addKey("{$tableCatalogProduct}_decimal", 'IDX_ATTRIBUTE_VALUE', array('entity_id', 'attribute_id', 'store_id'), 'unique');
+$types = array('datetime', 'decimal', 'int', 'text', 'varchar');
+
+foreach ($types as $type) {
+    $tableName = $tableCatalogProduct . '_' . $type;
+
+    $select = $installer->getConnection()->select()
+        ->from($tableName, array(
+            'entity_id'         => 'entity_id',
+            'attribute_id'      => 'attribute_id',
+            'store_id'          => 'store_id',
+            'rows_count'        => 'COUNT(*)'))
+        ->group(array('entity_id', 'attribute_id', 'store_id'))
+        ->having('rows_count > 1');
+    $query = $installer->getConnection()->query($select);
+
+    while ($row = $query->fetch()) {
+        $sql = 'DELETE FROM `' . $tableName . '`'
+            . ' WHERE entity_id=? AND attribute_id=? AND store_id=?'
+            . ' LIMIT ' . ($row['rows_count'] - 1);
+        $installer->getConnection()->query($sql, array(
+            $row['entity_id'],
+            $row['attribute_id'],
+            $row['store_id']
+        ));
+    }
+
+    $installer->getConnection()->addKey($tableName, 'IDX_ATTRIBUTE_VALUE', array('entity_id', 'attribute_id', 'store_id'), 'unique');
+}
+
+// exception for gallery table
+
+$tableName = $tableCatalogProduct . '_gallery';
+
+$select = $installer->getConnection()->select()
+    ->from($tableName, array(
+        'entity_type_id'    => 'entity_type_id',
+        'entity_id'         => 'entity_id',
+        'attribute_id'      => 'attribute_id',
+        'store_id'          => 'store_id',
+        'rows_count'        => 'COUNT(*)'))
+    ->group(array('entity_id', 'attribute_id', 'store_id'))
+    ->having('rows_count > 1');
+$query = $installer->getConnection()->query($select);
+
+while ($row = $query->fetch()) {
+    $sql = 'DELETE FROM `' . $tableName . '`'
+        . ' WHERE entity_type_id=? AND entity_id=? AND attribute_id=? AND store_id=?'
+        . ' LIMIT ' . ($row['rows_count'] - 1);
+    $installer->getConnection()->query($sql, array(
+        $row['entity_type_id'],
+        $row['entity_id'],
+        $row['attribute_id'],
+        $row['store_id']
+    ));
+}
+
 $installer->getConnection()->addKey("{$tableCatalogProduct}_gallery", 'IDX_BASE', array('entity_type_id','entity_id','attribute_id','store_id'), 'unique');
-$installer->getConnection()->addKey("{$tableCatalogProduct}_int", 'IDX_ATTRIBUTE_VALUE', array('entity_id', 'attribute_id', 'store_id'), 'unique');
-$installer->getConnection()->addKey("{$tableCatalogProduct}_text", 'IDX_ATTRIBUTE_VALUE', array('entity_id', 'attribute_id', 'store_id'), 'unique');
-$installer->getConnection()->addKey("{$tableCatalogProduct}_varchar", 'IDX_ATTRIBUTE_VALUE', array('entity_id', 'attribute_id', 'store_id'), 'unique');
