b4c4cafb0edc9a540a9e60cb1457566490497ae9
Victor Tihonchuk <noreply@magento.com>
2009-03-11 12:04:10 +0000

    Add to product Stock Item as Simple Varien_Object with Stock Status Data

--- app/code/core/Mage/CatalogInventory/Model/Stock/Status.php
+++ app/code/core/Mage/CatalogInventory/Model/Stock/Status.php
@@ -471,6 +471,13 @@ class Mage_CatalogInventory_Model_Stock_Status extends Mage_Core_Model_Abstract
                 }
             }
         }
+
+        /* back compatible stock item */
+        foreach ($productCollection as $product) {
+            $object = new Varien_Object(array('is_salable' => $product->getData('is_salable')));
+            $product->setStockItem($object);
+        }
+
         return $this;
     }
 }
