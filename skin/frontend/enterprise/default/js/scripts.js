0a560293e7000ce7c46e1133408d896388ca402b
Evgeniy Kolesov <evgeniy.kolesov@magento.com>
2009-03-19 11:05:51 +0000

    MEP Enterprise theme html/css/js

--- skin/frontend/enterprise/default/js/scripts.js
+++ skin/frontend/enterprise/default/js/scripts.js
@@ -26,52 +26,59 @@ if (!window.Enterprise) {
     window.Enterprise = {};
 }
 Enterprise.TopCart= {
-     initialize: function (container) {
-         this.container = $(container);
-         this.element = this.container.up(0);
-         this.elementHeader = this.container.previous(0);
-         this.check = 0;
-         this.intervalDuration = 4000;
-         this.interval = null;
-         this.onElementMouseOut = this.handleMouseOut.bindAsEventListener(this);
-         this.onElementMouseOver = this.handleMouseOver.bindAsEventListener(this);
-         this.onElementMouseClick = this.handleMouseClick.bindAsEventListener(this);
-         
-         this.element.observe('mouseout', this.onElementMouseOut);
-         this.element.observe('mouseover', this.onElementMouseOver);
-         this.elementHeader.observe('click', this.onElementMouseClick);
-         
-     },
-     handleMouseOut: function (evt) {
-        if($(this.elementHeader).hasClassName('expanded')) {
-            this.interval = setTimeout(this.hideElement.bind(this), this.intervalDuration);
-        }
-     },
+    initialize: function (container) {
+        this.container = $(container);
+        this.element = this.container.up(0);
+        this.elementHeader = this.container.previous(0);
+        this.intervalDuration = 4000;
+        this.interval = null;
+        this.onElementMouseOut = this.handleMouseOut.bindAsEventListener(this);
+        this.onElementMouseOver = this.handleMouseOver.bindAsEventListener(this);
+        this.onElementMouseClick = this.handleMouseClick.bindAsEventListener(this);
+
+        this.element.observe('mouseout', this.onElementMouseOut);
+        this.element.observe('mouseover', this.onElementMouseOver);
+        this.elementHeader.observe('click', this.onElementMouseClick);
      
-     handleMouseOver: function (evt) {
+    },
+
+    handleMouseOut: function (evt) {
+        if($(this.elementHeader).hasClassName('expanded')) {
+            this.interval = setTimeout(this.hideCart.bind(this), this.intervalDuration);
+        }
+    },
+
+    handleMouseOver: function (evt) {
         if (this.interval !== null) {
              clearTimeout(this.interval);
              this.interval = null;
         }
-     },
-     
-     handleMouseClick: function (evt) {
+    },
+
+    handleMouseClick: function (evt) {
         if (!$(this.elementHeader).hasClassName('expanded') && !$(this.container.id).hasClassName('process') )  {
-            this.check=1;
-            this.container.parentNode.style.zIndex=992;
-            new Effect.SlideDown(this.container.id, { duration: 0.5,
-                beforeStart: function(effect) {$( effect.element.id ).addClassName('process');}, 
-                afterFinish: function(effect) {$( effect.element.id ).removeClassName('process'); } 
-                });
-            $(this.elementHeader).addClassName('expanded');
+            this.showCart();
         }
         else {
-            this.hideElement();
+            this.hideCart();
         }
-     },     
+    },     
      
-     hideElement: function () {
+    showCart: function (timePeriod) {
+        this.container.parentNode.style.zIndex=992;
+        new Effect.SlideDown(this.container.id, { duration: 0.5,
+            beforeStart: function(effect) {$( effect.element.id ).addClassName('process');}, 
+            afterFinish: function(effect) {$( effect.element.id ).removeClassName('process'); } 
+            });
+        $(this.elementHeader).addClassName('expanded');
+        if(timePeriod) {
+            this.timePeriod = timePeriod*1000;
+            this.interval = setTimeout(this.hideCart.bind(this), this.timePeriod);
+        }        
+    },
      
+    hideCart: function () {
+
         if (!$(this.container.id).hasClassName('process') && $(this.elementHeader).hasClassName('expanded')) {     
             new Effect.SlideUp(this.container.id, { duration: 0.5,
                 beforeStart: function(effect) {$( effect.element.id ).addClassName('process');}, 
@@ -86,8 +93,7 @@ Enterprise.TopCart= {
             this.interval = null;
         }
         $(this.elementHeader).removeClassName('expanded');
-        this.check = 0;
-     }
+    }
 };
 
 Enterprise.Bundle = {
