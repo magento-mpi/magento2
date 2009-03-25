71b44e91beac1cd2d237270d5a02f7f17bfb9d5b
Alex Karanda <alex.karanda@magento.com>
2009-03-25 13:54:17 +0000

    backup model

--- app/code/core/Enterprise/Staging/Model/Mysql4/Staging/Backup.php
+++ app/code/core/Enterprise/Staging/Model/Mysql4/Staging/Backup.php
@@ -24,11 +24,11 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class Enterprise_Staging_Model_Mysql4_Staging_Event extends Mage_Core_Model_Mysql4_Abstract
+class Enterprise_Staging_Model_Mysql4_Staging_Backup extends Mage_Core_Model_Mysql4_Abstract
 {
     protected function _construct()
     {
-        $this->_init('enterprise_staging/staging_event', 'event_id');
+        $this->_init('enterprise_staging/staging_backup', 'backup_id');
     }
 
     /**
