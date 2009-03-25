faf314311fc6863d13e0ed50b6801e2a40be9177
Alex Karanda <alex.karanda@magento.com>
2009-03-25 12:45:41 +0000

    new models for Rollbeck implementation

---
+++ app/code/core/Enterprise/Staging/Model/Mysql4/Staging/Backup.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Enterprise
+ * @package    Enterprise_Staging
+ * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Enterprise_Staging_Model_Mysql4_Staging_Event extends Mage_Core_Model_Mysql4_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('enterprise_staging/staging_event', 'event_id');
+    }
+
+    /**
+     * Before save processing
+     *
+     * @param   Mage_Core_Model_Abstract $object
+     * @return  Enterprise_Staging_Model_Mysql4_Staging_Event
+     */
+    protected function _beforeSave(Mage_Core_Model_Abstract $object)
+    {
+        $staging = $object->getStaging();
+        if ($staging instanceof Enterprise_Staging_Model_Staging) {
+            if ($staging->getId()) {
+                $object->setStagingId($staging->getId());
+            }
+        }
+
+        if (!$object->getId()) {
+            $value = Mage::app()->getLocale()->date()->toString("YYYY-MM-dd HH:mm:ss");
+            $object->setCreatedAt($value);
+        } else {
+            $value = Mage::app()->getLocale()->date()->toString("YYYY-MM-dd HH:mm:ss");
+            $object->setUpdatedAt($value);
+        }
+
+        $ip = Mage::app()->getRequest()->getServer('REMOTE_ADDR');
+        $object->setIp($ip);
+
+        $user = Mage::getSingleton('admin/session')->getUser();
+        if ($user) {
+            $object->setUserId($user->getId());
+            $object->setUsername($user->getName());
+        }
+
+        parent::_beforeSave($object);
+
+        return $this;
+    }
+}
---
+++ app/code/core/Enterprise/Staging/Model/Staging/Backup.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Enterprise
+ * @package    Enterprise_Staging
+ * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+class Enterprise_Staging_Model_Staging_Event extends Mage_Core_Model_Abstract
+{
+    /**
+     * Staging instance
+     *
+     * @var Enterprise_Staging_Model_Staging
+     */
+    protected $_staging;
+
+    protected function _construct()
+    {
+        $this->_init('enterprise_staging/staging_event');
+    }
+
+    /**
+     * Declare staging instance
+     *
+     * @param   Enterprise_Staging_Model_Staging $staging
+     * @return  Enterprise_Staging_Model_Staging_Event_History
+     */
+    public function setStaging(Enterprise_Staging_Model_Staging $staging)
+    {
+        $this->_staging = $staging;
+        return $this;
+    }
+
+    /**
+     * Retrieve staging instance
+     *
+     * @return Enterprise_Staging_Model_Staging
+     */
+    public function getStaging()
+    {
+        if (!$this->_staging instanceof Enterprise_Staging_Model_Staging) {
+            $this->_staging = Mage::registry('staging');
+        }
+        return $this->_staging;
+    }
+
+    /**
+     * Retrieve event state label
+     *
+     * @return string
+     */
+    public function getStateLabel()
+    {
+        return Enterprise_Staging_Model_Staging_Config::getStateLabel($this->getState());
+    }
+
+    /**
+     * Retrieve event status label
+     *
+     * @return string
+     */
+    public function getStatusLabel()
+    {
+        return Enterprise_Staging_Model_Staging_Config::getStatusLabel($this->getStatus());
+    }
+
+    /**
+     * Retrieve event label
+     *
+     * @return string
+     */
+    public function getFrontendLabel()
+    {
+        return Enterprise_Staging_Model_Staging_Config::getEventLabel($this->getCode());
+    }
+
+    public function updateAttribute($attribute, $value)
+    {
+        return $this->getResource()->updateAttribute($this, $attribute, $value);
+    }
+    
+    public function restoreMap()
+    {
+        $map = $this->getMergeMap();
+        if (!empty($map)) {
+            $this->getStaging()->getMapperInstance()->unserialize($map);
+        }
+        return $this;
+    }
+}
