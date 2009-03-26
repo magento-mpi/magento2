719d5b023ae9b191963f206a659ed1f061a440b1
Michael Bessolov <michael.bessolov@magento.com>
2009-03-26 13:33:32 +0000

    Removed empty catch {} in upgrade file

--- app/code/core/Mage/Catalog/sql/catalog_setup/mysql4-upgrade-0.7.61-0.7.62.php
+++ app/code/core/Mage/Catalog/sql/catalog_setup/mysql4-upgrade-0.7.61-0.7.62.php
@@ -28,47 +28,29 @@ $installer = $this;
 /* @var $installer Mage_Eav_Model_Entity_Setup */
 $installer->startSetup();
 
-/* @table catalog_product_super_attribute_label */
-try {
-    $installer->getConnection()->addKey(
-        $installer->getTable('catalog_product_super_attribute_label'), 
-        'IDX_CATALOG_PRODUCT_SUPER_ATTRIBUTE_STORE_PSAI_SI', 
-        array('product_super_attribute_id', 'store_id'));
-} catch (Exception $e) {}
-    
-    
-/* @table catalog_product_option_price  */
-try {
-    $installer->getConnection()->addKey(
-        $installer->getTable('catalog_product_option_price'), 
-        'IDX_CATALOG_PRODUCT_OPTION_PRICE_SI_OI', 
-        array('store_id', 'option_id'));
-} catch(Exception $e) {}        
-    
+$installer->getConnection()->addKey(
+    $installer->getTable('catalog_product_super_attribute_label'),
+    'IDX_CATALOG_PRODUCT_SUPER_ATTRIBUTE_STORE_PSAI_SI',
+    array('product_super_attribute_id', 'store_id'));
 
-/* @table catalog_product_option_title  */
-try {
-    $installer->getConnection()->addKey(
-        $installer->getTable('catalog_product_option_title'), 
-        'IDX_CATALOG_PRODUCT_OPTION_TITLE_SI_OI', 
-        array('store_id', 'option_id'));
-} catch (Exception $e) {}    
+$installer->getConnection()->addKey(
+    $installer->getTable('catalog_product_option_price'),
+    'IDX_CATALOG_PRODUCT_OPTION_PRICE_SI_OI',
+    array('store_id', 'option_id'));
 
-/* @table catalog_product_option_type_price  */
-try {
-    $installer->getConnection()->addKey(
-        $installer->getTable('catalog_product_option_type_price'), 
-        'IDX_CATALOG_PRODUCT_OPTION_TYPE_PRICE_SI_OTI', 
-        array('store_id', 'option_type_id'));
-}catch (Exception $e) {}    
-    
-    
-/* @table catalog_product_option_type_title  */
-try{
-    $installer->getConnection()->addKey(
-        $installer->getTable('catalog_product_option_type_title'), 
-        'IDX_CATALOG_PRODUCT_OPTION_TYPE_TITLE_SI_OTI', 
-        array('store_id', 'option_type_id'));
-}catch (Exception $e) {}    
-    
+$installer->getConnection()->addKey(
+    $installer->getTable('catalog_product_option_title'),
+    'IDX_CATALOG_PRODUCT_OPTION_TITLE_SI_OI',
+    array('store_id', 'option_id'));
+
+$installer->getConnection()->addKey(
+    $installer->getTable('catalog_product_option_type_price'),
+    'IDX_CATALOG_PRODUCT_OPTION_TYPE_PRICE_SI_OTI',
+    array('store_id', 'option_type_id'));
+
+$installer->getConnection()->addKey(
+    $installer->getTable('catalog_product_option_type_title'),
+    'IDX_CATALOG_PRODUCT_OPTION_TYPE_TITLE_SI_OTI',
+    array('store_id', 'option_type_id'));
+
 $installer->endSetup();
