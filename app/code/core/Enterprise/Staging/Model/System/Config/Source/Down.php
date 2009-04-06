cbbf730a249c0dd9fdc0c8e4337d95b72b95ea0e
Anton Makarenko <anton.makarenko@magento.com>
2009-04-06 10:47:57 +0000

    MEP Enterprise_Pci - fixed ctrl+c in helper call, Enterprise_Staging - changed configuration value of frontend stub

--- app/code/core/Enterprise/Staging/Model/System/Config/Source/Down.php
+++ app/code/core/Enterprise/Staging/Model/System/Config/Source/Down.php
@@ -4,10 +4,10 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE_AFL.txt.
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
+ * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@magentocommerce.com so we can send you a copy immediately.
@@ -19,16 +19,16 @@
  * needs please refer to http://www.magentocommerce.com for more information.
  *
  * @category   Enterprise
- * @package    Enterprise_Pci
+ * @package    Enterprise_Staging
  * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
- * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
- * Source model for admin password change mode
+ * Options for staging frontend blocking
  *
  */
-class Enterprise_Pci_Model_System_Config_Source_Password extends Varien_Object
+class Enterprise_Staging_Model_System_Config_Source_Down extends Varien_Object
 {
     /**
      * Get options for select
@@ -40,11 +40,15 @@ class Enterprise_Pci_Model_System_Config_Source_Password extends Varien_Object
         return array(
             array(
                 'value' => 0,
-                'label' => Mage::helper('enterprise_pci')->__('Recommended'),
+                'label' => Mage::helper('enterprise_staging')->__('No'),
             ),
             array(
                 'value' => 1,
-                'label' => Mage::helper('enterprise_pci')->__('Forced'),
+                'label' => Mage::helper('enterprise_staging')->__('Take down entire frontend'),
+            ),
+            array(
+                'value' => 2,
+                'label' => Mage::helper('enterprise_staging')->__('Take down only relevant websites'),
             ),
         );
     }
