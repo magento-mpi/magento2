8a6388b9028cbd28302a89b6d28b268c80bcca8a
Victor Tihonchuk <noreply@magento.com>
2009-04-14 15:51:31 +0000

    Modify Log upgrade (sample data small fix)

--- app/code/core/Mage/Log/sql/log_setup/mysql4-upgrade-0.7.3-0.7.4.php
+++ app/code/core/Mage/Log/sql/log_setup/mysql4-upgrade-0.7.3-0.7.4.php
@@ -30,6 +30,9 @@ $installer = $this;
 
 $installer->startSetup();
 $installer->getConnection()->addKey($installer->getTable('log/customer'), 'IDX_VISITOR', 'visitor_id');
+$installer->getConnection()->run("
+DELETE `{$installer->getTable('log/url_table')}` FROM WHERE `url_id`=1 AND `visitor_id`
+");
 $installer->getConnection()->addKey($installer->getTable('log/url_table'), 'PRIMARY', 'url_id', 'primary');
 $installer->getConnection()->addKey($installer->getTable('log/url_table'), 'IDX_VISITOR', 'visitor_id');
 $installer->endSetup();
