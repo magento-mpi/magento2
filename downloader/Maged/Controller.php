ba10f3a376560db7f2dc77f52b4f2a9ab5400a09
Michael Bessolov <michael.bessolov@magento.com>
2009-04-18 02:07:27 +0000

    Updated downloader version

--- downloader/Maged/Controller.php
+++ downloader/Maged/Controller.php
@@ -177,11 +177,11 @@ final class Maged_Controller
     {
         if (!self::$_instance) {
             self::$_instance = new self;
-            if (!class_exists('Mage', false)) {
-                include_once self::$_instance->getMageFilename();
+            if (class_exists('Mage') && Mage::isInstalled()) {
+                // initialize Magento Config
+                Mage::app();
+                Mage::getSingleton('adminhtml/url')->turnOffSecretKey();
             }
-            Mage::app();
-            Mage::getSingleton('adminhtml/url')->turnOffSecretKey();
         }
         return self::$_instance;
     }
