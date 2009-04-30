a5b55be1d1bbe68bbf1c735e92573a4137339962
Vitaliy Golomoziy <vitaliy.golomoziy@magento.com>
2009-04-30 10:21:44 +0000

    MEP: 0013308: ip 0.0.0.0 in logging and staging history

---
+++ app/code/core/Enterprise/Logging/sql/enterprise_logging_setup/mysql4-upgrade-0.1.4-0.1.5.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Magento Enterprise Edition
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magento Enterprise Edition License
+ * that is bundled with this package in the file LICENSE_EE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.magentocommerce.com/license/enterprise-edition
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Enterprise
+ * @package    Enterprise_Logging
+ * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://www.magentocommerce.com/license/enterprise-edition
+ */
+
+
+$installer = $this;
+/* @var $installer Mage_Eav_Model_Entity_Setup */
+$installer->startSetup();
+
+$installer->run("ALTER TABLE `".$this->getTable('enterprise_logging/event')."` MODIFY `ip` bigint(20) NOT NULL default '0'");;
+
+$installer->endSetup();
