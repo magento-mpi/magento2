931e239843dff8b00dc4271eb221ea8c881d80fa
Victor Tihonchuk <noreply@magento.com>
2009-05-27 17:46:32 +0000

    Remove debug info from unused file :)

--- app/code/core/Mage/CatalogIndex/Model/Indexer/Tierprice.php
+++ app/code/core/Mage/CatalogIndex/Model/Indexer/Tierprice.php
@@ -45,8 +45,6 @@ class Mage_CatalogIndex_Model_Indexer_Tierprice extends Mage_CatalogIndex_Model_
 
     public function createIndexData(Mage_Catalog_Model_Product $object, Mage_Eav_Model_Entity_Attribute_Abstract $attribute = null)
     {
-        die('000');
-        Mage::log( mageDebugBacktrace(true));
         $data = array();
 
         $data['store_id'] = $attribute->getStoreId();
