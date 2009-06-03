486d2646aa71c047ee75c66f5460d17e2d5dbce5
Dmitriy Soroka <dmitriy.soroka@magento.com>
2009-06-03 12:04:37 +0000

    Revert changes related with new url rewrites processing

--- app/code/core/Mage/Core/Model/Url.php
+++ app/code/core/Mage/Core/Model/Url.php
@@ -342,7 +342,7 @@ class Mage_Core_Model_Url extends Varien_Object
 
         $route = array_shift($a);
         if ('*'===$route) {
-            $route = $this->getRequest()->getRequestedRouteName();
+            $route = $this->getRequest()->getRouteName();
         }
         $this->setRouteName($route);
         $routePath = $route.'/';
@@ -350,7 +350,7 @@ class Mage_Core_Model_Url extends Varien_Object
         if (!empty($a)) {
             $controller = array_shift($a);
             if ('*'===$controller) {
-                $controller = $this->getRequest()->getRequestedControllerName();
+                $controller = $this->getRequest()->getControllerName();
             }
             $this->setControllerName($controller);
             $routePath .= $controller.'/';
@@ -359,7 +359,7 @@ class Mage_Core_Model_Url extends Varien_Object
         if (!empty($a)) {
             $action = array_shift($a);
             if ('*'===$action) {
-                $action = $this->getRequest()->getRequestedActionName();
+                $action = $this->getRequest()->getActionName();
             }
             $this->setActionName($action);
             $routePath .= $action.'/';
