3ebf11ac935e066211c8d041512e0f7d519d7223
Anton Makarenko <anton.makarenko@magento.com>
2009-06-04 23:07:32 +0000

    Upgraded core files checksum scanner to make more usable

--- tools/support/check.php
+++ tools/support/check.php
@@ -1,5 +1,4 @@
 <?php
-
 function getCheckSum($dir = './', $recursive=true)
 {
     $result = array();
@@ -8,39 +7,69 @@ function getCheckSum($dir = './', $recursive=true)
         if (in_array($file->getFilename(), array('.', '..', '.svn'))) {
         	continue;
         }
-        if (!strpos($file->getPathname(), '/core') 
-            && !strpos($file->getPathname(), '/js')
-            && !strpos($file->getPathname(), '/app')
-            && !strpos($file->getPathname(), '/lib')) {
+        if (!strpos($file->getPathname(), DS . 'core')
+            && !strpos($file->getPathname(), DS . 'js')
+            && !strpos($file->getPathname(), DS . 'app')
+            && !strpos($file->getPathname(), DS . 'lib')) {
         	continue;
         }
-	    if ($file->isDir() && $recursive) {
+        if ($file->isFile()) {
+            $result[$file->getPathname()] = sha1_file($file->getPathname());
+        }
+	    elseif ($file->isDir() && $recursive) {
 	        $res = getCheckSum($file->getPathname());
 	        $result = array_merge($result, $res);
-	    }
-	    else {
-	    	$result[$file->getPathname()] = sha1_file($file->getPathname());
 	    }
     }
     return $result;
 }
 
-$result = getCheckSum();
+require './app/Mage.php';
 
-/**
- * Save information about source
- */
-//file_put_contents('./check/trunk.res', serialize($result));
+$version     = Mage::getVersion();
+$fileLocal   = "./var/checksum.{$version}.local";
+$localResult = false;
+$isWritten   = false;
+$fileRef     = "./var/checksum.{$version}.ref";
+$refResult   = false;
+$diff        = false;
+$error       = false;
 
-/**
- * Compare source with checking code
- */
-//$sourceResult = unserialize(file_get_contents('./check/magento-1.1.19700.res'));
-//
-//$sourceDiff = array_diff_assoc($sourceResult, $result);
-//$targetDiff = array_diff_assoc($result, $sourceResult);
-//
-//echo '<h1>Changed Core Files (count:'.count($sourceDiff).')</h1>';
-//foreach ($sourceDiff as $file => $sum) {
-//	echo $file.'</br>';
-//}
+try {
+    if (file_exists($fileLocal)) {
+        $localResult = unserialize(file_get_contents($fileLocal))  ;
+    }
+    else {
+        $localResult = getCheckSum();
+        $isWritten = @file_put_contents($fileLocal, serialize($localResult));
+    }
+    if (!is_writeable('./var')) {
+        $error = 'Please make folder ./var writeable.';
+    }
+    else {
+
+    }
+    if (!file_exists($fileRef)) {
+        throw new Exception("Please put reference checksum to {$fileRef}");
+    }
+    $refResult = unserialize(file_get_contents($fileRef));
+    $diff = array_diff_assoc($localResult, $refResult);
+}
+catch (Exception $e) {
+    $error = $e->getMessage();
+}
+
+header('Content-Type: text/plain; charset=UTF-8');
+printf("Version:            {$version}
+Local checksum:     %s
+Reference checksum: %s
+Diff:               %s\n",
+    (false !== $localResult ? 'OK, ' . ($isWritten ? "has been scanned and written to {$fileLocal}" : "has been read from {$fileLocal}. To scan again, remove it.") : 'N/A'),
+    (false !== $refResult ? $fileRef : 'N/A'),
+    (false !== $diff ? ($diff ? "\n" . implode("\n", array_keys($diff)) : 'all files match reference.' ) : 'N/A')
+);
+if ($error) {
+    print "\n{$error}";
+}
+
+exit;
