edca15c54c8af32bc70ea0e31fd26c03b6ea0d9a
Ruslan Voitenko <ruslan.voytenko@magento.com>
2009-06-16 12:54:38 +0000

    Fixed #14683: On Customer Information page, Configurable product shown does not correctly.

--- app/code/core/Mage/Adminhtml/Block/Customer/Edit/Tab/View/Accordion.php
+++ app/code/core/Mage/Adminhtml/Block/Customer/Edit/Tab/View/Accordion.php
@@ -52,7 +52,9 @@ class Mage_Adminhtml_Block_Customer_Edit_Tab_View_Accordion extends Mage_Adminht
             // count cart items
             $cartItemsCount = Mage::getModel('sales/quote')
                 ->setWebsite($website)->loadByCustomer($customer)
-                ->getItemsCollection(false)->getSize();
+                ->getItemsCollection(false)
+                ->addFieldToFilter('parent_item_id', array('null' => true))
+                ->getSize();
             // prepare title for cart
             $title = Mage::helper('customer')->__('Shopping Cart - %d item(s)', $cartItemsCount);
             if (count($customer->getSharedWebsiteIds()) > 1) {
