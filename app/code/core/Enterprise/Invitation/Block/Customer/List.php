c769a7dd7907c5d6099c4cf3848fe62f6a5cd085
Anton Makarenko <anton.makarenko@magento.com>
2009-06-18 10:06:35 +0000

    MEP Fixed #14733: Invitation emails representation on the website

--- app/code/core/Enterprise/Invitation/Block/Customer/List.php
+++ app/code/core/Enterprise/Invitation/Block/Customer/List.php
@@ -40,8 +40,8 @@ class Enterprise_Invitation_Block_Customer_List extends Mage_Customer_Block_Acco
     public function getInvitationCollection()
     {
         if (!$this->hasInvitationCollection()) {
-            $this->setData('invitation_collection', Mage::getModel('enterprise_invitation/invitation')
-                ->getCollection()
+            $this->setData('invitation_collection', Mage::getModel('enterprise_invitation/invitation')->getCollection()
+                ->addOrder('invitation_id', Varien_Data_Collection::SORT_ORDER_DESC)
                 ->loadByCustomerId(Mage::getSingleton('customer/session')->getCustomerId())
             );
         }
