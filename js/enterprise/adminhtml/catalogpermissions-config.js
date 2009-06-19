b87c75972fcb8c51e7d589f3c0f152a9e5bbeb07
Dmitriy Vasilenko <dmitriy.vasilenko@magento.com>
2009-06-19 15:56:36 +0000

    Fixed #14737: Landing Page in Category Permissions functionality

--- js/enterprise/adminhtml/catalogpermissions-config.js
+++ js/enterprise/adminhtml/catalogpermissions-config.js
@@ -20,37 +20,46 @@
  * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
  * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
- 
- if (!window.Enterprise) {
-     window.Enterprise = {};
- }
- 
- if (!Enterprise.CatalogPermissions) {
+
+if (!window.Enterprise) {
+    window.Enterprise = {};
+}
+
+if (!Enterprise.CatalogPermissions) {
     Enterprise.CatalogPermissions = {};
- }
- 
- Enterprise.CatalogPermissions.Config = Class.create();
- 
- Object.extend(Enterprise.CatalogPermissions.Config.prototype, {
-     initialize: function () {
-         Event.observe(window.document, 'dom:loaded', this.handleDomLoaded.bindAsEventListener(this));
-     },
-     handleDomLoaded: function () {
-         $$('.enterprise-grant-select').each(function(element) {
-                element.observe('change', this.updateFields.bind(this));
-         }, this);
-         
-         this.updateFields();
-     },
-     updateFields: function() {
-         $$('.enterprise-grant-select').each(function(element) {
-             if (parseInt(element.value) !== 2) {
-                 element.up('tr').next('tr').hide();
-             } else {
-                 element.up('tr').next('tr').show();
-             }
-         });
+}
+
+Enterprise.CatalogPermissions.Config = Class.create();
+
+Object.extend(Enterprise.CatalogPermissions.Config.prototype, {
+    initialize: function () {
+        Event.observe(window.document, 'dom:loaded', this.handleDomLoaded.bindAsEventListener(this));
+    },
+    handleDomLoaded: function () {
+        $$('.enterprise-grant-select').each(function(element) {
+            element.observe('change', this.updateFields.bind(this));
+        }, this);
+
+        this.updateFields();
+    },
+
+    updateFields: function() {
+        $$('.enterprise-grant-select').each(function(element) {
+            if (parseInt(element.value) !== 2) {
+                element.up('tr').next('tr').hide();
+            } else {
+                element.up('tr').next('tr').show();
+            }
+
+            if (element.hasClassName('browsing-catagories')) {
+                if (parseInt(element.value) === 1) {
+                    element.up('tr').next('tr', 1).hide();
+                } else {
+                    element.up('tr').next('tr', 1).show();
+                }
+            }
+        });
      }
- });
- 
- new Enterprise.CatalogPermissions.Config();
+});
+
+new Enterprise.CatalogPermissions.Config();
