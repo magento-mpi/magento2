b7bff31c6aa1243f836b427bb8960be16db9c77f
Ruslan Voitenko <ruslan.voytenko@magento.com>
2009-06-23 10:18:24 +0000

    Fixed #14742: catalog_product_attribute_tier_price.update

--- app/code/core/Mage/Catalog/Model/Product/Attribute/Tierprice/Api/V2.php
+++ app/code/core/Mage/Catalog/Model/Product/Attribute/Tierprice/Api/V2.php
@@ -66,6 +66,10 @@ class Mage_Catalog_Model_Product_Attribute_Tierprice_Api_V2 extends Mage_Catalog
                 }
             }
 
+            if (intval($tierPrice->website) > 0 && !in_array($tierPrice->website, $product->getWebsiteIds())) {
+                $this->_fault('data_invalid', Mage::helper('catalog')->__('Invalid tier prices. Product is not associated to the requested website.'));
+            }
+
             if (!isset($tierPrice->customer_group_id)) {
                 $tierPrice->customer_group_id = 'all';
             }
