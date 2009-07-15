e14ba50fc837f95927dd564e05b36647540d1b9f
Dmitriy Soroka <dmitriy.soroka@magento.com>
2009-07-15 10:20:07 +0000

    Fixed collectors collect process and mark old total models as deprecated

--- app/code/core/Mage/Sales/Model/Quote/Address/Total/Collector.php
+++ app/code/core/Mage/Sales/Model/Quote/Address/Total/Collector.php
@@ -32,6 +32,7 @@ class Mage_Sales_Model_Quote_Address_Total_Collector
     protected $_retrievers  = array();
     protected $_store;
 
+
     public function __construct($options)
     {
         if (isset($options['store'])) {
@@ -120,6 +121,56 @@ class Mage_Sales_Model_Quote_Address_Total_Collector
     }
 
     /**
+     * Aggregate before/after information from all items and sort totals based on this data
+     *
+     * @return array
+     */
+    protected function _getSortedCollectorCodes()
+    {
+        if (Mage::app()->useCache('config')) {
+            $cachedData = Mage::app()->loadCache('sorted_quote_collectors');
+            if ($cachedData) {
+                return unserialize($cachedData);
+            }
+        }
+        $configArray = $this->_modelsConfig;
+        foreach ($configArray as $code => $data) {
+            foreach ($data['before'] as $beforeCode) {
+                if (!isset($configArray[$beforeCode])) {
+                    continue;
+                }
+                $configArray[$code]['before'] = array_merge(
+                    $configArray[$code]['before'], $configArray[$beforeCode]['before']
+                );
+                $configArray[$beforeCode]['after']  = array_merge(
+                    $configArray[$beforeCode]['after'], array($code), $data['after']
+                );
+                $configArray[$beforeCode]['after']  = array_unique($configArray[$beforeCode]['after']);
+            }
+            foreach ($data['after'] as $afterCode) {
+                if (!isset($configArray[$afterCode])) {
+                    continue;
+                }
+                $configArray[$code]['after'] = array_merge(
+                    $configArray[$code]['after'], $configArray[$afterCode]['after']
+                );
+                $configArray[$afterCode]['before'] = array_merge(
+                    $configArray[$afterCode]['before'], array($code), $data['before']
+                );
+                $configArray[$afterCode]['before'] = array_unique($configArray[$afterCode]['before']);
+            }
+        }
+        uasort($configArray, array($this, '_compareTotals'));
+        $sortedCollectors = array_keys($configArray);
+        if (Mage::app()->useCache('config')) {
+            Mage::app()->saveCache(serialize($sortedCollectors), 'sorted_quote_collectors', array(
+                Mage_Core_Model_Config::CACHE_TAG
+            ));
+        }
+        return $sortedCollectors;
+    }
+
+    /**
      * Initialize collectors array.
      * Collectors array is array of total models ordered based on configuration settings
      *
@@ -127,15 +178,10 @@ class Mage_Sales_Model_Quote_Address_Total_Collector
      */
     protected function _initCollectors()
     {
-        $configArray = $this->_modelsConfig;
-        uasort($configArray, array($this, '_sortConfig'));
-        
-        foreach ($configArray as $code => $info) {
+        $sortedCodes = $this->_getSortedCollectorCodes();
+        foreach ($sortedCodes as $code) {
             $this->_collectors[$code] = $this->_models[$code];
         }
-//        echo '<pre>';
-//        print_r(array_keys($this->_collectors));
-//        echo '</pre>';
         
         return $this;
     }
@@ -147,16 +193,17 @@ class Mage_Sales_Model_Quote_Address_Total_Collector
      * @param   array $b
      * @return  int
      */
-    protected function _sortConfig($a, $b)
+    protected function _compareTotals($a, $b)
     {
         $aCode = $a['_code'];
         $bCode = $b['_code'];
         if (in_array($aCode, $b['after']) || in_array($bCode, $a['before'])) {
             $res = -1;
-        } else {
+        } elseif (in_array($bCode, $a['after']) || in_array($aCode, $b['before'])) {
             $res = 1;
+        } else {
+            $res = 0;
         }
-//        echo $aCode . ' :: ' . $bCode . ' == ' . $res . '<br>';
         return $res;
     }
 
