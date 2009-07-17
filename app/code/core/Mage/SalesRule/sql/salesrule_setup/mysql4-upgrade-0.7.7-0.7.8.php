3d74f44de664b97b44851c895f03f1b3a4cdfa44
Dmitriy Soroka <dmitriy.soroka@magento.com>
2009-07-17 08:55:27 +0000

    Added table for sales rule labels

--- app/code/core/Mage/SalesRule/sql/salesrule_setup/mysql4-upgrade-0.7.7-0.7.8.php
+++ app/code/core/Mage/SalesRule/sql/salesrule_setup/mysql4-upgrade-0.7.7-0.7.8.php
@@ -25,27 +25,6 @@
  */
 
 $installer = $this;
-/* @var $installer Mage_Sales_Model_Mysql4_Setup */
-//$installer->getConnection()->addColumn(
-//    $this->getTable('salesrule'),
-//    'apply_to_shipping',
-//    "tinyint(1) unsigned not null default '0' after simple_free_shipping"
-//);
-//
-//$installer->run("
-//CREATE TABLE `{$this->getTable('salesrule/label')}` (
-//   `label_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-//   `rule_id` int(10) unsigned NOT NULL,
-//   `store_id` smallint(5) unsigned NOT NULL,
-//   `label` varchar(255) DEFAULT NULL,
-//   PRIMARY KEY (`label_id`),
-//   UNIQUE KEY `IDX_RULE_STORE` (`rule_id`,`store_id`),
-//   KEY `FK_SALESRULE_LABEL_STORE` (`store_id`),
-//   KEY `FK_SALESRULE_LABEL_RULE` (`rule_id`),
-//   CONSTRAINT `FK_SALESRULE_LABEL_RULE` FOREIGN KEY (`rule_id`) REFERENCES `{$this->getTable('salesrule')}` (`rule_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-//   CONSTRAINT `FK_SALESRULE_LABEL_STORE` FOREIGN KEY (`store_id`) REFERENCES `{$this->getTable('core/store')}` (`store_id`) ON DELETE CASCADE ON UPDATE CASCADE
-//) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-//");
 
 /**
  * add attributes discount_description, shipping_discount_amount, base_shipping_discount_amount
