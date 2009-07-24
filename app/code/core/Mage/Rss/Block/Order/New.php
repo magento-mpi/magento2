6cfe388ed7d39a76c95a3814e4e24a8a6127df75
Alex Kovalenko <noreply@magento.com>
2009-07-24 18:00:06 +0000

    Fixed #15618 also fixed a similar problem for feed notify stock, reviews and new orders

--- app/code/core/Mage/Rss/Block/Order/New.php
+++ app/code/core/Mage/Rss/Block/Order/New.php
@@ -29,12 +29,21 @@
  *
  * @category   Mage
  * @package    Mage_Rss
- * @author      Magento Core Team <core@magentocommerce.com>
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Rss_Block_Order_New extends Mage_Core_Block_Template
 {
+
+    /**
+     * Cache tag constant for feed new orders
+     *
+     * @var string
+     */
+    const CACHE_TAG = 'block_html_rss_order_new';
+
     protected function _construct()
     {
+        $this->setCacheTags(array(self::CACHE_TAG));
         /*
         * setting cache to save the rss for 10 minutes
         */
