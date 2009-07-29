1a15170632a6a4a001a591b5c790b8ea67a8be50
Michael Bessolov <michael.bessolov@magento.com>
2009-07-29 09:03:09 +0000

    Fixed processing ldml data (currency symbol choice) in ZF after upgrade

--- lib/Zend/Locale/Data.php
+++ lib/Zend/Locale/Data.php
@@ -622,6 +622,10 @@ class Zend_Locale_Data
                 }
                 break;
 
+            case 'currencysymbolchoice':
+                $temp = self::_getFile($locale, '/ldml/numbers/currencies/currency[@type=\'' . $value . '\']/symbol/@choice', '', $value);
+                break;
+
             case 'question':
                 $temp  = self::_getFile($locale, '/ldml/posix/messages/yesstr',  '', 'yes');
                 $temp += self::_getFile($locale, '/ldml/posix/messages/nostr',   '', 'no');
@@ -1167,6 +1171,10 @@ class Zend_Locale_Data
 
             case 'currencysymbol':
                 $temp = self::_getFile($locale, '/ldml/numbers/currencies/currency[@type=\'' . $value . '\']/symbol', '', $value);
+                break;
+
+            case 'currencysymbolchoice':
+                $temp = self::_getFile($locale, '/ldml/numbers/currencies/currency[@type=\'' . $value . '\']/symbol/@choice', '', $value);
                 break;
 
             case 'question':
