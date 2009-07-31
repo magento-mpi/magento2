c6f397501fb80cada68bd778ad81e9ac24ad0786
Dmitriy Soroka <dmitriy.soroka@magento.com>
2009-07-31 10:42:45 +0000

    Fixed isVisible validation duering attributes form bilding Require next modification for castomer attributes (is_visible property)

--- app/code/core/Mage/Adminhtml/Block/Widget/Form.php
+++ app/code/core/Mage/Adminhtml/Block/Widget/Form.php
@@ -164,7 +164,7 @@ class Mage_Adminhtml_Block_Widget_Form extends Mage_Adminhtml_Block_Widget
         $this->_addElementTypes($fieldset);
         foreach ($attributes as $attribute) {
             /* @var $attribute Mage_Eav_Model_Entity_Attribute */
-            if (!$attribute || !$attribute->getIsVisible()) {
+            if (!$attribute || ($attribute->hasIsVisible() && !$attribute->getIsVisible())) {
                 continue;
             }
             if ( ($inputType = $attribute->getFrontend()->getInputType())
