a2e8dfa56d6edb04aa03251deb382a45f96932bd
Michael Bessolov <michael.bessolov@magento.com>
2009-08-31 14:39:42 +0000

    Added ability to specify the store to be run via environment variables (instead of modifying index.php):     MAGE_RUN_CODE - store or website code     MAGE_RUN_TYPE - code type
    E.g. (in httpd.conf or in virtual host config or in .htaccess):
        SetEnv MAGE_RUN_CODE "second_website"
        SetEnv MAGE_RUN_TYPE "website"

--- index.php
+++ index.php
@@ -62,4 +62,8 @@ require_once $mageFilename;
 #ini_set('display_errors', 1);
 
 umask(0);
-Mage::run();
+
+$mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : '';
+$mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
+
+Mage::run($mageRunCode, $mageRunType);
