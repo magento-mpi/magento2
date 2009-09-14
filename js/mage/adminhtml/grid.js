3c21e18fb258583915acc93829ec220a21f88831
Maxim Saveliev <maxim.saveliev@magento.com>
2009-09-14 15:57:35 +0000

    Implemented adminhtml grid serializer widget block

--- js/mage/adminhtml/grid.js
+++ js/mage/adminhtml/grid.js
@@ -637,3 +637,132 @@ var varienStringArray = {
         return string;
     }
 };
+
+var serializerController = Class.create();
+serializerController.prototype = {
+    initialize: function(hiddenDataHolder, predefinedData, inputsToManage, grid, reloadParamName){
+        //Grid inputs
+        this.tabIndex = 1000;
+        this.inputsToManage       = inputsToManage;
+        this.multidimensionalMode = inputsToManage.length > 0;
+
+        //Hash with grid data
+        this.gridData             = this.getGridDataHash(predefinedData);
+
+        //Hidden input data holder
+        this.hiddenDataHolder     = $(hiddenDataHolder);
+        this.hiddenDataHolder.value = this.serializeObject();
+
+        //Grid
+        this.grid = grid;
+        this.reloadParamName = reloadParamName;
+        this.grid.reloadParams = {};
+        this.grid.reloadParams[this.reloadParamName+'[]'] = this.getDataForReloadParam();
+        this.grid.rowClickCallback = this.rowClick.bind(this);
+        this.grid.initRowCallback = this.rowInit.bind(this);
+        this.grid.checkboxCheckCallback = this.registerData.bind(this);
+        this.grid.rows.each(this.eachRow.bind(this));
+    },
+
+    registerData : function(grid, element, checked) {
+        if(this.multidimensionalMode){
+            if(checked){
+                 if(element.inputElements) {
+                     this.gridData.set(element.value, {});
+                     for(var i = 0; i < element.inputElements.length; i++) {
+                         element.inputElements[i].disabled = false;
+                         this.gridData.get(element.value)[element.inputElements[i].name] = element.inputElements[i].value;
+                     }
+                 }
+            }
+            else{
+                if(element.inputElements){
+                    for(var i = 0; i < element.inputElements.length; i++) {
+                        element.inputElements[i].disabled = true;
+                    }
+                }
+                this.gridData.unset(element.value);
+            }
+        }
+        else{
+            if(checked){
+                this.gridData.set(element.value, element.value);
+            }
+            else{
+                this.gridData.unset(element.value);
+            }
+        }
+
+        this.hiddenDataHolder.value = this.serializeObject();
+        this.grid.reloadParams = {};
+        this.grid.reloadParams[this.reloadParamName+'[]'] = this.getDataForReloadParam();
+    },
+    eachRow : function(row) {
+        this.rowInit(this.grid, row);
+    },
+    rowClick : function(grid, event) {
+        var trElement = Event.findElement(event, 'tr');
+        var isInput   = Event.element(event).tagName == 'INPUT';
+        if(trElement){
+            var checkbox = Element.select(trElement, 'input');
+            if(checkbox[0]){
+                var checked = isInput ? checkbox[0].checked : !checkbox[0].checked;
+                this.grid.setCheckboxChecked(checkbox[0], checked);
+            }
+        }
+    },
+    inputChange : function(event) {
+        var element = Event.element(event);
+        if(element && element.checkboxElement && element.checkboxElement.checked){
+            this.gridData.get(element.checkboxElement.value)[element.name] = element.value;
+            this.hiddenDataHolder.value = this.serializeObject();
+        }
+    },
+    rowInit : function(grid, row) {
+        if(this.multidimensionalMode){
+            var checkbox = $(row).select('.checkbox')[0];
+            var selectors = this.inputsToManage.map(function (name) { return 'input[name="' + name + '"]' });
+            var inputs = $(row).select.apply($(row), selectors);
+            if(checkbox && inputs.length > 0) {
+                checkbox.inputElements = inputs;
+                for(var i = 0; i < inputs.length; i++) {
+                    inputs[i].checkboxElement = checkbox;
+                    if(this.gridData.get(checkbox.value) && this.gridData.get(checkbox.value)[inputs[i].name]) {
+                        inputs[i].value = this.gridData.get(checkbox.value)[inputs[i].name];
+                    }
+                    inputs[i].disabled = !checkbox.checked;
+                    inputs[i].tabIndex = this.tabIndex++;
+                    Event.observe(inputs[i],'keyup', this.inputChange.bind(this));
+                    Event.observe(inputs[i],'change', this.inputChange.bind(this));
+                }
+            }
+        }
+    },
+
+    //Stuff methods
+    getGridDataHash: function (_object){
+        return $H(this.multidimensionalMode ? _object : this.convertArrayToObject(_object))
+    },
+    getDataForReloadParam: function(){
+        return this.multidimensionalMode ? this.gridData.keys() : this.gridData.values();
+    },
+    serializeObject: function(){
+        if(this.multidimensionalMode){
+            var clone = this.gridData.clone();
+            clone.each(function(pair) {
+                clone.set(pair.key, encode_base64(Object.toQueryString(pair.value)));
+            });
+            return clone.toQueryString();
+        }
+        else{
+            return this.gridData.values().join('&');
+        }
+    },
+    convertArrayToObject: function (_array){
+        var _object = {};
+        for(var i = 0, l = _array.length; i < l; i++){
+            _object[_array[i]] = _array[i];
+        }
+        return _object;
+    }
+};
