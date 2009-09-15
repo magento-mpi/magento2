44187930816aa631135382ae7b7558407f08d157
Dmitriy Vasilenko <dmitriy.vasilenko@magento.com>
2009-09-15 12:13:44 +0000

    Added missing phpDoc for code committed in rev#47781

--- app/code/core/Mage/Customer/Block/Widget/Gender.php
+++ app/code/core/Mage/Customer/Block/Widget/Gender.php
@@ -24,24 +24,49 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+/**
+ * Block to render customer's gender attribute
+ *
+ * @category   Mage
+ * @package    Mage_Customer
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
 class Mage_Customer_Block_Widget_Gender extends Mage_Customer_Block_Widget_Abstract
 {
+    /**
+     * Initialize block
+     */
     public function _construct()
     {
         parent::_construct();
         $this->setTemplate('customer/widget/gender.phtml');
     }
 
+    /**
+     * Check if gender attribute enabled in system
+     *
+     * @return bool
+     */
     public function isEnabled()
     {
         return (bool)$this->getConfig('gender_show');
     }
 
+    /**
+     * Check if gender attribute marked as required
+     *
+     * @return bool
+     */
     public function isRequired()
     {
         return 'req' == $this->getConfig('gender_show');
     }
 
+    /**
+     * Get current customer from session
+     *
+     * @return Mage_Customer_Model_Customer
+     */
     public function getCustomer()
     {
         return Mage::getSingleton('customer/session')->getCustomer();
