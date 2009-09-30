e24e142331e3d6ccdeb108d63e4e67856c627758
Maxim Saveliev <maxim.saveliev@magento.com>
2009-09-30 13:41:49 +0000

    System Transactional Emails: moved creating add button to block

--- app/code/core/Mage/Adminhtml/Block/System/Email/Template.php
+++ app/code/core/Mage/Adminhtml/Block/System/Email/Template.php
@@ -34,25 +34,61 @@
 
 class Mage_Adminhtml_Block_System_Email_Template extends Mage_Adminhtml_Block_Template
 {
-    public function __construct()
+    /**
+     * Set transactional emails grid template
+     *
+     */
+    protected function _construct()
     {
-        parent::__construct();
+        parent::_construct();
         $this->setTemplate('system/email/template/list.phtml');
     }
 
+    /**
+     * Create add button and grid blocks
+     *
+     * @return Mage_Core_Block_Abstract
+     */
     protected function _prepareLayout()
     {
+        $this->setChild('add_button',
+            $this->getLayout()->createBlock('adminhtml/widget_button')
+                ->setData(array(
+                    'label'     => Mage::helper('adminhtml')->__('Add New Template'),
+                    'onclick'   => "window.location='" . $this->getCreateUrl() . "'",
+                    'class'     => 'add'
+        )));
         $this->setChild('grid', $this->getLayout()->createBlock('adminhtml/system_email_template_grid', 'email.template.grid'));
         return parent::_prepareLayout();
     }
 
+    /**
+     * Get URL for create new email template
+     *
+     * @return string
+     */
     public function getCreateUrl()
     {
         return $this->getUrl('*/*/new');
     }
 
+    /**
+     * Get transactional emails page header text
+     *
+     * @return string
+     */
     public function getHeaderText()
     {
         return Mage::helper('adminhtml')->__('Transactional Emails');
+    }
+
+    /**
+     * Get Add New Template button html
+     *
+     * @return string
+     */
+    protected function getAddButtonHtml()
+    {
+        return $this->getChildHtml('add_button');
     }
 }
