86d5ce8752375592de23653bd277b1914aad9477
Anton Makarenko <anton.makarenko@magento.com>
2009-10-05 14:53:21 +0000

    Added ability to support the <resource> directive for menu building (it was in adminhtml.xml, but wasn't in use) Reviewed all adminhtml.xml: - checked all existing <resource> directives, removed redundant or non-working - specified sort order for catalog menu - updated some text labels

--- app/code/core/Mage/Adminhtml/Block/Page/Menu.php
+++ app/code/core/Mage/Adminhtml/Block/Page/Menu.php
@@ -123,12 +123,12 @@ class Mage_Adminhtml_Block_Page_Menu extends Mage_Adminhtml_Block_Template
 
         $parentArr = array();
         $sortOrder = 0;
-        foreach ($parent->children() as $childName=>$child) {
+        foreach ($parent->children() as $childName => $child) {
             if (1 == $child->disabled) {
                 continue;
             }
 
-            $aclResource = 'admin/'.$path.$childName;
+            $aclResource = 'admin/' . ($child->resource ? (string)$child->resource : $path . $childName);
             if (!$this->_checkAcl($aclResource)) {
                 continue;
             }
