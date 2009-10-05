fdf0636cf85819424fd8d2c47ae5ef531efc4e9f
Maxim Saveliev <maxim.saveliev@magento.com>
2009-10-05 10:39:08 +0000

    EE Banner Module rename Enterprise_Banner_Block_Widget_Chooser to Enterprise_Banner_Block_Adminhtml_Widget_Chooser

--- app/code/core/Enterprise/Banner/Block/Adminhtml/Widget/Chooser.php
+++ app/code/core/Enterprise/Banner/Block/Adminhtml/Widget/Chooser.php
@@ -31,7 +31,7 @@
  * @package    Enterprise_Banner
  * @author     Magento Core Team <core@magentocommerce.com>
  */
-class Enterprise_Banner_Block_Widget_Chooser extends Enterprise_Banner_Block_Adminhtml_Banner_Grid
+class Enterprise_Banner_Block_Adminhtml_Widget_Chooser extends Enterprise_Banner_Block_Adminhtml_Banner_Grid
 {
     protected $_selectedBanners = array();
 
