9acb054a0c55e600846b070415682c36b9aa7abe
Maxim Gubar <maxim.gubar@magento.com>
2009-10-06 08:28:37 +0000

    Fixed #16336: CAVV bug in Authorizenet.php paygate module

--- app/code/core/Mage/Paygate/Model/Authorizenet.php
+++ app/code/core/Mage/Paygate/Model/Authorizenet.php
@@ -409,7 +409,7 @@ class Mage_Paygate_Model_Authorizenet extends Mage_Payment_Model_Method_Cc
                 ->setCustomerId($r[12])
                 ->setMd5Hash($r[37])
                 ->setCardCodeResponseCode($r[38])
-                ->setCAVVResponseCode($r[39]);
+                ->setCAVVResponseCode( (isset($r[39])) ? $r[39] : null);
         } else {
              Mage::throwException(
                 Mage::helper('paygate')->__('Error in payment gateway')
