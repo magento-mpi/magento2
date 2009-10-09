c97f2905d00bf4f40b551bbb708588f966f02e7c
Roman Goncharuk <roman.goncharuk@magento.com>
2009-10-09 13:26:11 +0000

    Bug fix #16627 : Password confirmation doesn't validate during checkout  - in checkout onepage billing information panel for customer registration

--- skin/frontend/default/default/js/opcheckout.js
+++ skin/frontend/default/default/js/opcheckout.js
@@ -286,10 +286,6 @@ Billing.prototype = {
 
         var validator = new Validation(this.form);
         if (validator.validate()) {
-            if (checkout.method=='register' && $('billing:customer_password').value != $('billing:confirm_password').value) {
-                alert(Translator.translate('Error: Passwords do not match'));
-                return;
-            }
             checkout.setLoadWaiting('billing');
 
 //            if ($('billing:use_for_shipping') && $('billing:use_for_shipping').checked) {
