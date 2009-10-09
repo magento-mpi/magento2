d19d7693b9b7a2db14f9d66f2f3813ac7e544f86
Maxim Gubar <maxim.gubar@magento.com>
2009-10-09 15:07:49 +0000

    Fixed #17278: Buttons 'Credit Memo' / 'Cancel' show for limited admin on Invoice View page

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Invoice/View.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Invoice/View.php
@@ -33,11 +33,19 @@
  */
 class Mage_Adminhtml_Block_Sales_Order_Invoice_View extends Mage_Adminhtml_Block_Widget_Form_Container
 {
+    /**
+     * Admin session
+     *
+     * @var Mage_Admin_Model_Session
+     */
+    protected $_session;
+
     public function __construct()
     {
         $this->_objectId    = 'invoice_id';
         $this->_controller  = 'sales_order_invoice';
         $this->_mode        = 'view';
+        $this->_session = Mage::getSingleton('admin/session');
 
         parent::__construct();
 
@@ -45,8 +53,7 @@ class Mage_Adminhtml_Block_Sales_Order_Invoice_View extends Mage_Adminhtml_Block
         $this->_removeButton('reset');
         $this->_removeButton('delete');
 
-        if (Mage::getSingleton('admin/session')->isAllowed('sales/order/actions/cancel')
-            && $this->getInvoice()->canCancel()) {
+        if ($this->_isAllowedAction('cancel') && $this->getInvoice()->canCancel()) {
             $this->_addButton('cancel', array(
                 'label'     => Mage::helper('sales')->__('Cancel'),
                 'class'     => 'delete',
@@ -62,8 +69,7 @@ class Mage_Adminhtml_Block_Sales_Order_Invoice_View extends Mage_Adminhtml_Block
             ) . '\', \'' . $this->getEmailUrl() . '\')'
         ));
 
-        if (Mage::getSingleton('admin/session')->isAllowed('sales/order/actions/creditmemo')
-            && $this->getInvoice()->getOrder()->canCreditmemo()) {
+        if ($this->_isAllowedAction('creditmemo') && $this->getInvoice()->getOrder()->canCreditmemo()) {
             if ($this->getInvoice()->getOrder()->getPayment()->canRefundPartialPerInvoice()
                 || !$this->getInvoice()->getIsUsedForRefund())
             {
@@ -76,8 +82,7 @@ class Mage_Adminhtml_Block_Sales_Order_Invoice_View extends Mage_Adminhtml_Block
             }
         }
 
-        if (Mage::getSingleton('admin/session')->isAllowed('sales/order/actions/capture')
-            && $this->getInvoice()->canCapture()) {
+        if ($this->_isAllowedAction('capture') && $this->getInvoice()->canCapture()) {
             $this->_addButton('capture', array(
                 'label'     => Mage::helper('sales')->__('Capture'),
                 'class'     => 'save',
@@ -191,5 +196,16 @@ class Mage_Adminhtml_Block_Sales_Order_Invoice_View extends Mage_Adminhtml_Block
             return $this->_updateButton('back', 'onclick', 'setLocation(\'' . $this->getUrl('*/sales_invoice/') . '\')');
         }
         return $this;
+    }
+
+    /**
+     * Check whether is allowed action
+     *
+     * @param string $action
+     * @return bool
+     */
+    protected function _isAllowedAction($action)
+    {
+        return $this->_session->isAllowed('sales/order/actions/' . $action);
     }
 }
