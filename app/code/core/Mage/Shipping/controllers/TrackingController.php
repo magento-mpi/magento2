e799e53890ecda23399ad24e322e60ef940bd39f
Maxim Gubar <maxim.gubar@magento.com>
2009-10-14 10:42:05 +0000

    Fixed #16732: Anonymous Access to any Tracking Number by ID

--- app/code/core/Mage/Shipping/controllers/TrackingController.php
+++ app/code/core/Mage/Shipping/controllers/TrackingController.php
@@ -34,6 +34,10 @@
 
 class Mage_Shipping_TrackingController extends Mage_Core_Controller_Front_Action
 {
+    /**
+     * Ajax action
+     *
+     */
     public function ajaxAction()
     {
         if ($order = $this->_initOrder()) {
@@ -56,6 +60,10 @@ class Mage_Shipping_TrackingController extends Mage_Core_Controller_Front_Action
         }
     }
 
+    /**
+     * Popup action
+     * Shows tracking info if it's present, otherwise redirects to 404
+     */
     public function popupAction()
     {
         $shippingInfoModel = Mage::getModel('shipping/info')->loadByHash($this->getRequest()->getParam('hash'));
