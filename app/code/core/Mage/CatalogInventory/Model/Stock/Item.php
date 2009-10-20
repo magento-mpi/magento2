38e3a502df6ab82e836b331494baedf1c6b3070a
Maxim Saveliev <maxim.saveliev@magento.com>
2009-10-20 13:52:42 +0000

    Fixed: 17446: Backorder message does not contain decimals numbers

--- app/code/core/Mage/CatalogInventory/Model/Stock/Item.php
+++ app/code/core/Mage/CatalogInventory/Model/Stock/Item.php
@@ -389,8 +389,8 @@ class Mage_CatalogInventory_Model_Stock_Item extends Mage_Core_Model_Abstract
                     }
                     $result->setItemBackorders($backorderQty);
                     if ($this->getBackorders() == Mage_CatalogInventory_Model_Stock::BACKORDERS_YES_NOTIFY) {
-                        $result->setMessage(Mage::helper('cataloginventory')->__('This product is not available in the requested quantity. %d of the items will be backordered.',
-                            $backorderQty,
+                        $result->setMessage(Mage::helper('cataloginventory')->__('This product is not available in the requested quantity. %s of the items will be backordered.',
+                            ($backorderQty * 1),
                             $this->getProductName())
                             )
                         ;
