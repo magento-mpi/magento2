18bbe5642ba73742e4f175c4bc82af3bc12ca5e0
Ruslan Voitenko <ruslan.voytenko@magento.com>
2009-11-03 10:21:34 +0000

    Fixed generating of admin url for wysiwyg action

--- app/code/core/Mage/Adminhtml/Block/Catalog/Helper/Form/Wysiwyg.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Helper/Form/Wysiwyg.php
@@ -46,7 +46,7 @@ class Mage_Adminhtml_Block_Catalog_Helper_Form_Wysiwyg extends Varien_Data_Form_
                 ->createBlock('adminhtml/widget_button', '', array(
                     'label'   => Mage::helper('catalog')->__('WYSIWYG Editor'),
                     'type'    => 'button',
-                    'onclick' => 'catalogWysiwygEditor.open(\''.Mage::getUrl('*/*/wysiwyg').'\', \''.$this->getHtmlId().'\')'
+                    'onclick' => 'catalogWysiwygEditor.open(\''.Mage::helper('adminhtml')->getUrl('*/*/wysiwyg').'\', \''.$this->getHtmlId().'\')'
                 ))->toHtml();
         }
         return $html;
