d57bcaa33ead9a0a8c4bc3f4bd248cb2475619bc
Dmitriy Volik <dmitriy.volik@magento.com>
2009-12-08 14:23:20 +0000

    removed source model extension from core_model

--- app/code/core/Enterprise/GiftCard/Model/Source/Open.php
+++ app/code/core/Enterprise/GiftCard/Model/Source/Open.php
@@ -24,7 +24,7 @@
  * @license     http://www.magentocommerce.com/license/enterprise-edition
  */
 
-class Enterprise_GiftCard_Model_Source_Open extends Mage_Core_Model_Abstract
+class Enterprise_GiftCard_Model_Source_Open
 {
     public function getAllOptions()
     {
