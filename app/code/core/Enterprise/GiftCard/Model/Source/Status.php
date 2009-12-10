e6e6cd6dce479f9b31732917a1d8135eca8856e3
Dmitriy Volik <dmitriy.volik@magento.com>
2009-12-10 11:25:49 +0000

    EE: reverted r.55840

--- app/code/core/Enterprise/GiftCard/Model/Source/Status.php
+++ app/code/core/Enterprise/GiftCard/Model/Source/Status.php
@@ -24,7 +24,7 @@
  * @license     http://www.magentocommerce.com/license/enterprise-edition
  */
 
-class Enterprise_GiftCard_Model_Source_Status
+class Enterprise_GiftCard_Model_Source_Status extends Mage_Core_Model_Abstract
 {
     public function toOptionArray()
     {
