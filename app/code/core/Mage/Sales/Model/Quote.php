585ad50b035ce4d44b3b2bb087051406e7cea037
Maxim Saveliev <maxim.saveliev@magento.com>
2009-12-10 09:19:52 +0000

    EE Fixed: #18651: Gift Cards are shared between websites

--- app/code/core/Mage/Sales/Model/Quote.php
+++ app/code/core/Mage/Sales/Model/Quote.php
@@ -220,7 +220,9 @@ class Mage_Sales_Model_Quote extends Mage_Core_Model_Abstract
     {
         if ($customer instanceof Mage_Customer_Model_Customer) {
             $customerId = $customer->getId();
-            $this->setStoreId($customer->getStoreId());
+            if (!$this->hasData('store_id')) {
+                $this->setStoreId($customer->getStoreId());
+            }
         }
         else {
             $customerId = (int) $customer;
