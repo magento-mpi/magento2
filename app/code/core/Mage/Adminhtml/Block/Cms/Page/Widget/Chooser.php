c95ef43a5e7638fbafe6bd4137b3abf427b412a5
Vladimir Pelipenko <vladimir.pelipenko@magento.com>
2009-12-23 14:50:05 +0000

    Fixed #19051: Strange HTML code in WYSIWYG content during add widget

--- app/code/core/Mage/Adminhtml/Block/Cms/Page/Widget/Chooser.php
+++ app/code/core/Mage/Adminhtml/Block/Cms/Page/Widget/Chooser.php
@@ -89,7 +89,7 @@ class Mage_Adminhtml_Block_Cms_Page_Widget_Chooser extends Mage_Adminhtml_Block_
             function (grid, event) {
                 var trElement = Event.findElement(event, "tr");
                 var pageTitle = trElement.down("td").next().innerHTML;
-                var pageId = trElement.down("td").innerHTML;
+                var pageId = trElement.down("td").innerHTML.replace(/^\s+|\s+$/g,"");
                 '.$chooserJsObject.'.setElementValue(pageId);
                 '.$chooserJsObject.'.setElementLabel(pageTitle);
                 '.$chooserJsObject.'.close();
