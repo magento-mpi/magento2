5744d152b3c76e861c85b3ef11c37497d49afeaf
Anton Makarenko <anton.makarenko@magento.com>
2010-01-16 03:20:25 +0000

    EE Reward Points - updated "Learn More" CMS-page content

--- app/code/core/Enterprise/Reward/sql/enterprise_reward_setup/mysql4-upgrade-1.7.0.0.10-1.7.0.0.11.php
+++ app/code/core/Enterprise/Reward/sql/enterprise_reward_setup/mysql4-upgrade-1.7.0.0.10-1.7.0.0.11.php
@@ -24,32 +24,3 @@
  * @license     http://www.magentocommerce.com/license/enterprise-edition
  */
 
-/* @var $installer Mage_Sales_Model_Mysql4_Setup */
-$installer = $this;
-$installer->startSetup();
-
-$now = new Zend_Date(time());
-$now = $now->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
-
-$installer->getConnection()->insert($installer->getTable('cms/page'), array(
-        'title' => 'Reward Points',
-        'identifier' => 'reward-points',
-        'content' => '<div class="box">
-<div class="content">
-<p>Gain Reward Points and spend them as you wish!</p>
-<p>If you are a registered member, please <a href="{{store url="customer/account/login"}}">log in here</a>.</p>
-</div>
-</div>',
-        'website_root' => '1',
-        'creation_time' => $now,
-        'update_time' => $now,
-        'is_active' => '1'
-));
-
-$pageId = $installer->getConnection()->lastInsertId();
-$installer->getConnection()->insert(
-    $installer->getTable('cms/page_store'),
-    array('page_id'=>$pageId, 'store_id'=>0)
-);
-
-$installer->endSetup();
