8535160d43b8c3caae16a0e110fd9a246b54f8df
Dmitriy Soroka <dmitriy.soroka@magento.com>
2010-01-27 19:05:29 +0000

    Reverted stand-alone format for month - not supported by most locales (including en_US)

--- app/code/core/Mage/Payment/Model/Config.php
+++ app/code/core/Mage/Payment/Model/Config.php
@@ -108,7 +108,7 @@ class Mage_Payment_Model_Config
      */
     public function getMonths()
     {
-        $data = Mage::app()->getLocale()->getTranslationList('month', array('gregorian', 'stand-alone', 'wide'));
+        $data = Mage::app()->getLocale()->getTranslationList('month');
         foreach ($data as $key => $value) {
             $monthNum = ($key < 10) ? '0'.$key : $key;
             $data[$key] = $monthNum . ' - ' . $value;
