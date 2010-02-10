02574b27ec7443e16aee295f689122510002ccc1
Vasiliy Seleznev <vasiliy.seleznev@magento.com>
2010-02-10 19:31:30 +0000

    Centinel - added "Verified By Visa" page content - added logos + cms page images - fixed html + css - W3C validation

--- app/code/core/Mage/Centinel/sql/centinel_setup/mysql4-install-1.4.0.0.0.php
+++ app/code/core/Mage/Centinel/sql/centinel_setup/mysql4-install-1.4.0.0.0.php
@@ -32,18 +32,14 @@ $installer->getConnection()->delete($installer->getTable('cms/page'), '`identifi
 $now = new Zend_Date(time());
 $now = $now->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
 $installer->getConnection()->insert($installer->getTable('cms/page'), array(
-    'title'           => 'Verified By Visa',
+    'title'           => 'Verified by Visa',
     'root_template'   => 'empty',
     'identifier'      => 'centinel-verified-by-visa',
-    'content_heading' => 'Verified By Visa',
+    'content_heading' => 'Verified by Visa',
     'creation_time'   => $now,
     'update_time'     => $now,
     'is_active'       => 1,
-    'content' => '
-
-    CONTENT FROM <a href="https://www.hotwire.com/pop-up/verified-by-visa.jsp">HERE</a>
-
-    ',
+    'content'         => "<h2 class=\"subtitle\">Get an extra layer of security when you shop online</h2>\r\n<p>In addition to our other ways of preventing, detecting, and resolving fraud, we offer Verified by Visa, a free, simple-to-use service that confirms your identity with an extra password when you make an online transaction.</p>\r\n<h2 class=\"subtitle\">How it works</h2>\r\n<ol class=\"ol\">\r\n    <li>\r\n        <p><strong>Shop at participating online merchants</strong><br />\r\n            Visit online merchants that display the Verified by Visa symbol; it's your assurance that your transaction will have an added layer of protection.</p>\r\n    </li>\r\n    <li>\r\n        <p><strong>Activate Verified by Visa While You Shop</strong><br />\r\n            You can enroll your card in the Verified by Visa program while shopping online. You may also be able to enroll and activate Verified by Visa through your issuer, please contact your issuer for details.</p>\r\n    </li>\r\n    <li>\r\n        <p><strong>Enjoy enhanced security</strong><br />\r\n            You can enjoy the peace of mind of Verified by Visa by activating both Visa credit and check cards</p>\r\n    </li>\r\n</ol>\r\n<h2 class=\"subtitle\">Activate Verified by Visa while you shop</h2>\r\n<p class=\"a-center\"><img src=\"{{skin url='images/centinel/v_activate_steps.gif'}}\" alt=\"Activate Verified by Visa\" title=\"Activate Verified by Visa\" /></p>\r\n<p>You may be prompted to activate your Visa card during the checkout process at one of our participating online merchants.</p>\r\n<p>It's quick and easy. If your issuer is participating in Verified by Visa you may be asked to complete a brief activation process. You will verify your identity, create your Verified by Visa password and you are done.</p>\r\n<p>Once your card is activated, your card number will be recognized whenever you shop at a participating VbV merchant. You will enter your password in the Verified by Visa window, your identity will be verified and the transaction will be completed. In stores that are not yet participating in Verified by Visa enabled, your Visa card will continue to work as usual.</p>",
 ));
 
 $pageId = $installer->getConnection()->lastInsertId();
