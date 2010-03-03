56e7a01f9144de78e18140bf96b04daa2a39f027
Maxim Saveliev <maxim.saveliev@magento.com>
2010-03-03 16:55:10 +0000

    Fixed: #18830: The link "My Cart" has disappeared if for "Display Shopping Cart Sidebar" set value "No"

--- app/code/core/Mage/Checkout/Block/Cart/Sidebar.php
+++ app/code/core/Mage/Checkout/Block/Cart/Sidebar.php
@@ -219,16 +219,12 @@ class Mage_Checkout_Block_Cart_Sidebar extends Mage_Checkout_Block_Cart_Abstract
     }
 
     /**
-     * Render block HTML
+     * Define if Shopping Cart Sidebar enabled
      *
-     * @return string
+     * @return bool
      */
-    protected function _toHtml()
+    public function getIsNeedToDisplaySideBar()
     {
-        $html = '';
-        if ((bool) Mage::app()->getStore()->getConfig('checkout/sidebar/display') || $this->getIgnoreHidingFlag()) {
-            $html = parent::_toHtml();
-        }
-        return $html;
+        return (bool) Mage::app()->getStore()->getConfig('checkout/sidebar/display');
     }
 }
