1a6ab5e72c0daebb9b6087a54ba6d05235c60088
Dmitriy Volik <dmitriy.volik@magento.com>
2010-03-17 13:15:59 +0000

    Added ability to assign id for field containers (TR tag) using node <use_container_id>1</use_container_id> in system configuration

--- app/code/core/Mage/Adminhtml/Block/System/Config/Form/Field.php
+++ app/code/core/Mage/Adminhtml/Block/System/Config/Form/Field.php
@@ -58,7 +58,9 @@ class Mage_Adminhtml_Block_System_Config_Form_Field
     {
         $id = $element->getHtmlId();
 
-        $html = '<tr><td class="label"><label for="'.$id.'">'.$element->getLabel().'</label></td>';
+        $useContainerId = $element->getData('use_container_id');
+        $html = '<tr'.($useContainerId ? ' id="row_' . $id . '"' : '').'>'
+              . '<td class="label"><label for="'.$id.'">'.$element->getLabel().'</label></td>';
 
         //$isDefault = !$this->getRequest()->getParam('website') && !$this->getRequest()->getParam('store');
         $isMultiple = $element->getExtType()==='multiple';
