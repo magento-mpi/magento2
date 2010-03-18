b02d81573f831ba9a3041dde9e19ea8874bf016d
Vasiliy Seleznev <vasiliy.seleznev@magento.com>
2010-03-18 15:02:47 +0000

    Menu refactoring

--- js/varien/menu.js
+++ js/varien/menu.js
@@ -32,57 +32,25 @@ var mainNav = function(){
 
     var main = {
         obj_nav :   $(arguments[0]) || $("nav"),
+
         settings :  {
-            show_delay :    0,
-            hide_delay :    0,
-            _ie6       :    /MSIE 6.+Win/.test(navigator.userAgent),
-            _ie7       :    /MSIE 7.+Win/.test(navigator.userAgent)
+            show_delay      :   0,
+            hide_delay      :   0,
+            _ie6            :   /MSIE 6.+Win/.test(navigator.userAgent),
+            _ie7            :   /MSIE 7.+Win/.test(navigator.userAgent)
         },
 
-        fireNavEvent :  function(elm,ev){
-            if(ev){
-                elm.addClassName("over");
-                if (elm.childElements()[1]) {
-                    main.show(elm.childElements()[1]);
+        init :  function(obj, level){
+            obj.lists = obj.childElements();
+            obj.lists.each(function(el,ind){
+                main.handlNavElement(el);
+                if((main.settings._ie6 || main.settings._ie7) && level){
+                    main.ieFixZIndex(el, ind, obj.lists.size());
                 }
-            } else {
-                elm.removeClassName("over");
-                if (elm.childElements()[1]) {
-                    main.hide(elm.childElements()[1]);
-                }
+            });
+            if(main.settings._ie6 && !level){
+                document.execCommand("BackgroundImageCache", false, true);
             }
-        },
-        show : function (sub_elm){
-            if (sub_elm.hide_time_id) {
-                clearTimeout(sub_elm.hide_time_id);
-            }
-            sub_elm.show_time_id = setTimeout(function(){
-                /*if (!sub_elm.hasClassName("shown-sub")){
-                    new Effect.Opacity(sub_elm, {
-                        from: 0,
-                        to: 1,
-                        duration: 0.5,
-                        afterSetup: function(){sub_elm.addClassName("shown-sub");}
-                    });
-                }*/
-                sub_elm.addClassName("shown-sub");
-            }, main.settings.show_delay);
-        },
-        hide : function (sub_elm) {
-            if (sub_elm.show_time_id) {
-                clearTimeout(sub_elm.show_time_id);
-            }
-            sub_elm.hide_time_id = setTimeout(function(){
-                /*if (sub_elm.hasClassName("shown-sub")){
-                    new Effect.Opacity(sub_elm, {
-                        from: 1,
-                        to: 0,
-                        duration: 0.5,
-                        afterFinish: function(){sub_elm.removeClassName("shown-sub");}
-                    });
-                }*/
-                sub_elm.removeClassName("shown-sub");
-            }, main.settings.hide_delay);
         },
 
         handlNavElement :   function(list){
@@ -99,21 +67,51 @@ var mainNav = function(){
                 }
             }
         },
-        fixZIndex : function(el, i, l){
+
+        ieFixZIndex : function(el, i, l){
             if(el.tagName.toString().toLowerCase().indexOf("iframe") == -1){
                 el.style.zIndex = l - i;
             }
         },
 
-        init :  function(obj, level){
-            obj.lists = obj.childElements();
-            obj.lists.each(function(el,ind){
-                main.handlNavElement(el);
-                if((main.settings._ie6 || main.settings._ie7) && level){
-                    main.fixZIndex(el, ind, obj.lists.size());
+        fireNavEvent :  function(elm,ev){
+            if(ev){
+                elm.addClassName("over");
+                elm.down("a").addClassName("over");
+                if (elm.childElements()[1]) {
+                    main.show(elm.childElements()[1]);
                 }
-            });
+            } else {
+                elm.removeClassName("over");
+                elm.down("a").removeClassName("over");
+                if (elm.childElements()[1]) {
+                    main.hide(elm.childElements()[1]);
+                }
+            }
+        },
+
+        show : function (sub_elm){
+            if (sub_elm.hide_time_id) {
+                clearTimeout(sub_elm.hide_time_id);
+            }
+            sub_elm.show_time_id = setTimeout(function() {
+                if (!sub_elm.hasClassName("shown-sub")) {
+                    sub_elm.addClassName("shown-sub");
+                }
+            }, main.settings.show_delay);
+        },
+
+        hide : function (sub_elm) {
+            if (sub_elm.show_time_id) {
+                clearTimeout(sub_elm.show_time_id);
+            }
+            sub_elm.hide_time_id = setTimeout(function(){
+                if (sub_elm.hasClassName("shown-sub")) {
+                    sub_elm.removeClassName("shown-sub");
+                }
+            }, main.settings.hide_delay);
         }
+
     };
     if (arguments[1]){
         main.settings = Object.extend(main.settings, arguments[1]);
@@ -124,5 +122,9 @@ var mainNav = function(){
 };
 
 document.observe("dom:loaded", function() {
+    //run navigation without delays and with default id="#nav"
+    //mainNav();
+
+    //run navigation with delays
     mainNav("nav", {"show_delay":"100","hide_delay":"100"})
 });
