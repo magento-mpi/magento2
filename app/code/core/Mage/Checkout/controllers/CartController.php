a7ddfdf38a50f69c215ed559537df44df8defc54
Dmitriy Vasilenko <dmitriy.vasilenko@magento.com>
2010-03-29 13:34:47 +0000

    Fixed #21877: Wishlist email and Catalog Permissions

--- app/code/core/Mage/Checkout/controllers/CartController.php
+++ app/code/core/Mage/Checkout/controllers/CartController.php
@@ -186,9 +186,12 @@ class Mage_Checkout_CartController extends Mage_Core_Controller_Front_Action
             Mage::dispatchEvent('checkout_cart_add_product_complete',
                 array('product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse())
             );
-            $message = $this->__('%s was added to your shopping cart.', Mage::helper('core')->htmlEscape($product->getName()));
+
             if (!$this->_getSession()->getNoCartRedirect(true)) {
-                $this->_getSession()->addSuccess($message);
+                if (!$cart->getQuote()->getHasError()){
+                    $message = $this->__('%s was added to your shopping cart.', Mage::helper('core')->htmlEscape($product->getName()));
+                    $this->_getSession()->addSuccess($message);
+                }
                 $this->_goBack();
             }
         }
