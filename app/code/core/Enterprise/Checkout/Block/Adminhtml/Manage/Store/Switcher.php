d2338f288ffba852e00a4daaff653ef9a3428fb0
Dmitriy Volik <dmitriy.volik@magento.com>
2010-03-30 15:55:33 +0000

    removed unnecessary confirmation from store switcher

--- app/code/core/Enterprise/Checkout/Block/Adminhtml/Manage/Store/Switcher.php
+++ app/code/core/Enterprise/Checkout/Block/Adminhtml/Manage/Store/Switcher.php
@@ -44,6 +44,7 @@ class Enterprise_Checkout_Block_Adminhtml_Manage_Store_Switcher extends Mage_Adm
     public function __construct()
     {
         parent::__construct();
+        $this->setUseConfirm(false);
         if ($this->_getCustomer()->getSharingConfig()->isWebsiteScope()) {
             $this->setWebsiteIds($this->_getCustomer()->getSharedWebsiteIds());
         }
