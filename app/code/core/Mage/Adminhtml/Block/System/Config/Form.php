9d25f694a9f17c96301eee81a7dafc284752bd2a
Andrey Bugaychuk <andrey.bugaychuk@magento.com>
2010-04-15 14:34:36 +0000

    Added ability to hide configuration fields if related module is not enabled (use 'if_module_enabled' node in system.xml)

--- app/code/core/Mage/Adminhtml/Block/System/Config/Form.php
+++ app/code/core/Mage/Adminhtml/Block/System/Config/Form.php
@@ -472,6 +472,11 @@ class Mage_Adminhtml_Block_System_Config_Form extends Mage_Adminhtml_Block_Widge
      */
     protected function _canShowField($field)
     {
+        $ifModuleEnabled = trim((string)$field->if_module_enabled);
+        if ($ifModuleEnabled && !Mage::helper('Core')->isModuleEnabled($ifModuleEnabled)) {
+            return false;
+        }
+
         switch ($this->getScope()) {
             case self::SCOPE_DEFAULT:
                 return (int)$field->show_in_default;
