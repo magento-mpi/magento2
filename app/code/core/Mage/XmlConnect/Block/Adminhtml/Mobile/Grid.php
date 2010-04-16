5dacf628b8984aaf462681cdb744e19082c97ef5
Dmitriy Soroka <dmitriy.soroka@magento.com>
2010-04-16 00:23:34 +0000

    XmlConnect - added base app management

---
+++ app/code/core/Mage/XmlConnect/Block/Adminhtml/Mobile/Grid.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_XmlConnect
+ * @copyright   Copyright (c) 2009 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Mage_Xmlconnect_Block_Adminhtml_Mobile_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+    /**
+     * Class constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('mobile_apps_grid');
+        $this->setDefaultSort('application_id');
+        $this->setDefaultDir('ASC');
+    }
+
+    /**
+     * Initialize grid data collection
+     */
+    protected function _prepareCollection()
+    {
+        $collection = Mage::getModel('xmlconnect/application')->getCollection();
+        $this->setCollection($collection);
+        return parent::_prepareCollection();
+    }
+
+    /**
+     * Delcare grid columns
+     */
+    protected function _prepareColumns()
+    {
+        $this->addColumn('name', array(
+            'header'    => Mage::helper('xmlconnect')->__('App Name'),
+            'align'     => 'left',
+            'index'     => 'name',
+        ));
+
+        $this->addColumn('code', array(
+            'header'    => Mage::helper('xmlconnect')->__('Code'),
+            'align'     => 'left',
+            'index'     => 'code',
+            'width'     => '200',
+        ));
+
+        if (!Mage::app()->isSingleStoreMode()) {
+            $this->addColumn('store_id', array(
+                'header'        => Mage::helper('xmlconnect')->__('Store View'),
+                'index'         => 'store_id',
+                'type'          => 'store',
+                'store_all'     => true,
+                'store_view'    => true,
+                'sortable'      => false,
+                'width'         => '250',
+            ));
+        }
+
+        $this->addColumn('active_from', array(
+            'header'    => Mage::helper('xmlconnect')->__('Active From'),
+            'index'     => 'active_from',
+            'type'      => 'date',
+            'width'     => '150',
+        ));
+
+        $this->addColumn('active_to', array(
+            'header'    => Mage::helper('xmlconnect')->__('Active To'),
+            'index'     => 'active_to',
+            'type'      => 'date',
+            'width'     => '150',
+        ));
+
+        return parent::_prepareColumns();
+    }
+
+    /**
+     * Row click url
+     *
+     * @return string
+     */
+    public function getRowUrl($row)
+    {
+        return $this->getUrl('*/*/edit', array('application_id' => $row->getId()));
+    }
+}
---
+++ app/code/core/Mage/XmlConnect/controllers/Adminhtml/MobileController.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_XmlConnect
+ * @copyright   Copyright (c) 2009 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Mage_XmlConnect_Adminhtml_MobileController extends Enterprise_Enterprise_Controller_Adminhtml_Action
+{
+    /**
+     * Initialize application
+     * @param string $paramName
+     * @return Mage_XmlConnect_Model_Application
+     */
+    protected function _initApp($paramName = 'application_id')
+    {
+        $id = (int) $this->getRequest()->getParam($paramName);
+        $app = Mage::getModel('xmlconnect/application');
+        if ($id) {
+            $app->load($id);
+            if (!$app->getId()) {
+                Mage::throwException($this->__('Aplication with is "%s" no longer exists.', $id));
+            }
+        }
+        Mage::register('current_app', $app);
+        return $app;
+    }
+
+    /**
+     * Mobile applications management
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->_title($this->__('Mobile Apps'));
+        $this->loadLayout();
+        $this->_setActiveMenu('mobile/app');
+        $this->renderLayout();
+    }
+
+    /**
+     * Create new app
+     */
+    public function newAction()
+    {
+        $this->_forward('edit');
+    }
+
+    /**
+     * Edit app form
+     */
+    public function editAction()
+    {
+        try {
+            $app = $this->_initApp();
+            $this->_title($app->getId() ? $app->getName() : $this->__('New Application'));
+            $data = Mage::getSingleton('adminhtml/session')->getFormData(true);
+            if (!empty($data)) {
+                $app->addData($data);
+            }
+            $this->loadLayout();
+            $this->_setActiveMenu('mobile/app');
+            $this->renderLayout();
+        } catch (Mage_Core_Exception $e) {
+            $this->_getSession()->addError($e->getMessage());
+            $this->_redirect('*/*/');
+        } catch (Exception $e) {
+            $this->_getSession()->addException($e, $this->__('Can\'t open application form'));
+            $this->_redirect('*/*/');
+        }
+    }
+
+    /**
+     * Save action
+     */
+    public function saveAction()
+    {
+        $data = $this->getRequest()->getPost();
+        $redirectBack = $this->getRequest()->getParam('back', false);
+        $app = false;
+        if ($data) {
+            try {
+                $app = $this->_initApp();
+                $app->setData($data);
+                $app->save();
+                $this->_getSession()->addSuccess($this->__('Application has been saved.'));
+            } catch (Mage_Core_Exception $e) {
+                $this->_getSession()->addException($e, $e->getMessage());
+                $redirectBack = true;
+            } catch (Exception $e) {
+                $this->_getSession()->addException($e, $this->__('Unable to save application.'));
+                $redirectBack = true;
+            }
+        }
+        if ($app && $redirectBack) {
+            $this->_redirect('*/*/edit', array('application_id' => $app->getId()));
+        } else {
+            $this->_redirect('*/*/');
+        }
+    }
+
+    /**
+     * Delete action
+     */
+    public function deleteAction()
+    {
+        try {
+            $app = $this->_initApp();
+            $app->delete();
+            $this->_getSession()->addSuccess($this->__('Application has been deleted.'));
+        } catch (Mage_Core_Exception $e) {
+            $this->_getSession()->addException($e, $e->getMessage());
+        } catch (Exception $e) {
+            $this->_getSession()->addException($e, $this->__('Unable to find a banner to delete.'));
+        }
+        $this->_redirect('*/*/');
+    }
+
+    /**
+     * Check the permission to run it
+     *
+     * @return boolean
+     */
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('mobile');
+    }
+
+    /**
+     * Render apps grid
+     */
+    public function gridAction()
+    {
+        $this->loadLayout(false);
+        $this->renderLayout();
+    }
+}
