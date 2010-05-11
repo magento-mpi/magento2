b5164a1931614a6a7c08ee0979266af72fe6f423
Ruslan Voitenko <ruslan.voytenko@magento.com>
2010-05-11 15:05:09 +0000

    Magento Pro (Commercial license) update

--- tools/license/config.php
+++ tools/license/config.php
@@ -104,6 +104,30 @@ define('NOTICE_EE',
  * @license     http://www.magentocommerce.com/license/enterprise-edition
  */');
 
+define('NOTICE_PRO',
+'/**
+ * Magento Commercial Edition
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magento Commercial Edition License
+ * that is available at: http://www.magentocommerce.com/license/commercial-edition
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    {category}
+ * @package     {package}
+ * @copyright   Copyright (c) 2010 Magento, Inc.
+ * @license     http://www.magentocommerce.com/license/commercial-edition
+ */');
+
 define('PHOENIX_OSL',
 '/**
  * Magento
