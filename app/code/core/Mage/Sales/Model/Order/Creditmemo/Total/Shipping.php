ce42f5f26b29fb246f309a7050d2ec49e4de2c66
Dmitriy Volik <dmitriy.volik@magento.com>
2010-05-31 14:37:13 +0000

    fixed Division by zero issue

--- app/code/core/Mage/Sales/Model/Order/Creditmemo/Total/Shipping.php
+++ app/code/core/Mage/Sales/Model/Order/Creditmemo/Total/Shipping.php
@@ -52,13 +52,13 @@ class Mage_Sales_Model_Order_Creditmemo_Total_Shipping extends Mage_Sales_Model_
          */
         if ($creditmemo->hasBaseShippingAmount()) {
             $baseShippingAmount = Mage::app()->getStore()->roundPrice($creditmemo->getBaseShippingAmount());
-            if ($isShippingInclTax) {
+            if ($isShippingInclTax && $baseShippingInclTax != 0) {
                 $part = $baseShippingAmount/$baseShippingInclTax;
                 $shippingInclTax    = Mage::app()->getStore()->roundPrice($shippingInclTax*$part);
                 $baseShippingInclTax= $baseShippingAmount;
                 $baseShippingAmount = Mage::app()->getStore()->roundPrice($baseShipping*$part);;
             }
-            if ($baseShippingAmount<= $baseAllowedAmount) {
+            if ($baseShippingAmount<= $baseAllowedAmount && $baseShipping != 0) {
                 $shipping       = $shipping*$baseShippingAmount/$baseShipping;
                 $shipping       = Mage::app()->getStore()->roundPrice($shipping);
                 $baseShipping   = $baseShippingAmount;
@@ -69,8 +69,10 @@ class Mage_Sales_Model_Order_Creditmemo_Total_Shipping extends Mage_Sales_Model_
                 );
             }
         } else {
-            $shippingInclTax    = Mage::app()->getStore()->roundPrice($shippingInclTax * $allowedAmount/$shipping);
-            $baseShippingInclTax= Mage::app()->getStore()->roundPrice($baseShippingInclTax * $baseAllowedAmount/$baseShipping);
+            if ($baseShipping != 0) {
+                $shippingInclTax    = Mage::app()->getStore()->roundPrice($shippingInclTax * $allowedAmount/$shipping);
+                $baseShippingInclTax= Mage::app()->getStore()->roundPrice($baseShippingInclTax * $baseAllowedAmount/$baseShipping);
+            }
             $shipping           = $allowedAmount;
             $baseShipping       = $baseAllowedAmount;
         }
