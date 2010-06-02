da68644a26971deadb5d8e409501114d04bc5dc2
Victor Rad <victor.rad@magento.com>
2010-06-02 08:28:34 +0000

    fix: add sales_ prefix to model entity

--- app/code/core/Enterprise/Customer/sql/enterprise_customer_setup/mysql4-upgrade-0.1.0-0.1.1.php
+++ app/code/core/Enterprise/Customer/sql/enterprise_customer_setup/mysql4-upgrade-0.1.0-0.1.1.php
@@ -34,29 +34,29 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
--- DROP TABLE IF EXISTS `{$installer->getTable('enterprise_customer/order')}`;
-CREATE TABLE `{$installer->getTable('enterprise_customer/order')}` (
+-- DROP TABLE IF EXISTS `{$installer->getTable('enterprise_customer/sales_order')}`;
+CREATE TABLE `{$installer->getTable('enterprise_customer/sales_order')}` (
   `entity_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`entity_id`),
   CONSTRAINT `FK_ENTERPRISE_CUSTOMER_SALES_ORDER` FOREIGN KEY (`entity_id`) REFERENCES `{$installer->getTable('sales/order')}` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- DROP TABLE IF EXISTS `{$installer->getTable('enterprise_customer/order_address')}`;
-CREATE TABLE `{$installer->getTable('enterprise_customer/order_address')}` (
+-- DROP TABLE IF EXISTS `{$installer->getTable('enterprise_customer/sales_order_address')}`;
+CREATE TABLE `{$installer->getTable('enterprise_customer/sales_order_address')}` (
   `entity_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`entity_id`),
   CONSTRAINT `FK_ENTERPRISE_CUSTOMER_SALES_ORDER_ADDRESS` FOREIGN KEY (`entity_id`) REFERENCES `{$installer->getTable('sales/order_address')}` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- DROP TABLE IF EXISTS `{$installer->getTable('enterprise_customer/quote')}`;
-CREATE TABLE `{$installer->getTable('enterprise_customer/quote')}` (
+-- DROP TABLE IF EXISTS `{$installer->getTable('enterprise_customer/sales_quote')}`;
+CREATE TABLE `{$installer->getTable('enterprise_customer/sales_quote')}` (
   `entity_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`entity_id`),
   CONSTRAINT `FK_ENTERPRISE_CUSTOMER_SALES_QUOTE` FOREIGN KEY (`entity_id`) REFERENCES `{$installer->getTable('sales/quote')}` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- DROP TABLE IF EXISTS `{$installer->getTable('enterprise_customer/quote_address')}`;
-CREATE TABLE `{$installer->getTable('enterprise_customer/quote_address')}` (
+-- DROP TABLE IF EXISTS `{$installer->getTable('enterprise_customer/sales_quote_address')}`;
+CREATE TABLE `{$installer->getTable('enterprise_customer/sales_quote_address')}` (
   `entity_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`entity_id`),
   CONSTRAINT `FK_ENTERPRISE_CUSTOMER_SALES_QUOTE_ADDRESS` FOREIGN KEY (`entity_id`) REFERENCES `{$installer->getTable('sales/quote_address')}` (`address_id`) ON DELETE CASCADE ON UPDATE CASCADE
