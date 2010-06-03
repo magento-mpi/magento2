b25031d212d248ae6e167311285a211ee9e0c3df
Vladimir Pelipenko <vladimir.pelipenko@magento.com>
2010-06-03 14:46:31 +0000

    Gift Registry required changes to allow checkout with hidden addresses: - added event dispatch to customer address format method; - changed filter to get address id in checkout multishipping model.

--- app/code/core/Mage/Customer/Model/Address/Abstract.php
+++ app/code/core/Mage/Customer/Model/Address/Abstract.php
@@ -302,6 +302,7 @@ class Mage_Customer_Model_Address_Abstract extends Mage_Core_Model_Abstract
             || !$formatType->getRenderer()) {
             return null;
         }
+        Mage::dispatchEvent('customer_address_format', array('type' => $formatType, 'address' => $this));
         return $formatType->getRenderer()->render($this);
     }
 
