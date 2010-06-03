2472cdf7493e2d4d1a51ef2775f35a4ed4b3a1cc
Andrey Maybrodskiy <andrey.maybrodskiy@magento.com>
2010-06-03 10:25:03 +0000

    Save customer additional attributes

---
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Customer/Show/Customer.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @copyright   Copyright (c) 2009 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Customer Show Customer Model
+ *
+ * @category   Mage
+ * @package    Mage_Adminhtml
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Adminhtml_Model_System_Config_Backend_Customer_Show_Customer extends Mage_Core_Model_Config_Data
+{
+    /**
+     * Actions after save
+     *
+     * @return Mage_Adminhtml_Model_System_Config_Backend_Customer_Address_Street
+     */
+    protected function _afterSave()
+    {
+        $field         = $this->getField();
+        $attributeCode = str_replace('show_', '', $field);
+
+        $attributeObjects = array(
+            Mage::getSingleton('eav/config')->getAttribute('customer', $attributeCode)
+        );
+        $valueConfig = array(
+            ''    => array('is_required' => 0, 'is_visible' => 0),
+            'opt' => array('is_required' => 0, 'is_visible' => 1),
+            'req' => array('is_required' => 1, 'is_visible' => 1),
+        );
+
+        $value  = $this->getValue();
+
+        foreach ($attributeObjects as $attributeObject) {
+            if ($this->getScope() == 'website') {
+                $website = Mage::app()->getWebsite($this->getWebsiteCode());
+                $attributeObject->setWebsite($website);
+                $dataFieldPrefix = 'scope_';
+            } else {
+                $dataFieldPrefix = '';
+            }
+
+            if (isset($valueConfig[$value])) {
+                $data = $valueConfig[$value];
+            } else {
+                $data = $valueConfig[''];
+            }
+
+            $isRequired = $attributeObject->getData['is_required'];
+            $isVisible  = $attributeObject->getData['is_visible'];
+
+            if ($isRequired != $data['is_required'] && $isVisible != $data['is_visible']) {
+                $attributeObject->setData("{$dataFieldPrefix}is_required", $data['is_required']);
+                $attributeObject->setData("{$dataFieldPrefix}is_visible",  $data['is_visible']);
+            }
+            $attributeObject->save();
+        }
+        return $this;
+    }
+}
