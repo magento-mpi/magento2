896db2aebce58b6e7117af27a368f376b34def92
Sergey Shymko <sergey.shymko@magento.com>
2010-06-07 16:56:01 +0000

    Fixed date range condition in tax report with order updated filter

--- app/code/core/Mage/Tax/Model/Mysql4/Report/Updatedat/Collection.php
+++ app/code/core/Mage/Tax/Model/Mysql4/Report/Updatedat/Collection.php
@@ -126,17 +126,6 @@ class Mage_Tax_Model_Mysql4_Report_Updatedat_Collection extends Mage_Sales_Model
         $columns = $this->_getSelectedColumns();
         $mainTable = $this->getResource()->getMainTable();
 
-        if (!is_null($this->_from) || !is_null($this->_to)) {
-            $where = (!is_null($this->_from)) ? "so.updated_at >= '{$this->_from}'" : '';
-            if (!is_null($this->_to)) {
-                $where .= (!empty($where)) ? " AND so.updated_at <= '{$this->_to}'" : "so.updated_at <= '{$this->_to}'";
-            }
-
-            $subQuery = clone $this->getSelect();
-            $subQuery->from(array('so' => $mainTable), array('DISTINCT DATE(so.updated_at)'))
-                ->where($where);
-        }
-
         $select = $this->getSelect()
             ->from(array('e' => $mainTable), $columns)
             ->joinInner(array('tax'=> $this->getTable('tax/sales_order_tax')), 'e.entity_id = tax.order_id', array());
@@ -144,8 +133,12 @@ class Mage_Tax_Model_Mysql4_Report_Updatedat_Collection extends Mage_Sales_Model
         $this->_applyStoresFilter();
         $this->_applyOrderStatusFilter();
 
-        if (!is_null($this->_from) || !is_null($this->_to)) {
-            $select->where("DATE(e.updated_at) IN(?)", new Zend_Db_Expr($subQuery));
+        if ($this->_from !== null) {
+            $select->where('DATE(e.updated_at) >= DATE(?)', $this->_from);
+        }
+
+        if ($this->_to !== null) {
+            $select->where('DATE(e.updated_at) <= DATE(?)', $this->_to);
         }
 
         if (!$this->isTotals() && !$this->isSubTotals()) {
