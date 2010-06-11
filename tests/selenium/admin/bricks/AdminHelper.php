eb63571d02bc627c1d9b2a9b9a70e7c63d09f56f
Dmitriy Romanov <dmitriy.romanov@magento.com>
2010-06-11 19:47:49 +0000

    ++AdminHelper.php

---
+++ tests/selenium/admin/bricks/AdminHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+class AdminHelper
+{
+    protected $_object;
+    public function  __construct($object = null) {
+        $this->_object = $object;
+    }
+	public function doLogin($baseurl, $username, $password)
+	{
+            $this->_object->open($baseurl);
+            $this->_object->waitForPageToLoad("10000");
+            $this->_object->type("username", $username);
+            $this->_object->type("login", $password);
+            $this->_object->click("//input[@title='Login']");
+            $this->_object->waitForPageToLoad("90000");
+
+	}
+
+    public function pleaseWait() {
+        //
+        // await for appear and disappear "Please wait" animated gif...
+        for ($second = 0; ; $second++) {
+            if ($second >= 60)  break; //fail("timeout");
+            try {
+                if (!$this->_object->isElementPresent("//div[@id='loading-mask' and contains(@style,'display: none')]")) break;
+            } catch (Exception $e) {
+
+            }
+            sleep(1);
+        }
+
+        for ($second = 0; ; $second++) {
+            if ($second >= 60)break;
+            try {
+                if ($this->_object->isElementPresent("//div[@id='loading-mask' and contains(@style,'display: none')]")) break;
+            } catch (Exception $e) {
+
+            }
+            sleep(1);
+        }
+    }
+}
