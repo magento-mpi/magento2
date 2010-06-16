66c5902915e574636f71d12e0f1a5605f1140573
Nikolay Boyko <nikolay.boyko@magento.com>
2010-06-16 08:34:17 +0000

    resolve file Application.php (added: submit application form for the  Mobile Application (XmlConnect module))

--- app/code/core/Mage/XmlConnect/Model/Application.php
+++ app/code/core/Mage/XmlConnect/Model/Application.php
@@ -32,6 +32,13 @@ class Mage_XmlConnect_Model_Application extends Mage_Core_Model_Abstract
     const APP_CODE_COOKIE_NAME = 'app_code';
 
     /**
+     * Url link to our aggregator.
+     * We can't change it lately.
+     * @var string
+     */
+    const APP_CONNECTOR_URL = 'www.magentocommerce.com/mobile/activate/';
+
+    /**
      * Initialize application
      */
     protected function _construct()
@@ -307,4 +314,53 @@ class Mage_XmlConnect_Model_Application extends Mage_Core_Model_Abstract
         $this->_getResource()->loadByCode($this, $code);
         return $this;
     }
+    
+
+    /**
+     * Send HTTP POST request to magentocommerce.com
+     *
+     * @param array $request
+     * 
+     * @throws Exception
+     */
+    public function processPostRequest($params = array())
+    {
+        try {
+            $params['name'] = $this->getName();
+            $params['app_code'] = $this->getCode();
+            $params['url'] = Mage::helper('xmlconnect/data')->getUrl('*/*', array('app_code' => $this->getCode()));
+
+            $confSubmit = $this->_data['conf']['submit'];
+            $dir = Mage::getBaseDir('media') . DS . 'xmlconnect' . DS;
+            $params['appIcon'] = '@' . $dir . $confSubmit['appIcon'];
+            $params['loaderImage'] = '@' . $dir . $confSubmit['loaderImage'];
+            $params['logo'] = '@' . $dir . $confSubmit['logo'];
+            $params['big_logo'] = '@' . $dir . $confSubmit['big_logo'];
+
+            $ch = curl_init(self::APP_CONNECTOR_URL . $params['key']);
+            // set URL and other appropriate options
+            curl_setopt($ch, CURLOPT_POST,1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,  2);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+
+            // Execute the request.
+            $result = curl_exec($ch);
+            $succeeded  = curl_errno($ch) == 0 ? true : false;
+
+            // close cURL resource, and free up system resources
+            curl_close($ch);
+
+            // Assert that we received an expected Centinel Message in reponse.
+            if ($result != '{"message":"","success":true}') {
+                throw new Exception('Submit Application postback failure.');
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+
 }
