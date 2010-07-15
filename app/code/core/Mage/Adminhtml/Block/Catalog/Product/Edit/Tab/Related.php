8652ba66b06dc74605cf4b45beea6d95adef6b45
Victor Tihonchuk <noreply@magento.com>
2010-07-15 15:00:14 +0000

    Fixed #MAGE-663 Related products are not saved when you attach them to a product

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Related.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Related.php
@@ -206,7 +206,7 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Related extends Mage_Adminht
             'validate_class'    => 'validate-number',
             'index'             => 'position',
             'width'             => 60,
-            'editable'          => !$this->_getProduct()->getUpsellReadonly(),
+            'editable'          => !$this->_getProduct()->getRelatedReadonly(),
             'edit_only'         => !$this->_getProduct()->getId()
         ));
 
