5a5e48ddd281f2f2edbaefd71a22392fe4c3878c
Alex Bomko <alex.bomko@magento.com>
2010-07-30 14:14:30 +0000

    Fixed #MAGE-670 Magento Connect -> Message about invalid URL is duplicated

--- app/code/core/Mage/Install/Model/Installer/Config.php
+++ app/code/core/Mage/Install/Model/Installer/Config.php
@@ -154,7 +154,7 @@ class Mage_Install_Model_Installer_Config extends Mage_Install_Model_Installer_A
 
         if ($body != Mage_Install_Model_Installer::INSTALLER_HOST_RESPONSE) {
             $this->_getInstaller()->getDataModel()->addError(Mage::helper('install')->__('The URL "%s" is invalid.', $url));
-            Mage::throwException(Mage::helper('install')->__('This URL is invalid.'));
+            Mage::throwException(Mage::helper('install')->__('Response from server isn\'t valid.'));
         }
         return $this;
     }
