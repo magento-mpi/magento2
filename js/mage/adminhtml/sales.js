91f679634d580466ab940dedb85cc641c67b7e4f
automation <noreply@magento.com>
2010-08-11 15:39:50 +0000

    Merged revisions 76419 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r76419 | vladimir.pelipenko | 2010-08-11 18:36:52 +0300 (Wed, 11 Aug 2010) | 2 lines
      
      Fixed MAGE-758: while creating gift message from backend it disappears in case of refreshing page
    ........

--- js/mage/adminhtml/sales.js
+++ js/mage/adminhtml/sales.js
@@ -677,6 +677,14 @@ AdminOrder.prototype = {
                 params[value[0]] = value[1];
             });
         }
+        if ($('order-giftmessage')) {
+            data = this.serializeData('order-giftmessage');
+            if (data) {
+                data.each(function(value) {
+                    params[value[0]] = value[1];
+                });
+            }
+        }
         return params;
     },
 
