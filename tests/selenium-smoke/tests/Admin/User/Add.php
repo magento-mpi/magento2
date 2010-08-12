c70b30d5597c18b4c5dd33fda6c8a7c5b713258a
automation <noreply@magento.com>
2010-08-12 15:16:30 +0000

    Merged revisions 76583 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r76583 | dmitriy.romanov | 2010-08-12 18:13:11 +0300 (Thu, 12 Aug 2010) | 1 line
      
      **admin_user_add: ++parameter object
    ........

--- tests/selenium-smoke/tests/Admin/User/Add.php
+++ tests/selenium-smoke/tests/Admin/User/Add.php
@@ -4,11 +4,6 @@ class Admin_User_Add extends Test_Admin_User_Abstract
 {
 
     /**
-     * Add new user to the system
-     *
-     */
-
-    /**
      * Setup procedure.
      * Must be overriden in the children having any additional code prepended with parent::setUp();
      */
@@ -16,8 +11,15 @@ class Admin_User_Add extends Test_Admin_User_Abstract
         parent::setUp();
     }
 
+    /**
+     * Add new user to the system
+     *
+     */
+
     function testUserCreation() {
         $this->debug("testUserCreation started");
+
+        // Test Flow
         $this->adminLogin($this->_baseUrl, $this->_userName, $this->_password);
         $this->addUser("test");
         $this->debug("testUserCreation finished");
