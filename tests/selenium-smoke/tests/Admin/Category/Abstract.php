45181c4fd5161f6c474f271222422947bbfffbdf
automation <noreply@magento.com>
2010-08-16 13:55:34 +0000

    Merged revisions 76863 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r76863 | dmitriy.romanov | 2010-08-16 16:31:18 +0300 (Mon, 16 Aug 2010) | 1 line
      
      ** Test_Admin_Category_Abstract: addSubCategory and addRootCategory functions added
    ........

--- tests/selenium-smoke/tests/Admin/Category/Abstract.php
+++ tests/selenium-smoke/tests/Admin/Category/Abstract.php
@@ -20,23 +20,28 @@ abstract class Test_Admin_Category_Abstract extends Test_Admin_Abstract
         parent::setUp();
    }
 
+
     /**
-     * Adds new root category into the
-     *@param $CategorName
-     *@param $StoreViewName
+     * Adds new sub $categoryName for $parentCategoryName into the $storeViewName store view
+     *@param $categorName
+     *@param $parentCategoryName
+     *@param $storeViewName
      * 
      */
-
-    public function addRootCategory($CategorName, $StoreViewName) {
-      $this->debug("addRootCategory started");
+    public function addSubCategory($categoryName, $parentCategoryName, $storeViewName) {
+      $this->debug("addSubCategory started");
       // Open Manage Categories Page
       $this->clickAndWait ($this->getUiElement("admin/topmenu/catalog/categories/managecategories"));
-      // Add new root ca
-      $this->click($this->getUiElement("admin/pages/catalog/categories/managecategories/buttons/addrootcategory"));
+      //Select Parent Category
+      $this->click($this->getUiElement("admin/pages/catalog/categories/managecategories/locators/parentcategory",$parentCategoryName));
+      // Add new sub category
+      $this->click($this->getUiElement("admin/pages/catalog/categories/managecategories/buttons/addsubcategory"));
       $this->pleaseWait();
       // Fill all fields
-      $this->type($this->getUiElement("admin/pages/catalog/categories/managecategories/inputs/name"),$CategorName);
+      $this->type($this->getUiElement("admin/pages/catalog/categories/managecategories/inputs/name"),$categoryName);
       $this->select($this->getUiElement("admin/pages/catalog/categories/managecategories/selectors/isactive"),"label=Yes");
+      $this->click($this->getUiElement("admin/pages/catalog/categories/managecategories/tabs/displaysettings"));
+      $this->select($this->getUiElement("admin/pages/catalog/categories/managecategories/selectors/isanchor"),"label=Yes");
       // Save category
       $this->clickAndWait($this->getUiElement("admin/pages/catalog/categories/managecategories/buttons/savecategory"));
       $this->pleaseWait();
@@ -44,6 +49,32 @@ abstract class Test_Admin_Category_Abstract extends Test_Admin_Abstract
       if (!$this->isElementPresent($this->getUiElement("admin/pages/catalog/categories/managecategories/messages/categorysaved"))) {
         $this->setVerificationErrors("addRootCategory check 1: no success message");
       } 
+      $this->debug("addSubCategory finished");
+    }
+
+    /**
+     * Adds new root $categoryName category into the $storeViewName store view
+     *@param $categoryName
+     *@param $storeViewName
+     *
+     */
+    public function addRootCategory($categoryName, $storeViewName) {
+      $this->debug("addRootCategory started");
+      // Open Manage Categories Page
+      $this->clickAndWait ($this->getUiElement("admin/topmenu/catalog/categories/managecategories"));
+      // Add new root ca
+      $this->click($this->getUiElement("admin/pages/catalog/categories/managecategories/buttons/addrootcategory"));
+      $this->pleaseWait();
+      // Fill all fields
+      $this->type($this->getUiElement("admin/pages/catalog/categories/managecategories/inputs/name"),$categoryName);
+      $this->select($this->getUiElement("admin/pages/catalog/categories/managecategories/selectors/isactive"),"label=Yes");
+      // Save category
+      $this->clickAndWait($this->getUiElement("admin/pages/catalog/categories/managecategories/buttons/savecategory"));
+      $this->pleaseWait();
+      // Check for success message
+      if (!$this->isElementPresent($this->getUiElement("admin/pages/catalog/categories/managecategories/messages/categorysaved"))) {
+        $this->setVerificationErrors("addRootCategory check 1: no success message");
+      }
       $this->debug("addRootCategory finished");
     }
 
