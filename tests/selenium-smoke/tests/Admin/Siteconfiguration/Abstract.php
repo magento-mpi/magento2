6cd57a610f727a3b0074580418421d7867c39a28
automation <noreply@magento.com>
2010-08-17 18:41:57 +0000

    Merged revisions 77159 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r77159 | andrey.poltoratskiy | 2010-08-17 21:29:41 +0300 (Tue, 17 Aug 2010) | 1 line
      
      ++Test_Admin_Siteconfiguration: new TC added  (abstract.php and setup.php)
    ........

---
+++ tests/selenium-smoke/tests/Admin/Siteconfiguration/Abstract.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Abstract test class for Admin module
+ *
+ * @author Magento Inc.
+ */
+abstract class Test_Admin_Siteconfiguration_Abstract extends Test_Admin_Abstract
+{
+    /**
+     * Helper local instance
+     *
+     * @var Helper_Admin
+     */
+    protected $_helper = null;
+
+    /**
+     * Initialize the environment
+     */
+    public function  setUp() {
+        parent::setUp();
+
+        // Get test parameters
+        $this->_siteName = Core::getEnvConfig('backend/managestores/site/name');
+        $this->_siteCode = Core::getEnvConfig('backend/managestores/site/code');
+    }
+
+    /**
+     * Admin-System-Configuration-Web: configurate base URL
+     *
+     */
+     public function configURL() {
+        Core::debug("adding URL started");
+        $this->clickAndWait($this->getUiElement("admin/topmenu/system/configuration"));
+        $this->selectAndWait($this->getUiElement("admin/pages/system/configuration/selectors/store"), $this->_siteName);
+        $this->clickAndWait($this->getUiElement("admin/pages/system/configuration/buttons/web"));
+        $this->click($this->getUiElement("admin/pages/system/configuration/buttons/secure"));
+        $this->click($this->getUiElement("admin/pages/system/configuration/checkboxes/secure/baselink"));
+        $this->waitForElement($this->getUiElement("admin/pages/system/configuration/inputs/secure/baselink"), 30);
+        $this->type($this->getUiElement("admin/pages/system/configuration/inputs/secure/baselink"), '{{secure_base_url}}' . $this->_siteCode . '/');
+        $this->click($this->getUiElement("admin/pages/system/configuration/buttons/unsecure"));
+        $this->click($this->getUiElement("admin/pages/system/configuration/checkboxes/unsecure/baselink"));
+        $this->waitForElement($this->getUiElement("admin/pages/system/configuration/inputs/unsecure/baselink"), 30);
+        $this->type($this->getUiElement("admin/pages/system/configuration/inputs/unsecure/baselink"), '{{unsecure_base_url}}' . $this->_siteCode . '/');
+        $this->click($this->getUiElement("admin/pages/system/configuration/buttons/save"));
+        $this->waitForElement($this->getUiElement("admin/messages/success"), 30);
+
+        //check for successful message
+        if (!$this->isElementPresent($this->getUiElement("admin/messages/success"))) {
+            $this->setVerificationErrors("Check 1 : No successfull message");
+        }
+        if (!$this->isTextPresent($this->getUiElement("admin/pages/system/configuration/inputs/secure/baselink"), '{{secure_base_url}}' . $this->_siteCode . '/')) {
+            $this->setVerificationErrors("Check 2 : value wasn't saved");
+        }
+        if (!$this->isTextPresent($this->getUiElement("admin/pages/system/configuration/inputs/unsecure/baselink"), '{{unsecure_base_url}}' . $this->_siteCode . '/')) {
+            $this->setVerificationErrors("Check 3 : value wasn't saved");
+        }
+        Core::debug("adding URL finished");       
+     }
+    /**
+     * Admin-System-Configuration: Reindex Data
+     *
+     */
+     public function reindex() {
+        Core::debug("reindex...");
+        $this->clickAndWait($this->getUiElement("admin/topmenu/system/indexmanagement"));
+        $this->click($this->getUiElement("admin/pages/system/reindex/buttons/selectall"));
+        $this->click($this->getUiElement("admin/pages/system/reindex/buttons/submit"));
+        $this->waitForElement($this->getUiElement("admin/messages/success"), 30);
+
+        //check for successful message
+        if (!$this->isElementPresent($this->getUiElement("admin/messages/success"))) {
+            $this->setVerificationErrors("Check 1 : No successfull message");
+        }
+        if (!$this->isTextPresent($this->getUiElement("admin/pages/system/reindex/messages/allsuccess"))) {
+            $this->setVerificationErrors("Check 2 : data wasn't reindexed");
+        }
+        Core::debug("reidex finished");
+     }
+
+}
+
