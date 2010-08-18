16591b41eb30105ba9b12b2bcdff08509c844be4
automation <noreply@magento.com>
2010-08-18 08:46:13 +0000

    Merged revisions 77174 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r77174 | victor.rad | 2010-08-18 11:40:57 +0300 (Wed, 18 Aug 2010) | 1 line
      
      Fixed MAGE-851 Report > Products Ordered ignores Store view switcher
    ........

--- app/code/core/Mage/Reports/Model/Resource/Product/Sold/Collection.php
+++ app/code/core/Mage/Reports/Model/Resource/Product/Sold/Collection.php
@@ -59,9 +59,11 @@ class Mage_Reports_Model_Resource_Product_Sold_Collection extends Mage_Reports_M
      */
     public function setStoreIds($storeIds)
     {
-        $storeId = array_pop($storeIds);
-        $this->setStoreId($storeId);
-        $this->addStoreFilter($storeId);
+        if (!empty($storeIds[0])) {
+            $storeIds = array_values($storeIds);
+            $this->getSelect()->where('order_items.store_id IN(?)', $storeIds);
+        }
+
         return $this;
     }
 }
