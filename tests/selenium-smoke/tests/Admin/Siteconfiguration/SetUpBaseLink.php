fc15cfb19bbc7ffdeed2bcdc2f65179bac08f097
automation <noreply@magento.com>
2010-08-18 13:17:26 +0000

    Merged revisions 77235 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r77235 | andrey.poltoratskiy | 2010-08-18 16:03:04 +0300 (Wed, 18 Aug 2010) | 1 line
      
      **Admin_Siteconfiguration_SetUpBaseLink: name of file was changed
    ........

---
+++ tests/selenium-smoke/tests/Admin/Siteconfiguration/SetUpBaseLink.php
@@ -0,0 +1,21 @@
+<?php
+
+class Admin_Siteconfiguration_SetUpBaseLink extends Test_Admin_Siteconfiguration_Abstract
+{
+
+    /**
+     * Setup procedure.
+     * Must be overriden in the children having any additional code prepended with parent::setUp();
+     */
+    function setUp() {
+        parent::setUp();
+    }
+
+    function testSiteConfiguration() {
+        Core::debug("testSiteCreation started");
+        $this->adminLogin($this->_baseUrl, $this->_userName, $this->_password);
+        $this->configURL();
+        $this->reindex();
+        Core::debug("testSiteCreation finished");
+    }
+}
