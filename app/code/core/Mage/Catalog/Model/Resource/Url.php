85ed436441bb2289346140a0e66a8710888ea891
automation <noreply@magento.com>
2010-08-19 09:20:18 +0000

    Merged revisions 77336 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r77336 | anton.makarenko | 2010-08-19 12:15:42 +0300 (Thu, 19 Aug 2010) | 1 line
      
      MAGE-817 - additional code style changes for 77224
    ........

--- app/code/core/Mage/Catalog/Model/Resource/Url.php
+++ app/code/core/Mage/Catalog/Model/Resource/Url.php
@@ -665,8 +665,9 @@ class Mage_Catalog_Model_Resource_Url extends Mage_Core_Model_Resource_Db_Abstra
             if (!is_null($storeId)) {
                 // Check the category to be either store's root or its descendant
                 // First - check that category's start is the same as root category
-                if (substr($row['path'], 0, $rootCategoryPathLength) != $rootCategoryPath)
+                if (substr($row['path'], 0, $rootCategoryPathLength) != $rootCategoryPath) {
                     continue;
+                }
                 // Second - check non-root category - that it's really a descendant, not a simple string match
                 if ((strlen($row['path']) > $rootCategoryPathLength) && ($row['path'][$rootCategoryPathLength] != '/')) {
                     continue;
