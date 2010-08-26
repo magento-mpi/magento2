50deec2fae33e8320ccffcb97b6785c5f94a395d
automation <noreply@magento.com>
2010-08-26 17:05:23 +0000

    Merged revisions 78045 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r78045 | dimav | 2010-08-26 19:48:01 +0300 (Thu, 26 Aug 2010) | 1 line
      
      Fixed brute test for builds and archive
    ........

--- tools/build/brutetest.sh
+++ tools/build/brutetest.sh
@@ -4,6 +4,7 @@
 cd $PWD
 
 log "Testing site..."
+chmod +x $BUILD_TOOLS/pavuk
 $BUILD_TOOLS/pavuk -quiet -nredirs 10 -retry 5 -mode dontstore --logfile /dev/null -lmax 5 -dmax 50 -remove_adv -skip_pattern *.jpg,*.gif,*.css,*.ico,*.png -noRobots -adomain kq.varien.com http://kq.varien.com/builds/$BUILD_NAME/$BUILD_NUMBER
 check_failure $?
 
