6d04595eed6346781a94cefa810560993ddf7924
automation <noreply@magento.com>
2010-08-30 16:43:22 +0000

    Merged revisions 78374 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r78374 | evgeniy.maksimenko | 2010-08-30 19:35:34 +0300 (Mon, 30 Aug 2010) | 1 line
      
      Updating
    ........

--- tests/selenium-smoke/tests/Admin/AttributeSet/AddSet.php
+++ tests/selenium-smoke/tests/Admin/AttributeSet/AddSet.php
@@ -1,12 +1,11 @@
 <?php
 
-class Admin_AttributeSet_AddSet extends Test_Admin_AttributeSet_Abstract
-{
+class Admin_AttributeSet_AddSet extends Test_Admin_AttributeSet_Abstract {
 
     /**
-     * Setup procedure.
-     * Must be overriden in the children having any additional code prepended with parent::setUp();
-     */
+    * Setup procedure.
+    * Must be overriden in the children having any additional code prepended with parent::setUp();
+    */
     function setUp() {
         parent::setUp();
 
@@ -14,11 +13,9 @@ class Admin_AttributeSet_AddSet extends Test_Admin_AttributeSet_Abstract
         $this->_attributeSetName = Core::getEnvConfig('backend/attributeSet/setName');
     }
 
-
     /**
-     * Test addition new Attribute Set
-     *
-     */
+    * Test addition new Attribute Set
+    */
     function testAttributeSetCreation() {
         Core::debug("testAttributeSetCreation started");
         // Test Flow
