225b671032116fc7c7cdadea4e00745c4d117f90
automation <noreply@magento.com>
2010-08-30 16:40:29 +0000

    Merged revisions 78373 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r78373 | evgeniy.maksimenko | 2010-08-30 19:35:22 +0300 (Mon, 30 Aug 2010) | 1 line
      
      Updating
    ........

--- tests/selenium-smoke/tests/Admin/AttributeSet/Abstract.php
+++ tests/selenium-smoke/tests/Admin/AttributeSet/Abstract.php
@@ -4,13 +4,11 @@
  *
  * @author Magento Inc.
  */
-abstract class Test_Admin_AttributeSet_Abstract extends Test_Admin_Abstract
-{
+abstract class Test_Admin_AttributeSet_Abstract extends Test_Admin_Abstract {
     /**
-     * Helper local instance
-     *
-     * @var Helper_Admin
-     */
+    * Helper local instance
+    * @var Helper_Admin
+    */
     protected $_helper = null;
 
     /**
@@ -18,35 +16,34 @@ abstract class Test_Admin_AttributeSet_Abstract extends Test_Admin_Abstract
      */
     public function  setUp() {
         parent::setUp();
-   }
-
+    }
 
     /**
-     * Adds new attribute set $SetName
-     *@param $setName
-     */
+    * Adds new attribute set $SetName
+    *@param $setName
+    */
     public function addSet($setName)
     {
-      Core::debug("addSet started");
-      // Open Manage Attribute Sets
-      $this->clickAndWait($this->getUiElement("admin/topmenu/catalog/attributes/manageAttibuteSet"));
-      // Add new Attribute Set
-      $this->clickAndWait($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/buttons/addSet"));
-      // Fill fields Name and Based On
-      $this->type($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/input/nameForSet"),$setName);
-      $this->select($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/input/basedOn"),"label=Default");
-      // Saving
-      $this->clickAndWait($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/buttons/saveSet"));
-      // check for error message
-      if ($this->waitForElement($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/messages/setNotSaved"),10)) {
+    Core::debug("addSet started");
+    // Open Manage Attribute Sets
+    $this->clickAndWait($this->getUiElement("admin/topmenu/catalog/attributes/manageAttibuteSet"));
+    // Add new Attribute Set
+    $this->clickAndWait($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/buttons/addSet"));
+    // Fill fields Name and Based On
+    $this->type($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/input/nameForSet"),$setName);
+    $this->select($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/input/basedOn"),"label=Default");
+    // Saving
+    $this->clickAndWait($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/buttons/saveSet"));
+    // check for error message
+    if ($this->waitForElement($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/messages/setNotSaved"),10)) {
         $etext = $this->getText($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/messages/setNotSaved"));
         Core::debug($etext );
         $this->setVerificationErrors("Check 1: " . $etext);
-      } else {
-      // Check for success message
-          if (!$this->waitForElement($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/messages/setSaved"),10)) {
-            $this->setVerificationErrors("Check 2: no success message");
-          }
-      }
+    } else {
+    // Check for success message
+    if (!$this->waitForElement($this->getUiElement("admin/pages/catalog/attributes/manageAttributeSets/messages/setSaved"),10)) {
+        $this->setVerificationErrors("Check 2: no success message");
+    }
+    }
     }
 }
