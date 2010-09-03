ac4eef12080136a58f89e56d9b71282c97122dde
automation <noreply@magento.com>
2010-09-03 09:47:33 +0000

    Merged revisions 78483 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r78483 | evgeniy.maksimenko | 2010-08-31 15:58:35 +0300 (Tue, 31 Aug 2010) | 1 line
      
      update
    ........

--- tests/selenium-smoke/tests/Admin/OrderCreation/OrderCreate.php
+++ tests/selenium-smoke/tests/Admin/OrderCreation/OrderCreate.php
@@ -11,7 +11,7 @@ class Test_Admin_OrderCreation_OrderCreate extends Test_Admin_OrderCreation_Abst
         parent::setUp();
 
         // Get test parameters
-        $this->_userEmail = Core::getEnvConfig('frontend/auth/email');
+        $this->_userEmail = Core::getEnvConfig('frontend/checkout/login/email');
         $this->_storeviewName = Core::getEnvConfig('backend/managestores/storeview/name');
         $this->_productSKU = Core::getEnvConfig('backend/createproduct/sku');
     }
@@ -21,15 +21,55 @@ class Test_Admin_OrderCreation_OrderCreate extends Test_Admin_OrderCreation_Abst
      * Test create new order in Admin and Invoice
      *
      */
-    function testAdminOrderCreate() {
+    function testAdminOrderWorkFlow() {
         // Test Flow
         $this->adminLogin($this->_baseUrl, $this->_userName, $this->_password);
         $this->adminOrderCreate($this->_userEmail, $this->_storeviewName, $this->_productSKU);
-        if ($this->isElementPresent($this->getUiElement("admin/pages/sales/orders/creationOrder/messages/orderCreated"),10)){
-            $ordNum = $this->getText($this->getUiElement("admin/pages/sales/orders/creationOrder/orderNumber"));
-            $ordNum = substr($ordNum, 8, 10);
+        if(!$this->isElementPresent($this->getUiElement("admin/pages/sales/orders/creationOrder/messages/orderCreated"),10)) {
+            return FALSE;
+        } else {
+        $ordNum = $this->getText($this->getUiElement("admin/pages/sales/orders/creationOrder/orderNumber"));
+        $ordNum = substr($ordNum, 8, 10);
+        $this->clickAndWait($this->getUiElement("admin/topmenu/sales/orders"));
+        $ordStatus = $this->getText($this->getUiElement("admin/pages/sales/orders/creationInvoice/orderStatus",$ordNum));
+        if ($ordStatus != 'Pending') {
+            $this->setVerificationErrors("Order has an incorrect status after creation order");
+        } else {
             $this->openOrder($ordNum);
             $this->createInvoice();
         }
+        $this->clickAndWait($this->getUiElement("admin/topmenu/sales/orders"));
+        $ordStatus = $this->getText($this->getUiElement("admin/pages/sales/orders/creationInvoice/orderStatus",$ordNum));
+        if ($ordStatus != 'Processing') {
+            $this->setVerificationErrors("Order has an incorrect status after creation Invoice");
+        } else {
+            $this->openOrder($ordNum);
+            $this->createShippment();
+        }
+        $this->clickAndWait($this->getUiElement("admin/topmenu/sales/orders"));
+        $ordStatus = $this->getText($this->getUiElement("admin/pages/sales/orders/creationInvoice/orderStatus",$ordNum));
+        if ($ordStatus != 'Complete') {
+            $this->setVerificationErrors("Order has an incorrect status after creation Shippment");
+        } else {
+            $this->openOrder($ordNum);
+            $this->createCreditMemo();
+        }
+        $this->clickAndWait($this->getUiElement("admin/topmenu/sales/orders"));
+        $ordStatus = $this->getText($this->getUiElement("admin/pages/sales/orders/creationInvoice/orderStatus",$ordNum));
+        if ($ordStatus != 'Closed') {
+            $this->setVerificationErrors("Order has an incorrect status after creation Credit Memo");
+        } else {
+            $this->openOrder($ordNum);
+            $this->reOrder();
+        }
+        $ordNum = $this->getText($this->getUiElement("admin/pages/sales/orders/creationOrder/orderNumber"));
+        $ordNum = substr($ordNum, 8, 10);
+        $this->clickAndWait($this->getUiElement("admin/topmenu/sales/orders"));
+        $ordStatus = $this->getText($this->getUiElement("admin/pages/sales/orders/creationInvoice/orderStatus",$ordNum));
+        if ($ordStatus != 'Pending') {
+            $this->setVerificationErrors("Order has an incorrect status after ReOrder");
+        }
+        }
     }
 }
+
