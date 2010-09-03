489010097a0429fe5abbf4b556a4abda8ca2fe1f
Evgeniy Tulika <evgeniy.tulika@magento.com>
2010-09-03 14:48:49 +0000

    fix comment

--- app/code/core/Mage/Eav/sql/eav_setup/install-1.5.0.0.php
+++ app/code/core/Mage/Eav/sql/eav_setup/install-1.5.0.0.php
@@ -19,7 +19,7 @@
  * needs please refer to http://www.magentocommerce.com for more information.
  *
  * @category    Mage
- * @package     Mage_CatalogInventory
+ * @package     Mage_Eav
  * @copyright   Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
