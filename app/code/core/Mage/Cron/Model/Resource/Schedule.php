44a40b21182c87d58c5bdc2d48b262b21f98bafb
Alexey Pilguy <alexey.pilguy@magento.com>
2010-09-03 12:30:42 +0000

    install file added

--- app/code/core/Mage/Cron/Model/Resource/Schedule.php
+++ app/code/core/Mage/Cron/Model/Resource/Schedule.php
@@ -35,7 +35,7 @@
 class Mage_Cron_Model_Resource_Schedule extends Mage_Core_Model_Resource_Db_Abstract
 {
     /**
-     * Enter description here ...
+     * Initialize resource
      *
      */
     public function _construct()
--- app/code/core/Mage/Cron/Model/Resource/Schedule/Collection.php
+++ app/code/core/Mage/Cron/Model/Resource/Schedule/Collection.php
@@ -35,7 +35,7 @@
 class Mage_Cron_Model_Resource_Schedule_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
 {
     /**
-     * Enter description here ...
+     * Initialize resource collection
      *
      */
     public function _construct()
