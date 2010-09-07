0604b13baf1a04b35150cb4c0ae25948f6c91cda
automation <noreply@magento.com>
2010-09-07 19:34:37 +0000

    Merged revisions 79056 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r79056 | dimav | 2010-09-06 11:38:50 +0300 (Mon, 06 Sep 2010) | 1 line
      
      small changes in load test scripts
    ........

--- tools/build/siege.sh
+++ tools/build/siege.sh
@@ -3,22 +3,25 @@
 . include.sh
 cd "$BUILD_TOOLS/siege"
 
-$CONFIG = "mage.cfg"
+CONFIG="mage.cfg"
 
 log "Searching for last successful build..."
 SB=`wget -q -O - http://guest:@kn.varien.com/teamcity/httpAuth/app/rest/buildTypes/id:bt19/builds/status:SUCCESS/number`
 
+DB_NAME="builds-$BUILD_NAME-$SB"
+DB_NAME=${DB_NAME//-/_}
+
 sed -i 's/{{build_name}}/trunk-sample-data/g' "$CONFIG"
-sed -i 's/{{build_number}}/$SB/g' "$CONFIG"
-sed -i 's/{{db_name}}/$DB_NAME/g' "$CONFIG"
-sed -i 's/{{db_user}}/$DB_USER/g' "$CONFIG"
-sed -i 's/{{db_pass}}/$DB_PASS/g' "$CONFIG"
+sed -i "s/{{build_number}}/$SB/g" "$CONFIG"
+sed -i "s/{{db_name}}/$DB_NAME/g" "$CONFIG"
+sed -i "s/{{db_user}}/$DB_USER/g" "$CONFIG"
+sed -i "s/{{db_pass}}/$DB_PASS/g" "$CONFIG"
 
 log "Started testing..."
-./test.py -m run -c "$CONFIG" -o ../../
+./test.py -m run -c "$CONFIG" -o ..
 check_failure $?
 log "Preparing report..."
-./test.py -m report -c "$CONFIG" -o ../../
+./test.py -m report -c "$CONFIG" -o ..
 check_failure $?
 
 cd $OLDPWD
