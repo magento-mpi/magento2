c485a08626fb9cad5ea394075e163d0dd02fe83e
Nadezhda Sivokonenko <nadezhda.sivokonenko@magento.com>
2010-09-09 08:54:43 +0000

    review status ids were added from constant values

--- app/code/core/Mage/Review/data/review_setup/data-install-1.5.0.0.php
+++ app/code/core/Mage/Review/data/review_setup/data-install-1.5.0.0.php
@@ -41,14 +41,21 @@ $reviewEntityCodes = array(
     Mage_Review_Model_Review::ENTITY_CUSTOMER_CODE,
     Mage_Review_Model_Review::ENTITY_CATEGORY_CODE,
 );
-foreach($reviewEntityCodes as $entityCode){
+foreach ($reviewEntityCodes as $entityCode) {
     $installer->getConnection()
             ->insert($installer->getTable('review/review_entity'), array('entity_code' => $entityCode));
 }
 
 //Fill table review/review_entity
-$reviewStatuses = array('Approved','Pending','Not Approved');
-foreach($reviewStatuses as $statusCode){
-    $installer->getConnection()
-            ->insert($installer->getTable('review/review_status'), array('status_code' => $statusCode));
+$reviewStatuses = array(
+    Mage_Review_Model_Review::STATUS_APPROVED       => 'Approved',
+    Mage_Review_Model_Review::STATUS_PENDING        => 'Pending',
+    Mage_Review_Model_Review::STATUS_NOT_APPROVED   => 'Not Approved'
+);
+foreach ($reviewStatuses as $k => $v) {
+    $bind = array(
+        'status_id'     => $k,
+        'status_code'   => $v
+    );
+    $installer->getConnection()->insertForce($installer->getTable('review/review_status'), $bind);
 }
