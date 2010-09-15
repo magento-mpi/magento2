054c499f0afa801160fabff541f6cc16e6b42584
Anton Kaplya <anton.kaplya@magento.com>
2010-09-15 09:21:58 +0000

    add typed fetch

--- lib/Varien/Db/Statement/Oracle.php
+++ lib/Varien/Db/Statement/Oracle.php
@@ -34,6 +34,19 @@
  */
 class Varien_Db_Statement_Oracle extends Zend_Db_Statement_Oracle
 {
+
+    protected $_datetypeMap = array(
+            'INTEGER'     => 'integer',
+            'NUMBER'      => 'integer',
+            'FLOAT'       => 'float',
+            'VARCHAR2'    => 'string',
+            'TIMESTAMP(6)'=> 'string',
+            'LONG'        => 'string',
+            'TIMESTAMP'   => 'string',
+            'CLOB'        => 'string',
+            'DATE'        => 'string',
+            'VARCHAR2'    => 'string',
+        );
     /**
      * Returns an array containing all of the result set rows.
      *
@@ -42,18 +55,37 @@ class Varien_Db_Statement_Oracle extends Zend_Db_Statement_Oracle
      * @return array Collection of rows, each in a format by the fetch mode.
      * @throws Zend_Db_Statement_Exception
      */
-    public function fetchAll($style = null, $col = 0)
-    {
-        $result = parent::fetchAll($style, $col);
-        if (is_array($result)) {
-            foreach ($result as $k => $row) {
-                if (is_array($row)) {
-                    $result[$k] = array_change_key_case($row, CASE_LOWER);
-                }
-            }
-        }
-        return $result;
-    }
+//    public function fetchAll($style = null, $col = 0)
+//    {
+//        $result = parent::fetchAll($style, $col);
+//        if (is_array($result)) {
+//            foreach ($result as $k => $row) {
+//                if (is_array($row)) {
+//                    $result[$k] = array_change_key_case($row, CASE_LOWER);
+//                }
+//            }
+//        }
+//        return $result;
+//    }
+
+    /**
+     * Fetches a row from the result set.
+     *
+     * @param int $style  OPTIONAL Fetch mode for this fetch operation.
+     * @param int $cursor OPTIONAL Absolute, relative, or other.
+     * @param int $offset OPTIONAL Number for absolute or relative cursors.
+     * @return mixed Array, object, or scalar depending on fetch mode.
+     * @throws Zend_Db_Statement_Exception
+     */
+//    public function fetch($style = null, $cursor = null, $offset = null)
+//    {
+//        $result = $this->t__fetch($style, $cursor, $offset);
+//        if (is_array($result)) {
+//            $result = array_change_key_case($result, CASE_LOWER);
+//        }
+//        return $result;
+//    }
+
 
     /**
      * Fetches a row from the result set.
@@ -66,10 +98,241 @@ class Varien_Db_Statement_Oracle extends Zend_Db_Statement_Oracle
      */
     public function fetch($style = null, $cursor = null, $offset = null)
     {
-        $result = parent::fetch($style, $cursor, $offset);
-        if (is_array($result)) {
-            $result = array_change_key_case($result, CASE_LOWER);
+        if (!$this->_stmt) {
+            return false;
+        }
+
+        if ($style === null) {
+            $style = $this->_fetchMode;
+        }
+
+        $lob_as_string = $this->getLobAsString() ? OCI_RETURN_LOBS : 0;
+
+        switch ($style) {
+            case Zend_Db::FETCH_NUM:
+                $row = $this->_typedFetch($style, OCI_NUM | OCI_RETURN_NULLS | $lob_as_string);
+                break;
+            case Zend_Db::FETCH_ASSOC:
+                $row = $this->_typedFetch($style, OCI_ASSOC | OCI_RETURN_NULLS | $lob_as_string);
+                break;
+            case Zend_Db::FETCH_BOTH:
+                $row = $this->_typedFetch($style, OCI_BOTH | OCI_RETURN_NULLS | $lob_as_string);
+                break;
+            case Zend_Db::FETCH_OBJ:
+                $row = oci_fetch_object($this->_stmt);
+                break;
+            case Zend_Db::FETCH_BOUND:
+                $row = $this->_typedFetch($style, OCI_BOTH | OCI_RETURN_NULLS | $lob_as_string);
+                if ($row !== false) {
+                    return $this->_fetchBound($row);
+                }
+                break;
+            default:
+                /**
+                 * @see Zend_Db_Adapter_Oracle_Exception
+                 */
+                #require_once 'Zend/Db/Statement/Oracle/Exception.php';
+                throw new Zend_Db_Statement_Oracle_Exception(
+                    array(
+                        'code'    => 'HYC00',
+                        'message' => "Invalid fetch mode '$style' specified"
+                    )
+                );
+                break;
+        }
+
+        if (! $row && $error = oci_error($this->_stmt)) {
+            /**
+             * @see Zend_Db_Adapter_Oracle_Exception
+             */
+            #require_once 'Zend/Db/Statement/Oracle/Exception.php';
+            throw new Zend_Db_Statement_Oracle_Exception($error);
+        }
+
+        if (is_array($row) && array_key_exists('zend_db_rownum', $row)) {
+            unset($row['zend_db_rownum']);
+        }
+        return $row;
+    }
+
+
+    /**
+     * Returns an array containing all of the result set rows.
+     *
+     * @param int $style OPTIONAL Fetch mode.
+     * @param int $col   OPTIONAL Column number, if fetch mode is by column.
+     * @return array Collection of rows, each in a format by the fetch mode.
+     * @throws Zend_Db_Statement_Exception
+     */
+    public function fetchAll($style = null, $col = 0)
+    {
+        if (!$this->_stmt) {
+            return false;
+        }
+
+        // make sure we have a fetch mode
+        if ($style === null) {
+            $style = $this->_fetchMode;
+        }
+
+        $ncols = oci_num_fields($this->_stmt);
+
+        $columns_datatype = array();
+
+        if ($style != Zend_Db::FETCH_NUM) {
+            for ($i = 1; $i <= $ncols; $i++) {
+                $columns_datatype[oci_field_name($this->_stmt, $i)] = $this->_datetypeMap[oci_field_type($this->_stmt, $i)];
+            }
+        } else {
+            for ($i = 1; $i <= $ncols; $i++) {
+                $columns_datatype[$i - 1] = $this->_datetypeMap[oci_field_type($this->_stmt, $i)];
+            }
+        }
+
+
+        $flags = OCI_FETCHSTATEMENT_BY_ROW;
+
+        switch ($style) {
+            case Zend_Db::FETCH_BOTH:
+                /**
+                 * @see Zend_Db_Adapter_Oracle_Exception
+                 */
+                #require_once 'Zend/Db/Statement/Oracle/Exception.php';
+                throw new Zend_Db_Statement_Oracle_Exception(
+                    array(
+                        'code'    => 'HYC00',
+                        'message' => "OCI8 driver does not support fetchAll(FETCH_BOTH), use fetch() in a loop instead"
+                    )
+                );
+                // notreached
+                $flags |= OCI_NUM;
+                $flags |= OCI_ASSOC;
+                break;
+            case Zend_Db::FETCH_NUM:
+                $flags |= OCI_NUM;
+                break;
+            case Zend_Db::FETCH_ASSOC:
+                $flags |= OCI_ASSOC;
+                break;
+            case Zend_Db::FETCH_OBJ:
+                break;
+            case Zend_Db::FETCH_COLUMN:
+                $flags = $flags &~ OCI_FETCHSTATEMENT_BY_ROW;
+                $flags |= OCI_FETCHSTATEMENT_BY_COLUMN;
+                $flags |= OCI_NUM;
+                break;
+            default:
+                /**
+                 * @see Zend_Db_Adapter_Oracle_Exception
+                 */
+                #require_once 'Zend/Db/Statement/Oracle/Exception.php';
+                throw new Zend_Db_Statement_Oracle_Exception(
+                    array(
+                        'code'    => 'HYC00',
+                        'message' => "Invalid fetch mode '$style' specified"
+                    )
+                );
+                break;
+        }
+
+        $result = Array();
+
+        if ( $flags != OCI_FETCHSTATEMENT_BY_ROW)
+
+        { /* not Zend_Db::FETCH_OBJ */
+            if (! ($rows = oci_fetch_all($this->_stmt, $result, 0, -1, $flags) )) {
+                if ($error = oci_error($this->_stmt)) {
+                    /**
+                     * @see Zend_Db_Adapter_Oracle_Exception
+                     */
+                    #require_once 'Zend/Db/Statement/Oracle/Exception.php';
+                    throw new Zend_Db_Statement_Oracle_Exception($error);
+                }
+                if (!$rows) {
+                    return array();
+                }
+            }
+            if ($style == Zend_Db::FETCH_COLUMN) {
+                $result = $result[$col];
+            }
+            foreach ($result as $k => &$row) {
+
+                if (is_array($row)) {
+                    $result[$k] = array_change_key_case($row, CASE_LOWER);
+                }
+                if (is_array($row) && array_key_exists('zend_db_rownum', $row)) {
+                    unset($row['zend_db_rownum']);
+                }
+                if (is_array($row)){
+                    $datarow = array();
+                    foreach ($row as $key => $item) {
+                        if (!is_null($item)) {
+                            settype($item, $columns_datatype[strtoupper($key)]);
+                        }
+                        $datarow[strtolower($key)] = $item;
+                    }
+                    $result[$k] = $datarow;
+                }
+            }
+        }
+        else {
+            while (($row = oci_fetch_object($this->_stmt)) !== false) {
+                if ($row){
+                    $datarow = array();
+                    foreach ($row as $key => $item) {
+                        if(!oci_field_is_null($this->_stmt, $key)) {
+                            settype($item, $columns_datatype[$key]);
+                        }
+                        $datarow[strtolower($key)] = $item;
+                    }
+                    $result [] = $datarow;
+                } else {
+                    $result [] = $row;
+                }
+            }
+            if ($error = oci_error($this->_stmt)) {
+                /**
+                 * @see Zend_Db_Adapter_Oracle_Exception
+                 */
+                #require_once 'Zend/Db/Statement/Oracle/Exception.php';
+                throw new Zend_Db_Statement_Oracle_Exception($error);
+            }
         }
         return $result;
     }
+
+    protected function _typedFetch($style, $fetchMode)
+    {
+
+        $ncols = oci_num_fields($this->_stmt);
+
+        $columns_datatype = array();
+
+        if ($style != Zend_Db::FETCH_NUM) {
+            for ($i = 1; $i <= $ncols; $i++) {
+                $columns_datatype[oci_field_name($this->_stmt, $i)] = $this->_datetypeMap[oci_field_type($this->_stmt, $i)];
+            }
+        } else {
+            for ($i = 1; $i <= $ncols; $i++) {
+                $columns_datatype[$i - 1] = $this->_datetypeMap[oci_field_type($this->_stmt, $i)];
+            }
+        }
+        $row = oci_fetch_array($this->_stmt, $fetchMode);
+
+        if ($row){
+            $datarow = array();
+            foreach ($row as $key => $item) {
+
+                if(!is_null($item)) {
+                    settype($item, $columns_datatype[$key]);
+                }
+                $datarow[strtolower($key)] = $item;
+            }
+            $row = $datarow;
+        }
+
+        return $row;
+    }
+
+
 }
