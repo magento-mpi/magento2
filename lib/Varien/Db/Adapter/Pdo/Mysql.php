f96fc57a6b3bdec77f5805b6642fb4a07d7c7686
Maxim Ekaterinenko <maxim.ekaterinenko@magento.com>
2010-09-17 11:15:07 +0000

    [rdbms] fix $groupColumns in addRankColumn

--- lib/Varien/Db/Adapter/Pdo/Mysql.php
+++ lib/Varien/Db/Adapter/Pdo/Mysql.php
@@ -2878,7 +2878,7 @@ class Varien_Db_Adapter_Pdo_Mysql extends Zend_Db_Adapter_Pdo_Mysql implements V
      */
     public function addRankColumn($select, $groupColumns, $orderSql)
     {
-        $select->group($columns);
+        $select->group($groupColumns);
         $select->reset(Varien_Db_Select::SOFT_GROUP);
 
         return '';
