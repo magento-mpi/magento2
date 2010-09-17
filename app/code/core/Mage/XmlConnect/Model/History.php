8715f45b258a4053b5631b270b83db6011a76aad
Victor Tihonchuk <noreply@magento.com>
2010-09-17 07:56:20 +0000

    Merged revisions 79138 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r79138 | maxim.saveliev | 2010-09-06 16:54:14 +0300 (Mon, 06 Sep 2010) | 1 line
      
      XmlConnect: added class methods phpdoc
    ........

--- app/code/core/Mage/XmlConnect/Model/History.php
+++ app/code/core/Mage/XmlConnect/Model/History.php
@@ -23,6 +23,8 @@
  * @copyright   Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+
+
 /**
  * Enter description here ...
  *
