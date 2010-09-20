47df0fd352050036e7a9a92726f22d6daeae463a
automation <noreply@magento.com>
2010-09-20 12:02:23 +0000

    Merged revisions 79729 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r79729 | evgeniy.maksimenko | 2010-09-09 18:12:16 +0300 (Thu, 09 Sep 2010) | 1 line
      
      edited function doDeleteAtrSet
    ........

--- tests/selenium4/models/Admin/AttributeSet.php
+++ tests/selenium4/models/Admin/AttributeSet.php
@@ -1,31 +1,31 @@
 <?php
+
 /**
  * Admin_Scope_Site model
  *
  * @author Magento Inc.
  */
 class Model_Admin_AttributeSet extends Model_Admin {
+
     /**
      * Loading configuration data for the testCase
      */
-    public function loadConfigData()
-    {
+    public function loadConfigData() {
         parent::loadConfigData();
 
-        $this->siteData = Core::getEnvConfig('backend/attribute_set');
+        $this->atrSetData = Core::getEnvConfig('backend/attribute_set');
     }
 
     /**
-    * Adds new attribute set
-    *
-    * @param array $params May contain the following params:
-    * set_name, default_set
-    */
-    public function doCreateAtrSet($params = array())
-    {
+     * Adds new attribute set
+     *
+     * @param array $params May contain the following params:
+     * set_name, default_set
+     */
+    public function doCreateAtrSet($params = array()) {
         $result = true;
-        $siteData = $params ? $params : $this->siteData;
-        
+        $atrSetData = $params ? $params : $this->atrSetData;
+
         // Open Manage Attribute Sets Page
         $this->clickAndWait($this->getUiElement("/admin/topmenu/catalog/attributes/manage_attibute_set"));
         // Add new Attribute Set
@@ -33,17 +33,18 @@ class Model_Admin_AttributeSet extends Model_Admin {
         // Set UiNamespace
         $this->setUiNamespace('admin/pages/catalog/attributes/create_attribute_set');
         // Fill fields Name and Based On
-        $this->type($this->getUiElement("input/set_name"),$siteData['set_name']);
-        $this->select($this->getUiElement("input/based_on"),$siteData['default_set']);
+        $this->type($this->getUiElement("input/set_name"), $atrSetData['set_name'] . $this->getStamp());
+        $this->select($this->getUiElement("input/based_on"), $atrSetData['default_set']);
         // Saving
-        $this->clickAndWait($this->getUiElement("buttons/save_set"));
+        $this->click($this->getUiElement("buttons/save_set"));
+        $this->waitForElement($this->getUiElement("messages/set_saved"), 25);
         // check for error message
         if ($this->isElementPresent($this->getUiElement("messages/set_not_saved"))) {
             $etext = $this->getText($this->getUiElement("messages/set_not_saved"));
             $this->setVerificationErrors($etext);
             return false;
         } else {
-        ($this->waitForElement($this->getUiElement("messages/set_saved"),20));
+            ($this->waitForElement($this->getUiElement("messages/set_saved"), 40));
         }
         // Check for success message
         if (!$this->isElementPresent($this->getUiElement("messages/set_saved"))) {
@@ -57,55 +58,75 @@ class Model_Admin_AttributeSet extends Model_Admin {
     }
 
     /**
-    * Open attribute set
-    *
-    * @param array $params May contain the following params:
-    * set_name, default_set
-    */
-    public function doOpenAtrSet($params = array())
-    {
-        $result = true;
-        $siteData = $params ? $params : $this->siteData;
-        
-	// Open Manage Attribute Sets Page
-	$this->clickAndWait($this->getUiElement("/admin/topmenu/catalog/attributes/manage_attibute_set"));
+     * Open attribute set
+     *
+     * @param array $params May contain the following params:
+     * set_name, default_set
+     */
+    public function doOpenAtrSet($params = array()) {
+        $atrSetData = $params ? $params : $this->atrSetData;
+
+        // Open Manage Attribute Sets Page
+        $this->clickAndWait($this->getUiElement("/admin/topmenu/catalog/attributes/manage_attibute_set"));
         // Set UiNamespace
         $this->setUiNamespace('admin/pages/catalog/attributes/manage_attribute_sets');
         // Search Attribute Set
-	$this->type($this->getUiElement("input/search_by_name"),$siteData['set_name']);
-	$this->clickAndWait($this->getUiElement("buttons/search"));
-	if ($this->isElementPresent($this->getUiElement("locators/open_set",$siteData['set_name'])))  {
-            $this->clickAndWait($this->getUiElement("locators/open_set",$siteData['set_name']));
-	} else {
-            $this->setVerificationErrors("Attribute Set does not exist");
-            $result = false;
-	}
-        if ($result) {
-            $this->printInfo('Attribute Set opened');
+        $this->type($this->getUiElement("input/search_by_name"), $atrSetData['set_name']);
+        $this->clickAndWait($this->getUiElement("buttons/search"));
+        //Determining the number of Attribute Sets which contains word $atrSetData['set_name']
+        $qtyRoot = $this->getXpathCount($this->getUiElement("locators/open_set", $atrSetData['set_name']));
+        if ($qtyRoot == 1) {
+            $nameRoot = $this->getText($this->getUiElement("locators/open_set", $atrSetData['set_name']));
+            $nameRoot = trim($nameRoot);
+            $this->printInfo($nameRoot);
+            if ($atrSetData['set_name'] == $nameRoot) {
+                $this->clickAndWait($this->getUiElement("locators/open_set", $atrSetData['set_name']));
+                $this->printInfo("Attribute Set with name '" . $atrSetData['set_name'] . "' opened");
+            } else {
+                $this->printInfo("Attribute Set with name '" . $atrSetData['set_name'] . "' does not exist");
+            }
         }
-        return $result;
+        if ($qtyRoot == 0) {
+            $this->printInfo("Attribute Set with name '" . $atrSetData['set_name'] . "' does not exist");
+        }
+        if ($qtyRoot >= 2) {
+            for ($i = 1; $i <= $qtyRoot; $i++) {
+                $mas = array($atrSetData['set_name'], $i);
+                $nameRoot = $this->getText($this->getUiElement("locators/set_many", $mas));
+                $nameRoot = trim($nameRoot);
+                if ($nameRoot == $atrSetData['set_name']) {
+                    $j = $i;
+                }
+            }
+            $mas = array($atrSetData['set_name'], $j);
+            $this->clickAndWait($this->getUiElement("locators/set_many", $mas));
+            $this->printInfo("Attribute Set with name '" . $atrSetData['set_name'] . "' opened");
+        }
     }
 
     /**
-    * Delete attribute set
-    *
-    */
-    public function doDeleteAtrSet() 
-    {
+     * Delete attribute set
+     *
+     */
+    public function doDeleteAtrSet() {
         $result = true;
+        $this->doOpenAtrSet();
         // Set UiNamespace
         $this->setUiNamespace('admin/pages/catalog/attributes/manage_attribute_sets');
         // Delete Attribute Set
-	$this->chooseOkOnNextConfirmation();
-	$this->clickAndWait($this->getUiElement("/admin/pages/catalog/attributes/edit_atrribute_set/buttons/delete_set"));
-	$this->waitForElement($this->getUiElement("messages/set_deleted"),20);
-	if (!$this->isElementPresent($this->getUiElement("messages/set_deleted"))) {
-            $this->setVerificationErrors("no success message");
-            $result = false;
-	}
-        if ($result) {
-            $this->printInfo('Attribute Set deleted');
+        if ($this->isElementPresent($this->getUiElement("/admin/pages/catalog/attributes/edit_atrribute_set/buttons/delete_set"))) {
+            $this->clickAndWait($this->getUiElement("/admin/pages/catalog/attributes/edit_atrribute_set/buttons/delete_set"));
+            $this->assertConfirmation("All products of this set will be deleted! Are you sure you want to delete this attribute set?");
+            $this->chooseOkOnNextConfirmation();
+            $this->waitForElement($this->getUiElement("messages/set_deleted"), 20);
+            if (!$this->isElementPresent($this->getUiElement("messages/set_deleted"))) {
+                $this->setVerificationErrors("no success message");
+                $result = false;
+            }
+            if ($result) {
+                $this->printInfo('Attribute Set deleted');
+            }
         }
-        return $result;
     }
+
 }
