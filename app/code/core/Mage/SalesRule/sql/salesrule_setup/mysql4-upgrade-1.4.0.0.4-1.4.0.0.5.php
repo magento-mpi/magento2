bac4e99ba9b87875539846ffe1a8a0e45f9d32b1
automation <noreply@magento.com>
2010-09-21 11:34:46 +0000

    Merged revisions 80402 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r80402 | dimav | 2010-09-17 10:48:37 +0300 (Fri, 17 Sep 2010) | 1 line
      
      Fixed MAGE-772: Disabling product attribute usage in promo rules not affect existing promo rules (table names retrieve fixed)
    ........

--- app/code/core/Mage/SalesRule/sql/salesrule_setup/mysql4-upgrade-1.4.0.0.4-1.4.0.0.5.php
+++ app/code/core/Mage/SalesRule/sql/salesrule_setup/mysql4-upgrade-1.4.0.0.4-1.4.0.0.5.php
@@ -48,18 +48,18 @@ CREATE TABLE `{$this->getTable('salesrule/product_attribute')}` (
  * We need to fill new table with product attributes already used in promo rules
  */
 $installer->run("
-INSERT INTO salesrule_product_attribute
+INSERT INTO `{$this->getTable('salesrule/product_attribute')}`
     SELECT
       sr.rule_id,
       cw.website_id,
       cg.customer_group_id,
       ea.attribute_id
-    FROM salesrule AS sr
-      INNER JOIN core_website AS cw
+    FROM `{$this->getTable('salesrule/rule')}` AS sr
+      INNER JOIN `{$this->getTable('core/website')}` AS cw
         ON FIND_IN_SET(cw.website_id, sr.website_ids)
-      INNER JOIN customer_group AS cg
+      INNER JOIN `{$this->getTable('customer/customer_group')}` AS cg
         ON FIND_IN_SET(cg.customer_group_id , sr.customer_group_ids)
-      INNER JOIN eav_attribute AS ea
+      INNER JOIN `{$this->getTable('eav/attribute')}` AS ea
         ON ea.entity_type_id = {$installer->getEntityTypeId('catalog_product')}
     WHERE
       sr.conditions_serialized LIKE CONCAT('%s:32:\"salesrule/rule_condition_product\";s:9:\"attribute\";s:', LENGTH(ea.attribute_code), ':\"', ea.attribute_code, '\"%')
