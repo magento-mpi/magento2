22b32190d75f5ba78ffa9776e6f942464f119d0c
automation <noreply@magento.com>
2010-09-30 17:13:32 +0000

    Merged revisions 81572 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r81572 | victor | 2010-09-29 21:17:52 +0300 (Wed, 29 Sep 2010) | 1 line
      
      RDBMS [MSSQL] CI build scripts
    ........

---
+++ tools/build/mmdb/install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+. mmdb/include.sh
+
+# Changing current working directory
+cd "$PWD"
+
+if [ "$DB_MODEL" = 'mysql4' ]; then
+    # Copying local.xml.template for read/write test
+    cp -f "$BUILD_TOOLS/local.xml.template" "app/etc/local.xml.template"
+    check_failure $?
+fi
+
+if [ "$DB_PORT" = "" ]
+    then
+        DB_HOSTNAME="$DB_HOST"
+    else
+        DB_HOSTNAME="${DB_HOST}:${DB_PORT}"
+fi
+
+# Installing build...
+$PHP_BIN -f install.php -- --license_agreement_accepted yes \
+--locale en_US --timezone "America/Los_Angeles" --default_currency USD \
+--db_host "$DB_HOSTNAME" --db_name "$DB_NAME"  --db_user "$DB_USER" --db_pass "$DB_PASS" \
+--db_prefix "$DB_PREFIX" --db_model "$DB_MODEL" \
+--use_rewrites yes \
+--admin_frontname "$MAGENTO_FRONTNAME" \
+--skip_url_validation yes \
+--url "http://kq.varien.com/builds/$BUILD_NAME/$BUILD_NUMBER/" \
+--secure_base_url "https://kq.varien.com/builds/$BUILD_NAME/$BUILD_NUMBER/" \
+--use_secure yes --use_secure_admin yes \
+--admin_lastname "$MAGENTO_LASTNAME" --admin_firstname "$MAGENTO_FIRSTNAME" --admin_email "$MAGENTO_EMAIL" \
+--admin_username "$MAGENTO_USERNAME" --admin_password "$MAGENTO_PASSWORD" \
+--encryption_key "$ENCRYPTION_KEY"
+check_failure $?
+
+# Changing permission to cache folder as it was created by user which runs install
+log "Changing permission for var/cache folder ..."
+chmod -R 777 var/cache
+check_failure $?
+
+log "Changing permission for media folder ..."
+chmod -R 777 media
+check_failure $?
+
+if [ "$DB_MODEL" = 'mysql4' ]; then
+    # Reverting local.xml.template
+    svn revert app/etc/local.xml.template
+    check_failure $?
+fi
+
+log "Rebuilding indexes ..."
+php -f shell/indexer.php -- reindexall
+check_failure $?
+chmod -fR 777 var/locks
+
+cd $OLDPWD
