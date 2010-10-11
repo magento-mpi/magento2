6570310821791aa4466a936d6b184422ff5b738f
Kirill Morozov <kirill.morozov@magento.com>
2010-10-11 14:20:39 +0000

    Fix too long index names for Oracle

--- app/code/core/Mage/Catalog/Model/Resource/Category/Flat.php
+++ app/code/core/Mage/Catalog/Model/Resource/Category/Flat.php
@@ -562,11 +562,11 @@ class Mage_Catalog_Model_Resource_Category_Flat extends Mage_Core_Model_Resource
         $table->addForeignKey(
             $_writeAdapter->getForeignKeyName($table, 'entity_id', $this->getTable('catalog/category'), 'entity_id'), 
             'entity_id', $this->getTable('catalog/category'), 'entity_id',
-            Varien_Db_Ddl_Table::ACTION_CASCADE. Varien_Db_Ddl_Table::ACTION_CASCADE);
+            Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE);
         $table->addForeignKey(
             $_writeAdapter->getForeignKeyName($table, 'store_id', $this->getTable('core/store'), 'store_id'),
             'store_id', $this->getTable('core/store'), 'store_id',
-            Varien_Db_Ddl_Table::ACTION_CASCADE. Varien_Db_Ddl_Table::ACTION_CASCADE);
+            Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE);
         $_writeAdapter->createTable($table);
         return $this;
     }
