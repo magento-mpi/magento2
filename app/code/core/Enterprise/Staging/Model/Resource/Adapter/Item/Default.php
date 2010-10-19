ad72cf254124091219bf35ffa27b7237631a2eaa
Nadezhda Sivokonenko <nadezhda.sivokonenko@magento.com>
2010-10-19 15:48:28 +0000

    In progress MDB-60: update conditions were fixed

--- app/code/core/Enterprise/Staging/Model/Resource/Adapter/Item/Default.php
+++ app/code/core/Enterprise/Staging/Model/Resource/Adapter/Item/Default.php
@@ -333,7 +333,7 @@ class Enterprise_Staging_Model_Resource_Adapter_Item_Default extends Enterprise_
 
             $writeAdapter->update(
                 $targetTable,
-                array('website_ids = ?' => $concatVal),
+                array('website_ids' => $concatVal),
                 array($stagingCond, ' NOT '. $masterCond)
             );
         } else {
@@ -636,7 +636,7 @@ class Enterprise_Staging_Model_Resource_Adapter_Item_Default extends Enterprise_
 
                 $writeAdapter->update(
                     $targetTable,
-                    array('website_ids = ?' => $concatVal),
+                    array('website_ids' => $concatVal),
                     array($stagingCond, ' NOT '. $masterCond)
                 );
             }
