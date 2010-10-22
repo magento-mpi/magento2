726ea4e382cb52ab230706f7e493ee73ecd8fe0c
automation <noreply@magento.com>
2010-10-22 10:44:52 +0000

    Merged revisions 83700 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r83700 | dmitriy.shatovskiy | 2010-10-21 18:57:44 +0300 (Thu, 21 Oct 2010) | 2 lines
      
      Added: HUB#12775: SIM Integration
      Create request from quote for Authorize.net
    ........

--- js/mage/adminhtml/directpost.js
+++ js/mage/adminhtml/directpost.js
@@ -26,54 +26,54 @@
 /**
  * Disable cart server validation in admin
  */
-AdminOrder.prototype.prepareParams = function(params){
-    if (!params) {
-        params = {};
-    }
-    if (!params.customer_id) {
-        params.customer_id = this.customerId;
-    }
-    if (!params.store_id) {
-        params.store_id = this.storeId;
-    }
-    if (!params.currency_id) {
-        params.currency_id = this.currencyId;
-    }
-    if (!params.form_key) {
-        params.form_key = FORM_KEY;
-    }
-    
-    if (this.paymentMethod != 'authorizenet_directpost') {
-	    var data = this.serializeData('order-billing_method');
-	    if (data) {
-	        data.each(function(value) {
-	            params[value[0]] = value[1];
-	        });
-	    }
-    }
-    else {
-    	params['payment[method]'] = 'authorizenet_directpost';
-    }
-    return params;
+AdminOrder.prototype.prepareParams = function(params) {
+	if (!params) {
+		params = {};
+	}
+	if (!params.customer_id) {
+		params.customer_id = this.customerId;
+	}
+	if (!params.store_id) {
+		params.store_id = this.storeId;
+	}
+	if (!params.currency_id) {
+		params.currency_id = this.currencyId;
+	}
+	if (!params.form_key) {
+		params.form_key = FORM_KEY;
+	}
+
+	if (this.paymentMethod != 'authorizenet_directpost') {
+		var data = this.serializeData('order-billing_method');
+		if (data) {
+			data.each(function(value) {
+				params[value[0]] = value[1];
+			});
+		}
+	} else {
+		params['payment[method]'] = 'authorizenet_directpost';
+	}
+	return params;
 };
-AdminOrder.prototype.getPaymentData = function(currentMethod){
-    if (typeof(currentMethod) == 'undefined') {
-        if (this.paymentMethod) {
-            currentMethod = this.paymentMethod;
-        } else {
-            return false;
-        }
-    }
-    if (currentMethod == 'authorizenet_directpost') {
-    	return false;
-    }
-    var data = {};
-    var fields = $('payment_form_' + currentMethod).select('input', 'select');
-    for(var i=0;i<fields.length;i++){
-        data[fields[i].name] = fields[i].getValue();
-    }
-    if ((typeof data['payment[cc_type]']) != 'undefined' && (!data['payment[cc_type]'] || !data['payment[cc_number]'])) {
-        return false;
-    }
-    return data;
+AdminOrder.prototype.getPaymentData = function(currentMethod) {
+	if (typeof (currentMethod) == 'undefined') {
+		if (this.paymentMethod) {
+			currentMethod = this.paymentMethod;
+		} else {
+			return false;
+		}
+	}
+	if (currentMethod == 'authorizenet_directpost') {
+		return false;
+	}
+	var data = {};
+	var fields = $('payment_form_' + currentMethod).select('input', 'select');
+	for ( var i = 0; i < fields.length; i++) {
+		data[fields[i].name] = fields[i].getValue();
+	}
+	if ((typeof data['payment[cc_type]']) != 'undefined'
+			&& (!data['payment[cc_type]'] || !data['payment[cc_number]'])) {
+		return false;
+	}
+	return data;
 };
