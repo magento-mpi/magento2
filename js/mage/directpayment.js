d65f428c27e58d04aaa70d6a38ee655520636aea
automation <noreply@magento.com>
2010-10-22 09:35:46 +0000

    Merged revisions 83435 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r83435 | dmitriy.shatovskiy | 2010-10-19 19:52:19 +0300 (Tue, 19 Oct 2010) | 2 lines
      
      Added: HUB#12775: SIM Integration
      Native save order errors processing
    ........

--- js/mage/directpayment.js
+++ js/mage/directpayment.js
@@ -256,6 +256,16 @@ directPayment.prototype = {
             if (msg) {
             	alert(msg);
             }
+            
+            if (response.update_section) {
+                $('checkout-'+response.update_section.name+'-load').update(response.update_section.html);
+                response.update_section.html.evalScripts();
+            }
+
+            if (response.goto_section) {
+                checkout.gotoSection(response.goto_section);
+                checkout.reloadProgressBlock();
+            }
         }
 	},
 	
