233fff9aa1791bc3c8b0edb3fd9a2b6d611ad98f
automation <noreply@magento.com>
2010-10-22 10:52:54 +0000

    Merged revisions 83779 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r83779 | sergiy.stotskiy | 2010-10-22 11:40:20 +0300 (Fri, 22 Oct 2010) | 3 lines
      
      Fixed MAGE-980: There is a spelling error with the translation
      - fixed the same issue in enterprise and iphone themes
    ........

--- skin/frontend/default/iphone/js/opcheckout.js
+++ skin/frontend/default/iphone/js/opcheckout.js
@@ -521,7 +521,7 @@ ShippingMethod.prototype = {
     validate: function() {
         var methods = document.getElementsByName('shipping_method');
         if (methods.length==0) {
-            alert(Translator.translate('Your order cannot be completed at this time as there is no shipping methods available for it. Please make neccessary changes in your shipping address.'));
+            alert(Translator.translate('Your order cannot be completed at this time as there is no shipping methods available for it. Please make necessary changes in your shipping address.'));
             return false;
         }
 
