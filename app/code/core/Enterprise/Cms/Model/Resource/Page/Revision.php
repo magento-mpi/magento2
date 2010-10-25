2481d9c5410539b1d195c720d25127ca2a8c55df
Evgeniy Tulika <evgeniy.tulika@magento.com>
2010-10-25 10:10:41 +0000

    Fixed MDB-172: Page isn't created in Mysql.

--- app/code/core/Enterprise/Cms/Model/Resource/Page/Revision.php
+++ app/code/core/Enterprise/Cms/Model/Resource/Page/Revision.php
@@ -165,7 +165,7 @@ class Enterprise_Cms_Model_Resource_Page_Revision extends Mage_Core_Model_Resour
             ->join(array('r'=> $sql), 'p.version_id = r.version_id', array('revisions_count'))
             ->where('r.version_id = ?', (int)$versionId);
 
-        $query = $adapter->updateFromSelect($select, array('p' => $this->_versionTable));
+        $query = $this->_getWriteAdapter()->updateFromSelect($select, array('p' => $this->_versionTable));
         $adapter->query($query);
 
         return $this;
