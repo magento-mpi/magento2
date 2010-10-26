a85168c9a504966db15cbb6c26c9a2e300959ffc
Evgeniy Tulika <evgeniy.tulika@magento.com>
2010-10-26 16:02:35 +0000

    Implemented MDB-4: Implement Oracle Adapter multiQuery() method

--- lib/Varien/Db/Adapter/Oracle.php
+++ lib/Varien/Db/Adapter/Oracle.php
@@ -1939,24 +1939,78 @@ class Varien_Db_Adapter_Oracle extends Zend_Db_Adapter_Oracle implements Varien_
      * Executes a SQL statement(s)
      *
      * @param string $sql
-     * @return Varien_Db_Adapter_Oracle
+     * @return Varien_Db_Adapter_Pdo_Mssql
      */
     public function multiQuery($sql)
     {
-        $this->beginTransaction();
         try {
             $stmts = $this->_splitMultiQuery($sql);
             $result = array();
             foreach ($stmts as $stmt) {
-                $result[] = $this->query($stmt);
+                $result[] = $this->raw_query($stmt);
             }
-            $this->commit();
         } catch (Exception $e) {
-            $this->rollback();
             throw $e;
         }
 
         return $result;
+    }
+    
+
+    /**
+     * Split multi statement query
+     *
+     * @param $sql string
+     * @return array
+     */
+    protected function _splitMultiQuery($sql)
+    {
+        $parts = preg_split('#(;|\'|"|\\\\|//|--|\n|/\*|\*/)#', $sql, null, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+
+        $q = false;
+        $c = false;
+        $stmts = array();
+        $s = '';
+
+        foreach ($parts as $i=>$part) {
+            // strings
+            if (($part==="'" || $part==='"') && ($i===0 || $parts[$i-1]!=='\\')) {
+                if ($q===false) {
+                    $q = $part;
+                } else if ($q===$part) {
+                    $q = false;
+                }
+            }
+
+            // single line comments
+            if (($part==='//' || $part==='--') && ($i===0 || $parts[$i-1]==="\n")) {
+                $c = $part;
+            } else if ($part==="\n" && ($c==='//' || $c==='--')) {
+                $c = false;
+            }
+
+            // multi line comments
+            if ($part==='/*' && $c===false) {
+                $c = '/*';
+            } else if ($part==='*/' && $c==='/*') {
+                $c = false;
+            }
+
+            // statements
+            if ($part===';' && $q===false && $c===false) {
+                if (trim($s)!=='') {
+                    $stmts[] = trim($s);
+                    $s = '';
+                }
+            } else {
+                $s .= $part;
+            }
+        }
+        if (trim($s)!=='') {
+            $stmts[] = trim($s);
+        }
+
+        return $stmts;
     }
 
     /**
