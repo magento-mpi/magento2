c2110804aa26e4b912c061a9a5f9fb833744fc2f
Evgeniy Tulika <evgeniy.tulika@magento.com>
2010-10-27 11:33:26 +0000

    Fixed MDB-180: CMS page isn't found error after installation

--- app/code/core/Mage/Cms/Model/Resource/Page.php
+++ app/code/core/Mage/Cms/Model/Resource/Page.php
@@ -118,7 +118,9 @@ class Mage_Cms_Model_Resource_Page extends Mage_Core_Model_Resource_Db_Abstract
     {
         $oldStores = $this->lookupStoreIds($object->getId());
         $newStores = (array)$object->getStores();
-
+        if (empty($newStores)) {
+            $newStores = (array)$object->getStoreId();
+        }
         $table  = $this->getTable('cms/page_store');
         $insert = array_diff($newStores, $oldStores);
         $delete = array_diff($oldStores, $newStores);
