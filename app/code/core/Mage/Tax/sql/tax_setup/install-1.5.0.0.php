34536ac245f55de353c5dbc8b551eff074d84ac5
Nadezhda Sivokonenko <nadezhda.sivokonenko@magento.com>
2010-11-12 11:58:01 +0000

    Fixed MDB-347: query for retrieving right tax value was fixed; - tax_postcode field for tax_calculation_rate table was set with nullable false as it was in previous version

--- app/code/core/Mage/Tax/sql/tax_setup/install-1.5.0.0.php
+++ app/code/core/Mage/Tax/sql/tax_setup/install-1.5.0.0.php
@@ -89,6 +89,7 @@ $table = $installer->getConnection()
         'nullable'  => false,
         ), 'Tax Region Id')
     ->addColumn('tax_postcode', Varien_Db_Ddl_Table::TYPE_TEXT, 21, array(
+        'nullable'  => false,
         ), 'Tax Postcode')
     ->addColumn('code', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
         'nullable'  => false,
