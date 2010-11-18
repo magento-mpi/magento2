80d892679ebd8a53a25b3c1fa37aafb5574d8d69
Nadezhda Sivokonenko <nadezhda.sivokonenko@magento.com>
2010-11-18 11:46:34 +0000

    Fixed MDB-386: attribute is_used_for_customer_segment was added to install

--- app/code/core/Enterprise/CustomerSegment/sql/enterprise_customersegment_setup/install-1.10.0.0.php
+++ app/code/core/Enterprise/CustomerSegment/sql/enterprise_customersegment_setup/install-1.10.0.0.php
@@ -149,14 +149,26 @@ $table = $installer->getConnection()
     ->setComment('Enterprise Customersegment Event');
 $installer->getConnection()->createTable($table);
 
+// add field that indicates that attribute is used for customer segments to attribute properties
+$installer->getConnection()
+    ->addColumn( $installer->getTable('customer/eav_attribute'), 'is_used_for_customer_segment', array(
+        'type'      => Varien_Db_Ddl_Table::TYPE_INTEGER,
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ));
 
-/**
- * Resave all segments for segment conditions regeneration
- */
-$collection = Mage::getResourceModel('enterprise_customersegment/segment_collection');
-foreach($collection as $segment) {
-    $segment->afterLoad();
-    $segment->save();
+// use specific attributes for customer segments
+$attributesOfEntities = array(
+    'customer' => array('dob', 'email', 'firstname', 'group_id', 'lastname', 'gender', 'default_billing', 'default_shipping', 'created_at'),
+    'customer_address' => array('firstname', 'lastname', 'company', 'street', 'city', 'region_id', 'postcode', 'country_id', 'telephone'),
+    'order_address' => array('firstname', 'lastname', 'company', 'street', 'city', 'region_id', 'postcode', 'country_id', 'telephone', 'email'),
+);
+
+foreach ($attributesOfEntities as $entityTypeId => $attributes) {
+    foreach ($attributes as $attributeCode){
+        $installer->updateAttribute($entityTypeId, $attributeCode, 'is_used_for_customer_segment', '1');
+    }
 }
 
 $installer->endSetup();
