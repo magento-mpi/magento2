3ca61ec76697ccf2c576e32dce4ba0354e358e13
Alexey Pilguy <alexey.pilguy@magento.com>
2010-11-22 16:56:43 +0000

    Fixed MDB-395 Can not open product on frontend (Oracle)

--- app/code/core/Mage/Reports/Model/Resource/Helper/Oracle.php
+++ app/code/core/Mage/Reports/Model/Resource/Helper/Oracle.php
@@ -46,14 +46,13 @@ class Mage_Reports_Model_Resource_Helper_Oracle extends Mage_Core_Model_Resource
     {
         $pseudoUnique = array();
         if (!empty($data['visitor_id'])) {
-            $pseudoUnique[] = 't1.visitor_id = t2.visitor_id';
+            $pseudoUnique['visitor_id'] = 't1.visitor_id = t2.visitor_id';
         }
         if (!empty($data['customer_id'])) {
-            $pseudoUnique[] = 't1.customer_id = t2.customer_id';
+            $pseudoUnique['customer_id'] = 't1.customer_id = t2.customer_id';
         }
         $selectPart = '';
-        $matchPart  = '';
-        $updateCond = implode(' OR ', $pseudoUnique);
+        $matchPart  = '( ' . implode(' OR ', $pseudoUnique) . ')';
         $insertPart = '';
         $updatePart = '';
         $columnsPart = implode(',', array_keys($data));
@@ -69,7 +68,9 @@ class Mage_Reports_Model_Resource_Helper_Oracle extends Mage_Core_Model_Resource
             $insertPart .= sprintf('t2.%s, ', $column);
 
             if (!in_array($column, $matchFields)) {
-                $updatePart .= sprintf('t1.%s = t2.%s, ', $column, $column);
+                if (count($pseudoUnique) == 1 && !isset($pseudoUnique[$column])) {
+                    $updatePart .= sprintf('t1.%s = t2.%s, ', $column, $column);
+                }
             } else {
                 $matchPart .= sprintf('AND t1.%s = t2.%s ', $column, $column);
             }
@@ -80,9 +81,9 @@ class Mage_Reports_Model_Resource_Helper_Oracle extends Mage_Core_Model_Resource
         $insertPart = rtrim($insertPart, ', ');
 
         $sql = 'MERGE INTO ' . $mainTable . ' t1 USING ('
-            . ' SELECT ' . $selectPart . ' FROM dual ) t2 ON ( 1=1 ' . $matchPart . ' )'
+            . ' SELECT ' . $selectPart . ' FROM dual ) t2 ON ( ' . $matchPart . ' )'
             . ' WHEN MATCHED THEN '
-            . ' UPDATE SET ' . $updatePart . ' WHERE (' . $updateCond . ')' 
+            . ' UPDATE SET ' . $updatePart
             . ' WHEN NOT MATCHED THEN INSERT (' . $columnsPart . ')'
             . ' VALUES ( ' . $insertPart . ')';
 
