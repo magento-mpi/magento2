633f944e7b411400b8cbd659903d838f9968f313
automation <noreply@magento.com>
2011-02-16 14:57:58 +0000

    Merged revisions 85247 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r85247 | vladimir.pelipenko | 2010-11-08 15:12:02 +0200 (Mon, 08 Nov 2010) | 3 lines
      
      Fixed MAGE-1433: PayPal API Certificate uses settings from the default configuration, instead of the website
      - added forgotten file from rev 84979
    ........

--- app/code/core/Mage/Adminhtml/Block/System/Config/Form/Field/File.php
+++ app/code/core/Mage/Adminhtml/Block/System/Config/Form/Field/File.php
@@ -54,7 +54,7 @@ class Mage_Adminhtml_Block_System_Config_Form_Field_File extends Varien_Data_For
     protected function _getDeleteCheckbox()
     {
         $html = '';
-        if ($this->getValue()) {
+        if ((string)$this->getValue()) {
             $label = Mage::helper('adminhtml')->__('Delete File');
             $html .= '<div>'.$this->getValue().' ';
             $html .= '<input type="checkbox" name="'.parent::getName().'[delete]" value="1" class="checkbox" id="'.$this->getHtmlId().'_delete"'.($this->getDisabled() ? ' disabled="disabled"': '').'/>';
