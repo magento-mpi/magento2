35b6dda085563170d60d4f330ccd49f95a4a72a7
automation <noreply@magento.com>
2011-02-18 12:10:54 +0000

    Merged revisions 85779 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r85779 | alex.bomko | 2010-11-16 15:08:34 +0200 (Tue, 16 Nov 2010) | 1 line
      
      Implemented part of MAGE-1359 Adding Composite Products in Admin Order Creation - update group product for backend cart 
    ........

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Composite/Fieldset/Grouped.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Composite/Fieldset/Grouped.php
@@ -52,4 +52,44 @@ class Mage_Adminhtml_Block_Catalog_Product_Composite_Fieldset_Grouped extends Ma
         return $product;
     }
 
+    /**
+     * Retrieve array of associated products
+     * 
+     * @return array
+     */
+    public function getAssociatedProducts()
+    {
+        return $this->getProduct()->getTypeInstance(true)
+            ->getAssociatedProducts($this->getProduct());
+    }
+
+
+    /**
+     * Set preconfigured values to grouped associated products
+     *
+     * @return Mage_Catalog_Block_Product_View_Type_Grouped
+     */
+    public function setPreconfiguredValue() {
+        $configValues = $this->getProduct()->getPreconfiguredValues()->getSuperGroup();
+        if (is_array($configValues)) {
+            $associatedProducts = $this->getAssociatedProducts();
+            foreach ($associatedProducts as $item) {
+                if (isset($configValues[$item->getId()])) {
+                    $item->setQty($configValues[$item->getId()]);
+                }
+            }
+        }
+        return $this;
+    }
+    
+    /**
+     * Check whether the price can be shown for the specified product
+     *
+     * @param Mage_Catalog_Model_Product $product
+     * @return bool
+     */
+    public function getCanShowProductPrice($product)
+    {
+        return $product->getCanShowPrice() !== false;
+    }
 }
