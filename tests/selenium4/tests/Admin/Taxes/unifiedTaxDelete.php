dceab09dd08058070adb4d6afb1421499cdd5a3f
automation <noreply@magento.com>
2011-02-18 13:28:46 +0000

    Merged revisions 85987 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r85987 | alex.malyshenko | 2010-11-18 13:30:24 +0200 (Thu, 18 Nov 2010) | 1 line
      
      function "unifiedTaxDeleating" changed to "testUnifiedTaxDeleating" (was not working)
    ........

--- tests/selenium4/tests/Admin/Taxes/unifiedTaxDelete.php
+++ tests/selenium4/tests/Admin/Taxes/unifiedTaxDelete.php
@@ -6,31 +6,25 @@ class Admin_Taxes_unifiedTaxDelete extends TestCaseAbstract {
      * Setup procedure.
      * Initializes model and loads configuration
      */
-    function setUp() {
-        $this->model = $this->getModel('admin/TaxRuleDelete');
+    function setUp()
+    {
+        $this->model = $this->getModel('admin/tax');
         $this->setUiNamespace();
     }
 
     /**
      * Test deleating Tax Rule
      */
-    function UnifiedTaxDeleating() {
+    function testUnifiedTaxDeleating()
+    {
         $taxData = array(
-        'product_tax_class_name' => Core::getEnvConfig('backend/tax/product_tax_class_name'),
-        'customer_tax_class_name' => Core::getEnvConfig('backend/tax/customer_tax_class_name'),
-        'tax_rate_identifier' => Core::getEnvConfig('backend/tax/tax_rate_identifier'),
-        'tax_rate_percent' => Core::getEnvConfig('backend/tax/tax_rate_percent'),
-        'tax_rule_name' => Core::getEnvConfig('backend/tax/tax_rule_name'),
-        'zip_post_code' => Core::getEnvConfig('backend/tax/zip_post_code'),
-        'country' => Core::getEnvConfig('backend/tax/country'),
-        'state' => Core::getEnvConfig('backend/tax/state'),
-        'tax_rule_priority' => Core::getEnvConfig('backend/tax/tax_rule_priority'),
-        'tax_rule_sort_order' => Core::getEnvConfig('backend/tax/tax_rule_sort_order'),
-        'store_view_name' => Core::getEnvConfig('backend/scope/store_view/name'),
-        'tax_store_view_title' => Core::getEnvConfig('backend/tax/tax_store_view_title'),
-            );
+            'search_tax_customer_class_name' => 'Test customer tax class 1',
+            'search_tax_product_class_name' => 'Test product tax class 1',
+            'search_tax_rate_identigier' => 'Test tax rate 1',
+            'search_tax_rule_name' => 'Test tax rule',
+        );
         if ($this->model->doLogin()) {
-            $this->model->unifiedTaxDelete($taxData,"manage_tax_zone_rate","tax_rate_identifier");
+            $this->model->unifiedTaxDelete($taxData, "manage_tax_rules", "tax_rule_name");
             /*
              * manage_tax_zone_rate, tax_rate_identifier
              * manage_tax_rules, tax_rule_name
@@ -39,4 +33,5 @@ class Admin_Taxes_unifiedTaxDelete extends TestCaseAbstract {
              */
         }
     }
+
 }
