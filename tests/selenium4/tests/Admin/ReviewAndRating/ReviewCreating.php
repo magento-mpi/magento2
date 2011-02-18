5a1344f6e7e18aa1761ace9e3a64a245ebdeb3c4
automation <noreply@magento.com>
2011-02-18 14:02:36 +0000

    Merged revisions 86138 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r86138 | alex.malyshenko | 2010-11-22 10:58:53 +0200 (Mon, 22 Nov 2010) | 1 line
      
      review test code optimazing
    ........

--- tests/selenium4/tests/Admin/ReviewAndRating/ReviewCreating.php
+++ tests/selenium4/tests/Admin/ReviewAndRating/ReviewCreating.php
@@ -18,13 +18,14 @@ class Admin_ReviewAndRating_ReviewCreating extends TestCaseAbstract {
     function testRating()
     {
         $review_rateData = array(
-            'status' => 'Pending', //Core::getEnvConfig('backend/rating_and_review/review/status'),
-            'rating_select' => 'Test Rating', //Core::getEnvConfig('backend/rating_and_review/rating_title_default'),
-            'rating_stars' => '4', //Core::getEnvConfig('backend/rating_and_review/review/stars'),
-            'store_view_name' => 'Xandr shop', //Core::getEnvConfig('backend/rating_and_review/store_view_name'),
-            'nickname' => 'Test user', //Core::getEnvConfig('backend/rating_and_review/review/nickname'),
-            'summary_of_review' => 'Test review', //Core::getEnvConfig('backend/rating_and_review/review/summary_of_review'),
-            'review_text' => 'Test review text', //Core::getEnvConfig('backend/rating_and_review/review/review_text'),
+            'status' => 'Pending',
+            'title_for_store' => array('Test-Rating-title_for_SmokeTestStoreView', 'Test-Rating-title_for_DefaultStoreView'),
+            'rating_stars' => array('Test-Rating-default_title_1', '123_4'),
+            'store_view_name_title' =>array('SmokeTestStoreView','Default Store View'),
+            'store_view_name_visible' => array('SmokeTestStoreView','Default Store View'),
+            'nickname' => 'Test user', 
+            'summary_of_review' => 'Test review', 
+            'review_text' => 'Test review text', 
             'search_product_sku' => 'Xandr simple product 1',
             'search_product_name' => 'Xandr simple product 1',
         );
