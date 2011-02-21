eef4d4e0e71dafee149301463a8da3e48978f329
automation <noreply@magento.com>
2011-02-21 18:34:48 +0000

    Merged revisions 88071 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r88071 | dmitriy.shatovskiy | 2010-12-08 13:14:09 +0200 (Wed, 08 Dec 2010) | 2 lines
      
      Fixed MAGE-1721: Security issue - the way to get URL-path of Admin side through Front-end URL
      - GoogleOptimizer module changes
    ........

--- app/code/core/Mage/GoogleOptimizer/controllers/Adminhtml/Googleoptimizer/IndexController.php
+++ app/code/core/Mage/GoogleOptimizer/controllers/Adminhtml/Googleoptimizer/IndexController.php
@@ -25,41 +25,33 @@
  */
 
 /**
- * Google Optimizer Block with additional js scripts in template
+ * GoogleOptimizer Front Controller
  *
  * @category   Mage
  * @package    Mage_GoogleOptimizer
+ * @name       Mage_GoogleOptimizer_Adminhtml_Googleoptimizer_IndexController
  * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Mage_GoogleOptimizer_Block_Js extends Mage_Adminhtml_Block_Template
+*/
+class Mage_GoogleOptimizer_Adminhtml_Googleoptimizer_IndexController extends Mage_Adminhtml_Controller_Action
 {
-    public function getJsonConversionPagesUrl()
+    /**
+     * Retrieve js scripts by parsing remote Google Optimizer page
+     */
+    public function codesAction()
     {
-        return Mage::helper('googleoptimizer')->getConversionPagesUrl()->toJson();
-    }
-
-    public function getMaxCountOfAttributes()
-    {
-        return Mage_GoogleOptimizer_Model_Code_Product::DEFAULT_COUNT_OF_ATTRIBUTES;
-    }
-
-    public function getExportUrl()
-    {
-        return $this->getUrl('*/googleoptimizer_index/codes');
-    }
-
-    public function getControlFieldKey ()
-    {
-        return $this->getDataSetDefault('control_field_key', 'control_script');
-    }
-
-    public function getTrackingFieldKey ()
-    {
-        return $this->getDataSetDefault('tracking_field_key', 'tracking_script');
-    }
-
-    public function getConversionFieldKey ()
-    {
-        return $this->getDataSetDefault('conversion_field_key', 'conversion_script');
+        if ($this->getRequest()->getQuery('url')) {
+            $client = new Varien_Http_Client($this->getRequest()->getQuery('url'));
+            $response = $client->request(Varien_Http_Client::GET);
+            $result = array();
+            if (preg_match_all('/<textarea[^>]*id="([_a-zA-Z0-9]+)"[^>]*>([^<]+)<\/textarea>/', $response->getRawBody(), $matches)) {
+                $c = count($matches[1]);
+                for ($i = 0; $i < $c; $i++) {
+                    $id = $matches[1][$i];
+                    $code = $matches[2][$i];
+                    $result[$id] = $code;
+                }
+            }
+            $this->getResponse()->setBody( Mage::helper('core')->jsonEncode($result) );
+        }
     }
 }
