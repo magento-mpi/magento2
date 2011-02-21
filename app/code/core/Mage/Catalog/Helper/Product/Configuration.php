7b7aa50df64fca912645ccf7345eacee7465de54
automation <noreply@magento.com>
2011-02-21 15:36:07 +0000

    Merged revisions 87602 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r87602 | andrey.tserkus | 2010-12-02 15:04:56 +0200 (Thu, 02 Dec 2010) | 4 lines
      
      Implemented MAGE-1359 - Adding Composite Products in Admin Order Creation
      - moved forming product configuration options to catalog product configuration helper
      - added downloadable helper to manage downloadable configuration options
      - moved downloadable option management logic from checkout item renderer to corresponding helper
    ........

--- app/code/core/Mage/Catalog/Helper/Product/Configuration.php
+++ app/code/core/Mage/Catalog/Helper/Product/Configuration.php
@@ -107,14 +107,14 @@ class Mage_Catalog_Helper_Product_Configuration extends Mage_Core_Helper_Abstrac
         if ($typeId != Mage_Catalog_Model_Product_Type_Grouped::TYPE_CODE) {
              Mage::throwException($this->__('Wrong product type to extract configurable options.'));
         }
-        
+
         $options = array();
         /**
          * @var Mage_Catalog_Model_Product_Type_Grouped
          */
         $typeInstance = $product->getTypeInstance(true);
         $associatedProducts = $typeInstance->getAssociatedProducts($product);
-        
+
         if ($associatedProducts) {
             foreach ($associatedProducts as $associatedProduct) {
                 $qty = $item->getOptionByCode('associated_product_' . $associatedProduct->getId());
@@ -125,7 +125,7 @@ class Mage_Catalog_Helper_Product_Configuration extends Mage_Core_Helper_Abstrac
 
                 $options[] = $option;
             }
-        }        
+        }
 
         return array_merge($options, $this->getCustomOptions($item));
     }
@@ -138,7 +138,7 @@ class Mage_Catalog_Helper_Product_Configuration extends Mage_Core_Helper_Abstrac
      */
     public function getOptions(Mage_Catalog_Model_Product_Configuration_Item_Interface $item)
     {
-        $typeId = $item->getProduct->getTypeId();
+        $typeId = $item->getProduct()->getTypeId();
         switch ($typeId) {
             case Mage_Catalog_Model_Product_Type_Configurable::TYPE_CODE:
                 return $this->getConfigurableOptions($item);
@@ -148,5 +148,93 @@ class Mage_Catalog_Helper_Product_Configuration extends Mage_Core_Helper_Abstrac
                 break;
         }
         return $this->getCustomOptions($item);
+    }
+
+    /**
+     * Accept option value and return its formatted view
+     *
+     * @param mixed $optionValue
+     * Method works well with these $optionValue format:
+     *      1. String
+     *      2. Indexed array e.g. array(val1, val2, ...)
+     *      3. Associative array, containing additional option info, including option value, e.g.
+     *          array
+     *          (
+     *              [label] => ...,
+     *              [value] => ...,
+     *              [print_value] => ...,
+     *              [option_id] => ...,
+     *              [option_type] => ...,
+     *              [custom_view] =>...,
+     *          )
+     * @param array $params
+     * All keys are options. Following supported:
+     *  - 'maxLength': truncate option value if needed, default: do not truncate
+     *  - 'cutReplacer': replacer for cut off value part when option value exceeds maxLength
+     *
+     * @return array
+     */
+    public function getFormattedOptionValue($optionValue, $params = null)
+    {
+        // Init params
+        if (!$params) {
+            $params = array();
+        }
+        $maxLength = isset($params['max_length']) ? $params['max_length'] : null;
+        $cutReplacer = isset($params['cut_replacer']) ? $params['cut_replacer'] : '...';
+
+        // Proceed with option
+        $optionInfo = array();
+
+        // Define input data format
+        if (is_array($optionValue)) {
+            if (isset($optionValue['option_id'])) {
+                $optionInfo = $optionValue;
+                if (isset($optionInfo['value'])) {
+                    $optionValue = $optionInfo['value'];
+                }
+            } else if (isset($optionValue['value'])) {
+                $optionValue = $optionValue['value'];
+            }
+        }
+
+        // Render customized option view
+        if (isset($optionInfo['custom_view']) && $optionInfo['custom_view']) {
+            $_default = array('value' => $optionValue);
+            if (isset($optionInfo['option_type'])) {
+                try {
+                    $group = Mage::getModel('catalog/product_option')->groupFactory($optionInfo['option_type']);
+                    return array('value' => $group->getCustomizedView($optionInfo));
+                } catch (Exception $e) {
+                    return $_default;
+                }
+            }
+            return $_default;
+        }
+
+        // Truncate standard view
+        $result = array();
+        if (is_array($optionValue)) {
+            $_truncatedValue = implode("\n", $optionValue);
+            $_truncatedValue = nl2br($_truncatedValue);
+            return array('value' => $_truncatedValue);
+        } else {
+            if ($maxLength) {
+                $_truncatedValue = Mage::helper('core/string')->truncate($optionValue, $maxLength, '');
+            } else {
+                $_truncatedValue = $optionValue;
+            }
+            $_truncatedValue = nl2br($_truncatedValue);
+        }
+
+        $result = array('value' => $_truncatedValue);
+
+        if ($maxLength && (Mage::helper('core/string')->strlen($optionValue) > $maxLength)) {
+            $result['value'] = $result['value'] . $cutReplacer;
+            $optionValue = nl2br($optionValue);
+            $result['full_view'] = $optionValue;
+        }
+
+        return $result;
     }
 }
--- app/code/core/Mage/Checkout/Block/Cart/Item/Renderer.php
+++ app/code/core/Mage/Checkout/Block/Cart/Item/Renderer.php
@@ -318,54 +318,13 @@ class Mage_Checkout_Block_Cart_Item_Renderer extends Mage_Core_Block_Template
      */
     public function getFormatedOptionValue($optionValue)
     {
-        $optionInfo = array();
-
-        // define input data format
-        if (is_array($optionValue)) {
-            if (isset($optionValue['option_id'])) {
-                $optionInfo = $optionValue;
-                if (isset($optionInfo['value'])) {
-                    $optionValue = $optionInfo['value'];
-                }
-            } elseif (isset($optionValue['value'])) {
-                $optionValue = $optionValue['value'];
-            }
-        }
-
-        // render customized option view
-        if (isset($optionInfo['custom_view']) && $optionInfo['custom_view']) {
-            $_default = array('value' => $optionValue);
-            if (isset($optionInfo['option_type'])) {
-                try {
-                    $group = Mage::getModel('catalog/product_option')->groupFactory($optionInfo['option_type']);
-                    return array('value' => $group->getCustomizedView($optionInfo));
-                } catch (Exception $e) {
-                    return $_default;
-                }
-            }
-            return $_default;
-        }
-
-        // truncate standard view
-        $result = array();
-        if (is_array($optionValue)) {
-            $_truncatedValue = implode("\n", $optionValue);
-            $_truncatedValue = nl2br($_truncatedValue);
-            return array('value' => $_truncatedValue);
-        } else {
-            $_truncatedValue = Mage::helper('core/string')->truncate($optionValue, 55, '');
-            $_truncatedValue = nl2br($_truncatedValue);
-        }
-
-        $result = array('value' => $_truncatedValue);
-
-        if (Mage::helper('core/string')->strlen($optionValue) > 55) {
-            $result['value'] = $result['value'] . ' <a href="#" class="dots" onclick="return false">...</a>';
-            $optionValue = nl2br($optionValue);
-            $result = array_merge($result, array('full_view' => $optionValue));
-        }
-
-        return $result;
+        /* @var $helper Mage_Catalog_Helper_Product_Configuration */
+        $helper = Mage::helper('catalog/product_configuration');
+        $params = array(
+            'max_length' => 55,
+            'cut_replacer' => ' <a href="#" class="dots" onclick="return false">...</a>'
+        );
+        return $helper->getFormattedOptionValue($optionValue, $params);
     }
 
     /**
