584c98d8d4a53e9d75e442b58ae8ceb7406dd10b
Vladimir Pelipenko <vladimir.pelipenko@magento.com>
2011-02-22 09:40:37 +0000

    Merged revisions 88144 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r88144 | dmitriy.shatovskiy | 2010-12-08 18:47:41 +0200 (Wed, 08 Dec 2010) | 1 line
      
      Fixed MAGE-1673: StoreView value not in FlatCatalog for multiple-select type attribute
    ........

--- app/code/core/Mage/Eav/Model/Resource/Entity/Attribute/Option.php
+++ app/code/core/Mage/Eav/Model/Resource/Entity/Attribute/Option.php
@@ -93,7 +93,11 @@ class Mage_Eav_Model_Resource_Entity_Attribute_Option extends Mage_Core_Model_Re
         $attributeTable = $attribute->getBackend()->getTable();
         $attributeCode  = $attribute->getAttributeCode();
 
-        $joinCondition = 'e.entity_id = t1.entity_id';
+        $joinConditionTemplate = "e.entity_id = %s.entity_id"
+            ." AND %s.entity_type_id = ".$attribute->getEntityTypeId()
+            ." AND %s.attribute_id = ".$attribute->getId()
+            ." AND %s.store_id = %d";
+        $joinCondition = sprintf($joinConditionTemplate, 't1', 't1', 't1', 't1', Mage_Core_Model_App::ADMIN_STORE_ID);
         if ($attribute->getFlatAddChildData()) {
             $joinCondition .= ' AND e.child_id = t1.entity_id';
         }
@@ -107,12 +111,8 @@ class Mage_Eav_Model_Resource_Entity_Attribute_Option extends Mage_Core_Model_Re
                 array())
             ->joinLeft(
                 array('t2' => $attributeTable),
-                $adapter->quoteInto('t2.entity_id = t1.entity_id'
-                    . ' AND t1.entity_type_id = t2.entity_type_id'
-                    . ' AND t1.attribute_id = t2.attribute_id'
-                    . ' AND t2.store_id = ?', $store),
+                sprintf($joinConditionTemplate, 't2', 't2', 't2', 't2', $store),
                 array($attributeCode => $valueExpr));
-
         if (($attribute->getFrontend()->getInputType() != 'multiselect') && $hasValueField) {
             $valueIdExpr  = $adapter->getCheckSql('to2.value_id > 0', 'to1.value', 'to2.value');
             $select->joinLeft(
@@ -125,11 +125,6 @@ class Mage_Eav_Model_Resource_Entity_Attribute_Option extends Mage_Core_Model_Re
                 array($attributeCode . '_value' => $valueIdExpr)
             );
         }
-
-        $select
-            ->where('t1.entity_type_id = ?', $attribute->getEntityTypeId())
-            ->where('t1.attribute_id = ?', $attribute->getId())
-            ->where('t1.store_id = 0');
 
         if ($attribute->getFlatAddChildData()) {
             $select->where('e.is_child = 0');
