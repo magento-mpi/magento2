dca25d50362e12c5f61bdfb73e097da4d9dd9de3
Vladimir Pelipenko <vladimir.pelipenko@magento.com>
2011-02-22 17:19:07 +0000

    Merged revisions 88572 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r88572 | dimav | 2010-12-14 13:12:11 +0200 (Tue, 14 Dec 2010) | 1 line
      
      CR Change MAGE-1612: Tax Report Shows Wrong Tax Percent After Changing Tax Rate
    ........

--- app/code/core/Mage/Tax/sql/tax_setup/mysql4-upgrade-1.5.0.0-1.5.0.0.1.php
+++ app/code/core/Mage/Tax/sql/tax_setup/mysql4-upgrade-1.5.0.0-1.5.0.0.1.php
@@ -30,9 +30,9 @@ $installer = $this;
 /**
  * Modify tax report aggregation table to have the tax percent field as part of unique key,
  * because tax rate can be changed (code will remain the same) and it will not be reflected in statistics
- * Data has to be truncated to avoid possible duplicates and then reaggregated to reflect the correct data 
+ * Data has to be truncated to avoid possible duplicates and then reaggregated to reflect the correct data
  */
-$table = $installer->getTable('tax_order_aggregated_created');
+$table = $installer->getTable('tax/tax_order_aggregated_created');
 $installer->getConnection()->truncate($table);
 $installer->getConnection()->addKey($table, 'UNQ_PERIOD_STORE_CODE_ORDER_STATUS',
     array('period', 'store_id', 'code', 'percent', 'order_status'), 'unique'
