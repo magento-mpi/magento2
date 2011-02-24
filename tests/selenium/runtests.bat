ff0a6cb7f73cc8d6d6b34241b491b500a125d50d
automation <noreply@magento.com>
2011-02-24 16:34:16 +0000

    Merged revisions 89811 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r89811 | michael | 2010-12-28 16:32:19 +0200 (Tue, 28 Dec 2010) | 1 line
      
      New selenium tests structure
    ........

---
+++ tests/selenium/runtests.bat
@@ -0,0 +1,22 @@
+REM
+REM Magento
+REM
+REM NOTICE OF LICENSE
+REM
+REM This source file is subject to the Open Software License (OSL 3.0)
+REM that is bundled with this package in the file LICENSE.txt.
+REM It is also available through the world-wide-web at this URL:
+REM http://opensource.org/licenses/osl-3.0.php
+REM If you did not receive a copy of the license and are unable to
+REM obtain it through the world-wide-web, please send an email
+REM to license@magentocommerce.com so we can send you a copy immediately.
+REM
+REM DISCLAIMER
+REM
+REM Do not edit or add to this file if you wish to upgrade Magento to newer
+REM versions in the future. If you wish to customize Magento for your
+REM needs please refer to http://www.magentocommerce.com for more information.
+REM
+REM @copyright    Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
+REM @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+REM
--- tests/selenium/testloader.php
+++
@@ -1,5 +0,0 @@
-<?php
-function __autoload($class) {
-    include str_replace('_', DIRECTORY_SEPARATOR, $class).'.php';
-}
-?>
