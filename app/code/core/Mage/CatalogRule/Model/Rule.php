0a1a7f46563c751c002ad663587ecdb00d45127e
automation <noreply@magento.com>
2011-02-25 16:25:29 +0000

    Merged revisions 90730 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r90730 | roman.stepanov | 2011-01-10 18:45:20 +0200 (Mon, 10 Jan 2011) | 3 lines
      
      Fixed MAGE-1369: Catalog Price Rule is not applied
      - Added Mage_CatalogRule_Model_Rule::applyAllRulesToProduct()
    ........

--- app/code/core/Mage/CatalogRule/Model/Rule.php
+++ app/code/core/Mage/CatalogRule/Model/Rule.php
@@ -291,6 +291,23 @@ class Mage_CatalogRule_Model_Rule extends Mage_Rule_Model_Rule
     }
 
     /**
+     * Apply all price rules to product
+     *
+     * @param int|Mage_Catalog_Model_Product $product
+     *
+     * @return Mage_CatalogRule_Model_Rule
+     */
+    public function applyAllRulesToProduct($productId)
+    {
+        $this->_getResource()->applyAllRulesForDateRange(NULL, NULL, $productId);
+        $this->_invalidateCache();
+        $indexProcess = Mage::getSingleton('index/indexer')->getProcessByCode('catalog_product_price');
+        if ($indexProcess) {
+            $indexProcess->reindexAll();
+        }
+    }
+
+    /**
      * Calculate price using catalog price rule of product
      *
      * @param Mage_Catalog_Model_Product $product
