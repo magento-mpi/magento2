2e5746eae0cdd33990e9efc41316e0d0df05ea14
automation <noreply@magento.com>
2011-02-25 09:36:57 +0000

    Merged revisions 89889 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r89889 | dimav | 2010-12-29 10:31:09 +0200 (Wed, 29 Dec 2010) | 2 lines
      
      Fixed Continues Integration build script
      - brute test failure detection changed to ignore anything from script and check only var/report folder
    ........

--- tools/build/brutetest.sh
+++ tools/build/brutetest.sh
@@ -6,7 +6,8 @@ cd $PWD
 log "Testing site..."
 chmod +x $BUILD_TOOLS/pavuk
 $BUILD_TOOLS/pavuk -quiet -nredirs 10 -retry 5 -mode dontstore --logfile /dev/null -lmax 5 -dmax 50 -remove_adv -skip_pattern *.jpg,*.gif,*.css,*.ico,*.png -noRobots -adomain kq.varien.com http://kq.varien.com/builds/$BUILD_NAME/$BUILD_NUMBER
-check_failure $?
+#we should ignore this bc it can be 503, 404, etc and we need failure only if report page present.
+#check_failure $?
 
 if [ -d var/report/ ] ; then
     find var/report/ -type f && failed "Please check report folder for more info..."
