79aea630a31e47a76c6359e5acd7c341d76d9932
automation <noreply@magento.com>
2011-02-25 14:44:27 +0000

    Merged revisions 90373 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r90373 | yevgen.kubichka | 2011-01-05 17:09:06 +0200 (Wed, 05 Jan 2011) | 2 lines
      
      Fixed MAGE-1846: Memory leak
      - Made new method clear(), that clears references to object and helps garbage collector
    ........

--- app/code/core/Mage/Catalog/Model/Product.php
+++ app/code/core/Mage/Catalog/Model/Product.php
@@ -1710,20 +1710,7 @@ class Mage_Catalog_Model_Product extends Mage_Catalog_Model_Abstract
      */
     public function reset()
     {
-        foreach ($this->_data as $data){
-            if (is_object($data) && method_exists($data, 'reset')){
-                $data->reset();
-            }
-        }
-
-        $this->setData(array());
-        $this->setOrigData();
-        $this->_customOptions       = array();
-        $this->_optionInstance      = null;
-        $this->_options             = array();
-        $this->_canAffectOptions    = false;
-        $this->_errors              = array();
-
+        $this->_clearData();
         return $this;
     }
 
@@ -1821,6 +1808,74 @@ class Mage_Catalog_Model_Product extends Mage_Catalog_Model_Abstract
             }
         }
 
+        return $this;
+    }
+
+    /**
+     * Clearing references on product
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _clearReferences()
+    {
+        $this->_clearStockItem();
+        $this->_clearOptionReferences();
+    }
+
+    /**
+     * Clearing product's data
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _clearData()
+    {
+        foreach ($this->_data as $data){
+            if (is_object($data) && method_exists($data, 'reset')){
+                $data->reset();
+            }
+        }
+
+        $this->setData(array());
+        $this->setOrigData();
+        $this->_customOptions       = array();
+        $this->_optionInstance      = null;
+        $this->_options             = array();
+        $this->_canAffectOptions    = false;
+        $this->_errors              = array();
+
+        return $this;
+    }
+
+    /**
+     * Clearing references to product from product's options
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _clearOptionReferences()
+    {
+        /**
+         * unload product options
+         */
+        if (!empty($this->_options)) {
+            foreach ($this->_options as $key => $option) {
+                $option->setProduct();
+                $option->clear();
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Clearing references to product from product's stock item
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _clearStockItem()
+    {
+        if ($this->hasStockItem()){
+            $this->getStockItem()->reset();
+        }
         return $this;
     }
 }
