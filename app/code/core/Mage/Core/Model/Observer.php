50b1885f6c04759d187d49091c10f683b14736e6
automation <noreply@magento.com>
2011-02-25 15:16:10 +0000

    Merged revisions 90467 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r90467 | andrey.abumuslimov | 2011-01-06 13:03:49 +0200 (Thu, 06 Jan 2011) | 4 lines
      
      Fixed MAGE-1847: A system error message is missing for cases when media storage synchronization is complete or an error occurred
      
      - output messages for notifications edited
    ........

--- app/code/core/Mage/Core/Model/Observer.php
+++ app/code/core/Mage/Core/Model/Observer.php
@@ -66,11 +66,11 @@ class Mage_Core_Model_Observer
                 $flagData = $flag->getFlagData();
                 if (isset($flagData['has_errors']) && $flagData['has_errors']) {
                     $severity       = Mage_AdminNotification_Model_Inbox::SEVERITY_MAJOR;
-                    $title          = Mage::helper('adminhtml')->__('An error has occured while syncronizing media storages');
+                    $title          = Mage::helper('adminhtml')->__('An error has occured while syncronizing media storages.');
                     $description    = Mage::helper('adminhtml')->__('One or more media files failed to be synchronized during the media storages syncronization process. Refer to the log file for details.');
                 } else {
                     $severity       = Mage_AdminNotification_Model_Inbox::SEVERITY_NOTICE;
-                    $title          = Mage::helper('adminhtml')->__('Media storage synchronization has complete');
+                    $title          = Mage::helper('adminhtml')->__('Media storage synchronization has completed!');
                     $description    = Mage::helper('adminhtml')->__('Synchronization of media storages has been successfully completed.');
                 }
 
