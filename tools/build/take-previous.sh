4f8ff6395a6881d46653ddf73d259f995f46e7fb
automation <noreply@magento.com>
2011-02-28 13:10:29 +0000

    Merged revisions 91561 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r91561 | dimav | 2011-01-17 14:47:18 +0200 (Mon, 17 Jan 2011) | 2 lines
      
      Fixed Continues Integration build script
      - update current script was not ignoring previous success build if it was not found
    ........

--- tools/build/take-previous.sh
+++ tools/build/take-previous.sh
@@ -2,14 +2,16 @@
 
 log "Searching for last successful build..."
 SB=`wget -q -O - http://rest:gyroscope@kn.varien.com/teamcity/httpAuth/app/rest/buildTypes/id:$3/builds/status:SUCCESS/number`
-if [[ -d $SB && $SKIP_IF_NEEDED -eq 1]]; then
+if [ -d $SB ]; then
     log "Searching for DB..."
     SB_DB_TEMP="builds-$BUILD_NAME-$SB"
     SB_DB=${SB_DB_TEMP//-/_}
     echo 'SHOW DATABASES;' | mysql -u root | grep $SB_DB > /dev/null
     check_failure $?
+elif [ $SKIP_IF_NEEDED -eq 1 ]; then
+    log "Not found"
+    SB=""
 else
     log "Not found"
     exit 1
 fi
-
