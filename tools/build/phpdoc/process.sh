aeb2076ad047c92a6a4755e74f2c42b999140b6a
automation <noreply@magento.com>
2011-03-02 18:11:22 +0000

    Merged revisions 98878 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r98878 | dimav | 2011-02-25 16:34:08 +0200 (Fri, 25 Feb 2011) | 1 line
      
      [CI] Updated build scripts for correct phpdoc generation
    ........

---
+++ tools/build/phpdoc/process.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ ! $1 ] || [ ! $2 ] || [ ! $3 ] || [ ! $4 ];
+then
+echo "Script usage: ./process.sh VERSION VARIEN_LIB_TITLE MAGE_TITLE DIR"
+exit 1
+fi
+
+cd $4
+
+#Fix file in all version
+find . -name '*.html' | xargs sed -i 's~#MAGE_VERSION#~$1~g'
+
+#Fix text and titles in Varien Files
+find ./Varien -name "*.html" | xargs sed -i 's~#MAGE_TITLE#~$2~g'
+find ./Varien -name "*.html" | xargs sed -i 's~#MAGE_LINK_TITLE#~$3~g'
+find ./Varien -name "*.html" | xargs sed -i 's~#MAGE_LINK_TEXT#~$3~g'
+find . -path "./Varien/*/*" -prune -name "*.html" | xargs sed -i 's~#MAGE_LINK_HREF#~../../index.html~g'
+find . -path "./Varien/*" -prune -name "*.html" | xargs sed -i 's~#MAGE_LINK_HREF#~../index.html~g'
+
+#Fix text and titles in Mage Files
+find . -name "*.html" | grep -v '/Varien/' | xargs sed -i 's~#MAGE_TITLE#~$3~g'
+find . -name "*.html" | grep -v '/Varien/' | xargs sed -i 's~#MAGE_LINK_TITLE#~$2~g'
+find . -name "*.html" | grep -v '/Varien/' | xargs sed -i 's~#MAGE_LINK_TEXT#~$2~g'
+find . -path "./*/*" -prune -name "*.html" | xargs sed -i 's~#MAGE_LINK_HREF#~../Varien/index.html~g'
+find . -path "./*" -prune -name "*.html" | xargs sed -i 's~#MAGE_LINK_HREF#~Varien/index.html~g'
+
+exit 0
