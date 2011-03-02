d3cd0fbc9eb32a3901ef5166599e21d8ef7a8ea1
automation <noreply@magento.com>
2011-03-02 18:32:45 +0000

    Merged revisions 99349 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r99349 | taras.shabatyn | 2011-02-28 15:51:00 +0200 (Mon, 28 Feb 2011) | 1 line
      
      [WS] Added helper functions
    ........

--- tests/WebService/Helper/Data.php
+++ tests/WebService/Helper/Data.php
@@ -29,5 +29,69 @@ class WebService_Helper_Data
         $xml = simplexml_load_file($xmlPath);
         return WebService_Helper_Xml::simpleXMLToArray($xml);        
     }
+
+    private static function _push(&$obj, $key, $value){
+        if(is_numeric($key)){
+            $key = 'Obj' . $key;
+        }
+        $obj->$key = $value;
+    }
+
+    /**
+     * @param Array $arr
+     * @return object
+     */
+    public static function arrayToObject($arr) {
+        $obj = new stdClass();
+        if(is_array($arr)){
+            foreach($arr as $key => $value){
+                if(is_array($value)){
+                    self::_push($obj, $key, self::arrayToObject($value));
+                } else {
+                    self::_push($obj, $key, $value);
+                }
+            }
+        }
+        return $obj;
+    }
+
+    /**
+     * @param object $obj
+     * @return Array
+     */
+    public static function objectToArray($obj)
+    {
+        if (is_object($obj) && null !== ($_data = get_object_vars($obj))) {
+            foreach ($_data as $key => $value) {
+                if(is_object($value)){
+                    $_data[$key] = self::objectToArray($value);
+                }
+            }
+            return $_data;
+        } elseif ( is_array($obj) ){
+            return $obj;
+        }
+        return array();
+    }
+
+    /**
+     * Will return an array, only with keys, specified at "$arrayKeys".
+     * @param Array $arr
+     * @param Array $arrayKeys
+     * @return Array
+     */
+    public static function filterArray(Array $arr, $arrayKeys) {
+        $res = array();
+        foreach ($arr as $arrItem) {
+            $tmpArr = array();
+            foreach ($arrayKeys as $arrayKeyItem) {
+                if(isset($arrItem[$arrayKeyItem])){
+                    $tmpArr[$arrayKeyItem] = $arrItem[$arrayKeyItem];
+                }
+            }
+            $res[] = $tmpArr;
+        }
+        return $res;
+    }
 }
 ?>
