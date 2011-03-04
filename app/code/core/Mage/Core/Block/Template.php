ba7a9ae6a310a96d18b6bd0cf87dfb6547128c0e
automation <noreply@magento.com>
2011-03-04 18:29:27 +0000

    Merged revisions 100525 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r100525 | alex.bomko | 2011-03-04 20:25:58 +0200 (Fri, 04 Mar 2011) | 1 line
      
      Fixed MAGE-2575: Symlinks do not work for back-end blocks
    ........

--- app/code/core/Mage/Core/Block/Template.php
+++ app/code/core/Mage/Core/Block/Template.php
@@ -53,6 +53,13 @@ class Mage_Core_Block_Template extends Mage_Core_Block_Abstract
 
     protected $_jsUrl;
 
+    /**
+     * Is allowed symlinks flag
+     *
+     * @var bool
+     */
+    protected $_allowSymlinks = null;
+
     protected static $_showTemplateHints;
     protected static $_showTemplateHintsBlocks;
 
@@ -203,16 +210,25 @@ class Mage_Core_Block_Template extends Mage_Core_Block_Abstract
             ob_start();
         }
         if ($this->getShowTemplateHints()) {
-            echo '<div style="position:relative; border:1px dotted red; margin:6px 2px; padding:18px 2px 2px 2px; zoom:1;"><div style="position:absolute; left:0; top:0; padding:2px 5px; background:red; color:white; font:normal 11px Arial; text-align:left !important; z-index:998;" onmouseover="this.style.zIndex=\'999\'" onmouseout="this.style.zIndex=\'998\'" title="'.$fileName.'">'.$fileName.'</div>';
+            echo <<<HTML
+<div style="position:relative; border:1px dotted red; margin:6px 2px; padding:18px 2px 2px 2px; zoom:1;">
+<div style="position:absolute; left:0; top:0; padding:2px 5px; background:red; color:white; font:normal 11px Arial;
+text-align:left !important; z-index:998;" onmouseover="this.style.zIndex='999'"
+onmouseout="this.style.zIndex='998'" title="{$fileName}">{$fileName}</div>
+HTML;
             if (self::$_showTemplateHintsBlocks) {
                 $thisClass = get_class($this);
-                echo '<div style="position:absolute; right:0; top:0; padding:2px 5px; background:red; color:blue; font:normal 11px Arial; text-align:left !important; z-index:998;" onmouseover="this.style.zIndex=\'999\'" onmouseout="this.style.zIndex=\'998\'" title="'.$thisClass.'">'.$thisClass.'</div>';
+                echo <<<HTML
+<div style="position:absolute; right:0; top:0; padding:2px 5px; background:red; color:blue; font:normal 11px Arial;
+text-align:left !important; z-index:998;" onmouseover="this.style.zIndex='999'" onmouseout="this.style.zIndex='998'"
+title="{$thisClass}">{$thisClass}</div>
+HTML;
             }
         }
 
         try {
             $includeFilePath = realpath($this->_viewDir . DS . $fileName);
-            if (strpos($includeFilePath, realpath($this->_viewDir)) === 0) {
+            if (strpos($includeFilePath, realpath($this->_viewDir)) === 0 || $this->_getAllowSymlinks()) {
                 include $includeFilePath;
             } else {
                 Mage::log('Not valid template file:'.$fileName, Zend_Log::CRIT, null, null, true);
@@ -316,5 +332,18 @@ class Mage_Core_Block_Template extends Mage_Core_Block_Abstract
             $this->getTemplateFile(),
             'template' => $this->getTemplate()
         );
+    }
+
+    /**
+     * Get is allowed symliks flag
+     *
+     * @return bool
+     */
+    protected function _getAllowSymlinks()
+    {
+        if (is_null($this->_allowSymlinks)) {
+            $this->_allowSymlinks = Mage::getStoreConfigFlag('dev/template/allow_symlink');
+        }
+        return $this->_allowSymlinks;
     }
 }
