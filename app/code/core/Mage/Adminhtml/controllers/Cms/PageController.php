1923ae0c400fb9a380f6201cae86f04d43bb9cf8
automation <noreply@magento.com>
2011-04-11 13:28:26 +0000

    Merged revisions 102483 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r102483 | andrey.roslik | 2011-03-23 17:51:46 +0200 (Wed, 23 Mar 2011) | 2 lines
      
      Fixed MAGE-2560: Ability to use helpers via XML Layout update
      Added validation to Custom Layout Update
    ........

--- app/code/core/Mage/Adminhtml/controllers/Cms/PageController.php
+++ app/code/core/Mage/Adminhtml/controllers/Cms/PageController.php
@@ -90,7 +90,8 @@ class Mage_Adminhtml_Cms_PageController extends Mage_Adminhtml_Controller_Action
         if ($id) {
             $model->load($id);
             if (! $model->getId()) {
-                Mage::getSingleton('adminhtml/session')->addError(Mage::helper('cms')->__('This page no longer exists.'));
+                Mage::getSingleton('adminhtml/session')->addError(
+                    Mage::helper('cms')->__('This page no longer exists.'));
                 $this->_redirect('*/*/');
                 return;
             }
@@ -109,7 +110,11 @@ class Mage_Adminhtml_Cms_PageController extends Mage_Adminhtml_Controller_Action
 
         // 5. Build edit form
         $this->_initAction()
-            ->_addBreadcrumb($id ? Mage::helper('cms')->__('Edit Page') : Mage::helper('cms')->__('New Page'), $id ? Mage::helper('cms')->__('Edit Page') : Mage::helper('cms')->__('New Page'));
+            ->_addBreadcrumb(
+                $id ? Mage::helper('cms')->__('Edit Page')
+                    : Mage::helper('cms')->__('New Page'),
+                $id ? Mage::helper('cms')->__('Edit Page')
+                    : Mage::helper('cms')->__('New Page'));
 
         $this->renderLayout();
     }
@@ -145,7 +150,8 @@ class Mage_Adminhtml_Cms_PageController extends Mage_Adminhtml_Controller_Action
                 $model->save();
 
                 // display success message
-                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('cms')->__('The page has been saved.'));
+                Mage::getSingleton('adminhtml/session')->addSuccess(
+                    Mage::helper('cms')->__('The page has been saved.'));
                 // clear previously saved data from session
                 Mage::getSingleton('adminhtml/session')->setFormData(false);
                 // check if 'Save and Continue'
@@ -161,7 +167,8 @@ class Mage_Adminhtml_Cms_PageController extends Mage_Adminhtml_Controller_Action
                 $this->_getSession()->addError($e->getMessage());
             }
             catch (Exception $e) {
-                $this->_getSession()->addException($e, Mage::helper('cms')->__('An error occurred while saving the page.'));
+                $this->_getSession()->addException($e,
+                    Mage::helper('cms')->__('An error occurred while saving the page.'));
             }
 
             $this->_getSession()->setFormData($data);
@@ -186,7 +193,8 @@ class Mage_Adminhtml_Cms_PageController extends Mage_Adminhtml_Controller_Action
                 $title = $model->getTitle();
                 $model->delete();
                 // display success message
-                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('cms')->__('The page has been deleted.'));
+                Mage::getSingleton('adminhtml/session')->addSuccess(
+                    Mage::helper('cms')->__('The page has been deleted.'));
                 // go to grid
                 Mage::dispatchEvent('adminhtml_cmspage_on_delete', array('title' => $title, 'status' => 'success'));
                 $this->_redirect('*/*/');
@@ -253,6 +261,10 @@ class Mage_Adminhtml_Cms_PageController extends Mage_Adminhtml_Controller_Action
             /** @var $validatorCustomLayout Mage_Adminhtml_Model_LayoutUpdate_Validator */
             $validatorCustomLayout = Mage::getModel('adminhtml/layoutUpdate_validator');
             if (!empty($data['layout_update_xml']) && !$validatorCustomLayout->isValid($data['layout_update_xml'])) {
+                $errorNo = false;
+            }
+            if (!empty($data['custom_layout_update_xml'])
+            && !$validatorCustomLayout->isValid($data['custom_layout_update_xml'])) {
                 $errorNo = false;
             }
             foreach ($validatorCustomLayout->getMessages() as $message) {
