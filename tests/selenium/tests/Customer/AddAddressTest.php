e16e9e997e077b40fab98e4317f93197866778ef
automation <noreply@magento.com>
2011-05-06 13:10:04 +0000

    Merged revisions 103988 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r103988 | evgeniy.maksimenko | 2011-04-06 18:06:28 +0300 (Wed, 06 Apr 2011) | 1 line
      
      [AUTOTEST]  add address tests  are edited
    ........

--- tests/selenium/tests/Customer/AddAddressTest.php
+++ tests/selenium/tests/Customer/AddAddressTest.php
@@ -28,7 +28,7 @@
  */
 
 /**
- * @TODO
+ * Add address tests.
  *
  * @package     selenium
  * @subpackage  tests
@@ -45,54 +45,76 @@ class Customer_Account_AddAddressTest extends Mage_Selenium_TestCase {
      */
     protected function assertPreConditions()
     {
-        $this->assertTrue($this->loginAdminUser());
+        $this->loginAdminUser();
         $this->assertTrue($this->admin());
-        $this->assertTrue($this->navigate('manage_customers'));
-
-        // for replacing in MCA
-        $this->addParameter('id', '0');
+        $this->navigate('manage_customers');
+        $this->assertTrue($this->checkCurrentPage('manage_customers'), 'Wrong page is opened');
     }
 
+    /**
+     * Create customer for add customer address tests
+     *
+     * @return array
+     */
     public function test_CreateCustomer()
     {
         //Data
-        $userData = $this->loadData('generic_customer_account', array('email' => $this->_dataGenerator->generateEmailAddress()));
+        $userData = $this->loadData('generic_customer_account',
+                        array('email' => $this->generate('email', 20, 'valid')));
         //Steps
         $this->clickButton('add_new_customer');
-
         $this->fillForm($userData, 'account_information');
         $this->clickButton('save_customer');
         //Verifying
-//        @TODO func 'navigated'
-//        $this->assertTrue($this->navigated('manage_customers'),
-//                'After successful customer creation should be redirected to Manage Customers page');
-
+        $this->assertTrue($this->checkCurrentPage('manage_customers'),
+                'After successful customer creation should be redirected to Manage Customers page');
         $this->assertTrue($this->successMessage('success_saved_customer'), $this->messages);
         return $userData;
     }
 
     /**
+     * Add address for customer. Fill in only required field.
+     *
+     * Steps:
+     *
+     * 1. Search and open customer.
+     *
+     * 2. Open 'Addresses' tab.
+     *
+     * 3. Click 'Add New Address' button.
+     *
+     * 4. Fill in required fields.
+     *
+     * 5. Click  'Save Customer' button
+     *
+     * Expected result:
+     *
+     * Customer address is added. Customer info is saved.
+     *
+     * Success Message is displayed
+     *
      * @depends test_CreateCustomer
+     *
+     * @param array $userData
+     * @return array
      */
     public function test_WithRequiredFieldsOnly(array $userData)
     {
         //Data
         $searchData = $this->loadData('search_customer', array('email' => $userData['email']));
         $addressData = $this->loadData('generic_address');
-
         //Steps
-        $this->assertTrue($this->searchAndOpen($searchData), 'Element not found.');
-
-        $this->_currentPage = 'edit_customer';
+        // 1.Open customer
+        $this->clickButton('reset_filter', FALSE);
+        $this->pleaseWait();
+        $this->assertTrue($this->searchAndOpen($searchData), 'Customer is not found');
+        // Defining and adding %address_number% for customer Uimap
+        $this->addAddressNumber();
+        // 2.Add Address and save
         $this->clickControl('tab', 'addresses', FALSE);
-        $xpath = $this->getCurrentUimapPage()->findFieldset('list_customer_addresses')->getXPath();
-        $addressCount = $this->getXpathCount('//' . $xpath . '//li') + 1;
-        $this->addParameter('address_number', $addressCount);
-
         $this->clickButton('add_new_address', FALSE);
         $this->fillForm($addressData, 'addresses');
         $this->clickButton('save_customer');
-
         //Verifying
         $this->assertTrue($this->successMessage('success_saved_customer'), $this->messages);
         return $searchData;
@@ -101,47 +123,67 @@ class Customer_Account_AddAddressTest extends Mage_Selenium_TestCase {
     /**
      * Add Address for customer with one empty reqired field.
      *
+     * Steps:
+     *
+     * 1. Search and open customer.
+     *
+     * 2. Open 'Addresses' tab.
+     *
+     * 3. Click 'Add New Address' button.
+     *
+     * 4. Fill in fields exept one required.
+     *
+     * 5. Click  'Save Customer' button
+     *
+     * Expected result:
+     *
+     * Customer address isn't added. Customer info is not saved.
+     *
+     * Error Message is displayed
+     *
      * @depends test_WithRequiredFieldsOnly
      * @dataProvider data_emptyFields
+     *
+     * @param array $emptyField
+     * @param array $searchData
      */
     public function test_WithRequiredFieldsEmpty($emptyField, array $searchData)
     {
         //Data
         $addressData = $this->loadData('generic_address', $emptyField);
         //Steps
+        // 1.Open customer
         $this->clickButton('reset_filter', FALSE);
         $this->pleaseWait();
-
-        $this->assertTrue($this->searchAndOpen($searchData), 'Element not found.');
-        $this->_currentPage = 'edit_customer';
+        $this->assertTrue($this->searchAndOpen($searchData), 'Customer is not found');
+        // Defining and adding %address_number% for customer Uimap
+        $this->addAddressNumber();
+        // 2.Add Address and save
         $this->clickControl('tab', 'addresses', FALSE);
-        $xpath = $this->getCurrentUimapPage()->findFieldset('list_customer_addresses')->getXPath();
-        $addressCount = $this->getXpathCount('//' . $xpath . '//li') + 1;
-        $this->addParameter('address_number', $addressCount);
         $this->clickButton('add_new_address', FALSE);
-
         $this->fillForm($addressData, 'addresses');
         $this->clickButton('save_customer');
-
         //Verifying
+        // Defining and adding %fieldXpath% for customer Uimap
+        $page = $this->getUimapPage('admin', 'edit_customer');
+        $fieldSet = $page->findFieldset('edit_address');
         foreach ($emptyField as $key => $value) {
-            if ($this->getUimapPage('admin', 'edit_customer')->getMainForm()->getTab('addresses')->getFieldset('edit_address')->findField($key) != Null) {
-                $fieldXpath = $this->getUimapPage('admin', 'edit_customer')->getMainForm()->getTab('addresses')->getFieldset('edit_address')->findField($key);
-                $xpath1 = '//' . $this->_paramsHelper->replaceParameters($fieldXpath);
-                if (!$this->isElementPresent($xpath1)) {
-                    $fieldXpath = $this->getUimapPage('admin', 'edit_customer')->getMainForm()->getTab('addresses')->getFieldset('edit_address')->findDropdown($key);
+            if ($fieldSet->findField($key) != Null) {
+                $fieldXpath = $fieldSet->findField($key);
+                if (!$this->isElementPresent('//' . $this->_paramsHelper->replaceParameters($fieldXpath))) {
+                    $fieldXpath = $fieldSet->findDropdown($key);
                 }
             } else {
-                $fieldXpath = $this->getUimapPage('admin', 'edit_customer')->getMainForm()->getTab('addresses')->getFieldset('edit_address')->findDropdown($key);
+                $fieldXpath = $fieldSet->findDropdown($key);
             }
             if (preg_match('/street_address/', $key)) {
-                $fieldXpath = $this->_paramsHelper->replaceParameters($fieldXpath) . "/ancestor::div[@class='multi-input']";
+                $fieldXpath = $this->_paramsHelper->replaceParameters($fieldXpath)
+                        . "/ancestor::div[@class='multi-input']";
             } else {
                 $fieldXpath = $this->_paramsHelper->replaceParameters($fieldXpath);
             }
             $this->addParameter('fieldXpath', $fieldXpath);
         }
-
         $this->assertTrue($this->errorMessage('empty_required_field'), $this->messages);
     }
 
@@ -160,14 +202,32 @@ class Customer_Account_AddAddressTest extends Mage_Selenium_TestCase {
     }
 
     /**
+     * Add address for customer. Fill in only required field. Use max long values for fields.
+     *
+     * Steps:
+     *
+     * 1. Search and open customer.
+     *
+     * 2. Open 'Addresses' tab.
+     *
+     * 3. Click 'Add New Address' button.
+     *
+     * 4. Fill in fields by long value alpha-numeric data exept 'country' field.
+     *
+     * 5. Click  'Save Customer' button
+     *
+     * Expected result:
+     *
+     * Customer address is added. Customer info is saved.
+     *
+     * Success Message is displayed. Length of fields are 255 characters.
+     *
      * @depends test_WithRequiredFieldsOnly
      */
     public function test_WithLongValues(array $searchData)
     {
         //Data
         $longValues = array(
-            'default_billing_address' => 'Yes',
-            'default_shipping_address' => 'yes',
             'prefix' => $this->generate('string', 255, ':alnum:'),
             'first_name' => $this->generate('string', 255, ':alnum:'),
             'middle_name_initial' => $this->generate('string', 255, ':alnum:'),
@@ -183,54 +243,121 @@ class Customer_Account_AddAddressTest extends Mage_Selenium_TestCase {
             'telephone' => $this->generate('string', 255, ':alnum:'),
             'fax' => $this->generate('string', 255, ':alnum:')
         );
-        $addressData = $this->loadData('all_fields_address', $longValues);
+        $addressData = $this->loadData('generic_address', $longValues);
         //Steps
-        $this->clickButton('reset_filter',FALSE);
+        // 1.Open customer
+        $this->clickButton('reset_filter', FALSE);
         $this->pleaseWait();
-
-        $this->assertTrue($this->searchAndOpen($searchData), 'Element not found.');
-        $this->_currentPage = 'edit_customer';
+        $this->assertTrue($this->searchAndOpen($searchData), 'Customer is not found');
+        // Defining and adding %address_number% for customer Uimap
+        $this->addAddressNumber();
+        // 2.Add Address and save
         $this->clickControl('tab', 'addresses', FALSE);
-        $xpath = $this->getCurrentUimapPage()->findFieldset('list_customer_addresses')->getXPath();
-        $addressCount = $this->getXpathCount('//' . $xpath . '//li') + 1;
-        $this->addParameter('address_number', $addressCount);
         $this->clickButton('add_new_address', FALSE);
         $this->fillForm($addressData, 'addresses');
         $this->clickButton('save_customer');
-        //Verifying
-//        @TODO func 'navigated'
-//        $this->assertTrue($this->navigated('manage_customers'),
-//                'After successful customer creation should be redirected to Manage Customers page');
+        //Verifying в„–1
+        $this->assertTrue($this->checkCurrentPage('manage_customers'),
+                'After successful customer creation should be redirected to Manage Customers page');
         $this->assertTrue($this->successMessage('success_saved_customer'), $this->messages);
-        // @TODO
-        // check saved values
-//        $this->clickButton('reset_filter'/* ,FALSE */);
-//        @TODO
-//        $this->pleaseWait();
-//        $this->searchAndOpen($searchData);
-//        $this->clickControl('tab', 'addresses', FALSE);
-//        foreach ($longValues as $key => $value) {
-//            if ($this->getUimapPage('admin', 'edit_customer')->getMainForm()->getTab('addresses')->getFieldset('edit_address')->findField($key) != Null) {
-//                $fieldXpath = $this->getUimapPage('admin', 'edit_customer')->getMainForm()->getTab('addresses')->getFieldset('edit_address')->findField($key);
-//                $fieldXpath = $this->_paramsHelper->replaceParameters($fieldXpath);
-//                $this->assertEquals(strlen($this->getValue('//' . $fieldXpath)), 255);
-//            }
-//        }
+        // 3.Open customer
+        $this->clickButton('reset_filter', FALSE);
+        $this->pleaseWait();
+        $this->assertTrue($this->searchAndOpen($searchData), 'Customer is not found');
+        $this->clickControl('tab', 'addresses', FALSE);
+        //Verifying в„–2 - Check saved values
+        $addressNumber = $this->isAddressPresent($addressData);
+        $this->assertNotEquals(0, $addressNumber, 'The specified address is not present.');
     }
 
-//    /**
-//     * @depends test_WithRequiredFieldsOnly
-//     */
-//    public function test_WithDefaultBillingAddress(array $searchData)
-//    {
-//        // @TODO
-//    }
-//
-//    /**
-//     * @depends test_WithRequiredFieldsOnly
-//     */
-//    public function test_WithDefaultShippingAddress(array $searchData)
-//    {
-//        // @TODO
-//    }
+    /**
+     * @depends test_WithRequiredFieldsOnly
+     */
+    public function test_WithDefaultBillingAddress(array $searchData)
+    {
+        // @TODO
+    }
+
+    /**
+     * @depends test_WithRequiredFieldsOnly
+     */
+    public function test_WithDefaultShippingAddress(array $searchData)
+    {
+        // @TODO
+    }
+
+    /**
+     * *********************************************
+     * *         HELPER FUNCTIONS                  *
+     * *********************************************
+     */
+
+    /**
+     * Verify that address is present.
+     *
+     * @param array $addressData
+     */
+    public function isAddressPresent(array $addressData)
+    {
+        $page = $this->getCurrentUimapPage();
+        $fieldSet = $page->findFieldset('list_customer_addresses');
+        $xpath = '//' . $fieldSet->getXPath() . '//li';
+        $addressCount = $this->getXpathCount($xpath);
+        for ($i = 1; $i <= $addressCount; $i++) {
+            $this->click($xpath . "[$i]");
+            $id = $this->getValue($xpath . "[$i]/@id");
+            $arrayId = explode('_', $id);
+            $id = end($arrayId);
+            $this->addParameter('address_number', $id);
+            $page = $this->getCurrentUimapPage();
+            $fieldSet = $page->findFieldset('edit_address');
+            $res = 0;
+            foreach ($addressData as $key => $value) {
+                // Get Xpath
+                if ($fieldSet->findField($key) != Null) {
+                    $fieldXpath = $fieldSet->findField($key);
+                    $fieldType = 'field';
+                    if (!$this->isElementPresent('//' . $this->_paramsHelper->replaceParameters($fieldXpath))) {
+                        $fieldXpath = $fieldSet->findDropdown($key);
+                        $fieldType = 'dropdown';
+                    }
+                } elseif ($fieldSet->findDropdown($key) != Null) {
+                    $fieldXpath = $fieldSet->findDropdown($key);
+                    $fieldType = 'dropdown';
+                }
+                $fieldXpath = $this->_paramsHelper->replaceParameters($fieldXpath);
+                // Get value
+                switch ($fieldType) {
+                    case 'field':
+                        $valueXpath = '//' . $fieldXpath . '/@value';
+                        $text = $this->getValue($valueXpath);
+                        break;
+                    case 'dropdown':
+                        $valueXpath = '//' . $fieldXpath . '/option[@selected]';
+                        $text = $this->getText($valueXpath);
+                        break;
+                }
+                if ($value === $text) {
+                    $res++;
+                }
+            }
+            if ($res == count($addressData)) {
+                return $id;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Defining and adding %address_number% for customer Uimap
+     */
+    public function addAddressNumber()
+    {
+        $page = $this->getCurrentUimapPage();
+        $fieldSet = $page->findFieldset('list_customer_addresses');
+        $xpath = $fieldSet->getXPath();
+        $addressCount = $this->getXpathCount('//' . $xpath . '//li') + 1;
+        $this->addParameter('address_number', $addressCount);
+    }
+
 }
