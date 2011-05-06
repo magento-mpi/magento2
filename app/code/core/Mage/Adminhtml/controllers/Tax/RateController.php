18a06de653ffdc9c6f7ba09a0e733e091f5ea6fc
automation <noreply@magento.com>
2011-05-06 16:31:54 +0000

    Merged revisions 104259 via svnmerge from http://svn.magentocommerce.com/svn/magento/base/magento/trunk
    ........
      r104259 | andrey.roslik | 2011-04-08 19:09:55 +0300 (Fri, 08 Apr 2011) | 2 lines
      
      Fixed MAGE-2957: Error on the saving tax rate with one store view
      Added the checking of exist titles
    ........

--- app/code/core/Mage/Adminhtml/controllers/Tax/RateController.php
+++ app/code/core/Mage/Adminhtml/controllers/Tax/RateController.php
@@ -97,8 +97,10 @@ class Mage_Adminhtml_Tax_RateController extends Mage_Adminhtml_Controller_Action
             /** @var $helper Mage_Adminhtml_Helper_Data */
             $helper = Mage::helper('adminhtml');
             $ratePost['code'] = $helper->stripTags($ratePost['code']);
-            foreach ($ratePost['title'] as &$title) {
-                $title = $helper->stripTags($title);
+            if (!empty($ratePost['title'])) {
+                foreach ($ratePost['title'] as &$title) {
+                    $title = $helper->stripTags($title);
+                }
             }
 
             $rateId = $this->getRequest()->getParam('tax_calculation_rate_id');
