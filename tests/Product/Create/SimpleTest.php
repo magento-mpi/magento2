4381b5309dacd44061c26bb4265ef1629b793c73
Evgeniy Maksimenko <evgeniy.maksimenko@magento.com>
2011-05-31 11:47:55 +0000

    [AUTOTEST] Added Uimap, data, tests for simple product

--- tests/Product/Create/SimpleTest.php
+++ tests/Product/Create/SimpleTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Magento
  *
@@ -37,139 +38,189 @@ class Product_Create_SimpleTest extends Mage_Selenium_TestCase
 {
 
     /**
-     * @TODO
+     * Log in to Backend.
+     */
+    public function setUpBeforeTests()
+    {
+        $this->loginAdminUser();
+    }
+
+    /**
+     * Preconditions:
+     * Navigate to System -> Manage Products
      */
     protected function assertPreConditions()
     {
-        // @TODO
+        $this->navigate('manage_products');
+        $this->assertTrue($this->checkCurrentPage('manage_products'), 'Wrong page is opened');
     }
-
 
     /**
      * @TODO
      */
     public function test_WithRequiredFieldsOnly()
     {
-        // @TODO
+        //Data
+        $productSettings = $this->loadData('product_create_settings_simple');
+        $productData = $this->loadData('simple_product', NULL, 'product_sku');
+
+        //Steps. Open 'Manage Products' page, click 'Add New Product' button, fill in form.
+        $this->navigate('manage_products');
+        $this->clickButton('add_new_product');
+        $this->assertTrue($this->checkCurrentPage('new_product_settings'), 'Wrong page is displayed');
+        $this->fillForm($productSettings);
+        // Defining and adding %attributeSetID% and %productType% for Uimap pages.
+        $page = $this->getCurrentUimapPage();
+        $fieldSet = $page->findFieldset('product_settings');
+        foreach ($productSettings as $fieldsName => $fieldValue) {
+            $xpath = $fieldSet->findDropdown($fieldsName);
+            switch ($fieldsName) {
+                case 'attribute_set':
+                    $attributeSetID = $this->getValue($xpath . "/option[text()='$fieldValue']");
+                    break;
+                case 'product_type':
+                    $productType = $this->getValue($xpath . "/option[text()='$fieldValue']");
+                    break;
+                default:
+                    break;
+            }
+        }
+        $this->addParameter('attributeSetID', $attributeSetID);
+        $this->addParameter('productType', $productType);
+        //Steps. Сlick 'Сontinue' button
+        $this->clickButton('continue_button');
+        $this->fillForm($productData, 'general');
+        $this->clickControl('tab', 'prices', FALSE);
+        $this->fillForm($productData, 'prices');
+        $this->clickControl('tab', 'meta_information', FALSE);
+        $this->fillForm($productData, 'meta_information');
+        $this->clickControl('tab', 'design', FALSE);
+        $this->fillForm($productData, 'design');
+        $this->clickControl('tab', 'inventory', FALSE);
+        $this->fillForm($productData, 'inventory');
+        $this->saveForm('save');
+        $this->assertTrue($this->successMessage('success_saved_product'), $this->messages);
+        $this->assertTrue($this->checkCurrentPage('manage_products'),
+                'After successful product creation should be redirected to Manage Products page');
     }
 
-    /**
-     * @TODO
-     */
-    public function test_WithSkuThatAlreadyExists()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithRequiredFieldsEmpty()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithSpecialCharacters()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithInvalidValueForFields_InvalidWeight()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithInvalidValueForFields_InvalidPrice()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithInvalidValueForFields_InvalidQty()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithCustomOptions_EmptyFields()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithCustomOptions_InvalidValues()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithSpecialPrice_EmptyValue()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithSpecialPrice_InvalidValue()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithTierPrice_EmptyFields()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_WithTierPrice_InvalidValues()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_OnConfigurableProductPage_QuickCreate()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_OnConfigurableProductPage_CreateEmpty()
-    {
-        // @TODO
-    }
-
-    /**
-     * @TODO
-     */
-    public function test_OnConfigurableProductPage_CopyFromConfigurable()
-    {
-        // @TODO
-    }
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithSkuThatAlreadyExists()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithRequiredFieldsEmpty()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithSpecialCharacters()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithInvalidValueForFields_InvalidWeight()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithInvalidValueForFields_InvalidPrice()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithInvalidValueForFields_InvalidQty()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithCustomOptions_EmptyFields()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithCustomOptions_InvalidValues()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithSpecialPrice_EmptyValue()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithSpecialPrice_InvalidValue()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithTierPrice_EmptyFields()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_WithTierPrice_InvalidValues()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_OnConfigurableProductPage_QuickCreate()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_OnConfigurableProductPage_CreateEmpty()
+//    {
+//        // @TODO
+//    }
+//
+//    /**
+//     * @TODO
+//     */
+//    public function test_OnConfigurableProductPage_CopyFromConfigurable()
+//    {
+//        // @TODO
+//    }
 }
