0aaea8f57c0cbd5b642bfc452da9c4174ffbf873
Sergiy Akulinin <sergiy.akulinin@magento.com>
2011-06-06 12:03:14 +0000

    Fixed MAGE-2999: Fixed show free payment method is enabled

--- app/code/core/Mage/Payment/Model/Config.php
+++ app/code/core/Mage/Payment/Model/Config.php
@@ -51,7 +51,7 @@ class Mage_Payment_Model_Config
             if (Mage::getStoreConfigFlag('payment/'.$code.'/active', $store)) {
                 if (array_key_exists('model', $methodConfig)) {
                     $methodModel = Mage::getModel($methodConfig['model']);
-                    if ($methodModel && $methodModel->isAvailable()) {
+                    if ($methodModel && $methodModel->getConfigData('active', $store)) {
                         $methods[$code] = $this->_getMethod($code, $methodConfig);
                     }
                 }
