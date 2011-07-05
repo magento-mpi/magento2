b396c1c7a40244e6cb583f17abae8d5413583bab
Denys Rudchenko <denys.rudchenko@magento.com>
2011-07-05 09:26:38 +0000

    Fixed MAGE-3446: Fatal error when trying to open configurable product with super attribute not in the appropriate attribute set.

--- app/code/core/Mage/Catalog/Model/Product/Type/Configurable.php
+++ app/code/core/Mage/Catalog/Model/Product/Type/Configurable.php
@@ -226,9 +226,11 @@ class Mage_Catalog_Model_Product_Type_Configurable extends Mage_Catalog_Model_Pr
             $usedProductAttributes = array();
             $usedAttributes        = array();
             foreach ($this->getConfigurableAttributes($product) as $attribute) {
-                $id = $attribute->getProductAttribute()->getId();
-                $usedProductAttributes[$id] = $attribute->getProductAttribute();
-                $usedAttributes[$id]        = $attribute;
+                if (!is_null($attribute->getProductAttribute())) {
+                    $id = $attribute->getProductAttribute()->getId();
+                    $usedProductAttributes[$id] = $attribute->getProductAttribute();
+                    $usedAttributes[$id]        = $attribute;
+                }
             }
             $this->getProduct($product)->setData($this->_usedAttributes, $usedAttributes);
             $this->getProduct($product)->setData($this->_usedProductAttributes, $usedProductAttributes);
