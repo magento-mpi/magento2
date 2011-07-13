a56a7ed6ea43765d185d0f09e31bcf125dd774cc
Sergiy Akulinin <sergiy.akulinin@magento.com>
2011-07-13 11:05:31 +0000

    Fixed MAGE-3896: Add new address through checkout does not work (Frontend)  - reverted changes from rev 114238. Bug will have "won't fix" resolution

--- app/code/core/Mage/Checkout/Block/Onepage/Billing.php
+++ app/code/core/Mage/Checkout/Block/Onepage/Billing.php
@@ -102,7 +102,17 @@ class Mage_Checkout_Block_Onepage_Billing extends Mage_Checkout_Block_Onepage_Ab
     public function getAddress()
     {
         if (is_null($this->_address)) {
-            $this->_address = $this->getNewAddress();
+            if ($this->isCustomerLoggedIn()) {
+                $this->_address = $this->getQuote()->getBillingAddress();
+                if(!$this->_address->getFirstname()) {
+                    $this->_address->setFirstname($this->getQuote()->getCustomer()->getFirstname());
+                }
+                if(!$this->_address->getLastname()) {
+                    $this->_address->setLastname($this->getQuote()->getCustomer()->getLastname());
+                }
+            } else {
+                $this->_address = Mage::getModel('sales/quote_address');
+            }
         }
 
         return $this->_address;
--- app/code/core/Mage/Checkout/Block/Onepage/Shipping.php
+++ app/code/core/Mage/Checkout/Block/Onepage/Shipping.php
@@ -69,7 +69,11 @@ class Mage_Checkout_Block_Onepage_Shipping extends Mage_Checkout_Block_Onepage_A
     public function getAddress()
     {
         if (is_null($this->_address)) {
-            $this->_address = $this->getNewAddress();
+            if ($this->isCustomerLoggedIn()) {
+                $this->_address = $this->getQuote()->getShippingAddress();
+            } else {
+                $this->_address = Mage::getModel('sales/quote_address');
+            }
         }
 
         return $this->_address;
