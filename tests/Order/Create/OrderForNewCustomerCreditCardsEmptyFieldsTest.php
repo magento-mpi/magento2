0f068176733ea6dc727ca4f09bc57c734e2e95d2
Artem Shugayev <artem.shugayev@magento.com>
2011-07-14 14:39:09 +0000

    [AUTOTEST] updated formatting according to php style

--- tests/Order/Create/OrderForNewCustomerCreditCardsEmptyFieldsTest.php
+++ tests/Order/Create/OrderForNewCustomerCreditCardsEmptyFieldsTest.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Magento
  *
@@ -26,7 +25,6 @@
  * @copyright   Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 /**
  * Creating order for new customer with one required field empty
  *
@@ -36,27 +34,24 @@
  */
 class OrderForNewCustomerCreditCardsEmptyFields_Test extends Mage_Selenium_TestCase
 {
-
-    /**
-     * Preconditions:
-     *
-     * Log in to Backend.
-     * 
-     */    
-    
+   /**
+    * Preconditions:
+    *
+    * Log in to Backend.
+    *
+    */
     public function setUpBeforeTests()
     {
         $this->windowMaximize();
         $this->loginAdminUser();
     }
-    
-    /**
-     * 
-     * Create products for testing.
-     * 
-     * Navigate to Sales-Orders page.
-     * 
-     */    
+   /**
+    *
+    * Create products for testing.
+    *
+    * Navigate to Sales-Orders page.
+    *
+    */
     protected function assertPreConditions()
     {
         $this->orderHelper()->createProducts('product_to_order1');
@@ -66,50 +61,49 @@ class OrderForNewCustomerCreditCardsEmptyFields_Test extends Mage_Selenium_TestC
         $this->addParameter('id', '0');
         $this->addParameter('shipMethod', 'Fixed');
     }
-    
-    /**
-     * Create customer via 'Create order' form (required fields are not filled).
-     * 
-     *
-     * Steps:
-     *
-     * 1.Go to Sales-Orders.
-     *
-     * 2.Press "Create New Order" button.
-     * 
-     * 3.Press "Create New Customer" button.
-     *
-     * 4.Choose 'Main Store' (First from the list of radiobuttons) if exists.
-     *
-     * 5.Fill all fields.
-     * 
-     * 6.Press 'Add Products' button.
-     * 
-     * 7.Add first two products.
-     * 
-     * 8.Choose shipping address the same as billing.
-     * 
-     * 9.Check payment method 'Credit Card - American Express'
-     * 
-     * 10. Fill in all required fields, except one.
-     * 
-     * 11.Choose first from 'Get shipping methods and rates'.
-     * 
-     * 12.Submit order.
-     * 
-     * 13.Invoice order.
-     * 
-     * 14. Ship order.
-     *
-     * Expected result:
-     *
-     * New customer is not created. Order is not created for the new customer. Messages for credit card fieldset appear.
-     *
-     * @dataProvider data_emptyFields
-     *
-     * @param array $emptyField
-     *
-     */    
+   /**
+    * Create customer via 'Create order' form (required fields are not filled).
+    *
+    *
+    * Steps:
+    *
+    * 1.Go to Sales-Orders.
+    *
+    * 2.Press "Create New Order" button.
+    *
+    * 3.Press "Create New Customer" button.
+    *
+    * 4.Choose 'Main Store' (First from the list of radiobuttons) if exists.
+    *
+    * 5.Fill all fields.
+    *
+    * 6.Press 'Add Products' button.
+    *
+    * 7.Add first two products.
+    *
+    * 8.Choose shipping address the same as billing.
+    *
+    * 9.Check payment method 'Credit Card - American Express'
+    *
+    * 10. Fill in all required fields, except one.
+    *
+    * 11.Choose first from 'Get shipping methods and rates'.
+    *
+    * 12.Submit order.
+    *
+    * 13.Invoice order.
+    *
+    * 14. Ship order.
+    *
+    * Expected result:
+    *
+    * New customer is not created. Order is not created for the new customer. Messages for credit card fieldset appear.
+    *
+    * @dataProvider data_emptyFields
+    *
+    * @param array $emptyField
+    *
+    */
     public function testOrderWithEmptyFieldsForCreditCard($emptyField)
     {
         //Data
@@ -134,7 +128,7 @@ class OrderForNewCustomerCreditCardsEmptyFields_Test extends Mage_Selenium_TestC
         $this->clickControl('radiobutton', 'credit_card', FALSE);
         $this->pleaseWait();
         $creditCardData = $this->loadData(
-                        'american_express',                
+                        'american_express',
                         $emptyField
                 );
         $this->fillForm($creditCardData, 'order_payment_method');
@@ -173,20 +167,19 @@ class OrderForNewCustomerCreditCardsEmptyFields_Test extends Mage_Selenium_TestC
                 {
                     $fieldXpath = $fieldSet->findDropdown($key);
                 }
-            }   
+            }
             if ($key == 'expiration_date_year'){
                 if ($fieldSet->findDropdown($key) != Null)
                 {
                     $fieldXpath = $fieldSet->findDropdown($key);
                 }
-            } 
+            }
             if ($key == 'cvv'){
                 if ($fieldSet->findField($key) != Null)
                 {
                     $fieldXpath = $fieldSet->findField($key);
                 }
             }
-
             $this->addParameter('fieldXpath', $fieldXpath);
             switch ($key) {
             case 'name_on_card':
@@ -211,11 +204,8 @@ class OrderForNewCustomerCreditCardsEmptyFields_Test extends Mage_Selenium_TestC
                 $this->assertTrue($this->errorMessage('empty_required_field'), $this->messages);
                 break;
             }
-        }   
-         
-      
+        }
     }
-    
     public function data_emptyFields()
     {
         return array(
@@ -227,5 +217,4 @@ class OrderForNewCustomerCreditCardsEmptyFields_Test extends Mage_Selenium_TestC
             array(array('cvv'   =>  ''))
         );
     }
-    
 }
