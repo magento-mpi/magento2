2b3785017ca77343a1f708d2cb73ebc8042f580e
Vladimir Korotovskih <vladimir.korotovskih@magento.com>
2011-08-03 15:07:16 +0000

    Fixed MAGE-2994: AddToCart takes forever if a configurable has massive count of child products - updated method Mage_Catalog_Model_Product_Type_Configurable::getProductByAttributes

--- app/code/core/Mage/Catalog/Model/Product/Type/Configurable.php
+++ app/code/core/Mage/Catalog/Model/Product/Type/Configurable.php
@@ -487,6 +487,15 @@ class Mage_Catalog_Model_Product_Type_Configurable extends Mage_Catalog_Model_Pr
     public function getProductByAttributes($attributesInfo, $product = null)
     {
         if (is_array($attributesInfo) && !empty($attributesInfo)) {
+            $productCollection = $this->getUsedProductCollection($product);
+            foreach ($attributesInfo as $attributeId => $attributeValue) {
+                $productCollection->addAttributeToFilter($attributeId, $attributeValue);
+            }
+            $productObject = $productCollection->getFirstItem();
+            if ($productObject->getId()) {
+                return $productObject;
+            }
+
             foreach ($this->getUsedProducts(null, $product) as $productObject) {
                 $checkRes = true;
                 foreach ($attributesInfo as $attributeId => $attributeValue) {
