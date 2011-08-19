a02a9179717de9969578004fa985bbe720b64654
Evgeniy Maksimenko <evgeniy.maksimenko@magento.com>
2011-08-19 12:33:50 +0000

    [AUTOTEST] changed test structure for Grouped Product

--- tests/Product/Create/Grouped.php
+++ tests/Product/Create/Grouped.php
@@ -28,13 +28,13 @@
  */
 
 /**
- * @TODO
+ * Grouped product creation tests
  *
  * @package     selenium
  * @subpackage  tests
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Product_Create_Grouped_PhysicalTest extends Mage_Selenium_TestCase
+class Product_Create_Grouped extends Mage_Selenium_TestCase
 {
 
     /**
@@ -86,69 +86,22 @@ class Product_Create_Grouped_PhysicalTest extends Mage_Selenium_TestCase
     /**
      * <p>Creating product with all fields</p>
      * <p>Steps:</p>
-     * <p>1. Click 'Add product' button;</p>
-     * <p>2. Fill in 'Attribute Set' and 'Product Type' fields;</p>
-     * <p>3. Click 'Continue' button;</p>
-     * <p>4. Fill in all fields;</p>
-     * <p>5. Click 'Save' button;</p>
+     * <p>1. Click "Add product" button;</p>
+     * <p>2. Fill in "Attribute Set" and "Product Type" fields;</p>
+     * <p>3. Click "Continue" button;</p>
+     * <p>4. Fill all fields;</p>
+     * <p>5. Click "Save" button;</p>
      * <p>Expected result:</p>
      * <p>Product is created, confirmation message appears;</p>
      *
+     * @depends onlyRequiredFieldsInGrouped
      * @test
      */
-    public function allFieldsForGrouped()
+    public function allFieldsInGrouped()
     {
-
         //Data
-        $productDataSimple = $this->loadData('simple_product_required', null,
-                array('general_name', 'general_sku'));
-        $productDataVirtual = $this->loadData('virtual_product_required', null,
-                array('general_name', 'general_sku'));
-
-        $productData = $this->loadData('grouped_product', null,
-                array('general_name', 'general_sku'));
-
-        //Creating Simple Product
-        $this->productHelper()->createProduct($productDataSimple);
-
-        //Verifying
-        $this->assertTrue($this->successMessage('success_saved_product'), $this->messages);
-        $this->assertTrue($this->checkCurrentPage('manage_products'),
-                'After successful product creation should be redirected to Manage Products page');
-        //Creating Virtual Product
-        $this->productHelper()->createProduct($productDataVirtual, 'virtual');
-        //Verifying
-        $this->assertTrue($this->successMessage('success_saved_product'), $this->messages);
-        $this->assertTrue($this->checkCurrentPage('manage_products'),
-                'After successful product creation should be redirected to Manage Products page');
-        //Creating Grouped Product with phisical Simple
-        $productData['associated_products_grouped_data']['associated_products_grouped_1']
-                ['associated_products_sku'] = $productDataSimple['general_sku'];
-        unset($productData['associated_products_grouped_data']['associated_products_grouped_2']);
-
-        $this->productHelper()->createProduct($productData, 'grouped');
-        //Verifying
-        $this->assertTrue($this->successMessage('success_saved_product'), $this->messages);
-        $this->assertTrue($this->checkCurrentPage('manage_products'),
-                'After successful product creation should be redirected to Manage Products page');
-        //Creating Grouped Product with phisical Simple
-        $productData = $this->loadData('grouped_product', null,
-                array('general_name', 'general_sku'));
-        unset($productData['associated_products_grouped_data']['associated_products_grouped_1']);
-        $productData['associated_products_grouped_data']['associated_products_grouped_2']
-                ['associated_products_sku'] = $productDataVirtual['general_sku'];
-        $this->productHelper()->createProduct($productData, 'grouped');
-        //Verifying
-        $this->assertTrue($this->successMessage('success_saved_product'), $this->messages);
-        $this->assertTrue($this->checkCurrentPage('manage_products'),
-                'After successful product creation should be redirected to Manage Products page');
-        //Creating Grouped Product with phisical Simple
-        $productData = $this->loadData('grouped_product', null,
-                array('general_name', 'general_sku'));
-        $productData['associated_products_grouped_data']['associated_products_grouped_1']
-                ['associated_products_sku'] = $productDataSimple['general_sku'];
-        $productData['associated_products_grouped_data']['associated_products_grouped_2']
-                ['associated_products_sku'] = $productDataVirtual['general_sku'];
+        $productData = $this->loadData('grouped_product', null, array('general_name', 'general_sku'));
+        //Steps
         $this->productHelper()->createProduct($productData, 'grouped');
         //Verifying
         $this->assertTrue($this->successMessage('success_saved_product'), $this->messages);
@@ -159,11 +112,11 @@ class Product_Create_Grouped_PhysicalTest extends Mage_Selenium_TestCase
     /**
      * <p>Creating product with existing SKU</p>
      * <p>Steps:</p>
-     * <p>1. Click 'Add product' button;</p>
-     * <p>2. Fill in 'Attribute Set' and 'Product Type' fields;</p>
-     * <p>3. Click 'Continue' button;</p>
+     * <p>1. Click "Add product" button;</p>
+     * <p>2. Fill in "Attribute Set" and "Product Type" fields;</p>
+     * <p>3. Click "Continue" button;</p>
      * <p>4. Fill in required fields using exist SKU;</p>
-     * <p>5. Click 'Save' button;</p>
+     * <p>5. Click "Save" button;</p>
      * <p>6. Verify error message;</p>
      * <p>Expected result:</p>
      * <p>Error message appears;</p>
@@ -183,32 +136,34 @@ class Product_Create_Grouped_PhysicalTest extends Mage_Selenium_TestCase
     /**
      * <p>Creating product with empty required fields</p>
      * <p>Steps:</p>
-     * <p>1. Click 'Add product' button;</p>
-     * <p>2. Fill in 'Attribute Set' and 'Product Type' fields;</p>
-     * <p>3. Click 'Continue' button;</p>
+     * <p>1. Click "Add product" button;</p>
+     * <p>2. Fill in "Attribute Set" and "Product Type" fields;</p>
+     * <p>3. Click "Continue" button;</p>
      * <p>4. Leave one required field empty and fill in the rest of fields;</p>
-     * <p>5. Click 'Save' button;</p>
+     * <p>5. Click "Save" button;</p>
      * <p>6. Verify error message;</p>
      * <p>7. Repeat scenario for all required fields for both tabs;</p>
      * <p>Expected result:</p>
      * <p>Product is not created, error message appears;</p>
      *
      * @dataProvider dataEmptyField
+     * @depends onlyRequiredFieldsInGrouped
      * @test
      */
     public function emptyRequiredFieldInGrouped($emptyField, $fieldType)
     {
         //Data
-        $field = key($emptyField);
-        if ($field != 'general_sku') {
-            $productData = $this->loadData('grouped_product_required', $emptyField, 'general_sku');
+        if ($emptyField == 'general_visibility') {
+            $productData = $this->loadData('grouped_product_required',
+                    array($emptyField => '-- Please Select --'), 'general_sku');
         } else {
-            $productData = $this->loadData('grouped_product_required', $emptyField);
+            $productData = $this->loadData('grouped_product_required',
+                    array($emptyField => '%noValue%'), 'general_sku');
         }
         //Steps
         $this->productHelper()->createProduct($productData, 'grouped');
         //Verifying
-        $this->addFieldIdToMessage($fieldType, $field);
+        $this->addFieldIdToMessage($fieldType, $emptyField);
         $this->assertTrue($this->validationMessage('empty_required_field'), $this->messages);
         $this->assertTrue($this->verifyMessagesCount(), $this->messages);
     }
@@ -216,26 +171,27 @@ class Product_Create_Grouped_PhysicalTest extends Mage_Selenium_TestCase
     public function dataEmptyField()
     {
         return array(
-            array(array('general_name' => '%noValue%'), 'field'),
-            array(array('general_description' => '%noValue%'), 'field'),
-            array(array('general_short_description' => '%noValue%'), 'field'),
-            array(array('general_sku' => '%noValue%'), 'field'),
-            array(array('general_status' => '%noValue%'), 'dropdown'),
-            array(array('general_visibility' => '-- Please Select --'), 'dropdown'),
+            array('general_name', 'field'),
+            array('general_description', 'field'),
+            array('general_short_description', 'field'),
+            array('general_sku', 'field'),
+            array('general_status', 'dropdown'),
+            array('general_visibility', 'dropdown')
         );
     }
 
     /**
      * <p>Creating product with special characters into required fields</p>
      * <p>Steps</p>
-     * <p>1. Click 'Add Product' button;</p>
-     * <p>2. Fill in 'Attribute Set', 'Product Type' fields;</p>
-     * <p>3. Click 'Continue' button;</p>
-     * <p>4. Fill in required fields with special symbols ('General' tab), rest - with normal data;
-     * <p>5. Click 'Save' button;</p>
+     * <p>1. Click "Add Product" button;</p>
+     * <p>2. Fill in "Attribute Set", "Product Type" fields;</p>
+     * <p>3. Click "Continue" button;</p>
+     * <p>4. Fill in required fields with special symbols ("General" tab), rest - with normal data;
+     * <p>5. Click "Save" button;</p>
      * <p>Expected result:</p>
      * <p>Product created, confirmation message appears</p>
      *
+     * @depends onlyRequiredFieldsInGrouped
      * @test
      */
     public function specialCharactersInRequiredFields()
@@ -243,10 +199,10 @@ class Product_Create_Grouped_PhysicalTest extends Mage_Selenium_TestCase
         //Data
         $productData = $this->loadData('grouped_product_required',
                 array(
-            'general_name' => $this->generate('string', 32, ':punct:'),
-            'general_description' => $this->generate('string', 32, ':punct:'),
-            'general_short_description' => $this->generate('string', 32, ':punct:'),
-            'general_sku' => $this->generate('string', 32, ':punct:')
+                    'general_name'              => $this->generate('string', 32, ':punct:'),
+                    'general_description'       => $this->generate('string', 32, ':punct:'),
+                    'general_short_description' => $this->generate('string', 32, ':punct:'),
+                    'general_sku'               => $this->generate('string', 32, ':punct:')
                 ));
         $productSearch = $this->loadData('product_search',
                 array('product_sku' => $productData['general_sku']));
@@ -265,14 +221,15 @@ class Product_Create_Grouped_PhysicalTest extends Mage_Selenium_TestCase
     /**
      * <p>Creating product with long values from required fields</p>
      * <p>Steps</p>
-     * <p>1. Click 'Add Product' button;</p>
-     * <p>2. Fill in 'Attribute Set', 'Product Type' fields;</p>
-     * <p>3. Click 'Continue' button;</p>
-     * <p>4. Fill in required fields with long values ('General' tab), rest - with normal data;
-     * <p>5. Click 'Save' button;</p>
+     * <p>1. Click "Add Product" button;</p>
+     * <p>2. Fill in "Attribute Set", "Product Type" fields;</p>
+     * <p>3. Click "Continue" button;</p>
+     * <p>4. Fill in required fields with long values ("General" tab), rest - with normal data;
+     * <p>5. Click "Save" button;</p>
      * <p>Expected result:</p>
      * <p>Product created, confirmation message appears</p>
      *
+     * @depends onlyRequiredFieldsInGrouped
      * @test
      */
     public function longValuesInRequiredFields()
@@ -280,10 +237,10 @@ class Product_Create_Grouped_PhysicalTest extends Mage_Selenium_TestCase
         //Data
         $productData = $this->loadData('grouped_product_required',
                 array(
-            'general_name' => $this->generate('string', 255, ':alnum:'),
-            'general_description' => $this->generate('string', 255, ':alnum:'),
-            'general_short_description' => $this->generate('string', 255, ':alnum:'),
-            'general_sku' => $this->generate('string', 64, ':alnum:')
+                    'general_name'              => $this->generate('string', 255, ':alnum:'),
+                    'general_description'       => $this->generate('string', 255, ':alnum:'),
+                    'general_short_description' => $this->generate('string', 255, ':alnum:'),
+                    'general_sku'               => $this->generate('string', 64, ':alnum:')
                 ));
         $productSearch = $this->loadData('product_search',
                 array('product_sku' => $productData['general_sku']));
@@ -297,5 +254,51 @@ class Product_Create_Grouped_PhysicalTest extends Mage_Selenium_TestCase
         $this->productHelper()->openProduct($productSearch);
         //Verifying
         $this->assertTrue($this->verifyForm($productData, 'general'), $this->messages);
+    }
+
+    /**
+     * <p>Creating product with SKU length more than 64 characters.</p>
+     * <p>Steps</p>
+     * <p>1. Click "Add Product" button;</p>
+     * <p>2. Fill in "Attribute Set", "Product Type" fields;</p>
+     * <p>3. Click "Continue" button;</p>
+     * <p>4. Fill in required fields, use for sku string with length more than 64 characters</p>
+     * <p>5. Click "Save" button;</p>
+     * <p>Expected result:</p>
+     * <p>Product is not created, error message appears;</p>
+     *
+     * @depends onlyRequiredFieldsInGrouped
+     * @test
+     */
+    public function incorrectSkuLengthInGrouped()
+    {
+        //Data
+        $productData = $this->loadData('grouped_product_required',
+                array('general_sku' => $this->generate('string', 65, ':alnum:')));
+        //Steps
+        $this->productHelper()->createProduct($productData, 'grouped');
+        //Verifying
+        $this->assertTrue($this->validationMessage('incorrect_sku_length'), $this->messages);
+        $this->assertTrue($this->verifyMessagesCount(), $this->messages);
+    }
+
+    public function groupedWithSimpleProduct()
+    {
+
+    }
+
+    public function groupedWithVirtualProduct()
+    {
+
+    }
+
+    public function groupedWithDownloadableProduct()
+    {
+
+    }
+
+    public function groupedWithAllTypesProduct()
+    {
+
     }
 }
