1e06c28e3e640dba31d0a511b6c30207ef7d7eed
Artem Komarov <artem.komarov@magento.com>
2011-08-23 16:36:45 +0000

    Fixed MAGE-4090: Removed the ability to work with base_url - Reverted changes (issue fixed in MAGE-4128)

--- app/code/core/Mage/Core/Controller/Varien/Router/Admin.php
+++ app/code/core/Mage/Core/Controller/Varien/Router/Admin.php
@@ -95,15 +95,9 @@ class Mage_Core_Controller_Varien_Router_Admin extends Mage_Core_Controller_Vari
      */
     protected function _shouldBeSecure($path)
     {
-        $useSecureInAdminhtml = Mage::getStoreConfigFlag('web/secure/use_in_adminhtml',
-            Mage_Core_Model_App::ADMIN_STORE_ID);
-        $globalSecureBaseUrl = (string)Mage::getConfig()->getNode('default/web/secure/base_url');
-        $storeSecureBaseUrl = (string)Mage::app()->getStore(Mage_Core_Model_App::ADMIN_STORE_ID)
-            ->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK, true);
-
-        return
-            $useSecureInAdminhtml
-            && (substr($storeSecureBaseUrl, 0, 5) === 'https' || substr($globalSecureBaseUrl, 0, 5) === 'https');
+        return substr((string)Mage::getConfig()->getNode('default/web/unsecure/base_url'),0,5)==='https'
+            || Mage::getStoreConfigFlag('web/secure/use_in_adminhtml', Mage_Core_Model_App::ADMIN_STORE_ID)
+            && substr((string)Mage::getConfig()->getNode('default/web/secure/base_url'),0,5)==='https';
     }
 
     /**
@@ -114,11 +108,8 @@ class Mage_Core_Controller_Varien_Router_Admin extends Mage_Core_Controller_Vari
      */
     protected function _getCurrentSecureUrl($request)
     {
-        $path = ltrim($request->getPathInfo(), '/');
-        $storeSecureBaseUrl = Mage::app()->getStore(Mage_Core_Model_App::ADMIN_STORE_ID)
-            ->getBaseUrl('link', true) . $path;
-
-        return $storeSecureBaseUrl || Mage::getConfig()->getNode('web/secure/base_url') . $path;
+        return Mage::app()->getStore(Mage_Core_Model_App::ADMIN_STORE_ID)
+            ->getBaseUrl('link', true) . ltrim($request->getPathInfo(), '/');
     }
 
     /**
