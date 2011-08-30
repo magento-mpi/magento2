6a996f0694a1f1230276e4ac13fa9de143d50d6c
Maxim Gubar <maxim.gubar@magento.com>
2011-08-30 13:30:23 +0000

    Fixed MAGE-4290: Redirect to base url should consider full request uri string

--- app/code/core/Mage/Core/Controller/Varien/Front.php
+++ app/code/core/Mage/Core/Controller/Varien/Front.php
@@ -299,7 +299,14 @@ class Mage_Core_Controller_Varien_Front extends Varien_Object
         if (!Mage::getStoreConfig('web/url/redirect_to_base')) {
             return;
         }
-        if ((string)Mage::getConfig()->getNode(Mage_Adminhtml_Helper_Data::XML_PATH_USE_CUSTOM_ADMIN_URL)) {
+
+        $adminPath = (string)Mage::getConfig()->getNode(Mage_Adminhtml_Helper_Data::XML_PATH_CUSTOM_ADMIN_PATH);
+        if (!$adminPath) {
+            $adminPath = (string)Mage::getConfig()
+                ->getNode(Mage_Adminhtml_Helper_Data::XML_PATH_ADMINHTML_ROUTER_FRONTNAME);
+        }
+        if (preg_match('#^' . $adminPath . '(\/.*)?$#', ltrim($request->getPathInfo(), '/'))
+            && (string)Mage::getConfig()->getNode(Mage_Adminhtml_Helper_Data::XML_PATH_USE_CUSTOM_ADMIN_URL)) {
             return;
         }
 
@@ -309,20 +316,20 @@ class Mage_Core_Controller_Varien_Front extends Varien_Object
             return;
         }
 
+        $redirectUrl  = $baseUrl . ltrim($request->getRequestUri(), '/');
         $redirectCode = 302;
-        if (Mage::getStoreConfig('web/url/redirect_to_base')==301) {
+        if (Mage::getStoreConfig('web/url/redirect_to_base') == 301) {
             $redirectCode = 301;
         }
 
-        $uri = @parse_url($baseUrl);
+        $uri  = @parse_url($redirectUrl);
         $host = isset($uri['host']) ? $uri['host'] : '';
         $path = isset($uri['path']) ? $uri['path'] : '';
 
         $requestUri = $request->getRequestUri() ? $request->getRequestUri() : '/';
-        if ($host && $host != $request->getHttpHost() || $path && strpos($requestUri, $path) === false)
-        {
+        if ($host && $host != $request->getHttpHost() || $path && strpos($requestUri, $path) === false) {
             Mage::app()->getFrontController()->getResponse()
-                ->setRedirect($baseUrl, $redirectCode)
+                ->setRedirect($redirectUrl, $redirectCode)
                 ->sendResponse();
             exit;
         }
