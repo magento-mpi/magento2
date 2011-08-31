810a243a264630af7459391eaec883f9fe4e71fa
Artem Komarov <artem.komarov@magento.com>
2011-08-31 14:32:10 +0000

    Fixed MAGE-2956: Inconsistency in order totals label names - Fixed translate '%s reward points' --> '%s Reward points'

--- app/code/core/Enterprise/Reward/Helper/Data.php
+++ app/code/core/Enterprise/Reward/Helper/Data.php
@@ -228,9 +228,9 @@ class Enterprise_Reward_Helper_Data extends Mage_Core_Helper_Abstract
         $points = sprintf($pointsFormat, $points);
         if ((null !== $amount) && $this->getHasRates()) {
             $amount = sprintf($amountFormat, $this->formatAmount($amount, true, $storeId));
-            return $this->__('%s reward points (%s)', $points, $amount);
+            return $this->__('%s Reward points (%s)', $points, $amount);
         }
-        return $this->__('%s reward points', $points);
+        return $this->__('%s Reward points', $points);
     }
 
     /**
