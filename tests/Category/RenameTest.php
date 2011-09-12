5cbf187383e4da69582610471603b8abc23a6ca3
Artem Shugayev <artem.shugayev@magento.com>
2011-09-12 14:10:41 +0000

    [AUTOTEST] MAUTOSEL-61: Add product to category

--- tests/Category/RenameTest.php
+++ tests/Category/RenameTest.php
@@ -37,27 +37,86 @@ class Category_RenameTest extends Mage_Selenium_TestCase
 {
 
     /**
-     * @TODO
+     * <p>Login to backend</p>
+     */
+    public function setUpBeforeTests()
+    {
+        $this->loginAdminUser();
+    }
+
+    /**
+     * <p>Preconditions:</p>
+     * <p>Navigate to Catalog -> Manage Categories</p>
      */
     protected function assertPreConditions()
     {
-        // @TODO
-    }
-
-
-    /**
-     * @TODO
-     */
-    public function test_RootCategory()
-    {
-        // @TODO
+        $this->navigate('manage_categories');
+        $this->assertTrue($this->checkCurrentPage('manage_categories'), 'Wrong page is opened');
+        $this->addParameter('id', '0');
     }
 
     /**
-     * @TODO
+     * <p>Rename root category.</p>
+     * <p>Steps</p>
+     * <p>1. Click "Add Root Category" button </p>
+     * <p>2. Fill in required fields</p>
+     * <p>3. Click "Save Category" button</p>
+     * <p>4. Open newly created category.</p>
+     * <p>5. Rename category.</p>
+     * <p>Expected Result:</p>
+     * <p>Root Category is renamed.</p>
+     *
+     * @test
      */
-    public function test_SubCategory()
+    public function renameRootCategory()
     {
-        // @TODO
+        $categoryData = $this->loadData('root_category', null, 'name');
+        $this->categoryHelper()->createRootCategory($categoryData);
+        $this->pleaseWait();
+        $this->assertTrue($this->successMessage('success_saved_category'), $this->messages);
+        $this->assertTrue($this->checkCurrentPage('manage_categories'),
+                'After successful product creation should be redirected to Manage Products page');
+        $this->clickButton('reset', false);
+        $this->categoryHelper()->selectCategory($categoryData['name']);
+        $categoryData = $this->loadData('root_category_rename', null, 'name');
+        $this->categoryHelper()->fillCategoryInfo($categoryData);
+        $this->saveForm('save_category');
+        $this->assertTrue($this->successMessage('success_saved_category'), $this->messages);
+    }
+
+    /**
+     * <p>Rename subcategory.</p>
+     * <p>Steps</p>
+     * <p>1. Click "Add Root Category" button </p>
+     * <p>2. Fill in required fields</p>
+     * <p>3. Click "Save Category" button</p>
+     * <p>4. Choose newly created root category</p>
+     * <p>5. Create subcategory for newly created root category.</p>
+     * <p>6. Open newly created subcategory.</p>
+     * <p>7. Rename subcategory.</p>
+     * <p>Expected Result:</p>
+     * <p>Subategory is renamed.</p>
+     *
+     * @test
+     */
+    public function renameSubCategory()
+    {
+        $categoryRoot = $this->loadData('root_category', null, 'name');
+        $this->categoryHelper()->createRootCategory($categoryRoot);
+        $this->assertTrue($this->successMessage('success_saved_category'), $this->messages);
+        $this->assertTrue($this->checkCurrentPage('manage_categories'),
+                'After successful product creation should be redirected to Manage Products page');
+        $this->clickButton('reset', false);
+        $categorySub = $this->loadData('sub_category_required', null, 'name');
+        $this->categoryHelper()->createSubCategory($categoryRoot['name'], $categorySub);
+        $this->pleaseWait();
+        $this->assertTrue($this->successMessage('success_saved_category'), $this->messages);
+        $this->assertTrue($this->checkCurrentPage('manage_categories'),
+                'After successful product creation should be redirected to Manage Products page');
+        $categoryData = $this->loadData('sub_category_required_rename', null, 'name');
+        $this->categoryHelper()->fillCategoryInfo($categoryData);
+        $this->clickButton('save_category', FALSE);
+        $this->pleaseWait();
+        $this->assertTrue($this->successMessage('success_saved_category'), $this->messages);
     }
 }
