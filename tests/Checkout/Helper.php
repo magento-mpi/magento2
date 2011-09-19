d6b5c0fa0b2d863d48138731fe47e3dc63979937
Artem Shugayev <artem.shugayev@magento.com>
2011-09-19 12:32:12 +0000

    [AUTOTEST] Fixed MAUTOSEL-128

--- tests/Checkout/Helper.php
+++ tests/Checkout/Helper.php
@@ -47,7 +47,7 @@ class Checkout_Helper extends Mage_Selenium_TestCase
      * @uses DataGenerator::generate()
      * @see DataGenerator::generate()
      */
-    public function customerAddressGenerator($charsType, $addrType = 'billing', $symNum = 32, $required = FALSE)
+    public function frontCustomerAddressGenerator($charsType, $addrType = 'billing', $symNum = 32, $required = FALSE)
     {
         $type = array(':alnum:', ':alpha:', ':digit:', ':lower:', ':upper:', ':punct:');
         if (array_search($charsType, $type) === FALSE
@@ -57,7 +57,7 @@ class Checkout_Helper extends Mage_Selenium_TestCase
         } else {
             if ($required == TRUE) {
                 return array(
-                    $addrType . '_address_choice'          => 'Add New Address',
+                    $addrType . '_address_select'          => 'New Address',
                     $addrType . '_prefix'                  => '%noValue%',
                     $addrType . '_first_name'              => $this->generate('string', $symNum, $charsType),
                     $addrType . '_middle_name'             => '%noValue%',
@@ -74,7 +74,7 @@ class Checkout_Helper extends Mage_Selenium_TestCase
                 );
             } else {
                 return array(
-                    $addrType . '_address_choice'          => 'Add New Address',
+                    $addrType . '_address_select'          => 'New Address',
                     $addrType . '_prefix'                  => $this->generate('string', $symNum, $charsType),
                     $addrType . '_first_name'              => $this->generate('string', $symNum, $charsType),
                     $addrType . '_middle_name'             => $this->generate('string', $symNum, $charsType),
@@ -93,7 +93,62 @@ class Checkout_Helper extends Mage_Selenium_TestCase
         }
     }
 
+    /**
+     * The way to ship the order
+     *
+     * @param array|string $shippingMethod
+     * @param bool         $validate
+     *
+     */
+    public function frontSelectShippingMethod($shippingMethod, $validate = TRUE)
+    {
+        if (is_string($shippingMethod)) {
+            $shippingMethod = $this->loadData($shippingMethod);
+        }
+        if (array_key_exists('shipping_service', $shippingMethod) &&
+                array_key_exists('shipping_method', $shippingMethod)) {
+            $this->addParameter('shipService', $shippingMethod['shipping_service']);
+            $this->addParameter('shipMethod', $shippingMethod['shipping_method']);
+            if ($this->errorMessage('ship_method_unavailable')) {
+                if ($validate) {
+                    $this->fail('This shipping method is currently unavailable.');
+                }
+            } else {
+                $this->clickControl('radiobutton', 'ship_method', FALSE);
+                if (array_key_exists('add_gift_options', $shippingMethod)) {
+                    $this->frontAddGiftMessage($shippingMethod['add_gift_options']);
+                }
+                $this->clickButton('continue', FALSE);
+                $this->pleaseWait();
+            }
+        }
+    }
 
+    /**
+     * Adding gift message for entire order of each item
+     *
+     * @param array|string $giftOptions
+     *
+     */
+    public function frontAddGiftMessage($giftOptions)
+    {
+        if (is_string($giftOptions)) {
+            $giftOptions = $this->loadData($giftOptions);
+        }
+        if (array_key_exists('entire_order', $giftOptions)) {
+            $this->fillForm($giftOptions['entire_order']);
+        }
+        if (array_key_exists('individual_items', $giftOptions)) {
+            $this->fillForm(array('gift_option_for_individual_items' => 'Yes'));
+            foreach ($giftOptions['individual_items'] as $clue => $dataset) {
+                if (preg_match('/product_name/', $clue)) {
+                    $this->addParameter('productName', $dataset);
+                } else {
+                    $this->fillForm($dataset);
+                }
+            }
+        }
+    }
 
 
 
