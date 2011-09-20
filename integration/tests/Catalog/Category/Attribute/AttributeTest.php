329abf01e743d26cc85a22d512b6af576b783587
Ivan Klymenko <ivan.klymenko@magento.com>
2011-09-20 09:30:22 +0000

    CR Change PAAS-416: Tests - Catalog Category Attribute - refactored attribute data check

--- integration/tests/Catalog/Category/Attribute/AttributeTest.php
+++ integration/tests/Catalog/Category/Attribute/AttributeTest.php
@@ -43,6 +43,9 @@ class Catalog_Category_Attribute_AttributeTest extends Magento_Test_Webservice
     /** @var array */
     protected static $_data;
 
+    /** @var array */
+    protected static $_expectedData;
+
     /**
      * Create new test category attribute with options
      *
@@ -56,6 +59,7 @@ class Catalog_Category_Attribute_AttributeTest extends Magento_Test_Webservice
         self::$_attributeFixture = simplexml_load_file(dirname(__FILE__) . '/_fixtures/attribute.xml');
         self::$_code = (string) self::$_attributeFixture->code;
         self::$_data = self::simpleXmlToArray(self::$_attributeFixture->attributeData);
+        self::$_expectedData = self::simpleXmlToArray(self::$_attributeFixture->expected);
 
         $installer = new Mage_Catalog_Model_Resource_Eav_Mysql4_Setup('core_setup');
 
@@ -93,6 +97,7 @@ class Catalog_Category_Attribute_AttributeTest extends Magento_Test_Webservice
         self::$_attributeFixture = null;
         self::$_data = null;
         self::$_code = null;
+        self::$_expectedData = null;
     }
 
     /**
@@ -109,9 +114,9 @@ class Catalog_Category_Attribute_AttributeTest extends Magento_Test_Webservice
 
         foreach ($attributeList as $attribute) {
             if ($attribute['code'] == self::$_code) {
-                $this->assertEquals('select', $attribute['type']);
-                $this->assertEquals(self::$_data['required'], $attribute['required']);
-                $this->assertEquals('store', $attribute['scope']);
+                foreach (self::$_expectedData as $key => $value ) {
+                    $this->assertEquals($value, $attribute[$key]);
+                }
                 break;
             }
         }
@@ -127,7 +132,7 @@ class Catalog_Category_Attribute_AttributeTest extends Magento_Test_Webservice
         $attributeOptions = $this->call('catalog_category_attribute.options', array(self::$_code));
 
         $this->assertEquals(true, is_array($attributeOptions));
-        $this->assertEquals(true, count($attributeOptions) > 0);
+        $this->assertGreaterThan(0, count($attributeOptions));
 
         $options = array();
         foreach ($attributeOptions as $option) {
