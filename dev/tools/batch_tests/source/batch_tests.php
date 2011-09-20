88377861015e641298916ca8e8d07a9b4e50a2ca
Andrey Tserkus <andrey.tserkus@magento.com>
2011-09-20 18:18:22 +0300

    CR Fixes MAGETWO-463: Batch Tool for Running All Tests Locally - added license notice

--- dev/tools/batch_tests/source/batch_tests.php
+++ dev/tools/batch_tests/source/batch_tests.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     tools
+ * @subpackage  batch_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
 require 'Batch.php';
 $batch = new Batch();
 $batch->run();
