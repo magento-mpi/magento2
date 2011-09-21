0942018a843717e666b15040b124f0d6dfc2d356
Evgeniy Miskov <evgeniy.miskov@magento.com>
2011-09-21 17:04:19 +0000

    [AUTOTEST] CheckoutOnePage Folder Structure updated Created test-cases for OnePageCheckout

---
+++ tests/CheckoutOnePage/WithRegistration/CheckingValidationTest.php
@@ -0,0 +1,395 @@
+<?php
+
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    tests
+ * @package     selenium
+ * @subpackage  tests
+ * @author      Magento Core Team <core@magentocommerce.com>
+ * @copyright   Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * One page Checkout test
+ *
+ * @package     selenium
+ * @subpackage  tests
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CheckoutOnePage_WithRegistration_CheckingValidationTest extends Mage_Selenium_TestCase
+{
+
+    /**
+     *
+     * <p>Creating products for testing.</p>
+     *
+     * <p>Navigate to Sales-Orders page.</p>
+     *
+     */
+    protected function assertPreConditions()
+    {
+        $this->frontend($page);
+        $this->assertTrue($this->checkCurrentPage('home'), $this->messages);
+        $this->addParameter('id', '0');
+    }
+
+    /**
+     * <p>Creating product with required fields only</p>
+     * <p>Steps:</p>
+     * <p>1. Click "Add product" button;</p>
+     * <p>2. Fill in "Attribute Set" and "Product Type" fields;</p>
+     * <p>3. Click "Continue" button;</p>
+     * <p>4. Fill in required fields;</p>
+     * <p>5. Click "Save" button;</p>
+     * <p>Expected result:</p>
+     * <p>Product is created, confirmation message appears;</p>
+     *
+     * @test
+     */
+    public function createSimple()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Checkout with required fields filling</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill in Billing Information tab</p>
+     * <p>4. Select "Ship to this address" option</p>
+     * <p>5. Click 'Continue' button.</p>
+     * <p>6. Select Shipping Method option</p>
+     * <p>7. Click 'Continue' button.</p>
+     * <p>8. Select Payment Method option</p>
+     * <p>9. Click 'Continue' button.</p>
+     * <p>Verify information into "Order Review" tab</p>
+     * <p>Expected result:</p>
+     * <p>Information window appears "Please specify payment method."</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontCheckoutRequiredFields()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Checkout method is not defined</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Leave Checkout Method options empty</p>
+     * <p>2. Click "Continue" button</p>
+     * <p>Expected Result</p>
+     * <p>Information window appears with message "Please choose to register or to checkout as a guest"</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontCheckoutMethodNotDefined()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Product not defined to shopping cart</p>
+     * <p>Steps</p>
+     * <p>1. Do not add product to shopping Cart</p>
+     * <p>2. Click "Checkout" button</p>
+     * <p>Expected Result</p>
+     * <p>Shopping Cart is Empty page appears</p>
+     *
+     * @test
+     */
+    public function frontEmptyShoppingCart()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Empty required fields in billing address tab</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Leave billing information fields empty</p>
+     * <p>4. Click "Continue" button</p>
+     * <p>5. Verify error message;</p>
+     * <p>6. Repeat scenario for all required fields in current tab;</p>
+     * <p>Expected result:</p>
+     * <p>Error message appears</p>
+     *
+     * @dataProvider dataEmptyField
+     * @depends createSimple
+     * @test
+     */
+    public function frontEmptyRequiredFildsInBillingAddress()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Filling required fields by invalid values</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill in 'Email' field by incorrect value.</p>
+     * <p>4. Fill other required fields by regular data.</p>
+     * <p>5. Click 'Continue' button.</p>
+     * <p>Expected result:</p>
+     * <p>Error message appears</p>
+     *
+     * @dataProvider dataEmptyField
+     * @depends createSimple
+     * @test
+     */
+    public function frontBillingAddressInvalidEmail()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Using existing Email Address for fill billing information form</p>
+     * <p>Preconditions</p>
+     * <p>1. Create customer</p>
+     * <p>2. Add product to Shopping Cart</p>
+     * <p>3. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill in 'Email' field by existing email.</p>
+     * <p>4. Fill other required fields by regular data.</p>
+     * <p>5. Click 'Continue' button.</p>
+     * <p>Expected result:</p>
+     * <p>Error message appears</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontBillingAddressExistingEmail()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Using long values for fill billing information form</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill required fields by long values data.</p>
+     * <p>4. Click 'Continue' button.</p>
+     * <p>Expected result:</p>
+     * <p>Customer successfully redirected to the next page, no error masseges appears</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontBillingWithLongValues()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Using incorrect confirmation password</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill required fields by regular data.</p>
+     * <p>4. Enter incorrect confirmation password</p>
+     * <p>4. Click 'Continue' button.</p>
+     * <p>Expected result:</p>
+     * <p>Error message appears - "Please make sure your passwords match."</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontBillingIncorrectConfirmationPassword()
+    {
+        //TODO
+    }
+
+
+    /**
+     * <p>Using special characters for fill billing information form (except email field)</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill required fields by special characters.</p>
+     * <p>4. Click 'Continue' button.</p>
+     * <p>Expected result:</p>
+     * <p>Customer successfully redirected to the next page, no error masseges appears</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontBillingWithSpecialCharactersExceptEmail()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Empty required fields in shipping address tab</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill billing information fields by regular data</p>
+     * <p>4. Click 'Continue' button.</p>
+     * <p>5. Leave shipping information fields empty</p>
+     * <p>6. Click "Continue" button</p>
+     * <p>7. Verify error message;</p>
+     * <p>8. Repeat scenario for all required fields in current tab;</p>
+     * <p>Expected result:</p>
+     * <p>Error message appears</p>
+     *
+     * @dataProvider dataEmptyField
+     * @depends createSimple
+     * @test
+     */
+    public function frontEmptyRequiredFildsInShippingAddress()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Using special characters for fill shipping information form</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill in Billing Information tab</p>
+     * <p>4. Select "Ship to different address" option</p>
+     * <p>5. Click 'Continue' button.</p>
+     * <p>6. Fill in Shipping Information required fields by special characters.</p>
+     * <p>7. Click 'Continue' button.</p>
+     * <p>Expected result:</p>
+     * <p>Customer successfully redirected to the next page, no error masseges appears</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontShippingAddressSpecialCharacters()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Verifying "Use Billing Address" checkbox functionality</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill in Billing Information tab</p>
+     * <p>4. Select "Ship to different address" option</p>
+     * <p>5. Click 'Continue' button.</p>
+     * <p>6. Check "Use Billing Address" checkbox</p>
+     * <p>7. Verify data used for filling form</p>
+     * <p>8. Click 'Continue' button.</p>
+     * <p>Expected result:</p>
+     * <p>Data must be the same as billing address</p>
+     * <p>Customer successfully redirected to the next page, no error massages appears</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontShippingAddressUseBillingAddress()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Shipping method not defined</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill in Billing Information tab</p>
+     * <p>4. Select "Ship to this address" option</p>
+     * <p>5. Click 'Continue' button.</p>
+     * <p>6. Leave Shipping Method options empty</p>
+     * <p>7. Click 'Continue' button.</p>
+     * <p>Expected result:</p>
+     * <p>Information window appears "Please specify shipping method."</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontShippingMethodNotDefined()
+    {
+        //TODO
+    }
+
+    /**
+     * <p>Payment method not defined</p>
+     * <p>Preconditions</p>
+     * <p>1. Add product to Shopping Cart</p>
+     * <p>2. Click "Proceed to Checkout"</p>
+     * <p>Steps</p>
+     * <p>1. Fill in Checkout Method tab</p>
+     * <p>2. Click 'Continue' button.</p>
+     * <p>3. Fill in Billing Information tab</p>
+     * <p>4. Select "Ship to this address" option</p>
+     * <p>5. Click 'Continue' button.</p>
+     * <p>6. Select Shipping Method option</p>
+     * <p>7. Click 'Continue' button.</p>
+     * <p>8. Leave Payment Method options empty</p>
+     * <p>9. Click 'Continue' button.</p>
+     * <p>Expected result:</p>
+     * <p>Information window appears "Please specify payment method."</p>
+     *
+     * @depends createSimple
+     * @test
+     */
+    public function frontPaymentMethodNotDefined()
+    {
+        //TODO
+    }
+}
+
+?>
