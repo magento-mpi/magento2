948ec0b83a0e362b38f9bfda7facb21804531eaf
Anton Voitovych <anton.voitovych@magento.com>
2011-09-22 11:37:29 +0000

    Fixed MAGE-4358: Attribute Set field should have client-side validation and appropriate information message

--- js/prototype/validation.js
+++ js/prototype/validation.js
@@ -412,7 +412,10 @@ Validation.add('IsEmpty', '', function(v) {
 });
 
 Validation.addAllThese([
-    ['validate-select', 'Please select an option.', function(v) {
+    ['validate-no-html-tags', 'HTML tags are not allowed', function(v) {
+				return !/<(\/)?\w+/.test(v);
+			}],
+	['validate-select', 'Please select an option.', function(v) {
                 return ((v != "none") && (v != null) && (v.length != 0));
             }],
     ['required-entry', 'This is a required field.', function(v) {
