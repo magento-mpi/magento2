9c3495bec320057a5f551b53ea9f6af9fc293843
Vladimir Korotovskih <vladimir.korotovskih@magento.com>
2011-09-23 16:55:47 +0300

    Fixed MAGETWO-482: CLONE - Enterprise_CatalogEvent module contains wrong table names - ported changes from MAGETWO-283 - fixed CatalogEvent table name - ported tests for CatalogEvent

---
+++ dev/tests/integration/testsuite/Enterprise/CatalogEvent/Model/EventTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Enterprise_CatalogEvent
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * @group module:Enterprise_CatalogEvent
+ */
+class Enterprise_CatalogEvent_Model_EventTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Enterprise_CatalogEvent_Model_Event
+     */
+    protected $_model;
+
+    protected function setUp()
+    {
+        $this->_model = new Enterprise_CatalogEvent_Model_Event;
+    }
+
+    protected function _getDate($time = 'now')
+    {
+        return date('Y-m-d H:i:s', strtotime($time));
+    }
+
+    public function testCRUD()
+    {
+        if (Magento_Test_Bootstrap::getInstance()->getDbVendorName() == 'mssql') {
+            $this->markTestIncomplete('Bug MAGETWO-294');
+        }
+        $this->_model
+            ->setCategoryId(1)
+            ->setDateStart($this->_getDate('-1 day'))
+            ->setDateEnd($this->_getDate('+1 day'))
+            ->setDisplayState(Enterprise_CatalogEvent_Model_Event::DISPLAY_CATEGORY_PAGE)
+            ->setSortOrder(null)
+        ;
+        $crud = new Magento_Test_Entity($this->_model, array(
+            'category_id'   => 2,
+            'date_start'    => $this->_getDate('-1 year'),
+            'date_end'      => $this->_getDate('+1 month'),
+            'display_state' => Enterprise_CatalogEvent_Model_Event::DISPLAY_PRODUCT_PAGE,
+            'sort_order'    => 123,
+        ));
+        $crud->testCrud();
+    }
+}
