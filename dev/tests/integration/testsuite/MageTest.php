079dbd5a758ccfe944f0968c1c13fe5dddaa210f
Dmitriy Soroka <dmitriy.soroka@magento.com>
2011-09-27 12:35:11 -0700

    MAINLINE-7: added tests for modified code

---
+++ dev/tests/integration/testsuite/MageTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Core
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class MageTest extends PHPUnit_Framework_TestCase
+{
+    public function testIsInstalled()
+    {
+        $this->assertTrue(Mage::isInstalled());
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    public function testReset()
+    {
+        Mage::reset();
+        $this->assertNull(Mage::getRoot());
+        $this->assertTrue(Mage::isInstalled());
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    public function testGetDesign()
+    {
+        $design = Mage::getDesign();
+        $this->assertEquals('frontend', $design->getArea());
+        $this->assertEquals('default/default/default', $design->getAreaParameters());
+        $this->assertSame(Mage::getDesign(), $design);
+    }
+}
