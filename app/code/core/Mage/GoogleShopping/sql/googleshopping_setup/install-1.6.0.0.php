210d62cbeb7d7fe23a695b823daff42169d9ffee
Andrey Konosov <andrey.konosov@magento.com>
2011-09-27 13:05:19 +0000

    Implemented MAGE-4359: Google Shopping New Specification

--- app/code/core/Mage/GoogleShopping/sql/googleshopping_setup/install-1.6.0.0.php
+++ app/code/core/Mage/GoogleShopping/sql/googleshopping_setup/install-1.6.0.0.php
@@ -98,14 +98,8 @@ $table = $connection->newTable($this->getTable('googleshopping/items'))
         'nullable' => false,
         'unsigned' => true
         ), 'Store Id')
-    ->addColumn('published', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(
-        'nullable'  => false,
-        'default' => $installer->getConnection()->getSuggestedZeroDate()
-        ), 'Published date')
-    ->addColumn('expires', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(
-        'nullable'  => false,
-        'default' => $installer->getConnection()->getSuggestedZeroDate()
-        ), 'Expires date')
+    ->addColumn('published', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(), 'Published date')
+    ->addColumn('expires', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(), 'Expires date')
     ->addForeignKey(
         $installer->getFkName(
             'googleshopping/items',
