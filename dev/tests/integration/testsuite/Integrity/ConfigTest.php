d12e0cf276e1639bd5b83fd05e992bd3db5d2d73
Anton Makarenko <anton.makarenko@magento.com>
2011-10-03 11:23:44 -0700

    Added integrity test, that verifies that there is no "fieldset" node in primary configuration

---
+++ dev/tests/integration/testsuite/Integrity/ConfigTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Integrity test for configuration (config.xml)
+ *
+ * {license_notice}
+ *
+ * @category    tests
+ * @package     integration
+ * @subpackage  integrity
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Integrity_ConfigTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * The node 'config/global/fieldsets' had been relocated from config.xml to fieldset.xml
+     *
+     * @param string $path
+     * @dataProvider fieldsetsRemovalDataProvider
+     */
+    public function testFieldsetsRemoval($path)
+    {
+        $this->assertFalse(Mage::getConfig()->getNode($path));
+        $config = new Mage_Core_Model_Config_Fieldset;
+        $this->assertInstanceOf('Mage_Core_Model_Config_Element', $config->getNode($path));
+    }
+
+    public function fieldsetsRemovalDataProvider()
+    {
+        return array(array('global/fieldsets'), array('admin/fieldsets'));
+    }
+}
