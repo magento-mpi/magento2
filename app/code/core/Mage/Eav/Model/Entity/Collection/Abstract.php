de7f5ca9d678d8a07b6184d98ad2d94a75b425cf
Dmitriy Vasilenko <dmitriy.vasilenko@magento.com>
2011-10-04 17:27:31 +0300

    Removed possible usage of removed configuration values

--- app/code/core/Mage/Eav/Model/Entity/Collection/Abstract.php
+++ app/code/core/Mage/Eav/Model/Entity/Collection/Abstract.php
@@ -694,10 +694,6 @@ abstract class Mage_Eav_Model_Entity_Collection_Abstract extends Varien_Data_Col
             );
         }
 
-        // validate table
-        if (strpos($table, '/')!==false) {
-            $table = Mage::getSingleton('core/resource')->getTableName($table);
-        }
         $tableAlias = $this->_getAttributeTableAlias($alias);
 
         // validate bind
@@ -760,10 +756,6 @@ abstract class Mage_Eav_Model_Entity_Collection_Abstract extends Varien_Data_Col
             $tableName = $table;
         }
 
-        // validate table
-        if (strpos($tableName, '/') !== false) {
-            $tableName = Mage::getSingleton('core/resource')->getTableName($tableName);
-        }
         if (empty($tableAlias)) {
             $tableAlias = $tableName;
         }
