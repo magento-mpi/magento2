ab019617335b1eb14e79b573c60fc3e309a03344
Taras Shabatyn <taras.shabatyn@magento.com>
2011-10-04 17:00:34 +0000

    Fixed MAGE-4041: Performance issue with Magento Compiler + APC results in too many I/O calls - now Autolouder works with real directories paths - appropriate changes were applied

--- index.php
+++ index.php
@@ -25,7 +25,12 @@
  */
 
 if (version_compare(phpversion(), '5.2.0', '<')===true) {
-    echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;"><div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;"><h3 style="margin:0; font-size:1.7em; font-weight:normal; text-transform:none; text-align:left; color:#2f2f2f;">Whoops, it looks like you have an invalid PHP version.</h3></div><p>Magento supports PHP 5.2.0 or newer. <a href="http://www.magentocommerce.com/install" target="">Find out</a> how to install</a> Magento using PHP-CGI as a work-around.</p></div>';
+    echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;">
+<div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
+<h3 style="margin:0; font-size:1.7em; font-weight:normal; text-transform:none; text-align:left; color:#2f2f2f;">
+Whoops, it looks like you have an invalid PHP version.</h3></div><p>Magento supports PHP 5.2.0 or newer.
+<a href="http://www.magentocommerce.com/install" target="">Find out</a> how to install</a>
+ Magento using PHP-CGI as a work-around.</p></div>';
     exit;
 }
 
@@ -34,7 +39,8 @@ if (version_compare(phpversion(), '5.2.0', '<')===true) {
  */
 error_reporting(E_ALL | E_STRICT);
 
-$mageFilename = 'app/Mage.php';
+define('MAGENTO_ROOT', getcwd());
+$mageFilename = MAGENTO_ROOT . '/app/Mage.php';
 $maintenanceFile = 'maintenance.flag';
 
 if (!file_exists($mageFilename)) {
