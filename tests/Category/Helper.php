9a736fde16136c7ed6a58152b31d477e35868f6a
Artem Shugayev <artem.shugayev@magento.com>
2011-10-04 13:19:03 +0000

    [AUTOTEST] Fixed MAUTOSEL-137: Moved to Category folder

--- tests/Category/Helper.php
+++ tests/Category/Helper.php
@@ -229,4 +229,94 @@ class Category_Helper extends Mage_Selenium_TestCase
         return false;
     }
 
+    /**
+     * Validates product information in catalog
+     *
+     * @param array|string $productsInfo
+     * @return bool
+     */
+    public function frontValidateProduct($productsInfo)
+    {
+        if (is_string($productsInfo)) {
+            $productsInfo = $this->loadData($productsInfo);
+        }
+        $productsInfo = $this->arrayEmptyClear($productsInfo);
+        $category = (isset($productsInfo['category'])) ? $productsInfo['category'] : NULL;
+        $productName = (isset($productsInfo['product_name'])) ? $productsInfo['product_name'] : NULL;
+        $verificationData = (isset($productsInfo['verification'])) ? $productsInfo['verification'] : NULL;
+        if ($category != NULL && $productName != NULL) {
+            $foundIt = $this->frontSearchPageWithProduct($productName, $category);
+            if (!$foundIt) {
+                $this->fail('Could not find the product');
+            }
+        }
+        $this->frontVerifyPrices($verificationData, $foundIt);
+    }
+
+    /**
+     * Searches the page with the product in the catalog
+     *
+     * @param string $productName
+     * @param string $category
+     * @return mixed
+     */
+    public function frontSearchPageWithProduct($productName, $category)
+    {
+        $this->addParameter('category', $category);
+        $this->getUimapPage('frontend', 'catalog_page')->assignParams($this->_paramsHelper);
+        $this->frontend('catalog_page');
+        $xpathNext = $this->_getControlXpath('link', 'next_page');
+        $this->addParameter('productName', $productName);
+        $xpathProduct = $this->_getControlXpath('link', 'product_name');
+        $i = 1;
+        for (;;) {
+            if ($this->isElementPresent($xpathProduct)) {
+                return $i;
+            } else {
+                if ($this->isElementPresent($xpathNext)) {
+                    $i++;
+                    $this->addParameter('param', '?p=' . $i);
+                    $this->getUimapPage('frontend', 'catalog_page_index')->assignParams($this->_paramsHelper);
+                    $this->navigate('catalog_page_index');
+                } else {
+                    return FALSE;
+                }
+            }
+        }
+    }
+
+    /**
+     * Verifies the correctness of prices in the catalog
+     *
+     * @param array $verificationData
+     * @param int $pageNum
+     */
+    public function frontVerifyPrices(array $verificationData, $pageNum)
+    {
+        $this->addParameter('param', '?p=' . $pageNum);
+        $this->getUimapPage('frontend', 'catalog_page_index')->assignParams($this->_paramsHelper);
+        $page = $this->getCurrentLocationUimapPage();
+        $xpathProduct = $this->_getControlXpath('link', 'product_name');
+        $this->addParameter('productNameXpath', $xpathProduct);
+        $pageelements = get_object_vars($page->getAllPageelements());
+        foreach ($verificationData as $key => $value) {
+            $this->addParameter('price', $value);
+            $xpathPrice = $this->getCurrentLocationUimapPage()->getMainForm()->findPageelement($key);
+            if (!$this->isElementPresent($xpathPrice)) {
+                $this->messages['error'][] = 'Could not find element ' . $key . ' with price ' . $value;
+            }
+            unset($pageelements['ex_' . $key]);
+        }
+        foreach ($pageelements as $key => $value) {
+            if (!preg_match('/^ex_/', $key)) {
+                $value = preg_replace('/\%productNameXpath\%/', $xpathProduct, $value);
+                if ($this->isElementPresent($value)) {
+                    $this->messages['error'][] = 'Element ' . $key . ' is on the page';
+                }
+            }
+        }
+        if (!empty($this->messages['error'])) {
+            $this->fail(implode("\n", $this->messages['error']));
+        }
+    }
 }
