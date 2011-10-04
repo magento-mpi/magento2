d360e85d4a7b1fb47c694a471e87f8b113f516eb
Maxim Gubar <maxim.gubar@magento.com>
2011-10-04 14:26:08 +0000

    Fixed MAGE-4438: Unable to view pictures during product editing  - CR fixes

--- app/code/core/Mage/Catalog/Block/Product/View/Media.php
+++ app/code/core/Mage/Catalog/Block/Product/View/Media.php
@@ -29,12 +29,22 @@
  *
  * @category   Mage
  * @package    Mage_Catalog
- * @author      Magento Core Team <core@magentocommerce.com>
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Catalog_Block_Product_View_Media extends Mage_Catalog_Block_Product_View_Abstract
 {
+    /**
+     * Flag, that defines whether gallery is disabled
+     *
+     * @var boolean
+     */
     protected $_isGalleryDisabled;
 
+    /**
+     * Retrieve list of gallery images
+     *
+     * @return array|Varien_Data_Collection
+     */
     public function getGalleryImages()
     {
         if ($this->_isGalleryDisabled) {
@@ -45,23 +55,28 @@ class Mage_Catalog_Block_Product_View_Media extends Mage_Catalog_Block_Product_V
     }
 
     /**
-     * get gallery url
+     * Retrieve gallery url
      *
-     * @param Varien_Object $image
+     * @param null|Varien_Object $image
      * @return string
      */
-    public function getGalleryUrl($image=null)
+    public function getGalleryUrl($image = null)
     {
-        $params = array('id'=>$this->getProduct()->getId());
+        $params = array('id' => $this->getProduct()->getId());
         if ($image) {
             $params['image'] = $image->getValueId();
-            return $this->getUrl('catalog/product/gallery', $params);
         }
         return $this->getUrl('catalog/product/gallery', $params);
     }
 
+    /**
+     * Disable gallery
+     *
+     * @return Mage_Catalog_Block_Product_View_Media
+     */
     public function disableGallery()
     {
         $this->_isGalleryDisabled = true;
+        return $this;
     }
 }
