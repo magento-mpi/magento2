c35bfb0be8b4587d2af8dfe679f00762fc5b4ab8
Andrey Konosov <andrey.konosov@magento.com>
2011-10-11 10:13:54 +0000

    Fixed MAGE-4573: FPC interacts incorrecty with Currency Switcher

--- app/code/core/Mage/Core/Model/Store.php
+++ app/code/core/Mage/Core/Model/Store.php
@@ -786,9 +786,9 @@ class Mage_Core_Model_Store extends Mage_Core_Model_Abstract
         if (in_array($code, $this->getAvailableCurrencyCodes())) {
             $this->_getSession()->setCurrencyCode($code);
             if ($code == $this->getDefaultCurrency()) {
-                //Mage::app()->getCookie()->delete('currency', $code);
+                Mage::app()->getCookie()->delete('currency', $code);
             } else {
-                //Mage::app()->getCookie()->set('currency', $code);
+                Mage::app()->getCookie()->set('currency', $code);
             }
         }
         return $this;
