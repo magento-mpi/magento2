9fb24158933c4ecc5ae403453f8af326da2567a0
Evgeniy Maksimenko <evgeniy.maksimenko@magento.com>
2011-10-12 17:05:28 +0000

    [AUTOTEST] - fixed admin order creation tests with SavedCC

--- tests/Order/SavedCC/NewCustomerWithSimpleSmokeTest.php
+++ tests/Order/SavedCC/NewCustomerWithSimpleSmokeTest.php
@@ -443,41 +443,6 @@ class Order_SavedCC_NewCustomerWithSimpleSmokeTest extends Mage_Selenium_TestCas
     }
 
     /**
-     * <p>Void order.</p>
-     * <p>Steps:</p>
-     * <p>1.Go to Sales-Orders.</p>
-     * <p>2.Press "Create New Order" button.</p>
-     * <p>3.Press "Create New Customer" button.</p>
-     * <p>4.Choose 'Main Store' (First from the list of radiobuttons) if exists.</p>
-     * <p>5.Fill all fields.</p>
-     * <p>6.Press 'Add Products' button.</p>
-     * <p>7.Add first two products.</p>
-     * <p>8.Choose shipping address the same as billing.</p>
-     * <p>9.Check payment method 'PayPal Direct - Visa'</p>
-     * <p>10. Fill in all required fields.</p>
-     * <p>11.Choose first from 'Get shipping methods and rates'.</p>
-     * <p>12.Submit order.</p>
-     * <p>13.Void Order.</p>
-     * <p>Expected result:</p>
-     * <p>New customer is created. Order is created for the new customer. Void successful</p>
-     *
-     * @depends orderWithout3DSecureSmoke
-     * @test
-     */
-    public function voidPendingOrderFromOrderPage($orderData)
-    {
-        //Steps
-        $this->navigate('manage_sales_orders');
-        $this->orderHelper()->createOrder($orderData);
-        //Verifying
-        $this->assertTrue($this->successMessage('success_created_order'), $this->messages);
-        //Steps
-        $this->clickButtonAndConfirm('void', 'confirmation_to_void');
-        //Verifying
-        $this->assertTrue($this->successMessage('success_voided_order'), $this->messages);
-    }
-
-    /**
      * <p>Create Orders using Saved CC payment method with 3DSecure</p>
      * <p>Steps:</p>
      * <p>1.Go to Sales-Orders.</p>
@@ -517,9 +482,9 @@ class Order_SavedCC_NewCustomerWithSimpleSmokeTest extends Mage_Selenium_TestCas
     public function dataWith3DSecure()
     {
         return array(
-            array('else_jcb_3d', true),
-            array('else_visa', false),
-            array('else_mastercard', false)
+            array('saved_jcb', true),
+            array('saved_visa', false),
+            array('saved_mastercard', false)
         );
     }
 
