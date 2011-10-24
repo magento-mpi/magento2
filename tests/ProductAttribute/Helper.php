a30bde217e8822ba5f00b97e93bcfb20b75e6caf
Evgeniy Maksimenko <evgeniy.maksimenko@magento.com>
2011-10-24 14:46:08 +0000

    [AUTOTEST] fixed helper function openAttribute()

--- tests/ProductAttribute/Helper.php
+++ tests/ProductAttribute/Helper.php
@@ -67,11 +67,11 @@ class ProductAttribute_Helper extends Mage_Selenium_TestCase
         $this->assertNotEquals(null, $xpathTR, 'Attribute is not found');
         $names = $this->shoppingCartHelper()->getColumnNamesAndNumbers('attributes_grid_head', false);
         if (array_key_exists('Attribute Label', $names)) {
-            $text = $this->getText($xpathTR . '//td[' . $names['Attribute Label'] . ']');
+            $text = $this->getText($xpathTR . '//td[' . $names['Attribute Code'] . ']');
             $this->addParameter('attribute_code', $text);
         }
         $this->addParameter('id', $this->defineIdFromTitle($xpathTR));
-        $this->click($xpathTR . "//a[text()='Edit']");
+        $this->click($xpathTR . '//td[' . $names['Attribute Code'] . ']');
         $this->waitForPageToLoad($this->_browserTimeoutPeriod);
         $this->validatePage($this->_findCurrentPageFromUrl($this->getLocation()));
     }
