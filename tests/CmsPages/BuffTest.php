f9339e03b4a6b92dd8399975b8c4326e816f5f4c
Artem Shugayev <artem.shugayev@magento.com>
2011-10-26 17:17:04 +0000

    [AUTOTEST] Fixed MAUTOSEL-152

---
+++ tests/CmsPages/BuffTest.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    tests
+ * @package     selenium
+ * @subpackage  tests
+ * @author      Magento Core Team <core@magentocommerce.com>
+ * @copyright   Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Delete Widget Test
+ *
+ * @package     selenium
+ * @subpackage  tests
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CmsPages_BuffTest extends Mage_Selenium_TestCase
+{
+    protected static $products = array();
+
+    public function setUpBeforeTests()
+    {
+        $this->loginAdminUser();
+    }
+
+    protected function assertPreconditions()
+    {
+        $this->addParameter('id', '0');
+    }
+
+    /**
+     * <p>Preconditions</p>
+     * <p>Creates Category to use during tests</p>
+     *
+     * @test
+     */
+    public function createCategory()
+    {
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->checkCategoriesPage();
+        $rootCat = 'Default Category';
+        $categoryData = $this->loadData('sub_category_required', null, 'name');
+        $this->categoryHelper()->createSubCategory($rootCat, $categoryData);
+        $this->assertTrue($this->successMessage('success_saved_category'), $this->messages);
+        $this->categoryHelper()->checkCategoriesPage();
+
+        return $rootCat . '/' . $categoryData['name'];
+    }
+
+    /**
+     * <p>Preconditions</p>
+     * <p>Creates Attribute (dropdown) to use during tests</p>
+     *
+     * @test
+     */
+    public function createAttribute()
+    {
+        $attrData = $this->loadData('product_attribute_dropdown_with_options', NULL,
+                array('admin_title', 'attribute_code'));
+        $associatedAttributes = $this->loadData('associated_attributes',
+                array('General' => $attrData['attribute_code']));
+        $this->navigate('manage_attributes');
+        $this->productAttributeHelper()->createAttribute($attrData);
+        $this->assertTrue($this->successMessage('success_saved_attribute'), $this->messages);
+        $this->navigate('manage_attribute_sets');
+        $this->attributeSetHelper()->openAttributeSet();
+        $this->attributeSetHelper()->addAttributeToSet($associatedAttributes);
+        $this->saveForm('save_attribute_set');
+        $this->assertTrue($this->successMessage('success_attribute_set_saved'), $this->messages);
+
+        return $attrData;
+    }
+
+    /**
+     * Create required products for testing
+     *
+     * @dataProvider dataProductTypes
+     * @depends createCategory
+     * @depends createAttribute
+     *
+     * @test
+     */
+    public function createProducts($dataProductType, $category, $attrData)
+    {
+        $this->navigate('manage_products');
+        //Data
+        if ($dataProductType == 'configurable') {
+            $productData = $this->loadData($dataProductType . '_product_required',
+                array('configurable_attribute_title' => $attrData['admin_title'],
+                    'categories' => $category), array('general_sku', 'general_name'));
+        } else {
+            $productData = $this->loadData($dataProductType . '_product_required',
+                array('categories' => $category), array('general_name', 'general_sku'));
+        }
+        //Steps
+        $this->productHelper()->createProduct($productData, $dataProductType);
+        //Verifying
+        $this->assertTrue($this->successMessage('success_saved_product'), $this->messages);
+        $this->assertTrue($this->checkCurrentPage('manage_products'), $this->messages);
+        self::$products['sku'][$dataProductType] = $productData['general_sku'];
+        self::$products['name'][$dataProductType] = $productData['general_name'];
+    }
+
+    public function dataProductTypes()
+    {
+        return array(
+            array('simple')
+        );
+    }
+
+    /**
+     * @depends createCategory
+     * @test
+     */
+    public function some($category)
+    {
+        $this->navigate('manage_cms_pages');
+        $temp = array();
+        $temp['filter_sku'] = self::$products['sku']['simple'];
+        $temp['category_path'] = $category;
+        $nodes = explode('/', $category);
+        $pageData = $this->loadData('new_page', $temp, array('page_title', 'url_key'));
+        $pageData['content']['widgets']['widget_3']['chosen_option']['title'] = $nodes[1];
+        $pageData['content']['widgets']['widget_5']['chosen_option']['title'] = $nodes[1] .
+                ' / ' . self::$products['name']['simple'];
+        $this->cmsPagesHelper()->createPage($pageData);
+        $this->cmsPagesHelper()->frontValidatePage($pageData);
+    }
+}
---
+++ tests/CmsPages/Helper.php
@@ -0,0 +1,299 @@
+<?php
+
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    tests
+ * @package     selenium
+ * @subpackage  tests
+ * @author      Magento Core Team <core@magentocommerce.com>
+ * @copyright   Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Helper class
+ *
+ * @package     selenium
+ * @subpackage  tests
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CmsPages_Helper extends Mage_Selenium_TestCase
+{
+
+    /**
+     * Creates page
+     *
+     * @param string|array $pageData
+     */
+    public function createPage($pageData)
+    {
+        if (is_string($pageData)) {
+            $pageData = $this->loadData($pageData);
+        }
+        $pageData = $this->arrayEmptyClear($pageData);
+        $pageInfo = (isset($pageData['page_information'])) ? $pageData['page_information'] : NULL;
+        $content = (isset($pageData['content'])) ? $pageData['content'] : NULL;
+        $design = (isset($pageData['design'])) ? $pageData['design'] : NULL;
+        $metaData = (isset($pageData['meta_data'])) ? $pageData['meta_data'] : NULL;
+        $this->clickButton('add_new_page');
+        if ($pageInfo) {
+            $this->clickControl('tab', 'page_information', FALSE);
+            $this->fillPageInfo($pageInfo);
+        }
+        if ($content) {
+            $this->fillContent($content);
+        }
+        if ($design) {
+            $this->clickControl('tab', 'design', FALSE);
+            $this->fillForm($design);
+        }
+        if ($metaData) {
+            $this->clickControl('tab', 'meta_data', FALSE);
+            $this->fillForm($widgetOptions);
+        }
+        $this->saveForm('save_page');
+    }
+
+    /**
+     * Fills Page Information tab
+     *
+     * @param array $pageInfo
+     * @param bool $validate
+     */
+    public function fillPageInfo(array $pageInfo, $validate = FALSE)
+    {
+        $xpath = $this->_getControlXpath('multiselect', 'store_view');
+        if ($this->isElementPresent($xpath) && $validate == FALSE) {
+            if (!array_key_exists('store_view', $pageInfo)) {
+                $pageInfo['store_view'] = 'All Store Views';
+            }
+        } elseif (!$this->isElementPresent($xpath) && $validate == FALSE) {
+            if (array_key_exists('store_view', $pageInfo)) {
+                unset($pageInfo['store_view']);
+            }
+        }
+        $this->fillForm($pageInfo);
+    }
+
+    /**
+     * Fills Content tab
+     *
+     * @param string|array $content
+     */
+    public function fillContent($content)
+    {
+        if (is_string($content)) {
+            $content = $this->loadData($content);
+        }
+        $content = $this->arrayEmptyClear($content);
+        $this->clickControl('tab', 'content', FALSE);
+        $this->fillForm($content);
+        if (array_key_exists('widgets', $content)) {
+            $this->insertWidget($content['widgets']);
+        }
+        if (array_key_exists('variable', $content)) {
+            $this->insertVariable($content['variable']);
+        }
+    }
+
+    /**
+     * Inserts widgets
+     *
+     * @param array $widgets
+     */
+    public function insertWidget(array $widgets)
+    {
+        $editor = $this->_getControlXpath('field', 'editor');
+        if (!$this->isElementPresent($editor)) {
+            $this->clickButton('show_hide_editor', FALSE);
+        }
+        foreach ($widgets as $key => $value) {
+            $this->clickButton('insert_widget', FALSE);
+            $this->waitForAjax();
+            $this->fillForm($value);
+            if (array_key_exists('chosen_option', $value)) {
+                $this->clickButton('select_option', FALSE);
+                $this->pleaseWait();
+                $options = $value['chosen_option'];
+                switch ($value['widget_type']) {
+                    case 'CMS Page Link':
+                    case 'CMS Static Block':
+                        $searchData = array();
+                        foreach ($options as $key => $value) {
+                            if (preg_match('/filter/' , $key)) {
+                                $searchData[$key] = $value;
+                            }
+                        }
+                        $this->searchAndOpen($searchData, FALSE);
+                        $this->checkChosenOption($options['title']);
+                        break;
+                    case 'Catalog Category Link':
+                        if (array_key_exists('category_path', $options)) {
+                            $this->categoryHelper()->selectCategory($options['category_path']);
+                        }
+                        $this->checkChosenOption($options['title']);
+                        break;
+                    case 'Catalog Product Link':
+                        if (array_key_exists('category_path', $options)) {
+                            $this->categoryHelper()->selectCategory($options['category_path']);
+                        }
+                        $searchData = array();
+                        foreach ($options as $key => $value) {
+                            if (preg_match('/filter/' , $key)) {
+                                $searchData[$key] = $value;
+                            }
+                        }
+                        $this->searchAndOpen($searchData, FALSE);
+                        $this->checkChosenOption($options['title']);
+                        break;
+                }
+            }
+            $this->clickButton('submit_widget_insert', FALSE);
+            $this->waitForAjax();
+        }
+    }
+
+    /**
+     * Checks the chosen option while inserting widget
+     *
+     * @param string $option
+     */
+    public function checkChosenOption($option)
+    {
+        $this->addParameter('elementName', $option);
+        $xpathOption = $this->_getControlXpath('pageelement', 'chosen_option');
+        if (!$this->isElementPresent($xpathOption)) {
+            $this->fail('The element ' . $option . ' was not selected');
+        }
+    }
+
+    /**
+     * Inserts variable
+     *
+     * @param string $variable
+     */
+    public function insertVariable($variable)
+    {
+        $editor = $this->_getControlXpath('field', 'editor');
+        if (!$this->isElementPresent($editor)) {
+            $this->clickButton('show_hide_editor', FALSE);
+        }
+        $this->clickButton('insert_variable', FALSE);
+        $this->waitForAjax();
+        $this->addParameter('variableName', $variable);
+        $this->clickControl('link', 'variable', FALSE);
+    }
+
+    /**
+     * Validates page after creation
+     *
+     * @param array $pageData
+     */
+    public function frontValidatePage($pageData)
+    {
+        $this->logoutCustomer();
+        $this->addParameter('url_key', $pageData['page_information']['url_key']);
+        $this->addParameter('page_title', $pageData['page_information']['page_title']);
+        $this->addParameter('content_heading', $pageData['content']['content_heading']);
+        $this->frontend('test_page');
+        $this->validatePage();
+        foreach ($this->countElements($pageData) as $key => $value) {
+            $xpath = $this->_getControlXpath('pageelement', $key);
+            $this->assertTrue($this->getXpathCount($xpath) == $value);
+        }
+    }
+
+    /**
+     * Count elements for validation
+     *
+     * @param array $pageData
+     * @return array
+     */
+    public function countElements(array $pageData)
+    {
+        $map = array(
+            'CMS Page Link' => 'widget_cms_link',
+            'CMS Static Block' => 'widget_static_block',
+            'Catalog Category Link' => 'widget_category_link',
+            'Catalog Product Link' => 'widget_product_link'
+        );
+        $resultArray = array();
+        foreach ($map as $key => $value) {
+            $resultArray[$value] = count($this->searchArray($pageData, $key));
+        }
+        return $resultArray;
+    }
+
+    /**
+     * Search array recursively
+     *
+     * @param array $pageData
+     * @param string $key
+     * @return array
+     */
+    function searchArray($pageData, $key = NULL){
+
+        $found = ($key !== null ? array_keys($pageData, $key) : array_keys($pageData));
+        foreach($pageData as $value){
+            if(is_array($value)){
+                $found = ($key !== null ? array_merge($found, $this->searchArray($value, $key))
+                        : array_merge($found, $this->searchArray($value)));
+            }
+        }
+        return $found ;
+    }
+
+    /**
+     * Opens page
+     *
+     * @param array $searchPage
+     */
+    public function openPage(array $searchPage)
+    {
+        $this->_prepareDataForSearch($searchPage);
+        $xpathTR = $this->search($searchPage, 'cms_pages_grid');
+        $this->assertNotEquals(NULL, $xpathTR, 'Page is not found');
+        $names = $this->shoppingCartHelper()->getColumnNamesAndNumbers('page_grid_head', FALSE);
+        if (array_key_exists('Title', $names)) {
+            $text = $this->getText($xpathTR . '//td[' . $names['Title'] . ']');
+            $this->addParameter('pageName', $text);
+        }
+        $this->addParameter('id', $this->defineIdFromTitle($xpathTR));
+        $this->click($xpathTR);
+        $this->waitForPageToLoad($this->_browserTimeoutPeriod);
+        $this->validatePage();
+    }
+
+    /**
+     * Deletes page
+     *
+     * @param array $searchPage
+     */
+    public function deletePage(array $searchPage)
+    {
+        $searchPage = $this->arrayEmptyClear($searchPage);
+        if (!empty($searchPage)) {
+            $this->openWidget($searchPage);
+            $this->answerOnNextPrompt('OK');
+            $this->clickButton('delete_page');
+            $this->assertTrue($this->checkMessage('successfully_deleted_page'), 'The page has not been deleted');
+        }
+    }
+}
