4a365013981054bd09583d823b1dbef4295eb205
Dmitriy Soroka <dmitriy.soroka@magento.com>
2011-10-28 17:36:05 -0700

    MAGETWO-545: fixed code standard

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -24,7 +24,7 @@
  */
 
 /* PHP version validation */
-if (version_compare(phpversion(), '5.3.0', '<')===true) {
+if (version_compare(phpversion(), '5.3.0', '<') === true) {
     echo <<<HTML
 <div style="font:12px/1.35em arial, helvetica, sans-serif;">
     <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
