f9e1ca907eb065e53bac7a31a2ea5e458d56abb2
Evgeniy Maksimenko <evgeniy.maksimenko@magento.com>
2011-11-01 16:38:22 +0000

    [AUTOTEST] Change name for test file

--- tests/ProductAttribute/Create/CreateFromProductPageTest.php
+++ tests/ProductAttribute/Create/CreateFromProductPageTest.php
@@ -34,7 +34,7 @@
  * @subpackage  tests
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ProductAttribute_Create_CreateFromProductPage extends Mage_Selenium_TestCase
+class ProductAttribute_Create_CreateFromProductPageTest extends Mage_Selenium_TestCase
 {
 
     /**
