a7ad318ec63ad8c0dda45605e3bbe35c3953b23f
Dmitriy Soroka <dmitriy.soroka@magento.com>
2011-11-02 15:59:25 -0700

    MAGETWO-569: fixed identified performance leak

--- app/code/core/Mage/Core/Model/Design.php
+++ app/code/core/Mage/Core/Model/Design.php
@@ -58,7 +58,7 @@ class Mage_Core_Model_Design extends Mage_Core_Model_Abstract
     public function loadChange($storeId, $date = null)
     {
         if (is_null($date)) {
-            $date = Mage::getSingleton('core/date')->date(Varien_Date::DATE_PHP_FORMAT);
+            $date = Varien_Date::formatDate(Mage::app()->getLocale()->storeTimeStamp($storeId), false);
         }
 
         $changeCacheId = 'design_change_' . md5($storeId . $date);
