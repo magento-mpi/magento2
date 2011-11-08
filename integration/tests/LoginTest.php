6be92b0b85e17099d4328fdcae692ad6c0148e03
Andrey Voinkov <andrey.voinkov@magento.com>
2011-11-08 13:36:56 +0000

    Implemented APIA-2: SQL injection using WebServices Client Session Timeout parameter [AFA]

--- integration/tests/LoginTest.php
+++ integration/tests/LoginTest.php
@@ -30,17 +30,50 @@
  */
 class LoginTest extends Magento_Test_Webservice
 {
-    public function testLogin()
+    /**
+     * Test API login
+     *
+     * @return void
+     */
+    public function _testLogin()
     {
         /** @var $client Magento_Test_Webservice_SoapV1|Magento_Test_Webservice_SoapV2|Magento_Test_Webservice_XmlRpc */
         $client = $this->getWebService();
         $this->assertTrue($client->hasSession());
     }
 
-    public function testLoginDirect()
+    /**
+     * Test API login through SoapClient (soap v1.1)
+     *
+     * @return void
+     */
+    public function _testLoginDirect()
     {
-        $client = new SoapClient(TESTS_WEBSERVICE_URL.'/api/soap/?wsdl=1', array('trace'=>true));
+        $client = new SoapClient(TESTS_WEBSERVICE_URL.'/api/soap/?wsdl=1', array('trace'=>true, 'exceptions'=>false));
         $sessionId = $client->login(TESTS_WEBSERVICE_USER, TESTS_WEBSERVICE_APIKEY);
         $this->assertNotEmpty($sessionId);
+    }
+
+    /**
+     * Test that Client Session Timeout param was properly quoted
+     *
+     * @magentoConfigFixture current_store api/config/session_timeout 3600)); DROP TABLE bbb;
+     */
+    public function testLoginCleanOldSessionsTimeoutSqlInjection()
+    {
+        $user = $this->getMock('Mage_Api_Model_User', array('getId'));
+        $user->expects($this->any())->method('getId')->will($this->returnValue('fake_user'));
+
+        $userResource = $this->getMock('Mage_Api_Model_Mysql4_User', array('getTable'));
+        $userResource->expects($this->any())->method('getTable')->will($this->returnValue('fake_table'));
+        try {
+            $userResource->cleanOldSessions($user);
+        } catch (Exception $e) {
+            $trace = $e->getTrace();
+            $query = $trace[3]['args'][0];
+
+            $this->assertStringMatchesFormat('%s3600%s', $query);
+            $this->assertStringNotMatchesFormat('%sDROP TABLE bbb%s', $query);
+        }
     }
 }
