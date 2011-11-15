e4486bb1dd4916a0b4ba5648058300fc0b28594b
Artem Shugayev <ashugayev@ebay.com>
2011-11-15 12:52:16 +0200

    [AUTOTEST] Removed navigation to index management and cache management pages from ReviewTest

--- tests/Product/ReviewTest.php
+++ tests/Product/ReviewTest.php
@@ -70,9 +70,7 @@ class Product_ReviewTest extends Mage_Selenium_TestCase
         $this->productHelper()->createProduct($productData, $productType);
         $this->assertTrue($this->successMessage('success_saved_product'), $this->messages);
         $this->assertTrue($this->checkCurrentPage('manage_products'), $this->messages);
-        $this->navigate('index_management');
         $this->reindexInvalidedData();
-        $this->navigate('cache_storage_management');
         $this->clearInvalidedCache();
         $this->productHelper()->frontVerifyProductInfo($productData);
     }
