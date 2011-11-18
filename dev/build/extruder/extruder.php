4531762eb63366e0e7e415d12f0f817b06f8d83c
Sergey Shymko <sergey.shymko@magento.com>
2011-11-18 11:50:25 -0800

    Implemented MAGETWO-597: License Notices Replacement into Placeholders

--- dev/build/extruder/extruder.php
+++ dev/build/extruder/extruder.php
@@ -1,22 +1,12 @@
 #!/usr/bin/php
 <?php
 /**
- * Command line tool for cleaning up Magento source code files.
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
+ * {license_notice}
  *
  * @category   build
  * @package    extruder
- * @copyright  Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright  {copyright}
+ * @license    {license_link}
  */
 
 require dirname(__FILE__) . '/Routine.php';
