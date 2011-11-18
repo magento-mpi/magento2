2b4197a1d09273a5bffc56eed4f5741dd1e1aa13
Andrey Bugaychuk <andrey.bugaychuk@magento.com>
2011-11-18 11:56:33 +0000

    Fixed APIA-12: Check sample code (test written)

--- integration/tests/Checkout/CartTest.php
+++ integration/tests/Checkout/CartTest.php
@@ -102,24 +102,82 @@ class Checkout_CartTest extends Magento_Test_Webservice
     /**
      * Test for product add to shopping cart
      *
-     *
      * @return void
      */
     public function testProductAddToCart()
     {
-        if (TESTS_WEBSERVICE_TYPE == Magento_Test_Webservice::TYPE_SOAPV1) {
-            //
-        } elseif (TESTS_WEBSERVICE_TYPE == Magento_Test_Webservice::TYPE_SOAPV2) {
-            $soapResult = $this->getWebService()->call('cart_product.add', array(
-                'quoteId'  => $this->_quote->getId(),
-                'products' => array(
-                    array('sku' => $this->_product->getSku(), 'qty' => 1)
-                )
-            ));
+        $soapResult = $this->getWebService()->call('cart_product.add', array(
+            'quoteId'  => $this->_quote->getId(),
+            'products' => array(
+                array('sku' => $this->_product->getSku(), 'qty' => 1)
+            )
+        ));
 
-            $this->assertTrue($soapResult, 'Error during product add to cart via SOAP API call');
-        } elseif (TESTS_WEBSERVICE_TYPE == Magento_Test_Webservice::TYPE_XMLRPC) {
-            //
+        $this->assertTrue($soapResult, 'Error during product add to cart via API call');
+    }
+
+    /**
+     * Test for product with custom options add to shopping cart
+     *
+     * @return void
+     */
+    public function testProductWithCustomOptionsAddToCart()
+    {
+        // Create custom option for product
+        $customOptionId    = null;
+        $customOptionTitle = 'My Text ' . uniqid();
+        $customOptionValue = 'Hello WORLD';
+
+        $this->_product->setCanSaveCustomOptions(true);
+        $this->_product->setProductOptions(array(
+            array('type' => 'field', 'title' => $customOptionTitle, 'is_require' => 0, 'max_characters' => 0)
+        ));
+        $this->_product->save();
+
+        // Find ID of created custom option for future use
+        /** @var $productOption Mage_Catalog_Model_Product_Option */
+        $productOption = Mage::getModel('catalog/product_option');
+
+        foreach ($productOption->getProductOptionCollection($this->_product) as $option) {
+            if ($option['default_title'] == $customOptionTitle) {
+                $customOptionId = $option['option_id'];
+                break;
+            }
         }
+        if (null === $customOptionId) {
+            $this->fail('Can not find custom option ID that been created');
+        }
+
+        // Add product with custom option to cart via API
+        if (TESTS_WEBSERVICE_TYPE == Magento_Test_Webservice::TYPE_SOAPV2) { // use associative array for SOAP v2
+            $customOptionsData = array(array('key' => $customOptionId, 'value' => $customOptionValue));
+        } else { // use numeric array otherwise
+            $customOptionsData = array($customOptionId => $customOptionValue);
+        }
+        $soapResult = $this->getWebService()->call('cart_product.add', array(
+            'quoteId'  => $this->_quote->getId(),
+            'products' => array(
+                array('sku' => $this->_product->getSku(), 'qty' => 1, 'options' => $customOptionsData)
+            )
+        ));
+
+        $this->assertTrue($soapResult, 'Error during product with custom options add to cart via API call');
+
+        /** @var $quoteItemOption Mage_Sales_Model_Resource_Quote_Item_Option_Collection */
+        $quoteItemOption = Mage::getResourceModel('sales/quote_item_option_collection');
+        $itemOptionValue = null;
+
+        foreach ($quoteItemOption->getOptionsByProduct($this->_product) as $row) {
+            if ('option_' . $customOptionId == $row['code']) {
+                $itemOptionValue = $row['value'];
+                break;
+            }
+        }
+        if (null === $itemOptionValue) {
+            $this->fail('Custom option value not found in DB after API call');
+        }
+        $this->assertEquals(
+            $customOptionValue, $itemOptionValue, 'Custom option value in DB does not match value passed by API'
+        );
     }
 }
