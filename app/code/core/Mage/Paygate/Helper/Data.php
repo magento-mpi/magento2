6e8d0a1effad40acb9d375387429b4d36613b8a7
Olga Kopylova <olga.kopylova@magento.com>
2011-11-25 10:08:19 +0000

    CR Changes MAGE-4370: Unable to cancel an order for an expired Authorize.net auth

--- app/code/core/Mage/Paygate/Helper/Data.php
+++ app/code/core/Mage/Paygate/Helper/Data.php
@@ -51,9 +51,9 @@ class Mage_Paygate_Helper_Data extends Mage_Core_Helper_Abstract
      * @param string $exception
      * @return bool|string
      */
-    public function getTransactionMessage(
-        $payment, $requestType, $lastTransactionId, $card, $amount = false, $exception = false)
-    {
+    public function getTransactionMessage($payment, $requestType, $lastTransactionId, $card, $amount = false,
+        $exception = false
+    ) {
         return $this->getExtendedTransactionMessage(
             $payment, $requestType, $lastTransactionId, $card, $amount, $exception
         );
@@ -72,8 +72,8 @@ class Mage_Paygate_Helper_Data extends Mage_Core_Helper_Abstract
      * @return bool|string
      */
     public function getExtendedTransactionMessage($payment, $requestType, $lastTransactionId, $card, $amount = false,
-                                          $exception = false, $additionalMessage = false)
-    {
+        $exception = false, $additionalMessage = false
+    ) {
         $operation = $this->_getOperation($requestType);
 
         if (!$operation) {
