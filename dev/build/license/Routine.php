b7c63497c6a2b7cc301b12b0fceefc8f463509c7
Vladimir Korotovskih <vladimir.korotovskih@magento.com>
2011-11-28 16:08:07 +0200

    Fixed MAGETWO-596: Extruder and License Tools Refinement - remove debug comments - update license header for tool

--- dev/build/license/Routine.php
+++ dev/build/license/Routine.php
@@ -1,24 +1,15 @@
 <?php
 /**
- * Service routines for license-tool command line script
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
+ * {license_notice}
  *
  * @category   build
  * @package    license
- * @copyright  Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright  {copyright}
+ * @license    {license_link}
  */
 
 /**
+ * Service routines for license-tool command line script
  * Routine run time functions
  *
  */
@@ -202,13 +193,13 @@ class Routine
         self::globSearch($directories, $fileMasks, $foundFiles, $recursive);
 
         foreach ($foundFiles as $filename) {
-            // self::printLog($filename . '...');
+            self::printLog($filename . '...');
             $contents = file_get_contents($filename);
 
             preg_match('#/\*\*(.*)\*/.*#Us', $contents, $matches);
 
             if (!isset($matches[1])) {
-                self::printLog($filename . '...'); self::printLog("Failed!\n");
+                self::printLog("Failed!\n");
                 self::$_isFailedUpdate = true;
                 continue;
             }
@@ -233,9 +224,9 @@ class Routine
                 if (!self::$dryRun) {
                     file_put_contents($filename, $newContents);
                 }
-                //self::printLog("Ok\n");
+                self::printLog("Ok\n");
             } else {
-                self::printLog($filename . '...'); self::printLog("Failed!\n");
+                self::printLog("Failed!\n");
                 self::$_isFailedUpdate = true;
             }
         }
