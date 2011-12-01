a8e92defb0dcbbd71bc9fe3c7f19c041a8451661
Andrey Bugaychuk <andrey.bugaychuk@magento.com>
2011-12-01 17:35:33 +0000

    Fixed APIA-77: API sales_order.cancel and sales_order.hold with incorrect return on Magento Go (fixed 'cancel' call)

--- app/code/core/Mage/Sales/Model/Order/Api.php
+++ app/code/core/Mage/Sales/Model/Order/Api.php
@@ -254,13 +254,18 @@ class Mage_Sales_Model_Order_Api extends Mage_Sales_Model_Api_Resource
     {
         $order = $this->_initOrder($orderIncrementId);
 
+        if (Mage_Sales_Model_Order::STATE_CANCELED == $order->getState()) {
+            $this->_fault('status_not_changed');
+        }
         try {
             $order->cancel();
             $order->save();
         } catch (Mage_Core_Exception $e) {
             $this->_fault('status_not_changed', $e->getMessage());
         }
-
+        if (Mage_Sales_Model_Order::STATE_CANCELED != $order->getState()) {
+            $this->_fault('status_not_changed');
+        }
         return true;
     }
 
