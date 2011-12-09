e96c869936b9793eda350d0458c67dc6c678d7c4
Olga Sizova <olga.sizova@magento.com>
2011-12-09 10:26:21 +0000

    Fixed MAGE-5187: Code improvement for Mage_Adminhtml_Tax_RateController::_importRates()

--- app/code/core/Mage/Adminhtml/controllers/Tax/RateController.php
+++ app/code/core/Mage/Adminhtml/controllers/Tax/RateController.php
@@ -302,7 +302,9 @@ class Mage_Adminhtml_Tax_RateController extends Mage_Adminhtml_Controller_Action
         $stores = array();
         $unset = array();
         $storeCollection = Mage::getModel('core/store')->getCollection()->setLoadDefault(false);
-        for ($i = count($csvFields); $i < count($csvData[0]); $i++) {
+        $cvsFieldsNum = count($csvFields);
+        $cvsDataNum   = count($csvData[0]);
+        for ($i = $cvsFieldsNum; $i < $cvsDataNum; $i++) {
             $header = $csvData[0][$i];
             $found = false;
             foreach ($storeCollection as $store) {
