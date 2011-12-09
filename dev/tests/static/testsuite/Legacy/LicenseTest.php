388ad6bcfa3db94bea344154de13274ae8f82b8b
Roman Goncharuk <roman.goncharuk@magento.com>
2011-12-09 21:11:26 +0200

    MAGETWO-630 Integrity_LicenseTest in Legacy Static Test Suite - Moved test into static/testsuite/Legacy - Implemented logic for testing specified path

---
+++ dev/tests/static/testsuite/Legacy/LicenseTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    tests
+ * @package     static
+ * @subpackage  Legacy
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Legacy_LicenseTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider legacyCommentDataProvider
+     */
+    public function testLegacyComment($filename)
+    {
+        $fileText = file_get_contents($filename);
+        if (!preg_match_all('#/\*\*.+@copyright.+?\*/#s', $fileText, $matches)) {
+            return;
+        }
+
+        foreach ($matches[0] as $commentText) {
+            foreach (array('Irubin Consulting Inc', 'DBA Varien', 'Magento Inc') as $legacyText) {
+                $this->assertNotContains(
+                    $legacyText,
+                    $commentText,
+                    'The license of file contains legacy text.'
+                );
+            }
+        }
+    }
+
+    public function legacyCommentDataProvider()
+    {
+        $recursiveIterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(
+            Integrity_ClassesTest::getMagentoRoot(), FilesystemIterator::SKIP_DOTS | FilesystemIterator::UNIX_PATHS
+        ));
+
+        $rootFolderName = substr(strrchr(Integrity_ClassesTest::getMagentoRoot(), DIRECTORY_SEPARATOR), 1);
+        $extensions = '(xml|css|php|phtml|js|dist|sample|additional)';
+        $paths =  array(
+            $rootFolderName . '/[^/]+\.' . $extensions,
+            $rootFolderName . '/app/.+\.' . $extensions,
+            $rootFolderName . '/dev/(?!tests/integration/tmp).+\.' . $extensions,
+            $rootFolderName . '/downloader/.+\.' . $extensions,
+            $rootFolderName . '/lib/(Mage|Magento|Varien)/.+\.' . $extensions,
+            $rootFolderName . '/pub/.+\.' . $extensions,
+        );
+        $regexIterator = new RegexIterator($recursiveIterator, '#(' . implode(' | ', $paths) . ')$#x');
+
+        $result = array();
+        foreach ($regexIterator as $fileInfo) {
+            $filename = (string)$fileInfo;
+            if (!file_exists($filename) || !is_readable($filename)) {
+                continue;
+            }
+            $result[] = array($filename);
+        }
+        return $result;
+    }
+}
