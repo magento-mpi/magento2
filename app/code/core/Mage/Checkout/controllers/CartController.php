e901ee1a808196bf6ad6de2939029cb0313bc434
Anton Kril <anton.kril@magento.com>
2011-12-12 16:45:38 +0000

    Fixed MAGE-4689: Message in the shopping cart contains no text when order amount in is not enough for minimum amount.  Added default values for error messages

--- app/code/core/Mage/Checkout/controllers/CartController.php
+++ app/code/core/Mage/Checkout/controllers/CartController.php
@@ -124,7 +124,14 @@ class Mage_Checkout_CartController extends Mage_Core_Controller_Front_Action
             $cart->save();
 
             if (!$this->_getQuote()->validateMinimumAmount()) {
-                $warning = Mage::getStoreConfig('sales/minimum_order/description');
+                $warning = Mage::getStoreConfig('sales/minimum_order/description') ?
+                    Mage::getStoreConfig('sales/minimum_order/description') :
+                    Mage::helper('checkout')->__('Minimum order amount is %s',
+                        Mage::app()->getLocale()->currency(
+                            Mage::app()->getStore($this->_getQuote()->getStoreId())->getCurrentCurrencyCode())
+                            ->toCurrency(Mage::getStoreConfig('sales/minimum_order/amount',
+                                $this->_getQuote()->getStoreId())));
+
                 $cart->getCheckoutSession()->addNotice($warning);
             }
         }
