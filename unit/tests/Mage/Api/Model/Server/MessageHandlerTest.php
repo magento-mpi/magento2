13d6f11a7acb6e29144da742ac8738249c1e10e0
Andrey Roslik <andrey.roslik@magento.com>
2011-12-14 15:56:29 +0000

    Implemented APIA-97: Write test for the help class MessageHandler Small fix

--- unit/tests/Mage/Api/Model/Server/MessageHandlerTest.php
+++ unit/tests/Mage/Api/Model/Server/MessageHandlerTest.php
@@ -24,7 +24,7 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
  /**
- * Test model admin api role controller
+ * MessageHandler model test
  *
  * @category    Mage
  * @package     Mage_Api
