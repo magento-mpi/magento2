d64d20848dafe3698379dfa2c438c307777cf55b
Andrey Bugaychuk <andrey.bugaychuk@magento.com>
2011-12-14 14:23:48 +0000

    Implemented APIA-78: Product Images upload creates invalid files (test finished)

--- integration/tests/Catalog/Product/ImageTest.php
+++ integration/tests/Catalog/Product/ImageTest.php
@@ -35,34 +35,174 @@
 class Catalog_Product_ImageTest extends Magento_Test_Webservice
 {
     /**
-     * Tests image for product creation
-     *
-     * @return void
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
      */
-    public function testCreateInvalidImage()
+    protected function setUp()
     {
-        $this->markTestIncomplete();
+        $product = new Mage_Catalog_Model_Product;
 
-        $validJpg      = dirname(__FILE__) . '/_fixtures/files/images/test.jpg.jpg';
-        $bmpWithJpgExt = dirname(__FILE__) . '/_fixtures/files/images/test.bmp.jpg';
-        $phpWithJpgExt = dirname(__FILE__) . '/_fixtures/files/images/test.php.jpg';
-        $pngWithJpgExt = dirname(__FILE__) . '/_fixtures/files/images/test.png.jpg';
+        $product->setData(array(
+            'sku'               => 'simple' . uniqid(),
+            'attribute_set_id'  => 4,
+            'type_id'           => Mage_Catalog_Model_Product_Type::TYPE_SIMPLE,
+            'name'              => 'Simple Product',
+            'website_ids'       => array(Mage::app()->getStore()->getWebsiteId()),
+            'description'       => '...',
+            'short_description' => '...',
+            'price'             => 0.99,
+            'tax_class_id'      => 2,
+            'visibility'        => Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
+            'status'            => Mage_Catalog_Model_Product_Status::STATUS_ENABLED
+        ));
 
-        $fileData = array(
+        $product->save();
+
+        $this->setFixture('product', $product);
+
+        $this->setFixture('requestData', array(
             'label'    => 'My Product Image',
             'position' => 2,
             'types'    => array('small_image', 'image', 'thumbnail'),
-            'exclude'  => 0,
+            'exclude'  => 1,
             'remove'   => 0,
             'file'     => array(
                 'name'    => 'my_image_file',
-                'content' => base64_encode(file_get_contents($validJpg)),
+                'content' => null,
                 'mime'    => 'image/jpeg'
             )
+        ));
+
+        parent::setUp();
+    }
+
+    /**
+     * Tears down the fixture, for example, close a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        $this->deleteFixture('product', true);
+        $this->deleteFixture('requestData');
+
+        parent::tearDown();
+    }
+
+    /**
+     * Tests valid image for product creation
+     *
+     * @dataProvider validImageProvider
+     * @param strign $validImgPath Absolute path to valid image file
+     * @return void
+     */
+    public function testCreateValidImage($validImgPath)
+    {
+        /** @var $product Mage_Catalog_Model_Product */
+        $product = $this->getFixture('product');
+        $requestData = $this->getFixture('requestData');
+
+        // valid JPG image file
+        $requestData['file']['content'] = base64_encode(file_get_contents($validImgPath));
+
+        $imagePath = $this->getWebService()->call(
+            'product_attribute_media.create', array($product->getSku(), $requestData)
+        );
+        $this->assertInternalType('string', $imagePath, 'String type of response expected but not received');
+
+        // reload product to reflect changes done by API request
+        $product->load($product->getId());
+
+        // retrieve saved image
+        $attributes  = $product->getTypeInstance(true)->getSetAttributes($product);
+        $imageParams = $attributes['media_gallery']->getBackend()->getImage($product, $imagePath);
+
+        $this->assertInternalType('array', $imageParams, 'Image not found');
+        $this->assertEquals($requestData['label'], $imageParams['label'], 'Label does not match');
+        $this->assertEquals($requestData['position'], $imageParams['position'], 'Position does not match');
+        $this->assertEquals($requestData['exclude'], $imageParams['disabled'], 'Disabled does not match');
+    }
+
+    /**
+     * Tests not an image for product creation
+     *
+     * @return void
+     */
+    public function testCreateNotAnImage()
+    {
+        /** @var $product Mage_Catalog_Model_Product */
+        $product = $this->getFixture('product');
+        $requestData = $this->getFixture('requestData');
+
+        // TXT file
+        $requestData['file']['content'] = base64_encode(
+            file_get_contents(dirname(__FILE__) . '/_fixtures/files/test.txt')
         );
 
-        $result = $this->getWebService()->call('product_attribute_media.create', array('simple1', $fileData));
+        try {
+            $this->getWebService()->call('product_attribute_media.create', array($product->getSku(), $requestData));
+        } catch (Exception $e) {
+            $this->assertEquals('Unsupported image format.', $e->getMessage(), 'Invalid exception message');
+        }
+        // reload product to reflect changes done by API request
+        $product->load($product->getId());
 
-        $this->assertInternalType('string', $result, 'String type response expected but not received');
+        $mediaData = $product->getData('media_gallery');
+
+        $this->assertCount(0, $mediaData['images'], 'Invalid image file has been saved');
+    }
+
+    /**
+     * Tests an invalid image for product creation
+     *
+     * @dataProvider invalidImageProvider
+     * @param strign $invalidImgPath Absolute path to invalid image file
+     * @return void
+     */
+    public function testCreateInvalidImage($invalidImgPath)
+    {
+        /** @var $product Mage_Catalog_Model_Product */
+        $product = $this->getFixture('product');
+        $requestData = $this->getFixture('requestData');
+
+        // Not an image file with JPG extension
+        $requestData['file']['content'] = base64_encode(file_get_contents($invalidImgPath));
+
+        try {
+            $this->getWebService()->call('product_attribute_media.create', array($product->getSku(), $requestData));
+        } catch (Exception $e) {
+            $this->assertEquals('Unsupported image format.', $e->getMessage(), 'Invalid exception message');
+        }
+        // reload product to reflect changes done by API request
+        $product->load($product->getId());
+
+        $mediaData = $product->getData('media_gallery');
+
+        $this->assertCount(0, $mediaData['images'], 'Invalid image file has been saved');
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function invalidImageProvider()
+    {
+        return array(
+            array(dirname(__FILE__) . '/_fixtures/files/images/test.bmp.jpg'),
+            array(dirname(__FILE__) . '/_fixtures/files/images/test.php.jpg')
+        );
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function validImageProvider()
+    {
+        return array(
+            array(dirname(__FILE__) . '/_fixtures/files/images/test.jpg.jpg'),
+            array(dirname(__FILE__) . '/_fixtures/files/images/test.png.jpg')
+        );
     }
 }
