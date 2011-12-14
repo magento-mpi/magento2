fabc3c1d89bab01444887539cce59d0d6b8aa94d
Anton Makarenko <anton.makarenko@magento.com>
2011-12-14 15:59:23 -0800

    MAINLINE-24 Contribution of filesystem structure update - fixed incorrect associative keys in the Legacy_FilesystemTest data provider

--- dev/tests/static/testsuite/Legacy/FilesystemTest.php
+++ dev/tests/static/testsuite/Legacy/FilesystemTest.php
@@ -26,12 +26,12 @@ class Legacy_FilesystemTest extends PHPUnit_Framework_TestCase
     public function relocationsDataProvider()
     {
         return array(
-            array('Relocated to pub/errors' => 'errors'),
-            array('Eliminated with Mage_Compiler' => 'includes'),
-            array('Relocated to pub/js' => 'js'),
-            array('Relocated to pub/media' => 'media'),
-            array('Eliminated as not needed' => 'pkginfo'),
-            array('Dissolved into themes under app/design ' => 'skin'),
+            'Relocated to pub/errors'                => array('errors'),
+            'Eliminated with Mage_Compiler'          => array('includes'),
+            'Relocated to pub/js'                    => array('js'),
+            'Relocated to pub/media'                 => array('media'),
+            'Eliminated as not needed'               => array('pkginfo'),
+            'Dissolved into themes under app/design' => array('skin'),
         );
     }
 }
