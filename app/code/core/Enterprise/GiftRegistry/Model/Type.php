bbeb10ae115a95dc39fcd7b747fb8adf3f5af7d9
Anton Kril <anton.kril@magento.com>
2011-12-21 11:52:50 +0000

    Fixed MAGE-4661: Gift Registry: Adding an attribute type of 'Static type' twice returns JS error - Also fixed attribute processing bugs in backend

--- app/code/core/Enterprise/GiftRegistry/Model/Type.php
+++ app/code/core/Enterprise/GiftRegistry/Model/Type.php
@@ -57,9 +57,9 @@ class Enterprise_GiftRegistry_Model_Type extends Mage_Core_Model_Abstract
     protected function _beforeSave()
     {
         if (!$this->hasStoreId() && !$this->getStoreId()) {
+            $this->_cleanupData();
             $xmlModel = Mage::getModel('enterprise_giftregistry/attribute_processor');
             $this->setMetaXml($xmlModel->processData($this));
-            $this->_cleanupData();
         }
 
         parent::_beforeSave();
@@ -188,14 +188,16 @@ class Enterprise_GiftRegistry_Model_Type extends Mage_Core_Model_Abstract
                             $optionsToSave = array();
                             foreach ($attribute['options'] as $option) {
                                 if ($option['is_deleted']) {
-                                  $this->_getResource()->deleteAttributeStoreData($this->getId(), $attribute['code'], $option['code']);
+                                  $this->_getResource()->deleteAttributeStoreData(
+                                      $this->getId(), $attribute['code'], $option['code']
+                                  );
                                 } else {
                                     $optionsToSave[] = $option;
                                 }
                             }
                             $attribute['options'] = $optionsToSave;
                         }
-                        $attributesToSave[$group] = $attribute;
+                        $attributesToSave[$group][] = $attribute;
                     }
                 }
                 $this->setAttributes($attributesToSave);
