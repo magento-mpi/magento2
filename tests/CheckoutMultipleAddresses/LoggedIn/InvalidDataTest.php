90ac60cb033e5a85a025f2c8831db2107e344556
Maksym Aposov <maposov@ebay.com>
2011-12-21 17:06:04 +0200

    [TESTS] Fix class name for InvalidDataTest tests

--- tests/CheckoutMultipleAddresses/LoggedIn/InvalidDataTest.php
+++ tests/CheckoutMultipleAddresses/LoggedIn/InvalidDataTest.php
@@ -34,7 +34,7 @@
  * @subpackage  tests
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class CheckoutMultipleAddresses_Existing_PaymentMethodsTest extends Mage_Selenium_TestCase
+class CheckoutMultipleAddresses_LoggedIn_InvalidDataTest extends Mage_Selenium_TestCase
 {
 
     /**
