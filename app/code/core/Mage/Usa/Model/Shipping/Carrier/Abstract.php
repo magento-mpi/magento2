1cacf9395311f4a688935f70468727c8c0047204
Yaroslav Voronoy <yaroslav.voronoy@magento.com>
2011-12-30 19:20:55 +0000

    CR Change MAGE-4339: "Maximum Package Weight" option works incorrect in case with decimal Qty in shopping Cart

--- app/code/core/Mage/Usa/Model/Shipping/Carrier/Abstract.php
+++ app/code/core/Mage/Usa/Model/Shipping/Carrier/Abstract.php
@@ -159,7 +159,7 @@ abstract class Mage_Usa_Model_Shipping_Carrier_Abstract extends Mage_Shipping_Mo
 
         foreach ($this->getAllItems($request) as $item) {
             if ($item->getProduct() && $item->getProduct()->getId()) {
-                if ($item->getProduct()->getWeight() * $item->getProduct()->getQty() > $maxAllowedWeight) {
+                if ($item->getProduct()->getWeight() * $item->getQty() > $maxAllowedWeight) {
                     $errorMsg = ($configErrorMsg) ? $configErrorMsg : $defaultErrorMsg;
                     break;
                 }
