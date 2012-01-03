d069118e01b6be63f8ee45d7d768ed40830e3b27
Artem Komarov <artem.komarov@magento.com>
2012-01-03 13:38:55 +0000

    Fixed MAGE-5410: HTTP 500 Internal Server Error on Admin Forgot Password page - Added forgotten method to retrieve session

--- app/code/core/Mage/Captcha/Model/Observer.php
+++ app/code/core/Mage/Captcha/Model/Observer.php
@@ -175,6 +175,16 @@ class Mage_Captcha_Model_Observer
     }
 
     /**
+     * Returns backend session
+     *
+     * @return Mage_Adminhtml_Model_Session
+     */
+    protected function _getBackendSession()
+    {
+        return Mage::getSingleton('adminhtml/session');
+    }
+
+    /**
      * Check Captcha On User Login Backend Page
      *
      * @param Varien_Event_Observer $observer
@@ -191,9 +201,9 @@ class Mage_Captcha_Model_Observer
         if (!empty($email) && !empty($params)){
             if ($captchaModel->isRequired()){
                 if (!$captchaModel->isCorrect($this->_getCaptchaString($controller->getRequest(), $formId))) {
-                    $this->_getSession()->setEmail((string) $controller->getRequest()->getPost('email'));
+                    $this->_getBackendSession()->setEmail((string) $controller->getRequest()->getPost('email'));
                     $controller->setFlag('', Mage_Core_Controller_Varien_Action::FLAG_NO_DISPATCH, true);
-                    $this->_getSession()->addError(Mage::helper('captcha')->__('Incorrect CAPTCHA.'));
+                    $this->_getBackendSession()->addError(Mage::helper('captcha')->__('Incorrect CAPTCHA.'));
                     $controller->getResponse()->setRedirect(Mage::getUrl('*/*/forgotpassword'));
                 }
             }
