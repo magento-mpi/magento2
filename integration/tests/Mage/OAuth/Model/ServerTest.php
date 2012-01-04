f4056814af731004c616f05a173e7f08998385e8
Igor Sidorenko <igor.sidorenko@magento.com>
2012-01-04 19:03:43 +0000

    Implemented APIA-115: Oauth - As QA, I want to run integration tests for oAuth compliance - Added new test cases

--- integration/tests/Mage/OAuth/Model/ServerTest.php
+++ integration/tests/Mage/OAuth/Model/ServerTest.php
@@ -32,6 +32,11 @@
 class Mage_OAuth_Model_ServerTest extends Magento_TestCase
 {
     /**
+     * Message of message of raised exception fail
+     */
+    const RAISED_EXCEPTION_FAIL_MESSAGE = 'An expected Zend_Oauth_Exception has not been raised.';
+
+    /**
      * Sets up the fixture, for example, open a network connection.
      * This method is called before a test is executed.
      *
@@ -89,16 +94,21 @@ class Mage_OAuth_Model_ServerTest extends Magento_TestCase
     {
         /** @var $client Zend_Oauth_Consumer */
         $client = $this->getFixture('client');
+
         /** @var $requestToken Zend_Oauth_Token_Request */
         $requestToken = $client->getRequestToken();
+        $this->assertTrue($requestToken->isValid());
 
         /** @var $token Mage_OAuth_Model_Token */
         $token = Mage::getModel('oauth/token');
         $token->load($requestToken->getParam('oauth_token'), 'token');
 
+        $this->setFixture('token', $token);
+
         $this->assertGreaterThan(0, $token->getId());
         $this->assertNull($token->getCustomerId());
         $this->assertNull($token->getAdminId());
+        $this->assertEmpty($token->getVerifier());
         $this->assertEquals($requestToken->getParam('oauth_token_secret'), $token->getSecret());
         $this->assertEquals(Mage_OAuth_Model_Token::TYPE_REQUEST, $token->getType());
         $this->assertEquals(0, $token->getAuthorized());
@@ -108,5 +118,137 @@ class Mage_OAuth_Model_ServerTest extends Magento_TestCase
         $consumer = $this->getFixture('consumer');
         $this->assertEquals($consumer->getCallbackUrl(), $token->getCallbackUrl());
         $this->assertEquals($consumer->getId(), $token->getConsumerId());
+    }
+
+    /**
+     * Test response code
+     *
+     * @return void
+     */
+    public function testSuccessResponseCode()
+    {
+        /** @var $client Zend_Oauth_Consumer */
+        $client = $this->getFixture('client');
+
+        /** @var $requestToken Zend_Oauth_Token_Request */
+        $requestToken = $client->getRequestToken();
+        $this->assertEquals(Mage_OAuth_Model_Server::HTTP_OK, $requestToken->getResponse()->getStatus());
+    }
+
+    /**
+     * Test count of response params
+     *
+     * @return void
+     */
+    public function testResponseParams()
+    {
+        /** @var $client Zend_Oauth_Consumer */
+        $client = $this->getFixture('client');
+
+        /** @var $requestToken Zend_Oauth_Token_Request */
+        $requestToken = $client->getRequestToken();
+        $body = $requestToken->getResponse()->getBody();
+
+        $params = explode('&', $body);
+        $this->assertEquals(3, count($params));
+
+        $params = explode('=', $body);
+        $this->assertEquals(4, count($params));
+
+        // oauth_callback_confirmed MUST be present and set to "true"
+        $this->assertEquals('true', $requestToken->getParam('oauth_callback_confirmed'));
+    }
+
+    /**
+     * Test request with a wrong signature method
+     *
+     * @return void
+     */
+    public function testWrongSignatureMethod()
+    {
+        /** @var $client Zend_Oauth_Consumer */
+        $client = $this->getFixture('client');
+
+        try {
+            $client->getRequestToken(array('oauth_signature_method' => 'qwerty'));
+        } catch (Zend_Oauth_Exception $e) {
+            /** @var $lastResponse Zend_Http_Response */
+            $lastResponse = $client->getHttpClient()->getLastResponse();
+            $this->assertEquals(Mage_OAuth_Model_Server::HTTP_BAD_REQUEST, $lastResponse->getStatus());
+            $this->assertEquals('oauth_problem=signature_method_rejected', $lastResponse->getBody());
+            return;
+        }
+
+        $this->fail(self::RAISED_EXCEPTION_FAIL_MESSAGE);
+    }
+
+    /**
+     * Test request with a wrong request method
+     *
+     * @return void
+     */
+    public function testWrongRequestMethod()
+    {
+        /** @var $client Zend_Oauth_Consumer */
+        $client = $this->getFixture('client');
+
+        try {
+            $client->getRequestToken(null, Zend_Oauth::GET);
+        } catch (Zend_Oauth_Exception $e) {
+            /** @var $lastResponse Zend_Http_Response */
+            $lastResponse = $client->getHttpClient()->getLastResponse();
+            $this->assertEquals(Mage_OAuth_Model_Server::HTTP_UNAUTHORIZED, $lastResponse->getStatus());
+            $this->assertEquals('oauth_problem=nonce_used', $lastResponse->getBody());
+            return;
+        }
+
+        $this->fail(self::RAISED_EXCEPTION_FAIL_MESSAGE);
+    }
+
+    /**
+     * Test request with a wrong OAuth version
+     *
+     * @return void
+     */
+    public function testWrongOauthVersion()
+    {
+        /** @var $client Zend_Oauth_Consumer */
+        $client = $this->getFixture('client');
+
+        try {
+            $client->getRequestToken(array('oauth_version' => '2.0'));
+        } catch (Zend_Oauth_Exception $e) {
+            /** @var $lastResponse Zend_Http_Response */
+            $lastResponse = $client->getHttpClient()->getLastResponse();
+            $this->assertEquals(Mage_OAuth_Model_Server::HTTP_BAD_REQUEST, $lastResponse->getStatus());
+            $this->assertEquals('oauth_problem=version_rejected', $lastResponse->getBody());
+            return;
+        }
+
+        $this->fail(self::RAISED_EXCEPTION_FAIL_MESSAGE);
+    }
+
+    /**
+     * Test request with an absent param
+     *
+     * @return void
+     */
+    public function testAbsentParam()
+    {
+        /** @var $client Zend_Oauth_Consumer */
+        $client = $this->getFixture('client');
+
+        try {
+            $client->getRequestToken(array('oauth_consumer_key' => ''));
+        } catch (Zend_Oauth_Exception $e) {
+            /** @var $lastResponse Zend_Http_Response */
+            $lastResponse = $client->getHttpClient()->getLastResponse();
+            $this->assertEquals(Mage_OAuth_Model_Server::HTTP_BAD_REQUEST, $lastResponse->getStatus());
+            $this->assertEquals('oauth_problem=parameter_absent&oauth_parameters_absent=oauth_consumer_key',
+                $lastResponse->getBody());
+            return;
+        }
+
+        $this->fail(self::RAISED_EXCEPTION_FAIL_MESSAGE);
     }
 }
