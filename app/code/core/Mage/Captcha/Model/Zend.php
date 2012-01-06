4d27df42b4bc95a4b7432b43758916d1a4fef443
Dmytro Vynograd <dmytro.vynograd@magento.com>
2012-01-06 14:59:43 +0000

    CR Changes MAGE-5220: CAPTCHA is not shown at Login form, if selected "After number of attempts to login" into "Displaying Mode" settings.

--- app/code/core/Mage/Captcha/Model/Zend.php
+++ app/code/core/Mage/Captcha/Model/Zend.php
@@ -304,7 +304,7 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
     {
         if ($this->_isEnabled() && in_array($this->_formId, $this->_getTargetForms())) {
             Mage::getResourceModel('captcha/log')->logAttempt($login);
-            if ($this->_isOverLimitLoginAttempts($login)){
+            if ($this->_isOverLimitLoginAttempts($login)) {
                 $this->getSession()->setData($this->_getFormIdKey('show_captcha'), 1);
             }
         }
