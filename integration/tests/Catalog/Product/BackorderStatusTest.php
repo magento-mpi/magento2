3d58e5c5c0d73d466b65d691dd164f7d0480f176
Andrey Voinkov <andrey.voinkov@magento.com>
2012-01-06 11:54:15 +0000

    Implemented APIA-155: Product backorder status and "Use Config Backorders" not being updated by API

---
+++ integration/tests/Catalog/Product/BackorderStatusTest.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Magento
+ * @package     Mage_Core
+ * @subpackage  integration_tests
+ * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magento.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Test updating product back-order status through API
+ *
+ * @category    Magento
+ * @package     Magento_Test
+ * @author      Magento Api Team <api-team@magento.com>
+ */
+class Catalog_Product_BackorderStatusTest extends Magento_Test_Webservice
+{
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $product = new Mage_Catalog_Model_Product;
+
+        $product->setData(array(
+            'sku'               => 'bo01-' . uniqid(),
+            'attribute_set_id'  => 4,
+            'type_id'           => Mage_Catalog_Model_Product_Type::TYPE_SIMPLE,
+            'name'              => 'Simple Product',
+            'website_ids'       => array(Mage::app()->getStore()->getWebsiteId()),
+            'description'       => '...',
+            'short_description' => '...',
+            'price'             => 0.99,
+            'tax_class_id'      => 2,
+            'visibility'        => Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
+            'status'            => Mage_Catalog_Model_Product_Status::STATUS_ENABLED
+        ));
+
+        $product->save();
+
+        $this->setFixture('product', $product);
+
+
+        parent::setUp();
+    }
+
+    /**
+     * Tears down the fixture, for example, close a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        $this->deleteFixture('product', true);
+        parent::tearDown();
+    }
+
+    /**
+     * Test updating product back-order status
+     *
+     * @return void
+     */
+    public function testBackorderStatusUpdate()
+    {
+        /** @var $product Mage_Catalog_Model_Product */
+        $product = $this->getFixture('product');
+
+        $newProductData = new stdClass();
+        $newProductData->use_config_manage_stock = 0;
+        $newProductData->manage_stock = 1;
+        $newProductData->is_in_stock = 0;
+        $newProductData->use_config_backorders = 0;
+        $newProductData->backorders = 1;
+
+        $result = $this->call('cataloginventory_stock_item.update', array($this->getFixture('product')->getSku(), (array)$newProductData));
+        $this->assertTrue($result);
+
+        // have to re-load product for stock item set
+        $product->load($product->getId());
+        $this->assertEquals(1, $product->getStockItem()->getBackorders());
+        $this->assertEquals(0, $product->getStockItem()->getUseConfigBackorders());
+    }
+}
