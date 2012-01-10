4fbacca045f900fb36077365d6f21cbc52c85724
Artem Komarov <artem.komarov@magento.com>
2012-01-10 15:03:07 +0000

    CR Changes MAGE-5494: Add by SCU:The products is left  in Shopping Cart after Place order - Placed @see to new line

--- app/code/core/Enterprise/Checkout/controllers/Adminhtml/CheckoutController.php
+++ app/code/core/Enterprise/Checkout/controllers/Adminhtml/CheckoutController.php
@@ -770,7 +770,7 @@ class Enterprise_Checkout_Adminhtml_CheckoutController extends Mage_Adminhtml_Co
     /**
      * Wrapper for _getListItemInfo() - extends with additional list types. New method has been created to leave
      * definition of original method unchanged (add_by_sku list type utilizes additional parameter - $info).
-     * For return format @see _getListItemInfo().
+     * @see _getListItemInfo() for return format
      *
      * @param string $listType
      * @param int    $itemId
