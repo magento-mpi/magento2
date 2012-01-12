5d4f6d7fee3ea20bc798d1d6b261f759c1dccdaa
Yevgen Kubichka <yevgen.kubichka@magento.com>
2012-01-12 21:12:22 +0200

    Implemented SAASCORE-82: Remove downloadable product from list - Core part(cherry picked from commit 2881d9b)

--- app/code/core/Mage/Adminhtml/controllers/Report/ProductController.php
+++ app/code/core/Mage/Adminhtml/controllers/Report/ProductController.php
@@ -221,6 +221,10 @@ class Mage_Adminhtml_Report_ProductController extends Mage_Adminhtml_Controller_
      */
     public function downloadsAction()
     {
+        if (!$this->_isDownloadableAvailable()) {
+            $this->norouteAction();
+            return;
+        };
         $this->_title($this->__('Reports'))
              ->_title($this->__('Products'))
              ->_title($this->__('Downloads'));
@@ -238,6 +242,10 @@ class Mage_Adminhtml_Report_ProductController extends Mage_Adminhtml_Controller_
      */
     public function exportDownloadsCsvAction()
     {
+        if (!$this->_isDownloadableAvailable()) {
+            $this->norouteAction();
+            return;
+        };
         $fileName   = 'products_downloads.csv';
         $content    = $this->getLayout()->createBlock('adminhtml/report_product_downloads_grid')
             ->setSaveParametersInSession(true)
@@ -252,6 +260,10 @@ class Mage_Adminhtml_Report_ProductController extends Mage_Adminhtml_Controller_
      */
     public function exportDownloadsExcelAction()
     {
+        if (!$this->_isDownloadableAvailable()) {
+            $this->norouteAction();
+            return;
+        };
         $fileName   = 'products_downloads.xml';
         $content    = $this->getLayout()->createBlock('adminhtml/report_product_downloads_grid')
             ->setSaveParametersInSession(true)
@@ -259,6 +271,18 @@ class Mage_Adminhtml_Report_ProductController extends Mage_Adminhtml_Controller_
 
         $this->_prepareDownloadResponse($fileName, $content);
     }
+
+    /**
+     * Check whether Mage_Downloadable module is on
+     *
+     * @return bool
+     */
+    protected function _isDownloadableAvailable()
+    {
+        return is_object(Mage::getConfig()->getNode('modules/Mage_Downloadable'))
+            && Mage::getConfig()->getNode('modules/Mage_Downloadable')->is('active');
+    }
+
 
     /**
      * Check is allowed for report
