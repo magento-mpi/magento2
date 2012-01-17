7f50579e1a5360bff887594dbe34f351e791c052
Andrey Abumuslimov <andrey.abumuslimov@ebay.com>
2012-01-17 17:47:37 +0200

    Fixed MAGE-5405: [SAASMERGE] Synchronize attribute's option position with layered navigation Fixed MAGE-5194: [Saas] Sorting products by attributes other than defaults settings returns blank page Fixed MAGE-5101: Solr search and catalog results sorting issue Fixed MAGE-5093: EE Catalog Search Indexer Performance

--- app/code/core/Enterprise/Search/Model/Resource/Index.php
+++ app/code/core/Enterprise/Search/Model/Resource/Index.php
@@ -48,14 +48,166 @@ class Enterprise_Search_Model_Resource_Index extends Mage_CatalogSearch_Model_Re
 
 
     /**
-     * Return array of category, position and visibility data by products
+     * Return array of price data per customer and website by products
      *
-     * @param   int $storeId
-     * @param   array $productIds
-     * @param   bool $visibility      add visibility data to result
+     * @param   null|array $productIds
      * @return  array
      */
-    protected function _getCatalogCategoryData($storeId, $productIds, $visibility = true)
+    protected function _getCatalogProductPriceData($productIds = null)
+    {
+        $adapter = $this->_getWriteAdapter();
+
+        $select = $adapter->select()
+            ->from($this->getTable('catalog/product_index_price'),
+                array('entity_id', 'customer_group_id', 'website_id', 'min_price'));
+
+        if ($productIds) {
+            $select->where('entity_id IN (?)', $productIds);
+        }
+
+        $result = array();
+        foreach ($adapter->fetchAll($select) as $row) {
+            $result[$row['website_id']][$row['entity_id']][$row['customer_group_id']] = round($row['min_price'], 2);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Retrieve price data for product
+     *
+     * @param   $productIds
+     * @param   $storeId
+     *
+     * @return  array
+     */
+    public function getPriceIndexData($productIds, $storeId)
+    {
+        $priceProductsIndexData = $this->_getCatalogProductPriceData($productIds);
+
+        $websiteId = Mage::app()->getStore($storeId)->getWebsiteId();
+        if (!isset($priceProductsIndexData[$websiteId])) {
+            return array();
+        }
+
+        return $priceProductsIndexData[$websiteId];
+    }
+
+    /**
+     * Prepare system index data for products.
+     *
+     * @param   int $storeId
+     * @param   int|array|null $productIds
+     *
+     * @return  array
+     */
+    public function getCategoryProductIndexData($storeId = null, $productIds = null)
+    {
+        $adapter = $this->_getWriteAdapter();
+
+        $select = $adapter->select()
+            ->from(
+                array($this->getTable('catalog/category_product_index')),
+                array(
+                    'category_id',
+                    'product_id',
+                    'position',
+                    'store_id'
+                )
+            )
+            ->where('store_id = ?', $storeId)
+            ->group('product_id');
+
+        if ($productIds) {
+            $select->where('product_id IN (?)', $productIds);
+        }
+
+        $result = array();
+        foreach ($adapter->fetchAll($select) as $row) {
+            $result[$row['product_id']][$row['category_id']] = $row['position'];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Retrieve moved categories product ids
+     *
+     * @param   int $categoryId
+     * @return  array
+     */
+    public function getMovedCategoryProductIds($categoryId)
+    {
+        $adapter = $this->_getWriteAdapter();
+
+        $select = $adapter->select()
+            ->distinct()
+            ->from(
+                array('c_p' => $this->getTable('catalog/category_product')),
+                array('product_id')
+            )
+            ->join(
+                array('c_e' => $this->getTable('catalog/category')),
+                'c_p.category_id = c_e.entity_id',
+                array()
+            )
+            ->where($adapter->quoteInto('c_e.path LIKE ?', '%/' . $categoryId . '/%'))
+            ->orWhere('c_p.category_id = ?', $categoryId);
+
+        return $adapter->fetchCol($select);
+    }
+
+
+
+
+
+    // Deprecated methods
+
+    /**
+     * Update category products indexes.
+     *
+     * @deprecated after 1.11.0.0
+     *
+     * @param   array $productIds
+     * @return  Enterprise_Search_Model_Resource_Index
+     */
+    public function updateCategoryIndexData($productIds)
+    {
+        return $this;
+    }
+
+    /**
+     * Update category products price index
+     *
+     * @deprecated after 1.11.0.0
+     *
+     * @return Enterprise_Search_Model_Resource_Index
+     */
+    public function updatePriceIndexData()
+    {
+        foreach (Mage::app()->getStores(false) as $store) {
+            $index = $this->_getCatalogProductPriceData();
+            foreach (array_chunk($index, $this->_limit, true) as $indexPart) {
+                $this->_engine->saveEntityIndexes($store->getId(), $indexPart, 'product');
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Return array of category, position and visibility data (optionally) for products.
+     *
+     * @deprecated after 1.11.2.0
+     * @see $this->getSystemProductIndexData()
+     *
+     * @param   int $storeId
+     * @param   null|array $productIds
+     * @param   bool $visibility
+     *
+     * @return  array
+     */
+    protected function _getCatalogCategoryData($storeId, $productIds = null, $visibility = true)
     {
         $adapter = $this->_getWriteAdapter();
         $prefix  = $this->_engine->getFieldsPrefix();
@@ -102,39 +254,9 @@ class Enterprise_Search_Model_Resource_Index extends Mage_CatalogSearch_Model_Re
     }
 
     /**
-     * Return array of price data per customer and website by products
+     * Prepare advanced index for products.
      *
-     * @param   null|array $productIds
-     * @return  array
-     */
-    protected function _getCatalogProductPriceData($productIds = null)
-    {
-        $adapter = $this->_getWriteAdapter();
-        $prefix  = $this->_engine->getFieldsPrefix();
-        $select = $adapter->select()
-            ->from($this->getTable('catalog/product_index_price'),
-                array('entity_id', 'customer_group_id', 'website_id', 'min_price'));
-
-        if ($productIds) {
-            $select->where('entity_id IN (?)', $productIds);
-        }
-
-        $result = array();
-        foreach ($adapter->fetchAll($select) as $row) {
-            if (!isset($result[$row['entity_id']])) {
-                $result[$row['entity_id']] = array();
-            }
-            $key = sprintf('%sprice_%s_%s', $prefix, $row['customer_group_id'], $row['website_id']);
-            $result[$row['entity_id']][$key] = round($row['min_price'], 2);
-        }
-
-        return $result;
-    }
-
-    /**
-     * Prepare advanced index for products
-     *
-     * @see Mage_CatalogSearch_Model_Resource_Fulltext->_getSearchableProducts()
+     * @deprecated after 1.11.2.0
      *
      * @param   array $index
      * @param   int $storeId
@@ -169,79 +291,6 @@ class Enterprise_Search_Model_Resource_Index extends Mage_CatalogSearch_Model_Re
             }
         }
 
-        unset($categoryData);
-        unset($priceData);
-
         return $index;
-    }
-
-    /**
-     * Retrieve moved categories product ids
-     *
-     * @param   int $categoryId
-     * @return  array
-     */
-    public function getMovedCategoryProductIds($categoryId)
-    {
-        $adapter = $this->_getWriteAdapter();
-
-        $select = $adapter->select()
-            ->distinct()
-            ->from(
-                array('c_p' => $this->getTable('catalog/category_product')),
-                array('product_id')
-            )
-            ->join(
-                array('c_e' => $this->getTable('catalog/category')),
-                'c_p.category_id = c_e.entity_id',
-                array()
-            )
-            ->where($adapter->quoteInto('c_e.path LIKE ?', '%/' . $categoryId . '/%'))
-            ->orWhere('c_p.category_id = ?', $categoryId);
-
-        return $adapter->fetchCol($select);
-    }
-
-
-
-
-
-    /**
-     * Update category'es products indexes
-     *
-     * @deprecated after 1.11.0.0
-     *
-     * @param   array $productIds
-     * @return  Enterprise_Search_Model_Resource_Index
-     */
-    public function updateCategoryIndexData($productIds)
-    {
-        foreach (Mage::app()->getStores(false) as $store) {
-            $index = $this->_getCatalogCategoryData($store->getId(), $productIds, false);
-            foreach (array_chunk($index, $this->_limit, true) as $indexPart) {
-                $this->_engine->saveEntityIndexes($store->getId(), $indexPart, 'product');
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * Update category'es products price index
-     *
-     * @deprecated after 1.11.0.0
-     *
-     * @return Enterprise_Search_Model_Resource_Index
-     */
-    public function updatePriceIndexData()
-    {
-        foreach (Mage::app()->getStores(false) as $store) {
-            $index = $this->_getCatalogProductPriceData();
-            foreach (array_chunk($index, $this->_limit, true) as $indexPart) {
-                $this->_engine->saveEntityIndexes($store->getId(), $indexPart, 'product');
-            }
-        }
-
-        return $this;
     }
 }
