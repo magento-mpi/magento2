4bbfe22d18af6a207160c72a80013ecebd38fdeb
Vladimir Korotovskih <vladimir.korotovskih@magento.com>
2012-01-20 19:36:43 +0200

    Fixed MSSQL test build

--- app/code/core/Enterprise/TargetRule/sql/enterprise_targetrule_setup/upgrade-1.11.0.0.1-1.11.0.0.2.php
+++ app/code/core/Enterprise/TargetRule/sql/enterprise_targetrule_setup/upgrade-1.11.0.0.1-1.11.0.0.2.php
@@ -18,7 +18,37 @@ $targetRuleCustomerSegmentTable = $this->getTable('enterprise_targetrule_custome
 if ($connection->isTableExists($targetRuleCustomerSegmentTable)) {
     $connection->dropTable($targetRuleCustomerSegmentTable);
 }
+
+$connection->dropIndex(
+    $installer->getTable('enterprise_targetrule'),
+    $installer->getIdxName('enterprise_targetrule', array('use_customer_segment'))
+);
 $connection->dropColumn($installer->getTable('enterprise_targetrule'), 'use_customer_segment');
+
+$targetRuleProductTable = $installer->getTable('enterprise_targetrule_product');
+// recreate primary key to exclude column 'store_id'
+$primaryIndexColumns = $primaryIndexKeyName = null;
+foreach($connection->getIndexList($targetRuleProductTable) as $currentIndex) {
+    if (strtolower($currentIndex['INDEX_TYPE']) == Varien_Db_Adapter_Interface::INDEX_TYPE_PRIMARY) {
+        $primaryIndexKeyName = $currentIndex['KEY_NAME'];
+        $primaryIndexColumns = $currentIndex['COLUMNS_LIST'];
+        break;
+    }
+}
+$connection->dropIndex($targetRuleProductTable, $primaryIndexKeyName);
+if ($key = array_search('store_id', $primaryIndexColumns)) {
+    unset($primaryIndexColumns[$key]);
+    $primaryIndexColumns = array_values($primaryIndexColumns);
+}
+$connection->addIndex(
+    $targetRuleProductTable, $primaryIndexKeyName, $primaryIndexColumns, Varien_Db_Adapter_Interface::INDEX_TYPE_PRIMARY
+);
+
+//drop column 'store_id'
+$connection->dropIndex(
+    $targetRuleProductTable,
+    $installer->getIdxName('enterprise_targetrule_product', array('store_id'))
+);
 $connection->dropColumn($installer->getTable('enterprise_targetrule_product'), 'store_id');
 
 $installer->endSetup();
