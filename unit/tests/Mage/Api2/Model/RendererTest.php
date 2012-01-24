b17be02c9c81b7ab96f93099c67c56d262400b09
Igor Sidorenko <igor.sidorenko@magento.com>
2012-01-24 10:31:04 +0000

    Implemented APIA-181: Interface, Abstract class to render content, render errors - Added new test cases to test config existence in config.xml - Moved Mage_Api2_Model_RendererTest from integration to unit pool

--- unit/tests/Mage/Api2/Model/RendererTest.php
+++ unit/tests/Mage/Api2/Model/RendererTest.php
@@ -21,7 +21,7 @@
  * @category    Magento
  * @package     Mage_Api2
  * @subpackage  integration_tests
- * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magento.com)
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magento.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -32,13 +32,45 @@
  * @package     Mage_Api2
  * @author      Magento Core Team <core@magentocommerce.com>
  */
-class Mage_Api2_Model_RendererTest extends Magento_TestCase
+class Mage_Api2_Model_RendererTest extends Mage_PHPUnit_TestCase
 {
     /**
+     * API2 data helper mock
+     *
+     * @var Mage_Api2_Helper_Data
+     */
+    protected $_helperMock;
+
+    /**
+     * API2 renders data fixture
+     *
+     * @var array
+     */
+    protected $_renders;
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->_renders = (array) simplexml_load_file(dirname(__FILE__) . '/_fixtures/xml/renders.xml');
+        $this->_helperMock = $this->getHelperMockBuilder('api2')->getMock();
+    }
+
+    /**
      * Test response content renderer factory
+     *
+     * @return void
      */
     public function testFactory()
     {
+        $this->_helperMock->expects($this->any())
+            ->method('getResponseRenderAdapters')
+            ->will($this->returnValue($this->_renders));
+
         $data = array(
             '*/*'               => 'Mage_Api2_Model_Renderer_Json',
             'application/*'     => 'Mage_Api2_Model_Renderer_Json',
@@ -57,9 +89,14 @@ class Mage_Api2_Model_RendererTest extends Magento_TestCase
      * Test response content renderer factory with unknown accept type
      *
      * @expectedException Mage_Api2_Exception
+     * @return void
      */
     public function testFactoryBadAcceptType()
     {
+        $this->_helperMock->expects($this->any())
+            ->method('getResponseRenderAdapters')
+            ->will($this->returnValue($this->_renders));
+
         /**
          * Try get adapter via invalid content type
          * and must be throw exception
