74320d97699aa3cee42bfcae861a1110468b97d5
Sergey Shymko <sergey.shymko@magento.com>
2012-01-26 12:36:28 -0800

    Improved MAGETWO-699: jQuery Adoption and Conflicts Resolution - implemented integration tests for the 'enterprise/default' theme's controller actions -- catalog/product/view -- review/product/list controller - excluded tests for the enterprise and pro (for the future use) themes from publication - fixed phpdoc for Mage_Core_Model_Design_package (cherry picked from commit 86571a7138759805144f4d633abb74a67be173be)

---
+++ dev/tests/integration/testsuite/theme/enterprise/default/ControllerTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     theme
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * @magentoDataFixture Mage/Catalog/controllers/_files/products.php
+ */
+class Theme_Enterprise_Default_ControllerTest extends Magento_Test_TestCase_ControllerAbstract
+{
+    protected function assertPreConditions()
+    {
+        $availableThemes = Mage::getDesign()->getThemeList();
+        if (!isset($availableThemes['enterprise']) || !in_array('default', $availableThemes['enterprise'])) {
+            $this->markTestSkipped('Test requires "enterprise/default" theme to be available in the system.');
+        }
+    }
+
+    /**
+     * Assert that a page content contains references to both jQuery and jQzoom JavaScript libraries
+     *
+     * @param string $content
+     */
+    protected function _assertContainsJqZoom($content)
+    {
+        $this->assertContains('http://localhost/pub/js/jquery/jquery-1.7.1.min.js', $content);
+        $this->assertContains('http://localhost/pub/js/jquery/jquery-no-conflict.js', $content);
+        $this->assertContains('/js/jqzoom/js/jquery.jqzoom-core-pack.js', $content);
+        $this->assertContains('/js/jqzoom/css/jquery.jqzoom.css', $content);
+    }
+
+    /**
+     * @group module:Mage_Catalog
+     * @magentoConfigFixture current_store design/theme/full_name enterprise/default/default
+     */
+    public function testCatalogProductView()
+    {
+        $this->dispatch('catalog/product/view/id/1');
+        $this->_assertContainsJqZoom($this->getResponse()->getBody());
+    }
+
+    /**
+     * @group module:Mage_Review
+     * @magentoConfigFixture current_store design/theme/full_name enterprise/default/default
+     */
+    public function testReviewProductList()
+    {
+        $this->dispatch('review/product/list/id/1');
+        $this->_assertContainsJqZoom($this->getResponse()->getBody());
+    }
+}
