b3cf76ea107c92f462628f3c7f0cd144431cefe0
Andrey Bugaychuk <abugaichuk@ebay.com>
2012-01-27 19:33:54 +0200

    Implemented APIA-225: ACL Local : Abstract Filter model to apply permissions to collection (code style)

--- app/code/core/Mage/Api2/Model/Acl/Local.php
+++ app/code/core/Mage/Api2/Model/Acl/Local.php
@@ -32,7 +32,7 @@ class Mage_Api2_Model_Acl_Local implements Mage_Api2_Model_Acl_Local_Interface
     public function isAllowed($userId, $operation)
     {
         $resourceId = $this->getId();
-        
+
         //TODO add implementation
         $isAllowed = true;
 
