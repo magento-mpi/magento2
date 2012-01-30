e3e768bb15bff5ab89258e585375bae5c9d9c845
Anatoliy Yastreb <ayastreb@ebay.com>
2012-01-30 17:17:12 +0000

    Implemented APIA-211: Improve Test Framework to run REST integration tests - added REST adapters for admin, customer and guest user types - added tests for products resource

---
+++ integration/tests/Api2/Catalog/Product/AdminTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Magento
+ * @package     Mage_Core
+ * @subpackage  integration_tests
+ * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magento.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Test product resource
+ *
+ * @category    Magento
+ * @package     Magento_Test
+ * @author      Magento Api Team <api-team@magento.com>
+ */
+/**
+ * @magentoDataFixture Api/SalesOrder/_fixtures/product_simple.php
+ */
+
+class Api2_Catalog_Product_AdminTest extends Magento_Test_Webservice_Rest_Admin
+{
+    /**
+     * Test product resource get
+     *
+     * @return void
+     */
+    public function testGet()
+    {
+        /** @var $product Mage_Catalog_Model_Product */
+        $product = $this->getFixture('product_simple');
+        $restResponse = $this->getWebService()->callGet('products/' . $product->getId());
+        // @todo: change test after admin ACL is implemented
+        $this->assertEquals(500, $restResponse->getStatus());
+    }
+}
+
