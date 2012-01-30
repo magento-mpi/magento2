e76c1822230d9502e5e33286d35a26969cbbfe9d
Andrey Tserkus <andrey.tserkus@magento.com>
2012-01-30 12:10:43 -0800

    Fixed MAGETWO-712: Skin Selector at GUI Editor Frontend Panel - added Visual Design Editor to the system

---
+++ app/code/core/Mage/DesignEditor/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_DesignEditor
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Design Editor main helper
+ */
+class Mage_DesignEditor_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
