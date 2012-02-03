fd9b6e9d3cd45aca8c8f534f0d9debff78c5e4ad
Alex Paliarush <apaliarush@ebay.com>
2012-02-03 16:24:00 +0200

    Implemented APIA-242: Single GET operation

---
+++ app/code/core/Mage/Review/Model/Api2/Review/Rest/Admin/V1.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Review
+ * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Api2 for review item
+ *
+ * @category   Mage
+ * @package    Mage_Review
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Review_Model_Api2_Review_Rest_Admin_V1 extends Mage_Review_Model_Api2_Review_Rest
+{
+    /**
+     * Retrieve information about specified review item
+     *
+     * @return array
+     */
+    protected function _retrieve()
+    {
+        $reviewId = $this->getRequest()->getParam('id');
+        /** @var $review Mage_Review_Model_Review */
+        $review = Mage::getModel('review/review')->load($reviewId);
+        if (!$review->getId()) {
+            $this->_critical(Mage_Api2_Model_Resource::RESOURCE_NOT_FOUND);
+        }
+
+        return $review->getData();
+    }
+}
