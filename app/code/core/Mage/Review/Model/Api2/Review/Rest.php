397c887d9638a071d774b54b6c7d8a2da7e7d4bb
Andrey Roslik <aroslik@ebay.com>
2012-02-07 16:30:34 +0200

    Implemented APIA-222: ACL Global: Admin global permissions control Fix strict for review api2 model

--- app/code/core/Mage/Review/Model/Api2/Review/Rest.php
+++ app/code/core/Mage/Review/Model/Api2/Review/Rest.php
@@ -89,7 +89,7 @@ abstract class Mage_Review_Model_Api2_Review_Rest extends Mage_Api2_Model_Resour
      * @return array
      * @todo Investigate list of attributes and add it to this method
      */
-    public function getAvailableAttributes()
+    static public function getAvailableAttributes()
     {
         return array(
              'entity_id' => 'ID',
