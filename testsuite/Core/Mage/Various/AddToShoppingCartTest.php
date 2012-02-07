93f37c87af0944000754ccfdb74fcf403d1e5eb9
Artem Shugayev <ashugayev@ebay.com>
2012-02-07 15:10:55 +0200

    [TESTS] Fixed Various tests

--- testsuite/Core/Mage/Various/AddToShoppingCartTest.php
+++ testsuite/Core/Mage/Various/AddToShoppingCartTest.php
@@ -34,7 +34,7 @@
  * @subpackage  tests
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Various_AddToShoppingCartTest extends Mage_Selenium_TestCase
+class Core_Mage_Various_AddToShoppingCartTest extends Mage_Selenium_TestCase
 {
     /**
      * <p>Login to backend</p>
--- testsuite/Core/Mage/Various/CheckoutLinkVerificationTest.php
+++ testsuite/Core/Mage/Various/CheckoutLinkVerificationTest.php
@@ -33,7 +33,7 @@
  * @subpackage  tests
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Various_CheckoutLinkVerificationTest extends Mage_Selenium_TestCase
+class Core_Mage_Various_CheckoutLinkVerificationTest extends Mage_Selenium_TestCase
 {
     /**
      * <p>Creating product with required fields only</p>
