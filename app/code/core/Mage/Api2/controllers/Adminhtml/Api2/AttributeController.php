50e7a7cc0e00ccb46bf8498bd316b1e3d31c5a92
Igor Sidorenko <isidorenko@ebay.com>
2012-02-07 18:42:44 +0200

    Implemented APIA-281: Attribute Filter: User types grid and form to assign attributes to user types operations - Added API2 attributes grid - Added indexAction()

--- app/code/core/Mage/Api2/controllers/Adminhtml/Api2/AttributeController.php
+++ app/code/core/Mage/Api2/controllers/Adminhtml/Api2/AttributeController.php
@@ -34,6 +34,24 @@
 class Mage_Api2_Adminhtml_Api2_AttributeController extends Mage_Adminhtml_Controller_Action
 {
     /**
+     * Show user types grid
+     */
+    public function indexAction()
+    {
+        $this->_title($this->__('System'))
+             ->_title($this->__('Web Services'))
+             ->_title($this->__('REST Attributes'));
+
+        $this->loadLayout()->_setActiveMenu('system/services/attributes');
+
+        $this->_addBreadcrumb($this->__('Web services'), $this->__('Web services'))
+            ->_addBreadcrumb($this->__('REST Attributes'), $this->__('REST Attributes'))
+            ->_addBreadcrumb($this->__('Attributes'), $this->__('Attributes'));
+
+        $this->renderLayout();
+    }
+
+    /**
      * Edit role
      */
     public function editAction()
