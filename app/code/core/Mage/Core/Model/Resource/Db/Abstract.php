4b80b7e052acbab6aebd8cea75a694532ae39149
Andrii Kasian <akasian@ebay.com>
2012-02-08 08:17:01 -0500

    Fixed MAGE-5779: Partial Reindex is not done on product save from Backend

--- app/code/core/Mage/Core/Model/Resource/Db/Abstract.php
+++ app/code/core/Mage/Core/Model/Resource/Db/Abstract.php
@@ -333,8 +333,8 @@ abstract class Mage_Core_Model_Resource_Db_Abstract extends Mage_Core_Model_Reso
     protected function _getReadAdapter()
     {
         $writeAdapter = $this->_getWriteAdapter();
-        if ($writeAdapter->getTransactionLevel() > 0) {
-            // if transaction was started We must read from "write adapter"
+        if ($writeAdapter && $writeAdapter->getTransactionLevel() > 0) {
+            // if transaction is started we should use write connection for reading
             return $writeAdapter;
         }
         return $this->_getConnection('read');
