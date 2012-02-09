aa937f80412a56b7788451098b461e363e7a1308
Olga Kopylova <okopylova@ebay.com>
2012-02-09 14:38:37 -0800

    MAGETWO-750: Backend sales orders page has a fatal error - code standarts fixes

--- dev/tests/integration/testsuite/Mage/Adminhtml/controllers/Sales/OrderControllerTest.php
+++ dev/tests/integration/testsuite/Mage/Adminhtml/controllers/Sales/OrderControllerTest.php
@@ -3,27 +3,25 @@
  * {license_notice}
  *
  * @category    Magento
- * @package     Magento_Catalog
+ * @package     Mage_Adminhtml
  * @subpackage  integration_tests
  * @copyright   {copyright}
  * @license     {license_link}
  */
 
 /**
- * @group module:Mage_DesignEditor
+ * @group module:Mage_Adminhtml
  *
  * @magentoDataFixture Mage/Admin/_files/user.php
  */
 class Mage_Adminhtml_Sales_OrderControllerTest extends Magento_Test_TestCase_ControllerAbstract
 {
-    protected $_session = null;
-
     public function setUp()
     {
         parent::setUp();
         Mage::getSingleton('Mage_Adminhtml_Model_Url')->turnOffSecretKey();
-        $this->_session = new Mage_Admin_Model_Session;
-        $this->_session->login('user', 'password');
+        $session = new Mage_Admin_Model_Session;
+        $session->login('user', 'password');
     }
 
     public function testIndexAction()
--- dev/tests/integration/testsuite/Mage/Sales/_files/order.php
+++ dev/tests/integration/testsuite/Mage/Sales/_files/order.php
@@ -3,7 +3,7 @@
  * {license_notice}
  *
  * @category    Magento
- * @package     Mage_Catalog
+ * @package     Mage_Sales
  * @subpackage  integration_tests
  * @copyright   {copyright}
  * @license     {license_link}
