8f83e2ceb234b271c7915fd72b38a6b8d95b779b
Andrii Kasian <akasian@ebay.com>
2012-02-09 11:43:10 -0500

    CR Changes MAGE-5671: Unable to sort products by price

--- app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
+++ app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
@@ -1880,7 +1880,7 @@ class Mage_Catalog_Model_Resource_Product_Collection extends Mage_Catalog_Model_
      *
      * @param string $attribute
      * @param string $dir
-     * @return Mage_CatalogSearch_Model_Resource_Fulltext_Collection
+     * @return Mage_Catalog_Model_Resource_Product_Collection
      */
     public function setOrder($attribute, $dir = 'desc')
     {
@@ -1891,5 +1891,4 @@ class Mage_Catalog_Model_Resource_Product_Collection extends Mage_Catalog_Model_
         }
         return $this;
     }
-
 }
