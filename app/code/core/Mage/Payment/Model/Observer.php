452a7467485f86cd8eb2f584f06a69477278a6eb
Dmitriy Volik <dmitriy.volik@magento.com>
2012-02-10 17:02:47 +0200

    Fixed MAGE-5998: [SAASMERGE] New payments: Cache On Delivery and Bank Transfer

--- app/code/core/Mage/Payment/Model/Observer.php
+++ app/code/core/Mage/Payment/Model/Observer.php
@@ -108,4 +108,21 @@ class Mage_Payment_Model_Observer
         }
         $product->addCustomOption('additional_options', serialize($infoOptions));
     }
+
+    /**
+     * Sets current instructions for bank transfer account
+     *
+     * @param Varien_Event_Observer $observer
+     * @return void
+     */
+    public function beforeOrderPaymentSave(Varien_Event_Observer $observer)
+    {
+        /** @var Mage_Sales_Model_Order_Payment $payment */
+        $payment = $observer->getEvent()->getPayment();
+        if($payment->getMethod() ===
+            Mage_Payment_Model_Method_Banktransfer::PAYMENT_METHOD_BANKTRANSFER_CODE) {
+            $payment->setAdditionalInformation('instructions',
+                $payment->getMethodInstance()->getInstructions());
+        }
+    }
 }
