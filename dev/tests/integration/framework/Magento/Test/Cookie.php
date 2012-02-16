c54c6ae13106a883cb89d49d3fd12a3456772b94
Andrey Tserkus <andrey.tserkus@magento.com>
2012-02-15 16:36:25 -0800

    Fixed MAGETWO-723: Output in Frontend Controller Actions via Layout Only - fixed problems with cookies being sent

---
+++ dev/tests/integration/framework/Magento/Test/Cookie.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Class, which overwrites native Magento cookie model in order to not set cookies during testing
+ */
+class Magento_Test_Cookie extends Mage_Core_Model_Cookie
+{
+    /**
+     * Dummy function instead of original that sets cookie. This one does nothing - in test environment we cannot
+     * set cookies.
+     *
+     * @param string $name The cookie name
+     * @param string $value The cookie value
+     * @param int $period Lifetime period
+     * @param string $path
+     * @param string $domain
+     * @param int|bool $secure
+     * @param bool $httponly
+     * @return Magento_Test_Cookie
+     */
+    public function set($name, $value, $period = null, $path = null, $domain = null, $secure = null, $httponly = null)
+    {
+        return $this;
+    }
+
+    /**
+     * Dummy function instead of original that deletes cookie. This one does nothing - in test environment we cannot
+     * set cookies.
+     *
+     * @param string $name
+     * @param string $path
+     * @param string $domain
+     * @param int|bool $secure
+     * @param int|bool $httponly
+     * @return Magento_Test_Cookie
+     */
+    public function delete($name, $path = null, $domain = null, $secure = null, $httponly = null)
+    {
+        return $this;
+    }
+}
