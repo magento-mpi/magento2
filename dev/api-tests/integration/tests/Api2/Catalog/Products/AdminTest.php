e17a63747b80315d605931ec2e3c784a499d2a33
Anatoliy Yastreb <ayastreb@ebay.com>
2012-02-17 19:45:51 +0200

    Implemented APIA-324: REST: Resource Product: POST - added inventory validation - added negative test for required fields

--- dev/api-tests/integration/tests/Api2/Catalog/Products/AdminTest.php
+++ dev/api-tests/integration/tests/Api2/Catalog/Products/AdminTest.php
@@ -36,13 +36,19 @@
 class Api2_Catalog_Products_AdminTest extends Magento_Test_Webservice_Rest_Admin
 {
     /**
+     * Delete fixtures
+     */
+    protected function tearDown()
+    {
+        $this->deleteFixture('product_simple', true);
+        parent::tearDown();
+    }
+
+    /**
      * Test product resource post
-     *
-     * @return void
      */
     public function testPostSimpleRequiredFieldsOnly()
     {
-        $this->markTestSkipped("Skipped until APIA-304 task merge into development branch");
         $productData = require dirname(__FILE__) . '/../_fixtures/Backend/SimpleProductData.php';
 
         $restResponse = $this->callPost('products', $productData);
@@ -53,12 +59,43 @@ class Api2_Catalog_Products_AdminTest extends Magento_Test_Webservice_Rest_Admin
         /** @var $product Mage_Catalog_Model_Product */
         $product = Mage::getModel('catalog/product')->load($productId);
         $this->assertNotNull($product->getId());
+        $this->setFixture('product_simple', $product);
         $this->assertEquals($product->getTypeId(), $productData['type']);
         $this->assertEquals($product->getAttributeSetId(), $productData['set']);
 
         $productAttributes = array_diff_key($productData, array_flip(array('type', 'set')));
         foreach ($productAttributes as $attribute => $value) {
             $this->assertEquals($product->getData($attribute), $value);
+        }
+    }
+
+    /**
+     * Test product create resource with empty required fields
+     * Negative test.
+     */
+    public function testPostEmptyRequiredFields()
+    {
+        $this->getWebService()->getClient()->setHeaders('Cookie', 'XDEBUG_SESSION=PHPSTORM');
+        $productData = require dirname(__FILE__) . '/../_fixtures/Backend/SimpleProductEmptyRequired.php';
+
+        $restResponse = $this->callPost('products', $productData);
+        $this->assertEquals(Mage_Api2_Model_Server::HTTP_BAD_REQUEST, $restResponse->getStatus());
+        $body = $restResponse->getBody();
+        $errors = $body['messages']['error'];
+        $this->assertNotEmpty($errors);
+        unset($productData['type']);
+        unset($productData['set']);
+        unset($productData['stock_data']);
+        $expectedErrors = array(
+            'Resource data pre-validation error.',
+            'Empty value for "stock_data:qty" in request.'
+        );
+        foreach ($productData as $key => $value) {
+            $expectedErrors[] = sprintf('Empty value for "%s" in request.', $key);
+        }
+        $this->assertEquals(count($expectedErrors), count($errors));
+        foreach ($errors as $error) {
+            $this->assertContains($error['message'], $expectedErrors);
         }
     }
 }
