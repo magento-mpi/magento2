308c33c6a4ee197364e57260bd2b2aafc8523913
Anatoliy Yastreb <ayastreb@ebay.com>
2012-02-17 12:02:26 +0200

    Fixed MAGE-6119: shoppingCartProductList V2 API Operation SoapFault

--- app/code/core/Mage/Checkout/Model/Cart/Product/Api.php
+++ app/code/core/Mage/Checkout/Model/Cart/Product/Api.php
@@ -233,12 +233,13 @@ class Mage_Checkout_Model_Cart_Product_Api extends Mage_Checkout_Model_Api_Resou
             /** @var $item Mage_Sales_Model_Quote_Item */
             $product = $item->getProduct();
             $productsResult[] = array( // Basic product data
-                'product_id' => $product->getId(),
-                'sku'        => $product->getSku(),
-                'set'        => $product->getAttributeSetId(),
-                'type'       => $product->getTypeId(),
-                'categories' => $product->getCategoryIds(),
-                'websites'   => $product->getWebsiteIds()
+                'product_id'   => $product->getId(),
+                'sku'          => $product->getSku(),
+                'name'         => $product->getName(),
+                'set'          => $product->getAttributeSetId(),
+                'type'         => $product->getTypeId(),
+                'category_ids' => $product->getCategoryIds(),
+                'website_ids'  => $product->getWebsiteIds()
             );
         }
 
