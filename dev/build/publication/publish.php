cda921e119c331d4db850e8d97377786a76e170e
Andrey Tserkus <andrey.tserkus@magento.com>
2012-02-21 10:48:19 -0800

    Fixed MAGETWO-693: Check extruder configuration to exclude functional tests from publishing to Github - added github config to remove non-ready files from github publication - added some internal credentials to sanity check

--- dev/build/publication/publish.php
+++ dev/build/publication/publish.php
@@ -62,11 +62,12 @@ try {
     // remove files that must not be published
     $extruderDir = __DIR__ . '/extruder';
     execVerbose(
-        'php -f %s -- -w %s -l %s -l %s -l %s -g -v',
+        'php -f %s -- -w %s -l %s -l %s -l %s -l %s -g -v',
         "$extruderDir/extruder.php",
         $targetDir,
         "$extruderDir/common.txt",
         "$extruderDir/common_tests.txt",
+        "$extruderDir/github.txt",
         "$extruderDir/ce.txt"
     );
 
