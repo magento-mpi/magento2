76359401adf81c7bd7175cead423561eabee50c8
Andrey Konosov <andrey.konosov@magento.com>
2012-02-21 17:25:52 +0200

    CR Changes MAGE-6127: [SAASMERGE]State/Province value is reset after user clicks Get a Quote button on the Shopping cart.

--- app/code/core/Mage/Adminhtml/Block/Customer/Edit/Renderer/Region.php
+++ app/code/core/Mage/Adminhtml/Block/Customer/Edit/Renderer/Region.php
@@ -29,7 +29,8 @@
  *
  * @author      Magento Core Team <core@magentocommerce.com>
  */
-class Mage_Adminhtml_Block_Customer_Edit_Renderer_Region extends Mage_Adminhtml_Block_Abstract implements Varien_Data_Form_Element_Renderer_Interface
+class Mage_Adminhtml_Block_Customer_Edit_Renderer_Region extends Mage_Adminhtml_Block_Abstract
+    implements Varien_Data_Form_Element_Renderer_Interface
 {
     /**
      * Output the region element and javasctipt that makes it dependent from country element
@@ -61,7 +62,8 @@ class Mage_Adminhtml_Block_Customer_Edit_Renderer_Region extends Mage_Adminhtml_
 
         $html.= '<script type="text/javascript">'."\n";
         $html.= '$("'.$selectId.'").setAttribute("defaultValue", "'.$regionId.'");'."\n";
-        $html.= 'new regionUpdater("'.$country->getHtmlId().'", "'.$element->getHtmlId().'", "'.$selectId.'", '.$this->helper('directory')->getRegionJson().');'."\n";
+        $html.= 'new regionUpdater("'.$country->getHtmlId().'", "'.$element->getHtmlId().'", "'.$selectId.'", ';
+        $html .= $this->helper('directory')->getRegionJson().');'."\n";
         $html.= '</script>'."\n";
 
         $html.= '</td></tr>'."\n";
