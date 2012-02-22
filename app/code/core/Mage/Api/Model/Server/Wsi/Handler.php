e0619ccc9e80caf60afc79a1c3e6d7de6dbde88b
Andrey Bugaychuk <abugaichuk@ebay.com>
2012-02-22 20:17:06 +0200

    Fixed APIA-191: Configure API tests run on CI (fixed Api_GiftCard_AccountTest, fixed misspell in error message, fixed Mage_Api_Model_Server_WSI_Handler exception invalid arguments)

--- app/code/core/Mage/Api/Model/Server/Wsi/Handler.php
+++ app/code/core/Mage/Api/Model/Server/Wsi/Handler.php
@@ -171,7 +171,7 @@ class Mage_Api_Model_Server_WSI_Handler extends Mage_Api_Model_Server_Handler_Ab
                 if($parameter->isOptional()){
                     $callArgs[$pName] = $parameter->getDefaultValue();
                 } else {
-                    Mage::logException(new Exception("Required parameter \"$pName\" is missing.", 0, null));
+                    Mage::logException(new Exception("Required parameter \"$pName\" is missing.", 0));
                     $this->_fault('invalid_request_param');
                 }
             }
