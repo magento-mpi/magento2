d8a49c6da9b67b1683a9520c14662371095f59e5
Valeriy Nayda <vnayda@ebay.com>
2012-02-22 11:54:20 +0200

    Implemented APIA-357: Update functionality stock items (create new xml intepreter)

--- app/code/core/Mage/Api2/Model/Request/Interpreter/Xml.php
+++ app/code/core/Mage/Api2/Model/Request/Interpreter/Xml.php
@@ -34,6 +34,15 @@
 class Mage_Api2_Model_Request_Interpreter_Xml implements Mage_Api2_Model_Request_Interpreter_Interface
 {
     /**
+     * Load file error string.
+     *
+     * Is null if there was no error while file loading
+     *
+     * @var string
+     */
+    protected $_loadFileErrorStr = null;
+
+    /**
      * Parse Request body into array of params
      *
      * @param string $body  Posted content from request
@@ -46,11 +55,88 @@ class Mage_Api2_Model_Request_Interpreter_Xml implements Mage_Api2_Model_Request
             throw new Exception(sprintf('Invalid data type "%s". String expected.', gettype($body)));
         }
         $body = false !== strpos($body, '<?xml') ? $body : '<?xml version="1.0"?>' . PHP_EOL . $body;
-        try {
-            $xml = new Zend_Config_Xml($body);
-        } catch (Zend_Config_Exception $e) {
+
+        set_error_handler(array($this, '_loadFileErrorHandler')); // Warnings and errors are suppressed
+        $config = simplexml_load_string($body);
+
+        restore_error_handler();
+        // Check if there was a error while loading file
+        if ($this->_loadFileErrorStr !== null) {
             throw new Mage_Api2_Exception('Decoding error.', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
         }
-        return $xml->toArray();
+
+        $xml = $this->_toArray($config);
+        return $xml;
+    }
+
+    /**
+     * Returns a string or an associative and possibly multidimensional array from
+     * a SimpleXMLElement.
+     *
+     * @param  SimpleXMLElement $xmlObject Convert a SimpleXMLElement into an array
+     * @return array|string
+     */
+    protected function _toArray(SimpleXMLElement $xmlObject)
+    {
+        $config = array();
+        // Search for parent node values
+        if (count($xmlObject->attributes()) > 0) {
+            foreach ($xmlObject->attributes() as $key => $value) {
+                $value = (string) $value;
+                if (array_key_exists($key, $config)) {
+                    if (!is_array($config[$key])) {
+                        $config[$key] = array($config[$key]);
+                    }
+                    $config[$key][] = $value;
+                } else {
+                    $config[$key] = $value;
+                }
+            }
+        }
+
+        // Search for children
+        if (count($xmlObject->children()) > 0) {
+            foreach ($xmlObject->children() as $key => $value) {
+                if (count($value->children()) > 0) {
+                    $value = $this->_toArray($value);
+                } else if (count($value->attributes()) > 0) {
+                    $attributes = $value->attributes();
+                    if (isset($attributes['value'])) {
+                        $value = (string) $attributes['value'];
+                    } else {
+                        $value = $this->_toArray($value);
+                    }
+                } else {
+                    $value = (string) $value;
+                }
+                if (array_key_exists($key, $config)) {
+                    if (!is_array($config[$key]) || !array_key_exists(0, $config[$key])) {
+                        $config[$key] = array($config[$key]);
+                    }
+                    $config[$key][] = $value;
+                } else {
+                    $config[$key] = $value;
+                }
+            }
+        }
+
+        return $config;
+    }
+
+    /**
+     * Handle any errors from simplexml_load_file or parse_ini_file
+     *
+     * @param integer $errno
+     * @param string $errstr
+     * @param string $errfile
+     * @param integer $errline
+     */
+    protected function _loadFileErrorHandler($errno, $errstr, $errfile, $errline)
+    {
+        if ($this->_loadFileErrorStr === null) {
+            $this->_loadFileErrorStr = $errstr;
+        } else {
+            $this->_loadFileErrorStr .= (PHP_EOL . $errstr);
+        }
     }
 }
