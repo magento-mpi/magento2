b2aa08648ea7314023f045447b9dbf7c34f01cb2
Alex Bomko <alex.bomko@magento.com>
2012-02-23 17:43:57 +0200

    Fixed MAGE-6195: PayPal HSS (Website Payment Pro Hosted Solutons) France does not work

--- app/code/core/Mage/Paypal/Block/Iframe.php
+++ app/code/core/Mage/Paypal/Block/Iframe.php
@@ -75,8 +75,13 @@ class Mage_Paypal_Block_Iframe extends Mage_Payment_Block_Form
             ->getMethod();
         if (in_array($paymentCode, $this->helper('paypal/hss')->getHssMethods())) {
             $this->_paymentMethodCode = $paymentCode;
-            $template_path = str_replace('_', '', $paymentCode);
-            $this->setTemplate("paypal/{$template_path}/iframe.phtml");
+            $templatePath = str_replace('_', '', $paymentCode);
+            $templateFile = "paypal/{$templatePath}/iframe.phtml";
+            if (file_exists(Mage::getDesign()->getTemplateFilename($templateFile))) {
+                $this->setTemplate($templateFile);
+            } else {
+                $this->setTemplate('paypal/hss/iframe.phtml');
+            }
         }
     }
 
