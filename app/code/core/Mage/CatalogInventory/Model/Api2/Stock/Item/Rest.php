2e054ad9f9c9aaa0e5ce1b22e71518608e09b9ad
Valeriy Nayda <vnayda@ebay.com>
2012-02-24 18:35:19 +0200

    Re-factored APIA-323: Update functionality (some changes)

--- app/code/core/Mage/CatalogInventory/Model/Api2/Stock/Item/Rest.php
+++ app/code/core/Mage/CatalogInventory/Model/Api2/Stock/Item/Rest.php
@@ -34,4 +34,55 @@
 abstract class Mage_CatalogInventory_Model_Api2_Stock_Item_Rest
     extends Mage_CatalogInventory_Model_Api2_Stock_Item
 {
+    /**
+     * Retrieve information about specified stock item
+     *
+     * @throws Mage_Api2_Exception
+     * @return array
+     */
+    protected function _retrieve()
+    {
+        /* @var $stockItem Mage_CatalogInventory_Model_Stock_Item */
+        $stockItem = $this->_loadStockItemById($this->getRequest()->getParam('id'));
+        return $stockItem->getData();
+    }
+
+    /**
+     * Update specified stock item
+     *
+     * @param array $data
+     * @throws Mage_Api2_Exception
+     */
+    protected function _update(array $data)
+    {
+        $this->_validate($data, array(), array('product_id', 'stock_id'));
+
+        /* @var $stockItem Mage_CatalogInventory_Model_Stock_Item */
+        $stockItem = $this->_loadStockItemById($this->getRequest()->getParam('id'));
+        $stockItem->addData($data);
+        try {
+            $stockItem->save();
+        } catch (Mage_Core_Exception $e) {
+            $this->_error($e->getMessage(), Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
+        } catch (Exception $e) {
+            $this->_critical(self::RESOURCE_INTERNAL_ERROR);
+        }
+    }
+
+    /**
+     * Load stock item by its id passed through request
+     *
+     * @param int $id
+     * @throws Mage_Api2_Exception
+     * @return Mage_CatalogInventory_Model_Stock_Item
+     */
+    protected function _loadStockItemById($id)
+    {
+        /* @var $stockItem Mage_CatalogInventory_Model_Stock_Item */
+        $stockItem = Mage::getModel('cataloginventory/stock_item')->load($id);
+        if (!$stockItem->getId()) {
+            $this->_critical(self::RESOURCE_NOT_FOUND);
+        }
+        return $stockItem;
+    }
 }
