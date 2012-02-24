62e91d1faae1f745ba7a9a1f8b6f61eb79f34bf4
Andrey Tserkus <andrey.tserkus@magento.com>
2012-02-24 11:02:51 -0800

    Fixed MAGETWO-730: Github Update (February 3rd) - added test from pull request regarding fatal error in loading non-existing admin user, the changes in model itself have been already done independently by us in other ticket

---
+++ dev/tests/integration/testsuite/Mage/Admin/Model/UserTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Admin
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * @group module:Mage_Admin
+ * @magentoDataFixture Mage/Admin/_files/user.php
+ */
+class Mage_Admin_Model_UserTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Mage_Admin_Model_User
+     */
+    protected $_model;
+
+    protected function setUp()
+    {
+        $this->_model = new Mage_Admin_Model_User;
+    }
+
+    /**
+     * Ensure that an exception is not thrown, if the user does not exist
+     */
+    public function testLoadByUsername()
+    {
+        $this->_model->loadByUsername('non_existing_user');
+        $this->assertNull($this->_model->getId(), 'The admin user has an unexpected ID');
+        $this->_model->loadByUsername('user');
+        $this->assertNotEmpty($this->_model->getId(), 'The admin user should have been loaded');
+    }
+}
