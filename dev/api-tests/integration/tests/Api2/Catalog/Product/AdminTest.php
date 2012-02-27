87694498c6c01a0a244487edcbef4124d4cd32d4
Alex Paliarush <apaliarush@ebay.com>
2012-02-27 18:42:09 +0200

    Implemented APIA-343: Product put (for admin role) - store added to route - added test - re-factored tests

--- dev/api-tests/integration/tests/Api2/Catalog/Product/AdminTest.php
+++ dev/api-tests/integration/tests/Api2/Catalog/Product/AdminTest.php
@@ -117,8 +117,7 @@ class Api2_Catalog_Product_AdminTest extends Magento_Test_Webservice_Rest_Admin
 
         /** @var $store Mage_Core_Model_Store */
         $store = $this->getFixture('store_on_new_website');
-        $params = array('store' => $store->getCode());
-        $restResponse = $this->callGet($this->_getResourcePath($product->getId()), $params);
+        $restResponse = $this->callGet($this->_getResourcePath($product->getId(), $store->getCode()));
 
         $this->assertEquals(Mage_Api2_Model_Server::HTTP_NOT_FOUND, $restResponse->getStatus());
     }
@@ -132,10 +131,7 @@ class Api2_Catalog_Product_AdminTest extends Magento_Test_Webservice_Rest_Admin
     {
         /** @var $product Mage_Catalog_Model_Product */
         $product = $this->getFixture('product_simple');
-
-        $params = array('store' => 'INVALID_STORE');
-        $restResponse = $this->callGet($this->_getResourcePath($product->getId()), $params);
-
+        $restResponse = $this->callGet($this->_getResourcePath($product->getId(), 'INVALID_STORE'));
         $this->assertEquals(Mage_Api2_Model_Server::HTTP_BAD_REQUEST, $restResponse->getStatus());
     }
 
@@ -154,29 +150,54 @@ class Api2_Catalog_Product_AdminTest extends Magento_Test_Webservice_Rest_Admin
         $this->assertEquals(Mage_Api2_Model_Server::HTTP_OK, $restResponse->getStatus());
 
         $updatedProduct = Mage::getModel('catalog/product')->load($product->getId());
-        $this->assertNotNull($updatedProduct->getId());
+        // Validate URL Key - all special chars should be replaced with dash sign
+        $productDataForUpdate['url_key'] = '123-abc';
+        $this->_checkProductData($updatedProduct, $productDataForUpdate);
+    }
 
+    /**
+     * Check if product data equals expected data
+     *
+     * @param Mage_Catalog_Model_Product $product
+     * @param array $expectedData
+     */
+    protected function _checkProductData($product, $expectedData)
+    {
+        $this->assertNotNull($product->getId());
         $dateAttributes = array('news_from_date', 'news_to_date', 'special_from_date', 'special_to_date',
             'custom_design_from', 'custom_design_to');
         foreach ($dateAttributes as $attribute) {
-            $this->assertEquals(strtotime($productDataForUpdate[$attribute]),
-                strtotime($updatedProduct->getData($attribute)));
+            $this->assertEquals(strtotime($expectedData[$attribute]),
+                strtotime($product->getData($attribute)));
         }
-
-        $exclude = array_merge($dateAttributes, array('group_price', 'tier_price', 'stock_data', 'url_key'));
-        // Validate URL Key - all special chars should be replaced with dash sign
-        $this->assertEquals('123-abc', $updatedProduct->getUrlKey());
-        $productAttributes = array_diff_key($productDataForUpdate, array_flip($exclude));
+        $exclude = array_merge($dateAttributes, array('group_price', 'tier_price', 'stock_data'));
+        $productAttributes = array_diff_key($expectedData, array_flip($exclude));
         foreach ($productAttributes as $attribute => $value) {
-            $this->assertEquals($value, $updatedProduct->getData($attribute));
+            $this->assertEquals($value, $product->getData($attribute));
         }
-
-        if (isset($productData['stock_data'])) {
-            $stockItem = $updatedProduct->getStockItem();
-            foreach ($productData['stock_data'] as $attribute => $value) {
+        if (isset($expectedData['stock_data'])) {
+            $stockItem = $product->getStockItem();
+            foreach ($expectedData['stock_data'] as $attribute => $value) {
                 $this->assertEquals($value, $stockItem->getData($attribute));
             }
         }
+    }
+
+    /**
+     * Test update with invalid store
+     *
+     * @magentoDataFixture Api/SalesOrder/_fixtures/product_simple.php
+     */
+    public function testUpdateWithInvalidStore()
+    {
+        /** @var $product Mage_Catalog_Model_Product */
+        $product = $this->getFixture('product_simple');
+        $productDataForUpdate = require dirname(__FILE__) . '/../_fixtures/Backend/SimpleProductAllFieldsData.php';
+
+        $restResponse = $this->callPut($this->_getResourcePath($product->getId(), 'INVALID_STORE'),
+            $productDataForUpdate);
+
+        $this->assertEquals(Mage_Api2_Model_Server::HTTP_BAD_REQUEST, $restResponse->getStatus());
     }
 
     /**
@@ -208,10 +229,15 @@ class Api2_Catalog_Product_AdminTest extends Magento_Test_Webservice_Rest_Admin
      * Create path to resource
      *
      * @param string $id
+     * @param string $storeId
      * @return string
      */
-    protected function _getResourcePath($id)
+    protected function _getResourcePath($id, $storeId = null)
     {
-        return 'products/' . $id;
+        $path = "products/$id";
+        if ($storeId) {
+            $path .= "/store/$storeId";
+        }
+        return $path;
     }
 }
