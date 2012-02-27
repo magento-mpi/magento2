e09bc7abd9a131beee4db7eb049cdab80c8dc1e8
Andrey Tserkus <andrey.tserkus@magento.com>
2012-02-27 13:05:32 -0800

    Fixed MAGETWO-741: Toggling Highlighting of Elements in Visual Design Editor - implemented javascript for toggling highlighting

--- app/code/core/Mage/DesignEditor/view/frontend/js/design_editor.js
+++ app/code/core/Mage/DesignEditor/view/frontend/js/design_editor.js
@@ -56,13 +56,17 @@
     DesignEditor.prototype._init = function () {
         this._dragged = null;
         this._placeholder = null;
+        this._highlightingSelector = null;
+        this._cbHighlightingClick = null;
+        this._highlightingOn = true;
 
         this._templateWrapper = '<div class="vde_block_wrapper" />';
         this._templateBlockTitle = '<div class="vde_block_title">%BLOCK_NAME%</div>';
         this._templatePlaceholder = '<div class="vde_placeholder"></div>';
 
         this._wrapBlocks()
-            ._enableDragging();
+            ._enableDragging()
+            ._triggerStartedEvent();
         return this;
     }
 
@@ -90,6 +94,11 @@
             start: function (event, ui) {thisObj._onDragStarted(event, ui)},
             stop: function (event, ui) {thisObj._onDragStopped(event, ui)}
         });
+        return this;
+    }
+
+    DesignEditor.prototype._triggerStartedEvent = function () {
+        $(document).trigger('started.vde', this);
         return this;
     }
 
@@ -148,6 +157,88 @@
             return this;
         }
         this._placeholder.remove();
+        return this;
+    }
+
+    DesignEditor.prototype.captureHighlighting = function (highlightingId) {
+        this.uncaptureHighlighting()
+            ._initHighlightingCallback();
+
+        this._highlightingSelector = '#' + highlightingId;
+        $(this._highlightingSelector).on('click', null, this._cbHighlightingClick);
+
+        this._syncHighlightingControl();
+        return this;
+    }
+
+    DesignEditor.prototype.uncaptureHighlighting = function () {
+        if (this._highlightingSelector) {
+            $(this._highlightingSelector).off('click', null, this._cbHighlightingClick);
+            this._highlightingSelector = null;
+        }
+        return this;
+    }
+
+    DesignEditor.prototype._initHighlightingCallback = function () {
+        if (!this._cbHighlightingClick) {
+            var thisObj = this;
+            this._cbHighlightingClick = function (event) {
+                thisObj._onHighlightingClick(event);
+            }
+        }
+        return this;
+    }
+
+    DesignEditor.prototype._syncHighlightingControl = function () {
+        var control = $(this._highlightingSelector);
+        if (control.prop('checked') != this._highlightingOn) {
+            control.prop('checked', this._highlightingOn);
+        }
+        return this;
+    }
+
+    DesignEditor.prototype._onHighlightingClick = function (event) {
+        this.highlight($(this._highlightingSelector).prop('checked'));
+        return this;
+    }
+
+    DesignEditor.prototype.highlight = function (isOn) {
+        if (!this._highlightingSelector) {
+            return this;
+        }
+        if (this._highlightingOn != isOn) {
+            if (isOn) {
+                this._turnHighlightingOn();
+            } else {
+                this._turnHighlightingOff();
+            }
+        }
+        this._syncHighlightingControl();
+        return this;
+    }
+
+    DesignEditor.prototype._turnHighlightingOn = function () {
+        $('.vde_block_wrapper').each(function () {
+            var elem = $(this);
+            var children = elem.prop('vdeChildren');
+            elem.show().append(children).removeProp('vdeChildren');
+            elem.children('.vde_block_title').slideDown('fast');
+        });
+
+        this._highlightingOn = true;
+        return this;
+    }
+
+    DesignEditor.prototype._turnHighlightingOff = function () {
+        $('.vde_block_wrapper').each(function () {
+            var elem = $(this);
+            elem.children('.vde_block_title').slideUp('fast', function () {
+                var children = elem.children(':not(.vde_block_title)');
+                elem.after(children).hide().prop('vdeChildren', children);
+            });
+        });
+
+        this._highlightingOn = false;
         return this;
     }
 })(jQuery);
