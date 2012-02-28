5df81d248db5eb62b3d3fe647df1d07229d67b30
Anatoliy Yastreb <ayastreb@ebay.com>
2012-02-28 12:34:17 +0200

    Implemented APIA-342: REST: Resource Product POST - added api implementation for customer & guest - added tests for customer & guest

---
+++ dev/api-tests/integration/tests/Api2/Catalog/Products/CustomerTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Magento
+ * @package     Mage_Catalog
+ * @subpackage  integration_tests
+ * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magento.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Test product resource
+ *
+ * @category    Magento
+ * @package     Magento_Test
+ * @author      Magento Api Team <api-team@magento.com>
+ */
+class Api2_Catalog_Products_CustomerTest extends Magento_Test_Webservice_Rest_Customer
+{
+    /**
+     * Test unsuccessful product create
+     */
+    public function testPost()
+    {
+        $productData = require dirname(__FILE__) . '/../_fixtures/Backend/SimpleProductData.php';
+        $restResponse = $this->callPost('products', $productData);
+        $this->assertEquals(Mage_Api2_Model_Server::HTTP_METHOD_NOT_ALLOWED, $restResponse->getStatus());
+    }
+}
