dea87c5a46cbc052426c44e548eb810fb60b0192
Igor Sidorenko <isidorenko@ebay.com>
2012-02-28 20:05:25 +0200

    Implemented APIA-366: API2 Resource: Customers - Implemented integration tests for admin (partially), customer and guest api types

---
+++ dev/api-tests/integration/tests/Api2/Customer/Customers/AdminTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Magento
+ * @package     Magento_Test
+ * @subpackage  integration_tests
+ * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magento.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Test for customers API2 by admin api user
+ *
+ * @category    Magento
+ * @package     Magento_Test
+ * @author      Magento Api Team <api-team@magento.com>
+ */
+class Api2_Customer_Customers_AdminTest extends Magento_Test_Webservice_Rest_Admin
+{
+    /**
+     * Customer model instance
+     *
+     * @var Mage_Customer_Model_Customer
+     */
+    protected $_customer;
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->_customer = require dirname(__FILE__) . '/../../../../fixtures/Customer/Customer.php';
+    }
+
+    public function testCreate()
+    {
+        $this->_customer->addData(array(
+            'password'   => '123123q',
+            'website_id' => 1,
+            'group_id'   => 1
+        ));
+
+        $response = $this->callPost('customers', $this->_customer->getData());
+        list($customerId) = array_reverse(explode('/', $response->getHeader('Location')));
+
+        /** @var $customer Mage_Customer_Model_Customer */
+        $customer = Mage::getModel('customer/customer')->load($customerId);
+        $this->assertGreaterThan(0, $customer->getId());
+
+        $this->addModelToDelete($customer, true);
+    }
+
+    /**
+     * Test update customer
+     */
+    public function testUpdate()
+    {
+        $response = $this->callPut('customers', array());
+        $this->assertEquals(Mage_Api2_Model_Server::HTTP_METHOD_NOT_ALLOWED, $response->getStatus());
+    }
+
+    /**
+     * Test delete customer
+     */
+    public function testDelete()
+    {
+        $response = $this->callDelete('customers');
+        $this->assertEquals(Mage_Api2_Model_Server::HTTP_METHOD_NOT_ALLOWED, $response->getStatus());
+    }
+}
