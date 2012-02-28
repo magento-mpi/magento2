c68a9865d18e4f78d3f6a4720352f4d846f80c5f
Dmytro Vynograd <dmytro.vynograd@magento.com>
2012-02-28 14:54:23 +0200

    CR Changes MAGE-5941: Notice message disappear after clearing cache - CR-MAGE-221

--- app/code/core/Mage/CatalogRule/Model/Flag.php
+++ app/code/core/Mage/CatalogRule/Model/Flag.php
@@ -24,7 +24,15 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+/**
+ * Flag stores status about availability not applied catalog price rules
+ */
 class Mage_CatalogRule_Model_Flag extends Mage_Core_Model_Flag
 {
+    /**
+     * Flag code
+     *
+     * @var string
+     */
     protected $_flagCode = 'catalog_rules_dirty';
 }
