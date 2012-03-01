e47419c6d8e8097a5926a3f4a0c9500fd48973f7
Igor Sidorenko <isidorenko@ebay.com>
2012-03-01 21:05:48 +0200

    Implemented APIA-366: API2 Resource: Customers - Fixed rights in api2.xml - Added retrieve functionality for admin api role to set 'Last Logged In' attribute

--- app/code/core/Mage/Customer/Model/Api2/Customer/Rest/Admin/V1.php
+++ app/code/core/Mage/Customer/Model/Api2/Customer/Rest/Admin/V1.php
@@ -34,6 +34,28 @@
 class Mage_Customer_Model_Api2_Customer_Rest_Admin_V1 extends Mage_Customer_Model_Api2_Customer_Rest
 {
     /**
+     * Retrieve information about customer
+     * Add last logged in datetime
+     *
+     * @throws Mage_Api2_Exception
+     * @return array
+     */
+    protected function _retrieve()
+    {
+        /** @var $log Mage_Log_Model_Customer */
+        $log = Mage::getModel('log/customer');
+        $log->loadByCustomer($this->getRequest()->getParam('id'));
+
+        $data = parent::_retrieve();
+
+        $lastLoginAt = $log->getLoginAt();
+        if (null !== $lastLoginAt) {
+            $data['last_logged_in'] = $lastLoginAt;
+        }
+        return $data;
+    }
+
+    /**
      * Delete customer
      */
     protected function _delete()
