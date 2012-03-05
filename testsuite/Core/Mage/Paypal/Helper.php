2e71993c07e8679cde34531de3f0f1dd2b1b91f7
E.Maksimenko <emaksimenko@ebay.com>
2012-03-05 16:17:09 +0200

    MAUTOSEL-350 [HELPER][UIMAP][CONF] fixed helper for paypal_developer area

--- testsuite/Core/Mage/Paypal/Helper.php
+++ testsuite/Core/Mage/Paypal/Helper.php
@@ -47,105 +47,141 @@ class Core_Mage_Paypal_Helper extends Mage_Selenium_TestCase
                                     '10' => '10 - October',
                                     '11' => '11 - November',
                                     '12' => '12 - December');
+
+    ################################################################################
+    #                                                                              #
+    #                                   PayPal Developer                           #
+    #                                                                              #
+    ################################################################################
+    /**
+     * Validate paypal Page
+     *
+     * @param string $page
+     */
+    protected function _validatePage($page = '')
+    {
+        if ($page) {
+            $this->assertTrue($this->checkCurrentPage($page), $this->getMessagesOnPage());
+        } else {
+            $page = $this->_findCurrentPageFromUrl();
+        }
+        $expectedTitle = $this->getUimapPage($this->_configHelper->getArea(), $page)->getTitle($this->_paramsHelper);
+        $this->assertSame($expectedTitle, $this->getTitle(), 'Title is unexpected for "' . $page . '" page');
+        $this->setCurrentPage($page);
+    }
+
+    /**
+     * Open paypal tab
+     *
+     * @param string $tabName
+     */
+    public function openPaypalTab($tabName = '')
+    {
+        $page = $this->getUimapPage('paypal_developer', 'paypal_developer_logged_in');
+        $this->click($this->_getControlXpath('tab', $tabName, $page));
+        $this->waitForNewPage();
+        $this->_validatePage();
+    }
+
     /**
      * Log into Paypal developer's site
-     *
-     * @param string|array $parameters
      */
-    public function paypalDeveloperLogin($parameters)
+    public function paypalDeveloperLogin()
     {
-        if (is_string($parameters)) {
-            $parameters = $this->loadData($parameters);
-        }
-        $xpath = $this->getUimapPage('paypal_developer', 'home')->findButton('button_login');
-        if ($this->isElementPresent($xpath)) {
-            $this->validatePage();
-            $this->fillForm($parameters);
-            $this->clickControl('button', 'button_login');
+        $this->goToArea('paypal_developer', 'paypal_developer_home');
+        $loginData = array('login_email'     => $this->_configHelper->getDefaultLogin(),
+                           'login_password'  => $this->_configHelper->getDefaultPassword());
+        if ($this->controlIsPresent('button', 'button_login')) {
+            $this->fillForm($loginData);
+            $this->clickButton('button_login', false);
+            $this->waitForNewPage();
+            $this->_validatePage('paypal_developer_logged_in');
         }
     }
 
     /**
-     * Creates Paypal Sandbox account
+     * Creates preconfigured Paypal Sandbox account
      *
      * @param string|array $parameters
+     *
+     * @return array
      */
-    public function createPaypalSandboxAccount($parameters)
+    public function createPreconfiguredAccount($parameters)
     {
         if (is_string($parameters)) {
-            $parameters = $this->loadData($parameters);
+            $parameters = $this->loadDataSet('Paypal', $parameters);
         }
-        $parameters = $this->arrayEmptyClear($parameters);
+        $this->openPaypalTab('test_accounts');
+        $this->clickControl('link', 'create_preconfigured_account', false);
+        $this->waitForNewPage();
+        $this->_validatePage();
         $this->fillForm($parameters);
         $this->clickButton('create_account', false);
         $this->waitForNewPage();
+        $this->_validatePage('developer_created_test_account_us');
+
+        return $this->getPaypalSandboxAccountInfo($parameters);
+    }
+
+    /**
+     * Gets the email for newly created sandbox account
+     *
+     * @param array $parameters
+     *
+     * @return array
+     */
+    public function getPaypalSandboxAccountInfo(array $parameters)
+    {
+        $this->addParameter('accountEmail', $parameters['login_email']);
+        $detailTable = $this->_getControlXpath('fieldset', 'account_details');
+        $countRows = $this->getXpathCount($detailTable . '//tr');
+        for ($i = 0; $i < $countRows; $i++) {
+            $key = $this->getTable($detailTable . '.' . $i . '.0');
+            $key = preg_replace('/ /', '_', strtolower(trim($key, ':')));
+            $value = $this->getTable($detailTable . '.' . $i . '.2');
+            if ($key == 'credit_card') {
+                $cardData = explode(':', $value);
+                $number = preg_replace('/\D/', '', $cardData[0]);
+                list($expMonth, $expYear) = explode('/', $cardData[1]);
+                $data[$key] = array('card_type'        => $parameters['add_credit_card'],
+                                    'card_number'      => $number,
+                                    'expiration_month' => self::$monthMap[trim($expMonth)],
+                                    'expiration_year'  => $expYear);
+            } else {
+                $data[$key] = $value;
+            }
+        }
+        $data['email'] = trim($this->getText($this->_getControlXpath('pageelement', 'email_account')));
+        return $data;
     }
 
     /**
      * Gets API Credentials for account
      *
      * @param string $email
+     *
      * @return array
      */
     public function getApiCredentials($email)
     {
-        $this->addParameter('emailPart', $email);
-        $pageelements = $this->getCurrentUimapPage()->getAllPageelements();
-        $info = array();
-        foreach ($pageelements as $key => $value) {
-            if ($this->isElementPresent($value)) {
-                $info[$key] = $this->getText($value);
+        $this->addParameter('accountEmail', $email);
+        $this->openPaypalTab('api_credentials');
+        $apiCredentials = array();
+        $detailTable = $this->_getControlXpath('fieldset', 'account_api_credentials');
+        $countRows = $this->getXpathCount($detailTable . '//tr');
+        for ($i = 0; $i < $countRows; $i++) {
+            $key = $this->getTable($detailTable . '.' . $i . '.0');
+            $key = preg_replace('/ /', '_', strtolower(trim($key, ':')));
+            $value = $this->getTable($detailTable . '.' . $i . '.1');
+            if ($key == 'test_account') {
+                $apiCredentials['email_associated_with_paypal_merchant_account'] = trim($value);
+            } elseif ($key == 'signature') {
+                $apiCredentials['api_signature'] = trim($value);
             } else {
-                $this->fail('Could not find element: ' . $key);
-            }
-
-        }
-        return $info;
-    }
-
-    /**
-     * Gets the email for newly created sandbox account
-     *
-     * @param $parameters
-     * @return string
-     */
-    public function getPaypalSandboxAccountInfo($parameters)
-    {
-        $this->addParameter('emailPart', $parameters['login_email']);
-        $info = array();
-        $info['credit_card']['card_type'] = $parameters['add_credit_card'];
-        $pageelements = $this->getCurrentUimapPage()->getAllPageelements();
-        foreach ($pageelements as $key => $value) {
-            if (!$this->isElementPresent($value)) {
-                $this->fail('Could not find element: ' . $value);
-            }
-            switch ($key) {
-                case 'credit_card':
-                    $text = $this->getText($value);
-                    $nodes = explode("\n", $text);
-                    foreach ($nodes as $line) {
-                        $exline = explode(' ', $line);
-                        foreach ($exline as $val) {
-                            if (!preg_match('/([0-9])/', $val)) {
-                                continue;
-                            }
-                            if (preg_match('/Exp Date/', $line)) {
-                                $expDate = explode('/', $val);
-                                $info[$key]['expiration_month'] = self::$monthMap[$expDate[0]];
-                                $info[$key]['expiration_year'] = $expDate[1];
-                            } else {
-                                $info[$key]['card_number'] = $val;
-                            }
-                        }
-                    }
-                    break;
-                case 'email':
-                    $info['email'] = $this->getText($value);
-                default:
-                    break;
+                $apiCredentials[$key] = trim($value);
             }
         }
-        return $info;
+        return $apiCredentials;
     }
 
     /**
@@ -153,10 +189,9 @@ class Core_Mage_Paypal_Helper extends Mage_Selenium_TestCase
      */
     public function deleteAllAccounts()
     {
-        $this->navigate('test_accounts');
-        while ($this->controlIsPresent('button', 'delete')) {
-            $this->navigate('test_accounts');
-            $this->clickButtonAndConfirm('delete', 'confirmation_to_delete_account', false);
+        $this->openPaypalTab('test_accounts');
+        while ($this->controlIsPresent('button', 'delete_account')) {
+            $this->clickButtonAndConfirm('delete_account', 'confirmation_to_delete_account', false);
             $this->waitForNewPage();
         }
     }
@@ -168,24 +203,20 @@ class Core_Mage_Paypal_Helper extends Mage_Selenium_TestCase
      */
     public function deleteAccount($email)
     {
-        $this->navigate('test_accounts');
-        $this->addParameter('emailPart', $email);
-        if ($this->controlIsPresent('checkbox', 'account')) {
-            $this->fillForm(array('account' => 'Yes'));
-            $this->navigate('test_accounts');
-            if ($this->controlIsPresent('button', 'delete')) {
-                $this->clickButtonAndConfirm('delete', 'confirmation_to_delete_account', false);
-                $this->waitForNewPage();
-                $this->navigate('test_accounts');
-            }
+        $this->addParameter('accountEmail', $email);
+        $this->openPaypalTab('test_accounts');
+        if ($this->controlIsPresent('checkbox', 'select_account')) {
+            $this->fillForm(array('select_account' => 'Yes'));
+            $this->clickButtonAndConfirm('delete_account', 'confirmation_to_delete_account', false);
+            $this->waitForNewPage();
         }
-
     }
 
     /**
      * Create Buyers Accounts on PayPal sandbox
      *
      * @param array|string $cards mastercard, visa, discover, amex
+     *
      * @return array $accounts
      * @test
      */
@@ -197,11 +228,8 @@ class Core_Mage_Paypal_Helper extends Mage_Selenium_TestCase
         }
         $accounts = array();
         foreach ($cards as $card) {
-            $this->navigate('create_preconfigured_account');
-            $info = $this->loadData('paypal_sandbox_new_buyer_account_' . $card);
-            $this->createPaypalSandboxAccount($info);
-            $this->navigate('test_accounts');
-            $accounts[$card] = $this->getPaypalSandboxAccountInfo($info);
+            $info = $this->loadDataSet('Paypal', 'paypal_sandbox_new_buyer_account_' . $card);
+            $accounts[$card] = $this->createPreconfiguredAccount($info);
             if ($card != 'amex') {
                 $accounts[$card]['credit_card']['card_verification_number'] = '111';
             } else {
@@ -211,28 +239,11 @@ class Core_Mage_Paypal_Helper extends Mage_Selenium_TestCase
         return $accounts;
     }
 
-    /**
-     * Create Pro Merchant Account on PayPal sandbox
-     *
-     * @param string|array $accountData
-     * @return array
-     * @test
-     */
-    public function createPayPalProAccount($accountData)
-    {
-        if (is_string($accountData)) {
-            $accountData = $this->loadData($accountData);
-        }
-        $accountData = $this->arrayEmptyClear($accountData);
-        $this->navigate('create_preconfigured_account');
-        $this->createPaypalSandboxAccount($accountData);
-        $this->navigate('test_accounts');
-        $info = $this->getPaypalSandboxAccountInfo($accountData);
-        $this->navigate('api_credentials');
-
-        return $this->getApiCredentials($info['email']);
-    }
-
+    ################################################################################
+    #                                                                              #
+    #                 PayPal Sandbox(@TODO check and rewrite)                      #
+    #                                                                              #
+    ################################################################################
     /**
      * Login using sandbox account
      * Function has not been verified and is not used right now
@@ -248,7 +259,7 @@ class Core_Mage_Paypal_Helper extends Mage_Selenium_TestCase
         $xpath = $this->getUimapPage('paypal_sandbox', 'paypal_sandbox')->findButton('button_login');
         if ($this->isElementPresent($xpath)) {
             $this->addParameter('pageTitle', $parameters['page_title']);
-            $this->validatePage();
+            $this->_validatePage();
             $this->fillForm($parameters['credentials']);
             $this->clickControl('button', 'button_login');
         }
@@ -267,7 +278,7 @@ class Core_Mage_Paypal_Helper extends Mage_Selenium_TestCase
             $parameters = $this->loadData($parameters);
         }
         $this->addParameter('pageTitle', $parameters['page_title']);
-        $this->validatePage();
+        $this->_validatePage();
         $this->fillForm($parameters['credentials']);
         $this->clickControl('button', 'button_login');
         $this->clickControl('button', 'button_iagree');
@@ -288,12 +299,12 @@ class Core_Mage_Paypal_Helper extends Mage_Selenium_TestCase
         $xpath = $this->getUimapPage('paypal_sandbox', 'paypal_sandbox')->findButton('button_login');
         if (!$this->isElementPresent($xpath)) {
             $this->addParameter('pageTitle', $parameters['page_title_pay_with']);
-            $this->validatePage();
+            $this->_validatePage();
             $this->addParameter('pageTitle', $parameters['page_title']);
             $this->clickControl('link', 'have_paypal_account');
         } else {
             $this->addParameter('pageTitle', $parameters['page_title']);
-            $this->validatePage();
+            $this->_validatePage();
         }
         $this->fillForm($parameters['credentials']);
         $this->addParameter('pageTitle', $parameters['page_title_review_info']);
