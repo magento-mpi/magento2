10bef5d34e74539a3516a19387254b502c859c96
Vlad Veselov <vveselov@ebay.com>
2012-03-07 15:19:53 +0200

    CR Change MAGE-6332: Customer Attributes and Customer Address Attributes validation

--- app/code/core/Mage/Customer/Helper/Address.php
+++ app/code/core/Mage/Customer/Helper/Address.php
@@ -180,7 +180,7 @@ class Mage_Customer_Helper_Address extends Mage_Core_Helper_Abstract
         $class = $attribute ? $attribute->getFrontend()->getClass() : '';
 
         if ($attributeCode === 'firstname' || $attributeCode === 'middlename' || $attributeCode === 'lastname') {
-            if (!$attribute->getIsVisible()) {
+            if ($class && !$attribute->getIsVisible()) {
                 $class = ''; // address attribute is not visible thus its validation rules are not applied
             }
 
