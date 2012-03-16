3046bb09e904686e92f5f60cc9bdf8267b004b1d
Vlad Veselov <vveselov@ebay.com>
2012-03-16 19:47:31 +0200

    Fixed MAGE-4665: FF Backend Email doesn't send if "Sender Name" field input more than 1000 characters

--- app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Email/Sender.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Email/Sender.php
@@ -30,15 +30,29 @@
  *
  * @category   Mage
  * @package    Mage_Adminhtml
- * @author      Magento Core Team <core@magentocommerce.com>
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Adminhtml_Model_System_Config_Backend_Email_Sender extends Mage_Core_Model_Config_Data
 {
+    /**
+     * Check sender name validity
+     *
+     * @return Mage_Adminhtml_Model_System_Config_Backend_Email_Sender
+     */
     protected function _beforeSave()
     {
         $value = $this->getValue();
         if (!preg_match("/^[\S ]+$/", $value)) {
             Mage::throwException(Mage::helper('adminhtml')->__('Invalid sender name "%s". Please use only visible characters and spaces.', $value));
+        }
+
+        /**
+         * @see Zend_Mail_Transport_Abstract::_prepareHeaders()
+         * Maximum mail header line length is 998, so the boundary case is
+         * From: {Sender Name - 735 characters including braces} <{Email - 254 characters including braces}>
+         */
+        if (strlen($value) > 735) {
+            Mage::throwException(Mage::helper('adminhtml')->__('Maximum sender name length is 735. Please correct your settings.'));
         }
         return $this;
     }
