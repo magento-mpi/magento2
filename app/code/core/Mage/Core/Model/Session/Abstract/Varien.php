61a55b0718caddaa0ac3146c814dea0d7fc9d81f
Andrey Konosov <andrey.konosov@magento.com>
2012-03-30 19:53:54 +0300

    Fixed MAGE-5487: [SAASMERGE] Add support of memcached php extension

--- app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
+++ app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
@@ -41,7 +41,7 @@ class Mage_Core_Model_Session_Abstract_Varien extends Varien_Object
      */
     public function start($sessionName=null)
     {
-        if (isset($_SESSION)) {
+        if (isset($_SESSION) && !$this->getSkipEmptySessionCheck()) {
             return $this;
         }
 
