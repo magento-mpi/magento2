67ff565d246944bd2e89963cbb25008c93f96289
Sergey Shymko <sergey.shymko@magento.com>
2012-04-03 11:47:48 -0700

    Fixed MAGETWO-837: Area is Set to Layout Incorrectly in Mage_Core_Model_Email_Template_Filter - fixed area passing in Mage_Core_Model_Email_Template_Filter::layoutDirective() - covered layout directive by integration tests

--- app/code/core/Mage/Core/Model/Email/Template/Filter.php
+++ app/code/core/Mage/Core/Model/Email/Template/Filter.php
@@ -180,14 +180,12 @@ class Mage_Core_Model_Email_Template_Filter extends Varien_Filter_Template
         $skipParams = array('handle', 'area');
 
         $params = $this->_getIncludeParameters($construction[2]);
-        $layout = Mage::getModel('Mage_Core_Model_Layout');
-        /* @var $layout Mage_Core_Model_Layout */
+        $layoutParams = array();
         if (isset($params['area'])) {
-            $layout->setArea($params['area']);
+            $layoutParams['area'] = $params['area'];
         }
-        else {
-            $layout->setArea(Mage::app()->getLayout()->getArea());
-        }
+        /** @var $layout Mage_Core_Model_Layout */
+        $layout = Mage::getModel('Mage_Core_Model_Layout', $layoutParams);
 
         $layout->getUpdate()->addHandle($params['handle']);
         $layout->getUpdate()->load();
