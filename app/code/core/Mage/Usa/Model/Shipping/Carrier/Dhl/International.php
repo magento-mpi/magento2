21a12d854d543951943f04dfcbe5d65d382807ac
Roman Stepanov <roman.stepanov@magento.com>
2012-04-03 19:20:21 +0300

    Fixed MAGE-6740: Reference and contents information absent in the shipping label (new DHL)

--- app/code/core/Mage/Usa/Model/Shipping/Carrier/Dhl/International.php
+++ app/code/core/Mage/Usa/Model/Shipping/Carrier/Dhl/International.php
@@ -909,7 +909,9 @@ class Mage_Usa_Model_Shipping_Carrier_Dhl_International
                             $result->setTrackingNumber((string)$xml->AirwayBillNumber);
                             try {
                                 /* @var $pdf Mage_Usa_Model_Shipping_Carrier_Dhl_Label_Pdf */
-                                $pdf = Mage::getModel('usa/shipping_carrier_dhl_label_pdf', array('info' => $xml));
+                                $pdf = Mage::getModel('usa/shipping_carrier_dhl_label_pdf', array('info' => $xml,
+                                    'request' => $this->_request
+                                ));
                                 $result->setShippingLabelContent($pdf->render());
                             } catch (Exception $e) {
                                 Mage::throwException(Mage::helper('usa')->__($e->getMessage()));
--- app/code/core/Mage/Usa/Model/Shipping/Carrier/Dhl/Label/Pdf.php
+++ app/code/core/Mage/Usa/Model/Shipping/Carrier/Dhl/Label/Pdf.php
@@ -39,13 +39,19 @@ class Mage_Usa_Model_Shipping_Carrier_Dhl_Label_Pdf
     protected $_info;
 
     /**
+     * @var Mage_Shipping_Model_Shipment_Request
+     */
+    protected $_request;
+
+    /**
      * Dhl International Label Creation Class constructor
      *
      * @param array $arguments
      */
     public function __construct(array $arguments)
     {
-        $this->_info = $arguments['info'];
+        $this->_info    = $arguments['info'];
+        $this->_request = $arguments['request'];
     }
 
     /**
@@ -77,7 +83,7 @@ class Mage_Usa_Model_Shipping_Carrier_Dhl_Label_Pdf
             )
             ->addServiceFeaturesCodes()
             ->addDeliveryDateCode()
-            ->addShipmentInformation()
+            ->addShipmentInformation($this->_request->getOrderShipment())
             ->addDateInfo($this->_info->ShipmentDate)
             ->addWeightInfo((string)$this->_info->ChargeableWeight, (string)$this->_info->WeightUnit)
             ->addWaybillBarcode((string)$this->_info->AirwayBillNumber, (string)$this->_info->Barcodes->AWBBarCode)
@@ -88,16 +94,20 @@ class Mage_Usa_Model_Shipping_Carrier_Dhl_Label_Pdf
             )
             ->addBorder();
 
+        $packges = array_values($this->_request->getPackages());
+        $i = 0;
         foreach ($this->_info->Pieces->Piece as $piece) {
             $page = new Mage_Usa_Model_Shipping_Carrier_Dhl_Label_Pdf_Page($template);
             $pdfBuilder->setPage($page)
                 ->addPieceNumber((int)$piece->PieceNumber, (int)$this->_info->Piece)
+                ->addContentInfo($packges[$i])
                 ->addPieceIdBarcode(
                     (string)$piece->DataIdentifier,
                     (string)$piece->LicensePlate,
                     (string)$piece->LicensePlateBarCode
                 );
             array_push($pdf->pages, $page);
+            $i++;
         }
         return $pdf->render();
     }
