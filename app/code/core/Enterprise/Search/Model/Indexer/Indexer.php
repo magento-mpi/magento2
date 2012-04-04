cf302008b3fe8eb869a97ea08ec9dde933dd07a1
Igor Minyaylo <iminiailo@ebay.com>
2012-04-04 14:20:30 +0300

    Fixed MAGE-6744: Solr Catalog Search reindexation by cron omits indexation mode settings

--- app/code/core/Enterprise/Search/Model/Indexer/Indexer.php
+++ app/code/core/Enterprise/Search/Model/Indexer/Indexer.php
@@ -75,7 +75,7 @@ class Enterprise_Search_Model_Indexer_Indexer
     public function reindexAll()
     {
         $helper = Mage::helper('enterprise_search');
-        if ($helper->isThirdPartSearchEngine() && $helper->isActiveEngine()) {
+        if ($helper->isThirdPartyEngineAvailable()) {
             /* Change index status to running */
             $indexProcess = Mage::getSingleton('index/indexer')->getProcessByCode('catalogsearch_fulltext');
             if ($indexProcess) {
