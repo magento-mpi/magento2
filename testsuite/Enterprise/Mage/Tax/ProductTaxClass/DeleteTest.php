1499fe0f2194e405ee3461afbefd8bb508f1dc12
Maksym Aposov <maposov@ebay.com>
2012-04-11 18:16:33 +0200

    [UIMAP] MAUTOSEL-415 Rename classes

--- testsuite/Enterprise/Mage/Tax/ProductTaxClass/DeleteTest.php
+++ testsuite/Enterprise/Mage/Tax/ProductTaxClass/DeleteTest.php
@@ -33,7 +33,7 @@
  * @subpackage  tests
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Core_Mage_Tax_ProductTaxClass_DeleteTest extends Mage_Selenium_TestCase
+class Enterprise_Mage_Tax_ProductTaxClass_DeleteTest extends Mage_Selenium_TestCase
 {
     /**
      * <p>Save rule name for clean up</p>
