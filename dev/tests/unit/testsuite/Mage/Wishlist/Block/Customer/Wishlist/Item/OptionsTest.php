c7612a272539fa09eb99e15d0e99fbff65dd6c9c
Olga Kopylova <okopylova@ebay.com>
2012-04-20 16:33:37 -0700

    Fixed MAGETWO-894: Update from Magento 1 (30-1)

---
+++ dev/tests/unit/testsuite/Mage/Wishlist/Block/Customer/Wishlist/Item/OptionsTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Wishlist
+ * @subpackage  unit_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * @group module:Mage_Wishlist
+ */
+/**
+ * Test class for Mage_Wishlist_Block_Customer_Wishlist_Item_Options.
+ */
+class Mage_Wishlist_Block_Customer_Wishlist_Item_OptionsTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetTemplate()
+    {
+        $block = new Mage_Wishlist_Block_Customer_Wishlist_Item_Options;
+        $this->assertEmpty($block->getTemplate());
+        $product = new Varien_Object(array('type_id' => 'test'));
+        $item = new Varien_Object(array('product' => $product));
+        $block->setItem($item);
+        $this->assertNotEmpty($block->getTemplate());
+        $block->setTemplate('template');
+        $this->assertEquals('template', $block->getTemplate());
+    }
+}
