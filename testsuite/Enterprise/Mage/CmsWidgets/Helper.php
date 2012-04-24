cd3afd1ca5e6e7825000527678bccfd77d4fbc8c
E.Maksimenko <emaksimenko@ebay.com>
2012-04-24 12:30:15 +0300

    [HELPER] fixed CmsWidgets Helper for EE

---
+++ testsuite/Enterprise/Mage/CmsWidgets/Helper.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    tests
+ * @package     selenium
+ * @subpackage  tests
+ * @author      Magento Core Team <core@magentocommerce.com>
+ * @copyright   Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Helper class
+ *
+ * @package     selenium
+ * @subpackage  tests
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Enterprise_Mage_CmsWidgets_Helper extends Core_Mage_CmsWidgets_Helper
+{
+    /**
+     * Fills settings for creating widget
+     *
+     * @param array $settings
+     */
+    public function fillWidgetSettings(array $settings)
+    {
+        if ($settings) {
+            $xpath = $this->_getControlXpath('dropdown', 'type');
+            $type = $this->getValue($xpath . '/option[text()="' . $settings['type'] . '"]');
+            $this->addParameter('type', str_replace('/', '-', $type));
+            $packageTheme = array_map('trim', (explode('/', $settings['design_package_theme'])));
+            $this->addParameter('package', $packageTheme[0]);
+            $this->addParameter('theme', $packageTheme[1]);
+            $this->fillFieldset($settings, 'settings_fieldset');
+        }
+        $this->clickButton('continue', false);
+        $this->pleaseWait();
+        $this->validatePage('add_widget_options');
+    }
+}
