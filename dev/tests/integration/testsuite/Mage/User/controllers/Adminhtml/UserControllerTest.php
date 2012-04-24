79b9f35a78cb21ce284b18ccefc4a0ac7ec72b6f
Alexey Korshenko <okorhsnko@ebay.com>
2012-04-24 20:19:19 +0300

    Implemented  MAGETWO-877: UI of Users and Roles Management Moving into URMM - fixed integration tests

--- dev/tests/integration/testsuite/Mage/User/controllers/Adminhtml/UserControllerTest.php
+++ dev/tests/integration/testsuite/Mage/User/controllers/Adminhtml/UserControllerTest.php
@@ -3,23 +3,23 @@
  * {license_notice}
  *
  * @category    Mage
- * @package     Mage_Adminhtml
+ * @package     Mage_User
  * @subpackage  integration_tests
  * @copyright   {copyright}
  * @license     {license_link}
  */
 
 /**
- * @group module:Mage_Adminhtml
+ * @group module:Mage_User
  */
-class Mage_Adminhtml_Permissions_UserControllerTest extends Mage_Adminhtml_Utility_Controller
+class Mage_User_Adminhtml_UserControllerTest extends Mage_Adminhtml_Utility_Controller
 {
     /**
      * @covers Mage_Adminhtml_Controller_Action::_addContent
      */
     public function testIndexAction()
     {
-        $this->dispatch('admin/permissions_user/index');
+        $this->dispatch('admin/user/index');
         $this->assertStringMatchesFormat('%a<div class="content-header">%aUsers%a', $this->getResponse()->getBody());
     }
 }
