f1595705e9d8a8ff93170c4246f206d8c100ea3b
E <Maksimenko>
2012-04-25 17:57:42 +0300

    [TESTS] added annotation @group skip_due_to_bug1.12

--- testsuite/Core/Mage/CheckoutOnePage/WithRegistration/CheckingValidationTest.php
+++ testsuite/Core/Mage/CheckoutOnePage/WithRegistration/CheckingValidationTest.php
@@ -379,6 +379,7 @@ class Core_Mage_CheckoutOnePage_WithRegistration_CheckingValidationTest extends 
      * @dataProvider addressLongValuesDataProvider
      * @depends preconditionsForTests
      * @TestlinkId TL-MAGE-3595
+     * @group skip_due_to_bug1.12
      */
     public function billingAddressLongValues($field, $fieldName, $simpleSku)
     {
@@ -414,6 +415,7 @@ class Core_Mage_CheckoutOnePage_WithRegistration_CheckingValidationTest extends 
      * @dataProvider addressLongValuesDataProvider
      * @depends preconditionsForTests
      * @TestlinkId TL-MAGE-5311
+     * @group skip_due_to_bug1.12
      */
     public function shippingAddressLongValues($field, $fieldName, $simpleSku)
     {
