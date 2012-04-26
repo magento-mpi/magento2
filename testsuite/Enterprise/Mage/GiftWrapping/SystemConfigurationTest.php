63609682c6bfb0c8063dac85f259ba3f390b6a06
Kateryna Muntianu <kmuntianu@ebay.com>
2012-04-26 14:48:28 +0300

    [TESTS][HELPER][UIMAP] MAUTOSEL-425 - Fixed CreateTest - Updated CreateTest, DeleteTest to use loadDataSet - Refactored SystemConfigurationTest

--- testsuite/Enterprise/Mage/GiftWrapping/SystemConfigurationTest.php
+++ testsuite/Enterprise/Mage/GiftWrapping/SystemConfigurationTest.php
@@ -35,114 +35,59 @@
  */
 class Enterprise_Mage_GiftWrapping_SystemConfigurationTest extends Mage_Selenium_TestCase
 {
-
     /**
      * <p>Preconditions:</p>
      * <p>Log in to Backend.</p>
      * <p>Navigate to System - System Configuration</p>
      */
-    protected function assertPreConditions()
+    public function setUpBeforeTests()
     {
         $this->loginAdminUser();
         $this->navigate('system_configuration');
     }
 
     /**
-     * <p>Test Case TL-MAGE-829: Enabling Gift Messages and Gift Wrapping options - Default scope</p>
+     * <p>Enabling/Disabling Gift Messages and Gift Wrapping options on Default and Website scope</p>
      * <p>Steps:</p>
      * <p>1. Log into backend;</p>
      * <p>2. Go to Sales page (System Configuration - Sales - Sales - Gift Options);</p>
-     * <p>3. Switch "Allow Gift Messages for Order Items" to "Yes";</p>
-     * <p>4. Switch "Allow Gift Wrapping on Order Level" to "Yes";</p>
-     * <p>5. Switch "Allow Gift Wrapping for Order Items" to "Yes";</p>
-     * <p>6. Switch "Allow Gift Receipt" to "Yes";</p>
-     * <p>7. Switch "Allow Printed Card" to "Yes";</p>
-     * <p>8. Switch "Allow Gift Messages on Order Level" to "Yes";</p>
-     * <p>9. Save Configuration.</p>
+     * <p>3. Switch to website scope when needed;</p>
+     * <p>4. Switch "Allow Gift Messages for Order Items", "Allow Gift Wrapping on Order Level",
+     * "Allow Gift Wrapping for Order Items", "Allow Gift Receipt",
+     * "Allow Printed Card", "Allow Gift Messages on Order Level" - all to "Yes"/"No".
+     * <p>5. Save Configuration.</p>
      * <p>Expected Results:</p>
-     * <p>1. Received the message "The configuration has been saved" on successful switching of all mentioned options in steps 3-8;</p>
-     * <p>2. All mentioned fields in steps 3-8 switched to "Yes".</p>
+     * <p>1. Received the message "The configuration has been saved"</p>
+     * <p>2. All changed field values are correctly set.</p>
      *
      * @test
+     * @param string $settings Name of the dataset with settings
+     * @dataProvider changeGiftOptionsSettingsDataProvider
+     *
+     * @TestlinkId TL-MAGE-829
+     * @TestlinkId TL-MAGE-839
+     * @TestlinkId TL-MAGE-841
+     * @TestlinkId TL-MAGE-843
      */
-    public function enableOptionsDefaultScope()
+    public function changeGiftOptionsSettings($settings)
     {
-        $this->systemConfigurationHelper()->configure('gift_wrapping_all_enable');
-        $this->systemConfigurationHelper()->configure('gift_message_all_enable');
+        $settings = $this->loadDataSet('GiftWrapping', $settings);
+        $this->systemConfigurationHelper()->configure($settings);
+        $this->assertTrue($this->verifyForm($settings['tab_1']['configuration']),
+                                            $settings['tab_1']['tab_name']);
     }
 
-    /**
-     * <p>Test Case TL-MAGE-839: Disabling Gift Messages and Gift Wrapping options - Default scope</p>
-     * <p>Steps:</p>
-     * <p>1. Log into backend;</p>
-     * <p>2. Go to Sales page (System Configuration - Sales - Sales - Gift Options);</p>
-     * <p>3. Switch "Allow Gift Messages for Order Items" to "No";</p>
-     * <p>4. Switch "Allow Gift Wrapping on Order Level" to "No";</p>
-     * <p>5. Switch "Allow Gift Wrapping for Order Items" to "No";</p>
-     * <p>6. Switch "Allow Gift Receipt" to "No";</p>
-     * <p>7. Switch "Allow Printed Card" to "No";</p>
-     * <p>8. Switch "Allow Gift Messages on Order Level" to "No";</p>
-     * <p>9. Save Configuration.</p>
-     * <p>Expected Results:</p>
-     * <p>1. Received the message "The configuration has been saved" on successful switching of all mentioned options in steps 3-8;</p>
-     * <p>2. All mentioned fields in steps 3-8 switched to "No".</p>
-     *
-     * @test
-     */
-    public function disableOptionsDefaultScope()
+    public function changeGiftOptionsSettingsDataProvider()
     {
-        $this->systemConfigurationHelper()->configure('gift_wrapping_all_disable');
-        $this->systemConfigurationHelper()->configure('gift_message_all_disable');
-    }
-
-    /**
-     * <p>Test Case TL-MAGE-841: Enabling Gift Messages and Gift Wrapping options - Website scope</p>
-     * <p>Steps:</p>
-     * <p>1. Log into backend;</p>
-     * <p>2. Go to Sales page (System Configuration - Sales - Sales - Gift Options);</p>
-     * <p>3. Switch to website scope;</p>
-     * <p>4. Switch "Allow Gift Messages for Order Items" to "Yes";</p>
-     * <p>5. Switch "Allow Gift Wrapping on Order Level" to "Yes";</p>
-     * <p>6. Switch "Allow Gift Wrapping for Order Items" to "Yes";</p>
-     * <p>7. Switch "Allow Gift Receipt" to "Yes";</p>
-     * <p>8. Switch "Allow Printed Card" to "Yes";</p>
-     * <p>9. Switch "Allow Gift Messages on Order Level" to "Yes";</p>
-     * <p>10. Save Configuration.</p>
-     * <p>Expected Results:</p>
-     * <p>1. Received the message "The configuration has been saved" on successful switching of all mentioned options in steps 4-9;</p>
-     * <p>2. All mentioned fields in steps 4-9 switched to "Yes".</p>
-     *
-     * @test
-     */
-    public function enableOptionsWebsiteScope()
-    {
-        $this->systemConfigurationHelper()->configure('gift_wrapping_all_enable_on_website');
-        $this->systemConfigurationHelper()->configure('gift_message_all_enable_on_website');
-    }
-
-    /**
-     * <p>Test Case TL-MAGE-843: Disabling Gift Messages and Gift Wrapping options - Website scope + Price</p>
-     * <p>Steps:</p>
-     * <p>1. Log into backend;</p>
-     * <p>2. Go to Sales page (System Configuration - Sales - Sales - Gift Options);</p>
-     * <p>3. Switch to website scope;</p>
-     * <p>4. Switch "Allow Gift Messages for Order Items" to "No";</p>
-     * <p>5. Switch "Allow Gift Wrapping on Order Level" to "No";</p>
-     * <p>6. Switch "Allow Gift Wrapping for Order Items" to "No";</p>
-     * <p>7. Switch "Allow Gift Receipt" to "No";</p>
-     * <p>8. Switch "Allow Printed Card" to "No";</p>
-     * <p>9. Switch "Allow Gift Messages on Order Level" to "No";</p>
-     * <p>10. Enter some price (for example "18.53") in the "Default Price for Printed Card" field;</p>
-     * <p>11. Save Configuration.</p>
-     * <p>Expected Results:</p>
-     * <p>1. Received the message "The configuration has been saved" on successful switching of all mentioned options in steps 4-10;</p>
-     * <p>2. All mentioned fields in steps 4-10 switched to "No".</p>
-     *
-     * @test
-     */
-    public function disableOptionsWebsiteScope()
-    {
-        $this->systemConfigurationHelper()->configure('gift_wrapping_all_disable_on_website');
-        $this->systemConfigurationHelper()->configure('gift_message_all_disable_on_website');
+        return array(
+            array('gift_wrapping_all_enable'),
+            array('gift_message_all_enable'),
+            array('gift_wrapping_all_disable'),
+            array('gift_message_all_disable'),
+            array('gift_wrapping_all_enable_on_website'),
+            array('gift_message_all_enable_on_website'),
+            array('gift_wrapping_all_disable_on_website'),
+            array('gift_message_all_disable_on_website'),
+        );
     }
 }
