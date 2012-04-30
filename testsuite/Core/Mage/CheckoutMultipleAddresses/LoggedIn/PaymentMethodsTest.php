fd619ab9f66becc96cd0e332e8bcfe26a24cc1b8
Kateryna Muntianu <kmuntianu@ebay.com>
2012-04-30 13:32:11 +0300

    [TESTS][DATA] MAUTOSEL-425 - Fixed all CheckoutMultipleAddresses/*/PaymentMethodsTest

--- testsuite/Core/Mage/CheckoutMultipleAddresses/LoggedIn/PaymentMethodsTest.php
+++ testsuite/Core/Mage/CheckoutMultipleAddresses/LoggedIn/PaymentMethodsTest.php
@@ -117,7 +117,7 @@ class Core_Mage_CheckoutMultipleAddresses_LoggedIn_PaymentMethodsTest extends Ma
         //Data
         $paymentData = $this->loadDataSet('Payment', 'payment_' . $payment);
         $checkoutData = $this->loadDataSet('MultipleAddressesCheckout', 'multiple_with_signed_in',
-                                           array('payment' => $paymentData),
+                                           array('payment_data' => $paymentData),
                                            $testData['products']);
         if ($payment != 'checkmoney') {
             if ($payment != 'payflowpro') {
@@ -192,7 +192,7 @@ class Core_Mage_CheckoutMultipleAddresses_LoggedIn_PaymentMethodsTest extends Ma
         //Data
         $paymentData = $this->loadDataSet('Payment', 'payment_' . $payment);
         $checkoutData = $this->loadDataSet('MultipleAddressesCheckout', 'multiple_with_signed_in',
-                                           array('payment' => $paymentData),
+                                           array('payment_data' => $paymentData),
                                            $testData['products']);
         $paymentConfig = $this->loadDataSet('PaymentMethod', $payment . '_with_3Dsecure');
         //Steps
