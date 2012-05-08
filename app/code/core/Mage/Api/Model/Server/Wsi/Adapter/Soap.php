65828daa68d8978cc930720028449fe7c2fd5a96
Andrii Kasian <akasian@ebay.com>
2012-05-08 12:24:23 -0400

    Implemented MAGETWO-1075: Add test to check class present in file to static tests

--- app/code/core/Mage/Api/Model/Server/Wsi/Adapter/Soap.php
+++ app/code/core/Mage/Api/Model/Server/Wsi/Adapter/Soap.php
@@ -15,7 +15,7 @@
  * @package    Mage_Api
  * @author     Magento Core Team <core@magentocommerce.com>
  */
-class Mage_Api_Model_Server_WSI_Adapter_Soap extends Mage_Api_Model_Server_Adapter_Soap
+class Mage_Api_Model_Server_Wsi_Adapter_Soap extends Mage_Api_Model_Server_Adapter_Soap
 {
     /**
      * Run webservice
