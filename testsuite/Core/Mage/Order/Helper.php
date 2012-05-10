9f46169c6fd0850ae3a1f61308088f29fbae11a2
E.Maksimenko <emaksimenko@ebay.com>
2012-05-10 12:55:23 +0300

    [HELPER] fixed Screenshot Name in helper methods

--- testsuite/Core/Mage/Order/Helper.php
+++ testsuite/Core/Mage/Order/Helper.php
@@ -473,7 +473,14 @@ class Core_Mage_Order_Helper extends Mage_Selenium_TestCase
             if ($this->isElementPresent($methodUnavailable) || $this->isElementPresent($noShipping)) {
                 if ($validate) {
                     //@TODO Remove workaround for getting fails, not skipping tests if shipping methods are not available
-                    $url = $this->takeScreenshot();
+                    $name = '';
+                    foreach (debug_backtrace() as $line) {
+                        if (preg_match('/Test$/', $line['class'])) {
+                            $name = time() . '-' . $line['class'] . '-' . $line['function'];
+                            break;
+                        }
+                    }
+                    $url = $this->takeScreenshot($name);
                     $this->markTestSkipped($url . 'Shipping Service "' . $shipService . '" is currently unavailable.');
                     //$this->addVerificationMessage('Shipping Service "' . $shipService . '" is currently unavailable.');
                 }
@@ -488,7 +495,14 @@ class Core_Mage_Order_Helper extends Mage_Selenium_TestCase
                 }
             } elseif ($validate) {
                 //@TODO Remove workaround for getting fails, not skipping tests if shipping methods are not available
-                $url = $this->takeScreenshot();
+                $name = '';
+                foreach (debug_backtrace() as $line) {
+                    if (preg_match('/Test$/', $line['class'])) {
+                        $name = time() . '-' . $line['class'] . '-' . $line['function'];
+                        break;
+                    }
+                }
+                $url = $this->takeScreenshot($name);
                 $this->markTestSkipped($url . $shipService . ': This shipping method is currently not displayed');
                 //$this->addVerificationMessage($shipService . ': This shipping method is currently not displayed');
             }
