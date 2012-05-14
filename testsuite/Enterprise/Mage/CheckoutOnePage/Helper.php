7176982c8c70074e54559ce9f55c3efbd42204d5
E.Maksimenko <emaksimenko@ebay.com>
2012-05-14 17:09:40 +0300

    [HELPER] fixed function submitOnePageCheckoutOrder() for Enterprise

---
+++ testsuite/Enterprise/Mage/CheckoutOnePage/Helper.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    tests
+ * @package     selenium
+ * @subpackage  tests
+ * @author      Magento Core Team <core@magentocommerce.com>
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Helper class Core_Mage_for OnePageCheckout
+ *
+ * @package     selenium
+ * @subpackage  tests
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Enterprise_Mage_CheckoutOnePage_Helper extends Core_Mage_CheckoutOnePage_Helper
+{
+    /**
+     * @return string
+     */
+    public function submitOnePageCheckoutOrder()
+    {
+        $this->clickButton('place_order', false);
+        $this->waitForElement(array($this->_getMessageXpath('success_checkout'),
+                                    $this->_getMessageXpath('general_error'),
+                                    $this->_getMessageXpath('general_validation')));
+        $this->waitForTextNotPresent('Submitting order information.');
+        $this->assertMessageNotPresent('error');
+        $this->waitForTextPresent('Thank you for your purchase!');
+        $this->validatePage('onepage_checkout_success');
+        $xpath = $this->_getControlXpath('link', 'order_number');
+        if ($this->isElementPresent($xpath)) {
+            return $this->getText($xpath);
+        }
+
+        return preg_replace('/[^0-9]/', '', $this->getText("//*[contains(text(),'Your order')]"));
+    }
+}
