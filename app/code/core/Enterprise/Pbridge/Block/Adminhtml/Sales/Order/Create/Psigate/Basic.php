423f326d5571e62ae87b655a0c7e32031c18355f
Dmitriy Volik <dmitriy.volik@magento.com>
2012-05-15 17:26:53 +0300

    Implemented MAGETWO-1145: Merge PsiGate (MAGETWO-1134 Accept and merge PBridge) - fixed hook 120

--- app/code/core/Enterprise/Pbridge/Block/Adminhtml/Sales/Order/Create/Psigate/Basic.php
+++ app/code/core/Enterprise/Pbridge/Block/Adminhtml/Sales/Order/Create/Psigate/Basic.php
@@ -31,7 +31,8 @@
  * @package     Enterprise_Pbridge
  * @author      Magento Core Team <core@magentocommerce.com>
  */
-class Enterprise_Pbridge_Block_Adminhtml_Sales_Order_Create_Psigate_Basic extends Enterprise_Pbridge_Block_Adminhtml_Sales_Order_Create_Abstract
+class Enterprise_Pbridge_Block_Adminhtml_Sales_Order_Create_Psigate_Basic
+    extends Enterprise_Pbridge_Block_Adminhtml_Sales_Order_Create_Abstract
 {
     /**
      * Psigate payment code
