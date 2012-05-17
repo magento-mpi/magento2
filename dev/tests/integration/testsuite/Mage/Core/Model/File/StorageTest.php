eb0720e5ae5aa4f71756455d2be912823bddf88c
maksym.moskvychev <maksym.moskvychev@magento1-server.local>
2012-05-17 14:51:33 +0000

    Fixed MAGETWO-1092: CDN: Magento design becomes broken after selecting database as media storage

--- dev/tests/integration/testsuite/Mage/Core/Model/File/StorageTest.php
+++ dev/tests/integration/testsuite/Mage/Core/Model/File/StorageTest.php
@@ -1,8 +1,32 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: maksym.moskvychev
- * Date: 16/05/12
- * Time: 18:37
- * To change this template use File | Settings | File Templates.
- */
+    /**
+     * {license_notice}
+     *
+     * @category    Magento
+     * @package     Mage_Core
+     * @subpackage  integration_tests
+     * @copyright   {copyright}
+     * @license     {license_link}
+     */
+
+class Mage_Core_Model_File_StorageTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @magentoConfigFixture current_store system/media_storage_configuration/configuration_update_time 1000
+     */
+    public function testGetScriptConfig()
+    {
+        $config = Mage_Core_Model_File_Storage::getScriptConfig();
+        $this->assertInternalType('array', $config);
+        $this->assertArrayHasKey('media_directory', $config);
+        $this->assertArrayHasKey('allowed_resources', $config);
+        $this->assertArrayHasKey('update_time', $config);
+        $this->assertEquals(Mage::getBaseDir('media'), $config['media_directory']);
+        $this->assertInternalType('array', $config['allowed_resources']);
+        $this->assertContains('css', $config['allowed_resources']);
+        $this->assertContains('css_secure', $config['allowed_resources']);
+        $this->assertContains('js', $config['allowed_resources']);
+        $this->assertContains('skin', $config['allowed_resources']);
+        $this->assertEquals(1000, $config['update_time']);
+    }
+}
