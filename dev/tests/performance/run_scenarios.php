3173cf1e31cf779952209a685742640977cfe189
Sergey Shymko <sergey.shymko@magento.com>
2012-05-21 13:22:37 -0700

    Implemented MAGETWO-1125 Performance Inspection CI Build - Performance tests -- added scenario existence validation -- introduced scenario dry runs to populate cache (if any) before measuring performance

--- dev/tests/performance/run_scenarios.php
+++ dev/tests/performance/run_scenarios.php
@@ -17,17 +17,24 @@ $configFile = file_exists($configFile) ? $configFile : "$configFile.dist";
 $config = require($configFile);
 $installOptions = isset($config['install_options']) ? $config['install_options'] : array();
 $scenarioFiles = $config['scenario_files'];
+$scenarioParams = $config['scenario_params'];
 $fixtures = $config['fixtures'];
 $reportDir = isset($config['report_dir']) ? $config['report_dir'] : __DIR__ . '/report';
+$dryRunLogFile = $reportDir . '/jmeter_dry_run.log';
+$runLogFile = $reportDir . '/jmeter.log';
 
-/* Build JMeter command */
-$jMeterJarFile = getenv('jmeter_jar_file') ?: 'ApacheJMeter.jar';
-$jMeterProperties = '';
-foreach ($config['scenario_params'] as $key => $value) {
-    $jMeterProperties .= " -J$key=$value";
+/* Validate scenarios existence */
+foreach ($scenarioFiles as $scenarioFile) {
+    if (!file_exists($scenarioFile)) {
+        echo "Scenario file '$scenarioFile' does not exist." . PHP_EOL;
+        exit(1);
+    }
 }
-$jMeterCmd = 'java -jar ' . escapeshellarg($jMeterJarFile);
-exec("$jMeterCmd --version 2>&1", $jMeterOutput, $exitCode);
+
+/* Validate JMeter command presence */
+$jMeterJarFile = getenv('jmeter_jar_file') ?: 'ApacheJMeter.jar';
+$jMeterExecutable = 'java -jar ' . escapeshellarg($jMeterJarFile);
+exec("$jMeterExecutable --version 2>&1", $jMeterOutput, $exitCode);
 if ($exitCode) {
     echo implode(PHP_EOL, $jMeterOutput);
     exit($exitCode);
@@ -51,25 +58,31 @@ if ($installOptions) {
 
 /* Magento initialization (for reports removal and fixtures) */
 require_once __DIR__ . '/../../../app/bootstrap.php';
-Mage::app();
 
 /* Clean reports */
 Varien_Io_File::rmdirRecursive($reportDir);
 
 /* Apply fixtures */
+Mage::app();
 foreach ($fixtures as $fixture) {
     require_once $fixture;
+}
+
+/* Execute each scenario couple times to populate cache (if any) before measuring performance */
+$scenarioDryRunParams = array_merge($scenarioParams, array('users' => 1, 'loops' => 2));
+foreach ($scenarioFiles as $scenarioFile) {
+    $scenarioCmd = buildJMeterCmd($jMeterExecutable, $scenarioFile, $scenarioDryRunParams, $dryRunLogFile);
+    passthru($scenarioCmd, $exitCode);
+    if ($exitCode) {
+        exit($exitCode);
+    }
 }
 
 /* Execute scenarios and collect failures */
 $failures = array();
 foreach ($scenarioFiles as $scenarioFile) {
     $scenarioLogFile = $reportDir . DIRECTORY_SEPARATOR . basename($scenarioFile, '.jmx') . '.jtl';
-    $scenarioCmd = sprintf(
-        $jMeterCmd . ' -n -t %s -l %s' . $jMeterProperties,
-        escapeshellarg($scenarioFile),
-        escapeshellarg($scenarioLogFile)
-    );
+    $scenarioCmd = buildJMeterCmd($jMeterExecutable, $scenarioFile, $scenarioParams, $runLogFile, $scenarioLogFile);
     passthru($scenarioCmd, $exitCode);
     if ($exitCode) {
         exit($exitCode);
@@ -77,21 +90,49 @@ foreach ($scenarioFiles as $scenarioFile) {
     $scenarioLogXml = simplexml_load_file($scenarioLogFile);
     $failedAssertions = $scenarioLogXml->xpath('//assertionResult[failure[text()="true"] or error[text()="true"]]');
     if ($failedAssertions) {
-        $failures[$scenarioFile] = $failedAssertions;
+        foreach ($failedAssertions as $assertionResult) {
+            if (isset($assertionResult->failureMessage)) {
+                $failures[$scenarioFile][] = (string)$assertionResult->failureMessage;
+            }
+            if (isset($assertionResult->errorMessage)) {
+                $failures[$scenarioFile][] = (string)$assertionResult->errorMessage;
+            }
+        }
     }
 }
 
 /* Handle failures */
 if ($failures) {
-    foreach ($failures as $scenarioFile => $failedAssertions) {
-        echo "Scenario '$scenarioFile' has failed!\n";
-        foreach ($failedAssertions as $assertionResult) {
-            if (isset($assertionResult->failureMessage)) {
-                echo $assertionResult->failureMessage . "\n";
-            } else if (isset($assertionResult->errorMessage)) {
-                echo $assertionResult->errorMessage . "\n";
-            }
-        }
+    foreach ($failures as $scenarioFile => $failureMessages) {
+        echo "Scenario '$scenarioFile' has failed!" . PHP_EOL;
+        echo implode(PHP_EOL, $failureMessages);
     }
     exit(1);
+}
+
+
+/**
+ * Build JMeter command
+ *
+ * @param string $jMeterExecutable
+ * @param string $testPlanFile
+ * @param array $localProperties
+ * @param string|null $runLogFile
+ * @param string|null $sampleLogFile
+ * @return string
+ */
+function buildJMeterCmd(
+    $jMeterExecutable, $testPlanFile, array $localProperties = array(), $runLogFile = null, $sampleLogFile = null
+) {
+    $result = $jMeterExecutable . ' -n -t ' . escapeshellarg($testPlanFile);
+    if ($runLogFile) {
+        $result .= ' -j ' . escapeshellarg($runLogFile);
+    }
+    if ($sampleLogFile) {
+        $result .= ' -l ' . escapeshellarg($sampleLogFile);
+    }
+    foreach ($localProperties as $key => $value) {
+        $result .= " -J$key=$value";
+    }
+    return $result;
 }
