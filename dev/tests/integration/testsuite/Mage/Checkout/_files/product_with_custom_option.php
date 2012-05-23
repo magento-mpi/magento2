1f24d20092a0ebe7043027a02ec29f9500a905ff
maksym.moskvychev <mmoskvychev@ebay.com>
2012-05-23 15:20:10 +0000

    CR-Change MAGETWO-1219: There is no possibility of going back to the shopping cart after "Edit" simple product

---
+++ dev/tests/integration/testsuite/Mage/Checkout/_files/product_with_custom_option.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Checkout
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+$product = new Mage_Catalog_Model_Product();
+$product->setTypeId(Mage_Catalog_Model_Product_Type::TYPE_SIMPLE)
+    ->setId(1)
+    ->setAttributeSetId(4)
+    ->setWebsiteIds(array(1))
+    ->setName('Simple Product')
+    ->setSku('simple')
+    ->setPrice(10)
+    ->setDescription('Description with <b>html tag</b> 2')
+
+    ->setMetaTitle('meta title 2')
+    ->setMetaKeyword('meta keyword 2')
+    ->setMetaDescription('meta description 2')
+
+    ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
+    ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
+
+    ->setCategoryIds(array(2))
+
+    ->setStockData(
+    array(
+        'use_config_manage_stock'   => 1,
+        'qty'                       => 100,
+        'is_qty_decimal'            => 0,
+        'is_in_stock'               => 1,
+    )
+)
+    ->setCanSaveCustomOptions(true)
+    ->setProductOptions(
+    array(
+        array(
+            'id'        => 1,
+            'option_id' => 0,
+            'previous_group' => 'text',
+            'title'     => 'Test Field',
+            'type'      => 'field',
+            'is_require'=> 1,
+            'sort_order'=> 0,
+            'price'     => 1,
+            'price_type'=> 'fixed',
+            'sku'       => '1-text',
+            'max_characters' => 100
+        )
+    )
+)
+    ->setHasOptions(true)
+    ->save();
+
+/** @var $product Mage_Catalog_Model_Product */
+$product = Mage::getModel('Mage_Catalog_Model_Product');
+$product->load(1);
+$optionId = key($product->getOptions());
+
+/** @var $cart Mage_Checkout_Model_Cart */
+$cart = Mage::getSingleton('Mage_Checkout_Model_Cart');
+$requestInfo = new Varien_Object(array(
+    'qty' => 1,
+    'options' => array(
+        $optionId => 'test'
+    )
+));
+
+$cart->addProduct($product, $requestInfo);
+$cart->save();
+$cart = Mage::getSingleton('Mage_Checkout_Model_Cart');
+
+$quoteItemId = $cart->getQuote()->getItemByProduct($product)->getId();
+Mage::register('product_with_custom_option/quoteItemId', $quoteItemId);
+Mage::unregister('_singleton/Mage_Checkout_Model_Session');
+Mage::unregister('_singleton/Mage_Checkout_Model_Cart');
--- dev/tests/integration/testsuite/Mage/Checkout/controllers/CartControllerTest.php
+++ dev/tests/integration/testsuite/Mage/Checkout/controllers/CartControllerTest.php
@@ -14,6 +14,21 @@
  */
 class Mage_Checkout_CartControllerTest extends Magento_Test_TestCase_ControllerAbstract
 {
+
+    /**
+     * Test for Mage_Catalog_ProductController::configureAction()
+     *
+     * Quote consists of simple product with custom option
+     * @magentoDataFixture Mage/Checkout/_files/product_with_custom_option.php
+     */
+    public function testConfigureActionWithCustomOption()
+    {
+        $quoteItemId = Mage::registry('product_with_custom_option/quoteItemId');
+        $this->dispatch('checkout/cart/configure/id/' . $quoteItemId);
+        $out = $this->getResponse()->getBody();
+        $this->assertContains('<button type="button" title="Update Cart" class="button btn-cart"', $out);
+    }
+
     /**
      * Test for Mage_Catalog_ProductController::configureAction()
      *
