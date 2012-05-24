494524944b4c6e02d5b5011227d922d3e6b12493
Anton Kril <akril@ebay.com>
2012-05-24 18:10:44 +0300

    Fixed MAGETWO-1300: Missing Role Grid

--- app/code/core/Mage/User/Block/Role.php
+++ app/code/core/Mage/User/Block/Role.php
@@ -18,10 +18,19 @@
 
 class Mage_User_Block_Role extends Mage_Backend_Block_Widget_Grid_Container
 {
+    /**
+     * @var string
+     */
+    protected $_controller = 'user_role';
+
+    /**
+     * @var string
+     */
+    protected $_blockGroup = 'Mage_User';
+
+
     public function __construct()
     {
-        $this->_controller = 'user_role';
-        $this->_blockGroup = 'Mage_User';
         $this->_headerText = Mage::helper('Mage_User_Helper_Data')->__('Roles');
         $this->_addButtonLabel = Mage::helper('Mage_User_Helper_Data')->__('Add New Role');
         parent::__construct();
@@ -34,13 +43,15 @@ class Mage_User_Block_Role extends Mage_Backend_Block_Widget_Grid_Container
 
     protected function _prepareLayout()
     {
-        $this->setChild(
-            'grid',
-            $this->getLayout()->createBlock(
-                $this->_blockGroup . '_Block_Role_Grid',
-                $this->_controller . '.grid')
-                ->setSaveParametersInSession(true)
-        );
+        if (!$this->getLayout()->getChildName($this->getNameInLayout(), 'grid')) {
+            $this->setChild(
+                'grid',
+                $this->getLayout()->createBlock(
+                    $this->_blockGroup . '_Block_Role_Grid',
+                    $this->_controller . '.grid')
+                    ->setSaveParametersInSession(true)
+            );
+        }
         return Mage_Backend_Block_Widget_Container::_prepareLayout();
     }
 
