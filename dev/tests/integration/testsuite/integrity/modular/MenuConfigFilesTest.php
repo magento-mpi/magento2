0f1ae484aebfaf3c9983ba8207f09298d63c00c0
Alexey Korshenko <okorhsnko@ebay.com>
2012-05-28 17:52:13 +0300

    Fixed MAGETWO-880: Menu Structure Definition Validation Test - fixed xsd scheme

--- dev/tests/integration/testsuite/integrity/modular/MenuConfigFilesTest.php
+++ dev/tests/integration/testsuite/integrity/modular/MenuConfigFilesTest.php
@@ -21,6 +21,20 @@ class Integrity_Modular_MenuConfigFilesTest extends PHPUnit_Framework_TestCase
     protected $_fileList = array();
 
     /**
+     * Mage_Backend_Model_Config_Menu
+     */
+    protected $_model;
+
+    public function setUp()
+    {
+        $this->_model = $this->getMockForAbstractClass(
+            'Mage_Backend_Model_Config_Menu',
+            array(),
+            '',
+            false
+        );
+    }
+    /**
      * Get Configuration File List
      * @return array
      */
@@ -35,36 +49,16 @@ class Integrity_Modular_MenuConfigFilesTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Get merged menu configuration
-     * @return Magento_Config_Dom
-     */
-    protected function _mergeConfigurationFiles()
-    {
-        $xml = '<menu>';
-        foreach ($this->_getConfigurationFileList() as $file) {
-            $element = simplexml_load_file($file);
-            foreach ($element->children() as $node) {
-                $xml .= $node->asXml();
-            }
-        }
-        $xml .= '</menu>';
-        return new Magento_Config_Dom($xml);
-    }
-
-    /**
      * Perform test whether a configuration file is valid
      *
      * @param string $file
-     * @param Magento_Config_Dom $domConfig
      * @throws PHPUnit_Framework_AssertionFailedError if file is invalid
      */
-    protected function _validateConfigFile($file, $domConfig = null)
+    protected function _validateConfigFile($file)
     {
-        $schemaFile = Mage::getBaseDir('lib') . '/Magento/Config/menu.xsd';
 
-        if (false == ($domConfig instanceof Magento_Config_Dom)) {
-            $domConfig = new Magento_Config_Dom(file_get_contents($file));
-        }
+        $schemaFile = $this->_model->getSchemaFile();
+        $domConfig = new Magento_Config_Dom(file_get_contents($file));
         $result = $domConfig->validate($schemaFile, $errors);
         $message = "Invalid XML-file: {$file}\n";
         foreach ($errors as $error) {
@@ -74,26 +68,33 @@ class Integrity_Modular_MenuConfigFilesTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Test each menu configuration file
+     * @param string $file
+     * @dataProvider menuConfigFileDataProvider
+     */
+    public function testMenuConfigFile($file)
+    {
+        $this->_validateConfigFile($file);
+    }
+
+    /**
+     * @return array
+     */
+    public function menuConfigFileDataProvider()
+    {
+        $output = array();
+        $list = $this->_getConfigurationFileList();
+        foreach ($list as $file) {
+            $output[$file] = array($file);
+        }
+        return $output;
+    }
+
+    /**
      * Test merged menu configuration
      */
     public function testMergedConfig()
     {
-        $dom = $this->_mergeConfigurationFiles();
-        $this->_validateConfigFile('merged', $dom);
-    }
-
-    /**
-     * Test each menu configuration file
-     */
-    public function testMenuConfigFile()
-    {
-        $list = $this->_getConfigurationFileList();
-        if (empty($list)) {
-            $this->markTestSkipped('Menu configuration files not found');
-        } else {
-            foreach ($list as $file) {
-                $this->_validateConfigFile($file);
-            }
-        }
+        $this->markTestIncomplete('Test is incomplete until implementation of MAGETWO-882');
     }
 }
