ee0ae15eab78e86d0e629aa34ab5b8e3df1ed556
Dmytro Vynograd <dmytro.vynograd@magento.com>
2012-05-28 17:29:42 +0300

    Fixed MAGETWO-1174: Separate fields to dynamic and static content - repair integration test Mage_Core_Model_Design_PackagePublicationTest::testGetPublicSkinDir & Mage_Core_Model_Config_OptionsTest::testGetters

--- dev/tests/integration/testsuite/Mage/Core/Model/Design/PackagePublicationTest.php
+++ dev/tests/integration/testsuite/Mage/Core/Model/Design/PackagePublicationTest.php
@@ -57,6 +57,7 @@ class Mage_Core_Model_Design_PackagePublicationTest extends PHPUnit_Framework_Te
      */
     public function testGetPublicSkinDir()
     {
+        Mage::app()->getConfig()->getOptions()->setSkinDir(__DIR__);
         $this->assertEquals(__DIR__, $this->_model->getPublicSkinDir());
     }
 
