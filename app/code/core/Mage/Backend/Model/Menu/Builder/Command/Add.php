986d53c31bc7668418a997c263644f40009de1ef
Alexey Korshenko <okorhsnko@ebay.com>
2012-05-29 18:56:00 +0300

    Fixed MAGETWO-882: Menu Rendering Adaptation and Moving - create stub-classes

---
+++ app/code/core/Mage/Backend/Model/Menu/Builder/Command/Add.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Backend
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+class Mage_Backend_Model_Menu_Builder_Command_Add extends Mage_Backend_Model_Menu_Builder_CommandAbstract
+{
+
+}
