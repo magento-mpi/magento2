56d731c43fec68eb40ea0343e7e8a9e6572b75cd
E.Maksimenko <emaksimenko@ebay.com>
2012-05-31 11:22:12 +0300

    [HELPER][UIMAP] fixes for CheckoutMultipleAddresses tests

--- testsuite/Core/Mage/CheckoutMultipleAddresses/Helper.php
+++ testsuite/Core/Mage/CheckoutMultipleAddresses/Helper.php
@@ -570,7 +570,7 @@ class Core_Mage_CheckoutMultipleAddresses_Helper extends Mage_Selenium_TestCase
             (isset($paymentData['payment']['payment_method'])) ? $paymentData['payment']['payment_method'] : array();
         $verifyPrices = (isset($checkout['verify_prices'])) ? $checkout['verify_prices'] : array();
         //Verify quantity orders
-        $actualQty = $this->getXpathCount($this->_getControlXpath('fieldset', 'shipping_address_info') . '/h3');
+        $actualQty = $this->getXpathCount($this->_getControlXpath('pageelement', 'shipping_method_products'));
         $this->assertEquals(count($shippings), $actualQty, 'orders quantity is wrong');
         //Verify selected Shipping addresses for orders
         $orderHeaders = array();
