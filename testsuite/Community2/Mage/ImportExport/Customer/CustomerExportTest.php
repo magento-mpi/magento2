33cdf6c9f8d894f60ba1b132589e0062d342488f
Roman Grebenchuk <rgrebenchuk@ebay.com>
2012-06-01 15:47:16 +0300

    [UIMAP][TESTS] MAGETWO-1441

--- testsuite/Community2/Mage/ImportExport/Customer/CustomerExportTest.php
+++ testsuite/Community2/Mage/ImportExport/Customer/CustomerExportTest.php
@@ -54,7 +54,29 @@ class Community2_Mage_ImportExport_CustomerExportTest extends Mage_Selenium_Test
     {
         //Step 1
         $this->navigate('export');
-        //Verifying dropdown Entity Type
-        $options = $this->getElementsByXpath($this->_getControlXpath('dropdown', 'entity_type'), 'text', 'option');
+        //Verifying
+        $entityTypes = $this->getElementsByXpath(
+                $this->_getControlXpath('dropdown', 'entity_type') . '/option',
+                'text');
+        $this->assertEquals(array('-- Please Select --','Products','Customers'),$entityTypes,
+                'Entity Type dropdown contains incorrect values');
+        $fileFormat = $this->getElementsByXpath(
+                $this->_getControlXpath('dropdown', 'file_format') . '/option',
+                'text');
+        $this->assertEquals(array('CSV'),$fileFormat,
+                'Export File Format dropdown contains incorrect values');
+        //Step 2
+        $this->fillDropdown('entity_type', 'Customers');
+        $this->waitForElementVisible($this->_getControlXpath('dropdown', 'export_file_version'));
+        //Verifying
+        $exportFileVersion = $this->getElementsByXpath(
+                $this->_getControlXpath('dropdown', 'export_file_version') . '/option',
+                'text');
+        $this->assertEquals(array('-- Please Select --','Magento 1.7 format','Magento 2.0 format'),$exportFileVersion,
+                'Export File Version dropdown contains incorrect values');
+        //Step 3
+        $this->fillDropdown('export_file_version', 'Magento 2.0 format');
+        $this->waitForAjax();
+        //Verifying
     }
 }
