fd419680f31c411bc07304a30c02bc5e3c739add
Anton Makarenko <anton.makarenko@magento.com>
2012-06-04 17:56:29 -0700

    MAGETWO-1385 VDE Crash: "Invalid argument supplied for foreach()" - "improved" test according to CR notes

--- dev/tests/integration/testsuite/Enterprise/Customer/Block/FormTest.php
+++ dev/tests/integration/testsuite/Enterprise/Customer/Block/FormTest.php
@@ -15,10 +15,21 @@ class Enterprise_Customer_Block_FormTest extends PHPUnit_Framework_TestCase
     {
         $layout = new Mage_Core_Model_Layout;
         $template = $layout->createBlock('Mage_Core_Block_Text', 'customer_form_template');
-        $template->setData('renderers', array('test' => array('block' => 'Mage_Core_Block_Text', 'template' => '1')));
+        $template->setData('renderers', array('test' => array(
+            'block' => 'Mage_Core_Block_Text', 'template' => '1.phtml'
+        )));
         $block = $layout->createBlock('Enterprise_Customer_Block_Form');
         $testRenderer = $block->getRenderer('test');
         $this->assertInstanceOf('Mage_Core_Block_Text', $testRenderer);
         $this->assertNotSame($template, $testRenderer);
+        $this->assertEquals('1.phtml', $testRenderer->getTemplate());
+    }
+
+    public function testPrepareLayoutNoRenderer()
+    {
+        $layout = new Mage_Core_Model_Layout;
+        $layout->createBlock('Mage_Core_Block_Text', 'customer_form_template');
+        $block = $layout->createBlock('Enterprise_Customer_Block_Form');
+        $this->assertEquals('', $block->toHtml());
     }
 }
