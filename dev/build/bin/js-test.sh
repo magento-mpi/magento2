b30a7bb10584f2b8a3d012b2eb846e394841aa8d
kayan <kayan@x.com>
2012-06-11 15:23:31 -0500

    linux version of js unit test using js test driver

---
+++ dev/build/bin/js-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+OUTPUT_DIR="../../tests/js/test-output"
+CONF_FILE="../../tests/js/jsTestDriver.conf"
+rm -r $OUTPUT_DIR
+mkdir $OUTPUT_DIR
+
+FIREFOX=`which firefox`
+if [ "$?" -eq 1 ];
+then
+    echo "Firefox not found."
+    exit 1
+fi
+
+java -jar JsTestDriver.jar --config $CONF_FILE --port 9876 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
+
+echo "Done."
