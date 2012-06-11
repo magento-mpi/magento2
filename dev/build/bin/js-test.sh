934016ff86f9d6b01a478a80f89c8cd98b01b2fc
kayan <kayan@x.com>
2012-06-11 15:30:30 -0500

    hardcode js test driver browser to /usr/bin/firefox

--- dev/build/bin/js-test.sh
+++ dev/build/bin/js-test.sh
@@ -4,13 +4,12 @@ CONF_FILE="../../tests/js/jsTestDriver.conf"
 rm -r $OUTPUT_DIR
 mkdir $OUTPUT_DIR
 
-FIREFOX=`which firefox`
-if [ "$?" -eq 1 ];
-then
-    echo "Firefox not found."
-    exit 1
-fi
+#FIREFOX=`which firefox`
+#if [ "$?" -eq 1 ];
+#then
+#    echo "Firefox not found."
+#    exit 1
+#fi
 
-java -jar JsTestDriver.jar --config $CONF_FILE --port 9876 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
-
-echo "Done."
+#java -jar JsTestDriver.jar --config $CONF_FILE --port 9876 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
+java -jar JsTestDriver.jar --config $CONF_FILE --port 9876 --browser /usr/bin/firefox --tests all --testOutput $OUTPUT_DIR
