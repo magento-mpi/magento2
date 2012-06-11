5acd23986aae7af6293068669b66048e8d29e2e0
kayan <kayan@x.com>
2012-06-11 15:53:26 -0500

    pass conf and output from ant script to js test driver sh script

--- dev/build/bin/js-test.sh
+++ dev/build/bin/js-test.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-OUTPUT_DIR="../../tests/js/test-output"
-CONF_FILE="../../tests/js/jsTestDriver.conf"
-rm -r $OUTPUT_DIR
-mkdir $OUTPUT_DIR
-
-#FIREFOX=`which firefox`
-#if [ "$?" -eq 1 ];
-#then
-#    echo "Firefox not found."
-#    exit 1
-#fi
-
-#java -jar JsTestDriver.jar --config $CONF_FILE --port 9876 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
-java -jar JsTestDriver.jar --config $CONF_FILE --port 9876 --browser /usr/bin/firefox --tests all --testOutput $OUTPUT_DIR
+rm -r $2
+mkdir $2
+java -jar JsTestDriver.jar --config $1 --port 9876 --browser /usr/bin/firefox --tests all --testOutput $2
