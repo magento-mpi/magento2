c97192c0f2ccebfb9f41d7a314df7cfd64c05e0b
Iuliia Babenko <ibabenko@ebay.com>
2012-06-12 17:31:12 +0300

    [TEST] TL-MAGE-5488 test moved to CustomerAttributeTest.php

--- testsuite/Enterprise2/Mage/ImportExport/Customer/CustomerAttributeTest.php
+++ testsuite/Enterprise2/Mage/ImportExport/Customer/CustomerAttributeTest.php
@@ -205,4 +205,53 @@ class Enterprise2_Mage_ImportExport_CustomerAttributeTest extends Mage_Selenium_
         $this->waitForAjax();
     }
 
+    /**
+     * <p>Customer Master file export with using some filters</p>
+     * <p>Steps</p>
+     * <p>1. On backend in System -> Import/ Export -> Export select "Customers" entity type</p>
+     * <p>2. Select the export version "Magento 2.0" and "Master Type File"</p>
+     * <p>3. In the "Filter" column according to you attribute select option that was used in your customer creation</p>
+     * <p>4. Press "Continue" button and save current file</p>
+     * <p>5. Open file</p>
+     * <p>Expected: In generated file just your customer with selected option of attribute is present</p>
+     *
+     * @test
+     * @TestlinkId TL-MAGE-5488
+     */
+    public function exportMasterFileWithFilters()
+    {
+        //Precondition: create attribute, create new customer, fill created attribute
+        $this->navigate('manage_customer_attributes');
+        $attrData = $this->loadDataSet('ImportExport.yml', 'generic_customer_attribute');
+        $this->customerAttributeHelper()->createAttribute($attrData);
+        $this->addParameter('attribute_name', $attrData['attribute_code']);
+        $this->navigate('manage_customers');
+        $userData = $this->loadDataSet('ImportExport.yml', 'customer_account_with_attribute');
+        $this->customerHelper()->createCustomer($userData);
+        $this->assertMessagePresent('success', 'success_saved_customer');
+        //Step 1
+        $this->admin('export');
+        $this->assertTrue($this->checkCurrentPage('export'), $this->getParsedMessages());
+        $this->fillDropdown('entity_type', 'Customers');
+        $this->waitForElementVisible($this->_getControlXpath('dropdown', 'export_file_version'));
+        //Step2
+        $this->fillDropdown('export_file_version', 'Magento 2.0 format');
+        $this->waitForElementVisible($this->_getControlXpath('dropdown', 'export_file'));
+        $this->fillDropdown('export_file', 'Customers Main File');
+        $this->waitForElementVisible($this->_getControlXpath('button', 'continue'));
+        //Step3
+        $this->ImportExportHelper()->setFilter(array(
+                $attrData['attribute_code'] => $userData['custom_attribute'])
+        );
+        //Step4-5
+        $report = $this->ImportExportHelper()->export();
+        //Verifying
+        $userData[$attrData['attribute_code']] = $userData['custom_attribute'];
+        unset($userData['custom_attribute']);
+        $this->assertNotNull($this->importExportHelper()->lookForEntity('master', $userData, $report),
+            "Customer not found in csv file");
+        $this->assertEquals(0, $this->importExportHelper()->lookForEntity('master', $userData, $report),
+            "Other customers are present in csv file");
+    }
+
 }
