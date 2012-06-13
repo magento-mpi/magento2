98354edf1b85857bfb40d05fea2870f82ce010c2
Roman Grebenchuk <rgrebenchuk@ebay.com>
2012-06-13 15:31:55 +0300

    [TESTS] CR MAGETWO-1546               MAGETWO-1577               MAGETWO-1583

--- testsuite/Enterprise2/Mage/ImportExport/Customer/Attribute/AttributeTest.php
+++ testsuite/Enterprise2/Mage/ImportExport/Customer/Attribute/AttributeTest.php
@@ -134,14 +134,14 @@ class Enterprise2_Mage_ImportExport_CustomerAttributeTest extends Mage_Selenium_
         //Step 5
         $this->ImportExportHelper()->customerFilterAttributes(
             array(
-                'attribute_code' => $attrData['attribute_code'],
-            );
+                'attribute_code' => $attrData['attribute_code']
+            ));
         //Step 6
         $isFound = $this->ImportExportHelper()->customerSearchAttributes(
             array(
                 'attribute_code' => $attrData['attribute_code'],
             'grid_and_filter'
-            );
+            ));
         $this->assertNotNull($isFound, 'Attribute was not found after filtering');
         //Step 7
         $this->clickButton('reset_filter', false);
