1c4f633969b49f895820c92d67399856f6783bcf
Evgeniy Miskov <emiskov@ebay.com>
2012-06-13 20:04:49 +0300

    Merge branch 'MAGETWO-1256' into MAGETWO-1237

--- testsuite/Community17/Mage/TermsAndConditions/CreateTest.php
+++ testsuite/Community17/Mage/TermsAndConditions/CreateTest.php
@@ -34,7 +34,7 @@
  * @subpackage  tests
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Core_Mage_TermsAndConditions_CreateTest extends Mage_Selenium_TestCase {
+class Community17_Mage_TermsAndConditions_CreateTest extends Mage_Selenium_TestCase {
 
     /**
      * <p>Preconditions:</p>
