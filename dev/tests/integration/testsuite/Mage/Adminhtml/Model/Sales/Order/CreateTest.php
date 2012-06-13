efbb50d3b3d70077d0c167bb4449dd1d30ffb095
Vlad Veselov <vveselov@ebay.com>
2012-06-13 17:01:18 +0300

    Fixed MAGETWO-1199: Impossible to reoder downloadable product

---
+++ dev/tests/integration/testsuite/Mage/Adminhtml/Model/Sales/Order/CreateTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Adminhtml
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Mage_Adminhtml_Model_Sales_Order_CreateTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Model instance
+     *
+     * @var Mage_Adminhtml_Model_Sales_Order_Create
+     */
+    protected $_model;
+
+    public function setUp()
+    {
+        $this->_model = new Mage_Adminhtml_Model_Sales_Order_Create();
+    }
+
+    /**
+     * @magentoDataFixture Mage/Downloadable/_files/product.php
+     * @magentoDataFixture Mage/Downloadable/_files/order_with_downloadable_product_commit_workaround.php
+     */
+    public function testInitFromOrderShippingAddressSameAsBillingWhenEmpty()
+    {
+        $order = new Mage_Sales_Model_Order();
+        $order->loadByIncrementId('100000001');
+        $this->assertFalse($order->getShippingAddress());
+
+        Mage::unregister('rule_data');
+        $this->_model->initFromOrder($order);
+
+        $this->assertFalse($order->getShippingAddress());
+    }
+
+    /**
+     * @magentoDataFixture Mage/Downloadable/_files/product.php
+     * @magentoDataFixture Mage/Downloadable/_files/order_with_downloadable_product_commit_workaround.php
+     * @magentoDataFixture Mage/Adminhtml/_files/order_shipping_address_same_as_billing_commit_workaround.php
+     */
+    public function testInitFromOrderShippingAddressSameAsBillingWhenSame()
+    {
+        $order = new Mage_Sales_Model_Order();
+        $order->loadByIncrementId('100000001');
+
+        $this->assertNull($order->getShippingAddress()->getSameAsBilling());
+
+        Mage::unregister('rule_data');
+        $this->_model->initFromOrder($order);
+
+        $this->assertTrue($order->getShippingAddress()->getSameAsBilling());
+    }
+
+    /**
+     * @magentoDataFixture Mage/Downloadable/_files/product.php
+     * @magentoDataFixture Mage/Downloadable/_files/order_with_downloadable_product_commit_workaround.php
+     * @magentoDataFixture Mage/Adminhtml/_files/order_shipping_address_different_to_billing_commit_workaround.php
+     */
+    public function testInitFromOrderShippingAddressSameAsBillingWhenDifferent()
+    {
+        $order = new Mage_Sales_Model_Order();
+        $order->loadByIncrementId('100000001');
+
+        $this->assertNull($order->getShippingAddress()->getSameAsBilling());
+
+        Mage::unregister('rule_data');
+        $this->_model->initFromOrder($order);
+
+        $this->assertFalse($order->getShippingAddress()->getSameAsBilling());
+    }
+}
