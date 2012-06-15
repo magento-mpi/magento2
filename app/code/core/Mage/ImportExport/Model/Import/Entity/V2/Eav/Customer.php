e51d38108444b1159d3ddee5457166f3fcc06278
Shishkin Yevgeniy <yshishkin@ebay.com>
2012-06-15 15:43:32 +0300

    Implemented MAGETWO-1619: Old/New format import and Behavior selectors - added dump class for import customer EAV entity

---
+++ app/code/core/Mage/ImportExport/Model/Import/Entity/V2/Eav/Customer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_ImportExport
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Import entity customer model
+ *
+ * @category    Mage
+ * @package     Mage_ImportExport
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_ImportExport_Model_Import_Entity_V2_Eav_Customer
+    extends Mage_ImportExport_Model_Import_Entity_V2_Eav_Abstract
+{
+}
