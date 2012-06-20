c9a88bfd0cc61e1a5c04f1b9929e5a00908c6b59
Vlad Veselov <vveselov@ebay.com>
2012-06-20 13:01:32 +0300

    CR Change MAGETWO-1756: Implement an opportunity to clean up the database before installation for all supported database adapters

--- app/code/core/Mage/Install/Model/Installer/Console.php
+++ app/code/core/Mage/Install/Model/Installer/Console.php
@@ -351,7 +351,7 @@ class Mage_Install_Model_Installer_Console extends Mage_Install_Model_Installer_
         $dbConfig = Mage::getConfig()->getResourceConnectionConfig(Mage_Core_Model_Resource::DEFAULT_SETUP_RESOURCE);
         $modelName = 'Mage_Install_Model_Installer_Db_' . ucfirst($dbConfig->model);
 
-        if (Magento_Autoload::getInstance()->classExists($modelName)) {
+        if (!Magento_Autoload::getInstance()->classExists($modelName)) {
             $this->addError('Database uninstall is not supported for the ' . ucfirst($dbConfig->model) . '.');
             return false;
         }
@@ -368,7 +368,6 @@ class Mage_Install_Model_Installer_Console extends Mage_Install_Model_Installer_
             $configOptions->getExportDir(),
             $configOptions->getLogDir(),
             $configOptions->getVarDir() . '/report',
-            $configOptions->getMediaDir() . '/skin',
         );
         foreach ($dirsToRemove as $dir) {
             Varien_Io_File::rmdirRecursive($dir);
