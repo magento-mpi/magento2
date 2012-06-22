f980e34e4fd6f5b1c6ffd8162c1a38b105f4798a
Vlad Veselov <vveselov@ebay.com>
2012-06-22 12:15:42 +0300

    Fixed MAGETWO-1623: Fix all possible fails of functional tests builds on bamboo on allocated time - tuned build parameters

--- dev/build/customization/functional/dev/tests/functional/config/install.php
+++ dev/build/customization/functional/dev/tests/functional/config/install.php
@@ -24,7 +24,6 @@ return array(
         'db_name'                    => '{{db_name}}',
         'db_user'                    => '{{db_user}}',
         'db_pass'                    => '{{db_password}}',
-        'use_secure'                 => 'no',
         'use_secure_admin'           => 'yes',
         'admin_no_form_key'          => 'yes',
         'use_rewrites'               => 'no',
