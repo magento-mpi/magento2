6a7ef7cf86f720aa19ba9488d2f1b7262bb6e31f
Dmytro Iekutenko <diekutenko@ebay.com>
2012-06-26 15:56:50 +0300

    Fixed MAGETWO-821: JavaScript Redirect VS Visual Design Editor - Remove global counter of setTimeout calls - wrap the setTimeout and setInterval methods into a closure and pass the bindBeforeUnload and unbindBeforeUnload methods as parameters.

--- app/code/core/Mage/DesignEditor/view/frontend/js/settimeout_override.js
+++ app/code/core/Mage/DesignEditor/view/frontend/js/settimeout_override.js
@@ -6,58 +6,55 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-    
-var timeoutCounter = 0,
-    unbindBeforeUnload = function () {
-        timeoutCounter--;
-        if (timeoutCounter < 1) {
-            window.onbeforeunload = null;
-        }
-    },
-    bindBeforeUnload = function() {
-        if (timeoutCounter < 1) {
-            window.onbeforeunload = function (e) {
-                var e = e || window.event,
-                    messageText = 'Are you sure, you want to leave this page?';
-                // For IE and Firefox
-                if (e) {
-                    e.returnValue = messageText;
+
+(function(before, after) {
+
+    window.setTimeout = (function(oldSetTimeout) {
+        return function(func, delay) {
+            return oldSetTimeout(function() {
+                try {
+                    before();
+                    func();
+                    after();
                 }
-                // For Chrome and Safari
-                return messageText;
-            };
-        }
-        timeoutCounter++;
+                catch (exception) {
+                    after();
+                    throw exception;
+                }
+            }, delay);
+        };
+    })(window.setTimeout);
+
+    window.setInterval = (function(oldSetInterval) {
+        return function(func, delay) {
+            return oldSetInterval(function() {
+                try {
+                    before();
+                    func();
+                    after();
+                }
+                catch (exception) {
+                    after();
+                    throw exception;
+                }
+            }, delay);
+        };
+    })(window.setInterval);
+
+})(
+    function() {
+        window.onbeforeunload = function(e) {
+            var e = e || window.event;
+            var messageText = 'Are you sure, you want to leave this page?';
+            // For IE and Firefox
+            if (e) {
+                e.returnValue = messageText;
+            }
+            // For Chrome and Safari
+            return messageText;
+        };
+    },
+    function () {
+        window.onbeforeunload = null;
     }
-
-window.setTimeout = (function(oldSetTimeout) {
-    return function(func, delay) {
-        oldSetTimeout(function() {
-            try {
-                bindBeforeUnload();
-                func();
-                unbindBeforeUnload();
-            }
-            catch (exception) {
-                unbindBeforeUnload();
-                throw exception;
-            }
-        }, delay);
-    };
-})(window.setTimeout);
-
-window.setInterval = (function(oldSetInterval) {
-    return function(func, delay) {
-        oldSetInterval(function() {
-            try {
-                bindBeforeUnload();
-                func();
-                unbindBeforeUnload();
-            }
-            catch (exception) {
-                unbindBeforeUnload();
-                throw exception;
-            }
-        }, delay);
-    };
-})(window.setInterval);
+);
