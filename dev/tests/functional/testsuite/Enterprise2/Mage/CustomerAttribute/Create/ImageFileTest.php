e3ba6075679bb4f9eebf791dce1c10aa4559e6a7
andrey.vergeles <avergeles@ebay.com>
2012-06-26 12:38:10 +0200

    [TEST] [UIMAP] [HELPER] [DATA] MAGETWO-1553 Customer Attributes - Implemented creating, validation and deleting all types of attributes

--- dev/tests/functional/testsuite/Enterprise2/Mage/CustomerAttribute/Create/ImageFileTest.php
+++ dev/tests/functional/testsuite/Enterprise2/Mage/CustomerAttribute/Create/ImageFileTest.php
@@ -265,8 +265,8 @@ class Enterprise2_Mage_CustomerAttribute_Create_ImageFileTest extends Mage_Selen
         //Data
         $attrData = $this->loadDataSet('CustomerAttribute', 'customer_attribute_image_file',
             array('admin_title' => $this->generate('string', 32, ':punct:')));
-        $attrData['manage_labels_options']['manage_titles']['admin_title'] = preg_replace('/<|>/', '',
-            $attrData['manage_labels_options']['manage_titles']['admin_title']);
+        $attrData['manage_labels_options']['admin_title'] = preg_replace('/<|>/', '',
+            $attrData['manage_labels_options']['admin_title']);
         $searchData = $this->loadDataSet('CustomerAttribute', 'attribute_search_data',
             array('attribute_code' => $attrData['properties']['attribute_code']));
         //Steps
@@ -302,7 +302,7 @@ class Enterprise2_Mage_CustomerAttribute_Create_ImageFileTest extends Mage_Selen
                   'admin_title'    => $this->generate('string', 255, ':alnum:')));
         $searchData = $this->loadDataSet('CustomerAttribute', 'attribute_search_data',
             array('attribute_code' => $attrData['properties']['attribute_code'], 'attribute_label' =>
-            $attrData['manage_labels_options']['manage_titles']['admin_title']));
+            $attrData['manage_labels_options']['admin_title']));
         //Steps
         $this->customerAttributeHelper()->createAttribute($attrData);
         //Verifying
