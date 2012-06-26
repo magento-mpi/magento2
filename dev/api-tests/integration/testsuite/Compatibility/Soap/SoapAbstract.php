3e900301a74bee149ea93246b17db013fa67c787
Yaroslav Ershov <yershov@ebay.com>
2012-06-26 18:35:15 +0300

    APIA-461: Implement Tests for API - Catalog Category Next methods were implemented:  - catalog_category.assignedProducts  - catalog_category.assignProduct  - catalog_category.create  - catalog_category.currentStore  - catalog_category.delete  - catalog_category.info  - catalog_category.level  - catalog_category.move  - catalog_category.removeProduct  - catalog_category.tree  - catalog_category.update  - catalog_category.updateProduct

---
+++ dev/api-tests/integration/testsuite/Compatibility/Soap/SoapAbstract.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Core
+ * @subpackage  compatibility_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * SoapAbstract class.
+ */
+abstract class Compatibility_Soap_SoapAbstract extends Magento_Test_Webservice_Compatibility
+{
+    /**
+     * Compare types of API responses (current and previous versions)
+     *
+     * @param mixed $prevResponse
+     * @param mixed $currResponse
+     * @param string $apiMethod
+     */
+    protected function _checkVersionCompatibility($prevResponse, $currResponse, $apiMethod)
+    {
+        $this->assertInternalType(gettype($prevResponse), $currResponse,
+            "The signature of $apiMethod has changed in the new API version.");
+    }
+
+    /**
+     * Create categories in current and previous API and return IDs
+     *
+     * @return array $categoryId
+     */
+    protected function _createCategories()
+    {
+        $categoryIds = array();
+        $categoryData = array(
+            'name' => 'Category ' . uniqid(),
+            'is_active' => '1',
+            'include_in_menu' => '1',
+            'available_sort_by' => array('position', 'name', 'price'),
+            'default_sort_by' => 'position'
+        );
+        $categoryIds['prevCategoryId'] = $this->prevCall('catalog_category.create', array(
+            Mage_Catalog_Model_Category::TREE_ROOT_ID,
+            $categoryData
+        ));
+        $categoryIds['currCategoryId'] = $this->currCall('catalog_category.create', array(
+            Mage_Catalog_Model_Category::TREE_ROOT_ID,
+            $categoryData
+        ));
+        return $categoryIds;
+    }
+
+    /**
+     * Create products in current and previous API and return IDs
+     *
+     * @return array $productId
+     */
+    protected function _createProducts()
+    {
+        $productIds = array();
+        $entityType = Mage::getModel('Mage_Eav_Model_Entity_Type')->loadByCode('catalog_product');
+        $productData = array(
+            'type' => 'simple',
+            'set' => $entityType->getDefaultAttributeSetId(),
+            'sku' => 'compatibility-' . uniqid(),
+            'productData' => array(
+                'name' => 'Compatibility Test ' . uniqid(),
+                'description' => 'Test description',
+                'short_description' => 'Test short description',
+                'status' => 1,
+                'visibility' => 4,
+                'price' => 9.99,
+                'tax_class_id' => 2,
+                'weight' => 1,
+            )
+        );
+        $productIds['prevProductId'] = $this->prevCall('catalog_product.create', $productData);
+        $productIds['currProductId'] = $this->currCall('catalog_product.create', $productData);
+        return $productIds;
+    }
+}
