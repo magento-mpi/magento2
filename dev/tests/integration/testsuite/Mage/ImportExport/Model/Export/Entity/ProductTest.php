4e339357855972af2a99f24c6bb63ed68b08fdc8
Dmitriy Zavalkin <dzavalkin@ebay.com>
2012-07-03 15:51:20 +0300

    Fixed MAGETWO-1880: Required product attributes are not exported - bug fixed - fix covered by integration test

--- dev/tests/integration/testsuite/Mage/ImportExport/Model/Export/Entity/ProductTest.php
+++ dev/tests/integration/testsuite/Mage/ImportExport/Model/Export/Entity/ProductTest.php
@@ -40,6 +40,34 @@ class Mage_ImportExport_Model_Export_Entity_ProductTest extends PHPUnit_Framewor
      */
     protected $_oldIsDeveloperMode;
 
+    /**
+     * Stock item attributes which must be exported
+     *
+     * @var array
+     */
+    public static $stockItemAttributes = array(
+        'qty',
+        'min_qty',
+        'use_config_min_qty',
+        'is_qty_decimal',
+        'backorders',
+        'use_config_backorders',
+        'min_sale_qty',
+        'use_config_min_sale_qty',
+        'max_sale_qty',
+        'use_config_max_sale_qty',
+        'is_in_stock',
+        'notify_stock_qty',
+        'use_config_notify_stock_qty',
+        'manage_stock',
+        'use_config_manage_stock',
+        'use_config_qty_increments',
+        'qty_increments',
+        'use_config_enable_qty_inc',
+        'enable_qty_increments',
+        'is_decimal_divided',
+    );
+
     protected function setUp()
     {
         parent::setUp();
@@ -76,5 +104,41 @@ class Mage_ImportExport_Model_Export_Entity_ProductTest extends PHPUnit_Framewor
 
         $this->_model->setWriter(new Mage_ImportExport_Model_Export_Adapter_Csv());
         $this->assertNotEmpty($this->_model->export());
+    }
+
+    /**
+     * Verify that all stock item attribute values are exported (aren't equal to empty string)
+     *
+     * @covers Mage_ImportExport_Model_Export_Entity_Product::export()
+     *
+     * @magentoDataFixture Mage/ImportExport/_files/product.php
+     */
+    public function testExportStockItemAttributesAreFilled()
+    {
+        $this->_model->setWriter(new Mage_ImportExport_Model_Export_Adapter_IntegrationTest())
+            ->export();
+    }
+}
+
+/**
+ * We create our test writer class to be sure that data sent to writer
+ * from Mage_ImportExport_Model_Export_Entity_Product::export() is correct and not affected by writer
+ */
+class Mage_ImportExport_Model_Export_Adapter_IntegrationTest extends
+    Mage_ImportExport_Model_Export_Adapter_Abstract
+{
+    /**
+     * Verify row data (stock item attribute values)
+     *
+     * @param array $rowData
+     * @return Mage_ImportExport_Model_Export_Adapter_IntegrationTest
+     */
+    public function writeRow(array $rowData)
+    {
+        foreach (Mage_ImportExport_Model_Export_Entity_ProductTest::$stockItemAttributes as $stockItemAttribute) {
+            PHPUnit_Framework_TestCase::assertNotSame('', $rowData[$stockItemAttribute],
+                "Stock item attribute {$stockItemAttribute} value is empty string"
+            );
+        }
     }
 }
