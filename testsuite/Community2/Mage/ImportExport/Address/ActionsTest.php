6a200d2626cf96c30cfc223e155ca4cf6dcee8e4
Roman Grebenchuk <rgrebenchuk@ebay.com>
2012-07-03 19:54:43 +0300

    Implemented MAGETWO-1956: Implemented TL-MAGE-5686, TL-MAGE-5687, TL-MAGE-5688

--- testsuite/Community2/Mage/ImportExport/Address/ActionsTest.php
+++ testsuite/Community2/Mage/ImportExport/Address/ActionsTest.php
@@ -55,7 +55,7 @@ class Enterprise2_Mage_ImportExport_CustomActionsImportAddressTest extends Mage_
                 'address' => $this->customerHelper()->isAddressPresent($userAddressData)
             );
         }
-        for($i = 2; $i<3; $i++){
+        for($i = 2; $i<4; $i++){
             $this->admin('manage_customers');
             $userData = $this->loadDataSet('ImportExport', 'generic_customer_account');
             $userAddressData = $this->loadDataSet('ImportExport', 'generic_address');
@@ -106,7 +106,7 @@ class Enterprise2_Mage_ImportExport_CustomActionsImportAddressTest extends Mage_
      */
     public function updateActionImport(array $data)
     {
-        //Precondition: create 2 new customers
+        //Precondition: set data for CSV
         $i = 0;
         foreach($data as $customerData){
             if ($data[$i]['_email'] == '%realEmail%'){
@@ -118,7 +118,7 @@ class Enterprise2_Mage_ImportExport_CustomActionsImportAddressTest extends Mage_
             $i++;
         }
         $this->admin('import');
-        $this->importExportHelper()->chooseImportOptions('Customers', 'Add/Update Complex Data',
+        $this->importExportHelper()->chooseImportOptions('Customers', 'Custom Action',
             'Magento 2.0 format', 'Customer Addresses');
         //Step 5, 6, 7
         $importResult = $this->importExportHelper()->import($data);
@@ -136,14 +136,15 @@ class Enterprise2_Mage_ImportExport_CustomActionsImportAddressTest extends Mage_
         $this->assertArrayHasKey('success', $importResult['import'], 'Import has been finished with issues: '
             . print_r($importResult));
         //Verifying
-        $i = 0;
+
         $userAddressData = $this->loadDataSet('ImportExport', 'generic_address');
-        foreach(self::$customersData as $customerData){
+        for($i = 0; $i < 4; $i++){
              $this->admin('manage_customers');
              $this->addParameter(
                  'customer_first_last_name',
-                 $customerData['first_name'] . ' ' . $customerData['last_name']);
-             $this->customerHelper()->openCustomer(array('email' => $customerData['email']));
+                 self::$customersData[$i]['first_name'] . ' ' . self::$customersData[$i]['last_name']
+             );
+             $this->customerHelper()->openCustomer(array('email' => self::$customersData[$i]['email']));
              $userAddressData['first_name'] = $data[$i]['firstname'];
              $userAddressData['last_name'] = $data[$i]['lastname'];
              $userAddressData['city'] = $data[$i]['city'];
@@ -157,7 +158,6 @@ class Enterprise2_Mage_ImportExport_CustomActionsImportAddressTest extends Mage_
              }
             $this->assertFalse(!$this->customerHelper()->isAddressPresent($userAddressData),
                              'Address not found for: ' . print_r($userAddressData));
-            $i++;
         }
     }
     /**
@@ -167,19 +167,19 @@ class Enterprise2_Mage_ImportExport_CustomActionsImportAddressTest extends Mage_
      * <p>Verify that all Customers finance information was imported</p>
      *
      * @test
-     * @dataProvider importUpdateData
-     * @TestlinkId TL-MAGE-5689
+     * @dataProvider importEmptyData
+     * @TestlinkId TL-MAGE-5688
      */
     public function emptyActionImport(array $data)
     {
-        //Precondition: create 2 new customers
+        //Precondition: set data for CSV
         $i = 0;
         foreach($data as $customerData){
             if ($data[$i]['_email'] == '%realEmail%'){
-                $data[$i]['_email'] = self::$customersData[$i]['email'];
+                $data[$i]['_email'] = self::$customersData[4]['email'];
             }
             if ($data[$i]['_entity_id'] == '%realEntityID%'){
-                $data[$i]['_entity_id'] = self::$customersData[$i]['address'];
+                $data[$i]['_entity_id'] = self::$customersData[4+$i]['address'];
             }
             $i++;
         }
@@ -194,7 +194,7 @@ class Enterprise2_Mage_ImportExport_CustomActionsImportAddressTest extends Mage_
         $this->assertArrayHasKey('error', $importResult['validation'], 'Import has been finished without issues: '
             . print_r($importResult));
         $this->assertEquals(
-            "E-mail is invalid in rows: 5",
+            "Invalid value in Website column (website does not exists?) in rows: 4",
             $importResult['validation']['error'][0],
             'Import has been finished with issues: ' . print_r($importResult));
         $this->assertArrayHasKey('import', $importResult, 'Import has been finished with issues: '
@@ -202,28 +202,29 @@ class Enterprise2_Mage_ImportExport_CustomActionsImportAddressTest extends Mage_
         $this->assertArrayHasKey('success', $importResult['import'], 'Import has been finished with issues: '
             . print_r($importResult));
         //Verifying
-        $i = 0;
         $userAddressData = $this->loadDataSet('ImportExport', 'generic_address');
-        foreach(self::$customersData as $customerData){
+        foreach($data as $customerData){
             $this->admin('manage_customers');
             $this->addParameter(
                 'customer_first_last_name',
-                $customerData['first_name'] . ' ' . $customerData['last_name']);
-            $this->customerHelper()->openCustomer(array('email' => $customerData['email']));
-            $userAddressData['first_name'] = $data[$i]['firstname'];
-            $userAddressData['last_name'] = $data[$i]['lastname'];
-            $userAddressData['city'] = $data[$i]['city'];
-            $userAddressData['zip_code'] = $data[$i]['postcode'];
-            $userAddressData['telephone'] = $data[$i]['telephone'];
-            $userAddressData['street_address_line_1'] = $data[$i]['street'];
+                self::$customersData[4]['first_name'] . ' ' . self::$customersData[4]['last_name']);
+            $this->customerHelper()->openCustomer(array('email' => self::$customersData[4]['email']));
+            $userAddressData['first_name'] = ($customerData['firstname']!='')?$customerData['firstname']:$userAddressData['first_name'];
+            $userAddressData['last_name'] = ($customerData['lastname']!='')?$customerData['lastname']:$userAddressData['last_name'];
+            $userAddressData['city'] = ($customerData['city']!='')?$customerData['city']:$userAddressData['city'];
+            $userAddressData['zip_code'] = ($customerData['postcode']!='')?$customerData['postcode']:$userAddressData['zip_code'];
+            $userAddressData['telephone'] = ($customerData['telephone']!='')?$customerData['telephone']:$userAddressData['telephone'];
+            $userAddressData['street_address_line_1'] = ($customerData['street']!='') ? $customerData['street']:$userAddressData['street_address_line_1'];
             $userAddressData['street_address_line_2'] = '';
-            $userAddressData['state'] = $data[$i]['region'];
-            if ($data[$i]['_entity_id'] == 'home'){
-                $userAddressData['middle_name'] = '';
+            $userAddressData['state'] = ($customerData['region']!='')? $customerData['region']:$userAddressData['state'];
+            if ($customerData['_website'] == 'invalid'){
+                $this->assertTrue(!$this->customerHelper()->isAddressPresent($userAddressData),
+                    'Address found for: ' . print_r($userAddressData));
+                $this->clearMessages();
+            } else {
+                $this->assertFalse(!$this->customerHelper()->isAddressPresent($userAddressData),
+                    'Address not found for: ' . print_r($userAddressData));
             }
-            $this->assertFalse(!$this->customerHelper()->isAddressPresent($userAddressData),
-                'Address not found for: ' . print_r($userAddressData));
-            $i++;
         }
     }
 
@@ -331,6 +332,95 @@ class Enterprise2_Mage_ImportExport_CustomActionsImportAddressTest extends Mage_
                         '_address_default_shipping_' => '',
                         '_entity_id' => '1234',
                         'action' => 'Update'
+                    )
+                )
+            )
+        );
+    }
+    public function importEmptyData()
+    {
+        return array(
+            array(
+                array(
+                    array(
+                        '_website' => 'base',
+                        '_email' => '%realEmail%',
+                        'region' => 'New York',
+                        'city' => 'New York',
+                        'country_id' => 'US',
+                        'firstname' => 'John',
+                        'lastname' => 'Nothing',
+                        'postcode' => '10005',
+                        'street' => 'ave 250',
+                        'telephone' => '1010101',
+                        'company' => "Earl Abel's",
+                        'fax' => '',
+                        'middlename' => '',
+                        'prefix' => '',
+                        '_address_default_billing_' => '',
+                        '_address_default_shipping_' => '',
+                        '_entity_id' => '%realEntityID%',
+                        'action' => ''
+                    ),
+                    array(
+                        '_website' => 'base',
+                        '_email' => '%realEmail%',
+                        'region' => '',
+                        'city' => '',
+                        'country_id' => '',
+                        'firstname' => '',
+                        'lastname' => '',
+                        'postcode' => '',
+                        'street' => '',
+                        'telephone' => '',
+                        'company' => '',
+                        'fax' => '',
+                        'middlename' => '',
+                        'prefix' => '',
+                        '_address_default_billing_' => '',
+                        '_address_default_shipping_' => '',
+                        '_entity_id' => '%realEntityID%',
+                        'action' => 'Please, delete'
+                    ),
+                    array(
+                        '_website' => 'base',
+                        '_email' => '%realEmail%',
+                        'region' => 'New York',
+                        'city' => 'New York',
+                        'country_id' => 'US',
+                        'firstname' => 'John',
+                        'lastname' => 'Nothing',
+                        'postcode' => '10007',
+                        'street' => 'ave 250',
+                        'telephone' => '1010101',
+                        'company' => "Earl Abel's",
+                        'fax' => '',
+                        'middlename' => '',
+                        'prefix' => '',
+                        '_address_default_billing_' => '',
+                        '_address_default_shipping_' => '',
+                        '_entity_id' => '',
+                        'action' => ''
+                    ),
+                    array(
+                        '_website' => 'invalid',
+                        '_email' => '%realEmail%',
+                        'region' => 'New York',
+                        'city' => 'New York',
+                        'country_id' => 'US',
+                        'firstname' => 'John',
+                        'lastname' => 'Nothing',
+                        'postcode' => '10008',
+                        'street' => 'ave 250',
+                        'telephone' => '1010101',
+                        'company' => "Earl Abel's",
+                        'fax' => '',
+                        'middlename' => '',
+                        'prefix' => '',
+                        '_address_default_billing_' => '',
+                        '_address_default_shipping_' => '',
+                        '_entity_id' => '',
+                        'action' => 'Please, delete'
                     )
                 )
             )
