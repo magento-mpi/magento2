135c9c8301777e0f18fd2ee4974586ba3ae65f5d
yaroslav.goncharuk <ygoncharuk@ebay.com>
2012-07-03 18:38:09 +0300

    [TEST] Implemented MAGETWO-1921 Update tags status with Mass action created tests for Tags Batch Updates functionality

--- testsuite/Community2/Mage/BatchUpdates/Tags/Test.php
+++ testsuite/Community2/Mage/BatchUpdates/Tags/Test.php
@@ -3,14 +3,14 @@
  * {license_notice}
  *
  * @category    Magento
- * @package     Mage_Tags
+ * @package     Community2_Mage_BatchUpdates_Tags_Test
  * @subpackage  functional_tests
  * @copyright   {copyright}
  * @license     {license_link}
  */
 
 /**
- * Tags deleting using batch updates tests for Backend
+ * Tags deleting/updating using batch updates tests for Backend
  *
  * @package     selenium
  * @subpackage  tests
@@ -66,5 +66,67 @@ class Community2_Mage_BatchUpdates_Tags_Test extends Mage_Selenium_TestCase
         $this->assertMessagePresent('success', 'success_saved_tag');
         //Steps
         $this->tagsHelper()->deleteAllTags();
+    }
+
+    /**
+     * <p>Deleting all tags using Batch Updates</p>
+     * <p>Steps:</p>
+     * <p>1. Click button "Add New Tag"</p>
+     * <p>2. Fill in the fields in General Information</p>
+     * <p>3. Click button "Save Tag"</p>
+     * <p>4. Click button "Add New Tag"</p>
+     * <p>5. Fill in the fields in General Information</p>
+     * <p>6. Click button "Save Tag"</p>
+     * <p>7. Click link "Select All"</p>
+     * <p>8. Select value "Change Status in "Action dropdown"</p>
+     * <p>9. Click button "Submit"</p>
+     * <p>Expected result:</p>
+     * <p>Received the message that the tags has been updated.</p>
+     *
+     * @test
+     * @TestlinkId
+     */
+    public function changeStatusTagsByBatchUpdates()
+    {
+        //Data
+        $setData1 = $this->loadDataSet('Tag', 'backend_new_tag');
+        $setData2 = $this->loadDataSet('Tag', 'backend_new_tag');
+        //Steps
+        $this->tagsHelper()->addTag($setData1);
+        $this->tagsHelper()->addTag($setData2);
+        //Verify
+        $this->assertTrue($this->checkCurrentPage('all_tags'), $this->getParsedMessages());
+        $this->assertMessagePresent('success', 'success_saved_tag');
+
+        if ($this->controlIsPresent('message', 'no_records_found')) {
+            return true;
+        }
+        $this->clickControl('link', 'select_all', false);
+        $this->waitForAjax();
+        $this->fillDropdown('tags_massaction', 'Change status');
+        $this->fillDropdown('tags_status', 'Disabled');
+        $this->_parseMessages();
+        foreach (self::$_messages as $key => $value) {
+            self::$_messages[$key] = array_unique($value);
+        }
+        $success = $this->_getMessageXpath('general_success');
+        $error = $this->_getMessageXpath('general_error');
+        $validation = $this->_getMessageXpath('general_validation');
+        $types = array('success', 'error', 'validation');
+        foreach ($types as $message) {
+            if (array_key_exists($message, self::$_messages)) {
+                $exclude = '';
+                foreach (self::$_messages[$message] as $messageText) {
+                    $exclude .= "[not(..//.='$messageText')]";
+                }
+                ${$message} .= $exclude;
+            }
+        }
+        $this->clickButton('submit', true);
+        $this->waitForElement(array($success, $error, $validation));
+        $this->addParameter('id', $this->defineIdFromUrl());
+        $this->validatePage();
+        $this->assertMessagePresent('success');
+        return true;
     }
 }
