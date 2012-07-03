7a32838dc41b9e9df80245d5ef4fc3710b9a919a
Dmytro Iekutenko <diekutenko@ebay.com>
2012-07-03 17:58:29 +0300

    Implemented MAGETWO-2031: Сover the “vde_checkbox” widget with js unit tests - Tests for JavaScript code implemented for widget "vde_checkbox"

---
+++ dev/tests/js/testsuite/mage/design_editor/checkboxTest.js
@@ -0,0 +1,41 @@
+/**
+ * {license_notice}
+ *
+ * @category    mage.design_editor
+ * @package     test
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+CheckboxTest = TestCase('CheckboxTest');
+
+CheckboxTest.prototype.testInit = function() {
+    /*:DOC += <div id="checkbox"></div> */
+    var checkbox = jQuery('#checkbox').vde_checkbox();
+    assertEquals(true, checkbox.is(':vde-vde_checkbox'));
+    checkbox.vde_checkbox('destroy');
+};
+
+CheckboxTest.prototype.testDefaultOptions = function() {
+    /*:DOC += <div id="checkbox"></div> */
+    var checkbox = jQuery('#checkbox').vde_checkbox();
+    assertEquals('checked', checkbox.vde_checkbox('option', 'checkedClass'));
+    checkbox.vde_checkbox('destroy');
+};
+
+CheckboxTest.prototype.testClickEvent = function() {
+    /*:DOC += <div id="checkbox"></div> */
+    var checkbox = jQuery('#checkbox').vde_checkbox();
+    var checkedClass = checkbox.vde_checkbox('option', 'checkedClass');
+    var checkedEventIsTriggered = false;
+    var uncheckedEventIsTriggered = false;
+    checkbox.on('checked.vde_checkbox', function() {checkedEventIsTriggered = true;});
+    checkbox.on('unchecked.vde_checkbox', function() {uncheckedEventIsTriggered = true;});
+    checkbox.trigger('click');
+    assertEquals(true, checkbox.hasClass(checkedClass));
+    assertEquals(true, checkedEventIsTriggered);
+    assertEquals(false, uncheckedEventIsTriggered);
+    checkbox.trigger('click');
+    assertEquals(false, checkbox.hasClass(checkedClass));
+    assertEquals(true, uncheckedEventIsTriggered);
+    checkbox.vde_checkbox('destroy');
+};
