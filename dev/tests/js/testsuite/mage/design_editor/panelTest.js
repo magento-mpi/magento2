487c69aec3bad7367cfbb2ce86ba1990d76f44e2
Dmytro Iekutenko <diekutenko@ebay.com>
2012-07-05 17:32:25 +0300

    Fixed MAGETWO-2008: Сover the “vde_panel” widget with js unit tests - Fixed code formatting

--- dev/tests/js/testsuite/mage/design_editor/panelTest.js
+++ dev/tests/js/testsuite/mage/design_editor/panelTest.js
@@ -7,44 +7,44 @@
  * @license     {license_link}
  */
 PanelTest = TestCase('PanelTest');
-
 PanelTest.prototype.testInit = function() {
-    /*:DOC += <div class="panel"></div> */
-    var panel = jQuery('.panel').vde_panel();
+    /*:DOC += <div id="panel"></div> */
+    var panel = jQuery('#panel').vde_panel();
     assertEquals(true, panel.is(':vde-vde_panel'));
     panel.vde_panel('destroy');
 };
-
 PanelTest.prototype.testDefaultOptions = function() {
-    /*:DOC += <div class="panel"></div> */
-    var panel = jQuery('.panel').vde_panel();
+    /*:DOC += <div id="panel"></div> */
+    var panel = jQuery('#panel').vde_panel();
     assertEquals('.vde_toolbar_cell', panel.vde_panel('option', 'cellSelector'));
     assertEquals('#vde_handles_hierarchy', panel.vde_panel('option', 'handlesHierarchySelector'));
     assertEquals('#vde_handles_tree', panel.vde_panel('option', 'treeSelector'));
     panel.vde_panel('destroy');
 };
-
 PanelTest.prototype.testInitToolbalCell = function() {
-    /*:DOC += <div class="panel">
+    /*:DOC +=
+    <div id="panel">
         <div class="vde_toolbar_cell">
              <div class="vde_toolbar_cell_title" />
              <div class="vde_toolbar_cell_content"  />
         </div>
-    </div> */
-    var panel = jQuery('.panel').vde_panel();
+    </div>
+    */
+    var panel = jQuery('#panel').vde_panel();
     var cellSelector = panel.vde_panel('option', 'cellSelector');
     assertEquals(true, panel.find(cellSelector).is(':vde-vde_menu'));
     panel.vde_panel('destroy');
 };
-
 PanelTest.prototype.testInitHandlesHierarchy = function() {
-    /*:DOC += <div class="panel">
+    /*:DOC +=
+    <div id="panel">
         <div class="vde_toolbar_cell" id="vde_handles_hierarchy">
             <div class="vde_toolbar_cell_title" />
             <div class="vde_toolbar_cell_content"  />
         </div>
-     </div> */
-    var panel = jQuery('.panel').vde_panel();
+    </div>
+    */
+    var panel = jQuery('#panel').vde_panel();
     var handlesHierarchySelector = panel.vde_panel('option', 'handlesHierarchySelector');
     var treeSelector = panel.vde_panel('option', 'treeSelector');
     var handlesHierarchy = panel.find(handlesHierarchySelector);
