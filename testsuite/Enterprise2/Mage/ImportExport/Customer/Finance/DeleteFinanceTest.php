ad613005f7f5bab817ce39ac677ee035bd7b6aeb
Irina Glazunova <iglazunova@ebay.com>
2012-07-06 18:22:47 +0300

    Implementation MAGETWO-2141: TL-MAGE-5717

--- testsuite/Enterprise2/Mage/ImportExport/Customer/Finance/DeleteFinanceTest.php
+++ testsuite/Enterprise2/Mage/ImportExport/Customer/Finance/DeleteFinanceTest.php
@@ -230,4 +230,115 @@ class Enterprise2_Mage_ImportExport_FinanceEmptyValues extends Mage_Selenium_Tes
             ))
         );
     }
+    /**
+     * <p>Deleting customer finances with wrong or not cpecified _finance_website</p>
+     * <p>Preconditions:</p>
+     * <p>1. Create two customers in Customers-> Manage Customers</p>
+     * <p>2. Update for both customers "Store Credit" and "Reward Points"</p>
+     * <p>3. Create csv file with empty _finance_website for customer1, with incorrect  _finance_website for customer2</p>
+     * <p>Steps</p>
+     * <p>1. In System -> Import/ Export -> Import in drop-down "Entity Type" select "Customers"</p>
+     * <p>2. Select "Delete" in selector "Import Behavior"</p>
+     * <p>3. Select "Magento 2.0 format"</p>
+     * <p>4. Select "Customer Finances"</p>
+     * <p>5. Choose file from precondition</p>
+     * <p>6. Press "Check Data"</p>
+     * <p>7. Open Customers-> Manage Customers</p>
+     * <p>Expected: After step 6 the message 'File is totaly invalid' is appeared</p>
+     * <p>Expected: After step 7 the finances for both customers aren't deleted</p>
+     *
+     * @test
+     * @dataProvider importFinanceData1
+     * @TestlinkId TL-MAGE-5717
+     */
+     public function deletingCustomerFinanceWithWrongFinanceWebsite($data)
+     {
+
+         //Create Customer1
+         $this->navigate('manage_customers');
+         $userData1 = $this->loadDataSet('Customers', 'generic_customer_account');
+         $this->customerHelper()->createCustomer($userData1);
+         $this->assertMessagePresent('success', 'success_saved_customer');
+         //Create Customer2
+         $this->navigate('manage_customers');
+         $userData2 = $this->loadDataSet('Customers', 'generic_customer_account');
+         $this->customerHelper()->createCustomer($userData2);
+         $this->assertMessagePresent('success', 'success_saved_customer');
+
+         //Update Customer 1
+         $this->addParameter('customer_first_last_name', $userData1['first_name'] . ' ' . $userData1['last_name']);
+         $this->customerHelper()->openCustomer(array('email' => $userData1['email']));
+
+         $this->customerHelper()->updateStoreCreditBalance(array('update_balance' => '100'));
+         $userData1['update_balance'] = '100';
+         $this->assertMessagePresent('success', 'success_saved_customer');
+         $this->customerHelper()->openCustomer(array('email' => $userData1['email']));
+         $this->customerHelper()->updateRewardPointsBalance(array('update_balance' => '150'));
+         $userData1['update_balance'] = '150';
+         $this->assertMessagePresent('success', 'success_saved_customer');
+
+         //Update Customer 2
+         $this->addParameter('customer_first_last_name', $userData2['first_name'] . ' ' . $userData2['last_name']);
+         $this->customerHelper()->openCustomer(array('email' => $userData2['email']));
+
+         $this->customerHelper()->updateStoreCreditBalance(array('update_balance' => '200'));
+         $userData2['update_balance'] = '200';
+         $this->assertMessagePresent('success', 'success_saved_customer');
+         $this->customerHelper()->openCustomer(array('email' => $userData2['email']));
+         $this->customerHelper()->updateRewardPointsBalance(array('update_balance' => '250'));
+         $userData2['update_balance'] = '250';
+         $this->assertMessagePresent('success', 'success_saved_customer');
+
+         $data[0]['email'] = $userData1['email'];;
+
+         $data[1]['email'] = $userData2['email'];
+
+         $this->admin('import');
+         $this->importExportHelper()->chooseImportOptions('Customers', 'Delete Entities',
+             'Magento 2.0 format', 'Customer Finances');
+         //Step 5, 6, 7
+         $report = $this->importExportHelper()->import($data);
+         //Check import
+         $this->assertArrayNotHasKey('import', $report, 'Import has been finished with issues:');
+         $this->assertArrayHasKey('error', $report['validation'], 'Import has been finished with issues:');
+         $this->assertEquals('Invalid value in Finance information website column in rows: 1',$report['validation']['error'][0]);
+         $this->assertEquals('Finance information website is not specified in rows: 2',$report['validation']['error'][1]);
+         //Step 8
+         $this->navigate('manage_customers');
+         //Step 9. First Customer
+         $this->addParameter('customer_first_last_name', $userData1['first_name'] . ' ' . $userData1['last_name']);
+         $this->customerHelper()->openCustomer(array('email' => $userData1['email']));
+         //Verify customer account
+         $this->assertEquals('$100.00', $this->customerHelper()->getStoreCreditBalance(),
+             'Store Credit balance is deleted');
+         $this->assertEquals('150', $this->customerHelper()->getRewardPointsBalance(),
+             'Reward Points Balance is deleted');
+         //Step 9. Second Customer
+         $this->navigate('manage_customers');
+         $this->addParameter('customer_first_last_name', $userData2['first_name'] . ' ' . $userData2['last_name']);
+         $this->customerHelper()->openCustomer(array('email' => $userData2['email']));
+         //Verify customer account
+         $this->assertEquals('$200.00', $this->customerHelper()->getStoreCreditBalance(),
+             'Store Credit balance is deleted');
+         $this->assertEquals('250', $this->customerHelper()->getRewardPointsBalance(),
+             'Reward Points Balance is deleted');
+     }
+    public function importFinanceData1()
+    {
+        return array(
+            array(array($this->loadDataSet('ImportExport', 'generic_finance_csv',
+                array(
+
+                    'store_credit' => '100',
+                    'reward_points' => '150',
+                    '_finance_website' => ''
+                )),
+                $this->loadDataSet('ImportExport', 'generic_finance_csv',
+                    array(
+                    'store_credit' => '200',
+                    'reward_points' => '250',
+                    '_finance_website' => $this->generate('string', 30, ':digit:')
+                ))))
+            );
+    }
 }
