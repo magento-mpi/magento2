978e021e03f368d15f824aa1a108fe8cd5e257fc
Kai Yan <kayan@x.com>
2012-07-06 10:28:58 -0500

    Implement MAGETWO-1185: javascript refactor - revert js test driver port back to 9876

--- dev/build/bin/js-test.sh
+++ dev/build/bin/js-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -r $2
 mkdir $2
-java -jar JsTestDriver.jar --config $1 --port 9877 --browser /usr/bin/firefox --tests all --testOutput $2
+java -jar JsTestDriver.jar --config $1 --port 9876 --browser /usr/bin/firefox --tests all --testOutput $2
