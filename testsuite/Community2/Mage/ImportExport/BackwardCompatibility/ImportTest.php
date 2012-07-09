93758b984ca731233ef26bb57cfafea1e3b52997
Iuliia Babenko <ibabenko@ebay.com>
2012-07-09 20:22:20 +0300

    Implemented MAGETWO-2178: added TL-MAGE-1167

--- testsuite/Community2/Mage/ImportExport/BackwardCompatibility/ImportTest.php
+++ testsuite/Community2/Mage/ImportExport/BackwardCompatibility/ImportTest.php
@@ -93,7 +93,7 @@ class Community2_Mage_ImportExport_Backward_Import_CustomerTest extends Mage_Sel
         //calculate number of entities in csv file
         $numberOfEntities = 0;
         foreach ($report as $value) {
-            if($value['email'] != '') {
+            if ($value['email'] != '') {
                 $numberOfEntities++;
             }
         }
@@ -111,5 +111,90 @@ class Community2_Mage_ImportExport_Backward_Import_CustomerTest extends Mage_Sel
             'Validation message is not correct');
         $this->assertEquals('File is valid! To start import process press "Import" button  Import',
             $importData['validation']['success'][0], 'Success message is not correct');
+    }
+
+    /**
+     * <p>Required columns</p>
+     * <p>Precond
+     * <p>Steps</p>
+     * <p>1. Go to System -> Import / Export -> Import</p>
+     * <p>2. Select Entity Type: Customers</p>
+     * <p>3. Select Export Format Version: Magento 1.7 format</p>
+     * <p>4. In Import Behavior dropdown field choose Append Complex Data parameter</p>
+     * <p>5. Choose file from precondition</p>
+     * <p>6. Click on Check Data</p>
+     * <p>7. Click on Import button</p>
+     * <p>8. Open Customers -> Manage Customers</p>
+     * <p>9. Open each of imported customers</p>
+     * <p>Expected: </p>
+     * <p>After step 6</p>
+     * <p>Verify that file is valid, the message 'File is valid!' is displayed</p>
+     * <p>After step 7</p>
+     * <p>Verify that import starting. The message 'Import successfully done.' is displayed</p>
+     * <p>After step 8</p>
+     * <p>Verify that imported customers display on customers grid</p>
+     * <p>After step 9</p>
+     * <p>Verify that all Customer information was imported</p>
+     *
+     * @test
+     * @dataProvider importWithRequiredColumnsData
+     * @TestlinkId TL-MAGE-1167
+     */
+    public function importWithRequiredColumns($data)
+    {
+        //Set email for existing customer
+        $data[0]['email'] = self::$customerData['email'];
+        //Steps 2-4
+        $this->importExportHelper()->chooseImportOptions('Customers', 'Append Complex Data',
+            'Magento 1.7 format');
+        //Steps 5-7
+        $report = $this->importExportHelper()->import($data);
+        //Verify import
+        $this->assertArrayHasKey('import', $report, 'Import has been finished with issues:' .
+            print_r($report) . print_r($data));
+        $this->assertArrayHasKey('success', $report['import'], 'Import has been finished with issues:' .
+            print_r($report) . print_r($data));
+        //Check updated customer
+        self::$customerData['first_name'] = $data[0]['firstname'];
+        self::$customerData['last_name'] = $data[0]['lastname'];
+        $this->admin('manage_customers');
+        $this->addParameter('customer_first_last_name',
+            self::$customerData['first_name'] . ' ' . self::$customerData['last_name']);
+        $this->customerHelper()->openCustomer(
+            array(
+                'email' => self::$customerData['email'])
+            );
+        $this->assertTrue($this->verifyForm(self::$customerData, 'account_information'),
+            'Customer has not been updated');
+        //Check new customer
+        $customerData = $this->loadDataSet('Customers', 'generic_customer_account', array(
+            'email' => $data[1]['email'],
+            'first_name' => $data[1]['firstname'],
+            'last_name' => $data[1]['lastname'],
+        ));
+        $this->admin('manage_customers');
+        $this->assertTrue($this->customerHelper()->isCustomerPresentInGrid($customerData),
+            'Customer has not been created');
+        $this->addParameter('customer_first_last_name', $customerData['first_name'] . ' ' . $customerData['last_name']);
+        $this->customerHelper()->openCustomer(
+            array(
+                'email' => strtolower($customerData['email'])
+            ));
+    }
+
+    public function importWithRequiredColumnsData()
+    {
+        return array(
+            array(
+                array($this->loadDataSet('ImportExport', 'generic_customer_csv', array(
+                    'firstname' => 'New First Name',
+                    'lastname' => 'New Last Name',
+                    '_store' => '',
+                )),
+                    $this->loadDataSet('ImportExport', 'generic_customer_csv', array(
+                    '_store' => '',
+                )),
+            )
+        ));
     }
  }
