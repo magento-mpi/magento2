447630128c64667d7464f4a151eae6cde21f7720
Kai Yan <kayan@x.com>
2012-07-09 10:22:06 -0500

    Implement MAGETWO-1185: javascript refactor - new js-test.sh to include Xvfb to run firefox in memory

--- dev/build/bin/js-test.sh
+++ dev/build/bin/js-test.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 rm -r $2
 mkdir $2
-java -jar JsTestDriver.jar --config $1 --port 9876 --browser /usr/bin/firefox --tests all --testOutput $2
+
+XVFB=`which Xvfb`
+if [ "$?" -eq 1 ];
+then
+    echo "Xvfb not found."
+    exit 1
+fi
+
+FIREFOX=`which firefox`
+if [ "$?" -eq 1 ];
+then
+    echo "Firefox not found."
+    exit 1
+fi
+
+$XVFB :99 -ac &    # launch virtual framebuffer into the background
+PID_XVFB="$!"      # take the process ID
+export DISPLAY=:99 # set display to use that of the xvfb
+
+# run the tests
+java -jar JsTestDriver.jar --config $1 --port 9876 --browser $FIREFOX --tests all --testOutput $2
+
+kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
+echo "Done."
