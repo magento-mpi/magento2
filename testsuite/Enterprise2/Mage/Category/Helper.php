886c59712be6b4c1c9b40c08b8da2946b951dc96
Artem Shugayev <ashugayev@ebay.com>
2012-07-10 19:08:03 +0300

    [HELPER][UIMAP] Fixed Helper for permissions in category

--- testsuite/Enterprise2/Mage/Category/Helper.php
+++ testsuite/Enterprise2/Mage/Category/Helper.php
@@ -42,53 +42,28 @@ class Enterprise2_Mage_Category_Helper extends Core_Mage_Category_Helper
      */
     public function fillCategoryInfo(array $categoryData)
     {
-        $page = $this->getCurrentUimapPage();
-        $tabs = $page->getAllTabs();
-        foreach ($tabs as $tab => $values)
-        {
-            if ($tab != 'category_products')
-            {
-                if ($tab == 'category_pemissions')
-                {
-                    if (($this->controlIsPresent('tab', 'category_pemissions')) && (isset($categoryData['customer_group'])))
-                    {
-                        $this->openTab($tab);
-                        $this->clickButton('new_permission', false);
-                        $this->addParameter('row','1');
-                        if (($this->controlIsPresent('dropdown', 'website'))&& (isset($categoryData['website'])))
-                        {
-                            $this->fillFieldset(array('website' => $categoryData['website']),'category_permissions');
-                        }
-                        $this->fillFieldset(array('customer_group' => $categoryData['customer_group']),'category_permissions');
-                        if (isset($categoryData['browsing_category']))
-                        {
-                            $this->addParameter('permissionCategory',$categoryData['browsing_category']);
-                            $this->clickControl('field','browsing_category',false);
-                        }
-                        if (isset($categoryData['displaying_price']))
-                        {
-                            $this->addParameter('permissionPrice',$categoryData['displaying_price']);
-                            $this->clickControl('field','displaying_price',false);
-                        }
-                        if (isset($categoryData['add_to_cart']))
-                        {
-                            $this->addParameter('permissionCart',$categoryData['add_to_cart']);
-                            $this->clickControl('field','add_to_cart',false);
-                        }
-                    }
-                }
-                else
-                    $this->fillForm($categoryData, $tab);
+        parent::fillCategoryInfo($categoryData);
+        if (isset($categoryData['category_permissions'])) {
+            $this->openTab('category_permissions_tab');
+            $count = 1;
+            foreach ($categoryData['category_permissions'] as $permission) {
+                $this->clickButton('new_permission', false);
+                $this->waitForAjax();
+                $this->addParameter('row', $count);
+                $this->fillFieldset($permission, 'category_permissions');
+                $count++;
             }
-            else
-            {
-                $arrayKey = $tab . '_data';
-                $this->openTab($tab);
-                if (array_key_exists($arrayKey, $categoryData) && is_array($categoryData[$arrayKey])) {
-                    foreach ($categoryData[$arrayKey] as $value) {
-                        $this->productHelper()->assignProduct($value, $tab);
-                    }
-                }
+        }
+    }
+
+    public function deletePermissions()
+    {
+        if (isset($categoryData['category_permissions'])) {
+            $this->openTab('category_permissions_tab');
+            foreach ($categoryData['category_permissions'] as $permission) {
+                $this->clickButton('new_permission', false);
+                $this->waitForAjax();
+                $this->fillFieldset($permission, 'category_permissions');
             }
         }
     }
