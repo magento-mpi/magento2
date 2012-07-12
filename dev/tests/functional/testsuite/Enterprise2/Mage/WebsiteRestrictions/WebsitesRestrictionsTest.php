0f056c01a0401f5b6c2a4ffa9c003c75e443e756
Artem Shugayev <ashugayev@ebay.com>
2012-07-12 11:06:36 +0300

    [TEST] Renamed WebsiteRestrictions test

--- dev/tests/functional/testsuite/Enterprise2/Mage/WebsiteRestrictions/WebsitesRestrictionsTest.php
+++ dev/tests/functional/testsuite/Enterprise2/Mage/WebsiteRestrictions/WebsitesRestrictionsTest.php
@@ -31,7 +31,7 @@
  * @subpackage  tests
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Enterprise2_Mage_WebsiteRestrictions extends Mage_Selenium_TestCase
+class Enterprise2_Mage_WebsiteRestrictionsTest extends Mage_Selenium_TestCase
 {
     /**
      * <p>Preconditions:</p>
