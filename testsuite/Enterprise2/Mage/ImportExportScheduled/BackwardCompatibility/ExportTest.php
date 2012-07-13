f030da2279084e2b255da35fbdea5677fedb75ef
Roman Grebenchuk <rgrebenchuk@ebay.com>
2012-07-13 13:55:48 +0300

    Fixed MAGETWO-2240: Removed helper from Community2 scope.

--- testsuite/Enterprise2/Mage/ImportExportScheduled/BackwardCompatibility/ExportTest.php
+++ testsuite/Enterprise2/Mage/ImportExportScheduled/BackwardCompatibility/ExportTest.php
@@ -19,7 +19,7 @@
 * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 * @method Community2_Mage_ImportExportScheduled_Helper  importExportScheduledHelper() importExportScheduledHelper()
 */
-class Community2_Mage_ImportExportScheduled_Backward_Export_CustomerTest extends Mage_Selenium_TestCase
+class Enterprise2_Mage_ImportExportScheduled_Backward_Export_CustomerTest extends Mage_Selenium_TestCase
 {
     protected function assertPreConditions()
     {
