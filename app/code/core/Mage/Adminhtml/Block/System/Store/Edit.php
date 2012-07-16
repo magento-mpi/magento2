f3c3ddd875c6cc36d0dd554b63ada5e0b244683c
Vladimir Korotovskih <vladimir.korotovskih@magento.com>
2012-07-16 14:01:35 +0300

    Implemented MAGETWO-2076: Cyclomatic Complexity Method Refactoring - refactored method Mage_Adminhtml_Block_System_Store_Edit_Form::_prepareForm

--- app/code/core/Mage/Adminhtml/Block/System/Store/Edit.php
+++ app/code/core/Mage/Adminhtml/Block/System/Store/Edit.php
@@ -29,19 +29,28 @@ class Mage_Adminhtml_Block_System_Store_Edit extends Mage_Adminhtml_Block_Widget
                 $this->_objectId = 'website_id';
                 $saveLabel   = Mage::helper('Mage_Core_Helper_Data')->__('Save Website');
                 $deleteLabel = Mage::helper('Mage_Core_Helper_Data')->__('Delete Website');
-                $deleteUrl   = $this->getUrl('*/*/deleteWebsite', array('item_id' => Mage::registry('store_data')->getId()));
+                $deleteUrl   = $this->getUrl(
+                    '*/*/deleteWebsite',
+                    array('item_id' => Mage::registry('store_data')->getId())
+                );
                 break;
             case 'group':
                 $this->_objectId = 'group_id';
                 $saveLabel   = Mage::helper('Mage_Core_Helper_Data')->__('Save Store');
                 $deleteLabel = Mage::helper('Mage_Core_Helper_Data')->__('Delete Store');
-                $deleteUrl   = $this->getUrl('*/*/deleteGroup', array('item_id' => Mage::registry('store_data')->getId()));
+                $deleteUrl   = $this->getUrl(
+                    '*/*/deleteGroup',
+                    array('item_id' => Mage::registry('store_data')->getId())
+                );
                 break;
             case 'store':
                 $this->_objectId = 'store_id';
                 $saveLabel   = Mage::helper('Mage_Core_Helper_Data')->__('Save Store View');
                 $deleteLabel = Mage::helper('Mage_Core_Helper_Data')->__('Delete Store View');
-                $deleteUrl   = $this->getUrl('*/*/deleteStore', array('item_id' => Mage::registry('store_data')->getId()));
+                $deleteUrl   = $this->getUrl(
+                    '*/*/deleteStore',
+                    array('item_id' => Mage::registry('store_data')->getId())
+                );
                 break;
             default:
                 $saveLabel = '';
@@ -91,5 +100,20 @@ class Mage_Adminhtml_Block_System_Store_Edit extends Mage_Adminhtml_Block_Widget
         }
 
         return Mage::registry('store_action') == 'add' ? $addLabel : $editLabel;
+    }
+
+    protected function _prepareLayout()
+    {
+        if ($this->_blockGroup && $this->_controller && $this->_mode
+            && !$this->_layout->getChildName($this->_nameInLayout, 'form')
+        ) {
+            $this->setChild('form', $this->getLayout()->createBlock(
+                $this->_blockGroup
+                    . '_Block_'
+                    . str_replace(' ', '_', ucwords(str_replace('_', ' ', $this->_controller . '_' . $this->_mode)))
+                    . '_Form' . '_' . ucwords(Mage::registry('store_type'))
+            ));
+        }
+        return parent::_prepareLayout();
     }
 }
