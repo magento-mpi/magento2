4089ecf36f3289b14f446c27fc6fa75bd9ce8e50
Roman Grebenchuk <rgrebenchuk@ebay.com>
2012-07-16 12:37:00 +0300

    Fixed MAGETWO-2240: Added preconditions as separated test cases

--- testsuite/Community2/Mage/ImportExport/Customer/ExportTest.php
+++ testsuite/Community2/Mage/ImportExport/Customer/ExportTest.php
@@ -20,19 +20,7 @@
 */
 class Community2_Mage_ImportExport_Export_CustomerTest extends Mage_Selenium_TestCase
 {
-    /**
-     * <p>set preconditions to run tests </p>
-     * <p>System settings:</p>
-     * <p>Secure Key is disabled</p>
-     * <p>HttpOnly cookies is disabled</p>
-     */
-    public function setUpBeforeTests()
-    {
-        $this->loginAdminUser();
-        $this->navigate('system_configuration');
-        $this->systemConfigurationHelper()->configure('General/disable_httponly');
-        $this->systemConfigurationHelper()->configure('Advanced/disable_secret_key');
-    }
+
     /**
      * <p>Preconditions:</p>
      * <p>Log in to Backend.</p>
---
+++ testsuite/Community2/Mage/ImportExport/Customer/_preconditionsTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Magento
+ *
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_ImportExport
+ * @subpackage  functional_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+* Customer Export
+*
+* @package     selenium
+* @subpackage  tests
+* @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*/
+class Community2_Mage_ImportExport_preconditionTest extends Mage_Selenium_TestCase
+{
+    /**
+     * <p>set preconditions to run tests </p>
+     * <p>System settings:</p>
+     * <p>Secure Key is disabled</p>
+     * <p>HttpOnly cookies is disabled</p>
+     *
+     * @test
+     */
+    public function preconditionForTests()
+    {
+        $this->loginAdminUser();
+        $this->navigate('system_configuration');
+        $this->systemConfigurationHelper()->configure('General/disable_httponly');
+        $this->systemConfigurationHelper()->configure('Advanced/disable_secret_key');
+    }
+}
