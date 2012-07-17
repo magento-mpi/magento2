dfb849c48a6bbf2b7123d15f88a10bdb55cd179f
Maksym Iakusha <miakusha@ebay.com>
2012-07-17 13:49:51 +0300

    [HELPER][TEST][DATA][UIMAP] Add tests for Category Permissions on Category level

--- testsuite/Enterprise2/Mage/Category/CategoryPermissions/CategoryLevelTest.php
+++ testsuite/Enterprise2/Mage/Category/CategoryPermissions/CategoryLevelTest.php
@@ -35,6 +35,7 @@
 
 class Enterprise2_Mage_Category_CategoryPermissions_CategoryLevelTest extends Mage_Selenium_TestCase
 {
+
     /**
      * Create website, category, customer and product
      * return array
@@ -53,13 +54,13 @@ class Enterprise2_Mage_Category_CategoryPermissions_CategoryLevelTest extends Ma
         $userData = $this->loadDataSet('Customers', 'generic_customer_account');
         //Steps
         $this->loginAdminUser();
-        $this->navigate('manage_stores');
-        $this->storeHelper()->createStore($websiteData, 'website');
-        $this->assertMessagePresent('success', 'success_saved_website');
-        $this->storeHelper()->createStore($storeData, 'store');
-        $this->assertMessagePresent('success', 'success_saved_store');
-        $this->storeHelper()->createStore($storeViewData, 'store_view');
-        $this->assertMessagePresent('success', 'success_saved_store_view');
+//        $this->navigate('manage_stores');
+//        $this->storeHelper()->createStore($websiteData, 'website');
+//        $this->assertMessagePresent('success', 'success_saved_website');
+//        $this->storeHelper()->createStore($storeData, 'store');
+//        $this->assertMessagePresent('success', 'success_saved_store');
+//        $this->storeHelper()->createStore($storeViewData, 'store_view');
+//        $this->assertMessagePresent('success', 'success_saved_store_view');
         $this->navigate('manage_categories');
         $this->categoryHelper()->checkCategoriesPage();
         $this->categoryHelper()->createCategory($category);
@@ -154,27 +155,50 @@ class Enterprise2_Mage_Category_CategoryPermissions_CategoryLevelTest extends Ma
     }
 
     /**
-     * test
-     */
-    public function samePermissions()
-    {
-
-    }
-
-    /**
+     * <p>Deny Add to cart</p>
+     * <p>Steps</p>
+     * <p>1.Go to Catalog -> Categories -> Manage Categories</p>
+     * <p>2.Select "Category" in category tree</p>
+     * <p>3.Open Category Permissions tab</p>
+     * <p>4.Click "New Permission" button</p>
+     * <p>5.Select "All Website"</p>
+     * <p>6.Select "All Customer Groups" in Customer Group</p>
+     * <p>7.Select "Allow" in Browsing Category column</p>
+     * <p>8.Select "Allow" in Display Product Prices column</p>
+     * <p>9.Select "Deny" in Add to Cart column</p>
+     * <p>10.Click "Save Category" button</p>
+     * <p>11.Clear Magento Cache </p>
+     * <p>12.Open category page at frontend</p>
+     * <p>13.Open Product page</p>
+     * <p></p>
+     * <p></p>
+     * <p>Expected result:</p>
+     * <p>1. After 12 Product price is visible, "Add to cart" button is missing</p>
+     * <p>2. After 13 Product price is visible, "Add to cart" button is missing</p>
+     *
      * @param array $testData
-     * @test
+     * test
      * @depends preconditionsForTests
+     * @TestlinkId TL-MAGE-5029
      */
     public function denyAddToCart($testData)
     {
         //Data
-        $permission = $this->loadDataSet('Category', 'permissions_deny_add_to_cart_for_guest');
+        $config = $this->loadDataSet('CategoryPermissions', 'category_permissions_enable');
+        $permission = $this->loadDataSet('Category', 'permissions_deny_add_to_cart');
         $this->addParameter('productName', $testData['product']['name']);
         $this->addParameter('price', '$' . $testData['product']['price']);
-        //Steps
+        //Preconditions
         $this->loginAdminUser();
-        $this->categoryHelper()->setPermissions($permission, $testData['catPath']);
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->deleteAllPermissions($testData['catPath']);
+        //Steps
+        $this->navigate('system_configuration');
+        $this->systemConfigurationHelper()->configure($config);
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->selectCategory($testData['catPath']);
+        $this->categoryHelper()->fillCategoryInfo($permission);
+        $this->clickButton('save_category');
         $this->pleaseWait();
         $this->assertMessagePresent('success', 'success_saved_category');
         $this->clearInvalidedCache();
@@ -188,28 +212,245 @@ class Enterprise2_Mage_Category_CategoryPermissions_CategoryLevelTest extends Ma
     }
 
     /**
+     * <p>Deny display price</p>
+     * <p>Steps</p>
+     * <p>1.Go to Catalog -> Categories -> Manage Categories</p>
+     * <p>2.Select "Category" in category tree</p>
+     * <p>3.Open Category Permissions tab</p>
+     * <p>4.Click "New Permission" button</p>
+     * <p>5.Select "All Website"</p>
+     * <p>6.Select "All Customer Groups" in Customer Group</p>
+     * <p>7.Select "Allow" in Browsing Category column</p>
+     * <p>8.Select "Deny" in Display Product Prices column</p>
+     * <p>9.Click "Save Category" button</p>
+     * <p>10.Clear Magento Cache </p>
+     * <p>11.Open category page at frontend</p>
+     * <p>12.Open Product page</p>
+     * <p></p>
+     * <p></p>
+     * <p>Expected result:</p>
+     * <p>1. After 11 Product price and "Add to cart" button are missing</p>
+     * <p>2. After 12 Product price and "Add to cart" button are missing</p>
+     *
+     * @param array $testData
      * test
+     * @depends preconditionsForTests
+     * @TestlinkId TL-MAGE-5028
      */
-    public function denyDisplayPrices()
+    public function denyDisplayPrices($testData)
     {
-
+        //Data
+        $config = $this->loadDataSet('CategoryPermissions', 'category_permissions_enable');
+        $permission = $this->loadDataSet('Category', 'permissions_deny_display_prices');
+        $this->addParameter('productName', $testData['product']['name']);
+        $this->addParameter('price', '$' . $testData['product']['price']);
+        //Preconditions
+        $this->logoutCustomer();
+        $this->loginAdminUser();
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->deleteAllPermissions($testData['catPath']);
+        //Steps
+        $this->navigate('system_configuration');
+        $this->systemConfigurationHelper()->configure($config);
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->selectCategory($testData['catPath']);
+        $this->categoryHelper()->fillCategoryInfo($permission);
+        $this->clickButton('save_category');
+        $this->pleaseWait();
+        $this->assertMessagePresent('success', 'success_saved_category');
+        $this->clearInvalidedCache();
+        $this->frontend();
+        $this->categoryHelper()->frontOpenCategory($testData['catName']);
+        $this->assertFalse($this->controlIsPresent('button', 'add_to_cart'), 'Button "Add to cart" should be absent');
+        $this->assertFalse($this->controlIsPresent('pageelement', 'price_regular'), 'Product price must be present');
+        $this->productHelper()->frontOpenProduct($testData['product']['name']);
+        $this->assertFalse($this->controlIsPresent('button', 'add_to_cart'), 'Button "Add to cart" should be absent');
+        $this->assertFalse($this->controlIsPresent('pageelement', 'price_regular'), 'Product price must be present');
     }
 
     /**
+     * <p>Deny Browsing Category</p>
+     * <p>Steps</p>
+     * <p>1.Go to Catalog -> Categories -> Manage Categories</p>
+     * <p>2.Select "Category" in category tree</p>
+     * <p>3.Open Category Permissions tab</p>
+     * <p>4.Click "New Permission" button</p>
+     * <p>5.Select "All Website"</p>
+     * <p>6.Select "All Customer Groups" in Customer Group</p>
+     * <p>7.Select "Allow" in Browsing Category column</p>
+     * <p>8.Select "Deny" in Display Product Prices column</p>
+     * <p>9.Click "Save Category" button</p>
+     * <p>10.Clear Magento Cache </p>
+     * <p>11.Open category page at frontend</p>
+     * <p>12.Open Product page</p>
+     * <p></p>
+     * <p></p>
+     * <p>Expected result:</p>
+     * <p>1. After 11 Product price and "Add to cart" button are missing</p>
+     * <p>2. After 12 Product price and "Add to cart" button are missing</p>
+     *
+     * @param array $testData
      * test
+     * @depends preconditionsForTests
+     * @TestlinkId TL-MAGE-5020
      */
-    public function denyBrowsingCategory()
+    public function denyBrowsingCategory($testData)
     {
-
+        //Data
+        $config = $this->loadDataSet('CategoryPermissions', 'category_permissions_enable');
+        $permission = $this->loadDataSet('Category', 'permissions_deny_browsing_category');
+        //Preconditions
+        $this->logoutCustomer();
+        $this->loginAdminUser();
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->deleteAllPermissions($testData['catPath']);
+        //Steps
+        $this->navigate('system_configuration');
+        $this->systemConfigurationHelper()->configure($config);
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->selectCategory($testData['catPath']);
+        $this->categoryHelper()->fillCategoryInfo($permission);
+        $this->clickButton('save_category');
+        $this->pleaseWait();
+        $this->assertMessagePresent('success', 'success_saved_category');
+        $this->clearInvalidedCache();
+        $this->frontend();
+        $catXpath = '//ul[@id="nav"]' . '//li[contains(a/span,"' . $testData['catName'] . '")]';
+        print_r ($catXpath);
+        $this->assertFalse($this->isElementPresent($catXpath));
     }
 
     /**
+     * <p>Set up several permissions for category</p>
+     * <p>Steps</p>
+     * <p>1.Go to System -> Configuration -> Catalog -> Catalog -> Category Permissions</p>
+     * <p>2.Select "Yes" in Enable field</p>
+     * <p>3.Select "No" in Allow Browsing Category</p>
+     * <p>4.Save config</p>
+     * <p>5.Go to Catalog -> Categories -> Manage Categories</p>
+     * <p>6.Select "Category" in category tree</p>
+     * <p>7.Open Category Permissions tab</p>
+     * <p>8.Click "New Permission" button</p>
+     * <p>9.Select "All Website"</p>
+     * <p>10.Select "All Customer Groups" in Customer Group</p>
+     * <p>11.Select "Allow" in Browsing Category column</p>
+     * <p>12.Select "Allow" in Display Product Prices column</p>
+     * <p>13.Select "Allow" in Add to Cart column</p>
+     * <p>14.Click "Save Category" button</p>
+     * <p>15.Clear Magento Cache </p>
+     * <p>16.Open frontend</p>
+     * <p>17.Open Category</p>
+     * <p>18.Open Product page</p>
+     * <p>Expected result:</p>
+     * <p>1. After 17 Category panel is present </p>
+     * <p>2. After 18 Product price and "Add to cart" button are present</p>
+     * <p>3. After 19 Product price and "Add to cart" button are present</p>
+     *
+     * @param array $testData
      * test
+     * @depends preconditionsForTests
+     * @TestlinkId TL-MAGE-5030, TL-MAGE-5040
      */
-    public function severalPermissions()
+    public function allowAll($testData)
     {
-
+        //Data
+        $config = $this->loadDataSet('CategoryPermissions', 'category_permissions_enable', array ('allow_browsing' => 'No, Redirect to Landing Page'));
+        $permission = $this->loadDataSet('Category', 'permissions_allow_all');
+        $this->addParameter('productName', $testData['product']['name']);
+        $this->addParameter('price', '$' . $testData['product']['price']);
+        //Preconditions
+        $this->logoutCustomer();
+        $this->loginAdminUser();
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->deleteAllPermissions($testData['catPath']);
+        //Steps
+        $this->navigate('system_configuration');
+        $this->systemConfigurationHelper()->configure($config);
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->selectCategory($testData['catPath']);
+        $this->categoryHelper()->fillCategoryInfo($permission);
+        $this->clickButton('save_category');
+        $this->pleaseWait();
+        $this->assertMessagePresent('success', 'success_saved_category');
+        $this->clearInvalidedCache();
+        $this->frontend();
+        $catXpath = '//ul[@id="nav"]' . '//li[contains(a/span,"' . $testData['catName'] . '")]';
+        $this->assertTrue($this->isElementPresent($catXpath));
+        $this->customerHelper()->frontLoginCustomer($testData['user']);
+        $this->categoryHelper()->frontOpenCategory($testData['catName']);
+        $this->assertTrue($this->controlIsPresent('button', 'add_to_cart'), 'Button "Add to cart" must be present');
+        $this->assertTrue($this->controlIsPresent('pageelement', 'price_regular'), 'Product price must be present');
+        $this->productHelper()->frontOpenProduct($testData['product']['name']);
+        $this->assertTrue($this->controlIsPresent('button', 'add_to_cart'), 'Button "Add to cart" must be present');
+        $this->assertTrue($this->controlIsPresent('pageelement', 'price_regular'), 'Product price must be present');
     }
 
-
+    /**
+     * <p>Set up several permissions for category</p>
+     * <p>Steps</p>
+     * <p>1.Go to Catalog -> Categories -> Manage Categories</p>
+     * <p>2.Select "Category" in category tree</p>
+     * <p>3.Open Category Permissions tab</p>
+     * <p>4.Click "New Permission" button</p>
+     * <p>5.Select "All Website"</p>
+     * <p>6.Select "All Customer Groups" in Customer Group</p>
+     * <p>7.Select "Deny" in Browsing Category column</p>
+     * <p>8.Click "New Permission" button</p>
+     * <p>9.Select "All Website</p>
+     * <p>10.Select "General" in Customer Group</p>
+     * <p>11.Select "Allow" in Browsing Category column</p>
+     * <p>12.Select "Allow" in Display Product Prices column</p>
+     * <p>13.Select "Deny" in Add to Cart column</p>
+     * <p>14.Click "Save Category" button</p>
+     * <p>15.Clear Magento Cache </p>
+     * <p>16.Open frontend</p>
+     * <p>17.Login to frontend</p>
+     * <p>18.Open Category</p>
+     * <p>19.Open Product page</p>
+     * <p>Expected result:</p>
+     * <p>1. After 16 Category button is absent</p>
+     * <p>2. After 17 Category button is present </p>
+     * <p>3. After 18 Product price is visible, "Add to cart" button is missing</p>
+     * <p>4. After 19 Product price is visible, "Add to cart" button is missing</p>
+     *
+     * @param array $testData
+     * @test
+     * @depends preconditionsForTests
+     * @TestlinkId TL-MAGE-5035, TL-MAGE-5042
+     */
+    public function severalPermissions($testData)
+    {
+        //Data
+        $config = $this->loadDataSet('CategoryPermissions', 'category_permissions_enable');
+        $permission = $this->loadDataSet('Category', 'several_permissions');
+        $this->addParameter('productName', $testData['product']['name']);
+        $this->addParameter('price', '$' . $testData['product']['price']);
+        $this->addParameter('catName', $testData['catName']);
+        //Preconditions
+        $this->logoutCustomer();
+        $this->loginAdminUser();
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->deleteAllPermissions($testData['catPath']);
+        //Steps
+        $this->navigate('system_configuration');
+        $this->systemConfigurationHelper()->configure($config);
+        $this->navigate('manage_categories');
+        $this->categoryHelper()->selectCategory($testData['catPath']);
+        $this->categoryHelper()->fillCategoryInfo($permission);
+        $this->clickButton('save_category');
+        $this->pleaseWait();
+        $this->assertMessagePresent('success', 'success_saved_category');
+        $this->clearInvalidedCache();
+        $this->frontend();
+       // $catXpath = '//ul[@id="nav"]' . '//li[contains(a/span,"' . $testData['catName'] . '")]';
+        $this->assertFalse($this->controlIsPresent('button', 'category_button'));
+        $this->customerHelper()->frontLoginCustomer($testData['user']);
+        $this->assertTrue($this->controlIsPresent('button', 'category_button'));
+        $this->categoryHelper()->frontOpenCategory($testData['catName']);
+        $this->assertFalse($this->controlIsPresent('button', 'add_to_cart'), 'Button "Add to cart" should be absent');
+        $this->assertTrue($this->controlIsPresent('pageelement', 'price_regular'), 'Product price must be present');
+        $this->productHelper()->frontOpenProduct($testData['product']['name']);
+        $this->assertFalse($this->controlIsPresent('button', 'add_to_cart'), 'Button "Add to cart" should be absent');
+        $this->assertTrue($this->controlIsPresent('pageelement', 'price_regular'), 'Product price must be present');
+    }
 }
