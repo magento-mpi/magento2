121b9569cebfea5e6d14fd8853fea99a336824b1
Sergey Shymko <sergey.shymko@magento.com>
2012-07-23 16:34:41 -0700

    Implemented MAGETWO-2346: Unified CI Build, Plan and Agents Configuration - introduced directory 'dev/build/unified' for to contain configuration for unified CI plans -- introduced subdirectory 'core_dev' for builds of 'Core, Development' CI project - implemented database credentials substitution for integration tests on MSSQL and Oracle (besides MySQL) - implemented database credentials and web access information substitution for performance tests - preserved old builds configuration in order to maintain fully-working legacy builds until transition to unified approach is complete

---
+++ dev/build/unified/core_dev/performance_tests/config.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     performance_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+return array(
+    'application' => array(
+        'url_host' => '{{web_access_host}}',
+        'url_path' => '{{web_access_path}}',
+        'installation' => array(
+            'options' => array(
+                'license_agreement_accepted' => 'yes',
+                'locale'                     => 'en_US',
+                'timezone'                   => 'America/Los_Angeles',
+                'default_currency'           => 'USD',
+                'db_host'                    => '{{db_host}}',
+                'db_name'                    => '{{db_name}}',
+                'db_user'                    => '{{db_user}}',
+                'db_pass'                    => '{{db_password}}',
+                'use_secure'                 => 'no',
+                'use_secure_admin'           => 'no',
+                'use_rewrites'               => 'no',
+                'admin_lastname'             => 'Admin',
+                'admin_firstname'            => 'Admin',
+                'admin_email'                => 'admin@example.com',
+                'admin_username'             => 'admin',
+                'admin_password'             => '123123q',
+            ),
+            'fixture_files' => 'testsuite/fixtures/*.php',
+        ),
+    ),
+    'scenario' => array(
+        'files' => 'testsuite/{home_page,product_view,category_view,quick_search,advanced_search}.jmx',
+        'common_params' => array(
+            'users' => 10,
+            'loops' => 100,
+        ),
+    ),
+    'report_dir' => 'report',
+);
