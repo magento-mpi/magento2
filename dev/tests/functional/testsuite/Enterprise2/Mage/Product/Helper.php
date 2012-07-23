6be1b12fe1ab1ffecd21bae28130d6c667d82a7f
Dmytro Aponasenko <daponasenko@ebay.com>
2012-07-23 21:07:13 +0300

    CR Change(s) MAGETWO-2229: Testing ability to copy Custom Option from existing product - [HELPER]: modified method deleteAllCustomOptions() - [TEST]: fixed space issues

--- dev/tests/functional/testsuite/Enterprise2/Mage/Product/Helper.php
+++ dev/tests/functional/testsuite/Enterprise2/Mage/Product/Helper.php
@@ -1,14 +1,15 @@
 <?php
-# Magento
-#
-# {license_notice}
-#
-# @category    Magento
-# @package     Mage_Product
-# @subpackage  functional_tests
-# @copyright   {copyright}
-# @license     {license_link}
-#
+/**
+ * Magento
+ *
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Product
+ * @subpackage  functional_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
 /**
  * Helper class
  */
