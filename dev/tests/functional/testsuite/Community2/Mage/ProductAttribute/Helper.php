bdacdc8529bb64338947ad2c269dc81713a63ed6
Dmytro Aponasenko <daponasenko@ebay.com>
2012-07-24 16:29:39 +0300

    Implemented MAGETWO-1932: Testing ability to define default values for system attributes - [DATA][HELPER][TEST]: implemented test cases for functionality to define default values for system attributes

---
+++ dev/tests/functional/testsuite/Community2/Mage/ProductAttribute/Helper.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Magento
+ *
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_ProductAttribute
+ * @subpackage  functional_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/**
+ * Helper class
+ */
+class Community2_Mage_ProductAttribute_Helper extends Core_Mage_ProductAttribute_Helper
+{
+    /**
+     * Set default value for dropdown attribute
+     *
+     * @param array $attrData
+     */
+    public function setDefaultAttributeValue(array $attrData)
+    {
+        $num = 1;
+        $fieldSetXpath = $this->_getControlXpath('fieldset', 'manage_options');
+        $option = $this->getXpathCount($fieldSetXpath . "//tr[contains(@class,'option-row')]");
+        foreach ($attrData as $fKey => $dValue) {
+            if (preg_match('/^option_/', $fKey) and is_array($attrData[$fKey])) {
+                if ($this->controlIsPresent('fieldset', 'manage_options')) {
+                    if ($option > 0) {
+                        $fieldOptionNumber = $this->getAttribute(
+                            $fieldSetXpath . "//tr[contains(@class,'option-row')][" . $num
+                            . "]//input[@class='input-radio']/@value");
+                        $this->addParameter('fieldOptionNumber', $fieldOptionNumber);
+                        if ($attrData[$fKey]['admin_option_name'] == $attrData['default_value']) {
+                            $this->fillFieldset(array('is_default' => 'Yes'), 'manage_options');
+                        }
+                        $num++;
+                        $option--;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Verify dropdown system attribute on Manage Options tab: Manage Titles is present, Manage Options are present and disabled, Delete and Add Option buttons are absent
+     *
+     * @param array $attrData
+     */
+    public function verifySystemAttribute($attrData)
+    {
+        $this->openTab('manage_labels_options');
+        $this->storeViewTitles($attrData, 'manage_titles', 'verify');
+        $this->assertFalse($this->buttonIsPresent('add_option'), 'It is possible to add new option');
+        $this->assertFalse($this->buttonIsPresent('delete_option'), 'Delete button is present in Manage Options tab');
+        $fieldSetXpath = $this->_getControlXpath('fieldset', 'manage_options');
+        $option = $this->getXpathCount($fieldSetXpath . "//tr[contains(@class,'option-row')]");
+        $num = 1;
+        foreach ($attrData as $fKey => $dValue) {
+            if (preg_match('/^option_/', $fKey) and is_array($attrData[$fKey])) {
+                if ($this->controlIsPresent('fieldset', 'manage_options')) {
+                    if ($option > 0) {
+                        $fieldOptionNumber = $this->getAttribute(
+                            $fieldSetXpath . "//tr[contains(@class,'option-row')][" . $num
+                            . "]//input[@class='input-radio']/@value");
+                        $this->addParameter('fieldOptionNumber', $fieldOptionNumber);
+                        $this->assertTrue($this->isElementPresent("//tr[contains(@class,'option-row')][" . $num
+                                                                  . "]//input[@class='input-text required-option' and @disabled='disabled']"),
+                            'Admin value attribute is not disabled');
+                        $num++;
+                        $option--;
+                    }
+                }
+            }
+        }
+    }
+}
