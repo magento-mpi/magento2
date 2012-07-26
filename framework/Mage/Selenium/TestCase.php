27bd6db66375ddfb4abe98a7ea715c9dc23c412e
E.Maksimenko <emaksimenko@ebay.com>
2012-07-26 18:15:50 +0300

    [LIB] revert changes in function pleaseWait()

--- framework/Mage/Selenium/TestCase.php
+++ framework/Mage/Selenium/TestCase.php
@@ -3581,7 +3581,8 @@ class Mage_Selenium_TestCase extends PHPUnit_Extensions_SeleniumTestCase
      */
     public function pleaseWait($waitAppear = 10, $waitDisappear = 30)
     {
-        $this->waitForElementPresent(self::$xpathLoadingHolder, $waitAppear * 1000);
+        //$this->waitForElementPresent(self::$xpathLoadingHolder, $waitAppear * 1000);
+        $this->waitForAjax();
         $this->waitForElementNotPresent(self::$xpathLoadingHolder, $waitDisappear * 1000);
 
         return $this;
