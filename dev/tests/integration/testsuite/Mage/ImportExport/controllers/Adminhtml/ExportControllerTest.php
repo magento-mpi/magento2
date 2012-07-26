e9ff70d4ee41ad3244b799f04d5e31aa2feb142e
Yevgeniy Shishkin <yshishkin@ebay.com>
2012-07-26 12:28:02 +0300

    Implemented MAGETWO-2512: UX changes on Export page - wrote integration test for preparing of export form

--- dev/tests/integration/testsuite/Mage/ImportExport/controllers/Adminhtml/ExportControllerTest.php
+++ dev/tests/integration/testsuite/Mage/ImportExport/controllers/Adminhtml/ExportControllerTest.php
@@ -83,7 +83,7 @@ class Mage_ImportExport_Adminhtml_ExportControllerTest extends Mage_Adminhtml_Ut
         $this->dispatch('backend/admin/export/index');
 
         $body = $this->getResponse()->getBody();
-        $this->assertSelectCount('div#head-settings_fieldset', 1, $body);
-        $this->assertSelectCount('div#settings_fieldset', 1, $body);
+        $this->assertSelectCount('div#head-base_fieldset', 1, $body);
+        $this->assertSelectCount('div#base_fieldset', 1, $body);
     }
 }
