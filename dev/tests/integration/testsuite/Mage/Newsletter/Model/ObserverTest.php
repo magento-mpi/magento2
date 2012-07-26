a1c3c6f807f81c52ad07a4bd6b25516f1eaa2946
Sergey Shymko <sergey.shymko@magento.com>
2012-07-25 19:15:35 -0700

    Fixed MAGETWO-2529: Additional Events and Decoupling in Mage_Core_Model_Email_Template - fixed failed static tests (PHP Mess Detector)

--- dev/tests/integration/testsuite/Mage/Newsletter/Model/ObserverTest.php
+++ dev/tests/integration/testsuite/Mage/Newsletter/Model/ObserverTest.php
@@ -14,9 +14,9 @@ class Mage_Newsletter_Model_ObserverTest extends PHPUnit_Framework_TestCase
     public function testEmailTemplateFilterBefore()
     {
         $model = new Mage_Newsletter_Model_Template;
-        $id = uniqid();
-        $subscriber = new Mage_Newsletter_Model_Subscriber(array('store_id' => $id));
+        $expectedStoreId = uniqid();
+        $subscriber = new Mage_Newsletter_Model_Subscriber(array('store_id' => $expectedStoreId));
         $model->setTemplateText('{{var subscriber.getStoreId()}}');
-        $this->assertEquals($id, $model->getProcessedTemplate(array('subscriber' => $subscriber)));
+        $this->assertEquals($expectedStoreId, $model->getProcessedTemplate(array('subscriber' => $subscriber)));
     }
 }
