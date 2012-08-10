45a52018c811f3171a5ded6c77bcd99bbe9f81e0
Dmytro Iekutenko <diekutenko@ebay.com>
2012-08-10 18:56:31 +0300

    Implemented MAGETWO-2573: Migration of js Calendar on jQuery - Apply jQuery Code Style

--- pub/js/mage/adminhtml/calendar.js
+++ pub/js/mage/adminhtml/calendar.js
@@ -18,43 +18,47 @@
             this.options = $.extend(
                 {},
                 $.calendarConfig ? $.calendarConfig : {},
-                this.options.showsTime ? {showTime: true, showHour: true, showMinute: true} : {},
+                this.options.showsTime ? {
+                    showTime: true,
+                    showHour: true,
+                    showMinute: true
+                } : {},
                 this.options
             );
-            this._initPicker(this.element);
+            this._initPicker( this.element );
         },
 
         //Fix for iso am pm
-        _enableAMPM: function(){
-            if(this.options.timeFormat && this.options.timeFormat.indexOf('tt')) {
+        _enableAMPM: function() {
+            if ( this.options.timeFormat && this.options.timeFormat.indexOf( 'tt' ) ) {
                 this.options.ampm = true;
             }
         },
 
         //If server timezone is defined then take to account server timezone shift
-        getTimezoneDate: function(date){
+        getTimezoneDate: function( date ) {
             date = date || new Date();
-            if(typeof(this.options.serverTimezoneSeconds) != "undefined"){
-                date.setTime((this.options.serverTimezoneSeconds + date.getTimezoneOffset()*60)*1000);
+            if ( typeof( this.options.serverTimezoneSeconds ) != "undefined" ) {
+                date.setTime( ( this.options.serverTimezoneSeconds + date.getTimezoneOffset() * 60 ) * 1000 );
             }
             return date;
         },
 
         //Set current date if the date is not setted
-        _setCurrentDate: function(element){
-            if(!element.val()) {
+        _setCurrentDate: function( element ) {
+            if ( !element.val() ) {
                 element
-                    .datetimepicker('setDate', this.getTimezoneDate())
-                    .val('')
+                    .datetimepicker( 'setDate', this.getTimezoneDate() )
+                    .val( '' )
             }
         },
 
-        _initPicker: function(element) {
+        _initPicker: function( element ) {
             element
-                .datetimepicker(this.options)
-                .next(".ui-datepicker-trigger")
-                .addClass("v-middle");
-            this._setCurrentDate(element);
+                .datetimepicker( this.options )
+                .next( ".ui-datepicker-trigger" )
+                .addClass( "v-middle" );
+            this._setCurrentDate( element );
         }
     });
 
@@ -63,13 +67,13 @@
      */
     var calendarBasePrototype = $.mage.calendar.prototype;
     $.widget('mage.calendar', $.extend({}, calendarBasePrototype, {
-        dateTimeFormat : {
+        dateTimeFormat: {
             date: {
                 "EEEE": "DD",
                 "EEE": "D",
                 "D": "o",
-                "MMMM" : "MM",
-                "MMM" : "M",
+                "MMMM": "MM",
+                "MMM": "M",
                 "MM": "mm",
                 "M": "mm",
                 "yyyy": "yy"
@@ -80,24 +84,24 @@
                 "H": "h"
             }
         },
-        _create: function(){
-            if(this.options.dateFormat) {
-                this.options.dateFormat = this._convertFormat(this.options.dateFormat, 'date');
+        _create: function() {
+            if ( this.options.dateFormat ) {
+                this.options.dateFormat = this._convertFormat( this.options.dateFormat, 'date' );
             }
-            if(this.options.timeFormat) {
-                this.options.timeFormat = this._convertFormat(this.options.timeFormat, 'time');
+            if ( this.options.timeFormat ) {
+                this.options.timeFormat = this._convertFormat( this.options.timeFormat, 'time' );
             }
-            calendarBasePrototype._create.apply(this, arguments);
+            calendarBasePrototype._create.apply( this, arguments );
         },
-        _convertFormat: function(format, type){
-            var symbols = format.match(/([A-Za-z]+)/ig),
-                separators = format.match(/(\W)/ig),
+        _convertFormat: function( format, type ) {
+            var symbols = format.match( /([A-Za-z]+)/ig ),
+                separators = format.match( /(\W)/ig ),
                 self = this;
             convertedFormat = "";
-            $.each(symbols, function(key, val){
+            $.each( symbols, function(key, val) {
                 convertedFormat +=
-                    (self.dateTimeFormat[type][val] || val) +
-                        (separators[key] || '');
+                    ( self.dateTimeFormat[type][val] || val ) +
+                        ( separators[key] || '' );
             })
             return convertedFormat;
         }
@@ -107,18 +111,18 @@
      * Widget date_range
      */
     $.widget('mage.date_range', $.mage.calendar, {
-        _initPicker: function(){
-            if(this.options.from && this.options.to) {
-                var from = this.element.find('#' + this.options.from.id),
-                    to = this.element.find('#' + this.options.to.id );
+        _initPicker: function() {
+            if ( this.options.from && this.options.to ) {
+                var from = this.element.find( '#' + this.options.from.id ),
+                    to = this.element.find( '#' + this.options.to.id );
                 this.options.onSelect = function( selectedDate ) {
                     to.datetimepicker( "option", "minDate", selectedDate );
                 }
-                $.mage.calendar.prototype._initPicker.apply(this, [from]);
+                $.mage.calendar.prototype._initPicker.apply( this, [from] );
                 this.options.onSelect = function( selectedDate ) {
                     from.datetimepicker( "option", "maxDate", selectedDate );
                 }
-                $.mage.calendar.prototype._initPicker.apply(this, [to]);
+                $.mage.calendar.prototype._initPicker.apply( this, [to] );
             }
         }
     })
