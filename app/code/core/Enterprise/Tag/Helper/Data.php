f8c1ad47733342d2a7b68d1d19ab2a7cc9283018
Andrew Zavadsky <azavadsky@ebay.com>
2012-08-13 14:09:29 +0300

    Implemented MAGETWO-2257: Extract tags functionality from Enterprise_Reward module - temporary commit: added skeleton for Enterprise_Tag module

---
+++ app/code/core/Enterprise/Tag/Helper/Data.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Enterprise
+ * @package     Enterprise_Tag
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Tag Helper
+ *
+ * @category    Enterprise
+ * @package     Enterprise_Tag
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Enterprise_Tag_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
