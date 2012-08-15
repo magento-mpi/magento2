ff72f4868b8153c37ef68f780734c7f8e36301bc
bimathew <bibumathew@gmail.com>
2012-08-15 12:24:17 -0500

    Implement MAGETWO-1185: javascript refactor based on code comments - added jsTestDriver in build.xml

--- dev/build/bin/js-test.sh
+++ dev/build/bin/js-test.sh
@@ -18,7 +18,7 @@ PID_XVFB="$!"      # take the process ID
 export DISPLAY=:99 # set display to use that of the xvfb
 
 # run the tests
-java -jar JsTestDriver.jar --config $1 --port 9876 --browser $FIREFOX --tests all --testOutput $2
+java -jar $1 --config $2 --port 9876 --browser $FIREFOX --tests all --testOutput $3
 
 kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
 echo "Done."
