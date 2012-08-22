e7655a8dd3cdf9446034d1e7eae7722287bd5c99
Maxim Shikula <mshikula@ebay.com>
2012-08-22 16:36:00 +0300

    Re-factored MAGETWO-2801: Create Database Structure CR-changes

--- app/code/core/Mage/Core/sql/core_setup/upgrade-1.6.0.3-1.6.0.4.php
+++ app/code/core/Mage/Core/sql/core_setup/upgrade-1.6.0.3-1.6.0.4.php
@@ -14,6 +14,23 @@ $installer = $this;
 $installer->startSetup();
 
 /**
+ * Create table 'core_package'
+ */
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('core_package'))
+    ->addColumn('package_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity' => true,
+        'unsigned' => true,
+        'nullable' => false,
+        'primary'  => true,
+    ), 'Package identifier')
+    ->addColumn('package_code', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array('nullable' => false), 'Package Code')
+    ->addColumn('package_title', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array('nullable' => false), 'Package Title')
+    ->setComment('Core package');
+
+$installer->getConnection()->createTable($table);
+
+/**
  * Create table 'core_theme'
  */
 $table = $installer->getConnection()
@@ -24,8 +41,10 @@ $table = $installer->getConnection()
         'nullable' => false,
         'primary'  => true,
     ), 'Theme identifier')
-    ->addColumn('package_code', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array('nullable' => false), 'Package Code')
-    ->addColumn('package_title', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array('nullable' => false), 'Package Title')
+    ->addColumn('package_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned' => true,
+        'nullable' => false,
+    ), 'Package identifier')
     ->addColumn('parent_theme', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array('nullable' => true), 'Parent Theme')
     ->addColumn('theme_code', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array('nullable' => false), 'Theme Code')
     ->addColumn('theme_version', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array('nullable' => false), 'Theme Version')
@@ -35,7 +54,11 @@ $table = $installer->getConnection()
     ), 'Magento Version From')
     ->addColumn('magento_version_to', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
         'nullable' => false
-    ), 'Magento Version To');
+    ), 'Magento Version To')
+    ->addForeignKey($installer->getFkName('core_theme', 'package_id', 'core_package', 'package_id'),
+        'package_id', $installer->getTable('core_package'), 'package_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
+    ->setComment('Core theme');
 
 $installer->getConnection()->createTable($table);
 
