90c6c8fc69dc1f88345f3afdf2836dd60b2f891f
Andrey Voinkov <avoinkov@ebay.com>
2012-08-22 20:00:43 +0300

    Changed  MAGETWO-2805: Implement history management  - CR changes

--- app/code/core/Mage/DesignEditor/Model/Change/Collection.php
+++ app/code/core/Mage/DesignEditor/Model/Change/Collection.php
@@ -13,10 +13,20 @@
  */
 class Mage_DesignEditor_Model_Change_Collection extends Varien_Data_Collection
 {
-    const ITEM_TYPE = 'Mage_DesignEditor_Model_ChangeAbstract';
+    /**
+     * Collection item class
+     *
+     * @var string
+     */
+    protected $_itemObjectClass = 'Mage_DesignEditor_Model_ChangeAbstract';
 
+    /**
+     * Get collection item class
+     *
+     * @return string
+     */
     public function getItemClass()
     {
-        return self::ITEM_TYPE;
+        return $this->_itemObjectClass;
     }
 }
