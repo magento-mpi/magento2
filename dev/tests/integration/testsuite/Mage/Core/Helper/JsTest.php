ca670aa0706a473f7ab6d0f6ca98ca7ea40eba16
Hryhorii Hrebiniuk <hhrebiniuk@ebay.com>
2012-08-23 17:44:36 +0300

    Implemented MAGETWO-2934: Replace all usages of old Translate mechanism with new one

--- dev/tests/integration/testsuite/Mage/Core/Helper/JsTest.php
+++ dev/tests/integration/testsuite/Mage/Core/Helper/JsTest.php
@@ -37,7 +37,7 @@ class Mage_Core_Helper_JsTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals(
             '<script type="text/javascript">//<![CDATA['
-                . "\nvar Translator = new Translate({$this->_helper->getTranslateJson()});\n"
+                . '(function($) {$.mage.translate.add(' . $this->_helper->getTranslateJson() . ')})(jQuery);'
                 . '//]]></script>',
             $this->_helper->getTranslatorScript()
         );
--- lib/Varien/Data/Form/Element/Editor.php
+++ lib/Varien/Data/Form/Element/Editor.php
@@ -91,10 +91,8 @@ class Varien_Data_Form_Element_Editor extends Varien_Data_Form_Element_Textarea
                 . $this->serialize($this->getHtmlAttributes()) . ' >' . $this->getEscapedValue() . '</textarea>'
                 . $js . '
                 <script type="text/javascript">
-                //<![CDATA[
-                    if ("undefined" != typeof(Translator)) {
-                        Translator.add(' . Zend_Json::encode($translatedString) . ');
-                    }'
+                //<![CDATA['
+                    . '(function($) {$.mage.translate.add(' . Zend_Json::encode($translatedString) . ')})(jQuery);'
                     . $jsSetupObject . ' = new tinyMceWysiwygSetup("' . $this->getHtmlId() . '", '
                     . Zend_Json::encode($this->getConfig()).');'
                     . $forceLoad.'
