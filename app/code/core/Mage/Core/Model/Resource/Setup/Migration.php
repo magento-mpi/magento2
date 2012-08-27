8bcc3f26312ff7843663bdae104a3b6f37548d21
Ivan Klymenko <iklymenko@ebay.com>
2012-08-27 19:56:06 +0300

    Implemented MAGETWO-3028: Create setup class with main functionality to replace aliases - added draft setup class implementation

--- app/code/core/Mage/Core/Model/Resource/Setup/Migration.php
+++ app/code/core/Mage/Core/Model/Resource/Setup/Migration.php
@@ -13,44 +13,212 @@
  */
 class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_Setup
 {
-    const FIELD_CONTENT_TYPE_PLAIN = 'plain';
-    const FIELD_CONTENT_TYPE_XML   = 'xml';
-    const FIELD_CONTENT_TYPE_WIKI  = 'wiki';
+    const FIELD_CONTENT_TYPE_PLAIN       = 'plain';
+    const FIELD_CONTENT_TYPE_XML         = 'xml';
+    const FIELD_CONTENT_TYPE_WIKI        = 'wiki';
+    const FIELD_CONTENT_TYPE_SERIALIZED  = 'serialized';
 
-    protected $_rowsPerPage = 100;
+    const ENTITY_TYPE_MODEL    = 'Model';
+    const ENTITY_TYPE_BLOCK    = 'Block';
+    const ENTITY_TYPE_RESOURCE = 'Model_Resource';
 
-    public function updateClassAliases($tableName, $fieldName, $entityType,
-        $fieldContentType = self::FIELD_CONTENT_TYPE_PLAIN
+    protected $_rowsPerPage = 1;
+
+
+    protected $_replaceRules = array();
+
+    protected $_replacements = array();
+
+    public function appendClassAliasReplace($tableName, $fieldName, $entityType,
+        $fieldContentType = self::FIELD_CONTENT_TYPE_PLAIN, $additionalWhere = ''
     ) {
-        $pagesCount = ceil($this->_getRowsCount() / $this->_rowsPerPage);
+        if (!isset($this->_replaceRules[$tableName])) {
+            $this->_replaceRules[$tableName] = array();
+        }
 
-        for ($page = 1; $page <= $pagesCount; $page++) {
-            $this->_updateClassAliasesForPage($tableName, $fieldName, $entityType, $fieldContentType, $page);
+        if (!isset($this->_replaceRules[$tableName][$fieldName])) {
+            $this->_replaceRules[$tableName][$fieldName] = array(
+                'entity_type'      => $entityType,
+                'content_type'     => $fieldContentType,
+                'additional_where' => $additionalWhere
+            );
         }
     }
 
-    protected function _getRowsCount()
+    public function doUpdateClassAliases()
     {
-
-    }
-
-    protected function _updateClassAliasesForPage($tableName, $fieldName, $entityType,
-        $fieldContentType = self::FIELD_CONTENT_TYPE_PLAIN, $currPage
-    ) {
-        $rowsData = $this->_getRowsData($tableName, $fieldName, $currPage);
-
-        foreach ($rowsData as $rowData) {
-            $this->_replaceAlias($rowData, $tableName, $fieldName, $entityType, $fieldContentType);
+        foreach ($this->_replaceRules as $tableName => $tableRules) {
+            $this->_updateClassAliasesInTable($tableName, $tableRules);
         }
     }
 
-    protected function _getRowsData($tableName, $fieldName, $currPage)
+    //$tableRules == array([field name] => array('entity type' => [entity type], 'content_type' => [content type]))
+    protected function _updateClassAliasesInTable($tableName, array $tableRules)
     {
-        return array();
+        foreach ($tableRules as $fieldName => $fieldRule) {
+            $pagesCount = ceil(
+                $this->_getRowsCount($tableName, $fieldName, $fieldRule['additional_where']) / $this->_rowsPerPage
+            );
+
+            if (!isset($this->_replacements[$tableName])) {
+                $this->_replacements[$tableName] = array();
+            }
+
+            for ($page = 1; $page <= $pagesCount; $page++) {
+                $this->_applyFieldRule($tableName, $fieldName, $fieldRule, $page);
+            }
+        }
+
+        exit;
     }
 
-    protected function _replaceAlias($data, $tableName, $fieldName, $entityType, $fieldContentType)
+    protected function _getRowsCount($tableName, $fieldName, $additionalWhere = '')
     {
+        $adapter = $this->getConnection();
 
+        $query = $adapter->select()
+            ->from($adapter->getTableName($tableName), array('rows_count' => new Zend_Db_Expr('COUNT(*)')))
+            ->where($fieldName . ' IS NOT NULL');
+
+        if (!empty($additionalWhere)) {
+            $query->where($additionalWhere);
+        }
+
+        return $adapter->fetchOne($query);
+    }
+
+    protected function _applyFieldRule($tableName, $fieldName, array $fieldRule, $currPage)
+    {
+        $tableData = $this->_getTableData($tableName, $fieldName, $fieldRule['additional_where'], $currPage);
+
+        if (!isset($this->_replacements[$tableName][$fieldName])) {
+            $this->_replacements[$tableName][$fieldName] = array();
+        }
+
+        $fieldReplacements = array();
+        foreach ($tableData as $rowData) {
+            if (!empty($rowData[$fieldName])) {
+                if (!isset($fieldReplacements[$rowData[$fieldName]])
+                    && !isset($this->_replacements[$tableName][$fieldName][$rowData[$fieldName]])
+                ) {
+                    $replacement =
+                        $this->_getReplacement(
+                            $rowData[$fieldName],
+                            $fieldRule['entity_type'],
+                            $fieldRule['content_type']
+                        );
+
+                    if ($replacement) {
+                        $fieldReplacements[$rowData[$fieldName]] = $replacement;
+                    }
+                }
+            }
+        }
+
+        $this->_updateRowsData($tableName, $fieldName, $fieldReplacements);
+
+        $this->_replacements[$tableName][$fieldName] =
+            array_merge($this->_replacements[$tableName][$fieldName], $fieldReplacements);
+    }
+
+    protected function _updateRowsData($tableName, $fieldName, array $fieldReplacements)
+    {
+        $adapter = $this->getConnection();
+
+        foreach ($fieldReplacements as $from => $to) {
+            $adapter->update(
+                $adapter->getTableName($tableName),
+                array($fieldName => $to),
+                array($adapter->quoteIdentifier($fieldName) . ' = ?' => $from)
+            );
+        }
+    }
+
+    protected function _getTableData($tableName, $fieldName, $additionalWhere = '', $currPage = 0)
+    {
+        $adapter = $this->getConnection();
+
+        $query = $adapter->select()
+            ->from($adapter->getTableName($tableName), array($fieldName))
+            ->where($fieldName . ' IS NOT NULL');
+
+        if (!empty($additionalWhere)) {
+            $query->where($additionalWhere);
+        }
+
+        if ($currPage) {
+            $query->limitPage($currPage, $this->_rowsPerPage);
+        }
+
+        return $adapter->fetchAll($query);
+    }
+
+    protected function _getReplacement($data, $entityType, $contentType)
+    {
+        if ($contentType == self::FIELD_CONTENT_TYPE_PLAIN) {
+            return $this->_getCorrespondingClassName($data, $entityType);
+        }
+
+        // TODO add appropriate behavior for all exist FIELD CONTENT TYPES
+
+        return $data;
+    }
+
+    protected function _getCorrespondingClassName($alias, $entityType)
+    {
+        if ($this->_isFactoryName($alias)) {
+            list($module, $name) = $this->_getModuleName($alias);
+            $className = $this->_getClassName($module, $entityType, $name);
+
+            if (Magento_Autoload::getInstance()->classExists($className)) {
+                return $className;
+            }
+        }
+
+        return '';
+    }
+
+    /**
+     * Build class name supported in magento 2
+     *
+     * @param string $module
+     * @param string $type
+     * @param string $name
+     * @return string|bool
+     */
+    protected function _getClassName($module, $type, $name = null)
+    {
+        return implode('_', array_map('ucfirst', explode('_', $module . '_' . $type . '_' . $name)));
+    }
+
+    /**
+     * Whether the given class name is a factory name
+     *
+     * @param string $factoryName
+     * @return bool
+     */
+    protected function _isFactoryName($factoryName)
+    {
+        return false !== strpos($factoryName, '/') || preg_match('/^[a-z\d]+(_[A-Za-z\d]+)?$/', $factoryName);
+    }
+
+    /**
+     * Transform factory name into a pair of module and name
+     *
+     * @param string $factoryName
+     * @return array
+     */
+    protected function _getModuleName($factoryName)
+    {
+        if (false !== strpos($factoryName, '/')) {
+            list($module, $name) = explode('/', $factoryName);
+        } else {
+            $module = $factoryName;
+            $name = false;
+        }
+        if (false === strpos($module, '_')) {
+            $module = "Mage_{$module}";
+        }
+        return array($module, $name);
     }
 }
