86d0862ab95f7d962ec5efaae7fcb9351f15e3d4
Michael Chiocca <mchiocca@x.com>
2012-08-27 14:14:56 -0700

    Implement MAGETWO-1185: Javascript unit testing DoD compliance - Code review comments from https://fisheye.corp.x.com/cru/CR-286

---
+++ dev/tests/js/jsTestDriverOrder.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Returns an array of Javascript files that should be loaded by JsTestDriver in the order that they
+ * appear in the array when the Javascript unit tests are run.
+ *
+ * @return array
+ */
+return array(
+    '/pub/lib/globalize/globalize.js',
+    '/pub/lib/jquery/jquery.js',
+    '/pub/lib/jquery/ui/jquery-ui.js',
+    '/pub/lib/jquery/head.js',
+    '/pub/lib/mage/mage.js',
+    '/pub/lib/mage/decorate.js',
+    '/pub/lib/jquery/jquery.validate.js',
+    '/pub/lib/jquery/jquery.metadata.js',
+    '/pub/lib/jquery/additional-methods.js'
+);
