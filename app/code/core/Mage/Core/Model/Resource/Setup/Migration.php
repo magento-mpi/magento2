ae050fbb728114d2c2c854084f9c85cd2da4fcfc
Ivan Klymenko <iklymenko@ebay.com>
2012-08-28 19:15:11 +0300

    Implemented MAGETWO-3028: Create setup class with main functionality to replace aliases - added phpDocs

--- app/code/core/Mage/Core/Model/Resource/Setup/Migration.php
+++ app/code/core/Mage/Core/Model/Resource/Setup/Migration.php
@@ -13,27 +13,90 @@
  */
 class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_Setup
 {
+    /**#@+
+     * Type of field content where class alias is used
+     */
     const FIELD_CONTENT_TYPE_PLAIN       = 'plain';
     const FIELD_CONTENT_TYPE_XML         = 'xml';
     const FIELD_CONTENT_TYPE_WIKI        = 'wiki';
     const FIELD_CONTENT_TYPE_SERIALIZED  = 'serialized';
+    /**#@-*/
 
+    /**#@+
+     *  Entity type of alias
+     */
     const ENTITY_TYPE_MODEL    = 'Model';
     const ENTITY_TYPE_BLOCK    = 'Block';
     const ENTITY_TYPE_RESOURCE = 'Model_Resource';
+    /**#@-*/
 
+    /**
+     * Config key for path to aliases map file
+     */
     const CONFIG_KEY_PATH_TO_MAP_FILE = 'global/migration/path_to_aliases_map_file';
 
+    /**
+     * List of possible entity types sorted by possibility of usage
+     *
+     * @var array
+     */
     protected $_entityTypes = array(self::ENTITY_TYPE_MODEL, self::ENTITY_TYPE_BLOCK, self::ENTITY_TYPE_RESOURCE);
 
+    /**
+     * Rows per page. To split processing data from tables
+     *
+     * @var int
+     */
     protected $_rowsPerPage = 1;
 
+    /**
+     * Replace rules for tables
+     *
+     * [table name] => array(
+     *     [field name] => array(
+     *         'entity_type'      => [entity type]
+     *         'content_type'     => [content type]
+     *         'additional_where' => [additional where]
+     *     )
+     * )
+     *
+     * @var array
+     */
     protected $_replaceRules = array();
 
+    /**
+     * Replacements cache
+     *
+     * [table name] => array(
+     *     [field name] => array(
+     *         [replace from] => [replace to]
+     *     )
+     * )
+     *
+     * @var array
+     */
     protected $_replacements = array();
 
+    /**
+     * Aliases to classes map
+     *
+     * [entity type] => array(
+     *     [alias] => [class name]
+     * )
+     *
+     * @var array
+     */
     protected $_aliasesMap;
 
+    /**
+     * Add alias replace rule
+     *
+     * @param string $tableName name of table to replace aliases in
+     * @param string $fieldName name of table column to replace aliases in
+     * @param string $entityType entity type of alias
+     * @param string $fieldContentType type of field content where class alias is used
+     * @param string $additionalWhere additional where condition
+     */
     public function appendClassAliasReplace($tableName, $fieldName, $entityType = '',
         $fieldContentType = self::FIELD_CONTENT_TYPE_PLAIN, $additionalWhere = ''
     ) {
@@ -50,6 +113,9 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
         }
     }
 
+    /**
+     * start process of replacing aliases with class names using rules
+     */
     public function doUpdateClassAliases()
     {
         foreach ($this->_replaceRules as $tableName => $tableRules) {
@@ -57,7 +123,12 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
         }
     }
 
-    //$tableRules == array([field name] => array('entity type' => [entity type], 'content_type' => [content type]))
+    /**
+     * Update class aliases in table
+     *
+     * @param string $tableName name of table to replace aliases in
+     * @param array $tableRules replacing rules for table
+     */
     protected function _updateClassAliasesInTable($tableName, array $tableRules)
     {
         foreach ($tableRules as $fieldName => $fieldRule) {
@@ -75,6 +146,15 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
         }
     }
 
+    /**
+     * Get amount of rows for table column which should be processed
+     *
+     * @param string $tableName name of table to replace aliases in
+     * @param string $fieldName name of table column to replace aliases in
+     * @param string $additionalWhere additional where condition
+     *
+     * @return int
+     */
     protected function _getRowsCount($tableName, $fieldName, $additionalWhere = '')
     {
         $adapter = $this->getConnection();
@@ -87,10 +167,18 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
             $query->where($additionalWhere);
         }
 
-        return $adapter->fetchOne($query);
+        return (int) $adapter->fetchOne($query);
     }
 
-    protected function _applyFieldRule($tableName, $fieldName, array $fieldRule, $currPage)
+    /**
+     * Replace aliases with class names in rows
+     *
+     * @param string $tableName name of table to replace aliases in
+     * @param string $fieldName name of table column to replace aliases in
+     * @param array $fieldRule
+     * @param int $currPage
+     */
+    protected function _applyFieldRule($tableName, $fieldName, array $fieldRule, $currPage = 0)
     {
         $tableData = $this->_getTableData($tableName, $fieldName, $fieldRule['additional_where'], $currPage);
 
@@ -104,16 +192,12 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
                 if (!isset($fieldReplacements[$rowData[$fieldName]])
                     && !isset($this->_replacements[$tableName][$fieldName][$rowData[$fieldName]])
                 ) {
-                    $replacement =
+                    $fieldReplacements[$rowData[$fieldName]] =
                         $this->_getReplacement(
                             $rowData[$fieldName],
                             $fieldRule['content_type'],
                             $fieldRule['entity_type']
                         );
-
-                    if ($replacement) {
-                        $fieldReplacements[$rowData[$fieldName]] = $replacement;
-                    }
                 }
             }
         }
@@ -124,19 +208,38 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
             array_merge($this->_replacements[$tableName][$fieldName], $fieldReplacements);
     }
 
+    /**
+     * Update rows data in database
+     *
+     * @param string $tableName
+     * @param string $fieldName
+     * @param array $fieldReplacements
+     */
     protected function _updateRowsData($tableName, $fieldName, array $fieldReplacements)
     {
         $adapter = $this->getConnection();
 
         foreach ($fieldReplacements as $from => $to) {
-            $adapter->update(
-                $adapter->getTableName($tableName),
-                array($fieldName => $to),
-                array($adapter->quoteIdentifier($fieldName) . ' = ?' => $from)
-            );
+            if ($to && $from != $to) {
+                $adapter->update(
+                    $adapter->getTableName($tableName),
+                    array($fieldName => $to),
+                    array($adapter->quoteIdentifier($fieldName) . ' = ?' => $from)
+                );
+            }
         }
     }
 
+    /**
+     * Get data for table column which should be processed
+     *
+     * @param string $tableName name of table to replace aliases in
+     * @param string $fieldName name of table column to replace aliases in
+     * @param string $additionalWhere additional where condition
+     * @param int $currPage
+     *
+     * @return array
+     */
     protected function _getTableData($tableName, $fieldName, $additionalWhere = '', $currPage = 0)
     {
         $adapter = $this->getConnection();
@@ -156,6 +259,15 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
         return $adapter->fetchAll($query);
     }
 
+    /**
+     * Get data with replaced aliases with class names
+     *
+     * @param string $data
+     * @param string $contentType type of data (field content)
+     * @param string $entityType entity type of alias
+     *
+     * @return string
+     */
     protected function _getReplacement($data, $contentType, $entityType = '')
     {
         if ($contentType == self::FIELD_CONTENT_TYPE_PLAIN) {
@@ -167,6 +279,14 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
         return $data;
     }
 
+    /**
+     * Get appropriate class name for alias
+     *
+     * @param string $alias
+     * @param string $entityType entity type of alias
+     *
+     * @return string
+     */
     protected function _getCorrespondingClassName($alias, $entityType = '')
     {
         if ($this->_isFactoryName($alias)) {
@@ -195,12 +315,13 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
     }
 
     /**
-     * Build class name supported in magento 2
+     * Generate class name
      *
      * @param string $module
      * @param string $type
      * @param string $name
-     * @return string|bool
+     *
+     * @return string
      */
     protected function _getClassName($module, $type, $name = null)
     {
@@ -217,6 +338,7 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
      * Whether the given class name is a factory name
      *
      * @param string $factoryName
+     *
      * @return bool
      */
     protected function _isFactoryName($factoryName)
@@ -228,6 +350,7 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
      * Transform factory name into a pair of module and name
      *
      * @param string $factoryName
+     *
      * @return array
      */
     protected function _getModuleName($factoryName)
@@ -244,6 +367,14 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
         return array($module, $name);
     }
 
+    /**
+     * Search class by alias in map
+     *
+     * @param string $alias
+     * @param string $entityType
+     *
+     * @return string
+     */
     protected function _getAliasFromMap($alias, $entityType = '')
     {
         if ($map = $this->_getAliasesMap()) {
@@ -267,6 +398,11 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
         return '';
     }
 
+    /**
+     * Retrieve aliases to classes map if exit
+     *
+     * @return array
+     */
     protected function _getAliasesMap()
     {
         if (is_null($this->_aliasesMap)) {
@@ -282,6 +418,13 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
         return $this->_aliasesMap;
     }
 
+    /**
+     * Load aliases to classes map from file
+     *
+     * @param string $pathToMapFile
+     *
+     * @return string
+     */
     protected function _loadMap($pathToMapFile)
     {
         $pathToMapFile = Mage::getBaseDir() . DS . $pathToMapFile;
@@ -292,6 +435,11 @@ class Mage_Core_Model_Resource_Setup_Migration extends Mage_Core_Model_Resource_
         return '';
     }
 
+    /**
+     * Get path to map file from config
+     *
+     * @return Mage_Core_Model_Config_Element
+     */
     protected function _getPathToMapFile()
     {
         return Mage::getConfig()->getNode(self::CONFIG_KEY_PATH_TO_MAP_FILE);
