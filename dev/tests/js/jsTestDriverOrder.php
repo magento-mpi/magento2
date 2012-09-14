a18159fb2ca5235152283ff571ab401e2a3e158b
Michael Chiocca <mchiocca@x.com>
2012-09-13 18:11:27 -0700

    Fix MAGETWO-1185: Javascript Refactor - Perform rebase of master into MAGETWO_JS branch

--- dev/tests/js/jsTestDriverOrder.php
+++ dev/tests/js/jsTestDriverOrder.php
@@ -19,9 +19,12 @@ return array(
     '/pub/lib/jquery/head.js',
     '/pub/lib/mage/mage.js',
     '/pub/lib/mage/decorate.js',
+    '/pub/lib/mage/translate.js',
     '/pub/lib/jquery/jquery.validate.js',
     '/pub/lib/jquery/jquery.metadata.js',
     '/pub/lib/jquery/additional-methods.js',
     '/pub/lib/jquery/jquery.cookie.js',
-    '/pub/lib/jquery/jquery.hook.js'
+    '/pub/lib/jquery/jquery.hook.js',
+    '/pub/lib/jquery/jstree/jquery.jstree.js',
+    '/pub/lib/jquery/slimScroll/slimScroll.min.js'
 );
