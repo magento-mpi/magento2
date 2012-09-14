dcda5d56810a111e789a7cee59c0c4a3020abc2b
Maksym Moskvychev <mmoskvychev@ebay.com>
2012-09-14 18:28:59 +0300

    Implemented MAGETWO-3322:  Implement Library ACL - created DB tables

---
+++ app/code/core/Mage/Webapi/sql/webapi_setup/install-1.0.0.0.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Webapi
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+$installer = $this;
+/* @var $installer Mage_Core_Model_Resource_Setup */
+
+$installer->startSetup();
+
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('webapi_role'))
+    ->addColumn(
+        'role_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null,
+        array(
+            'identity'  => true,
+            'unsigned'  => true,
+            'nullable'  => false,
+            'primary'   => true,),
+        'Webapi role ID')
+    ->addColumn(
+        'role_name', Varien_Db_Ddl_Table::TYPE_TEXT, 255,
+        array(
+            'nullable'  => false,),
+        'Role name is displayed in Adminhtml interface')
+    ->setComment('Roles of unified webapi ACL');
+$installer->getConnection()->createTable($table);
+
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('webapi_user'))
+    ->addColumn(
+        'user_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null,
+        array(
+            'identity'  => true,
+            'unsigned'  => true,
+            'nullable'  => false,
+            'primary'   => true,),
+        'Webapi user ID')
+    ->addColumn(
+        'user_name', Varien_Db_Ddl_Table::TYPE_TEXT, 255,
+        array(
+            'nullable'  => false,),
+        'User name is displayed in Adminhtml interface')
+    ->addColumn('role_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null,
+        array(
+            'default'   => '0',
+            'unsigned'  => true,
+            'nullable'  => false,),
+        'User role from webapi_role')
+    ->addIndex(
+        $installer->getIdxName('webapi_user', array('role_id'), Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX),
+        array('role_id'),
+        array('type' => Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX))
+    ->addForeignKey(
+        $installer->getFkName('webapi_user', 'role_id', 'webapi_role', 'role_id'),
+        'role_id',
+        $installer->getTable('webapi_role'),
+        'role_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE)
+    ->setComment('Users of unified webapi');
+$installer->getConnection()->createTable($table);
+
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('webapi_rule'))
+    ->addColumn(
+        'rule_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null,
+        array(
+            'identity'  => true,
+            'unsigned'  => true,
+            'nullable'  => false,
+            'primary'   => true,),
+        'Rule ID')
+    ->addColumn(
+        'resource_id', Varien_Db_Ddl_Table::TYPE_TEXT, 255,
+        array(
+            'nullable'  => false,),
+        'Resource name. Must match resource calls in xml.')
+    ->addColumn('role_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null,
+        array(
+            'default'   => '0',
+            'unsigned'  => true,
+            'nullable'  => false,),
+        'User role from webapi_role')
+    ->addIndex(
+        $installer->getIdxName('webapi_rule', array('role_id'), Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX),
+        array('role_id'),
+        array('type' => Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX))
+    ->addForeignKey(
+        $installer->getFkName('webapi_rule', 'role_id', 'webapi_role', 'role_id'),
+        'role_id',
+        $installer->getTable('webapi_role'),
+        'role_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE)
+    ->setComment('Permissions of roles to resources');
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
