f747dfbe781b28237cd12ddcb4f90be39de4368e
Maksym Moskvychev <mmoskvychev@ebay.com>
2012-09-19 14:13:42 +0300

    Implemented MAGETWO-3322:  Implement Library ACL

--- dev/tests/unit/testsuite/Mage/Webapi/Model/Authorization/RolelocatorTest.php
+++ dev/tests/unit/testsuite/Mage/Webapi/Model/Authorization/RolelocatorTest.php
@@ -9,29 +9,54 @@
  * @license     {license_link}
  */
 
+/**
+ * Test class for Mage_Webapi_Model_Authorization_RoleLocator
+ */
 class Mage_Webapi_Model_Authorization_RoleLocatorTest extends PHPUnit_Framework_TestCase
 {
 
+    /** @var Mage_Core_Model_Session */
+    protected $_sessionMock;
+
+    /** @var Mage_Backend_Model_Authorization_RoleLocator */
+    protected $_model;
+
+    public function setUp()
+    {
+        $this->_sessionMock = $this->getMock('Mage_Core_Model_Session',
+            array('getData', 'hasData'), array(), '', false);
+        $this->_model = new Mage_Webapi_Model_Authorization_RoleLocator(array('session' => $this->_sessionMock));
+    }
+
     /**
      * Test for Mage_Backend_Model_Authorization_RoleLocator::getAclRoleId()
-     *
      */
-    public function testGetAclRoleId()
+    public function testGetAclRoleIdReturnNullOnWrongSession()
     {
-        /** @var $sessionMock Mage_Core_Model_Session */
-        $sessionMock = $this->getMock('Mage_Core_Model_Session',
-            array('getData', 'hasData'), array(), '', false);
-        $model = new Mage_Backend_Model_Authorization_RoleLocator(array('session' => $sessionMock));
-        $sessionMock->expects($this->any())
-            ->method('hasData')
-            ->with('webapi_user')
-            ->will($this->returnValue(true));
-        $sessionMock->expects($this->any())
-            ->method('getData')
-            ->with('webapi_user')
-            ->will($this->returnValue(new Varien_Object(array(
-                'role_id' => 225
-            ))));
-        $this->assertEquals(255, $model->getAclRoleId());
+        $this->_sessionMock->expects($this->once())->method('hasData')->will($this->returnValue(false));
+        $this->assertNull($this->_model->getAclRoleId());
+    }
+
+    /**
+     * Test for Mage_Backend_Model_Authorization_RoleLocator::getAclRoleId()
+     */
+    public function testGetAclRoleIdReturnNullOnBrokenSession()
+    {
+        $this->_sessionMock->expects($this->once())->method('hasData')->will($this->returnValue(true));
+        $this->_sessionMock->expects($this->once())->method('getData')->will($this->returnValue(12));
+        $this->assertNull($this->_model->getAclRoleId());
+    }
+
+    /**
+     * Test for Mage_Backend_Model_Authorization_RoleLocator::getAclRoleId()
+     */
+    public function testGetAclRoleIdReturnRoleIdOnRightSession()
+    {
+        $user = new Varien_Object(array(
+            'role_id' => 255
+        ));
+        $this->_sessionMock->expects($this->once())->method('hasData')->will($this->returnValue(true));
+        $this->_sessionMock->expects($this->once())->method('getData')->will($this->returnValue($user));
+        $this->assertEquals(255, $this->_model->getAclRoleId());
     }
 }
