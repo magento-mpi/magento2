3c1ab416249bf30ce8a47f3d3ffbec82b9cce91a
Maxim Shikula <mshikula@ebay.com>
2012-09-27 18:26:34 +0300

    Implemented MAGETWO-3494: Theme registration (cherry picked from commit 6e85f74)

---
+++ app/code/core/Mage/Core/data/core_setup/data-install-1.6.0.0.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Cms
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+Mage::app()->addEventArea('install');
+Mage::dispatchEvent('theme_registration');
