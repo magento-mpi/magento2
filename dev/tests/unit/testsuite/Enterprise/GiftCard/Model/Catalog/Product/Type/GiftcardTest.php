cf39ab71e0eee06cb96a2add0b83fa7af7238c2e
Igor Tripolskiy <itripolskiy@ebay.com>
2012-10-01 18:51:52 +0200

    Re-factored MAGETWO-3004: Refactor Enterprise/GiftCard/Model/Catalog/Product/Type/Giftcard.php::_validate() method - Tests

--- dev/tests/unit/testsuite/Enterprise/GiftCard/Model/Catalog/Product/Type/GiftcardTest.php
+++ dev/tests/unit/testsuite/Enterprise/GiftCard/Model/Catalog/Product/Type/GiftcardTest.php
@@ -16,61 +16,278 @@ class Enterprise_GiftCard_Model_Catalog_Product_Type_GiftcardTest extends PHPUni
      */
     protected $_model;
 
+    /**
+     * @var array
+     */
+    protected $_customOptions;
+
+    /**
+     * @var Mage_Catalog_Model_Resource_Product
+     */
+    protected $_productResource;
+
+    /**
+     * @var Mage_Catalog_Model_Resource_Product_Option
+     */
+    protected $_productOptionResource;
+
+    /**
+     * @var Mage_Catalog_Model_Product
+     */
+    protected $_product;
+
+    /**
+     * @var Mage_Sales_Model_Quote_Item_Option
+     */
+    protected $_quoteItemOption;
+
     protected function setUp()
     {
-        $store = $this->getMock('Mage_Core_Model_Store', array(), array(), '', false);
+        $store = $this->getMock('Mage_Core_Model_Store', array('getCurrentCurrencyRate'), array(), '', false);
+        $store->expects($this->once())->method('getCurrentCurrencyRate')->will($this->returnValue(1));
 
         $helpers = array(
-            'Enterprise_GiftCard_Helper_Data' => $this->getMock('Enterprise_GiftCard_Helper_Data'),
-            'Mage_Core_Helper_Data' => $this->getMock('Mage_Core_Helper_Data'),
-            'Mage_Catalog_Helper_Data' => $this->getMock('Mage_Catalog_Helper_Data'),
+            'Enterprise_GiftCard_Helper_Data'        => $this->getMock('Enterprise_GiftCard_Helper_Data'),
+            'Mage_Core_Helper_Data'                  => $this->getMock('Mage_Core_Helper_Data'),
+            'Mage_Catalog_Helper_Data'               => $this->getMock('Mage_Catalog_Helper_Data'),
             'Mage_Core_Helper_File_Storage_Database' => $this->getMock('Mage_Core_Helper_File_Storage_Database')
         );
 
         foreach ($helpers as $helper) {
-            $helper->expects($this->any())
-                ->method('__')
-                ->will($this->returnArgument(0));
+            $helper->expects($this->any())->method('__')->will($this->returnArgument(0));
         }
 
-        $this->_model = new Enterprise_GiftCard_Model_Catalog_Product_Type_Giftcard(array(
-            'store' => $store,
-            'helpers' => $helpers
-        ));
-    }
+        $locale = $this->getMock('Varien_Object', array('getNumber'));
+        $locale->expects($this->any())->method('getNumber')->will($this->returnValue(100));
 
-    public function testCheckProductBuyState()
-    {
-        $productResource = $this->getMock('Mage_Catalog_Model_Resource_Product', array(), array(), '', false);
-        $productOptionResource = $this->getMock('Mage_Catalog_Model_Resource_Product_Option', array(), array(), '', false);
-        $product = new Mage_Catalog_Model_Product(array('resource' => $productResource));
-        $product->setSkipCheckRequiredOption(false);
+        $this->_model = $this->getMock(
+            'Enterprise_GiftCard_Model_Catalog_Product_Type_Giftcard',
+            array('_isStrictProcessMode'),
+            array(array(
+                'store'     => $store,
+                'helpers'   => $helpers,
+                'locale'    => $locale,
+        )));
 
-        $customOptions = array();
+        $this->_productResource = $this->getMock('Mage_Catalog_Model_Resource_Product', array(), array(), '', false);
+        $this->_productOptionResource = $this->getMock('Mage_Catalog_Model_Resource_Product_Option', array(), array(),
+            '', false);
+        $this->_product = $this->getMock('Mage_Catalog_Model_Product',
+            array('getGiftcardAmounts', 'getAllowOpenAmount', 'getOpenAmountMax', 'getOpenAmountMin'),
+            array(array('resource' => $this->_productResource)));
+
+        $this->_customOptions = array();
 
         for ($i = 1; $i <= 3; $i++) {
-            $option = new Mage_Catalog_Model_Product_Option(array('resource' => $productOptionResource));
+            $option = new Mage_Catalog_Model_Product_Option(array('resource' => $this->_productOptionResource));
             $option->setIdFieldName('id');
             $option->setId($i);
             $option->setIsRequire(true);
-            $customOptions[Mage_Catalog_Model_Product_Type_Abstract::OPTION_PREFIX . $i] = new Varien_Object(array('value'=>'lolo'));
-            $product->addOption($option);
+            $this->_customOptions[Mage_Catalog_Model_Product_Type_Abstract::OPTION_PREFIX . $i] = new Varien_Object(
+                array('value' => 'value'));
+            $this->_product->addOption($option);
         }
 
-        $quoteItemOption = $this->getMock('Mage_Sales_Model_Quote_Item_Option', array(), array(), '', false);
+        $this->_quoteItemOption = $this->getMock('Mage_Sales_Model_Quote_Item_Option', array(), array(), '', false);
 
-        $quoteItemOption->expects($this->any())
-            ->method('getValue')
-            ->will($this->returnValue(serialize(array('lolo'=>'qwerty'))));
+        $this->_customOptions['info_buyRequest'] = $this->_quoteItemOption;
 
-        $customOptions['info_buyRequest'] = $quoteItemOption;//(array('value' => serialize(array('lolo'=>'qwerty'))));
+        $this->_product->expects($this->any())->method('getAllowOpenAmount')->will($this->returnValue(true));
 
-        $product->setCustomOptions($customOptions);
-        $this->_model->checkProductBuyState($product);
-
-        $this->setExpectedException(
-            'Mage_Core_Exception'//, 'The product has required options'
-        );
+        $this->_product->setSkipCheckRequiredOption(false);
+        $this->_product->setCustomOptions($this->_customOptions);
     }
 
+    public function testValidateEmptyFields()
+    {
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array())));
+        $this->_setGetGiftcardAmountsReturnEmpty();
+
+        $this->_setStrictProcessMode(true);
+        $this->setExpectedException('Mage_Core_Exception', 'Please specify all the required information.');
+        $this->_model->checkProductBuyState($this->_product);
+    }
+
+    public function testValidateEmptyAmount()
+    {
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array(
+                'giftcard_recipient_name'   => 'name',
+                'giftcard_sender_name'      => 'name',
+                'giftcard_recipient_email'  => 'email',
+                'giftcard_sender_email'     => 'email',
+            ))));
+
+        $this->_setGetGiftcardAmountsReturnEmpty();
+        $this->_setStrictProcessMode(true);
+        $this->_runValidationWithExpectedException('Please specify Gift Card amount.');
+    }
+
+    public function testValidateMaxAmount()
+    {
+        $this->_product->expects($this->once())->method('getOpenAmountMax')->will($this->returnValue(10));
+        $this->_product->expects($this->once())->method('getOpenAmountMin')->will($this->returnValue(3));
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array(
+                'giftcard_recipient_name'   => 'name',
+                'giftcard_sender_name'      => 'name',
+                'giftcard_recipient_email'  => 'email',
+                'giftcard_sender_email'     => 'email',
+                'custom_giftcard_amount'    => 15,
+            ))));
+
+        $this->_setGetGiftcardAmountsReturnEmpty();
+        $this->_setStrictProcessMode(true);
+        $this->_runValidationWithExpectedException('Gift Card max amount is %s');
+    }
+
+    public function testValidateMinAmount()
+    {
+        $this->_product->expects($this->once())->method('getOpenAmountMax')->will($this->returnValue(10));
+        $this->_product->expects($this->once())->method('getOpenAmountMin')->will($this->returnValue(3));
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array(
+                'giftcard_recipient_name'   => 'name',
+                'giftcard_sender_name'      => 'name',
+                'giftcard_recipient_email'  => 'email',
+                'giftcard_sender_email'     => 'email',
+                'custom_giftcard_amount'    => 2,
+            ))));
+
+        $this->_setGetGiftcardAmountsReturnEmpty();
+        $this->_setStrictProcessMode(true);
+        $this->_runValidationWithExpectedException('Gift Card min amount is %s');
+    }
+
+    public function testValidateNoAllowedAmount()
+    {
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array(
+                'giftcard_recipient_name'   => 'name',
+                'giftcard_sender_name'      => 'name',
+                'giftcard_recipient_email'  => 'email',
+                'giftcard_sender_email'     => 'email',
+                'giftcard_amount'           => 7,
+            ))));
+
+        $this->_setGetGiftcardAmountsReturnEmpty();
+        $this->_setStrictProcessMode(true);
+        $this->_runValidationWithExpectedException('Please specify Gift Card amount.');
+    }
+
+    public function testValidateRecipientName()
+    {
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array(
+                'giftcard_sender_name'      => 'name',
+                'giftcard_recipient_email'  => 'email',
+                'giftcard_sender_email'     => 'email',
+                'giftcard_amount'           => 5,
+            ))));
+
+        $this->_setGetGiftcardAmountsReturnArray();
+        $this->_setStrictProcessMode(true);
+        $this->_runValidationWithExpectedException('Please specify recipient name.');
+    }
+
+    public function testValidateSenderName()
+    {
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array(
+                'giftcard_recipient_name'   => 'name',
+                'giftcard_recipient_email'  => 'email',
+                'giftcard_sender_email'     => 'email',
+                'giftcard_amount'           => 5,
+            ))));
+
+        $this->_setGetGiftcardAmountsReturnArray();
+        $this->_setStrictProcessMode(true);
+        $this->_runValidationWithExpectedException('Please specify sender name.');
+    }
+
+    public function testValidateRecipientEmail()
+    {
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array(
+                'giftcard_recipient_name'   => 'name',
+                'giftcard_sender_name'      => 'name',
+                'giftcard_sender_email'     => 'email',
+                'giftcard_amount'           => 5,
+            ))));
+
+        $this->_setGetGiftcardAmountsReturnArray();
+        $this->_setStrictProcessMode(true);
+        $this->_runValidationWithExpectedException('Please specify recipient email.');
+    }
+
+    public function testValidateSenderEmail()
+    {
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array(
+                'giftcard_recipient_name'   => 'name',
+                'giftcard_sender_name'      => 'name',
+                'giftcard_recipient_email'  => 'email',
+                'giftcard_amount'           => 5,
+            ))));
+
+        $this->_setGetGiftcardAmountsReturnArray();
+        $this->_setStrictProcessMode(true);
+        $this->_runValidationWithExpectedException('Please specify sender email.');
+    }
+
+    public function testValidate()
+    {
+        $this->_quoteItemOption->expects($this->any())->method('getValue')
+            ->will($this->returnValue(serialize(array())));
+        $this->_setGetGiftcardAmountsReturnEmpty();
+        $this->_customOptions['info_buyRequest'] = $this->_quoteItemOption;
+        $this->_product->setCustomOptions($this->_customOptions);
+
+        $this->_setStrictProcessMode(false);
+        $this->_model->checkProductBuyState($this->_product);
+    }
+
+    /**
+     * Running validation with specified exception message
+     *
+     * @param string $exceptionMessage
+     */
+    protected function _runValidationWithExpectedException($exceptionMessage)
+    {
+        $this->_customOptions['info_buyRequest'] = $this->_quoteItemOption;
+
+        $this->_product->setCustomOptions($this->_customOptions);
+
+        $this->setExpectedException('Mage_Core_Exception', $exceptionMessage);
+        $this->_model->checkProductBuyState($this->_product);
+    }
+
+    /**
+     * Set getGiftcardAmount return value to empty array
+     */
+    protected function _setGetGiftcardAmountsReturnEmpty()
+    {
+        $this->_product->expects($this->once())->method('getGiftcardAmounts')
+            ->will($this->returnValue(array()));
+    }
+
+    /**
+     * Set getGiftcardAmount return value
+     */
+    protected function _setGetGiftcardAmountsReturnArray()
+    {
+        $this->_product->expects($this->once())->method('getGiftcardAmounts')
+            ->will($this->returnValue(array(array('website_value' => 5))));
+    }
+
+    /**
+     * Set strict mode
+     *
+     * @param bool $mode
+     */
+    protected function _setStrictProcessMode($mode)
+    {
+        $this->_model->expects($this->once())->method('_isStrictProcessMode')->will($this->returnValue((bool)$mode));
+    }
 }
