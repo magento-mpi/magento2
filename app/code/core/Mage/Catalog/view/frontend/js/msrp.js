9bf1fab7da5702b6b2bc1139621e83f09c001976
Vijay Sriram <vsriram@x.com>
2012-10-02 16:30:59 -0700

    Fix MAGETWO-1185: javascript refactor -Simple Product View Add to Cart and Pricing, added missing js file

---
+++ app/code/core/Mage/Catalog/view/frontend/js/msrp.js
@@ -0,0 +1,88 @@
+/**
+ * {license_notice}
+ *
+ * @category    design
+ * @package     base_default
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/*jshint eqnull:true browser:true jquery:true*/
+/*global head:true */
+(function ($) {
+
+    var clickForPrice = {
+        helpLink: []
+    };
+    var popupCloseData = {
+        Id: ''
+    };
+
+    var helpLinkData = {
+        helpText: []
+    };
+
+    var popupCartData = {
+        Id: ''
+    };
+
+    var cartData = {
+        Id: ''
+    };
+
+    $(document).ready(function () {
+        $.mage.event.trigger("mage.price.helplink", clickForPrice);
+        $.mage.event.trigger("map.popup.close", popupCloseData);
+        $.mage.event.trigger("mage.popup.whatsthislink", helpLinkData);
+        $.mage.event.trigger("mage.popup.whatsthislink", helpLinkData);
+        $.mage.event.trigger("map.popup.button", popupCartData);
+        $.mage.event.trigger("product.addtocart.button", cartData);
+
+        $.each(clickForPrice.helpLink, function (index, value) {
+            $(value.Id).on('click', function (e) {
+                $('#map-popup-heading').text(value.productName);
+                $('#map-popup-price').html($(value.realPrice));
+                $('#map-popup-msrp').html(value.msrpPrice);
+
+                var height = $('#map-popup').height();
+                var width = $('#map-popup').width();
+                leftVal = e.pageX - (width / 2) + "px";
+                $('#map-popup').css({left: leftVal, top: e.pageY}).show();
+                $('#map-popup-content').show();
+                $('#map-popup-text').addClass('map-popup-only-text').show();
+                $('#map-popup-text-what-this').hide();
+                return false;
+            });
+        });
+
+        $.each(helpLinkData.helpText, function (index, value) {
+            $(value.Id).on('click', function (e) {
+                var height = $('#map-popup').height();
+                var width = $('#map-popup').width();
+                leftVal = e.pageX - (width / 2) + "px";
+                $('#map-popup').css({left: leftVal, top: e.pageY}).show();
+                $('#map-popup-content').hide();
+                $('#map-popup-text').hide();
+                $('#map-popup-text-what-this').show();
+                return false;
+            });
+        });
+
+        $(popupCloseData.Id).on('click', function () {
+            $('#map-popup').hide();
+            return false;
+        });
+
+        $(popupCartData.Id).on('click', function () {
+            $("#product_addtocart_form").submit();
+        });
+
+        $(cartData.Id).on('click', function () {
+
+            $("#product_addtocart_form").submit();
+        });
+
+    });
+
+})(jQuery);
+
