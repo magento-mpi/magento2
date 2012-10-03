3f0066849bbfa6828dcf7ef1bbb5c789e55a71f5
Michael Chiocca <mchiocca@x.com>
2012-10-02 17:50:44 -0700

    Fix MAGETWO-1994: Convert simple product view page to jQuery - Minor changes and improvements to jquery.popupwindow.js library - Remove popupWindow related code from compare.js - Implement popup-window.js to process the new data-mage-popwin attribute - Replace button onclick with data-mage-popwin attribute in product comparison - Replace link onclick with data-mage-popwin attribute in thumbnail image view

--- app/code/core/Mage/Catalog/view/frontend/js/compare.js
+++ app/code/core/Mage/Catalog/view/frontend/js/compare.js
@@ -11,31 +11,16 @@
 (function ($) {
     $(document).ready(function () {
         var _compare = {
-            // Filled in initialization event
             listSelector: null,
             removeConfirmMessage: null,
             removeSelector: null,
             clearAllConfirmMessage: null,
-            clearAllSelector: null,
-            popUpWindowSelector: null,
-            windowName: null, /* Name of window set from the name attribute of the element that invokes the click */
-            windowURL: null, /* Url used for the popup */
-            // Default values
-            centerBrowser: 0, /* Center window over browser window? {1 (YES) or 0 (NO)}. overrides top and left. */
-            centerScreen: 0, /* Center window over entire screen? {1 (YES) or 0 (NO)}. overrides top and left. */
-            height: 600, /* Sets the height in pixels of the window. */
-            left: 0, /* Left position when the window appears. */
-            location: 0, /* Determines whether the address bar is displayed {1 (YES) or 0 (NO)}. */
-            menubar: 0, /* Determines whether the menu bar is displayed {1 (YES) or 0 (NO)}. */
-            resizable: 1, /* Whether the window can be resized {1 (YES) or 0 (NO)}. Can also be overloaded using resizable.*/
-            scrollbars: 1, /* Determines whether scrollbars appear on the window {1 (YES) or 0 (NO)}. */
-            status: 0, /* Whether a status line appears at the bottom of the window {1 (YES) or 0 (NO)}. */
-            width: 820, /* Sets the width in pixels of the window. */
-            top: 0, /* Top position when the window appears. */
-            toolbar: 0 /* Determines whether a toolbar (includes the forward and back buttons) is displayed {1 (YES) or 0 (NO)}. */
+            clearAllSelector: null
         };
+
         $.mage.event.trigger('mage.compare.initialize', _compare);
         $.mage.decorator.list(_compare.listSelector, true);
+
         function _confirmMessage(selector, message) {
             $(selector).on('click', function () {
                 return confirm(message);
@@ -44,6 +29,5 @@
 
         _confirmMessage(_compare.removeSelector, _compare.removeConfirmMessage);
         _confirmMessage(_compare.clearAllSelector, _compare.clearAllConfirmMessage);
-        $(_compare.popUpWindowSelector).popupWindow(_compare);
     });
 })(jQuery);
