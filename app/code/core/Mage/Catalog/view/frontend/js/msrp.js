ae47cb61e876c599e6114e40b8977d116be13ca1
Vijay Sriram <vsriram@x.com>
2012-10-04 17:16:20 -0700

    Fix MAGETWO-1185: javascript refactor -Simple Product View Add to Cart and Pricing, validation

--- app/code/core/Mage/Catalog/view/frontend/js/msrp.js
+++ app/code/core/Mage/Catalog/view/frontend/js/msrp.js
@@ -1,51 +1,50 @@
 /**
- * {license_notice}
- *
- * @category    design
- * @package     base_default
- * @copyright   {copyright}
- * @license     {license_link}
- */
+* {license_notice}
+*
+* @category    design
+* @package     base_default
+* @copyright   {copyright}
+* @license     {license_link}
+*/
 
 /*jshint browser:true jquery:true*/
 (function ($) {
 
-    var clickForPrice = {
+    var _clickForPrice = {
         helpLink: []
     };
-    var popupCloseData = {
-        Id: ''
+    var _popupCloseData = {
+        closeButtonId: ''
     };
 
-    var helpLinkData = {
+    var _helpLinkData = {
         helpText: []
     };
 
-    var popupCartData = {
-        Id: ''
+    var _popupCartData = {
+        cartData: []
     };
 
-    var cartData = {
-        Id: ''
+    var _cartData = {
+        addToCartData: []
     };
 
     $(document).ready(function () {
-        $.mage.event.trigger("mage.price.helplink", clickForPrice);
-        $.mage.event.trigger("map.popup.close", popupCloseData);
-        $.mage.event.trigger("mage.popup.whatsthislink", helpLinkData);
-        $.mage.event.trigger("mage.popup.whatsthislink", helpLinkData);
-        $.mage.event.trigger("map.popup.button", popupCartData);
-        $.mage.event.trigger("product.addtocart.button", cartData);
+        $.mage.event.trigger("mage.price.helplink", _clickForPrice);
+        $.mage.event.trigger("map.popup.close", _popupCloseData);
+        $.mage.event.trigger("mage.popup.whatsthislink", _helpLinkData);
+        $.mage.event.trigger("map.popup.button", _popupCartData);
+        $.mage.event.trigger("product.addtocart.button", _cartData);
 
-        $.each(clickForPrice.helpLink, function (index, value) {
-            $(value.Id).on('click', function (e) {
+        $.each(_clickForPrice.helpLink, function (index, value) {
+            $(value.popupId).on('click', function (e) {
                 $('#map-popup-heading').text(value.productName);
                 $('#map-popup-price').html($(value.realPrice));
                 $('#map-popup-msrp').html(value.msrpPrice);
 
                 var width = $('#map-popup').width();
-                var leftVal = e.pageX - (width / 2) + "px";
-                $('#map-popup').css({left: leftVal, top: e.pageY}).show();
+                var offsetX = e.pageX - (width / 2) + "px";
+                $('#map-popup').css({left: offsetX, top: e.pageY}).show();
                 $('#map-popup-content').show();
                 $('#map-popup-text').addClass('map-popup-only-text').show();
                 $('#map-popup-text-what-this').hide();
@@ -53,11 +52,11 @@
             });
         });
 
-        $.each(helpLinkData.helpText, function (index, value) {
-            $(value.Id).on('click', function (e) {
+        $.each(_helpLinkData.helpText, function (index, value) {
+            $(value.helpLinkId).on('click', function (e) {
                 var width = $('#map-popup').width();
-                var leftVal = e.pageX - (width / 2) + "px";
-                $('#map-popup').css({left: leftVal, top: e.pageY}).show();
+                var offsetX = e.pageX - (width / 2) + "px";
+                $('#map-popup').css({left: offsetX, top: e.pageY}).show();
                 $('#map-popup-content').hide();
                 $('#map-popup-text').hide();
                 $('#map-popup-text-what-this').show();
@@ -65,18 +64,67 @@
             });
         });
 
-        $(popupCloseData.Id).on('click', function () {
+        $(_popupCloseData.closeButtonId).on('click', function () {
             $('#map-popup').hide();
             return false;
         });
 
-        $(popupCartData.Id).on('click', function () {
-            $("#product_addtocart_form").submit();
+        $.each(_popupCartData.cartData, function (index, value) {
+            $(value.popupButtonId).on('click', function () {
+                $(value.addToCartForm).mage().validate({
+                    errorPlacement: function (error, element) {
+                        if (element.is(':radio') || element.is(':checkbox')) {
+                            element.closest('ul').after(error);
+                        } else {
+                            element.after(error);
+                        }
+                    },
+                    highlight: function (element) {
+                        if ($(element).is(':radio') || $(element).is(':checkbox')) {
+                            $(element).closest('ul').addClass('mage-error');
+                        } else {
+                            return true;
+                        }
+                    },
+                    unhighlight: function (element) {
+                        if ($(element).is(':radio') || $(element).is(':checkbox')) {
+                            $(element).closest('ul').removeClass('mage-error');
+                        } else {
+                            return true;
+                        }
+                    }
+                });
+                $(value.addToCartForm).submit();
+            });
         });
 
-        $(cartData.Id).on('click', function () {
-
-            $("#product_addtocart_form").submit();
+        $.each(_cartData.addToCartData, function (index, value) {
+            $(value.addToCartButtonId).on('click', function () {
+                $(value.addToCartForm).mage().validate({
+                    errorPlacement: function (error, element) {
+                        if (element.is(':radio') || element.is(':checkbox')) {
+                            element.closest('ul').after(error);
+                        } else {
+                            element.after(error);
+                        }
+                    },
+                    highlight: function (element) {
+                        if ($(element).is(':radio') || $(element).is(':checkbox')) {
+                            $(element).closest('ul').addClass('mage-error');
+                        } else {
+                            return true;
+                        }
+                    },
+                    unhighlight: function (element) {
+                        if ($(element).is(':radio') || $(element).is(':checkbox')) {
+                            $(element).closest('ul').removeClass('mage-error');
+                        } else {
+                            return true;
+                        }
+                    }
+                });
+                $(value.addToCartForm).submit();
+            });
         });
 
     });
