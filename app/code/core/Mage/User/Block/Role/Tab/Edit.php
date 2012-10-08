f99ba140be265ffce2dc5f2c7782d1e2ae749d6a
Roman Grebenchuk <rgrebenchuk@ebay.com>
2012-10-08 18:34:46 +0300

    Implemented MAGETWO-3841: fixed Mage::getXxx calls

--- app/code/core/Mage/User/Block/Role/Tab/Edit.php
+++ app/code/core/Mage/User/Block/Role/Tab/Edit.php
@@ -73,9 +73,11 @@ class Mage_User_Block_Role_Tab_Edit extends Mage_Backend_Block_Widget_Form
         $rid = Mage::app()->getRequest()->getParam('rid', false);
 
         $acl = Mage::getSingleton('Mage_Core_Model_Acl_Builder',
-            array(
-                'areaConfig' => Mage::getConfig()->getAreaConfig(),
-                'objectFactory' => Mage::getConfig()
+            array('data' =>
+                array(
+                    'areaConfig' => Mage::getConfig()->getAreaConfig(),
+                    'objectFactory' => Mage::getConfig()
+                )
             )
         )->getAcl();
         $rulesSet = Mage::getResourceModel('Mage_User_Model_Resource_Rules_Collection')->getByRoles($rid)->load();
