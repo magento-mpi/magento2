9c22caa4aff9ba640b5e1a962e68666bf2de1240
Vladimir Korotovskih <vladimir.korotovskih@magento.com>
2012-10-09 17:02:58 +0300

    Fixed MAGETWO-3252: Upgrade CE 1.7(EE 1.12) to M2: Wrong determinated table when use eav for attributes Fixed MAGETWO-3264: Upgrade CE 1.7(EE 1.12) to M2: SQL error Catalog data upgrade Fixed MAGETWO-3265: Upgrade CE 1.7(EE 1.12) to M2: Category Table doesn't exist

--- app/code/core/Mage/Catalog/sql/catalog_setup/upgrade-1.6.0.0.10-1.6.0.0.11.php
+++ app/code/core/Mage/Catalog/sql/catalog_setup/upgrade-1.6.0.0.10-1.6.0.0.11.php
@@ -11,7 +11,5 @@
 /** @var $installer Mage_Catalog_Model_Resource_Setup */
 $installer  = $this;
 
-$installer->installEntities();
-
 $attributeId = $this->getAttribute('catalog_product', 'group_price', 'attribute_id');
 $installer->updateAttribute('catalog_product', $attributeId, array(), null, 5);
