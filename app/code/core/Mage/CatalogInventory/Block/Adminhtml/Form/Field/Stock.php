23d32631ac741e30f85d43eff3865a883eb23361
Igor Tripolskiy <itripolskiy@ebay.com>
2012-10-10 18:37:09 +0200

    Implemented MAGETWO-3781: Custom renderer for quantity_and_stock attribute

---
+++ app/code/core/Mage/CatalogInventory/Block/Adminhtml/Form/Field/Stock.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_CatalogInventory
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+
+/**
+ * HTML select element block
+ *
+ * @category   Mage
+ * @package    Mage_CatalogInventory
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_CatalogInventory_Block_Adminhtml_Form_Field_Stock extends Varien_Data_Form_Element_Select
+{
+    const QUANTITY_FIELD_HTML_ID = 'qty';
+
+    /**
+     * Quantity field element
+     *
+     * @var Mage_Core_Model_Abstract
+     */
+    protected $_qty;
+
+    public function __construct(array $data = array())
+    {
+        $this->_qty = $data['qty'] ? : $this->_createQtyElement();
+        unset($data['qty']);
+        parent::__construct($data);
+        $this->setName($data['name']);
+    }
+
+    /**
+     * Create quantity field
+     *
+     * @return Mage_Core_Model_Abstract
+     */
+    protected function _createQtyElement()
+    {
+        $element = Mage::getModel('Varien_Data_Form_Element_Text');
+        $element->setId(self::QUANTITY_FIELD_HTML_ID)->setName('qty');
+        return $element;
+    }
+
+    /**
+     * Join quantity and in stock elements' html
+     *
+     * @return string
+     */
+    public function getElementHtml()
+    {
+        return $this->_qty->getElementHtml() . parent::getElementHtml()
+            . $this->_getJs(self::QUANTITY_FIELD_HTML_ID, $this->getId());
+    }
+
+    /**
+     * Set form to quantity element in addition to current element
+     *
+     * @param $form
+     * @return Varien_Data_Form_Element_Abstract
+     */
+    public function setForm($form)
+    {
+        $this->_qty->setForm($form);
+        return parent::setForm($form);
+    }
+
+    /**
+     * Set value to quantity element in addition to current element
+     *
+     * @param $value
+     * @return mixed
+     */
+    public function setValue($value)
+    {
+        if (is_array($value)) {
+            $this->_qty->setValue($value['qty']);
+        }
+        return parent::setValue($value['is_in_stock']);
+    }
+
+    /**
+     * Set name to quantity element in addition to current element
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->_qty->setName($name . '[qty]');
+        parent::setName($name . '[is_in_stock]');
+    }
+
+    protected function _getJs($quantityFieldId, $inStockFieldId)
+    {
+        return "
+            <script>
+            Event.observe(window, 'load', function() {
+                (function ($) {
+                    var qty = $('#$quantityFieldId');
+                    var isInStock = $('#$inStockFieldId');
+                    var disabler = function(){
+                        if ('' === qty.val()) {
+                            isInStock.attr('disabled', 'disabled');
+                        } else {
+                            isInStock.removeAttr('disabled');
+                        }
+                    };
+                    disabler();
+                    qty.bind('keyup change blur', disabler)
+                })(jQuery);
+            });
+            </script>
+        ";
+    }
+}
