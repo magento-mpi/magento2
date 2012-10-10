0ae01e03b2898f5b4709e89fd94c550ad5d4be5b
Andrii Kasian <akasian@ebay.com>
2012-10-10 11:47:28 -0400

    Implements MAGETWO-3790: Visual appearance of category assignment control

---
+++ app/code/core/Mage/Adminhtml/view/adminhtml/js/jquery.select2.js
@@ -0,0 +1,72 @@
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     js
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+ (function ($, undefined) {
+    "use strict";
+    $.fn.select2 = function (options) {
+        this.each(function () {
+            var $element = $(
+                    '<div class="select2-container select2-container-multi">' +
+                    '<ul class="select2-choices">' +
+                    '<li class="select2-search-field">' +
+                    '<input type="text" autocomplete="off" class="select2-input">' +
+                    '</li></ul></div>'
+                ),
+                $list = $element.children(),
+                $this = $(this),
+                name = $this.attr('name'),
+                itemRenderer = function(value, text, data) {
+                    $list.prepend(
+                        $('<li class="select2-search-choice button"/>')
+                            .data(data || {})
+                            .append($('<input type="hidden" />').attr('name', name).val(value))
+                            .append($('<div/>').text(text))
+                            .append('<a href="#" onclick="return false;" class="select2-search-choice-close" tabindex="-1"></a>')
+                    );
+                };
+            $this.find('option').each(function(){
+                itemRenderer($(this).val(), $(this).text());
+            });
+            $this.attr('disabled', 'disabled').hide();
+            $this.data('select2-element', $element);
+            $element.insertAfter($this);
+            $list.delegate(".select2-search-choice-close", "click", function() {
+                $(this).parent().remove();
+            });
+            $element.find('.select2-input').autocomplete({
+                source: function(request, response) {
+                    $.ajax({
+                        url: options.url,
+                        dataType: "json",
+                        data: {
+                            name_part: request.term
+                        },
+                        success: function(data) {
+                            response($.map(data, function(item) {
+                                return {
+                                    label: item.text,
+                                    value: item.id,
+                                    item: item
+                                }
+                            }));
+                        }
+                    });
+                },
+                minLength: 3,
+                select: function(event, ui) {
+                    itemRenderer(ui.item.value, ui.item.label, ui.item);
+                    $element.find('.select2-input').val('');
+                    return false;
+                },
+                focus: function() {
+                    return false;
+                }
+            });
+        });
+    };
+})(jQuery);
