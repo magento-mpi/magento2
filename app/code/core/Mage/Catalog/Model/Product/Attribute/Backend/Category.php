46bae1675a6d92e0e2007789b137ca93a2bfd8a2
Max Gubar <mgubar@ebay.com>
2012-10-10 09:39:33 +0300

    Partially Implemented MAGETWO-3788: Backend model for category_ids attribute

--- app/code/core/Mage/Catalog/Model/Product/Attribute/Backend/Category.php
+++ app/code/core/Mage/Catalog/Model/Product/Attribute/Backend/Category.php
@@ -17,74 +17,18 @@
  */
 class Mage_Catalog_Model_Product_Attribute_Backend_Category extends Mage_Eav_Model_Entity_Attribute_Backend_Abstract
 {
-    /**
-     * Save product category relations
-     *
-     * @param Mage_Catalog_Model_Product $object
-     * @return Mage_Catalog_Model_Product_Attribute_Backend_Category|Mage_Eav_Model_Entity_Attribute_Backend_Abstract
-     */
-    public function afterSave($object)
-    {
-        /**
-         * If category ids data is not declared we haven't do manipulations
-         */
-        if (!$object->hasCategoryIds()) {
-            return $this;
-        }
-
-        $resource = $object->getResource();
-
-        $categoryIds = $object->getCategoryIds();
-        $oldCategoryIds = $resource->getCategoryIds($object);
-
-        $object->setIsChangedCategories(false);
-
-        $insert = array_diff($categoryIds, $oldCategoryIds);
-        $delete = array_diff($oldCategoryIds, $categoryIds);
-
-        $write = $resource->getWriteConnection();
-        $tableName = $resource->getTable('catalog_category_product');
-        if (!empty($insert)) {
-            $data = array();
-            foreach ($insert as $categoryId) {
-                if (empty($categoryId)) {
-                    continue;
-                }
-                $data[] = array(
-                    'category_id' => (int)$categoryId,
-                    'product_id'  => (int)$object->getId(),
-                    'position'    => 1
-                );
-            }
-            if ($data) {
-                $write->insertMultiple($tableName, $data);
-            }
-        }
-
-        if (!empty($delete)) {
-            foreach ($delete as $categoryId) {
-                $where = array(
-                    'product_id = ?'  => (int)$object->getId(),
-                    'category_id = ?' => (int)$categoryId,
-                );
-
-                $write->delete($tableName, $where);
-            }
-        }
-
-        if (!empty($insert) || !empty($delete)) {
-            $object->setAffectedCategoryIds(array_merge($insert, $delete));
-            $object->setIsChangedCategories(true);
-        }
-
-        return parent::afterSave($object);
-    }
-
     public function afterLoad($object)
     {
-        $qwerty = $object->getCategoryIds();
+        $object->setData($this->getAttribute()->getAttributeCode(), $object->getCategoryIds());
         return parent::afterLoad($object);
     }
 
-
+    public function beforeSave($object)
+    {
+        $object->setData(
+            $this->getAttribute()->getAttributeCode(),
+            $object->getData($this->getAttribute()->getAttributeCode())
+        );
+        return parent::beforeSave($object);
+    }
 }
