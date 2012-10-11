ea83bcfc06e59fa9719a2252fe215a6d4871a893
Michael Banin <mbanin@ebay.com>
2012-10-11 15:14:16 +0300

    [TEST] - created test TL-MAGE-6366

--- testsuite/Community2/Mage/ApiRoles/CreateTest.php
+++ testsuite/Community2/Mage/ApiRoles/CreateTest.php
@@ -67,39 +67,51 @@ class Community2_Mage_ApiRoles_CreateTest extends Mage_Selenium_TestCase
      */
     public function requiredFields()
     {
+        //Open API Roles Management page
         $this->navigate('api_roles_management');
+        //Click Add New Role button
         $this->clickButton('add_new_role', true);
-        $this->clickButton('save',false);
+        //Click Save API Role button
+        $this->clickButton('save', false);
+        //Verify that validation message appear
         $xpath = $this->_getControlXpath('field', 'role_name');
         $this->addParameter('fieldXpath', $xpath);
         $this->assertMessagePresent('error', 'empty_required_field');
     }
 
     /**
- * <p>Create API Role (Full Access)</p>
- * <p>Steps</p>
- * <p>1. Click Add New Role button</p>
- * <p>2. Fill Role name field</p>
- * <p>3. Click resources tab</p>
- * <p>4. At Resources tab select All at Resource Access</p>
- * <p>5. Click Save API role</p>
- * <p>Expected result:</p>
- * <p>API Role is created</p>
- * <p>Message "The API role has been saved." is displayed</p>
- *
- * @test
- * @author Michael Banin
- * @TestlinkId TL-MAGE-6291
- */
+     * <p>Create API Role (Full Access)</p>
+     * <p>Steps</p>
+     * <p>1. Click Add New Role button</p>
+     * <p>2. Fill Role name field</p>
+     * <p>3. Click resources tab</p>
+     * <p>4. At Resources tab select All at Resource Access</p>
+     * <p>5. Click Save API role</p>
+     * <p>Expected result:</p>
+     * <p>API Role is created</p>
+     * <p>Message "The API role has been saved." is displayed</p>
+     *
+     * @test
+     * @author Michael Banin
+     * @TestlinkId TL-MAGE-6291
+     */
     public function roleWithAllAccess()
     {
+        //Load data
         $fieldData = $this->loadDataSet('ApiRoles', 'api_role_new');
+        //Open API Roles Management page
         $this->navigate('api_roles_management');
+        //Click Add New Role button
         $this->clickButton('add_new_role', true);
+        //Fill Role name field
         $this->fillField('role_name', $fieldData['role_name']);
+        //Open Resources Tab
         $this->openTab('resources');
+        //Selecting All at Role Access Dropdown
         $this->fillDropdown('role_access', 'All');
+        //Saving API Role
         $this->clickButton('save');
+        //Verify that role is saved
         $this->assertMessagePresent('success', 'success_saved_role');
     }
 
@@ -123,14 +135,64 @@ class Community2_Mage_ApiRoles_CreateTest extends Mage_Selenium_TestCase
     public function roleWithCustomAccess()
     {
         $fieldData = $this->loadDataSet('ApiRoles', 'api_role_new');
+        //Open API Roles Management page
         $this->navigate('api_roles_management');
+        //Click Add New Role button
         $this->clickButton('add_new_role', true);
+        //Fill Role name field
         $this->fillField('role_name', $fieldData['role_name']);
+        //Open Resources Tab
         $this->openTab('resources');
+        //Selecting Custom at Role Access Dropdown
         $this->fillDropdown('role_access', 'Custom');
+        //Selecting Get Customer checkbox
         $this->addParameter('subName', 'Get Customer');
         $this->clickControl('link', 'sub_root', false);
+        //Saving API Role
         $this->clickButton('save');
+        //Verify that role is saved
         $this->assertMessagePresent('success', 'success_saved_role');
+    }
+
+    /**
+     * <p>Delete API Role</p>
+     * <p>Steps</p>
+     * <p>1. Open created role</p>
+     * <p>2. Click Delete API Roles button</p>
+     * <p>3. Verify that success deleted message appear</p>
+     * <p>Expected result:</p>
+     * <p>The role has been deleted.</p>
+     * <p>Message "The role has been deleted." is displayed</p>
+     *
+     * @test
+     * @author Michael Banin
+     * @TestlinkId TL-MAGE-6366
+     */
+    public function roleDelete()
+    {
+        //Load data
+        $fieldData = $this->loadDataSet('ApiRoles', 'api_role_new');
+        //Open API Roles Management page
+        $this->navigate('api_roles_management');
+        //Click Add New Role button
+        $this->clickButton('add_new_role', true);
+        //Fill Role name field
+        $this->fillField('role_name', $fieldData['role_name']);
+        //Open Resources Tab
+        $this->openTab('resources');
+        //Selecting All at Role Access Dropdown
+        $this->fillDropdown('role_access', 'All');
+        //Saving API Role
+        $this->clickButton('save');
+        //Verify that role is saved
+        $this->assertMessagePresent('success', 'success_saved_role');
+        $this->clickButton('save');
+        //Open created role from the role grid
+        $userSearch =array('filter_role_name' => $fieldData['role_name']);
+        $this->searchAndOpen($userSearch);
+        //Click Delete API Role button
+        $this->clickButton('delete');
+        //Verify that message "The role has been deleted." is displayed
+        $this->assertMessagePresent('success', 'success_deleted_role');
     }
 }
