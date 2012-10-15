e26d5ecd9d92249cafc70f2cbc669896c87aea92
Maksym Iakusha <miakusha@ebay.com>
2012-10-15 14:40:53 +0300

    Implemented MAGETWO-4044: Modify Controller

--- app/code/core/Mage/Adminhtml/controllers/Tax/RateController.php
+++ app/code/core/Mage/Adminhtml/controllers/Tax/RateController.php
@@ -533,17 +533,6 @@ class Mage_Adminhtml_Tax_RateController extends Mage_Adminhtml_Controller_Action
 
     protected function _isAllowed()
     {
-
-        switch ($this->getRequest()->getActionName()) {
-            case 'importExport':
-                return Mage::getSingleton('Mage_Core_Model_Authorization')->isAllowed('Mage_Tax::import_export');
-                break;
-            case 'index':
-                return Mage::getSingleton('Mage_Core_Model_Authorization')->isAllowed('Mage_Tax::tax_rates');
-                break;
-            default:
-                return Mage::getSingleton('Mage_Core_Model_Authorization')->isAllowed('Mage_Tax::tax_rates');
-                break;
-        }
+        return Mage::getSingleton('Mage_Core_Model_Authorization')->isAllowed('Mage_Tax::manage_tax');
     }
 }
