097ea94c22502c30ec75389f06b1babea8e522d2
Anatoliy Yastreb <ayastreb@ebay.com>
2012-10-19 19:11:05 +0300

    Implemented MAGETWO-4195: Implementing Resource Config Population - fixed incorrect tag problem

--- app/code/core/Mage/Webapi/Model/Config/Resource.php
+++ app/code/core/Mage/Webapi/Model/Config/Resource.php
@@ -492,7 +492,7 @@ class Mage_Webapi_Model_Config_Resource
             /** @var \Zend\Code\Reflection\DocBlock\Tag\GenericTag $varTag */
             $varTag = current($tags);
             // TODO: quickfix. In php 5.3.13 DocBlockReflection returns tags as "string\r"
-            $varType = str_replace('\r', '', $varTag->returnValue(0));
+            $varType = str_replace("\r", '', $varTag->returnValue(0));
             $varTypeArrayClean = str_replace('[]', '', $varType);
             $propertyType = ($varTypeArrayClean == $class || $varTypeArrayClean == $previouslyProcessedClass)
                 ? $this->translateTypeName($varType)
