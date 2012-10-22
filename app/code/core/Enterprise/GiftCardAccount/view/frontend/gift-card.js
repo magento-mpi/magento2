7cdb2c291425247c2ea1c500225e639021683063
Vijay Sriram <vsriram@x.com>
2012-10-22 14:26:21 -0700

    Fix MAGETWO-1185: javascript refactor -Cart Page, gift card option

---
+++ app/code/core/Enterprise/GiftCardAccount/view/frontend/gift-card.js
@@ -0,0 +1,39 @@
+/**
+ * {license_notice}
+ *
+ * @category    checkout gift card
+ * @package     mage
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/*jshint browser:true jquery:true*/
+(function ($) {
+    $.widget('mage.giftCard', {
+        options: {
+        },
+        _create: function () {
+            this.giftCardCode = $(this.options.giftCardCodeSelector);
+            $(this.options.applyButton).on('click', $.proxy(function () {
+                this.giftCardCode.attr('data-validate', '{required:true}');
+                this.element.mage().validate().submit();
+            }, this));
+
+            $(this.options.checkStatus).on('click', $.proxy(function () {
+                var giftCardStatusId = this.options.giftCardStatusId;
+                var giftCardSpinnerId = $(this.options.giftCardSpinnerId);
+                $.ajax({
+                    url: this.options.giftCardStatusUrl,
+                    type: 'post',
+                    data: {'giftcard_code': $(this.options.giftCardCodeSelector).val()},
+                    beforeSend: function () {
+                        giftCardSpinnerId.show();
+                    },
+                    success: function (response) {
+                        $(giftCardStatusId).html(response);
+                        giftCardSpinnerId.hide();
+                    }
+                });
+            }, this));
+        }
+    });
+})(jQuery);
