cf0676f00bf6a35aa93796b0d94b6086cc4f7c1f
Hryhorii Hrebiniuk <hhrebiniuk@ebay.com>
2012-10-22 19:05:06 +0300

    Fixed MAGETWO-4126: Get rid of instances of varienForm of type "onclick declaration"  Fixed passing data attributes to a button widget  Deleted excessive inclusion on jQuery lib   - jquery/jquery-1.7.1.min.js   - mage/jquery-no-conflict.js   - jquery/jquery-ui-1.8.18.custom.min.js

--- app/code/core/Mage/Backend/Block/Widget/Button.php
+++ app/code/core/Mage/Backend/Block/Widget/Button.php
@@ -67,6 +67,11 @@ class Mage_Backend_Block_Widget_Button extends Mage_Backend_Block_Widget
             'value'     => $this->getValue(),
             'disabled'  => $this->getDisabled() ? 'disabled' : ''
         );
+        if ($this->getDataAttr()) {
+            foreach ($this->getDataAttr() as $key => $attr) {
+                $attributes['data-' . $key] = str_replace('"', '&quot;', json_encode($attr));
+            }
+        }
 
         $html = '';
         foreach ($attributes as $attributeKey => $attributeValue) {
