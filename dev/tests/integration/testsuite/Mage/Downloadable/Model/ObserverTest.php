79b0fbd843004eff909b014a5525a3119cce605e
Maryna Ilnytska <milnytska@ebay.com>
2012-10-23 19:16:48 +0300

    Fixed MAGETWO-4103: It is impossible to duplicate downloadable product with uploaded sample file

---
+++ dev/tests/integration/testsuite/Mage/Downloadable/Model/ObserverTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_Downloadable
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ *
+ */
+class Mage_Downloadable_Model_ObserverTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @magentoAppIsolation enabled
+     * @magentoDataFixture Mage/Downloadable/_files/product_with_files.php
+     */
+    public function testDuplicate()
+    {
+        $currentProduct = new Mage_Catalog_Model_Product;
+        $currentProduct->load(1); // fixture for initial product
+        $currentLinks = $currentProduct->typeInstance($currentProduct)->getLinks($currentProduct);
+        $currentSamples = $currentProduct->getTypeInstance($currentProduct)->getSamples($currentProduct);
+
+        $newProduct = $currentProduct->duplicate();
+
+        $newLinks = $newProduct->getTypeInstance($newProduct)->getLinks($newProduct);
+        $newSamples = $newProduct->getTypeInstance($newProduct)->getSamples($newProduct);
+
+        $this->assertEquals($currentLinks, $newLinks,
+            'File for links has been lost after duplication');
+        $this->assertEquals($currentSamples, $newSamples,
+            'File for samples has been lost after duplication');
+    }
+}
