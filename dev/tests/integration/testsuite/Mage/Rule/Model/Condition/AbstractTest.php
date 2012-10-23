090c6774c352f8535cb2e7d604b07598da858928
Hryhorii Hrebiniuk <hhrebiniuk@ebay.com>
2012-10-23 13:26:29 +0300

    Fixed MAGETWO-4468: Failed integration two tests - fixed Mage_Adminhtml_Block_Report_GridTest::testGetDateFormat and Mage_Rule_Model_Condition_AbstractTest::testGetValueElement

--- dev/tests/integration/testsuite/Mage/Rule/Model/Condition/AbstractTest.php
+++ dev/tests/integration/testsuite/Mage/Rule/Model/Condition/AbstractTest.php
@@ -17,7 +17,12 @@ class Mage_Rule_Model_Condition_AbstractTest extends PHPUnit_Framework_TestCase
     public function testGetValueElement()
     {
         /** @var Mage_Rule_Model_Condition_Abstract $model */
-        $model = $this->getMockForAbstractClass('Mage_Rule_Model_Condition_Abstract');
+        $model = $this->getMockForAbstractClass('Mage_Rule_Model_Condition_Abstract', array(), '',
+            true, true, true, array('getValueElementRenderer'));
+        $model->expects($this->any())
+             ->method('getValueElementRenderer')
+             ->will($this->returnValue(new Mage_Rule_Block_Editable));
+
         $rule = new Mage_Rule_Model_Rule;
         $model->setRule($rule->setForm(new Varien_Data_Form()));
 
