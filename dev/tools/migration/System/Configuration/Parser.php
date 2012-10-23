d95464aadd097844030153b98d772f01bbb49373
Vladimir Korotovskih <vladimir.korotovskih@magento.com>
2012-10-23 18:27:37 +0300

    CR Changes MAGETWO-4219: Migration Tool

--- dev/tools/migration/System/Configuration/Parser.php
+++ dev/tools/migration/System/Configuration/Parser.php
@@ -4,32 +4,85 @@ class Tools_Migration_System_Configuration_Parser
 {
     public function parse(DOMDocument $dom)
     {
-        return array(
-            'comment' => 'licence text',
-            'tabs' => array(
-                'catalog' => array(
-                    '@attributes' => array(
-                        'translate' => 'label',
-                        'module' => 'Mage_Catalog',
-                    ),
-                    'label' => 'test',
-                    'sort_order' => 'test',
-                )
-            ),
-            'sections' => array(
-                'catalog' => array(
-                    '@attributes' => array(
-                        'translate' => 'label',
-                        'module' => 'Mage_Catalog',
-                    ),
-                    'label' => 'test',
-                    'sort_order' => 'test',
-                    'groups' => array(
+        $result = array();
+        if ($dom->hasChildNodes()) {
+            /** @var $child DOMNode */
+            foreach ($dom->childNodes as $child) {
+                if (XML_COMMENT_NODE == $child->nodeType) {
+                    $result['comment'] = $child->nodeValue;
+                } else if(XML_ELEMENT_NODE == $child->nodeType && 'config' == $child->nodeName) {
+                    $result = array_merge($result, $this->_parseNode($child));
+                }
+            }
+        }
+        return $result;
+    }
 
-                    )
-                )
-            ),
+    protected function _parseNode(DOMNode $node = null)
+    {
+        // return empty array if dom is blank
+        if (is_null($node)) {
+            return array();
+        }
 
-        );
+        if (!$node->hasChildNodes()) {
+            $result = $node->nodeValue;
+        } else {
+            $result = array();
+            foreach ($node->childNodes as $childNode) {
+                // how many of these child nodes do we have?
+                // this will give us a clue as to what the result structure should be
+                $oChildNodeList = $node->getElementsByTagName($childNode->nodeName);
+                $iChildCount = 0;
+                // there are x number of childs in this node that have the same tag name
+                // however, we are only interested in the # of siblings with the same tag name
+                foreach ($oChildNodeList as $oNode) {
+                    if ($oNode->parentNode->isSameNode($childNode->parentNode)) {
+                        $iChildCount++;
+                    }
+                }
+                $mValue = $this->_parseNode($childNode);
+                $sKey   = ($childNode->nodeName{0} == '#') ? 0 : $childNode->nodeName;
+                $mValue = (is_array($mValue) && !empty($mValue)) ? $mValue[$childNode->nodeName] : $mValue;
+                if ($sKey == 0 && is_array($mValue) && empty($mValue)) {
+                    continue;
+                }
+                // how many of thse child nodes do we have?
+                if ($iChildCount > 1) {  // more than 1 child - make numeric array
+                    $result[$sKey][] = $mValue;
+                } else {
+                    $result[$sKey] = $mValue;
+                }
+            }
+            // if the child is <foo>bar</foo>, the result will be array(bar)
+            // make the result just 'bar'
+            if (count($result) == 1 && isset($result[0]) && !is_array($result[0])) {
+                $result = $result[0];
+            }
+        }
+
+        // get our attributes if we have any
+        $attributes = array();
+        if ($node->hasAttributes()) {
+            foreach ($node->attributes as $sAttrName=>$oAttrNode) {
+                // retain namespace prefixes
+                $attributes["{$oAttrNode->nodeName}"] = $oAttrNode->nodeValue;
+            }
+        }
+
+        if (count($attributes)) {
+            if (!is_array($result)) {
+                $result = (trim($result)) ? array('nodeValue' => $result) : array();
+            }
+            $fResult = array($node->nodeName => array_merge($result, array('@attributes' => $attributes)));
+        } else {
+            if (is_array($result)) {
+                $fResult = array($node->nodeName => $result);
+            } else {
+                $fResult = (trim($result)) ? array($node->nodeName => $result) : array();
+            }
+        }
+
+        return $fResult;
     }
 }
--- dev/tools/migration/System/Configuration/Reader.php
+++ dev/tools/migration/System/Configuration/Reader.php
@@ -2,11 +2,57 @@
 
 class Tools_Migration_System_Configuration_Reader
 {
+
+    protected $_fileManager;
+
+    protected $_parser;
+
+    protected $_mapper;
+
+    protected $_basePath;
+
+    CONST SYSTEM_CONFIG_PATH_PATTERN = 'app/code/*/*/*/etc/system.xml';
+
+    public function __construct(
+        Tools_Migration_System_FileManager $fileManager,
+        Tools_Migration_System_Configuration_Parser $parser,
+        Tools_Migration_System_Configuration_Mapper $mapper
+    )
+    {
+        $this->_fileManager = $fileManager;
+        $this->_parser = $parser;
+        $this->_mapper = $mapper;
+
+        $this->_basePath = realpath(dirname(__FILE__) . '/../../../../..');
+    }
+
     /**
      * @return array
      */
     public function getConfiguration()
     {
-        return array();
+        $files = glob(
+            $this->_basePath . DIRECTORY_SEPARATOR
+            . Tools_Migration_System_Configuration_Reader::SYSTEM_CONFIG_PATH_PATTERN
+        );
+        $result = array();
+        foreach ($files as $fileName) {
+            $result[$fileName] = $this->_mapper->transform(
+                $this->_parser->parse(
+                    $this->_getDOMDocument(
+                        $this->_fileManager->getContents($fileName)
+                    )
+                )
+            );
+        }
+
+        return $result;
+    }
+
+    protected function _getDOMDocument($xml)
+    {
+        $dom = new DOMDocument();
+        $dom->loadXML($xml);
+        return $dom;
     }
 }
