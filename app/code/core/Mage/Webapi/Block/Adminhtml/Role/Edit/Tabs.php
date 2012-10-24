7a6c1d2a3988b48f5071b13534c13000065b8fba
Alexander Makeev <amakeev@ebay.com>
2012-10-24 19:44:27 +0300

    CR Changes MAGETWO-3810: Update ACL design according to wiki - Fixed JS bug

--- app/code/core/Mage/Webapi/Block/Adminhtml/Role/Edit/Tabs.php
+++ app/code/core/Mage/Webapi/Block/Adminhtml/Role/Edit/Tabs.php
@@ -38,7 +38,7 @@ class Mage_Webapi_Block_Adminhtml_Role_Edit_Tabs extends Mage_Backend_Block_Widg
      */
     protected function _beforeToHtml()
     {
-        /** @var $mainBlock Mage_Webapi_Block_Adminhtml_Role_Edit_Tab_Main */
+        /** @var Mage_Webapi_Block_Adminhtml_Role_Edit_Tab_Main $mainBlock */
         $mainBlock = $this->getLayout()->getBlock('webapi.role.edit.tab.main');
         $mainBlock->setApiRole($this->getApiRole());
         $this->addTab('main_section', array(
@@ -48,7 +48,7 @@ class Mage_Webapi_Block_Adminhtml_Role_Edit_Tabs extends Mage_Backend_Block_Widg
             'active' => true
         ));
 
-        /** @var $resourceBlock Mage_Webapi_Block_Adminhtml_Role_Edit_Tab_Resource */
+        /** @var Mage_Webapi_Block_Adminhtml_Role_Edit_Tab_Resource $resourceBlock */
         $resourceBlock = $this->getLayout()->getBlock('webapi.role.edit.tab.resource');
         $resourceBlock->setApiRole($this->getApiRole());
         $this->addTab('resource_section', array(
@@ -57,8 +57,8 @@ class Mage_Webapi_Block_Adminhtml_Role_Edit_Tabs extends Mage_Backend_Block_Widg
             'content' => $resourceBlock->toHtml()
         ));
 
-        /** @var $userBlock Mage_Webapi_Block_Adminhtml_Role_Edit_Tab_User */
         if ($this->getApiRole() && $this->getApiRole()->getRoleId() > 0) {
+            /** @var Mage_Webapi_Block_Adminhtml_Role_Edit_Tab_User $userBlock */
             $userBlock = $this->getLayout()->getBlock('webapi.role.edit.tab.user');
             $userBlock->setApiRole($this->getApiRole());
             $this->addTab('user_section', array(
@@ -66,6 +66,10 @@ class Mage_Webapi_Block_Adminhtml_Role_Edit_Tabs extends Mage_Backend_Block_Widg
                 'title' => Mage::helper('Mage_Webapi_Helper_Data')->__('Users'),
                 'content' => $userBlock->toHtml()
             ));
+        } else {
+            /** @var Mage_Core_Block_Template $usersJsBlock */
+            $usersJsBlock = $this->getLayout()->getBlock('roles-users-grid-js');
+            $usersJsBlock->setTemplate('');
         }
 
         return parent::_beforeToHtml();
