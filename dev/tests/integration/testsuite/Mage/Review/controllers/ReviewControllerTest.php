3fe3ff18167936bdc9d837745e07ea05dee668f2
Anna Avramenko <aavramenko@ebay.com>
2012-10-25 12:54:12 +0300

    Fixed MAGETWO-3125: [Vulnerability] Product name XSS vulnerabililty in customer review

---
+++ dev/tests/integration/testsuite/Mage/Review/controllers/ReviewControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Review
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Mage_Review_ReviewControllerTest extends Mage_Adminhtml_Utility_Controller
+{
+    /**
+     * @magentoDataFixture Mage/Review/_files/review_xss.php
+     */
+    public function testEditActionProductNameXss()
+    {
+        $this->dispatch('backend/admin/catalog_product_review/edit/id/1');
+        $responseBody = $this->getResponse()->getBody();
+        $this->assertContains('&lt;script&gt;alert(&quot;xss&quot;);&lt;/script&gt;', $responseBody);
+        $this->assertNotContains('<script>alert("xss");</script>', $responseBody);
+    }
+}
