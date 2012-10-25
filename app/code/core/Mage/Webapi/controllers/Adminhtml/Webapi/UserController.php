1c156d726f75d47a865fa688b84e669fd06df9fe
Alexander Makeev <amakeev@ebay.com>
2012-10-25 18:48:24 +0300

    Implemented MAGETWO-4520: Fix TAF test after implementing MAGETWO-3810 - Fixed bug in titles

--- app/code/core/Mage/Webapi/controllers/Adminhtml/Webapi/UserController.php
+++ app/code/core/Mage/Webapi/controllers/Adminhtml/Webapi/UserController.php
@@ -76,7 +76,7 @@ class Mage_Webapi_Adminhtml_Webapi_UserController extends Mage_Backend_Controlle
 
         // Update title and breadcrumb record
         $actionTitle = $user->getId()
-            ? Mage::helper('Mage_Webapi_Helper_Data')->escapeHtml($user->getContactEmail())
+            ? Mage::helper('Mage_Webapi_Helper_Data')->escapeHtml($user->getApiKey())
             : Mage::helper('Mage_Webapi_Helper_Data')->__('New API User');
         $this->_title($actionTitle);
         $this->_addBreadcrumb($actionTitle, $actionTitle);
