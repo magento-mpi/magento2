7ae436bc2e7eff60508027e54ae5c42bddc5a0c9
Michael Chiocca <mchiocca@x.com>
2012-10-25 09:05:00 -0700

    Fix MAGETWO-4456: Can't proceed with installation due to a Javascript error - Incorporate code review comments from CR-MAGETWO-1531

--- app/code/core/Mage/Install/view/install/js/install.js
+++ app/code/core/Mage/Install/view/install/js/install.js
@@ -8,15 +8,12 @@
  */
 /*jshint browser:true jquery:true */
 (function($) {
-    $.widget('mage.install', {
-        _create: function() {
-        },
-
+    $.widget('mage.begin', {
         /**
          * Beginning phase of the installation process. Check the box to agree to Terms and
          * Conditions, License, etc. and then click the Continue button.
          */
-        begin: function() {
+        _create: function() {
             this.element.on('click', $.proxy(function(e) {
                 var btn = $(this.options.submitButtonSelector);
                 if (e.target.checked) {
@@ -27,13 +24,15 @@
                         .attr('disabled', 'disabled');
                 }
             }, this));
-        },
+        }
+    });
 
+    $.widget('mage.configure', {
         /**
          * Configuration phase. Prompt for hostname, database information, and options,
          * such as whether to enable SSL, referred to as secure options.
          */
-        config: function() {
+        _create: function() {
             if (this.options.url) {
                 this.element.on('click', $.proxy(function() {
                     location.href = this.options.url;
@@ -46,21 +45,25 @@
                         $(this.options.useSecureOptionsSelector).hide();
                 }, this));
             }
-        },
+        }
+    });
 
+    $.widget('mage.createAdmin', {
         /**
          * Create backend administrator login form validation. Enter user's name, email,
          * admin username, and password. Validate the form.
          */
-        createAdmin: function() {
+        _create: function() {
             this.element.mage().validate({errorClass: 'mage-error', errorElement: 'div'});
-        },
+        }
+    });
 
+    $.widget('mage.setLocale', {
         /**
          * Generate a new URL whenever a different locale is selected and refresh the
          * page to that new locale based URL.
          */
-        changeUrl: function() {
+        _create: function() {
             this.element.on('change', $.proxy(function() {
                 location.href = this.options.url + 'locale/' + this.element.val() +
                     '/?timezone=' + $('#timezone').val() + '&amp;currency=' + $('#currency').val();
