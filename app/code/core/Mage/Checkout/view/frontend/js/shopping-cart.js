4887b63fd4e16300a41b201be6ff2bfa60ddf83c
Michael Chiocca <mchiocca@x.com>
2012-10-25 11:28:23 -0700

    Fix MAGETWO-1994: Refactor shopping cart page to use jQuery in all themes - Convert shopping cart to a jQuery widget and update all themes

--- app/code/core/Mage/Checkout/view/frontend/js/shopping-cart.js
+++ app/code/core/Mage/Checkout/view/frontend/js/shopping-cart.js
@@ -8,23 +8,18 @@
  */
 /*jshint browser:true jquery:true*/
 (function($) {
-    $(document).ready(function() {
-        var initData = {
-            updateCartActionContainer: undefined /* <!--[if lt IE 8]> Only */
-        };
-
-        $.mage.event.trigger('mage.shoppingCart.initialize', initData);
-
-        if ($(initData.updateCartActionContainer).length > 0) { /* <!--[if lt IE 8]> Only */
-            $(initData.emptyCartButton).on('click', function() {
-                $(initData.emptyCartButton).attr('name', 'update_cart_action_temp');
-                $(initData.updateCartActionContainer)
-                    .attr('name', 'update_cart_action').attr('value', 'empty_cart');
-            });
+    $.widget('mage.shoppingCart', {
+        _create: function() {
+            if ($(this.options.updateCartActionContainer).length > 0) { /* <!--[if lt IE 8]> Only */
+                $(this.options.emptyCartButton).on('click', $.proxy(function() {
+                    $(this.options.emptyCartButton).attr('name', 'update_cart_action_temp');
+                    $(this.options.updateCartActionContainer)
+                        .attr('name', 'update_cart_action').attr('value', 'empty_cart');
+                }, this));
+            }
+            $(this.options.continueShoppingButton).on('click', $.proxy(function() {
+                location.href = this.options.continueShoppingUrl;
+            }, this));
         }
-
-        $(initData.continueShoppingButton).on('click', function() {
-            location.href = initData.continueShoppingUrl;
-        });
     });
 })(jQuery);
