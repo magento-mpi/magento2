0d5be8728d67d4b40cd22d0d0c15e06d7f2401b9
Anna Avramenko <aavramenko@ebay.com>
2012-10-30 16:55:52 +0200

    CR Changes MAGETWO-3125: [Vulnerability] Product name XSS vulnerability in customer review

--- dev/tests/integration/testsuite/Mage/Adminhtml/controllers/Catalog/Product/ReviewControllerTest.php
+++ dev/tests/integration/testsuite/Mage/Adminhtml/controllers/Catalog/Product/ReviewControllerTest.php
@@ -3,7 +3,7 @@
  * {license_notice}
  *
  * @category    Magento
- * @package     Mage_Review
+ * @package     Mage_Adminhtml
  * @subpackage  integration_tests
  * @copyright   {copyright}
  * @license     {license_link}
