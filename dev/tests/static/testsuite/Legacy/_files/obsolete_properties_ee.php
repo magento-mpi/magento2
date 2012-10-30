f9662890213f57595a3bb0a98aedc24f86e80c1a
Sergey Kovalev <skovalev@ebay.com>
2012-10-30 12:38:20 +0200

    CR Changes MAGETWO-3605: Corrupted Legacy Tests (cherry picked from commit ce6cb8bf12868571fb1d59e1d6b6458277cfe323)
    Conflicts:
    
    	dev/tests/static/testsuite/Legacy/_files/obsolete_methods.php

--- dev/tests/static/testsuite/Legacy/_files/obsolete_properties_ee.php
+++ dev/tests/static/testsuite/Legacy/_files/obsolete_properties_ee.php
@@ -9,11 +9,11 @@
  * @license     {license_link}
  */
 return array(
-    $this->_addRule('_eventData', 'Enterprise_Logging_Block_Adminhtml_Container'),
-    $this->_addRule('_customerSegments', 'Enterprise_CustomerSegment_Model_Customer'),
-    $this->_addRule('_limit', 'Enterprise_Search_Model_Resource_Index'),
-    $this->_addRule('_amountCache', 'Enterprise_GiftCard_Block_Catalog_Product_Price'),
-    $this->_addRule('_minMaxCache', 'Enterprise_GiftCard_Block_Catalog_Product_Price'),
-    $this->_addRule('_skipFields', 'Enterprise_Logging_Model_Processor'),
-    $this->_addRule('_layoutUpdate', 'Enterprise_WebsiteRestriction_IndexController'),
+    $this->_getRule('_eventData', 'Enterprise_Logging_Block_Adminhtml_Container'),
+    $this->_getRule('_customerSegments', 'Enterprise_CustomerSegment_Model_Customer'),
+    $this->_getRule('_limit', 'Enterprise_Search_Model_Resource_Index'),
+    $this->_getRule('_amountCache', 'Enterprise_GiftCard_Block_Catalog_Product_Price'),
+    $this->_getRule('_minMaxCache', 'Enterprise_GiftCard_Block_Catalog_Product_Price'),
+    $this->_getRule('_skipFields', 'Enterprise_Logging_Model_Processor'),
+    $this->_getRule('_layoutUpdate', 'Enterprise_WebsiteRestriction_IndexController'),
 );
