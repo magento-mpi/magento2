1ab2fd1c6a0e34a535d1cdd1960e2cd80f92c380
Anton Kril <akril@ebay.com>
2012-10-31 20:15:33 +0200

    Implemented MAGETWO-4220: Unit tests for migration tool

--- dev/tests/unit/testsuite/tools/migration/System/Configuration/GeneratorTest.php
+++ dev/tests/unit/testsuite/tools/migration/System/Configuration/GeneratorTest.php
@@ -1,8 +1,77 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: User
- * Date: 25.10.12
- * Time: 14:19
- * To change this template use File | Settings | File Templates.
+ * {license_notice}
+ *
+ * @category   Magento
+ * @package    tools
+ * @copyright  {copyright}
+ * @license    {license_link}
  */
+
+require_once realpath(dirname(__FILE__) . '/../../../../../../../')
+    . '/tools/migration/System/Configuration/Generator.php';
+require_once realpath(dirname(__FILE__) . '/../../../../../../../')
+    . '/tools/migration/System/FileManager.php';
+require_once realpath(dirname(__FILE__) . '/../../../../../../../')
+    . '/tools/migration/System/Configuration/LoggerAbstract.php';
+require_once realpath(dirname(__FILE__) . '/../../../../../../../')
+    . '/tools/migration/System/Configuration/Logger/Console.php';
+require_once realpath(dirname(__FILE__) . '/../../../../../../../')
+    . '/tools/migration/System/Configuration/Formatter.php';
+
+
+class Tools_Migration_System_Configuration_GeneratorTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Tools_Migration_System_Configuration_Reader
+     */
+    protected $_model;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_fileManagerMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_loggerMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_formatterMock;
+
+    protected function setUp()
+    {
+        $this->_fileManagerMock = $this->getMock('Tools_Migration_System_FileManager', array(), array(), '', false);
+        $this->_loggerMock = $this->getMock('Tools_Migration_System_Configuration_Logger_Console', array(), array(),
+            '', false
+        );
+        $this->_formatterMock = $this->getMock('Tools_Migration_System_Configuration_Formatter', array(), array(),
+            '', false
+        );
+
+        $this->_model = new Tools_Migration_System_Configuration_Generator(
+            $this->_fileManagerMock, $this->_formatterMock, $this->_loggerMock
+        );
+    }
+
+    public function testGetConfiguration()
+    {
+        $this->_fileManagerMock->expects($this->once())->method('getFileList')
+            ->will($this->returnValue(array('testFile')));
+        $this->_fileManagerMock->expects($this->once())->method('getContents')->with('testFile')
+            ->will($this->returnValue('<config><system><tabs></tabs></system></config>'));
+        $parsedArray = array('config' => array('system' => array('tabs')));
+        $this->_parserMock->expects($this->once())->method('parse')->with($this->isInstanceOf('DOMDocument'))
+            ->will($this->returnValue($parsedArray));
+
+        $transformedArray = array('value' => 'expected');
+        $this->_mapperMock->expects($this->once())->method('transform')->with($parsedArray)
+            ->will($this->returnValue($transformedArray));
+
+
+        $this->assertEquals(array('testFile' => $transformedArray), $this->_model->getConfiguration());
+    }
+}
--- dev/tests/unit/testsuite/tools/migration/System/Configuration/ReaderTest.php
+++ dev/tests/unit/testsuite/tools/migration/System/Configuration/ReaderTest.php
@@ -11,13 +11,17 @@
 require_once realpath(dirname(__FILE__) . '/../../../../../../../')
     . '/tools/migration/System/Configuration/Reader.php';
 require_once realpath(dirname(__FILE__) . '/../../../../../../../')
+    . '/tools/migration/System/FileManager.php';
+require_once realpath(dirname(__FILE__) . '/../../../../../../../')
     . '/tools/migration/System/Configuration/Mapper.php';
+require_once realpath(dirname(__FILE__) . '/../../../../../../../')
+    . '/tools/migration/System/Configuration/Parser.php';
 
 
 class Tools_Migration_System_Configuration_ReaderTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * @var Tools_Migration_System_Writer_Factory
+     * @var Tools_Migration_System_Configuration_Reader
      */
     protected $_model;
 
@@ -42,8 +46,26 @@ class Tools_Migration_System_Configuration_ReaderTest extends PHPUnit_Framework_
         $this->_parserMock = $this->getMock('Tools_Migration_System_Configuration_Parser', array(), array(), '', false);
         $this->_mapperMock = $this->getMock('Tools_Migration_System_Configuration_Mapper', array(), array(), '', false);
 
-        $this->_model = new Tools_Migration_System_Writer_Factory(
+        $this->_model = new Tools_Migration_System_Configuration_Reader(
             $this->_fileManagerMock, $this->_parserMock, $this->_mapperMock
         );
+    }
+
+    public function testGetConfiguration()
+    {
+        $this->_fileManagerMock->expects($this->once())->method('getFileList')
+            ->will($this->returnValue(array('testFile')));
+        $this->_fileManagerMock->expects($this->once())->method('getContents')->with('testFile')
+            ->will($this->returnValue('<config><system><tabs></tabs></system></config>'));
+        $parsedArray = array('config' => array('system' => array('tabs')));
+        $this->_parserMock->expects($this->once())->method('parse')->with($this->isInstanceOf('DOMDocument'))
+            ->will($this->returnValue($parsedArray));
+
+        $transformedArray = array('value' => 'expected');
+        $this->_mapperMock->expects($this->once())->method('transform')->with($parsedArray)
+            ->will($this->returnValue($transformedArray));
+
+
+        $this->assertEquals(array('testFile' => $transformedArray), $this->_model->getConfiguration());
     }
 }
