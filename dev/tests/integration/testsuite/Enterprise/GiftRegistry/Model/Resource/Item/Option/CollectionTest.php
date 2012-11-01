250e531b32a2122b1d71e883dee42129535d18c5
Anton Makarenko <anton.makarenko@magento.com>
2012-11-01 11:36:22 -0700

    MAGETWO-4714 master-voodoo Synchronization 46 - fixed broken integration tests (error during merging)

--- dev/tests/integration/testsuite/Enterprise/GiftRegistry/Model/Resource/Item/Option/CollectionTest.php
+++ dev/tests/integration/testsuite/Enterprise/GiftRegistry/Model/Resource/Item/Option/CollectionTest.php
@@ -13,7 +13,7 @@ class Enterprise_GiftRegistry_Model_Resource_Item_Option_CollectionTest extends 
         $select = $collection->getSelect();
         $this->assertSame(array(), $select->getPart(Zend_Db_Select::WHERE));
 
-        $product = new Mage_Catalog_Model_Product;
+        $product = Mage::getModel('Mage_Catalog_Model_Product');
         $product->setId(4);
         $collection->addProductFilter(1)->addProductFilter(array(2, 3))->addProductFilter($product);
         $this->assertStringMatchesFormat(
