fca3a327bc8cb8b2191a5136e0742f2afd68546c
Michael Chiocca <mchiocca@x.com>
2012-11-01 11:02:20 -0700

    Fix MAGETWO-4478: Convert Magento 2 components to jQuery widgets - Add JSDoc block to product comparison sidebar code - Convert product comparison popup table (list) to a jQuery widget

--- app/code/core/Mage/Catalog/view/frontend/js/compare.js
+++ app/code/core/Mage/Catalog/view/frontend/js/compare.js
@@ -15,6 +15,14 @@
             this._confirm(this.options.removeSelector, this.options.removeConfirmMessage);
             this._confirm(this.options.clearAllSelector, this.options.clearAllConfirmMessage);
         },
+
+        /**
+         * Set up a click event on the given selector to display a confirmation request message
+         * and ask for that confirmation.
+         * @param selector Selector for the confirmation on click event
+         * @param message Message to display asking for confirmation to perform action
+         * @private
+         */
         _confirm: function(selector, message) {
             $(selector).on('click', function() {
                 return confirm(message);
--- app/code/core/Mage/Catalog/view/frontend/js/list.js
+++ app/code/core/Mage/Catalog/view/frontend/js/list.js
@@ -7,64 +7,53 @@
  * @license     {license_link}
  */
 /*jshint browser:true jquery:true*/
-
 (function ($) {
-    $(document).ready(function () {
-        var _compareList = {
-            productSelector: null,
-            productImageSelector: null,
-            productAddToCartSelector: null,
-            productWishListSelector: null,
-            productRemoveSelector: null,
-            productFormSelector: null,
-            ajaxSpinner: null,
-            windowCloseSelector: null,
-            printSelector: null
-        };
+    $.widget('mage.compareList', {
+        _create: function() {
+            this.element.decorate('table');
 
-        $.mage.event.trigger('mage.compare-list.initialize', _compareList);
-        $(_compareList.productFormSelector).decorate('table');
+            $(this.options.windowCloseSelector).on('click', function() {
+                window.close();
+            });
 
-        function _setParentWindow(selector) {
-            $(selector).on('click', function (e) {
+            $(this.options.windowPrintSelector).on('click', function(e) {
                 e.preventDefault();
-                window.opener.focus();
-                window.opener.location.href = $(this).data('url');
+                window.print();
+            });
+
+            var ajaxSpinner = $(this.options.ajaxSpinner);
+            $(this.options.productRemoveSelector).on('click', $.proxy(function(e) {
+                e.preventDefault();
+                $.ajax({
+                    url: $(e.target).data('url'),
+                    type: 'POST',
+                    beforeSend: function() {
+                        ajaxSpinner.show();
+                    }
+                }).done(function() {
+                    ajaxSpinner.hide();
+                    window.location.reload();
+                    window.opener.location.reload();
+                });
+            }, this));
+
+            var properties = [
+                'windowCloseSelector',
+                'windowPrintSelector',
+                'productRemoveSelector',
+                'ajaxSpinner',
+                'productTableSelector'
+            ];
+
+            $.each(this.options, function(index, option) {
+               if ($.inArray(index, properties) === -1) {
+                   $(option).on('click', function(e) {
+                       e.preventDefault();
+                       window.opener.focus();
+                       window.opener.location.href = $(this).data('url');
+                   });
+               }
             });
         }
-
-        $(_compareList.windowCloseSelector).on('click', function() {
-            window.close();
-        });
-
-        $(_compareList.printSelector).on('click', function(e) {
-            e.preventDefault();
-            window.print();
-        });
-
-        $(_compareList.productRemoveSelector).on('click', function(e) {
-            e.preventDefault();
-            $.ajax({
-                url: $(e.target).data('url'),
-                type: 'POST',
-                beforeSend: function() {
-                    $(_compareList.ajaxSpinner).show();
-                }
-            }).done(function() {
-                $(_compareList.ajaxSpinner).hide();
-                window.location.reload();
-                window.opener.location.reload();
-            });
-        });
-
-        $.each(_compareList, function(index, prop) {
-            // Removed properties that doesn't need to call _setParentWindow
-            var notAllowedProp = [
-                'windowCloseSelector', 'printSelector', 'productRemoveSelector', 'ajaxSpinner', 'productFormSelector'
-            ];
-            if ($.inArray(index, notAllowedProp) === -1) {
-                _setParentWindow(prop);
-            }
-        });
     });
 })(jQuery);
