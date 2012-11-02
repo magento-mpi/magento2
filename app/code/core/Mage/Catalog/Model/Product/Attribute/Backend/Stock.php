f77656a3ea454a7be7ae687205dc1e4d64c9a267
Igor Tripolskiy <itripolskiy@ebay.com>
2012-11-02 15:47:26 +0200

    Fixed MAGETWO-4271: Quantity field on General tab is a required field and doesn't allow to Manage Stock

--- app/code/core/Mage/Catalog/Model/Product/Attribute/Backend/Stock.php
+++ app/code/core/Mage/Catalog/Model/Product/Attribute/Backend/Stock.php
@@ -59,6 +59,8 @@ class Mage_Catalog_Model_Product_Attribute_Backend_Stock extends Mage_Eav_Model_
     {
         /** @var $object Mage_Catalog_Model_Product  */
         $stockData = $object->getData($this->getAttribute()->getAttributeCode());
+        $stockData['qty'] = $stockData['qty'] === '' ? null : $stockData['qty'];
+
         $object->setStockData(array_replace((array)$object->getStockData(), (array)$stockData));
         $object->unsetData($this->getAttribute()->getAttributeCode());
         parent::beforeSave($object);
