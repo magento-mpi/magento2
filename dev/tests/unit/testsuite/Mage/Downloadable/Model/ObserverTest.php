6e10cbd916b449478a0c2e11c06fd14bed1cb411
Evgeniy Maksimenko <emaksimenko@ebay.com>
2012-11-06 12:22:28 +0200

    CR changes MAGETWO-4781 After deselecting chechbox 'weight_and_type_switcher' in downloadable product product type doesn't change

--- dev/tests/unit/testsuite/Mage/Downloadable/Model/ObserverTest.php
+++ dev/tests/unit/testsuite/Mage/Downloadable/Model/ObserverTest.php
@@ -25,23 +25,23 @@ class Mage_Downloadable_Model_ObserverTest extends PHPUnit_Framework_TestCase
     {
         $downloadableData = array('links' => array());
 
-        $request = $this->getMock('Varien_Object', array('getPost', 'hasIsVirtual'));
+        $product = $this->getMock('Varien_Object', array('hasIsVirtual'));
+        $product->expects($this->any())->method('hasIsVirtual')->will($this->returnValue(true));
+
+        $request = $this->getMock('Varien_Object', array('getPost'));
         $request->expects($this->once())->method('getPost')->with($this->equalTo('downloadable'))
             ->will($this->returnValue($downloadableData));
-        $request->expects($this->any())->method('hasIsVirtual')->will($this->returnValue(true));
 
         $event = $this->getMock('Varien_Event', array('getRequest', 'getProduct'));
         $event->expects($this->once())->method('getRequest')->will($this->returnValue($request));
-        $event->expects($this->once())->method('getProduct')->will($this->returnValue($request));
+        $event->expects($this->once())->method('getProduct')->will($this->returnValue($product));
 
         $observer = $this->getMock('Varien_Event_Observer', array('getEvent'));
         $observer->expects($this->exactly(2))->method('getEvent')->will($this->returnValue($event));
 
         $this->_model->prepareProductSave($observer);
 
-        $this->assertEquals('downloadable', $request->getTypeId());
-        $data = $request->getDownloadableData();
-        $this->assertArrayHasKey('links', $data);
-        $this->assertEmpty($data['links']);
+        $this->assertEquals('downloadable', $product->getTypeId());
+        $this->assertEquals($downloadableData, $product->getDownloadableData());
     }
 }
