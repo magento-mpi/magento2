72103ee14a321abca91122cfed3b799dc7dc8200
Ignat Shcheglovskyi <gshcheglovsk@ebay.com>
2012-11-06 11:34:24 +0200

    Fixed MAGETWO-4562: Fix problem with loging actions for save/edit/delete roles and users autotests - fixed filter of ajax and isAjax parameters in MCA URL

--- framework/Mage/Selenium/TestCase.php
+++ framework/Mage/Selenium/TestCase.php
@@ -1789,7 +1789,9 @@ class Mage_Selenium_TestCase extends PHPUnit_Extensions_SeleniumTestCase
             //(when not using ajax to reload the page)
             $mca = preg_replace('|/filter/((\S)+)?/form_key/[A-Za-z0-9]+/?|', '/', $mca);
             //Delete secret key from url
-            $mca = preg_replace('|/(index/)?key/[A-Za-z0-9]+/?([?]ajax[=]true[&]isAjax[=]true)?|', '/', $mca);
+            $mca = preg_replace('|/(index/)?key/[A-Za-z0-9]+/?|', '/', $mca);
+            // Delete ajax and isAjax parameters from url
+            $mca = preg_replace(array('|(&?ajax=\w+)?(&?isAjax=\w+)?|', '|\?&|', '|\?$|'), array('', '?', ''), $mca);
             //Delete store view part of mca
             $mca = preg_replace('|/store/[A-Za-z0-9]+/?|', '/', $mca);
             //Delete action part of mca if it's index
