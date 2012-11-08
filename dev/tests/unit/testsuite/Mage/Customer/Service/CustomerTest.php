932500a094ce674bfd18cf72b6aa5c0ede964aa5
Dmitry Khrysev <dkhrysev@ebay.com>
2012-11-08 13:50:53 +0200

    Implemented MAGETWO-4567: Update layer for Customer - stub unit test added

--- dev/tests/unit/testsuite/Mage/Customer/Service/CustomerTest.php
+++ dev/tests/unit/testsuite/Mage/Customer/Service/CustomerTest.php
@@ -1,8 +1,50 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: dkhrysev
- * Date: 11/7/12
- * Time: 6:46 PM
- * To change this template use File | Settings | File Templates.
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Customer
+ * @subpackage  unit_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
  */
+
+class Mage_Customer_Service_CustomerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Mage_Customer_Service_Customer|PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_service;
+
+    protected function setUp()
+    {
+        $helper = $this->getMockBuilder('Mage_Customer_Helper_Data')
+            ->getMock();
+        $this->_service = new Mage_Customer_Service_Customer(array(
+            'helper' => $helper
+        ));
+    }
+
+    protected function tearDown()
+    {
+        unset($this->_service);
+    }
+
+    /**
+     * Test beforeSave and afterSave callback are set correctly
+     */
+    public function testSetBeforeSaveCallback()
+    {
+        $this->assertInstanceOf('Mage_Customer_Service_Customer', $this->_service->setBeforeSaveCallback('intval'));
+        $this->assertAttributeEquals('intval', '_beforeSaveCallback', $this->_service);
+    }
+
+    /**
+     * Test beforeSave and afterSave callback are set correctly
+     */
+    public function testSetAfterSaveCallback()
+    {
+        $this->assertInstanceOf('Mage_Customer_Service_Customer', $this->_service->setAfterSaveCallback('intval'));
+        $this->assertAttributeEquals('intval', '_afterSaveCallback', $this->_service);
+    }
+}
