2ae1162e1f692783009b1b20069daed40699da71
Alex Akimov <aakimov@ebay.com>
2012-11-08 16:41:38 +0200

    Implemented MAGETWO-4881: Create Landing Page Prototype

---
+++ app/code/core/Mage/Launcher/Block/LandingPage.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Launcher
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Landing Page Block
+ *
+ * @category   Mage
+ * @package    Mage_Launcher
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Launcher_Block_LandingPage extends Mage_Backend_Block_Abstract
+{
+}
