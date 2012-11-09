a04714e760d9f93d720af63f3f18688fa14f6770
Vladimir Korotovskih <vladimir.korotovskih@magento.com>
2012-11-09 18:19:24 +0200

    Fixed MAGETWO-4751: Convert Mage_Adminhtml_Block_Report_Grid - fixed static tests

--- app/code/core/Mage/Backend/Model/Widget/Grid/Totals.php
+++ app/code/core/Mage/Backend/Model/Widget/Grid/Totals.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Backend
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
 
 class Mage_Backend_Model_Widget_Grid_Totals extends Mage_Backend_Model_Widget_Grid_Totals_Abstract
 {
