da063b274446eff68d8726c65b711c86b8763c56
Kai Yan <kayan@x.com>
2012-11-13 10:05:38 -0600

    Implement MAGETWO-4480: refactor VarienForm using jQuery - convert account information page to use new validation widget - set-password widget to show and hide change password field based on change password checkbox checked or not

---
+++ app/code/core/Mage/Customer/view/frontend/set-password.js
@@ -0,0 +1,42 @@
+/**
+ * {license_notice}
+ *
+ * @category    mage customer view
+ * @package     mage
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/*jshint browser:true jquery:true expr:true*/
+(function ($) {
+    $.widget('mage.setPassword', {
+        _create: function() {
+            this.element.on('change', $.proxy(function(event) {
+                $(event.target).is(':checked') ? this._showPassword() : this._hidePassword();
+            }, this));
+            this.options.showOnDefault && this._showPassword();
+        },
+
+        /**
+         * Hide password input fields
+         * @private
+         */
+        _hidePassword: function() {
+            $(this.options.currentPasswordId).parents(this.options.passwordContainer).hide();
+            $(this.options.currentPasswordId).removeAttr('data-validate');
+            $(this.options.passwordId).removeAttr('data-validate');
+            $(this.options.confirmationId).removeAttr('data-validate');
+        },
+
+        /**
+         * Show password input fields
+         * @private
+         */
+        _showPassword: function() {
+            $(this.options.currentPasswordId).parents(this.options.passwordContainer).show();
+            $(this.options.currentPasswordId).attr('data-validate', '{required:true}');
+            $(this.options.passwordId).attr('data-validate', '{required:true, validatePassword:true}');
+            $(this.options.confirmationId).attr('data-validate',
+                '{required:true, equalTo:"' + this.options.passwordId + '"}');
+        }
+    });
+})(jQuery);
