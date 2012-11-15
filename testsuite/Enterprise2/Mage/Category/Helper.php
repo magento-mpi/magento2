bfc80c4c248e8be3b29e1be327e9deed2da09dbb
Evgeniy Maksimenko <emaksimenko@ebay.com>
2012-11-15 16:26:38 +0200

    Fixed fixtures after merge

--- testsuite/Enterprise2/Mage/Category/Helper.php
+++ testsuite/Enterprise2/Mage/Category/Helper.php
@@ -12,8 +12,12 @@
  */
 /**
  * Helper class
+ *
+ * @package     selenium
+ * @subpackage  tests
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Enterprise2_Mage_Category_Helper extends Community2_Mage_Category_Helper
+class Enterprise2_Mage_Category_Helper extends Core_Mage_Category_Helper
 {
     /**
      * Fill in Category information
