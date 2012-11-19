1b5d9279cdd563cc52e1dc8114ccd674ab107c18
Andrey Voinkov <avoinkov@ebay.com>
2012-11-19 18:47:19 +0200

    Fixed MAGETWO-5087: All Area Theme Registration - committed integration tests

--- dev/tests/integration/testsuite/Mage/Core/Model/Resource/Theme/CollectionTest.php
+++ dev/tests/integration/testsuite/Mage/Core/Model/Resource/Theme/CollectionTest.php
@@ -12,61 +12,144 @@
 class Mage_Core_Model_Resource_Theme_CollectionTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * @dataProvider themeList
+     * @return Mage_Core_Model_Resource_Theme_Collection
+     */
+    protected static function _getThemesCollection()
+    {
+        return  Mage::getObjectManager()->create('Mage_Core_Model_Resource_Theme_Collection');
+    }
+
+    /**
      * @magentoDbIsolation enabled
      */
-    public function testCollection($themeList)
+    public function testCollection()
     {
-        $themeCollection = Mage::getObjectManager()->create('Mage_Core_Model_Resource_Theme_Collection');
-        $themeCollection->load();
-        $oldTotalRecords = $themeCollection->getSize();
-        foreach ($themeList as $themeData) {
-            $themeModel = Mage::getObjectManager()->create('Mage_Core_Model_Theme');
-            $themeModel->setData($themeData);
-            $themeCollection->addItem($themeModel);
-        }
-        $themeCollection->save();
-        $themes = $themeCollection->toArray();
+        $oldTotalRecords = self::_getThemesCollection()->getSize();
 
-        $newThemeCollection = Mage::getObjectManager()->create('Mage_Core_Model_Resource_Theme_Collection');
+        $collection = $this->setThemeFixture();
+        $oldThemes = $collection->toArray();
+
+        $newThemeCollection = self::_getThemesCollection();
         $newThemes = $newThemeCollection->toArray();
 
-        $expectedTotalRecords = $oldTotalRecords + count($themeList);
+        $expectedTotalRecords = $oldTotalRecords + count(self::getThemeList());
         $this->assertEquals($expectedTotalRecords, $newThemes['totalRecords']);
-        $this->assertEquals($themes['items'], $newThemes['items']);
+        $this->assertEquals($oldThemes['items'], $newThemes['items']);
     }
 
     /**
-     * @dataProvider themeList
-     * @magentoDbIsolation enabled
+     * @param string $fullPath
+     * @param bool $shouldExist
+     * @magentoDataFixture setThemeFixture
+     * @magentoDataProvider getThemeByFullPathDataProvider
      */
-    public function testAddAreaFilter($themeList)
+    public function testGetThemeByFullPath($fullPath, $shouldExist)
     {
-        /** @var $themeCollection Mage_Core_Model_Resource_Theme_Collection */
-        $themeCollection = Mage::getObjectManager()->create('Mage_Core_Model_Resource_Theme_Collection');
+        $themeCollection = self::_getThemesCollection();
+        $hasFound = false;
+        /** @var $theme Mage_Core_Model_Theme */
+        foreach ($themeCollection as $theme) {
+            if ($theme->getFullPath() == $fullPath) {
+                $hasFound = true;
+                break;
+            }
+        }
+        $message = $shouldExist ? 'Theme not found' : 'Theme is found but it should not' ;
+        $this->assertEquals($shouldExist, $hasFound, $message);
+    }
+
+    public function getThemeByFullPathDataProvider()
+    {
+        return array(
+            array('test_area/test/default', true),
+            array('test_area2/test/pro', true),
+            array('test_area/test/pro', false),
+            array('test_area2/test/default', false),
+            array('', false),
+            array('test_area', false),
+            array('test_area/test', false),
+            array('test_area/test/something', false),
+        );
+    }
+
+    /**
+     * @magentoDataFixture setInheritedThemeFixture
+     */
+    public function testCheckParentInThemes()
+    {
+        $collection = self::_getThemesCollection()->checkParentInThemes();
+        foreach (self::getInheritedThemeList() as $themeData) {
+            $fullPath = $themeData['area'] . '/' . $themeData['theme_path'];
+            $parentIdActual = $collection->clear()->getThemeByFullPath($fullPath)->getParentId();
+            if ($themeData['parent_id']) {
+                $parentFullPath = trim($themeData['parent_id'], '{}');
+                $parentIdExpected = $collection->clear()->getThemeByFullPath($parentFullPath)->getId();
+                $this->assertEquals(
+                    $parentIdActual,
+                    $parentIdExpected,
+                    sprintf('Invalid parent_id for theme "%s"', $fullPath)
+                );
+            } else {
+                $parentIdExpected = 0;
+                $this->assertEquals(
+                    $parentIdExpected,
+                    $parentIdActual,
+                    sprintf('Parent id should be null for "%s"', $fullPath)
+                );
+            }
+        }
+    }
+
+    /**
+     * Set themes fixtures
+     *
+     * @return Mage_Core_Model_Resource_Theme_Collection
+     */
+    public static function setThemeFixture()
+    {
+        $themeCollection = self::_getThemesCollection();
         $themeCollection->load();
-        foreach ($themeList as $themeData) {
+        foreach (self::getThemeList() as $themeData) {
+            /** @var $themeModel Mage_Core_Model_Theme */
             $themeModel = Mage::getObjectManager()->create('Mage_Core_Model_Theme');
             $themeModel->setData($themeData);
             $themeCollection->addItem($themeModel);
         }
-        $themeCollection->save();
+        return $themeCollection->save();
+    }
 
-        /** @var $themeCollectionWithAreaFilter Mage_Core_Model_Resource_Theme_Collection */
-        $themeCollectionWithAreaFilter = Mage::getObjectManager()->create('Mage_Core_Model_Resource_Theme_Collection');
-        $themeCollectionWithAreaFilter->addAreaFilter('custom_area');
+    public static function setInheritedThemeFixture()
+    {
+        $fixture = self::getInheritedThemeList();
+        $idByPath = array();
+        foreach ($fixture as $themeData) {
+            /** @var $themeModel Mage_Core_Model_Theme */
+            $themeModel = Mage::getObjectManager()->create('Mage_Core_Model_Theme');
+            $themeModel->setData($themeData);
 
-        $this->assertEquals(count($themeList), count($themeCollectionWithAreaFilter));
+            //if ($themeModel->getFullPath() == 'test1/test1')
+
+            if ($themeData['parent_id'] && $themeData['parent_id'] == sprintf('{%s}', $themeModel->getFullPath())) {
+                if (isset($idByPath[$themeModel->getFullPath()])) {
+                    $themeModel->setParentId($idByPath[$themeModel->getFullPath()]);
+                } else {
+                    throw new Exception('Broken test, theme_id not found by full_name');
+                }
+            }
+            $themeModel->save();
+
+            $idByPath[$themeModel->getFullPath()] = $themeModel->getId();
+        }
     }
 
     /**
-     * Themes items
+     * Get themes for making fixture
      *
      * @return array
      */
-    public function themeList()
+    public static function getThemeList()
     {
-        return array(array(array(
+        return array(
             array(
                 'parent_id'            => '0',
                 'theme_path'           => 'test/default',
@@ -76,7 +159,7 @@ class Mage_Core_Model_Resource_Theme_CollectionTest extends PHPUnit_Framework_Te
                 'magento_version_from' => '2.0.0.0',
                 'magento_version_to'   => '*',
                 'is_featured'          => '1',
-                'area'                 => 'custom_area',
+                'area'                 => 'test_area'
             ),
             array(
                 'parent_id'            => '0',
@@ -87,8 +170,58 @@ class Mage_Core_Model_Resource_Theme_CollectionTest extends PHPUnit_Framework_Te
                 'magento_version_from' => '2.0.0.0',
                 'magento_version_to'   => '*',
                 'is_featured'          => '1',
-                'area'                 => 'custom_area',
+                'area'                 => 'test_area2'
             ),
-        )));
+        );
+    }
+
+    public static function getInheritedThemeList()
+    {
+        return array(
+            array(
+                'parent_id'            => '0',
+                'theme_path'           => 'test1/test1',
+                'theme_version'        => '2.0.0.0',
+                'theme_title'          => 'Test1',
+                'preview_image'        => 'test1_test1.jpg',
+                'magento_version_from' => '2.0.0.0',
+                'magento_version_to'   => '*',
+                'is_featured'          => '1',
+                'area'                 => 'area51'
+            ),
+            array(
+                'parent_id'            => '{area51/test1/test1}',
+                'theme_path'           => 'test1/test2',
+                'theme_version'        => '2.0.0.0',
+                'theme_title'          => 'Test2',
+                'preview_image'        => 'test1_test2.jpg',
+                'magento_version_from' => '2.0.0.0',
+                'magento_version_to'   => '*',
+                'is_featured'          => '1',
+                'area'                 => 'area51'
+            ),
+            array(
+                'parent_id'            => '{area51/test1/test2}',
+                'theme_path'           => 'test1/test3',
+                'theme_version'        => '2.0.0.0',
+                'theme_title'          => 'Test3',
+                'preview_image'        => 'test1_test3.jpg',
+                'magento_version_from' => '2.0.0.0',
+                'magento_version_to'   => '*',
+                'is_featured'          => '1',
+                'area'                 => 'area51'
+            ),
+            array(
+                'parent_id'            => '{area51/test1/test0}',
+                'theme_path'           => 'test1/test4',
+                'theme_version'        => '2.0.0.0',
+                'theme_title'          => 'Test4',
+                'preview_image'        => 'test1_test4.jpg',
+                'magento_version_from' => '2.0.0.0',
+                'magento_version_to'   => '*',
+                'is_featured'          => '1',
+                'area'                 => 'area51'
+            ),
+        );
     }
 }
