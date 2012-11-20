f34cdb43f4e54ef8981f95335fa362fe392f149b
Iekutenko Dmytro <diekutenko@ebay.com>
2012-11-20 15:34:54 +0200

    Implemented MAGETWO-5321 - Write js Tests for Form Widget - cover form widget with js unit tests - cover button widget with js unit tests

---
+++ dev/tests/js/testsuite/mage/button/button-test.js
@@ -0,0 +1,33 @@
+/**
+ * {license_notice}
+ *
+ * @category    mage.js
+ * @package     test
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+ButtonTest = TestCase('ButtonTest');
+ButtonTest.prototype.testInit = function() {
+    /*:DOC += <button id="test-button"></button><div id="event-target"></div>*/
+    var button = jQuery('#test-button').button();
+    assertEquals(true, button.is(':ui-button'));
+};
+ButtonTest.prototype.testProcessDataAttr = function() {
+    /*:DOC += <button id="test-button" data-widget-button="{&quot;event&quot;:&quot;testEvent&quot;,&quot;related&quot;:&quot;#event-target&quot;}"></button>
+        <div id="event-target"></div>*/
+    var button = jQuery('#test-button').button();
+    assertEquals('testEvent', button.button('option', 'event'));
+    assertEquals('#event-target', button.button('option', 'related'));
+};
+ButtonTest.prototype.testBind = function() {
+    /*:DOC += <button id="test-button" data-widget-button="{&quot;event&quot;:&quot;testEvent&quot;,&quot;related&quot;:&quot;#event-target&quot;}"></button>
+        <div id="event-target"></div>*/
+    var button = jQuery('#test-button').button(),
+        testEventTriggered = false;
+    jQuery('#event-target').on('testEvent', function(e) {
+        testEventTriggered = true;
+    });
+    button.click();
+
+    assertEquals(true, testEventTriggered);
+};
