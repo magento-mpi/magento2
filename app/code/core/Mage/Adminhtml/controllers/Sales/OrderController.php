84787d2f440327074aaa7eb262ee400b8bd89ff8
Maksym Iakusha <miakusha@ebay.com>
2012-11-21 14:18:09 +0200

    Fixed MAGETWO-3370: Order comment is not Posted without status change

--- app/code/core/Mage/Adminhtml/controllers/Sales/OrderController.php
+++ app/code/core/Mage/Adminhtml/controllers/Sales/OrderController.php
@@ -247,12 +247,17 @@ class Mage_Adminhtml_Sales_OrderController extends Mage_Adminhtml_Controller_Act
             try {
                 $response = false;
                 $data = $this->getRequest()->getPost('history');
+                if (empty($data['comment']) && ($data['status'] == $order->getDataByKey('status'))) {
+                    Mage::throwException($this->__('The Comment Text field cannot be empty.'));
+                }
+
                 $notify = isset($data['is_customer_notified']) ? $data['is_customer_notified'] : false;
                 $visible = isset($data['is_visible_on_front']) ? $data['is_visible_on_front'] : false;
 
-                $order->addStatusHistoryComment($data['comment'], $data['status'])
-                    ->setIsVisibleOnFront($visible)
-                    ->setIsCustomerNotified($notify);
+                $history = $order->addStatusHistoryComment($data['comment'], $data['status']);
+                $history->setIsVisibleOnFront($visible);
+                $history->setIsCustomerNotified($notify);
+                $history->save();
 
                 $comment = trim(strip_tags($data['comment']));
 
@@ -261,14 +266,12 @@ class Mage_Adminhtml_Sales_OrderController extends Mage_Adminhtml_Controller_Act
 
                 $this->loadLayout('empty');
                 $this->renderLayout();
-            }
-            catch (Mage_Core_Exception $e) {
+            } catch (Mage_Core_Exception $e) {
                 $response = array(
                     'error'     => true,
                     'message'   => $e->getMessage(),
                 );
-            }
-            catch (Exception $e) {
+            } catch (Exception $e) {
                 $response = array(
                     'error'     => true,
                     'message'   => $this->__('Cannot add order history.')
