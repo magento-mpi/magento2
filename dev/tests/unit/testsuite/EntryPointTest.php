caebdf68b128c4efb2de282a728b0799c8a8ffee
Olga Kopylova <okopylova@ebay.com>
2012-11-21 17:41:58 -0800

    Implemented MAGETWO-5027: Cover with Test(s) Multi-Tenant Entry Point - implemented unit test for the entry point - converted entry point script to EntryPoint callable class

---
+++ dev/tests/unit/testsuite/EntryPointTest.php
@@ -0,0 +1,72 @@
+<?php
+
+class EntryPointTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @param array $requestData
+     * @param array $inputConfig
+     * @param array $expectedConfig
+     *
+     * @dataProvider entryPointDataProvider
+     */
+    public function testEntryPoint($requestData = array(), $inputConfig = array(), $expectedConfig = array())
+    {
+        $config = $inputConfig;
+        $request = $this->getMock('Mage_Core_Controller_Request_Http', array('getServer'));
+        $request->expects($this->any())
+            ->method('getServer')
+            ->will($this->returnValue($requestData));
+
+        /** @var $app EntryPoint|PHPUnit_Framework_MockObject_MockObject */
+        $app = $this->getMock('EntryPoint', array('_run'), array($request));
+        $app->expects($this->once())
+            ->method('_run')
+            ->with($this->anything(), $this->anything(), $expectedConfig);
+
+        $app(serialize($config));
+    }
+
+    /**
+     * @return array
+     */
+    public function entryPointDataProvider()
+    {
+        return array(
+            'only request data' => array(
+                array(
+                    Mage_Core_Model_App_Options::OPTION_LOCAL_CONFIG_EXTRA_FILE => 'request_file',
+                ),
+                array(),
+                array(
+                    Mage_Core_Model_Config::OPTION_LOCAL_CONFIG_EXTRA_DATA => array(),
+                    Mage_Core_Model_Config::OPTION_LOCAL_CONFIG_EXTRA_FILE => 'request_file',
+                ),
+            ),
+            'only local data' => array(
+                array(),
+                array(
+                    'base_config' => array('base_opt' => 'local_base_value'),
+                    'options' => array(Mage_Core_Model_App_Options::OPTION_LOCAL_CONFIG_EXTRA_FILE => 'local_file'),
+                ),
+                array(
+                    Mage_Core_Model_Config::OPTION_LOCAL_CONFIG_EXTRA_DATA => array('base_opt' => 'local_base_value'),
+                    Mage_Core_Model_App_Options::OPTION_LOCAL_CONFIG_EXTRA_FILE => 'local_file',
+                ),
+            ),
+            'both request and local data' => array(
+                array(
+                    Mage_Core_Model_App_Options::OPTION_LOCAL_CONFIG_EXTRA_FILE => 'request_file',
+                ),
+                array(
+                    'base_config' => array('base_opt' => 'local_base_value'),
+                    'options' => array(Mage_Core_Model_Config::OPTION_LOCAL_CONFIG_EXTRA_FILE => 'local_file'
+                    ),
+                ),
+                array(
+                    Mage_Core_Model_Config::OPTION_LOCAL_CONFIG_EXTRA_DATA => array('base_opt' => 'local_base_value'),
+                    Mage_Core_Model_Config::OPTION_LOCAL_CONFIG_EXTRA_FILE => 'local_file',
+                ),
+            ),
+       );
+    }
+}
--- saas.php
+++ saas.php
@@ -18,16 +18,4 @@
  *
  * @param string $appConfigString
  */
-return function ($appConfigString) {
-    $appConfig = unserialize($appConfigString);
-
-    require_once __DIR__ . '/app/bootstrap.php';
-
-    $appOptions = new Mage_Core_Model_App_Options($_SERVER);
-    $appRunOptions = array_merge(
-        $appOptions->getRunOptions(),
-        array(Mage_Core_Model_Config::OPTION_LOCAL_CONFIG_EXTRA_DATA => $appConfig['base_config']),
-        $appConfig['options']
-    );
-    Mage::run($appOptions->getRunCode(), $appOptions->getRunType(), $appRunOptions);
-};
+return new EntryPoint;
