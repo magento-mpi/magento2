c4a998fad41f77c747f0f18652470c55ef03759f
Yaroslav Ershov <yershov@ebay.com>
2012-11-22 15:38:59 +0200

    Fixed MAGETWO-4910: Cover WebAPI Module with Tests   - Re-factored Webapi_Controller_Handler_ErrorProcessor class.   - Added isDeveloperMode method usage.   - Added renderJsonInDeveloperMode test.

--- dev/tests/unit/testsuite/Mage/Webapi/Controller/Handler/ErrorProcessorTest.php
+++ dev/tests/unit/testsuite/Mage/Webapi/Controller/Handler/ErrorProcessorTest.php
@@ -12,20 +12,26 @@ class Mage_Webapi_Controller_Handler_ErrorProcessorTest extends PHPUnit_Framewor
     /** @var Mage_Core_Helper_Data */
     protected $_helperMock;
 
+    /** @var Mage_Core_Model_App */
+    protected $_appMock;
+
     protected function setUp()
     {
         /** Set up mocks for SUT. */
         $this->_helperMock = $this->getMockBuilder('Mage_Core_Helper_Data')->getMock();
         $helperFactoryMock = $this->getMockBuilder('Mage_Core_Model_Factory_Helper')->getMock();
         $helperFactoryMock->expects($this->any())->method('get')->will($this->returnValue($this->_helperMock));
+        $this->_appMock = $this->getMockBuilder('Mage_Core_Model_App')->disableOriginalConstructor()->getMock();
         /** Initialize SUT. */
-        $this->_errorProcessor = new Mage_Webapi_Controller_Handler_ErrorProcessor($helperFactoryMock);
+        $this->_errorProcessor = new Mage_Webapi_Controller_Handler_ErrorProcessor($helperFactoryMock, $this->_appMock);
         parent::setUp();
     }
 
     protected function tearDown()
     {
         unset($this->_errorProcessor);
+        unset($this->_helperMock);
+        unset($this->_appMock);
         parent::tearDown();
     }
 
@@ -36,8 +42,48 @@ class Mage_Webapi_Controller_Handler_ErrorProcessorTest extends PHPUnit_Framewor
     {
         $_SERVER['HTTP_ACCEPT'] = 'json';
         /** Assert jsonEncode method will be executed once. */
-        $this->_helperMock->expects($this->once())->method('jsonEncode');
+        $this->_helperMock->expects($this->once())->method('jsonEncode')->will(
+            $this->returnCallback(array($this, 'callbackJsonEncode'), $this->returnArgument(0))
+        );
+        /** Init output buffering to catch output via echo function. */
+        ob_start();
         $this->_errorProcessor->render('Message');
+        /** Get output buffer. */
+        $actualResult = ob_get_flush();
+        $expectedResult = '{"messages":{"error":[{"code":500,"message":"Message"}]}}';
+        $this->assertEquals($expectedResult, $actualResult, 'Wrong rendering in Json.');
+    }
+
+    /**
+     * Callback function for RenderJson and RenderJsonInDeveloperMode tests.
+     *
+     * Method encode data to Json and return it.
+     *
+     * @param $data
+     * @return string
+     */
+    public function callbackJsonEncode($data)
+    {
+        return json_encode($data);
+    }
+
+    /**
+     * Test render method in Json format with turned on developer mode.
+     */
+    public function testRenderJsonInDeveloperMode()
+    {
+        $_SERVER['HTTP_ACCEPT'] = 'json';
+        /** Mock app to return enabled developer mode flag. */
+        $this->_appMock->expects($this->any())->method('isDeveloperMode')->will($this->returnValue(true));
+        /** Assert jsonEncode method will be executed once. */
+        $this->_helperMock->expects($this->once())->method('jsonEncode')->will(
+            $this->returnCallback(array($this, 'callbackJsonEncode'), $this->returnArgument(0))
+        );
+        ob_start();
+        $this->_errorProcessor->render('Message', 'Message trace.', 401);
+        $actualResult = ob_get_flush();
+        $expectedResult = '{"messages":{"error":[{"code":401,"message":"Message","trace":"Message trace."}]}}';
+        $this->assertEquals($expectedResult, $actualResult, 'Wrong rendering in Json.');
     }
 
     /**
@@ -62,16 +108,15 @@ class Mage_Webapi_Controller_Handler_ErrorProcessorTest extends PHPUnit_Framewor
     public function testRenderXmlInDeveloperMode()
     {
         $_SERVER['HTTP_ACCEPT'] = 'xml';
-        /** Turn on developer mode. */
-        Mage::setIsDeveloperMode(true);
+        /** Mock app to return enabled developer mode flag. */
+        $this->_appMock->expects($this->any())->method('isDeveloperMode')->will($this->returnValue(true));
         /** Init output buffering to catch output via echo function. */
         ob_start();
         $this->_errorProcessor->render('Message', 'Trace message.', 401);
         /** Get output buffer. */
         $actualResult = ob_get_flush();
-        $expectedResult = '<?xml version="1.0"?><magento_api><messages><error><data_item><code>401</code>'
-            . '<message>Message</message><trace><![CDATA[Trace message.]]></trace></data_item></error>'
-            . '</messages></magento_api>';
+        $expectedResult = '<?xml version="1.0"?><magento_api><messages><error><data_item><code>401</code><message>'
+            . 'Message</message><trace><![CDATA[Trace message.]]></trace></data_item></error></messages></magento_api>';
         $this->assertEquals($expectedResult, $actualResult, 'Wrong rendering in XML with turned on developer mode.');
     }
 
@@ -86,8 +131,7 @@ class Mage_Webapi_Controller_Handler_ErrorProcessorTest extends PHPUnit_Framewor
         $this->_errorProcessor->render('Message');
         /** Get output buffer. */
         $actualResult = ob_get_flush();
-        $expectedResult = 'messages%5Berror%5D%5B0%5D%5Bcode%5D=500&messages%5Berror%5D%5B0%5D%5Bmessage%5D=Message'
-            . '&messages%5Berror%5D%5B0%5D%5Btrace%5D=Trace+is+not+available.';
+        $expectedResult = 'messages%5Berror%5D%5B0%5D%5Bcode%5D=500&messages%5Berror%5D%5B0%5D%5Bmessage%5D=Message';
         $this->assertEquals($expectedResult, $actualResult, 'Wrong rendering in URL encode.');
     }
 
@@ -97,8 +141,8 @@ class Mage_Webapi_Controller_Handler_ErrorProcessorTest extends PHPUnit_Framewor
     public function testRenderUrlEncodeQueryInDeveloperMode()
     {
         $_SERVER['HTTP_ACCEPT'] = 'text/plain';
-        /** Turn on Developer mode. */
-        Mage::setIsDeveloperMode(true);
+        /** Mock app to return enabled developer mode flag. */
+        $this->_appMock->expects($this->any())->method('isDeveloperMode')->will($this->returnValue(true));
         /** Init output buffering to catch output via echo function. */
         ob_start();
         $this->_errorProcessor->render('Message', 'Trace message.', 401);
@@ -146,8 +190,8 @@ class Mage_Webapi_Controller_Handler_ErrorProcessorTest extends PHPUnit_Framewor
         $_SERVER['HTTP_ACCEPT'] = 'json';
         /** Init base Exception object. */
         $exception = new Exception('Message');
-        /** Turn on developer mode. */
-        Mage::setIsDeveloperMode(true);
+        /** Mock app to return enabled developer mode flag. */
+        $this->_appMock->expects($this->any())->method('isDeveloperMode')->will($this->returnValue(true));
         /** Assert jsonEncode will be executed once. */
         $this->_helperMock->expects($this->once())->method('jsonEncode');
         $this->_errorProcessor->renderException($exception);
