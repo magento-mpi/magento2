8149c8fcfbd28890c9a981f8f7cd57b2b72f6208
Alexey Korshenko <okorhsnko@ebay.com>
2012-11-23 17:10:38 +0200

    Implemented MAGETWO-5145: Field Dependencies System Configuration rendering Modification - implement dependencies mapper

--- app/code/core/Mage/Backend/Model/Config/Structure/Mapper/Dependencies.php
+++ app/code/core/Mage/Backend/Model/Config/Structure/Mapper/Dependencies.php
@@ -22,6 +22,84 @@ class Mage_Backend_Model_Config_Structure_Mapper_Dependencies
      */
     public function map(array $data)
     {
+        if ($this->_hasValue('config/system/sections', $data)) {
+            foreach ($data['config']['system']['sections'] as &$sectionConfig) {
+                $sectionConfig = $this->_processConfig($sectionConfig);
+            }
+        }
         return $data;
+    }
+
+    protected function _processConfig($groupConfig)
+    {
+        $groupConfig = $this->_processDepends($groupConfig);
+
+        if ($this->_hasValue('children', $groupConfig)) {
+            foreach ($groupConfig['children'] as &$fieldConfig) {
+                $fieldConfig = $this->_processConfig($fieldConfig);
+            }
+        }
+        return $groupConfig;
+    }
+
+    protected function _processDepends($groupConfig)
+    {
+        if ($this->_hasValue('depends/fields', $groupConfig)) {
+            foreach ($groupConfig['depends']['fields'] as &$field) {
+                $dependPath = $this->_getDependPath($field, $groupConfig);
+                $field['dependPath'] = $dependPath;
+                $field['id'] = implode('/', $dependPath);
+            }
+        }
+        return $groupConfig;
+    }
+
+    /**
+     * Check value existence
+     *
+     * @param string $key
+     * @param array $target
+     * @return bool
+     */
+    protected function _hasValue($key, array $target)
+    {
+        $paths = explode('/', $key);
+        foreach ($paths as $path) {
+            if (array_key_exists($path, $target)) {
+                $target = $target[$path];
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Get depend path
+     *
+     * @param array $field
+     * @param array $config
+     * @return array
+     * @throws InvalidArgumentException
+     */
+    protected function _getDependPath($field, $config)
+    {
+        $dependPath = $field['id'];
+        $elementPath = $config['path'] . '/' . $config['id'];
+
+        $dependPathParts = explode('/', $dependPath);
+        $elementPathParts = explode('/', $elementPath);
+        $output = array();
+        foreach ($dependPathParts as $index => $path) {
+
+            if ($path === '*') {
+                if (false == array_key_exists($index, $elementPathParts)) {
+                    throw new InvalidArgumentException('Invalid relative depends structure');
+                }
+                $path = $elementPathParts[$index];
+            }
+            $output[$index] = $path;
+        }
+        return $output;
     }
 }
