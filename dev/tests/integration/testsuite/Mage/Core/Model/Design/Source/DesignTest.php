bca69f8bd687f6d03692b033a1663a3522229b18
Maxim Shikula <mshikula@ebay.com>
2012-11-23 12:59:13 +0200

    Fixed MAGETWO-4929: Fix Broken Tests After Virtual Theme implementation (Part 2)

--- dev/tests/integration/testsuite/Mage/Core/Model/Design/Source/DesignTest.php
+++ dev/tests/integration/testsuite/Mage/Core/Model/Design/Source/DesignTest.php
@@ -14,63 +14,18 @@ class Mage_Core_Model_Design_Source_DesignTest extends PHPUnit_Framework_TestCas
     public function testGetAllOptions()
     {
         /** @var $model Mage_Core_Model_Design_Source_Design */
-        $model = Mage::getModel('Mage_Core_Model_Design_Source_Design');
-        $expectedLabelCollection = $this->_getLabelCollection();
-        $labelCollection = $model->getAllOptions(true);
+        $model = Mage::getObjectManager()->create('Mage_Core_Model_Design_Source_Design');
 
-        foreach ($labelCollection as $key => $data) {
-            $expectedValue = $expectedLabelCollection[$key];
+        /** @var $expectedLabelCollection Mage_Core_Model_Theme_Collection */
+        $expectedLabelCollection = Mage::getModel('Mage_Core_Model_Resource_Theme_Collection');
+        $expectedLabelCollection->addFilter('area', 'frontend');
 
-            $this->assertArrayHasKey('label', $data);
-            $this->assertArrayHasKey('value', $data);
-            $this->assertEquals($expectedValue['label'], $data['label']);
-        }
-    }
+        $expectedItemsCount = count($expectedLabelCollection);
 
-    /**
-     * Return sorted by title themes
-     *
-     * @return array
-     */
-    protected function _getLabelCollection()
-    {
-        return array(
-            array(
-                'value' => '',
-                'label' => '-- Please Select --'
-            ),
-            array(
-                'value' => '%d',
-                'label' => 'Magento Blank'
-            ),
-            array(
-                'value' => '%d',
-                'label' => 'Magento Demo'
-            ),
-            array(
-                'value' => '%d',
-                'label' => 'Magento Demo Blue'
-            ),
-            array(
-                'value' => '%d',
-                'label' => 'Magento Fixed Design'
-            ),
-            array(
-                'value' => '%d',
-                'label' => 'Magento Fluid Design (incompatible version)'
-            ),
-            array(
-                'value' => '%d',
-                'label' => 'Magento Iphone'
-            ),
-            array(
-                'value' => '%d',
-                'label' => 'Magento Iphone (HTML5)'
-            ),
-            array(
-                'value' => '%d',
-                'label' => 'Magento Modern'
-            )
-        );
+        $labelsCollection = $model->getAllOptions(false);
+        $this->assertEquals($expectedItemsCount, count($labelsCollection));
+
+        $labelsCollection = $model->getAllOptions(true);
+        $this->assertEquals(++$expectedItemsCount, count($labelsCollection));
     }
 }
