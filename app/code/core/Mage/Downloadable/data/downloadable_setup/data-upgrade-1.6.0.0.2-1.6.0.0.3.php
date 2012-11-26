a61838e70d7fb1b16b395068605f917e86a0cc86
Igor Tripolskiy <itripolskiy@ebay.com>
2012-11-26 19:39:39 +0200

    Fixed MAGETWO-5472: Reference to downloadable product type in Mage_Catalog data upgrade

--- app/code/core/Mage/Downloadable/data/downloadable_setup/data-upgrade-1.6.0.0.2-1.6.0.0.3.php
+++ app/code/core/Mage/Downloadable/data/downloadable_setup/data-upgrade-1.6.0.0.2-1.6.0.0.3.php
@@ -3,21 +3,14 @@
  * {license_notice}
  *
  * @category    Mage
- * @package     Mage_Catalog
+ * @package     Mage_Downloadable
  * @copyright   {copyright}
  * @license     {license_link}
  */
 
-/** @var $this Mage_Catalog_Model_Resource_Setup */
 $applyTo = array_merge(
     explode(',', $this->getAttribute(Mage_Catalog_Model_Product::ENTITY, 'weight', 'apply_to')),
-    array('virtual', 'configurable')
+    array('downloadable')
 );
 
-$this->updateAttribute(
-    Mage_Catalog_Model_Product::ENTITY,
-    'weight',
-    'frontend_input_renderer',
-    'Mage_Adminhtml_Block_Catalog_Product_Helper_Form_Weight_Renderer'
-);
 $this->updateAttribute(Mage_Catalog_Model_Product::ENTITY, 'weight', 'apply_to', implode(',', $applyTo));
