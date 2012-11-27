312fdf06b9a58006bfb05b573f59e3abd291d54f
Dmytro Aponasenko <daponasenko@ebay.com>
2012-11-27 09:26:42 +0200

    Implemented MAGETWO-5277: Assignment of existing simple product to configurable via grid

---
+++ dev/tests/unit/testsuite/Mage/Catalog/Model/Resource/Product/Collection/AssociatedProductUpdaterTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Catalog
+ * @subpackage  unit_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Mage_Catalog_Model_Resource_Product_Collection_AssociatedProductUpdaterTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test adding filtration by qty and stock availability to collection
+     */
+    public function testUpdate()
+    {
+        $inventory = array(
+            'qty' => 'qty',
+            'inventory_in_stock' => 'is_in_stock'
+        );
+        $argument = $this->getMockBuilder('Varien_Data_Collection_Db')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $stockItem = $this->getMockBuilder('Mage_CatalogInventory_Model_Resource_Stock_Item')
+            ->disableOriginalConstructor()
+            ->setMethods(array('addCatalogInventoryToProductCollection'))
+            ->getMock();
+        $stockItem->expects($this->any())
+            ->method('addCatalogInventoryToProductCollection')
+            ->with($argument, $inventory);
+
+        $model = new Mage_Catalog_Model_Resource_Product_Collection_AssociatedProductUpdater($stockItem);
+        $model->update($argument);
+    }
+}
