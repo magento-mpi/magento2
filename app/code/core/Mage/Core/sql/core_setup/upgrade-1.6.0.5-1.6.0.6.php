69887b38080c4d9125739006d9e7033deabdfb9e
yurii.torbyk <itorbyk@ebay.com>
2012-11-27 14:48:16 +0200

    Fixed MAGETWO-4947: Fix Broken Tests After Virtual Theme implementation (Part 3) - fix upgrade scripts

--- app/code/core/Mage/Core/sql/core_setup/upgrade-1.6.0.5-1.6.0.6.php
+++ app/code/core/Mage/Core/sql/core_setup/upgrade-1.6.0.5-1.6.0.6.php
@@ -13,31 +13,35 @@ $installer = $this;
 
 $installer->startSetup();
 $connection = $installer->getConnection();
-$table = $installer->getTable('core_layout_link');
+
+/**
+ * Modifying 'core_layout_link' table. Replace columns area, package, theme to theme_id
+ */
+$tableCoreLayoutLink = $installer->getTable('core_layout_link');
 
 $connection->dropForeignKey(
-    $table,
+    $tableCoreLayoutLink,
     $installer->getFkName('core_layout_link', 'store_id', 'core_store', 'store_id')
 );
 
-$connection->dropIndex($table, $installer->getIdxName(
+$connection->dropIndex($tableCoreLayoutLink, $installer->getIdxName(
     'core_layout_link',
     array('store_id', 'package', 'theme', 'layout_update_id'),
     Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
 ));
 
-$connection->dropColumn($table, 'area');
+$connection->dropColumn($tableCoreLayoutLink, 'area');
 
-$connection->dropColumn($table, 'package');
+$connection->dropColumn($tableCoreLayoutLink, 'package');
 
-$connection->changeColumn($table, 'theme', 'theme_id', array(
+$connection->changeColumn($tableCoreLayoutLink, 'theme', 'theme_id', array(
     'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
     'unsigned' => true,
     'nullable' => false,
     'comment'  => 'Theme id'
 ));
 
-$connection->addIndex($table, $installer->getIdxName(
+$connection->addIndex($tableCoreLayoutLink, $installer->getIdxName(
     'core_layout_link',
     array('store_id', 'theme_id', 'layout_update_id'),
     Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
@@ -45,7 +49,7 @@ $connection->addIndex($table, $installer->getIdxName(
 
 $connection->addForeignKey(
     $installer->getFkName('core_layout_link', 'store_id', 'core_store', 'store_id'),
-    $table,
+    $tableCoreLayoutLink,
     'store_id',
     $installer->getTable('core_store'),
     'store_id',
@@ -55,12 +59,22 @@ $connection->addForeignKey(
 
 $connection->addForeignKey(
     $installer->getFkName('core_layout_link', 'theme_id', 'core_theme', 'theme_id'),
-    $table,
+    $tableCoreLayoutLink,
     'theme_id',
     $installer->getTable('core_theme'),
     'theme_id',
     Varien_Db_Ddl_Table::ACTION_CASCADE,
     Varien_Db_Ddl_Table::ACTION_CASCADE
 );
+
+/**
+ * Add column 'area' to 'core_theme'
+ */
+$connection->addColumn($installer->getTable('core_theme'), 'area', array(
+    'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+    'length'   => '255',
+    'nullable' => false,
+    'comment'  => 'Theme Area'
+));
 
 $installer->endSetup();
