db6e3936e2c51f4f30b2e4b80a9a661b9f99ba46
Vlad Veselov <vveselov@ebay.com>
2012-11-27 17:41:44 +0200

    Fixed MAGETWO-5276: Table representing product variations matrix, combinations generation - too long variable name as for static test

--- dev/tests/integration/testsuite/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Super/ConfigTest.php
+++ dev/tests/integration/testsuite/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Super/ConfigTest.php
@@ -30,14 +30,14 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_ConfigTest extends PHP
     {
         Mage::register('current_product', Mage::getModel('Mage_Catalog_Model_Product')->load(1));
         Mage::app()->getLayout()->createBlock('Mage_Core_Block_Text', 'head');
-        $configurableAttribute = Mage::getSingleton('Mage_Catalog_Model_Entity_Attribute')->loadByCode(
+        $usedAttribute = Mage::getSingleton('Mage_Catalog_Model_Entity_Attribute')->loadByCode(
             Mage::getSingleton('Mage_Eav_Model_Config')->getEntityType('catalog_product')->getId(),
             'test_configurable'
         );
         /** @var $block Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config */
         $block = Mage::app()->getLayout()->createBlock('Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config');
         $selectedAttributes = $block->getSelectedAttributes();
-        $this->assertEquals(array($configurableAttribute->getId()), array_keys($selectedAttributes));
+        $this->assertEquals(array($usedAttribute->getId()), array_keys($selectedAttributes));
         $selectedAttribute = reset($selectedAttributes);
         $this->assertEquals('test_configurable', $selectedAttribute->getAttributeCode());
     }
@@ -50,18 +50,18 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_ConfigTest extends PHP
     {
         Mage::register('current_product', Mage::getModel('Mage_Catalog_Model_Product')->load(1));
         Mage::app()->getLayout()->createBlock('Mage_Core_Block_Text', 'head');
-        /** @var $configurableAttribute Mage_Catalog_Model_Entity_Attribute */
-        $configurableAttribute = Mage::getSingleton('Mage_Catalog_Model_Entity_Attribute')->loadByCode(
+        /** @var $usedAttribute Mage_Catalog_Model_Entity_Attribute */
+        $usedAttribute = Mage::getSingleton('Mage_Catalog_Model_Entity_Attribute')->loadByCode(
             Mage::getSingleton('Mage_Eav_Model_Config')->getEntityType('catalog_product')->getId(),
             'test_configurable'
         );
-        $attributeOptions = $configurableAttribute->getSource()->getAllOptions(false);
+        $attributeOptions = $usedAttribute->getSource()->getAllOptions(false);
         /** @var $block Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config */
         $block = Mage::app()->getLayout()->createBlock('Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config');
         $this->assertEquals(
             array(
-                array($configurableAttribute->getId() => $attributeOptions[0]),
-                array($configurableAttribute->getId() => $attributeOptions[1]),
+                array($usedAttribute->getId() => $attributeOptions[0]),
+                array($usedAttribute->getId() => $attributeOptions[1]),
             ),
             $block->getVariations()
         );
