43f788e9521c24675007fb5f4efa980e91ca1857
Alex Bomko <alex.bomko@magento.com>
2012-11-28 19:01:58 +0200

    Implemented MAGETWO-5642: Create Controller Action Interface for Drawer Controllers

---
+++ app/code/core/Mage/Launcher/Controller/Drawer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Launcher
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Drawer controller interface
+ *
+ * This interface has to be implemented by all drawer controllers
+ *
+ * @category    Mage
+ * @package     Mage_Launcher
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+interface Mage_Launcher_Controller_Drawer
+{
+    /**
+     * Drawer Save Action
+     */
+    public function saveAction();
+
+    /**
+     * Retrieve Drawer Content Action
+     */
+    public function getContentAction();
+}
