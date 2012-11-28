d86dabae7c0b8f47e6d9a22fb2113c414fa890ca
Andrey Tserkus <andrey.tserkus@magento.com>
2012-11-28 15:23:00 -0800

    Fixed MAGETWO-5598: Master-Voodoo Synchronization 48 - added missing license placeholder to the file, created in scope of MAGETWO-5027. Thus now we can have green static tests build (successful license replacement task).

--- dev/tests/unit/testsuite/EntryPointTest.php
+++ dev/tests/unit/testsuite/EntryPointTest.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Core
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
 
 class EntryPointTest extends PHPUnit_Framework_TestCase
 {
