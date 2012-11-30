f41d5e3e42090fdceab30b149656871229612bb8
Roman Grebenchuk <rgrebenchuk@ebay.com>
2012-11-30 19:58:45 +0200

    Implemented MAGETWO-5553: Create router with possibility to run frontend with 'vde' area code   - added unit test for DesignEditor_Helper_Data

---
+++ dev/tests/unit/testsuite/Mage/DesignEditor/Helper/DataTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_DesignEditor
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Mage_DesignEditor_Helper_DataTest extends PHPUnit_Framework_TestCase
+{
+    /** @var Mage_DesignEditor_Helper_Data */
+    protected $_helper;
+
+    public function setUp()
+    {
+        $this->_helper = new Mage_DesignEditor_Helper_Data();
+    }
+
+    protected function tearDown()
+    {
+        unset($this->_helper);
+    }
+
+    /**
+     * @dataProvider vdeRequestDataProvider
+     * @param string $url
+     * @param bool $expectedResult
+     */
+    public function testIsVdeRequest($url, $expectedResult)
+    {
+        /** @var $request Mage_Core_Controller_Request_Http */
+        $request = $this->getMock('Mage_Core_Controller_Request_Http',
+            array('getOriginalPathInfo'), array(), '', false
+        );
+        $request->expects($this->once())
+            ->method('getOriginalPathInfo')
+            ->will($this->returnValue($url));
+
+        $this->assertEquals($expectedResult, $this->_helper->isVdeRequest($request));
+    }
+
+    /**
+     * Data Provider for IsVdeRequest method
+     */
+    public function vdeRequestDataProvider()
+    {
+        return array(
+            'plane vde prefix' => array(
+                'url' => Mage_DesignEditor_Helper_Data::FRONT_NAME,
+                'expected result' => true
+            ),
+            'vde prefix with slashes' => array(
+                'url' => '/'. Mage_DesignEditor_Helper_Data::FRONT_NAME . '/',
+                'expected result' => true
+            ),
+            'vde prefix with action' => array(
+                'url' => Mage_DesignEditor_Helper_Data::FRONT_NAME . '/run',
+                'expected result' => true
+            ),
+            'not valid vde prefix' => array(
+                'url' => Mage_Core_Model_App_Area::AREA_FRONTEND . '/' . Mage_DesignEditor_Helper_Data::FRONT_NAME,
+                'expected result' => false
+            )
+        );
+    }
+}
