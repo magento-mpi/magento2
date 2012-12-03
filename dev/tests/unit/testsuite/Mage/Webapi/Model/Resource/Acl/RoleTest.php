67d1508a66a5617b9c48da2b20aecf27d711a788
Alex Paliarush <apaliarush@ebay.com>
2012-12-03 15:28:03 +0200

    Implemented MAGETWO-5387: Merge API to Master - copy-paste fix in tests to pass Static tests

--- dev/tests/unit/testsuite/Mage/Webapi/Model/Resource/Acl/RoleTest.php
+++ dev/tests/unit/testsuite/Mage/Webapi/Model/Resource/Acl/RoleTest.php
@@ -7,49 +7,8 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-class Mage_Webapi_Model_Resource_Acl_RoleTest extends PHPUnit_Framework_TestCase
+class Mage_Webapi_Model_Resource_Acl_RoleTest extends Mage_Webapi_Model_Resource_Acl_TestAbstract
 {
-    /**
-     * @var Magento_Test_Helper_ObjectManager
-     */
-    protected $_helper;
-
-    /**
-     * @var Mage_Webapi_Helper_Data
-     */
-    protected $_helperData;
-
-    /**
-     * @var Magento_ObjectManager|PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $_objectManager;
-
-    /**
-     * @var Mage_Core_Model_Resource|PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $_resource;
-
-    /**
-     * @var Varien_Db_Adapter_Pdo_Mysql|PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $_adapter;
-
-    protected function setUp()
-    {
-        $this->_helper = new Magento_Test_Helper_ObjectManager($this);
-
-        $this->_helperData = $this->getMockBuilder('Mage_Webapi_Helper_Data')
-            ->disableOriginalConstructor()
-            ->setMethods(array('__'))
-            ->getMock();
-        $this->_helperData->expects($this->any())->method('__')->will($this->returnArgument(0));
-
-        $this->_objectManager = $this->getMockBuilder('Magento_ObjectManager')
-            ->disableOriginalConstructor()
-            ->setMethods(array('create'))
-            ->getMockForAbstractClass();
-    }
-
     /**
      * Create resource model
      *
