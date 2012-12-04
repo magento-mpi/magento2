5d11d7c524446a29d005188e926a880d47d4b36e
Iuliia Babenko <ibabenko@ebay.com>
2012-12-04 15:49:31 +0200

    Implemented MAGETWO-5549: TAF for UI validation - added VDE uimaps, functional test

---
+++ dev/tests/functional/testsuite/Community2/Mage/Vde/UiValidationTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento
+ * @subpackage  functional_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+class Community2_Mage_Vde_UiValidationTest extends Mage_Selenium_TestCase
+{
+    protected function assertPreConditions()
+    {
+        $this->loginAdminUser();
+    }
+
+    /**
+     * @test
+     * @TestlinkId TL-MAGE-6501
+     */
+    public function uiValidationTest()
+    {
+        $this->admin('vde_design');
+        $this->assertTrue($this->controlIsPresent('dropdown', 'page_selector'),
+            'Page selector is not present on the page');
+        $this->assertTrue($this->controlIsPresent('dropdown', 'view_options'),
+            'View options dropdown is not present on the page');
+        $this->assertTrue($this->buttonIsPresent('view_layout'), 'View Layout button is not present on the page');
+        $this->assertTrue($this->buttonIsPresent('compact_log'), 'Compact Log button is not present on the page');
+        $this->assertTrue($this->buttonIsPresent('quit'), 'Quit button is not present on the page');
+        $this->assertTrue($this->controlIsPresent('fieldset', 'iframe'), 'iFrame is not present on the page');
+    }
+}
