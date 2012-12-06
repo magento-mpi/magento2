38a64bf227cd1164dbb2b3ead00d5e3e4c35d524
Kai Yan <kayan@x.com>
2012-12-06 14:12:28 -0600

    Implement MAGETWO-5308: Refactor Customer component - refactor customer login and register to use frontend validation widget - implement date of birth validation rule

---
+++ pub/lib/mage/validation/dob-rule.js
@@ -0,0 +1,53 @@
+/**
+ * {license_notice}
+ *
+ * @category    validation - dob rule
+ * @package     mage
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/*jshint jquery:true*/
+(function($) {
+    "use strict";
+    $.validator.addMethod(
+        'validate-dob',
+        function (val, element, params) {
+            $('.customer-dob').find('.' + this.settings.errorClass).removeClass(this.settings.errorClass);
+            var dayVal = $(params[0]).find('input:text').val(),
+                monthVal = $(params[1]).find('input:text').val(),
+                yearVal = $(params[2]).find('input:text').val();
+            if (params[3] && dayVal.length + monthVal.length + yearVal.length === 0) {
+                this.dobErrorMessage = 'This is a required field.';
+                return false;
+            }
+            var day = parseInt(dayVal, 10) || 0,
+                month = parseInt(monthVal, 10) || 0,
+                year = parseInt(yearVal, 10) || 0,
+                curYear = (new Date()).getFullYear();
+            if (!day || !month || !year) {
+                this.dobErrorMessage = 'Please enter a valid full date.';
+                return false;
+            }
+            if (month < 1 || month > 12) {
+                this.dobErrorMessage = 'Please enter a valid month (1-12).';
+                return false;
+            }
+            if (year < 1900 || year > curYear) {
+                this.dobErrorMessage = 'Please enter a valid year (1900-%d).'.replace('%d', curYear);
+                return false;
+            }
+            var validateDayInMonth = new Date(year, month, 0).getDate();
+            if (day < 1 || day > validateDayInMonth) {
+                this.dobErrorMessage = 'Please enter a valid day (1-%d).'.replace('%d', validateDayInMonth);
+                return false;
+            }
+            day = day % 10 === day ? '0' + day : day;
+            month = month % 10 === month ? '0' + month : month;
+            $(element).val(month + '/' + day + '/' + year);
+            return true;
+        },
+        function(){
+            return this.dobErrorMessage;
+        }
+    );
+})(jQuery);
