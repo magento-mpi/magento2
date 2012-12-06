cb77fb5d33462bafe06ab16aa23dbaed80ccbc15
Maxim Shikula <mshikula@ebay.com>
2012-12-06 19:21:42 +0200

    Cr-change MAGETWO-5619: Implement Theme Controls - Unit test

--- dev/tests/unit/testsuite/Mage/DesignEditor/Block/Adminhtml/Theme/Selector/List/AbstractTest.php
+++ dev/tests/unit/testsuite/Mage/DesignEditor/Block/Adminhtml/Theme/Selector/List/AbstractTest.php
@@ -22,13 +22,13 @@ class Mage_DesignEditor_Block_Adminhtml_Theme_Selector_List_AbstractTest
             array('getChildBlock')
         );
 
-        $itemBlock =  $this->getMock('Mage_DesignEditor_Block_Adminhtml_Theme', array(), array(), '', false);
+        $themeMock = $this->getMock('Mage_DesignEditor_Block_Adminhtml_Theme', array(), array(), '', false);
 
         $listAbstractBlock->setCollection($collection);
 
         $listAbstractBlock->expects($this->atLeastOnce())
             ->method('getChildBlock')
-            ->will($this->returnValue($itemBlock));
+            ->will($this->returnValue($themeMock));
 
         $this->assertEquals(2, count($listAbstractBlock->getListItems()));
     }
@@ -42,5 +42,44 @@ class Mage_DesignEditor_Block_Adminhtml_Theme_Selector_List_AbstractTest
             array('first_item'),
             array('second_item')
         )));
+    }
+
+    public function testAddAssignButtonHtml()
+    {
+        /** @var $listAbstractBlock Mage_DesignEditor_Block_Adminhtml_Theme_Selector_List_Abstract */
+        $listAbstractBlock = $this->getMockForAbstractClass(
+            'Mage_DesignEditor_Block_Adminhtml_Theme_Selector_List_Abstract', array(), '', false, false, true,
+            array('getChildBlock', 'getLayout', '__')
+        );
+        /** @var $themeMock Mage_Core_Model_Theme */
+        $themeMock = $this->getMock('Mage_Core_Model_Theme', array(), array(), '', false);
+        /** @var $themeBlockMock Mage_DesignEditor_Block_Adminhtml_Theme */
+        $themeBlockMock = $this->getMock(
+            'Mage_DesignEditor_Block_Adminhtml_Theme', array('getTheme'), array(), '', false
+        );
+        /** @var $layoutMock Mage_Core_Model_Layout */
+        $layoutMock  = $this->getMock('Mage_Core_Model_Layout', array('createBlock'), array(), '', false);
+        /** @var $buttonMock Mage_Backend_Block_Widget_Button */
+        $buttonMock = $this->getMock('Mage_Backend_Block_Widget_Button', array(), array(), '', false);
+
+        $layoutMock->expects($this->once())
+            ->method('createBlock')
+            ->will($this->returnValue($buttonMock));
+
+        $themeBlockMock->expects($this->once())
+            ->method('getTheme')
+            ->will($this->returnValue($themeMock));
+
+        $listAbstractBlock->expects($this->once())
+            ->method('getLayout')
+            ->will($this->returnValue($layoutMock));
+
+        $themeMock->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue(1));
+
+        $method = new ReflectionMethod($listAbstractBlock, '_addAssignButtonHtml');
+        $method->setAccessible(true);
+        $method->invoke($listAbstractBlock, $themeBlockMock);
     }
 }
