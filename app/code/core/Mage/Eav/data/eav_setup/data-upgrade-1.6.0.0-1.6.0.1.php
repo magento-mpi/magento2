b746ad6d6f610e80b8583eee6945a79b74335710
Andrew Zavadsky <azavadsky@ebay.com>
2012-12-12 13:03:57 +0200

    CR changes MAGETWO-4977: "Gender" Customer Attribute Options are duplicated - deleted unnecessary logic - created main map file with migration aliases info

--- app/code/core/Mage/Eav/data/eav_setup/data-upgrade-1.6.0.0-1.6.0.1.php
+++ app/code/core/Mage/Eav/data/eav_setup/data-upgrade-1.6.0.0-1.6.0.1.php
@@ -8,8 +8,8 @@
  * @license     {license_link}
  */
 
-/** @var $installer Mage_Eav_Model_Resource_Setup_Migration */
-$installer = Mage::getResourceModel('Mage_Eav_Model_Resource_Setup_Migration',
+/** @var $installer Mage_Core_Model_Resource_Setup_Migration */
+$installer = Mage::getResourceModel('Mage_Core_Model_Resource_Setup_Migration',
     array('resourceName' => 'core_setup'));
 $installer->startSetup();
 
