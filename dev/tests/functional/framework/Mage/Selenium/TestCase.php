4af28ac005fcda2648983c8c86d386869ed0a8f7
Artem Shugayev <ashugayev@ebay.com>
2012-12-13 11:43:12 +0200

    Removed Staging from static tests

--- dev/tests/functional/framework/Mage/Selenium/TestCase.php
+++ dev/tests/functional/framework/Mage/Selenium/TestCase.php
@@ -69,7 +69,6 @@
  * @method Enterprise_Mage_ImportExportScheduled_Helper                                                importExportScheduledHelper()
  * @method Enterprise_Mage_Rma_Helper                                                                  rmaHelper()
  * @method Enterprise_Mage_Rollback_Helper                                                             rollbackHelper()
- * @method Enterprise_Mage_StagingWebsite_Helper                                                       stagingWebsiteHelper()
  * @method Enterprise_Mage_WebsiteRestrictions_Helper                                                  websiteRestrictionsHelper()
  * @method Core_Mage_Grid_Helper                                                                       gridHelper()
  */
