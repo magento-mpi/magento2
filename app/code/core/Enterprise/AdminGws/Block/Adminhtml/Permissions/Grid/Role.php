537d72ccdbf7a5dea91a808bf0f2a80e3e71de8b
Iryna Lagno <ilagno@ebay.com>
2012-12-17 15:29:31 +0200

    Fix MAGETWO-6144 after CR.  - add extending Enterprise_AdminGws_Block_Adminhtml_Permissions_Grid_Role from proper class

--- app/code/core/Enterprise/AdminGws/Block/Adminhtml/Permissions/Grid/Role.php
+++ app/code/core/Enterprise/AdminGws/Block/Adminhtml/Permissions/Grid/Role.php
@@ -12,7 +12,7 @@
  * Admin roles grid
  *
  */
-class Enterprise_AdminGws_Block_Adminhtml_Permissions_Grid_Role extends Mage_Backend_Block_Widget_Grid_Column
+class Enterprise_AdminGws_Block_Adminhtml_Permissions_Grid_Role extends Mage_Backend_Block_Widget_Grid
 {
     /**
      * Add allowed websites/stores column
