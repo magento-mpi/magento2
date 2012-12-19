b4d1784fb7903bbb0468b7cd2d8bf6487775f612
Igor Krasnykh <igor@x.com>
2012-12-19 10:50:56 -0600

    Implemented MAGETWO-6322: Refactor Tabs component - Implemented code review comments

---
+++ app/code/core/Mage/Page/view/frontend/js/global.js
@@ -0,0 +1,29 @@
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Page
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+// Temporary solution, will be replaced when plug-in "mage" will be merged to master
+(function($){
+	"use strict";
+	var bootstrap = function() {
+		$('[data-mage-init]').each(function(){
+			var inits = $(this).data('mage-init') || {};
+			// in case it's not well-formed JSON inside data attribute, evaluate it manually
+			if (typeof inits === 'string') {
+				try {
+					inits = eval('(' + inits + ')');
+				} catch (e) {
+					inits = {};
+				}
+			}
+			$.each(inits, $.proxy(function(key, args){
+				$(this)[key].apply($(this), $.makeArray(args));
+			}, this));
+		});
+	};
+	$(document).ready(bootstrap);
+})(jQuery);
