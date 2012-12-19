8207106e5aa6cd11f6c86c18d0f67b227f93f66d
Anton Makarenko <anton.makarenko@magento.com>
2012-12-19 12:35:26 -0800

    MAGETWO-6347 Ability to Run Specified Type of Tests Using Batch Tool - added ability to specify type of tests to run using batch tool - added integrity and legacy test types to the list

--- dev/tools/batch_tests/batch.php
+++ dev/tools/batch_tests/batch.php
@@ -9,22 +9,43 @@
  * @license     {license_link}
  */
 
-$tests = array(
-    'unit'             => array('../../tests/unit', ''),
-    'unit-performance' => array('../../tests/performance/framework/tests/unit', ''),
-    'unit-static'      => array('../../tests/static/framework/tests/unit', ''),
-    'unit-integration' => array('../../tests/integration/framework/tests/unit', ''),
-    'integration'      => array('../../tests/integration', ''),
-    'static'           => array('../../tests/static', ''),
+$commands = array(
+    'unit'                  => array('../../tests/unit', ''),
+    'unit-performance'      => array('../../tests/performance/framework/tests/unit', ''),
+    'unit-static'           => array('../../tests/static/framework/tests/unit', ''),
+    'unit-integration'      => array('../../tests/integration/framework/tests/unit', ''),
+    'integration'           => array('../../tests/integration', ''),
+    'static'                => array('../../tests/static', ' -c phpunit-all.xml.dist'),
+    'integration-integrity' => array('../../tests/integration', ' testsuite/integrity'),
+    'legacy'                => array('../../tests/static', ' testsuite/Legacy'),
 );
-$arguments = getopt('', array('all'));
+$types = array(
+    'unit'        => array('unit', 'unit-performance', 'unit-static', 'unit-integration'),
+    'integration' => array('integration', 'integration-integrity'),
+    'static'      => array('static'),
+    'integrity'   => array('static', 'integration-integrity'),
+    'legacy'      => array('legacy'),
+);
+$arguments = getopt('', array('all', 'type::'));
+if (isset($arguments['type'])) {
+    $availableTypes = array_keys($commands);
+    foreach ($availableTypes as $type) {
+        if (!in_array($type, $types[$arguments['type']])) {
+            unset($commands[$type]);
+        }
+    }
+}
 if (isset($arguments['all'])) {
-    $tests['integration-integrity'] = array('../../tests/integration', ' testsuite/integrity');
-    $tests['static'][1] = ' -c phpunit-all.xml.dist';
+    unset($commands['legacy']); // the static with "all" option covers it
+} else {
+    unset($commands['integration-integrity']);
+    if (isset($commands['static'])) {
+        $commands['static'][1] = '';
+    }
 }
 
 $failures = array();
-foreach ($tests as $row) {
+foreach ($commands as $row) {
     list($dir, $options) = $row;
     $dirName = realpath(__DIR__ . '/' . $dir);
     chdir($dirName);
@@ -41,10 +62,10 @@ foreach ($tests as $row) {
 
 echo "\n" , str_repeat('-', 70), "\n";
 if ($failures) {
-    echo "\nFAILED - " . count($failures) . ' of ' . count($tests) . ":\n";
+    echo "\nFAILED - " . count($failures) . ' of ' . count($commands) . ":\n";
     foreach ($failures as $message) {
         echo ' - ' . $message . "\n";
     }
 } else {
-    echo "\nPASSED (" . count($tests) . ")\n";
+    echo "\nPASSED (" . count($commands) . ")\n";
 }
