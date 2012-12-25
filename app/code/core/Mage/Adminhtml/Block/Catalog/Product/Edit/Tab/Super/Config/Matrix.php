685b9d52ac81f371b617a0f3b431391a296ad088
Andrii Kasian <akasian@ebay.com>
2012-12-25 08:12:35 -0500

    Implemented MAGETWO-6205: Polish up configurable attribute block

---
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Super/Config/Matrix.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Product variations matrix block
+ *
+ * @category   Mage
+ * @package    Mage_Adminhtml
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config_Matrix
+    extends Mage_Backend_Block_Template
+{
+    /**
+     * Get configurable product type
+     *
+     * @return Mage_Catalog_Model_Product_Type_Configurable
+     */
+    protected function _getProductType()
+    {
+        return Mage::getModel('Mage_Catalog_Model_Product_Type_Configurable');
+    }
+
+    /**
+     * Retrieve currently edited product object
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _getProduct()
+    {
+        return Mage::registry('current_product');
+    }
+
+    /**
+     * Retrieve all possible attribute values combinations
+     *
+     * @return array
+     */
+    public function getVariations()
+    {
+        $variationalAttributes = array();
+        $usedProductAttributes = $this->getAttributes();
+        foreach ($usedProductAttributes as &$attribute) {
+            $options = array();
+            foreach ($attribute['options'] as $key => $valueInfo) {
+                foreach ($attribute['values'] as $priceData) {
+                    if ($priceData['value_index'] == $valueInfo['value']
+                        && (!isset($priceData['include']) || $priceData['include'])
+                    ) {
+                        $valueInfo['price'] = $priceData;
+                        $options[] = $valueInfo;
+                    }
+                }
+            }
+            /** @var $attribute Mage_Catalog_Model_Resource_Eav_Attribute */
+            $variationalAttributes[] = array(
+                'id' => $attribute['attribute_id'],
+                'values' => $options,
+            );
+
+        }
+        $attributesCount = count($variationalAttributes);
+
+        $variations = array();
+        $currentVariation = array_fill(0, $attributesCount, 0);
+        $variationalAttributes = array_reverse($variationalAttributes);
+        $lastAttribute = $attributesCount - 1;
+        do {
+            for ($attributeIndex = 0; $attributeIndex < $attributesCount - 1; ++$attributeIndex) {
+                if ($currentVariation[$attributeIndex] >= count($variationalAttributes[$attributeIndex]['values'])) {
+                    $currentVariation[$attributeIndex] = 0;
+                    ++$currentVariation[$attributeIndex + 1];
+                }
+            }
+            if ($currentVariation[$lastAttribute] >= count($variationalAttributes[$lastAttribute]['values'])) {
+                break;
+            }
+
+            $filledVariation = array();
+            for ($attributeIndex = $attributesCount; $attributeIndex--;) {
+                $currentAttribute = $variationalAttributes[$attributeIndex];
+                $filledVariation[$currentAttribute['id']] =
+                    $currentAttribute['values'][$currentVariation[$attributeIndex]];
+            }
+
+            $variations[] = $filledVariation;
+            $currentVariation[0]++;
+        } while (1);
+        return $variations;
+    }
+
+    /**
+     * Get url for product edit
+     *
+     * @param $id
+     *
+     * @return string
+     */
+    public function getEditProductUrl($id)
+    {
+        return $this->getUrl('*/*/edit', array('id' => $id));
+    }
+
+
+    /**
+     * Retrieve attributes data
+     *
+     * @return array
+     */
+    public function getAttributes()
+    {
+        if (!$this->hasData('attributes')) {
+            $attributes = (array)$this->_getProductType()->getConfigurableAttributesAsArray($this->_getProduct());
+            $productData = (array)$this->getRequest()->getParam('product');
+            if (isset($productData['configurable_attributes_data'])) {
+                $configurableData = $productData['configurable_attributes_data'];
+                foreach ($attributes as $key => &$attribute) {
+                    if (isset($configurableData[$key])) {
+                        $attribute['values'] = array_merge(
+                            isset($attribute['values']) ? $attribute['values'] : array(),
+                            isset($configurableData[$key]['values']) ? array_filter($configurableData[$key]['values']) : array()
+                        );
+                    }
+                }
+            }
+
+            foreach ($attributes as &$attribute) {
+                if (isset($attribute['values']) && is_array($attribute['values'])) {
+                    foreach ($attribute['values'] as &$attributeValue) {
+                        if (!$this->getCanReadPrice()) {
+                            $attributeValue['pricing_value'] = '';
+                            $attributeValue['is_percent'] = 0;
+                        }
+                        $attributeValue['can_edit_price'] = $this->getCanEditPrice();
+                        $attributeValue['can_read_price'] = $this->getCanReadPrice();
+                    }
+                }
+            }
+            $this->setData('attributes', $attributes);
+        }
+        return $this->getData('attributes');
+    }
+
+    public function getUsedAttributes()
+    {
+        return $this->_getProductType()->getUsedProductAttributes($this->_getProduct());
+    }
+
+    public function getUsedProducts()
+    {
+        $productByUsedAttributes = array();
+        foreach ($this->_getProduct()->getTypeInstance()->getUsedProducts($this->_getProduct()) as $product) {
+            $keys = array();
+            foreach ($this->getUsedAttributes() as $attribute) {
+                /** @var $attribute Mage_Catalog_Model_Resource_Eav_Attribute */
+                $keys[] = $product->getData($attribute->getAttributeCode());
+            }
+
+            $productByUsedAttributes[implode('-', $keys)] = $product;
+        }
+        return $productByUsedAttributes;
+    }
+}
---
+++ app/code/core/Mage/Adminhtml/view/adminhtml/catalog/product-variation.js
@@ -0,0 +1,45 @@
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+(function($) {
+    $.widget("mage.configarableAttributes", {
+        //Set up the widget
+        _create: function () {
+            this.element.sortable({
+                axis: "y",
+                handle: '.entry-edit-head',
+                //containment: "parent",
+                update: function (event, ui) {
+                    $(this).find('.attribute-position').each(function (index) {
+                        $(this).val(index)
+                    })
+                }
+            });
+            this.element.on('click change', 'input.price-variation', function (event) {
+                var $this = $(this),
+                    $block = $this.closest('.entry-edit');
+
+                if ($this.is(':checked')) {
+                    $block.addClass('have-price');
+                } else {
+                    $block.removeClass('have-price');
+                    $block.find('.pricing-value').val('');
+                }
+            });
+            this.element.on('click', '.remove', function (event) {
+                $(this).closest('.entry-edit').remove();
+            });
+            this.element.on('click', '.toggle', function (event) {
+                $(this).parent().next('fieldset').toggle();
+            });
+            this.element.on('add', function (event, attribute) {
+                $('#attribute-template').tmpl({attribute: attribute}).appendTo($(this));
+            });
+        }
+    });
+})(jQuery);
