5a6370a5ad6b7cfd5ae49f7bf50af3326a0d9922
Sergii Dzhepa <sdzhepa@ebay.com>
2012-12-26 18:06:00 +0200

    Fixed  MAGETWO-6516: Websites are not displayed in Automated Email Marketing Reminder Rules grid

---
+++ app/code/core/Enterprise/Reminder/Model/Resource/Reminder/Collection.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Enterise
+ * @package     Enterpise_Reminder
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+
+/**
+ * Customer segment data grid collection
+ *
+ * @category    Enterise
+ * @package     Enterpise_Reminder
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Enterprise_Reminder_Model_Resource_Reminder_Collection
+    extends Enterprise_Reminder_Model_Resource_Rule_Collection
+{
+    /**
+     * @return Enterprise_Reminder_Model_Resource_Reminder_Collection|Mage_Core_Model_Resource_Db_Collection_Abstract
+     */
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+        $this->addWebsitesToResult();
+        return $this;
+    }
+}
