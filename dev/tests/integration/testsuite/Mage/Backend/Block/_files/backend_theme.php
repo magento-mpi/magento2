316d04ea9191a680d73f9a33b0c9f335df345c98
Andrey Tserkus <andrey.tserkus@magento.com>
2012-12-25 16:48:53 -0800

    CR Fixes MAGETWO-5995: Update voodoo-MAGETWO-5609 Branch with voodoo Branch Changes: - fixed lack of space

--- dev/tests/integration/testsuite/Mage/Backend/Block/_files/backend_theme.php
+++ dev/tests/integration/testsuite/Mage/Backend/Block/_files/backend_theme.php
@@ -9,5 +9,5 @@
 $registration = Mage::getModel('Mage_Core_Model_Theme_Registration');
 $registration->register(
     __DIR__ . DIRECTORY_SEPARATOR . 'design',
-    implode(DIRECTORY_SEPARATOR, array('*','*', '*', 'theme.xml'))
+    implode(DIRECTORY_SEPARATOR, array('*', '*', '*', 'theme.xml'))
 );
