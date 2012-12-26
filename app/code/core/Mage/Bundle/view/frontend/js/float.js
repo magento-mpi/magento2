e980bf94b22fb8e823ad2ef8dc358740e11f9c9e
Bibu Mathew <bimathew@x.com>
2012-12-26 15:31:42 -0600

    Implement MAGETWO-6394: enterprise bundle summary widget -created float widget for the bundle summary

---
+++ app/code/core/Mage/Bundle/view/frontend/js/float.js
@@ -0,0 +1,55 @@
+/**
+ * {license_notice}
+ *
+ * @category    mage
+ * @package     float
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/*jshint browser:true jquery:true*/
+(function($, window) {
+    $.widget('mage.float', {
+        options: {
+            productOptionsSelector: '#product-options-wrapper'
+        },
+
+        /**
+         * Bind handlers to scroll event
+         * @private
+         */
+        _create: function() {
+            $(window).on('scroll', $.proxy(this._setTop, this));
+        },
+
+        /**
+         * float bundleSummary on windowScroll
+         * @private
+         */
+        _setTop: function() {
+            if ((this.element).is(':visible')) {
+                var starTop = $(this.options.productOptionsSelector).offset().top,
+                    offset = $(document).scrollTop(),
+                    maxTop = this.element.parent().offset().top;
+                if (!this.options.top) {
+                    this.options.top = this.element.position().top;
+                    this.element.css('top', this.options.top);
+                }
+
+                if (starTop > offset) {
+                    return false;
+                }
+
+                if (offset < this.options.top) {
+                    offset = this.options.top;
+                }
+
+                var allowedTop = this.options.top + offset - starTop;
+
+                if (allowedTop < maxTop) {
+                    this.element.css('top', allowedTop + "px");
+                }
+            }
+        }
+    });
+})(jQuery, window);
+
