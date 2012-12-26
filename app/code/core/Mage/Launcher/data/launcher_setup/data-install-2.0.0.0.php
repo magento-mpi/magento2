25822bbb22959fd8e9b69d54ebf8a0f9bb12b9bf
Maksym Iakusha <miakusha@ebay.com>
2012-12-26 16:55:07 +0200

    Implemented MAGETWO-4390: Cross-Sells, Up-Sells, Related Products Tile

--- app/code/core/Mage/Launcher/data/launcher_setup/data-install-2.0.0.0.php
+++ app/code/core/Mage/Launcher/data/launcher_setup/data-install-2.0.0.0.php
@@ -102,3 +102,10 @@ $tile->setCode('ebay');
 $tile->setPageId(2);
 $tile->setState(Mage_Launcher_Model_Tile::STATE_TODO);
 $tile->save();
+
+/** @var $tile Mage_Launcher_Model_Tile */
+$tile = Mage::getModel('Mage_Launcher_Model_Tile');
+$tile->setCode('related_products');
+$tile->setPageId(2);
+$tile->setState(Mage_Launcher_Model_Tile::STATE_TODO);
+$tile->save();
