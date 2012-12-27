973246fdff6752b8c363197c7c94a5299fc98d0b
Maksym Moskvychev <mmoskvychev@ebay.com>
2012-12-27 16:57:54 +0200

    Implemented MAGETWO-6371: Create local filesystem adapter

--- dev/tests/integration/testsuite/Magento/Filesystem/Sream/LocalTest.php
+++ dev/tests/integration/testsuite/Magento/Filesystem/Sream/LocalTest.php
@@ -14,9 +14,22 @@ class Magento_Filesystem_Stream_LocalTest extends PHPUnit_Framework_TestCase
      */
     protected $_stream;
 
+    /**
+     * @var string
+     */
+    protected $_writeFileName;
+
     protected function setUp()
     {
         $this->_stream = new Magento_Filesystem_Stream_Local(__DIR__ . DS . '..' . DS . '_files' . DS . 'popup.css');
+        $this->_writeFileName = __DIR__ . DS . '..' . DS . '_files' . DS . 'new.css';
+    }
+
+    protected function tearDown()
+    {
+        if (file_exists($this->_writeFileName)) {
+            unlink($this->_writeFileName);
+        }
     }
 
     /**
@@ -30,7 +43,7 @@ class Magento_Filesystem_Stream_LocalTest extends PHPUnit_Framework_TestCase
 
     public function testOpenNewFile()
     {
-        $stream = new Magento_Filesystem_Stream_Local(__DIR__ . DS . '..' . DS . '_files' . DS . 'new.css');
+        $stream = new Magento_Filesystem_Stream_Local($this->_writeFileName);
         $stream->open(new Magento_Filesystem_Stream_Mode('w'));
     }
 
@@ -57,6 +70,66 @@ class Magento_Filesystem_Stream_LocalTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(array('field3', 'field4'), $data);
         $data = $stream->readCsv(0);
         $this->assertFalse($data);
+    }
+
+    public function testWrite()
+    {
+        $stream = new Magento_Filesystem_Stream_Local($this->_writeFileName);
+        $stream->open(new Magento_Filesystem_Stream_Mode('w'));
+        $stream->write('test data');
+        $this->assertEquals('test data', file_get_contents($this->_writeFileName));
+    }
+
+    public function testWriteCsv()
+    {
+        $stream = new Magento_Filesystem_Stream_Local($this->_writeFileName);
+        $stream->open(new Magento_Filesystem_Stream_Mode('w'));
+        $stream->writeCsv(array('data1', 'data2'));
+        $stream->open(new Magento_Filesystem_Stream_Mode('r'));
+        $this->assertEquals(array('data1', 'data2'), $stream->readCsv());
+    }
+
+    /**
+     * @expectedException Magento_Filesystem_Exception
+     */
+    public function testClose()
+    {
+        $this->_stream->close();
+        $this->_stream->read(1);
+    }
+
+    public function testSeek()
+    {
+        $this->_stream->open(new Magento_Filesystem_Stream_Mode('r'));
+        $this->_stream->seek(14);
+        $this->assertEquals(';', $this->_stream->read(1));
+    }
+
+    /**
+     * @expectedException Magento_Filesystem_Exception
+     * @expectedExceptionMessage seek operation on the stream caused an error.
+     */
+    public function testSeekError()
+    {
+        $this->_stream->open(new Magento_Filesystem_Stream_Mode('r'));
+        $this->_stream->seek(-1);
+    }
+
+    public function testTell()
+    {
+        $this->_stream->open(new Magento_Filesystem_Stream_Mode('r'));
+        $this->assertEquals(0, $this->_stream->tell());
+        $this->_stream->seek(14);
+        $this->assertEquals(14, $this->_stream->tell());
+    }
+
+    public function testEof()
+    {
+        $this->_stream->open(new Magento_Filesystem_Stream_Mode('r'));
+        $this->assertFalse($this->_stream->eof());
+        $this->_stream->read(15);
+        $this->_stream->read(15);
+        $this->assertTrue($this->_stream->eof());
     }
 
     /**
@@ -96,7 +169,7 @@ class Magento_Filesystem_Stream_LocalTest extends PHPUnit_Framework_TestCase
      */
     public function testForbiddenRead($method)
     {
-        $stream = new Magento_Filesystem_Stream_Local(__DIR__ . DS . '..' . DS . '_files' . DS . 'new.css');
+        $stream = new Magento_Filesystem_Stream_Local($this->_writeFileName);
         $stream->open(new Magento_Filesystem_Stream_Mode('w'));
         $stream->$method(1);
     }
@@ -109,6 +182,29 @@ class Magento_Filesystem_Stream_LocalTest extends PHPUnit_Framework_TestCase
         return array(
             array('read'),
             array('readCsv'),
+        );
+    }
+
+    /**
+     * @param string $method
+     * @dataProvider forbiddenWriteDataProvider
+     * @expectedException Magento_Filesystem_Exception
+     * @expectedExceptionMessage The stream does not allow write.
+     */
+    public function testForbiddenWrite($method)
+    {
+        $this->_stream->open(new Magento_Filesystem_Stream_Mode('r'));
+        $this->_stream->$method(1);
+    }
+
+    /**
+     * @return array
+     */
+    public function forbiddenWriteDataProvider()
+    {
+        return array(
+            array('write'),
+            array('writeCsv'),
         );
     }
 }
