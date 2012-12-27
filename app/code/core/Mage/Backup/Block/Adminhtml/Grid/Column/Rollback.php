258e981c36c06ffec6a11223d0d24ca27a4bdb2c
Anton Kril <akril@ebay.com>
2012-12-27 11:56:50 +0200

    Implemented MAGETWO-6429: Integration task

--- app/code/core/Mage/Backup/Block/Adminhtml/Grid/Column/Rollback.php
+++ app/code/core/Mage/Backup/Block/Adminhtml/Grid/Column/Rollback.php
@@ -1,20 +1,20 @@
 <?php
 /**
-* {license_notice}
-*
-* @category    Mage
-* @package     Mage_Backup
-* @copyright   {copyright}
-* @license     {license_link}
-*/
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Backup
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
 
 /**
-* Grid column block that is displayed only if rollback allowed
-*
-* @category   Mage
-* @package    Mage_Backup
-* @author     Magento Core Team <core@magentocommerce.com>
-*/
+ * Grid column block that is displayed only if rollback allowed
+ *
+ * @category   Mage
+ * @package    Mage_Backup
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
 class Mage_Backup_Block_Adminhtml_Grid_Column_Rollback extends Mage_Backend_Block_Widget_Grid_Column
 {
     /**
