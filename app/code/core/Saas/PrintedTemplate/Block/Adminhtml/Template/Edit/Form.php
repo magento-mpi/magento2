c2f5576f61cfbd09ecaf3c06c0816cc7ed395f0f
ruslan.grabovoy <rgrabovoy@ebay.com>
2012-12-27 13:59:13 +0200

    Fixed MAGETWO-6426: Adopt templates and layout
    - move back to tinyMCE

--- app/code/core/Saas/PrintedTemplate/Block/Adminhtml/Template/Edit/Form.php
+++ app/code/core/Saas/PrintedTemplate/Block/Adminhtml/Template/Edit/Form.php
@@ -147,7 +147,7 @@ class Saas_PrintedTemplate_Block_Adminhtml_Template_Edit_Form extends Mage_Backe
         ));
 
 
-        $fieldset->addField('content', 'textarea', array(
+        $fieldset->addField('content', 'editor', array(
             'name'      => 'content',
             'label'     => $this->__('Template Content'),
             'title'     => $this->__('Template Content'),
