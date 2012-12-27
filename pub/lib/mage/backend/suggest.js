d48f10e46cd2a022e62f7a937fbf0caca1e4917e
dmytro.iekutenko <oatuvVeOv2>
2012-12-27 11:44:10 +0200

    implemented MAGETWO-6509 : Implement Recent Search Results for Widget suggest - implemented recent search results of suggest widget

--- pub/lib/mage/backend/suggest.js
+++ pub/lib/mage/backend/suggest.js
@@ -9,23 +9,56 @@
 /*jshint jquery:true*/
 
 (function($) {
+    $.extend(true, $, {
+        mage: {
+            isStorageAvailable: function() {
+                try {
+                    return 'localStorage' in window && window['localStorage'] !== null;
+                } catch (e) {
+                    return false;
+                }
+            }
+        }
+    });
+
     'use strict';
     $.widget('mage.suggest', {
         options: {
             template: '#menuTemplate',
-            minLength: 1
+            minLength: 1,
+            source: 'http://testsuggest.lo/suggest.php'
         },
         _create: function() {
             this._setTemplate();
+            this.contentContainer = $('<div></div>');
+            this.element.after(this.contentContainer);
             this._bind();
         },
         _value: function() {
-            return this.element[this.element.is( "input,textarea" ) ? "val" : "text"]();
+            return this.element[this.element.is(":input") ? "val" : "text"]();
         },
         _bind: function() {
-            this._on( this.element, {
-                keypress: this.search
-            });
+            this._on($.extend({
+                keydown: function(event) {
+                    var keyCode = $.ui.keyCode;
+                    switch (event.keyCode) {
+                        case keyCode.UP:
+                            this.contentContainer
+                                .find(this.options.contentType)
+                                .triggerHandler(event);
+                            break;
+                        case keyCode.DOWN:
+                            this.contentContainer
+                                .find(this.options.contentType)
+                                .triggerHandler(event);
+                            break;
+                    }
+                },
+                keyup: this.search
+            }, this.options.events || {}));
+            this.contentContainer.on('menufocus', $.proxy(function(e, ui){
+                this.element.val(ui.item.text());
+            }, this))
         },
         _setTemplate: function() {
             this.template = $(this.options.template).length ?
@@ -48,8 +81,9 @@
             if(this.suggestElement) {
                 this.suggestElement.remove();
             }
-            this.suggestElement = $.tmpl(this.template, {data: data});
-            this.element.after(this.suggestElement);
+
+            this.suggestElement = $.tmpl(this.template, {data: data}).appendTo(this.contentContainer);
+            this.suggestElement.trigger('contentUpdated');
         },
         _source: function(value, render) {
             if ($.isArray(this.options.source)) {
@@ -90,4 +124,11 @@
             }
         }
     });
+
+    /*$.widget('mage.suggest', $.mage.suggest, {
+     options: {
+     showRecent: true
+     },
+     _create: function()
+     });*/
 })(jQuery);
