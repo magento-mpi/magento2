3bcacc14be82d543bfb09f1c95814aceb8e313bc
Andrey Voinkov <avoinkov@ebay.com>
2012-12-28 14:26:29 +0200

    Fixed MAGETWO-6518: Additions to Rename Theme Customization Ability -fixed bug with incorrect fallback model

--- app/code/core/Mage/Core/Model/Design/Package.php
+++ app/code/core/Mage/Core/Model/Design/Package.php
@@ -414,14 +414,17 @@ class Mage_Core_Model_Design_Package
             $params['skipProxy']
         ));
         if (!isset($this->_fallback[$cacheKey])) {
-            if (!isset($params['skipProxy']) && Mage::getIsDeveloperMode()) {
-                $params['skipProxy'] = true;
-            }
             $this->_fallback[$cacheKey] = $this->_createFallback($params);
         }
         return $this->_fallback[$cacheKey];
     }
 
+    /**
+     * Create fallback model
+     *
+     * @param array $params
+     * @return Mage_Core_Model_Design_Fallback|Mage_Core_Model_Design_Fallback_CachingProxy
+     */
     protected function _createFallback($params)
     {
         $model = 'Mage_Core_Model_Design_Fallback_CachingProxy';
