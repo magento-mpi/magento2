a0efdccdc15e8dc8a694fda2b9422a9c5c37a9b2
Iuliia Babenko <ibabenko@ebay.com>
2012-12-28 18:48:58 +0200

    Implemented MAGETWO-6439: TAF tests - added helper methods for remove and drag blocks in VDE

--- dev/tests/functional/testsuite/Core/Mage/Vde/Helper.php
+++ dev/tests/functional/testsuite/Core/Mage/Vde/Helper.php
@@ -129,4 +129,68 @@ class Core_Mage_Vde_Helper extends Mage_Selenium_AbstractHelper
         $this->addParameter('themeId', $id);
         $this->validatePage();
     }
+
+    /**
+     * Search for removable/draggable element in container and return its name
+     *
+     * @param string $operation Operation with element (draggable/removable)
+     * @param string|null $wrapperDataName
+     * @return string|null
+     */
+    public function getOperableBlockName($operation = 'draggable', $wrapperDataName = null)
+    {
+        $locator = $this->_getControlXpath('pageelement', $operation . '_elements');
+        if ($wrapperDataName) {
+            $this->addParameter('dataName', $wrapperDataName);
+            if ($this->controlIsPresent('pageelement', 'vde_element')) {
+                $locator = $this->_getControlXpath('pageelement', 'vde_element') . $locator;
+            }
+        }
+        if ($this->elementIsPresent($locator)) {
+            $elements = $this->getElements($locator);
+            /** @var $block PHPUnit_Extensions_Selenium2TestCase_Element */
+            $block = $elements[0];
+            return $block->attribute('data-name');
+        }
+
+        return null;
+    }
+
+    /**
+     * Remove VDE block in design mode
+     *
+     * @param string $name
+     * @return Core_Mage_Vde_Helper
+     */
+    public function removeBlock($name)
+    {
+        $this->addParameter('dataName', $name);
+        $this->clickControl('link', 'remove_vde_element', false);
+
+        return $this;
+    }
+
+    /**
+     * Drag and drop VDE block in design mode
+     *
+     * @param string $blockName
+     * @param string $destinationElementName
+     * @return Core_Mage_Vde_Helper
+     */
+    public function dragBlock($blockName, $destinationElementName)
+    {
+        $this->addParameter('dataName', $blockName);
+        $block = $this->getElement($this->_getControlXpath('pageelement', 'vde_element'));
+
+        $this->addParameter('dataName', $destinationElementName);
+        $destinationElement = $this->getElement($this->_getControlXpath('pageelement', 'vde_element'));
+
+        $block->click();
+        $this->moveto($block);
+        $this->buttondown();
+        $this->moveto($destinationElement);
+        $this->buttonup();
+
+        return $this;
+    }
 }
