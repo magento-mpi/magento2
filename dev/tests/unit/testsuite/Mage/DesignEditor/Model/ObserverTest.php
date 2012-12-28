85df0dd35adbe8f25215ab027745086d6657f9b8
Yevgeniy Shishkin <yshishkin@ebay.com>
2012-12-28 16:29:14 +0200

    Implemented MAGETWO-6563: Provide ability to automatically clean expired layout updates  - wrote unit test for Mage_DesignEditor_Model_Observer

--- dev/tests/unit/testsuite/Mage/DesignEditor/Model/ObserverTest.php
+++ dev/tests/unit/testsuite/Mage/DesignEditor/Model/ObserverTest.php
@@ -12,6 +12,11 @@
 class Mage_DesignEditor_Model_ObserverTest extends PHPUnit_Framework_TestCase
 {
     /**
+     * Test number of days after which layout will be removed
+     */
+    const TEST_DAYS_TO_EXPIRE = 5;
+
+    /**
      * @var Mage_DesignEditor_Model_Observer
      */
     protected $_model;
@@ -59,5 +64,78 @@ class Mage_DesignEditor_Model_ObserverTest extends PHPUnit_Framework_TestCase
             'no theme id'      => array('$themeId' => null),
             'correct theme id' => array('$themeId' => 1),
         );
+    }
+
+    public function testClearLayoutUpdates()
+    {
+        // important mocks
+        $helper = $this->getMock('Mage_DesignEditor_Helper_Data', array('getDaysToExpire'), array(), '', false);
+        $helper->expects($this->once())
+            ->method('getDaysToExpire')
+            ->will($this->returnValue(self::TEST_DAYS_TO_EXPIRE));
+
+        /** @var $linkCollection Mage_Core_Model_Resource_Layout_Link_Collection */
+        $linkCollection = $this->getMock(
+            'Mage_Core_Model_Resource_Layout_Link_Collection',
+            array('addTemporaryFilter', 'addUpdatedDaysBeforeFilter', 'load'),
+            array(),
+            '',
+            false
+        );
+        $linkCollection->expects($this->once())
+            ->method('addTemporaryFilter')
+            ->with(true)
+            ->will($this->returnSelf());
+        $linkCollection->expects($this->once())
+            ->method('addUpdatedDaysBeforeFilter')
+            ->with(self::TEST_DAYS_TO_EXPIRE)
+            ->will($this->returnSelf());
+        for ($i = 0; $i < 3; $i++) {
+            $link = $this->getMock('Mage_Core_Model_Layout_Link', array('delete'), array(), '', false);
+            $link->expects($this->once())
+                ->method('delete');
+            $linkCollection->addItem($link);
+        }
+
+        /** @var $layoutCollection Mage_Core_Model_Resource_Layout_Update_Collection */
+        $layoutCollection = $this->getMock(
+            'Mage_Core_Model_Resource_Layout_Update_Collection',
+            array('addNoLinksFilter', 'addUpdatedDaysBeforeFilter', 'load'),
+            array(),
+            '',
+            false
+        );
+        $layoutCollection->expects($this->once())
+            ->method('addNoLinksFilter')
+            ->will($this->returnSelf());
+        $layoutCollection->expects($this->once())
+            ->method('addUpdatedDaysBeforeFilter')
+            ->with(self::TEST_DAYS_TO_EXPIRE)
+            ->will($this->returnSelf());
+        for ($i = 0; $i < 3; $i++) {
+            $layout = $this->getMock('Mage_Core_Model_Layout_Update', array('delete'), array(), '', false);
+            $layout->expects($this->once())
+                ->method('delete');
+            $layoutCollection->addItem($layout);
+        }
+
+        $objectManager = $this->getMock('Magento_ObjectManager_Zend', array('create'), array(), '', false);
+        $objectManager->expects($this->at(0))
+            ->method('create')
+            ->with('Mage_Core_Model_Resource_Layout_Link_Collection')
+            ->will($this->returnValue($linkCollection));
+        $objectManager->expects($this->at(1))
+            ->method('create')
+            ->with('Mage_Core_Model_Resource_Layout_Update_Collection')
+            ->will($this->returnValue($layoutCollection));
+
+        // not important mocks
+        /** @var $session Mage_Backend_Model_Session */
+        $session = $this->getMock('Mage_Backend_Model_Session', array(), array(), '', false);
+        $design  = $this->getMock('Mage_Core_Model_Design_Package', array(), array(), '', false);
+
+        // test
+        $this->_model = new Mage_DesignEditor_Model_Observer($objectManager, $session, $design, $helper);
+        $this->_model->clearLayoutUpdates();
     }
 }
