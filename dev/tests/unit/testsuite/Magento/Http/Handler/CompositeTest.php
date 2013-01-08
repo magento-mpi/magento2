68196b8e3eb18d8ca85565d937f82ab652f29f7f
Anton Kril <akril@ebay.com>
2013-01-08 19:03:24 +0200

    Implemented MAGETWO-6771: Composite request handler

---
+++ dev/tests/unit/testsuite/Magento/Http/Handler/CompositeTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+class Magento_Http_Handler_CompositeTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Magento_Http_Handler_Composite
+     */
+    protected $_model;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_cacheHandlerMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_appHandlerMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_requestMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_responseMock;
+
+    public function setUp()
+    {
+        $this->_cacheHandlerMock = $this->getMock('Magento_Http_Handler');
+        $this->_appHandlerMock = $this->getMock('Magento_Http_Handler');
+        $this->_requestMock = $this->getMock('Zend_Controller_Request_Http', array(), array(), '', false);
+        $this->_responseMock = $this->getMock('Zend_Controller_Response_Http', array(), array(), '', false);
+        $this->_model = new Magento_Http_Handler_Composite(array($this->_cacheHandlerMock, $this->_appHandlerMock));
+    }
+
+    public function tearDown()
+    {
+        unset($this->_appHandlerMock);
+        unset($this->_cacheHandlerMock);
+        unset($this->_requestMock);
+        unset($this->_responseMock);
+        unset($this->_model);
+    }
+
+    public function testHandleBreaksCycleIfRequestIsDispatched()
+    {
+        $this->_cacheHandlerMock->expects($this->once())->method('handle');
+        $this->_appHandlerMock->expects($this->never())->method('handle');
+        $this->_requestMock->expects($this->once())->method('isDispatched')->will($this->returnValue(true));
+        $this->_model->handle($this->_requestMock, $this->_responseMock);
+    }
+
+    public function testHandleCallsAllHandlersUntilRequestIsDispatched()
+    {
+        $this->_cacheHandlerMock->expects($this->once())->method('handle');
+        $this->_appHandlerMock->expects($this->once())->method('handle');
+        $this->_requestMock->expects($this->any())->method('isDispatched')->will($this->returnValue(false));
+        $this->_model->handle($this->_requestMock, $this->_responseMock);
+    }
+}
---
+++ lib/Magento/Http/Handler/Composite.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Composite http request handler. Used to apply multiple request handlers
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+class Magento_Http_Handler_Composite implements Magento_Http_Handler
+{
+    /**
+     * Leaf request handlers
+     *
+     * @var Magento_Http_Handler[]
+     */
+    protected $_children;
+
+    /**
+     * @param Magento_Http_Handler[] $handlers
+     */
+    public function __construct(array $handlers)
+    {
+        $this->_children = $handlers;
+    }
+
+    /**
+     * Handle http request
+     *
+     * @param Zend_Controller_Request_Http $request
+     * @param Zend_Controller_Response_Http $response
+     */
+    public function handle(Zend_Controller_Request_Http $request, Zend_Controller_Response_Http $response)
+    {
+        foreach ($this->_children as $handler) {
+            $handler->handle($request, $response);
+            if ($request->isDispatched()) {
+                break;
+            }
+        }
+    }
+}
+
