99a1e7890c9b502859da5f9067c7d491292e3159
Vladimir Korotovskih <vladimir.korotovskih@magento.com>
2013-01-09 17:54:07 +0200

    Implemented MAGETWO-6857: Massaction Additional Base Block

---
+++ app/code/core/Mage/Backend/Block/Widget/Grid/Massaction/Additional.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Backend
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Backend widget grid massaction additional action
+ *
+ * @category   Mage
+ * @package    Mage_Backend
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Backend_Block_Widget_Grid_Massaction_Additional extends Mage_Backend_Block_Widget_Form
+{
+    /**
+     * @var Mage_Core_Model_Layout_Argument_HandlerFactory
+     */
+    protected $_handlerFactory;
+
+    /**
+     * @param Mage_Core_Controller_Request_Http $request
+     * @param Mage_Core_Model_Layout $layout
+     * @param Mage_Core_Model_Event_Manager $eventManager
+     * @param Mage_Backend_Model_Url $urlBuilder
+     * @param Mage_Core_Model_Translate $translator
+     * @param Mage_Core_Model_Cache $cache
+     * @param Mage_Core_Model_Design_Package $designPackage
+     * @param Mage_Core_Model_Session $session
+     * @param Mage_Core_Model_Store_Config $storeConfig
+     * @param Mage_Core_Controller_Varien_Front $frontController
+     * @param Mage_Core_Model_Factory_Helper $helperFactory
+     * @param Mage_Core_Model_Layout_Argument_HandlerFactory $handlerFactory,
+     * @param array $data
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        Mage_Core_Controller_Request_Http $request,
+        Mage_Core_Model_Layout $layout,
+        Mage_Core_Model_Event_Manager $eventManager,
+        Mage_Backend_Model_Url $urlBuilder,
+        Mage_Core_Model_Translate $translator,
+        Mage_Core_Model_Cache $cache,
+        Mage_Core_Model_Design_Package $designPackage,
+        Mage_Core_Model_Session $session,
+        Mage_Core_Model_Store_Config $storeConfig,
+        Mage_Core_Controller_Varien_Front $frontController,
+        Mage_Core_Model_Factory_Helper $helperFactory,
+        Mage_Core_Model_Layout_Argument_HandlerFactory $handlerFactory,
+        array $data = array()
+    ) {
+        parent::__construct($request, $layout, $eventManager, $urlBuilder, $translator, $cache, $designPackage,
+            $session, $storeConfig, $frontController, $helperFactory, $data
+        );
+        $this->_handlerFactory = $handlerFactory;
+    }
+
+    /**
+     * Prepare form before rendering HTML
+     *
+     * @return Mage_Backend_Block_Widget_Form
+     */
+    protected function _prepareForm()
+    {
+        $form = new Varien_Data_Form();
+        foreach ($this->getData('fields') as $itemId => $item) {
+            $this->_prepareFormItem($item);
+            $item['class'] = isset($item['class']) ? $item['class'] . ' absolute-advice' : 'absolute-advice';
+            $form->addField($itemId, $item['type'], $item);
+        }
+        $this->setForm($form);
+        return $this;
+    }
+
+    /**
+     * @param array $item
+     */
+    protected function _prepareFormItem(array &$item)
+    {
+        if ($item['type'] == 'select' && is_string($item['values'])) {
+            $argumentHandler = $this->_handlerFactory->getArgumentHandlerByType('options');
+            $item['values'] = $argumentHandler->process($item['values']);
+        }
+    }
+}
