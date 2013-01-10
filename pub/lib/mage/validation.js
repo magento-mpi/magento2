312fa8bd9cef758d4b76660d8b95ea88ad441ef2
Michael Chiocca <mchiocca@ebay.com>
2013-01-10 08:42:58 -0800

    Fix MAGETWO-5231: Convert wishlist sharing form to jQuery for CE - Code review comments regarding the new validation rule in the base widget

--- pub/lib/mage/validation.js
+++ pub/lib/mage/validation.js
@@ -843,8 +843,8 @@
                 if ($.mage.isEmpty(value)) {
                     return true;
                 }
-                var valid_regexp = /^[a-z0-9\._-]{1,30}@([a-z0-9_-]{1,30}\.){1,5}[a-z]{2,4}$/i;
-                var emails = value.split(',');
+                var valid_regexp = /^[a-z0-9\._-]{1,30}@([a-z0-9_-]{1,30}\.){1,5}[a-z]{2,4}$/i,
+                    emails = value.split(',');
                 for (var i = 0; i < emails.length; i++) {
                     if(!valid_regexp.test(emails[i].strip())) {
                         return false;
