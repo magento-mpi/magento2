27925b1198226f3fc5588ae2cec57300f19f8cb3
Anatoliy Yastreb <ayastreb@ebay.com>
2013-01-11 13:42:39 +0200

    Implemented MAGETWO-6876: Cover Store Resource With Tests - implemented store tests

--- dev/tests/integration/testsuite/Mage/Core/Model/Store/ApiTest.php
+++ dev/tests/integration/testsuite/Mage/Core/Model/Store/ApiTest.php
@@ -9,18 +9,44 @@
  */
 
 /**
- * Magento info Api tests
+ * Magento store Api tests
+ *
+ * @magentoDataFixture Mage/Core/_files/store.php
  */
 class Mage_Core_Model_Store_ApiTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * Test magento magento info retrieving
+     * Test store info.
      */
     public function testInfo()
     {
-        $magentoInfo = Magento_Test_Helper_Api::call($this, 'magentoInfo');
-        $this->assertNotEmpty($magentoInfo['magento_version']);
-        $this->assertNotEmpty($magentoInfo['magento_edition']);
-        $this->assertEquals(Mage::getEdition(), $magentoInfo['magento_edition']);
+        $expectedStore = Mage::app()->getStore('fixturestore');
+        $storeInfo = Magento_Test_Helper_Api::call($this, 'storeInfo', array(
+            'storeId' => 'fixturestore',
+        ));
+        $expectedData= $expectedStore->getData();
+        $this->assertEquals($expectedData, $storeInfo);
+    }
+
+    /**
+     * Test stores list.
+     */
+    public function testList()
+    {
+        $actualStores = Magento_Test_Helper_Api::call($this, 'storeList');
+        $expectedStores = Mage::app()->getStores();
+        /** @var Mage_Core_Model_Store $expectedStore */
+        foreach ($expectedStores as $expectedStore) {
+            $expectedStoreFound = false;
+            foreach ($actualStores as $actualStore) {
+                if ($actualStore['store_id'] == $expectedStore->getId()) {
+                    $this->assertEquals($expectedStore->getData(), $actualStore);
+                    $expectedStoreFound = true;
+                }
+            }
+            if (!$expectedStoreFound) {
+                $this->fail(sprintf('Store "%s" was not found in API response.', $expectedStore->getFrontendName()));
+            }
+        }
     }
 }
