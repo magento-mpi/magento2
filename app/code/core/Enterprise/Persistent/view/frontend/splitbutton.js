d8d0c3f54290ddae8b7c597120a3b7ae438cd64a
Vijay Sriram <vsriram@x.com>
2013-01-14 15:50:18 -0800

    Implemented MAGETWO-6888 : - Create splitbutton jQuery widget to use in Enterprise Wishlist page. Code cleanup

--- app/code/core/Enterprise/Persistent/view/frontend/splitbutton.js
+++ app/code/core/Enterprise/Persistent/view/frontend/splitbutton.js
@@ -18,10 +18,14 @@
         },
         _create: function() {
             $(this.options.splitButton + ' ' + this.options.arrowButton).on('click', $.proxy(this.toggleDropDown, this));
+            $(document).on('click', $.proxy(this.hideDropDown, this));
         },
         toggleDropDown: function(e) {
             $(e.target).closest(this.options.splitButton).toggleClass(this.options.activeClass);
             return false;
+        },
+        hideDropDown: function(e) {
+            $('.' + this.options.activeClass).removeClass(this.options.activeClass);
         }
     });
 })(jQuery);
