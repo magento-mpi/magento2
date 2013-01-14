83050a112809ef8cb54c4c08eabc1b16db31e7f9
Dmitriy Shatovskiy <dshatovskiy@ebay.com>
2013-01-14 16:12:20 +0200

    Implemented MAGETWO-6738: Cover Model/Resource/Tax/Order and Model/WYSIWYG/Config.php by integration tests

---
+++ dev/tests/integration/testsuite/Saas/PrintedTemplate/Model/Resource/Tax/Order/Item/CollectionTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Saas
+ * @package     Saas_PrintedTemplate
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Saas_PrintedTemplate_Model_Resource_Tax_Order_Item_CollectionTest extends PHPUnit_Framework_TestCase
+{}
