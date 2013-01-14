e2e3a017d8cb7155a0d9ff833da80b5c06cdbdc7
Irina Glazunova <iglazunova@ebay.com>
2013-01-14 15:59:14 +0200

    MAGETWO-3440: No ability to create Shipping Label (in case with DHL)  - CR changes in ViewFilesTest.php

--- dev/tests/integration/testsuite/integrity/modular/ViewFilesTest.php
+++ dev/tests/integration/testsuite/integrity/modular/ViewFilesTest.php
@@ -110,7 +110,7 @@ class Integrity_Modular_ViewFilesTest extends Magento_Test_TestCase_IntegrityAbs
     public function testViewFilesFromModulesCode($application, $file)
     {
         Mage::getDesign()->setArea($application)->setDefaultDesignTheme();
-        $params = $application == 'frontend' ? array() : array();
+        $params = $application == 'frontend' ? array('theme' => false) : array();
         $this->assertFileExists(Mage::getDesign()->getViewFile($file, $params));
     }
 
