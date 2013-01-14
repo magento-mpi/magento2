bfea684ca11e7a573aca63f8ab67a021931ee2bf
Paul Lewis <plewis@ebay.com>
2013-01-13 22:47:03 -0600

    Implemented MAGETWO-6867: Cover Customer Address Resource With Tests

--- dev/tests/integration/testsuite/Mage/Customer/Model/Address/Api/_files/customer_address.php
+++ dev/tests/integration/testsuite/Mage/Customer/Model/Address/Api/_files/customer_address.php
@@ -1,26 +1,49 @@
 <?php
 /**
- * Customer address fixture with entity_id = 1
- *
  * {license_notice}
  *
- * @copyright   {copyright}
- * @license     {license_link}
+ * @copyright {copyright}
+ * @license {license_link}
  */
+$customer = Mage::getModel('Mage_Customer_Model_Customer');
+$customer->setStoreId(1)
+    ->setCreatedIn('Default Store View')
+    ->setDefaultBilling(1)
+    ->setDefaultShipping(1)
+    ->setEmail('mr.test.creditmemo.' . uniqid() . '@test.com')
+    ->setFirstname('Test')
+    ->setLastname('Test')
+    ->setMiddlename('Test')
+    ->setGroupId(1)
+    ->setRewardUpdateNotification(1)
+    ->setRewardWarningNotification(1)
+    ->save();
+Mage::register('customer', $customer);
 
-/** @var Mage_Customer_Model_Address $customerAddress */
 $customerAddress = Mage::getModel('Mage_Customer_Model_Address');
-$customerAddress->setCustomerId(1)
-    ->setData(array(
-        'entity_id' => 1,
-        'telephone' => 3468676,
-        'postcode' => 75477,
-        'country_id' => 'AL',
-        'city' => 'CityM',
-        'street' => 'Green str, 67',
-        'lastname' => 'Smith',
-        'firstname' => 'John',
-        'parent_id' => 1,
-        'created_at' => date('YYY-MM-DD hh:mm:ss')
-    ));
+$customerAddress->setData(
+    array(
+        'city' => 'New York',
+        'country_id' => 'US',
+        'fax' => '56-987-987',
+        'firstname' => 'Jacklin',
+        'lastname' => 'Sparrow',
+        'middlename' => 'John',
+        'postcode' => '10012',
+        'region' => 'New York',
+        'region_id' => '43',
+        'street' => 'Main Street',
+        'telephone' => '718-452-9207',
+        'is_default_billing' => true,
+        'is_default_shipping' => true
+    )
+);
+$customerAddress->setCustomer($customer);
 $customerAddress->save();
+Mage::register('customer_address', $customerAddress);
+
+//Set customer default shipping and billing address
+$customer->addAddress($customerAddress);
+$customer->setDefaultShipping($customerAddress->getId());
+$customer->setDefaultBilling($customerAddress->getId());
+$customer->save();
