03310f06c13adbe301392a1177afae0137706109
Sergey Shymko <sergey.shymko@magento.com>
2013-01-15 00:13:56 -0800

    Improved MAGETWO-6945: Integration Test to Measure Memory Leak on Application Reinitialization - fixed capturing STDERR by Magento_Shell when commands piping (|) is used

--- dev/tests/unit/testsuite/Magento/ShellTest.php
+++ dev/tests/unit/testsuite/Magento/ShellTest.php
@@ -12,57 +12,73 @@
 class Magento_ShellTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * Test that PHP command returns an expected result
+     * Test that a command with input arguments returns an expected result
      *
      * @param Magento_Shell $shell
-     * @param string $phpCommand
+     * @param string $command
+     * @param array $commandArgs
      * @param string $expectedResult
      */
-    protected function _testExecutePhpCommand(Magento_Shell $shell, $phpCommand, $expectedResult)
+    protected function _testExecuteCommand(Magento_Shell $shell, $command, $commandArgs, $expectedResult)
     {
         $this->expectOutputString(''); // nothing is expected to be ever printed to the standard output
-        $actualResult = $shell->execute('php -r %s', array($phpCommand));
+        $actualResult = $shell->execute($command, $commandArgs);
         $this->assertEquals($expectedResult, $actualResult);
     }
 
     /**
-     * @param string $phpCommand
+     * @param string $command
+     * @param array $commandArgs
      * @param string $expectedResult
      * @dataProvider executeDataProvider
      */
-    public function testExecute($phpCommand, $expectedResult)
+    public function testExecute($command, $commandArgs, $expectedResult)
     {
-        $this->_testExecutePhpCommand(new Magento_Shell(), $phpCommand, $expectedResult);
+        $this->_testExecuteCommand(new Magento_Shell(), $command, $commandArgs, $expectedResult);
     }
 
     /**
-     * @param string $phpCommand
+     * @param string $command
+     * @param array $commandArgs
      * @param string $expectedResult
      * @param array $expectedLogRecords
      * @dataProvider executeDataProvider
      */
-    public function testExecuteLog($phpCommand, $expectedResult, $expectedLogRecords)
+    public function testExecuteLog($command, $commandArgs, $expectedResult, $expectedLogRecords)
     {
+        $quoteChar = substr(escapeshellarg(' '), 0, 1); // environment-dependent quote character
         $logger = $this->getMock('Zend_Log', array('log'));
         foreach ($expectedLogRecords as $logRecordIndex => $expectedLogMessage) {
+            $expectedLogMessage = str_replace('`', $quoteChar, $expectedLogMessage);
             $logger
                 ->expects($this->at($logRecordIndex))
                 ->method('log')
                 ->with($expectedLogMessage, Zend_Log::INFO)
             ;
         }
-        $this->_testExecutePhpCommand(new Magento_Shell($logger), $phpCommand, $expectedResult);
+        $this->_testExecuteCommand(new Magento_Shell($logger), $command, $commandArgs, $expectedResult);
     }
 
     public function executeDataProvider()
     {
-        $quote = substr(escapeshellarg(' '), 0, 1);
+        // backtick symbol (`) has to be replaced with environment-dependent quote character
         return array(
             'STDOUT' => array(
-                'echo 27181;', '27181', array("php -r {$quote}echo 27181;{$quote} 2>&1", '27181'),
+                'php -r %s', array('echo 27181;'), '27181',
+                array('php -r `echo 27181;` 2>&1', '27181'),
             ),
             'STDERR' => array(
-                'fwrite(STDERR, 27182);', '27182', array("php -r {$quote}fwrite(STDERR, 27182);{$quote} 2>&1", '27182'),
+                'php -r %s', array('fwrite(STDERR, 27182);'), '27182',
+                array('php -r `fwrite(STDERR, 27182);` 2>&1', '27182'),
+            ),
+            'piping STDERR -> STDOUT' => array(
+                // intentionally no spaces around the pipe symbol
+                'php -r %s|php -r %s', array('fwrite(STDERR, 27183);', 'echo fgets(STDIN);'), '27183',
+                array('php -r `fwrite(STDERR, 27183);` 2>&1|php -r `echo fgets(STDIN);` 2>&1', '27183'),
+            ),
+            'piping STDERR -> STDERR' => array(
+                'php -r %s | php -r %s', array('fwrite(STDERR, 27184);', 'fwrite(STDERR, fgets(STDIN));'), '27184',
+                array('php -r `fwrite(STDERR, 27184);` 2>&1 | php -r `fwrite(STDERR, fgets(STDIN));` 2>&1', '27184'),
             ),
         );
     }
@@ -79,16 +95,17 @@ class Magento_ShellTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @param string $phpCommand
+     * @param string $command
+     * @param array $commandArgs
      * @param string $expectedError
      * @dataProvider executeDataProvider
      */
-    public function testExecuteFailureDetails($phpCommand, $expectedError)
+    public function testExecuteFailureDetails($command, $commandArgs, $expectedError)
     {
         try {
             /* Force command to return non-zero exit code */
-            $phpFailingCommand = $phpCommand . ' exit(42);';
-            $this->testExecute($phpFailingCommand, ''); // no result is expected in a case of a command failure
+            $commandArgs[count($commandArgs) - 1] .= ' exit(42);';
+            $this->testExecute($command, $commandArgs, ''); // no result is expected in a case of a command failure
         } catch (Magento_Exception $e) {
             $this->assertInstanceOf('Exception', $e->getPrevious());
             $this->assertEquals($expectedError, $e->getPrevious()->getMessage());
