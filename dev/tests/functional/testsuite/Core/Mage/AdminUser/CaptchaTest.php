ccb798800f185f1fd8a05906fd025da1c457a159
Iryna Lagno <ilagno@ebay.com>
2013-01-15 13:44:25 +0200

    Fix MAGETWO-6673:Fix Mage-Admin User - fix CaptchaTest.php

--- dev/tests/functional/testsuite/Core/Mage/AdminUser/CaptchaTest.php
+++ dev/tests/functional/testsuite/Core/Mage/AdminUser/CaptchaTest.php
@@ -152,7 +152,7 @@ class Core_Mage_AdminUser_CaptchaTest extends Mage_Selenium_TestCase
         $this->addParameter('adminEmail', $emailData['email']);
         $this->addParameter('fieldId', 'captcha');
         $this->assertMessagePresent('validation', 'empty_field');
-        $this->assertTrue(1, count($this->getParsedMessages('required')));
+        $this->assertEquals(1, count($this->getParsedMessages('required')));
     }
 
     /**
