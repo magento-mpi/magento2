7fdb99d84ef16ee110a575043e785ca4ecc38c83
Igor Tripolskiy <itripolskiy@ebay.com>
2013-01-16 16:38:38 +0200

    Implemented MAGETWO-6962: Implement inline grid to display assigned products

---
+++ app/code/core/Mage/Catalog/Model/Resource/Product/Collection/GroupedProducts.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Catalog
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Catalog compare item resource model
+ *
+ * @category    Mage
+ * @package     Mage_Catalog
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Catalog_Model_Resource_Product_Collection_GroupedProducts
+    extends Mage_Catalog_Model_Resource_Product_Link_Product_Collection
+{
+
+    /**
+     * Retrieve currently edited product model
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _getProduct()
+    {
+        return Mage::registry('current_product');
+    }
+
+    /**
+     * Prepare select for load
+     *
+     * @param Varien_Db_Select $select
+     * @return string
+     */
+    public function _prepareSelect(Varien_Db_Select $select)
+    {
+        $allowProductTypes = array();
+        $allowProductTypeNodes = Mage::getConfig()
+            ->getNode('global/catalog/product/type/grouped/allow_product_types')->children();
+        foreach ($allowProductTypeNodes as $type) {
+            $allowProductTypes[] = $type->getName();
+        }
+
+        $this->setProduct($this->_getProduct())
+            ->addAttributeToSelect('*')
+            ->addFilterByRequiredOptions()
+            ->addAttributeToFilter('type_id', $allowProductTypes);
+
+        return parent::_prepareSelect($select);
+    }
+}
