d495d4b4b449e7ad8e23a646f05dc8f8c332ffbe
Vijay Sriram <vsriram@x.com>
2013-01-16 11:09:02 -0800

    Implemented MAGETWO-6888 : - Gift Registry quantity greater than zero validaiton added. - Create splitbutton jQuery widget to use in Enterprise Wishlist page. Code cleanup

--- app/code/core/Enterprise/Persistent/view/frontend/splitbutton.js
+++ app/code/core/Enterprise/Persistent/view/frontend/splitbutton.js
@@ -11,20 +11,41 @@
 (function($, undefined) {
     "use strict";
     $.widget('mage.splitButton', {
+
+        /**
+         * options with default values
+         */
         options: {
             splitButton: '.split-button',
             arrowButton: '.change',
             activeClass: 'active'
         },
+
+        /**
+         * Initialize split button events
+         * @private
+         */
         _create: function() {
-            $(this.options.splitButton + ' ' + this.options.arrowButton).on('click', $.proxy(this.toggleDropDown, this));
-            $(document).on('click', $.proxy(this.hideDropDown, this));
+            $(this.options.splitButton + ' ' + this.options.arrowButton).on('click', $.proxy(this._toggleDropDown, this));
+            $(document).on('click', $.proxy(this._hideDropDown, this));
         },
-        toggleDropDown: function(e) {
+
+        /**
+         * Toggle css class for the split button to hide or show drop down menu
+         * @private
+         * @param {Object} e
+         */
+        _toggleDropDown: function(e) {
             $(e.target).closest(this.options.splitButton).toggleClass(this.options.activeClass);
             return false;
         },
-        hideDropDown: function(e) {
+
+        /**
+         * Hide drop down menu when clicked any where on the page
+         * @private
+         * @param {Object} e
+         */
+        _hideDropDown: function(e) {
             $('.' + this.options.activeClass).removeClass(this.options.activeClass);
         }
     });
