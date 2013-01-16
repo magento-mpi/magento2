f8d468e4180b5735725fc503a3c365d2cbf8d371
Andrey Tserkus <andrey.tserkus@magento.com>
2013-01-15 17:26:36 -0800

    Fixed MAGETWO-6946: Data-Upgrades are not applied on Multi-Tenant Build - made sample data upgrade to see in build log, whether functionality works

---
+++ app/code/core/Mage/Core/data/core_setup/data-upgrade-1.6.0.9-1.6.0.10.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Core
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+echo "---\n";
+echo "DATA UPGRADE 1.6.0.9 -> 1.6.0.10 EXECUTED\n";
+echo "---\n";
