c4a39822b50e2ee8418dc32d57e9d893bef0bc9a
Maryna Ilnytska <milnytska@ebay.com>
2013-01-17 13:38:55 +0200

    CR Changes MAGETWO-6911: Fix functional tests for fulfilled Weight for Configurable product

--- dev/tests/functional/testsuite/Core/Mage/Product/ChangeProductTypeTest.php
+++ dev/tests/functional/testsuite/Core/Mage/Product/ChangeProductTypeTest.php
@@ -53,6 +53,205 @@ class Core_Mage_Product_ChangeProductTypeTest extends Mage_Selenium_TestCase
     }
 
     /**
+     * <p>Data provider for changing product type from simple to virtual/downloadable</p>
+     *
+     * @return array
+     */
+    public function fromSimpleToVirtualDataProvider()
+    {
+        return array(
+            array('virtual', 'Virtual Product'),
+            array('downloadable', 'Downloadable Product')
+        );
+    }
+
+    /**
+     * <p>Virtual/Downloadable to Simple/Downloadable/Virtual product type switching on product creation</p>
+     *
+     * @param string $initialProduct
+     * @param string $changedProduct
+     * @param string $changedType
+     *
+     * @test
+     * @dataProvider fromVirtualDownloadableDataProvider
+     * @TestLinkId TL-MAGE-6428, TL-MAGE-6429, TL-MAGE-6430, TL-MAGE-6431
+     */
+    public function fromVirtualDownloadableDuringCreation($initialProduct, $changedProduct, $changedType)
+    {
+        //Data
+        $productData = $this->loadDataSet('Product', $changedProduct . '_product_visible');
+        $search = $this->loadDataSet('Product', 'product_search', array('product_sku' => $productData['general_sku']));
+        //Steps
+        $this->productHelper()->selectTypeProduct($initialProduct);
+        $this->assertTrue($this->controlIsVisible('field', 'general_weight_disabled'),
+            'Weight field is editable or is not visible');
+        $this->assertTrue($this->getControlElement('checkbox', 'general_weight_and_type_switcher')->selected(),
+            'Weight checkbox is not selected');
+        if ($changedProduct == 'simple') {
+            $this->fillCheckbox('general_weight_and_type_switcher', 'no');
+        }
+        $this->productHelper()->fillProductInfo($productData);
+        $this->saveForm('save');
+        //Verifying
+        $this->assertMessagePresent('success', 'success_saved_product');
+        $this->assertEquals($changedType, $this->productHelper()->getProductDataFromGrid($search, 'Type'),
+            'Incorrect product type has been created');
+        $this->productHelper()->openProduct($search);
+        if ($changedProduct == 'simple') {
+            $this->assertTrue($this->controlIsVisible('field', 'general_weight'), 'Weight field is not editable');
+            $this->assertFalse($this->controlIsVisible('tab', 'downloadable_information'),
+                'Downloadable Information is absent');
+        } else {
+            $this->assertTrue($this->controlIsVisible('field', 'general_weight_disabled'),
+                'Weight field is editable or is not visible');
+            $this->assertTrue($this->controlIsVisible('tab', 'downloadable_information'),
+                'Downloadable Information is absent');
+        }
+    }
+
+    /**
+     * <p>Data provider for changing virtual product type to simple/virtual/downloadable</p>
+     *
+     * @return array
+     */
+    public function fromVirtualDownloadableDataProvider()
+    {
+        return array(
+            array('virtual', 'simple', 'Simple Product', 'Virtual Product'),
+            array('virtual', 'downloadable', 'Downloadable Product', 'Virtual Product'),
+            array('downloadable', 'simple', 'Simple Product', 'Downloadable Product'),
+            array('downloadable', 'virtual', 'Virtual Product', 'Downloadable Product')
+        );
+    }
+
+    /**
+     * <p>Simple to Virtual/Downloadable product type switching while product editing</p>
+     *
+     * @param string $changedProduct
+     * @param string $changedType
+     *
+     * @test
+     * @dataProvider fromSimpleToVirtualDataProvider
+     * @TestLinkId TL-MAGE-6432, TL-MAGE-6433
+     */
+    public function fromSimpleToVirtualDuringEditing($changedProduct, $changedType)
+    {
+        //Data
+        $simpleProduct = $this->loadDataSet('Product', 'simple_product_visible');
+        $productData = $this->loadDataSet('Product', $changedProduct . '_product_visible');
+        $search = $this->loadDataSet('Product', 'product_search', array('product_sku' => $productData['general_sku']));
+        //Steps
+        $this->productHelper()->createProduct($simpleProduct);
+        $this->assertMessagePresent('success', 'success_saved_product');
+        $this->productHelper()->openProduct(array('sku' => $simpleProduct['general_sku']));
+        $this->fillCheckbox('general_weight_and_type_switcher', 'yes');
+        $this->productHelper()->fillProductInfo($productData);
+        $this->saveForm('save');
+        //Verifying
+        $this->assertMessagePresent('success', 'success_saved_product');
+        $this->assertEquals($changedType, $this->productHelper()->getProductDataFromGrid($search, 'Type'),
+            'Incorrect product type has been created');
+        $this->productHelper()->openProduct($search);
+        $this->assertTrue($this->controlIsVisible('field', 'general_weight_disabled'),
+            'Weight field is editable or is not visible');
+        $this->assertTrue($this->controlIsVisible('tab', 'downloadable_information'),
+            'Downloadable Information is absent');
+    }
+
+    /**
+     * <p>Virtual/Downloadable to Simple/Downloadable/Virtual product type switching while product editing</p>
+     *
+     * @param string $initialProduct
+     * @param string $changedProduct
+     * @param string $changedType
+     * @param string $initialType
+     *
+     * @test
+     * @dataProvider fromVirtualDownloadableDataProvider
+     * @TestLinkId TL-MAGE-6434, TL-MAGE-6435, TL-MAGE-6436, TL-MAGE-6437
+     */
+    public function fromVirtualDownloadableDuringEditing($initialProduct, $changedProduct, $changedType, $initialType)
+    {
+        //Data
+        $initialProductData = $this->loadDataSet('Product', $initialProduct . '_product_visible');
+        $productData = $this->loadDataSet('Product', $changedProduct . '_product_visible');
+        $search = $this->loadDataSet('Product', 'product_search', array('product_sku' => $productData['general_sku']));
+        //Steps
+        $this->productHelper()->createProduct($initialProductData, $initialProduct);
+        $this->assertMessagePresent('success', 'success_saved_product');
+        $this->productHelper()->openProduct(array('sku' => $initialProductData['general_sku']));
+        if ($changedProduct == 'simple') {
+            $this->fillCheckbox('general_weight_and_type_switcher', 'no');
+        } else {
+            if ($changedProduct == 'virtual') {
+                $this->productHelper()->deleteDownloadableInformation('sample');
+                $this->productHelper()->deleteDownloadableInformation('link');
+                $changedType = $initialType;
+            }
+        }
+        $this->productHelper()->fillProductInfo($productData);
+        $this->saveForm('save');
+        //Verifying
+        $this->assertMessagePresent('success', 'success_saved_product');
+        $this->assertEquals($changedType, $this->productHelper()->getProductDataFromGrid($search, 'Type'),
+            'Incorrect product type has been created');
+        $this->productHelper()->openProduct($search);
+        if ($changedProduct == 'simple') {
+            $this->assertTrue($this->controlIsVisible('field', 'general_weight'), 'Weight field is not editable');
+            $this->assertFalse($this->controlIsVisible('tab', 'downloadable_information'),
+                'Downloadable Information is absent');
+        } else {
+            $this->assertTrue($this->controlIsVisible('field', 'general_weight_disabled'),
+                'Weight field is editable or is not visible');
+            $this->assertTrue($this->controlIsVisible('tab', 'downloadable_information'),
+                'Downloadable Information is absent');
+        }
+    }
+
+    /**
+     * <p>Verify, that Weight field and Is Virtual checkbox is absent for Configurable and Grouped products</p>
+     * <p>that Is Virtual checkbox isn't selected for Bundle products</p>
+     *
+     * @param $productType
+     * @param $isWeightDisabled
+     *
+     * @test
+     * @dataProvider isWeightDisabledDataProvider
+     * @TestLinkId TL-MAGE-6459, TL-MAGE-6460
+     */
+    public function checkDefaultIsVirtual($productType, $isWeightDisabled)
+    {
+        //Steps
+        $this->productHelper()->selectTypeProduct($productType);
+        //Verification grouped product
+        if ($isWeightDisabled === null) {
+            $this->assertFalse($this->controlIsVisible('field', 'general_weight'));
+            $this->assertFalse($this->controlIsVisible('checkbox', 'general_weight_and_type_switcher'));
+        }
+        //Verification for Bundle product (Dynamic and Fixed)
+        else {
+            $this->assertEquals($isWeightDisabled,
+                $this->getControlElement('checkbox', 'general_weight_and_type_switcher')->selected());
+            $this->assertFalse($this->getControlElement('field', 'general_weight')->enabled());
+            $this->fillDropdown('general_weight_type', 'Fixed');
+            $this->assertTrue($this->getControlElement('field', 'general_weight')->enabled());
+        }
+    }
+
+    /**
+     * <p>Data provider for default values for Is Virtual checkbox according product types</p>
+     *
+     * @return array
+     */
+    public function isWeightDisabledDataProvider()
+    {
+        return array(
+            array('grouped', null),
+            //array('bundle', false), MAGETWO-6269
+        );
+    }
+
+    /**
      * <p>Create attribute, attribute set and simple product to use while creating configurable product</p>
      *
      * @return array
