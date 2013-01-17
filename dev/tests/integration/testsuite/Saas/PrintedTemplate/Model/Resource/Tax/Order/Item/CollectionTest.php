417c9892d8d15199c5d949341ffb34aea0bbf90b
Dmitriy Shatovskiy <dshatovskiy@ebay.com>
2013-01-17 18:05:58 +0200

    CR Changes MAGETWO-6738: Cover Model/Resource/Tax/Order and Model/WYSIWYG/Config.php by integration tests

--- dev/tests/integration/testsuite/Saas/PrintedTemplate/Model/Resource/Tax/Order/Item/CollectionTest.php
+++ dev/tests/integration/testsuite/Saas/PrintedTemplate/Model/Resource/Tax/Order/Item/CollectionTest.php
@@ -10,4 +10,47 @@
  */
 
 class Saas_PrintedTemplate_Model_Resource_Tax_Order_Item_CollectionTest extends PHPUnit_Framework_TestCase
-{}
+{
+    /**
+     * @var Saas_PrintedTemplate_Model_Resource_Template_Collection
+     */
+    protected $_collection;
+
+    protected function setUp()
+    {
+        $this->_collection = Mage::getResourceModel(
+            'Saas_PrintedTemplate_Model_Resource_Tax_Order_Item_Collection'
+        );
+    }
+
+    protected function tearDown()
+    {
+        Mage::getConfig()->setCurrentAreaCode(
+            Mage::helper('Mage_Backend_Helper_Data')->getAreaCode()
+        );
+
+        $this->_collection = null;
+    }
+
+    /**
+     * @magentoDataFixture Saas/PrintedTemplate/_files/order.php
+     */
+    public function testAddFilterByInvoice()
+    {
+        $order = Mage::getModel('Mage_Sales_Model_Order')->loadByIncrementId('100000001');
+        $items = array();
+        $orderItems = $order->getAllItems();
+        foreach ($orderItems as $item) {
+            $items[$item->getItemId()] = $item->getQtyOrdered();
+        }
+
+        $invoice = Mage::getModel('Mage_Sales_Model_Service_Order', array('order' => $order))
+            ->prepareInvoice($items)->save();
+        $collection = $this->_collection->addFilterByInvoice($invoice);
+        foreach ($collection->getItems() as $key => $item) {
+            $expectedId = $orderItems[$key]->getItemId();
+            $this->assertEquals($expectedId, $item->getItemId());
+        }
+
+    }
+}
