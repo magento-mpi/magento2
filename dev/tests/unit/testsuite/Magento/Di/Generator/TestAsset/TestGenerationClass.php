e61a812a57b9c53ccb9924dce9c4b49d774a78da
Yevgeniy Shishkin <yshishkin@ebay.com>
2013-01-17 17:47:23 +0200

    Implemented MAGETWO-5270: Add logic into DI classes that provide possibility to generate entity on-the-fly  - implemented unit test for Magento_Di_Generator_Class

---
+++ dev/tests/unit/testsuite/Magento/Di/Generator/TestAsset/TestGenerationClass.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_Di
+ * @subpackage  unit_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Magento_Di_Generator_TestAsset_TestGenerationClass
+{
+    public function __construct(
+        Magento\Di\Generator\TestAsset\ParentClass $parentClass,
+        Magento\Di\Generator\TestAsset\SourceClass $sourceClass,
+        Not_Existing_Class $notExistingClass
+    ) {
+    }
+}
