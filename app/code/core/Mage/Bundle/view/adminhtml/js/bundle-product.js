b0b95d5e87af9f10bff8ade167b31561486bd8f3
Vlad Veselov <vveselov@ebay.com>
2013-01-17 17:21:11 +0200

    Implemented MAGETWO-7033: Redesign "Bundle Items" tab and move it to separate block on "General" tab

---
+++ app/code/core/Mage/Bundle/view/adminhtml/js/bundle-product.js
@@ -0,0 +1,75 @@
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Bundle
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/*jshint browser:true jquery:true*/
+jQuery(function($) {
+    $.widget('mage.bundleProduct', {
+        _create: function () {
+            this._initSortableOptions();
+            this._initSortableSelections();
+            this._on({
+                'change .is-required': function (event) {
+                    var $this = $(event.target);
+                    $this.closest('.option-box').find('[name$="[required]"]').val($this.is(':checked') ? 1 : 0);
+                },
+                'change .is-user-defined-qty': function (event) {
+                    var $this = $(event.target);
+                    $this.closest('.qty-box').find('.select').val($this.is(':checked') ? 1 : 0);
+                },
+                'click .remove':  function (event) {
+                    $(event.target).closest('.option-box').find('.delete-product-option').trigger('click');
+                },
+                'click .toggle': function (event) {
+                    $(event.target).closest('.option-box').find('.option-header,.form-list,.selection-search').toggle();
+                }
+            });
+
+            $(this.element).find('.is-required').each(function () {
+                $(this).prop('checked', $(this).closest('.option-box').find('[name$="[required]"]').val() > 0);
+            });
+            $(this.element).find('.is-user-defined-qty').each(function () {
+                $(this).prop('checked', $(this).closest('.qty-box').find('.select').val() > 0);
+            });
+        },
+        _initSortableOptions: function () {
+            this.element.sortable({
+                axis: 'y',
+                handle: '.entry-edit-head:first',
+                items: '.option-box',
+                update: this._updateBundleOptionPositions
+            });
+        },
+        _initSortableSelections: function () {
+            this.element.find('.option-box .form-list tbody').sortable({
+                axis: 'y',
+                helper: function(event, ui) {
+                    ui.children().each(function() {
+                        $(this).width($(this).width());
+                    });
+                    return ui;
+                },
+                update: function () {
+                    $(this).find('[name^=bundle_selections][name$="[position]"]').each(function (index) {
+                        $(this).val(index);
+                    });
+                }
+            })
+        },
+        _updateBundleOptionPositions: function () {
+            $(this).find('[name^=bundle_options][name$="[position]"]').each(function (index) {
+                $(this).val(index);
+            });
+        },
+        refreshSortableElements: function () {
+            this.element.sortable('refresh');
+            this._updateBundleOptionPositions.apply(this.element);
+            this._initSortableSelections();
+            return this;
+        }
+    });
+});
