37f5f47298bbea07ebbb9fbee45e36a9245e6472
Anton Kril <akril@ebay.com>
2013-01-17 16:15:31 +0200

    Implemented MAGETWO-6770: HTTP entry point object manager

--- pub/index.php
+++ pub/index.php
@@ -4,19 +4,11 @@
  *
  * {license_notice}
  *
- * @category   Mage
- * @package    Mage
  * @copyright  {copyright}
  * @license    {license_link}
  */
-
 require __DIR__ . '/../app/bootstrap.php';
 $params = $_SERVER;
-$params[Mage_Core_Model_App::INIT_OPTION_URIS][Mage_Core_Model_Dir::PUB] = '';
-try {
-    /** @var $app Mage_Core_Model_App */
-    $app = Mage::getObjectManager()->get('Mage_Core_Model_App');
-    $app->run($params);
-} catch (Exception $e) {
-    Mage::printException($e);
-}
+$params['app_uris'][Mage_Core_Model_Dir::PUB] = '';
+$entryPoint = new Mage_Core_Model_EntryPoint_Http(BP, $params);
+$entryPoint->processRequest();
