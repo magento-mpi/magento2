999e35879a1e1bf1959c8c30c179074c2e1e2b07
Anton Kril <akril@ebay.com>
2013-01-18 13:53:08 +0200

    Implemented MAGETWO-6798: get.php entry point adoption

--- app/code/core/Mage/Core/Model/EntryPoint/Media.php
+++ app/code/core/Mage/Core/Model/EntryPoint/Media.php
@@ -19,8 +19,8 @@ class Mage_Core_Model_EntryPoint_Media extends Mage_Core_Model_EntryPointAbstrac
         $baseDir, $mediaDirectory, array $params = array(), $objectManagerClass = 'Mage_Core_Model_ObjectManager_Http'
     ) {
         if (empty($mediaDirectory)) {
-            $params['allowed_modules'] = array('Mage_Core');
-            $params['cache_options']['disable_save'] = true;
+            $params[Mage::PARAM_ALLOWED_MODULES] = array('Mage_Core');
+            $params[Mage::PARAM_CACHE_OPTIONS]['disable_save'] = true;
         }
         parent::__construct($baseDir, $params, $objectManagerClass);
     }
