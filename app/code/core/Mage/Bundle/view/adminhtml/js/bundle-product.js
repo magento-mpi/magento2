8d8f403d91b30b137bd0ac3bdebdbb4a340be0f6
Vlad Veselov <vveselov@ebay.com>
2013-01-18 14:03:48 +0200

    Refactored MAGETWO-7033: Redesign "Bundle Items" tab and move it to separate block on "General" tab - instantiation using mage plugin - code related to bundle moved outta Mage_Adminhtml - some cleanup

--- app/code/core/Mage/Bundle/view/adminhtml/js/bundle-product.js
+++ app/code/core/Mage/Bundle/view/adminhtml/js/bundle-product.js
@@ -12,15 +12,8 @@ jQuery(function($) {
         _create: function () {
             this._initSortableOptions();
             this._initSortableSelections();
+            this._bindCheckboxHandlers();
             this._on({
-                'change .is-required': function (event) {
-                    var $this = $(event.target);
-                    $this.closest('.option-box').find('[name$="[required]"]').val($this.is(':checked') ? 1 : 0);
-                },
-                'change .is-user-defined-qty': function (event) {
-                    var $this = $(event.target);
-                    $this.closest('.qty-box').find('.select').val($this.is(':checked') ? 1 : 0);
-                },
                 'click .remove':  function (event) {
                     $(event.target).closest('.option-box').find('.delete-product-option').trigger('click');
                 },
@@ -29,14 +22,12 @@ jQuery(function($) {
                 }
             });
 
-            this.element.find('.is-required').each(function () {
-                $(this).prop('checked', $(this).closest('.option-box').find('[name$="[required]"]').val() > 0);
-            });
-            this.element.find('.is-user-defined-qty').each(function () {
-                $(this).prop('checked', $(this).closest('.qty-box').find('.select').val() > 0);
-            });
-
             $('#weight_and_type_switcher, label[for=weight_and_type_switcher]').hide();
+
+            var element = this.element;
+            $('#product_info_tabs').on('tabsbeforeactivate', function (event, ui) {
+                element[$(ui.newPanel).find('#attribute-name-container').length ? 'show' : 'hide']();
+            });
         },
         _initSortableOptions: function () {
             this.element.sortable({
@@ -61,6 +52,24 @@ jQuery(function($) {
                     });
                 }
             })
+        },
+        _bindCheckboxHandlers: function () {
+            this._on({
+                'change [data-mage-role=is-required].is-required': function (event) {
+                    var $this = $(event.target);
+                    $this.closest('.option-box').find('[name$="[required]"]').val($this.is(':checked') ? 1 : 0);
+                },
+                'change .is-user-defined-qty': function (event) {
+                    var $this = $(event.target);
+                    $this.closest('.qty-box').find('.select').val($this.is(':checked') ? 1 : 0);
+                }
+            });
+            this.element.find('.is-required').each(function () {
+                $(this).prop('checked', $(this).closest('.option-box').find('[name$="[required]"]').val() > 0);
+            });
+            this.element.find('.is-user-defined-qty').each(function () {
+                $(this).prop('checked', $(this).closest('.qty-box').find('.select').val() > 0);
+            });
         },
         _updateBundleOptionPositions: function () {
             $(this).find('[name^=bundle_options][name$="[position]"]').each(function (index) {
