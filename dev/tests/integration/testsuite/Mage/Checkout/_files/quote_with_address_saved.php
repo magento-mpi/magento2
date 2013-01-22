7a38ec1891f72f12b0f5ea7b3898edc4f1af2294
artem.komarov <akomarov@ebay.com>
2013-01-22 19:34:12 +0200

    CR-Changes MAGETWO-6872: Cover Cart Payment Resource With Tests - Corrected file description

--- dev/tests/integration/testsuite/Mage/Checkout/_files/quote_with_address_saved.php
+++ dev/tests/integration/testsuite/Mage/Checkout/_files/quote_with_address_saved.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Save quote_with_address fixture
+ *
  * The quote is not saved inside the original fixture. It is later saved inside child fixtures, but along with some
  * additional data which may break some tests.
  *
