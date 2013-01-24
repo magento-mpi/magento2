b0812076c0deee4cc812f23956e24d43787ca8dd
Michael Chiocca <mchiocca@ebay.com>
2013-01-24 15:02:45 -0800

    Implement MAGETWO-7127: Refactor product gallery image popup window - Write a jQuery widget for the product image gallery. - Perform head.js with callback inside window.onload function. - Add a role to the close window link anchors for selector specificity.

--- app/code/core/Mage/Catalog/view/frontend/js/gallery.js
+++ app/code/core/Mage/Catalog/view/frontend/js/gallery.js
@@ -7,16 +7,33 @@
  * @license     {license_link}
  */
 /*jshint browser:true, jquery:true*/
-/*global window:true*/
-(function($) {
+(function($, window) {
     "use strict";
-    $(function() {
-        $('div.buttons-set > a.button').on('click', function() {
-            window.close();
-            return false;
-        });
-        var img = $('#product-gallery-image'),
-            width = img.width() > 300 ? 300 : img.width();
-        window.resizeTo(width + 90, img.height() + 210);
+    $.widget('mage.gallery', {
+        options: {
+            minWidth: 300, // Minimum width of the gallery image.
+            widthOffset: 90, // Offset added to the width of the gallery image.
+            heightOffset: 210, // Offset added to the height of the gallery image.
+            closeWindow: "div.buttons-set a[role='close-window']" // Selector for closing the gallery popup window.
+        },
+
+        /**
+         * Bind click handler for closing the popup window and resize the popup based on the image size.
+         * @private
+         */
+        _create: function() {
+            $(this.options.closeWindow).on('click', function() { window.close(); });
+            this.resizeWindow();
+        },
+
+        /**
+         * Resize the gallery image popup window based on the image's dimensions.
+         * @public
+         */
+        resizeWindow: function() {
+            var img = this.element,
+                width = img.width() < this.options.minWidth ? this.options.minWidth : img.width();
+            window.resizeTo(width + this.options.widthOffset, img.height() + this.options.heightOffset);
+        }
     });
-})(jQuery);
+})(jQuery, window);
