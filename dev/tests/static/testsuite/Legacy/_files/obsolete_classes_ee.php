a04c7b9c102b2713f811baff54bc27928ca11fee
Iryna Lagno <ilagno@ebay.com>
2013-01-24 12:32:04 +0200

    Implement  MAGETWO-7082 :Convert Enterprise_Invitation_Block_Adminhtml_Invitation_Grid: - delete converted grid and add it to obsolete classes

--- dev/tests/static/testsuite/Legacy/_files/obsolete_classes_ee.php
+++ dev/tests/static/testsuite/Legacy/_files/obsolete_classes_ee.php
@@ -19,6 +19,7 @@ return array(
     $this->_getClassRule('Enterprise_CatalogEvent_Block_Adminhtml_Event_Grid'),
     $this->_getClassRule('Enterprise_Cms_Model_Resource_Setup'),
     $this->_getClassRule('Enterprise_GiftRegistry_Block_Adminhtml_Customer_View'),
+    $this->_getClassRule('Enterprise_Invitation_Block_Adminhtml_Invitation_Grid'),
     $this->_getClassRule('Enterprise_Invitation_Block_Adminhtml_Report_Invitation_Customer_Grid'),
     $this->_getClassRule('Enterprise_Invitation_Block_Adminhtml_Report_Invitation_Order_Grid'),
     $this->_getClassRule('Enterprise_Invitation_Block_Adminhtml_Report_Invitation_General_Grid'),
