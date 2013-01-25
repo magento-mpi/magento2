a5c3d2b5bf83146b53210b4303e7818b9e6e0b41
Iryna Lagno <ilagno@ebay.com>
2013-01-25 19:06:31 +0200

    Implement  MAGETWO-7201 Convert Mage_Tag_Block_Adminhtml_Report_Product_Detail_Grid -fix layout according to header test

--- app/code/core/Mage/Tag/controllers/Adminhtml/Report/TagController.php
+++ app/code/core/Mage/Tag/controllers/Adminhtml/Report/TagController.php
@@ -193,11 +193,14 @@ class Mage_Tag_Adminhtml_Report_TagController extends Mage_Adminhtml_Controller_
     public function productDetailAction()
     {
         $this->_initAction();
-        $product = Mage::getModel('Mage_Catalog_Model_Product')->load($this->getRequest()->getParam('id'));
+
+        /** @var $detailBlock Mage_Tag_Block_Adminhtml_Report_Product_Detail */
+        $detailBlock = $this->getLayout()->getBlock('report.tag.product.productdetail.container');
+
         $this->_title($this->__('Reports'))
-             ->_title($this->__('Tags'))
-             ->_title($this->__('Products'))
-             ->_title($this->__('Tags submitted to %s', $product->getName()));
+            ->_title($this->__('Tags'))
+            ->_title($this->__('Products'))
+            ->_title($detailBlock->getHeaderText());
 
         $this->_setActiveMenu('Mage_Tag::report_tags')
             ->_addBreadcrumb(
