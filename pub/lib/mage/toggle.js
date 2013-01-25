2cbdf4d804950e7974f983922b2bf6a38a406a88
Igor Krasnykh <ikrasnykh@ebay.com>
2013-01-25 16:13:36 -0600

    Refactored MAGETWO-7123: -Fixed JsHint issue

--- pub/lib/mage/toggle.js
+++ pub/lib/mage/toggle.js
@@ -15,7 +15,7 @@
          */
         _create: function() {
             this.element.on('click', $.proxy(this._onClick, this));
-        }
+        },
 
         /**
          * Binding Click event
@@ -26,7 +26,7 @@
          *  'data-toggle-label'     - contains label which will be used instead of current label upon click
          * @protected
          */
-        , _onClick: function() {
+        _onClick: function() {
             if (this.element.data('toggle-label')) {
                 this._toggleLabel();
             }
@@ -36,24 +36,24 @@
                 this.element.toggle();
             }
             return false;
-        }
+        },
 
         /**
          * Method responsible for replacing clicked element labels
          * @protected
          */
-        , _toggleLabel: function() {
+        _toggleLabel: function() {
             var _currentLabelSelector = (this.element.data('current-label-el')) ? $(this.element.data('current-label-el')) : this.element;
             var _newLabel = this.element.data('toggle-label');
             this.element.data('toggle-label', _currentLabelSelector.html());
             _currentLabelSelector.html(_newLabel);
-        }
+        },
 
         /**
          * Method responsible for hiding and revealing specified DOM elements
          * @protected
          */
-        , _toggleSelectors: function () {
+        _toggleSelectors: function () {
             $(this.element.data('toggle-selectors')).toggleClass('hidden');
         }
     });
