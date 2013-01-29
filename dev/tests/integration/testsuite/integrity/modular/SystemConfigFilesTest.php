203960a5b92d743ecad138b4d0c9d22a2b25cc0b
Anton Kril <akril@ebay.com>
2013-01-29 19:34:58 +0200

    Implemented MAGETWO-7076: Integration Tests fixing

--- dev/tests/integration/testsuite/integrity/modular/SystemConfigFilesTest.php
+++ dev/tests/integration/testsuite/integrity/modular/SystemConfigFilesTest.php
@@ -16,11 +16,15 @@ class Integrity_Modular_SystemConfigFilesTest extends PHPUnit_Framework_TestCase
         $fileList = glob(Mage::getBaseDir('app') . '/*/*/*/*/etc/adminhtml/system.xml');
         try {
             $configMock = $this->getMock(
-                'Mage_Core_Model_Config_Modules_Reader', array('getModuleConfigurationFiles'), array(), '', false
+                'Mage_Core_Model_Config_Modules_Reader', array('getModuleConfigurationFiles', 'getModuleDir'),
+                array(), '', false
             );
             $configMock->expects($this->any())
                 ->method('getModuleConfigurationFiles')
                 ->will($this->returnValue($fileList));
+            $configMock->expects($this->any())
+                ->method('getModuleDir')
+                ->will($this->returnValue(Mage::getBaseDir('app') . '/code/core/Mage/Backend/etc'));
             $cacheMock = $this->getMock('Mage_Core_Model_Cache', array(), array(), '', false);
             $cacheMock->expects($this->any())->method('canUse')->will($this->returnValue(false));
             $converter = new Mage_Backend_Model_Config_Structure_Converter(
