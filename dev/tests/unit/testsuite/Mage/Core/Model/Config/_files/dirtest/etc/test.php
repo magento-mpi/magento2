e7d5effc32c5abd7b3573e009006f6d40fc16bc6
Anton Kril <akril@ebay.com>
2013-01-29 17:50:34 +0200

    Implemented MAGETWO-6800: Request processing integration

--- dev/tests/unit/testsuite/Mage/Core/Model/Config/_files/dirtest/etc/test.php
+++ dev/tests/unit/testsuite/Mage/Core/Model/Config/_files/dirtest/etc/test.php
@@ -1,1 +1,11 @@
 <?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Mage_Core
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+?>
