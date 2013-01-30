0874b58cd54d152a75e8307c45fd711b7f0ae89d
Maryna Ilnytska <milnytska@ebay.com>
2013-01-30 13:03:49 +0200

    Fixed MAGETWO-7232: Fix functional tests for product attribute set

--- dev/tests/functional/testsuite/Core/Mage/AttributeSet/CreateTest.php
+++ dev/tests/functional/testsuite/Core/Mage/AttributeSet/CreateTest.php
@@ -80,8 +80,7 @@ class Core_Mage_AttributeSet_CreateTest extends Mage_Selenium_TestCase
         //Steps
         $this->attributeSetHelper()->createAttributeSet($setData);
         //Verifying
-        $fieldXpath = $this->_getControlXpath('field', 'set_name');
-        $this->addParameter('fieldXpath', $fieldXpath);
+        $this->addFieldIdToMessage('field', 'set_name');
         $this->assertMessagePresent('validation', 'empty_required_field');
         $this->assertTrue($this->verifyMessagesCount(), $this->getParsedMessages());
     }
