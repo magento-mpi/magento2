73f84f32b18485d4066aeac7d70059dcdb7fd53a
Sergey Kovalev <skovalev@ebay.com>
2013-01-31 11:59:12 +0200

    Fixed MAGETWO-7218: Merge Master with Tango Sprint 12 - CachingProxy unit test fix

--- app/code/core/Mage/Core/Model/Design/Fallback/CachingProxy.php
+++ app/code/core/Mage/Core/Model/Design/Fallback/CachingProxy.php
@@ -211,6 +211,7 @@ class Mage_Core_Model_Design_Fallback_CachingProxy implements Mage_Core_Model_De
      */
     public function notifyViewFilePublished($publicFilePath, $file, $module = null)
     {
+        $this->_setToMap('view', $file, $module, $publicFilePath);
         return $this;
     }
 }
