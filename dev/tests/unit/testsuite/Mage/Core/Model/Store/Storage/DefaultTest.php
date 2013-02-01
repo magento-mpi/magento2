145f1781444fc449c334d0730a6daa155a65a406
Evgeniy Miskov <emiskov@ebay.com>
2013-02-01 16:40:53 +0200

    MAGETWO-7233 Store initialization - Unit Tests implementation

--- dev/tests/unit/testsuite/Mage/Core/Model/Store/Storage/DefaultTest.php
+++ dev/tests/unit/testsuite/Mage/Core/Model/Store/Storage/DefaultTest.php
@@ -98,9 +98,7 @@ class Mage_Core_Model_Store_Storage_DefaultTest extends PHPUnit_Framework_TestCa
     public function testGetWebsite()
     {
         $websiteId = 'testWebsite';
-        $this->assertEquals(null, $this->_model->getWebsite($websiteId));
-        $data = 'testWebsite';
-        $this->assertInstanceOf('Mage_Core_Model_Website', $this->_model->getWebsite($data));
+        $this->assertInstanceOf('Mage_Core_Model_Website', $this->_model->getWebsite($websiteId));
     }
 
     public function testGetWebsitesWithDefault()
