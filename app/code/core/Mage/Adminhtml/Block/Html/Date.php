d17bcdcdafd80cfc21f9dcbd9e3bb6f91f6215c3
Olga Kopylova <okopylova@ebay.com>
2013-02-01 17:15:51 -0800

    Fixed MAGETWO-7318: "Edit" Link on "management Products" Page Doesn't Work - fixed classes

--- app/code/core/Mage/Adminhtml/Block/Html/Date.php
+++ app/code/core/Mage/Adminhtml/Block/Html/Date.php
@@ -29,6 +29,9 @@ class Mage_Adminhtml_Block_Html_Date extends Mage_Core_Block_Html_Date
      * @param Mage_Core_Model_Store_Config $storeConfig
      * @param Mage_Core_Controller_Varien_Front $frontController
      * @param Mage_Core_Model_Factory_Helper $helperFactory
+     * @param Mage_Core_Model_Dir $dirs
+     * @param Mage_Core_Model_Logger $logger
+     * @param Magento_Filesystem $filesystem
      * @param array $data
      *
      * @SuppressWarnings(PHPMD.ExcessiveParameterList)
@@ -45,10 +48,13 @@ class Mage_Adminhtml_Block_Html_Date extends Mage_Core_Block_Html_Date
         Mage_Core_Model_Store_Config $storeConfig,
         Mage_Core_Controller_Varien_Front $frontController,
         Mage_Core_Model_Factory_Helper $helperFactory,
+        Mage_Core_Model_Dir $dirs,
+        Mage_Core_Model_Logger $logger,
+        Magento_Filesystem $filesystem,
         array $data = array()
     ) {
         parent::__construct($request, $layout, $eventManager, $urlBuilder, $translator, $cache, $designPackage,
-            $session, $storeConfig, $frontController, $helperFactory, $data
+            $session, $storeConfig, $frontController, $helperFactory, $dirs, $logger, $filesystem, $data
         );
     }
 }
