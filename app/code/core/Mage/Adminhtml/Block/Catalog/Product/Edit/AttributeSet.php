ee3223371dcb43f03b1ac38fe650ceb50c8ab957
Igor Tripolskiy <itripolskiy@ebay.com>
2013-02-05 12:50:18 +0200

    CR Changes MAGETWO-7163: Refactor new attribute set selector to be based on suggest widget rather than on dropdown

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/AttributeSet.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/AttributeSet.php
@@ -25,15 +25,14 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_AttributeSet extends Mage_Backen
      */
     public function getSelectorOptions()
     {
-        return Mage::helper('Mage_Core_Helper_Data')->jsonEncode(array(
-            'source' => Mage::helper('Mage_Backend_Helper_Data')
-                ->getUrl('adminhtml/catalog_product/suggestAttributeSet'),
+        return array(
+            'source' => Mage::helper('Mage_Backend_Helper_Data')->getUrl('*/catalog_product/suggestAttributeSet'),
             'className' => 'category-select',
             'template' => '#product-template-selector-template',
             'showRecent' => true,
             'storageKey' => 'product-template-key',
             'minLength' => 0,
             'ajaxData' => array('current_template_id' => Mage::registry('product')->getAttributeSetId()),
-        ));
+        );
     }
 }
