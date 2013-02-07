e7068620e96c73cb732f316ec97f7e6b07325388
Alex Akimov <aakimov@ebay.com>
2013-02-07 11:32:22 +0200

    Implemented MAGETWO-7378: Implement State Resolver for Product Tile

--- app/code/core/Mage/Launcher/Model/Storelauncher/Product/StateResolver.php
+++ app/code/core/Mage/Launcher/Model/Storelauncher/Product/StateResolver.php
@@ -17,4 +17,30 @@
  */
 class Mage_Launcher_Model_Storelauncher_Product_StateResolver extends Mage_Launcher_Model_Tile_MinimalStateResolver
 {
+    /**
+     * @var Mage_Catalog_Model_Resource_Product_Collection
+     */
+    protected $_productCollection;
+
+    /**
+     * @param Mage_Catalog_Model_Resource_Product_Collection $productCollection
+     */
+    function __construct(
+        Mage_Catalog_Model_Resource_Product_Collection $productCollection
+    ) {
+        // for now this collection is used only once so no cloning is needed before use
+        $this->_productCollection = $productCollection;
+    }
+
+    /**
+     * Resolve state
+     *
+     * @return bool
+     */
+    public function isTileComplete()
+    {
+        $isTileComplete = parent::isTileComplete();
+        // product tile is considered to be complete if at least one product has been created
+        return $isTileComplete && ($this->_productCollection->getSize() > 0);
+    }
 }
