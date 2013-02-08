f382541384dbe2507adea60180a413a5799e1726
Dmytro Vynograd <dvynograd@ebay.com>
2013-02-08 17:28:02 +0200

    Implemented MAGETWO-7276: Theme Selector on Design Drawer

--- app/code/core/Mage/Launcher/Block/Adminhtml/Storelauncher/Design/Drawer.php
+++ app/code/core/Mage/Launcher/Block/Adminhtml/Storelauncher/Design/Drawer.php
@@ -18,6 +18,59 @@
 class Mage_Launcher_Block_Adminhtml_Storelauncher_Design_Drawer extends Mage_Launcher_Block_Adminhtml_Drawer
 {
     /**
+     * @var Mage_Core_Model_Theme_Service
+     */
+    protected $_themeService;
+
+    /**
+     * Constructor
+     *
+     * @param Mage_Core_Controller_Request_Http $request
+     * @param Mage_Core_Model_Layout $layout
+     * @param Mage_Core_Model_Event_Manager $eventManager
+     * @param Mage_Backend_Model_Url $urlBuilder
+     * @param Mage_Core_Model_Translate $translator
+     * @param Mage_Core_Model_Cache $cache
+     * @param Mage_Core_Model_Design_Package $designPackage
+     * @param Mage_Core_Model_Session $session
+     * @param Mage_Core_Model_Store_Config $storeConfig
+     * @param Mage_Core_Controller_Varien_Front $frontController
+     * @param Mage_Core_Model_Factory_Helper $helperFactory
+     * @param Mage_Core_Model_Dir $dirs
+     * @param Mage_Core_Model_Logger $logger
+     * @param Magento_Filesystem $filesystem
+     * @param Mage_Launcher_Model_LinkTracker $linkTracker
+     * @param Mage_Core_Model_Theme_Service $themeService
+     * @param array $data
+     */
+    public function __construct(
+        Mage_Core_Controller_Request_Http $request,
+        Mage_Core_Model_Layout $layout,
+        Mage_Core_Model_Event_Manager $eventManager,
+        Mage_Backend_Model_Url $urlBuilder,
+        Mage_Core_Model_Translate $translator,
+        Mage_Core_Model_Cache $cache,
+        Mage_Core_Model_Design_Package $designPackage,
+        Mage_Core_Model_Session $session,
+        Mage_Core_Model_Store_Config $storeConfig,
+        Mage_Core_Controller_Varien_Front $frontController,
+        Mage_Core_Model_Factory_Helper $helperFactory,
+        Mage_Core_Model_Dir $dirs,
+        Mage_Core_Model_Logger $logger,
+        Magento_Filesystem $filesystem,
+        Mage_Launcher_Model_LinkTracker $linkTracker,
+        Mage_Core_Model_Theme_Service $themeService,
+        array $data = array()
+    )
+    {
+        parent::__construct($request, $layout, $eventManager, $urlBuilder, $translator, $cache, $designPackage,
+            $session, $storeConfig, $frontController, $helperFactory, $dirs, $logger, $filesystem, $linkTracker, $data
+        );
+
+        $this->_themeService = $themeService;
+    }
+
+    /**
      * Get Translated Tile Header
      *
      * @return string
@@ -25,5 +78,29 @@ class Mage_Launcher_Block_Adminhtml_Storelauncher_Design_Drawer extends Mage_Lau
     public function getTileHeader()
     {
         return $this->helper('Mage_Launcher_Helper_Data')->__('Store Design');
+    }
+
+    /**
+     * Get Themes
+     *
+     * @return Mage_Core_Model_Resource_Theme_Collection
+     */
+    public function getThemes()
+    {
+        return $this->_themeService->getAllThemes();
+    }
+
+    /**
+     * Get preview url for selected theme
+     *
+     * @param int $themeId
+     * @return string
+     */
+    public function getPreviewUrl($themeId)
+    {
+        return $this->getUrl('adminhtml/system_design_editor/launch', array(
+            'theme_id' => $themeId,
+            'mode'     => Mage_DesignEditor_Model_State::MODE_NAVIGATION
+        ));
     }
 }
