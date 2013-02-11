fc85acd176e0630066853dcba6782d03c3c89891
Alex Bomko <alex.bomko@magento.com>
2013-02-11 18:26:33 +0200

    Implemented MAGETWO-7497: Implement Design Drawer Save Handler

--- app/code/core/Mage/Launcher/Model/Storelauncher/Design/SaveHandler.php
+++ app/code/core/Mage/Launcher/Model/Storelauncher/Design/SaveHandler.php
@@ -17,5 +17,59 @@
  */
 class Mage_Launcher_Model_Storelauncher_Design_SaveHandler extends Mage_Launcher_Model_Tile_MinimalSaveHandler
 {
+    /**
+     * @var Mage_Backend_Model_Config
+     */
+    protected $_config;
 
+    /**
+     * Constructor
+     *
+     * @param Mage_Backend_Model_Config $config
+     */
+    function __construct(
+        Mage_Backend_Model_Config $config
+    ) {
+        $this->_config = $config;
+    }
+
+    /**
+     * Save function handle the whole Tile save process
+     *
+     * @param array $data Request data
+     */
+    public function save($data)
+    {
+        $sections = array('general', 'design');
+        $preparedData = $this->prepareData($data);
+
+        //Write all config data on the GLOBAL scope
+        foreach ($sections as $section) {
+            if (!empty($preparedData[$section])) {
+                $this->_config->setSection($section)
+                    ->setGroups($preparedData[$section])
+                    ->save();
+            }
+        }
+    }
+
+    /**
+     * Prepare Data for system configuration
+     *
+     * @param array $data
+     * @return array
+     * @throws Mage_Launcher_Exception
+     */
+    public function prepareData($data)
+    {
+        $groups = $data['groups'];
+        if (empty($groups['design']['theme']['fields']['theme_id']['value'])) {
+            throw new Mage_Launcher_Exception('Theme is required.');
+        }
+        if (empty($groups['general']['store_information']['fields']['name']['value'])) {
+            throw new Mage_Launcher_Exception('Store name is required.');
+        }
+
+        return $groups;
+    }
 }
