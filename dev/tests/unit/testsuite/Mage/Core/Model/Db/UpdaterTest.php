3c922e40627ecbc5ec7cecb70393b6bf128193a2
Andrey Tserkus <andrey.tserkus@magento.com>
2013-02-14 19:03:29 -0800

    Fixed MAGETWO-7490: Ability to Disable Automatic Upgrade - implemented just unit test, as the functionality already exists

---
+++ dev/tests/unit/testsuite/Mage/Core/Model/Db/UpdaterTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+class Mage_Core_Model_Design_Db_UpdaterTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        /**
+         * Clear internal class static state to a default one, as it is not designed to be run several times
+         */
+        $reflectionProperty = new ReflectionProperty('Mage_Core_Model_Db_Updater', '_schemaUpdatesChecked');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue(null, false);
+    }
+
+    /**
+     * Automatic updates must be enabled/disabled according to config flags
+     *
+     * @dataProvider updateSchemeAndDataConfigDataProvider
+     */
+    public function testUpdateSchemeAndDataConfig($configXml, $isDevMode, $expectedUpdates)
+    {
+        // Configuration
+        $configuration = new Varien_Simplexml_Config($configXml);
+        $storage = $this->getMock('Mage_Core_Model_Config_Storage', array(), array(), '', false);
+        $storage->expects($this->any())
+            ->method('getConfiguration')
+            ->will($this->returnValue($configuration));
+        $modulesConfig = new Mage_Core_Model_Config_Modules($storage);
+
+        // Data updates model
+        $actualSqlUpdates = false;
+        $setSqlUpdates = function () use (&$actualSqlUpdates) {
+            $actualSqlUpdates = true;
+        };
+        $actualDataUpdates = false;
+        $setDataUpdates = function () use (&$actualDataUpdates) {
+            $actualDataUpdates = true;
+        };
+        $setupModel = $this->getMock('Mage_Core_Model_Resource_Setup', array(), array(), '', false);
+        $setupModel->expects($this->any())
+            ->method('applyUpdates')
+            ->will($this->returnCallback($setSqlUpdates));
+        $setupModel->expects($this->any())
+            ->method('applyDataUpdates')
+            ->will($this->returnCallback($setDataUpdates));
+
+        $factory = $this->getMock('Mage_Core_Model_Resource_SetupFactory', array(), array(), '', false);
+        $factory->expects($this->any())
+            ->method('create')
+            ->will($this->returnValue($setupModel));
+
+        // Application state
+        $appState = $this->getMock('Mage_Core_Model_App_State', array(), array(), '', false);
+        $appState->expects($this->any())
+            ->method('isInstalled')
+            ->will($this->returnValue(true));
+        $appState->expects($this->any())
+            ->method('isDeveloperMode')
+            ->will($this->returnValue($isDevMode));
+        $updater = new Mage_Core_Model_Db_Updater($modulesConfig, $factory, $appState);
+
+        // Run and verify
+        $updater->updateScheme();
+        $this->assertEquals($expectedUpdates, $actualSqlUpdates);
+
+        $updater->updateData();
+        $this->assertEquals($expectedUpdates, $actualDataUpdates);
+    }
+
+    public static function updateSchemeAndDataConfigDataProvider()
+    {
+        $fixturePath = __DIR__ . '/_files/';
+        return array(
+            'updates (default config)' => array(
+                file_get_contents($fixturePath . 'config.xml'),
+                false,
+                true
+            ),
+            'no updates when skipped' => array(
+                file_get_contents($fixturePath . 'config_skip_updates.xml'),
+                false,
+                false
+            ),
+            'updates when skipped, if in dev mode' => array(
+                file_get_contents($fixturePath . 'config_skip_updates.xml'),
+                true,
+                true
+            ),
+            'skipped updates, even in dev mode' => array(
+                file_get_contents($fixturePath . 'config_skip_updates_even_in_dev_mode.xml'),
+                true,
+                false
+            )
+        );
+    }
+}
