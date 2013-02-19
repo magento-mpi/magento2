65c83cf9d19a25d8dd7d8d352b6c34b40bb2b4d8
Igor Tripolskiy <itripolskiy@ebay.com>
2013-02-19 18:03:20 +0200

    Implemented MAGETWO-7702: User can not specify 'Allow Open Amount' for Gift Cards

--- app/code/core/Enterprise/GiftCard/Block/Adminhtml/Renderer/OpenAmount.php
+++ app/code/core/Enterprise/GiftCard/Block/Adminhtml/Renderer/OpenAmount.php
@@ -52,7 +52,12 @@ class Enterprise_GiftCard_Block_Adminhtml_Renderer_OpenAmount extends Varien_Dat
      */
     public function getElementHtml()
     {
-        $this->_element->setId($this->getHtmlId())->setName($this->getData('name'))->setChecked(true);
-        return $this->_element->getElementHtml() . ' ' . Mage::helper('Enterprise_GiftCard_Helper_Data')->__('allow');
+        $this->_element->setId($this->getHtmlId())->setName($this->getData('name'))
+            ->setChecked($this->getForm()->getDataObject()->getData('allow_open_amount'))
+            ->setValue(Enterprise_GiftCard_Model_Giftcard::OPEN_AMOUNT_ENABLED);
+        $hiddenField = '<input type="hidden" name="' . $this->getName()
+            . '" value="' . Enterprise_GiftCard_Model_Giftcard::OPEN_AMOUNT_DISABLED . '"/>';
+        return $hiddenField . $this->_element->getElementHtml()
+            . ' ' . Mage::helper('Enterprise_GiftCard_Helper_Data')->__('allow');
     }
 }
