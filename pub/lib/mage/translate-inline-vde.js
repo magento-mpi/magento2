7ffa0ee35ec64d1b14ef69b657d2072fc1fae179
Bryant Luk <bluk@ebay.com>
2013-02-25 12:02:06 -0600

    Fixed MAGETWO-7612: Inline Text Editor Text Translation
    - Fixes icon to be a bit smaller
    - Add some styling to buttons
    - Updates CSS stylesheet names
    - Sets the value of the text field after saving
    - When resizing browser window, change location of icon
    - Hit ESC in text input cancels popup
    - Replaces the text in the DOM with the submitted text
    - Refactors code to be compliant with coding standards

--- pub/lib/mage/translate-inline-vde.js
+++ pub/lib/mage/translate-inline-vde.js
@@ -8,105 +8,235 @@
  */
 /*jshint browser:true jquery:true */
 (function($) {
-    $.widget("mage.translateInlineIconVde", {
+    $.widget("mage.translateInlineDialogVde", {
         options: {
-          img: '',
-          offsetLeft: -33,
-          template: '<img src="${img}">',
-          translateForm: {
-              template: '<form id="${data.id}">{{each(i, item) data.items}}' +
-                  '<input id="perstore_${i}" name="translate[${i}][perstore]" type="hidden" value="0"/>' +
-                  '<input name="translate[${i}][original]" type="hidden" value="${item.scope}::${escape(item.original)}"/>' +
-                  '<input id="custom_${i}" name="translate[${i}][custom]" value="${escape(item.translated)}" />' +
-                  '{{/each}}</form>',
-              data: {
-                  id: 'translate-inline-form',
-                  message: 'Please refresh the page to see your changes after submitting this form.'
-              }
-          },
+            translateForm: {
+                template: '<form id="${data.id}">{{each(i, item) data.items}}' +
+                    '<input id="perstore_${i}" name="translate[${i}][perstore]" type="hidden" value="0"/>' +
+                    '<input name="translate[${i}][original]" type="hidden"' +
+                    ' value="${item.scope}::${escape(item.original)}"/>' +
+                    '<input id="custom_${i}" name="translate[${i}][custom]" value="${escape(item.translated)}"' +
+                    ' class="translate-dialog-translate-value-input"/>' +
+                    '{{/each}}</form>',
+                data: {
+                    id: "translate-inline-form"
+                }
+            },
+            dialog: {
+                id: "translate-dialog",
+                autoOpen : false,
+                dialogClass: "translate-dialog",
+                draggable: false,
+                modal: false,
+                resizable: false,
+                height: "auto",
+                minHeight: 0,
+                buttons: [{
+                    text: "Cancel",
+                    "class" : "translate-dialog-cancel",
+                },
+                {
+                    text: "Save",
+                    "class" : "translate-dialog-save",
+                }]
+            },
+            templateName: "translateInlineDialogVdeTemplate",
+            dataAttrName: "translate",
+            onSubmitComplete: function() { },
+            onCancel: function() { }
         },
 
         /**
-         * Translate Inline creation
-         * @protected
+         * Creates the translation dialog widget. Fulfills jQuery WidgetFactory _create hook.
          */
         _create: function() {
-            var offset = this.element.offset();
-            this.template = $.tmpl(this.options.template, this.options).css({
-              position: 'absolute',
-              cursor: 'pointer',
-              'z-index': 2000,
-              top: offset.top,
-              left: offset.left + this.element.outerWidth() + this.options.offsetLeft
-            }).appendTo('body');
+	          $.template(this.options.templateName, this.options.translateForm.template);
+            this.translateDialog = jQuery("<div />", { id: this.options.dialog.id })
+                .prependTo("body")
+                .dialog($.extend(true, {
+                    buttons : [
+                        {
+                            click: $.proxy(this.close, this)
+                        },
+                        {
+                            click: $.proxy(this._formSubmit, this)
+                        }
+                    ]
+                }, this.options.dialog));
+        },
 
-            var self = this;
-            this.template.on('click', function() {
-                $.template("translateInlineVde", self.options.translateForm.template);
+        /**
+         * Opens the dialog.
+         *
+         * @param {Element} element the element to open the dialog near,
+         *     must also contain data-translate attribute
+         */
+        open: function(element) {
+            this.translateElement = element;
 
-                $('#translateDialog')
-                    .html($.tmpl("translateInlineVde", {
-                    data: $.extend({items: $(self.element).data('translate')},
-                        self.options.translateForm.data),
+            this._fillDialogContent(element);
+            this._positionDialog(element);
+            this.translateDialog.dialog("open");
+        },
+
+        /**
+         * Closes the dialog.
+         */
+        close: function() {
+            this.translateDialog.dialog("close");
+            this.options.onCancel();
+        },
+
+        /**
+         * Fills the main dialog content. Replaces the dialog content with a
+         * form with translation data.
+         *
+         * @param {Element} element the element to get the translation data from
+         */
+        _fillDialogContent: function(element) {
+            this.translateDialog
+                .html($.tmpl(this.options.templateName, {
+                    data: $.extend({items: $(element).data(this.options.dataAttrName)},
+                        this.options.translateForm.data),
                     escape: $.mage.escapeHTML
                 }));
 
-                $("#translateDialog").dialog("option", {
-                    position: { of : self.element, my: "left top", at: "left-3 top-3" },
-                    width: self.element.width(),
-                    height: "auto",
-                    minHeight : 0,
-                    buttons: [
-                        {
-                            text: "Cancel",
-                            click: function() {
-                                $('body').addClass('trnslate-inline-area');
-                                $("#translateDialog").dialog('close');
-                                $('[data-translate]').translateInlineIconVde('show');
-                            }
-                        },
-                        {
-                            id: "saveTranslate",
-                            text: "Save",
-                            click: $.proxy(self._formSubmit, self),
-                        }
-                    ]
+            var self = this;
+            this.translateDialog.find("input.translate-dialog-translate-value-input").each(function(count, input) {
+                /* discard changes if pressing esc */
+                $(input).keydown(function(e) {
+                    if (e.keyCode == 27) {
+                        e.preventDefault();
+                        $.proxy(self.close, self)();
+                    }
                 });
-                $('body').removeClass('trnslate-inline-area');
-                $('[data-translate]').translateInlineIconVde('hide');
-                $("#translateDialog").dialog("open");
             });
         },
 
+        /**
+         * Positions the dialog relative to the element.
+         *
+         * @param {Element} element the element to position the dialog near
+         */
+        _positionDialog: function(element) {
+            this.translateDialog.dialog("option", {
+                position: { of : element, my: "left top", at: "left-3 top-9" },
+                width: $(element).width(),
+            });
+        },
+
+        /**
+         * Submits the form.
+         */
+        _formSubmit: function() {
+            var parameters = jQuery.param({area: this.options.area}) +
+                "&" + jQuery("#" + this.options.translateForm.data.id).serialize();
+            $.ajax({
+                url: this.options.ajaxUrl,
+                type: "POST",
+                data: parameters,
+                context: this.translateDialog,
+                showLoader: true
+            }).complete($.proxy(this._formSubmitComplete, this));
+        },
+
+        /**
+         * Callback for when the AJAX call in _formSubmit is completed.
+         */
+        _formSubmitComplete: function() {
+            var self = this;
+            this.translateDialog.find("input").each(function(count, elem) {
+                var id = elem.id;
+                if (id.indexOf("custom_") === 0) {
+                   var index = id.substring(7), 
+                       value = elem.value,
+                       translateData = $(self.translateElement).data("translate"),
+                       innerHtmlStr = $(self.translateElement).html();
+
+                   innerHtmlStr =  innerHtmlStr.replace(translateData[index]["shown"], value);
+                   $(self.translateElement).html(innerHtmlStr);
+
+                   translateData[index]["shown"] = value;
+                   translateData[index]["translated"] = value;
+                   $(self.translateElement).attr("data-translate", JSON.stringify(translateData));
+                }
+            });
+
+            this.translateDialog.dialog("close");
+
+            this.options.onSubmitComplete();
+        }
+    });
+
+    $.widget("mage.translateInlineIconVde", {
+        options: {
+            img: null,
+            area: "vde",
+            ajaxUrl: null,
+            offsetLeft: -16,
+            template: '<img src="${img}" height="16" width="16">',
+            onClick: function(element) { },
+        },
+
+        /**
+         * Creates the icon widget to indicate text that can be translated.
+         * Fulfills jQuery's WidgetFactory _create hook.
+         */
+        _create: function() {
+            this._initTemplate();
+
+            $(window).on("resize", $.proxy(this._positionTemplate, this));
+        },
+
+        /**
+         * Shows the widget.
+         */
+        show: function() {
+            this._positionTemplate();
+            this.template.show();
+        },
+
+        /**
+         * Hides the widget.
+         */
         hide: function() {
             this.template.hide();
         },
 
-        show: function() {
-            this.template.show();
+        /**
+         * Initializes the template for the widget. Sets the widget up to
+         * respond to events.
+         */
+        _initTemplate: function() {
+            this.template = $.tmpl(this.options.template, this.options).css({
+	              position: "absolute",
+                cursor: "pointer",
+                "z-index": 2000,
+            }).appendTo("body");
+            this._positionTemplate();
+
+            var self = this;
+            this.template.on("click", function() {
+                self.options.onClick(self.element);
+            });
         },
 
+        /**
+         * Positions the template to the correct location.
+         */
+        _positionTemplate: function() {
+            var offset = this.element.offset();
+            this.template.css({
+                top: offset.top,
+                left: offset.left + this.element.outerWidth() + this.options.offsetLeft
+            });
+        },
+
+        /**
+         * Destroys the widget. Fulfills jQuery's WidgetFactory _destroy hook.
+         */
         _destroy: function() {
             this.template.remove();
-        },
-
-        _formSubmit: function() {
-            console.log("hello");
-
-            var parameters = jQuery.param({area: this.options.area}) +
-                '&' + jQuery('#' + this.options.translateForm.data.id).serialize();
-
-            jQuery.ajax({
-                url: this.options.ajaxUrl,
-                type: 'POST',
-                data: parameters,
-                context: $('#translateDialog'),
-                showLoader: true
-            }).complete(jQuery.proxy(function() {
-                $('body').addClass('trnslate-inline-area');
-                $("#translateDialog").dialog('close');
-                $('[data-translate]').translateInlineIconVde('show');
-            }, this));
         }
     });
 
@@ -117,9 +247,10 @@
         mage: {
             escapeHTML: function(str) {
                 return str ?
-                    jQuery('<div/>').text(str).html().replace(/"/g, '&quot;'):
+                    jQuery("<div/>").text(str).html().replace(/"/g, "&quot;"):
                     false;
             }
         }
     });
 })(jQuery);
+
