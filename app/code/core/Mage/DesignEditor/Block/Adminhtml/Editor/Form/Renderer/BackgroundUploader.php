2135f6ddfbf7b534eec0ca5d740b7ef5e5143f67
Maxim Shikula <mshikula@ebay.com>
2013-02-25 16:19:17 +0200

    Implemented MAGETWO-7507: Upload and Delete Quick Style Images

--- app/code/core/Mage/DesignEditor/Block/Adminhtml/Editor/Form/Renderer/BackgroundUploader.php
+++ app/code/core/Mage/DesignEditor/Block/Adminhtml/Editor/Form/Renderer/BackgroundUploader.php
@@ -15,4 +15,28 @@ class Mage_DesignEditor_Block_Adminhtml_Editor_Form_Renderer_BackgroundUploader
     extends Mage_DesignEditor_Block_Adminhtml_Editor_Form_Renderer
 {
     protected $_template = 'Mage_DesignEditor::editor/form/renderer/background-uploader.phtml';
+
+    /**
+     * Get image upload url
+     *
+     * @return string
+     */
+    public function getImageUploadUrl()
+    {
+        return $this->getUrl('*/system_design_editor_tools/uploadQuickStyleImage',
+            array('theme_id' => Mage::registry('theme')->getId())
+        );
+    }
+
+    /**
+     * Get image upload url
+     *
+     * @return string
+     */
+    public function getImageRemoveUrl()
+    {
+        return $this->getUrl('*/system_design_editor_tools/removeQuickStyleImage',
+            array('theme_id' => Mage::registry('theme')->getId())
+        );
+    }
 }
