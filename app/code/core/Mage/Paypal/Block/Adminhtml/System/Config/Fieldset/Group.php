7ef44dfb17561cd6d87fcf9502265e787dfd9100
Anton Voitovych <avoitovych@gmail.com>
2013-02-26 17:05:52 +0200

    Fixed MPI-823: [DEV] Payments configuration section loads without content

--- app/code/core/Mage/Paypal/Block/Adminhtml/System/Config/Fieldset/Group.php
+++ app/code/core/Mage/Paypal/Block/Adminhtml/System/Config/Fieldset/Group.php
@@ -43,7 +43,7 @@ class Mage_Paypal_Block_Adminhtml_System_Config_Fieldset_Group
      */
     protected function _isCollapseState($element)
     {
-        $extra = Mage::getSingleton('Mage_Backend_Model_Session')->getUser()->getExtra();
+        $extra = Mage::getSingleton('Mage_Backend_Model_Auth_Session')->getUser()->getExtra();
         if (isset($extra['configState'][$element->getId()])) {
             return $extra['configState'][$element->getId()];
         }
