607bb449b617948f4db292c59f5badc5689d2f75
Andrey Tserkus <andrey.tserkus@magento.com>
2013-03-05 11:49:47 -0800

    Fixed MAGETWO-8101: Production Mode for Static View Files - added back Theme module helper, because it is needed as default helper for the module (e.g. in backend menu item)

---
+++ app/code/core/Mage/Theme/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Theme
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Theme module helper
+ */
+class Mage_Theme_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
