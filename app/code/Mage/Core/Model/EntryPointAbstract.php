b3229c2c5ab084334a99f09c85d76134c773ad37
Anton Kril <akril@ebay.com>
2013-03-11 18:37:56 +0200

    Fixed MAGETWO-7620: Stack Trace when Generation of Proxy Classes Fails

--- app/code/Mage/Core/Model/EntryPointAbstract.php
+++ app/code/Mage/Core/Model/EntryPointAbstract.php
@@ -22,13 +22,18 @@ abstract class Mage_Core_Model_EntryPointAbstract
      */
     public function __construct(Mage_Core_Model_Config_Primary $config, Magento_ObjectManager $objectManager = null)
     {
-        if (!$objectManager) {
-            $definitionFactory = new Mage_Core_Model_ObjectManager_DefinitionFactory();
-            $definitions =  $definitionFactory->create($config);
-            $objectManager = new Mage_Core_Model_ObjectManager($definitions, $config);
+        try {
+            if (!$objectManager) {
+                $definitionFactory = new Mage_Core_Model_ObjectManager_DefinitionFactory();
+                $definitions =  $definitionFactory->create($config);
+                $objectManager = new Mage_Core_Model_ObjectManager($definitions, $config);
+            }
+            $this->_objectManager = $objectManager;
+            Mage::setObjectManager($objectManager);
+        } catch (Magento_BootstrapException $e) {
+            header('Content-Type: text/plain', true, 503);
+            die($e->getMessage());
         }
-        $this->_objectManager = $objectManager;
-        Mage::setObjectManager($objectManager);
     }
 
     /**
