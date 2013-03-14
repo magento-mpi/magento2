0d5c0b73df8a85addb52fc4445318c234475c014
Vijay Sriram <vsriram@x.com>
2013-03-14 13:33:30 -0700

    Fixed MAGETWO-8138 : Convert inline prototype code for paypal shortcut in product detail page to jquery widget -Code cleanup

--- app/code/Mage/Paypal/view/frontend/paypal-checkout.js
+++ app/code/Mage/Paypal/view/frontend/paypal-checkout.js
@@ -17,15 +17,14 @@
         _create: function() {
             this.element.on('click', $.proxy(function(e) {
                 var returnUrl = $(e.target).parent().prop("href");
-                if (this.options.confirmUrl.length > 0) {
+                if (this.options.confirmUrl) {
                     if (confirm(this.options.confirmMessage)) {
                         returnUrl = this.options.confirmUrl;
                     }
                 }
-                if (this.options.isCatalogProduct === '1') {
+                if (this.options.isCatalogProduct == 1) {
                     $(this.options.paypalCheckoutSelector).val(returnUrl);
                     $(this.options.productAddToCartForm).submit();
-                    e.stopPropagation();
                 }
             }, this));
         }
