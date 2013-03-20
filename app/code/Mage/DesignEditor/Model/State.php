8d65b3992e50f7db20e55e796bafa30892bed108
Andrey Voinkov <avoinkov@ebay.com>
2013-03-20 17:48:05 +0200

    Implemented MAGETWO-8462: Images in quick_style.css Not Present in Filesystemes

--- app/code/Mage/DesignEditor/Model/State.php
+++ app/code/Mage/DesignEditor/Model/State.php
@@ -233,6 +233,7 @@ class Mage_DesignEditor_Model_State
     {
         $themeId = $this->_backendSession->getData(self::CURRENT_THEME_SESSION_KEY);
         if ($themeId !== null) {
+            echo $themeId.'<br />' . PHP_EOL;
             $this->_application->getStore()->setConfig(Mage_Core_Model_Design_Package::XML_PATH_THEME_ID, $themeId);
         }
     }
