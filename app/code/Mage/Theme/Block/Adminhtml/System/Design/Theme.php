840ffd9ddeff04540d8367373231da04107bdaae
Sergey Shkolyarenko <sshkolyarenko@ebay.com>
2013-03-21 17:01:12 +0200

    Fixed MAGETWO-4181: Reskin Admin Application Small fix for System_Design_Theme block

--- app/code/Mage/Theme/Block/Adminhtml/System/Design/Theme.php
+++ app/code/Mage/Theme/Block/Adminhtml/System/Design/Theme.php
@@ -22,9 +22,8 @@ class Mage_Theme_Block_Adminhtml_System_Design_Theme extends Mage_Backend_Block_
         $this->_blockGroup = 'Mage_Theme';
         $this->_controller = 'Adminhtml_System_Design_Theme';
         if (!$this->getLayout()->getBlock('page-title')) {
-            $this->getLayout()->addBlock('Mage_Page_Block_Html_Title', 'page-title', 'content');
+            $this->getLayout()->getBlock('page-title')->setPageTitle('Themes');
         }
-        $this->getLayout()->getBlock('page-title')->setPageTitle('Themes');
         $this->_updateButton('add', 'label', $this->__('Add New Theme'));
     }
 
