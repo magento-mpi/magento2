04cef42445c781c721a872a50bb276ea15765430
Sergey Shymko <sergey.shymko@magento.com>
2013-03-22 10:13:08 -0700

    Implemented MAGETWO-8271: Tool for Materializing All Static View Files for Production Mode - implemented the minimal command line interface for static view files deployment to be used by the SaaS infrastructure

---
+++ saas/deploy_view_files.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * A command line tool that pre-populates static view files for the production mode with minimal filesystem lookup.
+ * Interface of the tool is invariant, SaaS infrastructure relies on it when deploying a tenant on any Magento version.
+ *
+ * {license_notice}
+ *
+ * @copyright  {copyright}
+ * @license    {license_link}
+ */
+
+define('SYNOPSIS', <<<USAGE
+Usage: php -f generate_view_files.php -- --destination <dir>
+
+    --destination <dir> absolute directory path to deploy static view files to
+
+USAGE
+);
+
+$options = getopt('', array('destination:'));
+if (empty($options['destination'])) {
+    echo SYNOPSIS;
+    exit(1);
+}
+
+$command = sprintf(
+    'php -f %s -- --destination %s',
+    escapeshellarg(dirname(__DIR__) . '/dev/tools/view/generator.php'),
+    escapeshellarg($options['destination'])
+);
+passthru($command, $exitCode);
+exit($exitCode);
