8b8bd1934fa84d35cdc5440d6e47cde4a85ee160
Denys Rudchenko <drudchenko@ebay.com>
2013-03-28 19:09:19 +0200

    CR changes MAGETWO-8097: Fix 2-level Cache Backend in Magento Go 2.x - fixed integration test.

--- dev/tests/integration/testsuite/Mage/Core/Model/Cache/Frontend/PoolTest.php
+++ dev/tests/integration/testsuite/Mage/Core/Model/Cache/Frontend/PoolTest.php
@@ -12,24 +12,47 @@ class Mage_Core_Model_Cache_Frontend_PoolTest extends PHPUnit_Framework_TestCase
      */
     protected $_model;
 
+    /*
+     * @var Mage_Core_Model_Config_Primary
+     */
+    protected $_configPrimary;
+
     public function setUp()
     {
-        $this->_model = Mage::getModel('Mage_Core_Model_Cache_Frontend_Pool');
+        $this->_configPrimary = Mage::getSingleton('Mage_Core_Model_Config_Primary');
+        $this->_model = new Mage_Core_Model_Cache_Frontend_Pool(
+            $this->_configPrimary,
+            Mage::getModel('Mage_Core_Model_Cache_Frontend_Factory')
+        );
     }
 
-    public function testDbCacheAdapter()
+    /**
+     * @dataProvider cacheBackendsDataProvider
+     */
+    public function testDbCacheAdapter($cacheBackendName)
     {
-        $config = Mage::getSingleton('Mage_Core_Model_Config_Primary');
-        $frontendNode = $config->getNode(Mage_Core_Model_Cache_Frontend_Pool::XML_PATH_SETTINGS_DEFAULT);
-        $frontendOptions = $frontendNode ? $frontendNode->asArray() : array();
-        if (
-            !((isset($frontendOptions['backend']) && $frontendOptions['backend'] == 'database')
-            || (isset($frontendOptions['slow_backend']) && $frontendOptions['slow_backend'] == 'database'))
-        ) {
-            $this->markTestSkipped('Backend cache adapters is not \'database\'.');
-        }
+        $cacheTypePath = Mage_Core_Model_Cache_Frontend_Pool::XML_PATH_SETTINGS_DEFAULT . '/backend';
+        $oldCacheBackend = (string)$this->_configPrimary->getNode($cacheTypePath);
+        $this->_configPrimary->setNode($cacheTypePath, $cacheBackendName);
 
         $cache = $this->_model->get(Mage_Core_Model_Cache_Frontend_Pool::DEFAULT_FRONTEND_ID);
         $this->assertInstanceOf('Magento_Cache_FrontendInterface', $cache);
+        $this->assertInstanceOf('Zend_Cache_Backend_Interface', $cache->getBackend());
+
+        $this->_configPrimary->setNode($cacheTypePath, $oldCacheBackend);
+    }
+
+    public function cacheBackendsDataProvider()
+    {
+        return array(
+            array('sqlite'),
+            array('memcached'),
+            array('apc'),
+            array('xcache'),
+            array('eaccelerator'),
+            array('database'),
+            array('File'),
+            array('')
+        );
     }
 }
