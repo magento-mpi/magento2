98b87ff96223133bfcadff7cc71efa67723e3b85
Oleh Kobchenko <okobchenko@ebay.com>
2013-03-28 15:21:22 +0200

    Fixed MAGETWO-8343: Language selector for My Account page - CR changes

--- dev/tests/unit/testsuite/Mage/Core/Model/Locale/ValidatorTest.php
+++ dev/tests/unit/testsuite/Mage/Core/Model/Locale/ValidatorTest.php
@@ -3,7 +3,7 @@
  * {license_notice}
  *
  * @category    Magento
- * @package     Mage_Backend
+ * @package     Mage_Core
  * @subpackage  unit_tests
  * @copyright   {copyright}
  * @license     {license_link}
