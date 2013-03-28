fc863b9749b0f753c462c811d8dcab9f4dc6be6a
darja.baranova <6GroggIrcA>
2013-03-28 13:14:37 +0200

    Implemented MAGETWO-8608: Implement Flush Cache task

---
+++ app/code/Saas/Saas/Model/Cache.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Saas cache model
+ *
+ * {license_notice}
+ *
+ * @copyright {copyright}
+ * @license {license_link}
+ */
+class Saas_Saas_Model_Cache extends Mage_Core_Model_Cache
+{
+    /**
+     * @var Mage_Core_Model_Event_Manager
+     */
+    protected $_eventManager;
+
+    /**
+     * @param Magento_ObjectManager $objectManager
+     * @param Mage_Core_Model_Cache_Frontend_Pool $frontendPool
+     * @param Mage_Core_Model_Cache_Types $cacheTypes
+     * @param Mage_Core_Model_ConfigInterface $config
+     * @param Mage_Core_Model_Dir $dirs
+     * @param Mage_Core_Model_Factory_Helper $helperFactory
+     * @param Mage_Core_Model_Event_Manager $eventManager
+     */
+    public function __construct(
+        Magento_ObjectManager $objectManager,
+        Mage_Core_Model_Cache_Frontend_Pool $frontendPool,
+        Mage_Core_Model_Cache_Types $cacheTypes,
+        Mage_Core_Model_ConfigInterface $config,
+        Mage_Core_Model_Dir $dirs,
+        Mage_Core_Model_Factory_Helper $helperFactory,
+        Mage_Core_Model_Event_Manager $eventManager
+    )
+    {
+        $this->_eventManager = $eventManager;
+
+        parent::__construct($objectManager, $frontendPool, $cacheTypes, $config, $dirs, $helperFactory);
+    }
+
+    /**
+     * Refresh cache
+     *
+     * @param array|string $typeCode
+     * @return Mage_Core_Model_CacheInterface
+     */
+    public function invalidateType($typeCode)
+    {
+        if (!is_array($typeCode)) {
+            $typeCode = array($typeCode);
+        }
+
+        $this->_eventManager->dispatch('refresh_cache', array('cache_types' => $typeCode));
+
+        return $this->_callOriginInvalidateType($typeCode);
+    }
+
+    /**
+     * @param array $typeCode
+     * @return Mage_Core_Model_CacheInterface
+     */
+    protected function  _callOriginInvalidateType($typeCode)
+    {
+        return parent::invalidateType($typeCode);
+    }
+}
---
+++ dev/tests/unit/testsuite/Saas/Saas/Model/CacheTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright {copyright}
+ * @license {license_link}
+ */
+class Saas_Saas_Model_CacheTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_eventManagerMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_modelCacheMock;
+
+    protected function setUp()
+    {
+        $this->_eventManagerMock = $this->getMockBuilder('Mage_Core_Model_Event_Manager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->_modelCacheMock = $this->getMockBuilder('Saas_Saas_Model_Cache')
+            ->setConstructorArgs(array(
+                $this->getMock('Magento_ObjectManager', array(), array(), '', false),
+                $this->getMock('Mage_Core_Model_Cache_Frontend_Pool', array(), array(), '', false),
+                $this->getMock('Mage_Core_Model_Cache_Types', array(), array(), '', false),
+                $this->getMock('Mage_Core_Model_ConfigInterface', array(), array(), '', false),
+                $this->getMock('Mage_Core_Model_Dir', array(), array(), '', false),
+                $this->getMock('Mage_Core_Model_Factory_Helper', array(), array(), '', false),
+                $this->_eventManagerMock,
+            ))
+            ->setMethods(array('_callOriginInvalidateType'))
+            ->getMock();
+    }
+
+    public function testInvalidateType()
+    {
+        $this->_eventManagerMock->expects($this->once())->method('dispatch')->with($this->equalTo('refresh_cache'));
+        $this->_modelCacheMock->expects($this->once())->method('_callOriginInvalidateType')->will($this->returnSelf());
+
+        $this->assertEquals($this->_modelCacheMock, $this->_modelCacheMock->invalidateType('test'));
+    }
+}
