30a1256b865c2b487e45b8356ec6067f5f1412e6
Evgeniy Kolesov <evgeniy.kolesov@magento.com>
2013-03-29 13:39:30 +0200

    Fixed MAGETWO-4176: Reference Store Theme - Fixed test failures

--- pub/lib/mage/zoom.js
+++ pub/lib/mage/zoom.js
@@ -1,24 +1,10 @@
 /**
- * Magento Go
+ * {license_notice}
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Magento Go License Agreement.
- * The licenses is available through the world-wide-web at this URL:
- * http://www.magentocommerce.com/license/Go.
- * If you are unable to obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    design
- * @package     saas
- * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://www.magentocommerce.com/license/Go
+ * @category    Mage
+ * @package     js
+ * @copyright   {copyright}
+ * @license     {license_link}
  */
 
 /**
