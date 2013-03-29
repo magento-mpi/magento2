04848d7d059ca4b28ade86ed267e33f4b74088f9
Bibu Mathew <bimathew@paypal.com>
2013-03-29 11:35:24 -0500

    code review updates for MAGETWO-8699

--- app/code/Mage/Captcha/view/frontend/onepage.js
+++ app/code/Mage/Captcha/view/frontend/onepage.js
@@ -10,7 +10,7 @@
 (function($, window, document) {
     "use strict";
     $(document).on("login",function() {
-        $("[role='guest_checkout'], [role='register_during_checkout']").hide();
+        $("[data-captcha='guest_checkout'], [data-captcha='register_during_checkout']").hide();
         var type = ($("#login\\:guest").is(':checked')) ? 'guest_checkout' : 'register_during_checkout';
         $("[role='" + type + "']").show();
     }).on('billingSave', function() {
