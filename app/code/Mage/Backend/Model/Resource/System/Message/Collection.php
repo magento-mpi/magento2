8fc48d582850a066a8a12c0918992ebc71aba680
Anton Kril <akril@ebay.com>
2013-04-02 14:10:38 +0300

    Implemented MAGETWO-5747: Admin notification redesign - system messages

--- app/code/Mage/Backend/Model/Resource/System/Message/Collection.php
+++ app/code/Mage/Backend/Model/Resource/System/Message/Collection.php
@@ -15,6 +15,12 @@ class Mage_Backend_Model_Resource_System_Message_Collection extends Mage_Core_Mo
      */
     protected $_messageList;
 
+    /**
+     * Number of messages by severity
+     *
+     * @var array
+     */
+    protected $_countBySeverity = array();
 
     /**
      * @param Mage_Backend_Model_System_MessageList $messageList
@@ -42,5 +48,49 @@ class Mage_Backend_Model_Resource_System_Message_Collection extends Mage_Core_Mo
         parent::_initSelect();
         $this->addOrder('severity', self::SORT_ORDER_ASC)
             ->addOrder('created_at');
+    }
+
+    /**
+     * @return Mage_Core_Model_Resource_Db_Abstract
+     */
+    protected function _afterLoad()
+    {
+        foreach ($this->_items as $key => $item) {
+            $message = $this->_messageList->getMessageByIdentity($item->getIdentity());
+            if ($message) {
+                $item->setText($message->getText());
+                $item->setLink($message->getLink());
+                if (array_key_exists($message->getSeverity(), $this->_countBySeverity)) {
+                    $this->_countBySeverity[$message->getSeverity()]++;
+                } else {
+                    $this->_countBySeverity[$message->getSeverity()] = 1;
+                }
+            } else {
+                unset($this->_items[$key]);
+            }
+        }
+    }
+
+    /**
+     * Set message severity filter
+     *
+     * @param $severity
+     * @return Mage_Core_Model_Resource_Db_Abstract
+     */
+    public function setSeverity($severity)
+    {
+        $this->addFieldToFilter('severity', array('eq' => $severity * 1));
+        return $this;
+    }
+
+    /**
+     * Retrieve number of messages by severity
+     *
+     * @param int $severity
+     * @return int
+     */
+    public function getCountBySeverity($severity)
+    {
+        return isset($this->_countBySeverity[$severity]) ? $this->_countBySeverity[$severity] : 0;
     }
 }
