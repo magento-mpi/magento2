37392a705da5c57fcbad1acbafed4aa5c987a9eb
Andrii Kasian <akasian@ebay.com>
2013-04-03 08:31:12 -0400

    Fixed MAGETWO-5467: Integration tests Enterprise_GiftCardAccount_Block_Adminhtml_Giftcardaccount_Edit_Tab_Info/SendTest break separate tests execution

--- dev/tests/integration/testsuite/Enterprise/GiftCardAccount/Block/Adminhtml/Giftcardaccount/Edit/Tab/InfoTest.php
+++ dev/tests/integration/testsuite/Enterprise/GiftCardAccount/Block/Adminhtml/Giftcardaccount/Edit/Tab/InfoTest.php
@@ -17,20 +17,22 @@ class Enterprise_GiftCardAccount_Block_Adminhtml_Giftcardaccount_Edit_Tab_InfoTe
     /** @var Enterprise_GiftCardAccount_Block_Adminhtml_Giftcardaccount_Edit_Tab_Info */
     protected $_block;
 
-    public static function setUpBeforeClass()
-    {
-        parent::setUpBeforeClass();
-        $model = Mage::getModel('Enterprise_GiftCardAccount_Model_Giftcardaccount');
-        Mage::register('current_giftcardaccount', $model);
-    }
-
-    public function setUp()
+    protected function setUp()
     {
         parent::setUp();
-        $layout = Mage::getModel('Mage_Core_Model_Layout');
 
+        $model = Mage::getModel('Enterprise_GiftCardAccount_Model_Giftcardaccount');
+        Mage::register('current_giftcardaccount', $model);
+
+        $layout = Mage::getModel('Mage_Core_Model_Layout');
         $this->_block = $layout
             ->createBlock('Enterprise_GiftCardAccount_Block_Adminhtml_Giftcardaccount_Edit_Tab_Info');
+    }
+
+    protected function tearDown()
+    {
+        Mage::unregister('current_giftcardaccount');
+        parent::tearDown();
     }
 
     /**
