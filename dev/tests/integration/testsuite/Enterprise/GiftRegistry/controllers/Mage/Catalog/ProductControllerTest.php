1cf5cc536cf24f15927ac0cd47b0c53201cfaed4
Olga Kopylova <okopylova@ebay.com>
2013-04-04 22:14:11 -0700

    Fixed MAGETWO-8829: Remove Dependency on Enterprise_GiftRegistry in app/design/frontend/enterprise/fixed/local.xml - moved test to the design scope

--- dev/tests/integration/testsuite/Enterprise/GiftRegistry/controllers/Mage/Catalog/ProductControllerTest.php
+++ dev/tests/integration/testsuite/Enterprise/GiftRegistry/controllers/Mage/Catalog/ProductControllerTest.php
@@ -19,17 +19,4 @@ class Enterprise_GiftRegistry_Mage_Catalog_ProductControllerTest extends Magento
         $this->assertContains('<span>Add to Gift Registry</span>', $body);
         $this->assertContains('http://localhost/index.php/giftregistry/index/cart/', $body);
     }
-
-    /**
-     * @magentoDataFixture Mage/Bundle/_files/product.php
-     */
-    public function testViewActionBundle()
-    {
-        $this->getRequest()->setParam('options', Enterprise_GiftRegistry_Block_Product_View::FLAG);
-        $this->dispatch('catalog/product/view/id/3');
-        $body = $this->getResponse()->getBody();
-        $this->assertContains('<span>Customize and Add to Gift Registry</span>', $body);
-        $this->assertContains('<span>Add to Gift Registry</span>', $body);
-        $this->assertContains('http://localhost/index.php/giftregistry/index/cart/', $body);
-    }
 }
