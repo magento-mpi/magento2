bec27258b066f619d145133afcec4282d2b2203e
grygorii.gerashchenko <ggerashchenko@ebay.com>
2013-04-15 19:26:23 +0300

    Fix MAGETWO-9250: [TAF] Fix fails from COREDEV-FUNCTIONAL54-1

---
+++ dev/tests/functional/testsuite/Saas/Mage/Product/Create/ConfigurableTest.php
@@ -0,0 +1,31 @@
+<?php
+    /**
+     * {license_notice}
+     *
+     * @category    Magento
+     * @package     Mage_Product
+     * @subpackage  functional_tests
+     * @copyright   {copyright}
+     * @license     {license_link}
+     */
+
+    /**
+     * Configurable product creation tests
+     *
+     * @package     selenium
+     * @subpackage  tests
+     * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+     */
+class Saas_Mage_Product_Create_ConfigurableTest extends Core_Mage_Product_Create_ConfigurableTest
+{
+    /**
+     * <p>Creating Configurable product with Downloadable product</p>
+     *
+     * @test
+     * @TestlinkId TL-MAGE-3363
+     */
+    public function configurableWithDownloadableProduct()
+    {
+        $this->markTestIncomplete('Functionality is absent in Magento Go.');
+    }
+}
