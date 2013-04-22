ee347484099c7ee9b24ed12fb57fa7210e4181fc
Evgeniy Kolesov <evgeniy.kolesov@magento.com>
2013-04-22 08:05:15 +0300

    Fixed MAGETWO-4176: Reference Store Theme - Products compare - Tax row in order review

---
+++ app/design/frontend/magento2/reference/Mage_Catalog/js/list.js
@@ -0,0 +1,48 @@
+/**
+ * {license_notice}
+ *
+ * @category    mage compare list
+ * @package     mage
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/*jshint browser:true jquery:true*/
+(function ($, window) {
+    $.widget('mage.compareList', {
+        _create: function() {
+
+            var products = $('thead td', this.element);
+
+            if (products.length > this.options.productsInRow) {
+                //var headings = $('<table></table>').addClass('comparison headings data table').insertBefore(this.element.closest('.container'));
+                var headings = $('<table></table>').addClass('comparison headings data table').insertBefore(this.element);
+                this.element.addClass('scroll');
+                $('th',this.element).each(function(){
+                    //console.log(el);
+                    var some = $(this).clone();
+                    var height = $(this).height();
+                    some.css('height', function() {
+                        return height;
+                    });
+
+                    $(some).appendTo(headings);
+                    $(some).wrap('<tr />');
+                    //this.hide();
+                });
+            }
+
+            $(this.options.windowPrintSelector).on('click', function(e) {
+                e.preventDefault();
+                window.print();
+            });
+
+            $.each(this.options.selectors, function(i, selector) {
+                $(selector).on('click', function(e) {
+                    e.preventDefault();
+                    window.location.href = $(this).data('url');
+                });
+            });
+
+        }
+    });
+})(jQuery, window);
