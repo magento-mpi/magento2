44e700a499064d898f25022f67e968cb91a14c9e
Sergiy Akulinin <sakulinin@ebay.com>
2013-04-23 17:55:21 +0300

    Fixed MPI-993: "Use Default" is always checked in PayPal configuration on website scope

--- app/code/Mage/Backend/Block/System/Config/Form.php
+++ app/code/Mage/Backend/Block/System/Config/Form.php
@@ -291,9 +291,9 @@ class Mage_Backend_Block_System_Config_Form extends Mage_Backend_Block_Widget_Fo
             if ($element instanceof Mage_Backend_Model_Config_Structure_Element_Group) {
                 $this->_initGroup($element, $section, $fieldset);
             } else {
-                $path = $element->getPath($fieldPrefix);
-                if ($element->getSectionId() != $section->getId()) {
-                    $groupPath = $element->getGroupPath();
+                $path = $element->getConfigPath();
+                $groupPath = current(explode('/', $path));
+                if ($groupPath != $section->getId()) {
                     if (!isset($extraConfigGroups[$groupPath])) {
                         $this->_configData = $this->_configDataObject
                             ->extendConfig($groupPath, false, $this->_configData);
