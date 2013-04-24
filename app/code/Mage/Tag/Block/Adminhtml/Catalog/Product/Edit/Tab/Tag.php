5cd3c69deb188094212fd79c9e6c9b1a62b5ad74
Igor Tripolskiy <itripolskiy@ebay.com>
2013-04-24 19:18:29 +0300

    Fixed MAGETWO-8429: Product Tags tab is absent at product page

--- app/code/Mage/Tag/Block/Adminhtml/Catalog/Product/Edit/Tab/Tag.php
+++ app/code/Mage/Tag/Block/Adminhtml/Catalog/Product/Edit/Tab/Tag.php
@@ -142,4 +142,12 @@ class Mage_Tag_Block_Adminhtml_Catalog_Product_Edit_Tab_Tag
     {
         return 'reviews';
     }
+
+    /**
+     * @return string
+     */
+    public function getGroupCode()
+    {
+        return Mage_Adminhtml_Block_Catalog_Product_Edit_Tabs::ADVANCED_TAB_GROUP_CODE;
+    }
 }
