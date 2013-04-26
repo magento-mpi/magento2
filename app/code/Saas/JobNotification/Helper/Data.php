22ab09b5dd3ddae7246de9afe62fe679e5513133
Sergey Shkolyarenko <sshkolyarenko@ebay.com>
2013-04-26 20:21:10 +0300

    Fixed MAGETWO-9657: Notifications view Added draft files of helper, ACL and menu

---
+++ app/code/Saas/JobNotification/Helper/Data.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Saas_Launcher
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Launcher data helper
+ *
+ * @category    Mage
+ * @package     Saas_JobNotification
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Saas_JobNotification_Helper_Data extends Mage_Core_Helper_Data
+{
+}
