dfd763609b2114c85595fa855934960e82f3a4e0
Olga Kopylova <okopylova@ebay.com>
2013-05-01 18:00:20 -0700

    MAGETWO-9556: Disable Enterprise_CustomerSegment Module in Go Edition - tests for customer segment logging

--- app/code/Enterprise/CustomerSegment/Model/Logging.php
+++ app/code/Enterprise/CustomerSegment/Model/Logging.php
@@ -8,24 +8,56 @@
  * @license     {license_link}
  */
 
+/**
+ * Class Enterprise_CustomerSegment_Model_Logging
+ *
+ * Model for logging event related to Customer Segment, active only if Enterprise_Logging module is enabled
+ */
 class Enterprise_CustomerSegment_Model_Logging
 {
+    /**
+     * @var Enterprise_CustomerSegment_Model_Resource_Segment|null
+     */
+    protected $_resourceModel = null;
+
+    /**
+     * @var Mage_Core_Controller_Request_Http|null
+     */
+    protected $_request = null;
+
+    /**
+     * @var Enterprise_CustomerSegment_Helper_Data|null
+     */
+    protected $_helper = null;
+
+    /**
+     * @param Enterprise_CustomerSegment_Model_Resource_Segment $resourceModel
+     * @param Mage_Core_Controller_Request_Http $request
+     * @param Enterprise_CustomerSegment_Helper_Data $helper
+     */
+    public function __construct(Enterprise_CustomerSegment_Model_Resource_Segment $resourceModel,
+        Mage_Core_Controller_Request_Http $request, Enterprise_CustomerSegment_Helper_Data $helper
+    ) {
+        $this->_resourceModel = $resourceModel;
+        $this->_request = $request;
+        $this->_helper = $helper;
+    }
+
     /**
      * Handler for logging customer segment match
      *
      * @param Varien_Simplexml_Element $config
      * @param Enterprise_Logging_Model_Event $eventModel
      * @return Enterprise_Logging_Model_Event
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function postDispatchCustomerSegmentMatch($config, $eventModel)
-    {
-        $request = Mage::app()->getRequest();
-        $customersQty = Mage::getModel('Enterprise_CustomerSegment_Model_Segment')->getResource()
-            ->getSegmentCustomersQty($request->getParam('id'));
-        return $eventModel->setInfo(
-            $request->getParam('id') ?
-                Mage::helper('Enterprise_CustomerSegment_Helper_Data')->__('Matched %d Customers of Segment %s', $customersQty, $request->getParam('id')) :
-                '-'
+    public function postDispatchCustomerSegmentMatch(Varien_Simplexml_Element $config,
+        Enterprise_Logging_Model_Event $eventModel
+    ) {
+        $customersQty = $this->_resourceModel->getSegmentCustomersQty($this->_request->getParam('id'));
+        return $eventModel->setInfo($this->_request->getParam('id') ?
+            $this->_helper->__('Matched %d Customers of Segment %s', $customersQty, $this->_request->getParam('id'))
+            : '-'
         );
     }
 }
