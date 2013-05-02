8bdb639ee4ef5739ce009bd2f4fa7d003e423688
Sergey Shymko <sergey.shymko@magento.com>
2013-05-01 18:51:36 -0700

    Implemented MAGETWO-9555: Enterprise_Banner Module Is Independent of Enterprise_CustomerSegment - implemented integration tests for checking influence of Enterprise_BannerCustomerSegment_Model_Observer - moved subscription to events from 'frontend' to 'global' area in order for them to have effect in testing environment

---
+++ dev/tests/integration/testsuite/Enterprise/CustomerSegment/_files/customer_developer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+require __DIR__ . '/segment_developers.php';
+require __DIR__ . '/../../../Mage/Customer/_files/customer.php';
+
+/** @var $segment Enterprise_CustomerSegment_Model_Segment */
+$segment = Mage::getModel('Enterprise_CustomerSegment_Model_Segment');
+$segment->load('Developers', 'name');
+
+/** @var Enterprise_CustomerSegment_Model_Customer $segmentCustomer */
+$segmentCustomer = Mage::getSingleton('Enterprise_CustomerSegment_Model_Customer');
+$segmentCustomer->addCustomerToWebsiteSegments(1, Mage::app()->getWebsite()->getId(), array($segment->getId()));
