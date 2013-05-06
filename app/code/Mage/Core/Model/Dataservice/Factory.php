3cbee1fc2a0d6811627d90ac2d896b81e42d8c28
rbates <rbates@x.com>
2013-05-06 12:09:08 -0500

    Added MDS-346: Service Calls refactoring - Added unit tests for the Factory Service

--- app/code/Mage/Core/Model/Dataservice/Factory.php
+++ app/code/Mage/Core/Model/Dataservice/Factory.php
@@ -5,7 +5,7 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-class Mage_Core_Model_Dataservice_Factory // implements Mage_Core_Model_Dataservice_Path_Visitable
+class Mage_Core_Model_Dataservice_Factory
 {
     /**
      * @var Mage_Core_Model_Dataservice_Config_Interface
@@ -30,6 +30,7 @@ class Mage_Core_Model_Dataservice_Factory // implements Mage_Core_Model_Dataserv
      * @param Mage_Core_Model_Dataservice_Config_Interface $config
      * @param Magento_ObjectManager $objectManager
      * @param Mage_Core_Model_Dataservice_Path_Composite
+     * @param Mage_Core_Model_Dataservice_Request_Visitor_Factory
      * @param Mage_Core_Model_Dataservice_Repository $repository
      */
     public function __construct(
@@ -45,14 +46,6 @@ class Mage_Core_Model_Dataservice_Factory // implements Mage_Core_Model_Dataserv
         $this->_composite = $composite;
         $this->_visitorFactory = $visitorFactory;
         $this->_repository = $repository;
-    }
-
-    /**
-     * @return Mage_Core_Model_Dataservice_Config_Interface
-     */
-    public function getConfig()
-    {
-        return $this->_config;
     }
 
     /**
@@ -77,7 +70,95 @@ class Mage_Core_Model_Dataservice_Factory // implements Mage_Core_Model_Dataserv
     }
 
     /**
+     * Init single service call
+     *
+     * @param $sourceName
+     * @return bool|mixed
+     */
+    public function initDataService($sourceName)
+    {
+        if (($dataService = $this->getRepository()->get($sourceName)) !== null) {
+            return $dataService;
+        }
+        $classInformation = $this->getConfig()->getClassByAlias($sourceName);
+        $instance = $this->_objectManager->create($classInformation['class']);
+        $dataService = $this->_applyMethod(
+            $instance, $classInformation['retrieveMethod'],
+            $classInformation['methodArguments']
+        );
+        $this->getRepository()->add($sourceName, $dataService);
+        return $dataService;
+    }
+
+    /**
+     * Retrieve repository for the data from service calls
+     *
+     * @return array|Mage_Core_Model_Dataservice_Repository
+     */
+    public function getRepository()
+    {
+        return $this->_repository;
+    }
+
+    /**
+     * @return Mage_Core_Model_Dataservice_Config_Interface
+     */
+    public function getConfig()
+    {
+        return $this->_config;
+    }
+
+    /**
+     * Invoke method configured for service call
+     *
+     * @param $object
+     * @param $methodName
+     * @param $methodArguments
+     * @return mixed
+     */
+    protected function _applyMethod($object, $methodName, $methodArguments)
+    {
+        $result = null;
+        $arguments = array();
+        if (is_array($methodArguments)) {
+            $arguments = $this->_prepareArguments($methodArguments);
+        }
+        $result = call_user_func_array(array($object, $methodName), $arguments);
+        return $result;
+    }
+
+    /**
+     * Prepare  values for the method params
+     *
+     * @param $argumentsList
+     * @return array
+     */
+    protected function _prepareArguments($argumentsList)
+    {
+        $result = array();
+        foreach ($argumentsList as $name => $value) {
+            $result[$name] = $this->getArgumentValue($value);
+        }
+        return $result;
+    }
+
+    /**
+     * Get the value for the method argument
+     *
+     * @param $path
+     * @return null
+     */
+    public function getArgumentValue($path)
+    {
+        /** @var $visitor Mage_Core_Model_Dataservice_Path_Visitor */
+        $visitor = $this->_visitorFactory->get($path);
+        $result = $visitor->visit($this->_composite);
+        return $result;
+    }
+
+    /**
      * Assign service call name to the namespace
+     *
      * @param $dataServiceName
      * @param $namespaceConfig
      * @throws Exception
@@ -106,6 +187,7 @@ class Mage_Core_Model_Dataservice_Factory // implements Mage_Core_Model_Dataserv
 
     /**
      * Retrieve the data or the service call based on its name
+     *
      * @param $sourceName
      * @return bool|mixed
      */
@@ -117,95 +199,4 @@ class Mage_Core_Model_Dataservice_Factory // implements Mage_Core_Model_Dataserv
         }
         return $dataService;
     }
-
-    /**
-     * Retrieve repository for the data from service calls
-     * @return array|Mage_Core_Model_Dataservice_Repository
-     */
-    public function getRepository()
-    {
-        return $this->_repository;
-    }
-
-    /**
-     * Init single service call
-     *
-     * @param $sourceName
-     * @return bool|mixed
-     */
-    public function initDataService($sourceName)
-    {
-        if ($dataService = $this->getRepository()->get($sourceName) !== null) {
-            return $dataService;
-        }
-
-        $classInformation = $this->getConfig()->getClassByAlias($sourceName);
-        $instance = $this->_objectManager->create($classInformation['class']);
-        $dataService = $this->_applyMethod($instance, $classInformation['retrieveMethod'],
-            $classInformation['methodArguments']);
-
-        $this->getRepository()->add($sourceName, $dataService);
-        return $dataService;
-    }
-
-    /**
-     * Invoke method configured for service call
-     *
-     * @param $object
-     * @param $methodName
-     * @param $methodArguments
-     * @return mixed
-     */
-    protected function _applyMethod($object, $methodName, $methodArguments)
-    {
-        $result = null;
-        $arguments = array();
-        if (is_array($methodArguments)) {
-            $arguments = $this->_prepareArguments($methodArguments);
-        }
-        $result = call_user_func_array(array($object, $methodName), $arguments);
-        return $result;
-    }
-
-    /**
-     * Get the value for the method argument
-     *
-     * @param $path
-     * @return null
-     */
-    public function getArgumentValue($path)
-    {
-        /** @var $visitor Mage_Core_Model_Dataservice_Path_Visitor */
-        $visitor = $this->_visitorFactory->get($path);
-        $result = $visitor->visit($this->_composite);
-        return $result;
-    }
-
-    /**
-     * Prepare  values for the method params
-     *
-     * @param $argumentsList
-     * @return array
-     */
-    protected function _prepareArguments($argumentsList)
-    {
-        $result = array();
-        foreach ($argumentsList as $name => $value) {
-            $result[$name] = $this->getArgumentValue($value);
-        }
-        return $result;
-    }
-
-
-    /**
-     * Make the Dataservice Object visitable
-     *
-     * @param Mage_Core_Model_Dataservice_Path_Visitor $visitor
-     * @return bool|mixed
-     */
-    /* public function visit(Mage_Core_Model_Dataservice_Path_Visitor $visitor)
-     {
-         // return $this->getRepository()->visit($visitor);
-         return $this->get($visitor->getCurrentPathElement());
-     }*/
 }
---
+++ dev/tests/unit/testsuite/Mage/Core/Model/Dataservice/FactoryTest.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: rbates
+ * Date: 5/3/13
+ * Time: 11:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Mage_Core_Model_Dataservice_FactoryTest extends PHPUnit_Framework_TestCase
+{
+    const TEST_CLASS_NAME = 'TEST_CLASS_NAME';
+
+    const TEST_DATA_SERVICE_NAME = 'TEST_DATA_SERVICE_NAME';
+
+    const TEST_NAMESPACE = 'TEST_NAMESPACE';
+
+    const TEST_NAMESPACE_ALIAS = 'TEST_NAMESPACE_ALIAS';
+
+    /** @var Mage_Core_Model_Dataservice_Factory */
+    protected $_factory;
+
+    /** @var Mage_Core_Model_Dataservice_Config_Interface */
+    protected $_configMock;
+
+    /** @var  Magento_ObjectManager */
+    protected $_objectManagerMock;
+
+    /** @var  Mage_Core_Model_Dataservice_Path_Composite */
+    protected $_compositeMock;
+
+    /** @var  Mage_Core_Model_Dataservice_Request_Visitor_Factory */
+    protected $_visitorFactoryMock;
+
+    /** @var  Mage_Core_Model_Dataservice_Repository */
+    protected $_repositoryMock;
+
+    protected $_dataserviceMock;
+
+    public function retrieveMethod()
+    {
+        return $this->_dataserviceMock;
+    }
+
+    public function setup()
+    {
+        $this->_configMock = $this->getMock(
+            'Mage_Core_Model_Dataservice_Config_Interface', array(), array(), "", false
+        );
+        $this->_objectManagerMock = $this->getMock('Magento_ObjectManager', array(), array(), "", false);
+        $this->_compositeMock = $this->getMock(
+            'Mage_Core_Model_Dataservice_Path_Composite', array(), array(), "", false
+        );
+        $this->_visitorFactoryMock = $this->getMock(
+            'Mage_Core_Model_Dataservice_Request_Visitor_Factory', array(), array(), "", false
+        );
+        $this->_repositoryMock = $this->getMock('Mage_Core_Model_Dataservice_Repository', array(), array(), "", false);
+        $this->_factory
+            = new Mage_Core_Model_Dataservice_Factory($this->_configMock, $this->_objectManagerMock, $this->_compositeMock, $this->_visitorFactoryMock,
+            $this->_repositoryMock);
+        $this->_dataserviceMock = (object)array();
+    }
+
+    public function testInit()
+    {
+        $this->_repositoryMock->expects($this->once())->method('addNameInNamespace')->with(
+            Mage_Core_Model_Dataservice_FactoryTest::TEST_NAMESPACE,
+            Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME,
+            Mage_Core_Model_Dataservice_FactoryTest::TEST_NAMESPACE_ALIAS
+        );
+        $namespaceConfig
+            = array('namespaces' => array(Mage_Core_Model_Dataservice_FactoryTest::TEST_NAMESPACE => Mage_Core_Model_Dataservice_FactoryTest::TEST_NAMESPACE_ALIAS));
+        $this->_repositoryMock->expects($this->once())->method("get")->with(
+            $this->equalTo(Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME)
+        )->will($this->returnValue(null));
+        $classInformation = array('class'          => Mage_Core_Model_Dataservice_FactoryTest::TEST_CLASS_NAME,
+                                  'retrieveMethod' => 'retrieveMethod', 'methodArguments' => array()
+        );
+        $this->_configMock->expects($this->once())->method("getClassByAlias")->with(
+            $this->equalTo(Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME)
+        )->will($this->returnValue($classInformation));
+        $this->_objectManagerMock->expects($this->once())->method("create")->with(
+            $this->equalTo(Mage_Core_Model_Dataservice_FactoryTest::TEST_CLASS_NAME)
+        )->will($this->returnValue($this));
+        $this->_repositoryMock->expects($this->once())->method("add")->with(
+            $this->equalTo(Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME),
+            $this->equalTo($this->_dataserviceMock)
+        );
+        $this->_factory->init(
+            array(Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME => $namespaceConfig)
+        );
+    }
+
+    public function testGet()
+    {
+        $this->_dataserviceMock = (object)array();
+        $this->_repositoryMock->expects($this->at(0))->method("get")->with(
+            $this->equalTo(Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME)
+        )->will($this->returnValue(null));
+        $this->_repositoryMock->expects($this->at(1))->method("get")->with(
+            $this->equalTo(Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME)
+        )->will($this->returnValue($this->_dataserviceMock));
+        $this->assertEquals(
+            $this->_dataserviceMock,
+            $this->_factory->get(Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME)
+        );
+    }
+
+    public function testGetByNamespace()
+    {
+        $this->_repositoryMock->expects($this->once())->method('getByNamespace')->with(
+            Mage_Core_Model_Dataservice_FactoryTest::TEST_NAMESPACE
+        )->will($this->returnValue(Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME));
+        $this->assertEquals(
+            Mage_Core_Model_Dataservice_FactoryTest::TEST_DATA_SERVICE_NAME,
+            $this->_factory->getByNamespace(
+                Mage_Core_Model_Dataservice_FactoryTest::TEST_NAMESPACE
+            )
+        );
+    }
+
+    public function testGetArgumentValue()
+    {
+        $visitorMock = $this->getMock('Mage_Core_Model_Dataservice_Path_Visitor', array(), array(), "", false);
+        $path = 'path';
+        $result = 'result';
+        $this->_visitorFactoryMock->expects($this->once())->method('get')->with($path)->will(
+            $this->returnValue($visitorMock)
+        );
+        $visitorMock->expects($this->once())->method('visit')->with($this->_compositeMock)->will(
+            $this->returnValue($result)
+        );
+        $this->assertEquals($result, $this->_factory->getArgumentValue($path));
+    }
+}
