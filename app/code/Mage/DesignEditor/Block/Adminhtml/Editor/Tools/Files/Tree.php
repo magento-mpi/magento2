18e8688f162f150749f60710036ee23f29cd8e02
Ann Beeskau <abeeskau@ebay.com>
2013-05-09 11:50:30 -0700

    Implemented MAGETWO-9836:  Render Fonts Uploader Dialog Tree - added helper method to return storage type name for display - override node name to be vde specific

--- app/code/Mage/DesignEditor/Block/Adminhtml/Editor/Tools/Files/Tree.php
+++ app/code/Mage/DesignEditor/Block/Adminhtml/Editor/Tools/Files/Tree.php
@@ -13,4 +13,13 @@
  */
 class Mage_DesignEditor_Block_Adminhtml_Editor_Tools_Files_Tree extends Mage_Theme_Block_Adminhtml_Wysiwyg_Files_Tree
 {
+    /**
+     * Override root node name of tree specific to design editor.
+     *
+     * @return string
+     */
+    public function getRootNodeName()
+    {
+        return $this->__('CSS Editor ') . $this->__($this->helper('Mage_Theme_Helper_Storage')->getStorageTypeName());
+    }
 }
