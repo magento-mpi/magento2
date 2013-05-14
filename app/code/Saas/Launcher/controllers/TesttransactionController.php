6c34110f836cebb1413888066a6c7ca2cdff721e
Igor Miniailo <iminiailo@ebay.com>
2013-05-14 16:38:31 +0300

    Implemented MAGETWO-8967: On 'Launch My Store' Popup link 'Test Transaction' should point to shopping cart with product in it.

--- app/code/Saas/Launcher/controllers/TesttransactionController.php
+++ app/code/Saas/Launcher/controllers/TesttransactionController.php
@@ -18,23 +18,67 @@
 class Saas_Launcher_TesttransactionController extends Mage_Core_Controller_Front_Action
 {
     /**
+     * @var Mage_Checkout_Model_Cart
+     */
+    protected $_cartModel;
+
+    /**
+     * @var Mage_Catalog_Model_Product
+     */
+    protected $_productModel;
+
+    /**
+     * @var Mage_Checkout_Model_Session
+     */
+    protected $_checkoutSession;
+
+    /**
+     * Constructor
+     *
+     * @param Mage_Core_Controller_Request_Http $request
+     * @param Mage_Core_Controller_Response_Http $response
+     * @param Magento_ObjectManager $objectManager
+     * @param Mage_Core_Controller_Varien_Front $frontController
+     * @param Mage_Core_Model_Layout_Factory $layoutFactory
+     * @param Mage_Checkout_Model_Cart $cartModel
+     * @param Mage_Catalog_Model_Product $productModel
+     * @param Mage_Checkout_Model_Session $checkoutSession
+     * @param string $areaCode
+     */
+    public function __construct(
+        Mage_Core_Controller_Request_Http $request,
+        Mage_Core_Controller_Response_Http $response,
+        Magento_ObjectManager $objectManager,
+        Mage_Core_Controller_Varien_Front $frontController,
+        Mage_Core_Model_Layout_Factory $layoutFactory,
+        Mage_Checkout_Model_Cart $cartModel,
+        Mage_Catalog_Model_Product $productModel,
+        Mage_Checkout_Model_Session $checkoutSession,
+        $areaCode = null
+    ) {
+        parent::__construct($request, $response, $objectManager, $frontController, $layoutFactory, $areaCode);
+
+        $this->_cartModel = $cartModel;
+        $this->_productModel = $productModel;
+        $this->_checkoutSession = $checkoutSession;
+    }
+
+    /**
      * Add product to cart if needed and redirect to Shopping Cart page
      *
      * @return null
      */
     public function indexAction()
     {
-        $cart = $this->_objectManager->create('Mage_Checkout_Model_Cart');
-        if (!$cart->getQuote()->getItemsCount()) {
-            $productModel = $this->_objectManager->create('Mage_Catalog_Model_Product');
-            $products = $productModel->getResourceCollection()
+        if (!$this->_cartModel->getQuote()->getItemsCount()) {
+            $products = $this->_productModel->getResourceCollection()
                 ->addFieldToFilter('status', Mage_Catalog_Model_Product_Status::STATUS_ENABLED);
             if ($products->count()) {
                 $productId = $products->addAttributeToSort('entity_id', 'ASC')->getFirstItem()->getId();
-                $product = $productModel->load($productId);
-                $cart->addProduct($product, 1);
-                $cart->save();
-                $this->_objectManager->create('Mage_Checkout_Model_Session')->setCartWasUpdated(true);
+                $product = $this->_productModel->load($productId);
+                $this->_cartModel->addProduct($product, 1);
+                $this->_cartModel->save();
+                $this->_checkoutSession->setCartWasUpdated(true);
             }
         }
         $this->_redirect('checkout/cart');
