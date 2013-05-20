1985e8f67fea5ec3995ca834de329478ea4243fb
Susan Wright <swright@ebay.com>
2013-05-20 09:00:06 -0700

    Implemented MAGETWO-9823 Miscellaneous - Fixed static test failures.

--- app/code/Mage/DesignEditor/Block/Adminhtml/Editor/Tools/Files/Content/Files.php
+++ app/code/Mage/DesignEditor/Block/Adminhtml/Editor/Tools/Files/Content/Files.php
@@ -14,12 +14,14 @@
  * @method Mage_Theme_Block_Adminhtml_Wysiwyg_Files_Content_Files setStorage(Mage_Theme_Model_Wysiwyg_Storage $storage)
  * @method Mage_Theme_Model_Wysiwyg_Storage getStorage
  */
-class Mage_DesignEditor_Block_Adminhtml_Editor_Tools_Files_Content_Files extends Mage_Theme_Block_Adminhtml_Wysiwyg_Files_Content_Files
+class Mage_DesignEditor_Block_Adminhtml_Editor_Tools_Files_Content_Files
+    extends Mage_Theme_Block_Adminhtml_Wysiwyg_Files_Content_Files
 {
     /**
      * @return string
      */
-    public function getStorageType() {
+    public function getStorageType()
+    {
         return $this->__($this->helper('Mage_Theme_Helper_Storage')->getStorageType());
     }
 
