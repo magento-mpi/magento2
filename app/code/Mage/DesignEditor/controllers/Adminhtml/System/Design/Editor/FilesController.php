4fece8ff972b52c844f605b422aa19975677c0d9
Ann Beeskau <abeeskau@ebay.com>
2013-05-20 13:34:31 -0700

    Fixed MAGETWO-9835: Add Support to Upload Fonts on Custom CSS Tab - extend theme files controller to resolve static copy paste test failure

--- app/code/Mage/DesignEditor/controllers/Adminhtml/System/Design/Editor/FilesController.php
+++ app/code/Mage/DesignEditor/controllers/Adminhtml/System/Design/Editor/FilesController.php
@@ -11,18 +11,11 @@
 /**
  * Files controller
  */
-class Mage_DesignEditor_Adminhtml_System_Design_Editor_FilesController extends Mage_Adminhtml_Controller_Action
+
+require_once  'Mage/Theme/controllers/Adminhtml/System/Design/Wysiwyg/FilesController.php';
+
+class Mage_DesignEditor_Adminhtml_System_Design_Editor_FilesController extends Mage_Theme_Adminhtml_System_Design_Wysiwyg_FilesController
 {
-
-    /**
-     * Index action
-     */
-    public function indexAction()
-    {
-        $this->loadLayout('overlay_popup');
-        $this->renderLayout();
-    }
-
     /**
      * Tree json action
      */
@@ -36,40 +29,6 @@ class Mage_DesignEditor_Adminhtml_System_Design_Editor_FilesController extends M
         } catch (Exception $e) {
             $this->_objectManager->get('Mage_Core_Model_Logger')->logException($e);
             $this->getResponse()->setBody($this->_objectManager->get('Mage_Core_Helper_Data')->jsonEncode(array()));
-        }
-    }
-
-    /**
-     * New folder action
-     */
-    public function newFolderAction()
-    {
-        $name = $this->getRequest()->getPost('name');
-        try {
-            $path = $this->_getSession()->getStoragePath();
-            $result = $this->_getStorage()->createFolder($name, $path);
-        } catch (Mage_Core_Exception $e) {
-            $result = array('error' => true, 'message' => $e->getMessage());
-            $this->_redirect('*/*/');
-        } catch (Exception $e) {
-            $result = array('error' => true, 'message' => $this->__('Unknown error'));
-            $this->_objectManager->get('Mage_Core_Model_Logger')->logException($e);
-            $this->_redirect('*/*/');
-        }
-        $this->getResponse()->setBody($this->_objectManager->get('Mage_Core_Helper_Data')->jsonEncode($result));
-    }
-
-    /**
-     * Delete folder action
-     */
-    public function deleteFolderAction()
-    {
-        try {
-            $path = $this->_getSession()->getStoragePath();
-            $this->_getStorage()->deleteDirectory($path);
-        } catch (Exception $e) {
-            $result = array('error' => true, 'message' => $e->getMessage());
-            $this->getResponse()->setBody($this->_objectManager->get('Mage_Core_Helper_Data')->jsonEncode($result));
         }
     }
 
@@ -90,80 +49,5 @@ class Mage_DesignEditor_Adminhtml_System_Design_Editor_FilesController extends M
             $result = array('error' => true, 'message' => $e->getMessage());
             $this->getResponse()->setBody($this->_objectManager->get('Mage_Core_Helper_Data')->jsonEncode($result));
         }
-    }
-
-    /**
-     * Files upload action
-     */
-    public function uploadAction()
-    {
-        try {
-            $path = $this->_getSession()->getStoragePath();
-            $result = $this->_getStorage()->uploadFile($path);
-        } catch (Exception $e) {
-            $result = array('error' => $e->getMessage(), 'errorcode' => $e->getCode());
-        }
-        $this->getResponse()->setBody($this->_objectManager->get('Mage_Core_Helper_Data')->jsonEncode($result));
-    }
-
-    /**
-     * Preview image action
-     */
-    public function previewImageAction()
-    {
-        $file = $this->getRequest()->getParam('file');
-        /** @var $helper Mage_Theme_Helper_Storage */
-        $helper = $this->_objectManager->get('Mage_Theme_Helper_Storage');
-        try {
-            $this->_prepareDownloadResponse($file, array(
-                'type'  => 'filename',
-                'value' => $helper->getThumbnailPath($file)
-            ));
-        } catch (Exception $e) {
-            $this->_objectManager->get('Mage_Core_Model_Logger')->logException($e);
-            $this->_redirect('core/index/notfound');
-        }
-    }
-
-    /**
-     * Delete file from media storage
-     * @throws Exception
-     */
-    public function deleteFilesAction()
-    {
-        try {
-            if (!$this->getRequest()->isPost()) {
-                throw new Exception('Wrong request');
-            }
-            $files = $this->_objectManager->get('Mage_Core_Helper_Data')->jsonDecode(
-                $this->getRequest()->getParam('files')
-            );
-            foreach ($files as $file) {
-                $this->_getStorage()->deleteFile($file);
-            }
-        } catch (Exception $e) {
-            $result = array('error' => true, 'message' => $e->getMessage());
-            $this->getResponse()->setBody($this->_objectManager->get('Mage_Core_Helper_Data')->jsonEncode($result));
-        }
-    }
-
-    /**
-     * Fire when select image
-     */
-    public function onInsertAction()
-    {
-        /** @var $helperStorage Mage_Theme_Helper_Storage */
-        $helperStorage = $this->_objectManager->get('Mage_Theme_Helper_Storage');
-        $this->getResponse()->setBody($helperStorage->getRelativeUrl());
-    }
-
-    /**
-     * Get storage
-     *
-     * @return Mage_Theme_Model_Wysiwyg_Storage
-     */
-    protected function _getStorage()
-    {
-        return $this->_objectManager->get('Mage_Theme_Model_Wysiwyg_Storage');
     }
 }
