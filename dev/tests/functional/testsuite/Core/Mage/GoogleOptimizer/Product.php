85093b793933e83d1d89f83629e3dc2e9dfcba2a
Valeriy Nayda <vnayda@ebay.com>
2013-05-21 15:12:07 +0300

    Implemented MAGETWO-7812: Implement Google Content Experiment functionality  - created taf structure

---
+++ dev/tests/functional/testsuite/Core/Mage/GoogleOptimizer/Product.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+class Core_Mage_GoogleOptimizer_Product extends Mage_Selenium_TestCase
+{
+    public function setUpBeforeTests()
+    {
+        $this->loginAdminUser();
+    }
+
+    /**
+     * @test
+     * @group goinc
+     */
+    public function checkBehaviorOnCreate()
+    {
+    }
+
+    /**
+     * @test
+     * @group goinc
+     */
+    public function checkBehaviorOnUpdate()
+    {
+    }
+
+    /**
+     * @test
+     * @group goinc
+     */
+    public function checkBehaviorOnDelete()
+    {
+    }
+
+    /**
+     * @test
+     * @group goinc
+     */
+    public function checkBehaviorIfDisabled()
+    {
+    }
+}
