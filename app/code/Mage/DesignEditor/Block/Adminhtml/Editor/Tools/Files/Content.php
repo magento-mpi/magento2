de6e9e64757ceeb8a34094b18e024ead95f3edd4
Susan Wright <swright@ebay.com>
2013-05-22 08:57:08 -0700

    Implemented MAGETWO-10232 "We found no files." note is displayed. - Removed message, since not part of requirements. Also, removed breadcrumb that was added back as a result of a unit test fix.

--- app/code/Mage/DesignEditor/Block/Adminhtml/Editor/Tools/Files/Content.php
+++ app/code/Mage/DesignEditor/Block/Adminhtml/Editor/Tools/Files/Content.php
@@ -25,4 +25,29 @@ class Mage_DesignEditor_Block_Adminhtml_Editor_Tools_Files_Content
     {
         return $this->__('CSS Editor ') . $this->__($this->helper('Mage_Theme_Helper_Storage')->getStorageTypeName());
     }
+    /**
+     * Javascript setup object for filebrowser instance
+     *
+     * @return string
+     */
+    public function getFilebrowserSetupObject()
+    {
+        $setupObject = new Varien_Object();
+
+        $setupObject->setData(array(
+            'newFolderPrompt'                 => $this->__('New Folder Name:'),
+            'deleteFolderConfirmationMessage' => $this->__('Are you sure you want to delete this folder?'),
+            'deleteFileConfirmationMessage'   => $this->__('Are you sure you want to delete this file?'),
+            'targetElementId' => $this->getTargetElementId(),
+            'contentsUrl'     => $this->getContentsUrl(),
+            'onInsertUrl'     => $this->getOnInsertUrl(),
+            'newFolderUrl'    => $this->getNewfolderUrl(),
+            'deleteFolderUrl' => $this->getDeletefolderUrl(),
+            'deleteFilesUrl'  => $this->getDeleteFilesUrl(),
+            'headerText'      => $this->getHeaderText(),
+            'showBreadcrumbs' => false
+        ));
+
+        return $this->helper('Mage_Core_Helper_Data')->jsonEncode($setupObject);
+    }
 }
