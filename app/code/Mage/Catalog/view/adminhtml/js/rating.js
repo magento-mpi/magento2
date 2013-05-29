a7c1c96b6956f900f447f5b5860e401cee4f978b
Pavel Yukhnevych <pyukhnevych@ebay.com>
2013-05-29 14:33:31 +0300

    CR-Changes MAGETWO-9844: [IE8] Impossible to set product rating from backend. Static tests fix

--- app/code/Mage/Catalog/view/adminhtml/js/rating.js
+++ app/code/Mage/Catalog/view/adminhtml/js/rating.js
@@ -13,6 +13,7 @@
         options: {
             colorFilled: '#333',
             colorUnfilled: '#CCCCCC',
+            colorHover: '#f30'
         },
 
         _create: function() {
@@ -21,19 +22,35 @@
         },
 
         _bind: function() {
-            this._labels.on('click', $.proxy(function(e) {
-                var elem = $(e.currentTarget);
-                $('#' + elem.attr('for')).attr('checked', 'checked');
-                this._updateRating();
-            }, this));
+            this._labels.on({
+                click: $.proxy(function(e) {
+                    var elem = $(e.currentTarget);
+                    $('#' + elem.attr('for')).attr('checked', 'checked');
+                    this._updateRating();
+                }, this),
+
+                hover: $.proxy(function(e) {
+                    this._updateHover($(e.currentTarget), this.options.colorHover);
+                }, this),
+
+                mouseleave: $.proxy(function(e) {
+                    this._updateHover($(e.currentTarget), this.options.colorUnfilled);
+                }, this)
+            })
+
             this._updateRating();
+        },
+
+        _updateHover: function(elem, color) {
+            elem.nextAll('label').andSelf().filter(function() {
+                return !$(this).data('checked');
+            }).css('color', color);
         },
 
         _updateRating: function() {
             var checkedInputs = this.element.find('input[type="radio"]:checked');
-            checkedInputs.css('color', this.options.colorFilled)
-                .nextAll('label').css('color', this.options.colorFilled);
-            checkedInputs.prevAll('label').css('color', this.options.colorUnfilled)
+            checkedInputs.nextAll('label').andSelf().css('color', this.options.colorFilled).data('checked', true);
+            checkedInputs.prevAll('label').css('color', this.options.colorUnfilled).data('checked', false);
         }
     });
 })(jQuery);
