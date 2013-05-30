705801c04598edb33f728d970032896c017bfc82
Andrey Voinkov <avoinkov@ebay.com>
2013-05-30 13:03:38 +0300

    Fixed MAGETWO-10374: Implement Revert Theme Modifications to Theme Default Values (Physical Theme) - tests

--- dev/tests/integration/testsuite/Mage/Core/Model/Theme/Domain/VirtualTest.php
+++ dev/tests/integration/testsuite/Mage/Core/Model/Theme/Domain/VirtualTest.php
@@ -51,6 +51,9 @@ class Mage_Core_Model_Theme_Domain_VirtualTest extends PHPUnit_Framework_TestCas
      */
     protected $_virtualThemeId;
 
+    /**
+     * @magentoDbIsolation
+     */
     public function testGetPhysicalTheme()
     {
         //1. set up fixture
