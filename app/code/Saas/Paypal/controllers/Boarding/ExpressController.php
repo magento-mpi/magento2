47677e985afb64d6ceae2b37f01e492662cb9fef
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2013-05-30 19:24:03 +0300

    Fixed MAGETWO-10416: It's impossible to place order via PayPal Express Checkout which is configured as "Accelerated Boarding" or "Permissions"
     - Layout for boarding review was moved from Go 1.x
     - Small refactor was made for Saas_Paypal_Boarding_ExpressController

--- app/code/Saas/Paypal/controllers/Boarding/ExpressController.php
+++ app/code/Saas/Paypal/controllers/Boarding/ExpressController.php
@@ -28,11 +28,72 @@ class Saas_Paypal_Boarding_ExpressController extends Mage_Paypal_Controller_Expr
     protected $_configMethod = Saas_Paypal_Model_Boarding_Config::METHOD_EXPRESS_BOARDING;
 
     /**
+     * Helper factory
+     *
+     * @var Mage_Core_Model_Factory_Helper
+     */
+    protected $_helperFactory;
+
+    /**
+     * Store config model
+     *
+     * @var Mage_Core_Model_Store_Config
+     */
+    protected $_storeConfig;
+
+    /**
+     * Checkout session model
+     *
+     * @var Mage_Checkout_Model_Session
+     */
+    protected $_checkoutSession;
+
+    /**
+     * Customer session model
+     *
+     * @var Mage_Customer_Model_Session
+     */
+    protected $_customerSession;
+
+    /**
+     * URL Model instance
+     *
+     * @var Mage_Core_Model_Url
+     */
+    protected $_url;
+
+    /**
+     * Logger
+     *
+     * @var Mage_Core_Model_Logger
+     */
+    protected $_logger;
+
+    /**
      * Checkout mode type
      *
      * @var string
      */
     protected $_checkoutType = 'Saas_Paypal_Model_Boarding_Express_Checkout';
+
+    public function __construct(
+        Mage_Core_Controller_Varien_Action_Context $context,
+        Mage_Core_Model_Factory_Helper $helperFactory,
+        Mage_Core_Model_Store_Config $storeConfig,
+        Mage_Checkout_Model_Session $checkoutSession,
+        Mage_Customer_Model_Session $customerSession,
+        Mage_Core_Model_Url $url,
+        Mage_Core_Model_Logger $logger,
+        $areaCode = null
+    ) {
+        parent::__construct($context, $areaCode);
+        $this->_helperFactory = $helperFactory;
+        $this->_storeConfig = $storeConfig;
+        $this->_checkoutSession = $checkoutSession;
+        $this->_customerSession = $customerSession;
+        $this->_url = $url;
+        $this->_logger = $logger;
+    }
 
     /**
      * Redirect to login page
@@ -40,10 +101,10 @@ class Saas_Paypal_Boarding_ExpressController extends Mage_Paypal_Controller_Expr
     public function redirectLogin()
     {
         $this->setFlag('', 'no-dispatch', true);
-        Mage::getSingleton('Mage_Customer_Model_Session')->setBeforeAuthUrl(Mage::getUrl('checkout/cart'));
+        $this->_customerSession->setBeforeAuthUrl($this->_url->getUrl('checkout/cart'));
         $this->getResponse()->setRedirect(
-            Mage::helper('Mage_Core_Helper_Url')->addRequestParam(
-                Mage::helper('Mage_Customer_Helper_Data')->getLoginUrl(),
+            $this->_helperFactory->get('Mage_Core_Helper_Url')->addRequestParam(
+                $this->_helperFactory->get('Mage_Customer_Helper_Data')->getLoginUrl(),
                 array('context' => 'checkout')
             )
         );
@@ -62,7 +123,7 @@ class Saas_Paypal_Boarding_ExpressController extends Mage_Paypal_Controller_Expr
         try {
             $this->_initCheckout();
 
-            $customer = Mage::getSingleton('Mage_Customer_Model_Session')->getCustomer();
+            $customer = $this->_customerSession->getCustomer();
             if ($customer && $customer->getId()) {
                 $this->_checkout->setCustomerWithAddressChange(
                     $customer, $this->_getQuote()->getBillingAddress(), $this->_getQuote()->getShippingAddress()
@@ -78,57 +139,27 @@ class Saas_Paypal_Boarding_ExpressController extends Mage_Paypal_Controller_Expr
 
             // giropay
             $this->_checkout->prepareGiropayUrls(
-                Mage::getUrl('checkout/onepage/success'),
-                Mage::getUrl('paypal/boarding_express/cancel'),
-                Mage::getUrl('checkout/onepage/success')
+                $this->_url->getUrl('checkout/onepage/success'),
+                $this->_url->getUrl('paypal/boarding_express/cancel'),
+                $this->_url->getUrl('checkout/onepage/success')
             );
 
             $button = (bool)$this->getRequest()->getParam(Mage_Paypal_Model_Express_Checkout::PAYMENT_INFO_BUTTON);
-            $token  = $this->_checkout->start(Mage::getUrl('*/*/return'), Mage::getUrl('*/*/cancel'), $button);
+            $token  = $this->_checkout->start($this->_url->getUrl('*/*/return'),
+                $this->_url->getUrl('*/*/cancel'), $button);
             if ($token && $url = $this->_checkout->getRedirectUrl()) {
                 $this->_initToken($token);
                 $this->getResponse()->setRedirect($url);
                 return;
             }
         } catch (Mage_Core_Exception $e) {
-            $this->_getCheckoutSession()->addError($e->getMessage());
+            $this->_checkoutSession->addError($e->getMessage());
         } catch (Exception $e) {
-            $this->_getCheckoutSession()->addError($this->__('Unable to start Express Checkout.'));
-            Mage::logException($e);
+            $this->_checkoutSession->addError($this->__('Unable to start Express Checkout.'));
+            $this->_logger->logException($e);
         }
 
         $this->_redirect('checkout/cart');
-    }
-
-    /**
-     * Instantiate quote and checkout
-     *
-     * @return mixed
-     * @throws Mage_Core_Exception
-     */
-    protected function _initCheckout()
-    {
-        $quote = $this->_getQuote();
-        if (!$quote->hasItems() || $quote->getHasError()) {
-            $this->getResponse()->setHeader('HTTP/1.1','403 Forbidden');
-            Mage::throwException(Mage::helper('Mage_Paypal_Helper_Data')->__('Unable to initialize Express Checkout.'));
-        }
-        $this->_checkout = Mage::getSingleton($this->_checkoutType, array(
-            'config' => $this->_config,
-            'quote'  => $quote,
-        ));
-
-        return $this->_checkout;
-    }
-
-    /**
-     * Return checkout session object
-     *
-     * @return Mage_Checkout_Model_Session
-     */
-    private function _getCheckoutSession()
-    {
-        return Mage::getSingleton('Mage_Checkout_Model_Session');
     }
 
     /**
@@ -139,7 +170,7 @@ class Saas_Paypal_Boarding_ExpressController extends Mage_Paypal_Controller_Expr
     private function _getQuote()
     {
         if (!$this->_quote) {
-            $this->_quote = $this->_getCheckoutSession()->getQuote();
+            $this->_quote = $this->_checkoutSession->getQuote();
         }
         return $this->_quote;
     }
@@ -151,12 +182,10 @@ class Saas_Paypal_Boarding_ExpressController extends Mage_Paypal_Controller_Expr
      */
     private function _getValidationMinAmount()
     {
-        $baseSubtotal = $this->_getCheckoutSession()->getQuote()->getBaseSubtotal();
-        $minAmountActive = Mage::getStoreConfig('sales/minimum_order/active');
-        $minAmount = $minAmountActive ? Mage::getStoreConfig('sales/minimum_order/amount') : 0;
-        if ($baseSubtotal >= $minAmount) {
-            return true;
-        }
-        return false;
+        $baseSubtotal = $this->_checkoutSession->getQuote()->getBaseSubtotal();
+        $minAmountActive = $this->_storeConfig->getConfig('sales/minimum_order/active');
+        $minAmount = $minAmountActive ? $this->_storeConfig->getConfig('sales/minimum_order/amount') : 0;
+
+        return $baseSubtotal >= $minAmount;
     }
 }
