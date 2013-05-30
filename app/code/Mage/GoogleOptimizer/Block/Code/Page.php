643266b9544590b9965a258ac63ba57783623766
darja.baranova <6GroggIrcA>
2013-05-30 12:43:10 +0300

    Fixed MAGETWO-10254: Google Content Experiment. Problem with deleting of experiment code

--- app/code/Mage/GoogleOptimizer/Block/Code/Page.php
+++ app/code/Mage/GoogleOptimizer/Block/Code/Page.php
@@ -18,6 +18,7 @@ class Mage_GoogleOptimizer_Block_Code_Page extends Mage_GoogleOptimizer_Block_Co
      * @param Mage_Core_Block_Template_Context $context
      * @param Mage_GoogleOptimizer_Helper_Data $helper
      * @param Mage_Core_Model_Registry $registry
+     * @param Mage_GoogleOptimizer_Helper_Code $codeHelper
      * @param Mage_Cms_Model_Page $page
      * @param array $data
      */
@@ -25,21 +26,23 @@ class Mage_GoogleOptimizer_Block_Code_Page extends Mage_GoogleOptimizer_Block_Co
         Mage_Core_Block_Template_Context $context,
         Mage_GoogleOptimizer_Helper_Data $helper,
         Mage_Core_Model_Registry $registry,
+        Mage_GoogleOptimizer_Helper_Code $codeHelper,
         Mage_Cms_Model_Page $page,
         array $data = array()
     ) {
         // Mage_Cms_Model_Page is singleton
         $this->_page = $page;
-        parent::__construct($context, $helper, $registry, $data);
+        parent::__construct($context, $helper, $registry, $codeHelper, $data);
     }
 
     /**
-     * Get google experiment code model
+     * Get entity from registry
      *
-     * @return Mage_GoogleOptimizer_Model_Code
+     * @return mixed
+     * @throws RuntimeException
      */
-    protected function _getGoogleExperiment()
+    protected function _getEntity()
     {
-        return $this->_page->getGoogleExperiment();
+        return $this->_page;
     }
 }
