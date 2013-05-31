0b8a2f88a6739e03ac526ce63632285c6dc6a398
Vadym Popov <vapopov@ebay.com>
2013-05-31 15:55:14 +0300

    Implemented MAGETWO-10372:  Add Buttons for Reverting Theme Modifications

--- app/code/Mage/DesignEditor/Block/Adminhtml/Editor/Toolbar/Buttons/Edit.php
+++ app/code/Mage/DesignEditor/Block/Adminhtml/Editor/Toolbar/Buttons/Edit.php
@@ -15,11 +15,39 @@ class Mage_DesignEditor_Block_Adminhtml_Editor_Toolbar_Buttons_Edit
     extends Mage_Backend_Block_Widget_Button_Split
 {
     /**
-     * Current theme
-     *
-     * @var Mage_Core_Model_Theme
+     * @var Mage_DesignEditor_Model_Theme_Context
      */
-    protected $_theme;
+    protected $_themeContext;
+
+    /**
+     * @var Mage_DesignEditor_Model_Theme_ChangeFactory
+     */
+    protected $_changeFactory;
+
+    /**
+     * @var Mage_Core_Model_Date
+     */
+    protected $_dateModel;
+
+    /**
+     * @param Mage_Core_Block_Template_Context $context
+     * @param Mage_DesignEditor_Model_Theme_Context $themeContext
+     * @param Mage_DesignEditor_Model_Theme_ChangeFactory $changeFactory
+     * @param Mage_Core_Model_Date $dateModel
+     * @param array $data
+     */
+    public function __construct(
+        Mage_Core_Block_Template_Context $context,
+        Mage_DesignEditor_Model_Theme_Context $themeContext,
+        Mage_DesignEditor_Model_Theme_ChangeFactory $changeFactory,
+        Mage_Core_Model_Date $dateModel,
+        array $data = array()
+    ) {
+        $this->_themeContext = $themeContext;
+        $this->_changeFactory = $changeFactory;
+        $this->_dateModel = $dateModel;
+        parent::__construct($context, $data);
+    }
 
     /**
      * Init edit button
@@ -29,33 +57,6 @@ class Mage_DesignEditor_Block_Adminhtml_Editor_Toolbar_Buttons_Edit
     public function init()
     {
         $this->_initEditButton();
-        return $this;
-    }
-
-    /**
-     * Get current theme
-     *
-     * @return Mage_Core_Model_Theme
-     * @throws InvalidArgumentException
-     */
-    public function getTheme()
-    {
-        if (null === $this->_theme) {
-            throw new InvalidArgumentException('Current theme was not passed to edit button');
-        }
-        return $this->_theme;
-    }
-
-    /**
-     * Set current theme
-     *
-     * @param Mage_Core_Model_Theme $theme
-     * @return Mage_DesignEditor_Block_Adminhtml_Editor_Toolbar_Buttons
-     */
-    public function setTheme($theme)
-    {
-        $this->_theme = $theme;
-
         return $this;
     }
 
@@ -89,7 +90,7 @@ class Mage_DesignEditor_Block_Adminhtml_Editor_Toolbar_Buttons_Edit
     public function getRevertUrl($revertType)
     {
         return $this->getUrl('*/system_design_editor/revert', array(
-            'theme_id'  => $this->getTheme()->getId(),
+            'theme_id'  => $this->_themeContext->getEditableTheme()->getId(),
             'revert_to' => $revertType
         ));
     }
@@ -106,11 +107,13 @@ class Mage_DesignEditor_Block_Adminhtml_Editor_Toolbar_Buttons_Edit
             'options'        => array(
                 array(
                     'label'          => $this->__('Revert Styles to Last Saved'),
-                    'data_attribute' => array('mage-init' => $this->_getAssignInitData('revert-to-last', 'last_saved'))
+                    'data_attribute' => array('mage-init' => $this->_getDataRevertToPrevious()),
+                    'disabled'       => !$this->_isAbleRevertToPrevious()
                 ),
                 array(
                     'label'          => $this->__('Revert Styles to Theme Default Values'),
-                    'data_attribute' => array('mage-init' => $this->_getAssignInitData('revert-to-default', 'physical'))
+                    'data_attribute' => array('mage-init' => $this->_getDataRevertToDefault()),
+                    'disabled'       => !$this->_isAbleRevertToDefault()
                 )
             )
         ));
@@ -119,24 +122,96 @@ class Mage_DesignEditor_Block_Adminhtml_Editor_Toolbar_Buttons_Edit
     }
 
     /**
-     * Get 'data-mage-init' attribute value for 'Edit' button
+     * Get json options for button (restore-to-previous)
      *
-     * @param string $event
-     * @param string $revertType
-     * @return string
+     * @return string|bool
      */
-    protected function _getAssignInitData($event, $revertType)
+    protected function _getDataRevertToPrevious()
     {
+        $sourceChange = $this->_changeFactory->create();
+        $sourceChange->loadByThemeId($this->_themeContext->getEditableTheme()->getId());
+
+        $message = '';
+        if ($sourceChange->getId()) {
+            $message = $this->__('Are you sure you want to revert changes to last saved (%s)?',
+                $this->_dateModel->date('d-m-Y H:i', $sourceChange->getChangeTime()));
+        }
+
         $data = array(
             'vde-edit-button' => array(
-                'event'     => $event,
+                'event'     => 'revert-to-last',
                 'target'    => 'body',
                 'eventData' => array(
-                    'theme_id' => $this->getTheme()->getId(),
-                    'url'      => $this->getRevertUrl($revertType)
+                    'url'             => $this->getRevertUrl('last_saved'),
+                    'confirm_message' => $message
                 )
-            ),
+            )
         );
         return $this->helper('Mage_Backend_Helper_Data')->escapeHtml(json_encode($data));
+    }
+
+    /**
+     * Get json options for button (restore-to-default)
+     *
+     * @return string|bool
+     */
+    protected function _getDataRevertToDefault()
+    {
+        $message = $this->__('Are you sure you want to revert changes to the Theme defaults?');
+        $data = array(
+            'vde-edit-button' => array(
+                'event'     => 'revert-to-default',
+                'target'    => 'body',
+                'eventData' => array(
+                    'url'             => $this->getRevertUrl('physical'),
+                    'confirm_message' => $message
+                )
+            )
+        );
+        return $this->helper('Mage_Backend_Helper_Data')->escapeHtml(json_encode($data));
+    }
+
+    /**
+     * Check themes by change time (compare staging and virtual theme)
+     *
+     * @return bool
+     */
+    protected function _isAbleRevertToPrevious()
+    {
+        return $this->_hasThemeChanged(
+            $this->_themeContext->getStagingTheme(),
+            $this->_themeContext->getEditableTheme()
+        );
+    }
+
+    /**
+     * Check themes by change time (compare staging and physical theme)
+     *
+     * @return bool
+     */
+    protected function _isAbleRevertToDefault()
+    {
+        return $this->_hasThemeChanged(
+            $this->_themeContext->getStagingTheme(),
+            $this->_themeContext->getEditableTheme()->getParentTheme()
+        );
+    }
+
+    /**
+     * Checks themes for changes by time
+     *
+     * @param Mage_Core_Model_Theme $sourceTheme
+     * @param Mage_Core_Model_Theme $targetTheme
+     * @return bool
+     */
+    protected function _hasThemeChanged(Mage_Core_Model_Theme $sourceTheme, Mage_Core_Model_Theme $targetTheme)
+    {
+        $sourceChange = $this->_changeFactory->create();
+        $sourceChange->loadByThemeId($sourceTheme->getId());
+
+        $targetChange = $this->_changeFactory->create();
+        $targetChange->loadByThemeId($targetTheme->getId());
+
+        return $sourceChange->getChangeTime() !== $targetChange->getChangeTime();
     }
 }
--- app/code/Mage/DesignEditor/view/adminhtml/js/theme-revert.js
+++ app/code/Mage/DesignEditor/view/adminhtml/js/theme-revert.js
@@ -14,7 +14,10 @@
      */
     $.widget('vde.vde-edit-button', $.ui.button, {
         options: {
-            eventData: {}
+            dialogSelector:  '#dialog-message-confirm',
+            eventData: {
+                url: undefined
+            }
         },
 
         /**
@@ -32,30 +35,82 @@
          */
         _bind: function() {
             this.element.on('click.vde-edit-button',  $.proxy(this._onRevertEvent, this));
+            $('body').on('refreshIframe', $.proxy(this._enableButton, this));
         },
 
         /**
          * Handler for 'revert-to-last' and 'revert-to-default' event
-         *
          * @private
          */
         _onRevertEvent: function() {
+            if (this.element.hasClass('disabled')) {
+                return false;
+            }
+            var dialog = this._getDialog();
+            if (this.options.eventData.confirm_message && dialog) {
+                this._showConfirmMessage(dialog, $.proxy(this._sendRevertRequest, this));
+            } else {
+                this._sendRevertRequest();
+            }
+        },
+
+        /**
+         * Show confirmation message if it was assigned
+         * @private
+         */
+        _showConfirmMessage: function(dialogElement, callback) {
+            var dialog = dialogElement.data('dialog');
+            var buttons = {
+                text: $.mage.__('Ok'),
+                click: callback,
+                'class': 'primary'
+            };
+            if (this.options.eventData.confirm_message) {
+                dialog.text.set(this.options.eventData.confirm_message);
+                dialog.setButtons(buttons);
+                dialog.open();
+            }
+        },
+
+        /**
+         * Sent request to revert changes
+         * @private
+         */
+        _sendRevertRequest: function() {
             $.ajax({
                 url: this.options.eventData.url,
-                type: 'POST',
+                type: 'GET',
                 dataType: 'JSON',
                 async: false,
-                success: function(data) {
+                success: $.proxy(function(data) {
                     if (data.error) {
-                        /** @todo add error validator */
                         throw Error($.mage.__('Some problem with revert action'));
                         return;
                     }
-                },
-                error: function(data) {
+                    document.location.reload();
+                }, this),
+                error: function() {
                     throw Error($.mage.__('Some problem with revert action'));
                 }
             });
+        },
+
+        /**
+         * Enable button
+         * @private
+         */
+        _enableButton: function() {
+            this.element.removeClass('disabled');
+        },
+
+        /**
+         * Get dialog element
+         *
+         * @returns {*|HTMLElement}
+         * @private
+         */
+        _getDialog: function() {
+            return $(this.options.dialogSelector);
         }
     });
 })(jQuery);
