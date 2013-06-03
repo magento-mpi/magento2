da5892892914632bf2e4eff78d327a1cef3b812a
rbates <rbates@x.com>
2013-06-03 15:03:08 -0500

    Fixed MDS-553: Remove unnecessary DI declarations from tests configuration
     - Fixed failing integration test Mage_Core_Model_Dataservice_ConfigTest

--- dev/tests/integration/testsuite/Mage/Core/Model/Dataservice/ConfigTest.php
+++ dev/tests/integration/testsuite/Mage/Core/Model/Dataservice/ConfigTest.php
@@ -21,33 +21,31 @@ class Mage_Core_Model_Dataservice_ConfigTest extends PHPUnit_Framework_TestCase
 <?xml version="1.0"?>
 <config>
     <modules>
-        <Testmodule>
+        <Mage_Test>
             <version>1.6.0.0.23</version>
             <active>true</active>
             <depends/>
-        </Testmodule>
+        </Mage_Test>
     </modules>
-    <global>
-        <di/>
-        <service_calls>
-            <testmodule>
-                <file>Mage_TestModule/service_calls.xml</file>
-            </testmodule>
-        </service_calls>
-    </global>
 </config>
 XML;
         $dirs = Mage::getObjectManager()->create(
             'Mage_Core_Model_Dir', array('baseDir' => array(__DIR__ . '/_files'),
                                          'dirs'    => array(Mage_Core_Model_Dir::MODULES => __DIR__ . '/_files'),)
         );
-        $loader = Mage::getObjectManager()->create('Mage_Core_Model_Config_Loader_Modules', array('dirs' => $dirs,));
-        $config = Mage::getObjectManager()->create('Mage_Core_Model_Config_Base', array($xml));
-        $loader->load($config);
         $fileReader = Mage::getObjectManager()->create(
             'Mage_Core_Model_Config_Loader_Modules_File', array('dirs' => $dirs)
         );
-        $this->_config = new Mage_Core_Model_Dataservice_Config($config, $fileReader);
+        $loader = Mage::getObjectManager()->create(
+            'Mage_Core_Model_Config_Loader_Modules', array('dirs' => $dirs, 'fileReader' => $fileReader)
+        );
+        $config = Mage::getObjectManager()->create('Mage_Core_Model_Config_Base', array('sourceData' => $xml));
+        $loader->load($config);
+        /** @var Mage_Core_Model_Config_Modules_Reader $moduleReader */
+        $moduleReader = Mage::getObjectManager()->create(
+            'Mage_Core_Model_Config_Modules_Reader', array('fileReader' => $fileReader, 'modulesConfig' => $config)
+        );
+        $this->_config = new Mage_Core_Model_Dataservice_Config($moduleReader);
     }
 
     public function testGetClassByAlias()
