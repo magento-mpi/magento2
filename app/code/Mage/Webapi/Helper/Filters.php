4159dd32843f04df34dce1606b57942922be8587
Michael Chiocca <mchiocca@ebay.com>
2013-06-04 06:14:14 -0700

    Fixed MDS-202: Implement a helper to validate JSON filter strings - Moved filter helper, integration test classes, and JSON schema files to Webapi module.

--- app/code/Mage/Webapi/Helper/Filters.php
+++ app/code/Mage/Webapi/Helper/Filters.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Service Helper.
+ * Webapi Helper.
  *
  * {license_notice}
  *
  * @copyright   {copyright}
  * @license     {license_link}
  */
-class Mage_Core_Service_Helper_Filters
+class Mage_Webapi_Helper_Filters extends Mage_Core_Helper_Abstract
 {
     /**
      * @var \stdClass
@@ -27,7 +27,7 @@ class Mage_Core_Service_Helper_Filters
     public function __construct(Mage_Core_Helper_Context $context)
     {
         parent::__construct($context);
-        $moduleDir = Mage::getModuleDir('etc', 'Mage_Core');
+        $moduleDir = Mage::getModuleDir('etc', 'Mage_Webapi');
         $retriever = new JsonSchema\Uri\UriRetriever();
         $this->_schema = $retriever->retrieve('file://' . $moduleDir . DS . 'json' . DS . 'filters-schema.json');
         $refResolver = new JsonSchema\RefResolver($retriever);
