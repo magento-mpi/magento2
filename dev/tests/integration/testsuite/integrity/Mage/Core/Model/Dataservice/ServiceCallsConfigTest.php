599236db05d16447927b7b14bb217bb29566a33a
rbates <rbates@x.com>
2013-06-04 15:53:25 -0500

    Fixed MDS-532: removed unnecessary assertions that are validated as part of the xsd
     - improved quality of failure messages

--- dev/tests/integration/testsuite/integrity/Mage/Core/Model/Dataservice/ServiceCallsConfigTest.php
+++ dev/tests/integration/testsuite/integrity/Mage/Core/Model/Dataservice/ServiceCallsConfigTest.php
@@ -20,38 +20,23 @@ class Integrity_Mage_Core_Dataservice_Model_ServiceCallsConfigTest extends PHPUn
         $dom = new DOMDocument();
         $dom->loadXML(file_get_contents($configFile));
         $this->assertNotNull($dom);
-        $serviceCalls = $dom->getElementsByTagName('service_calls');
-        $this->assertNotNull($serviceCalls, "$configFile does not contain a service_calls tag");
-        $this->assertEquals(1, $serviceCalls->length, "$configFile contains more than one service_calls tag");
-        $serviceCalls = $serviceCalls->item(0);
+        $serviceCalls = $dom->getElementsByTagName('service_calls')->item(0);
         if ($serviceCalls->hasChildNodes()) {
             foreach ($serviceCalls->childNodes as $serviceCall) {
                 /** @var $serviceCall DOMNode */
                 if ($serviceCall->localName == 'service_call') {
-                    $this->assertTrue(
-                        $serviceCall->hasAttributes(), "$configFile has service_call tag with no attributes"
-                    );
-                    $this->assertNotNull(
-                        $serviceCall->attributes->getNamedItem('name'),
-                        "$configFile has service_call tag with no name attribute"
-                    );
                     $name = $serviceCall->attributes->getNamedItem('name')->nodeValue;
-                    $this->assertNotNull($name, "$configFile has service_call tag with empty name attribute");
-                    $this->assertNotNull(
-                        $serviceCall->attributes->getNamedItem('service'),
-                        "$configFile has service_call tag with no service attribute"
-                    );
                     $service = $serviceCall->attributes->getNamedItem('service')->nodeValue;
-                    $this->assertNotNull($service, "$configFile has service_call tag with empty service attribute");
-                    $this->assertNotNull(
-                        $serviceCall->attributes->getNamedItem('method'),
-                        "$configFile has service_call tag with no method attribute"
-                    );
                     $method = $serviceCall->attributes->getNamedItem('method')->nodeValue;
-                    $this->assertNotNull($method, "$configFile has service_call tag with empty method attribute");
-                    $ref = new ReflectionClass($service);
-                    $this->assertNotNull($ref, "$configFile has service_call tag with non-existent service class");
-                    $this->assertTrue($ref->hasMethod($method), "$configFile has service_call tag invalid method");
+                    try {
+                        $ref = new ReflectionClass($service);
+                    } catch (ReflectionException $re) {
+                        $this->fail("$configFile has service_call $name with non-existent service class $service: $re");
+                    }
+                    $this->assertTrue(
+                        $ref->hasMethod($method),
+                        "$configFile has service_call $name invalid method $method"
+                    );
                 }
             }
         }
