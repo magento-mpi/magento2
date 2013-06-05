30febc930a9e9e1317010bfa552f45c7bdd4d31a
Sergey Shymko <sergey.shymko@magento.com>
2013-06-04 18:09:04 -0700

    Implemented MAGETWO-10510: Move Store View Limitation from CE/EE to Go Edition - renamed Saas_Limitation_Model_Observer -> Saas_Limitation_Model_Store_Observer and its methods to not duplicate the store context in each method - removed excessive dependency of the store limitation observer on Mage_Core_Controller_Request_Http in favor of subscription to more specific event - implemented unit test for the store limitation observer

--- app/code/Saas/Limitation/Model/Store/Observer.php
+++ app/code/Saas/Limitation/Model/Store/Observer.php
@@ -5,13 +5,8 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-class Saas_Limitation_Model_Observer
+class Saas_Limitation_Model_Store_Observer
 {
-    /**
-     * @var Mage_Core_Controller_Request_Http
-     */
-    private $_request;
-
     /**
      * @var Mage_Backend_Model_Session
      */
@@ -23,30 +18,27 @@ class Saas_Limitation_Model_Observer
     private $_storeLimitation;
 
     /**
-     * @param Mage_Core_Controller_Request_Http $request
      * @param Mage_Backend_Model_Session $session
      * @param Saas_Limitation_Model_Store_Limitation $storeLimitation
      */
     public function __construct(
-        Mage_Core_Controller_Request_Http $request,
         Mage_Backend_Model_Session $session,
         Saas_Limitation_Model_Store_Limitation $storeLimitation
     ) {
-        $this->_request = $request;
         $this->_session = $session;
         $this->_storeLimitation = $storeLimitation;
     }
 
     /**
-     * Display message in the notification area upon reaching limitations
+     * Restrict creation of new stores upon reaching the limitation
      *
      * @param Varien_Event_Observer $observer
      * @throws Mage_Core_Exception
      */
-    public function restrictStoreCreation(Varien_Event_Observer $observer)
+    public function restrictEntityCreation(Varien_Event_Observer $observer)
     {
         /** @var Mage_Core_Model_Abstract $model */
-        $model = $observer->getEvent()->getData('store');
+        $model = $observer->getEvent()->getData('data_object');
         if ($model->isObjectNew() && $this->_storeLimitation->isCreateRestricted()) {
             $message = $this->_storeLimitation->getCreateRestrictedMessage();
             $exception = new Mage_Core_Exception($message);
@@ -56,11 +48,11 @@ class Saas_Limitation_Model_Observer
     }
 
     /**
-     * Remove/disable grid buttons upon reaching limitations
+     * Disable the store creation button in the grid upon reaching the limitation
      *
      * @param Varien_Event_Observer $observer
      */
-    public function updateGridButtons(Varien_Event_Observer $observer)
+    public function disableCreationButton(Varien_Event_Observer $observer)
     {
         /** @var Mage_Backend_Block_Widget_Container $block */
         $block = $observer->getEvent()->getData('block');
@@ -72,29 +64,15 @@ class Saas_Limitation_Model_Observer
     }
 
     /**
-     * Display message in the notification area upon reaching limitations
+     * Display message in the notification area upon reaching the limitation
      *
      * @param Varien_Event_Observer $observer
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function displayNotification(Varien_Event_Observer $observer)
     {
-        if ($this->_getControllerAction() == 'Mage_Adminhtml/system_store/index') {
-            if ($this->_storeLimitation->isCreateRestricted()) {
-                $this->_session->addNotice($this->_storeLimitation->getCreateRestrictedMessage());
-            }
+        if ($this->_storeLimitation->isCreateRestricted()) {
+            $this->_session->addNotice($this->_storeLimitation->getCreateRestrictedMessage());
         }
-    }
-
-    /**
-     * Retrieve a unique identifier for the current controller action
-     *
-     * @return string
-     */
-    protected function _getControllerAction()
-    {
-        return $this->_request->getControllerModule()
-            . '/' . $this->_request->getControllerName()
-            . '/' . $this->_request->getActionName();
     }
 }
