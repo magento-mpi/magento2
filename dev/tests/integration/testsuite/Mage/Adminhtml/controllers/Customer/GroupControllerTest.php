529e0290e7c899059592df3049eb16b53b8168da
Maksym Aposov <maposov@ebay.com>
2013-06-06 17:26:49 +0300

    Fixed MAGETWO-5774: Customer Group with NOT LOGGED IN gets deleted on delete of non existing group id

--- dev/tests/integration/testsuite/Mage/Adminhtml/controllers/Customer/GroupControllerTest.php
+++ dev/tests/integration/testsuite/Mage/Adminhtml/controllers/Customer/GroupControllerTest.php
@@ -20,4 +20,42 @@ class Mage_Adminhtml_Customer_GroupControllerTest extends Mage_Backend_Utility_C
         $responseBody = $this->getResponse()->getBody();
         $this->assertRegExp('/<h1 class\="title">\s*New Customer Group\s*<\/h1>/', $responseBody);
     }
+
+    /**
+     * @magentoAppIsolation enabled
+     * @magentoDataFixture Mage/Adminhtml/controllers/_files/customer_group_sample.php
+     */
+    public function testDeleteActionExistingGroup()
+    {
+        /** @var $registry Mage_Core_Model_Registry */
+        $registry = Mage::getObjectManager()->get('Mage_Core_Model_Registry');
+        /** @var $group Mage_Customer_Model_Group */
+        $group = $registry->registry('_fixture/Mage_Customer_Model_Group');
+
+        $this->getRequest()->setParam('id', $group->getId());
+        $this->dispatch('backend/admin/customer_group/delete');
+
+        /**
+         * Check that success message is set
+         */
+        $this->assertSessionMessages(
+            $this->equalTo(array('The customer group has been deleted.')), Mage_Core_Model_Message::SUCCESS
+        );
+    }
+
+    /**
+     * @magentoAppIsolation enabled
+     */
+    public function testDeleteActionNonExistingGroupId()
+    {
+        $this->getRequest()->setParam('id', 10000);
+        $this->dispatch('backend/admin/customer_group/delete');
+
+        /**
+         * Check that error message is set
+         */
+        $this->assertSessionMessages(
+            $this->equalTo(array('The customer group no longer exists.')), Mage_Core_Model_Message::ERROR
+        );
+    }
 }
