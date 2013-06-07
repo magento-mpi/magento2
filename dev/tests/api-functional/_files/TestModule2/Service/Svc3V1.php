faae6267a753edf1236b7b19a5ae68e82e6dfdb2
Michael Chiocca <mchiocca@ebay.com>
2013-06-07 10:29:32 -0700

    Implemented MDS-546: Implement TestModule2 with Three Services - Created TestModule2 containing three test services. - Created api-functional directory under dev/tests for service testing.

---
+++ dev/tests/api-functional/_files/TestModule2/Service/Svc3V1.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+class Mage_TestModule2_Service_Svc3V1 implements Mage_TestModule2_Service_Svc3InterfaceV1
+{
+    /**
+     * @param array $request
+     * @return array
+     */
+    public function item($request)
+    {
+        return array(
+            'id' => $request['id']
+        );
+    }
+
+    /**
+     * @param array $request
+     * @return array
+     */
+    public function items($request)
+    {
+        return array(
+            array('Hello' => 'World')
+        );
+    }
+
+    /**
+     * @param array $request
+     * @return array
+     */
+    public function create($request)
+    {
+        return array(
+            'id' => rand()
+        );
+    }
+
+    /**
+     * @param array $request
+     * @return array
+     */
+    public function update($request)
+    {
+        return array(
+            'id' => $request['id']
+        );
+    }
+
+    /**
+     * @param array $request
+     * @return array
+     */
+    public function remove($request)
+    {
+        return array(
+            'id' => $request['id']
+        );
+    }
+}
