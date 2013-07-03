8ebd88be073577256dbbb3de56af12c313772ae1
Iryna Lagno <ilagno@ebay.com>
2013-07-03 12:05:39 +0300

    Implemented MAGETWO-11015 : Convert Mage_Adminhtml_Block_Api_User_Grid on layout: - add options class

---
+++ app/code/Mage/Api/Model/Resource/User/Options/Statuses.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Mage
+ * @package     Mage_Api
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Api User Resource Options Statuses
+ *
+ * @category    Mage
+ * @package     Mage_Api
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Api_Model_Resource_User_Options_Statuses implements Mage_Core_Model_Option_ArrayInterface
+{
+
+    /**
+     * @var
+     */
+    protected $_helper;
+
+    /**
+     * @param Mage_Backend_Helper_Data $helper
+     */
+    public function __construct(Mage_Backend_Helper_Data $helper)
+    {
+        $this->_helper = $helper;
+    }
+
+    /**
+     * Return options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return array(
+            '1' => $this->_helper->__('Active'),
+            '0' => $this->_helper->__('Inactive'),
+        );
+    }
+}
