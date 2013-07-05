3e89a94ba134e0a11824a6b328a2618829bcc203
Serhii Dzhepa <sdzhepa@ebay.com>
2013-07-05 17:07:13 +0300

    Fixed MAGETWO-11062: Convert Mage_Adminhtml_Block_Promo_Catalog_Grid through layout - fixed after CR

--- app/code/Mage/Promo/Catalog/Model/Resource/Grid/Options/Statuses.php
+++ app/code/Mage/Promo/Catalog/Model/Resource/Grid/Options/Statuses.php
@@ -10,14 +10,14 @@
 class Mage_Promo_Catalog_Model_Resource_Grid_Options_Statuses implements Mage_Core_Model_Option_ArrayInterface
 {
     /**
-     * @var Mage_Index_Helper_Data
+     * @var Mage_CatalogRule_Helper_Data
      */
     protected $_helper;
 
     /**
-     * @param Mage_Index_Helper_Data $helper
+     * @param Mage_CatalogRule_Helper_Data $helper
      */
-    public function __construct(Mage_Index_Helper_Data $helper)
+    public function __construct(Mage_CatalogRule_Helper_Data $helper)
     {
         $this->_helper = $helper;
     }
@@ -30,8 +30,8 @@ class Mage_Promo_Catalog_Model_Resource_Grid_Options_Statuses implements Mage_Co
     public function toOptionArray()
     {
         return array(
-            '0' => $this->_helper->__('Inactive'),
             '1' => $this->_helper->__('Active'),
+            '0' => $this->_helper->__('Inactive'),
         );
     }
 }
