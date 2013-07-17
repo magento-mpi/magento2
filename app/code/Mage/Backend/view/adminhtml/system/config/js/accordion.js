5619652c93d79076008b2247e42108e3eab514de
Mark Gee <mgee@ebay.com>
2013-07-17 10:26:58 -0700

    FIXED MAGETWO-11194 - added a class/widget comment header per code review

--- app/code/Mage/Backend/view/adminhtml/system/config/js/accordion.js
+++ app/code/Mage/Backend/view/adminhtml/system/config/js/accordion.js
@@ -5,7 +5,12 @@
  * @license     {license_link}
  */
 
+
 (function($) {
+
+    /**
+     * Accordion Widget - this widget is a wrapper for the jQuery UI Accordion
+     */
     $.widget('mage.accordion', $.ui.accordion, {
     });
 })(jQuery);
