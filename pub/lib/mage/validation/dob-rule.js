f87b7706d5d6cf7eddb66f4110fe89f7772fbade
Ann Beeskau <abeeskau@ebay.com>
2013-07-19 15:21:03 -0700

    CR Change MAGETWO-6685: DOB validation allows entering a future date in the current year  - single line var declaration

--- pub/lib/mage/validation/dob-rule.js
+++ pub/lib/mage/validation/dob-rule.js
@@ -45,8 +45,8 @@
                 this.dobErrorMessage = $.mage.__('Please enter a valid day (1-%d).').replace('%d', validateDayInMonth);
                 return false;
             }
-            var today = new Date();
-            var dateEntered = new Date();
+            var today = new Date(),
+                dateEntered = new Date();
             dateEntered.setFullYear(year, month - 1, day);
             if (dateEntered > today) {
                 this.dobErrorMessage = $.mage.__('Please enter a date in the past.');
