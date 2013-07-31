d81555fd68a766110be0b100990f04e8df4a7f41
Valeriy Nayda <vnayda@ebay.com>
2013-07-31 13:07:04 +0300

    Implemented MAGETWO-11900: Unbind from module context  - removed context from Mage_Core_Block_Abstract

--- app/code/Mage/Core/Block/Abstract.php
+++ app/code/Mage/Core/Block/Abstract.php
@@ -838,10 +838,7 @@ abstract class Mage_Core_Block_Abstract extends Varien_Object
      */
     public function __()
     {
-        $args = func_get_args();
-        $expr = new Mage_Core_Model_Translate_Expr(array_shift($args), $this->getModuleName());
-        array_unshift($args, $expr);
-        return $this->_translator->translate($args);
+        return $this->_translator->translate(func_get_args());
     }
 
     /**
