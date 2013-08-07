410c958f6918c5bf102421bb5d1d7b0c61de0f76
Valeriy Nayda <vnayda@ebay.com>
2013-08-07 16:38:38 +0300

    Implemented MAGETWO-11900: Unbind from module context  - removed module context from  logging.xml files (removed by phpstorm by pattern ' module="[A-Z]+[a-z0-9]{1,}_[A-Z]+[A-Z0-9a-z]{1,}"')   - removed module context from Enterprise_Logging_Model_Config

--- app/code/Enterprise/Logging/Model/Config.php
+++ app/code/Enterprise/Logging/Model/Config.php
@@ -122,11 +122,7 @@ class Enterprise_Logging_Model_Config
     {
         if (!$this->_labels) {
             foreach ($this->_xmlConfig->getXpath('/logging/*/label') as $labelNode) {
-                $helperName = $labelNode->getParent()->getAttribute('module');
-                if (!$helperName) {
-                    $helperName = 'Enterprise_Logging_Helper_Data';
-                }
-                $this->_labels[$labelNode->getParent()->getName()] = Mage::helper($helperName)->__((string)$labelNode);
+                $this->_labels[$labelNode->getParent()->getName()] = __((string)$labelNode);
             }
             asort($this->_labels);
         }
@@ -148,11 +144,7 @@ class Enterprise_Logging_Model_Config
             return $action;
         }
 
-        $label = (string)$actionLabelNode;
-        $module = $actionLabelNode->getParent()->getAttribute('module');
-        $helper = $module ? Mage::helper($module) : Mage::helper('Enterprise_Logging_Helper_Data');
-
-        return $helper->__($label);
+        return __((string)$actionLabelNode);
     }
 
     /**
