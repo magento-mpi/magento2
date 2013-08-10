2d97ecb918a71b82f233b308f85ffb78127d2340
Ann Beeskau <abeeskau@ebay.com>
2013-08-10 08:07:04 -0700

    Fixed MAGETWO-12302: Make /dev/tests/ PSR-0 Compliant - added new dir path to static bootstrap and phpunit xml

--- dev/tests/static/framework/bootstrap.php
+++ dev/tests/static/framework/bootstrap.php
@@ -13,7 +13,7 @@ $baseDir = realpath(__DIR__ . '/../../../../');
 require $baseDir . '/app/autoload.php';
 Magento_Autoload_IncludePath::addIncludePath(array(
     __DIR__,
-    dirname(__DIR__) . '/testsuite',
+    dirname(__DIR__) . '/testsuite/Magento/Test',
     $baseDir . '/lib',
 ));
 Magento_TestFramework_Utility_Files::init(new Magento_TestFramework_Utility_Files($baseDir));
