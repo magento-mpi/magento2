ec90fded8c39e1c9ffad548e4318828bc7b47522
Ann Beeskau <abeeskau@ebay.com>
2013-08-11 12:08:27 -0500

    Implemented MAGETWO-12536: Make Mage_Catalog* modules PSR-0 Compliant
    - Refactored Mage_CatalogRule to Magento_CatalogRule

---
+++ app/code/Magento/CatalogRule/Helper/Data.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_CatalogRule
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * CatalogRule data helper
+ */
+class Magento_CatalogRule_Helper_Data extends Magento_Core_Helper_Abstract
+{
+    /**
+     * Algorithm for calculating price rule
+     *
+     * @param  string $actionOperator
+     * @param  int $ruleAmount
+     * @param  float $price
+     * @return float|int
+     */
+    public function calcPriceRule($actionOperator, $ruleAmount, $price)
+    {
+        $priceRule = 0;
+        switch ($actionOperator) {
+            case 'to_fixed':
+                $priceRule = min($ruleAmount, $price);
+                break;
+            case 'to_percent':
+                $priceRule = $price * $ruleAmount / 100;
+                break;
+            case 'by_fixed':
+                $priceRule = max(0, $price - $ruleAmount);
+                break;
+            case 'by_percent':
+                $priceRule = $price * (1 - $ruleAmount / 100);
+                break;
+        }
+        return $priceRule;
+    }
+}
---
+++ app/code/Magento/CatalogRule/Model/Rule/Action/Product.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_CatalogRule
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+
+class Magento_CatalogRule_Model_Rule_Action_Product extends Mage_Rule_Model_Action_Abstract
+{
+    public function loadAttributeOptions()
+    {
+        $this->setAttributeOption(array(
+            'rule_price'=>Mage::helper('Magento_CatalogInventory_Helper_Data')->__('Rule price'),
+        ));
+        return $this;
+    }
+
+    public function loadOperatorOptions()
+    {
+        $this->setOperatorOption(array(
+            'to_fixed'=>Mage::helper('Magento_CatalogInventory_Helper_Data')->__('To Fixed Value'),
+            'to_percent'=>Mage::helper('Magento_CatalogInventory_Helper_Data')->__('To Percentage'),
+            'by_fixed'=>Mage::helper('Magento_CatalogInventory_Helper_Data')->__('By Fixed value'),
+            'by_percent'=>Mage::helper('Magento_CatalogInventory_Helper_Data')->__('By Percentage'),
+        ));
+        return $this;
+    }
+
+    public function asHtml()
+    {
+        $html = $this->getTypeElement()->getHtml().Mage::helper('Magento_CatalogRule_Helper_Data')->__("Update product's %s %s: %s", $this->getAttributeElement()->getHtml(), $this->getOperatorElement()->getHtml(), $this->getValueElement()->getHtml());
+        $html.= $this->getRemoveLinkHtml();
+        return $html;
+    }
+}
