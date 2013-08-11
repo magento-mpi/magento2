7e2ffc3160d989f193bb14b642fa9acecea0c066
Ann Beeskau <abeeskau@ebay.com>
2013-08-11 12:06:49 -0500

    Implemented MAGETWO-12536: Make Mage_Catalog* modules PSR-0 Compliant
    - Refactored Mage_CatalogInventory to Magento_CatalogInventory

---
+++ app/code/Magento/CatalogInventory/Model/Source/Backorders.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_CatalogInventory
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Magento_CatalogInventory_Model_Source_Backorders
+{
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => Magento_CatalogInventory_Model_Stock::BACKORDERS_NO, 'label'=>Mage::helper('Magento_CatalogInventory_Helper_Data')->__('No Backorders')),
+            array('value' => Magento_CatalogInventory_Model_Stock::BACKORDERS_YES_NONOTIFY, 'label'=>Mage::helper('Magento_CatalogInventory_Helper_Data')->__('Allow Qty Below 0')),
+            array('value' => Magento_CatalogInventory_Model_Stock::BACKORDERS_YES_NOTIFY , 'label'=>Mage::helper('Magento_CatalogInventory_Helper_Data')->__('Allow Qty Below 0 and Notify Customer')),
+        );
+    }
+}
