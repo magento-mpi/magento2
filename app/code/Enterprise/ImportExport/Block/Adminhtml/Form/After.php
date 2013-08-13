c3c6b9adf3f253aae726aecc58c08d71a53fda50
Maxim Shikula <mshikula@ebay.com>
2013-08-13 16:59:40 +0300

    Implemented MAGETWO-12598: Elimination of "Mage::" from templates

---
+++ app/code/Enterprise/ImportExport/Block/Adminhtml/Form/After.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Enterprise
+ * @package     Enterprise_ImportExport
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Block form after
+ */
+class Enterprise_ImportExport_Block_Adminhtml_Form_After extends Mage_Backend_Block_Template
+{
+    /**
+     * @var Mage_Core_Model_Registry
+     */
+    protected $_registry;
+
+    /**
+     * @param Mage_Core_Block_Template_Context $context
+     * @param Mage_Core_Model_Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        Mage_Core_Block_Template_Context $context,
+        Mage_Core_Model_Registry $registry,
+        array $data = array()
+    ) {
+        $this->_registry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return Mage_Core_Model_Registry
+     */
+    public function getRegistry()
+    {
+        return $this->_registry;
+    }
+}
--- app/code/Mage/Catalog/Block/Navigation.php
+++ app/code/Mage/Catalog/Block/Navigation.php
@@ -34,6 +34,25 @@ class Mage_Catalog_Block_Navigation extends Mage_Core_Block_Template
      */
     protected $_itemLevelPositions = array();
 
+    /**
+     * @var Mage_Core_Model_Registry
+     */
+    protected $_registry;
+
+    /**
+     * @param Mage_Core_Block_Template_Context $context
+     * @param Mage_Core_Model_Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        Mage_Core_Block_Template_Context $context,
+        Mage_Core_Model_Registry $registry,
+        array $data = array()
+    ) {
+        $this->_registry = $registry;
+        parent::__construct($context, $data);
+    }
+
     protected function _construct()
     {
         $this->addData(array(
@@ -43,6 +62,14 @@ class Mage_Catalog_Block_Navigation extends Mage_Core_Block_Template
                 Mage_Core_Model_Store_Group::CACHE_TAG
             ),
         ));
+    }
+
+    /**
+     * @return Mage_Core_Model_Registry
+     */
+    public function getRegistry()
+    {
+        return $this->_registry;
     }
 
     /**
