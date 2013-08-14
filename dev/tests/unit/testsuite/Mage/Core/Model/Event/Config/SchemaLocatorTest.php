9ba82816cb06a994849108cdad91b8114fc10b5e
Iryna Lagno <ilagno@ebay.com>
2013-08-14 14:55:46 +0300

    Implemented MAGETWO-12757: Event configuration model refactoring: -cover with unit tests

--- dev/tests/unit/testsuite/Mage/Core/Model/Event/Config/SchemaLocatorTest.php
+++ dev/tests/unit/testsuite/Mage/Core/Model/Event/Config/SchemaLocatorTest.php
@@ -8,35 +8,30 @@
 class Mage_Core_Model_Event_Config_SchemaLocatorTest extends PHPUnit_Framework_TestCase
 {
     /**
-    * @var PHPUnit_Framework_MockObject_MockObject
+     * @var PHPUnit_Framework_MockObject_MockObject
      */
     protected $_moduleReaderMock;
 
     /**
-    * @var Mage_Core_Model_Event_Config_SchemaLocator
+     * @var Mage_Core_Model_Event_Config_SchemaLocator
      */
     protected $_model;
 
     protected function setUp()
     {
-        /** @todo Implement test logic here */
-        
         $this->_moduleReaderMock = $this->getMock('Mage_Core_Model_Config_Modules_Reader', array(), array(), '', false);
-        
+        $this->_moduleReaderMock->expects($this->once())
+            ->method('getModuleDir')->with('etc', 'Mage_Core')->will($this->returnValue('schema_dir'));
         $this->_model = new Mage_Core_Model_Event_Config_SchemaLocator($this->_moduleReaderMock);
     }
 
     public function testGetSchema()
     {
-        /** @todo Implement test logic here */
-        
-        $this->_model->getSchema();
+        $this->assertEquals('schema_dir' . DIRECTORY_SEPARATOR. 'event.xsd', $this->_model->getSchema());
     }
 
     public function testGetPerFileSchema()
     {
-        /** @todo Implement test logic here */
-        
-        $this->_model->getPerFileSchema();
+        $this->assertEquals(null, $this->_model->getPerFileSchema());
     }
 }
