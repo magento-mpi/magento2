b0844e70ef7fbf5b9d383c90017b0a7d26e169a9
Iryna Lagno <ilagno@ebay.com>
2013-08-14 14:58:08 +0300

    Implemented MAGETWO-12759: Enterprise Queue adoption: -cover with unit tests

--- dev/tests/unit/testsuite/Enterprise/Queue/Model/Event/Config/ConverterTest.php
+++ dev/tests/unit/testsuite/Enterprise/Queue/Model/Event/Config/ConverterTest.php
@@ -8,23 +8,31 @@
 class Enterprise_Queue_Model_Event_Config_ConverterTest extends PHPUnit_Framework_TestCase
 {
     /**
-    * @var Enterprise_Queue_Model_Event_Config_Converter
+     * @var Enterprise_Queue_Model_Event_Config_Converter
      */
     protected $_model;
 
+    /**
+     * @var string
+     */
+    protected $_filePath;
+
+    /**
+     * @var DOMDocument
+     */
+    protected $_source;
+
     protected function setUp()
     {
-        /** @todo Implement test logic here */
-        
+        $this->_filePath = __DIR__ . DIRECTORY_SEPARATOR . '/../../../_files' . DIRECTORY_SEPARATOR;
+        $this->_source = new DOMDocument();
         $this->_model = new Enterprise_Queue_Model_Event_Config_Converter();
     }
 
     public function testConvert()
     {
-        /** @todo Implement test logic here */
-
-        $source = null;
-
-        $this->_model->convert($source);
+        $this->_source->loadXML(file_get_contents($this->_filePath. 'event_config.xml'));
+        $convertedFile = include ($this->_filePath. 'event_config.php');
+        $this->assertEquals($convertedFile, $this->_model->convert($this->_source));
     }
 }
