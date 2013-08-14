07f8069b97c5a751d8d1702ce77315547a8b6871
Mark Gee <mgee@ebay.com>
2013-08-14 11:23:58 -0700

    Fixed MAGETWO-12559: JavaScript in themes - app\design\frontend\enterprise\fixed\Mage_Checkout\opcheckout.js
    - deleted /app/design/frontend/magento_fixed_width/Mage_Checkout/opcheckout.js
    - deleted /app/code/Mage/Checkout/view/frontend/opcheckout.js
    - move the class definition for 'Review' from /app/design/frontend/magento_fixed_width/Mage_Checkout/opcheckout.js to
    /app/design/frontend/magento_iphone_html5/js/iphone.js
    
    NOTE: This is a squash of 3 commits.

--- app/design/frontend/magento_iphone_html5/js/iphone.js
+++ app/design/frontend/magento_iphone_html5/js/iphone.js
@@ -1125,3 +1125,77 @@ function taxToggle(details, switcher, expandedClassName) {
         $(switcher).removeClassName(expandedClassName);
     }
 }
+
+var Review = Class.create();
+Review.prototype = {
+    initialize: function(saveUrl, successUrl, agreementsForm){
+        this.saveUrl = saveUrl;
+        this.successUrl = successUrl;
+        this.agreementsForm = agreementsForm;
+        this.onSave = this.nextStep.bindAsEventListener(this);
+        this.onComplete = this.resetLoadWaiting.bindAsEventListener(this);
+    },
+
+    save: function(){
+        if (checkout.loadWaiting!=false) return;
+        checkout.setLoadWaiting('review');
+        var params = Form.serialize(payment.form);
+        if (this.agreementsForm) {
+            params += '&'+Form.serialize(this.agreementsForm);
+        }
+        params.save = true;
+        var request = new Ajax.Request(
+            this.saveUrl,
+            {
+                method:'post',
+                parameters:params,
+                onComplete: this.onComplete,
+                onSuccess: this.onSave,
+                onFailure: checkout.ajaxFailure.bind(checkout)
+            }
+        );
+    },
+
+    resetLoadWaiting: function(transport){
+        checkout.setLoadWaiting(false, this.isSuccess);
+    },
+
+    nextStep: function(transport){
+        if (transport && transport.responseText) {
+            try{
+                response = eval('(' + transport.responseText + ')');
+            }
+            catch (e) {
+                response = {};
+            }
+            if (response.redirect) {
+                this.isSuccess = true;
+                location.href = response.redirect;
+                return;
+            }
+            if (response.success) {
+                this.isSuccess = true;
+                window.location=this.successUrl;
+            }
+            else{
+                var msg = response.error_messages;
+                if (typeof(msg)=='object') {
+                    msg = msg.join("\n");
+                }
+                if (msg) {
+                    alert(msg);
+                }
+            }
+
+            if (response.update_section) {
+                $('checkout-'+response.update_section.name+'-load').update(response.update_section.html);
+            }
+
+            if (response.goto_section) {
+                checkout.gotoSection(response.goto_section);
+            }
+        }
+    },
+
+    isSuccess: false
+}
