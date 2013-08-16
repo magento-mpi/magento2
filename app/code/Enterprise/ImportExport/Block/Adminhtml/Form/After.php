8bc0a9a65e1a9d895fd06f283756a8a33c350f7f
Andrey Voinkov <avoinkov@ebay.com>
2013-08-16 18:16:57 +0300

    Fixed MAGETWO-12912: Fatal error on Import/Export pages - another error

--- app/code/Enterprise/ImportExport/Block/Adminhtml/Form/After.php
+++ app/code/Enterprise/ImportExport/Block/Adminhtml/Form/After.php
@@ -19,12 +19,12 @@ class Enterprise_ImportExport_Block_Adminhtml_Form_After extends Mage_Backend_Bl
     protected $_registry;
 
     /**
-     * @param Mage_Core_Block_Template_Context $context
+     * @param Mage_Backend_Block_Template_Context $context
      * @param Mage_Core_Model_Registry $registry
      * @param array $data
      */
     public function __construct(
-        Mage_Core_Block_Template_Context $context,
+        Mage_Backend_Block_Template_Context $context,
         Mage_Core_Model_Registry $registry,
         array $data = array()
     ) {
