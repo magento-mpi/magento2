e33612319b8c278b906fead0e959d637282d06e6
Iryna Lagno <ilagno@ebay.com>
2013-08-19 13:48:47 +0300

    Implemented MAGETWO-12735: Convert Enterprise_CustomerSegment_Block_Adminhtml_Report_Customer_Segment_Detail: - cover with functional test; - add "Zip" column to grid; - fix options class using DI

--- app/code/Enterprise/CustomerSegment/Model/Resource/Segment/Report/Detail/Group/Option.php
+++ app/code/Enterprise/CustomerSegment/Model/Resource/Segment/Report/Detail/Group/Option.php
@@ -9,11 +9,24 @@ class Enterprise_CustomerSegment_Model_Resource_Segment_Report_Detail_Group_Opti
     implements Mage_Core_Model_Option_ArrayInterface
 {
     /**
+     * @var Mage_Customer_Model_Resource_Group_Collection
+     */
+    protected $_resourceCollection;
+
+    /**
+     * @param Mage_Customer_Model_Resource_Group_Collection $groupCollection
+     */
+    public function __construct(Mage_Customer_Model_Resource_Group_Collection $groupCollection)
+    {
+        $this->_resourceCollection = $groupCollection;
+    }
+
+    /**
      * @return array
      */
     public function toOptionArray()
     {
-        return Mage::getResourceModel('Mage_Customer_Model_Resource_Group_Collection')
+        return $this->_resourceCollection
             ->addFieldToFilter('customer_group_id', array('gt'=> 0))
             ->load()
             ->toOptionHash();
