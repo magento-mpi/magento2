7b95bd787159a5f4d4a95dc91e0aff35c858ea8e
Oleh Kobchenko <okobchenko@ebay.com>
2013-08-19 17:26:21 +0300

    Fixed MAGETWO-12746: Client code adoption - unit tests fixes

--- dev/tests/unit/testsuite/Mage/Core/Controller/Varien/Router/FactoryTest.php
+++ dev/tests/unit/testsuite/Mage/Core/Controller/Varien/Router/FactoryTest.php
@@ -56,24 +56,4 @@ class Mage_Core_Controller_Varien_Router_FactoryTest extends PHPUnit_Framework_T
 
         $this->assertEquals('TestRouterInstance', $this->_controller->createRouter(self::CLASS_NAME));
     }
-
-    public function testCreateRouterWithArguments()
-    {
-        $arguments = array(
-            'areaCode'       => self::AREA,
-            'baseController' => self::BASE_CONTROLLER,
-        );
-
-        $routerInfo = array(
-            'area'            => self::AREA,
-            'base_controller' => self::BASE_CONTROLLER,
-        );
-
-        $this->_objectManager->expects($this->once())
-            ->method('create')
-            ->with(self::CLASS_NAME, $arguments)
-            ->will($this->returnValue('TestRouterInstance'));
-
-        $this->assertEquals('TestRouterInstance', $this->_controller->createRouter(self::CLASS_NAME, $routerInfo));
-    }
 }
