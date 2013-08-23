a3f709c705defbbe416667811b901628480cfd41
Bibu Mathew <bimathew@paypal.com>
2013-08-23 15:07:49 -0500

    Namespace script added

--- dev/tools/nmparser/PHP-Parser-0.9.3/nmscanner/namespace.php
+++ dev/tools/nmparser/PHP-Parser-0.9.3/nmscanner/namespace.php
@@ -41,8 +41,8 @@ class namespacer
     {
         $this->path = $path;
         $this->rootDirectory = $rootDirectory;
-        $this->renameFileLogger=$this->renameFileLogger.time();
-        $this->renameClassLogger=$this->renameClassLogger.time();
+        $this->renameFileLogger=time().$this->renameFileLogger;
+        $this->renameClassLogger=time().$this->renameClassLogger;
 
     }
 
