6e942e838115fd3119b646113dde393fee2c5fba
Bibu Mathew <bimathew@paypal.com>
2013-08-24 00:06:50 -0500

    Fixed the interface issue

--- dev/tools/nmparser/PHP-Parser-0.9.3/nmscanner/namespace.php
+++ dev/tools/nmparser/PHP-Parser-0.9.3/nmscanner/namespace.php
@@ -84,10 +84,12 @@ class namespacer
         $php_code = file_get_contents($file);
         $tokens = token_get_all($php_code);
         $count = count($tokens);
+
         for ($i = 2; $i < $count; $i++) {
-            if ($tokens[$i - 2][0] == T_CLASS
+
+            if ($tokens[$i - 2][0]==T_INTERFACE||($tokens[$i - 2][0] == T_CLASS
                 && $tokens[$i - 1][0] == T_WHITESPACE
-                && $tokens[$i][0] == T_STRING
+                && $tokens[$i][0] == T_STRING)
             ) {
 
                 $class_name = $tokens[$i][1];
