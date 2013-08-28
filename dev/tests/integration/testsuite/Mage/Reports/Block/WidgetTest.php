25fe60518a8085dd859f2c0e4edee2361a4330d2
rbates <rbates@x.com>
2013-08-28 11:16:22 -0500

    Fixed MAGETWO-13407: Fix integration test failures for Mage_Reports_Block_WidgetTest

--- dev/tests/integration/testsuite/Mage/Reports/Block/WidgetTest.php
+++ dev/tests/integration/testsuite/Mage/Reports/Block/WidgetTest.php
@@ -15,16 +15,23 @@ class Mage_Reports_Block_WidgetTest extends PHPUnit_Framework_TestCase
     {
         $model = Mage::getModel('Mage_Widget_Model_Widget_Instance');
         $config = $model->setType('Mage_Reports_Block_Product_Widget_Viewed')->getWidgetConfigInArray();
-        $templates = $config->xpath('parameters/template/values');
-        $templates = (array) $templates[0]->children();
+
+        $this->assertArrayHasKey('parameters', $config);
+        $templates = $config['parameters'];
+        $this->assertArrayHasKey('template', $templates);
+        $templates = $templates['template'];
+        $this->assertArrayHasKey('values', $templates);
+        $templates = $templates['values'];
+
         $this->assertArrayHasKey('default', $templates);
         $this->assertArrayHasKey('list', $templates);
         $this->assertArrayHasKey('list_default', $templates);
         $this->assertArrayHasKey('list_names', $templates);
         $this->assertArrayHasKey('list_images', $templates);
 
-        $blocks = $config->xpath('supported_containers');
-        $blocks = (array) $blocks[0]->children();
+        $this->assertArrayHasKey('supported_containers', $config);
+        $blocks = $config['supported_containers'];
+
         $this->assertArrayHasKey('left_column', $blocks);
         $this->assertArrayHasKey('main_content', $blocks);
         $this->assertArrayHasKey('right_column', $blocks);
@@ -34,16 +41,23 @@ class Mage_Reports_Block_WidgetTest extends PHPUnit_Framework_TestCase
     {
         $model = Mage::getModel('Mage_Widget_Model_Widget_Instance');
         $config = $model->setType('Mage_Reports_Block_Product_Widget_Compared')->getWidgetConfigInArray();
-        $templates = $config->xpath('parameters/template/values');
-        $templates = (array) $templates[0]->children();
+
+        $this->assertArrayHasKey('parameters', $config);
+        $templates = $config['parameters'];
+        $this->assertArrayHasKey('template', $templates);
+        $templates = $templates['template'];
+        $this->assertArrayHasKey('values', $templates);
+        $templates = $templates['values'];
+
         $this->assertArrayHasKey('default', $templates);
         $this->assertArrayHasKey('list', $templates);
         $this->assertArrayHasKey('list_default', $templates);
         $this->assertArrayHasKey('list_names', $templates);
         $this->assertArrayHasKey('list_images', $templates);
 
-        $blocks = $config->xpath('supported_containers');
-        $blocks = (array) $blocks[0]->children();
+        $this->assertArrayHasKey('supported_containers', $config);
+        $blocks = $config['supported_containers'];
+
         $this->assertArrayHasKey('left_column', $blocks);
         $this->assertArrayHasKey('main_content', $blocks);
         $this->assertArrayHasKey('right_column', $blocks);
