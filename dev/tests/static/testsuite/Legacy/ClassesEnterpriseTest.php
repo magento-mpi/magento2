744fd8126c8faa915c5d53f647311929afd3b5ca
Chiu,Lawrence(lachiu) <lachiu@ebay.com>
2013-08-28 11:33:33 -0700

    Fixed MAGETWO-13040: Add Integrity Test to Enforce PSR-0 Standard -Updated code based on code review feedback CR-MAGETWO-5095

--- dev/tests/static/testsuite/Legacy/ClassesEnterpriseTest.php
+++ dev/tests/static/testsuite/Legacy/ClassesEnterpriseTest.php
@@ -12,7 +12,7 @@
 /**
  * Scans source code for references to classes and see if they indeed exist
  */
-class Legacy_ClassesTest extends PHPUnit_Framework_TestCase
+class Legacy_ClassesEnterpriseTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @param string $file
@@ -21,8 +21,7 @@ class Legacy_ClassesTest extends PHPUnit_Framework_TestCase
     public function testPhpCode($file)
     {
         $classes = self::collectPhpCodeClasses(file_get_contents($file));
-        $this->_assertNonFactoryName($classes);
-        $this->_assertDeprecatedMage($classes);
+        $this->_assertDeprecatedEnterprise($classes);
     }
 
     /**
@@ -92,108 +91,39 @@ class Legacy_ClassesTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @param string $path
-     * @dataProvider configFileDataProvider
-     */
-    public function testConfiguration($path)
-    {
-        $xml = simplexml_load_file($path);
-
-        $classes = Utility_Classes::collectClassesInConfig($xml);
-        $this->_assertNonFactoryName($classes);
-
-        $modules = Utility_Classes::getXmlAttributeValues($xml, '//@module', 'module');
-        $this->_assertNonFactoryName(array_unique($modules));
-    }
-
-    /**
-     * @return array
-     */
-    public function configFileDataProvider()
-    {
-        return Utility_Files::init()->getConfigFiles();
-    }
-
-    /**
-     * @param string $path
-     * @dataProvider layoutFileDataProvider
-     */
-    public function testLayouts($path)
-    {
-        $xml = simplexml_load_file($path);
-        $classes = Utility_Classes::collectLayoutClasses($xml);
-        foreach (Utility_Classes::getXmlAttributeValues($xml, '/layout//@helper', 'helper') as $class) {
-            $classes[] = Utility_Classes::getCallbackClass($class);
-        }
-        $classes = array_merge($classes, Utility_Classes::getXmlAttributeValues($xml, '/layout//@module', 'module'));
-        $this->_assertNonFactoryName(array_unique($classes));
-
-        $tabs = Utility_Classes::getXmlNodeValues($xml, '/layout//action[@method="addTab"]/block');
-        $this->_assertNonFactoryName(array_unique($tabs), true);
-    }
-
-    /**
-     * @return array
-     */
-    public function layoutFileDataProvider()
-    {
-        return Utility_Files::init()->getLayoutFiles();
-    }
-
-    /**
-     * Check whether specified classes or module names correspond to a file according PSR-0 standard
-     *
-     * Suppressing "unused variable" because of the "catch" block
-     *
-     * @param array $names
-     * @param bool $softComparison
-     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
-     */
-    protected function _assertNonFactoryName($names, $softComparison = false)
-    {
-        if (!$names) {
-            return;
-        }
-        $factoryNames = array();
-        foreach ($names as $name) {
-            try {
-                if ($softComparison) {
-                    $this->assertNotRegExp('/\//', $name);
-                } else {
-                    $this->assertFalse(false === strpos($name, '_'));
-                    $this->assertRegExp('/^([A-Z][A-Za-z\d_]+)+$/', $name);
-                }
-            } catch (PHPUnit_Framework_AssertionFailedError $e) {
-                $factoryNames[] = $name;
-            }
-        }
-        if ($factoryNames) {
-            $this->fail('Obsolete factory name(s) detected:' . "\n" . implode("\n", $factoryNames));
-        }
-    }
-
-    /**
-     * Check if the Class contains the string 'Mage_'.  'Mage_' has been refactored to 'Magento_'
+     * Check if the class contains the string 'Enterprise_'.
+     * 'Enterprise_' has been refactored to the the Magento Namespace
      *
      * @param array $names
      */
-    protected function _assertDeprecatedMage($names)
+    protected function _assertDeprecatedEnterprise($names)
     {
         if (!$names) {
             return;
         }
         $obsoleteClasses = array();
+        $exceptions = array('Enterprise_Tag', 'Magento_Enterprise');
         foreach ($names as $name) {
-            try {
-                $this->assertStringStartsNotWith('Mage_',$name);
+            $excludeItem = false;
+            foreach ($exceptions as $exception) {
+                $result = strpos($name, $exception);
+                if ($result !== false) {
+                    $excludeItem = true;
+                    break;
+                }
             }
-            catch (PHPUnit_Framework_AssertionFailedError $e) {
-                $obsoleteClasses[] = $name;
+            if (!$excludeItem) {
+                try {
+                    $this->assertStringStartsNotWith('Enterprise',$name);
+                }
+                catch (PHPUnit_Framework_AssertionFailedError $e) {
+                    $obsoleteClasses[] = $name;
+                }
             }
         }
-
         if ($obsoleteClasses) {
             $this->fail('Obsolete Class name(s) detected:' . "\n" . implode("\n", $obsoleteClasses));
         }
     }
+
 }
