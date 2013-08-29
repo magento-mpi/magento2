16d6de04b7e92a3590a2661bc6eaa637ac63e36d
Olga Kopylova <okopylova@ebay.com>
2013-08-29 12:01:51 -0700

    MAGETWO-12239: Update System Requirements for PHP 5.4 - moved checking of PHP version a first priority task

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,19 +7,40 @@
  * @license    {license_link}
  */
 
-/**#@+
- * Shortcut constants
- */
-define('DS', DIRECTORY_SEPARATOR);
-define('BP', dirname(__DIR__));
-/**#@-*/
-
 /**
  * Environment initialization
  */
 error_reporting(E_ALL);
 #ini_set('display_errors', 1);
 umask(0);
+
+/* PHP version validation */
+if (version_compare(phpversion(), '5.4.0', '<') === true) {
+    if (PHP_SAPI == 'cli') {
+        echo 'Magento supports PHP 5.4.0 or newer. Please read http://www.magento.com/install.';
+    } else {
+        echo <<<HTML
+<div style="font:12px/1.35em arial, helvetica, sans-serif;">
+    <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
+        <h3 style="margin:0;font-size:1.7em;font-weight:normal;text-transform:none;text-align:left;color:#2f2f2f;">
+        Whoops, it looks like you have an invalid PHP version.</h3>
+    </div>
+    <p>Magento supports PHP 5.4.0 or newer.
+    <a href="http://www.magento.com/install" target="">Find out</a>
+    how to install Magento using PHP-CGI as a work-around.
+    </p>
+</div>
+HTML;
+    }
+    exit;
+}
+
+/**#@+
+ * Shortcut constants
+ */
+define('DS', DIRECTORY_SEPARATOR);
+define('BP', dirname(__DIR__));
+/**#@-*/
 
 /**
  * Require necessary files
@@ -41,26 +62,6 @@ if (file_exists($classMapPath)) {
 }
 
 if (!defined('BARE_BOOTSTRAP')) {
-    /* PHP version validation */
-    if (version_compare(phpversion(), '5.4.0', '<') === true) {
-        if (PHP_SAPI == 'cli') {
-            echo 'Magento supports PHP 5.4.0 or newer. Please read http://www.magento.com/install.';
-        } else {
-            echo <<<HTML
-<div style="font:12px/1.35em arial, helvetica, sans-serif;">
-    <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
-        <h3 style="margin:0;font-size:1.7em;font-weight:normal;text-transform:none;text-align:left;color:#2f2f2f;">
-        Whoops, it looks like you have an invalid PHP version.</h3>
-    </div>
-    <p>Magento supports PHP 5.4.0 or newer.
-    <a href="http://www.magento.com/install" target="">Find out</a>
-    how to install Magento using PHP-CGI as a work-around.
-    </p>
-</div>
-HTML;
-        }
-        exit;
-    }
     if (file_exists(BP . '/maintenance.flag')) {
         if (PHP_SAPI == 'cli') {
             echo 'Service temporarily unavailable due to maintenance downtime.';
