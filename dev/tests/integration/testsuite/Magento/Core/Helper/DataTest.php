5a2ec74ba1c952a5534edf1b28fe6d7f45404a32
Eugene Tulika <etulika@ebay.com>
2013-08-29 15:13:51 -0500

    Fixed MAGETWO-12978: Update Mage_Core_Helper_Data - updated Data Helper to return target or null instead of boolean, which enables to-array conversion - updated tests to verify to-array conversion

--- dev/tests/integration/testsuite/Magento/Core/Helper/DataTest.php
+++ dev/tests/integration/testsuite/Magento/Core/Helper/DataTest.php
@@ -164,10 +164,27 @@ class Magento_Core_Helper_DataTest extends PHPUnit_Framework_TestCase
         $expectedTarget = new Magento_Object($data);
         $expectedTarget->setDataChanges(true); // hack for assertion
 
-        $this->assertFalse($this->_helper->copyFieldset($fieldset, $aspect, 'invalid_source', array()));
-        $this->assertFalse($this->_helper->copyFieldset('invalid_fieldset', $aspect, array(), array()));
-        $this->assertTrue($this->_helper->copyFieldset($fieldset, $aspect, $source, $target));
+        $this->assertNull($this->_helper->copyFieldset($fieldset, $aspect, 'invalid_source', array()));
+        $this->assertNull($this->_helper->copyFieldset($fieldset, $aspect, array(), 'invalid_target'));
+        $this->assertEquals($target, $this->_helper->copyFieldset('invalid_fieldset', $aspect, $source, $target));
+        $this->assertSame($target, $this->_helper->copyFieldset($fieldset, $aspect, $source, $target));
         $this->assertEquals($expectedTarget, $target);
+    }
+
+    public function testCopyFieldsetArrayTarget()
+    {
+        $fieldset = 'sales_copy_order';
+        $aspect = 'to_edit';
+        $data = array(
+            'customer_email' => 'admin@example.com',
+            'customer_group_id' => '1',
+        );
+        $source = new Magento_Object($data);
+        $target = array();
+        $expectedTarget = $data;
+
+        $this->assertEquals($target, $this->_helper->copyFieldset('invalid_fieldset', $aspect, $source, $target));
+        $this->assertEquals($expectedTarget, $this->_helper->copyFieldset($fieldset, $aspect, $source, $target));
     }
 
     public function testDecorateArray()
