4999cdc089055594a5fac5a39ca280c508e5287e
Valeriy Nayda <vnayda@ebay.com>
2013-08-30 17:45:00 +0300

    Fixed MAGETWO-11901: Create new translation generation tools - Fixed unit tests

--- dev/tests/unit/testsuite/tools/I18n/Code/Dictionary/FilesCollectorTest.php
+++ dev/tests/unit/testsuite/tools/I18n/Code/Dictionary/FilesCollectorTest.php
@@ -22,8 +22,8 @@ class FilesCollectorTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        // dev/tests/unit/testsuite/tools/I18n/_files
-        $this->_testDir = str_replace('\\', '/', realpath(dirname(__FILE__) . '/../..')) . '/_files';
+        // dev/tests/unit/testsuite/tools/I18n/_files/
+        $this->_testDir = str_replace('\\', '/', realpath(dirname(__FILE__) . '/../..')) . '/_files/';
 
         $objectManagerHelper = new \Magento_Test_Helper_ObjectManager($this);
         $this->_filesCollector = $objectManagerHelper->getObject('Magento\Tools\I18n\Code\Dictionary\FilesCollector');
@@ -32,16 +32,18 @@ class FilesCollectorTest extends \PHPUnit_Framework_TestCase
     public function testGetFilesWithoutMask()
     {
         $expectedResult = array(
-            $this->_testDir . DS . 'file.js',
-            $this->_testDir . DS . 'layout.xml',
+            $this->_testDir . 'file.js',
+            $this->_testDir . 'layout.xml',
         );
-        $this->assertEquals($expectedResult, $this->_filesCollector->getFiles(array($this->_testDir)));
+        $files = $this->_filesCollector->getFiles(array($this->_testDir));
+        sort($files);
+        $this->assertEquals($expectedResult, $files);
     }
 
     public function testGetFilesWithMask()
     {
         $expectedResult = array(
-            $this->_testDir . DS .'file.js',
+            $this->_testDir . 'file.js',
         );
         $this->assertEquals($expectedResult, $this->_filesCollector->getFiles(array($this->_testDir), '/\.js$/'));
     }
