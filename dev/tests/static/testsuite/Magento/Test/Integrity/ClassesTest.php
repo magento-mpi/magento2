d253d69e0387262c450c944a54d744afe84c6771
Chiu,Lawrence(lachiu) <lachiu@ebay.com>
2013-08-30 17:22:00 -0700

    Fixed MAGETWO-12203: Make /dev/ conversion PSR-0 Compliant -Updated test to check for PSR-0 and PSR-1 namespaces.

--- dev/tests/static/testsuite/Magento/Test/Integrity/ClassesTest.php
+++ dev/tests/static/testsuite/Magento/Test/Integrity/ClassesTest.php
@@ -189,11 +189,14 @@ class Magento_Test_Integrity_ClassesTest extends PHPUnit_Framework_TestCase
 
         $classPattern = '/^class\s[A-Z][^\s\/]+/m';
         $namespacePattern = '/(Maged|Magento|Zend)\/[a-zA-Z]+[^\.]+/';
+        $formalNamespacePattern = '/^namespace\s[\\\\a-zA-Z]+/m';
 
         $classNameMatch = array();
         $namespaceMatch = array();
+        $formalNamespaceArray = array();
         $className = null;
         $namespace = null;
+        $namespaceFolderStructure = null;
 
         // exception made because the file is already using formal namespace
         if ($relativePath == "/app/code/Zend/Soap/Wsdl.php") {
@@ -208,10 +211,23 @@ class Magento_Test_Integrity_ClassesTest extends PHPUnit_Framework_TestCase
 
         if (preg_match($namespacePattern, $relativePath, $namespaceMatch) != 0) {
             $namespace = str_replace('/', '_', $namespaceMatch[0]);
+            $namespaceFolderStructure = $namespaceMatch[0];
         }
-        if ($className != null && $namespace != null) {
-            $this->assertEquals($className, $namespace,
-                "Declaration of $file does not match namespace: $namespace\n");
+
+        if (preg_match($formalNamespacePattern, $contents, $formalNamespaceArray) != 0) {
+            $formalNamespace = substr($formalNamespaceArray[0], 10);
+            $formalNamespace = str_replace('\\', '/', $formalNamespace);
+            $formalNamespace .= '/'. $className;
+            if ($namespaceFolderStructure != null && $formalNamespace != null) {
+                $this->assertEquals($formalNamespace, $namespaceFolderStructure,
+                    "Location of $file does not match formal namespace: $formalNamespace\n");
+            }
+        }
+        else{
+            if ($className != null && $namespace != null) {
+                $this->assertEquals($className, $namespace,
+                    "Declaration of $file does not match namespace: $namespace\n");
+            }
         }
     }
 
