e85e7bf613a2e3dd58d5da502e73ba4b2b53f391
Christopher O'Toole <otoolec@x.com>
2013-09-03 16:09:09 -0500

    Implemented MAGETWO-13477: Add negative widget.xsd testing
    Added some tests to verify that the XSD file is validating properly.

--- dev/tests/static/testsuite/Integrity/Magento/Widget/WidgetConfigTest.php
+++ dev/tests/static/testsuite/Integrity/Magento/Widget/WidgetConfigTest.php
@@ -12,9 +12,9 @@ class Integrity_Magento_Widget_WidgetConfigTest extends PHPUnit_Framework_TestCa
     /**
      * @param string $configFile
      *
-     * @dataProvider schemaDataProvider
+     * @dataProvider xmlDataProvider
      */
-    public function testSchema($configFile)
+    public function testXml($configFile)
     {
         $dom = new DOMDocument();
         $dom->loadXML(file_get_contents($configFile));
@@ -26,10 +26,35 @@ class Integrity_Magento_Widget_WidgetConfigTest extends PHPUnit_Framework_TestCa
         }
     }
 
+    public function testSchemaUsingValidXml()
+    {
+        $xmlFile = __DIR__ . '/_files/new_widget.xml';
+        $dom = new DOMDocument();
+        $dom->loadXML(file_get_contents($xmlFile));
+        $schema = Utility_Files::init()->getPathToSource() . '/app/code/Magento/Widget/etc/widget.xsd';
+        $errors = Magento_Config_Dom::validateDomDocument($dom, $schema);
+        if ($errors) {
+            $this->fail('There is a problem with the schema.  A known good XML file failed validation: '
+                . PHP_EOL . implode(PHP_EOL . PHP_EOL, $errors));
+        }
+    }
+
+    public function testSchemaUsingInvalidXml()
+    {
+        $xmlFile = __DIR__ . '/_files/old_widget.xml';
+        $dom = new DOMDocument();
+        $dom->loadXML(file_get_contents($xmlFile));
+        $schema = Utility_Files::init()->getPathToSource() . '/app/code/Magento/Widget/etc/widget.xsd';
+        $errors = Magento_Config_Dom::validateDomDocument($dom, $schema);
+        if (!$errors) {
+            $this->fail('There is a problem with the schema.  A known bad XML file passed validation');
+        }
+    }
+
     /**
      * @return array
      */
-    public function schemaDataProvider()
+    public function xmlDataProvider()
     {
         // Include design directory (last parameter)
         return Utility_Files::init()->getConfigFiles('widget.xml', array(), true, true);
