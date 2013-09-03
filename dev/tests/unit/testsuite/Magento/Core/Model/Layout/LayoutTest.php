a5e5071fd372b24bf352711fba235d563992beb8
Bryant Luk <bluk@ebay.com>
2013-09-03 09:42:31 -0500

    Fixed MAGETWO-13070: Merge remaining changes related to PSR-0 changes to Mainline
    - Update new tests (ValidatorTest and LayoutTest) to use moved
      Magento_TestFramework

--- dev/tests/unit/testsuite/Magento/Core/Model/Layout/LayoutTest.php
+++ dev/tests/unit/testsuite/Magento/Core/Model/Layout/LayoutTest.php
@@ -18,7 +18,7 @@ class Magento_Core_Model_LayoutTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $objectManagerHelper = new Magento_Test_Helper_ObjectManager($this);
+        $objectManagerHelper = new Magento_TestFramework_Helper_ObjectManager($this);
         $this->_layout = $objectManagerHelper->getObject('Magento_Core_Model_Layout');
     }
 
