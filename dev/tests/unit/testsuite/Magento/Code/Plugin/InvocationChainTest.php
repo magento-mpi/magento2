b87b39ad4a13ad37a6a0c56a18528ed7a084f3c0
Susan Wright <swright@ebay.com>
2013-09-03 18:55:18 -0700

    Fixed MAGETWO-12302: Make /dev/tests/ PSR-0 Compliant
    - Fixed pathing.

--- dev/tests/unit/testsuite/Magento/Code/Plugin/InvocationChainTest.php
+++ dev/tests/unit/testsuite/Magento/Code/Plugin/InvocationChainTest.php
@@ -8,10 +8,10 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-require_once __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'SimpleClass.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'SimpleClassPluginA.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'SimpleClassPluginB.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'SimpleObjectManager.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'GeneratorTest' . DIRECTORY_SEPARATOR . 'SimpleClass.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'GeneratorTest' . DIRECTORY_SEPARATOR . 'SimpleClassPluginA.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'GeneratorTest' . DIRECTORY_SEPARATOR . 'SimpleClassPluginB.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'GeneratorTest' . DIRECTORY_SEPARATOR . 'SimpleObjectManager.php';
 
 class Magento_Code_Plugin_InvocationChainTest extends PHPUnit_Framework_TestCase
 {
