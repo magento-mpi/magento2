b4001f849e8e063c4b78f03cb19f8ca2f2f99332
rbates <rbates@x.com>
2013-09-05 15:09:09 -0500

    Fixed MAGETWO-13565: Conformed to coding standards

--- dev/tools/xml/translate.php
+++ dev/tools/xml/translate.php
@@ -14,8 +14,9 @@ $saxon = 'dev/tools/xml/saxon9he.jar';
 
 foreach (preg_split("/((\r?\n)|(\r\n?))/", $files) as $file) {
     if (!empty($file)) {
-        if(!file_exists($saxon)) {
-            system("wget http://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/9.5.1-1/Saxon-HE-9.5.1-1.jar --output-document=$saxon");
+        if (!file_exists($saxon)) {
+            $url = 'http://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/9.5.1-1/Saxon-HE-9.5.1-1.jar';
+            system("wget $url --output-document=$saxon");
         }
         $cmd = "java -jar $saxon -l:on -s:$file -xsl:$xsl -o:$file";
         echo "$cmd \n";
