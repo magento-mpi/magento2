fe16bc770bf6671753a5dcbe7fdc645cd07a94d3
Oleh Kobchenko <okobchenko@ebay.com>
2013-09-06 13:11:31 +0300

    Implemented MAGETWO-13877: Replace "log_log_clean_before" and "log_log_clean_after" events with plugin(s) - first event deleted as no observers listen to it - second event replaced by 2 plugins

---
+++ dev/tests/unit/testsuite/Magento/Catalog/Model/Plugin/LogTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+class Magento_Catalog_Model_Plugin_LogTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Magento_Catalog_Model_Plugin_Log
+     */
+    protected $_model;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_compareItemMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_logResourceMock;
+
+    protected function setUp()
+    {
+        $this->_logResourceMock = $this->getMock('Magento_Log_Model_Resource_Log', array(), array(), '', false);
+        $this->_compareItemMock = $this->getMock(
+            'Magento_Catalog_Model_Product_Compare_Item', array(), array(), '', false
+        );
+        $this->_model = new Magento_Catalog_Model_Plugin_Log($this->_compareItemMock);
+    }
+
+    /**
+     * @covers Magento_Catalog_Model_Plugin_Log::afterClean
+     */
+    public function testAfterClean()
+    {
+        $this->_compareItemMock->expects($this->once())
+            ->method('clean');
+
+        $this->assertEquals($this->_logResourceMock, $this->_model->afterClean($this->_logResourceMock));
+    }
+}
