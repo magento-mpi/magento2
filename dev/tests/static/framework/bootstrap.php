dc862790bbf34bd45be10ef92bcb2b11605e271e
Valeriy Nayda <vnayda@ebay.com>
2013-09-06 17:28:48 +0300

    Fixed MAGETWO-12799: Fixed static tests

--- dev/tests/static/framework/bootstrap.php
+++ dev/tests/static/framework/bootstrap.php
@@ -10,6 +10,8 @@
  */
 
 $baseDir = realpath(__DIR__ . '/../../../../');
+define('DS', DIRECTORY_SEPARATOR);
+define('BP', $baseDir);
 require $baseDir . '/app/autoload.php';
 Magento_Autoload_IncludePath::addIncludePath(array(
     __DIR__,
@@ -18,3 +20,19 @@ Magento_Autoload_IncludePath::addIncludePath(array(
     $baseDir . '/dev/lib',
 ));
 Utility_Files::init(new Utility_Files($baseDir));
+
+function tool_autoloader($className)
+{
+    if (strpos($className, 'Magento\\Tools\\') === false) {
+        return false;
+    }
+    $filePath = str_replace('\\', DS, str_replace('Magento\\Tools\\', '', $className));
+    $filePath = BP . DS . 'dev' . DS . 'tools' . DS . $filePath . '.php';
+
+    if (file_exists($filePath)) {
+        include($filePath);
+    } else {
+        return false;
+    }
+}
+spl_autoload_register('tool_autoloader');
