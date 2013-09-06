67bec021912681ce791a567a91d8123495640381
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2013-09-06 13:12:33 +0300

    Fixed MAGETWO-13367: Fix broken tests after code elimination
    - fixed Magento_Data_Form instantiations

--- app/code/Magento/Adminhtml/Block/Api/User/Edit/Form.php
+++ app/code/Magento/Adminhtml/Block/Api/User/Edit/Form.php
@@ -15,15 +15,16 @@
  * @package    Magento_Adminhtml
  * @author      Magento Core Team <core@magentocommerce.com>
  */
-class Magento_Adminhtml_Block_Api_User_Edit_Form extends Magento_Adminhtml_Block_Widget_Form
+class Magento_Adminhtml_Block_Api_User_Edit_Form extends Magento_Backend_Block_Widget_Form_Generic
 {
-
     protected function _prepareForm()
     {
-        $form = new Magento_Data_Form(array('id' => 'edit_form', 'action' => $this->getData('action'), 'method' => 'post'));
+        /** @var Magento_Data_Form $form */
+        $form = $this->_formFactory->create(
+            array('id' => 'edit_form', 'action' => $this->getData('action'), 'method' => 'post')
+        );
         $form->setUseContainer(true);
         $this->setForm($form);
         return parent::_prepareForm();
     }
-
 }
