8c079f6b55517ea901be6012a675c286718ca258
johe <joan@x.com>
2013-09-06 11:17:22 -0500

    Updated MAGETWO-12955: add space before empty tag and updated transform_logging.php

--- dev/tools/xml/transform_logging.php
+++ dev/tools/xml/transform_logging.php
@@ -6,7 +6,19 @@
  * @license   {license_link}
  */
 
-$source = '../../../app/code/Enterprise/Logging/etc/logging.xml';
-$xsl = 'logging.xslt';
-$out = 'logging.xml';
-echo shell_exec("java -jar saxon9he.jar -s:$source -xsl:$xsl -o:$out");
+$search = $argv[1];
+$files = shell_exec("find . -name $search");
+$xsl = 'dev/tools/xml/logging.xslt';
+$saxon = 'dev/tools/xml/saxon9he.jar';
+
+foreach (preg_split("/((\r?\n)|(\r\n?))/", $files) as $file) {
+    if (!empty($file)) {
+        if (!file_exists($saxon)) {
+            $url = 'http://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/9.5.1-1/Saxon-HE-9.5.1-1.jar';
+            system("wget $url --output-document=$saxon");
+        }
+        $cmd = "java -jar $saxon -l:on -s:$file -xsl:$xsl -o:$file";
+        echo "$cmd \n";
+        system($cmd);
+    }
+}
