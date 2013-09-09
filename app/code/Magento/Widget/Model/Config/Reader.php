dc6c1cd2b13068fe8e88ababff830ad382c7fdbe
Christopher O'Toole <otoolec@x.com>
2013-09-09 10:38:27 -0500

    Implemented MAGETWO-13579: Fix Widget Config_Reader
    Changes needed to be made to use SchemaLocator instead of strings for
    identifying the locatoin of widget.xsd and widget_file.xsd to the parent
    class.

--- app/code/Magento/Widget/Model/Config/Reader.php
+++ app/code/Magento/Widget/Model/Config/Reader.php
@@ -21,32 +21,31 @@ class Magento_Widget_Model_Config_Reader extends Magento_Config_Reader_Filesyste
     );
 
     /**
-     * @param Magento_Core_Model_Config_Modules_Reader $moduleReader
      * @param Magento_Config_FileResolverInterface $fileResolver
      * @param Magento_Widget_Model_Config_Converter $converter
+     * @param Magento_Config_SchemaLocatorInterface $schemaLocator
      * @param Magento_Config_ValidationStateInterface $validationState
      * @param string $fileName
      * @param array $idAttributes
-     * @param null $schema
-     * @param null $perFileSchema
      * @param string $domDocumentClass
      */
     public function __construct(
-        Magento_Core_Model_Config_Modules_Reader $moduleReader,
         Magento_Config_FileResolverInterface $fileResolver,
         Magento_Widget_Model_Config_Converter $converter,
+        Magento_Config_SchemaLocatorInterface $schemaLocator,
         Magento_Config_ValidationStateInterface $validationState,
         $fileName = 'widget.xml',
         $idAttributes = array(),
-        $schema = null,
-        $perFileSchema = null,
         $domDocumentClass = 'Magento_Config_Dom'
     ) {
-        $schema = $schema ?: $moduleReader->getModuleDir('etc', 'Magento_Widget') . '/widget.xsd';
-        $perFileSchema = $perFileSchema ?: $moduleReader->getModuleDir('etc', 'Magento_Widget') . '/widget_file.xsd';
         parent::__construct(
-            $fileResolver, $converter, $fileName, $idAttributes, $schema,
-            $perFileSchema, $validationState->isValidated(), $domDocumentClass
+            $fileResolver,
+            $converter,
+            $schemaLocator,
+            $validationState,
+            $fileName,
+            $idAttributes,
+            $domDocumentClass
         );
     }
 
