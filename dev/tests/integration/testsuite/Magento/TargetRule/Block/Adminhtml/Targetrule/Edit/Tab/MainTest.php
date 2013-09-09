af3b7375db45c9830882ed7ed17d6f72426153a5
Yurii Torbyk <itorbyk@ebay.com>
2013-09-09 16:28:03 +0300

    Fixed  MAGETWO-11971: Elimination of "Mage::registry" from tests - fix merge

--- dev/tests/integration/testsuite/Magento/TargetRule/Block/Adminhtml/Targetrule/Edit/Tab/MainTest.php
+++ dev/tests/integration/testsuite/Magento/TargetRule/Block/Adminhtml/Targetrule/Edit/Tab/MainTest.php
@@ -21,9 +21,11 @@ class Magento_TargetRule_Block_Adminhtml_Targetrule_Edit_Tab_MainTest extends PH
      */
     public function testPrepareForm()
     {
-        Mage::getDesign()->setArea(Magento_Core_Model_App_Area::AREA_ADMINHTML)->setDefaultDesignTheme();
         /** @var $objectManager Magento_Test_ObjectManager */
         $objectManager = Magento_Test_Helper_Bootstrap::getObjectManager();
+        $objectManager->get('Magento_Core_Model_View_DesignInterface')
+            ->setArea(Magento_Core_Model_App_Area::AREA_ADMINHTML)
+            ->setDefaultDesignTheme();
         $objectManager->get('Magento_Core_Model_Registry')
             ->register('current_target_rule', Mage::getModel('Magento_TargetRule_Model_Rule'));
 
