3a26505d6f8c745550c2d35b3d7a0c898a27091b
Michael Chiocca <mchiocca@ebay.com>
2013-09-10 15:10:24 -0700

    Fixed MAGETWO-11717: Verify all Bamboo Build Plans are Green - Move WSDL Cache setup into the Soap Server constructor. - Fix some of the mocking in the Soap Server integration test.

--- dev/tests/integration/testsuite/Magento/Webapi/Model/Soap/ServerTest.php
+++ dev/tests/integration/testsuite/Magento/Webapi/Model/Soap/ServerTest.php
@@ -27,19 +27,14 @@ class Magento_Webapi_Model_Soap_ServerTest extends PHPUnit_Framework_TestCase
     protected function setUp()
     {
         /** Init all dependencies for SUT. */
-        $this->_storeMock = $this->getMockBuilder('Magento_Core_Model_Store')->disableOriginalConstructor()->getMock();
+        $this->_storeMock = $this->getMockBuilder('Magento_Core_Model_Store')
+            ->disableOriginalConstructor()->getMock();
         $this->_applicationMock = $this->getMockBuilder('Magento_Core_Model_App')
             ->disableOriginalConstructor()->getMock();
-        $this->_applicationMock->expects($this->any())->method('getStore')->will($this->returnValue($this->_storeMock));
-        $this->_requestMock = $this->getMockBuilder('Magento_Webapi_Controller_Soap_Request')
-            ->disableOriginalConstructor()
-            ->getMock();
         $this->_applicationMock->expects($this->any())
-            ->method('getStore')
-            ->will($this->returnValue($this->_storeMock));
-        $this->_requestMock = $this->getMockBuilder('Magento_Webapi_Controller_Request_Soap')
-            ->disableOriginalConstructor()
-            ->getMock();
+            ->method('getStore')->will($this->returnValue($this->_storeMock));
+        $this->_requestMock = $this->getMockBuilder('Magento_Webapi_Controller_Soap_Request')
+            ->disableOriginalConstructor()->getMock();
         $this->_domDocumentFactory = $this->getMockBuilder('Magento_DomDocument_Factory')
             ->disableOriginalConstructor()->getMock();
         $this->_soapHandler = $this->getMockBuilder('Magento_Webapi_Controller_Soap_Handler')
@@ -55,7 +50,7 @@ class Magento_Webapi_Model_Soap_ServerTest extends PHPUnit_Framework_TestCase
     public function testConstructEnableWsdlCache()
     {
         /** Mock getConfig method to return true. */
-        $this->_storeMock->expects($this->any())->method('getConfig')->will($this->returnValue(true));
+        $this->_storeMock->expects($this->once())->method('getConfig')->will($this->returnValue(true));
         /** Create SOAP server object. */
         $server = new Magento_Webapi_Model_Soap_Server(
             $this->_applicationMock,
@@ -74,7 +69,7 @@ class Magento_Webapi_Model_Soap_ServerTest extends PHPUnit_Framework_TestCase
     public function testConstructDisableWsdlCache()
     {
         /** Mock getConfig method to return false. */
-        $this->_storeMock->expects($this->any())->method('getConfig')->will($this->returnValue(false));
+        $this->_storeMock->expects($this->once())->method('getConfig')->will($this->returnValue(false));
         /** Create SOAP server object. */
         $server = new Magento_Webapi_Model_Soap_Server(
             $this->_applicationMock,
