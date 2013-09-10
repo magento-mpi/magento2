409828f151685e4f8baf040756810de5e81478af
Bibu Mathew <bimathew@paypal.com>
2013-09-09 20:30:17 -0500

    MAGETWO-13687:Manually update 15 occurrences of PSR-1 violations related to SideEffects & UpperCaseConstantName. Fix: PSR1 violations related to UpperCaseConstantName.

--- app/code/Magento/Paypal/Model/Config.php
+++ app/code/Magento/Paypal/Model/Config.php
@@ -65,10 +65,10 @@ class Magento_Paypal_Model_Config
     const EC_FLAVOR_STATIC  = 'static';
     const EC_BUTTON_TYPE_SHORTCUT = 'ecshortcut';
     const EC_BUTTON_TYPE_MARK     = 'ecmark';
-    const PAYMENT_MARK_37x23   = '37x23';
-    const PAYMENT_MARK_50x34   = '50x34';
-    const PAYMENT_MARK_60x38   = '60x38';
-    const PAYMENT_MARK_180x113 = '180x113';
+    const PAYMENT_MARK_37X23   = '37x23';
+    const PAYMENT_MARK_50X34   = '50x34';
+    const PAYMENT_MARK_60X38   = '60x38';
+    const PAYMENT_MARK_180X113 = '180x113';
 
     const DEFAULT_LOGO_TYPE = 'wePrefer_150x60';
 
@@ -675,13 +675,13 @@ class Magento_Paypal_Model_Config
             $staticSize = $this->paymentMarkSize;
         }
         switch ($staticSize) {
-            case self::PAYMENT_MARK_37x23:
-            case self::PAYMENT_MARK_50x34:
-            case self::PAYMENT_MARK_60x38:
-            case self::PAYMENT_MARK_180x113:
+            case self::PAYMENT_MARK_37X23:
+            case self::PAYMENT_MARK_50X34:
+            case self::PAYMENT_MARK_60X38:
+            case self::PAYMENT_MARK_180X113:
                 break;
             default:
-                $staticSize = self::PAYMENT_MARK_37x23;
+                $staticSize = self::PAYMENT_MARK_37X23;
         }
         return sprintf('https://www.paypal.com/%s/i/logo/PayPal_mark_%s.gif',
             $this->_getSupportedLocaleCode($localeCode), $staticSize);
