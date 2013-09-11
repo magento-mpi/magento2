2aca66f9197310182d4d6262639705b709c17852
johe <joan@x.com>
2013-09-11 11:21:51 -0500

    Fixed MAGETWO-13629: fixed testXml for partial xml files

--- dev/tests/static/framework/Integrity/ConfigAbstract.php
+++ dev/tests/static/framework/Integrity/ConfigAbstract.php
@@ -12,20 +12,22 @@ abstract class Integrity_ConfigAbstract extends PHPUnit_Framework_TestCase
     /**
      * @param string $configFile
      *
-     * @dataProvider schemaDataProvider
+     * @dataProvider configFilesDataProvider
      */
     public function testXml($configFile)
     {
         $schema = Magento_TestFramework_Utility_Files::init()->getPathToSource() . $this->_getXsd();
-        $this->_validateFileExpectSuccess($configFile, $schema);
+        $fileSchema = Magento_TestFramework_Utility_Files::init()->getPathToSource() . $this->_getFileXsd();
+        $this->_validateFileExpectSuccess($configFile, $schema, $fileSchema);
     }
 
     /**
-     * Returns the name of the XSD file to be used to validate the XML
-     *
-     * @return string
+     * @return array
      */
-    abstract protected function _getXsd();
+    public function configFilesDataProvider()
+    {
+        return Magento_TestFramework_Utility_Files::init()->getConfigFiles($this->_getXmlName());
+    }
 
     public function testSchemaUsingValidXml()
     {
@@ -34,26 +36,12 @@ abstract class Integrity_ConfigAbstract extends PHPUnit_Framework_TestCase
         $this->_validateFileExpectSuccess($xmlFile, $schema);
     }
 
-    /**
-     * The location of a single valid complete xml file
-     *
-     * @return string
-     */
-    abstract protected function _getKnownValidXml();
-
     public function testSchemaUsingInvalidXml()
     {
         $xmlFile = $this->_getKnownInvalidXml();
         $schema = Magento_TestFramework_Utility_Files::init()->getPathToSource() . $this->_getXsd();
         $this->_validateFileExpectFailure($xmlFile, $schema);
     }
-
-    /**
-     * The location of a single known invalid complete xml file
-     *
-     * @return string
-     */
-    abstract protected function _getKnownInvalidXml();
 
     public function testFileSchemaUsingPartialXml()
     {
@@ -62,34 +50,12 @@ abstract class Integrity_ConfigAbstract extends PHPUnit_Framework_TestCase
         $this->_validateFileExpectSuccess($xmlFile, $schema);
     }
 
-    /**
-     * The location of a single known valid partial xml file
-     *
-     * @return string
-     */
-    abstract protected function _getKnownValidPartialXml();
-
-    /**
-     * Returns the name of the XSD file to be used to validate partial XML
-     *
-     * @return string
-     */
-    abstract protected function _getFileXsd();
-
     public function testFileSchemaUsingInvalidXml()
     {
         $xmlFile = $this->_getKnownInvalidPartialXml();
         $schema = Magento_TestFramework_Utility_Files::init()->getPathToSource() . $this->_getFileXsd();
         $this->_validateFileExpectFailure($xmlFile, $schema);
     }
-
-    /**
-     * The location of a single known invalid partial xml file
-     *
-     * @return string
-     */
-    abstract protected function _getKnownInvalidPartialXml();
-
 
     public function testSchemaUsingPartialXml()
     {
@@ -99,34 +65,28 @@ abstract class Integrity_ConfigAbstract extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @return array
-     */
-    public function schemaDataProvider()
-    {
-        return Magento_TestFramework_Utility_Files::init()->getConfigFiles($this->_getXmlName());
-    }
-
-    /**
-     * Returns the name of the xml files to validate
-     *
-     * @return string
-     */
-    abstract protected function _getXmlName();
-
-    /**
      * Run schema validation against a known bad xml file with a provided schema.
      *
      * This helper expects the validation to fail and will fail a test if no errors are found.
      *
      * @param $xmlFile string a known bad xml file.
      * @param $schemaFile string schema that should find errors in the known bad xml file.
+     * @param $fileSchemaFile string schema that should find errors in the known bad xml file
      */
-    protected function _validateFileExpectSuccess($xmlFile, $schemaFile)
+    protected function _validateFileExpectSuccess($xmlFile, $schemaFile, $fileSchemaFile=null)
     {
         $dom = new DOMDocument();
         $dom->loadXML(file_get_contents($xmlFile));
         $errors = Magento_Config_Dom::validateDomDocument($dom, $schemaFile);
         if ($errors) {
+            if (!is_null($fileSchemaFile)) {
+                $moreErrors = Magento_Config_Dom::validateDomDocument($dom, $fileSchemaFile);
+                if (empty($moreErrors)) {
+                    return;
+                } else {
+                    $errors = array_merge($errors, $moreErrors);
+                }
+            }
             $this->fail('There is a problem with the schema.  A known good XML file failed validation: '
                         . PHP_EOL . implode(PHP_EOL . PHP_EOL, $errors));
         }
@@ -149,4 +109,53 @@ abstract class Integrity_ConfigAbstract extends PHPUnit_Framework_TestCase
             $this->fail('There is a problem with the schema.  A known bad XML file passed validation');
         }
     }
+
+    /**
+     * Returns the name of the XSD file to be used to validate the XML
+     *
+     * @return string
+     */
+    abstract protected function _getXsd();
+
+    /**
+     * The location of a single valid complete xml file
+     *
+     * @return string
+     */
+    abstract protected function _getKnownValidXml();
+
+    /**
+     * The location of a single known invalid complete xml file
+     *
+     * @return string
+     */
+    abstract protected function _getKnownInvalidXml();
+
+    /**
+     * The location of a single known valid partial xml file
+     *
+     * @return string
+     */
+    abstract protected function _getKnownValidPartialXml();
+
+    /**
+     * Returns the name of the XSD file to be used to validate partial XML
+     *
+     * @return string
+     */
+    abstract protected function _getFileXsd();
+
+    /**
+     * The location of a single known invalid partial xml file
+     *
+     * @return string
+     */
+    abstract protected function _getKnownInvalidPartialXml();
+
+    /**
+     * Returns the name of the xml files to validate
+     *
+     * @return string
+     */
+    abstract protected function _getXmlName();
 }
