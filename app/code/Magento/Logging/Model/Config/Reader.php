0b81dcf89cb58628b532c12a3dddb2b67a118b76
Eugene Tulika <etulika@ebay.com>
2013-09-11 13:32:32 -0500

    Fixed MAGETWO-13988: Need grouping nodes for array elements - fixed docblock for constructor

--- app/code/Magento/Logging/Model/Config/Reader.php
+++ app/code/Magento/Logging/Model/Config/Reader.php
@@ -28,14 +28,12 @@ class Magento_Logging_Model_Config_Reader extends Magento_Config_Reader_Filesyst
     );
 
     /**
-     * @param Magento_Core_Model_Config_Modules_Reader $moduleReader
      * @param Magento_Config_FileResolverInterface $fileResolver
      * @param Magento_Logging_Model_Config_Converter $converter
+     * @param Magento_Logging_Model_Config_SchemaLocator $schemaLocator
      * @param Magento_Config_ValidationStateInterface $validationState
      * @param string $fileName
      * @param array $idAttributes
-     * @param null $schema
-     * @param null $perFileSchema
      * @param string $domDocumentClass
      */
     public function __construct(
