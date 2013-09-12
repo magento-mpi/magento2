933267bfab0806af6b2ef004bb0430f3be6c2195
Vadym Popov <vapopov@ebay.com>
2013-09-12 14:59:25 +0300

    Fixed MAGETWO-13390: Merge Story: Elimination of "Mage::(config)" from tests - Fix tests after merge

--- dev/tests/integration/testsuite/Magento/Catalog/Model/Category/CategoryImageTest/StubZendLogWriterStreamTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/Category/CategoryImageTest/StubZendLogWriterStreamTest.php
@@ -11,8 +11,7 @@
 
 Mage::app()->getStore()->setConfig('dev/log/active', 1);
 Mage::app()->getStore()->setConfig('dev/log/exception_file', 'save_category_without_image.log');
-$configModel = Magento_TestFramework_Helper_Bootstrap::getObjectManager()->get('Magento_Core_Model_Config');
-$configModel->setNode(
+Magento_TestFramework_Helper_Bootstrap::getObjectManager()->get('Magento_Core_Model_Config')->setNode(
     'global/log/core/writer_model',
     'Magento_Catalog_Model_Category_CategoryImageTest_StubZendLogWriterStreamTest'
 );
