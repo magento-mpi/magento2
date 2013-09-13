18995fa9217f276bdcf402c43dde4d2a1ebd707b
Yevgen Kubichka <ykubichka@ebay.com>
2013-09-13 15:53:34 -0700

    CR Changes MAGETWO-13998: Implement XSD Validation - removed unnecessary PHPDoc comments in file header

--- dev/tests/integration/testsuite/Magento/Test/Integrity/Modular/Magento/Customer/AddressFormatsFilesTest.php
+++ dev/tests/integration/testsuite/Magento/Test/Integrity/Modular/Magento/Customer/AddressFormatsFilesTest.php
@@ -2,9 +2,6 @@
 /**
  * {license_notice}
  *
- * @category    Magento
- * @package     Magento_Core
- * @subpackage  integration_tests
  * @copyright   {copyright}
  * @license     {license_link}
  */
