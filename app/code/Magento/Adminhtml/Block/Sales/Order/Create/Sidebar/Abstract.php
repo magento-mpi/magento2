1318a3e2096557df32a3163e9c5a1f732f6368bf
Yurii Torbyk <itorbyk@ebay.com>
2013-09-16 16:13:59 +0300

    Fixed MAGETWO-13387: Merge Story: Elimination of "Mage::(config)" from modules

--- app/code/Magento/Adminhtml/Block/Sales/Order/Create/Sidebar/Abstract.php
+++ app/code/Magento/Adminhtml/Block/Sales/Order/Create/Sidebar/Abstract.php
@@ -32,19 +32,18 @@ class Magento_Adminhtml_Block_Sales_Order_Create_Sidebar_Abstract extends Magent
     /**
      * Constructor
      *
+     * @param Magento_Core_Helper_Data $coreData
      * @param Magento_Backend_Block_Template_Context $context
      * @param Magento_Core_Model_Config $coreConfig
      * @param array $data
      */
     public function __construct(
+        Magento_Core_Helper_Data $coreData,
         Magento_Backend_Block_Template_Context $context,
         Magento_Core_Model_Config $coreConfig,
         array $data = array()
     ) {
-        parent::__construct(
-            $context,
-            $data
-        );
+        parent::__construct($coreData, $context, $data);
         $this->_coreConfig = $coreConfig;
     }
 
