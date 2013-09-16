021416c0216948473ab889e17f133e499845c8b1
Olga Kopylova <okopylova@ebay.com>
2013-09-16 11:58:21 -0700

    MAGETWO-14083: Contribute Changes to Mainline - fixed unit tests

--- dev/tests/unit/testsuite/Magento/Directory/Helper/DataTest.php
+++ dev/tests/unit/testsuite/Magento/Directory/Helper/DataTest.php
@@ -37,7 +37,8 @@ class Magento_Directory_Helper_DataTest extends PHPUnit_Framework_TestCase
     {
         $translator = $this->getMock('Magento_Core_Model_Translate', array(), array(), '', false);
         $moduleManager = $this->getMock('Magento_Core_Model_ModuleManager', array(), array(), '', false);
-        $context = new Magento_Core_Helper_Context($translator, $moduleManager);
+        $eventManager = $this->getMock('Magento_Core_Model_Event_Manager', array(), array(), '', false);
+        $context = new Magento_Core_Helper_Context($translator, $moduleManager, $eventManager);
 
         $configCacheType = $this->getMock('Magento_Core_Model_Cache_Type_Config', array(), array(), '', false);
 
