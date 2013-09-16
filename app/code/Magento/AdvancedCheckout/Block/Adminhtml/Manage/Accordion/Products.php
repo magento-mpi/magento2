9392c656371efa1e5a7c7c3c0ec08e0dca1ba061
Sergey Kovalev <skovalev@ebay.com>
2013-09-16 18:08:20 +0300

    Fixed MAGETWO-11957: Elimination of "Mage::(config)" from modules - Integration test fixes

--- app/code/Magento/AdvancedCheckout/Block/Adminhtml/Manage/Accordion/Products.php
+++ app/code/Magento/AdvancedCheckout/Block/Adminhtml/Manage/Accordion/Products.php
@@ -26,6 +26,7 @@ class Magento_AdvancedCheckout_Block_Adminhtml_Manage_Accordion_Products
     /**
      * Constructor
      *
+     * @param Magento_Core_Helper_Data $coreData
      * @param Magento_Backend_Block_Template_Context $context
      * @param Magento_Core_Model_StoreManagerInterface $storeManager
      * @param Magento_Core_Model_Url $urlModel
@@ -34,6 +35,7 @@ class Magento_AdvancedCheckout_Block_Adminhtml_Manage_Accordion_Products
      * @param array $data
      */
     public function __construct(
+        Magento_Core_Helper_Data $coreData,
         Magento_Backend_Block_Template_Context $context,
         Magento_Core_Model_StoreManagerInterface $storeManager,
         Magento_Core_Model_Url $urlModel,
@@ -42,6 +44,7 @@ class Magento_AdvancedCheckout_Block_Adminhtml_Manage_Accordion_Products
         array $data = array()
     ) {
         parent::__construct(
+            $coreData,
             $context,
             $storeManager,
             $urlModel,
