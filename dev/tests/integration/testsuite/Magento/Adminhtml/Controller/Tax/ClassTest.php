4adc6a838cceca29dbf7a81ce0ab2a43d46adfe2
Chiu,Lawrence(lachiu) <lachiu@ebay.com>
2013-09-17 16:51:56 -0700

    Fixed MAGETWO-11929: Convert all Classes in Magento2 Namespace to Use Formal Namespace Syntax -Fixed integration Tax Class test.

--- dev/tests/integration/testsuite/Magento/Adminhtml/Controller/Tax/ClassTest.php
+++ dev/tests/integration/testsuite/Magento/Adminhtml/Controller/Tax/ClassTest.php
@@ -25,7 +25,7 @@ class Magento_Adminhtml_Controller_Tax_ClassTest extends Magento_Backend_Utility
     {
         $this->getRequest()->setPost($postData);
 
-        $this->dispatch('backend/admin/tax_class/ajaxSave');
+        $this->dispatch('backend/admin/tax_classtax/ajaxSave');
 
         $jsonBody = $this->getResponse()->getBody();
         $result = Magento_TestFramework_Helper_Bootstrap::getObjectManager()->get('Magento\Core\Helper\Data')
