39c2f3df4396ffe3149719b96e9d4b77d386b839
Paul Lewis <plewis@ebay.com>
2013-09-17 16:13:33 -0500

    Implemented MAGETWO-12967: update Js.php
     - Removed reading of jstranslator.xml files, per new story design

--- app/code/Magento/Core/Helper/Js.php
+++ app/code/Magento/Core/Helper/Js.php
@@ -1,5 +1,9 @@
 <?php
 /**
+ * JavaScript helper
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ *
  * {license_notice}
  *
  * @category    Magento
@@ -7,49 +11,14 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-
-/**
- * JavaScript helper
- *
- * @author      Magento Core Team <core@magentocommerce.com>
- */
 class Magento_Core_Helper_Js extends Magento_Core_Helper_Abstract
 {
-    /**
-     * Key for cache
-     */
-    const JAVASCRIPT_TRANSLATE_CONFIG_KEY = 'javascript_translate_config';
-
-    /**
-     * Translate file name
-     */
-    const JAVASCRIPT_TRANSLATE_CONFIG_FILENAME = 'jstranslator.xml';
-
     /**
      * Array of senteces of JS translations
      *
      * @var array
      */
     protected $_translateData = null;
-
-    /**
-     * Translate config
-     *
-     * @var Magento_Simplexml_Config
-     */
-    protected $_config = null;
-
-    /**
-     * Modules configuration reader
-     *
-     * @var Magento_Core_Model_Config_Modules_Reader
-     */
-    protected $_configReader;
-
-    /**
-     * @var Magento_Core_Model_Cache_Type_Config
-     */
-    protected $_configCacheType;
 
     /**
      * @var Magento_Core_Model_View_Url
@@ -66,21 +35,15 @@ class Magento_Core_Helper_Js extends Magento_Core_Helper_Abstract
     /**
      * @param Magento_Core_Helper_Data $coreData
      * @param Magento_Core_Helper_Context $context
-     * @param Magento_Core_Model_Config_Modules_Reader $configReader
-     * @param Magento_Core_Model_Cache_Type_Config $configCacheType
      * @param Magento_Core_Model_View_Url $viewUrl
      */
     public function __construct(
         Magento_Core_Helper_Data $coreData,
         Magento_Core_Helper_Context $context,
-        Magento_Core_Model_Config_Modules_Reader $configReader,
-        Magento_Core_Model_Cache_Type_Config $configCacheType,
         Magento_Core_Model_View_Url $viewUrl
     ) {
         $this->_coreData = $coreData;
         parent::__construct($context);
-        $this->_configReader = $configReader;
-        $this->_configCacheType = $configCacheType;
         $this->_viewUrl = $viewUrl;
     }
 
@@ -127,51 +90,4 @@ class Magento_Core_Helper_Js extends Magento_Core_Helper_Abstract
         return '<script type="text/javascript" src="' . $this->_viewUrl->getViewFileUrl($file) . '"></script>' . "\n";
     }
 
-    /**
-     * Retrieve JS translation array
-     *
-     * @return array
-     */
-    protected function _getTranslateData()
-    {
-        if ($this->_translateData === null) {
-            $this->_translateData = array();
-            $messages = $this->_getXmlConfig()->getXpath('*/message');
-            if (!empty($messages)) {
-                foreach ($messages as $message) {
-                    $messageText = (string)$message;
-                    $this->_translateData[$messageText] = __($messageText);
-                }
-            }
-
-            foreach ($this->_translateData as $key => $value) {
-                if ($key == $value) {
-                    unset($this->_translateData[$key]);
-                }
-            }
-        }
-        return $this->_translateData;
-    }
-
-    /**
-     * Load config from files and try to cache it
-     *
-     * @return Magento_Simplexml_Config
-     */
-    protected function _getXmlConfig()
-    {
-        if (is_null($this->_config)) {
-            $cachedXml = $this->_configCacheType->load(self::JAVASCRIPT_TRANSLATE_CONFIG_KEY);
-            if ($cachedXml) {
-                $xmlConfig = new Magento_Simplexml_Config($cachedXml);
-            } else {
-                $xmlConfig = new Magento_Simplexml_Config();
-                $xmlConfig->loadString('<?xml version="1.0"?><jstranslator></jstranslator>');
-                $this->_configReader->loadModulesConfiguration(self::JAVASCRIPT_TRANSLATE_CONFIG_FILENAME, $xmlConfig);
-                $this->_configCacheType->save($xmlConfig->getXmlString(), self::JAVASCRIPT_TRANSLATE_CONFIG_KEY);
-            }
-            $this->_config = $xmlConfig;
-        }
-        return $this->_config;
-    }
 }
