6c16d9c400cf257c5abd94a2a38dc4ca4f5530f2
oivashchenko <oivashchenko@ebay.com>
2013-09-17 11:18:24 +0300

    Implemented MAGETWO-14435:  Elimination of "Mage::getSingleton" usages from modules CurrencySymbol, CustomAttribute and DesignEditor
     - getSingleton was eliminated from Magento_CurrencySymbol

--- app/code/Magento/CurrencySymbol/Controller/Adminhtml/System/Currencysymbol.php
+++ app/code/Magento/CurrencySymbol/Controller/Adminhtml/System/Currencysymbol.php
@@ -52,11 +52,11 @@ class Magento_CurrencySymbol_Controller_Adminhtml_System_Currencysymbol extends 
 
         try {
             Mage::getModel('Magento_CurrencySymbol_Model_System_Currencysymbol')->setCurrencySymbolsData($symbolsDataArray);
-            Mage::getSingleton('Magento_Connect_Model_Session')->addSuccess(
+            $this->_objectManager->get('Magento_Connect_Model_Session')->addSuccess(
                 __('The custom currency symbols were applied.')
             );
         } catch (Exception $e) {
-            Mage::getSingleton('Magento_Adminhtml_Model_Session')->addError($e->getMessage());
+            $this->_objectManager->get('Magento_Backend_Model_Session')->addError($e->getMessage());
         }
 
         $this->_redirectReferer();
