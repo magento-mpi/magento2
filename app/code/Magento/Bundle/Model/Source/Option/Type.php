25fb6b85238609dcd69077ce3f418477c5eb322d
Olexii Korshenko <okorshenko@ebay.com>
2013-09-17 21:05:44 +0300

    Implemented MAGETWO-14251: Product options configuration removal from config.xml - moved bundle product options to DI config

--- app/code/Magento/Bundle/Model/Source/Option/Type.php
+++ app/code/Magento/Bundle/Model/Source/Option/Type.php
@@ -1,36 +1,41 @@
 <?php
 /**
+ * Bundle Option Type Source Model
+ *
  * {license_notice}
  *
- * @category    Magento
- * @package     Magento_Bundle
  * @copyright   {copyright}
  * @license     {license_link}
  */
-
-/**
- * Bundle Option Type Source Model
- *
- * @category   Magento
- * @package    Magento_Bundle
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Magento_Bundle_Model_Source_Option_Type
+class Magento_Bundle_Model_Source_Option_Type implements Magento_Core_Model_Option_ArrayInterface
 {
-    const BUNDLE_OPTIONS_TYPES_PATH = 'global/catalog/product/options/bundle/types';
+    /**
+     * @var array
+     */
+    protected $_options = array();
 
+    /**
+     * @param array $options
+     */
+    public function __construct(array $options)
+    {
+        $this->_options = $options;
+    }
+
+    /**
+     * Get Bundle Option Type
+     *
+     * @return array
+     */
     public function toOptionArray()
     {
         $types = array();
-
-        foreach (Mage::getConfig()->getNode(self::BUNDLE_OPTIONS_TYPES_PATH)->children() as $type) {
-            $labelPath = self::BUNDLE_OPTIONS_TYPES_PATH . '/' . $type->getName() . '/label';
+        foreach ($this->_options as $value => $label) {
             $types[] = array(
-                'label' => (string) Mage::getConfig()->getNode($labelPath),
-                'value' => $type->getName()
+                'label' => $label,
+                'value' => $value
             );
         }
-
         return $types;
     }
 }
