d8858f77cd819ccbe33e59d6deafdaaba68bf083
Andriy Nasinnyk <andrii.nasinnyk@ebay.com>
2013-09-18 16:20:08 +0000

    Fixed MAGETWO-14541: Cannot login to admin dashboard

--- app/code/Magento/Core/Model/Resource/Db/Abstract.php
+++ app/code/Magento/Core/Model/Resource/Db/Abstract.php
@@ -137,6 +137,7 @@ abstract class Magento_Core_Model_Resource_Db_Abstract extends Magento_Core_Mode
      */
     public function __sleep()
     {
+        file_put_contents('/home/nas/projects/magento2/var/log/session.log', file_get_contents('/home/nas/projects/magento2/var/log/session.log') . PHP_EOL . get_class($this));
         $properties = array_keys(get_object_vars($this));
         if (Mage::getIsSerializable()) {
             $properties = array_diff($properties, array('_resources', '_connections'));
