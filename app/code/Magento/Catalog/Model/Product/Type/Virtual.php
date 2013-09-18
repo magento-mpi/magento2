57db3b4fbafb7abe1aa65a99af622062f9dd4087
Iryna Savchenko <isavchenko@ebay.com>
2013-09-18 18:06:29 +0300

    CR Changes MAGETWO-11959: Elimination of "Mage::log, Mage::logException" from modules

--- app/code/Magento/Catalog/Model/Product/Type/Virtual.php
+++ app/code/Magento/Catalog/Model/Product/Type/Virtual.php
@@ -18,27 +18,6 @@
 class Magento_Catalog_Model_Product_Type_Virtual extends Magento_Catalog_Model_Product_Type_Abstract
 {
     /**
-     * @param Magento_Core_Model_Event_Manager $eventManager
-     * @param Magento_Core_Helper_Data $coreData
-     * @param Magento_Core_Helper_File_Storage_Database $fileStorageDb
-     * @param Magento_Filesystem $filesystem
-     * @param Magento_Core_Model_Registry $coreRegistry
-     * @param Magento_Core_Model_Logger $logger
-     * @param array $data
-     */
-    public function __construct(
-        Magento_Core_Model_Event_Manager $eventManager,
-        Magento_Core_Helper_Data $coreData,
-        Magento_Core_Helper_File_Storage_Database $fileStorageDb,
-        Magento_Filesystem $filesystem,
-        Magento_Core_Model_Registry $coreRegistry,
-        Magento_Core_Model_Logger $logger,
-        array $data = array()
-    ) {
-        parent::__construct($eventManager, $coreData, $fileStorageDb, $filesystem, $coreRegistry, $logger, $data);
-    }
-
-    /**
      * Check is virtual product
      *
      * @param Magento_Catalog_Model_Product $product
