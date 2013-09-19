7c86efcad3492e1d2b52bfc3847a9b7182b16ee9
Kevin Fries <kfries@ebay.com>
2013-09-19 12:10:10 -0700

    Fixed MAGETWO-11929: Convert all Classes in Magento2 Namespace to Use Formal Namespace Syntax - refactoring a class for eventual PSR-2 compliance by having one class per file.

---
+++ dev/tests/unit/testsuite/Magento/Core/Model/Route/ConfigTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+* {license_notice}
+*
+* @copyright   {copyright}
+* @license     {license_link}
+*/
+
+namespace Magento\Core\Model\Route;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+    * @var \Magento\Core\Model\Route\Config
+    */
+    protected $_config;
+
+    /**
+    * @var Wrapper
+    */
+    protected $_readerMock;
+
+    /**
+    * @var Wrapper
+    */
+    protected $_cacheMock;
+
+    protected function setUp()
+    {
+        $this->_readerMock = $this->getMock('Magento\Core\Model\Route\Config\Reader', array(), array(), '', false);
+            $this->_cacheMock = new Wrapper();
+            $this->_config = new \Magento\Core\Model\Route\Config(
+                $this->_readerMock,
+                $this->_cacheMock
+            );
+    }
+
+    public function testGetIfCacheIsArray()
+    {
+        $this->_cacheMock->getRealMock()->expects($this->once())
+        ->method('get')->with('areaCode', 'RoutesConfig-routerCode')
+        ->will($this->returnValue(array('expected')));
+        $this->assertEquals(array('expected'), $this->_config->getRoutes('areaCode', 'routerCode'));
+    }
+
+    public function testGetIfKeyExist()
+    {
+        $this->_readerMock->expects($this->once())
+        ->method('read')->with('areaCode')->will($this->returnValue(array()));
+        $this->assertEquals(array(), $this->_config->getRoutes('areaCode', 'routerCode'));
+    }
+
+    public function testGetRoutes()
+    {
+        $areaConfig['routerCode']['routes'] = 'Expected Value';
+        $this->_readerMock->expects($this->once())
+        ->method('read')->with('areaCode')->will($this->returnValue($areaConfig));
+        $this->_cacheMock->getRealMock()->expects($this->once())
+        ->method('put')->with('Expected Value', 'areaCode', 'RoutesConfig-routerCode');
+        $this->assertEquals('Expected Value', $this->_config->getRoutes('areaCode', 'routerCode'));
+    }
+}
--- dev/tests/unit/testsuite/Magento/Core/Model/Route/Wrapper.php
+++ dev/tests/unit/testsuite/Magento/Core/Model/Route/Wrapper.php
@@ -1,69 +1,8 @@
 <?php
 /**
- * {license_notice}
- *
- * @copyright   {copyright}
- * @license     {license_link}
- */
-class ConfigTest extends \PHPUnit_Framework_TestCase
-{
-    /**
-     * @var \Magento\Core\Model\Route\Config
-     */
-    protected $_config;
-
-    /**
-     * @var \Cache\Mock\Wrapper
-     */
-    protected $_readerMock;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $_cacheMock;
-
-    protected function setUp()
-    {
-        $this->_readerMock = $this->getMock('Magento\Core\Model\Route\Config\Reader', array(), array(), '', false);
-        $this->_cacheMock = new \Cache\Mock\Wrapper();
-        $this->_config = new \Magento\Core\Model\Route\Config(
-            $this->_readerMock,
-            $this->_cacheMock
-        );
-    }
-
-    public function testGetIfCacheIsArray()
-    {
-        $this->_cacheMock->getRealMock()->expects($this->once())
-            ->method('get')->with('areaCode', 'RoutesConfig-routerCode')
-            ->will($this->returnValue(array('expected')));
-        $this->assertEquals(array('expected'), $this->_config->getRoutes('areaCode', 'routerCode'));
-    }
-
-    public function testGetIfKeyExist()
-    {
-        $this->_readerMock->expects($this->once())
-            ->method('read')->with('areaCode')->will($this->returnValue(array()));
-        $this->assertEquals(array(), $this->_config->getRoutes('areaCode', 'routerCode'));
-    }
-
-    public function testGetRoutes()
-    {
-        $areaConfig['routerCode']['routes'] = 'Expected Value';
-        $this->_readerMock->expects($this->once())
-            ->method('read')->with('areaCode')->will($this->returnValue($areaConfig));
-        $this->_cacheMock->getRealMock()->expects($this->once())
-            ->method('put')->with('Expected Value', 'areaCode', 'RoutesConfig-routerCode');
-        $this->assertEquals('Expected Value', $this->_config->getRoutes('areaCode', 'routerCode'));
-    }
-}
-
-/**
  * Wrapper to pass method calls and arguments to mockup inside it
  */
 namespace Magento\Core\Model\Route;
-
-namespace Cache\Mock;
 
 class Wrapper extends \PHPUnit_Framework_TestCase implements \Magento\Config\CacheInterface
 {
@@ -74,7 +13,7 @@ class Wrapper extends \PHPUnit_Framework_TestCase implements \Magento\Config\Cac
 
     public function __construct()
     {
-        $this->_mock = $this->getMock('SomeClass', array('get', 'put'));
+         $this->_mock = $this->getMock('SomeClass', array('get', 'put'));
     }
 
     public function getRealMock()
