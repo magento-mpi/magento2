360afa3b482bb8c8b1b9d9e9953a97fdf3047c3b
Susan Wright <swright@ebay.com>
2013-09-19 15:57:19 -0700

    Fixed MAGETWO-12156: Resolve conflicts as a result of mainline merges.
    - Searched for "\Magento\ and >\Magento\ .

--- dev/tests/integration/testsuite/Magento/Adminhtml/controllers/_files/newsletter_sample.php
+++ dev/tests/integration/testsuite/Magento/Adminhtml/controllers/_files/newsletter_sample.php
@@ -13,7 +13,7 @@ $template = Mage::getModel('Magento\Newsletter\Model\Template');
 
 $templateData = array(
     'template_code'=>'some_unique_code',
-    'template_type'=>\Magento\Newsletter\Model\Template::TYPE_TEXT,
+    'template_type'=>Magento\Newsletter\Model\Template::TYPE_TEXT,
     'subject'=>'test data2__22',
     'template_sender_email'=>'sender@email.com',
     'template_sender_name'=>'Test Sender Name 222',
