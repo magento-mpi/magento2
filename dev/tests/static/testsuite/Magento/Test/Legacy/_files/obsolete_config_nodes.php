f1060a7e8d25d145d97e0ef5af5b80b11f9fe13f
Sergey Shymko <sergey.shymko@magento.com>
2013-09-19 15:22:18 +0300

    Implemented MAGETWO-14537: Extract Email Template Configuration Segment to Separate Files - extracted email templates configuration from config.xml to email_templates.xml - converted format of email templates configuration -- removed template directory prefix 'email/', invariant for all email templates -- removed specifying module name in the configuration, it always matches to module config file belongs to - added obsolete config nodes to static legacy tests

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_config_nodes.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_config_nodes.php
@@ -43,4 +43,5 @@ return array(
     '/config/global/catalog/product/flat/attribute_nodes'
         => 'Use /config/global/catalog/product/flat/attribute_groups',
     '/config/global/customer/address/formats' => 'Use /config/format of address_formats.xml',
+    '/config/global/template' => 'Use email_templates.xml',
 );
