e05524cb854cdd382c1e1dcc9ad7a9a0e967d5c2
Yuxing Zheng <yuxzheng@ebay.com>
2013-09-19 10:22:22 -0700

    Fix MAGETWO-11929: Convert all Classes in Magento2 Namespace to Use Formal Namespace Syntax

--- dev/tests/static/testsuite/Magento/Test/Legacy/Magento/Catalog/Model/Product/TypeTest.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/Magento/Catalog/Model/Product/TypeTest.php
@@ -19,9 +19,10 @@ class Magento_Test_Legacy_Magento_Catalog_Model_Product_TypeTest
      * @var array
      */
     protected $_productTypeFiles = array(
-        '/app/code/Magento/Catalog/Model/Product/Type/Abstract.php',
+        '/app/code/Magento/Catalog/Model/Product/Type/AbstractType.php',
         '/app/code/Magento/Catalog/Model/Product/Type/Configurable.php',
         '/app/code/Magento/Catalog/Model/Product/Type/Grouped.php',
+        '/app/code/Magento/Catalog/Model/Product/Type/Price.php',
         '/app/code/Magento/Catalog/Model/Product/Type/Simple.php',
         '/app/code/Magento/Catalog/Model/Product/Type/Virtual.php',
     );
