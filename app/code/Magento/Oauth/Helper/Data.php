a2acab635521728e15652f75d24a8a49528511ff
Alexander Paliarush <apaliarush@ebay.com>
2013-09-19 17:33:14 +0300

    Implemented MAGETWO-12285: Remove unnecessary UI flows

--- app/code/Magento/Oauth/Helper/Data.php
+++ app/code/Magento/Oauth/Helper/Data.php
@@ -31,11 +31,6 @@ class Magento_Oauth_Helper_Data extends Magento_Core_Helper_Abstract
     const XML_PATH_CLEANUP_EXPIRATION_PERIOD = 'oauth/cleanup/expiration_period';
     /**#@-*/
 
-    /**#@+ Email template */
-    const XML_PATH_EMAIL_TEMPLATE = 'oauth/email/template';
-    const XML_PATH_EMAIL_IDENTITY = 'oauth/email/identity';
-    /**#@-*/
-
     /**
      * Cleanup expiration period in minutes
      */
@@ -172,53 +167,6 @@ class Magento_Oauth_Helper_Data extends Magento_Core_Helper_Abstract
     }
 
     /**
-     * Return complete callback URL or boolean FALSE if no callback provided
-     *
-     * @param Magento_Oauth_Model_Token $token Token object
-     * @param bool $rejected OPTIONAL Add user reject sign
-     * @return bool|string
-     */
-    public function getFullCallbackUrl(Magento_Oauth_Model_Token $token, $rejected = false)
-    {
-        $callbackUrl = $token->getCallbackUrl();
-
-        if (Magento_Oauth_Model_Server::CALLBACK_ESTABLISHED == $callbackUrl) {
-            return false;
-        }
-        if ($rejected) {
-            /** @var $consumer Magento_Oauth_Model_Consumer */
-            $consumer = $this->_consumerFactory->create()->load($token->getConsumerId());
-
-            if ($consumer->getId() && $consumer->getRejectedCallbackUrl()) {
-                $callbackUrl = $consumer->getRejectedCallbackUrl();
-            }
-        } else if (!$token->getAuthorized()) {
-            Mage::throwException('Token is not authorized');
-        }
-
-        $callbackUrl .= (false === strpos($callbackUrl, '?') ? '?' : '&');
-        $callbackUrl .= 'oauth_token=' . $token->getToken() . '&';
-        $callbackUrl .= $rejected ? self::QUERY_PARAM_REJECTED . '=1' : 'oauth_verifier=' . $token->getVerifier();
-
-        return $callbackUrl;
-    }
-
-    /**
-     * Retrieve URL of specified endpoint.
-     *
-     * @param string $type Endpoint type (one of ENDPOINT_ constants)
-     * @return string
-     * @throws Exception    Exception when endpoint not found
-     */
-    public function getProtocolEndpointUrl($type)
-    {
-        if (!in_array($type, $this->_endpoints)) {
-            throw new Exception('Invalid endpoint type passed.');
-        }
-        return rtrim(Mage::getUrl($type), '/');
-    }
-
-    /**
      * Calculate cleanup possibility for data with lifetime property
      *
      * @return bool
@@ -239,90 +187,5 @@ class Magento_Oauth_Helper_Data extends Magento_Core_Helper_Abstract
     {
         $minutes = (int) $this->_store->getConfig(self::XML_PATH_CLEANUP_EXPIRATION_PERIOD);
         return $minutes > 0 ? $minutes : self::CLEANUP_EXPIRATION_PERIOD_DEFAULT;
-    }
-
-    /**
-     * Send Email to Token owner
-     *
-     * @param string $userEmail
-     * @param string $userName
-     * @param string $applicationName
-     * @param string $status
-     */
-    public function sendNotificationOnTokenStatusChange($userEmail, $userName, $applicationName, $status)
-    {
-        /* @var $mailTemplate Magento_Core_Model_Email_Template */
-        $mailTemplate = $this->_objectManager->create('Magento_Core_Model_Email_Template');
-
-        $mailTemplate->sendTransactional(
-            $this->_store->getConfig(self::XML_PATH_EMAIL_TEMPLATE),
-            $this->_store->getConfig(self::XML_PATH_EMAIL_IDENTITY),
-            $userEmail,
-            $userName,
-            array(
-                'name'              => $userName,
-                'email'             => $userEmail,
-                'applicationName'   => $applicationName,
-                'status'            => $status,
-
-            )
-        );
-    }
-
-    /**
-     * Is current authorize page is simple
-     *
-     * @return boolean
-     */
-    protected function _isSimple()
-    {
-        $simple = false;
-        if (stristr($this->_getRequest()->getActionName(), 'simple')
-            || !is_null($this->_getRequest()->getParam('simple', null))
-        ) {
-            $simple = true;
-        }
-
-        return $simple;
-    }
-
-    /**
-     * Get authorize endpoint url
-     *
-     * @param string $userType
-     * @throws Exception
-     * @return string
-     */
-    public function getAuthorizeUrl($userType)
-    {
-        $simple = $this->_isSimple();
-
-        if (Magento_Oauth_Model_Token::USER_TYPE_CUSTOMER == $userType) {
-            if ($simple) {
-                $route = self::ENDPOINT_AUTHORIZE_CUSTOMER_SIMPLE;
-            } else {
-                $route = self::ENDPOINT_AUTHORIZE_CUSTOMER;
-            }
-        } elseif (Magento_Oauth_Model_Token::USER_TYPE_ADMIN == $userType) {
-            if ($simple) {
-                $route = self::ENDPOINT_AUTHORIZE_ADMIN_SIMPLE;
-            } else {
-                $route = self::ENDPOINT_AUTHORIZE_ADMIN;
-            }
-        } else {
-            throw new Exception('Invalid user type.');
-        }
-
-        return $this->_getUrl($route, array('_query' => array('oauth_token' => $this->getOauthToken())));
-    }
-
-    /**
-     * Retrieve oauth_token param from request
-     *
-     * @return string|null
-     */
-    public function getOauthToken()
-    {
-        return $this->_getRequest()->getParam('oauth_token', null);
     }
 }
