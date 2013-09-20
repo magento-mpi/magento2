20c7e431a567ea85b29b33c4bdd8e4c3d70166ec
Maxim Shikula <mshikula@ebay.com>
2013-09-20 17:45:57 +0300

    Fixed MAGETWO-14655: Impossible to install Magento

--- app/code/Magento/WebsiteRestriction/Model/Resource/Setup.php
+++ app/code/Magento/WebsiteRestriction/Model/Resource/Setup.php
@@ -24,28 +24,30 @@ class Magento_WebsiteRestriction_Model_Resource_Setup extends Magento_Core_Model
     protected $_pageFactory;
 
     /**
+     * @param Magento_Core_Model_Logger $logger
      * @param Magento_Core_Model_Event_Manager $eventManager
      * @param Magento_Core_Model_Config_Resource $resourcesConfig
      * @param Magento_Core_Model_Config $config
      * @param Magento_Core_Model_ModuleListInterface $moduleList
      * @param Magento_Core_Model_Resource $resource
      * @param Magento_Core_Model_Config_Modules_Reader $modulesReader
-     * @param $resourceName
      * @param Magento_Cms_Model_PageFactory $pageFactory
+     * @param $resourceName
      */
     public function __construct(
+        Magento_Core_Model_Logger $logger,
         Magento_Core_Model_Event_Manager $eventManager,
         Magento_Core_Model_Config_Resource $resourcesConfig,
         Magento_Core_Model_Config $config,
         Magento_Core_Model_ModuleListInterface $moduleList,
         Magento_Core_Model_Resource $resource,
         Magento_Core_Model_Config_Modules_Reader $modulesReader,
-        $resourceName,
-        Magento_Cms_Model_PageFactory $pageFactory
+        Magento_Cms_Model_PageFactory $pageFactory,
+        $resourceName
     ) {
         $this->_pageFactory = $pageFactory;
         parent::__construct(
-            $eventManager, $resourcesConfig, $config, $moduleList, $resource, $modulesReader, $resourceName
+            $logger, $eventManager, $resourcesConfig, $config, $moduleList, $resource, $modulesReader, $resourceName
         );
     }
 
