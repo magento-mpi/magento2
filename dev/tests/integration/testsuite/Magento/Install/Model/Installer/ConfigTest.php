5c5b4b88351ec2d2c790527d101881d756eb34ba
Bibu Mathew <bimathew@paypal.com>
2013-09-21 12:02:57 -0500

    Fixed MAGETWO-13463:Modify Unit, Static and Integration Tests As Needed to Support Formal Namespaces - removed __CLASS__ and passed the added class name

--- dev/tests/integration/testsuite/Magento/Install/Model/Installer/ConfigTest.php
+++ dev/tests/integration/testsuite/Magento/Install/Model/Installer/ConfigTest.php
@@ -16,7 +16,7 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
 
     public static function setUpBeforeClass()
     {
-        self::$_tmpDir = \Mage::getBaseDir(\Magento\Core\Model\Dir::VAR_DIR) . DIRECTORY_SEPARATOR . __CLASS__;
+        self::$_tmpDir = \Mage::getBaseDir(\Magento\Core\Model\Dir::VAR_DIR) . DIRECTORY_SEPARATOR . 'ConfigTest';
         mkdir(self::$_tmpDir);
     }
 
