e1b10b9306d30a14a51788c6d8a142d43b30d643
oivashchenko <oivashchenko@ebay.com>
2013-09-21 19:23:02 +0300

    CR Changes MAGETWO-14592: Elimination of "Mage" class usages from module "Magento_CatalogPermissions"
     - property type was changed

--- app/code/Magento/CatalogPermissions/Model/Adminhtml/Observer.php
+++ app/code/Magento/CatalogPermissions/Model/Adminhtml/Observer.php
@@ -49,12 +49,12 @@ class Magento_CatalogPermissions_Model_Adminhtml_Observer
     protected $_indexer;
 
     /**
-     * @var Magento_Core_Model_App
+     * @var Magento_Core_Model_CacheInterface
      */
-    protected $_coreApp;
+    protected $_coreCache;
 
     /**
-     * @param Magento_Core_Model_App $coreApp
+     * @param Magento_Core_Model_CacheInterface $coreCache
      * @param Magento_Catalog_Model_CategoryFactory $categoryFactory
      * @param Magento_CatalogPermissions_Model_PermissionFactory $permissionFactory
      * @param Magento_Index_Model_Indexer $indexer
@@ -62,14 +62,14 @@ class Magento_CatalogPermissions_Model_Adminhtml_Observer
      * @param Magento_AuthorizationInterface $authorization
      */
     public function __construct(
-        Magento_Core_Model_App $coreApp,
+        Magento_Core_Model_CacheInterface $coreCache,
         Magento_Catalog_Model_CategoryFactory $categoryFactory,
         Magento_CatalogPermissions_Model_PermissionFactory $permissionFactory,
         Magento_Index_Model_Indexer $indexer,
         Magento_CatalogPermissions_Helper_Data $catalogPermData,
         Magento_AuthorizationInterface $authorization
     ) {
-        $this->_coreApp = $coreApp;
+        $this->_coreCache = $coreCache;
         $this->_indexer = $indexer;
         $this->_categoryFactory = $categoryFactory;
         $this->_permissionFactory = $permissionFactory;
@@ -173,7 +173,7 @@ class Magento_CatalogPermissions_Model_Adminhtml_Observer
                 Magento_CatalogPermissions_Model_Permission_Index::ENTITY_CATEGORY,
                 Magento_CatalogPermissions_Model_Permission_Index::EVENT_TYPE_REINDEX_PRODUCTS
             );
-            $this->_coreApp->cleanCache(array(Magento_Catalog_Model_Category::CACHE_TAG));
+            $this->_coreCache->cleanCache(array(Magento_Catalog_Model_Category::CACHE_TAG));
         }
 
         if (!empty($this->_indexProductQueue)) {
@@ -203,7 +203,7 @@ class Magento_CatalogPermissions_Model_Adminhtml_Observer
      */
     public function cleanCacheOnConfigChange()
     {
-        $this->_coreApp->cleanCache(array(Magento_Catalog_Model_Category::CACHE_TAG));
+        $this->_coreCache->cleanCache(array(Magento_Catalog_Model_Category::CACHE_TAG));
         $this->_indexer->processEntityAction(
             new Magento_Object(),
             Magento_CatalogPermissions_Model_Permission_Index::ENTITY_CONFIG,
