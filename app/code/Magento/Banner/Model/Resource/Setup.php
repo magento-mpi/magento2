573224fcf1896f52ab17c74ab6419b6ca457f0b6
oivashchenko <oivashchenko@ebay.com>
2013-09-21 20:38:09 +0300

    Fixed MAGETWO-14702: Merge with mainline after pull request #70
     - fix after merge

--- app/code/Magento/Banner/Model/Resource/Setup.php
+++ app/code/Magento/Banner/Model/Resource/Setup.php
@@ -51,6 +51,7 @@ class Magento_Banner_Model_Resource_Setup extends Magento_Sales_Model_Resource_S
      * @SuppressWarnings(PHPMD.ExcessiveParameterList)
      */
     public function __construct(
+        Magento_Core_Model_Resource_Setup_MigrationFactory $migrationFactory,
         Magento_Core_Model_Logger $logger,
         Magento_Core_Helper_Data $coreData,
         Magento_Core_Model_Event_Manager $eventManager,
@@ -66,6 +67,7 @@ class Magento_Banner_Model_Resource_Setup extends Magento_Sales_Model_Resource_S
         $resourceName
     ) {
         parent::__construct(
+            $migrationFactory,
             $logger,
             $coreData,
             $eventManager,
