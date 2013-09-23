a5d7a4c3c6b7e57e902c4de5cadc5d923edb4fdf
Bibu Mathew <bimathew@paypal.com>
2013-09-23 17:20:07 -0500

    Fixed MAGETWO-14810:Resolve error - PSR1.Classes.ClassDeclaration.MultipleClasses: - removed multiple class declaration and added new file - multiple class declaration psr1 violation is not fixed as this test classes are for phpmd so added to blacklist

--- dev/tests/integration/testsuite/Magento/Cms/Controller/ManagerStub.php
+++ dev/tests/integration/testsuite/Magento/Cms/Controller/ManagerStub.php
@@ -47,30 +47,3 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     }
 }
 
-/**
- * Event manager stub
- */
-namespace Magento\Cms\Controller;
-
-namespace Magento\Core\Model\Event;
-
-class ManagerStub extends \Magento\Core\Model\Event\Manager
-{
-    /**
-     * Stub dispatch event
-     *
-     * @param string $eventName
-     * @param array $params
-     * @return \Magento\Core\Model\App|null
-     */
-    public function dispatch($eventName, array $params = array())
-    {
-        switch ($eventName) {
-            case 'cms_controller_router_match_before' :
-                $params['condition']->setRedirectUrl('http://www.example.com/');
-                break;
-        }
-
-        return null;
-    }
-}
