28823d9a17461219defab055ced5e946f4f60315
Bryant Luk <bluk@ebay.com>
2013-09-23 15:15:19 -0500

    Fixed MAGETWO-12156: Resolve conflicts as a result of mainline merges
    - Fixed Legacy test for Magento GiftCard Model Product TypeTest

--- dev/tests/static/testsuite/Magento/Test/Legacy/Magento/GiftCard/Model/Product/TypeTest.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/Magento/GiftCard/Model/Product/TypeTest.php
@@ -13,7 +13,7 @@
  * Tests for obsolete methods in Product Type instances
  */
 class Magento_Test_Legacy_Magento_GiftCard_Model_Product_TypeTest
-    extends Magento_Test_Legacy_\Magento\Catalog\Model\Product_AbstractTypeTest
+    extends Magento_Test_Legacy_Magento_Catalog_Model_Product_AbstractTypeTest
 {
     /**
      * @var array
