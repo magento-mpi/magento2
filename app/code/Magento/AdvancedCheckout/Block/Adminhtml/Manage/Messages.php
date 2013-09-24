79185c319c68b90c6118d5b498a18fdda469414a
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2013-09-24 15:56:31 +0300

    Fixed MAGETWO-14820: Merge south-develop and stabilize builds
    - fix tests

--- app/code/Magento/AdvancedCheckout/Block/Adminhtml/Manage/Messages.php
+++ app/code/Magento/AdvancedCheckout/Block/Adminhtml/Manage/Messages.php
@@ -18,11 +18,32 @@
 class Magento_AdvancedCheckout_Block_Adminhtml_Manage_Messages extends Magento_Adminhtml_Block_Messages
 {
     /**
+     * @var Magento_Backend_Model_Session
+     */
+    protected $_backendSession;
+
+    /**
+     * @param Magento_Backend_Model_Session $backendSession
+     * @param Magento_Core_Helper_Data $coreData
+     * @param Magento_Core_Block_Template_Context $context
+     * @param array $data
+     */
+    public function __construct(
+        Magento_Backend_Model_Session $backendSession,
+        Magento_Core_Helper_Data $coreData,
+        Magento_Core_Block_Template_Context $context,
+        array $data = array()
+    ) {
+        $this->_backendSession = $backendSession;
+        parent::__construct($coreData, $context, $data);
+    }
+
+    /**
      * Prepares layout for current block
      */
     protected function _prepareLayout()
     {
-        $this->addMessages($this->_session->getMessages(true));
+        $this->addMessages($this->_backendSession->getMessages(true));
         parent::_prepareLayout();
     }
 }
