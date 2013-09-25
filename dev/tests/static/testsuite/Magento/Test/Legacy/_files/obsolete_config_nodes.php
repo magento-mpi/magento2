4a4b206e0ddb3110ef46c8161c196cd3910583e8
johe <joan@x.com>
2013-09-25 14:06:25 -0500

    Implemented MAGETWO-14935: add legacy test

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_config_nodes.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_config_nodes.php
@@ -20,6 +20,8 @@ return array(
     '/config/global/blocks/*/class'            => '',
     '/config/global/models/*/resourceModel'    => '',
     '/config/global/payment'                   => 'Move them to payment.xml.',
+    '/config/global/payment/cc/types/*/validator' =>
+        'This configuration moved to Di configuration of Magento_Centinel_Model_StateFactory',
     '/config/adminhtml/menu'                   => 'Move them to adminhtml.xml.',
     '/config/adminhtml/acl'                    => 'Move them to adminhtml.xml.',
     '/config/*[self::global|self::adminhtml|self::frontend]/di' => 'This configuration moved to di.xml file',
