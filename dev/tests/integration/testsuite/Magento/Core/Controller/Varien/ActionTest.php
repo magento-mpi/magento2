d3e4d2c4fa0073ed4f9b5e8bd12df096beaef87c
Bryant Luk <bluk@ebay.com>
2013-09-25 10:00:50 -0500

    Fixed MAGETWO-12156: Resolve conflicts as a result of mainline merges
    - Fixed Magento/Core/Controller/Varien/ActionTest merge conflict
      Removed extra code that existed prior but was re-added due to
      namespace change conflict.

--- dev/tests/integration/testsuite/Magento/Core/Controller/Varien/ActionTest.php
+++ dev/tests/integration/testsuite/Magento/Core/Controller/Varien/ActionTest.php
@@ -177,15 +177,6 @@ class ActionTest extends \PHPUnit_Framework_TestCase
      */
     public function testAddActionLayoutHandlesInherited($route, $controller, $action, $expected)
     {
-        $arguments = array(
-            'request'  => $this->_objectManager->get('Magento\TestFramework\Request'),
-            'response' => $this->_objectManager->get('Magento\TestFramework\Response'),
-            'isRenderInherited' => true,
-        );
-        $context = $this->_objectManager->create('Magento\Core\Controller\Varien\Action\Context', $arguments);
-        $this->_object = $this->getMockForAbstractClass('Magento\Core\Controller\Varien\Action',
-            array('context' => $context));
-
         $this->_object->getRequest()
             ->setRouteName($route)
             ->setControllerName($controller)
