b6674e07fe881a7f761c690888e6b448f0a54a09
Maxim Shikula <mshikula@ebay.com>
2013-09-26 15:04:21 +0300

    Fixed MAGETWO-13784: Elimination of "Mage" class usages from module "Magento_Rule" - Resolve merge conflicts

--- app/code/Magento/SalesRule/Model/Resource/Setup.php
+++ app/code/Magento/SalesRule/Model/Resource/Setup.php
@@ -14,52 +14,6 @@
 class Magento_SalesRule_Model_Resource_Setup extends Magento_Sales_Model_Resource_Setup
 {
     /**
-     * @var Magento_Core_Model_Resource_Setup_MigrationFactory
-     */
-    protected $_migrationFactory;
-
-    /**
-     * @param Magento_Core_Model_Logger $logger
-     * @param Magento_Core_Helper_Data $coreData
-     * @param Magento_Core_Model_Event_Manager $eventManager
-     * @param Magento_Core_Model_Config_Resource $resourcesConfig
-     * @param Magento_Core_Model_Config $modulesConfig
-     * @param Magento_Core_Model_ModuleListInterface $moduleList
-     * @param Magento_Core_Model_Resource $resource
-     * @param Magento_Core_Model_Config_Modules_Reader $modulesReader
-     * @param Magento_Core_Model_CacheInterface $cache
-     * @param $resourceName
-     * @param Magento_Core_Model_Resource_Setup_MigrationFactory $migrationFactory
-     */
-    public function __construct(
-        Magento_Core_Model_Logger $logger,
-        Magento_Core_Helper_Data $coreData,
-        Magento_Core_Model_Event_Manager $eventManager,
-        Magento_Core_Model_Config_Resource $resourcesConfig,
-        Magento_Core_Model_Config $modulesConfig,
-        Magento_Core_Model_ModuleListInterface $moduleList,
-        Magento_Core_Model_Resource $resource,
-        Magento_Core_Model_Config_Modules_Reader $modulesReader,
-        Magento_Core_Model_CacheInterface $cache,
-        $resourceName,
-        Magento_Core_Model_Resource_Setup_MigrationFactory $migrationFactory
-    ) {
-        $this->_migrationFactory = $migrationFactory;
-        parent::__construct(
-            $logger,
-            $coreData,
-            $eventManager,
-            $resourcesConfig,
-            $modulesConfig,
-            $moduleList,
-            $resource,
-            $modulesReader,
-            $cache,
-            $resourceName
-        );
-    }
-
-    /**
      * @return Magento_Core_Model_Resource_Setup_Migration
      */
     public function getMigrationModel()
