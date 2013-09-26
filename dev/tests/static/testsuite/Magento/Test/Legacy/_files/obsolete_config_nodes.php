bcdc59315f2383769566d39240caf0ec7ca37bbd
Christopher O'Toole <otoolec@x.com>
2013-09-26 11:42:58 -0500

    Fixed MAGETWO-14942: Story prevents any nodes under payment
    Legacy test is being updated to be as generic as before.  We need to
    prevent any config that exists under /config/global/payment per the story,
    not just groups and types as this task would indicate.

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_config_nodes.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_config_nodes.php
@@ -22,8 +22,7 @@ return array(
     '/config/global/payment'                   => 'Move them to payment.xml.',
     '/config/global/payment/cc/types/*/validator' =>
         'This configuration was moved to DI configuration of Magento_Centinel_Model_StateFactory',
-    '/config/global/payment/cc/types'                   => 'Move them to payment.xml.',
-    '/config/global/payment/groups'                   => 'Move them to payment.xml.',
+    '/config/global/payment'                   => 'Move them to payment.xml.',
     '/config/adminhtml/menu'                   => 'Move them to adminhtml.xml.',
     '/config/adminhtml/acl'                    => 'Move them to adminhtml.xml.',
     '/config/*[self::global|self::adminhtml|self::frontend]/di' => 'This configuration moved to di.xml file',
