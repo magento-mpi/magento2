f04a222b7f438ba5d1db9c43dc728f57300969ff
Bryant Luk <bluk@ebay.com>
2013-09-28 16:16:09 -0500

    Fixed MAGETWO-12156: Resolve conflicts as a result of mainline merges
    - Restores AdminGws Observer hardcoded class reference change
      to use namespaces instead of _

--- app/code/Magento/AdminGws/Model/Observer.php
+++ app/code/Magento/AdminGws/Model/Observer.php
@@ -539,6 +539,7 @@ class Observer extends \Magento\AdminGws\Model\Observer\AbstractObserver
                  */
                 //if (class_exists($className, false)) {
                 if ($className) {
+                    $className = str_replace('_', '\\', $className);
                     $this->_callbacks[$callbackGroup][$className] = $this->_recognizeCallbackString($callback);
                 }
                 //}
