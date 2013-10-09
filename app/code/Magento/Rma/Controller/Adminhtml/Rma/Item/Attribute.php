9716be8ac120365557de76b32fd592dc6cd917ac
Alexander Paliarush <apaliarush@ebay.com>
2013-10-09 15:03:50 +0300

    Fixed MAGETWO-14130: Impossible to create new RMA attribute from backend - Refactoring

--- app/code/Magento/Rma/Controller/Adminhtml/Rma/Item/Attribute.php
+++ app/code/Magento/Rma/Controller/Adminhtml/Rma/Item/Attribute.php
@@ -192,7 +192,7 @@ class Attribute extends \Magento\Adminhtml\Controller\Action
         if ($this->getRequest()->isPost() && $data) {
             /* @var $attributeObject \Magento\Rma\Model\Item\Attribute */
             $attributeObject = $this->_initAttribute();
-            /* @var $helper 'Magento\CustomAttribute\Helper\Data */
+            /* @var $helper Magento\CustomAttribute\Helper\Data */
             $helper = $this->_objectManager->get('Magento\CustomAttribute\Helper\Data');
 
             try {
