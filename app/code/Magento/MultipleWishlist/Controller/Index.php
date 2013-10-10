b0753bb42ade397edd2f630e97644df5086a12ad
Vladimir Tarasov <vtarasov@ebay.com>
2013-10-10 14:02:18 +0300

    Fixed MAGETWO-15375: Fatal errors using SoapClient and DomainException without namespace

--- app/code/Magento/MultipleWishlist/Controller/Index.php
+++ app/code/Magento/MultipleWishlist/Controller/Index.php
@@ -305,7 +305,7 @@ class Index extends \Magento\Wishlist\Controller\Index
      * @param \Magento\Wishlist\Model\Item $item
      * @param \Magento\Wishlist\Model\Wishlist $wishlist
      * @param int $qty
-     * @throws \InvalidArgumentException|DomainException
+     * @throws \InvalidArgumentException|\DomainException
      */
     protected function _copyItem(\Magento\Wishlist\Model\Item $item, \Magento\Wishlist\Model\Wishlist $wishlist, $qty = null)
     {
@@ -313,7 +313,7 @@ class Index extends \Magento\Wishlist\Controller\Index
             throw new \InvalidArgumentException();
         }
         if ($item->getWishlistId() == $wishlist->getId()) {
-            throw new DomainException();
+            throw new \DomainException();
         }
         $buyRequest = $item->getBuyRequest();
         if ($qty) {
@@ -369,7 +369,7 @@ class Index extends \Magento\Wishlist\Controller\Index
                 $this->_getSession->addError(
                     __('The item was not found.')
                 );
-            } catch (DomainException $e) {
+            } catch (\DomainException $e) {
                 $this->_getSession()->addError(
                     __('"%1" is already present in %2.', $productName, $wishlistName)
                 );
@@ -423,7 +423,7 @@ class Index extends \Magento\Wishlist\Controller\Index
                     $copied[$id] = $item;
                 } catch (\InvalidArgumentException $e) {
                     $notFound[] = $id;
-                } catch (DomainException $e) {
+                } catch (\DomainException $e) {
                     $alreadyPresent[$id] = $item;
                 } catch (\Exception $e) {
                     $this->_objectManager->get('Magento\Core\Model\Logger')->logException($e);
@@ -472,7 +472,7 @@ class Index extends \Magento\Wishlist\Controller\Index
      * @param \Magento\Wishlist\Model\Wishlist $wishlist
      * @param \Magento\Wishlist\Model\Resource\Wishlist\Collection $customerWishlists
      * @param int $qty
-     * @throws \InvalidArgumentException|DomainException
+     * @throws \InvalidArgumentException|\DomainException
      */
     protected function _moveItem(
         \Magento\Wishlist\Model\Item $item,
@@ -484,10 +484,10 @@ class Index extends \Magento\Wishlist\Controller\Index
             throw new \InvalidArgumentException();
         }
         if ($item->getWishlistId() == $wishlist->getId()) {
-            throw new DomainException(null, 1);
+            throw new \DomainException(null, 1);
         }
         if (!$customerWishlists->getItemById($item->getWishlistId())) {
-            throw new DomainException(null, 2);
+            throw new \DomainException(null, 2);
         }
 
         $buyRequest = $item->getBuyRequest();
@@ -539,7 +539,7 @@ class Index extends \Magento\Wishlist\Controller\Index
                 $this->_getSession()->addError(
                     __("An item with this ID doesn't exist.")
                 );
-            } catch (DomainException $e) {
+            } catch (\DomainException $e) {
                 if ($e->getCode() == 1) {
                     $this->_getSession()->addError(
                         __('"%1" is already present in %2.', $productName, $wishlistName)
@@ -592,7 +592,7 @@ class Index extends \Magento\Wishlist\Controller\Index
                     $moved[$id] = $item;
                 } catch (\InvalidArgumentException $e) {
                     $notFound[] = $id;
-                } catch (DomainException $e) {
+                } catch (\DomainException $e) {
                     if ($e->getCode() == 1) {
                         $alreadyPresent[$id] = $item;
                     } else {
