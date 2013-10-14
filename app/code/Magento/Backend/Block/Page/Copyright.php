71e20053b972065ca021145edb886c62359e78c2
mslabko <mslabko@ebay.com>
2013-10-14 14:17:10 +0300

    Implemented MAGETWO-14444: [TD] Update Copyright Text in Admin Panel

---
+++ app/code/Magento/Backend/Block/Page/Copyright.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright {copyright}
+ * @license   {license_link}
+ */
+
+namespace Magento\Backend\Block\Page;
+
+/**
+ * Copyright footer block
+ */
+class Copyright extends \Magento\Backend\Block\Template
+{
+    protected $_template = 'page/copyright.phtml';
+}
