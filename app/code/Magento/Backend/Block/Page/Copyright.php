014def96c20cd4df60e7b2673e32d4b082fb5e1e
mslabko <mslabko@ebay.com>
2013-10-15 15:27:38 +0300

    Implemented MAGETWO-14444: [TD] Update Copyright Text in Admin Panel - changed copyright in error-pages, install-page, downloader

--- app/code/Magento/Backend/Block/Page/Copyright.php
+++ app/code/Magento/Backend/Block/Page/Copyright.php
@@ -14,9 +14,10 @@ namespace Magento\Backend\Block\Page;
 class Copyright extends \Magento\Backend\Block\Template
 {
     /**
-     * Path to template file in theme
-     *
-     * @var string
+     * {@inheritdoc}
      */
-    protected $_template = 'page/copyright.phtml';
+    protected function _toHtml()
+    {
+        echo __('Magento is an eBay Inc. company. Copyright&copy; %1 Magento, Inc. All rights reserved.', date('Y'));
+    }
 }
