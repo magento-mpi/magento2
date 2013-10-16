666776c28941e52291e88aff8a6e7e58d596bf5d
Maryna Ilnytska <milnytska@ebay.com>
2013-10-16 12:10:04 +0300

    Implemented MAGETWO-15842: Move framework part out of "/app/code/Mage/Core/functions.php" Part 2
    - __()  function was moved to app/functions.php

--- app/code/Magento/Core/functions.php
+++ app/code/Magento/Core/functions.php
@@ -142,22 +142,3 @@ function mageCoreErrorHandler($errorNo, $errorStr, $errorFile, $errorLine)
         $logger->log($errorMessage, \Zend_Log::ERR);
     }
 }
-
-/**
- * Create value-object \Magento\Phrase
- *
- * @return string
- */
-function __()
-{
-    $argc = func_get_args();
-
-    /**
-     * Type casting to string is a workaround.
-     * Many places in client code at the moment are unable to handle the \Magento\Phrase object properly.
-     * The intended behavior is to use __toString(),
-     * so that rendering of the phrase happens only at the last moment when needed
-     */
-    return (string)new \Magento\Phrase(array_shift($argc), $argc);
-}
-
