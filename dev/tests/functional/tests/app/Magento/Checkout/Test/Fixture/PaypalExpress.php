e091a33ecca8dd06460101bfbbbefcbf1e70d350
Max Pronko <mpronko@ebay.com>
2013-10-23 09:57:32 +0300

    Fixed MAGETWO-15509: Checkout on paypal side button (Payflow Edition) from the shopping cart  - fixed telephone number variable assignment

--- dev/tests/functional/tests/app/Magento/Checkout/Test/Fixture/PaypalExpress.php
+++ dev/tests/functional/tests/app/Magento/Checkout/Test/Fixture/PaypalExpress.php
@@ -99,9 +99,9 @@ class PaypalExpress extends Checkout
         $this->paypalCustomer = Factory::getFixtureFactory()->getMagentoPaypalCustomer();
         $this->paypalCustomer->switchData('customer_US');
 
-        $this->telephoneNumber = Factory::getFixtureFactory()->getMagentoCustomerAddress();
-        $this->telephoneNumber->switchData('address_US_1');
-        $this->telephoneNumber->getTelephone();
+        $customerAddress = Factory::getFixtureFactory()->getMagentoCustomerAddress();
+        $customerAddress->switchData('address_US_1');
+        $this->telephoneNumber = $customerAddress->getTelephone();
 
         //Verification data
         $this->_data = array(
