38f072b782579c48e833f14336c59ef4381fd8ea
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2013-10-23 13:36:03 +0300

    Fixed MAGETWO-16213: Error in logs on any action in Frontend
     - update construct

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
@@ -1029,4 +1029,6 @@ return array(
     array('Magento\Core\Model\Layout\File\Source\Override\Theme','Magento\View\Layout\File\Override\Theme'),
     array('Magento\Core\Model\Layout\File\Source\Theme','Magento\View\Layout\File\Source\Theme'),
     array('Magento\Core\Model\Layout\File\SourceInterface','Magento\View\Layout\File\SourceInterface'),
+    array('Magento\Core\Model\LayoutFactory','Magento\View\LayoutFactory'),
+    array('Magento\Core\Model\Layout','Magento\View\LayoutInterface'),
 );
