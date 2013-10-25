b082c3734d97a8b7ccae61ff8ae576dc00204978
Vadym Popov <vapopov@ebay.com>
2013-10-25 18:44:43 +0300

    Fixed MAGETWO-15836: Helpers to Libraries Decomposition Part 1 - Fix HeaderTest

--- dev/tests/integration/testsuite/Magento/HTTP/HeaderTest.php
+++ dev/tests/integration/testsuite/Magento/HTTP/HeaderTest.php
@@ -16,25 +16,30 @@ class HeaderTest extends \PHPUnit_Framework_TestCase
     /**
      * @var \Magento\HTTP\Header
      */
-    protected $_helper;
+    protected $_header;
 
     protected function setUp()
     {
-        $this->_helper = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\HTTP\Header');
+        $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $this->_header = $objectManager->get('Magento\HTTP\Header');
+
+        /** @var \Magento\TestFramework\Request $request */
+        $request = $objectManager->get('Magento\TestFramework\Request');
+        $request->setServer(array('HTTP_HOST' => 'localhost'));
     }
 
     public function testGetHttpHeaderMethods()
     {
         $host = 'localhost';
-        $this->assertEquals($host, $this->_helper->getHttpHost());
-        $this->assertEquals(false, $this->_helper->getHttpUserAgent());
-        $this->assertEquals(false, $this->_helper->getHttpAcceptLanguage());
-        $this->assertEquals(false, $this->_helper->getHttpAcceptCharset());
-        $this->assertEquals(false, $this->_helper->getHttpReferer());
+        $this->assertEquals($host, $this->_header->getHttpHost());
+        $this->assertEquals(false, $this->_header->getHttpUserAgent());
+        $this->assertEquals(false, $this->_header->getHttpAcceptLanguage());
+        $this->assertEquals(false, $this->_header->getHttpAcceptCharset());
+        $this->assertEquals(false, $this->_header->getHttpReferer());
     }
 
     public function testGetRequestUri()
     {
-        $this->assertNull($this->_helper->getRequestUri());
+        $this->assertNull($this->_header->getRequestUri());
     }
 }
