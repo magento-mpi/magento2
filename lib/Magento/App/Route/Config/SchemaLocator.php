a86a5716e187e620790ca6b8c99294e6dc28cf39
Anton Kril <akril@ebay.com>
2013-10-28 12:57:21 +0200

    CR Changes MAGETWO-15658: Application Bootstrapping Components in library

--- lib/Magento/App/Route/Config/SchemaLocator.php
+++ lib/Magento/App/Route/Config/SchemaLocator.php
@@ -12,45 +12,22 @@ namespace Magento\App\Route\Config;
 class SchemaLocator implements \Magento\Config\SchemaLocatorInterface
 {
     /**
-     * Path to corresponding XSD file with validation rules for merged config
-     *
-     * @var string
-     */
-    protected $_schema = null;
-
-    /**
-     * Path to corresponding XSD file with validation rules for separate config files
-     *
-     * @var string
-     */
-    protected $_perFileSchema = null;
-
-    /**
-     * @param \Magento\Core\Model\Config\Modules\Reader $moduleReader
-     */
-    public function __construct(\Magento\Core\Model\Config\Modules\Reader $moduleReader)
-    {
-        $this->_perFileSchema = $moduleReader->getModuleDir('etc', 'Magento_Core') . DIRECTORY_SEPARATOR . 'routes.xsd';
-        $this->_schema = $moduleReader->getModuleDir('etc', 'Magento_Core') . DIRECTORY_SEPARATOR . 'routes_merged.xsd';
-    }
-
-    /**
      * Get path to merged config schema
      *
-     * @return string|null
+     * @return string
      */
     public function getSchema()
     {
-        return $this->_schema;
+        return realpath(__DIR__ . '/../../etc/routes_merged.xsd');
     }
 
     /**
      * Get path to pre file validation schema
      *
-     * @return string|null
+     * @return string
      */
     public function getPerFileSchema()
     {
-        return $this->_perFileSchema;
+        return realpath(__DIR__ . '/../../etc/routes.xsd');
     }
 }
