c2cc9d120894b68429c3f928787016f1deb4d660
Iryna Lagno <ilagno@ebay.com>
2013-10-29 11:37:09 +0200

    Implemented MAGETWO-16296: Compiler adoption: -cover with unit tests for \Magento\Code\Validator\ConstructorIntegrity, \Magento\Tools\Di\Code\Scanner\PhpScanner,  \Magento\Tools\Di\Code\Scanner\XmlScanner

---
+++ dev/tests/unit/testsuite/Magento/Test/Tools/Di/_files/app/code/Magento/SomeModule/ElementFactory.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\SomeModule;
+require_once __DIR__  . '/Element.php';
+class ElementFactory
+{
+    /**
+     * @var \Magento\ObjectManager
+     */
+    protected $_objectManager;
+
+    /**
+     * @param \Magento\ObjectManager $objectManager
+     */
+    public function __construct(\Magento\ObjectManager $objectManager)
+    {
+        $this->_objectManager = $objectManager;
+    }
+
+    /**
+     * @param string $className
+     * @param array $data
+     * @return Magento\SomeModule\Element\Collection
+     */
+    public function create($className, array $data = array())
+    {
+        $instance = $this->_objectManager->create($className, $data);
+        return $instance;
+    }
+}
