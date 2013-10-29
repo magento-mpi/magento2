aea02c896c70d2f1cd103b5dd9be70bed05db037
Ann Beeskau <abeeskau@ebay.com>
2013-10-28 18:06:13 -0700

    Implemented MAGETWO-15715: No page-fragments in the framework - added fragment layout files back - removed label and type="fragment" attributes - put tests back in place

--- dev/tests/integration/testsuite/Magento/Checkout/Controller/OnepageTest.php
+++ dev/tests/integration/testsuite/Magento/Checkout/Controller/OnepageTest.php
@@ -38,6 +38,32 @@ class OnepageTest extends \Magento\TestFramework\TestCase\AbstractController
         $this->assertSelectCount('form[id="co-billing-form"][action=""]', 1, $html);
     }
 
+    /**
+     * Covers app/code/Magento/Checkout/Block/Onepage/Payment/Info.php
+     */
+    public function testProgressAction()
+    {
+        $steps = array(
+            'payment' => array('is_show' => true, 'complete' => true),
+            'billing' => array('is_show' => true),
+            'shipping' => array('is_show' => true),
+            'shipping_method' => array('is_show' => true),
+        );
+        \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Checkout\Model\Session')
+            ->setSteps($steps);
+
+        $this->dispatch('checkout/onepage/progress');
+        $html = $this->getResponse()->getBody();
+        $this->assertContains('Checkout', $html);
+        $methodTitle = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+            ->get('Magento\Checkout\Model\Session')
+            ->getQuote()
+            ->getPayment()
+            ->getMethodInstance()
+            ->getTitle();
+        $this->assertContains('<p>' . $methodTitle . '</p>', $html);
+    }
+
     public function testShippingMethodAction()
     {
         $this->dispatch('checkout/onepage/shippingmethod');
---
+++ dev/tests/integration/testsuite/Magento/Rma/Controller/GuestTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_Rma
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Rma\Controller;
+
+class GuestTest extends \Magento\TestFramework\TestCase\AbstractController
+{
+    /**
+     * @param string $uri
+     * @param string $content
+     * @magentoConfigFixture current_store sales/magento_rma/enabled 1
+     * @magentoDataFixture Magento/Rma/_files/rma.php
+     * @dataProvider isResponseContainDataProvider
+     */
+    public function testIsResponseContain($uri, $content)
+    {
+        /** @var $rma \Magento\Rma\Model\Rma */
+        $rma = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+            ->create('Magento\Rma\Model\Rma');
+        $rma->load(1, 'increment_id');
+
+        $this->getRequest()->setParam('entity_id', $rma->getEntityId());
+        $this->getRequest()->setPost('oar_type', 'email');
+        $this->getRequest()->setPost('oar_order_id', $rma->getOrder()->getIncrementId());
+        $this->getRequest()->setPost('oar_billing_lastname', $rma->getOrder()->getBillingAddress()->getLastname());
+        $this->getRequest()->setPost('oar_email', $rma->getOrder()->getBillingAddress()->getEmail());
+        $this->getRequest()->setPost('oar_zip', '');
+
+        $this->dispatch($uri);
+        $this->assertContains($content, $this->getResponse()->getBody());
+    }
+
+    public function isResponseContainDataProvider()
+    {
+        return array(
+            array('rma/guest/addlabel', '<td>CarrierTitle</td>'),
+            array('rma/guest/dellabel', '<td>CarrierTitle</td>'),
+        );
+    }
+}
