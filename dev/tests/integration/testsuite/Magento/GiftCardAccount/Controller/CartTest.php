63075698660cead581703924f91abffb5e145ae8
Ann Beeskau <abeeskau@ebay.com>
2013-10-29 22:21:26 -0700

    Fixed MAGETWO-15715: No page-fragments in the framework - added CartTest back to test giftcardaccount quickCheckAction fragment

---
+++ dev/tests/integration/testsuite/Magento/GiftCardAccount/Controller/CartTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_GiftCardAccount
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\GiftCardAccount\Controller;
+
+class CartTest extends \Magento\TestFramework\TestCase\AbstractController
+{
+    /**
+     * @magentoDataFixture Magento/GiftCardAccount/_files/giftcardaccount.php
+     */
+    public function testQuickCheckAction()
+    {
+        $this->getRequest()->setParam('giftcard_code', 'giftcardaccount_fixture');
+        $this->dispatch('giftcard/cart/quickCheck');
+        $output = $this->getResponse()->getBody();
+        $this->assertContains('giftcardaccount_fixture', $output);
+        $this->assertContains('$9.99', $output);
+    }
+}
