61b130958c1ec7a234f9bc8e274a1285d4fa6b84
Alexander Paliarush <apaliarush@ebay.com>
2013-10-30 17:53:15 +0200

    Implemented MAGETWO-16409: Create Integration config XSD Schema and Cover It with Test

--- dev/tests/unit/testsuite/Magento/Integration/Model/Config/Api/ConverterTest.php
+++ dev/tests/unit/testsuite/Magento/Integration/Model/Config/Api/ConverterTest.php
@@ -5,10 +5,13 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-namespace \Magento\Integration\Model\Config\Api;
+namespace Magento\Integration\Model\Config\Api;
 
 use \Magento\Integration\Model\Config\Api\Converter;
 
+/**
+ * Test for conversion of integration API XML config into array representation.
+ */
 class ConverterTest extends \PHPUnit_Framework_TestCase
 {
     /**
--- dev/tests/unit/testsuite/Magento/Integration/Model/Config/ConverterTest.php
+++ dev/tests/unit/testsuite/Magento/Integration/Model/Config/ConverterTest.php
@@ -5,10 +5,13 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-namespace \Magento\Integration\Model\Config;
+namespace Magento\Integration\Model\Config;
 
 use \Magento\Integration\Model\Config\Converter;
 
+/**
+ * Test for conversion of integration XML config into array representation.
+ */
 class ConverterTest extends \PHPUnit_Framework_TestCase
 {
     /**
