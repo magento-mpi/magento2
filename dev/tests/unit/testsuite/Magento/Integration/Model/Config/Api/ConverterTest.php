c8e7d812973cdee1b91cd64dfbefb8ff0127b26e
Alexander Paliarush <apaliarush@ebay.com>
2013-10-30 12:31:12 +0200

    Implemented MAGETWO-16497: Implement the config file reader for API configuration - Added Unit tests (marked as incomplete before functionality is ready)

---
+++ dev/tests/unit/testsuite/Magento/Integration/Model/Config/Api/ConverterTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace \Magento\Integration\Model\Config\Api;
+
+use \Magento\Integration\Model\Config\Api\Converter;
+
+class ConverterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Converter
+     */
+    protected $_model;
+
+    public function setUp()
+    {
+        $this->markTestIncomplete('Functionality is not implemented yet');
+        $this->_model = new Converter();
+    }
+
+    public function testConvert()
+    {
+        $inputData = new \DOMDocument();
+        $inputData->load(__DIR__ . '/_files/api.xml');
+        $expectedResult = require __DIR__ . '/_files/api.php';
+        $this->assertEquals($expectedResult, $this->_model->convert($inputData));
+    }
+}
