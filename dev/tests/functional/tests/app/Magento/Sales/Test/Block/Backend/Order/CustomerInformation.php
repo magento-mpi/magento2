3642a02db494bbe5790e5a061000220ec9ce5406
Vladislav Dyachkov <vdyachkov@ebay.com>
2013-11-01 17:37:47 +0200

    Implemented MAGETWO-15703: Fill create order page and place order for simple product  - Fixed wrong locator for work with EE  - implemented KISS principle

--- dev/tests/functional/tests/app/Magento/Sales/Test/Block/Backend/Order/CustomerInformation.php
+++ dev/tests/functional/tests/app/Magento/Sales/Test/Block/Backend/Order/CustomerInformation.php
@@ -18,7 +18,7 @@ class CustomerInformation extends Block
 {
     public function getCustomerEmail()
     {
-        $selector = '//div[@class="order-account-information"]//th[text()="Email"]/following-sibling::*/a';
+        $selector = '//th[text()="Email"]/following-sibling::*/a';
         $email = $this->_rootElement->find($selector, Locator::SELECTOR_XPATH)->getText();
         return $email;
     }
