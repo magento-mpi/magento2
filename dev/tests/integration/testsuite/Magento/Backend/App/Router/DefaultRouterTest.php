89c68af8b7e994e485f8cf599d50b7c67b0c1d3a
Alex Akimov <aakimov@ebay.com>
2013-11-01 13:11:02 +0200

    Implemented MAGETWO-16307: Fix broken integration && api tests - Fixed failures related to Backend module;

--- dev/tests/integration/testsuite/Magento/Backend/App/Router/DefaultRouterTest.php
+++ dev/tests/integration/testsuite/Magento/Backend/App/Router/DefaultRouterTest.php
@@ -21,92 +21,20 @@ class DefaultRouterTest extends \PHPUnit_Framework_TestCase
      */
     protected $_model;
 
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $_frontMock;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $_routeConfigMock;
-
     protected function setUp()
     {
-        parent::setUp();
-
-        $this->_routeConfigMock = $this->getMock('Magento\App\Route\Config', array(), array(), '', false);
-        $options = array(
-            'areaCode'        => \Magento\Core\Model\App\Area::AREA_ADMINHTML,
-            'baseController'  => 'Magento\Backend\Controller\AbstractAction',
-            'routeConfig' => $this->_routeConfigMock
-        );
-        $this->_frontMock = $this->getMock('Magento\App\FrontController', array(), array(), '', false);
-        $this->_model = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
-            ->create('Magento\Backend\App\Router\DefaultRouter', $options);
-        $this->_model->setFront($this->_frontMock);
+        $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $this->_model = $objectManager->create('Magento\Backend\App\Router\DefaultRouter');
     }
 
-    public function testRouterCannotProcessRequestsWithWrongFrontName()
-    {
-        $request = $this->getMock('Magento\App\Request\Http', array(), array(), '', false);
-        $request->expects($this->once())
-            ->method('getPathInfo')
-            ->will($this->returnValue('frontend/admin/dashboard'));
-        $this->_frontMock->expects($this->never())
-            ->method('setDefault');
-        $this->_model->match($request);
-    }
-
-    public function testRouterCanProcessRequestsWithProperFrontName()
+    public function testRouterCanProcessRequestsWithProperPathInfo()
     {
         $request = $this->getMock('Magento\App\Request\Http', array(), array(), '', false);
         $request->expects($this->once())
             ->method('getPathInfo')
             ->will($this->returnValue('backend/admin/dashboard'));
-        $this->_frontMock->expects($this->once())
-            ->method('setDefault');
 
-        $adminRoute = array(
-            'adminhtml' => array(
-                'id'        => 'adminhtml',
-                'frontName' => 'admin',
-                'modules'   => array(
-                    'Magento_Adminhtml'
-                )
-            )
-        );
-
-        $this->_routeConfigMock->expects($this->once())
-            ->method('getRoutes')
-            ->will($this->returnValue($adminRoute));
-        $this->_model->match($request);
-    }
-
-    public function testFetchDefault()
-    {
-        $default = array(
-            'area' => '',
-            'module' => 'admin',
-            'controller' => 'index',
-            'action' => 'index'
-        );
-        $routes = array(
-            'adminhtml' => array(
-                'id' => 'adminhtml',
-                'frontName' => 'admin',
-                'modules' => array()
-            ),
-            'key1' => array('frontName' => 'something'),
-        );
-
-        $this->_routeConfigMock->expects($this->once())->method('getRoutes')
-            ->will($this->returnValue($routes));
-
-        $this->_frontMock->expects($this->once())
-            ->method('setDefault')
-            ->with($this->equalTo($default));
-        $this->_model->fetchDefault();
+        $this->assertInstanceOf('Magento\Adminhtml\Controller\Dashboard', $this->_model->match($request));
     }
 
     /**
