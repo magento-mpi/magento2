8bfb8988dd1444431f7266395d38dcbffa4445a2
Michail Slabko <mslabko@ebay.com>
2013-11-01 19:24:34 +0200

    Implemented MAGETWO-15394: [Test] [GoInc] Automation 'Using advanced search to find the product'

---
+++ dev/tests/functional/tests/app/Magento/Page/Test/Block/Html/Footer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @spi
+ * @category    Mtf
+ * @package     Mtf
+ * @subpackage  functional_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Page\Test\Block\Html;
+
+use Mtf\Block\Block;
+use Mtf\Client\Element\Locator;
+
+/**
+ * Class top menu navigation block
+ *
+ * @package Magento\Page\Test\Block\Html
+ */
+class Footer extends Block
+{
+    protected $linkSelector = '//*[@class="links"]//a[contains(text(), "%s")]';
+
+    /**
+     * Click on link by name
+     *
+     * @param string $linkName
+     * @return \Mtf\Client\Element
+     * @throws \Exception
+     */
+    public function clickLink($linkName)
+    {
+        $link = $this->_rootElement->find(sprintf($this->linkSelector, $linkName), Locator::SELECTOR_XPATH);
+        if (!$link->isVisible()) {
+            throw new \Exception(sprintf('"%s" link is not visible', $linkName));
+        }
+        $link->click();
+    }
+}
