87c1dc508415de0e584816391c5576e3714b9d1d
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2013-11-07 19:41:00 +0200

    Fixed MAGETWO-16366: Stabilize builds
    - fix integration test

--- dev/tests/integration/testsuite/Magento/Image/AdapterFactoryTest.php
+++ dev/tests/integration/testsuite/Magento/Image/AdapterFactoryTest.php
@@ -17,24 +17,23 @@ namespace Magento\Image;
  */
 class AdapterFactoryTest extends \PHPUnit_Framework_TestCase
 {
-    protected function setUp()
-    {
-        $this->_model = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
-            ->create('Magento\Image\AdapterFactory');
-        $this->_config = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
-            ->create('Magento\Core\Model\ConfigInterface');
-    }
-
     /**
      * @magentoAppIsolation enabled
      * @magentoDbIsolation enabled
      */
     public function testCreate()
     {
-        $result = $this->_model->create();
+        /** @var AdapterFactory $model */
+        $model = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+            ->create('Magento\Image\AdapterFactory');
+        /** @var \Magento\Core\Model\ConfigInterface $config */
+        $config = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+            ->create('Magento\Core\Model\ConfigInterface');
+
+        $result = $model->create();
         $this->assertInstanceOf('Magento\Image\Adapter\AbstractAdapter', $result);
         $this->assertNotEmpty(
-            $this->_config->getNode(\Magento\Core\Model\Image\Adapter\Config::XML_PATH_IMAGE_ADAPTER)
+            $config->getValue(\Magento\Core\Model\Image\Adapter\Config::XML_PATH_IMAGE_ADAPTER)
         );
     }
 }
