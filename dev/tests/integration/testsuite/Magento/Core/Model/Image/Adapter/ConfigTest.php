88db6e4d5816773b95c5d5a91702f1ce57a9a443
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2013-11-11 14:04:52 +0200

    Fixed MAGETWO-16990: Error appears on atempt to save settings for Image Adapter
     - fix integration test

--- dev/tests/integration/testsuite/Magento/Core/Model/Image/Adapter/ConfigTest.php
+++ dev/tests/integration/testsuite/Magento/Core/Model/Image/Adapter/ConfigTest.php
@@ -9,12 +9,11 @@ namespace Magento\Core\Model\Image\Adapter;
 
 class ConfigTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testGetAdapterName()
     {
         /** @var Config $config */
         $config = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
             ->create('Magento\Core\Model\Image\Adapter\Config');
-        $this->assertEquals('Magento\Image\Adapter\AdapterInterface', $config->getAdapterAlias());
+        $this->assertEquals(\Magento\Image\Adapter\AdapterInterface::ADAPTER_GD2, $config->getAdapterAlias());
     }
 }
