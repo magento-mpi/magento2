a9e9b937028a772acf7145b23e68aa717f6b3572
Kevin Fries <kfries@ebay.com>
2013-11-14 10:06:06 -0800

    Implemented MAGETWO-15966: [Test] [PolarSeals] Automate - Product Upselling  - test class, building pre-requesites.

--- dev/tests/functional/tests/app/Magento/Catalog/Test/TestCase/Product/UpsellTest.php
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/TestCase/Product/UpsellTest.php
@@ -18,7 +18,13 @@ use Magento\Catalog\Test\Fixture\ConfigurableProduct;
 
 class UpsellTest extends Functional {
 
-
+    /**
+     * Login into backend area before test
+     */
+    protected function setUp()
+    {
+        Factory::getApp()->magentoBackendLoginUser();
+    }
 
     protected function createConfigurable() {
         //Data
@@ -72,13 +78,33 @@ class UpsellTest extends Functional {
         $configurableFixture->persist();
 
 
-
-        $this->markTestIncomplete('MAGETWO-15966');
+        // Test Steps
         //Page
         // For Simple 1 add as up-sells:- Configurable 1 & Simple 2
+        $productGridPage = Factory::getPageFactory()->getCatalogProductIndex();
+        $productGridPage->open();
+        $gridBlock = $productGridPage->getProductGrid();
 
+        $productGridPage->getProductGrid()->searchAndOpen(array('sku' => $product1Fixture->getProductSku()));
+        //$productGridPage->editProduct(array('id' => $product1Fixture->getProductId()));
+
+/*        $product = Factory::getFixtureFactory()->getMagentoCatalogProduct();
+        $product->switchData('simple');
+        //Data
+        $createProductPage = Factory::getPageFactory()->getCatalogProductNew();
+        $createProductPage->init($product);
+        $productBlockForm = $createProductPage->getProductBlockForm();
+        //Steps
+        $createProductPage->open();
+        $productBlockForm->fill($product);
+        $productBlockForm->save($product);
+        //Verifying
+        $createProductPage->getMessagesBlock()->assertSuccessMessage();
+*/
         //
         // For Configurable 1 add as up-sells: Simple 2
+
+        $this->markTestIncomplete('MAGETWO-15966');
     }
 
 }
