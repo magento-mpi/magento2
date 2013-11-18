0ebf5962497495c042d603a33648747e39182db0
Susan Wright <swright@ebay.com>
2013-11-18 12:53:53 -0800

    Implemented MAGETWO-15967: [Test] [PolarSeals] Automate - Promoting products as related
    - Set the parameter to indicate to 'new' state (popup=1).

--- dev/tests/functional/tests/app/Magento/Catalog/Test/Handler/Curl/CreateConfigurable.php
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/Handler/Curl/CreateConfigurable.php
@@ -155,7 +155,7 @@ class CreateConfigurable extends Curl
     {
         $url = $_ENV['app_backend_url']
             . 'catalog/product/save/'
-            . $fixture->getUrlParams('create_url_params');
+            . $fixture->getUrlParams('create_url_params') . '/popup/1/';
         $params = $this->_prepareData($fixture);
         $curl = new BackendDecorator(new CurlTransport(), new Config());
         $curl->write(CurlInterface::POST, $url, '1.0', array(), $params);
