7842059b34e33b963419fe9a5e1158e3eccd4ef1
Mark Gee <mgee@ebay.com>
2013-11-19 14:26:06 -0800

    MAGETWO-15962: [Test] [UXMen] Automate - New Customer Segment creation for specific Customer Group
    - added support to create a customer that has a group value of 'retailer'

--- dev/tests/functional/tests/app/Magento/Customer/Test/Repository/Customer.php
+++ dev/tests/functional/tests/app/Magento/Customer/Test/Repository/Customer.php
@@ -32,8 +32,8 @@ class Customer extends AbstractRepository
         );
 
         $this->_data['customer_US_1'] = $this->_getUS1();
-        $this->_data['backend_customer'] = $this->_getBackendCustomer();
-        $this->_data['backend_retailer_customer'] = $this->_getRetailerCustomer();
+        $this->_data['backend_customer'] = $this->_getBackendCustomer(array('value' => 'General', 'input_value' => '1'));
+        $this->_data['backend_retailer_customer'] = $this->_getBackendCustomer(array('value' => 'Retailer', 'input_value' => '3'));
     }
 
     protected function _getUS1()
@@ -64,7 +64,7 @@ class Customer extends AbstractRepository
         );
     }
 
-    protected function _getBackendCustomer()
+    protected function _getBackendCustomer($groupType)
     {
         return array(
             'data' => array(
@@ -88,45 +88,10 @@ class Customer extends AbstractRepository
                         'input_value' => '1'
                     ),
                     'group_id' => array(
-                        'value' => 'General',
+                        'value' => $groupType['value'],
                         'group' => 'customer_info_tabs_account',
                         'input' => 'select',
-                        'input_value' => '1'
-                    )
-                ),
-                'addresses' => array()
-            )
-        );
-    }
-
-    protected function _getRetailerCustomer()
-    {
-        return array(
-            'data' => array(
-                'fields' => array(
-                    'firstname' => array(
-                        'value' => 'Jane',
-                        'group' => 'customer_info_tabs_account'
-                    ),
-                    'lastname' => array(
-                        'value' => 'Doe',
-                        'group' => 'customer_info_tabs_account'
-                    ),
-                    'email' => array(
-                        'value' => 'Jane.Doe%isolation%@example.com',
-                        'group' => 'customer_info_tabs_account'
-                    ),
-                    'website_id' => array(
-                        'value' => 'Main Website',
-                        'group' => 'customer_info_tabs_account',
-                        'input' => 'select',
-                        'input_value' => '1'
-                    ),
-                    'group_id' => array(
-                        'value' => 'Retailer',
-                        'group' => 'customer_info_tabs_account',
-                        'input' => 'select',
-                        'input_value' => '3'
+                        'input_value' => $groupType['input_value']
                     )
                 ),
                 'addresses' => array()
