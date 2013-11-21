b81c44c520385da9a7113e3b19a0e59f8f35e593
Mark Gee <mgee@ebay.com>
2013-11-21 14:22:23 -0800

    MAGETWO-15963: [Test] [UXMen] Automate - Using customer segmentation to apply shopping cart promotion
    - add cURL support for adding a customer segment with a condition

--- dev/tests/functional/tests/app/Magento/CustomerSegment/Test/Repository/Conditions.php
+++ dev/tests/functional/tests/app/Magento/CustomerSegment/Test/Repository/Conditions.php
@@ -31,6 +31,7 @@ class Conditions extends AbstractRepository {
         );
 
         $this->_data['retailer_condition'] = $this->_getRetailerCondition();
+        $this->_data['retailer_condition_curl'] = $this->_getRetailerConditionCurl();
     }
 
     protected function _getRetailerCondition()
@@ -49,6 +50,86 @@ class Conditions extends AbstractRepository {
                         'group' => 'magento_customersegment_segment_tabs_conditions_section',
                         'input' => 'select',
                         'input_value' => '3'
+                    )
+                ),
+            )
+        );
+    }
+
+    protected function _getRetailerConditionCurl()
+    {
+        return array(
+            'data' => array(
+                'fields' => array(
+                    'segment_id' => array(
+                        'value' => '1',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section'
+                    ),
+                    'name' => array(
+                        'value' => 'All Retail Customers',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'text'
+                    ),
+                    'description' => array(
+                        'value' => 'Customer Segment test for retailer customers',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section'
+                    ),
+                    'website_ids[]' => array(
+                        'value' => 'Main Website',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'select',
+                        'input_value' => '1'
+                    ),
+                    'is_active' => array(
+                        'value' => 'Active',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'select',
+                        'input_value' => '1'
+                    ),
+                    'rule[conditions][1][type]' => array(
+                        'value' => 'Magento\CustomerSegment\Model\Segment\Condition\Combine\Root',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'hidden'
+                    ),
+                    'rule[conditions][1][aggregator]' => array(
+                        'value' => 'ANY',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'select',
+                        'input_value' => 'any'
+                    ),
+                    'rule[conditions][1][value]' => array(
+                        'value' => 'TRUE',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'select',
+                        'input_value' => '1'
+                    ),
+                    'rule[conditions][1--1][type]' => array(
+                        'value' => 'Magento\CustomerSegment\Model\Segment\Condition\Customer\Attributes',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'hidden'
+                    ),
+                    'rule[conditions][1--1][attribute]' => array(
+                        'value' => 'group_id',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'hidden'
+                    ),
+                    'rule[conditions][1--1][operator]' => array(
+                        'value' => 'is',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'select',
+                        'input_value' => '=='
+                    ),
+                    'rule[conditions][1--1][value]' => array(
+                        'value' => 'Retailer',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'select',
+                        'input_value' => '3'
+                    ),
+                    'rule[conditions][1][new_child]' => array(
+                        'value' => 'Group',
+                        'group' => 'magento_customersegment_segment_tabs_conditions_section',
+                        'input' => 'select',
+                        'input_value' => 'Magento\CustomerSegment\Model\Segment\Condition\Customer\Attributes|group_id'
                     )
                 ),
             )
