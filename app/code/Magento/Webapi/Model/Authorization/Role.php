3d3d58eeeff57e4eb4f3fcd07084977d8bd407f0
Alexander Paliarush <apaliarush@ebay.com>
2013-11-21 16:06:09 +0200

    Implemented MAGETWO-17384: Make Authz Module Ready to be Pushed to Mainline - Reverted Removal of ACL-related logic from Webapi module and related tests (due to existing dependencies from Webhook module)

---
+++ app/code/Magento/Webapi/Model/Authorization/Role.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * API ACL Role
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Webapi\Model\Authorization;
+
+class Role extends \Zend_Acl_Role
+{
+
+}
---
+++ dev/tests/integration/testsuite/Magento/Webapi/_files/role.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_Webapi
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/** @var \Magento\Webapi\Model\Acl\Role $role */
+$role = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+    ->create('Magento\Webapi\Model\Acl\Role');
+$role->setRoleName('test_role')->save();
