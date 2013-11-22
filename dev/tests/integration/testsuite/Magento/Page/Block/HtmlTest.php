26d826f91ce74a6ca6a37847d6176f1b26e6862d
Maxim Shikula <mshikula@ebay.com>
2013-11-22 11:56:45 +0200

    Implemented MAGETWO-17024: Move FormKey logic from Session

--- dev/tests/integration/testsuite/Magento/Page/Block/HtmlTest.php
+++ dev/tests/integration/testsuite/Magento/Page/Block/HtmlTest.php
@@ -39,6 +39,7 @@ class HtmlTest extends \PHPUnit_Framework_TestCase
                 $this->getMock('Magento\Core\Model\App', array(), array(), '', false),
                 $this->getMock('Magento\Core\Model\StoreManager', array(), array(), '', false),
                 $this->getMock('Magento\Core\Model\Session', array(), array(), '', false),
+                $this->getMock('Magento\Session\SidResolverInterface', array(), array(), '', false),
                 null,
                 array()
             )
