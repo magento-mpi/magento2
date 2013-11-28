23220145da91e52ce351a42d13abebb48ebc66a3
Lawrence Chiu <lachiu@ebay.com>
2013-11-27 16:34:00 -0800

    Implemented MAGETWO-15969: [Test] [PolarSeals] Automate - Returning items using return merchandise authorization -Partial implementation of precondition 2.

--- dev/tests/functional/tests/app/Magento/Rma/Test/TestCase/RmaTest.php
+++ dev/tests/functional/tests/app/Magento/Rma/Test/TestCase/RmaTest.php
@@ -33,11 +33,26 @@ Class RmaTest extends Functional
      */
     public function testRma()
     {
-
         // precondition 1: Configure RMA Settings
         $enableRma = Factory::getFixtureFactory()->getMagentoCoreConfig();
         $enableRma->switchData('enable_rma');
         $enableRma->persist();
 
+        // precondition 2a: MAGETWO-12415 - Guest Checkout using "Checkout with PayPal":
+        $payPalExpressOrder = Factory::getFixtureFactory()->getMagentoSalesPaypalExpressOrder();
+        $payPalExpressOrder->persist();
+
+        // precondition 2b: MAGETWO-12434 - Closing a Sales Order paid with PayPal Express Checkout
+        $orderId = $payPalExpressOrder->getOrderId();
+        //Pages
+        $orderPage = Factory::getPageFactory()->getSalesOrder();
+        $newInvoicePage = Factory::getPageFactory()->getSalesOrderInvoiceNew();
+        $newShipmentPage = Factory::getPageFactory()->getSalesOrderShipmentNew();
+        Factory::getApp()->magentoBackendLoginUser();
+        $orderPage->open();
+        $orderPage->getOrderGridBlock()->searchAndOpen(array('id' => $orderId));
+        $orderPage->getOrderActionsBlock()->clickShipButton();
+
+        $newShipmentPage->getTotalsBlock()->submit();
     }
 }
