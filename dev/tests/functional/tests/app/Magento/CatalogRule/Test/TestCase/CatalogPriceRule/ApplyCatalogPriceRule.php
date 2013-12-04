7e052528c3b826145c1733cf10dbb4292ed5eb1f
Jay Rutten <jrutten@ebay.com>
2013-12-04 12:42:10 -0800

    MAGETWO-17318: Verify Price Rule Applied on Category Page
    - Added start of verifying data on frontend.

--- dev/tests/functional/tests/app/Magento/CatalogRule/Test/TestCase/CatalogPriceRule/ApplyCatalogPriceRule.php
+++ dev/tests/functional/tests/app/Magento/CatalogRule/Test/TestCase/CatalogPriceRule/ApplyCatalogPriceRule.php
@@ -11,6 +11,7 @@
 
 namespace Magento\CatalogRule\Test\TestCase\CatalogPriceRule;
 
+use Magento\Catalog\Test\Fixture;
 use Magento\Catalog\Test\Repository\Product;
 use Mtf\Factory\Factory;
 use Mtf\TestCase\Functional;
@@ -61,6 +62,7 @@ class ApplyCatalogPriceRule extends Functional
         $banner->persist();
 
         // Verify applied catalog price rules
+        $this->verifyPriceRules($simple);
     }
 
     /**
@@ -111,5 +113,21 @@ class ApplyCatalogPriceRule extends Functional
 
         // Return Catalog Price Rule Id
         return $catalogPriceRuleId;
+    }
+
+    /**
+     * This method verifies information on the storefront.
+     * @param Fixture\Product $product
+     */
+    protected function verifyPriceRules(Fixture\Product $product) {
+        // open the front end home page of the store
+        $frontendHomePage = Factory::getPageFactory()->getCmsIndexIndex();
+        $frontendHomePage->open();
+        // open the category associated with the product
+        $frontendHomePage->getTopmenu()->selectCategoryByName($product->getCategoryName());
+        // verify the product is displayed in the category
+        $categoryPage = Factory::getPageFactory()->getCatalogCategoryView();
+        $productListBlock = $categoryPage->getListProductBlock();
+        $this->assertTrue($productListBlock->isProductVisible($product->getProductName()));
     }
 }
