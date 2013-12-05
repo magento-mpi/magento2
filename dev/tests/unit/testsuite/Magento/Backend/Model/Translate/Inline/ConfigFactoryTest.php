4b3350e8ec4d1a3d7a54ad9d25b0832c62d2fb26
Dmitriy Volik <dvolik@ebay.com>
2013-12-05 18:58:45 +0200

    Fixed MAGETWO-17581: Investigate and review interfaces added during implementation of MAGETWO-15883 - fixed backend configFactory

--- dev/tests/unit/testsuite/Magento/Backend/Model/Translate/Inline/ConfigFactoryTest.php
+++ dev/tests/unit/testsuite/Magento/Backend/Model/Translate/Inline/ConfigFactoryTest.php
@@ -11,11 +11,10 @@ class ConfigFactoryTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @param string|null $area
-     * @param null|string $appStateArea
      * @param string $expectedName
      * @dataProvider createDataProvider
      */
-    public function testCreate($area, $appStateArea, $expectedName)
+    public function testCreate($area, $expectedName)
     {
         $result = 'result';
         $objectManager = $this->getMockForAbstractClass('Magento\ObjectManager');
@@ -24,42 +23,19 @@ class ConfigFactoryTest extends \PHPUnit_Framework_TestCase
             ->method('create')
             ->with($this->equalTo($expectedName))
             ->will($this->returnValue($result));
-        $factory = new ConfigFactory($objectManager, $this->getAppState($appStateArea));
+        $factory = new ConfigFactory($objectManager);
         $this->assertEquals($result, $factory->create($area));
     }
 
     public function createDataProvider()
     {
         return array(
-            array(null, 'some area', 'Magento\Core\Model\Translate\Inline\Config'),
-            array(
-                null,
-                \Magento\Backend\App\Area\FrontNameResolver::AREA_CODE,
-                'Magento\Backend\Model\Translate\Inline\Config'
-            ),
-            array('some area', null, 'Magento\Core\Model\Translate\Inline\Config'),
+            array(null, 'Magento\Backend\Model\Translate\Inline\Config'),
+            array('some area', 'Magento\Backend\Model\Translate\Inline\Config'),
             array(
                 \Magento\Backend\App\Area\FrontNameResolver::AREA_CODE,
-                null,
                 'Magento\Backend\Model\Translate\Inline\Config'
-            ),
+            )
         );
-    }
-
-    /**
-     * Get mock of app state
-     *
-     * @param null|string $area
-     * @return \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function getAppState($area)
-    {
-        $appState = $this->getMock('Magento\App\State', array('getAreaCode'), array(), '', false);
-        if (isset($area)) {
-            $appState->expects($this->once())->method('getAreaCode')->will($this->returnValue($area));
-        } else {
-            $appState->expects($this->never())->method('getAreaCode');
-        }
-        return $appState;
     }
 }
