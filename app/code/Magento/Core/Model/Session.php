17825ecea11155df575a5539afa5d65bd132d31e
Andrey Voinkov <avoinkov@ebay.com>
2013-12-06 15:27:23 +0200

    Implemented MAGETWO-17671: Implement Session Save Handler: Cover with tests - removed duplicate code

--- app/code/Magento/Core/Model/Session.php
+++ app/code/Magento/Core/Model/Session.php
@@ -17,27 +17,6 @@ namespace Magento\Core\Model;
  * @method \Magento\Core\Model\Session setCookieShouldBeReceived(bool $flag)
  * @method \Magento\Core\Model\Session unsCookieShouldBeReceived()
  */
-class Session extends \Magento\Session\SessionManager
+class Session extends \Magento\Core\Model\Session\Generic
 {
-    /**
-     * @param \Magento\App\RequestInterface $request
-     * @param \Magento\Session\SidResolverInterface $sidResolver
-     * @param \Magento\Session\Config\ConfigInterface $sessionConfig
-     * @param \Magento\Session\SaveHandlerInterface $saveHandler
-     * @param \Magento\Session\ValidatorInterface $validator
-     * @param \Magento\Session\StorageInterface $storage
-     * @param null|string $sessionName
-     */
-    public function __construct(
-        \Magento\App\RequestInterface $request,
-        \Magento\Session\SidResolverInterface $sidResolver,
-        \Magento\Session\Config\ConfigInterface $sessionConfig,
-        \Magento\Session\SaveHandlerInterface $saveHandler,
-        \Magento\Session\ValidatorInterface $validator,
-        \Magento\Session\StorageInterface $storage,
-        $sessionName = null
-    ) {
-        parent::__construct($request, $sidResolver, $sessionConfig, $saveHandler, $validator, $storage);
-        $this->start($sessionName);
-    }
 }
