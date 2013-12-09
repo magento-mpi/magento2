f5b6769f193c485bd5091ededc9dee7b206693c7
Sergey Kovalev <skovalev@ebay.com>
2013-12-09 12:19:42 +0200

    Implemented MAGETWO-17001: Move session handlers into library - generic session namespaces fixed

--- lib/Magento/Session/Generic.php
+++ lib/Magento/Session/Generic.php
@@ -7,24 +7,24 @@
  */
 namespace Magento\Session;
 
-class Generic extends \Magento\Session\SessionManager
+class Generic extends SessionManager
 {
     /**
      * @param \Magento\App\RequestInterface $request
-     * @param \Magento\Session\SidResolverInterface $sidResolver
+     * @param SidResolverInterface $sidResolver
      * @param \Magento\Session\Config\ConfigInterface $sessionConfig
-     * @param \Magento\Session\SaveHandlerInterface $saveHandler
-     * @param \Magento\Session\ValidatorInterface $validator
-     * @param \Magento\Session\StorageInterface $storage
+     * @param SaveHandlerInterface $saveHandler
+     * @param ValidatorInterface $validator
+     * @param StorageInterface $storage
      * @param null $sessionName
      */
     public function __construct(
         \Magento\App\RequestInterface $request,
-        \Magento\Session\SidResolverInterface $sidResolver,
+        SidResolverInterface $sidResolver,
         \Magento\Session\Config\ConfigInterface $sessionConfig,
-        \Magento\Session\SaveHandlerInterface $saveHandler,
-        \Magento\Session\ValidatorInterface $validator,
-        \Magento\Session\StorageInterface $storage,
+        SaveHandlerInterface $saveHandler,
+        ValidatorInterface $validator,
+        StorageInterface $storage,
         $sessionName = null
     ) {
         parent::__construct($request, $sidResolver, $sessionConfig, $saveHandler, $validator, $storage);
