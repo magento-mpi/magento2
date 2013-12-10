e1ec44c51c8696946fda5201fd2a84f19bceccb7
Jeff Farley <jfarley@ebay.com>
2013-12-10 09:30:04 -0800

    MAGETWO-15965: [Test] [UXMen] Automate - Applying Catalog Price Rules to specific customer group
    - Code review changes and ran the code formatter.

--- dev/tests/functional/tests/app/Magento/CatalogRule/Test/Repository/CatalogPriceRule.php
+++ dev/tests/functional/tests/app/Magento/CatalogRule/Test/Repository/CatalogPriceRule.php
@@ -8,7 +8,6 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-
 namespace Magento\CatalogRule\Test\Repository;
 
 use Mtf\Factory\Factory;
@@ -21,22 +20,22 @@ use Mtf\Repository\AbstractRepository;
  */
 class CatalogPriceRule extends AbstractRepository
 {
-    /**
-     * Groups for mapping data into ui tabs
-     */
+    const CATALOG_PRICE_RULE = 'catalog_price_rule';
+
+    const CUSTOMER_GROUP_GENERAL = 'customer_group_catalog_rule';
+
     const GROUP_RULE_INFORMATION = 'promo_catalog_edit_tabs_main_section';
+
     const GROUP_CONDITIONS = 'promo_catalog_edit_tabs_conditions_section';
+
     const GROUP_ACTIONS = 'promo_catalog_edit_tabs_actions_section';
 
     public function __construct(array $defaultConfig, array $defaultData)
     {
-        $this->_data['default'] = array(
-            'config' => $defaultConfig,
-            'data' => $defaultData
-        );
+        $this->_data['default'] = array('config' => $defaultConfig, 'data' => $defaultData);
 
-        $this->_data['catalog_price_rule'] = $this->_getCatalogPriceRule();
-        $this->_data['customer_group_catalog_rule'] = $this->_getCustomerGroupCatalogRule();
+        $this->_data[self::CATALOG_PRICE_RULE] = $this->_getCatalogPriceRule();
+        $this->_data[self::CUSTOMER_GROUP_GENERAL] = $this->_getCustomerGroupCatalogRule();
     }
 
     protected function _getCatalogPriceRule()
@@ -44,36 +43,30 @@ class CatalogPriceRule extends AbstractRepository
         return array(
             'data' => array(
                 'fields' => array(
-                    'rule_name' => array(
-                        'value' => 'rule1',
-                        'group' => static::GROUP_RULE_INFORMATION
-                    ),
+                    'rule_name' => array('value' => 'rule1', 'group' => static::GROUP_RULE_INFORMATION),
                     'rule_is_active' => array(
                         'value' => 'Active',
                         'group' => static::GROUP_RULE_INFORMATION,
                         'input' => 'select'
                     ),
                     'rule_website_ids' => array(
-                        'value' => ['Main Website'],
+                        'value' => array('Main Website'),
                         'group' => static::GROUP_RULE_INFORMATION,
                         'input' => 'multiselect',
-                        'input_value' => ['1']
+                        'input_value' => array('1')
                     ),
                     'rule_customer_group_ids' => array(
-                        'value' => ['NOT LOGGED IN', 'General', 'Wholesale', 'Retailer'],
+                        'value' => array('NOT LOGGED IN', 'General', 'Wholesale', 'Retailer'),
                         'group' => static::GROUP_RULE_INFORMATION,
                         'input' => 'multiselect',
-                        'input_value' => ['0', '1', '2', '3']
+                        'input_value' => array('0', '1', '2', '3')
                     ),
                     'rule_simple_action' => array(
                         'value' => 'By Percentage of the Original Price',
                         'group' => static::GROUP_ACTIONS,
                         'input' => 'select'
                     ),
-                    'rule_discount_amount' => array(
-                        'value' => '50.0000',
-                        'group' => static::GROUP_ACTIONS,
-                    ),
+                    'rule_discount_amount' => array('value' => '50.0000', 'group' => static::GROUP_ACTIONS),
                     'conditions__1__new_child' => array(
                         'value' => 'Category',
                         'group' => static::GROUP_CONDITIONS,
@@ -95,36 +88,30 @@ class CatalogPriceRule extends AbstractRepository
         return array(
             'data' => array(
                 'fields' => array(
-                    'rule_name' => array(
-                        'value' => 'Customer Group Rule',
-                        'group' => static::GROUP_RULE_INFORMATION
-                    ),
+                    'rule_name' => array('value' => 'Customer Group Rule', 'group' => static::GROUP_RULE_INFORMATION),
                     'rule_is_active' => array(
                         'value' => 'Active',
                         'group' => static::GROUP_RULE_INFORMATION,
                         'input' => 'select'
                     ),
                     'rule_website_ids' => array(
-                        'value' => ['Main Website'],
+                        'value' => array('Main Website'),
                         'group' => static::GROUP_RULE_INFORMATION,
                         'input' => 'multiselect',
-                        'input_value' => ['1']
+                        'input_value' => array('1')
                     ),
                     'rule_customer_group_ids' => array(
-                        'value' => ['General'],
+                        'value' => array('General'),
                         'group' => static::GROUP_RULE_INFORMATION,
                         'input' => 'multiselect',
-                        'input_value' => ['1']
+                        'input_value' => array('1')
                     ),
                     'rule_simple_action' => array(
                         'value' => 'By Percentage of the Original Price',
                         'group' => static::GROUP_ACTIONS,
                         'input' => 'select'
                     ),
-                    'rule_discount_amount' => array(
-                        'value' => '50.0000',
-                        'group' => static::GROUP_ACTIONS,
-                    ),
+                    'rule_discount_amount' => array('value' => '50.0000', 'group' => static::GROUP_ACTIONS),
                     'conditions__1__new_child' => array(
                         'value' => 'Category',
                         'group' => static::GROUP_CONDITIONS,
