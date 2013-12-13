bb71a20981e9ada0ec094f0d631a1735250caf60
Olexii Korshenko <okorshenko@ebay.com>
2013-12-13 14:09:56 +0200

    Implemented MAGETWO-17946: Elimination of \Magento\App\Helper\HelperFactory - fixed unit tests

--- app/code/Magento/DesignEditor/Block/Adminhtml/Editor/Tools/Files/Content.php
+++ app/code/Magento/DesignEditor/Block/Adminhtml/Editor/Tools/Files/Content.php
@@ -19,33 +19,6 @@ class Content
     extends \Magento\Theme\Block\Adminhtml\Wysiwyg\Files\Content
 {
     /**
-     * @var \Magento\Theme\Helper\Storage
-     */
-    protected $_storageHelper;
-
-    /**
-     * @var \Magento\Core\Helper\Data
-     */
-    protected $_coreHelper;
-
-    /**
-     * @param \Magento\Backend\Block\Template\Context $context
-     * @param \Magento\Theme\Helper\Storage $storageHelper
-     * @param \Magento\Core\Helper\Data $coreHelper
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Backend\Block\Template\Context $context,
-        \Magento\Theme\Helper\Storage $storageHelper,
-        \Magento\Core\Helper\Data $coreHelper,
-        array $data = array()
-    ) {
-        $this->_coreHelper = $coreHelper;
-        $this->_storageHelper = $storageHelper;
-        parent::__construct($context, $storageHelper, $coreHelper, $data);
-    }
-
-    /**
      * Get header text
      *
      * @return string
