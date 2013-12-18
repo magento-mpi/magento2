d557cebb854ae78bbb28ac416e2b26fc96c48e7b
Dmytro Aponasenko <daponasenko@ebay.com>
2013-12-18 16:15:51 +0200

    CR Changes MAGETWO-17801: [MTF] Hard-coded value in Category fixture leads to fail

--- dev/tests/functional/lib/Mtf/Client/Driver/Selenium/Element/TreeElement.php
+++ dev/tests/functional/lib/Mtf/Client/Driver/Selenium/Element/TreeElement.php
@@ -115,7 +115,7 @@ class TreeElement extends Element
     protected function deep($pathChunk, $structureChunk)
     {
         foreach ($structureChunk as $structureNode) {
-            if (isset($structureNode) && strstr($structureNode['name'], $pathChunk)) {
+            if (isset($structureNode) && preg_match('/' . $pathChunk . ' \(\d+\)/', $structureNode['name'])) {
                 return $structureNode;
             }
         }
