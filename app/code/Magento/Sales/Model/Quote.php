0cc72a61332efab1185c54b08f0bc6e8b5bd1d75
Dmitriy Volik <dvolik@ebay.com>
2013-12-20 15:47:48 +0200

    Fixed MAGETWO-17223: Default billing and shipping address not honored through backend

--- app/code/Magento/Sales/Model/Quote.php
+++ app/code/Magento/Sales/Model/Quote.php
@@ -544,6 +544,7 @@ class Quote extends \Magento\Core\Model\AbstractModel
         if ($customer->getId()) {
             $this->setCustomer($customer);
 
+            $customerBillingAddressId = null;
             if (null !== $billingAddress) {
                 $this->setBillingAddress($billingAddress);
             } else {
@@ -552,17 +553,23 @@ class Quote extends \Magento\Core\Model\AbstractModel
                     $billingAddress = $this->_quoteAddressFactory->create()
                         ->importCustomerAddress($defaultBillingAddress);
                     $this->setBillingAddress($billingAddress);
+                    $customerBillingAddressId = $defaultBillingAddress->getId();
                 }
             }
 
+            $customerShippingAddressId = null;
             if (null === $shippingAddress) {
                 $defaultShippingAddress = $customer->getDefaultShippingAddress();
                 if ($defaultShippingAddress && $defaultShippingAddress->getId()) {
                     $shippingAddress = $this->_quoteAddressFactory->create()
                         ->importCustomerAddress($defaultShippingAddress);
+                    $customerShippingAddressId = $defaultShippingAddress->getId();
                 } else {
                     $shippingAddress = $this->_quoteAddressFactory->create();
                 }
+            }
+            if ($customerBillingAddressId !== null && $customerBillingAddressId != $customerShippingAddressId) {
+                $shippingAddress->setSameAsBilling(false);
             }
             $this->setShippingAddress($shippingAddress);
         }
