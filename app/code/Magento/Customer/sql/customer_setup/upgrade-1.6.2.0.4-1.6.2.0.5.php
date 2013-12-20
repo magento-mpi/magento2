4ba6a6e25cc98a1512aac34d8fb9939ef3bc4696
Stanislav Lopukhov <slopukhov@ebay.com>
2013-12-20 12:43:18 +0200

    Implemented MAGETWO-18452: CLONE - [Support] Possibility to create customer with duplicated email - updated

--- app/code/Magento/Customer/sql/customer_setup/upgrade-1.6.2.0.4-1.6.2.0.5.php
+++ app/code/Magento/Customer/sql/customer_setup/upgrade-1.6.2.0.4-1.6.2.0.5.php
@@ -12,44 +12,6 @@
 $installer = $this;
 $connection = $installer->getConnection();
 
-//get all duplicated emails
-$select  = $connection->select()
-    ->from($installer->getTable('customer_entity'), array('email', 'website_id', 'cnt' => 'COUNT(*)'))
-    ->group('email')
-    ->group('website_id')
-    ->having('cnt > 1');
-$emails = $connection->fetchAll($select);
-
-foreach ($emails as $data) {
-    $email = $data['email'];
-    $websiteId = $data['website_id'];
-
-    $select = $connection->select()
-        ->from($installer->getTable('customer_entity'), array('entity_id'))
-        ->where('email = ?', $email)
-        ->where('website_id = ?', $websiteId);
-    $activeId = $connection->fetchOne($select);
-
-    //receive all other duplicated customer ids
-    $select = $connection->select()
-        ->from($installer->getTable('customer_entity'), array('entity_id', 'email'))
-        ->where('email = ?', $email)
-        ->where('website_id = ?', $websiteId)
-        ->where('entity_id <> ?', $activeId);
-
-    $result = $connection->fetchAll($select);
-
-    //change email to unique value
-    foreach ($result as $row) {
-        $changedEmail = $connection->getConcatSql(array('"(duplicate"', $row['entity_id'], '")"', '"' . $row['email'] . '"'));
-        $connection->update(
-            $installer->getTable('customer_entity'),
-            array('email' => $changedEmail),
-            array('entity_id =?' => $row['entity_id'])
-        );
-    }
-}
-
 /**
  * Add unique index for customer_entity table
  */
