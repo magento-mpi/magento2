43bf049f55b0446bd0f598f2fe609fee8cc52799
Andrey Tserkus <atserkus@ebay.com>
2013-12-20 17:37:33 -0800

    CR Fixes MAGETWO-18576: Redirect on dashboard if click "Search", "Reset Filter" buttons on Search Terms page - added missing license placeholders

--- dev/tests/integration/testsuite/Magento/Search/Controller/Adminhtml/_files/search_term_laptop.php
+++ dev/tests/integration/testsuite/Magento/Search/Controller/Adminhtml/_files/search_term_laptop.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
 
 $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 
