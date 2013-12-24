2efded2ff4810f0c5df039eaefadbfb5da0cdd9a
Andriy Nasinnyk <anasinnyk@ebay.com>
2013-12-24 16:49:04 +0200

    Fixed MAGETWO-18675: View Files Population Tool does not Work
    - added static test for view generator tool

---
+++ dev/tests/static/testsuite/Magento/Test/Integrity/View/GeneratorTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Test\Integrity\View;
+
+class GeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Shell
+     */
+    protected $shell;
+
+    /**
+     * @var \Magento\Filesystem\Driver\File
+     */
+    protected $filesystem;
+
+    /**
+     * Temporary destination directory
+     *
+     * @var string
+     */
+    protected $tmpDir;
+
+    protected function setUp()
+    {
+        $this->tmpDir = BP . '/var/static';
+        $this->shell = new \Magento\Shell();
+        $this->filesystem = new \Magento\Filesystem\Driver\File();
+        $this->filesystem->createDirectory($this->tmpDir, 0777);
+    }
+
+    protected function tearDown()
+    {
+        if ($this->filesystem->isExists($this->tmpDir)) {
+            $this->filesystem->deleteDirectory($this->tmpDir);
+        }
+    }
+
+    /**
+     * Test view generator
+     */
+    public function testViewGenerator()
+    {
+        try {
+            $result = $this->shell->execute(
+                'php -f %s -- --source %s --destination %s',
+                array(BP . '/dev/tools/Magento/Tools/View/generator.php', BP . '/app/design', $this->tmpDir)
+            );
+            $this->assertContains('Completed successfully', $result);
+        } catch (\Magento\Exception $exception) {
+            $this->fail($exception->getPrevious()->getMessage());
+        }
+    }
+}
