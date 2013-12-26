7c68ecb0a9f0f2eb0d1940ec2738c9f714bb8181
Vlad Veselov <vveselov@ebay.com>
2013-12-26 09:26:00 -0500

    CR Changes MAGETWO-18590: Fatal error on attempt to save integration with duplicate name

--- dev/tests/unit/testsuite/Magento/Integration/Controller/Adminhtml/IntegrationTest.php
+++ dev/tests/unit/testsuite/Magento/Integration/Controller/Adminhtml/IntegrationTest.php
@@ -310,6 +310,31 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         $integrationContr->saveAction();
     }
 
+    public function testSaveActionExceptionDuringServiceCreation()
+    {
+        $exceptionMessage = 'Service could not be saved.';
+        $integration = $this->_getSampleIntegrationData();
+        // No id when New Integration is Post-ed
+        $integration->unsetData(array(IntegrationModel::ID, 'id'));
+        $this->_requestMock->expects($this->any())->method('getPost')->will(
+            $this->returnValue($integration->getData())
+        );
+        $integration->setData('id', self::INTEGRATION_ID);
+        $this->_integrationSvcMock->expects($this->any())->method('create')->with($this->anything())
+            ->will($this->throwException(new \Magento\Integration\Exception($exceptionMessage)));
+        $this->_integrationSvcMock->expects($this->any())->method('get')->with(self::INTEGRATION_ID)->will(
+            $this->returnValue(null)
+        );
+        // Use real translate model
+        $this->_translateModelMock = null;
+        // Verify success message
+        $this->_messageManager->expects($this->once())
+            ->method('addError')
+            ->with($exceptionMessage);
+        $integrationController = $this->_createIntegrationController();
+        $integrationController->saveAction();
+    }
+
     public function testDeleteAction()
     {
         $intData = $this->_getSampleIntegrationData();
