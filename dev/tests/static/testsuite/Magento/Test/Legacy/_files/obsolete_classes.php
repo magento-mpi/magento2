8d59af9d7b993cfffc9045a44a83986e3201460d
Max Yekaterynenko <myekaterynenko@ebay.com>
2013-12-27 22:04:30 +0200

    Fixed MAGETWO-1104 Occurrences of non-existent resource model Mage_Catalog_Model_Resource_Convert

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
@@ -1758,4 +1758,5 @@ return array(
     array('Magento\Core\Model\Session\Exception', 'Magento\Session\Exception'),
     array('Magento\Core\Model\Session\Context'),
     array('Magento\Core\Model\Session\AbstractSession', 'Magento\Session\SessionManager'),
+    array('Mage_Catalog_Model_Resource_Convert', 'Magento\Catalog\Model\Resource\Convert'),
 );
