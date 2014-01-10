bb7e8f343d04d2115a67460d6239f6bc81eb99d9
Dmytro Kvashnin <dkvashnin@ebay.com>
2014-01-10 18:04:09 +0200

    MAGETWO-18825: It's impossible to save configurable product with FPT options - DI compiler test fixed Cyclomatic complexity

--- dev/tests/static/testsuite/Magento/Test/Integrity/Di/CompilerTest.php
+++ dev/tests/static/testsuite/Magento/Test/Integrity/Di/CompilerTest.php
@@ -101,14 +101,10 @@ class CompilerTest extends \PHPUnit_Framework_TestCase
             if (\Magento\TestFramework\Utility\Classes::isVirtual($instanceName)) {
                 $instanceName = \Magento\TestFramework\Utility\Classes::resolveVirtualType($instanceName);
             }
-            $parameters = $parameters['parameters'];
 
-            if (!class_exists($instanceName)) {
-                // check for generated factory
-                if (substr($instanceName, -7) == 'Factory' && class_exists(substr($instanceName, 0, -7))) {
-                    continue;
-                }
-                $this->fail('Detected configuration of non existed class: ' . $instanceName);
+
+            if (!$this->_classExistsAsReal($instanceName)) {
+                continue;
             }
 
             $reflectionClass = new \ReflectionClass($instanceName);
@@ -118,6 +114,7 @@ class CompilerTest extends \PHPUnit_Framework_TestCase
                 $this->fail('Class ' . $instanceName . ' does not have __constructor');
             }
 
+            $parameters = $parameters['parameters'];
             $classParameters = $constructor->getParameters();
             foreach ($classParameters as $classParameter) {
                 $parameterName = $classParameter->getName();
@@ -129,6 +126,24 @@ class CompilerTest extends \PHPUnit_Framework_TestCase
                 . ' contains data for non-existed parameters: ' . implode(', ', array_keys($parameters));
             $this->assertEmpty($parameters, $message);
         }
+    }
+
+    /**
+     * Checks if class is a real one or generated Factory
+     * @param $instanceName class name
+     * @throws \PHPUnit_Framework_AssertionFailedError
+     * @return bool
+     */
+    protected function _classExistsAsReal($instanceName)
+    {
+        if (class_exists($instanceName)) {
+            return true;
+        }
+        // check for generated factory
+        if (substr($instanceName, -7) == 'Factory' && class_exists(substr($instanceName, 0, -7))) {
+            return false;
+        }
+        $this->fail('Detected configuration of non existed class: ' . $instanceName);
     }
 
     /**
