36a2d654824eb6f4aa8284adc7aa9848915caa56
Yurii Torbyk <itorbyk@ebay.com>
2014-01-10 18:49:02 +0200

    Implemented MAGETWO-19235: Oyejorge Less.php library incorporation

---
+++ lib/Magento/Css/PreProcessor/Adapter/Oyejorge.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Css\PreProcessor\Adapter;
+
+if (!class_exists('Less_Parser')) {
+    require_once __DIR__ . '/../../../../../lib/oyejorge/phpless/phpless/Less.php';
+}
+
+/**
+ * Oyejorge adapter model
+ */
+class Oyejorge implements \Magento\Css\PreProcessor\AdapterInterface
+{
+    /**
+     * @param $sourceFilePath
+     * @return string
+     */
+    public function process($sourceFilePath)
+    {
+        $parser = new \Less_Parser();
+        $parser->parseFile($sourceFilePath, '');
+        return $parser->getCss();
+    }
+}
