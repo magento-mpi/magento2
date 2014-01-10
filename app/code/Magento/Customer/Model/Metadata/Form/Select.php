78c9f3336aede0823de2d54783b014ca1e67e432
Eugene Tulika <etulika@ebay.com>
2014-01-10 14:28:35 +0200

    MAGETWO-19259: Re-Factor block Magento/Sales/Block/Adminhtml/Order/Create/Form/Address to use customer service(s) - Port metadata forms from extensibility branch

---
+++ app/code/Magento/Customer/Model/Metadata/Form/Select.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Form Element Select Data Model
+ *
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_Eav
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Customer\Model\Metadata\Form;
+
+class Select extends AbstractData
+{
+    /**
+     * Extract data from request and return value
+     *
+     * @param \Magento\App\RequestInterface $request
+     * @return array|string
+     */
+    public function extractValue(\Magento\App\RequestInterface $request)
+    {
+        return $this->_getRequestValue($request);
+    }
+
+    /**
+     * Validate data
+     * Return true or array of errors
+     *
+     * @param array|string $value
+     * @return boolean|array
+     */
+    public function validateValue($value)
+    {
+        $errors     = array();
+        $attribute  = $this->getAttribute();
+        $label      = __($attribute->getStoreLabel());
+
+        if ($value === false) {
+            // try to load original value and validate it
+            $value = $this->_value;
+        }
+
+        if ($attribute->getIsRequired() && empty($value) && $value != '0') {
+            $errors[] = __('"%1" is a required value.', $label);
+        }
+
+        if (!$errors && !$attribute->getIsRequired() && empty($value)) {
+            return true;
+        }
+
+        if (count($errors) == 0) {
+            return true;
+        }
+
+        return $errors;
+    }
+
+    /**
+     * Export attribute value to entity model
+     *
+     * @param array|string $value
+     * @return string
+     */
+    public function compactValue($value)
+    {
+        if ($value !== false) {
+            $value;
+        }
+        return false;
+    }
+
+    /**
+     * Restore attribute value from SESSION to entity model
+     *
+     * @param array|string $value
+     * @return string
+     */
+    public function restoreValue($value)
+    {
+        return $this->compactValue($value);
+    }
+
+    /**
+     * Return a text for option value
+     *
+     * @param int $value
+     * @return string
+     */
+    protected function _getOptionText($value)
+    {
+        $optionText = false;
+        foreach ($this->getAttribute()->getOptions() as $optionKey => $optionValue) {
+            if ($optionValue == $value) {
+                $optionText = $optionKey;
+            }
+        }
+        $output[] = $optionText;
+    }
+
+    /**
+     * Return formated attribute value from entity model
+     *
+     * @return string|array
+     */
+    public function outputValue($format = \Magento\Eav\Model\AttributeDataFactory::OUTPUT_FORMAT_TEXT)
+    {
+        $value = $this->_value;
+        switch ($format) {
+            case \Magento\Eav\Model\AttributeDataFactory::OUTPUT_FORMAT_JSON:
+                $output = $value;
+                break;
+            default:
+                if ($value != '') {
+                    $output = $this->_getOptionText($value);
+                } else {
+                    $output = '';
+                }
+                break;
+        }
+
+        return $output;
+    }
+}
