45af208625acbbda06ea02d0dce36bbee46018c3
Susan Wright <swright@ebay.com>
2014-01-15 08:24:21 -0800

    Implemented MAGETWO-19447: Dockblock type annotation for libraries (phase 2)
    - Xml.

--- lib/Magento/Xml/Generator.php
+++ lib/Magento/Xml/Generator.php
@@ -16,7 +16,14 @@ class Generator
      */
     const DEFAULT_ENTITY_ITEM_NAME = 'item';
 
+    /**
+     * @var \DOMDocument|null
+     */
     protected $_dom = null;
+
+    /**
+     * @var \DOMDocument
+     */
     protected $_currentDom;
 
     /** @var string */
@@ -30,16 +37,26 @@ class Generator
         return $this;
     }
 
+    /**
+     * @return \DOMDocument|null
+     */
     public function getDom()
     {
         return $this->_dom;
     }
 
+    /**
+     * @return \DOMDocument
+     */
     protected function _getCurrentDom()
     {
         return $this->_currentDom;
     }
 
+    /**
+     * @param \DOMDocument $node
+     * @return $this
+     */
     protected function _setCurrentDom($node)
     {
         $this->_currentDom = $node;
@@ -47,8 +64,9 @@ class Generator
     }
 
     /**
-    * @param array $content
-    */
+     * @param array $content
+     * @return $this
+     */
     public function arrayToXml($content)
     {
         $parentNode = $this->_getCurrentDom();
@@ -94,11 +112,18 @@ class Generator
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return $this->getDom()->saveXML();
     }
 
+    /**
+     * @param string $file
+     * @return $this
+     */
     public function save($file)
     {
         $this->getDom()->save($file);
@@ -108,7 +133,7 @@ class Generator
     /**
      * Set xml node name to use instead of numeric index during numeric arrays conversion.
      *
-     * @param $name
+     * @param string $name
      * @return \Magento\Xml\Generator
      */
     public function setIndexedArrayItemName($name)
