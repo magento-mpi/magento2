5b0a82da676d9bcf283d70aae5055d379384d64d
Sergiy Akulinin <sakulinin@ebay.com>
2014-01-15 16:01:13 +0200

    CR Changes MAGETWO-17924: [Payment] Consolidate Autorizenet method in separate module

--- dev/tests/unit/testsuite/Magento/Authorizenet/Model/Authorizenet/CardsTest.php
+++ dev/tests/unit/testsuite/Magento/Authorizenet/Model/Authorizenet/CardsTest.php
@@ -3,7 +3,7 @@
  * {license_notice}
  *
  * @category    Magento
- * @package     Magento_Paygate
+ * @package     Magento_Authorizenet
  * @subpackage  unit_tests
  * @copyright   {copyright}
  * @license     {license_link}
