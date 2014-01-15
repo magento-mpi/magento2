6e51cf3ba420f93b28fccefa5a093f942f5f0e6d
Andriy Nasinnyk <anasinnyk@ebay.com>
2014-01-15 15:15:43 +0200

    Implemented MAGETWO-19560: Create class structure for cron_groups config

---
+++ app/code/Magento/Cron/Model/Groups/Config/Data.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_Cron
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Prepare cron jobs data
+ */
+namespace Magento\Cron\Model\Groups\Config;
+
+class Data extends \Magento\Config\Data
+{
+    /**
+     * @param \Magento\Cron\Model\Groups\Config\Reader\Xml $reader
+     * @param \Magento\Config\CacheInterface $cache
+     * @param string $cacheId
+     */
+    public function __construct(
+        \Magento\Cron\Model\Groups\Config\Reader\Xml $reader,
+        \Magento\Config\CacheInterface $cache,
+        $cacheId = 'cron_groups_config_cache'
+    ) {
+        parent::__construct($reader, $cache, $cacheId);
+    }
+
+    /**
+     * Return config by group id
+     *
+     * @param string $groupId
+     * @return array
+     */
+    public function getByGroupId($groupId)
+    {
+        var_dump($this->get());
+        return $this->get()[$groupId];
+    }
+}
