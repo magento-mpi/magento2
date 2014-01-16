c738c521f8486ebf0e1ec5c520d24a8ecadb128b
Quoc Vu <qvu@ebay.com>
2014-01-16 14:50:03 -0800

    Implemented MAGETWO-19366: Test for Magento/Customer/Controller/Address * address code review feedback

--- dev/tests/integration/testsuite/Magento/Customer/Controller/AddressTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Controller/AddressTest.php
@@ -3,8 +3,6 @@
  * {license_notice}
  *
  * @category    Magento
- * @package     Magento_Customer
- * @subpackage  integration_tests
  * @copyright   {copyright}
  * @license     {license_link}
  */
@@ -13,16 +11,20 @@ namespace Magento\Customer\Controller;
 
 class AddressTest extends \Magento\TestFramework\TestCase\AbstractController
 {
+    protected function setup()
+    {
+        $logger = $this->getMock('Magento\Logger', array(), array(), '', false);
+        $session = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+            ->create('Magento\Customer\Model\Session', array($logger));
+        $session->login('customer@example.com', 'password');
+    }
+
     /**
      * @magentoDataFixture Magento/Customer/_files/customer.php
      * @magentoDataFixture Magento/Customer/_files/customer_address.php
      */
     public function testIndexAction()
     {
-        $logger = $this->getMock('Magento\Logger', array(), array(), '', false);
-        $session = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
-            ->create('Magento\Customer\Model\Session', array($logger));
-        $session->login('customer@example.com', 'password');
         $this->dispatch('customer/address/index');
 
         $body = $this->getResponse()->getBody();
@@ -37,22 +39,17 @@ class AddressTest extends \Magento\TestFramework\TestCase\AbstractController
      */
     public function testFormPostAction()
     {
-        $logger = $this->getMock('Magento\Logger', array(), array(), '', false);
-        $session = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
-            ->create('Magento\Customer\Model\Session', array($logger));
-        $session->login('customer@example.com', 'password');
-
         $this->getRequest()
             ->setParam('id', 1)
             ->setServer(['REQUEST_METHOD' => 'POST'])
             ->setPost([
-                'form_key' => $formKey = $this->_objectManager->get('Magento\Data\Form\FormKey')->getFormKey(),
+                'form_key' => $this->_objectManager->get('Magento\Data\Form\FormKey')->getFormKey(),
                 'firstname' => 'James',
                 'lastname' => 'Bond',
                 'company' => 'Ebay',
                 'telephone' => '1112223333',
                 'fax' => '2223334444',
-                'street[]' => array('1234 Monterey Rd'),
+                'street[]' => ['1234 Monterey Rd'],
                 'city' => 'San Jose',
                 'region_id' => '12',
                 'region' => 'California',
@@ -60,13 +57,13 @@ class AddressTest extends \Magento\TestFramework\TestCase\AbstractController
                 'country_id' => 'US',
                 'success_url' => '',
                 'error_url' => ''
-        ]);
+            ]);
         // we are overwriting the address coming from the fixture
         $this->dispatch('customer/address/formPost');
 
         $this->assertRedirect($this->stringContains('customer/address/index'));
         $this->assertSessionMessages(
-            $this->equalTo(array('The address has been saved.')),
+            $this->equalTo(['The address has been saved.']),
             \Magento\Message\MessageInterface::TYPE_SUCCESS
         );
     }
