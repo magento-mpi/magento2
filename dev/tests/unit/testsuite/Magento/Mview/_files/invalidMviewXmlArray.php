47002285cf4128ade1cfb5fbe5352d85c3c7c4d7
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-01-16 10:50:30 +0200

    Implemented MAGETWO-19086: Create new indexer model configuration for new Indexer module
     - fixture updated

--- dev/tests/unit/testsuite/Magento/Mview/_files/invalidMviewXmlArray.php
+++ dev/tests/unit/testsuite/Magento/Mview/_files/invalidMviewXmlArray.php
@@ -5,4 +5,46 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-return array();
+return array(
+    'without_mview_handle' => array(
+        '<?xml version="1.0"?><config></config>',
+        array("Element 'config': Missing child element(s). Expected is ( view )."),
+    ),
+
+    'mview_with_notallowed_attribute' => array(
+        '<?xml version="1.0" encoding="UTF-8"?><config><view id="view_one" notallow="notallow" class="Ogogo\Class\One">'
+        . '<subscriptions><table name="some_entity" entity_column="entity_id" /></subscriptions></view></config>',
+        array("Element 'view', attribute 'notallow': The attribute 'notallow' is not allowed."),
+    ),
+
+    'mview_without_class_attribute' => array(
+        '<?xml version="1.0" encoding="UTF-8"?><config><view id="view_one" ><subscriptions>'
+        . '<table name="some_entity" entity_column="entity_id" /></subscriptions></view></config>',
+        array("Element 'view': The attribute 'class' is required but missing."),
+    ),
+
+    'mview_with_empty_subscriptions' => array(
+        '<?xml version="1.0" encoding="UTF-8"?><config><view id="view_one" class="Ogogo\Class\One"><subscriptions>'
+        . '</subscriptions></view></config>',
+        array("Element 'subscriptions': Missing child element(s). Expected is ( table )."),
+    ),
+
+    'subscriptions_without_table' => array(
+        '<?xml version="1.0" encoding="UTF-8"?><config><view id="view_one" class="Ogogo\Class\One"><subscriptions>'
+        . '</subscriptions></view></config>',
+        array("Element 'subscriptions': Missing child element(s). Expected is ( table )."),
+    ),
+
+    'table_without_column_attribute' => array(
+        '<?xml version="1.0" encoding="UTF-8"?><config><view id="view_one" class="Ogogo\Class\One"><subscriptions>'
+        . '<table name="some_entity" /></subscriptions></view></config>',
+        array("Element 'table': The attribute 'entity_column' is required but missing."),
+    ),
+
+    'subscriptions_duplicate_table' => array(
+        '<?xml version="1.0" encoding="UTF-8"?><config><view id="view_one" class="Ogogo\Class\One"><subscriptions>'
+        . '<table name="some_entity" entity_column="entity_id" />'
+        . '<table name="some_entity" entity_column="entity_id" /></subscriptions></view></config>',
+        array("Element 'table': Duplicate key-sequence ['some_entity', 'entity_id'] in unique identity-constraint 'uniqueSubscriptionsTable'."),
+    ),
+);
