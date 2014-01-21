32db58a3a49c0da9795cb7a37cd8bfb26cd54cb1
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-01-21 10:40:59 +0200

    Fixed MAGETWO-19088: Create view (mview) functionality for new indexer module
     - fixed unit test

--- dev/tests/unit/testsuite/Magento/Mview/_files/mview_config.php
+++ dev/tests/unit/testsuite/Magento/Mview/_files/mview_config.php
@@ -6,13 +6,15 @@
  * @license     {license_link}
  */
 return array(
-    'inputXML' => '<?xml version="1.0" encoding="UTF-8"?><config><view id="view_one" class="Ogogo\Class\One">'
+    'inputXML' => '<?xml version="1.0" encoding="UTF-8"?><config>'
+        . '<view id="view_one" class="Ogogo\Class\One" group="some_view_group">'
         . '<subscriptions><table name="some_entity" entity_column="entity_id" />'
         . '<table name="some_product_relation" entity_column="product_id" /></subscriptions></view></config>',
     'expected' => array(
         'view_one' => array(
             'view_id'       => 'view_one',
             'action_class'  => 'Ogogo\Class\One',
+            'group' => 'some_view_group',
             'subscriptions' => array(
                 'some_entity' => array(
                     'name'   => 'some_entity',
