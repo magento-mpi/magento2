3ba7d2c17b420ae055347826047da17d9d43f6e4
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2014-01-21 10:46:47 +0200

    MAGETWO-18786: Dependency Inversion Between Magento_CatalogInventory/Magento_Sales and Magento_GroupedProduct Modules refactored AdminOrder\Create model

---
+++ app/code/Magento/GroupedProduct/Model/Sales/AdminOrder/Product/Quote/Plugin/Initializer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Product quote initializer plugin
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ *
+ */
+namespace Magento\GroupedProduct\Model\Sales\AdminOrder\Product\Quote\Plugin;
+
+
+class Initializer
+{
+    /**
+     * @param array $arguments
+     * @param \Magento\Code\Plugin\InvocationChain $invocationChain
+     * @return \Magento\Sales\Model\Quote\Item|string
+     */
+    public function aroundInit(array $arguments, \Magento\Code\Plugin\InvocationChain $invocationChain)
+    {
+        /** @var \Magento\Sales\Model\Quote $quote */
+        $quote = $arguments[0];
+        /** @var \Magento\Catalog\Model\Product $product */
+        $product = $arguments[1];
+        /** @var \Magento\Object $config */
+        $config = $arguments[2];
+
+        $item = $invocationChain->proceed($arguments);
+
+        if ($product->getTypeId() != \Magento\GroupedProduct\Model\Product\Type\Grouped::TYPE_CODE) {
+            $item = $quote->addProductAdvanced(
+                $product,
+                $config,
+                \Magento\Catalog\Model\Product\Type\AbstractType::PROCESS_MODE_LITE
+            );
+        }
+        return $item;
+    }
+}
