686238f369c6be6e875fc977292eb7fff2f60e91
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-01-21 10:39:59 +0200

    Fixed MAGETWO-19629: Create tests for Indexer module
     - fixed unit test

--- dev/tests/unit/testsuite/Magento/Indexer/Model/IndexerTest.php
+++ dev/tests/unit/testsuite/Magento/Indexer/Model/IndexerTest.php
@@ -34,6 +34,11 @@ class IndexerTest extends \PHPUnit_Framework_TestCase
      */
     protected $stateFactoryMock;
 
+    /**
+     * @var \Magento\Indexer\Model\Indexer\CollectionFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $indexFactoryMock;
+
     protected function setUp()
     {
         $this->configMock = $this->getMock('Magento\Indexer\Model\Config', array('get'), array(), '', false);
@@ -44,11 +49,15 @@ class IndexerTest extends \PHPUnit_Framework_TestCase
         $this->stateFactoryMock = $this->getMock(
             'Magento\Indexer\Model\Indexer\StateFactory', array('create'), array(), '', false
         );
+        $this->indexFactoryMock = $this->getMock(
+            'Magento\Indexer\Model\Indexer\CollectionFactory', array('create'), array(), '', false
+        );
         $this->model = new Indexer(
             $this->configMock,
             $this->actionFactoryMock,
             $this->viewFactoryMock,
-            $this->stateFactoryMock
+            $this->stateFactoryMock,
+            $this->indexFactoryMock
         );
     }
 
@@ -117,16 +126,99 @@ class IndexerTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\Magento\Indexer\Model\Indexer\State', $this->model->getState());
     }
 
+    /**
+     * @param string $mode
+     * @param string $status
+     * @param bool $statusCall
+     * @param bool $stateCall
+     * @param string $result
+     * @dataProvider indexerStatusProvider
+     */
+    public function testGetStatus($mode, $status, $statusCall, $stateCall, $result)
+    {
+        $indexId = 'indexer_internal_name';
+        $view = $this->getMock(
+            'Magento\Mview\View',
+            array('load', 'getMode', 'getStatus'),
+            array(),
+            '',
+            false
+        );
+        $view->expects($this->once())
+            ->method('load')
+            ->with('view_test')
+            ->will($this->returnSelf());
+        $view->expects($this->once())
+            ->method('getMode')
+            ->will($this->returnValue($mode));
+        $view->expects($statusCall ? $this->once() : $this->never())
+            ->method('getStatus')
+            ->will($this->returnValue($status));
+        $this->viewFactoryMock->expects($this->once())
+            ->method('create')
+            ->will($this->returnValue($view));
+
+        $stateMock = $this->getMock(
+            '\Magento\Indexer\Model\Indexer\State',
+            array('load', 'getId', 'setIndexerId', '__wakeup', 'getStatus'),
+            array(),
+            '',
+            false
+        );
+        if ($stateCall) {
+            $stateMock->expects($this->once())
+                ->method('load')
+                ->with($indexId, 'indexer_id')
+                ->will($this->returnSelf());
+            $stateMock->expects($this->never())
+                ->method('setIndexerId');
+            $stateMock->expects($this->once())
+                ->method('getId')
+                ->will($this->returnValue(1));
+            $stateMock->expects($this->once())
+                ->method('getStatus')
+                ->will($this->returnValue($status));
+            $this->stateFactoryMock->expects($this->once())
+                ->method('create')
+                ->will($this->returnValue($stateMock));
+        }
+
+        $this->configMock->expects($this->once())
+            ->method('get')
+            ->with($indexId)
+            ->will($this->returnValue($this->getIndexerData()));
+        $this->model->load($indexId);
+
+        $this->assertEquals($result, $this->model->getStatus());
+    }
+
+    /**
+     * @return array
+     */
+    public function indexerStatusProvider()
+    {
+        return array(
+            'enabled_working' => array('enabled', 'working', true, false, 'working'),
+            'enabled_idle'  => array('enabled', 'idle', true, true, 'idle'),
+            'disabled_working' => array('disabled', 'working', false, true, 'working'),
+            'disabled_idle' => array('disabled', 'idle', false, true, 'idle'),
+        );
+    }
+
+    public function testGetUpdated()
+    {
+        $this->assertFalse(false);
+    }
+
     protected function getIndexerData()
     {
         return array(
             'indexer_id' => 'indexer_internal_name',
             'view_id' => 'view_test',
             'class' => 'Some\Class\Name',
+            'group' => 'some_group',
             'title' => 'Indexer public name',
-            'title_translate' => true,
             'description' => 'Indexer public description',
-            'description_translate' => true,
         );
     }
 }
--- dev/tests/unit/testsuite/Magento/Indexer/_files/indexer_config.php
+++ dev/tests/unit/testsuite/Magento/Indexer/_files/indexer_config.php
@@ -7,7 +7,7 @@
  */
 return array(
     'inputXML' => '<?xml version="1.0" encoding="UTF-8"?><config>'
-        . '<indexer id="indexer_internal_name" view_id="view01" class="Index\Class\Name"><title translate="true">'
+        . '<indexer id="indexer_internal_name" view_id="view01" class="Index\Class\Name" group="some_group"><title translate="true">'
         . 'Indexer public name</title><description translate="true">Indexer public description</description>'
         . '</indexer></config>',
     'expected' => array(
@@ -15,6 +15,7 @@ return array(
             'indexer_id' => 'indexer_internal_name',
             'view_id' => 'view01',
             'action_class' => 'Index\Class\Name',
+            'group' => 'some_group',
             'title' => 'Indexer public name',
             'description' => 'Indexer public description',
         ),
