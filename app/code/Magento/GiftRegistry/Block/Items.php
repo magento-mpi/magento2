6d14253a17f6467d350a9d5a658e77285837f83b
Anton Kril <akril@ebay.com>
2014-01-21 19:47:16 +0200

    MAGETWO-19727: GiftRegistry module dependency

--- app/code/Magento/GiftRegistry/Block/Items.php
+++ app/code/Magento/GiftRegistry/Block/Items.php
@@ -192,4 +192,28 @@ class Items extends \Magento\Checkout\Block\Cart
     {
         return $this->getUrl('giftregistry');
     }
+
+
+    /**
+     * Retrieve item renderer block
+     *
+     * @param string $type
+     * @return \Magento\View\Element\AbstractBlock
+     * @throws \RuntimeException
+     */
+    public function getItemRenderer($type)
+    {
+        /** @var \Magento\View\Element\RendererList $rendererList */
+        $rendererList = $this->getChildBlock('renderer.list');
+        if (!$rendererList) {
+            throw new \RuntimeException('Renderer list fo block "' . $this->getNameInLayout() . '" is not defined');
+        }
+        $renderer = $rendererList->getRenderer($type) ?: $rendererList->getRenderer(self::DEFAULT_TYPE);
+        if (!$renderer instanceof \Magento\View\Element\BlockInterface) {
+            throw new \RuntimeException('Renderer for type "' . $type . '" does not exist.');
+        }
+        $renderer->setTemplate($this->getItemRendererTemplate());
+        $renderer->setRenderedBlock($this);
+        return $renderer;
+    }
 }
