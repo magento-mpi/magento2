810498543ae1c55df28c26e12fe973d72bef88ed
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-01-21 13:08:29 +0200

    Fixed MAGETWO-19629: Create tests for Indexer module
     - fixed static test

--- dev/tests/unit/testsuite/Magento/Indexer/_files/indexer_config.php
+++ dev/tests/unit/testsuite/Magento/Indexer/_files/indexer_config.php
@@ -7,7 +7,8 @@
  */
 return array(
     'inputXML' => '<?xml version="1.0" encoding="UTF-8"?><config>'
-        . '<indexer id="indexer_internal_name" view_id="view01" class="Index\Class\Name" group="some_group"><title translate="true">'
+        . '<indexer id="indexer_internal_name" view_id="view01" class="Index\Class\Name" group="some_group">'
+        . '<title translate="true">'
         . 'Indexer public name</title><description translate="true">Indexer public description</description>'
         . '</indexer></config>',
     'expected' => array(
