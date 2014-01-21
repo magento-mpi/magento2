0062e70f70199e73c43fe3aa37cb0ddd90f03da1
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-01-21 10:57:32 +0200

    Fixed MAGETWO-19629: Create tests for Indexer module
     - fixed unit test

--- dev/tests/unit/testsuite/Magento/Indexer/Model/Indexer/CollectionTest.php
+++ dev/tests/unit/testsuite/Magento/Indexer/Model/Indexer/CollectionTest.php
@@ -9,28 +9,10 @@ namespace Magento\Indexer\Model\Indexer;
 
 class CollectionTest extends \PHPUnit_Framework_TestCase
 {
-//if (!$this->isLoaded()) {
-//$states = $this->statesFactory->create();
-//foreach (array_keys($this->config->getAll()) as $indexerId) {
-//    /** @var \Magento\Indexer\Model\Indexer $indexer */
-//$indexer = $this->getNewEmptyItem();
-//$indexer->load($indexerId);
-//foreach ($states->getItems() as $state) {
-//    /** @var \Magento\Indexer\Model\Indexer\State $state */
-//if ($state->getIndexerId() == $indexerId) {
-//$indexer->setState($state);
-//break;
-//}
-//}
-//$this->_addItem($indexer);
-//}
-//$this->_setIsLoaded(true);
-//}
-
     public function testLoadData()
     {
-        $indexerId1 = 'first_indexer_id';
-        $indexerId2 = 'second_indexer_id';
+        $indexerIdOne = 'first_indexer_id';
+        $indexerIdSecond = 'second_indexer_id';
 
         $entityFactory = $this->getMockBuilder('Magento\Data\Collection\EntityFactoryInterface')
             ->disableOriginalConstructor()
@@ -42,6 +24,7 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
 
         $statesFactory = $this->getMockBuilder('Magento\Indexer\Model\Resource\Indexer\State\CollectionFactory')
             ->disableOriginalConstructor()
+            ->setMethods(array('create'))
             ->getMock();
 
         $states = $this->getMockBuilder('Magento\Indexer\Model\Resource\Indexer\State\Collection')
@@ -68,7 +51,7 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
 
         $indexer->expects($this->any())
             ->method('load')
-            ->with($this->logicalOr($indexerId1, $indexerId2));
+            ->with($this->logicalOr($indexerIdOne, $indexerIdSecond));
 
         $entityFactory->expects($this->any())
             ->method('create')
@@ -81,7 +64,7 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
 
         $config->expects($this->once())
             ->method('getAll')
-            ->will($this->returnValue(array($indexerId1 => 1, $indexerId2 => 2)));
+            ->will($this->returnValue(array($indexerIdOne => 1, $indexerIdSecond => 2)));
 
         $states->expects($this->any())
             ->method('getItems')
