f66832dd595c42500d6cc5e1f61ca1fec32b1f81
Paul Lewis <plewis@ebay.com>
2014-01-23 13:57:01 -0600

    MAGETWO-19572: UT Cust/Model/Metadata/Form/Boolean

---
+++ dev/tests/unit/testsuite/Magento/Customer/Model/Metadata/Form/BooleanTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * test Magento\Customer\Model\Model\Metadata\Form\Boolean
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Customer\Model\Metadata\Form;
+
+class BooleanTest extends \PHPUnit_Framework_TestCase
+{
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $localeMock;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $loggerMock;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $attributeMetadataMock;
+
+    protected function setUp()
+    {
+        $this->localeMock = $this->getMockBuilder('Magento\Core\Model\LocaleInterface')->getMock();
+        $this->loggerMock = $this->getMockBuilder('Magento\Logger')->disableOriginalConstructor()->getMock();
+        $this->attributeMetadataMock = $this->getMockBuilder('Magento\Customer\Service\V1\Dto\Eav\AttributeMetadata')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @param $value to assign to boolean
+     * @param $expected text output
+     * @dataProvider getOptionTextDataProvider
+     */
+    public function testGetOptionText($value, $expected)
+    {
+        // calling outputValue() will cause the protected method getOptionText() to be called
+        $boolean = new Boolean($this->localeMock, $this->loggerMock, $this->attributeMetadataMock, $value, 0);
+        $actual = $boolean->outputValue();
+        $this->assertSame($expected, $actual);
+    }
+
+    public function getOptionTextDataProvider()
+    {
+        return [
+            '0' => ['0', 'No'],
+            '1' => ['1', 'Yes'],
+            'int 5' => [5, ''],
+            'Null' => [null, ''],
+            'Invalid' => ['Invalid', ''],
+        ];
+    }
+}
