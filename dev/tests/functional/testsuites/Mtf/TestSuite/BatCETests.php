0239ae324c2a0a8d10ceeb919c919b47f29955ea
Aleksandr Gorbivskyi <agorbivskyi@ebay.com>
2014-01-27 18:34:07 +0200

    Implemented MAGETWO-20110: Bamboo running end-to-end tests for CE

---
+++ dev/tests/functional/testsuites/Mtf/TestSuite/BatCETests.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * BAT CE
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Mtf\TestSuite;
+
+class BatCETests
+{
+    public static function suite()
+    {
+        $suite = new \PHPUnit_Framework_TestSuite('BAT CE');
+
+        // Product
+        $suite->addTestSuite('Magento\Bundle\Test\TestCase\BundleFixedTest');
+        $suite->addTestSuite('Magento\Catalog\Test\TestCase\Product\CreateTest');
+        $suite->addTestSuite('Magento\Catalog\Test\TestCase\Product\CreateConfigurableTest');
+        $suite->addTestSuite('Magento\Catalog\Test\TestCase\Product\Configurable\CreateWithAttributeTest');
+        $suite->addTestSuite('Magento\Catalog\Test\TestCase\Product\CreateSimpleWithCustomOptionsAndCategoryTest');
+
+        // Category
+        $suite->addTestSuite('Magento\Catalog\Test\TestCase\Category\CreateTest');
+
+        // Search
+        $suite->addTestSuite('Magento\CatalogSearch\Test\TestCase\SearchTest');
+
+        // Checkout
+        $suite->addTestSuite('Magento\Checkout\Test\TestCase\Guest\CheckMoneyOrderTest');
+        $suite->addTestSuite('Magento\Checkout\Test\TestCase\ProductAdvancedPricingTest');
+
+        // Stores
+        $suite->addTestSuite('Magento\Core\Test\TestCase\StoreTest');
+
+        return $suite;
+    }
+}
