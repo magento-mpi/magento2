4cf14767e7e0b50d9bdb8fceaa92126b1fe3329c
Alexander Paliarush <apaliarush@ebay.com>
2014-01-28 13:54:44 +0200

    Implemented MAGETWO-20028: Refactor methods _getCustomerAddressForm and _setQuoteAddress - Added tests which cover \Magento\Sales\Model\AdminOrder\Create::_getCustomerAddressForm() and \Magento\Sales\Model\AdminOrder\Create::_setQuoteAddress()

--- dev/tests/integration/testsuite/Magento/Sales/Model/AdminOrder/CreateTest.php
+++ dev/tests/integration/testsuite/Magento/Sales/Model/AdminOrder/CreateTest.php
@@ -16,16 +16,21 @@ use Magento\TestFramework\Helper\Bootstrap;
 class CreateTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Model instance
-     *
      * @var \Magento\Sales\Model\AdminOrder\Create
      */
     protected $_model;
 
+    /** @var \Magento\Message\ManagerInterface */
+    protected $_messageManager;
+
     protected function setUp()
     {
         parent::setUp();
-        $this->_model = Bootstrap::getObjectManager()->create('Magento\Sales\Model\AdminOrder\Create');
+        $this->_messageManager = Bootstrap::getObjectManager()->get('Magento\Message\ManagerInterface');
+        $this->_model = Bootstrap::getObjectManager()->create(
+            'Magento\Sales\Model\AdminOrder\Create',
+            ['messageManager' => $this->_messageManager]
+        );
     }
 
     /**
@@ -203,5 +208,99 @@ class CreateTest extends \PHPUnit_Framework_TestCase
             $this->_model->getCustomerWishlist(true),
             'New wishlist is expected to be created when cache is forced to be refreshed.'
         );
+    }
+
+    /**
+     * @magentoDbIsolation enabled
+     */
+    public function testSetBillingAddress()
+    {
+        $addressData = $this->_getValidAddressData();
+        /** Validate data before creating address object */
+        $this->_model->setIsValidate(true)->setBillingAddress($addressData);
+        $this->assertInstanceOf(
+            'Magento\Sales\Model\Quote\Address',
+            $this->_model->getBillingAddress(),
+            'Billing address object was not created.'
+        );
+
+        $expectedAddressData = array_merge(
+            $addressData,
+            ['address_type' => 'billing', 'quote_id' => null, 'street' => "Line1\nLine2", 'save_in_address_book' => 0]
+        );
+        $this->assertEquals(
+            $expectedAddressData,
+            $this->_model->getBillingAddress()->getData(),
+            'Created billing address is invalid.'
+        );
+    }
+
+    /**
+     * @magentoDataFixture Magento/Customer/_files/customer.php
+     * @magentoAppIsolation enabled
+     * @magentoDbIsolation enabled
+     */
+    public function testSetBillingAddressValidationErrors()
+    {
+        $customerIdFromFixture = 1;
+        /** @var \Magento\Backend\Model\Session\Quote $session */
+        $session = Bootstrap::getObjectManager()->create('Magento\Backend\Model\Session\Quote');
+        $session->setCustomerId($customerIdFromFixture);
+        $invalidAddressData = array_merge($this->_getValidAddressData(), ['firstname' => '', 'lastname' => '']);
+        /**
+         * Note that validation errors are collected during setBillingAddress() call in the internal class variable,
+         * but they are not set to message manager at this step.
+         * They are set to message manager only during createOrder() call.
+         */
+        $this->_model->setIsValidate(true)->setBillingAddress($invalidAddressData);
+        try{
+            $this->_model->createOrder();
+            $this->fail('Validation errors are expected to lead to exception during createOrder() call.');
+        } catch (\Magento\Core\Exception $e) {
+            /** createOrder is expected to throw exception with empty message when validation error occurs */
+        }
+        $errorMessages = [];
+        /** @var $validationError \Magento\Message\Error */
+        foreach ($this->_messageManager->getMessages()->getItems() as $validationError) {
+            $errorMessages[] = $validationError->getText();
+        }
+        $this->assertTrue(
+            in_array('Billing Address: "First Name" is a required value.', $errorMessages),
+            'Expected validation message is absent.'
+        );
+        $this->assertTrue(
+            in_array('Billing Address: "Last Name" is a required value.', $errorMessages),
+            'Expected validation message is absent.'
+        );
+    }
+
+    /**
+     * Get valid address data for address creation.
+     *
+     * @return array
+     */
+    protected function _getValidAddressData()
+    {
+        return [
+            'customer_address_id' => '',
+            'prefix' => 'prefix',
+            'firstname' => 'FirstName',
+            'middlename' => 'MiddleName',
+            'lastname' => 'LastName',
+            'suffix' => 'suffix',
+            'company' => 'Company Name',
+            'street' => [
+                0 => 'Line1',
+                1 => 'Line2',
+            ],
+            'city' => 'City',
+            'country_id' => 'US',
+            'region' => '',
+            'region_id' => '1',
+            'postcode' => '76868',
+            'telephone' => '+8709273498729384',
+            'fax' => '',
+            'vat_id' => '',
+        ];
     }
 }
