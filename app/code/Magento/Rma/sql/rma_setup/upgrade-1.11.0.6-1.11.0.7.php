6893c90b84f3c8c3b1f35323c5d0d3d56d918893
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2014-01-29 19:27:37 +0200

    MAGETWO-20175: There is no ability to create RMA(Returns) for Grouped Product fixed grouped product config

--- app/code/Magento/Rma/sql/rma_setup/upgrade-1.11.0.6-1.11.0.7.php
+++ app/code/Magento/Rma/sql/rma_setup/upgrade-1.11.0.6-1.11.0.7.php
@@ -33,7 +33,10 @@ $installer->addAttribute(\Magento\Catalog\Model\Product::ENTITY, 'is_returnable'
     'comparable'        => false,
     'visible_on_front'  => false,
     'unique'            => false,
-    'apply_to'          => implode($this->getRefundableProducts(), ','),
+    'apply_to'          =>
+        \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE . ',' .
+        \Magento\Catalog\Model\Product\Type::TYPE_CONFIGURABLE . ',' .
+        \Magento\Catalog\Model\Product\Type::TYPE_BUNDLE,
     'is_configurable'   => false,
     'input_renderer'    => 'Magento\Rma\Block\Adminhtml\Product\Renderer',
 ));
