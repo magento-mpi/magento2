957e759856f9804522b355f88a6b43d57b28932e
Michael Chiocca <mchiocca@ebay.com>
2014-01-28 16:56:33 -0800

    MAGETWO-20159: Update Placeholder renderer to use named parameters - Updated so numeric placeholders use % and named placeholders use $

--- lib/Magento/Phrase/Renderer/Placeholder.php
+++ lib/Magento/Phrase/Renderer/Placeholder.php
@@ -19,7 +19,7 @@ class Placeholder implements \Magento\Phrase\RendererInterface
         if ($arguments) {
             $placeholders = [];
             foreach (array_keys($arguments) as $key) {
-                $placeholders[] = "%" . (is_int($key) ? strval($key + 1) : $key);
+                $placeholders[] = is_int($key) ? "%" . strval($key + 1) : "$" . $key;
             }
             $text = str_replace($placeholders, $arguments, $text);
         }
