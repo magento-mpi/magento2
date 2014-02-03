magento-mpi/magento2

commit 5e6ecc90317deec8f50bb87730df381277d3f4ee

MAGETWO-14489: Fix errors when saving address from FE

app/code/Magento/Customer/Block/Address/Edit.php
@@ -117,6 +117,11 @@ class Edit extends \Magento\Directory\Block\Data
         }
 
         if ($postedData = $this->_customerSession->getAddressFormData(true)) {
+            if (!empty($postedData['region_id'])) {
+                $postedData['region'] = [
+                    'region_id' => $postedData['region_id'],
+                ];
+            }
             $this->_address = $this->_addressBuilder
                 ->populateWithArray(array_merge($this->_address->__toArray(), $postedData))
                 ->create();
@@ -226,16 +231,12 @@ class Edit extends \Magento\Directory\Block\Data
 
     public function isDefaultBilling()
     {
-        $defaultBilling = $this->_addressService
-            ->getDefaultBillingAddress($this->_customerSession->getCustomerId());
-        return $this->getAddress()->getId() && $this->getAddress()->getId() == $defaultBilling;
+        return $this->getAddress()->isDefaultBilling();
     }
 
     public function isDefaultShipping()
     {
-        $defaultShipping = $this->_addressService
-            ->getDefaultShippingAddress($this->_customerSession->getCustomerId());
-        return $this->getAddress()->getId() && $this->getAddress()->getId() == $defaultShipping;
+        return $this->getAddress()->isDefaultShipping();
     }
 
     /**

app/code/Magento/Customer/Controller/Address.php
@@ -177,7 +177,7 @@ class Address extends \Magento\App\Action\Action
         }
 
         $this->_getSession()->setAddressFormData($this->getRequest()->getPost());
-        $url = $this->_buildUrl('*/*/edit', array('id' => $address->getId()));
+        $url = $this->_buildUrl('*/*/edit', array('id' => $this->getRequest()->getParam('id')));
         $this->getResponse()->setRedirect($this->_redirect->error($url));
     }
 
@@ -188,12 +188,10 @@ class Address extends \Magento\App\Action\Action
      */
     protected function _extractAddress()
     {
-        $customerId = $this->_getSession()->getCustomerId();
-
         $addressId = $this->getRequest()->getParam('id');
         $existingAddressData = [];
         if ($addressId) {
-            $existingAddress = $this->_addressService->getAddressById($customerId, $addressId);
+            $existingAddress = $this->_addressService->getAddressById($addressId);
             if ($existingAddress->getId()) {
                 $existingAddressData = $existingAddress->__toArray();
             }
@@ -207,14 +205,12 @@ class Address extends \Magento\App\Action\Action
         );
         $addressData = $addressForm->extractData($this->getRequest());
         $attributeValues = $addressForm->compactData($addressData);
-        $region = $this->_regionBuilder->setRegionCode('')
-            ->setRegion($attributeValues['region'])
-            ->setRegionId($attributeValues['region_id'])
-            ->create();
+        $region = [
+            'region_id' => $attributeValues['region_id'],
+        ];
         unset($attributeValues['region'], $attributeValues['region_id']);
-
+        $attributeValues['region'] = $region;
         return $this->_addressBuilder->populateWithArray(array_merge($existingAddressData, $attributeValues))
-            ->setRegion($region)
             ->setDefaultBilling($this->getRequest()->getParam('default_billing', false))
             ->setDefaultShipping($this->getRequest()->getParam('default_shipping', false))
             ->create();

app/code/Magento/Customer/view/frontend/address/edit.phtml
@@ -80,7 +80,6 @@
                 <select id="region_id" name="region_id" title="<?php echo __('State/Province') ?>" class="validate-select" style="display:none;"<?php echo (!$this->getConfig('general/region/display_all')) ? ' disabled="disabled"' :'';?>>
                     <option value=""><?php echo __('Please select region, state or province') ?></option>
                 </select>
-                <input type="text" id="region" name="region" value="<?php echo $this->escapeHtml($this->getAddress()->getRegion()->getRegion()) ?>"  title="<?php echo __('State/Province') ?>" class="input-text <?php echo $this->helper('Magento\Customer\Helper\Address')->getAttributeValidationClass('region') ?>"<?php echo (!$this->getConfig('general/region/display_all')) ? ' disabled="disabled"' :'';?>/>
             </div>
         </div>
         <div class="field zip required">
@@ -139,7 +138,7 @@
                     postcodeId: '#zip',
                     form: $('#form-validate').validation(),
                     regionJson: <?php echo $this->helper('Magento\Directory\Helper\Data')->getRegionJson() ?>,
-                    defaultRegion: "<?php echo $this->getAddress()->getRegion()->getRegionId() ?>",
+                    defaultRegion: "<?php echo $this->getRegionId() ?>",
                     countriesWithOptionalZip: <?php echo $this->helper('Magento\Directory\Helper\Data')->getCountriesWithOptionalZip(true) ?>
                 });
             });

commit cd2aeda2ce7c9da605b3ea7bd4deec7d1d3ab519

MAGETWO-14489: Add extra verification for Address controller
Also fix some issues caused by 5e6ecc9

app/code/Magento/Customer/Block/Address/Edit.php
@@ -117,9 +117,10 @@ class Edit extends \Magento\Directory\Block\Data
         }
 
         if ($postedData = $this->_customerSession->getAddressFormData(true)) {
-            if (!empty($postedData['region_id'])) {
+            if (!empty($postedData['region_id']) || !empty($postedData['region'])) {
                 $postedData['region'] = [
                     'region_id' => $postedData['region_id'],
+                    'region' => $postedData['region'],
                 ];
             }
             $this->_address = $this->_addressBuilder
@@ -203,6 +204,21 @@ class Edit extends \Magento\Directory\Block\Data
         return parent::getCountryId();
     }
 
+    /**
+     * Return the name of the region for the address being edited
+     *
+     * @return string region name
+     */
+    public function getRegion()
+    {
+        return $this->getAddress()->getRegion()->getRegion();
+    }
+
+    /**
+     * Return the id of the region being edited
+     *
+     * @return int region id
+     */
     public function getRegionId()
     {
         return $this->getAddress()->getRegion()->getRegionId();
@@ -231,12 +247,12 @@ class Edit extends \Magento\Directory\Block\Data
 
     public function isDefaultBilling()
     {
-        return $this->getAddress()->isDefaultBilling();
+        return (bool)$this->getAddress()->isDefaultBilling();
     }
 
     public function isDefaultShipping()
     {
-        return $this->getAddress()->isDefaultShipping();
+        return (bool)$this->getAddress()->isDefaultShipping();
     }
 
     /**

app/code/Magento/Customer/Controller/Address.php
@@ -207,6 +207,7 @@ class Address extends \Magento\App\Action\Action
         $attributeValues = $addressForm->compactData($addressData);
         $region = [
             'region_id' => $attributeValues['region_id'],
+            'region' => $attributeValues['region'],
         ];
         unset($attributeValues['region'], $attributeValues['region_id']);
         $attributeValues['region'] = $region;

app/code/Magento/Customer/view/frontend/address/edit.phtml
@@ -80,6 +80,7 @@
                 <select id="region_id" name="region_id" title="<?php echo __('State/Province') ?>" class="validate-select" style="display:none;"<?php echo (!$this->getConfig('general/region/display_all')) ? ' disabled="disabled"' :'';?>>
                     <option value=""><?php echo __('Please select region, state or province') ?></option>
                 </select>
+                <input type="text" id="region" name="region" value="<?php echo $this->escapeHtml($this->getRegion()) ?>"  title="<?php echo __('State/Province') ?>" class="input-text <?php echo $this->helper('Magento\Customer\Helper\Address')->getAttributeValidationClass('region') ?>"<?php echo (!$this->getConfig('general/region/display_all')) ? ' disabled="disabled"' :'';?>/>
             </div>
         </div>
         <div class="field zip required">

dev/tests/integration/testsuite/Magento/Customer/Controller/AddressTest.php
@@ -9,6 +9,8 @@
 
 namespace Magento\Customer\Controller;
 
+use Magento\TestFramework\Helper\Bootstrap;
+
 class AddressTest extends \Magento\TestFramework\TestCase\AbstractController
 {
     protected function setUp()
@@ -16,7 +18,7 @@ class AddressTest extends \Magento\TestFramework\TestCase\AbstractController
         parent::setUp();
 
         $logger = $this->getMock('Magento\Logger', array(), array(), '', false);
-        $session = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+        $session = Bootstrap::getObjectManager()
             ->create('Magento\Customer\Model\Session', array($logger));
         $session->login('customer@example.com', 'password');
     }
@@ -50,12 +52,12 @@ class AddressTest extends \Magento\TestFramework\TestCase\AbstractController
 
     /**
      * @magentoDataFixture Magento/Customer/_files/customer.php
-     * @magentoDataFixture Magento/Customer/_files/customer_address.php
+     * @magentoDataFixture Magento/Customer/_files/customer_two_addresses.php
      */
     public function testFormPostAction()
     {
         $this->getRequest()
-            ->setParam('id', 1)
+            ->setParam('id', 2)
             ->setServer(['REQUEST_METHOD' => 'POST'])
             ->setPost([
                 'form_key' => $this->_objectManager->get('Magento\Data\Form\FormKey')->getFormKey(),
@@ -64,12 +66,11 @@ class AddressTest extends \Magento\TestFramework\TestCase\AbstractController
                 'company' => 'Ebay',
                 'telephone' => '1112223333',
                 'fax' => '2223334444',
-                'street' => ['1234 Monterey Rd'],
-                'city' => 'San Jose',
-                'region_id' => '12',
-                'region' => 'California',
+                'street' => ['1234 Monterey Rd', 'Apt 13'],
+                'city' => 'Kyiv',
+                'region' => 'Kiev',
                 'postcode' => '55555',
-                'country_id' => 'US',
+                'country_id' => 'UA',
                 'success_url' => '',
                 'error_url' => ''
             ]);
@@ -81,6 +82,13 @@ class AddressTest extends \Magento\TestFramework\TestCase\AbstractController
             $this->equalTo(['The address has been saved.']),
             \Magento\Message\MessageInterface::TYPE_SUCCESS
         );
+        /** @var \Magento\Customer\Service\V1\CustomerAddressService $addressService */
+        $addressService = Bootstrap::getObjectManager()->create('Magento\Customer\Service\V1\CustomerAddressService');
+        $address = $addressService->getAddressById(2);
+
+        $this->assertEquals('UA', $address->getCountryId());
+        $this->assertEquals('Kyiv', $address->getCity());
+        $this->assertEquals('Kiev', $address->getRegion()->getRegion());
     }
 
     /**