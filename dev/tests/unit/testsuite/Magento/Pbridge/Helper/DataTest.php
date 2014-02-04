55b9c9dfc0c1ac5cc5476c44563328234c84cc6e
Sergiy Akulinin <sakulinin@ebay.com>
2014-02-04 14:30:51 +0200

    Fixed MAGETWO-17927: [Payment] Move abstraction logic from \MagentoPayPal\Model\Cart - fixed helper test

--- dev/tests/unit/testsuite/Magento/Pbridge/Helper/DataTest.php
+++ dev/tests/unit/testsuite/Magento/Pbridge/Helper/DataTest.php
@@ -2,9 +2,6 @@
 /**
  * {license_notice}
  *
- * @category    Magento
- * @package     Magento_Pbridge
- * @subpackage  unit_tests
  * @copyright   {copyright}
  * @license     {license_link}
  */
@@ -14,58 +11,78 @@ namespace Magento\Pbridge\Helper;
 class DataTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var \Magento\Pbridge\Helper\Data|PHPUnit_Framework_MockObject_MockObject
+     * @var Data|\PHPUnit_Framework_MockObject_MockObject
      */
     protected $_model;
 
     /**
-     * @var PHPUnit_Framework_MockObject_MockObject
+     * @var \PHPUnit_Framework_MockObject_MockObject|\Magento\Core\Model\AbstractModel
      */
     protected $_order;
 
     /**
-     * @var array()
+     * @var \PHPUnit_Framework_MockObject_MockObject|\Magento\Payment\Model\CartFactory
      */
-    protected $_allItems;
+    protected $_cartFactory;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|\Magento\Paypal\Model\CartFactory
+     */
+    protected $_paypalCartFactory;
 
     /**
      * setUp
      */
     protected function setUp()
     {
-        $context = $this->getMock('Magento\App\Helper\Context', array(), array(), '', false);
-        $coreStoreConfig = $this->getMock('Magento\Core\Model\Store\Config', array(), array(), '', false);
-        $customerSession = $this->getMock('Magento\Customer\Model\Session', array(), array(), '', false);
-        $checkoutSession = $this->getMock('Magento\Checkout\Model\Session', array(), array(), '', false);
-        $storeManager = $this->getMock('Magento\Core\Model\StoreManagerInterface', array(), array(), '', false);
-        $locale = $this->getMock('Magento\Core\Model\LocaleInterface', array(), array(), '', false);
-        $layout = $this->getMock('Magento\View\LayoutInterface', array(), array(), '', false);
-        $encryptionFactory = $this->getMock('Magento\Pbridge\Model\EncryptionFactory', array(), array(), '', false);
-        $appState = $this->getMock('Magento\App\State', array(), array(), '', false);
-        $cartFactory = $this->getMock('Magento\Paypal\Model\CartFactory', array('create'), array(), '', false);
-
+        $this->_cartFactory = $this->getMock('Magento\Payment\Model\CartFactory', ['create'], [], '', false);
+        $this->_paypalCartFactory = $this->getMock('Magento\Paypal\Model\CartFactory', ['create'], [], '', false);
+        $helper = new \Magento\TestFramework\Helper\ObjectManager($this);
+        $this->_model = $helper->getObject(
+            'Magento\Pbridge\Helper\Data',
+            ['cartFactory' => $this->_cartFactory, 'paypalCartFactory' => $this->_paypalCartFactory]
+        );
         $this->_order = $this->getMock('Magento\Core\Model\AbstractModel', array(), array(), '', false);
-        $this->_allItems = array(
+    }
+
+    public function testPrepareCart()
+    {
+        $expected = $this->_prepareCartExpectations($this->_cartFactory);
+        $this->assertEquals($expected, $this->_model->prepareCart($this->_order));
+    }
+
+    public function testPreparePaypalCart()
+    {
+        $expected = $this->_prepareCartExpectations($this->_paypalCartFactory);
+        $this->assertEquals($expected, $this->_model->preparePaypalCart($this->_order));
+    }
+
+    /**
+     * Prepare cart factory expectations
+     *
+     * @param \PHPUnit_Framework_MockObject_MockObject|\Magento\Payment\Model\CartFactory $cartFactory
+     * @return array
+     */
+    public function _prepareCartExpectations($cartFactory)
+    {
+        $items = [new \Magento\Object([
             'parent_item' => 'parent',
             'name' => 'name',
             'qty' => '1',
             'price' => '12.2',
             'original_item' => new \Magento\Object()
-        );
-        $cartFactory
-            ->expects($this->once())
+        ])];
+        $cart = $this->getMock('Magento\Payment\Model\Cart', [], [], '', false);
+        $cart->expects($this->once())
+            ->method('getAmounts')
+            ->will($this->returnValue(['28']));
+        $cart->expects($this->once())
+            ->method('getAllItems')
+            ->will($this->returnValue($items));
+        $cartFactory->expects($this->once())
             ->method('create')
             ->with(array('salesModel' => $this->_order))
-            ->will($this->returnValue(new \Magento\Object(['amounts' => '28', 'all_items' => $this->_allItems])));
-
-        $this->_model = new \Magento\Pbridge\Helper\Data(
-            $context, $coreStoreConfig, $customerSession, $checkoutSession, $storeManager, $locale, $layout,
-            $encryptionFactory, $appState, $cartFactory
-        );
-    }
-
-    public function testPrepareCart()
-    {
-        $this->assertEquals(array($this->_allItems, '28'), $this->_model->prepareCart($this->_order));
+            ->will($this->returnValue($cart));
+        return array_merge(['items' => [$items[0]->getData()]], ['28']);
     }
 }
