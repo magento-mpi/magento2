d419273d65c95d75d9e75d6524ca965ad924d2a7
Eugene Tulika <etulika@ebay.com>
2014-02-04 22:09:34 -0600

    Fixed MAGETWO-20253: refactor getCustomer usage in \Magento\Sales\Block\Payment\Form\Billing\Agreement - add integration test

---
+++ dev/tests/integration/testsuite/Magento/Sales/Block/Payment/Form/Billing/AgreementTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Test for \Magento\Sales\Block\Payment\Form\Billing\Agreement
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Sales\Block\Payment\Form\Billing;
+
+class AgreementTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \Magento\Sales\Block\Payment\Form\Billing\Agreement */
+    protected $_block;
+
+    protected function setUp()
+    {
+        $quote = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Sales\Model\Quote')
+            ->load(1);
+
+        /** @var \Magento\View\LayoutInterface $layout */
+        $layout = $this->getMockBuilder('Magento\View\LayoutInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $layout->expects($this->once())
+            ->method('getBlock')
+            ->will($this->returnValue(new \Magento\Object(['quote' => $quote])));
+        $layout->expects($this->once())
+            ->method('getParentName')
+            ->will($this->returnValue('billing_agreement_form'));
+
+        $this->_block = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+            ->create('Magento\Sales\Block\Payment\Form\Billing\Agreement');
+        $this->_block->setLayout($layout);
+    }
+
+    /**
+     * @magentoDataFixture Magento/Customer/_files/customer.php
+     * @magentoDataFixture Magento/Sales/_files/quote_with_customer.php
+     * @magentoDataFixture Magento/Sales/_files/billing_agreement.php
+     */
+    public function testGetBillingAgreements()
+    {
+        $billingAgreements = $this->_block->getBillingAgreements();
+        $this->assertEquals(1, count($billingAgreements));
+    }
+}
