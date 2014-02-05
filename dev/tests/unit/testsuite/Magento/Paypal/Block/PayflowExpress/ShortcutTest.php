3cfc9a276907b0e273769e1986ea8238891801cd
Iryna Savchenko <isavchenko@ebay.com>
2014-02-05 16:36:00 +0200

    Fixed MAGETWO-17929: [Payment] Remove dependency on PayPal shortcut from modules - unit tests

---
+++ dev/tests/unit/testsuite/Magento/Paypal/Block/PayflowExpress/ShortcutTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Paypal\Block\PayflowExpress;
+
+class ShortcutTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Paypal\Block\PayflowExpress\Shortcut|PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $model;
+
+    protected function setUp()
+    {
+        $context = $this->getMock('Magento\View\Element\Template\Context', [], [], '', false);
+        $paypalData = $this->getMock('Magento\Paypal\Helper\Data', [], [], '', false);
+        $paymentData = $this->getMock('Magento\Payment\Helper\Data', [], [], '', false);
+        $registry = $this->getMock('Magento\Core\Model\Registry', [], [], '', false);
+        $customerSession = $this->getMock('Magento\Customer\Model\Session', [], [], '', false);
+        $paypalConfigFactory = $this->getMock('Magento\Paypal\Model\ConfigFactory', [], [], '', false);
+        $checkoutFactory = $this->getMock('Magento\Paypal\Model\Express\Checkout\Factory', [], [], '', false);
+        $mathRandom = $this->getMock('Magento\Math\Random', [], [], '', false);
+        $productTypeConfig = $this->getMock('Magento\Catalog\Model\ProductTypes\ConfigInterface', [], [], '', false);
+        $checkoutSession = $this->getMock('Magento\Checkout\Model\Session', [], [], '', false);
+
+        $this->model = new \Magento\Paypal\Block\PayflowExpress\Shortcut(
+            $context, $paypalData, $paymentData, $registry, $customerSession, $paypalConfigFactory,
+            $checkoutFactory, $mathRandom, $productTypeConfig, $checkoutSession, []
+        );
+    }
+
+    public function testGetAlias()
+    {
+        $this->assertEquals('product.info.addtocart.payflow', $this->model->getAlias());
+    }
+}
