c17c58d30ff5d2404e1fb9315a712b0ef715b4b0
Tatiana Shevchenko <tshevchenko@ebay.com>
2014-02-06 17:29:10 +0200

    Fixed MAGETWO-20797: Add avlidation to the export Varnish config button - added js for fields validation - added values by default

---
+++ app/code/Magento/PageCache/Block/System/Config/Form/Field/Validation.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_PageCache
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\PageCache\Block\System\Config\Form\Field;
+
+/**
+ * Class Form
+ * @package Magento\PageCache\Block
+ */
+class Validation extends \Magento\View\Element\Template
+{
+}
