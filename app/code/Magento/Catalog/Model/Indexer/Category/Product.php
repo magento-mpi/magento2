90ad8e6da2ffff4a9ab44fc9b4d6ade0f1fbd7e3
Sergey Semenov <ssemenov@ebay.com>
2014-02-06 18:57:40 +0200

    Implemented MAGETWO-20810: [Dev] Create Indexer Skele†on
    - created entry point class.
    - created action classes.
    - updated indexer configuration.
    - updated mview configuration.

---
+++ app/code/Magento/Catalog/Model/Indexer/Category/Product.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Catalog\Model\Indexer\Category;
+
+class Product implements \Magento\Indexer\Model\ActionInterface, \Magento\Mview\ActionInterface
+{
+    /**
+     * Indexer ID in configuration
+     */
+    const INDEXER_ID = 'catalog_category_product';
+
+    /**
+     * @var \Magento\Catalog\Model\Indexer\Category\Product\Action\FullFactory
+     */
+    protected $fullActionFactory;
+
+    /**
+     * @var \Magento\Catalog\Model\Indexer\Category\Product\Action\RowsFactory
+     */
+    protected $rowsActionFactory;
+
+    /**
+     * @var \Magento\Indexer\Model\IndexerInterface
+     */
+    protected $indexer;
+
+    /**
+     * @param Product\Action\FullFactory $fullActionFactory
+     * @param Product\Action\RowsFactory $rowsActionFactory
+     * @param \Magento\Indexer\Model\IndexerInterface $indexer
+     */
+    public function __construct(
+        Product\Action\FullFactory $fullActionFactory,
+        Product\Action\RowsFactory $rowsActionFactory,
+        \Magento\Indexer\Model\IndexerInterface $indexer
+    ) {
+        $this->fullActionFactory = $fullActionFactory;
+        $this->rowsActionFactory = $rowsActionFactory;
+        $this->indexer = $indexer;
+    }
+
+    /**
+     * Execute materialization on ids entities
+     *
+     * @param int[] $ids
+     */
+    public function execute($ids)
+    {
+        $this->executeAction($ids);
+    }
+
+    /**
+     * Execute full indexation
+     */
+    public function executeFull()
+    {
+        $this->fullActionFactory->create()
+            ->execute();
+    }
+
+    /**
+     * Execute partial indexation by ID list
+     *
+     * @param int[] $ids
+     */
+    public function executeList($ids)
+    {
+        $this->executeAction($ids);
+    }
+
+    /**
+     * Execute partial indexation by ID
+     *
+     * @param int $id
+     */
+    public function executeRow($id)
+    {
+        $this->executeAction(array($id));
+    }
+
+    /**
+     * Execute action for single entity or list of entities
+     *
+     * @param int[] $ids
+     * @return $this
+     */
+    protected function executeAction($ids)
+    {
+        $this->indexer->load(self::INDEXER_ID);
+        if ($this->indexer->isInvalid()) {
+            return;
+        }
+
+        /** @var Product\Action\Rows $action */
+        $this->rowsActionFactory->create()
+            ->execute($ids);
+
+        return $this;
+    }
+}
---
+++ app/code/Magento/Catalog/Model/Indexer/Category/Product/Action/Full.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright {copyright}
+ * @license   {license_link}
+ */
+
+namespace Magento\Catalog\Model\Indexer\Category\Product\Action;
+
+class Full
+{
+    /**
+     * Run full reindex
+     *
+     * @return Full
+     */
+    public function reindexAll()
+    {
+        return $this;
+    }
+}
