1d43a761c222eda8c1bf66e7db320c3478e3f646
Sergey Semenov <ssemenov@ebay.com>
2014-02-06 19:10:10 +0200

    Implemented MAGETWO-20829: [Dev] Create Indexer Skele†on
    - created entry point class.
    - created action classes.
    - updated indexer configuration.
    - updated mview configuration.

---
+++ app/code/Magento/Catalog/Model/Indexer/Product/Category/Action/Rows.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright {copyright}
+ * @license   {license_link}
+ */
+
+namespace Magento\Catalog\Model\Indexer\Product\Category\Action;
+
+class Rows
+{
+    /**
+     * Refresh entities index
+     *
+     * @param int[] $entityIds
+     * @return Rows
+     */
+    public function execute(array $entityIds = array())
+    {
+        return $this;
+    }
+}
