4e845f10fe0bd53dff72fb8da645194e9e91e74c
Michail Slabko <mslabko@ebay.com>
2014-02-06 18:14:59 +0200

    Implemented MAGETWO-18268 [Checkout - Multishipping]  - MAGETWO-20682: Move method isMultishippingCheckoutAvailable from Checkout to Multishipping

---
+++ app/code/Magento/Multishipping/Helper/Data.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Multishipping\Helper;
+
+/**
+ * Data helper
+ */
+class Data extends \Magento\App\Helper\AbstractHelper
+{
+    /**#@+
+     * Xml paths for multishipping checkout
+     **/
+    const XML_PATH_CHECKOUT_MULTIPLE_AVAILABLE = 'shipping/option/checkout_multiple';
+    const XML_PATH_CHECKOUT_MULTIPLE_MAXIMUM_QUANTITY = 'shipping/option/checkout_multiple_maximum_qty';
+    /**#@-*/
+
+    /**
+     * Core store config
+     *
+     * @var \Magento\Core\Model\Store\Config
+     */
+    protected $coreStoreConfig;
+
+    /**
+     * Checkout session
+     *
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $checkoutSession;
+
+    /**
+     * Construct
+     *
+     * @param \Magento\App\Helper\Context $context
+     * @param \Magento\Core\Model\Store\Config $coreStoreConfig
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     */
+    public function __construct(
+        \Magento\App\Helper\Context $context,
+        \Magento\Core\Model\Store\Config $coreStoreConfig,
+        \Magento\Checkout\Model\Session $checkoutSession
+    ) {
+        $this->coreStoreConfig = $coreStoreConfig;
+        $this->checkoutSession = $checkoutSession;
+        parent::__construct($context);
+    }
+
+    /**
+     * Retrieve checkout quote
+     *
+     * @return \Magento\Sales\Model\Quote
+     */
+    public function getQuote()
+    {
+        return $this->checkoutSession->getQuote();
+    }
+
+    /**
+     * Check if multishipping checkout is available
+     * There should be a valid quote in checkout session. If not, only the config value will be returned
+     *
+     * @return bool
+     */
+    public function isMultishippingCheckoutAvailable()
+    {
+        $quote = $this->getQuote();
+        $isMultiShipping = $this->coreStoreConfig->getConfigFlag(self::XML_PATH_CHECKOUT_MULTIPLE_AVAILABLE);
+        if (!$quote || !$quote->hasItems()) {
+            return $isMultiShipping;
+        }
+        $maximumQty = $this->coreStoreConfig->getConfig(self::XML_PATH_CHECKOUT_MULTIPLE_MAXIMUM_QUANTITY);
+
+        return $isMultiShipping
+            && !$quote->hasItemsWithDecimalQty()
+            && $quote->validateMinimumAmount(true)
+            && ($quote->getItemsSummaryQty() - $quote->getItemVirtualQty() > 0)
+            && ($quote->getItemsSummaryQty() <= $maximumQty)
+            && !$quote->hasNominalItems();
+    }
+}
