7e331fa3951ba5a3e46226e7a767b4447d21af56
Michael Logvin <mlogvin@ebay.com>
2014-02-06 17:23:13 +0200

    fixed MAGETWO-20139: Generate vcl from template

--- dev/tests/integration/testsuite/Magento/PageCache/Controller/Adminhtml/PageCacheTest.php
+++ dev/tests/integration/testsuite/Magento/PageCache/Controller/Adminhtml/PageCacheTest.php
@@ -20,11 +20,13 @@ use Magento\TestFramework\Bootstrap;
 class PageCacheTest extends \Magento\TestFramework\TestCase\AbstractController
 {
     /**
+     * @codingStandardsIgnoreStart
      * @magentoConfigFixture current_store system/varnish_configuration_settings/ttl 360
      * @magentoConfigFixture current_store system/varnish_configuration_settings/access_list 127.0.0.1
      * @magentoConfigFixture current_store system/varnish_configuration_settings/backend_port 8080
      * @magentoConfigFixture current_store system/varnish_configuration_settings/backend_host 127.0.0.1
      * @magentoConfigFixture current_store design/theme/ua_regexp a:1:{i:0;a:2:{s:6:"regexp";s:4:"/ie/";s:5:"value";i:1;}}
+     * @codingStandardsIgnoreEnd
      */
     public function testExportVarnishConfigAction()
     {
