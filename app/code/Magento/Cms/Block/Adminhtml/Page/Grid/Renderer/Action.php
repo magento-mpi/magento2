3f551e5ba80726a763832e784757c53b123a6f9a
Yurii Torbyk <itorbyk@ebay.com>
2014-02-07 15:55:35 +0200

    Fixed MAGETWO-20057: CMS preview from grid returns 404 error

--- app/code/Magento/Cms/Block/Adminhtml/Page/Grid/Renderer/Action.php
+++ app/code/Magento/Cms/Block/Adminhtml/Page/Grid/Renderer/Action.php
@@ -10,38 +10,37 @@
 
 namespace Magento\Cms\Block\Adminhtml\Page\Grid\Renderer;
 
-class Action
-    extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer
+class Action extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer
 {
     /**
-     * @var \Magento\UrlFactory
+     * @var \Magento\Url
      */
-    protected $_urlFactory;
+    protected $urlBuilder;
 
     /**
      * @param \Magento\Backend\Block\Context $context
-     * @param \Magento\UrlFactory $urlFactory
+     * @param \Magento\Url $urlBuilder
      * @param array $data
      */
     public function __construct(
         \Magento\Backend\Block\Context $context,
-        \Magento\UrlFactory $urlFactory,
+        \Magento\Url $urlBuilder,
         array $data = array()
     ) {
-        $this->_urlFactory = $urlFactory;
+        $this->urlBuilder = $urlBuilder;
         parent::__construct($context, $data);
     }
 
     public function render(\Magento\Object $row)
     {
-        /** @var \Magento\UrlInterface $urlModel */
-        $urlModel = $this->_urlFactory->create()->setScope($row->getData('_first_store_id'));
-        $href = $urlModel->getUrl(
-            $row->getIdentifier(), array(
+        $this->urlBuilder->setScope($row->getData('_first_store_id'));
+        $href = $this->urlBuilder->getUrl(
+            $row->getIdentifier(),
+            array(
                 '_current' => false,
-                '_query' => '___store='.$row->getStoreCode()
+                '_query' => '___store=' . $row->getStoreCode()
            )
         );
-        return '<a href="'.$href.'" target="_blank">'.__('Preview').'</a>';
+        return '<a href="' . $href . '" target="_blank">' . __('Preview') . '</a>';
     }
 }
