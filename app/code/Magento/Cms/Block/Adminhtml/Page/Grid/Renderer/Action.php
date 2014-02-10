670ce8ecebfd92213999545aaea2decb82af1bda
Yurii Torbyk <itorbyk@ebay.com>
2014-02-10 10:41:51 +0200

    Fixed MAGETWO-20057: CMS preview from grid returns 404 error

--- app/code/Magento/Cms/Block/Adminhtml/Page/Grid/Renderer/Action.php
+++ app/code/Magento/Cms/Block/Adminhtml/Page/Grid/Renderer/Action.php
@@ -12,29 +12,10 @@ namespace Magento\Cms\Block\Adminhtml\Page\Grid\Renderer;
 
 class Action extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer
 {
-    /**
-     * @var \Magento\UrlInterface
-     */
-    protected $urlBuilder;
-
-    /**
-     * @param \Magento\Backend\Block\Context $context
-     * @param \Magento\UrlInterface $urlBuilder
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Backend\Block\Context $context,
-        \Magento\UrlInterface $urlBuilder,
-        array $data = array()
-    ) {
-        $this->urlBuilder = $urlBuilder;
-        parent::__construct($context, $data);
-    }
-
     public function render(\Magento\Object $row)
     {
-        $this->urlBuilder->setScope($row->getData('_first_store_id'));
-        $href = $this->urlBuilder->getUrl(
+        $this->_urlBuilder->setScope($row->getData('_first_store_id'));
+        $href = $this->_urlBuilder->getUrl(
             $row->getIdentifier(),
             array(
                 '_current' => false,
