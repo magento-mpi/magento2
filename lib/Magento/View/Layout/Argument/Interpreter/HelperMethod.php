6707344bf36e6aa08833a0f4f54f1c46efd30ccb
Sergey Shymko <sergey.shymko@magento.com>
2014-02-10 16:41:43 -0800

    Implemented MAGETWO-20211: Move Processing of Layout Arguments to Library - Added validation of method existence for the "helper method" arguments

--- lib/Magento/View/Layout/Argument/Interpreter/HelperMethod.php
+++ lib/Magento/View/Layout/Argument/Interpreter/HelperMethod.php
@@ -45,7 +45,11 @@ class HelperMethod implements InterpreterInterface
         if (!isset($data['helper']) || substr_count($data['helper'], '::') != 1) {
             throw new \InvalidArgumentException('Helper method name in format "\Class\Name::methodName" is expected.');
         }
-        list($helperClass, $methodName) = explode('::', $data['helper'], 2);
+        $helperMethod = $data['helper'];
+        list($helperClass, $methodName) = explode('::', $helperMethod, 2);
+        if (!method_exists($helperClass, $methodName)) {
+            throw new \InvalidArgumentException("Helper method '$helperMethod' does not exist.");
+        }
         $methodParams = $this->paramsInterpreter->evaluate($data);
         $methodParams = array_values($methodParams); // Use positional argument binding instead of named binding
         $helperInstance = $this->objectManager->get($helperClass);
