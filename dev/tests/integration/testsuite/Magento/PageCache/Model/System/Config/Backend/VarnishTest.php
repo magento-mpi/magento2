ae9239c55b58d9d076aaa6219586aa42b6b69ef7
Tatiana Shevchenko <tshevchenko@ebay.com>
2014-02-12 13:59:26 +0200

    Fixed MAGETWO-20797: Add validation to the export Varnish config button - fixed bamboo test

--- dev/tests/integration/testsuite/Magento/PageCache/Model/System/Config/Backend/VarnishTest.php
+++ dev/tests/integration/testsuite/Magento/PageCache/Model/System/Config/Backend/VarnishTest.php
@@ -1,12 +1,12 @@
 <?php
 /**
-* {license_notice}
-*
-* @category    Magento
-* @package     Magento_PageCache
-* @subpackage  integration_tests
-* @copyright   {copyright}
-* @license     {license_link}
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_PageCache
+ * @subpackage  integration_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
  */
 namespace Magento\PageCache\Model\System\Config\Backend;
 
