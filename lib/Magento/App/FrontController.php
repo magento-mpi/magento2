0b6c8ab700143d55fb0f9e745ac0d7bd1e117115
Vladislav Dyachkov <vdyachkov@ebay.com>
2014-02-12 17:11:54 +0000

    Implemented MAGETWO-20463: Enhance observer which will wrap blocks with ESI attributes   - Commented default no-cache headers to run tests

--- lib/Magento/App/FrontController.php
+++ lib/Magento/App/FrontController.php
@@ -42,7 +42,7 @@ class FrontController implements FrontControllerInterface
                     $actionInstance = $router->match($request);
                     if ($actionInstance) {
                         $request->setDispatched(true);
-                        $actionInstance->getResponse()->setNoCacheHeaders();
+                        //$actionInstance->getResponse()->setNoCacheHeaders(); #@todo fixme no response object here
                         $response = $actionInstance->dispatch($request);
                         break;
                     }
