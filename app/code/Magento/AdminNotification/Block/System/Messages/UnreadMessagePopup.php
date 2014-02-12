0cd02756f8d5b379e67cec98a4a69662711020fc
Jeff Farley <jfarley@ebay.com>
2014-02-12 09:44:57 -0800

    MAGETWO-20643: Annotations Sweep Module Magento\AdminNotification

--- app/code/Magento/AdminNotification/Block/System/Messages/UnreadMessagePopup.php
+++ app/code/Magento/AdminNotification/Block/System/Messages/UnreadMessagePopup.php
@@ -56,7 +56,7 @@ class UnreadMessagePopup extends \Magento\Backend\Block\Template
     /**
      * Retrieve list of unread messages
      *
-     * @return mixed
+     * @return \Magento\AdminNotification\Model\System\MessageInterface[]
      */
     public function getUnreadMessages()
     {
@@ -82,7 +82,7 @@ class UnreadMessagePopup extends \Magento\Backend\Block\Template
      * Retrieve item class by severity
      *
      * @param \Magento\AdminNotification\Model\System\MessageInterface $message
-     * @return mixed
+     * @return string
      */
     public function getItemClass(\Magento\AdminNotification\Model\System\MessageInterface $message)
     {
