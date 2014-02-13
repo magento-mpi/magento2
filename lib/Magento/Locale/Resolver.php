3ae745af8cafa0bdc830cddea12684a737899636
Pavel Yukhnevich <pyukhnevych@ebay.com>
2014-02-13 12:25:44 +0200

    Implemented MAGETWO-20694: Introduce Locale Resolver

--- lib/Magento/Locale/Resolver.php
+++ lib/Magento/Locale/Resolver.php
@@ -44,17 +44,27 @@ class Resolver implements \Magento\Locale\ResolverInterface
     protected $_app;
 
     /**
+     * Emulated locales stack
+     *
+     * @var array
+     */
+    protected $_emulatedLocales = array();
+
+    /**
      * @param \Magento\Core\Model\Store\ConfigInterface $coreStoreConfig
      * @param \Magento\AppInterface $app
-     * @param string|null $locale
+     * @param \Magento\Core\Helper\Translate $translate
+     * @param null $locale
      */
     public function __construct(
         \Magento\Core\Model\Store\ConfigInterface $coreStoreConfig,
         \Magento\AppInterface $app,
+        \Magento\Core\Helper\Translate $translate,
         $locale = null
     ) {
         $this->_app = $app;
         $this->_coreStoreConfig = $coreStoreConfig;
+        $this->_translate = $translate;
         $this->setLocale($locale);
     }
 
@@ -129,5 +139,40 @@ class Resolver implements \Magento\Locale\ResolverInterface
         $this->_localeCode = $code;
         $this->_locale = null;
         return $this;
+    }
+
+    /**
+     * Push current locale to stack and replace with locale from specified store
+     *
+     * @param int $storeId
+     */
+    public function emulate($storeId)
+    {
+        if ($storeId) {
+            $this->_emulatedLocales[] = clone $this->getLocale();
+            $this->_locale = new \Zend_Locale(
+                $this->_coreStoreConfig->getConfig(
+                    \Magento\Locale\ResolverInterface::XML_PATH_DEFAULT_LOCALE, $storeId
+                ));
+            $this->_localeCode = $this->_locale->toString();
+
+            $this->_translate->initTranslate($this->_localeCode, true);
+        } else {
+            $this->_emulatedLocales[] = false;
+        }
+    }
+
+    /**
+     * Get last locale, used before last emulation
+     */
+    public function revert()
+    {
+        $locale = array_pop($this->_emulatedLocales);
+        if ($locale) {
+            $this->_locale = $locale;
+            $this->_localeCode = $this->_locale->toString();
+
+            $this->_translate->initTranslate($this->_localeCode, true);
+        }
     }
 }
