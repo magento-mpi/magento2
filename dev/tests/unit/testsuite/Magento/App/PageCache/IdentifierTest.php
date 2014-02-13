39e1151a433ee25c4de3b124a96d75a71b21dd82
Vladislav Dyachkov <vdyachkov@ebay.com>
2014-02-13 11:50:52 +0000

    Implemented MAGETWO-21205: Fix static, unit and integration test for pagecache branch   - Fixed static tests

--- dev/tests/unit/testsuite/Magento/App/PageCache/IdentifierTest.php
+++ dev/tests/unit/testsuite/Magento/App/PageCache/IdentifierTest.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* {license_notice}
-*
-* @copyright   {copyright}
-* @license     {license_link}
-*/
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
 namespace Magento\App\PageCache;
 
 class IdentifierTest extends \PHPUnit_Framework_TestCase
