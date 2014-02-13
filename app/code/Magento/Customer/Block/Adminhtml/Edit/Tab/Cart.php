0c34940e21c9d9084cbeefbed53daf8d37cce4a4
Richard Bates <rbates@x.com>
2014-02-13 12:21:12 -0600

    MAGETWO-20751: fixed annotations

--- app/code/Magento/Customer/Block/Adminhtml/Edit/Tab/Cart.php
+++ app/code/Magento/Customer/Block/Adminhtml/Edit/Tab/Cart.php
@@ -2,20 +2,15 @@
 /**
  * {license_notice}
  *
- * @category    Magento
- * @package     Magento_Customer
  * @copyright   {copyright}
  * @license     {license_link}
  */
 
 /**
  * Adminhtml customer orders grid block
- *
- * @category   Magento
- * @package    Magento_Customer
- * @author     Magento Core Team <core@magentocommerce.com>
  */
 namespace Magento\Customer\Block\Adminhtml\Edit\Tab;
+
 use Magento\Customer\Controller\Adminhtml\Index;
 use \Magento\Directory\Model\Currency;
 
