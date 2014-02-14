6e2bbeb1409d058ee90d4f9dfd1f9f50d8ae23c4
Anton Kaplya <akaplya@ebay.com>
2014-02-14 17:18:30 +0200

    Implement MAGETWO-19694: Emulate depersonalized session object for public cache-able request

--- dev/tests/unit/testsuite/Magento/App/Action/ForwardTest.php
+++ dev/tests/unit/testsuite/Magento/App/Action/ForwardTest.php
@@ -27,7 +27,7 @@ class ForwardTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->_request = $this->getMock('Magento\App\Request\Http', array(), array(), '', false );
-        $this->_response = $this->getMock('\Magento\App\Response\Http');
+        $this->_response = $this->getMock('\Magento\App\Response\Http', array(), array(), '', false);
 
         $this->_actionAbstract = new \Magento\App\Action\Forward(
             $this->_request,
@@ -72,8 +72,9 @@ class ForwardTest extends \PHPUnit_Framework_TestCase
     {
         $infoProcessorMock = $this->getMock('Magento\App\Request\PathInfoProcessorInterface');
         $routerListMock = $this->getMock('Magento\App\Route\ConfigInterface');
+        $cookieMock = $this->getMock('Magento\Stdlib\Cookie', array(), array(), '', false);
         $request = new \Magento\App\Request\Http($routerListMock, $infoProcessorMock);
-        $response = new \Magento\App\Response\Http();
+        $response = new \Magento\App\Response\Http($cookieMock);
         $response->headersSentThrowsException = false;
         $action = new \Magento\App\Action\Forward($request, $response);
 
