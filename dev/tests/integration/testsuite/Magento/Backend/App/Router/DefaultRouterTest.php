96cffdd06cc800f68cf2c3c385544148dec91774
Yurii Torbyk <itorbyk@ebay.com>
2014-02-17 12:02:42 +0200

    Fixed MAGETWO-19581: Fatal error instead "Page not found" if entered invalid url for Reports - add test

--- dev/tests/integration/testsuite/Magento/Backend/App/Router/DefaultRouterTest.php
+++ dev/tests/integration/testsuite/Magento/Backend/App/Router/DefaultRouterTest.php
@@ -19,12 +19,17 @@ class DefaultRouterTest extends \PHPUnit_Framework_TestCase
     /**
      * @var \Magento\Backend\App\Router\DefaultRouter
      */
-    protected $_model;
+    protected $model;
+
+    /**
+     * @var \Magento\ObjectManager
+     */
+    protected $objectManager;
 
     protected function setUp()
     {
-        $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
-        $this->_model = $objectManager->create('Magento\Backend\App\Router\DefaultRouter');
+        $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $this->model = $this->objectManager->create('Magento\Backend\App\Router\DefaultRouter');
     }
 
     public function testRouterCanProcessRequestsWithProperPathInfo()
@@ -34,7 +39,7 @@ class DefaultRouterTest extends \PHPUnit_Framework_TestCase
             ->method('getPathInfo')
             ->will($this->returnValue('backend/admin/dashboard'));
 
-        $this->assertInstanceOf('Magento\Backend\Controller\Adminhtml\Dashboard', $this->_model->match($request));
+        $this->assertInstanceOf('Magento\Backend\Controller\Adminhtml\Dashboard', $this->model->match($request));
     }
 
     /**
@@ -46,7 +51,7 @@ class DefaultRouterTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetControllerClassName($module, $controller, $className)
     {
-        $this->assertEquals($className, $this->_model->getControllerClassName($module, $controller));
+        $this->assertEquals($className, $this->model->getControllerClassName($module, $controller));
     }
 
     public function getControllerClassNameDataProvider()
@@ -56,5 +61,51 @@ class DefaultRouterTest extends \PHPUnit_Framework_TestCase
             array('Magento_Index', 'process', 'Magento\Index\Controller\Adminhtml\Process'),
             array('Magento_Index_Adminhtml', 'process', 'Magento\Index\Controller\Adminhtml\Process'),
         );
+    }
+
+    /**
+     * @magentoDataFixture Magento/Backend/_files/controllerIndexClass.php
+     */
+    public function testMatchCustomNoRouteAction()
+    {
+        if (!\Magento\TestFramework\Helper\Bootstrap::canTestHeaders()) {
+            $this->markTestSkipped('Can\'t test get match without sending headers');
+        }
+
+        $routers = array(
+            'testmodule' => array(
+                'frontName' => 'testmodule',
+                'id' => 'testmodule',
+                'modules' => ['Magento_Testmodule_Adminhtml']
+            )
+        );
+
+        $routeConfig = $this->getMock(
+            'Magento\App\Route\Config',
+            ['_getRoutes'],
+            array(
+                'reader' => $this->objectManager->get('Magento\App\Route\Config\Reader'),
+                'cache' => $this->objectManager->get('Magento\Config\CacheInterface'),
+                'configScope' => $this->objectManager->get('Magento\Config\ScopeInterface'),
+                'areaList' => $this->objectManager->get('Magento\App\AreaList'),
+                'cacheId' => 'RoutesConfig',
+            )
+        );
+
+        $routeConfig->expects($this->any())
+            ->method('_getRoutes')
+            ->will($this->returnValue($routers));
+
+        $defaultRouter = $this->objectManager->create('Magento\Backend\App\Router\DefaultRouter', array(
+            'routeConfig' => $routeConfig
+        ));
+
+        /** @var $request \Magento\TestFramework\Request */
+        $request = $this->objectManager->get('Magento\TestFramework\Request');
+
+        $request->setPathInfo('backend/testmodule/test_controller');
+        $controller = $defaultRouter->match($request);
+        $this->assertInstanceOf('Magento\Testmodule\Controller\Adminhtml\Index', $controller);
+        $this->assertEquals('noroute', $request->getActionName());
     }
 }
---
+++ dev/tests/integration/testsuite/Magento/Backend/_files/controllerIndexClass.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Testmodule\Controller\Adminhtml;
+
+/**
+ * Mock index controller class
+ */
+class Index
+{
+    /**
+     * No route action
+     */
+    public function norouteAction()
+    {
+    }
+}
