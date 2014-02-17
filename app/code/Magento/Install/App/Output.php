02f4a0185df73f7c49f607138301530c52fe8be0
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2014-02-17 19:16:10 +0200

    MAGETWO-6949: Synopsis of install.php Script is incorrect fixed synopsis

--- app/code/Magento/Install/App/Output.php
+++ app/code/Magento/Install/App/Output.php
@@ -10,14 +10,83 @@ namespace Magento\Install\App;
 class Output
 {
     /**
-     * Export variable
-     *
-     * @param mixed $var
-     * @return void
+     * @param array $data
+     * @return int longest key length
      */
-    public function export($var)
+    protected function getLongestKey(array $data)
     {
-        var_export($var);
+        $longest = 0;
+        $keys = array_keys($data);
+        foreach($keys as $key) {
+            if (strlen($key) > $longest) {
+                $longest = strlen($key);
+            }
+        }
+        return $longest;
+    }
+
+    /**
+     * Make array keys aligned to the longest
+     *
+     * @param $data
+     * @return array
+     */
+    public function alignKeys($data)
+    {
+        $formattedData = array();
+        $length = $this->getLongestKey($data);
+        foreach($data as $key => $value) {
+            $formattedData[str_pad($key, $length, ' ', STR_PAD_RIGHT)] = $value;
+        }
+        return $formattedData;
+
+    }
+
+    /**
+     * Process an array to $key => $value format
+     * and adapt keys to pretty output
+     *
+     * @param array $rawData
+     * @return array
+     */
+    public function prepareArray(array $rawData)
+    {
+        $keyValData = array();
+
+        // transform data to key => value format
+        foreach ($rawData as $item) {
+            $keyValData[$item['value']] = $item['label'];
+        }
+
+        return $this->alignKeys($keyValData);
+    }
+
+    /**
+     * Make output human readable
+     *
+     * @param $var
+     */
+    public function readableOutput($var)
+    {
+        switch(true) {
+            case is_array($var):
+                $eol = '';
+                foreach($var as $key => $value) {
+                    if (is_array($value) || !is_scalar($value)) {
+                        echo $eol . $key . ' => ' . var_export($value, true);
+                    } else {
+                        echo $eol . $key . ' -- ' . $value;
+                    }
+                    $eol = ',' . PHP_EOL;
+                }
+                echo PHP_EOL;
+                break;
+            case is_scalar($var):
+                echo $var . PHP_EOL;
+                break;
+            default:
+                var_export($var, true);
+        }
     }
 
     /**
