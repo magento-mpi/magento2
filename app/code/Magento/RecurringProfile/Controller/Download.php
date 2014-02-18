03b5087d523b5fc28ba9d6f97d1e7e62aef9c685
Vlad Veselov <vveselov@ebay.com>
2014-02-18 09:00:45 -0500

    Implemented MAGETWO-18142: [Sales] Resolve dependencies of Sales on Recurring Profile - MAGETWO-20666: Resolve dependencies in controllers - cyclomatic complexity of downloadProfileCustomOptionAction() method decreased

--- app/code/Magento/RecurringProfile/Controller/Download.php
+++ app/code/Magento/RecurringProfile/Controller/Download.php
@@ -9,15 +9,13 @@ namespace Magento\RecurringProfile\Controller;
 
 class Download extends \Magento\App\Action\Action
 {
-    /**
-     * @var \Magento\Sales\Model\Download
-     */
-    protected $_download;
+    /** @var \Magento\Sales\Model\Download */
+    protected $download;
 
     public function __construct(\Magento\App\Action\Context $context, \Magento\Sales\Model\Download $download)
     {
-        $this->_download = $download;
         parent::__construct($context);
+        $this->download = $download;
     }
 
     /**
@@ -34,38 +32,42 @@ class Download extends \Magento\App\Action\Action
 
         $orderItemInfo = $recurringProfile->getData('order_item_info');
         try {
-            $request = unserialize($orderItemInfo['info_buyRequest']);
-
-            if ($request['product'] != $orderItemInfo['product_id']) {
-                $this->_forward('noroute');
-                return;
+            $buyRequest = unserialize($orderItemInfo['info_buyRequest']);
+            if ($buyRequest['product'] != $orderItemInfo['product_id']) {
+                throw new \Exception();
             }
-
-            $optionId = $this->getRequest()->getParam('option_id');
-            if (!isset($request['options'][$optionId])) {
-                $this->_forward('noroute');
-                return;
-            }
-            // Check if the product exists
-            $product = $this->_objectManager->create('Magento\Catalog\Model\Product')->load($request['product']);
-            if (!$product || !$product->getId()) {
-                $this->_forward('noroute');
-                return;
-            }
-            // Try to load the option
-            $option = $product->getOptionById($optionId);
-            if (!$option || !$option->getId() || $option->getType() != 'file') {
-                $this->_forward('noroute');
-                return;
-            }
-            $info = $request['options'][$this->getRequest()->getParam('option_id')];
-            if ($this->getRequest()->getParam('key') != $info['secret_key']) {
-                $this->_forward('noroute');
-                return;
-            }
-            $this->_download->downloadFile($info);
+            $this->download->downloadFile($this->getOptionInfo($buyRequest));
         } catch (\Exception $e) {
             $this->_forward('noroute');
         }
+    }
+
+    /**
+     * Retrieve custom option information
+     *
+     * @param array $buyRequest
+     * @return array
+     * @throws \Exception
+     */
+    protected function getOptionInfo($buyRequest)
+    {
+        $optionId = $this->getRequest()->getParam('option_id');
+        if (!isset($buyRequest['options'][$optionId])) {
+            throw new \Exception();
+        }
+        /** @var Magento\Catalog\Model\Product $product */
+        $product = $this->_objectManager->create('Magento\Catalog\Model\Product')->load($buyRequest['product']);
+        if (!$product->getId()) {
+            throw new \Exception();
+        }
+        $option = $product->getOptionById($optionId);
+        if (!$option || !$option->getId() || $option->getType() != 'file') {
+            throw new \Exception();
+        }
+        $info = $buyRequest['options'][$this->getRequest()->getParam('option_id')];
+        if ($this->getRequest()->getParam('key') != $info['secret_key']) {
+            throw new \Exception();
+        }
+        return $info;
     }
 }
