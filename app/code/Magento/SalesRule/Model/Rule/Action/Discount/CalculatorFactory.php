8a2e9858f25fd0da8e76f1e10d8292f47c45b1a6
Andrey Voinkov <avoinkov@ebay.com>
2014-02-19 11:37:43 +0200

    Fixed MAGETWO-19507: CLONE - Stop further rules processing is ignored

--- app/code/Magento/SalesRule/Model/Rule/Action/Discount/CalculatorFactory.php
+++ app/code/Magento/SalesRule/Model/Rule/Action/Discount/CalculatorFactory.php
@@ -10,8 +10,6 @@
 
 namespace Magento\SalesRule\Model\Rule\Action\Discount;
 
-use Magento\ObjectManager\Factory;
-
 class CalculatorFactory
 {
     /**
@@ -22,12 +20,12 @@ class CalculatorFactory
     private $_objectManager;
 
     protected $classByType = array(
-        \Magento\SalesRule\Model\Rule::TO_PERCENT_ACTION  => 'Magento\SalesRule\Model\Rule\Action\Discount',
-        \Magento\SalesRule\Model\Rule::BY_PERCENT_ACTION  => 'Magento\SalesRule\Model\Rule\Action\Discount',
+        \Magento\SalesRule\Model\Rule::TO_PERCENT_ACTION  => 'Magento\SalesRule\Model\Rule\Action\Discount\ToPercent',
+        \Magento\SalesRule\Model\Rule::BY_PERCENT_ACTION  => 'Magento\SalesRule\Model\Rule\Action\Discount\ByPercent',
         \Magento\SalesRule\Model\Rule::TO_FIXED_ACTION    => 'Magento\SalesRule\Model\Rule\Action\Discount\ToFixed',
-        \Magento\SalesRule\Model\Rule::BY_FIXED_ACTION    => 'Magento\SalesRule\Model\Rule\Action\Discount',
-        \Magento\SalesRule\Model\Rule::CART_FIXED_ACTION  => 'Magento\SalesRule\Model\Rule\Action\Discount',
-        \Magento\SalesRule\Model\Rule::BUY_X_GET_Y_ACTION => 'Magento\SalesRule\Model\Rule\Action\Discount',
+        \Magento\SalesRule\Model\Rule::BY_FIXED_ACTION    => 'Magento\SalesRule\Model\Rule\Action\Discount\ByFixed',
+        \Magento\SalesRule\Model\Rule::CART_FIXED_ACTION  => 'Magento\SalesRule\Model\Rule\Action\Discount\CartFixed',
+        \Magento\SalesRule\Model\Rule::BUY_X_GET_Y_ACTION => 'Magento\SalesRule\Model\Rule\Action\Discount\BuyXGetY',
     );
 
     /**
