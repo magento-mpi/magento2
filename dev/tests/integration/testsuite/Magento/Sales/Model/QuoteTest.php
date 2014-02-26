89bc4087228501c12e88ec05975618a160caf21d
Alexander Paliarush <apaliarush@ebay.com>
2014-02-26 13:47:03 +0200

    Implemented MAGETWO-21534: Refactor \Magento\Sales\Model\Quote - getCustomerTaxClassId, getCustomerGroupId, assignCustomerWithAddressChange methods of \Magento\Sales\Model\Quote covered with tests

--- dev/tests/integration/testsuite/Magento/Sales/Model/QuoteTest.php
+++ dev/tests/integration/testsuite/Magento/Sales/Model/QuoteTest.php
@@ -81,6 +81,214 @@ class QuoteTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('test@example.com', $quote->getCustomerEmail());
     }
 
+    /**
+     * Customer data is set to quote (which contains valid group ID).
+     */
+    public function testGetCustomerGroupFromCustomer()
+    {
+        /** Preconditions */
+        /** @var \Magento\Customer\Service\V1\Data\CustomerBuilder $customerBuilder */
+        $customerBuilder = Bootstrap::getObjectManager()->create('Magento\Customer\Service\V1\Data\CustomerBuilder');
+        $customerGroupId = 3;
+        $customerData = $customerBuilder->setId(1)->setGroupId($customerGroupId)->create();
+        /** @var \Magento\Sales\Model\Quote $quote */
+        $quote = Bootstrap::getObjectManager()->create('Magento\Sales\Model\Quote');
+        $quote->setCustomerData($customerData);
+        $quote->unsetData('customer_group_id');
+
+        /** Execute SUT */
+        $this->assertEquals(
+            $customerGroupId,
+            $quote->getCustomerGroupId(),
+            "Customer group ID is invalid"
+        );
+    }
+
+    /**
+     * @magentoDataFixture Magento/Customer/_files/customer_group.php
+     */
+    public function testGetCustomerTaxClassId()
+    {
+        /**
+         * Preconditions: create quote and assign ID of customer group created in fixture to it.
+         */
+        $fixtureGroupCode = 'custom_group';
+        $fixtureTaxClassId = 3;
+        /** @var \Magento\Customer\Model\Group $group */
+        $group = Bootstrap::getObjectManager()->create('Magento\Customer\Model\Group');
+        $fixtureGroupId = $group->load($fixtureGroupCode, 'customer_group_code')->getId();
+        /** @var \Magento\Sales\Model\Quote $quote */
+        $quote = Bootstrap::getObjectManager()->create('Magento\Sales\Model\Quote');
+        $quote->setCustomerGroupId($fixtureGroupId);
+
+        /** Execute SUT */
+        $this->assertEquals($fixtureTaxClassId, $quote->getCustomerTaxClassId(), 'Customer tax class ID is invalid.');
+    }
+
+    /**
+     * Billing and shipping address arguments are not passed, customer has default billing and shipping addresses.
+     *
+     * @magentoDataFixture Magento/Customer/_files/customer.php
+     * @magentoDataFixture Magento/Customer/_files/customer_address.php
+     * @magentoDataFixture Magento/Customer/_files/customer_two_addresses.php
+     */
+    public function testAssignCustomerWithAddressChangeAddressesNotSpecified()
+    {
+        /** Preconditions:
+         * Customer with two addresses created
+         * First address is default billing, second is default shipping.
+         */
+        /** @var \Magento\Sales\Model\Quote $quote */
+        $quote = Bootstrap::getObjectManager()->create('Magento\Sales\Model\Quote');
+        $customerData = $this->_prepareQuoteForTestAssignCustomerWithAddressChange($quote);
+
+        /** Execute SUT */
+        $quote->assignCustomerWithAddressChange($customerData);
+
+        /** Check if SUT caused expected effects */
+        $fixtureCustomerId = 1;
+        $this->assertEquals($fixtureCustomerId, $quote->getCustomerId(), 'Customer ID in quote is invalid.');
+        $expectedBillingAddressData = [
+            'street' => 'Green str, 67',
+            'telephone' => 3468676,
+            'postcode' => 75477,
+            'country_id' => 'US',
+            'city' => 'CityM',
+            'lastname' => 'Smith',
+            'firstname' => 'John',
+            'customer_id' => 1,
+            'customer_address_id' => 1,
+            'region_id' => 1
+        ];
+        $billingAddress = $quote->getBillingAddress();
+        foreach ($expectedBillingAddressData as $field => $value) {
+            $this->assertEquals(
+                $value,
+                $billingAddress->getData($field),
+                "'{$field}' value in quote billing address is invalid."
+            );
+        }
+        $expectedShippingAddressData = [
+            'customer_address_id' => 2,
+            'telephone' => 3234676,
+            'postcode' => 47676,
+            'country_id' => 'US',
+            'city' => 'CityX',
+            'street' => 'Black str, 48',
+            'lastname' => 'Smith',
+            'firstname' => 'John',
+            'customer_id' => 1,
+            'region_id' => 1
+        ];
+        $shippingAddress = $quote->getShippingAddress();
+        foreach ($expectedShippingAddressData as $field => $value) {
+            $this->assertEquals(
+                $value,
+                $shippingAddress->getData($field),
+                "'{$field}' value in quote shipping address is invalid."
+            );
+        }
+    }
+
+    /**
+     * Billing and shipping address arguments are passed, customer has default billing and shipping addresses.
+     *
+     * @magentoDataFixture Magento/Customer/_files/customer.php
+     * @magentoDataFixture Magento/Customer/_files/customer_address.php
+     * @magentoDataFixture Magento/Customer/_files/customer_two_addresses.php
+     */
+    public function testAssignCustomerWithAddressChange()
+    {
+        /** Preconditions:
+         * Customer with two addresses created
+         * First address is default billing, second is default shipping.
+         */
+        /** @var \Magento\Sales\Model\Quote $quote */
+        $objectManager = Bootstrap::getObjectManager();
+        $quote = $objectManager->create('Magento\Sales\Model\Quote');
+        $customerData = $this->_prepareQuoteForTestAssignCustomerWithAddressChange($quote);
+        /** @var \Magento\Sales\Model\Quote\Address $quoteBillingAddress */
+        $expectedBillingAddressData = [
+            'street' => 'Billing str, 67',
+            'telephone' => 16546757,
+            'postcode' => 2425457,
+            'country_id' => 'US',
+            'city' => 'CityBilling',
+            'lastname' => 'LastBilling',
+            'firstname' => 'FirstBilling',
+            'region_id' => 1
+        ];
+        $quoteBillingAddress = $objectManager->create('Magento\Sales\Model\Quote\Address');
+        $quoteBillingAddress->setData($expectedBillingAddressData);
+
+        $expectedShippingAddressData = [
+            'telephone' => 787878787,
+            'postcode' => 117785,
+            'country_id' => 'US',
+            'city' => 'CityShipping',
+            'street' => 'Shipping str, 48',
+            'lastname' => 'LastShipping',
+            'firstname' => 'FirstShipping',
+            'region_id' => 1
+        ];
+        $quoteShippingAddress = $objectManager->create('Magento\Sales\Model\Quote\Address');
+        $quoteShippingAddress->setData($expectedShippingAddressData);
+
+        /** Execute SUT */
+        $quote->assignCustomerWithAddressChange($customerData, $quoteBillingAddress, $quoteShippingAddress);
+
+        /** Check if SUT caused expected effects */
+        $fixtureCustomerId = 1;
+        $this->assertEquals($fixtureCustomerId, $quote->getCustomerId(), 'Customer ID in quote is invalid.');
+
+        $billingAddress = $quote->getBillingAddress();
+        foreach ($expectedBillingAddressData as $field => $value) {
+            $this->assertEquals(
+                $value,
+                $billingAddress->getData($field),
+                "'{$field}' value in quote billing address is invalid."
+            );
+        }
+        $shippingAddress = $quote->getShippingAddress();
+        foreach ($expectedShippingAddressData as $field => $value) {
+            $this->assertEquals(
+                $value,
+                $shippingAddress->getData($field),
+                "'{$field}' value in quote shipping address is invalid."
+            );
+        }
+    }
+
+    /**
+     * Prepare quote for testing assignCustomerWithAddressChange method.
+     *
+     * Customer with two addresses created. First address is default billing, second is default shipping.
+     *
+     * @param \Magento\Sales\Model\Quote $quote
+     * @return \Magento\Customer\Service\V1\Data\Customer
+     */
+    protected function _prepareQuoteForTestAssignCustomerWithAddressChange($quote)
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        /** @var \Magento\Customer\Service\V1\CustomerServiceInterface $customerService */
+        $customerService = $objectManager->create('Magento\Customer\Service\V1\CustomerServiceInterface');
+        $fixtureCustomerId = 1;
+        /** @var \Magento\Customer\Model\Customer $customer */
+        $customer = $objectManager->create('Magento\Customer\Model\Customer');
+        $fixtureSecondAddressId = 2;
+        $customer->load($fixtureCustomerId)->setDefaultShipping($fixtureSecondAddressId)->save();
+        $customerData = $customerService->getCustomer($fixtureCustomerId);
+        $this->assertEmpty(
+            $quote->getBillingAddress()->getId(),
+            "Precondition failed: billing address should be empty."
+        );
+        $this->assertEmpty(
+            $quote->getShippingAddress()->getId(),
+            "Precondition failed: shipping address should be empty."
+        );
+        return $customerData;
+    }
+
     protected function _getCustomerDataArray()
     {
         return [
