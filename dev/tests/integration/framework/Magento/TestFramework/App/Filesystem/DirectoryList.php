b6ce01c9bcce9f90b784d5bf806b2b1f3c11d4fe
Yurii Torbyk <itorbyk@ebay.com>
2014-03-04 10:06:31 +0200

    Fixed MAGETWO-21315: [GITHUB] Params passed in pub/index.php being overwritten

--- dev/tests/integration/framework/Magento/TestFramework/App/Filesystem/DirectoryList.php
+++ dev/tests/integration/framework/Magento/TestFramework/App/Filesystem/DirectoryList.php
@@ -14,6 +14,7 @@ class DirectoryList extends \Magento\App\Filesystem\DirectoryList
      *
      * @param string $code
      * @return bool
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function isConfigured($code)
     {
