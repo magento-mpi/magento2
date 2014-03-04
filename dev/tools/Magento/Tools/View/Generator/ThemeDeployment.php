3bb20692bd57816e286749fe304b3172d93ecde4
Sergey Shymko <sergey.shymko@magento.com>
2014-03-04 10:00:57 -0800

    Implemented MAGETWO-21199: Sign Static Files with Global Version Instead of Individual Timestamps - Implemented skipping of the deployment file creation by the view files population tool in the "dry run" mode - Implemented missing unit test for Magento\App\View\Deployment\Version\Storage\File::save() - Fixed excessive public visibility of Magento\App\View\Deployment\Version::readValue() - Fixed misleading fixture of returned value in the unit test Magento\App\View\Deployment\Version\Storage\FileTest

--- dev/tools/Magento/Tools/View/Generator/ThemeDeployment.php
+++ dev/tools/Magento/Tools/View/Generator/ThemeDeployment.php
@@ -157,7 +157,9 @@ class ThemeDeployment
             );
         }
 
-        $this->_versionStorage->save($this->_versionGenerator->generate());
+        if (!$this->_isDryRun) {
+            $this->_versionStorage->save($this->_versionGenerator->generate());
+        }
     }
 
 
