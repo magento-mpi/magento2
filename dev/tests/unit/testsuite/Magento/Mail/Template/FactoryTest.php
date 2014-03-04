babb68ad288c4a9560bee77b1964cecbbacbe4b5
Oleh Kobchenko <okobchenko@ebay.com>
2014-03-04 12:23:07 +0200

    Implemented MAGETWO-21901: Coverage of Mail classes

---
+++ dev/tests/unit/testsuite/Magento/Mail/Template/FactoryTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Mail\Template;
+
+class FactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Mail\Template\Factory
+     */
+    protected $_factory;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject
+     */
+    protected $_objectManagerMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject
+     */
+    protected $_templateMock;
+
+    public function setUp()
+    {
+        $this->_objectManagerMock = $this->getMock('\Magento\ObjectManager');
+        $this->_templateMock = $this->getMock('\Magento\Mail\TemplateInterface');
+        $this->_factory = new \Magento\Mail\Template\Factory($this->_objectManagerMock);
+    }
+
+    /**
+     * @covers \Magento\Mail\Template\Factory::get
+     */
+    public function testGet()
+    {
+        $this->_objectManagerMock->expects($this->once())
+            ->method('create')
+            ->with('Magento\Mail\TemplateInterface', array('data' => array('template_id' => 'identifier')))
+            ->will($this->returnValue($this->_templateMock));
+
+        $this->assertInstanceOf('\Magento\Mail\TemplateInterface', $this->_factory->get('identifier'));
+    }
+}
