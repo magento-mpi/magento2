239ad293f77f346c1a606d09d512f4db96af170a
Andriy Nasinnyk <anasinnyk@ebay.com>
2014-03-04 16:55:32 +0200

    Implemented MAGETWO-21682: [Dev] Implement row re-indexation on product change

--- app/code/Magento/CatalogPermissions/Model/Indexer/Plugin/Import.php
+++ app/code/Magento/CatalogPermissions/Model/Indexer/Plugin/Import.php
@@ -3,7 +3,7 @@
  * {license_notice}
  *
  * @category    Magento
- * @package     Magento_Catalog
+ * @package     Magento_CatalogPermissions
  * @copyright   {copyright}
  * @license     {license_link}
  */
@@ -12,5 +12,41 @@ namespace Magento\CatalogPermissions\Model\Indexer\Plugin;
 
 class Import
 {
+    /**
+     * @var \Magento\CatalogPermissions\Model\Indexer\Product
+     */
+    protected $productIndexer;
 
+    /**
+     * @var \Magento\CatalogPermissions\Model\Indexer\Category
+     */
+    protected $categoryIndexer;
+
+    /**
+     * @param \Magento\CatalogPermissions\Model\Indexer\Product $productIndexer
+     * @param \Magento\CatalogPermissions\Model\Indexer\Category $categoryIndexer
+     */
+    public function __construct(
+        \Magento\CatalogPermissions\Model\Indexer\Product $productIndexer,
+        \Magento\CatalogPermissions\Model\Indexer\Category $categoryIndexer
+    ) {
+        $this->productIndexer = $productIndexer;
+        $this->categoryIndexer = $categoryIndexer;
+    }
+
+    /**
+     * After improt handler
+     *
+     * @param \Magento\ImportExport\Model\Import $subject
+     * @param Object $import
+     *
+     * @return mixed
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterImportSource(\Magento\ImportExport\Model\Import $subject, $import)
+    {
+        $this->productIndexer->markIndexerAsInvalid();
+        $this->categoryIndexer->markIndexerAsInvalid();
+        return $import;
+    }
 }
