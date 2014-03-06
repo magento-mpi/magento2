8f23c45ddbde8043f98f1e7eafa78f977962e7e0
Tatiana Shevchenko <tshevchenko@ebay.com>
2014-03-06 23:53:14 +0200

    Fixed MAGETWO-21644: No caching should be performed while work in design editor - fixed fatal error on design editor page

--- app/code/Magento/DesignEditor/Controller/Varien/Router/Standard.php
+++ app/code/Magento/DesignEditor/Controller/Varien/Router/Standard.php
@@ -157,10 +157,11 @@ class Standard extends \Magento\Core\App\Router\Base
      */
     protected function _getMatchedRouters()
     {
-        $routers = $this->_routerList->getRouters();
-        foreach (array_keys($routers) as $name) {
-            if (in_array($name, $this->_excludedRouters)) {
-                unset($routers[$name]);
+        $routers = [];
+        foreach ($this->_routerList as $router) {
+            $name = $this->_routerList->key();
+            if (!in_array($name, $this->_excludedRouters)) {
+                $routers[$name] = $router;
             }
         }
         return $routers;
