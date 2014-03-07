36d4ee756372d2f1c007d0ddc38c48c0c46d5129
Andriy Nasinnyk <anasinnyk@ebay.com>
2014-03-07 17:32:00 +0200

    Implemented MAGETWO-21669: [Dev] Create tests to cover Catalog Category/Product Permissions Indexer

--- dev/tests/integration/testsuite/Magento/CatalogPermissions/Model/Indexer/CategoryTest.php
+++ dev/tests/integration/testsuite/Magento/CatalogPermissions/Model/Indexer/CategoryTest.php
@@ -101,39 +101,6 @@ class CategoryTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @test
-     *
-     * @magentoAppArea adminhtml
-     * @magentoConfigFixture current_store catalog/magento_catalogpermissions/enabled 1
-     * @magentoDataFixture Magento/Catalog/_files/categories.php
-     * @magentoDataFixture Magento/CatalogPermissions/_files/permission.php
-     */
-    public function testCategoryDelete()
-    {
-        $this->category->setId(13)
-            ->setName('New')
-            ->setParentId(6)
-            ->setPath('1/2/6/13')
-            ->setLevel(3)
-            ->setAvailableSortBy('name')
-            ->setDefaultSortBy('name')
-            ->setIsActive(true)
-            ->setPosition(3)
-            ->save();
-
-        var_dump($this->category->getData());
-        $this->assertContains(
-            $this->getCategoryDataById(13),
-            $this->indexTable->getIndexForCategory(13)
-        );
-
-        $this->category->load(13);
-        var_dump($this->category->getData());
-        $this->category->delete();
-        $this->assertEmpty($this->indexTable->getIndexForCategory(13));
-    }
-
-    /**
      * Return default row from permission by category id
      *
      * @param int $id
