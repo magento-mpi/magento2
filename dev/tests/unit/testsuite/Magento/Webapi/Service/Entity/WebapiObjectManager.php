2f67a6b3eabfb785bc4b7b74732b6f4b86c5e0bb
Eugene Tulika <etulika@ebay.com>
2014-03-08 15:09:55 -0600

    MAGETWO-22127: fix tests - make fixes to unit tests

---
+++ dev/tests/unit/testsuite/Magento/Webapi/Service/Entity/WebapiObjectManager.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Webapi\Service\Entity;
+
+class WebapiObjectManager implements \Magento\ObjectManager
+{
+    /**
+     * Create new object instance
+     *
+     * @param string $type
+     * @param array $arguments
+     * @return mixed
+     */
+    public function create($type, array $arguments = array())
+    {
+        return new $type();
+    }
+
+    /**
+     * Retrieve cached object instance
+     *
+     * @param string $type
+     * @return mixed
+     */
+    public function get($type)
+    {
+        return new $type;
+    }
+
+    /**
+     * Configure object manager
+     *
+     * @param array $configuration
+     * @return void
+     */
+    public function configure(array $configuration)
+    {
+
+    }
+}
