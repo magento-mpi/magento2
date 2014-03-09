693f48647475ec4c9879237ebaa3cc1ce8376745
Anup Dugar <anup@x.com>
2014-03-08 18:19:59 -0600

    MAGETWO-22128: Webapi framework does not convert scalar types from request to the method input type - Fixed typo

--- app/code/Magento/Webapi/Model/Config/ClassReflector/TypeProcessor.php
+++ app/code/Magento/Webapi/Model/Config/ClassReflector/TypeProcessor.php
@@ -374,15 +374,15 @@ class TypeProcessor
             $arrayItemType = $this->getArrayItemType($type);
             foreach ($value as $item) {
                 if (!settype($item, $arrayItemType)) {
-                    throw new \Magento\Webapi\Exception(sprintf($invalidTypeMsg), $value, $type);
+                    throw new \Magento\Webapi\Exception(sprintf($invalidTypeMsg, $value, $type));
                 }
             }
         } elseif (!$this->isArrayType($type) && !is_array($value)) {
             if (!settype($value, $type)) {
-                throw new \Magento\Webapi\Exception(sprintf($invalidTypeMsg), $value, $type);
+                throw new \Magento\Webapi\Exception(sprintf($invalidTypeMsg, $value, $type));
             }
         } else {
-            throw new \Magento\Webapi\Exception(sprintf($invalidTypeMsg), $value, $type);
+            throw new \Magento\Webapi\Exception(sprintf($invalidTypeMsg, $value, $type));
         }
         return $value;
     }
