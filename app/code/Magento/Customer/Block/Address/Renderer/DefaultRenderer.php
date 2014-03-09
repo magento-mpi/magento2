6513222d2b1c4aa39f41efe3a5a5d37bb8d404b2
Eugene Tulika <etulika@ebay.com>
2014-03-08 21:44:27 -0600

    MAGETWO-22127: fix tests

--- app/code/Magento/Customer/Block/Address/Renderer/DefaultRenderer.php
+++ app/code/Magento/Customer/Block/Address/Renderer/DefaultRenderer.php
@@ -169,7 +169,7 @@ class DefaultRenderer
             $attributeCode = $attributeMetadata->getAttributeCode();
             if ($attributeCode == 'country_id'
                 && isset($addressAttributes['country_id'])
-                && $addressAttributes['country_id'] <> 0
+                && $addressAttributes['country_id'] != 0
             ) {
                 $data['country'] = $this->_countryFactory->create()
                     ->loadByCode($addressAttributes['country_id'])
