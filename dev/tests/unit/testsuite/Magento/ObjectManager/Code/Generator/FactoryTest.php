7fee2e63c2f4cb877cf988b5b7beee2e09b4daf5
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2014-03-11 10:21:10 +0200

    MAGETWO-21899: Coverage of ObjectManager classes improved Factory and Proxy tests

--- dev/tests/unit/testsuite/Magento/ObjectManager/Code/Generator/FactoryTest.php
+++ dev/tests/unit/testsuite/Magento/ObjectManager/Code/Generator/FactoryTest.php
@@ -15,82 +15,23 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
      */
     protected $ioObjectMock;
 
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $classGeneratorMock;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $autoloaderMock;
-
     protected function setUp()
     {
         $this->ioObjectMock = $this->getMock('\Magento\Code\Generator\Io', array(), array(), '', false);
-        $this->classGeneratorMock = $this->getMock('\Magento\Code\Generator\CodeGenerator\CodeGeneratorInterface',
-            array(), array(), '', false);
-        $this->autoloaderMock = $this->getMock('\Magento\Autoload\IncludePath', array(), array(), '', false);
     }
 
     public function testGenerate()
     {
-        $classProperties = array (
-            0 =>
-                array (
-                    'name' => '_objectManager',
-                    'visibility' => 'protected',
-                    'docblock' =>
-                        array (
-                            'shortDescription' => 'Object Manager instance',
-                            'tags' =>
-                                array (
-                                    0 =>
-                                        array (
-                                            'name' => 'var',
-                                            'description' => '\\Magento\\ObjectManager',
-                                        ),
-                                ),
-                        ),
-                ),
-            1 =>
-                array (
-                    'name' => '_instanceName',
-                    'visibility' => 'protected',
-                    'docblock' =>
-                        array (
-                            'shortDescription' => 'Instance name to create',
-                            'tags' =>
-                                array (
-                                    0 =>
-                                        array (
-                                            'name' => 'var',
-                                            'description' => 'string',
-                                        ),
-                                ),
-                        ),
-                ),
-        );
-
         $model = $this->getMock('\Magento\ObjectManager\Code\Generator\Factory',
             array('_validateData'),
-            array('Exception', null, $this->ioObjectMock, $this->classGeneratorMock, $this->autoloaderMock)
+            array('Exception', null, $this->ioObjectMock, null, null)
         );
-        $this->classGeneratorMock->expects($this->once())->method('setName')->with('ExceptionFactory')
-            ->will($this->returnValue($this->classGeneratorMock));
-        $this->classGeneratorMock->expects($this->once())->method('addProperties')->with($classProperties)
-            ->will($this->returnValue($this->classGeneratorMock));
-        $this->classGeneratorMock->expects($this->once())->method('addMethods')
-            ->will($this->returnValue($this->classGeneratorMock));
-        $this->classGeneratorMock->expects($this->once())->method('setClassDocBlock')
-            ->will($this->returnValue($this->classGeneratorMock));
-        $this->classGeneratorMock->expects($this->once())->method('generate')
-            ->will($this->returnValue('source code example'));
-
+        $exceptionFactoryCode = file_get_contents(__DIR__ . '/fixture/ExceptionFactory.txt');
+        
         $this->ioObjectMock->expects($this->once())->method('getResultFileName')
             ->with('ExceptionFactory')->will($this->returnValue('sample_file.php'));
         $this->ioObjectMock->expects($this->once())->method('writeResultFile')
-            ->with('sample_file.php', 'source code example');
+            ->with('sample_file.php', $exceptionFactoryCode);
 
         $model->expects($this->once())->method('_validateData')->will($this->returnValue(true));
         $this->assertTrue($model->generate());
--- dev/tests/unit/testsuite/Magento/ObjectManager/Code/Generator/ProxyTest.php
+++ dev/tests/unit/testsuite/Magento/ObjectManager/Code/Generator/ProxyTest.php
@@ -15,47 +15,24 @@ class ProxyTest extends \PHPUnit_Framework_TestCase
      */
     protected $ioObjectMock;
 
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $classGeneratorMock;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $autoloaderMock;
-
     protected function setUp()
     {
         $this->ioObjectMock = $this->getMock('\Magento\Code\Generator\Io', array(), array(), '', false);
-        $this->classGeneratorMock = $this->getMock('\Magento\Code\Generator\CodeGenerator\CodeGeneratorInterface',
-            array(), array(), '', false);
-        $this->autoloaderMock = $this->getMock('\Magento\Autoload\IncludePath', array(), array(), '', false);
     }
 
     public function testGenerate()
     {
+
         $model = $this->getMock('\Magento\ObjectManager\Code\Generator\Proxy',
             array('_validateData'),
-            array('Exception', null, $this->ioObjectMock, $this->classGeneratorMock, $this->autoloaderMock)
+            array('Exception', null, $this->ioObjectMock, null, null)
         );
-
-        $this->classGeneratorMock->expects($this->once())->method('setExtendedClass')->with('\Exception');
-        $this->classGeneratorMock->expects($this->once())->method('setName')->with('Exception_Proxy')
-            ->will($this->returnValue($this->classGeneratorMock));
-        $this->classGeneratorMock->expects($this->once())->method('addProperties')
-            ->will($this->returnValue($this->classGeneratorMock));
-        $this->classGeneratorMock->expects($this->once())->method('addMethods')
-            ->will($this->returnValue($this->classGeneratorMock));
-        $this->classGeneratorMock->expects($this->once())->method('setClassDocBlock')
-            ->will($this->returnValue($this->classGeneratorMock));
-        $this->classGeneratorMock->expects($this->once())->method('generate')
-            ->will($this->returnValue('source code example'));
-
+        $exceptionProxyCode = file_get_contents(__DIR__ . '/fixture/ExceptionProxy.txt');
+        
         $this->ioObjectMock->expects($this->once())->method('getResultFileName')
             ->with('Exception_Proxy')->will($this->returnValue('sample_file.php'));
         $this->ioObjectMock->expects($this->once())->method('writeResultFile')
-            ->with('sample_file.php', 'source code example');
+            ->with('sample_file.php', $exceptionProxyCode);
 
         $model->expects($this->once())->method('_validateData')->will($this->returnValue(true));
         $this->assertTrue($model->generate());
