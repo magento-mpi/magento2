1ff9a4d528c4cba1ef95a804cae183c7e5b347f4
Andriy Nasinnyk <anasinnyk@ebay.com>
2014-03-12 16:50:44 +0200

    Implemented MAGETWO-22247: Increase coverage of new and legacy code with unit tests for lib / Magento / Controller

---
+++ dev/tests/unit/testsuite/Magento/Controller/Router/Route/FactoryTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Framework
+ * @subpackage  unit_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Controller\Router\Route;
+
+class FactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|\Magento\ObjectManager
+     */
+    protected $objectManager;
+
+    public function setUp()
+    {
+        $this->objectManager = $this->getMockBuilder('Magento\ObjectManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @test
+     */
+    public function testCreateRoute()
+    {
+        $routerClass = 'router';
+
+        $router = $this->getMockBuilder('Zend_Controller_Router_Route_Interface')
+            ->setMockClassName($routerClass)
+            ->getMock();
+
+        $parameterRoute    = 'route';
+        $parameterDefaults = 'defaults';
+        $parameterRegs     = 'regs';
+        $parameterLocale   = 'locale';
+
+        $this->objectManager->expects($this->once())
+            ->method('create')
+            ->with(
+                $this->logicalOr(
+                    $routerClass,
+                    array(
+                        'route'    => $parameterRoute,
+                        'defaults' => $parameterDefaults,
+                        'regs'     => $parameterRegs,
+                        'locale'   => $parameterLocale,
+                    )
+                )
+            )
+            ->will($this->returnValue($router));
+
+        $object = new \Magento\Controller\Router\Route\Factory($this->objectManager);
+        $expectedRouter = $object->createRoute(
+            $routerClass,
+            $parameterRoute,
+            $parameterDefaults,
+            $parameterRegs,
+            $parameterLocale
+        );
+
+        $this->assertInstanceOf($routerClass, $expectedRouter);
+        $this->assertInstanceOf('Zend_Controller_Router_Route_Interface', $expectedRouter);
+    }
+
+    /**
+     * @test
+     * @expectedException \LogicException
+     */
+    public function testCreateRouteNegative()
+    {
+        $this->objectManager->expects($this->once())
+            ->method('create')
+            ->will($this->returnValue($this->getMockBuilder('StdClass')->getMock()));
+
+        $object = new \Magento\Controller\Router\Route\Factory($this->objectManager);
+        $object->createRoute(
+            'routerClass',
+            'router'
+        );
+    }
+}
