b47a7b0e874560daf49de25df8a239b28fad61e6
Stanislav Lopukhov <slopukhov@ebay.com>
2014-03-18 15:32:18 +0200

    MAGETWO-22405: Unit tests, add new unit testsuite to dev build for bamboo

---
+++ dev/tools/toolkit/framework/tests/unit/testsuite/Magento/Test/Helper/CliTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     performance_tests
+ * @subpackage  unit_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Test\Helper;
+
+/**
+ * Class CliTest
+ *
+ * @package Magento\Test\Helper
+ */
+class CliTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Getopt object
+     *
+     * @var \Zend_Console_Getopt
+     */
+    protected $_getOpt;
+
+    /**
+     * Param constants
+     */
+    const TEST_OPTION_NAME  = 'name';
+    const TEST_OPTION_VALUE = 'test_option_value';
+
+    /**
+     * Set up before test
+     */
+    public function setUp()
+    {
+
+        $this->_getOpt =  $this->getMock(
+            'Zend_Console_Getopt',
+            array('getOption'),
+            array(array())
+        );
+        $this->_getOpt->expects($this->any())->method('getOption')->will($this->returnValueMap(
+            array(
+                array(self::TEST_OPTION_NAME, self::TEST_OPTION_VALUE),
+                array('xxx', null),
+            )
+        ));
+
+        \Magento\ToolkitFramework\Helper\Cli::setOpt($this->_getOpt);
+    }
+
+    /**
+     * Tesr down after test
+     */
+    public function tearDown()
+    {
+        $this->_getOpt = null;
+        $this->_object = null;
+    }
+
+    /**
+     * Test CLI helper
+     */
+    public function testCli()
+    {
+        $this->assertEquals(
+            self::TEST_OPTION_VALUE,
+            \Magento\ToolkitFramework\Helper\Cli::getOption(self::TEST_OPTION_NAME)
+        );
+        $this->assertEquals(
+            null,
+            \Magento\ToolkitFramework\Helper\Cli::getOption('xxx')
+        );
+        $this->assertEquals(
+            'default',
+            \Magento\ToolkitFramework\Helper\Cli::getOption('xxx', 'default')
+        );
+    }
+}
