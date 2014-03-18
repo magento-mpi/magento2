5b95cdfeb30b27163de5de527bba426541aab137
Vladimir Tarasov <vtarasov@ebay.com>
2014-03-18 11:19:46 +0200

    Fixed MAGETWO-22278: Stage 2. Fix integration tests on L2

--- dev/tests/integration/testsuite/Magento/Catalog/Block/Product/NewTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Block/Product/NewTest.php
@@ -27,6 +27,12 @@ class NewTest extends \PHPUnit_Framework_TestCase
     {
         \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Core\Model\App')
             ->loadArea(\Magento\Core\Model\App\Area::AREA_FRONTEND);
+        \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\App\Http\Context')
+            ->setValue(
+                \Magento\Customer\Helper\Data::CONTEXT_GROUP,
+                \Magento\Customer\Model\Group::NOT_LOGGED_IN_ID,
+                \Magento\Customer\Model\Group::NOT_LOGGED_IN_ID
+            );
         $this->_block = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\View\LayoutInterface')
             ->createBlock('Magento\Catalog\Block\Product\NewProduct');
     }
