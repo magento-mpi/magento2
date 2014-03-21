32f96522018e112607446285149312ba66a0d3da
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2014-03-21 18:08:35 +0200

    MAGETWO-22235: Stabilize builds
    - fixed integration tests

--- dev/tests/integration/testsuite/Magento/Data/Argument/Interpreter/StringTest.php
+++ dev/tests/integration/testsuite/Magento/Data/Argument/Interpreter/StringTest.php
@@ -11,7 +11,7 @@ namespace Magento\Data\Argument\Interpreter;
 class StringTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var Number
+     * @var \Magento\Data\Argument\Interpreter\String
      */
     protected $_model;
 
@@ -36,7 +36,7 @@ class StringTest extends \PHPUnit_Framework_TestCase
         $translateRenderer->expects($this->any())
             ->method('render')
             ->will($this->returnCallback(function ($input) {
-                return $input . ' (translated)';
+                return end($input) . ' (translated)';
             }));
         \Magento\Phrase::setRenderer($translateRenderer);
     }
