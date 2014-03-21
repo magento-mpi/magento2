fd6cb9ad3faf4334d5be80115f5f54685691198e
Maxim Shikula <mshikula@ebay.com>
2014-03-21 12:30:37 +0200

    MAGETWO-22218: Implement Group Price Type

---
+++ app/code/Magento/Catalog/Pricing/Price/GroupPrice.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_Catalog
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Catalog\Pricing\Price;
+
+/**
+ * Group price model
+ */
+class GroupPrice extends Price
+{
+    /**
+     * @var string
+     */
+    protected $priceType = 'group_price';
+}
