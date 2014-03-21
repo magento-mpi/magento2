572fa2c55a42ddc8600de4bee854bef61bc5c91f
Maxim Shikula <mshikula@ebay.com>
2014-03-21 12:31:48 +0200

    MAGETWO-22217: Implement Special Price Type

---
+++ app/code/Magento/Catalog/Pricing/Price/SpecialPrice.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_Catalog
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Catalog\Pricing\Price;
+
+/**
+ * Special price model
+ */
+class SpecialPrice extends Price
+{
+    /**
+     * @var string
+     */
+    protected $priceType = 'special_price';
+}
