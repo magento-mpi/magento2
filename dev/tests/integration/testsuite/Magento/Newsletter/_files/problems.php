16e86db8b6bc97afc68f32705c59453203890ddb
Bibu Mathew <bimathew@paypal.com>
2014-03-21 17:21:14 -0500

    MAGETWO-22298 : Newsletter\Model\Resource\Problem\Collection.php
     - added fxiture for unit test

---
+++ dev/tests/integration/testsuite/Magento/Newsletter/_files/problems.php
@@ -0,0 +1,12 @@
+<?php
+require "queue.php";
+
+$problem = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+    ->create('Magento\Newsletter\Model\problem');
+$problem->setSubscriberId($subscriber->getSubscriberId())
+    ->setQueueId($queue->getQueueId())
+    ->setProblemErrorCode(11)
+    ->setProblemErrorText('error text')
+    ->save();
+
+
