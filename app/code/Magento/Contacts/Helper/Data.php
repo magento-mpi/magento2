8ea4e7e24ad518be8092caf83d7408bf5d206f07
Anup Dugar <anup@x.com>
2014-04-01 17:02:24 -0500

    MAGETWO-22868: Refactor Contacts
    - Refactored Contacts Helper
    - Added new integration tests

--- app/code/Magento/Contacts/Helper/Data.php
+++ app/code/Magento/Contacts/Helper/Data.php
@@ -2,21 +2,18 @@
 /**
  * {license_notice}
  *
- * @category    Magento
- * @package     Magento_Contacts
  * @copyright   {copyright}
  * @license     {license_link}
  */
 
-/**
- * Contacts base helper
- *
- * @category   Magento
- * @package    Magento_Contacts
- * @author      Magento Core Team <core@magentocommerce.com>
- */
 namespace Magento\Contacts\Helper;
 
+use Magento\Customer\Service\V1\Data\Customer;
+use Magento\Customer\Helper\View as ViewHelper;
+
+/**
+ * Contacts base helper
+ */
 class Data extends \Magento\App\Helper\AbstractHelper
 {
     const XML_PATH_ENABLED = 'contacts/contacts/enabled';
@@ -36,17 +33,25 @@ class Data extends \Magento\App\Helper\AbstractHelper
     protected $_customerSession;
 
     /**
+     * @var \Magento\Customer\Helper\View
+     */
+    protected $_viewHelper;
+
+    /**
      * @param \Magento\App\Helper\Context $context
      * @param \Magento\Core\Model\Store\Config $coreStoreConfig
      * @param \Magento\Customer\Model\Session $customerSession
+     * @param ViewHelper $viewHelper
      */
     public function __construct(
         \Magento\App\Helper\Context $context,
         \Magento\Core\Model\Store\Config $coreStoreConfig,
-        \Magento\Customer\Model\Session $customerSession
+        \Magento\Customer\Model\Session $customerSession,
+        ViewHelper $viewHelper
     ) {
         $this->_coreStoreConfig = $coreStoreConfig;
         $this->_customerSession = $customerSession;
+        $this->_viewHelper = $viewHelper;
         parent::__construct($context);
     }
 
@@ -70,8 +75,11 @@ class Data extends \Magento\App\Helper\AbstractHelper
         if (!$this->_customerSession->isLoggedIn()) {
             return '';
         }
-        $customer = $this->_customerSession->getCustomer();
-        return trim($customer->getName());
+        /**
+         * @var Customer $customer
+         */
+        $customer = $this->_customerSession->getCustomerDataObject();
+        return trim($this->_viewHelper->getCustomerName($customer));
     }
 
     /**
@@ -84,7 +92,10 @@ class Data extends \Magento\App\Helper\AbstractHelper
         if (!$this->_customerSession->isLoggedIn()) {
             return '';
         }
-        $customer = $this->_customerSession->getCustomer();
+        /**
+         * @var Customer $customer
+         */
+        $customer = $this->_customerSession->getCustomerDataObject();
         return $customer->getEmail();
     }
 }
