9e0e8f111372a7a69c29980fbf59b36f0c282b50
Oleh Kobchenko <okobchenko@ebay.com>
2014-04-01 17:14:30 +0300

    MAGETWO-22562: Wrong special price calculating for Bundle Product
    - revert of deleted method and covering case with test

---
+++ dev/tests/unit/testsuite/Magento/Bundle/Model/Product/PriceTest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Bundle\Model\Product;
+
+class PriceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $ruleFactoryMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $localeDateMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $storeManagerMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $customerSessionMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $eventManagerMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $taxHelperMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $storeMock;
+
+    /**
+     * @var \Magento\Bundle\Model\Product\Price
+     */
+    protected $model;
+
+    protected function setUp()
+    {
+        $this->ruleFactoryMock = $this->getMock(
+            '\Magento\CatalogRule\Model\Resource\RuleFactory', array(), array(), '', false
+        );
+        $this->storeManagerMock = $this->getMock('\Magento\Core\Model\StoreManagerInterface');
+        $this->localeDateMock = $this->getMock('\Magento\Stdlib\DateTime\TimezoneInterface');
+        $this->customerSessionMock = $this->getMock('\Magento\Customer\Model\Session', array(), array(), '', false);
+        $this->eventManagerMock = $this->getMock('\Magento\Event\ManagerInterface');
+        $this->taxHelperMock = $this->getMock('\Magento\Tax\Helper\Data', array(), array(), '', false);
+        $this->storeMock = $this->getMock('\Magento\Core\Model\Store', array(), array(), '', false);
+
+        $this->model = new \Magento\Bundle\Model\Product\Price(
+            $this->ruleFactoryMock,
+            $this->storeManagerMock,
+            $this->localeDateMock,
+            $this->customerSessionMock,
+            $this->eventManagerMock,
+            $this->taxHelperMock
+        );
+    }
+
+    /**
+     * @param float $finalPrice
+     * @param float $specialPrice
+     * @param string $from
+     * @param string $to
+     * @param int $callsNumber
+     * @param bool $dateInInterval
+     * @param float $expected
+     *
+     * @covers \Magento\Bundle\Model\Product\Price::calculateSpecialPrice
+     * @covers \Magento\Bundle\Model\Product\Price::__construct
+     * @dataProvider calculateSpecialPrice
+     */
+    public function testCalculateSpecialPrice(
+        $finalPrice, $specialPrice, $from, $to, $callsNumber, $dateInInterval, $expected
+    ) {
+        $this->localeDateMock->expects($this->exactly($callsNumber))
+            ->method('isScopeDateInInterval')->will($this->returnValue($dateInInterval));
+
+        $this->storeManagerMock->expects($this->any())
+            ->method('getStore')->will($this->returnValue($this->storeMock));
+
+        $this->storeMock->expects($this->any())
+            ->method('roundPrice')->will($this->returnArgument(0));
+
+        $this->assertEquals($expected, $this->model->calculateSpecialPrice($finalPrice, $specialPrice, $from, $to));
+    }
+
+    /**
+     * @return array
+     */
+    public function calculateSpecialPrice()
+    {
+        return array(
+            array(10, null, '2014-03-02', '2014-03-03', 0, true, 10),
+            array(10, false, '2014-03-02', '2014-03-03', 0, true, 10),
+            array(10, 50, '2014-03-02', '2014-03-03', 1, false, 10),
+            array(10, 50, '2014-03-02', '2014-03-03', 1, true, 5),
+            array(0, 50, '2014-03-02', '2014-03-03', 1, true, 0),
+            array(10, 100, '2014-03-02', '2014-03-03', 1, true, 10),
+        );
+    }
+}
