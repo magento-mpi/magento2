36d05286302c76290e3d8843e41f00f37aad7c89
Eugene Tulika <etulika@ebay.com>
2014-04-02 12:27:37 -0500

    MAGETWO-22866: Refactor Bundle
    - removed unused code

--- app/code/Magento/Bundle/Model/Price/Index.php
+++ app/code/Magento/Bundle/Model/Price/Index.php
@@ -29,21 +29,10 @@ namespace Magento\Bundle\Model\Price;
  */
 class Index extends \Magento\Model\AbstractModel
 {
-    /**
-     * @var \Magento\Customer\Model\Session
-     */
-    protected $_customerSession;
-
-    /**
-     * @var \Magento\Core\Model\StoreManagerInterface
-     */
-    protected $_storeManager;
 
     /**
      * @param \Magento\Model\Context $context
      * @param \Magento\Registry $registry
-     * @param \Magento\Core\Model\StoreManagerInterface $storeManager
-     * @param \Magento\Customer\Model\Session $customerSession
      * @param \Magento\Model\Resource\AbstractResource $resource
      * @param \Magento\Data\Collection\Db $resourceCollection
      * @param array $data
@@ -51,15 +40,11 @@ class Index extends \Magento\Model\AbstractModel
     public function __construct(
         \Magento\Model\Context $context,
         \Magento\Registry $registry,
-        \Magento\Core\Model\StoreManagerInterface $storeManager,
-        \Magento\Customer\Model\Session $customerSession,
         \Magento\Model\Resource\AbstractResource $resource = null,
         \Magento\Data\Collection\Db $resourceCollection = null,
         array $data = array()
     ) {
         parent::__construct($context, $registry, $resource, $resourceCollection, $data);
-        $this->_customerSession = $customerSession;
-        $this->_storeManager = $storeManager;
     }
 
     /**
@@ -73,29 +58,6 @@ class Index extends \Magento\Model\AbstractModel
     }
 
     /**
-     * Retrieve resource instance wrapper
-     *
-     * @return \Magento\Bundle\Model\Resource\Price\Index
-     */
-    protected function _getResource()
-    {
-        return parent::_getResource();
-    }
-
-    /**
-     * Reindex Product price
-     *
-     * @param int $productId
-     * @param int $priceType
-     * @return $this
-     */
-    protected function _reindexProduct($productId, $priceType)
-    {
-        $this->_getResource()->reindexProduct($productId, $priceType);
-        return $this;
-    }
-
-    /**
      * Reindex Bundle product Price Index
      *
      * @param \Magento\Catalog\Model\Product|\Magento\Catalog\Model\Product\Condition\ConditionInterface|array|int $products
@@ -104,78 +66,6 @@ class Index extends \Magento\Model\AbstractModel
     public function reindex($products = null)
     {
         $this->_getResource()->reindex($products);
-        return $this;
-    }
-
-    /**
-     * Add bundle price range index to Product collection
-     *
-     * @param \Magento\Catalog\Model\Resource\Product\Collection $collection
-     * @return $this
-     */
-    public function addPriceIndexToCollection($collection)
-    {
-        $productObjects = array();
-        $productIds = array();
-        foreach ($collection->getItems() as $product) {
-            /* @var $product \Magento\Catalog\Model\Product */
-            if ($product->getTypeId() == \Magento\Catalog\Model\Product\Type::TYPE_BUNDLE) {
-                $productIds[] = $product->getEntityId();
-                $productObjects[$product->getEntityId()] = $product;
-            }
-        }
-        $websiteId = $this->_storeManager->getStore($collection->getStoreId())->getWebsiteId();
-        $groupId = $this->_customerSession->getCustomerGroupId();
-
-        $addOptionsToResult = false;
-        $prices = $this->_getResource()->loadPriceIndex($productIds, $websiteId, $groupId);
-        foreach ($productIds as $productId) {
-            if (isset($prices[$productId])) {
-                $productObjects[$productId]->setData(
-                    '_price_index',
-                    true
-                )->setData(
-                    '_price_index_min_price',
-                    $prices[$productId]['min_price']
-                )->setData(
-                    '_price_index_max_price',
-                    $prices[$productId]['max_price']
-                );
-            } else {
-                $addOptionsToResult = true;
-            }
-        }
-
-        if ($addOptionsToResult) {
-            $collection->addOptionsToResult();
-        }
-
-        return $this;
-    }
-
-    /**
-     * Add price index to bundle product after load
-     *
-     * @param \Magento\Catalog\Model\Product $product
-     * @return $this
-     */
-    public function addPriceIndexToProduct($product)
-    {
-        $websiteId = $product->getStore()->getWebsiteId();
-        $groupId = $this->_customerSession->getCustomerGroupId();
-        $prices = $this->_getResource()->loadPriceIndex($product->getId(), $websiteId, $groupId);
-        if (isset($prices[$product->getId()])) {
-            $product->setData(
-                '_price_index',
-                true
-            )->setData(
-                '_price_index_min_price',
-                $prices[$product->getId()]['min_price']
-            )->setData(
-                '_price_index_max_price',
-                $prices[$product->getId()]['max_price']
-            );
-        }
         return $this;
     }
 }
