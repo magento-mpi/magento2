051a8aa39802f3cc0e8b6cab4aca2ccfcaf416d9
Bryant Luk <bluk@ebay.com>
2014-04-02 11:14:33 -0500

    MAGETWO-22876: Use Group Registry object in CustomerGroup service
    - Add phpdoc @throws for NoSuchEntityException

--- app/code/Magento/Customer/Service/V1/CustomerGroupServiceInterface.php
+++ app/code/Magento/Customer/Service/V1/CustomerGroupServiceInterface.php
@@ -60,6 +60,7 @@ interface CustomerGroupServiceInterface
      * Check if the group can be deleted
      *
      * @param int $groupId
+     * @throws \Magento\Exception\NoSuchEntityException If group is not found
      * @return bool True, if this group can be deleted
      */
     public function canDelete($groupId);
