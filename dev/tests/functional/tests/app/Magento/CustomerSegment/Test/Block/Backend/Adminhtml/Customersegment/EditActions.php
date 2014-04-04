a07ab17abf7f615decb70580d0df92c1e31761ed
Evgeniy Kolesov <ikolesov@ebay.com>
2014-04-04 16:49:55 +0300

    MAGETWO-17439: Apply New Buttons Toolbar Style for each form on Admin MAGETWO-21910: New Header&Footer Visual Styling MAGETWO-21911: Dashboard Visual Styling Fixed MAGETWO-18197: Admin Look&Feel bugs: alignment Fixed MAGETWO-19226: "Gift Wrapping" section is displayed not aligned on "Create new Order" backend page Fixed MAGETWO-19676: Invalid grid columns width while adding products to group Fixed MAGETWO-19683: UI issues on "Create/ Edit Event" page form Fixed MAGETWO-19696: Bundle option form is displayed smashed on "Create/Edit bundle Product" backend page

---
+++ dev/tests/functional/tests/app/Magento/CustomerSegment/Test/Block/Backend/Adminhtml/Customersegment/EditActions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Config actions block
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\CustomerSegment\Test\Block\Backend\Adminhtml\Customersegment;
+
+use Magento\Backend\Test\Block\FormPageActions as AbstractPageActions;
+
+/**
+ * Class EditActions
+ *
+ * @package Magento\CustomerSegment\Test\Block\Backend\Adminhtml\Customersegment
+ */
+class EditActions extends AbstractPageActions
+{
+    /**
+     * Custom "Save and Continue Edit" button
+     *
+     * @var string
+     */
+    protected $saveAndContinueButton = '#save_and_continue_edit';
+}
