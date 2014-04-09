71889ef4e85b2072bbb2fab96574951d6862897a
Joan He <joan@x.com>
2014-04-09 11:38:02 -0500

    MAGETWO-22665: fix merge issue (merge MAGETWO-22665 to develop)

--- dev/tests/api-functional/testsuite/Magento/Webapi/Routing/GettersTest.php
+++ dev/tests/api-functional/testsuite/Magento/Webapi/Routing/GettersTest.php
@@ -49,9 +49,9 @@ class GettersTest extends \Magento\Webapi\Routing\BaseService
         $requestData = ['id' => $itemId];
         $item = $this->_webApiCall($serviceInfo, $requestData);
         $this->assertEquals($itemId, $item['id'], 'Item was retrieved unsuccessfully');
-        $isEnabled = isset($item['enabled']) && $item['enabled'] === true;
+        $isEnabled = isset($item['isEnabled']) && $item['isEnabled'] === true;
         $this->assertTrue($isEnabled, 'Getter with "is" prefix is processed incorrectly.');
-        $hasOrders = isset($item['orders']) && $item['orders'] === true;
-        $this->assertTrue($hasOrders, 'Getter with "has" prefix is processed incorrectly.');
+        $hasName = isset($item['hasName']) && $item['hasName'] === true;
+        $this->assertTrue($hasName, 'Getter with "has" prefix is processed incorrectly.');
     }
 }
