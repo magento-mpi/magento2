e38066aed9d5e06aadec8ec6f360dab82c64291d
Bryant Luk <bluk@ebay.com>
2014-04-10 18:41:17 -0500

    MAGETWO-23377: Sales/Helper/Data.php
    - Add unit tests for Magento\Sales\Helper\Data

---
+++ dev/tests/unit/testsuite/Magento/Sales/Helper/DataTest.php
@@ -0,0 +1,252 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Sales\Helper;
+
+class DataTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Sales\Helper\Data
+     */
+    protected $helper;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject | \Magento\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfigMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject | \Magento\Sales\Model\Store
+     */
+    protected $storeMock;
+
+    /**
+     * @return void
+     */
+    protected function setUp()
+    {
+        $contextMock = $this->getMockBuilder('Magento\App\Helper\Context')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->scopeConfigMock = $this->getMockBuilder('Magento\App\Config')
+            ->setMethods(['isSetFlag'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $storeManagerMock = $this->getMockBuilder('Magento\Store\Model\StoreManagerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $appStateMock = $this->getMockBuilder('Magento\App\State')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->helper = new \Magento\Sales\Helper\Data(
+            $contextMock,
+            $this->scopeConfigMock,
+            $storeManagerMock,
+            $appStateMock
+        );
+
+        $this->storeMock = $this->getMockBuilder('Magento\Sales\Model\Store')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->quoteMock = $this->getMockBuilder('Magento\Sales\Model\Quote')
+            ->setMethods(['getHasError', 'setHasError', 'addMessage', '__wakeup'])
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testCheckQuoteAmountExistingError()
+    {
+        $this->quoteMock->expects($this->once())
+            ->method('getHasError')
+            ->will($this->returnValue(true));
+
+        $this->quoteMock->expects($this->never())
+            ->method('setHasError');
+
+        $this->quoteMock->expects($this->never())
+            ->method('addMessage');
+
+        $this->assertSame(
+            $this->helper,
+            $this->helper->checkQuoteAmount($this->quoteMock, Data::MAXIMUM_AVAILABLE_NUMBER + 1)
+        );
+    }
+
+    public function testCheckQuoteAmountAmountLessThanAvailable()
+    {
+        $this->quoteMock->expects($this->once())
+            ->method('getHasError')
+            ->will($this->returnValue(false));
+
+        $this->quoteMock->expects($this->never())
+            ->method('setHasError');
+
+        $this->quoteMock->expects($this->never())
+            ->method('addMessage');
+
+        $this->assertSame(
+            $this->helper,
+            $this->helper->checkQuoteAmount($this->quoteMock, Data::MAXIMUM_AVAILABLE_NUMBER - 1)
+        );
+    }
+
+    public function testCheckQuoteAmountAmountGreaterThanAvailable()
+    {
+        $this->quoteMock->expects($this->once())
+            ->method('getHasError')
+            ->will($this->returnValue(false));
+
+        $this->quoteMock->expects($this->once())
+            ->method('setHasError')
+            ->with(true);
+
+        $this->quoteMock->expects($this->once())
+            ->method('addMessage')
+            ->with(__('This item price or quantity is not valid for checkout.'));
+
+        $this->assertSame(
+            $this->helper,
+            $this->helper->checkQuoteAmount($this->quoteMock, Data::MAXIMUM_AVAILABLE_NUMBER + 1)
+        );
+    }
+
+    /**
+     * @dataProvider getScopeConfigValue
+     * @return void
+     */
+    public function testCanSendNewOrderConfirmationEmail($scopeConfigValue)
+    {
+        $this->setupScopeConfigIsSetFlag(\Magento\Sales\Model\Order::XML_PATH_EMAIL_ENABLED, $scopeConfigValue);
+
+        $this->assertEquals($scopeConfigValue, $this->helper->canSendNewOrderConfirmationEmail($this->storeMock));
+    }
+
+    /**
+     * @dataProvider getScopeConfigValue
+     * @return void
+     */
+    public function testCanSendNewOrderEmail($scopeConfigValue)
+    {
+        $this->setupScopeConfigIsSetFlag(\Magento\Sales\Model\Order::XML_PATH_EMAIL_ENABLED, $scopeConfigValue);
+
+        $this->assertEquals($scopeConfigValue, $this->helper->canSendNewOrderEmail($this->storeMock));
+    }
+
+    /**
+     * @dataProvider getScopeConfigValue
+     * @return void
+     */
+    public function testCanSendOrderCommentEmail($scopeConfigValue)
+    {
+        $this->setupScopeConfigIsSetFlag(\Magento\Sales\Model\Order::XML_PATH_UPDATE_EMAIL_ENABLED, $scopeConfigValue);
+
+        $this->assertEquals($scopeConfigValue, $this->helper->canSendOrderCommentEmail($this->storeMock));
+    }
+
+    /**
+     * @dataProvider getScopeConfigValue
+     * @return void
+     */
+    public function testCanSendNewShipmentEmail($scopeConfigValue)
+    {
+        $this->setupScopeConfigIsSetFlag(\Magento\Sales\Model\Order\Shipment::XML_PATH_EMAIL_ENABLED, $scopeConfigValue);
+
+        $this->assertEquals($scopeConfigValue, $this->helper->canSendNewShipmentEmail($this->storeMock));
+    }
+
+    /**
+     * @dataProvider getScopeConfigValue
+     * @return void
+     */
+    public function testCanSendShipmentCommentEmail($scopeConfigValue)
+    {
+        $this->setupScopeConfigIsSetFlag(\Magento\Sales\Model\Order\Shipment::XML_PATH_UPDATE_EMAIL_ENABLED, $scopeConfigValue);
+
+        $this->assertEquals($scopeConfigValue, $this->helper->canSendShipmentCommentEmail($this->storeMock));
+    }
+
+    /**
+     * @dataProvider getScopeConfigValue
+     * @return void
+     */
+    public function testCanSendNewInvoiceEmail($scopeConfigValue)
+    {
+        $this->setupScopeConfigIsSetFlag(\Magento\Sales\Model\Order\Invoice::XML_PATH_EMAIL_ENABLED, $scopeConfigValue);
+
+        $this->assertEquals($scopeConfigValue, $this->helper->canSendNewInvoiceEmail($this->storeMock));
+    }
+
+    /**
+     * @dataProvider getScopeConfigValue
+     * @return void
+     */
+    public function testCanSendInvoiceCommentEmail($scopeConfigValue)
+    {
+        $this->setupScopeConfigIsSetFlag(\Magento\Sales\Model\Order\Invoice::XML_PATH_UPDATE_EMAIL_ENABLED, $scopeConfigValue);
+
+        $this->assertEquals($scopeConfigValue, $this->helper->canSendInvoiceCommentEmail($this->storeMock));
+    }
+
+    /**
+     * @dataProvider getScopeConfigValue
+     * @return void
+     */
+    public function testCanSendNewCreditmemoEmail($scopeConfigValue)
+    {
+        $this->setupScopeConfigIsSetFlag(\Magento\Sales\Model\Order\Creditmemo::XML_PATH_EMAIL_ENABLED, $scopeConfigValue);
+
+        $this->assertEquals($scopeConfigValue, $this->helper->canSendNewCreditmemoEmail($this->storeMock));
+    }
+
+    /**
+     * @dataProvider getScopeConfigValue
+     * @return void
+     */
+    public function testCanSendCreditmemoCommentEmail($scopeConfigValue)
+    {
+        $this->setupScopeConfigIsSetFlag(\Magento\Sales\Model\Order\Creditmemo::XML_PATH_UPDATE_EMAIL_ENABLED, $scopeConfigValue);
+
+        $this->assertEquals($scopeConfigValue, $this->helper->canSendCreditmemoCommentEmail($this->storeMock));
+    }
+
+    /**
+     * Sets up the scope config mock which will return a specified value for a config flag.
+     *
+     * @param string $flagName
+     * @param bool $returnValue
+     * @return void
+     */
+    protected function setupScopeConfigIsSetFlag($flagName, $returnValue)
+    {
+        $this->scopeConfigMock->expects($this->once())
+            ->method('isSetFlag')
+            ->with(
+                $flagName,
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                $this->storeMock
+            )
+            ->will($this->returnValue($returnValue));
+    }
+
+    /**
+     * @return array
+     */
+    public function getScopeConfigValue()
+    {
+        return [
+            [true],
+            [false]
+        ];
+    }
+
+}
