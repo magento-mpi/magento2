1dd1f51114ccc42de5de8618e7f35bf3de0af547
Anton Makarenko <amakarenko@ebay.com>
2014-04-14 14:23:03 -0700

    MAGETWO-22048 Cover Updated Classes with Tests
    - typos

--- dev/tests/unit/testsuite/Magento/Theme/Block/Html/Head/ScriptTest.php
+++ dev/tests/unit/testsuite/Magento/Theme/Block/Html/Head/ScriptTest.php
@@ -23,8 +23,8 @@ class ScriptTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $objectManagerHelper = new \Magento\TestFramework\Helper\ObjectManager($this);
-        $context = $this->getMock('\Magento\View\Element\Template\Context', array(), array(), '', false );
-        $this->_assetRepo = $this->getMock('\Magento\View\Asset\Repository', array(), array(), '', false );
+        $context = $this->getMock('\Magento\View\Element\Template\Context', array(), array(), '', false);
+        $this->_assetRepo = $this->getMock('\Magento\View\Asset\Repository', array(), array(), '', false);
 
         $context->expects($this->once())
             ->method('getAssetRepository')
@@ -45,7 +45,7 @@ class ScriptTest extends \PHPUnit_Framework_TestCase
 
     public function testGetAsset()
     {
-        $asset = $this->getMock('\Magento\View\Asset\File', array(), array(), '', false );
+        $asset = $this->getMock('\Magento\View\Asset\File', array(), array(), '', false);
 
         $this->_assetRepo->expects($this->once())
             ->method('createAsset')
