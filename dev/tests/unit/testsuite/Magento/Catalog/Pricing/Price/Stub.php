7f879e3c98a371cf0ffc606491e40b7adeb68912
Vladislav Dyachkov <vdyachkov@ebay.com>
2014-04-15 14:39:42 +0000

    MAGETWO-23506: Implement abstract price type
     - Added licence and comments to stub class

--- dev/tests/unit/testsuite/Magento/Catalog/Pricing/Price/Stub.php
+++ dev/tests/unit/testsuite/Magento/Catalog/Pricing/Price/Stub.php
@@ -1,19 +1,30 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: flammus
- * Date: 4/15/14
- * Time: 2:16 PM
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_Pricing
+ * @copyright   {copyright}
+ * @license     {license_link}
  */
 
 namespace Magento\Catalog\Pricing\Price;
 
-
+/**
+ * Class Stub for testing abstract class AbstractPrice
+ *
+ * @package Magento\Catalog\Pricing\Price
+ */
 class Stub extends AbstractPrice
 {
+    /**
+     * Get price value
+     *
+     * @return float
+     */
     public function getValue()
     {
-        $examplePrice = 77;
+        $examplePrice = 77.0;
         return $examplePrice;
     }
 } 
