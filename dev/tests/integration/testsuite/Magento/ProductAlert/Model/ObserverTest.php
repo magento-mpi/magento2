11740d11853342d4780cef35f351d2de547b479c
Iryna Savchenko <isavchenko@ebay.com>
2014-04-16 19:23:08 +0300

    MAGETWO-20481: Refactor ProductAlert module to use customer services

--- dev/tests/integration/testsuite/Magento/ProductAlert/Model/ObserverTest.php
+++ dev/tests/integration/testsuite/Magento/ProductAlert/Model/ObserverTest.php
@@ -13,82 +13,77 @@ namespace Magento\ProductAlert\Model;
 class ObserverTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @magentoDataFixture Magento/Customer/_files/customer.php
-     * @magentoDataFixture Magento/Catalog/_files/product_simple.php
+     * @var \Magento\ObjectManager
+     */
+    protected $_objectManager;
+
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $_customerSession;
+
+    /**
+     * @var \Magento\Customer\Helper\View
+     */
+    protected $_customerViewHelper;
+
+    public function setUp()
+    {
+        $this->_objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $logger = $this->getMock('Magento\Logger', array(), array(), '', false);
+        $this->_customerSession = $this->_objectManager->get(
+            'Magento\Customer\Model\Session',
+            array($logger)
+        );
+        $service = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+            'Magento\Customer\Service\V1\CustomerAccountService'
+        );
+        $customer = $service->authenticate('customer@example.com', 'password');
+        $this->_customerSession->setCustomerDataAsLoggedIn($customer);
+        $this->_customerViewHelper = $this->_objectManager->create('Magento\Customer\Helper\View');
+    }
+
+    /**
+     * @magentoConfigFixture current_store catalog/productalert/allow_price 1
+     *
+     * @magentoDataFixture Magento/ProductAlert/_files/product_alert.php
      */
     public function testProcess()
     {
-        $price = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-            'Magento\ProductAlert\Model\Price'
-        );
-        $price->setWebsiteId(1)->setId(1)->setCustomerId(1)->setProductId(1)->setPrice(10)->setSendCount(0)->setStatus(
-            0
-        );
-        $price->isObjectNew(true);
-        $price->save();
-        $price->load(1);
-        $stock = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-            'Magento\ProductAlert\Model\Stock'
-        );
-        $stock->setWebsiteId(1)->setId(1)->setCustomerId(1)->setProductId(1)->setSendCount(0)->setStatus(0);
-        $stock->isObjectNew(true);
-        $stock->save();
-        $stock->load(1);
-        $priceCol = $this->getMock(
-            'Magento\ProductAlert\Model\Resource\Price\Collection',
-            ['addWebsiteFilter', 'getSelect', 'setCustomerOrder'],
-            [],
-            '',
-            false
-        );
-        $priceCol->expects($this->any())->method('addWebsiteFilter')->will($this->returnSelf());
-        $priceCol->expects($this->any())->method('setCustomerOrder')->will($this->returnValue([$price]));
-        $priceColFactory = $this->getMock(
-            'Magento\ProductAlert\Model\Resource\Price\CollectionFactory',
-            [],
-            [],
-            '',
-            false
-        );
-        $priceColFactory->expects($this->any())->method('create')->will($this->returnValue($priceCol));
-
-        $stockCol = $this->getMock(
-            'Magento\ProductAlert\Model\Resource\Stock\Collection',
-            ['addWebsiteFilter', 'addStatusFilter', 'getSelect', 'setCustomerOrder'],
-            [],
-            '',
-            false
-        );
-        $stockCol->expects($this->any())->method('addWebsiteFilter')->will($this->returnSelf());
-        $stockCol->expects($this->any())->method('addStatusFilter')->will($this->returnSelf());
-        $stockCol->expects($this->any())->method('setCustomerOrder')->will($this->returnValue([$stock]));
-        $stockColFactory = $this->getMock(
-            'Magento\ProductAlert\Model\Resource\Stock\CollectionFactory',
-            [],
-            [],
-            '',
-            false
-        );
-        $stockColFactory->expects($this->any())->method('create')->will($this->returnValue($stockCol));
-        $email = $this->getMock('Magento\ProductAlert\Model\Email', ['setCustomerData'], [], '', false);
-        $emailFactory = $this->getMock('Magento\ProductAlert\Model\EmailFactory', [], [], '', false);
-        $emailFactory->expects($this->once())->method('create')->will($this->returnValue($email));
-        $coreStoreConfig = $this->getMock('Magento\Core\Model\Store\Config', [], [], '', false);
-        $coreStoreConfig->expects($this->any())->method('getConfig')->will($this->returnValue(true));
-        $customerAccountService = $this->getMock(
-            'Magento\Customer\Service\V1\CustomerAccountServiceInterface', [], [], '', false
-        );
-        $customerAccountService->expects($this->atLeastOnce())->method('getCustomer')->with(1);
-        $model = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-            'Magento\ProductAlert\Model\Observer',
+        $this->_objectManager->configure(
             [
-                'emailFactory' => $emailFactory,
-                'coreStoreConfig' => $coreStoreConfig,
-                'priceColFactory' => $priceColFactory,
-                'stockColFactory' => $stockColFactory,
-                'customerAccountService' => $customerAccountService
+                'Magento\ProductAlert\Model\Observer' => [
+                    'arguments' => [
+                        'transportBuilder' => [
+                            'instance' => 'Magento\TestFramework\Mail\Template\TransportBuilderMock'
+                        ]
+                    ]
+                ],
+                'Magento\ProductAlert\Model\Email' => [
+                    'arguments' => [
+                        'transportBuilder' => [
+                            'instance' => 'Magento\TestFramework\Mail\Template\TransportBuilderMock'
+                        ]
+                    ]
+                ],
+                'preferences' => [
+                    'Magento\Mail\TransportInterface' => 'Magento\TestFramework\Mail\TransportInterfaceMock',
+                    'Magento\TestFramework\Mail\Template\TransportBuilder' =>
+                        'Magento\TestFramework\Mail\Template\TransportBuilderMock'
+                ]
             ]
         );
-        $model->process();
+        \Magento\TestFramework\Helper\Bootstrap::getInstance()->loadArea(\Magento\Core\Model\App\Area::AREA_FRONTEND);
+        $observer = $this->_objectManager->get('Magento\ProductAlert\Model\Observer');
+        $observer->process();
+
+        /** @var \Magento\TestFramework\Mail\Template\TransportBuilderMock $transportBuilder */
+        $transportBuilder = $this->_objectManager->get('Magento\TestFramework\Mail\Template\TransportBuilderMock');
+
+        $this->assertStringMatchesFormat(
+            '%AHello %A'
+                . $this->_customerViewHelper->getCustomerName($this->_customerSession->getCustomerDataObject()) . ',%A',
+            $transportBuilder->getSentMessage()->getBodyHtml()->getContent()
+        );
     }
 }
