88d42d9a291ac847231027b023207fe3e167c9ed
Vladislav Dyachkov <vdyachkov@ebay.com>
2014-04-18 11:55:45 +0000

    MAGETWO-23437: Pull request processing and build stabilization
     - Fixes for tests

--- app/code/Magento/ProductAlert/Block/Email/AbstractEmail.php
+++ app/code/Magento/ProductAlert/Block/Email/AbstractEmail.php
@@ -130,4 +130,37 @@ abstract class AbstractEmail extends \Magento\View\Element\Template
             ['data'=> ['price_render_handle' => 'catalog_product_prices']]
         );
     }
+
+    /**
+     * Return HTML block with tier price
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @param string $priceType
+     * @param string $renderZone
+     * @param array $arguments
+     * @return string
+     */
+    public function getProductPriceHtml(
+        \Magento\Catalog\Model\Product $product,
+        $priceType,
+        $renderZone = \Magento\Pricing\Render::ZONE_ITEM_LIST,
+        array $arguments = []
+    ) {
+        if (!isset($arguments['zone'])) {
+            $arguments['zone'] = $renderZone;
+        }
+
+        /** @var \Magento\Pricing\Render $priceRender */
+        $priceRender = $this->getPriceRender();
+        $price = '';
+
+        if ($priceRender) {
+            $price = $priceRender->render(
+                $priceType,
+                $product,
+                $arguments
+            );
+        }
+        return $price;
+    }
 }
--- app/code/Magento/ProductAlert/Block/Email/Price.php
+++ app/code/Magento/ProductAlert/Block/Email/Price.php
@@ -45,37 +45,4 @@ class Price extends \Magento\ProductAlert\Block\Email\AbstractEmail
     {
         return $this->getUrl('productalert/unsubscribe/priceAll', $this->_getUrlParams());
     }
-
-    /**
-     * Return HTML block with tier price
-     *
-     * @param \Magento\Catalog\Model\Product $product
-     * @param string $priceType
-     * @param string $renderZone
-     * @param array $arguments
-     * @return string
-     */
-    public function getProductPriceHtml(
-        \Magento\Catalog\Model\Product $product,
-        $priceType,
-        $renderZone = \Magento\Pricing\Render::ZONE_ITEM_LIST,
-        array $arguments = []
-    ) {
-        if (!isset($arguments['zone'])) {
-            $arguments['zone'] = $renderZone;
-        }
-
-        /** @var \Magento\Pricing\Render $priceRender */
-        $priceRender = $this->getPriceRender();
-        $price = '';
-
-        if ($priceRender) {
-            $price = $priceRender->render(
-                $priceType,
-                $product,
-                $arguments
-            );
-        }
-        return $price;
-    }
 }
