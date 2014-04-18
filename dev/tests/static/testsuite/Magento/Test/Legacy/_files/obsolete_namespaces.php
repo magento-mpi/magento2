5e93f194bc61440c70e321d825287c8e5fcc8a8f
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2014-04-18 18:49:18 +0300

    MAGETWO-23720: Add static tests for checking obsolete namespaces and paths

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_namespaces.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_namespaces.php
@@ -9,4 +9,33 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-return array(array('/app/code/core/Mage', '/app/code/Magento'));
+return [
+    ['Mage', 'Magento'],
+    ['Magento\App', 'Magento\Framework\App'],
+    ['Magento\Data', 'Magento\Framework\Data'],
+    ['Magento\Filesystem', 'Magento\Framework\Filesystem'],
+    ['Magento\Config', 'Magento\Framework\Config'],
+    ['Magento\Model', 'Magento\Framework\Model'],
+    ['Magento\View', 'Magento\Framework\View'],
+    ['Magento\DB', 'Magento\Framework\DB'],
+    ['Magento\Event', 'Magento\Framework\Event'],
+    ['Magento\Acl', 'Magento\Framework\Acl'],
+    ['Magento\Archive', 'Magento\Framework\Archive'],
+    ['Magento\Session', 'Magento\Framework\Session'],
+    ['Magento\Authorization', 'Magento\Framework\Authorization'],
+    ['Magento\Cache', 'Magento\Framework\Cache'],
+    ['Magento\ObjectManager', 'Magento\Framework\ObjectManager'],
+    ['Magento\Exception', 'Magento\Framework\Exception'],
+    ['Magento\Autoload', 'Magento\Framework\Autoload'],
+    ['Magento\Translate', 'Magento\Framework\Translate'],
+    ['Magento\Code', 'Magento\Framework\Code'],
+    ['Magento\Stdlib', 'Magento\Framework\Stdlib'],
+    ['Magento\Phrase', 'Magento\Framework\Phrase'],
+    ['Magento\Locale', 'Magento\Framework\Locale'],
+    ['Magento\Message', 'Magento\Framework\Message'],
+    ['Magento\Logger', 'Magento\Framework\Logger'],
+    ['Magento\Error', 'Magento\Framework\Error'],
+    ['Magento\Filter', 'Magento\Framework\Filter'],
+    ['Magento\DomDocument', 'Magento\Framework\DomDocument'],
+    ['Magento\Module', 'Magento\Framework\Module'],
+];
---
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_paths.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Obsolete pathes
+ *
+ * Format: array(<path>[, <replacement>])
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+return [
+    ['/app/code/core/Mage', '/app/code/Magento'],
+    ['/lib/Magento/App', '/lib/Magento/Framework/App'],
+    ['/dev/tests/integration/testsuite/Magento/App', '/dev/tests/integration/testsuite/Magento/Framework/App'],
+    ['/dev/tests/unit/testsuite/Magento/App', '/dev/tests/unit/testsuite/Magento/Framework/App'],
+    ['/lib/Magento/Data', '/lib/Magento/Framework/Data'],
+    ['/dev/tests/integration/testsuite/Magento/Data', '/dev/tests/integration/testsuite/Magento/Framework/Data'],
+    ['/dev/tests/unit/testsuite/Magento/Data', '/dev/tests/unit/testsuite/Magento/Framework/Data'],
+    ['/lib/Magento/Filesystem', '/lib/Magento/Framework/Filesystem'],
+    [
+        '/dev/tests/integration/testsuite/Magento/Filesystem',
+        '/dev/tests/integration/testsuite/Magento/Framework/Filesystem'
+    ],
+    ['/dev/tests/unit/testsuite/Magento/Filesystem', '/dev/tests/unit/testsuite/Magento/Framework/Filesystem'],
+    ['/lib/Magento/Config', '/lib/Magento/Framework/Config'],
+    ['/dev/tests/integration/testsuite/Magento/Config', '/dev/tests/integration/testsuite/Magento/Framework/Config'],
+    ['/dev/tests/unit/testsuite/Magento/Config', '/dev/tests/unit/testsuite/Magento/Framework/Config'],
+    ['/lib/Magento/Model', '/lib/Magento/Framework/Model'],
+    ['/dev/tests/integration/testsuite/Magento/Model', '/dev/tests/integration/testsuite/Magento/Framework/Model'],
+    ['/dev/tests/unit/testsuite/Magento/Model', '/dev/tests/unit/testsuite/Magento/Framework/Model'],
+    ['/lib/Magento/View', '/lib/Magento/Framework/View'],
+    ['/dev/tests/integration/testsuite/Magento/View', '/dev/tests/integration/testsuite/Magento/Framework/View'],
+    ['/dev/tests/unit/testsuite/Magento/View', '/dev/tests/unit/testsuite/Magento/Framework/View'],
+    ['/lib/Magento/DB', '/lib/Magento/Framework/DB'],
+    ['/dev/tests/integration/testsuite/Magento/DB', '/dev/tests/integration/testsuite/Magento/Framework/DB'],
+    ['/dev/tests/unit/testsuite/Magento/DB', '/dev/tests/unit/testsuite/Magento/Framework/DB'],
+    ['/lib/Magento/Event', '/lib/Magento/Framework/Event'],
+    ['/dev/tests/integration/testsuite/Magento/Event', '/dev/tests/integration/testsuite/Magento/Framework/Event'],
+    ['/dev/tests/unit/testsuite/Magento/Event', '/dev/tests/unit/testsuite/Magento/Framework/Event'],
+    ['/lib/Magento/Acl', '/lib/Magento/Framework/Acl'],
+    ['/dev/tests/integration/testsuite/Magento/Acl', '/dev/tests/integration/testsuite/Magento/Framework/Acl'],
+    ['/dev/tests/unit/testsuite/Magento/Acl', '/dev/tests/unit/testsuite/Magento/Framework/Acl'],
+    ['/lib/Magento/Archive', '/lib/Magento/Framework/Archive'],
+    [
+        '/dev/tests/integration/testsuite/Magento/Archive',
+        '/dev/tests/integration/testsuite/Magento/Framework/Archive'
+    ],
+    ['/dev/tests/unit/testsuite/Magento/Archive', '/dev/tests/unit/testsuite/Magento/Framework/Archive'],
+    ['/lib/Magento/Backup', '/lib/Magento/Framework/Backup'],
+    ['/lib/Magento/Session', '/lib/Magento/Framework/Session'],
+    [
+        '/dev/tests/integration/testsuite/Magento/Session',
+        '/dev/tests/integration/testsuite/Magento/Framework/Session'
+    ],
+    ['/dev/tests/unit/testsuite/Magento/Session', '/dev/tests/unit/testsuite/Magento/Framework/Session'],
+    ['/lib/Magento/Authorization', '/lib/Magento/Framework/Authorization'],
+    [
+        '/dev/tests/integration/testsuite/Magento/Authorization',
+        '/dev/tests/integration/testsuite/Magento/Framework/Authorization'
+    ],
+    ['/dev/tests/unit/testsuite/Magento/Authorization', '/dev/tests/unit/testsuite/Magento/Framework/Authorization'],
+    ['/lib/Magento/Cache', '/lib/Magento/Framework/Cache'],
+    ['/dev/tests/integration/testsuite/Magento/Cache', '/dev/tests/integration/testsuite/Magento/Framework/Cache'],
+    ['/dev/tests/unit/testsuite/Magento/Cache', '/dev/tests/unit/testsuite/Magento/Framework/Cache'],
+    ['/lib/Magento/ObjectManager', '/lib/Magento/Framework/ObjectManager'],
+    [
+        '/dev/tests/integration/testsuite/Magento/ObjectManager',
+        '/dev/tests/integration/testsuite/Magento/Framework/ObjectManager'
+    ],
+    ['/dev/tests/unit/testsuite/Magento/ObjectManager', '/dev/tests/unit/testsuite/Magento/Framework/ObjectManager'],
+    ['/lib/Magento/Exception', '/lib/Magento/Framework/Exception'],
+    [
+        '/dev/tests/integration/testsuite/Magento/Exception',
+        '/dev/tests/integration/testsuite/Magento/Framework/Exception'
+    ],
+    ['/dev/tests/unit/testsuite/Magento/Exception', '/dev/tests/unit/testsuite/Magento/Framework/Exception'],
+    ['/lib/Magento/Autoload', '/lib/Magento/Framework/Autoload'],
+    [
+        '/dev/tests/integration/testsuite/Magento/Autoload',
+        '/dev/tests/integration/testsuite/Magento/Framework/Autoload'
+    ],
+    ['/dev/tests/unit/testsuite/Magento/Autoload', '/dev/tests/unit/testsuite/Magento/Framework/Autoload'],
+    ['/lib/Magento/Translate', '/lib/Magento/Framework/Translate'],
+    [
+        '/dev/tests/integration/testsuite/Magento/Translate',
+        '/dev/tests/integration/testsuite/Magento/Framework/Translate'
+    ],
+    ['/dev/tests/unit/testsuite/Magento/Translate', '/dev/tests/unit/testsuite/Magento/Framework/Translate'],
+    ['/lib/Magento/Code', '/lib/Magento/Framework/Code'],
+    ['/dev/tests/integration/testsuite/Magento/Code', '/dev/tests/integration/testsuite/Magento/Framework/Code'],
+    ['/dev/tests/unit/testsuite/Magento/Code', '/dev/tests/unit/testsuite/Magento/Framework/Code'],
+    ['/lib/Magento/Stdlib', '/lib/Magento/Framework/Stdlib'],
+    ['/dev/tests/integration/testsuite/Magento/Stdlib', '/dev/tests/integration/testsuite/Magento/Framework/Stdlib'],
+    ['/dev/tests/unit/testsuite/Magento/Stdlib', '/dev/tests/unit/testsuite/Magento/Framework/Stdlib'],
+    ['/lib/Magento/Phrase', '/lib/Magento/Framework/Phrase'],
+    ['/dev/tests/integration/testsuite/Magento/Phrase', '/dev/tests/integration/testsuite/Magento/Framework/Phrase'],
+    ['/dev/tests/unit/testsuite/Magento/Phrase', '/dev/tests/unit/testsuite/Magento/Framework/Phrase'],
+    ['/lib/Magento/Locale', '/lib/Magento/Framework/Locale'],
+    ['/dev/tests/integration/testsuite/Magento/Locale', '/dev/tests/integration/testsuite/Magento/Framework/Locale'],
+    ['/dev/tests/unit/testsuite/Magento/Locale', '/dev/tests/unit/testsuite/Magento/Framework/Locale'],
+    ['/lib/Magento/Message', '/lib/Magento/Framework/Message'],
+    ['/dev/tests/integration/testsuite/Magento/Message', '/dev/tests/integration/testsuite/Magento/Framework/Message'],
+    ['/dev/tests/unit/testsuite/Magento/Message', '/dev/tests/unit/testsuite/Magento/Framework/Message'],
+    ['/lib/Magento/Logger', '/lib/Magento/Framework/Logger'],
+    ['/dev/tests/integration/testsuite/Magento/Logger', '/dev/tests/integration/testsuite/Magento/Framework/Logger'],
+    ['/dev/tests/unit/testsuite/Magento/Logger', '/dev/tests/unit/testsuite/Magento/Framework/Logger'],
+    ['/lib/Magento/Error', '/lib/Magento/Framework/Error'],
+    ['/dev/tests/integration/testsuite/Magento/Error', '/dev/tests/integration/testsuite/Magento/Framework/Error'],
+    ['/dev/tests/unit/testsuite/Magento/Error', '/dev/tests/unit/testsuite/Magento/Framework/Error'],
+    ['/lib/Magento/Filter', '/lib/Magento/Framework/Filter'],
+    ['/dev/tests/integration/testsuite/Magento/Filter', '/dev/tests/integration/testsuite/Magento/Framework/Filter'],
+    ['/dev/tests/unit/testsuite/Magento/Filter', '/dev/tests/unit/testsuite/Magento/Framework/Filter'],
+    ['/lib/Magento/DomDocument', '/lib/Magento/Framework/DomDocument'],
+    [
+        '/dev/tests/integration/testsuite/Magento/DomDocument',
+        '/dev/tests/integration/testsuite/Magento/Framework/DomDocument'
+    ],
+    ['/dev/tests/unit/testsuite/Magento/DomDocument', '/dev/tests/unit/testsuite/Magento/Framework/DomDocument'],
+    ['/lib/Magento/Module', '/lib/Magento/Framework/Module'],
+    ['/dev/tests/integration/testsuite/Magento/Module', '/dev/tests/integration/testsuite/Magento/Framework/Module'],
+    ['/dev/tests/unit/testsuite/Magento/Module', '/dev/tests/unit/testsuite/Magento/Framework/Module'],
+];
