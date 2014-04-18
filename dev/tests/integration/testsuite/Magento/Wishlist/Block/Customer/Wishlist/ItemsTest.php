1724412a6818de855e3a43130831c8657ebb7a63
Vladislav Dyachkov <vdyachkov@ebay.com>
2014-04-18 18:10:51 +0000

    MAGETWO-23437: Pull request processing and build stabilization
     - Fixed memory leak in integration Wishlist test

--- dev/tests/integration/testsuite/Magento/Wishlist/Block/Customer/Wishlist/ItemsTest.php
+++ dev/tests/integration/testsuite/Magento/Wishlist/Block/Customer/Wishlist/ItemsTest.php
@@ -14,15 +14,23 @@ class ItemsTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetColumns()
     {
+        ini_set('memory_limit', '1024M');
         $layout = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\View\LayoutInterface');
         $block = $layout->addBlock('Magento\Wishlist\Block\Customer\Wishlist\Items', 'test');
         $child = $this->getMock(
-            'Magento\View\Element\Text',
+            'Magento\Wishlist\Block\Customer\Wishlist\Item\Column',
             array('isEnabled'),
-            array(\Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\View\Element\Context'))
+            array(\Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\View\Element\Context')),
+            '',
+            false
         );
         $child->expects($this->any())->method('isEnabled')->will($this->returnValue(true));
         $layout->addBlock($child, 'child', 'test');
-        $this->assertSame(array($child), $block->getColumns());
+        $expected = $child->getType();
+        $columns = $block->getColumns();
+        $this->assertNotEmpty($columns);
+        foreach ($columns as $column) {
+            $this->assertSame($expected, $column->getType());
+        }
     }
 }
