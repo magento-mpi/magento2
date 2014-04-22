13ff270e1bf1b02f1095baa2248e5675e0e52a85
Joan He <joan@x.com>
2014-04-22 14:34:30 -0500

    MAGETWO-23698: fixed Invalid error message when save customer group with existing group name

--- app/code/Magento/Customer/Service/V1/CustomerGroupService.php
+++ app/code/Magento/Customer/Service/V1/CustomerGroupService.php
@@ -284,7 +284,7 @@ class CustomerGroupService implements CustomerGroupServiceInterface
              *  difficult to do without imposing more database calls
              */
             if ($e->getMessage() === __('Customer Group already exists.')) {
-                throw new InvalidTransitionException($e->getMessage());
+                throw new InvalidTransitionException('Customer Group already exists.');
             }
             throw $e;
         }
