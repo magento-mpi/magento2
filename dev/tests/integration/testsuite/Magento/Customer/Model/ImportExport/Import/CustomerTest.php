fb8c0e6c4cf12da337409526264261c69393704d
Bryant Luk <bluk@ebay.com>
2014-04-22 17:55:15 -0500

    MAGETWO-23577: Merge with mainline
    - Fix moved \Magento\Core\Model\App\Area to \Magento\Framework\App\Area

--- dev/tests/integration/testsuite/Magento/Customer/Model/ImportExport/Import/CustomerTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Model/ImportExport/Import/CustomerTest.php
@@ -144,7 +144,7 @@ class CustomerTest extends \PHPUnit_Framework_TestCase
     public function testDeleteData()
     {
         \Magento\TestFramework\Helper\Bootstrap::getInstance()
-            ->loadArea(\Magento\Core\Model\App\Area::AREA_FRONTEND);
+            ->loadArea(\Magento\Framework\App\Area::AREA_FRONTEND);
         $source = new \Magento\ImportExport\Model\Import\Source\Csv(
             __DIR__ . '/_files/customers_to_import.csv',
             $this->directoryWrite
