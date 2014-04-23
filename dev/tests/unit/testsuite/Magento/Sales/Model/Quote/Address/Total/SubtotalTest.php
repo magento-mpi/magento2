97d724e8a09c90223e2da6393b98f241ed0a4a4c
Denys Rudchenko <drudchenko@ebay.com>
2014-04-23 19:41:59 +0300

    MAGETWO-7666: Special Price displayed in place of original price in "Items Ordered" column for orders.

--- dev/tests/unit/testsuite/Magento/Sales/Model/Quote/Address/Total/SubtotalTest.php
+++ dev/tests/unit/testsuite/Magento/Sales/Model/Quote/Address/Total/SubtotalTest.php
@@ -10,40 +10,45 @@ namespace Magento\Sales\Model\Quote\Address\Total;
 class SubtotalTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var \Magento\TestFramework\Helper\ObjectManager
+     */
+    protected $objectManager;
+
+    /**
      * @var \Magento\Sales\Model\Quote\Address\Total\Subtotal
      */
     protected $subtotalModel;
 
     protected function setUp()
     {
-        $objectManager = new \Magento\TestFramework\Helper\ObjectManager($this);
-        $this->subtotalModel = $objectManager->getObject('Magento\Sales\Model\Quote\Address\Total\Subtotal');
+        $this->objectManager = new \Magento\TestFramework\Helper\ObjectManager($this);
+        $this->subtotalModel = $this->objectManager->getObject('Magento\Sales\Model\Quote\Address\Total\Subtotal');
     }
 
     public function collectDataProvider()
     {
         return array(
-            array(12, 10, false, true, true),
-            array(12, 0, false, true, true),
-            array(0, 10, false, true, true),
-            array(12, 10, true, false, false),
-            array(12, 10, false, false, true),
+            array(12, 10, false, 12, 10),
+            array(12, 0, false, 12, 12),
+            array(0, 10, false, 0, 10),
+            array(12, 10, true, null, null),
+            array(12, 10, false, 12, 10)
         );
     }
 
     /**
      * @dataProvider collectDataProvider
+     *
+     * @param int $price
+     * @param int $originalPrice
+     * @param bool $itemHasParent
+     * @param int $expectedPrice
+     * @param int $expectedOriginalPrice
      */
-    public function testCollect($finalPrice, $originalPrice, $hasParent, $isChildrenCalculated, $isTotalCalculated)
+    public function testCollect($price, $originalPrice, $itemHasParent, $expectedPrice, $expectedOriginalPrice)
     {
         /** @var \Magento\Sales\Model\Quote\Item|\PHPUnit_Framework_MockObject_MockObject $quoteItem */
-        $quoteItem = $this->getMock(
-            'Magento\Sales\Model\Quote\Item',
-            array(),
-            array(),
-            '',
-            false
-        );
+        $quoteItem = $this->objectManager->getObject('Magento\Sales\Model\Quote\Item');
         /** @var \Magento\Sales\Model\Quote\Address|\PHPUnit_Framework_MockObject_MockObject $address */
         $address = $this->getMock(
             'Magento\Sales\Model\Quote\Address',
@@ -65,7 +70,6 @@ class SubtotalTest extends \PHPUnit_Framework_TestCase
             false
         );
         $product->expects($this->any())->method('getPrice')->will($this->returnValue($originalPrice));
-        $quoteItem->expects($this->once())->method('getProduct')->will($this->returnValue($product));
         /** @var \Magento\Sales\Model\Quote|\PHPUnit_Framework_MockObject_MockObject $quote */
         $quote = $this->getMock(
             'Magento\Sales\Model\Quote',
@@ -74,49 +78,40 @@ class SubtotalTest extends \PHPUnit_Framework_TestCase
             '',
             false
         );
+        $store = $this->objectManager->getObject('Magento\Store\Model\Store');
+        $store->setCurrentCurrency('');
+        $quote->expects($this->any())->method('getStore')->will($this->returnValue($store));
+        $quoteItem->setProduct($product)->setQuote($quote)->setOriginalCustomPrice($price);
 
         $address->expects($this->any())->method('getAllNonNominalItems')->will(
             $this->returnValue(array($quoteItem))
         );
-        $quoteItem->expects($this->any())->method('getQuote')->will($this->returnValue($quote));
         $address->expects($this->any())->method('getQuote')->will($this->returnValue($quote));
         $product->expects($this->any())->method('isVisibleInCatalog')->will($this->returnValue(true));
-        $parentQuoteItem = $this->getMock(
-            'Magento\Sales\Model\Quote\Item',
-            array(),
-            array(),
-            '',
-            false
-        );
-        if (!$hasParent) {
-            $parentQuoteItem = false;
-        }
-        $quoteItem->expects($this->any())->method('getParentItem')->will($this->returnValue($parentQuoteItem));
-        $quoteItem->expects($this->any())->method('isChildrenCalculated')->will(
-            $this->returnValue($isChildrenCalculated)
-        );
 
-        if ($isTotalCalculated) {
-            if ($hasParent && $isChildrenCalculated) {
-                $parentQuoteItem->expects($this->any())->method('getProduct')->will($this->returnValue($product));
-                $priceModel = $this->getMock('\Magento\Catalog\Model\Product\Type\Price', array(), array(), '', false);
-                $priceModel->expects($this->any())->method('getChildFinalPrice')->will(
-                    $this->returnValue($finalPrice)
-                );
-                $product->expects($this->once())->method('getPriceModel')->will(
-                    $this->returnValue($priceModel)
-                );
-            } else if(!$hasParent) {
-                $product->expects($this->any())->method('getFinalPrice')->will($this->returnValue($finalPrice));
-            }
-            $quoteItem->expects($this->once())->method('setPrice')->with($this->equalTo($finalPrice))->will(
-                $this->returnValue($quoteItem)
+        $parentQuoteItem = false;
+        if ($itemHasParent) {
+            $parentQuoteItem = $this->getMock(
+                'Magento\Sales\Model\Quote\Item',
+                array(),
+                array(),
+                '',
+                false
             );
-            $quoteItem->expects($this->once())->method('calcRowTotal');
-        } else {
-            $quoteItem->expects($this->never())->method('calcRowTotal');
+            $parentQuoteItem->expects($this->any())->method('getProduct')->will($this->returnValue($product));
         }
+        $quoteItem->setParentItem($parentQuoteItem);
 
+        $priceModel = $this->getMock('\Magento\Catalog\Model\Product\Type\Price', array(), array(), '', false);
+        $priceModel->expects($this->any())->method('getChildFinalPrice')->will(
+            $this->returnValue($price)
+        );
+        $product->expects($this->any())->method('getPriceModel')->will(
+            $this->returnValue($priceModel)
+        );
+        $product->expects($this->any())->method('getFinalPrice')->will($this->returnValue($price));
         $this->subtotalModel->collect($address);
+        $this->assertEquals($expectedPrice, $quoteItem->getPrice());
+        $this->assertEquals($expectedOriginalPrice, $quoteItem->getBaseOriginalPrice());
     }
 }
