43fa01863aba70a72d495eff4ff96cc5f059123b
Yurii Torbyk <itorbyk@ebay.com>
2014-04-23 16:10:56 +0300

    MAGETWO-23697: Impossible reset password for admin user(incorrect link in email)

--- app/code/Magento/Email/Model/Template/Filter.php
+++ app/code/Magento/Email/Model/Template/Filter.php
@@ -109,6 +109,11 @@ class Filter extends \Magento\Filter\Template
     protected $_appState;
 
     /**
+     * @var \Magento\Backend\Model\UrlInterface
+     */
+    protected $backendUrlBuilder;
+
+    /**
      * @param \Magento\Stdlib\String $string
      * @param \Magento\Logger $logger
      * @param \Magento\Escaper $escaper
@@ -119,6 +124,7 @@ class Filter extends \Magento\Filter\Template
      * @param \Magento\Framework\View\LayoutInterface $layout
      * @param \Magento\Framework\View\LayoutFactory $layoutFactory
      * @param \Magento\Framework\App\State $appState
+     * @param \Magento\Backend\Model\UrlInterface $backendUrlBuilder
      * @param array $variables
      *
      * @SuppressWarnings(PHPMD.ExcessiveParameterList)
@@ -134,6 +140,7 @@ class Filter extends \Magento\Filter\Template
         \Magento\Framework\View\LayoutInterface $layout,
         \Magento\Framework\View\LayoutFactory $layoutFactory,
         \Magento\Framework\App\State $appState,
+        \Magento\Backend\Model\UrlInterface $backendUrlBuilder,
         $variables = array()
     ) {
         $this->_escaper = $escaper;
@@ -146,6 +153,7 @@ class Filter extends \Magento\Filter\Template
         $this->_layout = $layout;
         $this->_layoutFactory = $layoutFactory;
         $this->_appState = $appState;
+        $this->backendUrlBuilder = $backendUrlBuilder;
         parent::__construct($string, $variables);
     }
 
@@ -394,7 +402,12 @@ class Filter extends \Magento\Filter\Template
             unset($params['url']);
         }
 
-        return $this->_storeManager->getStore($this->getStoreId())->getUrl($path, $params);
+        if (0 === $this->getStoreId() || \Magento\Store\Model\Store::ADMIN_CODE === $this->getStoreId()) {
+            $url = $this->backendUrlBuilder->getUrl($path, $params);
+        } else {
+            $url = $this->_storeManager->getStore($this->getStoreId())->getUrl($path, $params);
+        }
+        return $url;
     }
 
     /**
