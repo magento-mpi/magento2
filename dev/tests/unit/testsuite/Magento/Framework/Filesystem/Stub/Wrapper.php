019c36cba4b2fa5ed581768d5eb273d66efc9c43
Sergey Ivashchenko <sivashchenko@ebay.com>
2014-04-23 19:14:42 +0300

    MAGETWO-22312: Unit test coverage: Prepare pull request

--- dev/tests/unit/testsuite/Magento/Framework/Filesystem/Stub/Wrapper.php
+++ dev/tests/unit/testsuite/Magento/Framework/Filesystem/Stub/Wrapper.php
@@ -12,10 +12,17 @@ namespace Magento\Framework\Filesystem\Stub;
  * Class Wrapper
  * @package Magento\Framework\Filesystem\Stub
  */
-class Wrapper {
+class Wrapper
+{
+    /**
+     * Driver
+     *
+     * @var \Magento\Framework\Filesystem\DriverInterface
+     */
+    protected $driver;
 
     public function __construct(\Magento\Framework\Filesystem\DriverInterface $driver)
     {
-
+        $this->driver = $driver;
     }
 } 
