71d0b674487cf8e229f22c1561e72b45eb57bf75
Vladislav Dyachkov <vdyachkov@ebay.com>
2014-04-25 18:52:33 +0000

    MAGETWO-20400: Pull request processing and build stabilization
     - Re-placed price lib after move

--- dev/tests/unit/testsuite/Magento/Framework/Pricing/Render/AmountTest.php
+++ dev/tests/unit/testsuite/Magento/Framework/Pricing/Render/AmountTest.php
@@ -83,7 +83,7 @@ class AmountTest extends \PHPUnit_Framework_TestCase
         $this->saleableItemMock = $this->getMockForAbstractClass('Magento\Framework\Pricing\Object\SaleableInterface');
         $this->priceMock = $this->getMockForAbstractClass('Magento\Framework\Pricing\Price\PriceInterface');
 
-        $eventManager = $this->getMock('Magento\Event\ManagerStub', [], [], '', false);
+        $eventManager = $this->getMock('Magento\Framework\Event\ManagerStub', [], [], '', false);
         $config = $this->getMock('Magento\Store\Model\Store\Config', [], [], '', false);
         $scopeConfigMock = $this->getMockForAbstractClass('Magento\Framework\App\Config\ScopeConfigInterface');
         $context = $this->getMock('Magento\Framework\View\Element\Template\Context', [], [], '', false);
