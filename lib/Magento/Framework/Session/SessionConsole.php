f6c9a9977d5baf7af0bdf5f1d60a2f721b2ede60
Bibu Mathew <bimathew@paypal.com>
2014-04-28 18:28:26 -0500

    MAGETWO-23657: Fatal error after uninstall if session saved in the database
     - added unit test
     - fixed PSR2 errors

--- lib/Magento/Framework/Session/SessionConsole.php
+++ lib/Magento/Framework/Session/SessionConsole.php
@@ -7,6 +7,9 @@
  */
 namespace Magento\Framework\Session;
 
+/**
+ * To prevent the session start in console uninstall
+ */
 class SessionConsole extends Generic
 {
     public function __construct()
