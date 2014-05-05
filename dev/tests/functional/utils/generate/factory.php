c38ec3ff0e8045dd8f40ecf1651fbc8f3c1e1cc1
Michael Logvin <mlogvin@ebay.com>
2014-05-05 16:28:08 +0300

    MAGETWO-23775: Resolve dependency on Magento code base for generators

--- dev/tests/functional/utils/generate/factory.php
+++ dev/tests/functional/utils/generate/factory.php
@@ -8,24 +8,7 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-umask(0);
-
-$appRoot = dirname(dirname(dirname(dirname(dirname(__DIR__)))));
-
-require $appRoot . '/app/bootstrap.php';
-
-$mtfRoot = dirname(dirname(dirname(__FILE__)));
-$mtfRoot = str_replace('\\', '/', $mtfRoot);
-define('MTF_BP', $mtfRoot);
-define('MTF_TESTS_PATH', MTF_BP . '/tests/app/');
-
-$path = get_include_path();
-$path = rtrim($path, PATH_SEPARATOR);
-$path .= PATH_SEPARATOR . MTF_BP;
-$path .= PATH_SEPARATOR . MTF_BP . '/lib';
-$path .= PATH_SEPARATOR . MTF_BP . '/tests/app';
-$path .= PATH_SEPARATOR . MTF_BP . '/vendor/magento/mtf';
-set_include_path($path);
+require_once dirname(__DIR__) . '/' . 'bootstrap.php';
 
 $entryPoint = new \Magento\Framework\App\EntryPoint\EntryPoint(BP, $_SERVER);
 $entryPoint->run('Mtf\Util\Generate\Factory');
