c285b6a2030bb750545795595676bdc7cf0f8fb4
Richard Bates <rbates@x.com>
2014-05-06 11:30:28 -0500

    MAGETWO-22824: update getRandomString to respect character set limitations

--- lib/Magento/Framework/Math/Random.php
+++ lib/Magento/Framework/Math/Random.php
@@ -26,7 +26,7 @@ class Random
     /**
      * Get random string
      *
-     * @param int $length
+     * @param int         $length
      * @param null|string $chars
      * @return string
      */
@@ -36,26 +36,33 @@ class Random
         if (is_null($chars)) {
             $chars = self::CHARS_LOWERS . self::CHARS_UPPERS . self::CHARS_DIGITS;
         }
-        $bytes = '';
-        $fp = null;
+
+        $rand = 0;
         if (function_exists('openssl_random_pseudo_bytes')) {
             // use openssl lib if it is installed
-            $bytes = openssl_random_pseudo_bytes(ceil($length / 2));
-            $hex = bin2hex($bytes); // hex() doubles the length of the string
-            $str = substr($hex, 0, $length); // we truncate at most 1 char if length parameter is an odd number
+            for ($i = 0, $lc = strlen($chars) - 1; $i < $length; $i++) {
+                $bytes = openssl_random_pseudo_bytes(4);
+                $hex = bin2hex($bytes); // hex() doubles the length of the string
+                $rand = hexdec($hex) % $lc; // random integer from 0 to $lc
+                $str .= $chars[$rand]; // random character in $chars
+            }
         } elseif ($fp = @fopen('/dev/urandom', 'rb')) {
             // attempt to use /dev/urandom if it exists but openssl isn't available
-            $bytes .= @fread($fp, $length);
+            for ($i = 0, $lc = strlen($chars) - 1; $i < $length; $i++) {
+                $bytes = @fread($fp, 4);
+                $hex = bin2hex($bytes); // hex() doubles the length of the string
+                $rand = hexdec($hex) % $lc; // random integer from 0 to $lc
+                $str .= $chars[$rand]; // random character in $chars
+            }
             fclose($fp);
-            $hex = bin2hex($bytes); // hex() doubles the length of the string
-            $str = substr($hex, 0, $length); // we truncate at most 1 char if length parameter is an odd number
         } else {
             // fallback to mt_rand() if all else fails
-            for ($i = 0, $str = '', $lc = strlen($chars) - 1; $i < $length; $i++) {
-                $str .= $chars[mt_rand(0, $lc)];
+            for ($i = 0, $lc = strlen($chars) - 1; $i < $length; $i++) {
+                $rand = mt_rand(0, $lc); // random integer from 0 to $lc
+                $str .= $chars[$rand]; // random character in $chars
             }
-            // log error here to warn merchant of insecure randomness
         }
+
         return $str;
     }
 
