86e4289356770cbb26d6a488a0f5eddf5848adee
orykh <orykh@ebay.com>
2014-05-12 16:44:37 +0300

    MTA-42: Create customer group entity
    - Revert deleted repository
    - Revert deleted fixture
    - Remove package

--- dev/tests/functional/tests/app/Magento/Customer/Test/Fixture/CustomerGroup.php
+++ dev/tests/functional/tests/app/Magento/Customer/Test/Fixture/CustomerGroup.php
@@ -2,50 +2,72 @@
 /**
  * {license_notice}
  *
+ * @category    Mtf
+ * @package     Mtf
+ * @subpackage  functional_tests
  * @copyright   {copyright}
  * @license     {license_link}
  */
 
 namespace Magento\Customer\Test\Fixture;
 
-use Mtf\Fixture\InjectableFixture;
+use Mtf\Factory\Factory;
+use Mtf\Fixture\DataFixture;
 
 /**
- * Class CustomerGroup
+ * Class Customer Group Fixture
  *
  * @package Magento\Customer\Test\Fixture
  */
-class CustomerGroup extends InjectableFixture
+class CustomerGroup extends DataFixture
 {
-    protected $defaultDataSet = [
-        'customer_group_code' => 'customer_code_%isolation%',
-        'tax_class_id' => 'Retail Customer',
-    ];
-
-    protected $customer_group_code = [
-        'attribute_code' => 'code',
-        'backend_type' => 'varchar',
-        'is_required' => '1',
-        'default_value' => '',
-        'input' => 'text',
-    ];
-
-    protected $tax_class_id = [
-        'attribute_code' => 'tax_class',
-        'backend_type' => 'varchar',
-        'is_required' => '1',
-        'default_value' => '',
-        'input' => 'select',
-        'fixture' => 'Magento\Customer\Test\Fixture\CustomerGroup\TaxClassIds',
-    ];
-
-    public function getCustomerGroupCode()
+    /**
+     * Create customer group
+     */
+    public function persist()
     {
-        return $this->getData('customer_group_code');
+        $this->_data['fields']['id']['value'] = Factory::getApp()->magentoCustomerCreateCustomerGroup($this);
     }
 
-    public function getTaxClassId()
+    /**
+     * Initialize fixture data
+     */
+    protected function _initData()
     {
-        return $this->getData('tax_class_id');
+        $this->_data = array(
+            'fields' => array(
+                'code' => array(
+                    'value' => 'Test group %isolation%',
+                ),
+                'tax_class' => array(
+                    'value' => 'Retail Customer',
+                    'input_value' => 3,
+                ),
+            ),
+        );
+        $this->_defaultConfig = array();
+
+        $this->_repository = Factory::getRepositoryFactory()
+            ->getMagentoCustomerCustomerGroup($this->_dataConfig, $this->_data);
+    }
+
+    /**
+     * Get group name
+     *
+     * @return string
+     */
+    public function getGroupName()
+    {
+        return $this->getData('fields/code/value');
+    }
+
+    /**
+     * Get group id
+     *
+     * @return string
+     */
+    public function getGroupId()
+    {
+        return $this->getData('fields/id/value');
     }
 }
