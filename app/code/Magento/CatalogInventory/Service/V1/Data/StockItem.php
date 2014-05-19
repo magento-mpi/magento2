1a83b06342d98e9db1be2aa8189b8445a56297a7
Yurii Torbyk <itorbyk@ebay.com>
2014-05-19 18:26:50 +0300

    MAGETWO-24509: Create StockItem interface

---
+++ app/code/Magento/CatalogInventory/Service/V1/Data/StockItem.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\CatalogInventory\Service\V1\Data;
+
+/**
+ * Stock item data object
+ */
+class StockItem
+{
+}
