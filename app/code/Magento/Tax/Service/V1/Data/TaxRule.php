7d9f1c4d70c978b41a66771dda38dc4e072d6107
Bibu Mathew <bimathew@paypal.com>
2014-05-20 14:21:22 -0500

    MAGETWO-24620: Create Dataobject
     - created TaxRule Dataobject

---
+++ app/code/Magento/Tax/Service/V1/Data/TaxRule.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Tax\Service\V1\Data;
+/**
+ * Class TaxRule
+ */
+class TaxRule extends \Magento\Framework\Service\Data\AbstractObject
+{
+    /**#@+
+     * Constants for Data Object keys
+     */
+    const ID = 'id';
+
+    const CODE = 'code';
+
+    const CUSTOMER_TAX_CLASS_ID = 'customer_tax_class_id';
+
+    const PRODUCT_TAX_CLASS_ID = 'product_tax_class_id';
+
+    const TAX_RATES = 'tax_rates';
+
+    const PRIORITY = 'priority';
+
+    const SORT_ORDER = 'sort_order';
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->_get(self::ID);
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->_get(self::CODE);
+    }
+
+    /**
+     * Get customer tax class id
+     *
+     * @return int
+     */
+    public function getCustomerTaxClassId()
+    {
+        return $this->_get(self::CUSTOMER_TAX_CLASS_ID);
+    }
+
+    /**
+     * Get product tax class id
+     *
+     * @return int
+     */
+    public function getProductTaxClassId()
+    {
+        return $this->_get(self::PRODUCT_TAX_CLASS_ID);
+    }
+
+    /**
+     * Get tax rates
+     *
+     * @return Magento\Tax\Service\V1\Data\TaxRate[]| null
+     */
+    public function getTaxRates()
+    {
+        return $this->_get(self::TAX_RATES);
+    }
+
+    /**
+     * Get priority
+     *
+     * @return int
+     */
+    public function getPriority()
+    {
+        return $this->_get(self::PRIORITY);
+    }
+
+    /**
+     * Get sort order.
+     *
+     * @return int
+     */
+    public function getSortOrder()
+    {
+        return $this->_get(self::SORT_ORDER);
+    }
+}
