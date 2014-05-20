852aa5b151845783015ca35d170e9179f6ef4628
Iryna Savchenko <isavchenko@ebay.com>
2014-05-20 17:29:12 +0300

    MAGETWO-23885: Remove Saved Credit Card payment method
    - CR Changes

---
+++ dev/tests/integration/testsuite/Magento/Centinel/CreateOrderTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Centinel;
+
+/**
+ * @magentoAppArea adminhtml
+ */
+class CreateOrderTest extends \Magento\Backend\Utility\Controller
+{
+    public function setUp()
+    {
+        $this->markTestIncomplete('MAGETWO-23885: Remove Saved Credit Card payment method');
+    }
+
+    /**
+     * @magentoConfigFixture default_store payment/ccsave/centinel 1
+     * @magentoDataFixture Magento/Catalog/_files/product_simple.php
+     */
+    public function testIndexAction()
+    {
+        /** @var $order \Magento\Sales\Model\AdminOrder\Create */
+        $order = $this->_objectManager->get('Magento\Sales\Model\AdminOrder\Create');
+        $paymentData = array(
+            'cc_owner' => 'Test User',
+            'cc_type' => 'visa',
+            'cc_number' => '4111111111111111',
+            'cc_exp_month' => '12',
+            'cc_exp_year' => '2013',
+            'cc_cid' => '123',
+            'method' => 'ccsave'
+        );
+        $quote = $order->addProducts(array(1 => array('qty' => 1)))->getQuote();
+        $defaultStoreId = $this->_objectManager->get(
+            'Magento\Store\Model\StoreManagerInterface'
+        )->getStore(
+            'default'
+        )->getId();
+        $quote->setStoreId($defaultStoreId);
+        $quote->getPayment()->addData($paymentData);
+        $this->dispatch('backend/sales/order_create/index');
+        $this->assertContains('<div class="centinel">', $this->getResponse()->getBody());
+    }
+}
