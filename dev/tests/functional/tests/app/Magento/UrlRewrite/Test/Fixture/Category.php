11c5ad2cc24e3ba5bffa1fa881aa97eab55df504
Oleksandr Rykh <orykh@ebay.com>
2014-05-22 17:25:54 +0300

    MTA-82: Create category rewrites entity
    - Change module

---
+++ dev/tests/functional/tests/app/Magento/UrlRewrite/Test/Fixture/Category.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\UrlRewrite\Test\Fixture;
+
+use Mtf\System\Config;
+use Mtf\Factory\Factory;
+use Mtf\Fixture\DataFixture;
+use Magento\Catalog\Test\Fixture\Category as CategoryFixture;
+
+/**
+ * Class Category
+ * URL rewrite category fixture
+ */
+class Category extends DataFixture
+{
+    /**
+     * Category for which URL rewrite is created
+     *
+     * @var CategoryFixture
+     */
+    protected $category;
+
+    /**
+     * @param Config $configuration
+     * @param array $placeholders
+     */
+    public function __construct(Config $configuration, array $placeholders = array())
+    {
+        parent::__construct($configuration, $placeholders);
+        $this->_placeholders['rewritten_category_request_path'] = array($this, 'getRewrittenRequestPath');
+        $this->_repository = Factory::getRepositoryFactory()
+            ->getMagentoUrlRewriteCategory($this->_dataConfig, $this->_data);
+
+        $this->category = Factory::getFixtureFactory()->getMagentoCatalogCategory();
+        $this->category->persist();
+    }
+
+    /**
+     * Retrieve rewritten request path
+     *
+     * @return string
+     */
+    public function getRewrittenRequestPath()
+    {
+        $categoryPath = str_replace(' ', '-', strtolower($this->category->getCategoryName()));
+        return $categoryPath . '-custom-redirect.html';
+    }
+
+    /**
+     * Retrieve original request path
+     *
+     * @return string
+     */
+    public function getOriginalRequestPath()
+    {
+        $categoryPath = str_replace(' ', '-', strtolower($this->category->getCategoryName()));
+        return $categoryPath . '.html';
+    }
+
+    /**
+     * Retrieve category name to which the product is assigned
+     *
+     * @return string
+     */
+    public function getCategoryName()
+    {
+        return $this->category->getCategoryName();
+    }
+
+    /**
+     * Initialize fixture data
+     */
+    protected function _initData()
+    {
+    }
+}
