82b53847871aff4ca61ae0b9d4fdf0fb1f21ffda
Maksym Aposov <maposov@ebay.com>
2014-05-22 17:40:01 +0300

    MAGETWO-23306 Create Product service
    - MAGETWO-24484 Create method catalog_product.delete

--- app/code/Magento/Catalog/Service/V1/ProductService.php
+++ app/code/Magento/Catalog/Service/V1/ProductService.php
@@ -3,6 +3,7 @@
 namespace Magento\Catalog\Service\V1;
 
 use Magento\Catalog\Controller\Adminhtml\Product;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class ProductService implements ProductServiceInterface
 {
@@ -22,19 +23,28 @@ class ProductService implements ProductServiceInterface
     protected $productTypeManager;
 
     /**
+     * @var \Magento\Catalog\Model\ProductFactory
+     */
+    protected $productFactory;
+
+
+    /**
      * @param Product\Initialization\Helper $initializationHelper
      * @param Product\Builder $productBuilder
      * @param \Magento\Catalog\Model\Product\TypeTransitionManager $productTypeManager
+     * @param \Magento\Catalog\Model\ProductFactory $productFactory
      */
     public function __construct(
         Product\Initialization\Helper $initializationHelper,
         Product\Builder $productBuilder,
-        \Magento\Catalog\Model\Product\TypeTransitionManager $productTypeManager
+        \Magento\Catalog\Model\Product\TypeTransitionManager $productTypeManager,
+        \Magento\Catalog\Model\ProductFactory $productFactory
     )
     {
         $this->initializationHelper = $initializationHelper;
         $this->productBuilder = $productBuilder;
         $this->productTypeManager = $productTypeManager;
+        $this->productFactory = $productFactory;
     }
 
     /**
@@ -57,6 +67,13 @@ class ProductService implements ProductServiceInterface
      */
     public function delete($id)
     {
+        $product = $this->productFactory->create();
+        $product->load($id);
+        if (!$product->getId()) {
+            // product does not exist
+            throw NoSuchEntityException::singleField('id', $id);
+        }
+        $product->delete();
         return true;
     }
 
