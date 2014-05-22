19fe5502d836a0fe077d6b422b824b3faa686d0e
Joan He <joan@x.com>
2014-05-22 16:19:42 -0500

    MAGETWO-24616: created tax rate model/data object converter

---
+++ app/code/Magento/Tax/Model/Calculation/Rate/Converter.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Tax\Model\Calculation\Rate;
+
+use Magento\Tax\Model\Resource\Calculation\Rate;
+use Magento\Tax\Service\V1\Data\TaxRate as TaxRateDataObject;
+use Magento\Tax\Service\V1\Data\TaxRateBuilder as TaxRateDataObjectBuilder;
+use Magento\Tax\Service\V1\Data\ZipRangeBuilder as ZipRangeDataObjectBuilder;
+use Magento\Tax\Model\Calculation\RateFactory as TaxRateModelFactory;
+
+/**
+ * Tax Rate Model converter.
+ *
+ * Converts a Tax Rate Model to a Data Object or vice versa.
+ */
+class Converter
+{
+    /**
+     * @var TaxRateDataObjectBuilder
+     */
+    protected $taxRateDataObjectBuilder;
+
+    /**
+     * @var TaxRateModelFactory
+     */
+    protected $taxRateModelFactory;
+
+    /**
+     * @var ZipRangeDataObjectBuilder
+     */
+    protected $zipRangeDataObjectBuilder;
+
+    /**
+     * @param TaxRateDataObjectBuilder $taxRateDataObjectBuilder
+     * @param TaxRateModelFactory $taxRateModelFactory
+     * @param ZipRangeDataObjectBuilder $zipRangeBuilder
+     */
+    public function __construct(
+        TaxRateDataObjectBuilder $taxRateDataObjectBuilder,
+        TaxRateModelFactory $taxRateModelFactory,
+        ZipRangeDataObjectBuilder $zipRangeDataObjectBuilder
+    ) {
+        $this->taxRateDataObjectBuilder = $taxRateDataObjectBuilder;
+        $this->taxRateModelFactory = $taxRateModelFactory;
+        $this->zipRangeDataObjectBuilder = $zipRangeDataObjectBuilder;
+    }
+
+    /**
+     * Convert a rate model to a TaxRate data object
+     *
+     * @param Rate $rateModel
+     * @return TaxRateDataObject
+     */
+    public function createTaxRateFromModel(Rate $rateModel)
+    {
+        $this->taxRateDataObjectBuilder->populateWithArray([]);
+        if ($rateModel->getId()) {
+            $this->taxRateDataObjectBuilder->setId($rateModel->getId());
+        }
+        if ($rateModel->getCountryId()) {
+            $this->taxRateDataObjectBuilder->setCountryId($rateModel->getCountryId());
+        }
+        if ($rateModel->getRegionId()) {
+            $this->taxRateDataObjectBuilder->setRegionId($rateModel->getRegionId());
+        }
+        if ($rateModel->getTaxPostcode()) {
+            $this->taxRateDataObjectBuilder->setZip($rateModel->getTaxPostcode());
+        }
+        if ($rateModel->getCode()) {
+            $this->taxRateDataObjectBuilder->setCode($rateModel->getCode());
+        }
+        if ($rateModel->getRate()) {
+            $this->taxRateDataObjectBuilder->setPercentageRate($rateModel->getRate());
+        }
+        if ($rateModel->getZipIsRange()) {
+            $zipRange = $this->zipRangeDataObjectBuilder->populateWithArray([])
+                ->setFrom($rateModel->getZipFrom())
+                ->setTo($rateModel->getZipTo())
+                ->create();
+            $this->taxRateDataObjectBuilder->setZipRange($zipRange);
+        }
+        return $this->taxRateDataObjectBuilder->create();
+    }
+}
