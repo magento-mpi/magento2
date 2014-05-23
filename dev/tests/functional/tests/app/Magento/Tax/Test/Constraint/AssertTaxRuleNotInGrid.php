0a91fb217fbb952143b5e393d9d3e00b984251df
Oleksii Kolesnyk <okolesnyk@ebay.com>
2014-05-23 21:04:17 +0300

    MTA-4: Delete Tax Rule Entity

---
+++ dev/tests/functional/tests/app/Magento/Tax/Test/Constraint/AssertTaxRuleNotInGrid.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Tax\Test\Constraint; 
+
+use Mtf\Constraint\AbstractConstraint;
+use Magento\Tax\Test\Page\Adminhtml\TaxRuleIndex;
+use Magento\Tax\Test\Fixture\TaxRule;
+
+/**
+ * Class AssertTaxRuleNotInGrid
+ */
+class AssertTaxRuleNotInGrid extends AbstractConstraint
+{
+    /**
+     * Constraint severeness
+     *
+     * @var string
+     */
+    protected $severeness = 'high';
+
+    /**
+     * Assert that tax rule not available in Tax Rule grid
+     *
+     * @param TaxRuleIndex $taxRuleIndex
+     * @param TaxRule $taxRule
+     * @return void
+     */
+    public function processAssert(
+        TaxRuleIndex $taxRuleIndex,
+        TaxRule $taxRule
+    ) {
+        $taxRuleCode = $taxRule->getCode();
+        $filter = [
+            'code' => $taxRuleCode,
+        ];
+
+        $taxRuleIndex->open();
+        \PHPUnit_Framework_Assert::assertFalse(
+            $taxRuleIndex->getTaxRuleGrid()->isRowVisible($filter),
+            'Tax Rule \'' . $filter['code'] . '\' is present in Tax Rule grid.'
+        );
+    }
+
+    /**
+     * Text of Tax Rule not in grid assert
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return 'Tax rule is absent in grid.';
+    }
+}
---
+++ dev/tests/functional/tests/app/Magento/Tax/Test/TestCase/DeleteTaxRuleEntityTest.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Tax\Test\TestCase;
+
+use Magento\Customer\Test\Fixture\AddressInjectable;
+use Magento\Tax\Test\Fixture\TaxRule;
+use Magento\Tax\Test\Page\Adminhtml\TaxRuleIndex;
+use Magento\Tax\Test\Page\Adminhtml\TaxRuleNew;
+use Mtf\Fixture\FixtureFactory;
+use Mtf\TestCase\Injectable;
+
+/**
+ * Test Creation for Delete TaxRuleEntity
+ *
+ * Test Flow:
+ * Preconditions:
+ * 1. Tax Rule is created
+ *
+ * Steps:
+ * 1. Log in as default admin user.
+ * 2. Go to Sales > Tax Rules
+ * 3. Select required tax rule from preconditions
+ * 4. Click on the "Delete Rule" button
+ * 5. Perform all assertions
+ *
+ * @group Tax_(CS)
+ * @ZephyrId MAGETWO-20924
+ */
+class DeleteTaxRuleEntityTest extends Injectable
+{
+    /**
+     * Tax Rule grid page
+     *
+     * @var TaxRuleIndex
+     */
+    protected $taxRuleIndexPage;
+
+    /**
+     * Tax Rule new and edit page
+     *
+     * @var TaxRuleNew
+     */
+    protected $taxRuleNewPage;
+
+    /**
+     * Preparing data
+     *
+     * @param FixtureFactory $fixtureFactory
+     * @return array
+     */
+    public function __prepare(FixtureFactory $fixtureFactory)
+    {
+        $productSimple = $fixtureFactory->createByCode('catalogProductSimple', ['dataSet' => '100_dollar_product']);
+        $productSimple->persist();
+        $customer = $fixtureFactory->createByCode('customerInjectable', ['dataSet' => 'default']);
+        $customer->persist();
+
+        return [
+            'productSimple' => $productSimple,
+            'customer' => $customer,
+        ];
+    }
+
+    /**
+     * Injection data
+     *
+     * @param TaxRuleIndex $taxRuleIndexPage
+     * @param TaxRuleNew $taxRuleNewPage
+     */
+    public function __inject(
+        TaxRuleIndex $taxRuleIndexPage,
+        TaxRuleNew $taxRuleNewPage
+    ) {
+        $this->taxRuleIndexPage = $taxRuleIndexPage;
+        $this->taxRuleNewPage = $taxRuleNewPage;
+    }
+
+    /**
+     * Delete Tax Rule Entity test
+     *
+     * @param TaxRule $taxRule
+     * @param AddressInjectable $address
+     * @param $shipping
+     */
+    public function testDeleteTaxRule(
+        TaxRule $taxRule,
+        AddressInjectable $address,
+        $shipping
+    ) {
+        // Precondition
+        $taxRule->persist();
+
+        // Steps
+        $filters = [
+            'code' => $taxRule->getCode(),
+        ];
+        $this->taxRuleIndexPage->open();
+        $this->taxRuleIndexPage->getTaxRuleGrid()->searchAndOpen($filters);
+        $this->taxRuleNewPage->getFormPageActions()->delete();
+    }
+}
