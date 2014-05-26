9bb17feab6d45fa257bacdf4754ad7d5942d4bcc
Evgeniy Miskov <emiskov@ebay.com>
2014-05-26 13:01:00 +0300

    MAGETWO-24595: Cover Magento\Framework\File\Csv
    - CR changes

--- dev/tests/unit/testsuite/Magento/Framework/File/CsvTest.php
+++ dev/tests/unit/testsuite/Magento/Framework/File/CsvTest.php
@@ -13,8 +13,6 @@ namespace Magento\Framework\File;
 
 class CsvTest extends \PHPUnit_Framework_TestCase
 {
-
-    public static $_fileExists;
     /**
      * Csv model
      *
@@ -47,55 +45,18 @@ class CsvTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\Magento\Framework\File\Csv', $this->_model->setEnclosure('"'));
     }
 
-    public function testGetDataFileNotExists()
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage File "FileName" do not exists
+     */
+    public function testGetDataFileNonExistent()
     {
         $file = 'FileName';
-        $this->setExpectedException('\Exception', 'File "FileName" do not exists');
         $this->_model->getData($file);
     }
-
-    public function testGetDataFileExists()
-    {
-        $file = 'FileExists';
-        $this->assertTrue($this->_model->getData($file));
-    }
-
-    public function testGetDataPairs()
-    {
-        $keyIndex = 0;
-        $valueIndex= 1;
-        $file = 'fileName';
-
-    }
-
-    public function testSaveData()
-    {
-
-    }
-
-    public function testFputcsv()
-    {
-
-    }
-
 }
 
-    function file_exists($fname)
-    {
-        return ($fname == 'FileExists') ? true : false;
-    }
-
-    function fopen()
-    {
-        return true;
-    }
-
-    function fgetcsv()
-    {
-
-    }
-
-    function fclose()
-    {
-        return true;
-    }
+function file_exists($fname)
+{
+    return ($fname == 'FileExists') ? true : false;
+}
