e11a9a6eee7131d916ed6c7e1ab8ee4c343be2a1
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2014-05-26 20:11:46 +0300

    MAGETWO-24451: AttributeSet remove service
    cosmetical fixes

--- dev/tests/api-functional/testsuite/Magento/Catalog/Service/V1/ProductAttributeSetWriteServiceTest.php
+++ dev/tests/api-functional/testsuite/Magento/Catalog/Service/V1/ProductAttributeSetWriteServiceTest.php
@@ -57,7 +57,6 @@ class ProductAttributeSetWriteServiceTest extends WebapiAbstract
         ];
         $requestData = [
             'id' => $id,
-            // attributeSetId ???
         ];
 
         $this->_webApiCall($serviceInfo, $requestData);
