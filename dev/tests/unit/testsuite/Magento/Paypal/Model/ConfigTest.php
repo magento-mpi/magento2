1321f87efea782d2efdf1343f081b42b78a649e9
Sergiy Akulinin <sakulinin@ebay.com>
2014-05-27 16:42:58 +0300

    MAGETWO-24488: [DEV] Remove Payments Pro Payflow Edition payment solution
    MAGETWO-23859: Remove Payments Pro Payflow Edition payment solution
    - unit tests

---
+++ dev/tests/unit/testsuite/Magento/Paypal/Model/ConfigTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Paypal\Model;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Config
+     */
+    protected $_model;
+
+    protected function setUp()
+    {
+        $helper = new \Magento\TestFramework\Helper\ObjectManager($this);
+        $this->_model = $helper->getObject('Magento\Paypal\Model\Config');
+    }
+
+    public function testGetCountryMethods()
+    {
+        $this->assertNotContains('payflow_direct', $this->_model->getCountryMethods('GB'));
+        $this->markTestIncomplete('MAGETWO-13169');
+        $this->assertContains(Config::METHOD_WPP_PE_EXPRESS, $this->_model->getCountryMethods('CA'));
+        $this->assertNotContains(Config::METHOD_WPP_PE_EXPRESS, $this->_model->getCountryMethods('GB'));
+        $this->assertContains(Config::METHOD_WPP_PE_EXPRESS, $this->_model->getCountryMethods('CA'));
+        $this->assertContains(Config::METHOD_WPP_EXPRESS, $this->_model->getCountryMethods('DE'));
+        $this->assertContains(Config::METHOD_BILLING_AGREEMENT, $this->_model->getCountryMethods('DE'));
+    }
+
+    public function testGetBuildNotationCode()
+    {
+        $this->_model->setMethod('payflow_direct');
+        $this->assertEquals('Magento_Cart_WPP_some-country', $this->_model->getBuildNotationCode('some-country'));
+    }
+
+    public function testIsMethodActive()
+    {
+        $this->assertFalse($this->_model->isMethodActive('payflow_direct'));
+    }
+
+    public function testIsMethodAvailable()
+    {
+        $this->assertFalse($this->_model->isMethodAvailable('payflow_direct'));
+    }
+
+    public function testIsCreditCardMethod()
+    {
+        $this->assertFalse($this->_model->getIsCreditCardMethod('payflow_direct'));
+    }
+
+    public function testGetSpecificConfigPath()
+    {
+        $this->_model->setMethod('payflow_direct');
+        // TODO: Update after MAGETWO-24243
+        $this->assertNull($this->_model->useccv);
+        $this->assertNull($this->_model->vendor);
+    }
+}
