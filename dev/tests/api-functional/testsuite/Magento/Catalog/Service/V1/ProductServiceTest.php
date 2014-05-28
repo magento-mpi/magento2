33cec10d51dc34019ca64f9798a312e373e23163
Maksym Aposov <maposov@ebay.com>
2014-05-28 19:43:46 +0300

    MAGETWO-23306 Create Product service
    - MAGETWO-24496: API tests for catalog_product.list

--- dev/tests/api-functional/testsuite/Magento/Catalog/Service/V1/ProductServiceTest.php
+++ dev/tests/api-functional/testsuite/Magento/Catalog/Service/V1/ProductServiceTest.php
@@ -10,6 +10,9 @@ namespace Magento\Catalog\Service\V1;
 use Magento\TestFramework\TestCase\WebapiAbstract;
 use Magento\Webapi\Model\Rest\Config as RestConfig;
 use Magento\Webapi\Exception as HTTPExceptionCodes;
+use Magento\Framework\Service\V1\Data\FilterBuilder;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Framework\Service\V1\Data\SearchCriteria;
 
 use \Magento\Catalog\Service\V1\Data\Product;
 
@@ -25,7 +28,7 @@ class ProductServiceTest extends WebapiAbstract
     /**
      * @return array
      */
-    public static function productCreationProvider()
+    public function productCreationProvider()
     {
         $productBuilder = function($data) {
             return array_replace_recursive(
@@ -68,31 +71,19 @@ class ProductServiceTest extends WebapiAbstract
      */
     public function testCreate($product)
     {
-        $serviceInfo = [
-            'rest' => [
-                'resourcePath' => self::RESOURCE_PATH,
-                'httpMethod' => RestConfig::HTTP_METHOD_POST
-            ],
-            'soap' => [
-                'service' => self::SERVICE_NAME,
-                'serviceVersion' => self::SERVICE_VERSION,
-                'operation' => self::SERVICE_NAME . 'create'
-            ],
-        ];
-
-        $requestData = ['product' => $product];
-        $response = $this->_webApiCall($serviceInfo, $requestData);
-        $this->assertGreaterThan(0, $response);
+        $response = $this->_createProduct($product);
+        $this->assertGreaterThan(0, $response[Product::ID]);
     }
 
     /**
-     * @magentoApiDataFixture Magento/Catalog/_files/product_simple.php
+     * @depends testCreate
      */
     public function testDelete()
     {
+        $productData = $this->_createProduct($this->getSimpleProductData());
         $serviceInfo = [
             'rest' => [
-                'resourcePath' => self::RESOURCE_PATH  . '/1' ,
+                'resourcePath' => self::RESOURCE_PATH  . '/' . $productData[Product::ID] ,
                 'httpMethod' => RestConfig::HTTP_METHOD_DELETE
             ],
             'soap' => [
@@ -102,11 +93,8 @@ class ProductServiceTest extends WebapiAbstract
             ]
         ];
 
-        if (TESTS_WEB_API_ADAPTER == self::ADAPTER_SOAP) {
-            $response = $this->_webApiCall($serviceInfo, ['id' => 1]);
-        } else {
-            $response = $this->_webApiCall($serviceInfo);
-        }
+        $response = (TESTS_WEB_API_ADAPTER == self::ADAPTER_SOAP) ?
+            $this->_webApiCall($serviceInfo, ['id' => $productData[Product::ID]]) : $this->_webApiCall($serviceInfo);
         $this->assertTrue($response);
     }
 
@@ -146,6 +134,173 @@ class ProductServiceTest extends WebapiAbstract
             $this->assertEquals(['fieldName' => 'id', 'fieldValue' => $invalidId], $errorObj['parameters']);
             $this->assertEquals(HTTPExceptionCodes::HTTP_NOT_FOUND, $e->getCode());
         }
+    }
+
+    /**
+     * @dataProvider searchDataProvider
+     * @depends testCreate
+     */
+    public function testSearch($filterGroups, $expected, $sortData)
+    {
+        /** @var $searchCriteriaBuilder  \Magento\Framework\Service\V1\Data\SearchCriteriaBuilder*/
+        $searchCriteriaBuilder = Bootstrap::getObjectManager()->create(
+            'Magento\Framework\Service\V1\Data\SearchCriteriaBuilder'
+        );
+        foreach($filterGroups as $filterGroup) {
+            $group = array();
+            foreach($filterGroup as $filter) {
+                list($filterKey, $filterValue) = $filter;
+                $group[] = (new FilterBuilder())
+                    ->setField($filterKey)
+                    ->setValue($filterValue)
+                    ->create();
+            }
+            $searchCriteriaBuilder->addFilter($group);
+        }
+        list($sortField, $sortValue) = $sortData;
+        $searchCriteriaBuilder->setSortOrders([$sortField => $sortValue]);
+        $searchData = $searchCriteriaBuilder->create()->__toArray();
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => self::RESOURCE_PATH,
+                'httpMethod' => RestConfig::HTTP_METHOD_GET
+            ],
+            'soap' => [
+                'service' => self::SERVICE_NAME,
+                'serviceVersion' => self::SERVICE_VERSION,
+                'operation' => self::SERVICE_NAME . 'search'
+            ]
+        ];
+
+        $requestData = ['searchCriteria' => $searchData];
+        $searchResults = $this->_webApiCall($serviceInfo, $requestData);
+        $this->assertArrayHasKey('items', $searchResults);
+        $this->assertEquals(count($expected), count($searchResults['items']));
+        $this->assertEquals(count($expected), $searchResults['total_count']);
+        foreach($expected as $key => $productSku) {
+            $this->assertEquals($productSku, $searchResults['items'][$key][Product::SKU]);
+        }
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function searchDataProvider()
+    {
+        $product1 = $this->_createProduct($this->getSimpleProductData());
+        $product2 = $this->_createProduct($this->getSimpleProductData());
+        return array(
+            array(
+                [ //Groups
+                    [ //Group(AND)
+                        [Product::SKU, $product1[Product::SKU]] //Filters(OR)
+                    ],
+                ],
+                [0 => $product1[Product::SKU]],
+                [Product::ID, SearchCriteria::SORT_ASC]
+            ),
+            array(
+                [ //Groups
+                    [ //Group(AND)
+                        [Product::SKU, $product1[Product::SKU]],
+                    ],
+                    [
+                        [Product::NAME, $product1[Product::NAME]],
+                    ],
+                    [
+                        [Product::VISIBILITY, $product1[Product::VISIBILITY]],
+                    ],
+                    [
+                        [Product::TYPE_ID, $product1[Product::TYPE_ID]],
+                    ],
+                    [
+                        [Product::PRICE, $product1[Product::PRICE]],
+                    ],
+                    [
+                        [Product::STATUS, $product1[Product::STATUS]],
+                    ],
+                    [
+                        [Product::TYPE_ID, $product1[Product::TYPE_ID]],
+                    ]
+                ],
+                [0 => $product1[Product::SKU]],
+                [Product::ID, SearchCriteria::SORT_ASC]
+            ),
+            array(
+                [
+                    [
+                        [Product::SKU, $product1[Product::SKU]],
+                        [Product::SKU, $product2[Product::SKU]]
+                    ],
+                ],
+                [0 => $product2[Product::SKU], 1 => $product1[Product::SKU]],
+                [Product::ID, SearchCriteria::SORT_DESC]
+            ),
+            array(
+                [
+                    [
+                        [Product::SKU, $product1[Product::SKU]],
+                        [Product::SKU, $product2[Product::SKU]]
+                    ],
+                ],
+                [0 => $product1[Product::SKU], 1 => $product2[Product::SKU]],
+                [Product::ID, SearchCriteria::SORT_ASC]
+            ),
+            array(
+                [
+                    [
+                        [Product::SKU, $product1[Product::SKU]],
+                        [Product::SKU, $product2[Product::SKU]],
+                        [Product::SKU, 'N']
+                    ],
+                ],
+                [0 => $product1[Product::SKU], 1 => $product2[Product::SKU]],
+                [Product::ID, SearchCriteria::SORT_ASC]
+            ),
+            array(
+                [ //Groups
+                    [ //Group(AND)
+                        [Product::SKU, $product1[Product::SKU]], //Filters(OR)
+                        [Product::SKU, $product2[Product::SKU]]
+                    ],
+                    [
+                        [Product::SKU, 'N']
+                    ]
+                ],
+                [], //No Items expected
+                [Product::ID, SearchCriteria::SORT_ASC]
+            )
+        );
+    }
+
+    protected function getSimpleProductData()
+    {
+        return array(
+            Product::SKU => uniqid('sku-', true),
+            Product::NAME => uniqid('name-', true),
+            Product::VISIBILITY => 4,
+            Product::TYPE_ID => 'simple',
+            Product::PRICE => 3.62,
+            Product::STATUS => 1,
+            Product::TYPE_ID => 'simple'
+        );
+    }
+
+    protected function _createProduct($product)
+    {
+        $serviceInfo = [
+            'rest' => ['resourcePath' => self::RESOURCE_PATH, 'httpMethod' => RestConfig::HTTP_METHOD_POST],
+            'soap' => [
+                'service' => self::SERVICE_NAME,
+                'serviceVersion' => self::SERVICE_VERSION,
+                'operation' => self::SERVICE_NAME . 'create'
+            ],
+        ];
+        $requestData = ['product' => $product];
+        $product[Product::ID] = $this->_webApiCall($serviceInfo, $requestData);
+        return $product;
     }
 
     /**
