c51943024aed33e6e63d12b35ab7f3ec4b50c878
Iryna Lagno <ilagno@ebay.com>
2014-05-29 16:21:12 +0300

    MAGETWO-24461: ProductLink assign service

--- dev/tests/unit/testsuite/Magento/Catalog/Service/V1/Product/Link/ReadServiceTest.php
+++ dev/tests/unit/testsuite/Magento/Catalog/Service/V1/Product/Link/ReadServiceTest.php
@@ -9,6 +9,7 @@
 namespace Magento\Catalog\Service\V1\Product\Link;
 
 use Magento\Catalog\Service\V1\Product\Link\Data\LinkTypeEntity;
+use Magento\Catalog\Service\V1\Product\Link\Data\ProductLinkEntity;
 
 class ReadServiceTest extends \PHPUnit_Framework_TestCase
 {
@@ -27,6 +28,36 @@ class ReadServiceTest extends \PHPUnit_Framework_TestCase
      */
     protected $builderMock;
 
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $productBuilderMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $productLoaderMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $collectionProviderMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $linkFactoryMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $linkBuilderMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $linkResolverMock;
+
     protected function setUp()
     {
         $helper = new \Magento\TestFramework\Helper\ObjectManager($this);
@@ -39,11 +70,66 @@ class ReadServiceTest extends \PHPUnit_Framework_TestCase
             '',
             false
         );
+        $this->productBuilderMock = $this->getMock(
+            'Magento\Catalog\Service\V1\Product\Link\Data\ProductLinkEntityBuilder',
+            [],
+            [],
+            '',
+            false
+        );
+
+        $this->productLoaderMock = $this->getMock(
+            'Magento\Catalog\Service\V1\Product\Link\ProductLoader',
+            [],
+            [],
+            '',
+            false
+        );
+
+        $this->collectionProviderMock = $this->getMock(
+            'Magento\Catalog\Service\V1\Product\Link\Data\ProductLinkEntity\CollectionProvider',
+            [],
+            [],
+            '',
+            false
+        );
+
+        $this->linkFactoryMock = $this->getMock(
+            'Magento\Catalog\Model\Product\LinkFactory',
+            ['create'],
+            [],
+            '',
+            false
+        );
+
+        $this->linkBuilderMock = $this->getMock(
+            'Magento\Catalog\Service\V1\Product\Link\Data\LinkAttributeEntityBuilder',
+            [],
+            [],
+            '',
+            false
+        );
+
+        $this->linkResolverMock = $this->getMock(
+            'Magento\Catalog\Service\V1\Product\Link\LinkTypeResolver',
+            [],
+            [],
+            '',
+            false
+        );
+
         $this->service = $helper->getObject(
             'Magento\Catalog\Service\V1\Product\Link\ReadService',
             [
                 'linkTypeProvider' => $this->providerMock,
                 'builder' => $this->builderMock,
+                'productLoader'  => $this->productLoaderMock,
+                'productEntityBuilder' => $this->productBuilderMock,
+                'entityCollectionProvider' => $this->collectionProviderMock,
+                'linkFactory' => $this->linkFactoryMock,
+                'linkAttributeBuilder' => $this->linkBuilderMock,
+                'linkTypeResolver' => $this->linkResolverMock
+
             ]
         );
     }
@@ -72,6 +158,67 @@ class ReadServiceTest extends \PHPUnit_Framework_TestCase
 
     public function testGetLinkedProducts()
     {
-        $this->markTestIncomplete('need to be implemented');
+        $productMock = $this->getMock('Magento\Catalog\Model\Product', array(), array(), '', false);
+        $this->productLoaderMock
+            ->expects($this->once())->method('load')
+            ->with('product_sku')
+            ->will($this->returnValue($productMock));
+        $this->linkResolverMock
+            ->expects($this->once())
+            ->method('getTypeIdByCode')
+            ->with('productType')
+            ->will($this->returnValue('type_id'));
+        $methods = array('getTypeId', 'getAttributeSetId', 'getSku', 'getPosition', '__wakeup');
+        $itemMock = $this->getMock('Magento\Catalog\Model\Product', $methods, array(), '', false);
+        $this->collectionProviderMock
+            ->expects($this->once())
+            ->method('getCollection')
+            ->with($productMock, 'type_id')
+            ->will($this->returnValue(array($itemMock)));
+        $itemMock->expects($this->once())->method('getTypeId')->will($this->returnValue('typeId'));
+        $itemMock->expects($this->once())->method('getAttributeSetId')->will($this->returnValue(10));
+        $itemMock->expects($this->once())->method('getSku')->will($this->returnValue('sku'));
+        $itemMock->expects($this->once())->method('getPosition')->will($this->returnValue(0));
+        $data = [
+            ProductLinkEntity::TYPE => 'typeId',
+            ProductLinkEntity::ATTRIBUTE_SET_ID => 10,
+            ProductLinkEntity::SKU => 'sku',
+            ProductLinkEntity::POSITION => 0
+        ];
+        $this->productBuilderMock
+            ->expects($this->once())
+            ->method('populateWithArray')
+            ->with($data)
+            ->will($this->returnSelf());
+        $this->productBuilderMock->expects($this->once())->method('create')->will($this->returnValue('Expected'));
+        $this->assertequals(array('Expected'), $this->service->getLinkedProducts('product_sku', 'productType'));
+    }
+
+    public function testGetLinkedAttributes()
+    {
+        $linkMock = $this->getMock('Magento\Catalog\Model\Product\Link', array(), array(), '', false);
+        $attributeMock = [['code' => 'code_name', 'type' => 'type_name']];
+        $this->linkResolverMock
+            ->expects($this->once())
+            ->method('getTypeIdByCode')
+            ->with('productType')
+            ->will($this->returnValue('type_id'));
+        $this->linkFactoryMock
+            ->expects($this->once())
+            ->method('create')
+            ->with(['data' => ['link_type_id' => 'type_id']])
+            ->will($this->returnValue($linkMock));
+        $data = [
+            Data\LinkAttributeEntity::CODE => 'code_name',
+            Data\LinkAttributeEntity::TYPE => 'type_name'
+        ];
+        $linkMock->expects($this->once())->method('getAttributes')->will($this->returnValue($attributeMock));
+        $this->linkBuilderMock
+            ->expects($this->once())
+            ->method('populateWithArray')
+            ->with($data)
+            ->will($this->returnSelf());
+        $this->linkBuilderMock->expects($this->once())->method('create')->will($this->returnValue('Expected'));
+        $this->assertequals(array('Expected'), $this->service->getLinkAttributes('productType'));
     }
 } 
