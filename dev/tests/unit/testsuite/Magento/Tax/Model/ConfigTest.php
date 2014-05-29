9f56d0ce526da61c34584ae8ea96d340d552bbe8
Mike Weis <miweis@ebay.com>
2014-05-29 15:37:46 -0500

    MAGETWO-24801: Unit Test for \Magento\Tax\Model\Config.php
     - updated to use modern array notation

--- dev/tests/unit/testsuite/Magento/Tax/Model/ConfigTest.php
+++ dev/tests/unit/testsuite/Magento/Tax/Model/ConfigTest.php
@@ -42,20 +42,22 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
      */
     public function dataProviderDirectSettersGettersMethods()
     {
-        return $this->_buildTrueFalseArray(array(
-            array(
-                'setShippingPriceIncludeTax',
-                'shippingPriceIncludesTax'
-            ),
-            array(
-                'setNeedUseShippingExcludeTax',
-                'getNeedUseShippingExcludeTax'
-            ),
-            array(
-                'setPriceIncludesTax',
-                'priceIncludesTax'
-            )
-        ));
+        return $this->_buildTrueFalsePairs(
+            [
+                [
+                    'setShippingPriceIncludeTax',
+                    'shippingPriceIncludesTax'
+                ],
+                [
+                    'setNeedUseShippingExcludeTax',
+                    'getNeedUseShippingExcludeTax'
+                ],
+                [
+                    'setPriceIncludesTax',
+                    'priceIncludesTax'
+                ]
+            ]
+        );
     }
 
     /**
@@ -65,16 +67,16 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
      * @param array $arrayIn
      * @return array
      */
-    protected function _buildTrueFalseArray($arrayIn)
+    protected function _buildTrueFalsePairs($arrayIn)
     {
-        $arrayOut = array();
+        $arrayOut = [];
 
         foreach ($arrayIn as $paramArray) {
-            // Replicate the paramArray, append 'true', and add the new array to the output array
+            // replicate the paramArray, append 'true', and add the new array to the output array
             $arrayT = $paramArray;
             $arrayT[] = true;
             $arrayOut[] = $arrayT;
-            // Replicate the paramArray, append 'false', and add the new array to the output array
+            // replicate the paramArray, append 'false', and add the new array to the output array
             $arrayF = $paramArray;
             $arrayF[] = false;
             $arrayOut[] = $arrayF;
@@ -110,12 +112,12 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
      */
     public function dataProviderGetCalculationSequence()
     {
-        return array(
-            array(true,  true,  \Magento\Tax\Model\Calculation::CALC_TAX_AFTER_DISCOUNT_ON_INCL),
-            array(true,  false, \Magento\Tax\Model\Calculation::CALC_TAX_AFTER_DISCOUNT_ON_EXCL),
-            array(false, true,  \Magento\Tax\Model\Calculation::CALC_TAX_BEFORE_DISCOUNT_ON_INCL),
-            array(false, false, \Magento\Tax\Model\Calculation::CALC_TAX_BEFORE_DISCOUNT_ON_EXCL)
-        );
+        return [
+            [true,  true,  \Magento\Tax\Model\Calculation::CALC_TAX_AFTER_DISCOUNT_ON_INCL],
+            [true,  false, \Magento\Tax\Model\Calculation::CALC_TAX_AFTER_DISCOUNT_ON_EXCL],
+            [false, true,  \Magento\Tax\Model\Calculation::CALC_TAX_BEFORE_DISCOUNT_ON_INCL],
+            [false, false, \Magento\Tax\Model\Calculation::CALC_TAX_BEFORE_DISCOUNT_ON_EXCL]
+        ];
     }
 
 
@@ -147,241 +149,241 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
      */
     public function dataProviderScopeConfigMethods()
     {
-        return array(
-            array(
+        return [
+            [
                 'priceIncludesTax',
                 Config::CONFIG_XML_PATH_PRICE_INCLUDES_TAX,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'applyTaxAfterDiscount',
                 Config::CONFIG_XML_PATH_APPLY_AFTER_DISCOUNT,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'getPriceDisplayType',
                 Config::CONFIG_XML_PATH_PRICE_DISPLAY_TYPE,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'discountTax',
                 Config::CONFIG_XML_PATH_DISCOUNT_TAX,
                 1,
                 true
-            ),
-            array(
+            ],
+            [
                 'getAlgorithm',
                 Config::XML_PATH_ALGORITHM,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'getShippingTaxClass',
                 Config::CONFIG_XML_PATH_SHIPPING_TAX_CLASS,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'getShippingPriceDisplayType',
                 Config::CONFIG_XML_PATH_DISPLAY_SHIPPING,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'shippingPriceIncludesTax',
                 Config::CONFIG_XML_PATH_SHIPPING_INCLUDES_TAX,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartPricesInclTax',
                 Config::XML_PATH_DISPLAY_CART_PRICE,
                 Config::DISPLAY_TYPE_INCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartPricesExclTax',
                 Config::XML_PATH_DISPLAY_CART_PRICE,
                 Config::DISPLAY_TYPE_EXCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartPricesBoth',
                 Config::XML_PATH_DISPLAY_CART_PRICE,
                 Config::DISPLAY_TYPE_BOTH,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartSubtotalInclTax',
                 Config::XML_PATH_DISPLAY_CART_SUBTOTAL,
                 Config::DISPLAY_TYPE_INCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartSubtotalExclTax',
                 Config::XML_PATH_DISPLAY_CART_SUBTOTAL,
                 Config::DISPLAY_TYPE_EXCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartSubtotalBoth',
                 Config::XML_PATH_DISPLAY_CART_SUBTOTAL,
                 Config::DISPLAY_TYPE_BOTH,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartShippingInclTax',
                 Config::XML_PATH_DISPLAY_CART_SHIPPING,
                 Config::DISPLAY_TYPE_INCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartShippingExclTax',
                 Config::XML_PATH_DISPLAY_CART_SHIPPING,
                 Config::DISPLAY_TYPE_EXCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartShippingBoth',
                 Config::XML_PATH_DISPLAY_CART_SHIPPING,
                 Config::DISPLAY_TYPE_BOTH,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartDiscountInclTax',
                 Config::XML_PATH_DISPLAY_CART_DISCOUNT,
                 Config::DISPLAY_TYPE_INCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartDiscountExclTax',
                 Config::XML_PATH_DISPLAY_CART_DISCOUNT,
                 Config::DISPLAY_TYPE_EXCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartDiscountBoth',
                 Config::XML_PATH_DISPLAY_CART_DISCOUNT,
                 Config::DISPLAY_TYPE_BOTH,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartTaxWithGrandTotal',
                 Config::XML_PATH_DISPLAY_CART_GRANDTOTAL,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartFullSummary',
                 Config::XML_PATH_DISPLAY_CART_FULL_SUMMARY,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'displayCartZeroTax',
                 Config::XML_PATH_DISPLAY_CART_ZERO_TAX,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesPricesInclTax',
                 Config::XML_PATH_DISPLAY_SALES_PRICE,
                 Config::DISPLAY_TYPE_INCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesPricesExclTax',
                 Config::XML_PATH_DISPLAY_SALES_PRICE,
                 Config::DISPLAY_TYPE_EXCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesPricesBoth',
                 Config::XML_PATH_DISPLAY_SALES_PRICE,
                 Config::DISPLAY_TYPE_BOTH,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesSubtotalInclTax',
                 Config::XML_PATH_DISPLAY_SALES_SUBTOTAL,
                 Config::DISPLAY_TYPE_INCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesSubtotalExclTax',
                 Config::XML_PATH_DISPLAY_SALES_SUBTOTAL,
                 Config::DISPLAY_TYPE_EXCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesSubtotalBoth',
                 Config::XML_PATH_DISPLAY_SALES_SUBTOTAL,
                 Config::DISPLAY_TYPE_BOTH,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesShippingInclTax',
                 Config::XML_PATH_DISPLAY_SALES_SHIPPING,
                 Config::DISPLAY_TYPE_INCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesShippingExclTax',
                 Config::XML_PATH_DISPLAY_SALES_SHIPPING,
                 Config::DISPLAY_TYPE_EXCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesShippingBoth',
                 Config::XML_PATH_DISPLAY_SALES_SHIPPING,
                 Config::DISPLAY_TYPE_BOTH,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesDiscountInclTax',
                 Config::XML_PATH_DISPLAY_SALES_DISCOUNT,
                 Config::DISPLAY_TYPE_INCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalestDiscountExclTax',
                 Config::XML_PATH_DISPLAY_SALES_DISCOUNT,
                 Config::DISPLAY_TYPE_EXCLUDING_TAX,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesDiscountBoth',
                 Config::XML_PATH_DISPLAY_SALES_DISCOUNT,
                 Config::DISPLAY_TYPE_BOTH,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesTaxWithGrandTotal',
                 Config::XML_PATH_DISPLAY_SALES_GRANDTOTAL,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesFullSummary',
                 Config::XML_PATH_DISPLAY_SALES_FULL_SUMMARY,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'displaySalesZeroTax',
                 Config::XML_PATH_DISPLAY_SALES_ZERO_TAX,
                 true,
                 true
-            ),
-            array(
+            ],
+            [
                 'crossBorderTradeEnabled',
                 Config::CONFIG_XML_PATH_CROSS_BORDER_TRADE_ENABLED,
                 true,
                 true
-            )
-        );
+            ]
+        ];
     }
 }
