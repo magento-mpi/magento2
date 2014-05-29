72b3d0e28f87505f36faaf81cb2479f20c190733
Eric Nguyen <ernguyen@ebay.com>
2014-05-29 15:00:24 -0500

    MAGETWO-24633: Cover lib/Magento/Framework/Less/PreProcessor/Instruction/Import/Import.php
     - Fixed many issues regarding code readability to conform to Coding Standards
     - Refactored tests and code to improve readability
     - Added test case for malformed string

--- dev/tests/unit/testsuite/Magento/Framework/Less/PreProcessor/Instruction/ImportTest.php
+++ dev/tests/unit/testsuite/Magento/Framework/Less/PreProcessor/Instruction/ImportTest.php
@@ -8,79 +8,163 @@
  */
 namespace Magento\Framework\Less\PreProcessor\Instruction;
 
+use Magento\Framework\Filesystem\FilesystemException;
+
 class ImportTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * Sample @import instructions.
      */
-    const SAMPLE_IMPORT_1 = '@import "foo";';
-    const SAMPLE_IMPORT_2 = '@import (reference) "foo" (image);';
+    const SAMPLE_IMPORT_SIMPLE = '@import "foo";';
+    const SAMPLE_IMPORT_COMPLEX = '@import (reference) "foo" (image);';
+    const SAMPLE_IMPORT_MALFORMED = '@this "is a malformed" (input);';
 
     /** @var  \Magento\Framework\View\RelatedFile | PHPUnit_Framework_MockObject */
-    protected $relatedFileMock;
+    private $relatedFileMock;
 
     /** @var  \Magento\Framework\Less\PreProcessor\ErrorHandlerInterface | PHPUnit_Framework_MockObject */
-    protected $errorHandlerMock;
+    private $errorHandlerMock;
 
     /** @var  \Magento\Framework\Less\PreProcessor\File\FileList | PHPUnit_Framework_MockObject */
-    protected $lessFileListMock;
+    private $lessFileListMock;
 
     /** @var  \Magento\Framework\Less\PreProcessor\File\Less | PHPUnit_Framework_MockObject */
-    protected $lessFileMock;
+    private $lessFileMock;
+
+    /** @var  \Magento\Framework\Less\PreProcessor\Instruction\Import */
+    private $importObject;
 
     public function setUp()
     {
-        $this->relatedFileMock = $this->getMockBuilder('\Magento\Framework\View\RelatedFile')
-                                      ->disableOriginalConstructor()->getMock();
-        $this->errorHandlerMock = $this->getMockBuilder('\Magento\Framework\Less\PreProcessor\ErrorHandlerInterface')
-                                       ->getMock();
-        $this->lessFileListMock = $this->getMockBuilder('\Magento\Framework\Less\PreProcessor\File\FileList')
-                                       ->disableOriginalConstructor()->getMock();
+        $this->relatedFileMock =
+            $this->getMockBuilder('\Magento\Framework\View\RelatedFile')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $this->lessFileMock = $this->getMockBuilder('\Magento\Framework\Less\PreProcessor\File\Less')
-                                   ->disableOriginalConstructor()->getMock();
+        $this->errorHandlerMock =
+            $this->getMockBuilder('\Magento\Framework\Less\PreProcessor\ErrorHandlerInterface')
+            ->getMock();
 
-        $this->lessFileMock->expects($this->any())->method('getFilePath')->will($this->returnValue('/'));
+        $this->lessFileListMock =
+            $this->getMockBuilder('\Magento\Framework\Less\PreProcessor\File\FileList')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $this->lessFileListMock->expects($this->any())->method('createFile')
-                               ->will($this->returnValue($this->lessFileMock));
+        $this->lessFileMock =
+            $this->getMockBuilder('\Magento\Framework\Less\PreProcessor\File\Less')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->lessFileMock
+            ->expects($this->any())
+            ->method('getFilePath')
+            ->will($this->returnValue('/'));
+
+        $this->lessFileListMock
+            ->expects($this->any())
+            ->method('createFile')
+            ->will($this->returnValue($this->lessFileMock));
+
+        $this->importObject = new Import(
+            $this->relatedFileMock,
+            $this->errorHandlerMock,
+            $this->lessFileListMock
+        );
     }
 
-    public function testProcessNoReplacement()
+    public function testProcessBlankRequestNoReplacement()
     {
-        $importObject = new Import($this->relatedFileMock, $this->errorHandlerMock, $this->lessFileListMock);
+        $expectedValue = '';
 
-        $this->assertEquals('', $importObject->process($this->lessFileMock, ''));
+        $this->lessFileMock
+            ->expects($this->never())
+            ->method('getViewParams');
 
-        $this->assertEquals('', $importObject->process($this->lessFileMock, self::SAMPLE_IMPORT_1));
+        $actualValueBlankRequest = $this->importObject
+            ->process($this->lessFileMock, '');
+        $this->assertEquals($expectedValue, $actualValueBlankRequest);
+
     }
 
-    public function testProcessSingleReplacement()
+    public function testProcessSimpleRequestNoReplacement()
     {
-        $importObject = new Import($this->relatedFileMock, $this->errorHandlerMock, $this->lessFileListMock);
+        $expectedValue = '';
 
-        $this->lessFileMock->expects($this->any())->method('getPublicationPath')->will($this->returnValue('bar'));
+        $this->lessFileMock
+            ->expects($this->once())
+            ->method('getViewParams');
 
-        $this->assertEquals("@import 'bar';", $importObject->process($this->lessFileMock, self::SAMPLE_IMPORT_1));
+        $this->lessFileListMock
+            ->expects($this->once())
+            ->method('createFile')
+            ->with(null, null);
 
+        $this->lessFileListMock
+            ->expects($this->once())
+            ->method('addFile');
+
+        $actualValueSimpleRequestNoReplacement = $this->importObject
+            ->process($this->lessFileMock, self::SAMPLE_IMPORT_SIMPLE);
+        $this->assertEquals($expectedValue, $actualValueSimpleRequestNoReplacement);
+    }
+
+    public function testProcessSimpleRequestSingleReplacement()
+    {
+        $expectedValue = "@import 'bar';";
+
+        $this->lessFileMock
+            ->expects($this->once())
+            ->method('getPublicationPath')
+            ->will($this->returnValue('bar'));
+
+        $actualValueSimpleReplacement = $this->importObject
+            ->process($this->lessFileMock, self::SAMPLE_IMPORT_SIMPLE);
+
+        $this->assertEquals($expectedValue, $actualValueSimpleReplacement);
+    }
+
+    public function testProcessComplexRequestSingleReplacement()
+    {
         $expectedValue = "@import (reference) 'bar'  (image);";
 
-        $this->assertEquals($expectedValue,
-            $importObject->process($this->lessFileMock, self::SAMPLE_IMPORT_2)
-        );
+        $this->lessFileMock
+            ->expects($this->once())
+            ->method('getPublicationPath')
+            ->will($this->returnValue('bar'));
 
+        $actualValueComplexReplacement = $this->importObject
+            ->process($this->lessFileMock, self::SAMPLE_IMPORT_COMPLEX);
+
+        $this->assertEquals($expectedValue, $actualValueComplexReplacement);
+    }
+
+    public function testMalformedRequest()
+    {
+        $expectedValue = self::SAMPLE_IMPORT_MALFORMED;
+
+        $this->lessFileMock
+            ->expects($this->never())
+            ->method('getViewParams');
+
+        $actualValueMalformedRequest = $this->importObject
+            ->process($this->lessFileMock, self::SAMPLE_IMPORT_MALFORMED);
+
+        $this->assertEquals($expectedValue, $actualValueMalformedRequest);
     }
 
     public function testProcessException()
     {
-        $importObject = new Import($this->relatedFileMock, $this->errorHandlerMock, $this->lessFileListMock);
+        $expectedValue = '';
 
-        $this->lessFileListMock->expects($this->any())->method('addFile')
-                               ->will($this->throwException(new \Magento\Framework\Filesystem\FilesystemException));
+        $this->lessFileMock
+            ->expects($this->once())
+            ->method('getPublicationPath')
+            ->will($this->throwException(new FilesystemException));
 
-        $this->lessFileMock->expects($this->any())->method('getPublicationPath')->will($this->returnValue('car'));
+        $actualValueExceptionRequest = $this->importObject
+            ->process($this->lessFileMock, self::SAMPLE_IMPORT_SIMPLE);
 
-        $this->assertEquals('', $importObject->process($this->lessFileMock, self::SAMPLE_IMPORT_1));
+        $this->assertEquals($expectedValue, $actualValueExceptionRequest);
     }
 
 }
