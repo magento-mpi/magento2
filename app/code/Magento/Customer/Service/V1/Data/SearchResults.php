514bcfe720ffd11f49590b5022a1d44473f156de
Joan He <joan@x.com>
2014-05-29 15:53:25 -0500

    MAGETWO-24717: CR change

--- app/code/Magento/Customer/Service/V1/Data/SearchResults.php
+++ app/code/Magento/Customer/Service/V1/Data/SearchResults.php
@@ -10,36 +10,10 @@ namespace Magento\Customer\Service\V1\Data;
 
 /**
  * SearchResults Service Data Object used for the search service requests
+ *
+ * @method \Magento\Customer\Service\V1\Data\CustomerDetails[] getItems()
  */
-class SearchResults extends \Magento\Framework\Service\Data\AbstractObject
+class SearchResults extends \Magento\Framework\Service\V1\Data\SearchResults
 {
-    /**
-     * Get items
-     *
-     * @return \Magento\Customer\Service\V1\Data\CustomerDetails[]
-     */
-    public function getItems()
-    {
-        return is_null($this->_get('items')) ? array() : $this->_get('items');
-    }
 
-    /**
-     * Get search criteria
-     *
-     * @return \Magento\Framework\Service\V1\Data\SearchCriteria
-     */
-    public function getSearchCriteria()
-    {
-        return $this->_get('search_criteria');
-    }
-
-    /**
-     * Get total count
-     *
-     * @return int
-     */
-    public function getTotalCount()
-    {
-        return $this->_get('total_count');
-    }
 }
--- app/code/Magento/Tax/Service/V1/Data/TaxRuleSearchResults.php
+++ app/code/Magento/Tax/Service/V1/Data/TaxRuleSearchResults.php
@@ -12,36 +12,10 @@ use Magento\Framework\Service\Data\AbstractObject;
 
 /**
  * TaxRuleSearchResults Service Data Object used for the search service requests
+ *
+ * @method \Magento\Tax\Service\V1\Data\TaxRule[] getItems()
  */
 class TaxRuleSearchResults extends AbstractObject
 {
-    /**
-     * Get tax rule items
-     *
-     * @return \Magento\Tax\Service\V1\Data\TaxRule[]
-     */
-    public function getItems()
-    {
-        return is_null($this->_get('items')) ? [] : $this->_get('items');
-    }
 
-    /**
-     * Get search criteria
-     *
-     * @return \Magento\Framework\Service\V1\Data\SearchCriteria
-     */
-    public function getSearchCriteria()
-    {
-        return $this->_get('search_criteria');
-    }
-
-    /**
-     * Get total count
-     *
-     * @return int
-     */
-    public function getTotalCount()
-    {
-        return $this->_get('total_count');
-    }
 }
