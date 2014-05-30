6dd93a3bd8aa39f397ceca1e332f047762fdc00d
Joan He <joan@x.com>
2014-05-30 08:55:26 -0500

    MAGETWO-24718: CR change

--- app/code/Magento/Customer/Service/V1/Data/SearchResultsBuilder.php
+++ app/code/Magento/Customer/Service/V1/Data/SearchResultsBuilder.php
@@ -13,32 +13,10 @@ use Magento\Framework\Service\V1\Data\SearchCriteria;
 /**
  * Builder for the SearchResults Service Data Object
  *
- * @method SearchResults create()
+ * @method \Magento\Customer\Service\V1\Data\SearchResults create()
  */
-class SearchResultsBuilder extends \Magento\Framework\Service\Data\AbstractObjectBuilder
+class SearchResultsBuilder extends \Magento\Framework\Service\V1\Data\SearchResultsBuilder
 {
-    /**
-     * Set search criteria
-     *
-     * @param SearchCriteria $searchCriteria
-     * @return $this
-     */
-    public function setSearchCriteria(SearchCriteria $searchCriteria)
-    {
-        return $this->_set('search_criteria', $searchCriteria);
-    }
-
-    /**
-     * Set total count
-     *
-     * @param int $totalCount
-     * @return $this
-     */
-    public function setTotalCount($totalCount)
-    {
-        return $this->_set('total_count', $totalCount);
-    }
-
     /**
      * Set items
      *
@@ -47,6 +25,6 @@ class SearchResultsBuilder extends \Magento\Framework\Service\Data\AbstractObjec
      */
     public function setItems($items)
     {
-        return $this->_set('items', $items);
+        return parent::setItems($items);
     }
 }
---
+++ app/code/Magento/Tax/Service/V1/Data/TaxRuleSearchResultsBuilder.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Tax\Service\V1\Data;
+
+use Magento\Framework\Service\V1\Data\SearchCriteria;
+use Magento\Framework\Service\V1\Data\SearchResultsBuilder;
+
+/**
+ * Builder for the TaxRuleSearchResults Service Data Object
+ *
+ * @method TaxRuleSearchResults create()
+ */
+class TaxRuleSearchResultsBuilder extends SearchResultsBuilder
+{
+    /**
+     * Constructor
+     *
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param TaxRuleBuilder $itemObjectBuilder
+     */
+    public function __construct(
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        TaxRuleBuilder $itemObjectBuilder
+    ) {
+        parent::__construct($searchCriteriaBuilder, $itemObjectBuilder);
+    }
+
+    /**
+     * Set items
+     *
+     * @param \Magento\Tax\Service\V1\Data\TaxRule[] $items
+     * @return $this
+     */
+    public function setItems($items)
+    {
+        return parent::setItems($items);
+    }
+}
