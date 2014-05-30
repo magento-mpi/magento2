67e103a476dc9df74d0ce069dcd79e79334b1a4b
Yaroslav Onischenko <yonischenko@ebay.com>
2014-05-30 22:01:19 +0300

    MAGETWO-10727: 'Recurring Profile' section isn't updated after changing product template

--- dev/tests/unit/testsuite/Magento/Catalog/Controller/Adminhtml/ProductTest.php
+++ dev/tests/unit/testsuite/Magento/Catalog/Controller/Adminhtml/ProductTest.php
@@ -32,6 +32,27 @@ class ProductTest extends \PHPUnit_Framework_TestCase
             array('get')
         );
         $objectManagerMock->expects($this->any())->method('get')->will($this->returnValue($productActionMock));
+        $eventManager = $this->getMockBuilder('Magento\Framework\Event\Manager')
+            ->setMethods(['dispatch'])->disableOriginalConstructor()->getMock();
+        $eventManager->expects($this->any())->method('dispatch')->will($this->returnSelf());
+
+        $block = $this->getMockBuilder('\Magento\Framework\View\Element\AbstractBlock')
+            ->disableOriginalConstructor()->getMockForAbstractClass();
+        $layout = $this->getMockBuilder('Magento\Framework\View\Layout\Element\Layout')
+            ->setMethods(['getBlock'])->disableOriginalConstructor()
+            ->getMock();
+        $layout->expects($this->any())->method('getBlock')->will($this->returnValue($block));
+        $view = $this->getMockBuilder('Magento\Framework\App\View')
+            ->setMethods(['loadLayout', 'getLayout', 'renderLayout'])
+            ->disableOriginalConstructor()->getMock();
+        $view->expects($this->any())->method('renderLayout')->will($this->returnSelf());
+        $view->expects($this->any())->method('getLayout')->will($this->returnValue($layout));
+        $view->expects($this->any())->method('loadLayout')->with(array(
+            'popup',
+            'catalog_product_new',
+            'catalog_product_simple'
+        ))->will($this->returnSelf());
+
         $this->_priceProcessor = $this->getMock(
             'Magento\Catalog\Model\Indexer\Product\Price\Processor',
             array(),
@@ -39,7 +60,14 @@ class ProductTest extends \PHPUnit_Framework_TestCase
             '',
             false
         );
-        $requestInterfaceMock = $this->getMock('Magento\Framework\App\RequestInterface', array(), array(), '', false);
+        $requestInterfaceMock = $this->getMock(
+            'Magento\Framework\App\Request\Http',
+            array('getParam', 'getFullActionName'),
+            array(),
+            '',
+            false
+        );
+
         $responseInterfaceMock = $this->getMock(
             'Magento\Framework\App\ResponseInterface',
             array('setRedirect', 'sendResponse')
@@ -60,24 +88,44 @@ class ProductTest extends \PHPUnit_Framework_TestCase
                 'getRequest',
                 'getResponse',
                 'getObjectManager',
+                'getEventManager',
                 'getMessageManager',
                 'getSession',
                 'getActionFlag',
-                'getHelper'
+                'getHelper',
+                'getTitle',
+                'getView'
             ),
             array(),
             '',
             false
         );
+        $title = $this->getMockBuilder('\Magento\Framework\App\Action\Title')
+            ->setMethods(['add'])->disableOriginalConstructor()->getMock();
+        $title->expects($this->any())->method('add')->withAnyParameters()->will($this->returnSelf());
+
+        $contextMock->expects($this->any())->method('getTitle')->will($this->returnValue($title));
         $contextMock->expects($this->any())->method('getRequest')->will($this->returnValue($requestInterfaceMock));
         $contextMock->expects($this->any())->method('getResponse')->will($this->returnValue($responseInterfaceMock));
         $contextMock->expects($this->any())->method('getObjectManager')->will($this->returnValue($objectManagerMock));
+        $contextMock->expects($this->any())->method('getEventManager')->will($this->returnValue($eventManager));
+        $contextMock->expects($this->any())->method('getView')->will($this->returnValue($view));
         $contextMock->expects($this->any())
             ->method('getMessageManager')
             ->will($this->returnValue($managerInterfaceMock));
         $contextMock->expects($this->any())->method('getSession')->will($this->returnValue($sessionMock));
         $contextMock->expects($this->any())->method('getActionFlag')->will($this->returnValue($actionFlagMock));
         $contextMock->expects($this->any())->method('getHelper')->will($this->returnValue($helperDataMock));
+        $productBuilder = $this->getMockBuilder('Magento\Catalog\Controller\Adminhtml\Product\Builder')->setMethods([
+            'build'
+        ])->disableOriginalConstructor()->getMock();
+
+        $product = $this->getMockBuilder('\Magento\Catalog\Model\Product')->disableOriginalConstructor()
+            ->setMethods(['getTypeId', 'getStoreId', '__sleep', '__wakeup'])->getMock();
+        $product->expects($this->any())->method('getTypeId')->will($this->returnValue('simple'));
+        $product->expects($this->any())->method('getStoreId')->will($this->returnValue('1'));
+        $productBuilder->expects($this->any())->method('build')->will($this->returnValue($product));
+
         $this->_controller = new \Magento\Catalog\Controller\Adminhtml\Product(
             $contextMock,
             $this->getMock('Magento\Framework\Registry', array(), array(), '', false),
@@ -97,7 +145,7 @@ class ProductTest extends \PHPUnit_Framework_TestCase
                 false
             ),
             $this->getMock('Magento\Catalog\Model\Product\Copier', array(), array(), '', false),
-            $this->getMock('Magento\Catalog\Controller\Adminhtml\Product\Builder', array(), array(), '', false),
+            $productBuilder,
             $this->getMock('Magento\Catalog\Model\Product\Validator', array(), array(), '', false),
             $this->getMock('Magento\Catalog\Model\Product\TypeTransitionManager', array(), array(), '', false),
             $this->_priceProcessor
@@ -109,5 +157,19 @@ class ProductTest extends \PHPUnit_Framework_TestCase
         $this->_priceProcessor->expects($this->once())->method('reindexList');
 
         $this->_controller->massStatusAction();
+    }
+
+    /**
+     * Testing `newAction` method
+     */
+    public function testNewAction()
+    {
+        $this->_controller->getRequest()->expects($this->at(0))->method('getParam')
+            ->with('set')->will($this->returnValue(true));
+        $this->_controller->getRequest()->expects($this->at(1))->method('getParam')
+            ->with('popup')->will($this->returnValue(true));
+        $this->_controller->getRequest()->expects($this->any())->method('getFullActionName')
+            ->will($this->returnValue('catalog_product_new'));
+        $this->_controller->newAction();
     }
 }
