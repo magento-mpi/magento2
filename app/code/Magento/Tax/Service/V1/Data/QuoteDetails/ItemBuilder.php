8e76880073f60dd07ec229d630c2446ffa288dd7
Joan He <joan@x.com>
2014-06-03 15:57:59 -0500

    MAGETWO-25007: Created QuoteDetails Item builder

--- app/code/Magento/Tax/Service/V1/Data/QuoteDetails/ItemBuilder.php
+++ app/code/Magento/Tax/Service/V1/Data/QuoteDetails/ItemBuilder.php
@@ -17,100 +17,110 @@ class ItemBuilder extends \Magento\Framework\Service\Data\AbstractObjectBuilder
     /**
      * Set code (sku or shipping code)
      *
+     * @param string $code
      * @return $this
      */
-    public function setCode()
+    public function setCode($code)
     {
-        return $this->_set(Item::KEY_CODE);
+        return $this->_set(Item::KEY_CODE, $code);
     }
 
     /**
      * Set type (e.g., shipping, product, wee, gift wrapping, etc.)
      *
+     * @param string $type
      * @return $this
      */
-    public function setType()
+    public function setType($type)
     {
-        return $this->_set(Item::KEY_TYPE);
+        return $this->_set(Item::KEY_TYPE, $type);
     }
 
     /**
      * Set tax class id
      *
+     * @param int $taxClassId
      * @return $this
      */
-    public function setTaxClassId()
+    public function setTaxClassId($taxClassId)
     {
-        return $this->_set(Item::KEY_TAX_CLASS_ID);
+        return $this->_set(Item::KEY_TAX_CLASS_ID, $taxClassId);
     }
 
     /**
      * Set row total
      *
+     * @param float $rowTotal
      * @return $this
      */
-    public function setRowTotal()
+    public function setRowTotal($rowTotal)
     {
-        return $this->_set(Item::KEY_ROW_TOTAL);
+        return $this->_set(Item::KEY_ROW_TOTAL, $rowTotal);
     }
 
     /**
      * Set unit price
      *
+     * @param float $unitPrice
      * @return $this
      */
-    public function setUnitPrice()
+    public function setUnitPrice($unitPrice)
     {
-        return $this->_set(Item::KEY_UNIT_PRICE);
+        return $this->_set(Item::KEY_UNIT_PRICE, $unitPrice);
     }
 
     /**
      * Set quantity
      *
+     * @param int $qty
      * @return $this
      */
-    public function setQty()
+    public function setQty($qty)
     {
-        return $this->_set(Item::KEY_QTY);
+        return $this->_set(Item::KEY_QTY, $qty);
     }
 
     /**
      * Set indicate that if the tax is included in the unit price and row total
      *
+     * @param bool $taxIncluded
      * @return $this
      */
-    public function setTaxIncluded()
+    public function setTaxIncluded($taxIncluded)
     {
-        return $this->_set(Item::KEY_TAX_INCLUDED);
+        return $this->_set(Item::KEY_TAX_INCLUDED, $taxIncluded);
     }
 
     /**
      * Set short description
      *
+     * @param string $shortDescription
      * @return $this
      */
-    public function setShortDescription()
+    public function setShortDescription($shortDescription)
     {
-        return $this->_set(Item::KEY_SHORT_DESCRIPTION);
+        return $this->_set(Item::KEY_SHORT_DESCRIPTION, $shortDescription);
     }
 
     /**
      * Set discount amount
      *
+     * @param float $amount
      * @return $this
      */
-    public function setDiscountAmount()
+    public function setDiscountAmount($amount)
     {
-        return $this->_set(Item::KEY_DISCOUNT_AMOUNT);
+        return $this->_set(Item::KEY_DISCOUNT_AMOUNT, $amount);
     }
 
     /**
      * Set related code
      *
+     * @param string $code
      * @return $this
      */
-    public function setRelatedCode()
+    public function setRelatedCode($code)
     {
-        return $this->_set(Item::KEY_RELATED_CODE);
+        return $this->_set(Item::KEY_RELATED_CODE, $code);
     }
 }
