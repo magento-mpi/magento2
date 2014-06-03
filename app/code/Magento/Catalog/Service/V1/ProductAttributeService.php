68c7b3dfeee50afea12096655a32b5cc76adc72c
Maksym Iakusha <miakusha@ebay.com>
2014-06-03 17:23:38 +0300

    MAGETWO-23312: Create ProductAttributesService
     - MAGETWO-24911: Create method product_attribute.options

---
+++ app/code/Magento/Catalog/Service/V1/ProductAttributeService.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Catalog\Service\V1;
+
+
+/**
+ * Class ProductAttributeService
+ * @package Magento\Catalog\Service\V1
+ */
+class ProductAttributeService implements ProductAttributeServiceInterface
+{
+    /**
+     * @var ProductMetadataServiceInterface
+     */
+    private $metadataService;
+
+
+    /**
+     * @param ProductMetadataServiceInterface $metadataService
+     */
+    public function __construct(
+        ProductMetadataServiceInterface $metadataService
+    ) {
+        $this->metadataService = $metadataService;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function options($id)
+    {
+
+        return $this->metadataService->getAttributeMetadata(
+            ProductMetadataServiceInterface::ENTITY_TYPE_PRODUCT,
+            $id
+        )->getOptions();
+    }
+}
