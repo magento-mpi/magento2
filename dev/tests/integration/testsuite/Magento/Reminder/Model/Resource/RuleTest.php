aafffc236ea69739ee7056caba5d23d1f9133fb0
Victor Rad <vrad@ebay.com>
2014-06-03 18:13:01 +0300

    MAGETWO-24579: Pull Request (Sprint1)
    - fix for auto-test of MAGETWO-21056 bug

--- dev/tests/integration/testsuite/Magento/Reminder/Model/Resource/RuleTest.php
+++ dev/tests/integration/testsuite/Magento/Reminder/Model/Resource/RuleTest.php
@@ -39,7 +39,7 @@ class RuleTest extends \PHPUnit_Framework_TestCase
 
     public function dataProviderTestGetCustomersForNotification()
     {
-        $beforeYesterday = date('Y-m-d 03:00:00', strtotime('-2 day', time()));
+        $beforeYesterday = date('Y-m-d h:00:00', strtotime('-2 day', time()));
         return ['first' =>
             [[['customer_id' => 1, 'coupon_id' => null, 'rule_id' => null, 'schedule' => 2,
                 'log_sent_at_max' => $beforeYesterday, 'log_sent_at_min' => $beforeYesterday]]]
--- dev/tests/integration/testsuite/Magento/Reminder/_files/customersForNotification.php
+++ dev/tests/integration/testsuite/Magento/Reminder/_files/customersForNotification.php
@@ -65,8 +65,9 @@ $rule->setData(
     )
 )->save();
 
-$beforeYesterday = date('Y-m-d 03:00:00', strtotime('-2 day', time()));
-$adapter = $rule->getResource()->getReadConnection();
-$adapter->query("UPDATE sales_flat_quote SET updated_at = '$beforeYesterday'");
-$adapter->query("INSERT INTO magento_reminder_rule_log (`rule_id`, `customer_id`, `sent_at`)
+$beforeYesterday = date('Y-m-d h:00:00', strtotime('-2 day', time()));
+$resource = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Framework\App\Resource');
+$adapter = $resource->getConnection('core_write');
+$adapter->query("UPDATE {$resource->getTableName('sales_flat_quote')} SET updated_at = '{$beforeYesterday}'");
+$adapter->query("INSERT INTO {$resource->getTableName('magento_reminder_rule_log')} (`rule_id`, `customer_id`, `sent_at`)
     VALUES ({$rule->getId()}, 1, '{$beforeYesterday}');");
