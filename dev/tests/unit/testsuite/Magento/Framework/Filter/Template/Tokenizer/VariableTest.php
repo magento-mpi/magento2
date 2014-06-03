44d8e1bf41ac3e96bfb1184a7c0b9a353fe4ae5b
Anup Dugar <anup@x.com>
2014-06-03 12:32:43 -0500

    MAGETWO-24749: Cover lib/Magento/Framework/Filter files. Part 1

--- dev/tests/unit/testsuite/Magento/Framework/Filter/Template/Tokenizer/VariableTest.php
+++ dev/tests/unit/testsuite/Magento/Framework/Filter/Template/Tokenizer/VariableTest.php
@@ -19,34 +19,26 @@ class VariableTest extends \PHPUnit_Framework_TestCase
         $this->_filter = new Variable();
     }
 
-    public function testTokenize()
+    /**
+     * @param string $string String to tokenize
+     * @param string $expectedValue
+     * @dataProvider sampleTokenizeStringProvider
+     */
+    public function testTokenize($string, $expectedValue)
     {
-        $this->assertEquals([], $this->_filter->tokenize());
+        $this->_filter->setString($string);
+        $this->assertEquals($expectedValue, $this->_filter->tokenize());
     }
 
-    public function testGetString()
+    public function sampleTokenizeStringProvider()
     {
-        $this->assertEquals('', $this->_filter->getString());
+        return [
+            ["firstname", [['type' => 'variable', 'name' => 'firstname']]],
+            [
+                "invoke(arg1, arg2, 2, 2.7, -1, 'Mike\\'s')",
+                [['type' => 'method', 'name' => 'invoke', 'args' => ['arg1', 'arg2', 2, 2.7, -1, "Mike's"]]]
+            ],
+            ["  ", []]
+        ];
     }
-
-    public function testIsNumeric()
-    {
-        $this->assertFalse($this->_filter->isNumeric());
-    }
-
-    public function testIsQuote()
-    {
-        $this->assertFalse($this->_filter->isQuote());
-    }
-
-    public function testGetMethodArgs()
-    {
-        $this->assertEquals([], $this->_filter->getMethodArgs());
-    }
-
-    public function testGetNumber()
-    {
-        $this->assertEquals(0, $this->_filter->getNumber());
-    }
-
 }
