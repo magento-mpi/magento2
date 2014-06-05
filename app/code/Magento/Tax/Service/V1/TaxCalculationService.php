1bedd98fe7c013de8e27b5b9b33624d1f726dba1
Bibu Mathew <bimathew@paypal.com>
2014-06-05 10:19:42 -0500

    MAGETWO-25015: Port _processItem
    - ported _processItem to Service processItem

--- app/code/Magento/Tax/Service/V1/TaxCalculationService.php
+++ app/code/Magento/Tax/Service/V1/TaxCalculationService.php
@@ -8,6 +8,7 @@
 
 namespace Magento\Tax\Service\V1;
 
+use Magento\Tax\Model\Calculation;
 use Magento\Tax\Service\V1\Data\QuoteDetails;
 use Magento\Tax\Service\V1\Data\TaxDetails;
 
@@ -20,7 +21,7 @@ class TaxCalculationService implements TaxCalculationServiceInterface
     /**
      * Tax calculation model
      *
-     * @var \Magento\Tax\Model\Calculation
+     * @var Calculation
      */
     protected $calculator = null;
 
@@ -34,12 +35,12 @@ class TaxCalculationService implements TaxCalculationServiceInterface
     /**
      * Constructor
      *
-     * @param \Magento\Tax\Model\Calculation $calculation
+     * @param Calculation $calculation
      * @param \Magento\Tax\Model\Config $config
      * @param StoreManagerInterface $storeManager
      */
     public function __construct(
-        \Magento\Tax\Model\Calculation $calculation,
+        Calculation $calculation,
         \Magento\Tax\Model\Config $config,
         StoreManagerInterface $storeManager
     ) {
@@ -85,11 +86,20 @@ class TaxCalculationService implements TaxCalculationServiceInterface
      * @param Data\QuoteDetails\Item $item
      * @param \Magento\Framework\Object $taxRequest
      * @param int $storeId
-     * @return Data\TaxDetails
+     * @return Data\TaxDetails|null
      */
     protected function processItem($item, $taxRequest, $storeId)
     {
-
+        switch ($this->config->getAlgorithm($storeId)) {
+            case Calculation::CALC_UNIT_BASE:
+                return $this->unitBaseCalculation($item, $taxRequest, $storeId);
+            case Calculation::CALC_ROW_BASE:
+                return $this->rowBaseCalculation($item, $taxRequest, $storeId);
+            case Calculation::CALC_TOTAL_BASE:
+                return $this->totalBaseCalculation($item, $taxRequest, $storeId);
+            default:
+                return null;
+        }
     }
 
     /**
