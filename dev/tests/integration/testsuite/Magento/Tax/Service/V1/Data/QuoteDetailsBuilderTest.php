793274db37620c6129d99e460ac9579e707f5d63
Bibu Mathew <bimathew@paypal.com>
2014-06-05 09:52:29 -0500

    MAGETWO-25008: Create QuoteDetails Builder
    - fixed the phpcs issue

--- dev/tests/integration/testsuite/Magento/Tax/Service/V1/Data/QuoteDetailsBuilderTest.php
+++ dev/tests/integration/testsuite/Magento/Tax/Service/V1/Data/QuoteDetailsBuilderTest.php
@@ -176,8 +176,8 @@ class QuoteDetailsBuilderTest extends \PHPUnit_Framework_TestCase
         }
         if (array_key_exists(QuoteDetails::KEY_ITEMS, $dataArray)) {
             $items = [];
-            foreach ($dataArray[QuoteDetails::KEY_ITEMS] as $ItemsArray) {
-                $items[] = $this->itemBuilder->populateWithArray($ItemsArray)->create();
+            foreach ($dataArray[QuoteDetails::KEY_ITEMS] as $itemsArray) {
+                $items[] = $this->itemBuilder->populateWithArray($itemsArray)->create();
             }
             $this->builder->setItems($items);
         }
