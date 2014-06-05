7a4dded9b702f02883a2f5ff042fce3301ef74ec
Iryna Savchenko <isavchenko@ebay.com>
2014-06-05 13:43:39 +0300

    MAGETWO-20860: Functional parity between Payment Bridge module in Magento 1.x and 2.0
    - fixed builds

--- app/code/Magento/PbridgePaypal/Block/Checkout/Payment/PaypalDirect.php
+++ app/code/Magento/PbridgePaypal/Block/Checkout/Payment/PaypalDirect.php
@@ -14,6 +14,13 @@ namespace Magento\PbridgePaypal\Block\Checkout\Payment;
 class PaypalDirect extends \Magento\Pbridge\Block\Payment\Form\AbstractForm
 {
     /**
+     * Paypal payment code
+     *
+     * @var string
+     */
+    protected $_code = \Magento\Paypal\Model\Config::METHOD_WPP_DIRECT;
+
+    /**
      * Return 3D validation flag
      *
      * @return bool
