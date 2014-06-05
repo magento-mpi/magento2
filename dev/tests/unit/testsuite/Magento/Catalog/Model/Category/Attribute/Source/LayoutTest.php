536528a03c67f6b32165b38f24791ac141f539e4
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-06-05 14:39:14 +0300

    MAGETWO-24245: [Dragons MX] Unit Test Coverage
     - MAGETWO-24926: Added test for class \Magento\Catalog\Model\Category\Attribute\Source\Layout

---
+++ dev/tests/unit/testsuite/Magento/Catalog/Model/Category/Attribute/Source/LayoutTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aohorodnyk
+ * Date: 05.06.14
+ * Time: 14:15
+ */
+
+namespace Magento\Catalog\Model\Category\Attribute\Source;
+
+use Magento\TestFramework\Helper\ObjectManager;
+
+class LayoutTest extends \PHPUnit_Framework_TestCase
+{
+    private $testArray = ['test1', ['test1']];
+    /**
+     * @var \Magento\Catalog\Model\Category\Attribute\Source\Layout
+     */
+    private $model;
+
+    public function testGetAllOptions()
+    {
+        $assertArray = $this->testArray;
+        array_unshift($assertArray, ['value' => '', 'label' => __('No layout updates')]);
+        $this->assertEquals($assertArray, $this->model->getAllOptions());
+    }
+
+    protected function setUp()
+    {
+        $helper = new ObjectManager($this);
+        $this->model = $helper->getObject(
+            '\Magento\Catalog\Model\Category\Attribute\Source\Layout',
+            [
+                'pageSourceLayout' => $this->getMockedLayout()
+            ]
+        );
+    }
+
+    /**
+     * @return \Magento\Theme\Model\Layout\Source\Layout
+     */
+    private function getMockedLayout()
+    {
+        $mockBuilder = $this->getMockBuilder('\Magento\Theme\Model\Layout\Source\Layout');
+        $mockBuilder->disableOriginalConstructor();
+        $mock = $mockBuilder->getMock();
+
+        $mock->expects($this->any())
+            ->method('toOptionArray')
+            ->will($this->returnValue($this->testArray));
+
+        return $mock;
+    }
+}
