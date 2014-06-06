32914d9e77c594d2838b1d0491bafba263e52099
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-06-06 18:08:04 +0300

    MAGETWO-24245: [Dragons MX] Unit Test Coverage
     - MAGETWO-24926: Not used methods was removed

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_methods.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_methods.php
@@ -1699,4 +1699,6 @@ return array(
         'Magento\Framework\View\Asset\ModuleNotation\Resolver::convertModuleNotationToPath'
     ],
     ['getViewFile', 'Magento\Framework\View\FileSystem', 'Magento\Framework\View\Asset\File::getSourceFile()'],
+    ['prepareValueForDuplicate', 'Magento\Catalog\Model\Product\Option\Value'],
+    ['prepareOptionForDuplicate', '\Magento\Catalog\Model\Product\Option'],
 );
