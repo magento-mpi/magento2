c225930865a44ced9eb2662b28e30180bfc8a1b6
Max Gubar <mgubar@ebay.com>
2014-06-11 17:35:54 +0300

    MAGETWO-25071: Tests [Unit]

---
+++ dev/tests/unit/testsuite/Magento/CatalogInventory/Model/Indexer/Stock/Plugin/ImportTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_CatalogInventory
+ * @subpackage  unit_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\CatalogInventory\Model\Indexer\Stock\Plugin;
+
+class ImportTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\CatalogInventory\Model\Indexer\Stock\Plugin\Import
+     */
+    protected $_model;
+
+    /**
+     * @var \Magento\Indexer\Model\IndexerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_indexerMock;
+
+    public function setUp()
+    {
+        $this->_indexerMock = $this->getMock('Magento\Indexer\Model\IndexerInterface');
+        $this->_model = new \Magento\CatalogInventory\Model\Indexer\Stock\Plugin\Import($this->_indexerMock);
+    }
+
+    public function testAfterImportSource()
+    {
+        $subjectMock = $this->getMock('Magento\ImportExport\Model\Import', array(), array(), '', false);
+        $result = 'result';
+
+        $this->_indexerMock->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue(11));
+        $this->_indexerMock->expects($this->once())
+            ->method('invalidate');
+
+        $this->assertEquals(
+            $result,
+            $this->_model->afterImportSource($subjectMock, $result)
+        );
+    }
+}
