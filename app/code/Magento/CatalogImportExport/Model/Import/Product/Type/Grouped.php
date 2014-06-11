46a76a661286ae247429ea8fc513ad36e4399946
Bryant Luk <bluk@ebay.com>
2014-06-11 16:47:41 -0500

    MAGETWO-25137: Move ImportExport code into own modules
    - Fix CatalogImportExport Grouped namespace

--- app/code/Magento/CatalogImportExport/Model/Import/Product/Type/Grouped.php
+++ app/code/Magento/CatalogImportExport/Model/Import/Product/Type/Grouped.php
@@ -7,7 +7,7 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-namespace Magento\CatalogImportExport\Model\Import\Entity\Product\Type;
+namespace Magento\CatalogImportExport\Model\Import\Product\Type;
 
 class Grouped extends AbstractType
 {
