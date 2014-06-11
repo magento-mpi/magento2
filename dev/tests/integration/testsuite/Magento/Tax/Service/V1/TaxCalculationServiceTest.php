816805fd6a7fd56f3e9670d5819d9c3290726aff
Eric Nguyen <ernguyen@ebay.com>
2014-06-11 13:59:27 -0500

    MAGETWO-25148: Updated TaxCalculationService Integration Tests
     - Added creation of objects via object manager.

--- dev/tests/integration/testsuite/Magento/Tax/Service/V1/TaxCalculationServiceTest.php
+++ dev/tests/integration/testsuite/Magento/Tax/Service/V1/TaxCalculationServiceTest.php
@@ -19,10 +19,62 @@ class TaxCalculationServiceTest
      */
     private $objectManager;
 
+    /**
+     * Tax calculation model
+     *
+     * @var \Magento\Tax\Model\Calculation
+     */
+    private $calculator;
+
+    /**
+     * Tax configuration object
+     *
+     * @var \Magento\Tax\Model\Config
+     */
+    private $config;
+
+    /**
+     * Tax Helper
+     *
+     * @var \Magento\Tax\Helper\Data
+     */
+    private $helper;
+
+    /**
+     * Tax Details Builder
+     *
+     * @var \Magento\Tax\Service\V1\Data\TaxDetails\TaxDetailsBuilder
+     */
+    private $taxDetailsBuilder;
+
+    /**
+     * Tax Details Item Builder
+     *
+     * @var \Magento\Tax\Service\V1\Data\TaxDetails\ItemBuilder
+     */
+    private $taxDetailsItemBuilder;
+
     protected function setUp()
     {
         $this->objectManager = Bootstrap::getObjectManager();
+        $this->calculator = $this
+            ->objectManager
+            ->create('Magento\Tax\Model\Calculation');
+        $this->config = $this
+            ->objectManager
+            ->create('Magento\Tax\Model\Config');
+        $this->helper = $this
+            ->objectManager
+            ->create('Magento\Tax\Helper\Data');
+        $this->taxDetailsBuilder = $this
+            ->objectManager
+            ->create('Magento\Tax\Service\V1\Data\TaxDetails\TaxDetailsBuilder');
+        $this->taxDetailsItemBuilder = $this
+            ->objectManager
+            ->create('Magento\Tax\Service\V1\data\TaxDetails\ItemBuilder');
     }
+
+
 
 
 }
