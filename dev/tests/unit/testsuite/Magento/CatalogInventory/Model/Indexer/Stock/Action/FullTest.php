9688a3c51d71458786997fa72d490a87eb84420e
Max Gubar <mgubar@ebay.com>
2014-06-12 15:08:42 +0300

    MAGETWO-25071: Tests [Unit]

---
+++ dev/tests/unit/testsuite/Magento/CatalogInventory/Model/Indexer/Stock/Action/FullTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @category    Magento
+ * @package     Magento_CatalogInventory
+ * @subpackage  unit_tests
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\CatalogInventory\Model\Indexer\Stock\Action;
+
+class FullTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecuteWithAdapterErrorThrowsException()
+    {
+        $indexerFactoryMock = $this->getMock(
+            'Magento\CatalogInventory\Model\Resource\Indexer\StockFactory',
+            array(),
+            array(),
+            '',
+            false
+        );
+        $resourceMock = $this->getMock('Magento\Framework\App\Resource', array('getConnection'), array(), '', false);
+        $loggerMock = $this->getMock('Magento\Framework\Logger', array(), array(), '', false);
+        $productTypeMock = $this->getMock('Magento\Catalog\Model\Product\Type', array(), array(), '', false);
+        $adapterMock = $this->getMock('Magento\Framework\DB\Adapter\AdapterInterface');
+
+        $exceptionMessage = 'exception message';
+        $exception = new \Exception($exceptionMessage);
+
+        $adapterMock->expects($this->once())
+            ->method('delete')
+            ->will($this->throwException($exception));
+
+        $resourceMock->expects($this->any())
+            ->method('getConnection')
+            ->will($this->returnValue($adapterMock));
+
+        $model = new \Magento\CatalogInventory\Model\Indexer\Stock\Action\Full(
+            $resourceMock,
+            $loggerMock,
+            $indexerFactoryMock,
+            $productTypeMock
+        );
+
+        $this->setExpectedException('\Magento\CatalogInventory\Exception', $exceptionMessage);
+
+        $model->execute();
+    }
+}
