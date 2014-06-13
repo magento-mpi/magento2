9c1d89951068ef13cbdb265d942713a228b12d74
Roman Ganin <rganin@ebay.com>
2014-06-13 18:13:05 +0300

    MAGETWO-25241: Refactor indexer processors and fix code under PRS2

--- app/code/Magento/CatalogInventory/Model/Indexer/Stock/Processor.php
+++ app/code/Magento/CatalogInventory/Model/Indexer/Stock/Processor.php
@@ -16,4 +16,26 @@ class Processor extends \Magento\Indexer\Model\Indexer\AbstractProcessor
      * Indexer ID
      */
     const INDEXER_ID = 'cataloginventory_stock';
+
+    /**
+     * Run Row reindex
+     *
+     * @param int $id
+     * @return void
+     */
+    public function reindexRow($id)
+    {
+        $this->getIndexer()->reindexRow($id);
+    }
+
+    /**
+     * Run List reindex
+     *
+     * @param int[] $ids
+     * @return void
+     */
+    public function reindexList($ids)
+    {
+        $this->getIndexer()->reindexList($ids);
+    }
 }
