4723e543377c5a78f6c2dc8d887867d0ac10826f
Igor Miniailo <iminiailo@ebay.com>
2014-06-13 15:39:44 +0300

    MAGETWO-23312: Create ProductAttributesService
    - MAGETWO-24913: API tests for product_attribute.list

--- dev/tests/api-functional/testsuite/Magento/Catalog/Service/V1/ProductAttributeReadServiceTest.php
+++ dev/tests/api-functional/testsuite/Magento/Catalog/Service/V1/ProductAttributeReadServiceTest.php
@@ -10,6 +10,9 @@ namespace Magento\Catalog\Service\V1;
 use Magento\TestFramework\TestCase\WebapiAbstract;
 use Magento\Webapi\Model\Rest\Config as RestConfig;
 use Magento\Webapi\Exception as HTTPExceptionCodes;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Catalog\Service\V1\Data\Eav\Attribute;
+use Magento\Framework\Service\V1\Data\SearchCriteria;
 
 /**
  * Class ProductAttributeReadServiceTest
@@ -75,6 +78,206 @@ class ProductAttributeReadServiceTest extends WebapiAbstract
         return array(
             array('price'),
             array(95),
+        );
+    }
+
+    /**
+     * @dataProvider searchDataProvider
+     */
+    public function testSearch($filterGroups, $expectedAttributes, $sortData)
+    {
+        list($sortField, $sortValue) = $sortData;
+        if ($sortValue === SearchCriteria::SORT_DESC && TESTS_WEB_API_ADAPTER == self::ADAPTER_SOAP) {
+            $this->markTestSkipped('Sorting doesn\'t work in SOAP');
+        }
+        /** @var $searchCriteriaBuilder  \Magento\Framework\Service\V1\Data\SearchCriteriaBuilder */
+        $searchCriteriaBuilder = Bootstrap::getObjectManager()->create(
+            'Magento\Framework\Service\V1\Data\SearchCriteriaBuilder'
+        );
+        /** @var $filterBuilder  \Magento\Framework\Service\V1\Data\FilterBuilder */
+        $filterBuilder = Bootstrap::getObjectManager()->create(
+            'Magento\Framework\Service\V1\Data\FilterBuilder'
+        );
+        foreach ($filterGroups as $filterGroup) {
+            $group = array();
+            foreach ($filterGroup as $filter) {
+                list($filterKey, $filterValue) = $filter;
+                $group[] = $filterBuilder
+                    ->setField($filterKey)
+                    ->setValue($filterValue)
+                    ->create();
+            }
+            $searchCriteriaBuilder->addFilter($group);
+        }
+
+        $searchCriteriaBuilder->setSortOrders([$sortField => $sortValue]);
+        $searchData = $searchCriteriaBuilder->create()->__toArray();
+        $requestData = ['searchCriteria' => $searchData];
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => self::RESOURCE_PATH . '?' . http_build_query($requestData),
+                'httpMethod' => RestConfig::HTTP_METHOD_GET
+            ],
+            'soap' => [
+                'service' => self::SERVICE_NAME,
+                'serviceVersion' => self::SERVICE_VERSION,
+                'operation' => self::SERVICE_NAME . 'search'
+            ]
+        ];
+
+        $searchResults = $this->_webApiCall($serviceInfo, $requestData);
+        $this->assertArrayHasKey('items', $searchResults);
+        $this->assertEquals(count($expectedAttributes), count($searchResults['items']));
+        $this->assertEquals(count($expectedAttributes), $searchResults['total_count']);
+        // Prepare actual data for check
+        foreach ($searchResults['items'] as &$attribute) {
+            // Add empty default values because SOAP service does not return null values
+            if (!isset($attribute['default_value'])) {
+                $attribute['default_value'] = null;
+            }
+            // Remove attribute IDs (in order to make test more clear i.e. without hardcoded IDs)
+            unset($attribute['id']);
+        }
+        foreach ($expectedAttributes as $expectedAttribute) {
+            $this->assertContains($expectedAttribute, $searchResults['items']);
+        }
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function searchDataProvider()
+    {
+        return array(
+            array(
+                [ //Groups
+                    [ //Group(AND)
+                        [Attribute::FRONTEND_INPUT, 'textarea']
+                    ],
+                ],
+                [
+                    [
+                        'code' => 'description',
+                        'frontend_label' => 'Description',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'textarea'
+                    ],
+                    [
+                        'code' => 'meta_keywords',
+                        'frontend_label' => 'Meta Keywords',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'textarea'
+                    ],
+                    [
+                        'code' => 'meta_description',
+                        'frontend_label' => 'Meta Description',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'textarea'
+                    ],
+                    [
+                        'code' => 'custom_layout_update',
+                        'frontend_label' => 'Custom Layout Update',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'textarea'
+                    ],
+                    [
+                        'code' => 'description',
+                        'frontend_label' => 'Description',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'textarea'
+                    ],
+                    [
+                        'code' => 'short_description',
+                        'frontend_label' => 'Short Description',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'textarea'
+                    ],
+                    [
+                        'code' => 'meta_keyword',
+                        'frontend_label' => 'Meta Keywords',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'textarea'
+                    ],
+                    [
+                        'code' => 'meta_description',
+                        'frontend_label' => 'Meta Description',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'textarea'
+                    ],
+                    [
+                        'code' => 'custom_layout_update',
+                        'frontend_label' => 'Custom Layout Update',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'textarea'
+                    ],
+                ],
+                [Attribute::IS_REQUIRED, SearchCriteria::SORT_ASC]
+            ),
+            array(
+                [ //Groups
+                    [ //Group(AND)
+                        [Attribute::FRONTEND_INPUT, 'text']
+                    ],
+                    [ //Group(AND)
+                        ['is_configurable', 1]
+                    ],
+                ],
+                [
+                    [
+                        'code' => 'related_tgtr_position_limit',
+                        'frontend_label' => 'Related Target Rule Rule Based Positions',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'text'
+                    ],
+                    [
+                        'code' => 'related_tgtr_position_behavior',
+                        'frontend_label' => 'Related Target Rule Position Behavior',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'text'
+                    ],
+                    [
+                        'code' => 'upsell_tgtr_position_limit',
+                        'frontend_label' => 'Upsell Target Rule Rule Based Positions',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'text'
+                    ],
+                    [
+                        'code' => 'upsell_tgtr_position_behavior',
+                        'frontend_label' => 'Upsell Target Rule Position Behavior',
+                        'default_value' => null,
+                        'is_required' => false,
+                        'is_user_defined' => false,
+                        'frontend_input' => 'text'
+                    ],
+                ],
+                [Attribute::IS_REQUIRED, SearchCriteria::SORT_ASC]
+            )
         );
     }
 }
