4478448fefd3df84786689a03eb2ab111a4f6391
Roman Ganin <rganin@ebay.com>
2014-06-16 17:15:59 +0300

    MAGETWO-25241: Refactor indexer processors and fix code under PRS2
     - fixed integration tests

--- dev/tests/unit/testsuite/Magento/ScheduledImportExport/Model/ImportTest.php
+++ dev/tests/unit/testsuite/Magento/ScheduledImportExport/Model/ImportTest.php
@@ -46,8 +46,8 @@ class ImportTest extends \PHPUnit_Framework_TestCase
             $this->getMock('\Magento\Framework\HTTP\Adapter\FileTransferFactory', array(), array(), '', false),
             $this->getMock('Magento\Core\Model\File\UploaderFactory', array(), array(), '', false),
             $this->getMock('Magento\ImportExport\Model\Source\Import\Behavior\Factory', array(), array(), '', false),
-            $this->getMock('Magento\CatalogInventory\Model\Indexer\Stock\Processor', array(), array(), '', false),
-            $indexer
+            $indexer,
+            $this->getMock('Magento\CatalogInventory\Model\Indexer\Stock\Processor', array(), array(), '', false)
         );
     }
 
