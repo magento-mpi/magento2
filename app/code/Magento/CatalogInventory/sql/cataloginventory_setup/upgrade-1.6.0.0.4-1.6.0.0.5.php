f84cc9afdbe218d69d1bb6949e3bc281bf3a7011
Roman Ganin <rganin@ebay.com>
2014-06-16 19:25:38 +0300

    MAGETWO-25241: Refactor indexer processors and fix code under PRS2
     - fixed install script in CatalogInventory module

--- app/code/Magento/CatalogInventory/sql/cataloginventory_setup/upgrade-1.6.0.0.4-1.6.0.0.5.php
+++ app/code/Magento/CatalogInventory/sql/cataloginventory_setup/upgrade-1.6.0.0.4-1.6.0.0.5.php
@@ -11,17 +11,17 @@
 $this->startSetup();
 
 $this->getConnection()->dropForeignKey(
-    $this->getConnection()->getTableName('cataloginventory_stock_status'),
+    $this->getTable('cataloginventory_stock_status'),
     $this->getFkName('cataloginventory_stock_status', 'stock_id', 'cataloginventory_stock', 'stock_id')
 );
 
 $this->getConnection()->dropForeignKey(
-    $this->getConnection()->getTableName('cataloginventory_stock_status'),
+    $this->getTable('cataloginventory_stock_status'),
     $this->getFkName('cataloginventory_stock_status', 'product_id', 'catalog_product_entity', 'entity_id')
 );
 
 $this->getConnection()->dropForeignKey(
-    $this->getConnection()->getTableName('cataloginventory_stock_status'),
+    $this->getTable('cataloginventory_stock_status'),
     $this->getFkName('cataloginventory_stock_status', 'website_id', 'store_website', 'website_id')
 );
 
