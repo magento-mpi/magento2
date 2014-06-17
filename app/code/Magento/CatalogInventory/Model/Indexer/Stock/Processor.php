7fddc52be7f8eb50886e528d99db378730f2036d
Roman Ganin <rganin@ebay.com>
2014-06-17 18:13:13 +0300

    MAGETWO-25241: Refactor indexer processors and fix code under PRS2
     - reverted logic to basic new indexer logic - reindex in scheduled mode only by schedule

--- app/code/Magento/CatalogInventory/Model/Indexer/Stock/Processor.php
+++ app/code/Magento/CatalogInventory/Model/Indexer/Stock/Processor.php
@@ -16,26 +16,4 @@ class Processor extends \Magento\Indexer\Model\Indexer\AbstractProcessor
      * Indexer ID
      */
     const INDEXER_ID = 'cataloginventory_stock';
-
-    /**
-     * Run Row reindex
-     *
-     * @param int $id
-     * @return void
-     */
-    public function reindexRow($id)
-    {
-        $this->getIndexer()->reindexRow($id);
-    }
-
-    /**
-     * Run List reindex
-     *
-     * @param int[] $ids
-     * @return void
-     */
-    public function reindexList($ids)
-    {
-        $this->getIndexer()->reindexList($ids);
-    }
 }
