f02d0120a2264ab8d853ee10e06616e44a7367b5
Tatyana Gonchar <tgonchar@ebay.com>
2014-06-18 13:35:16 +0300

    MAGETWO-25335: Add warning about non-https identity link URL

--- app/code/Magento/Integration/Block/Adminhtml/Widget/Grid/Column/Renderer/Name.php
+++ app/code/Magento/Integration/Block/Adminhtml/Widget/Grid/Column/Renderer/Name.php
@@ -22,8 +22,11 @@ class Name extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\Text
      */
     public function render(\Magento\Framework\Object $row)
     {
+        /** @var \Magento\Integration\Model\Integration $row */
         $text = parent::render($row);
-        if (strpos($row->getEndpoint(), 'http:') === 0) {
+        $isEndpointSecure = strpos($row->getEndpoint(), 'http:') !== 0;
+        $isIdentityLinkSecure = strpos($row->getIdentityLinkUrl(), 'http:') !== 0;
+        if (!$isEndpointSecure || !$isIdentityLinkSecure) {
             $text .= '<span class="icon-error"><span>Integration not secure</span></span>';
         }
         return $text;
