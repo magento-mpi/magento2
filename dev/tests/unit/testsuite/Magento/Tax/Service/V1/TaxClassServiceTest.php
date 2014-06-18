3184eba17b80bfb1b57d04259232bfebd0ffe402
Anup Dugar <anup@x.com>
2014-06-18 17:28:06 -0500

    MAGETWO-25153: Tax Class create service
    - Unit tests

--- dev/tests/unit/testsuite/Magento/Tax/Service/V1/TaxClassServiceTest.php
+++ dev/tests/unit/testsuite/Magento/Tax/Service/V1/TaxClassServiceTest.php
@@ -9,7 +9,8 @@
 namespace Magento\Tax\Service\V1;
 
 use Magento\Framework\Exception\InputException;
-use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Tax\Service\V1\Data\TaxClass;
+use Magento\Tax\Service\V1\Data\TaxClassBuilder;
 
 /**
  * Test for \Magento\Tax\Service\V1\TaxClassService
@@ -27,6 +28,21 @@ class TaxClassServiceTest extends \PHPUnit_Framework_TestCase
     private $taxClassModelMock;
 
     /**
+     * @var \PHPUnit_Framework_MockObject_MockObject | \Magento\Tax\Model\Class
+     */
+    private $converterMock;
+
+    /**
+     * @var TaxClassBuilder
+     */
+    private $taxClassBuilder;
+
+    /**
+     * @var TaxClassService
+     */
+    private $taxClassService;
+
+    /**
      * @var \Magento\TestFramework\Helper\ObjectManager
      */
     private $objectManager;
@@ -42,19 +58,105 @@ class TaxClassServiceTest extends \PHPUnit_Framework_TestCase
 
         $this->taxClassModelMock = $this->getMockBuilder('Magento\Tax\Model\ClassModel')
             ->disableOriginalConstructor()
-            ->setMethods(['load', 'getId', 'delete', '__wakeup'])
+            ->setMethods(['load', 'getId', 'delete', 'save', '__wakeup'])
             ->getMock();
 
         $this->taxClassModelFactoryMock->expects($this->any())
             ->method('create')
             ->will($this->returnValue($this->taxClassModelMock));
+
+        $this->taxClassBuilder = $this->objectManager->getObject('Magento\Tax\Service\V1\Data\TaxClassBuilder');
+
+        $this->taxClassService = $this->createService();
+    }
+
+    public function testCreateTaxClass()
+    {
+        $taxClassId = 1;;
+
+        $taxClassSample = $this->taxClassBuilder
+            ->setType(TaxClass::TYPE_PRODUCT)
+            ->setName('Wholesale product')
+            ->create();
+
+        $this->taxClassModelMock
+            ->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue($taxClassId));
+
+        $this->taxClassModelMock
+            ->expects($this->once())
+            ->method('save');
+
+        $this->converterMock
+            ->expects($this->once())
+            ->method('createTaxClassModel')
+            ->with($taxClassSample)
+            ->will($this->returnValue($this->taxClassModelMock));
+
+        $this->assertEquals($taxClassId, $this->taxClassService->createTaxClass($taxClassSample));
+    }
+
+    /**
+     * @expectedException \Magento\Framework\Exception\InputException
+     * @expectedExceptionMessage A class with the same name already exists for ClassType PRODUCT.
+     */
+    public function testCreateTaxClassException()
+    {
+        $taxClassSample = $this->taxClassBuilder
+            ->setType(TaxClass::TYPE_PRODUCT)
+            ->setName('Wholesale product')
+            ->create();
+
+        $this->taxClassModelMock
+            ->expects($this->once())
+            ->method('save')
+            ->will($this->throwException(new \Magento\Framework\Model\Exception()));
+
+        $this->taxClassModelMock
+            ->expects($this->never())
+            ->method('getId');
+
+        $this->converterMock
+            ->expects($this->once())
+            ->method('createTaxClassModel')
+            ->with($taxClassSample)
+            ->will($this->returnValue($this->taxClassModelMock));
+
+        $this->taxClassService->createTaxClass($taxClassSample);
+    }
+
+    public function testCreateTaxClassInvalidData()
+    {
+        $taxClassSample = $this->taxClassBuilder
+            ->create();
+
+        $this->taxClassModelMock
+            ->expects($this->never())
+            ->method('save');
+
+        $this->taxClassModelMock
+            ->expects($this->never())
+            ->method('getId');
+
+        //Make sure that the conversion is avoided in case of data validation
+        $this->converterMock
+            ->expects($this->never())
+            ->method('createTaxClassModel');
+
+        try {
+            $this->taxClassService->createTaxClass($taxClassSample);
+        } catch(InputException $e) {
+            $errors = $e->getErrors();
+            $this->assertEquals('name is a required field.', $errors[0]->getMessage());
+            $this->assertEquals('type is a required field.', $errors[1]->getMessage());
+            $this->assertEquals('Invalid value of "" provided for the type field.', $errors[2]->getMessage());
+        }
     }
 
     public function testDeleteModelDeleteThrowsException()
     {
         $taxClassId = 1;
-
-        $service = $this->createService();
 
         $this->taxClassModelMock->expects($this->once())
             ->method('load')
@@ -69,14 +171,12 @@ class TaxClassServiceTest extends \PHPUnit_Framework_TestCase
             ->method('delete')
             ->will($this->throwException(new \Exception()));
 
-        $this->assertFalse($service->deleteTaxClass($taxClassId));
+        $this->assertFalse($this->taxClassService->deleteTaxClass($taxClassId));
     }
 
     public function testDeleteModelDeleteSuccess()
     {
         $taxClassId = 1;
-
-        $service = $this->createService();
 
         $this->taxClassModelMock->expects($this->once())
             ->method('load')
@@ -90,7 +190,7 @@ class TaxClassServiceTest extends \PHPUnit_Framework_TestCase
         $this->taxClassModelMock->expects($this->once())
             ->method('delete');
 
-        $this->assertTrue($service->deleteTaxClass($taxClassId));
+        $this->assertTrue($this->taxClassService->deleteTaxClass($taxClassId));
     }
 
     /**
@@ -99,8 +199,6 @@ class TaxClassServiceTest extends \PHPUnit_Framework_TestCase
     public function testDeleteNonExistentModel()
     {
         $taxClassId = 1;
-
-        $service = $this->createService();
 
         $this->taxClassModelMock->expects($this->once())
             ->method('load')
@@ -111,7 +209,7 @@ class TaxClassServiceTest extends \PHPUnit_Framework_TestCase
             ->method('getId')
             ->will($this->returnValue(null));
 
-        $service->deleteTaxClass($taxClassId);
+        $this->taxClassService->deleteTaxClass($taxClassId);
     }
 
     /**
@@ -126,14 +224,17 @@ class TaxClassServiceTest extends \PHPUnit_Framework_TestCase
         $searchResultsBuilder = $this->objectManager
             ->getObject('\Magento\Tax\Service\V1\Data\SearchResultsBuilder');
 
-        $converter = $this->objectManager->getObject('Magento\Tax\Model\Converter');
+        $this->converterMock = $this->getMockBuilder('Magento\Tax\Model\Converter')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $taxClassService = $this->objectManager->getObject('Magento\Tax\Service\V1\TaxClassService',
+        $taxClassService = $this->objectManager->getObject(
+            'Magento\Tax\Service\V1\TaxClassService',
             [
                 'taxClassCollectionFactory' => $taxClassCollectionFactory,
                 'taxClassModelFactory' => $this->taxClassModelFactoryMock,
                 'searchResultsBuilder' => $searchResultsBuilder,
-                'converter' => $converter
+                'converter' => $this->converterMock
             ]
         );
 
