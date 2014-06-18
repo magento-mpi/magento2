c1a222a83ffc896e333e126a4349cdb1735dfbb8
Victor Rad <vrad@ebay.com>
2014-06-18 12:49:22 +0300

    MAGETWO-25376: Pull Request (Sprint2)
    - fix for auto-test of MAGETWO-21056 bug

--- dev/tests/integration/testsuite/Magento/Reminder/Model/Resource/RuleTest.php
+++ dev/tests/integration/testsuite/Magento/Reminder/Model/Resource/RuleTest.php
@@ -8,38 +8,34 @@
 namespace Magento\Reminder\Model\Resource;
 
 use Magento\Reminder\Model\Rule;
+use Magento\TestFramework\Helper\Bootstrap;
 
 class RuleTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @magentoDataFixture Magento/Reminder/_files/customersForNotification.php
-     * @dataProvider dataProviderTestGetCustomersForNotification
      */
-    public function testGetCustomersForNotification($customersForNotification)
+    public function testGetCustomersForNotification()
     {
-        $collection = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-            'Magento\Reminder\Model\Resource\Rule\Collection'
-        );
+        $beforeYesterday = date('Y-m-d 03:00:00', strtotime('-2 day', time()));
+        $customersForNotification = [['customer_id' => '1', 'coupon_id' => null, 'rule_id' => null, 'schedule' => '2',
+                'log_sent_at_max' => $beforeYesterday, 'log_sent_at_min' => $beforeYesterday]];
+        $resource = Bootstrap::getObjectManager()->get('Magento\Framework\App\Resource')->getConnection('core_write');
+        $adapter = $resource->getConnection('core_write');
+        $adapter->query("UPDATE {$resource->getTableName('sales_flat_quote')} SET updated_at = '{$beforeYesterday}'");
+
+        $collection = Bootstrap::getObjectManager()->create('Magento\Reminder\Model\Resource\Rule\Collection');
         $rules = $collection->addIsActiveFilter(1);
-        $ruleResource = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-            'Magento\Reminder\Model\Resource\Rule'
-        );
+        $ruleResource = Bootstrap::getObjectManager()->create('Magento\Reminder\Model\Resource\Rule');
         foreach ($rules as $rule) {
             $customersForNotification[0]['rule_id'] = $rule->getId();
+            $adapter->query("INSERT INTO {$resource->getTableName('magento_reminder_rule_log')} " .
+                "(`rule_id`, `customer_id`, `sent_at`) VALUES ({$rule->getId()}, 1, '{$beforeYesterday}');");
             $websiteIds = $rule->getWebsiteIds();
             foreach ($websiteIds as $websiteId) {
                 $ruleResource->saveMatchedCustomers($rule, null, $websiteId, null);
             }
         }
         $this->assertEquals($customersForNotification, $ruleResource->getCustomersForNotification());
-    }
-
-    public function dataProviderTestGetCustomersForNotification()
-    {
-        $beforeYesterday = date('Y-m-d 03:00:00', strtotime('-2 day', time()));
-        return ['first' =>
-            [[['customer_id' => '1', 'coupon_id' => null, 'rule_id' => null, 'schedule' => '2',
-                'log_sent_at_max' => $beforeYesterday, 'log_sent_at_min' => $beforeYesterday]]]
-        ];
     }
 }
