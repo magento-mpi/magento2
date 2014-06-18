86129b6615799c6e52a99a3f9e3f0fcf9df1c026
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-06-18 18:10:22 +0300

    MAGETWO-23305: Create Category service
    MAGETWO-25319: Create API test for method catalog_category.delete
      - Fixed annotation for WriteServiceInterface

--- app/code/Magento/Catalog/Service/V1/Category/WriteServiceInterface.php
+++ app/code/Magento/Catalog/Service/V1/Category/WriteServiceInterface.php
@@ -11,7 +11,6 @@ interface WriteServiceInterface
 {
     /**
      * Delete category
-
      *
      * @param int $categoryId category which will deleted
      * @return bool Will returned True if deleted
