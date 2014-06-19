813ce3eabc3850708ac8c409db0aecc44b2aec4f
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-06-19 19:03:24 +0300

    MAGETWO-23305: Create Category service
    MAGETWO-25316: API tests for catalog_category.info
      - Formatter code

---
+++ dev/tests/api-functional/testsuite/Magento/Catalog/Service/V1/Category/ReadServiceTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Catalog\Service\V1\Category;
+
+use Magento\Catalog\Model\Category;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\ObjectManager;
+use Magento\TestFramework\TestCase\WebapiAbstract;
+use Magento\Webapi\Model\Rest\Config;
+
+class ReadServiceTest extends WebapiAbstract
+{
+    const SERVICE_WRITE_NAME = 'catalogCategoryReadServiceV1';
+    const SERVICE_VERSION = 'V1';
+    const RESOURCE_PATH = '/V1/categories';
+
+    private static $categoryData = ['name' => 'Test Category'];
+    private static $modelId;
+
+    public static function infoDataFixture()
+    {
+        /** @var \Magento\Catalog\Model\CategoryFactory $categoryFactory */
+        $categoryFactory = Bootstrap::getObjectManager()->create('Magento\Catalog\Model\CategoryFactory');
+        /** @var Category $category */
+        $category = $categoryFactory->create();
+        $category->setData(self::$categoryData);
+        $category->save();
+        self::$modelId = $category->getId();
+    }
+
+    /**
+     * @magentoApiDataFixture infoDataFixture
+     */
+    public function testInfo()
+    {
+        $expected = [
+            'custom_attributes' => [
+                0 => [
+                    'attribute_code' => 'available_sort_by',
+                    'value' => '',
+                ],
+                1 => [
+                    'attribute_code' => 'include_in_menu',
+                    'value' => '1',
+                ],
+                2 => [
+                    'attribute_code' => 'name',
+                    'value' => 'Test Category',
+                ],
+                3 => [
+                    'attribute_code' => 'url_key',
+                    'value' => 'test-category',
+                ]
+            ]
+        ];
+
+        $this->assertEquals($expected, $this->getInfoCategory(self::$modelId));
+    }
+
+    public function testInfoNoSuchEntityException()
+    {
+        try {
+            $this->getInfoCategory(-1);
+        } catch (\Exception $e) {
+            $this->assertContains('No such entity with %fieldName = %fieldValue', $e->getMessage());
+        }
+    }
+
+    /**
+     * @param int $id
+     * @return string
+     */
+    protected function getInfoCategory($id)
+    {
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => self::RESOURCE_PATH . '/' . $id,
+                'httpMethod' => Config::HTTP_METHOD_DELETE
+            ],
+            'soap' => [
+                'service' => self::SERVICE_WRITE_NAME,
+                'serviceVersion' => self::SERVICE_VERSION,
+                'operation' => self::SERVICE_WRITE_NAME . 'info'
+            ]
+        ];
+        return $this->_webApiCall($serviceInfo, ['categoryId' => $id]);
+    }
+}
