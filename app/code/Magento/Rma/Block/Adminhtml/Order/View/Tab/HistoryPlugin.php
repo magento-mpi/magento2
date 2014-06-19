aa47436a3e7c476552cca9611707beedf15c4ac5
Sergey Ivashchenko <sivashchenko@ebay.com>
2014-06-19 19:02:45 +0300

    MAGETWO-24722: Order comments history updates for RMA

--- app/code/Magento/Rma/Block/Adminhtml/Order/View/Tab/HistoryPlugin.php
+++ app/code/Magento/Rma/Block/Adminhtml/Order/View/Tab/HistoryPlugin.php
@@ -55,13 +55,14 @@ class HistoryPlugin
         $historyCollection = $this->historyCollectionFactory->create();
         /** @var \Magento\Rma\Model\Rma $rma */
         foreach ($collection as $rma) {
+            $rmaId = $rma->getId();
             /** @var $comments \Magento\Rma\Model\Rma\Status\History[] */
-            $comments = $historyCollection->getItemsByColumnValue('rma_entity_id', $rma->getId());
+            $comments = $historyCollection->getItemsByColumnValue('rma_entity_id', $rmaId);
             foreach ($comments as $comment) {
-                if ($comment['comment'] == $creationSystemComment) {
+                if ($comment->getComment() == $creationSystemComment) {
                     $history[] = [
-                        'title' => sprintf('Return #%s created', $rma->getId()),
-                        'notified' => $comment['is_customer_notified'],
+                        'title' => sprintf('Return #%s created', $rmaId),
+                        'notified' => $comment->getIsCustomerNotified(),
                         'comment' => '',
                         'created_at' => $comment->getCreatedAtDate()
                     ];
