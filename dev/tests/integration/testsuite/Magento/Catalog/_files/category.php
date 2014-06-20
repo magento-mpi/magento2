71fce34c65f0c981fa963fa1904b12669f00158b
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-06-20 11:08:47 +0300

    MAGETWO-23305: Create Category service
    MAGETWO-25320: Create API test for method catalog_category.delete
      - fixed fixtures

--- dev/tests/integration/testsuite/Magento/Catalog/_files/category.php
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/category.php
@@ -6,12 +6,6 @@
  * @license     {license_link}
  */
 
-/** @var $category \Magento\Catalog\Model\Category */
-$category = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Category');
-$category->load(333);
-if ($category->getId()) {
-    $category->delete();
-}
 $category = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Category');
 $category->setId(
     333
--- dev/tests/integration/testsuite/Magento/Catalog/_files/category_rollback.php
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/category_rollback.php
@@ -6,10 +6,14 @@
  * @license     {license_link}
  */
 
+/** @var \Magento\Framework\Registry $registry */
+$registry = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Framework\Registry');
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
+
 /** @var $category \Magento\Catalog\Model\Category */
 $category = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Category');
 $category->load(333);
 if ($category->getId()) {
     $category->delete();
 }
-
