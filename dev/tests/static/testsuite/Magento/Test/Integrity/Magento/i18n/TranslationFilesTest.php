84212bfb4b9140082d6cacb2d2abf83af463c857
Maxim Shikula <mshikula@ebay.com>
2014-06-23 15:29:54 +0300

    MAGETWO-25405: Create Integrity test to check English dictionary

--- dev/tests/static/testsuite/Magento/Test/Integrity/Magento/i18n/TranslationFilesTest.php
+++ dev/tests/static/testsuite/Magento/Test/Integrity/Magento/i18n/TranslationFilesTest.php
@@ -19,6 +19,13 @@ class TranslationFilesTest extends \PHPUnit_Framework_TestCase
      */
     protected $baseLocale = 'en_US';
 
+    /**
+     * Context
+     *
+     * @var \Magento\Tools\I18n\Code\Context
+     */
+    protected $context;
+
     protected function setUp()
     {
         $this->csvParser = new \Magento\Framework\File\Csv();
@@ -121,5 +128,135 @@ class TranslationFilesTest extends \PHPUnit_Framework_TestCase
             $failures['extra'] = $extra;
         }
         return $failures;
+    }
+
+    /**
+     * Test default locale
+     */
+    public function testDefaultLocale()
+    {
+        $parser = $this->prepareParser();
+        $parser->parse($this->getI18nPattern());
+
+        $convertedPhrases = $this->convertPhrasesByFile($parser->getPhrases());
+
+        $failures = [];
+
+        foreach ($convertedPhrases as $file => $phrases) {
+            $fail = $this->comparePhrase($phrases, $this->csvParser->getDataPairs($file));
+            if (!empty($fail)) {
+                $failures[] = $fail;
+            }
+
+        }
+
+        $this->assertEmpty(
+            $failures,
+            $this->printMessage($failures)
+        );
+    }
+
+    /**
+     * @param \Magento\Tools\I18n\Code\Dictionary\Phrase[] $phrases
+     * @return array
+     * @throws \RuntimeException
+     */
+    protected function convertPhrasesByFile($phrases)
+    {
+        $files = array();
+        foreach ($phrases as $key => $phrase) {
+            if (!$phrase->getContextType() || !$phrase->getContextValue()) {
+                throw new \RuntimeException(sprintf('Missed context in row #%d.', $key + 1));
+            }
+            foreach ($phrase->getContextValue() as $context) {
+                $files[$this->buildFilePath($phrase, $context)][$phrase->getPhrase()] = $phrase->getTranslation();
+            }
+        }
+
+        return $files;
+    }
+
+    /**
+     * @param \Magento\Tools\I18n\Code\Dictionary\Phrase $phrase
+     * @param array $context
+     * @return string
+     */
+    protected function buildFilePath($phrase, $context)
+    {
+        $path = $this->getContext()->buildPathToLocaleDirectoryByContext($phrase->getContextType(), $context);
+        return \Magento\TestFramework\Utility\Files::init()->getPathToSource() . '/'
+        . $path . \Magento\Tools\I18n\Code\Locale::DEFAULT_SYSTEM_LOCALE
+        . '.' . \Magento\Tools\I18n\Code\Pack\Writer\File\Csv::FILE_EXTENSION;
+    }
+
+    /**
+     * @return \Magento\Tools\I18n\Code\Context
+     */
+    protected function getContext()
+    {
+        if ($this->context === null) {
+            $this->context = new \Magento\Tools\I18n\Code\Context();
+        }
+        return $this->context;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getI18nPattern()
+    {
+        $magentoPath = \Magento\TestFramework\Utility\Files::init()->getPathToSource();
+        $filesPattern = [
+            [
+                'type' => 'php',
+                'paths' => array($magentoPath . '/app/code/', $magentoPath . '/app/design/'),
+                'fileMask' => '/\.(php|phtml)$/'
+            ],
+            [
+                'type' => 'js',
+                'paths' => [
+                    $magentoPath . '/app/code/',
+                    $magentoPath . '/app/design/',
+                    $magentoPath . '/lib/web/mage/',
+                    $magentoPath . '/lib/web/varien/'
+                ],
+                'fileMask' => '/\.(js|phtml)$/'
+            ],
+            [
+                'type' => 'xml',
+                'paths' => array($magentoPath . '/app/code/', $magentoPath . '/app/design/'),
+                'fileMask' => '/\.xml$/'
+            ]
+        ];
+
+        return $filesPattern;
+    }
+
+    /**
+     * @return \Magento\Tools\I18n\Code\Parser\Contextual
+     */
+    protected function prepareParser()
+    {
+        $filesCollector = new \Magento\Tools\I18n\Code\FilesCollector();
+
+        $phraseCollector = new \Magento\Tools\I18n\Code\Parser\Adapter\Php\Tokenizer\PhraseCollector(
+            new \Magento\Tools\I18n\Code\Parser\Adapter\Php\Tokenizer()
+        );
+        $adapters = array(
+            'php' => new \Magento\Tools\I18n\Code\Parser\Adapter\Php($phraseCollector),
+            'js' =>  new \Magento\Tools\I18n\Code\Parser\Adapter\Js(),
+            'xml' => new \Magento\Tools\I18n\Code\Parser\Adapter\Xml()
+        );
+
+        $parserContextual = new \Magento\Tools\I18n\Code\Parser\Contextual(
+            $filesCollector,
+            new \Magento\Tools\I18n\Code\Factory(),
+            new \Magento\Tools\I18n\Code\Context()
+        );
+        foreach ($adapters as $type => $adapter) {
+            $parserContextual->addAdapter($type, $adapter);
+        }
+
+        return $parserContextual;
     }
 }
