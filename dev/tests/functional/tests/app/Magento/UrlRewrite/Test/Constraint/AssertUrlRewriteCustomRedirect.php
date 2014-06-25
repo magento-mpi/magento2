170c030e09ee94327d36f0080e486c30dbc860a0
Oleksandr Rykh <orykh@ebay.com>
2014-06-25 17:29:08 +0300

    MTA-334: Test Creation for CreateCustomUrlRewriteEntity

---
+++ dev/tests/functional/tests/app/Magento/UrlRewrite/Test/Constraint/AssertUrlRewriteCustomRedirect.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\UrlRewrite\Test\Constraint;
+
+use Mtf\Client\Browser;
+use Magento\Cms\Test\Page\CmsIndex;
+use Mtf\Constraint\AbstractConstraint;
+use Magento\UrlRewrite\Test\Fixture\UrlRewrite;
+
+/**
+ * Class AssertUrlRewriteCustomRedirect
+ * Assert check URL rewrite custom redirect
+ */
+class AssertUrlRewriteCustomRedirect extends AbstractConstraint
+{
+    /**
+     * Constraint severeness
+     *
+     * @var string
+     */
+    protected $severeness = 'low';
+
+    /**
+     * Assert check URL rewrite custom redirect
+     *
+     * @param UrlRewrite $urlRewrite
+     * @param Browser $browser
+     * @param CmsIndex $cmsIndex
+     * @return void
+     */
+    public function processAssert(UrlRewrite $urlRewrite, Browser $browser, CmsIndex $cmsIndex)
+    {
+        $browser->open($_ENV['app_frontend_url'] . $urlRewrite->getRequestPath());
+        $entity = $urlRewrite->getDataFieldConfig('id_path')['source']->getEntity();
+        $title = $entity->hasData('name') ? $entity->getName() : $entity->getTitle();
+        $pageTitle = $cmsIndex->getTitleBlock()->getTitle();
+        \PHPUnit_Framework_Assert::assertEquals(
+            $pageTitle,
+            $title,
+            'URL rewrite product redirect false.'
+            . "\nExpected: " . $title
+            . "\nActual: " . $pageTitle
+        );
+    }
+
+    /**
+     * URL rewrite custom redirect success
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return 'Customer available by new URL on the front.';
+    }
+}
---
+++ dev/tests/functional/tests/app/Magento/UrlRewrite/Test/TestCase/CreateCustomUrlRewriteEntityTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\UrlRewrite\Test\TestCase;
+
+use Mtf\TestCase\Injectable;
+use Magento\UrlRewrite\Test\Fixture\UrlRewrite;
+use Magento\UrlRewrite\Test\Page\Adminhtml\UrlrewriteEdit;
+use Magento\UrlRewrite\Test\Page\Adminhtml\UrlrewriteIndex;
+
+/**
+ * Test Creation for CreateCustomUrlRewriteEntity
+ *
+ * Test Flow:
+ *
+ * Preconditions:
+ * 1. Create CMS Page
+ * 2. Create subcategory
+ * 3. Create simple product
+ * 4. Create custom storeView
+ *
+ * Steps:
+ * 1. Login to backend as Admin
+ * 2. Go to the Marketing-> SEO & Search->URL Redirects
+ * 3. Click "Add Url Rewrite" button
+ * 4. Select "Custom" in Create URL Rewrite dropdown
+ * 5. Fill data according to data set
+ * 6. Save Rewrite
+ * 7. Perform all assertions
+ *
+ * @group URL_Rewrites_(PS)
+ * @ZephyrId MTA-334
+ */
+class CreateCustomUrlRewriteEntityTest extends Injectable
+{
+    /**
+     * Url rewrite index page
+     *
+     * @var UrlrewriteIndex
+     */
+    protected $urlRewriteIndex;
+
+    /**
+     * Url rewrite edit page
+     *
+     * @var UrlrewriteEdit
+     */
+    protected $urlRewriteEdit;
+
+    /**
+     * Inject  pages
+     *
+     * @param UrlrewriteIndex $urlRewriteIndex
+     * @param UrlrewriteEdit $urlRewriteEdit
+     * @return void
+     */
+    public function __inject(UrlrewriteIndex $urlRewriteIndex, UrlrewriteEdit $urlRewriteEdit)
+    {
+        $this->urlRewriteIndex = $urlRewriteIndex;
+        $this->urlRewriteEdit = $urlRewriteEdit;
+    }
+
+    /**
+     * Create custom URL Rewrite
+     *
+     * @param UrlRewrite $urlRewrite
+     * @return void
+     */
+    public function test(UrlRewrite $urlRewrite)
+    {
+        $this->urlRewriteIndex->open();
+        $this->urlRewriteIndex->getPageActionsBlock()->addNew();
+        $this->urlRewriteEdit->getUrlRewriteTypeSelectorBlock()->selectType('Custom');
+        $this->urlRewriteEdit->getFormBlock()->fill($urlRewrite);
+        $this->urlRewriteEdit->getPageMainActions()->save();
+    }
+}
