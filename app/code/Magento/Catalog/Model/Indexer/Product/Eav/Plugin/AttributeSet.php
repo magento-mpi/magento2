a825c7f075244622fb58fa8edbaa913c0790868d
Max Gubar <mgubar@ebay.com>
2014-06-26 13:43:59 +0300

    MAGETWO-25499: Unit tests

--- app/code/Magento/Catalog/Model/Indexer/Product/Eav/Plugin/AttributeSet.php
+++ app/code/Magento/Catalog/Model/Indexer/Product/Eav/Plugin/AttributeSet.php
@@ -15,20 +15,20 @@ class AttributeSet
     protected $_indexerEavProcessor;
 
     /**
-     * @var \Magento\Catalog\Model\Resource\Eav\AttributeFactory
+     * @var AttributeSet\IndexableAttributeFilter
      */
-    protected $_attributeFactory;
+    protected $_attributeFilter;
 
     /**
      * @param \Magento\Catalog\Model\Indexer\Product\Eav\Processor $indexerEavProcessor
-     * @param \Magento\Catalog\Model\Resource\Eav\AttributeFactory $attributeFactory
+     * @param AttributeSet\IndexableAttributeFilter $filter
      */
     public function __construct(
         \Magento\Catalog\Model\Indexer\Product\Eav\Processor $indexerEavProcessor,
-        \Magento\Catalog\Model\Resource\Eav\AttributeFactory $attributeFactory
+        AttributeSet\IndexableAttributeFilter $filter
     ) {
         $this->_indexerEavProcessor = $indexerEavProcessor;
-        $this->_attributeFactory = $attributeFactory;
+        $this->_attributeFilter = $filter;
     }
 
     /**
@@ -41,60 +41,18 @@ class AttributeSet
      *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function aroundSave(
-        \Magento\Eav\Model\Entity\Attribute\Set $subject,
-        \Closure $proceed
-    ) {
+    public function aroundSave(\Magento\Eav\Model\Entity\Attribute\Set $subject, \Closure $proceed)
+    {
         if ($subject->getId()) {
             $originalSet = clone $subject;
             $originalSet->initFromSkeleton($subject->getId());
-            $originalAttributeCodes = $this->_fetchIndexableAttributeCodesFromSet($originalSet);
-            $subjectAttributeCodes = $this->_fetchIndexableAttributeCodesFromSet($subject);
+            $originalAttributeCodes = $this->_attributeFilter->filter($originalSet);
+            $subjectAttributeCodes = $this->_attributeFilter->filter($subject);
             if (count(array_diff($originalAttributeCodes, $subjectAttributeCodes))) {
                 $this->_indexerEavProcessor->markIndexerAsInvalid();
             }
         }
         $proceed();
         return $subject;
-    }
-
-    /**
-     * Fetch indexable attribute models from set
-     *
-     * @param \Magento\Eav\Model\Entity\Attribute\Set $set
-     * @return array
-     */
-    protected function _fetchIndexableAttributesFromSet(\Magento\Eav\Model\Entity\Attribute\Set $set)
-    {
-        $catalogResource = $this->_attributeFactory->create();
-        $attributes = [];
-        foreach ($set->getGroups() as $group) {
-            /** @var $group \Magento\Eav\Model\Entity\Attribute\Group */
-            foreach ($group->getAttributes() as $attribute) {
-                /** @var $attribute \Magento\Eav\Model\Entity\Attribute */
-                $catalogResource->clearInstance()->load($attribute->getId());
-                if ($catalogResource->isIndexable()) {
-                    $attributes[] = $attribute;
-                }
-            }
-        }
-        return $attributes;
-    }
-
-    /**
-     * Fetch indexable attribute codes from set
-     *
-     * @param \Magento\Eav\Model\Entity\Attribute\Set $set
-     * @return array
-     */
-    protected function _fetchIndexableAttributeCodesFromSet(\Magento\Eav\Model\Entity\Attribute\Set $set)
-    {
-        $codes = [];
-        foreach ($this->_fetchIndexableAttributesFromSet($set) as $attribute) {
-            /** @var \Magento\Eav\Model\Entity\Attribute $attribute */
-            $attribute->load($attribute->getAttributeId());
-            $codes[] = $attribute->getAttributeCode();
-        }
-        return $codes;
     }
 }
---
+++ dev/tests/unit/testsuite/Magento/Catalog/Model/Indexer/Product/Eav/Plugin/AttributeSetTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Catalog\Model\Indexer\Product\Eav\Plugin;
+
+class AttributeSetTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAroundSave()
+    {
+        $eavProcessorMock = $this->getMockBuilder('Magento\Catalog\Model\Indexer\Product\Eav\Processor')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $eavProcessorMock->expects($this->once())
+            ->method('markIndexerAsInvalid');
+
+        $filter = $this->getMockBuilder(
+            'Magento\Catalog\Model\Indexer\Product\Eav\Plugin\AttributeSet\IndexableAttributeFilter'
+        )
+            ->disableOriginalConstructor()
+            ->getMock();
+        $filter->expects($this->at(0))
+            ->method('filter')
+            ->will($this->returnValue(array(1, 2, 3)));
+        $filter->expects($this->at(1))
+            ->method('filter')
+            ->will($this->returnValue(array(1, 2)));
+
+        $subjectMock = $this->getMockBuilder('Magento\Eav\Model\Entity\Attribute\Set')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $subjectMock->expects($this->any())
+            ->method('getId')
+            ->will($this->returnValue(11));
+
+        $model = new \Magento\Catalog\Model\Indexer\Product\Eav\Plugin\AttributeSet(
+            $eavProcessorMock,
+            $filter
+        );
+
+        $this->assertEquals(
+            $subjectMock,
+            $model->aroundSave($subjectMock, function () {})
+        );
+    }
+}
