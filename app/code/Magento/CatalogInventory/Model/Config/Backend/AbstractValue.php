33f2587ecc656f165917ac5e314c43d8c23d2ead
Roman Ganin <rganin@ebay.com>
2014-06-26 12:10:14 +0300

    MAGETWO-25497: Remove old EAV indexer

---
+++ app/code/Magento/CatalogInventory/Model/Config/Backend/AbstractValue.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+
+/**
+ * Catalog Inventory Config Backend Model
+ */
+namespace Magento\CatalogInventory\Model\Config\Backend;
+
+abstract class AbstractValue extends \Magento\Framework\App\Config\Value
+{
+    /**
+     * @var \Magento\CatalogInventory\Model\Stock\Status
+     */
+    protected $_stockStatus;
+
+    /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
+     * @param \Magento\CatalogInventory\Model\Stock\Status $stockStatus
+     * @param \Magento\Framework\Model\Resource\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\Db $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\CatalogInventory\Model\Stock\Status $stockStatus,
+        \Magento\Framework\Model\Resource\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\Db $resourceCollection = null,
+        array $data = array()
+    ) {
+        $this->_stockStatus = $stockStatus;
+        parent::__construct($context, $registry, $config, $resource, $resourceCollection, $data);
+    }
+}
