ec2a004578335e0a33a13e924c5eab96828fcb55
Maxim Medinskiy <mmedinskiy@ebay.com>
2014-06-27 17:00:09 +0300

    MAGETWO-25514: Convert SQL scripts

---
+++ app/code/Magento/SalesArchive/sql/salesarchive_setup/install-1.11.0.0.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/* @var $installer \Magento\SalesArchive\Model\Resource\Setup */
+$installer = $this;
