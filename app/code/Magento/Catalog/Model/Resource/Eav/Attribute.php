d4d427e9e06bd71c155e5d683eca445a2133591e
Max Gubar <mgubar@ebay.com>
2014-06-27 11:42:02 +0300

    MAGETWO-25505: CI/CD activity and PSR2 codefixes for EAV indexer

--- app/code/Magento/Catalog/Model/Resource/Eav/Attribute.php
+++ app/code/Magento/Catalog/Model/Resource/Eav/Attribute.php
@@ -238,10 +238,10 @@ class Attribute extends \Magento\Eav\Model\Entity\Attribute
     protected function _isEnabledInFlat()
     {
         return $this->getData('backend_type') == 'static'
-            || $this->_productFlatIndexerHelper->isAddFilterableAttributes()
-            && $this->getData('is_filterable') > 0
-            || $this->getData('used_in_product_listing') == 1
-            || $this->getData('used_for_sort_by') == 1;
+        || $this->_productFlatIndexerHelper->isAddFilterableAttributes()
+        && $this->getData('is_filterable') > 0
+        || $this->getData('used_in_product_listing') == 1
+        || $this->getData('used_for_sort_by') == 1;
     }
 
     /**
@@ -252,10 +252,10 @@ class Attribute extends \Magento\Eav\Model\Entity\Attribute
     protected function _isOriginalEnabledInFlat()
     {
         return $this->getOrigData('backend_type') == 'static'
-            || $this->_productFlatIndexerHelper->isAddFilterableAttributes()
-            && $this->getOrigData('is_filterable') > 0
-            || $this->getOrigData('used_in_product_listing') == 1
-            || $this->getOrigData('used_for_sort_by') == 1;
+        || $this->_productFlatIndexerHelper->isAddFilterableAttributes()
+        && $this->getOrigData('is_filterable') > 0
+        || $this->getOrigData('used_in_product_listing') == 1
+        || $this->getOrigData('used_for_sort_by') == 1;
     }
 
     /**
