e5885fdcb59d8c00179afc5076e47073d37731e1
Sergey Semenov <ssemenov@ebay.com>
2014-07-01 21:26:41 +0300

    MAGETWO-25504: Implement Web Configuration step
    - added web-config form.

--- setup/pub/magento/setup/app.js
+++ setup/pub/magento/setup/app.js
@@ -7,7 +7,42 @@ app.controller('navigationController', ['$scope', 'navigationService', function 
         console.log('addDatabaseController');
     }])
     .controller('webConfigurationController', ['$scope', function ($scope) {
-        console.log('webConfigurationController');
+
+        $scope.webAddress = {
+            value: 'http://www.example.com/',
+            hint: false
+        };
+
+        $scope.adminAddress = '';
+
+        $scope.httpsOptions = {
+            storefront: true,
+            admin: true
+        };
+
+        $scope.rewrites = {
+            allowed: true
+        };
+
+        $scope.encryptionKey = {
+            value: '',
+            type: 'magento',
+            hint: false
+        };
+
+        $scope.expanded = false;
+
+        $scope.updateOnExpand = function() {
+            $scope.expanded = !$scope.expanded;
+        }
+
+        $scope.showHint = function(element) {
+            element.hint = true;
+        }
+
+        $scope.showEncryptionKey = function() {
+            return angular.equals($scope.encryptionKey.type, 'customer');
+        }
     }])
     .controller('customizeYourStoreController', ['$scope', function ($scope) {
         console.log('customizeYourStoreController');
