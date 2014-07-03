ee7d2305728872ed727354c7cc260c29d726b508
Andriy Nasinnyk <anasinnyk@ebay.com>
2014-07-03 18:02:15 +0300

    MAGETWO-25502: Implement Add a database step

--- setup/pub/magento/setup/landing.js
+++ setup/pub/magento/setup/landing.js
@@ -1,22 +1,29 @@
 'use strict';
-var landing = angular.module('landing', []);
-landing.controller('landingController', ['$scope', '$location', 'languageService', function ($scope, $location, languageService) {
-    $scope.selectLanguage = function () {
-        window.location = '/setup/' + $scope.modelLanguage.code + '/index';
-    };
-    languageService.load(function (response) {
-        $scope.languages = response.data.languages;
-        var indexOf = $location.absUrl().search('setup/') + 6;
-        var value = $location.absUrl().slice(indexOf, indexOf + 5);
-        $scope.languages.forEach(function (lang, index) {
-            if (lang.code == value) {
-                $scope.modelLanguage = $scope.languages[index];
-            }
-        });
+var landing = angular.module('landing', ['$localStorage']);
+landing.controller('landingController', [
+        '$scope',
+        '$location',
+        '$localStorage',
+        'languageService',
+        function ($scope, $location, $localStorage, languageService) {
+            $scope.selectLanguage = function () {
+                $localStorage.lang = $scope.modelLanguage.code;
+                window.location = '/setup/' + $scope.modelLanguage.code + '/index';
+            };
+            languageService.load(function (response) {
+                $scope.languages = response.data.languages;
+                var indexOf = $location.absUrl().search('setup/') + 6;
+                var value = $location.absUrl().slice(indexOf, indexOf + 5);
+                $scope.languages.forEach(function (lang, index) {
+                    if (lang.code == value) {
+                        $scope.modelLanguage = $scope.languages[index];
+                    }
+                });
 
-    });
-
-}])
+            });
+        }
+    ]
+)
 .service('languageService', ['$http', function ($http) {
     return {
         languages: [],
