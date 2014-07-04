b3a0f2e95b6dbca18fd7d09fd27b52c704d2aa43
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-07-04 16:25:02 +0300

    MAGETWO-25373: Make all API functional tests atomic and independent on each other for Attribute       - WriteServiceTest - create_attribute_service_rollback.php added security to rollback

--- dev/tests/integration/testsuite/Magento/Catalog/Model/Product/Attribute/_files/create_attribute_service_rollback.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/Product/Attribute/_files/create_attribute_service_rollback.php
@@ -7,6 +7,11 @@
  */
 use Magento\TestFramework\Helper\Bootstrap;
 
+/** @var \Magento\Framework\Registry $registry */
+$registry = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Framework\Registry');
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
+
 /** @var \Magento\Catalog\Model\Resource\Eav\Attribute $attribute */
 $attribute = Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Resource\Eav\Attribute');
 $attribute->loadByCode(4, 'label_attr_code3df4tr3');
