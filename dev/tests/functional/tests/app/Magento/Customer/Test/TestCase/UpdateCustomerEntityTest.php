0d959bbac292c7c33b3867ac56b31ef062f36fa7
Igor Kruchymskyi <ikruchynskyi@ebay.com>
2014-07-04 10:10:59 +0300

    MTA-50: Test Creation for Update Customer Entity

--- dev/tests/functional/tests/app/Magento/Customer/Test/TestCase/UpdateCustomerEntityTest.php
+++ dev/tests/functional/tests/app/Magento/Customer/Test/TestCase/UpdateCustomerEntityTest.php
@@ -126,7 +126,10 @@ class UpdateCustomerEntityTest extends Injectable
         $this->customerAccountEdit->getAccountInfoForm()->fill($customerInit);
         $this->customerAccountEdit->getAccountInfoForm()->submit();
 
-        $assertCustomerInfoSuccessSavedMessage->configure($this, ['customerAccountIndex' => $this->customerAccountIndex]);
+        $assertCustomerInfoSuccessSavedMessage->configure(
+            $this,
+            ['customerAccountIndex' => $this->customerAccountIndex]
+        );
         \PHPUnit_Framework_Assert::assertThat($this->getName(), $assertCustomerInfoSuccessSavedMessage);
 
         $this->customerAccountIndex->getDashboardAddress()->editBillingAddress();
