f1d4fa79b22752957db151b9803da645695760c3
Andriy Nasinnyk <anasinnyk@ebay.com>
2014-07-07 13:18:40 +0300

    MAGETWO-25509: Implement Create Admin Account step

--- setup/pub/magento/setup/app.js
+++ setup/pub/magento/setup/app.js
@@ -13,8 +13,41 @@ var app = angular.module(
     ]);
 
 app.controller('createAdminAccountController', ['$scope', function ($scope) {
-        console.log('createAdminAccountController');
+        $scope.admin = {};
+        $scope.admin.passwordStatus = {
+            class: 'none',
+            label: 'None'
+        };
+        $scope.passwordStatusChange = function () {
+            var p = $scope.admin.password;
+            if (p.length > 6 && p.match(/[\d]+/) && p.match(/[a-z]+/) && p.match(/[A-Z]+/) && p.match(/[!@#$%^*()_\/\\\-\+=]+/)) {
+                $scope.admin.passwordStatus.class = 'strong';
+                $scope.admin.passwordStatus.label = 'Strong';
+            } else if (p.length > 6 && p.match(/[\d]+/) && p.match(/[a-z]+/) && p.match(/[A-Z]+/)) {
+                $scope.admin.passwordStatus.class = 'good';
+                $scope.admin.passwordStatus.label = 'Good';
+            } else if (p.length > 6 && p.match(/[\d]+/) && p.match(/[a-z]+/)) {
+                $scope.admin.passwordStatus.class = 'weak';
+                $scope.admin.passwordStatus.label = 'Weak';
+            } else if (p.length > 6) {
+                $scope.admin.passwordStatus.class = 'to-short';
+                $scope.admin.passwordStatus.label = 'To Short';
+            } else {
+                $scope.admin.passwordStatus.class = 'none';
+                $scope.admin.passwordStatus.label = 'None';
+            }
+            console.log(p, $scope.admin.passwordStatus);
+        }
     }])
+//    .directive('checkStrength', function () {
+//        return {
+//            replace: false,
+//            restrict: 'E',
+//            link: function (scope, iElement, iAttributes) {
+//                console.log(scope, iElement, iAttributes);
+//            }
+//        };
+//    })
     .controller('installController', ['$scope', function ($scope) {
         console.log('installController');
     }])
