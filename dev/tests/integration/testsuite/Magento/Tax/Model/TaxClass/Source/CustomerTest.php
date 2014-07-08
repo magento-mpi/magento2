39b2a13337a24f044f4c7ca99753c108305d2b40
Tatyana Vynograd <tvynograd@ebay.com>
2014-07-08 17:34:42 +0300

    MAGETWO-25889: Refactor Source Models
    - Add integration tests

---
+++ dev/tests/integration/testsuite/Magento/Tax/Model/TaxClass/Source/CustomerTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Tax\Model\TaxClass\Source;
+
+use Magento\TestFramework\Helper\Bootstrap;
+
+class CustomerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetAllOptions()
+    {
+        /** @var \Magento\Tax\Model\Resource\TaxClass\Collection $collection */
+        $collection = Bootstrap::getObjectManager()->get('Magento\Tax\Model\Resource\TaxClass\Collection');
+        $expectedResult = [];
+        /** @var \Magento\Tax\Model\ClassModel $taxClass */
+        foreach ($collection as $taxClass) {
+            if ($taxClass->getClassType() == \Magento\Tax\Service\V1\Data\TaxClass::TYPE_CUSTOMER){
+                $expectedResult[] = ['value' => $taxClass->getId(), 'label' => $taxClass->getClassName()];
+            }
+        }
+        if (empty($expectedResult)) {
+            $this->fail('Preconditions failed: At least one tax class should be available.');
+        }
+        /** @var \Magento\Tax\Model\TaxClass\Source\Product $source */
+        $source = Bootstrap::getObjectManager()->get('Magento\Tax\Model\TaxClass\Source\Customer');
+        $this->assertEquals(
+            $expectedResult,
+            $source->getAllOptions(),
+            'Tax Class options are invalid.'
+        );
+    }
+}
