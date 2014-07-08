cd1747d5c956be0c765d3963bfd74ede16aeb8cf
Yuxing Zheng <yuxzheng@ebay.com>
2014-07-08 12:44:10 -0500

    MAGETWO-25191: Refactor Customer
    - Added integration test for changes

--- dev/tests/integration/testsuite/Magento/Customer/Block/Adminhtml/Group/Edit/FormTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Block/Adminhtml/Group/Edit/FormTest.php
@@ -8,12 +8,10 @@
 
 namespace Magento\Customer\Block\Adminhtml\Group\Edit;
 
-use Magento\Backend\App\Area\FrontNameResolver;
 use Magento\Customer\Controller\RegistryConstants;
 use Magento\Customer\Service\V1\Data\CustomerGroup;
-use Magento\Framework\Service\V1\Data\FilterBuilder;
-use Magento\Framework\Service\V1\Data\SearchCriteriaBuilder;
 use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Tax\Service\V1\Data\TaxClass;
 
 /**
  * Magento\Customer\Block\Adminhtml\Group\Edit\Form
@@ -82,6 +80,7 @@ class FormTest extends \PHPUnit_Framework_TestCase
         $this->assertNotNull($idElement);
         $this->assertEquals('1', $idElement->getValue());
         $this->assertEquals('3', $taxClassIdElement->getValue());
+        $this->assertEquals($this->getAllTaxClassOptions(), $taxClassIdElement->getData('values'));
         $this->assertEquals('General', $groupCodeElement->getValue());
     }
 
@@ -114,6 +113,28 @@ class FormTest extends \PHPUnit_Framework_TestCase
         $this->assertNotNull($idElement);
         $this->assertEquals($customerGroup->getId(), $idElement->getValue());
         $this->assertEquals($customerGroup->getTaxClassId(), $taxClassIdElement->getValue());
+        $this->assertEquals($this->getAllTaxClassOptions(), $taxClassIdElement->getData('values'));
         $this->assertEquals($customerGroup->getCode(), $groupCodeElement->getValue());
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAllTaxClassOptions()
+    {
+        $filters[] = Bootstrap::getObjectManager()->create('\Magento\Framework\Service\V1\Data\FilterBuilder')
+            ->setField('class_type')
+            ->setValue(TaxClass::TYPE_CUSTOMER)
+            ->create();
+        /** @var \Magento\Framework\Service\V1\Data\SearchCriteriaBuilder $searchCriteria */
+        $searchCriteriaBuilder = Bootstrap::getObjectManager()->create('\Magento\Framework\Service\V1\Data\SearchCriteriaBuilder');
+        $searchCriteriaBuilder->addFilter($filters);
+        return Bootstrap::getObjectManager()->create('\Magento\Framework\Convert\Object')->toOptionArray(
+            Bootstrap::getObjectManager()->create('\Magento\Tax\Service\V1\TaxClassServiceInterface')
+                ->searchTaxClass($searchCriteriaBuilder->create())
+                ->getItems(),
+            'classid',
+            'classname'
+        );
     }
 }
