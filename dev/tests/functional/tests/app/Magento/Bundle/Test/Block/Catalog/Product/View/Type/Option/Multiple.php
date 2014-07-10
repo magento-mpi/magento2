540d8948c35395590e1f62084265818a1e9a682a
Vitaliy Honcharenko <vgoncharenko@ebay.com>
2014-07-10 13:32:19 +0300

    MTA-17: Create Bundle Product Entity
    - del fill in Multiple-type bundle option

--- dev/tests/functional/tests/app/Magento/Bundle/Test/Block/Catalog/Product/View/Type/Option/Multiple.php
+++ dev/tests/functional/tests/app/Magento/Bundle/Test/Block/Catalog/Product/View/Type/Option/Multiple.php
@@ -16,15 +16,5 @@ use Magento\Bundle\Test\Block\Catalog\Product\View\Type\Option;
  */
 class Multiple extends Option
 {
-    /**
-     * Set data in multiselect option
-     *
-     * @param array $data
-     * @return void
-     */
-    public function fillOption(array $data)
-    {
-       $mapping = $this->dataMapping($data);
-       $this->getElement($this->_rootElement, $mapping['name'])->setValue($mapping['name']['value'], false);
-    }
+    // Parent behavior
 }
