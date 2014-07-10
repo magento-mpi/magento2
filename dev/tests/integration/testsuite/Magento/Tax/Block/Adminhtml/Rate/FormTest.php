0da6ca0a8bb3c0c77de524a47ff126a14aae39aa
Bryant Luk <bluk@ebay.com>
2014-07-10 13:58:06 -0500

    MAGETWO-25871: Refactor Dependencies on Tax Rate Model for getTitles
    - Fix namespace

--- dev/tests/integration/testsuite/Magento/Tax/Block/Adminhtml/Rate/FormTest.php
+++ dev/tests/integration/testsuite/Magento/Tax/Block/Adminhtml/Rate/FormTest.php
@@ -5,7 +5,7 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-namespace Magento\Tax\Block\Adminhtml\Rate\Edit;
+namespace Magento\Tax\Block\Adminhtml\Rate;
 
 use Magento\TestFramework\Helper\Bootstrap;
 
