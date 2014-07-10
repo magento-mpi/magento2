5dba84078e24a0acfaf110814b2a392bd3c2ee6b
Robert He <rohe@ebay.com>
2014-07-10 15:09:22 -0500

    MAGETWO-24340 : improve Kiwis unit test coverage
     -- added tests in lib / Magento / Framework / Data

---
+++ dev/tests/unit/testsuite/Magento/Framework/Data/TreeTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Framework\Data;
+
+/**
+ * Tests for \Magento\Framework\Data\FormFactory
+ * @SuppressWarnings(PHPMD.LongVariable)
+ */
+class TreeTest extends \PHPUnit_Framework_TestCase
+{
+    protected $_tree;
+
+    public function setUp()
+    {
+        $this->_tree = new Tree();
+    }
+
+    public function testTreeOperations()
+    {
+        $newNode1 = new Tree\Node('abc', 'node1', $this->_tree);
+        $this->_tree->addNode($newNode1);
+        $newNode2 = new Tree\Node('def', 'node2', $this->_tree);
+        $this->_tree->addNode($newNode2, $newNode1);
+        $newNode3 = new Tree\Node('ghi', 'node3', $this->_tree);
+        $this->_tree->addNode($newNode3, $newNode1);
+        $data1 = ['j', 'k', 'l'];
+        $this->_tree->appendChild($data1, $newNode3);
+        $newNode4 = new Tree\Node('mno', 'node4', $this->_tree);
+        $this->_tree->appendChild($newNode4, $newNode3);
+
+        $this->_tree->removeNode($newNode4);
+        $this->_tree->removeNode($newNode3);
+        $this->_tree->removeNode($newNode2);
+        $this->_tree->removeNode($newNode1);
+
+        $this->assertEmpty($this->_tree->getNodes()->getNodes());
+    }
+}
+ 
