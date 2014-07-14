503f88f9e268784db8752a00c0828facad6691db
Anton Kril <akril@ebay.com>
2014-07-14 12:40:14 +0300

    MAGETWO-24455: CLONE - Updating Qty on Shopping cart has no effect on subtotal price

---
+++ dev/tests/unit/testsuite/Magento/Multishipping/Controller/Checkout/PluginTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Multishipping\Controller\Checkout;
+
+class PluginTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $cartMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $quoteMock;
+
+    /**
+     * @var Plugin
+     */
+    protected $object;
+
+    protected function setUp()
+    {
+        $this->cartMock = $this->getMock('Magento\Checkout\Model\Cart', [], [], '', false);
+        $this->quoteMock = $this->getMock(
+            'Magento\Sales\Model\Quote',
+            ['__wakeUp','setIsMultiShipping'],
+            [],
+            '',
+            false
+        );
+        $this->cartMock->expects($this->once())->method('getQuote')->will($this->returnValue($this->quoteMock));
+        $this->object = new Plugin($this->cartMock);
+    }
+
+    public function testExecuteTurnsOffMultishippingModeOnQuote()
+    {
+        $subject = $this->getMock('Magento\Checkout\Controller\Onepage\Index', [], [], '', false);
+        $this->quoteMock->expects($this->once())->method('setIsMultiShipping')->with(0);
+        $this->object->beforeExecute($subject);
+    }
+} 
