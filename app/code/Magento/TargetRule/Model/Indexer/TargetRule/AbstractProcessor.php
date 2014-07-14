f3c85bcf47858b1ecd7e36639784c8b5e555b8fb
Roman Ganin <rganin@ebay.com>
2014-07-14 17:28:55 +0300

    MAGETWO-25986: Indexer locks for product/rule and rule/product targetrule indexers

--- app/code/Magento/TargetRule/Model/Indexer/TargetRule/AbstractProcessor.php
+++ app/code/Magento/TargetRule/Model/Indexer/TargetRule/AbstractProcessor.php
@@ -10,18 +10,8 @@
 
 namespace Magento\TargetRule\Model\Indexer\TargetRule;
 
-abstract class AbstractProcessor
+abstract class AbstractProcessor extends \Magento\Indexer\Model\Indexer\AbstractProcessor
 {
-    /**
-     * Indexer ID
-     */
-    const INDEXER_ID = '';
-
-    /**
-     * @var \Magento\Indexer\Model\IndexerInterface
-     */
-    protected $_indexer;
-
     /**
      * State container
      *
@@ -42,67 +32,6 @@ abstract class AbstractProcessor
     }
 
     /**
-     * Get indexer
-     *
-     * @return \Magento\Indexer\Model\IndexerInterface
-     */
-    public function getIndexer()
-    {
-        if (!$this->_indexer->getId()) {
-            $this->_indexer->load($this->getIndexerId());
-        }
-        return $this->_indexer;
-    }
-
-    /**
-     * Run Row reindex
-     *
-     * @param int $id
-     * @return void
-     */
-    public function reindexRow($id)
-    {
-        if ($this->getIndexer()->isScheduled()) {
-            return;
-        }
-        $this->getIndexer()->reindexRow($id);
-    }
-
-    /**
-     * Run List reindex
-     *
-     * @param int[] $ids
-     * @return void
-     */
-    public function reindexList($ids)
-    {
-        if ($this->getIndexer()->isScheduled()) {
-            return;
-        }
-        $this->getIndexer()->reindexList($ids);
-    }
-
-    /**
-     * Run Full reindex
-     *
-     * @return void
-     */
-    public function reindexAll()
-    {
-        $this->getIndexer()->reindexAll();
-    }
-
-    /**
-     * Mark Product price indexer as invalid
-     *
-     * @return void
-     */
-    public function markIndexerAsInvalid()
-    {
-        $this->getIndexer()->invalidate();
-    }
-
-    /**
      * Get processor state container
      *
      * @return Status\Container
@@ -110,16 +39,6 @@ abstract class AbstractProcessor
     public function getStatusContainer()
     {
         return $this->_statusContainer;
-    }
-
-    /**
-     * Get processor indexer ID
-     *
-     * @return string
-     */
-    public function getIndexerId()
-    {
-        return static::INDEXER_ID;
     }
 
     /**
@@ -139,6 +58,7 @@ abstract class AbstractProcessor
      */
     public function setFullReindexPassed()
     {
+        $this->getIndexer()->getState()->setStatus(\Magento\Indexer\Model\Indexer\State::STATUS_VALID)->save();
         $this->getStatusContainer()->setFullReindexPassed($this->getIndexerId());
     }
 }
