a9d1ba076251a730267a84f8dbafa6af83149112
Bryant Luk <bluk@ebay.com>
2014-07-14 17:46:22 -0500

    MAGETWO-24550: Merge stories together
    - Fix testcase so it does not rely on the autoincrement of the id to be reset

--- dev/tests/integration/testsuite/Magento/Tax/Service/V1/Collection/TaxRuleCollectionTest.php
+++ dev/tests/integration/testsuite/Magento/Tax/Service/V1/Collection/TaxRuleCollectionTest.php
@@ -14,6 +14,7 @@ class TaxRuleCollectionTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
+     * @magentoAppIsolation enabled
      * @magentoDataFixture Magento/Tax/_files/tax_classes.php
      */
     public function testCreateTaxRuleCollectionItem()
@@ -21,7 +22,12 @@ class TaxRuleCollectionTest extends \PHPUnit_Framework_TestCase
         /** @var \Magento\Tax\Model\Resource\Calculation\Rule\Collection $collection */
         $collection = Bootstrap::getObjectManager()->get('Magento\Tax\Model\Resource\Calculation\Rule\Collection');
         $dbTaxRulesQty = $collection->count();
-        if (($dbTaxRulesQty == 0) || ($collection->getFirstItem()->getId() != 1)) {
+
+        $firstTaxRuleFixture = Bootstrap::getObjectManager()->get('Magento\Framework\Registry')
+            ->registry('_fixture/Magento_Tax_Model_Calculation_Rule');
+        $expectedFirstTaxRuleId = $firstTaxRuleFixture->getId();
+
+        if (($dbTaxRulesQty == 0) || ($collection->getFirstItem()->getId() != $expectedFirstTaxRuleId)) {
             $this->fail("Preconditions failed.");
         }
         /** @var \Magento\Tax\Service\V1\Collection\TaxRuleCollection $taxRulesCollection */
@@ -31,15 +37,16 @@ class TaxRuleCollectionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($dbTaxRulesQty, $collectionTaxRulesQty, 'Tax rules quantity is invalid.');
         $taxRule = $taxRulesCollection->getFirstItem()->getData();
         $expectedTaxRuleData = [
-            'tax_calculation_rule_id' => '1',
+            'tax_calculation_rule_id' => $expectedFirstTaxRuleId,
             'code' => 'Test Rule',
             'priority' => '0',
             'position' => '0',
             'calculate_subtotal' => '0',
-            'customer_tax_classes' => ['4', '5'],
-            'product_tax_classes' => ['6', '7'],
-            'tax_rates' => ['3'],
+            'customer_tax_classes' => $firstTaxRuleFixture->getTaxCustomerClass(),
+            'product_tax_classes' => $firstTaxRuleFixture->getTaxProductClass(),
+            'tax_rates' => $firstTaxRuleFixture->getTaxRate()
         ];
+
         $this->assertEquals($expectedTaxRuleData, $taxRule, 'Tax rule data is invalid.');
     }
 }
