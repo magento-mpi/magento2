5d421e860c8056384791d1a34b2d36a54017c0d9
Paul Lewis <plewis@ebay.com>
2014-07-15 16:13:38 -0500

    MAGETWO-26224: Create unit tests for CalculatorFactory

---
+++ dev/tests/unit/testsuite/Magento/Tax/Model/Calculation/CalculatorFactoryTest.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Tax\Model\Calculation;
+
+use Magento\Customer\Service\V1\Data\Address;
+use Magento\TestFramework\Helper\ObjectManager;
+
+/**
+ * Test class for \Magento\Tax\Model\CalculatorFactory
+ */
+class CalculatorFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\TestFramework\Helper\ObjectManager
+     */
+    public $objectManager;
+
+    public function setUp()
+    {
+        $this->objectManager = new ObjectManager($this);
+    }
+
+    /**
+     * @param string $type Type of calculator
+     * @param int $storeId
+     * @param Address $billingAddress
+     * @param Address $shippingAddress
+     * @param null|int $customerTaxClassId
+     * @param \Magento\Tax\Model\Calculation\AbstractCalculator $expectedInstanceType
+     *  expected type of calculator instance
+     *
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(
+        $type,
+        $storeId,
+        $billingAddress,
+        $shippingAddress,
+        $customerTaxClassId,
+        $expectedInstanceType
+    ) {
+        $instanceMock = $this->getMockBuilder($expectedInstanceType)->disableOriginalConstructor()->getMock();
+        $objectManagerMock = $this->getMockBuilder('Magento\Framework\ObjectManager')->getMock();
+
+        // Verify create() is called with correct concrete type
+        $objectManagerMock->expects($this->once())
+            ->method('create')
+            ->with($expectedInstanceType, ['storeId' => $storeId])
+            ->will($this->returnValue($instanceMock));
+
+        /** @var CalculatorFactory $calculatorFactory */
+        $calculatorFactory = $this->objectManager->getObject(
+            'Magento\Tax\Model\Calculation\CalculatorFactory',
+            ['objectManager' => $objectManagerMock]
+        );
+
+        // Verify billing is set correctly if passed in
+        if ($billingAddress != null) {
+            $instanceMock->expects($this->once())
+                ->method('setBillingAddress')
+                ->with($billingAddress);
+        } else {
+            $instanceMock->expects($this->never())
+                ->method('setBillingAddress');
+        }
+
+        // Verify shipping is set correctly if passed in
+        if ($shippingAddress != null) {
+            $instanceMock->expects($this->once())
+                ->method('setShippingAddress')
+                ->with($shippingAddress);
+        } else {
+            $instanceMock->expects($this->never())
+                ->method('setShippingAddress');
+        }
+
+        // Verify customerTaxClassId is set correctly if passed in
+        if ($customerTaxClassId != null) {
+            $instanceMock->expects($this->once())
+                ->method('setCustomerTaxClassId')
+                ->with($customerTaxClassId);
+        } else {
+            $instanceMock->expects($this->never())
+                ->method('setCustomerTaxClassId');
+        }
+
+        // Call create()
+        $calculator = $calculatorFactory
+            ->create($type, $storeId, $billingAddress, $shippingAddress, $customerTaxClassId);
+
+        // Verify correct type is returned
+        $this->assertInstanceOf($expectedInstanceType, $calculator);
+    }
+
+    /**
+     * Returns a set of 'true' and 'false' parameters for each of the setter/getter method pairs
+     *
+     * @return array
+     */
+    public function createDataProvider()
+    {
+        $billingAddressMock = $this->getMockBuilder('\Magento\Customer\Service\V1\Data\Address')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $shippingAddressMock = $this->getMockBuilder('\Magento\Customer\Service\V1\Data\Address')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return [
+            'Unit' => [
+                CalculatorFactory::CALC_UNIT_BASE,
+                1,
+                null,
+                null,
+                null,
+                'Magento\Tax\Model\Calculation\UnitBaseCalculator'
+            ],
+            'Row HasBilling' => [
+                CalculatorFactory::CALC_ROW_BASE,
+                2,
+                $billingAddressMock,
+                null,
+                null,
+                'Magento\Tax\Model\Calculation\RowBaseCalculator'
+            ],
+            'Row HasCustomerTaxClassId' => [
+                CalculatorFactory::CALC_ROW_BASE,
+                3,
+                null,
+                null,
+                123,
+                'Magento\Tax\Model\Calculation\RowBaseCalculator'
+            ],
+            'Total HasShipping' => [
+                CalculatorFactory::CALC_TOTAL_BASE,
+                1,
+                null,
+                $shippingAddressMock,
+                null,
+                'Magento\Tax\Model\Calculation\TotalBaseCalculator'
+            ],
+            'Total HasShipping HasBilling HasCustomer' => [
+                CalculatorFactory::CALC_TOTAL_BASE,
+                1,
+                $billingAddressMock,
+                $shippingAddressMock,
+                1,
+                'Magento\Tax\Model\Calculation\TotalBaseCalculator'
+            ],
+        ];
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Unknown calculation type: NOT_A_TYPE
+     */
+    public function testCreateInvalid()
+    {
+        /** @var CalculatorFactory $calculatorFactory */
+        $calculatorFactory = $this->objectManager->getObject(
+            'Magento\Tax\Model\Calculation\CalculatorFactory'
+        );
+
+        // Call create() with a bad type to generate exception
+        $calculatorFactory->create('NOT_A_TYPE', 1);
+    }
+}
