cd15927ac6eb490b1896515dbd407a4c3605d7a8
Tatyana Vynograd <tvynograd@ebay.com>
2014-07-15 13:57:27 +0300

    MAGETWO-26181: Fix tests

--- dev/tests/integration/testsuite/Magento/Tax/Model/TaxClass/Source/CustomerTest.php
+++ dev/tests/integration/testsuite/Magento/Tax/Model/TaxClass/Source/CustomerTest.php
@@ -34,4 +34,28 @@ class CustomerTest extends \PHPUnit_Framework_TestCase
             'Tax Class options are invalid.'
         );
     }
+
+    public function testGetAllOptionsWithDefaultValues()
+    {
+        /** @var \Magento\Tax\Model\Resource\TaxClass\Collection $collection */
+        $collection = Bootstrap::getObjectManager()->get('Magento\Tax\Model\Resource\TaxClass\Collection');
+        $expectedResult = [];
+        /** @var \Magento\Tax\Model\ClassModel $taxClass */
+        foreach ($collection as $taxClass) {
+            if ($taxClass->getClassType() == \Magento\Tax\Service\V1\TaxClassServiceInterface::TYPE_CUSTOMER) {
+                $expectedResult[] = ['value' => $taxClass->getId(), 'label' => $taxClass->getClassName()];
+            }
+        }
+        if (empty($expectedResult)) {
+            $this->fail('Preconditions failed: At least one tax class should be available.');
+        }
+        $expectedResult = array_merge(array(array('value' => '0', 'label' => __('None'))), $expectedResult);
+        /** @var \Magento\Tax\Model\TaxClass\Source\Product $source */
+        $source = Bootstrap::getObjectManager()->get('Magento\Tax\Model\TaxClass\Source\Customer');
+        $this->assertEquals(
+            $expectedResult,
+            $source->getAllOptions(true),
+            'Tax Class options are invalid.'
+        );
+    }
 }
