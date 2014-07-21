c0d37b0d369e3a3d0a0b6dd9aefe7cc95e4954b7
Maddy Chellathurai <mchellathura@ebay.com>
2014-07-21 13:12:34 -0500

    MAGETWO-25341: Checkin to Mainline S13
    - changed file permissions for tmp folder

--- dev/tests/unit/testsuite/Magento/Framework/Backup/_files/app_dirs.php
+++ dev/tests/unit/testsuite/Magento/Framework/Backup/_files/app_dirs.php
@@ -13,6 +13,6 @@ $appDirs = ['code', 'media', 'pub/media', 'var/log'];
 foreach ($appDirs as $dir) {
     $appDir = TESTS_TEMP_DIR . '/Magento/Backup/data/' . $dir;
     if (!is_dir($appDir)) {
-        mkdir($appDir, 777, true);
+        mkdir($appDir, 0777, true);
     }
 }
