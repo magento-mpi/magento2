e012e76b5bb95cef0abfd2718b9ef8a5d6c90a5b
Bibu Mathew <bimathew@paypal.com>
2014-07-21 16:33:14 -0500

    MAGETWO-26538: Create a PublicCookieMetadata object
    - added PublicCookieMetadata class

--- lib/internal/Magento/Framework/Stdlib/Cookie/PublicCookieMetadata.php
+++ lib/internal/Magento/Framework/Stdlib/Cookie/PublicCookieMetadata.php
@@ -10,5 +10,72 @@ namespace Magento\Framework\Stdlib;
 
 class PublicCookieMetadata
 {
+    /**
+     * @var  int
+     */
+    protected $duration;
 
+    /**
+     * @var  bool
+     */
+    protected $httpOnly;
+
+    /*
+     * @var bool
+     */
+    protected $secure;
+
+    /**
+     * Set expire time
+     *
+     * @param int $duration
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Set HTTPOnly flag
+     *
+     * @param bool $httpOnly
+     */
+    public function setHttpOnly($httpOnly)
+    {
+        $this->httpOnly = $httpOnly;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getHttpOnly()
+    {
+        return $this->httpOnly;
+    }
+
+    /**
+     * Set Whether the cookie is only available under HTTPS
+     *
+     * @param bool $secure
+     */
+    public function setSecure($secure)
+    {
+        $this->secure = $secure;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getSecure()
+    {
+        return $this->secure;
+    }
 }
