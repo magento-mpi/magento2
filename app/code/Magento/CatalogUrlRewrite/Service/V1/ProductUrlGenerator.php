4a8b8487c76e09c7e123edb56ad6a89db90bc6fc
Valeriy Nayda <vnayda@ebay.com>
2014-07-22 13:24:33 +0300

    MAGETWO-25952: [Implement URL Rewrites] Eliminate old Url Rewrite module, part 1
    - MAGETWO-26283: Enhance ProductGenerator
    -- ProductUrlGenerator refactoring

--- app/code/Magento/CatalogUrlRewrite/Service/V1/ProductUrlGenerator.php
+++ app/code/Magento/CatalogUrlRewrite/Service/V1/ProductUrlGenerator.php
@@ -8,9 +8,10 @@
 namespace Magento\CatalogUrlRewrite\Service\V1;
 
 use Magento\CatalogUrlRewrite\Helper\Data as CatalogUrlRewriteHelper;
-use Magento\Store\Model\StoreManagerInterface;
 use Magento\UrlRedirect\Model\OptionProvider;
+use Magento\UrlRedirect\Service\V1\Data\Converter;
 use Magento\UrlRedirect\Service\V1\Data\FilterFactory;
+use Magento\UrlRedirect\Service\V1\Data\UrlRewrite;
 use Magento\UrlRedirect\Service\V1\UrlMatcherInterface;
 
 /**
@@ -33,13 +34,6 @@ class ProductUrlGenerator implements ProductUrlGeneratorInterface
     protected $filterFactory;
 
     /**
-     * Store manager
-     *
-     * @var StoreManagerInterface
-     */
-    protected $storeManager;
-
-    /**
      * @var UrlMatcherInterface
      */
     protected $urlMatcher;
@@ -48,6 +42,16 @@ class ProductUrlGenerator implements ProductUrlGeneratorInterface
      * @var CatalogUrlRewriteHelper
      */
     protected $catalogUrlRewriteHelper;
+
+    /**
+     * @var StoreViewService
+     */
+    protected $storeViewService;
+
+    /**
+     * @var Converter
+     */
+    protected $converter;
 
     /**
      * @var \Magento\Catalog\Model\Product
@@ -66,20 +70,23 @@ class ProductUrlGenerator implements ProductUrlGeneratorInterface
 
     /**
      * @param FilterFactory $filterFactory
-     * @param StoreManagerInterface $storeManager
      * @param UrlMatcherInterface $urlMatcher
      * @param CatalogUrlRewriteHelper $catalogUrlRewriteHelper
+     * @param StoreViewService $storeViewService
+     * @param Converter $converter
      */
     public function __construct(
         FilterFactory $filterFactory,
-        StoreManagerInterface $storeManager,
         UrlMatcherInterface $urlMatcher,
-        CatalogUrlRewriteHelper $catalogUrlRewriteHelper
+        CatalogUrlRewriteHelper $catalogUrlRewriteHelper,
+        StoreViewService $storeViewService,
+        Converter $converter
     ) {
         $this->filterFactory = $filterFactory;
-        $this->storeManager = $storeManager;
         $this->urlMatcher = $urlMatcher;
         $this->catalogUrlRewriteHelper = $catalogUrlRewriteHelper;
+        $this->storeViewService = $storeViewService;
+        $this->converter = $converter;
     }
 
     /**
@@ -91,9 +98,8 @@ class ProductUrlGenerator implements ProductUrlGeneratorInterface
         $this->product = $product;
         $storeId = $this->product->getStoreId();
 
-        // TODO: Check this logic (@TODO: UrlRewrite)
-        $urls = $this->catalogUrlRewriteHelper->isDefaultStore($storeId)
-            ? $this->generateForDefaultStore() : $this->generateForStore($storeId);
+        $urls = $this->storeViewService->isGlobalScope($storeId)
+            ? $this->generateForGlobalScope() : $this->generateForSpecificStoreView($storeId);
 
         $this->product = null;
         $this->categories = null;
@@ -101,54 +107,67 @@ class ProductUrlGenerator implements ProductUrlGeneratorInterface
     }
 
     /**
-     * {@inheritdoc}
-     *  TODO: hack for obtaining data from changed categories. Replace on Service Data Object (@TODO: UrlRewrite)
-     */
-    public function generateWithChangedCategories($product, $changedCategories)
-    {
-        $this->changedCategories = $changedCategories;
-
-        return $this->generate($product);
-    }
-
-    /**
-     * Generate list of urls for default store
+     * Generate list of urls for global scope
      *
      * @return \Magento\UrlRedirect\Service\V1\Data\UrlRewrite[]
      */
-    protected function generateForDefaultStore()
+    protected function generateForGlobalScope()
     {
         $urls = [];
-        foreach ($this->storeManager->getStores() as $store) {
-            if ($this->catalogUrlRewriteHelper->isNeedCreateUrlRewrite($store->getStoreId(), $this->product->getId())) {
-                $urls = array_merge($urls, $this->generateForStore($store->getStoreId()));
+        foreach ($this->product->getStoreIds() as $storeId) {
+            if (!$this->storeViewService->doesProductHaveOverriddenUrlKeyForStore($storeId, $this->product->getId())) {
+                $urls = array_merge($urls, $this->generateForSpecificStoreView($storeId));
             }
         }
         return $urls;
     }
 
     /**
-     * Generate list of urls per store
+     * Generate list of urls for specific store view
      *
      * @param int $storeId
      * @return \Magento\UrlRedirect\Service\V1\Data\UrlRewrite[]
      */
-    protected function generateForStore($storeId)
+    protected function generateForSpecificStoreView($storeId)
     {
-        $urls[] = $this->createUrlRewrite(
+        return array_merge(
+            $this->generateRewritesBasedOnStoreView($storeId),
+            $this->generateRewritesBasedOnCategories($storeId),
+            $this->generateRewritesBasedOnCurrentRewrites($storeId)
+        );
+    }
+
+    /**
+     * Generate list based on store view
+     *
+     * @param int $storeId
+     * @return \Magento\UrlRedirect\Service\V1\Data\UrlRewrite[]
+     */
+    protected function generateRewritesBasedOnStoreView($storeId)
+    {
+        return [$this->createUrlRewrite(
             $storeId,
             $this->catalogUrlRewriteHelper->getProductUrlKeyPath($this->product, $storeId),
             $this->catalogUrlRewriteHelper->getProductCanonicalUrlPath($this->product)
-        );
+        )];
+    }
 
+    /**
+     * Generate list based on categories
+     *
+     * @param int $storeId
+     * @return \Magento\UrlRedirect\Service\V1\Data\UrlRewrite[]
+     */
+    protected function generateRewritesBasedOnCategories($storeId)
+    {
+        $urls = [];
         // TODO: check if $category is not in this tore view (@TODO: UrlRewrite)
         foreach ($this->getCategories() as $category) {
             // TODO: hack for obtaining data from changed categories. Replace on Service Data Object (@TODO: UrlRewrite)
             if (isset($this->changedCategories[$category->getId()])) {
                 $category = $this->changedCategories[$category->getId()];
             }
-
-            if ($this->catalogUrlRewriteHelper->isRootCategory($category)) {
+            if ($this->storeViewService->isRootCategoryForStore($category->getId(), $storeId)) {
                 continue;
             }
             $urls[] = $this->createUrlRewrite(
@@ -157,19 +176,26 @@ class ProductUrlGenerator implements ProductUrlGeneratorInterface
                 $this->catalogUrlRewriteHelper->getProductCanonicalUrlPathWithCategory($this->product, $category)
             );
         }
-        return array_merge($urls, $this->generateRewritesBasedOnCurrentRewrites($storeId));
+        return $urls;
     }
 
     /**
-     * Generate permanent rewrites based on current rewrites
+     * Generate list based on current rewrites
      *
      * @param int $storeId
-     * @return array
+     * @return \Magento\UrlRedirect\Service\V1\Data\UrlRewrite[]
      */
     protected function generateRewritesBasedOnCurrentRewrites($storeId)
     {
+        /** @var \Magento\UrlRedirect\Service\V1\Data\Filter $filter */
+        $filter = $this->filterFactory->create();
+
+        $filter->setStoreId($storeId);
+        $filter->setEntityId($this->product->getId());
+        $filter->setEntityType(self::ENTITY_TYPE_PRODUCT);
+
         $urls = [];
-        foreach ($this->urlMatcher->findAllByFilter($this->createCurrentUrlRewritesFilter($storeId)) as $url) {
+        foreach ($this->urlMatcher->findAllByFilter($filter) as $url) {
             $targetPath = null;
             // TODO: 'Create Permanent Redirect for old URL' for products thorough category page (@TODO: UrlRewrite)
             if ($url->getRedirectType()) {
@@ -198,18 +224,14 @@ class ProductUrlGenerator implements ProductUrlGeneratorInterface
     }
 
     /**
-     * @param int $storeId
-     * @return \Magento\UrlRedirect\Service\V1\Data\Filter
+     * {@inheritdoc}
+     *  TODO: hack for obtaining data from changed categories. Replace on Service Data Object (@TODO: UrlRewrite)
      */
-    protected function createCurrentUrlRewritesFilter($storeId)
+    public function generateWithChangedCategories($product, $changedCategories)
     {
-        /** @var \Magento\UrlRedirect\Service\V1\Data\Filter $filter */
-        $filter = $this->filterFactory->create();
+        $this->changedCategories = $changedCategories;
 
-        $filter->setStoreId($storeId);
-        $filter->setEntityId($this->product->getId());
-        $filter->setEntityType(self::ENTITY_TYPE_PRODUCT);
-        return $filter;
+        return $this->generate($product);
     }
 
     /**
@@ -238,13 +260,13 @@ class ProductUrlGenerator implements ProductUrlGeneratorInterface
      */
     protected function createUrlRewrite($storeId, $requestPath, $targetPath, $redirectType = null)
     {
-        return $this->catalogUrlRewriteHelper->createUrlRewrite(
-            self::ENTITY_TYPE_PRODUCT,
-            $this->product->getId(),
-            $storeId,
-            $requestPath,
-            $targetPath,
-            $redirectType
-        );
+        return $this->converter->convertArrayToObject([
+            UrlRewrite::ENTITY_TYPE => self::ENTITY_TYPE_PRODUCT,
+            UrlRewrite::ENTITY_ID => $this->product->getId(),
+            UrlRewrite::STORE_ID => $storeId,
+            UrlRewrite::REQUEST_PATH => $requestPath,
+            UrlRewrite::TARGET_PATH => $targetPath,
+            UrlRewrite::REDIRECT_TYPE => $redirectType,
+        ]);
     }
 }
---
+++ app/code/Magento/CatalogUrlRewrite/Service/V1/StoreViewService.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\CatalogUrlRewrite\Service\V1;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Config;
+use Magento\Framework\App\Resource;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Store view service
+ */
+class StoreViewService
+{
+    /**
+     * @var \Magento\Eav\Model\Config
+     */
+    protected $eavConfig;
+
+    /**
+     * @var \Magento\Framework\DB\Adapter\AdapterInterface
+     */
+    protected $connection;
+
+    /**
+     * Store manager
+     *
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @param Config $eavConfig
+     * @param Resource $resource
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        Config $eavConfig,
+        Resource $resource,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->eavConfig = $eavConfig;
+        $this->connection = $resource->getConnection(Resource::DEFAULT_READ_RESOURCE);
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * Check is root category for store view
+     *
+     * @param int $categoryId
+     * @param int $storeId
+     * @return bool
+     */
+    public function isRootCategoryForStore($categoryId, $storeId)
+    {
+        $store = $this->storeManager->getStore($storeId);
+
+        return $categoryId == $store->getRootCategoryId();
+    }
+
+    /**
+     * Check is global scope
+     *
+     * @param int|null $storeId
+     * @return bool
+     */
+    public function isGlobalScope($storeId)
+    {
+        return null === $storeId || $storeId == Store::DEFAULT_STORE_ID;
+    }
+
+    /**
+     * Check that product has overridden url key for specific store
+     *
+     * @param int $storeId
+     * @param int $productId
+     * @return bool
+     */
+    public function doesProductHaveOverriddenUrlKeyForStore($storeId, $productId)
+    {
+        $attribute = $this->eavConfig->getAttribute(Product::ENTITY, 'url_key');
+        $select = $this->connection->select()
+            ->from($attribute->getBackendTable(), 'store_id')
+            ->where('attribute_id = ?', $attribute->getId())
+            ->where('entity_id = ?', $productId);
+
+        return in_array($storeId, $this->connection->fetchCol($select));
+    }
+}
