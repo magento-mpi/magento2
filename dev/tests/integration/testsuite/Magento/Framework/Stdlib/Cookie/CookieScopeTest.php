bcb856cadd10eec7955308574575a0f426c13ab3
Christopher O'Toole <otoolec@x.com>
2014-07-23 13:51:24 -0500

    MAGETWO-26539: Add CookieScope Integration test

---
+++ dev/tests/integration/testsuite/Magento/Framework/Stdlib/Cookie/CookieScopeTest.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Framework\Stdlib\Cookie;
+
+use Magento\Framework\ObjectManager;
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * Test CookieScope
+ *
+ */
+class CookieScopeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    public function setUp()
+    {
+        $this->objectManager = Bootstrap::getObjectManager();
+
+    }
+
+    public function testGetSensitiveCookieMetadataEmpty()
+    {
+        $cookieScope = $this->createCookieScope();
+
+        $this->assertEquals([], $cookieScope->getSensitiveCookieMetadata()->__toArray());
+    }
+
+    public function testGetPublicCookieMetadataEmpty()
+    {
+        $cookieScope = $this->createCookieScope();
+
+        $this->assertEquals([], $cookieScope->getPublicCookieMetadata()->__toArray());
+    }
+
+    public function testGetSensitiveCookieMetadataDefaults()
+    {
+        $defaultValues = [
+            SensitiveCookieMetadata::KEY_PATH => 'default path',
+            SensitiveCookieMetadata::KEY_DOMAIN => 'default domain',
+        ];
+        $sensitive = $this->createSensitiveMetadata($defaultValues);
+        $cookieScope = $this->createCookieScope(
+            [
+                'sensitiveCookieMetadata' => $sensitive,
+                'publicCookieMetadata' => null,
+            ]
+        );
+
+        $this->assertEquals([], $cookieScope->getPublicCookieMetadata()->__toArray());
+        $this->assertEquals($defaultValues, $cookieScope->getSensitiveCookieMetadata()->__toArray());
+    }
+
+    public function testGetPublicCookieMetadataDefaults()
+    {
+        $defaultValues = [
+            PublicCookieMetadata::KEY_PATH => 'default path',
+            PublicCookieMetadata::KEY_DOMAIN => 'default domain',
+            PublicCookieMetadata::KEY_DURATION => 'default duration',
+            PublicCookieMetadata::KEY_HTTP_ONLY => 'default http',
+            PublicCookieMetadata::KEY_SECURE => 'default secure',
+        ];
+        $public = $this->createPublicMetadata($defaultValues);
+        $cookieScope = $this->createCookieScope(
+            [
+                'sensitiveCookieMetadata' => null,
+                'publicCookieMetadata' => $public,
+            ]
+        );
+
+        $this->assertEquals([], $cookieScope->getSensitiveCookieMetadata()->__toArray());
+        $this->assertEquals($defaultValues, $cookieScope->getPublicCookieMetadata()->__toArray());
+    }
+
+    public function testGetSensitiveCookieMetadataOverrides()
+    {
+        $defaultValues = [
+            SensitiveCookieMetadata::KEY_PATH => 'default path',
+            SensitiveCookieMetadata::KEY_DOMAIN => 'default domain',
+        ];
+        $overrideValues = [
+            SensitiveCookieMetadata::KEY_PATH => 'override path',
+            SensitiveCookieMetadata::KEY_DOMAIN => 'override domain',
+        ];
+        $sensitive = $this->createSensitiveMetadata($defaultValues);
+        $cookieScope = $this->createCookieScope(
+            [
+                'sensitiveCookieMetadata' => $sensitive,
+                'publicCookieMetadata' => null,
+            ]
+        );
+        $override = $this->createSensitiveMetadata($overrideValues);
+
+        $this->assertEquals([], $cookieScope->getPublicCookieMetadata($this->createPublicMetadata())->__toArray());
+        $this->assertEquals($overrideValues, $cookieScope->getSensitiveCookieMetadata($override)->__toArray());
+    }
+
+    public function testGetPublicCookieMetadataOverrides()
+    {
+        $defaultValues = [
+            PublicCookieMetadata::KEY_PATH => 'default path',
+            PublicCookieMetadata::KEY_DOMAIN => 'default domain',
+            PublicCookieMetadata::KEY_DURATION => 'default duration',
+            PublicCookieMetadata::KEY_HTTP_ONLY => 'default http',
+            PublicCookieMetadata::KEY_SECURE => 'default secure',
+        ];
+        $overrideValues = [
+            PublicCookieMetadata::KEY_PATH => 'override path',
+            PublicCookieMetadata::KEY_DOMAIN => 'override domain',
+            PublicCookieMetadata::KEY_DURATION => 'override duration',
+            PublicCookieMetadata::KEY_HTTP_ONLY => 'override http',
+            PublicCookieMetadata::KEY_SECURE => 'override secure',
+        ];
+        $public = $this->createPublicMetadata($defaultValues);
+        $cookieScope = $this->createCookieScope(
+            [
+                'sensitiveCookieMetadata' => null,
+                'publicCookieMetadata' => $public,
+            ]
+        );
+        $override = $this->createPublicMetadata($overrideValues);
+
+        $this->assertEquals(
+            [],
+            $cookieScope->getSensitiveCookieMetadata($this->createSensitiveMetadata())->__toArray()
+        );
+        $this->assertEquals($overrideValues, $cookieScope->getPublicCookieMetadata($override)->__toArray());
+    }
+
+    /**
+     * Creates a CookieScope object with the given parameters.
+     *
+     * @param array $params
+     * @return CookieScope
+     */
+    private function createCookieScope($params = [])
+    {
+        return $this->objectManager->create('Magento\Framework\Stdlib\Cookie\CookieScope', $params);
+    }
+
+    /**
+     * Creates a SensitiveCookieMetadata object with provided metadata values.
+     *
+     * @param array $metadata
+     * @return SensitiveCookieMetadata
+     */
+    public function createSensitiveMetadata($metadata = [])
+    {
+        return $this->objectManager->create(
+            'Magento\Framework\Stdlib\Cookie\SensitiveCookieMetadata',
+            ['metadata' => $metadata]
+        );
+    }
+
+    /**
+     * Creates a PublicCookieMetadata object with provided metadata values.
+     *
+     * @param array $metadata
+     * @return PublicCookieMetadata
+     */
+    public function createPublicMetadata($metadata = [])
+    {
+        return $this->objectManager->create(
+            'Magento\Framework\Stdlib\Cookie\PublicCookieMetadata',
+            ['metadata' => $metadata]
+        );
+    }
+
+}
