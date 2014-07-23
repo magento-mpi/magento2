0f2517aa7ae54a193d3e6bbdfdc9e7164e6c07cc
Bryant Luk <bluk@ebay.com>
2014-07-23 12:03:24 -0500

    MAGETWO-26609: Create Web API Role Locator
    - Remove CompositeRoleLocator
    - Add WebapiRoleLocator

---
+++ app/code/Magento/Webapi/Model/WebapiRoleLocator.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Webapi\Model;
+
+use Magento\Authorization\Model\UserContextInterface;
+use Magento\Framework\Authorization\RoleLocator;
+use Magento\User\Model\Role;
+use Magento\User\Model\Resource\Role\CollectionFactory as RoleCollectionFactory;
+
+class WebapiRoleLocator implements RoleLocator
+{
+    /**
+     * @var UserContextInterface
+     */
+    protected $userContext;
+
+    /**
+     * @var RoleCollectionFactory
+     */
+    protected $roleCollectionFactory;
+
+    /**
+     * Constructs a role locator using the user context.
+     *
+     * @param UserContextInterface $userContext
+     * @param RoleCollectionFactory $roleCollectionFactory
+     */
+    public function __construct(
+        UserContextInterface $userContext,
+        RoleCollectionFactory $roleCollectionFactory
+    ) {
+        $this->userContext = $userContext;
+        $this->roleCollectionFactory = $roleCollectionFactory;
+    }
+
+    /**
+     * Retrieve current role.
+     *
+     * @return string
+     */
+    public function getAclRoleId()
+    {
+        $userId = $this->userContext->getUserId();
+        $userType = $this->userContext->getUserType();
+
+        $roleCollection = $this->roleCollectionFactory->create();
+        /** @var Role $role */
+        $role = $roleCollection->setUserFilter($userId, $userType)->getFirstItem();
+
+        if (!$role->getId()) {
+            return '';
+        }
+
+        return $role->getId();
+    }
+}
