d7fd3742cbdb063540277b3ea1b6383adc9cedcf
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-07-23 14:01:44 +0300

    MAGETWO-26250 [Discovered]: CRUD service & APIs to manage options for bundled products
    MAGETWO-26507: Create service Remove
     - Created unit test for remove service

---
+++ dev/tests/unit/testsuite/Magento/Bundle/Service/V1/Product/Option/WriteServiceTest.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Bundle\Service\V1\Product\Option;
+
+use Magento\TestFramework\Helper\ObjectManager;
+
+class WriteServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Bundle\Service\V1\Product\Option\WriteService
+     */
+    private $model;
+
+    /**
+     * @var \Magento\Catalog\Model\ProductRepository|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $productRepository;
+
+    /**
+     * @var \Magento\Bundle\Model\Product\Type|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $productType;
+
+    /**
+     * @var \Magento\Catalog\Model\Product|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $product;
+
+    /**
+     * @var \Magento\Bundle\Model\Option|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $option;
+
+    public function setUp()
+    {
+        $objectManager = new ObjectManager($this);
+
+        $this->productRepository = $this->getMockBuilder('Magento\Catalog\Model\ProductRepository')
+            ->setMethods(['get'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->productType= $this->getMockBuilder('Magento\Bundle\Model\Product\Type')
+            ->setMethods(['getOptionsCollection'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->product = $this->getMockBuilder('Magento\Catalog\Model\Product')
+            ->setMethods(['__wakeup', 'getTypeId', 'getTypeInstance'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->product->expects($this->any())->method('getTypeInstance')->will($this->returnValue($this->productType));
+
+        $this->option = $this->getMockBuilder('Magento\Bundle\Model\Option')
+            ->setMethods(['__wakeup', 'getId', 'delete'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->model = $objectManager->getObject(
+            'Magento\Bundle\Service\V1\Product\Option\WriteService',
+            ['productRepository' => $this->productRepository]
+        );
+    }
+
+    public function testRemove()
+    {
+        $productSku = 'oneSku';
+        $optionId = 3;
+
+        $this->productRepository->expects($this->once())->method('get')
+            ->with($this->equalTo($productSku))
+            ->will($this->returnValue($this->product));
+
+        $this->product->expects($this->once())->method('getTypeId')
+            ->will($this->returnValue(\Magento\Catalog\Model\Product\Type::TYPE_BUNDLE));
+
+        $this->productType->expects($this->once())->method('getOptionsCollection')
+            ->with($this->equalTo($this->product))
+            ->will($this->returnValue([$this->option]));
+
+        $this->option->expects($this->once())->method('getId')->will($this->returnValue($optionId));
+        $this->option->expects($this->once())->method('delete');
+
+        $this->assertTrue($this->model->remove($productSku, $optionId));
+    }
+
+    /**
+     * @expectedException \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function testRemoveNoSuchEntityException()
+    {
+        $productSku = 'oneSku';
+        $optionId = 3;
+
+        $this->productRepository->expects($this->once())->method('get')
+            ->with($this->equalTo($productSku))
+            ->will($this->returnValue($this->product));
+
+        $this->product->expects($this->once())->method('getTypeId')
+            ->will($this->returnValue(\Magento\Catalog\Model\Product\Type::TYPE_BUNDLE));
+
+        $this->productType->expects($this->once())->method('getOptionsCollection')
+            ->with($this->equalTo($this->product))
+            ->will($this->returnValue([$this->option]));
+
+        $this->option->expects($this->once())->method('getId')->will($this->returnValue($optionId + 1));
+
+        $this->model->remove($productSku, $optionId);
+    }
+
+    /**
+     * @expectedException \Magento\Webapi\Exception
+     * @expectedExceptionCode 403
+     */
+    public function testRemoveWebApiException()
+    {
+        $productSku = 'oneSku';
+        $optionId = 3;
+
+        $this->productRepository->expects($this->once())->method('get')
+            ->with($this->equalTo($productSku))
+            ->will($this->returnValue($this->product));
+
+        $this->product->expects($this->once())->method('getTypeId')
+            ->will($this->returnValue(\Magento\Catalog\Model\Product\Type::TYPE_SIMPLE));
+
+        $this->model->remove($productSku, $optionId);
+    }
+}
