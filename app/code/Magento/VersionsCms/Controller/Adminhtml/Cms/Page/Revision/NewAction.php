2546f8550e3284dd00c3ae016e2a12458661a60a
Maxim Shikula <mshikula@ebay.com>
2014-07-25 13:27:11 +0300

    MAGETWO-26627: Exception on saving CMS page version

--- app/code/Magento/VersionsCms/Controller/Adminhtml/Cms/Page/Revision/NewAction.php
+++ app/code/Magento/VersionsCms/Controller/Adminhtml/Cms/Page/Revision/NewAction.php
@@ -12,4 +12,11 @@ use Magento\VersionsCms\Controller\Adminhtml\Cms\Page\RevisionInterface;
 
 class NewAction extends Edit implements RevisionInterface
 {
+    /**
+     * Forward to edit
+     */
+    public function execute()
+    {
+        $this->_forward('edit');
+    }
 }
