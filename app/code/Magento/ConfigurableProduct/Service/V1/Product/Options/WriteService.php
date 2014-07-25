940e8497c8a868c0586bcdf75ebca562c998c2cd
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-07-25 15:14:34 +0300

    MAGETWO-26249 [Discovered]: Service/API to assign configurable attribute to configurable product
    MAGETWO-26480: Create service Remove
     - Implemented remove service for configurable product

--- app/code/Magento/ConfigurableProduct/Service/V1/Product/Options/WriteService.php
+++ app/code/Magento/ConfigurableProduct/Service/V1/Product/Options/WriteService.php
@@ -8,16 +8,17 @@
 
 namespace Magento\ConfigurableProduct\Service\V1\Product\Options;
 
+use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\ProductRepository;
-use Magento\ConfigurableProduct\Service\V1\Data\Option;
-use Magento\ConfigurableProduct\Service\V1\Data\OptionConverter;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable\AttributeFactory as ConfigurableAttributeFactory;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable as ConfigurableType;
 use Magento\ConfigurableProduct\Model\Resource\Product\Type\Configurable\Attribute\CollectionFactory;
+use Magento\ConfigurableProduct\Service\V1\Data\Option;
+use Magento\ConfigurableProduct\Service\V1\Data\OptionConverter;
 use Magento\Eav\Model\Config as EavConfig;
-use Magento\Catalog\Model\Product;
-use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Webapi\Exception;
 
 class WriteService implements WriteServiceInterface
@@ -55,6 +56,10 @@ class WriteService implements WriteServiceInterface
      * @var \Magento\ConfigurableProduct\Service\V1\Data\OptionConverter
      */
     protected $optionConverter;
+    /**
+     * @var \Magento\ConfigurableProduct\Model\Product\Type\Configurable
+     */
+    private $productType;
 
     /**
      * @param ProductRepository $productRepository
@@ -62,19 +67,22 @@ class WriteService implements WriteServiceInterface
      * @param CollectionFactory $collectionFactory
      * @param EavConfig $eavConfig
      * @param OptionConverter $optionConverter
+     * @param Configurable $productType
      */
     public function __construct(
         ProductRepository $productRepository,
         ConfigurableAttributeFactory $configurableAttributeFactory,
         CollectionFactory $collectionFactory,
         EavConfig $eavConfig,
-        OptionConverter $optionConverter
+        OptionConverter $optionConverter,
+        Configurable $productType
     ) {
         $this->productRepository = $productRepository;
         $this->configurableAttributeFactory = $configurableAttributeFactory;
         $this->collectionFactory = $collectionFactory;
         $this->eavConfig = $eavConfig;
         $this->optionConverter = $optionConverter;
+        $this->productType = $productType;
     }
 
     /**
@@ -103,5 +111,45 @@ class WriteService implements WriteServiceInterface
         }
 
         return $this->optionConverter->convert($configurableAttribute);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($productSku, $optionId)
+    {
+        $product = $this->getProduct($productSku);
+
+        $attributeCollection = $this->productType->getConfigurableAttributeCollection($product);
+        /** @var \Magento\ConfigurableProduct\Model\Product\Type\Configurable\Attribute $option */
+        $option = $attributeCollection->getItemById($optionId);
+
+        if ($option === null) {
+            throw new NoSuchEntityException('Requested option doesn\'t exist');
+        }
+        $option->delete();
+
+        return true;
+    }
+
+    /**
+     * @param string $productSku
+     * @return \Magento\Catalog\Model\Product
+     * @throws \Magento\Webapi\Exception
+     */
+    private function getProduct($productSku)
+    {
+        $product = $this->productRepository->get($productSku);
+        if (ConfigurableType::TYPE_CODE !== $product->getTypeId()) {
+            throw new Exception(
+                'Product with specified sku: "%1" is not a configurable product',
+                Exception::HTTP_FORBIDDEN,
+                Exception::HTTP_FORBIDDEN,
+                [
+                    $product->getSku()
+                ]
+            );
+        }
+        return $product;
     }
 }
