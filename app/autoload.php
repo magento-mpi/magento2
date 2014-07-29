85261b1057fb5de98901db35d7a5379f7736a11f
Olga Kopylova <okopylova@ebay.com>
2014-07-29 12:35:53 -0500

    MAGETWO-26253: 3rd party components available to magento
    - re-implemented including of composer autoloader
    - implemented autoload adapter for a project (non-skeleton)

--- app/autoload.php
+++ app/autoload.php
@@ -7,5 +7,8 @@
  * @copyright  {copyright}
  * @license    {license_link}
  */
+if (file_exists(__DIR__ . '/vendor_autoload.php')) {
+    require_once __DIR__ . '/vendor_autoload.php';
+}
 require_once __DIR__ . '/../lib/internal/Magento/Framework/Autoload/IncludePath.php';
 spl_autoload_register([new \Magento\Framework\Autoload\IncludePath(), 'load'], true, true);
---
+++ app/vendor_autoload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+/**
+ * Adapter for composer autoload
+ */
+if (file_exists(BP . '/vendor/autoload.php')) {
+    require_once BP . '/vendor/autoload.php';
+}
