8fc3178da1680e8928880601fc7c9f800a36ccd0
Stanislav Lopukhov <slopukhov@ebay.com>
2014-07-29 15:57:42 +0300

    MAGETWO-26429: Integration tests

--- dev/tests/integration/testsuite/Magento/TargetRule/Model/Indexer/TargetRule/Rule/Product/Action/RowTest.php
+++ dev/tests/integration/testsuite/Magento/TargetRule/Model/Indexer/TargetRule/Rule/Product/Action/RowTest.php
@@ -54,4 +54,68 @@ class RowTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(2, count($this->_rule->getMatchingProductIds()));
     }
+
+    /**
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     * @magentoDataFixture Magento/Catalog/_files/categories.php
+     */
+    public function testReindexRowByCategories()
+    {
+        $this->_processor->getIndexer()->setScheduled(false);
+        $this->assertFalse($this->_processor->getIndexer()->isScheduled());
+
+        $data = array(
+            'name' => 'related',
+            'is_active' => '1',
+            'apply_to' => 1,
+            'use_customer_segment' => '0',
+            'customer_segment_ids' => array('0' => ''),
+            'conditions' => array(
+                '1' => array(
+                    'type' => 'Magento\TargetRule\Model\Rule\Condition\Combine',
+                    'aggregator' => 'all',
+                    'value' => '1',
+                    'new_child' => ''
+                ),
+                '1--1' => array(
+                    'type' => 'Magento\TargetRule\Model\Rule\Condition\Product\Attributes',
+                    'attribute' => 'category_ids',
+                    'operator' => '==',
+                    'value' => '11'
+                )
+            )
+        );
+        $this->_rule->loadPost($data);
+        $this->_rule->save();
+        
+        $this->assertEquals(2, count($this->_rule->getMatchingProductIds()));
+
+        $data = array(
+            'name' => 'related',
+            'is_active' => '1',
+            'apply_to' => 1,
+            'use_customer_segment' => '0',
+            'customer_segment_ids' => array('0' => ''),
+            'conditions' => array(
+                '1' => array(
+                    'type' => 'Magento\TargetRule\Model\Rule\Condition\Combine',
+                    'aggregator' => 'all',
+                    'value' => '1',
+                    'new_child' => ''
+                ),
+                '1--1' => array(
+                    'type' => 'Magento\TargetRule\Model\Rule\Condition\Product\Attributes',
+                    'attribute' => 'category_ids',
+                    'operator' => '==',
+                    'value' => '5'
+                )
+            )
+        );
+        $this->_rule->loadPost($data);
+        $this->_rule->save();
+        $this->_rule->prepareMatchingProducts();
+
+        $this->assertEquals(1, count($this->_rule->getMatchingProductIds()));
+    }
 }
