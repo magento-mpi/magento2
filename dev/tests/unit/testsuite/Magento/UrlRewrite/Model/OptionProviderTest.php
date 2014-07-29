f827d9065a046c6d6fac88332201b75102101727
Vlad Veselov <vveselov@ebay.com>
2014-07-29 07:43:27 -0400

    MAGETWO-25952: [Implement URL Rewrites] Eliminate old Url Rewrite module, part 1
    - MAGETWO-26299: Enhance URL rewrite redirect_type source model and UI
    - adopt Magento/UrlRewrite/Model/OptionProviderTest unit test

--- dev/tests/unit/testsuite/Magento/UrlRewrite/Model/OptionProviderTest.php
+++ dev/tests/unit/testsuite/Magento/UrlRewrite/Model/OptionProviderTest.php
@@ -10,12 +10,12 @@ namespace Magento\UrlRewrite\Model;
 
 class OptionProviderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetAllOptions()
+    public function testToOptionArray()
     {
         $model = new OptionProvider();
-        $options = $model->getAllOptions();
+        $options = $model->toOptionArray();
         $this->assertInternalType('array', $options);
-        $expectedOptions = array('' => 'No', 'R' => 'Temporary (302)', 'RP' => 'Permanent (301)');
+        $expectedOptions = array('' => 'No', 302 => 'Temporary (302)', 301 => 'Permanent (301)');
         $this->assertEquals($expectedOptions, $options);
     }
 }
