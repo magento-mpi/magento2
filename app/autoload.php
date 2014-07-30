4bfb60b6cf1ee58a407c60d6628638cf8247bab0
Olga Kopylova <okopylova@ebay.com>
2014-07-30 18:32:30 -0500

    MAGETWO-26253: 3rd party components available to magento
    - added check for existence of composer autoload file (temporary solution while CI builds don't run composer install)

--- app/autoload.php
+++ app/autoload.php
@@ -8,6 +8,9 @@
  * @license    {license_link}
  */
 $vendorDir = require __DIR__ . '/etc/vendor_path.php';
-require_once __DIR__ . '/../' . $vendorDir . '/autoload.php';
+$vendorAutoload = __DIR__ . '/../' . $vendorDir . '/autoload.php';
+if (file_exists($vendorAutoload)) {
+    require_once $vendorAutoload;
+}
 require_once __DIR__ . '/../lib/internal/Magento/Framework/Autoload/IncludePath.php';
 spl_autoload_register([new \Magento\Framework\Autoload\IncludePath(), 'load'], true, true);
