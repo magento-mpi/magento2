44819734786c81e8e1f7a3174278a20f6d2c035d
Andrii Kasian <akasian@ebay.com>
2014-07-31 08:54:17 +0300

    MAGETWO-26056: Create Search request configuration
     - MAGETWO-26520:  Static Integrity Test for configuration validation

---
+++ dev/tests/static/testsuite/Magento/Test/Integrity/Magento/Search/ConfigTest.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Test search_request.xsd and xml files.
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Test\Integrity\Magento\Search;
+
+class ConfigTest extends \Magento\TestFramework\Integrity\AbstractConfig
+{
+    /**
+     * Returns the name of the XSD file to be used to validate the XML
+     *
+     * @return string
+     */
+    protected function _getXsd()
+    {
+        return '/lib/internal/Magento/Framework/Search/etc/search_request.xsd';
+    }
+
+    /**
+     * Returns the name of the XSD file to be used to validate partial XML
+     *
+     * @return string
+     */
+    protected function _getFileXsd()
+    {
+        return '/lib/internal/Magento/Framework/Search/etc/search_request.xsd';
+    }
+
+    /**
+     * The location of a single valid complete xml file
+     *
+     * @return string
+     */
+    protected function _getKnownValidXml()
+    {
+        return __DIR__ . '/_files/valid.xml';
+    }
+
+    /**
+     * The location of a single known invalid complete xml file
+     *
+     * @return string
+     */
+    protected function _getKnownInvalidXml()
+    {
+        return __DIR__ . '/_files/invalid.xml';
+    }
+
+    /**
+     * The location of a single known valid partial xml file
+     *
+     * @return string
+     */
+    protected function _getKnownValidPartialXml()
+    {
+        return __DIR__ . '/_files/valid.xml';
+    }
+
+    public function testFileSchemaUsingPartialXml()
+    {
+        parent::testFileSchemaUsingPartialXml();
+    }
+
+    public function testSchemaUsingInvalidXml($expectedErrors = null)
+    {
+        $this->markTestSkipped('shema redefine problem');
+        $expectedErrors = array_filter(
+            explode(
+                "\n",
+                "
+Element 'from': This element is not expected. Expected is ( filters ).
+"
+            )
+        );
+        parent::testSchemaUsingInvalidXml($expectedErrors);
+    }
+
+    public function testFileSchemaUsingInvalidXml($expectedErrors = null)
+    {
+        $expectedErrors = array_filter(
+            explode(
+                "\n",
+                "
+Element 'queryReference': The attribute 'ref' is required but missing.
+Element 'filterReference': The attribute 'ref' is required but missing.
+Element 'filter': The attribute 'field' is required but missing.
+Element 'filter': The attribute 'value' is required but missing.
+Element 'filterReference': The attribute 'clause' is required but missing.
+Element 'filterReference': The attribute 'ref' is required but missing.
+Element 'bucket': Missing child element(s). Expected is ( metrics ).
+Element 'metric', attribute 'type': [facet 'enumeration'] The value 'sumasdasd' is not an element of the set {'sum', 'count', 'min', 'max'}.
+Element 'metric', attribute 'type': 'sumasdasd' is not a valid value of the local atomic type.
+Element 'bucket': Missing child element(s). Expected is ( ranges ).
+Element 'request': Missing child element(s). Expected is ( from )."
+            )
+        );
+        parent::testFileSchemaUsingInvalidXml($expectedErrors);
+    }
+
+    public function testSchemaUsingPartialXml($expectedErrors = null)
+    {
+        $this->markTestSkipped('shema redefine problem');
+        parent::testSchemaUsingPartialXml();
+    }
+
+    /**
+     * Returns the name of the xml files to validate
+     *
+     * @return string
+     */
+    protected function _getXmlName()
+    {
+        return 'search_request.xml';
+    }
+
+    /**
+     * The location of a single known invalid partial xml file
+     *
+     * @return string
+     */
+    protected function _getKnownInvalidPartialXml()
+    {
+        return __DIR__ . '/_files/invalid_partial.xml';
+    }
+
+    public function testSchemaUsingValidXml()
+    {
+        $this->markTestSkipped('shema redefine problem');
+        parent::testSchemaUsingValidXml();
+    }
+}
