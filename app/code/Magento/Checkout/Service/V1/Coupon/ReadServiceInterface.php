4928ee99a3a669f369ce8b89d40ccbfc683d76c7
Iryna Lagno <ilagno@ebay.com>
2014-08-04 18:05:30 +0300

    MAGETWO-26929: ShoppingCartCoupon get service

---
+++ app/code/Magento/Checkout/Service/V1/Coupon/ReadServiceInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Checkout\Service\V1\Coupon;
+
+interface ReadServiceInterface
+{
+    /**
+     * Retrieve information about coupon in cart
+     *
+     * @param int $cartId
+     * @return \Magento\Checkout\Service\V1\Data\Cart\Coupon[]
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function get($cartId);
+}
---
+++ app/code/Magento/Checkout/Service/V1/Data/Cart/Coupon.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Checkout\Service\V1\Data\Cart;
+
+/**
+ * @codeCoverageIgnore
+ */
+class Coupon extends \Magento\Framework\Service\Data\AbstractObject
+{
+    /**#@+
+     * Constants defined for keys of array
+     */
+    const COUPON_CODE = 'coupon_code';
+
+    /**
+     * @return string|null
+     */
+    public function getCouponCode()
+    {
+        return $this->_get(self::COUPON_CODE);
+    }
+}
