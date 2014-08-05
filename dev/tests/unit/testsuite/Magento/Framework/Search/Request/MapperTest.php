2f690e74c167f6307bc11953932c60e44d3c7fdf
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-08-05 17:57:25 +0300

    MAGETWO-26062 (4) Create Filter object structure from XML declaration
    MAGETWO-27061: Improve test coverage
     - Implemented tests for match and bool queries

---
+++ dev/tests/unit/testsuite/Magento/Framework/Search/Request/MapperTest.php
@@ -0,0 +1,240 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Framework\Search\Request;
+
+use Magento\TestFramework\Helper\ObjectManager;
+
+class MapperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\TestFramework\Helper\ObjectManager
+     */
+    private $helper;
+
+    /**
+     * @var \Magento\Framework\ObjectManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $objectManager;
+
+    /**
+     * @var \Magento\Framework\Search\Request\Query\Match|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $queryMatch;
+
+    /**
+     * @var \Magento\Framework\Search\Request\Query\Bool|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $queryBool;
+
+    protected function setUp()
+    {
+        $this->helper = new ObjectManager($this);
+
+        $this->objectManager = $this->getMockBuilder('Magento\Framework\ObjectManager')
+            ->setMethods(['create', 'get', 'configure'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->queryMatch = $this->getMockBuilder('Magento\Framework\Search\Request\Query\Match')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->queryBool = $this->getMockBuilder('Magento\Framework\Search\Request\Query\Bool')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @param $queries
+     * @dataProvider getQueryMatchProvider
+     */
+    public function testGetQueryMatch($queries)
+    {
+        $query = $queries['someQuery'];
+        /** @var \Magento\Framework\Search\Request\Mapper $mapper */
+        $mapper = $this->helper->getObject(
+            'Magento\Framework\Search\Request\Mapper',
+            [
+                'objectManager' => $this->objectManager,
+                'queries' => $queries,
+                'filters' => []
+            ]
+        );
+
+        $this->objectManager->expects($this->once())->method('create')
+            ->with(
+                $this->equalTo('Magento\Framework\Search\Request\Query\Match'),
+                $this->equalTo(
+                    [
+                        'name' => $query['name'],
+                        'boost' => isset($query['boost']) ? $query['boost'] : 1,
+                        'matches' => $query['match']
+                    ]
+                )
+            )
+            ->will($this->returnValue($this->queryMatch));
+
+        $this->assertEquals($this->queryMatch, $mapper->get('someQuery'));
+    }
+
+    /**
+     * @param $queries
+     * @dataProvider getQueryBoolProvider
+     */
+    public function testGetQueryBool($queries)
+    {
+        /** @var \Magento\Framework\Search\Request\Mapper $mapper */
+        $mapper = $this->helper->getObject(
+            'Magento\Framework\Search\Request\Mapper',
+            [
+                'objectManager' => $this->objectManager,
+                'queries' => $queries,
+                'filters' => []
+            ]
+        );
+
+        $query = $queries['someQueryMatch'];
+        $this->objectManager->expects($this->at(0))->method('create')
+            ->with(
+                $this->equalTo('Magento\Framework\Search\Request\Query\Match'),
+                $this->equalTo(
+                    [
+                        'name' => $query['name'],
+                        'boost' => 1,
+                        'matches' => 'someMatches'
+                    ]
+                )
+            )
+            ->will($this->returnValue($this->queryMatch));
+        $query = $queries['someQuery'];
+        $this->objectManager->expects($this->at(1))->method('create')
+            ->with(
+                $this->equalTo('Magento\Framework\Search\Request\Query\Bool'),
+                $this->equalTo(
+                    [
+                        'name' => $query['name'],
+                        'boost' => isset($query['boost']) ? $query['boost'] : 1,
+                        'someClause' => ['someQueryMatch' => $this->queryMatch]
+                    ]
+                )
+            )
+            ->will($this->returnValue($this->queryBool));
+
+        $this->assertEquals($this->queryBool, $mapper->get('someQuery'));
+    }
+
+    /**
+     * #@expectedException \InvalidArgumentException
+     */
+    public function testGetQueryInvalidArgumentException()
+    {
+        /** @var \Magento\Framework\Search\Request\Mapper $mapper */
+        $mapper = $this->helper->getObject(
+            'Magento\Framework\Search\Request\Mapper',
+            [
+                'objectManager' => $this->objectManager,
+                'queries' => [
+                    'someQuery' => [
+                        'type' => 'invalid_type'
+                    ]
+                ],
+                'filters' => []
+            ]
+        );
+
+        $mapper->get('someQuery');
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testGetQueryException()
+    {
+        /** @var \Magento\Framework\Search\Request\Mapper $mapper */
+        $mapper = $this->helper->getObject(
+            'Magento\Framework\Search\Request\Mapper',
+            [
+                'objectManager' => $this->objectManager,
+                'queries' => [],
+                'filters' => []
+            ]
+        );
+
+        $mapper->get('someQuery');
+    }
+
+    public function getQueryMatchProvider()
+    {
+        return [
+            [
+                [
+                    'someQuery' => [
+                        'type' => QueryInterface::TYPE_MATCH,
+                        'name' => 'someName',
+                        'boost' => 3,
+                        'match' => 'someMatches'
+                    ]
+                ]
+            ],
+            [
+                [
+                    'someQuery' => [
+                        'type' => QueryInterface::TYPE_MATCH,
+                        'name' => 'someName',
+                        'match' => 'someMatches'
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    public function getQueryBoolProvider()
+    {
+        return [
+            [
+                [
+                    'someQuery' => [
+                        'type' => QueryInterface::TYPE_BOOL,
+                        'name' => 'someName',
+                        'boost' => 3,
+                        'queryReference' => [
+                            [
+                                'ref' => 'someQueryMatch',
+                                'clause' => 'someClause',
+                            ]
+                        ]
+                    ],
+                    'someQueryMatch' => [
+                        'type' => QueryInterface::TYPE_MATCH,
+                        'name' => 'someName',
+                        'match' => 'someMatches'
+                    ]
+                ]
+            ],
+            [
+                [
+                    'someQuery' => [
+                        'type' => QueryInterface::TYPE_BOOL,
+                        'name' => 'someName',
+                        'queryReference' => [
+                            [
+                                'ref' => 'someQueryMatch',
+                                'clause' => 'someClause',
+                            ]
+                        ]
+                    ],
+                    'someQueryMatch' => [
+                        'type' => QueryInterface::TYPE_MATCH,
+                        'name' => 'someName',
+                        'match' => 'someMatches'
+                    ]
+                ]
+            ],
+        ];
+    }
+}
