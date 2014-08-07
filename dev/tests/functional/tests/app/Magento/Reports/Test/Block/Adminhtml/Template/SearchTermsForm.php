92ff77a2ef4f55149050c3116d8cdfffe2d8f135
Oleksandr Rykh <orykh@ebay.com>
2014-08-07 13:53:39 +0300

    MTA-268: Test Creation for SearchTermsReportEntity

---
+++ dev/tests/functional/tests/app/Magento/Reports/Test/Block/Adminhtml/Template/SearchTermsForm.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Reports\Test\Block\Adminhtml\Template;
+
+use Magento\Backend\Test\Block\Widget\Form;
+
+/**
+ * Class SearchTermsForm
+ * Search Terms form
+ */
+class SearchTermsForm extends Form
+{
+    //
+}
