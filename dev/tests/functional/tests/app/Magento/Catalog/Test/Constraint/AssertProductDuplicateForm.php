4d02546d935ae46971c6b01b958b2228635c5e49
Vitaliy Honcharenko <vgoncharenko@ebay.com>
2014-08-07 18:04:16 +0300

    MTA-302: Test Creation for ImportCustomOptions
    - change assert AssertProductDuplicateForm

--- dev/tests/functional/tests/app/Magento/Catalog/Test/Constraint/AssertProductDuplicateForm.php
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/Constraint/AssertProductDuplicateForm.php
@@ -64,7 +64,7 @@ class AssertProductDuplicateForm extends AssertProductForm
         $productGrid->open()->getProductGrid()->searchAndOpen($filter);
 
         $formData = $productPage->getForm()->getData($product);
-        $fixtureData = $this->prepareFixtureData($product->getData());
+        $fixtureData = $this->prepareFixtureData($product->getData(), $product);
 
         $errors = $this->verifyData($fixtureData, $formData);
         \PHPUnit_Framework_Assert::assertEmpty($errors, $errors);
