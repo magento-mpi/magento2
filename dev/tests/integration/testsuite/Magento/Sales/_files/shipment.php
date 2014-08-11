8b8fa531380a45d9340074c44f96b809b1433987
Tatiana Shevchenko <tshevchenko@ebay.com>
2014-08-11 18:50:38 +0300

    MAGETWO-27211: Create service salesShipmentEmail

--- dev/tests/integration/testsuite/Magento/Sales/_files/shipment.php
+++ dev/tests/integration/testsuite/Magento/Sales/_files/shipment.php
@@ -5,40 +5,7 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-require __DIR__ . '/../../../Magento/Catalog/_files/product_simple.php';
-/** @var \Magento\Catalog\Model\Product $product */
-
-$addressData = include __DIR__ . '/address_data.php';
-
-$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
-
-$billingAddress = $objectManager->create('Magento\Sales\Model\Order\Address', ['data' => $addressData]);
-$billingAddress->setAddressType('billing');
-
-$shippingAddress = clone $billingAddress;
-$shippingAddress->setId(null)->setAddressType('shipping');
-
-$payment = $objectManager->create('Magento\Sales\Model\Order\Payment');
-$payment->setMethod('checkmo');
-
-/** @var \Magento\Sales\Model\Order\Item $orderItem */
-$orderItem = $objectManager->create('Magento\Sales\Model\Order\Item');
-$orderItem->setProductId($product->getId())->setQtyOrdered(2);
-
-/** @var \Magento\Sales\Model\Order $order */
-$order = $objectManager->create('Magento\Sales\Model\Order');
-$order->setIncrementId('100000001')
-    ->setState(\Magento\Sales\Model\Order::STATE_PROCESSING, true)
-    ->setSubtotal(100)
-    ->setBaseSubtotal(100)
-    ->setCustomerIsGuest(true)
-    ->setCustomerEmail('customer@null.com')
-    ->setBillingAddress($billingAddress)
-    ->setShippingAddress($shippingAddress)
-    ->setStoreId($objectManager->get('Magento\Store\Model\StoreManagerInterface')->getStore()->getId())
-    ->addItem($orderItem)
-    ->setPayment($payment);
-$order->save();
+require __DIR__ . '/../../../Magento/Sales/_files/order.php';
 
 $payment = $order->getPayment();
 $paymentInfoBlock = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
@@ -52,7 +19,9 @@ $shipment->setOrder($order);
 
 $packages = [['1'], ['2']];
 
-$shipment->addItem($objectManager->create('Magento\Sales\Model\Order\Shipment\Item'));
+$shipmentItem = $objectManager->create('Magento\Sales\Model\Order\Shipment\Item');
+$shipmentItem->setOrderItem($orderItem);
+$shipment->addItem($shipmentItem);
 $shipment->setPackages($packages);
 $shipment->setShipmentStatus(\Magento\Sales\Model\Order\Shipment::STATUS_NEW);
 
