96391036f94a0e9c8e40775bd3ba221f6cc36ca1
Bryant Luk <bluk@ebay.com>
2014-08-12 18:04:27 -0500

    MAGETWO-27065: Cache the results of the Customer Metadata service
    - Add caches for the Customer/Address metadata services

---
+++ app/code/Magento/Customer/Service/V1/AddressMetadataServiceCached.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Customer\Service\V1;
+
+/**
+ * Cached Address metadata service
+ */
+class AddressMetadataServiceCached implements AddressMetadataServiceInterface
+{
+    /**
+     * @var AddressMetadataService
+     */
+    private $metadataService;
+
+    /**
+     * @var array
+     */
+    private $attributeMetadataCache = [];
+
+    /**
+     * @var array
+     */
+    private $attributesCache = [];
+
+    /**
+     * @var \Magento\Customer\Service\V1\Data\Eav\AttributeMetadata[]
+     */
+    private $allAttributeMetadataCache = null;
+
+    /**
+     * @var \Magento\Customer\Service\V1\Data\Eav\AttributeMetadata[]
+     */
+    private $customAttributesMetadataCache = null;
+
+    const CACHE_SEPARATOR = ';';
+
+    /**
+     * @param AddressMetadataServiceInterface $metadataService
+     */
+    public function __construct(
+        AddressMetadataServiceInterface $metadataService
+    ) {
+        $this->metadataService = $metadataService;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAttributes($formCode)
+    {
+        $key = $formCode;
+        if (isset($this->attributesCache[$key])) {
+            return $this->attributesCache[$key];
+        }
+
+        $value = $this->metadataService->getAttributes($formCode);
+        $this->attributesCache[$key] = $value;
+
+        return $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAttributeMetadata($attributeCode)
+    {
+        $key = $attributeCode;
+        if (isset($this->attributeMetadataCache[$key])) {
+            return $this->attributeMetadataCache[$key];
+        }
+
+        $value = $this->metadataService->getAttributeMetadata($attributeCode);
+        $this->attributeMetadataCache[$key] = $value;
+
+        return $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAllAttributesMetadata()
+    {
+        if (!is_null($this->allAttributeMetadataCache)) {
+            return $this->allAttributeMetadataCache;
+        }
+
+        $this->allAttributeMetadataCache = $this->metadataService->getAllAttributesMetadata();
+        return $this->allAttributeMetadataCache;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCustomAttributesMetadata()
+    {
+        if (!is_null($this->customAttributesMetadataCache)) {
+            return $this->customAttributesMetadataCache;
+        }
+
+        $this->customAttributesMetadataCache = $this->metadataService->getCustomAttributesMetadata();
+        return $this->customAttributesMetadataCache;
+    }
+}
