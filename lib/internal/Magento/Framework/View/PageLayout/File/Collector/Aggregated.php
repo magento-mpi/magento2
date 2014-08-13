f7801bdb329c1c628747fda12cbec5e3525bf208
Maxim Shikula <mshikula@ebay.com>
2014-08-13 10:29:38 +0300

    MAGETWO-26550: Page layout functionality is supported in backend

--- lib/internal/Magento/Framework/View/PageLayout/File/Collector/Aggregated.php
+++ lib/internal/Magento/Framework/View/PageLayout/File/Collector/Aggregated.php
@@ -8,93 +8,13 @@
 
 namespace Magento\Framework\View\PageLayout\File\Collector;
 
-use Magento\Framework\View\File\CollectorInterface;
 use Magento\Framework\View\Design\ThemeInterface;
-use Magento\Framework\View\File\FileList\Factory;
 
 /**
  * Source of layout files aggregated from a theme and its parents according to merging and overriding conventions
  */
-class Aggregated implements CollectorInterface
+class Aggregated extends \Magento\Framework\View\Layout\File\Collector\Aggregated
 {
-    /**
-     * File list factory
-     *
-     * @var Factory
-     */
-    protected $fileListFactory;
-
-    /**
-     * Base files
-     *
-     * @var CollectorInterface
-     */
-    protected $baseFiles;
-
-    /**
-     * Theme files
-     *
-     * @var CollectorInterface
-     */
-    protected $themeFiles;
-
-    /**
-     * Overridden base files
-     *
-     * @var CollectorInterface
-     */
-    protected $overrideBaseFiles;
-
-    /**
-     * Overridden theme files
-     *
-     * @var \Magento\Framework\View\File\CollectorInterface
-     */
-    protected $overrideThemeFiles;
-
-    /**
-     * @param Factory $fileListFactory
-     * @param CollectorInterface $baseFiles
-     * @param CollectorInterface $themeFiles
-     * @param CollectorInterface $overrideBaseFiles
-     * @param CollectorInterface $overrideThemeFiles
-     */
-    public function __construct(
-        Factory $fileListFactory,
-        CollectorInterface $baseFiles,
-        CollectorInterface $themeFiles,
-        CollectorInterface $overrideBaseFiles,
-        CollectorInterface $overrideThemeFiles
-    ) {
-        $this->fileListFactory = $fileListFactory;
-        $this->baseFiles = $baseFiles;
-        $this->themeFiles = $themeFiles;
-        $this->overrideBaseFiles = $overrideBaseFiles;
-        $this->overrideThemeFiles = $overrideThemeFiles;
-    }
-
-    /**
-     * Retrieve files
-     *
-     * Aggregate layout files from modules and a theme and its ancestors
-     *
-     * @param ThemeInterface $theme
-     * @param string $filePath
-     * @return \Magento\Framework\View\File[]
-     */
-    public function getFiles(ThemeInterface $theme, $filePath)
-    {
-        $list = $this->fileListFactory->create();
-        $list->add($this->baseFiles->getFiles($theme, $filePath));
-
-        foreach ($theme->getInheritedThemes() as $currentTheme) {
-            $list->add($this->themeFiles->getFiles($currentTheme, $filePath));
-            $list->replace($this->overrideBaseFiles->getFiles($currentTheme, $filePath));
-            $list->replace($this->overrideThemeFiles->getFiles($currentTheme, $filePath));
-        }
-        return $list->getAll();
-    }
-
     /**
      * @param ThemeInterface $theme
      * @param string $filePath
