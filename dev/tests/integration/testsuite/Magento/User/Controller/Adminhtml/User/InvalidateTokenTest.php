c7f0a97cec3448b0f40e5928505c8ab27e83575a
Cari Spruiell <cspruiell@ebay.com>
2014-08-14 11:09:03 -0500

    MAGETWO-26312 Add code to invalidate all of an admin's tokens
    Modified TokenService with a method to revoke an admin's tokens.
    Updated controller with implementation
    Added integration tests

---
+++ dev/tests/integration/testsuite/Magento/User/Controller/Adminhtml/User/InvalidateTokenTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\User\Controller\Adminhtml\User;
+
+use Magento\Framework\Message\MessageInterface;
+
+/**
+ * Test class for Magento\User\Controller\Adminhtml\User\InvalidateToken.
+ */
+class InvalidateTokenTest extends \Magento\Backend\Utility\Controller
+{
+    /**
+     * @magentoDataFixture Magento/User/_files/user_with_role.php
+     */
+    public function testInvalidateToken()
+    {
+        $tokenService = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Integration\Service\V1\TokenService');
+        $tokenModel = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Integration\Model\Oauth\Token');
+        $userModel = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\User\Model\User');
+
+        $adminUserNameFromFixture = 'adminUser';
+        $accessToken = $tokenService->createAdminAccessToken(
+            $adminUserNameFromFixture,
+            \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD
+        );
+        $adminUserId = $userModel->loadByUsername($adminUserNameFromFixture)->getId();
+        /** @var $token TokenModel */
+        $token = $tokenModel->loadByAdminId($adminUserId);
+        $this->assertEquals($accessToken, $token->getToken());
+
+        // invalidate token
+        $this->getRequest()->setParam('user_id', $adminUserId);
+        $this->dispatch('backend/admin/user/invalidateToken');
+        $token = $tokenModel->loadByAdminId($adminUserId);
+        $this->assertEquals(1, $token->getRevoked());
+    }
+
+    /**
+     * @magentoDataFixture Magento/User/_files/user_with_role.php
+     */
+    public function testInvalidateToken_NoTokens()
+    {
+        $userModel = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\User\Model\User');
+        $adminUserNameFromFixture = 'adminUser';
+        $adminUserId = $userModel->loadByUsername($adminUserNameFromFixture)->getId();
+        // invalidate token
+        $this->getRequest()->setParam('user_id', $adminUserId);
+        $this->dispatch('backend/admin/user/invalidateToken');
+        $this->assertSessionMessages(
+            $this->equalTo(['This user has no tokens to invalidate.']),
+            MessageInterface::TYPE_ERROR
+        );
+    }
+
+    public function testInvalidateToken_NoUser()
+    {
+        $this->dispatch('backend/admin/user/invalidateToken');
+        $this->assertSessionMessages(
+            $this->equalTo(['We can\'t find a user to invalidate.']),
+            MessageInterface::TYPE_ERROR
+        );
+    }
+
+    public function testInvalidateToken_InvalidUser()
+    {
+        $adminUserId = 999;
+        // invalidate token
+        $this->getRequest()->setParam('user_id', $adminUserId);
+        $this->dispatch('backend/admin/user/invalidateToken');
+        $this->assertSessionMessages(
+            $this->equalTo(['This user has no tokens to invalidate.']),
+            MessageInterface::TYPE_ERROR
+        );
+    }
+}
