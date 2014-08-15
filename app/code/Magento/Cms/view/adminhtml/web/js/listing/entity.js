8dee998426def99fe87ea6b580cf115f8a6bd91d
Mykhailo Miroshnikov <mmiroshnikov@ebay.com>
2014-08-15 14:29:10 +0300

    MAGETWO-26382: Implement JavaScript Grid Component
     - Moved non-common grid functionality out from m2/listing

--- app/code/Magento/Cms/view/adminhtml/web/js/listing/entity.js
+++ app/code/Magento/Cms/view/adminhtml/web/js/listing/entity.js
@@ -1,11 +1,18 @@
 define(['m2/listing'], function (Listing) {
   return Listing.extend({
-    initialize: function () {
-      Listing.prototype.initialize.apply(this, arguments);
+
+    getSortableClassFor: function (heading) {
+      var rule = heading.sorted;
+      return rule ? 'sort-arrow-' + rule : 'not-sorted';
+    },
+
+    getTextFor: function (status) {
+      return t(status ? 'Enabled' : 'Disabled'); 
     },
 
     updateAttributes: function () {
       alert('You want to update items with ids: ' + this.checkedIds());
     }
+    
   });
 });
