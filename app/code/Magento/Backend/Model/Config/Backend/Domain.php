f8379f174f7817d997f8f81cc0c5b57aa75d3e44
Joan He <johe@ebay.com>
2014-08-15 12:46:50 -0500

    MAGETWO-26000: fixed site crashes when set invalid cookie domain

---
+++ app/code/Magento/Backend/Model/Config/Backend/Domain.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Backend\Model\Config\Backend;
+
+/**
+ * Backend model for domain config value
+ */
+class Domain extends \Magento\Framework\App\Config\Value
+{
+    /**
+     * Validate a domain name value
+     *
+     * @return void
+     * @throws \Magento\Framework\Model\Exception
+     */
+    protected function _beforeSave()
+    {
+        $value = $this->getValue();
+
+        $validator = new \Zend\Validator\Hostname(\Zend\Validator\Hostname::ALLOW_ALL);
+
+        if (!empty($value) && !$validator->isValid($value)) {
+            throw new \Magento\Framework\Model\Exception(
+                'Invalid domain name: ' . join('; ', $validator->getMessages())
+            );
+        }
+    }
+}
