93a9a8bec1538c0bf78dbaa6c76c9b12b49a2279
Jay <japatel@paypal.com>
2014-08-15 18:38:23 -0500

    MAGETWO-27336: Fix Setup UI data update
    - Removed unused code

--- setup/module/Magento/Setup/src/Controller/Install/StartController.php
+++ setup/module/Magento/Setup/src/Controller/Install/StartController.php
@@ -173,20 +173,4 @@ class StartController extends AbstractActionController
         return $this->json;
     }
 
-    private function execVerbose($command)
-    {
-        $args = func_get_args();
-        $args = array_map('escapeshellarg', $args);
-        $args[0] = $command;
-        $command = call_user_func_array('sprintf', $args);
-        echo $command . PHP_EOL;
-        exec($command, $output, $exitCode);
-        foreach ($output as $line) {
-            echo $line . PHP_EOL;
-        }
-        if (0 !== $exitCode) {
-            throw new Exception("Command has failed with exit code: $exitCode.");
-        }
-        return $output;
-    }
 }
