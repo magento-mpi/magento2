d9052bca89832481925c220feca2d678624820d9
Cari Spruiell <cspruiell@ebay.com>
2014-08-18 14:06:45 -0500

    MAGETWO-26311 Add code to invalidate all of a customer's tokens
    Correct unit tests

--- dev/tests/unit/testsuite/Magento/Integration/Service/V1/TokenServiceTest.php
+++ dev/tests/unit/testsuite/Magento/Integration/Service/V1/TokenServiceTest.php
@@ -27,6 +27,12 @@ class TokenServiceTest extends \PHPUnit_Framework_TestCase
     /** \Magento\Customer\Service\V1\CustomerAccountServiceInterface|\PHPUnit_Framework_MockObject_MockObject */
     protected $_customerAccountServiceMock;
 
+    /** \Magento\Integration\Model\Resource\Oauth\Token\Collection|\PHPUnit_Framework_MockObject_MockObject */
+    protected $_tokenModelCollectionMock;
+
+    /** \Magento\Integration\Model\Resource\Oauth\Token\CollectionFactory|\PHPUnit_Framework_MockObject_MockObject */
+    protected $_tokenModelCollectionFactoryMock;
+
     /**
      * @var \Magento\Integration\Model\Oauth\Token|\PHPUnit_Framework_MockObject_MockObject
      */
@@ -39,48 +45,64 @@ class TokenServiceTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-
         $this->_userModelMock = $this->getMockBuilder('Magento\User\Model\User')
             ->disableOriginalConstructor()
             ->getMock();
+
         $this->_customerAccountServiceMock = $this
             ->getMockBuilder('Magento\Customer\Service\V1\CustomerAccountServiceInterface')
             ->disableOriginalConstructor()
             ->getMock();
+
         $this->_tokenMock = $this->getMockBuilder('Magento\Integration\Model\Oauth\Token')
             ->disableOriginalConstructor()
             ->setMethods(['getToken', 'loadByCustomerId', 'setRevoked', 'save', '__wakeup'])->getMock();
 
-        $this->_tokenModelFactoryMock->expects($this->once())
+        $this->_tokenModelCollectionMock = $this->getMockBuilder('Magento\Integration\Model\Resource\Oauth\Token\Collection')
+            ->disableOriginalConstructor()
+            ->setMethods(['addFilterByCustomerId', 'getSize', '__wakeup', '_beforeLoad', '_afterLoad', 'getIterator'])->getMock();
+
+        $this->_tokenModelCollectionFactoryMock = $this->getMockBuilder('Magento\Integration\Model\Resource\Oauth\Token\CollectionFactory')
+            ->setMethods(['create'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->_tokenModelCollectionFactoryMock->expects($this->once())
             ->method('create')
-            ->will($this->returnValue($this->_tokenMock));
+            ->will($this->returnValue($this->_tokenModelCollectionMock));
 
         $this->_tokenService = new \Magento\Integration\Service\V1\TokenService(
             $this->_tokenModelFactoryMock,
             $this->_userModelMock,
-            $this->_customerAccountServiceMock
+            $this->_customerAccountServiceMock,
+            $this->_tokenModelCollectionFactoryMock
         );
+
     }
 
     public function testRevokeCustomerAccessToken()
     {
         $customerId = 1;
 
-        $this->_tokenMock->expects($this->once())
-            ->method('getToken')
-            ->will($this->returnValue('test'));
-        $this->_tokenMock->expects($this->once())
-            ->method('loadByCustomerId')
-            ->will($this->returnValue($this->_tokenMock));
-        $this->_tokenMock->expects($this->once())
-            ->method('save');
+        $this->_tokenModelCollectionMock->expects($this->once())
+            ->method('addFilterByCustomerId')
+            ->with($customerId)
+            ->will($this->returnValue($this->_tokenModelCollectionMock));
+        $this->_tokenModelCollectionMock->expects($this->any())
+            ->method('getSize')
+            ->will($this->returnValue(1));
+        $this->_tokenModelCollectionMock->expects($this->once())
+            ->method('getIterator')
+            ->will($this->returnValue(new \ArrayIterator([$this->_tokenMock])));
+        $this->_tokenModelCollectionMock->expects($this->any())
+            ->method('_fetchAll')
+            ->with(null)
+            ->will($this->returnValue(1));
         $this->_tokenMock->expects($this->once())
             ->method('setRevoked')
             ->will($this->returnValue($this->_tokenMock));
         $this->_tokenMock->expects($this->once())
-            ->method('loadByCustomerId')
-            ->with($customerId)
-            ->will($this->returnValue($this->_tokenService));
+            ->method('save');
+
         $this->assertTrue($this->_tokenService->revokeCustomerAccessToken($customerId));
     }
 
@@ -90,12 +112,10 @@ class TokenServiceTest extends \PHPUnit_Framework_TestCase
      */
     public function testRevokeCustomerAccessTokenWithoutCustomerId()
     {
-        $this->_tokenMock->expects($this->any())
-            ->method('getToken')
-            ->will($this->returnValue(''));
-        $this->_tokenMock->expects($this->once())
-            ->method('loadByCustomerId')
-            ->will($this->returnValue($this->_tokenMock));
+        $this->_tokenModelCollectionMock->expects($this->once())
+            ->method('addFilterByCustomerId')
+            ->with(null)
+            ->will($this->returnValue($this->_tokenModelCollectionMock));
         $this->_tokenMock->expects($this->never())
             ->method('save');
         $this->_tokenMock->expects($this->never())
@@ -112,12 +132,17 @@ class TokenServiceTest extends \PHPUnit_Framework_TestCase
     {
         $exception = new \Exception();
         $customerId = 1;
-        $this->_tokenMock->expects($this->any())
-            ->method('getToken')
-            ->will($this->returnValue('test'));
-        $this->_tokenMock->expects($this->once())
-            ->method('loadByCustomerId')
-            ->will($this->returnValue($this->_tokenMock));
+        $this->_tokenModelCollectionMock->expects($this->once())
+            ->method('addFilterByCustomerId')
+            ->with($customerId)
+            ->will($this->returnValue($this->_tokenModelCollectionMock));
+        $this->_tokenModelCollectionMock->expects($this->once())
+            ->method('getSize')
+            ->will($this->returnValue(1));
+        $this->_tokenModelCollectionMock->expects($this->once())
+            ->method('getIterator')
+            ->will($this->returnValue(new \ArrayIterator([$this->_tokenMock])));
+
         $this->_tokenMock->expects($this->never())
             ->method('save');
         $this->_tokenMock->expects($this->once())
