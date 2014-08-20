18c35c3f1b889a1c9c8a9dbd0487b6f1882fe85e
Denys Rudchenko <drudchenko@ebay.com>
2014-08-20 16:16:11 +0300

    MAGETWO-26150: Modularity of ImportExport for Tax
    - MAGETWO-27531: Move blocks, layouts and templates.

--- app/code/Magento/Tax/Controller/Adminhtml/Rate/ImportExport.php
+++ app/code/Magento/Tax/Controller/Adminhtml/Rate/ImportExport.php
@@ -25,9 +25,9 @@ class ImportExport extends \Magento\Tax\Controller\Adminhtml\Rate
         $this->_setActiveMenu(
             'Magento_Tax::system_convert_tax'
         )->_addContent(
-            $this->_view->getLayout()->createBlock('Magento\Tax\Block\Adminhtml\Rate\ImportExportHeader')
+            $this->_view->getLayout()->createBlock('Magento\TaxImportExport\Block\Adminhtml\Rate\ImportExportHeader')
         )->_addContent(
-            $this->_view->getLayout()->createBlock('Magento\Tax\Block\Adminhtml\Rate\ImportExport')
+            $this->_view->getLayout()->createBlock('Magento\TaxImportExport\Block\Adminhtml\Rate\ImportExport')
         );
         $this->_view->renderLayout();
     }
