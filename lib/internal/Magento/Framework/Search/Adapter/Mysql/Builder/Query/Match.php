e2a34695f284406f25af69002495bacee2a082de
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-08-21 19:11:51 +0300

    MAGETWO-27572: Implement Match query builder      - Changed query type from natural language to boolean mode

--- lib/internal/Magento/Framework/Search/Adapter/Mysql/Builder/Query/Match.php
+++ lib/internal/Magento/Framework/Search/Adapter/Mysql/Builder/Query/Match.php
@@ -7,8 +7,11 @@
  */
 namespace Magento\Framework\Search\Adapter\Mysql\Builder\Query;
 
+use Magento\Framework\DB\Select;
 use Magento\Framework\Search\Adapter\Mysql\QueryInterface;
 use Magento\Framework\Search\Adapter\Mysql\ScoreManager;
+use Magento\Framework\Search\Request\Query\Bool;
+use Magento\Framework\Search\Request\QueryInterface as RequestQueryInterface;
 
 class Match implements QueryInterface
 {
@@ -29,20 +32,25 @@ class Match implements QueryInterface
      * {@inheritdoc}
      */
     public function buildQuery(
-        \Magento\Framework\DB\Select $select,
-        \Magento\Framework\Search\Request\QueryInterface $query,
+        Select $select,
+        RequestQueryInterface $query,
         $queryType
     ) {
         $this->scoreManager->setQueryBoost($query->getName(), $query->getBoost());
 
         /** @var $query \Magento\Framework\Search\Request\Query\Match */
         foreach ($query->getMatches() as $match) {
+            if ($queryType === Bool::QUERY_TYPE_NOT) {
+                $match['value'] = '-' . $match['value'];
+            }
+
             $this->scoreManager->addCondition(
                 $query->getName(),
                 $select->getMatchQuery($match['field'], $match['value']),
-                isset($match['boost']) ? $match['boost'] : 1
+                isset($match['boost']) ? $match['boost'] : 1,
+                Select::FULLTEXT_MODE_BOOLEAN
             );
-            $select->match($match['field'], $match['value'], true);
+            $select->match($match['field'], $match['value'], true, Select::FULLTEXT_MODE_BOOLEAN);
         }
 
         return $select;
