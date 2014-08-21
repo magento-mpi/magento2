8d7be39ba45e9705fe091eb7acd9afa9a6e79d1e
Iryna Lagno <ilagno@ebay.com>
2014-08-21 12:22:48 +0300

    MAGETWO-27583:Cart Item service should use itemId instead itemSku

---
+++ dev/tests/integration/testsuite/Magento/Checkout/_files/quote_with_items_saved_rollback.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+$registry = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Framework\Registry');
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
+$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+$quote = $objectManager->create('Magento\Sales\Model\Quote');
+$quote->load('test_order_item_with_items', 'reserved_order_id');
+$message = $objectManager->create('\Magento\GiftMessage\Model\Message');
+$product = $objectManager->create('Magento\Catalog\Model\Product');
+foreach ($quote->getAllItems() as $item) {
+    $sku = $item->getSku();
+    $product->load($product->getIdBySku($sku));
+    if ($product->getId()) {
+        $product->delete();
+    }
+};
+$quote->delete();
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', false);
