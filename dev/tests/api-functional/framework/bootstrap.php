8283ace7acbbff5244402c6e60646f1f9457487f
Olga Kopylova <okopylova@ebay.com>
2014-08-22 09:44:35 -0500

    MAGETWO-27425: Failing Uninstall on CI Builds
    - returned installation via script, so the application in base dir is installed

--- dev/tests/api-functional/framework/bootstrap.php
+++ dev/tests/api-functional/framework/bootstrap.php
@@ -39,4 +39,34 @@ $bootstrap->runBootstrap();
 \Magento\TestFramework\Helper\Bootstrap::setInstance(new \Magento\TestFramework\Helper\Bootstrap($bootstrap));
 \Magento\TestFramework\Utility\Files::setInstance(new \Magento\TestFramework\Utility\Files($magentoBaseDir));
 
-unset($bootstrap);
+/** Magento installation */
+if (defined('TESTS_MAGENTO_INSTALLATION') && TESTS_MAGENTO_INSTALLATION === 'enabled') {
+    if (defined('TESTS_CLEANUP') && TESTS_CLEANUP === 'enabled') {
+        $unInstallCmd = sprintf('php -f %s --', escapeshellarg(realpath($testsBaseDir . '/../../shell/uninstall.php')));
+        passthru($unInstallCmd, $exitCode);
+        if ($exitCode) {
+            exit($exitCode);
+        }
+        echo $unInstallCmd . "\n";
+    }
+    $installConfigFile = $testsBaseDir . '/config/install.php';
+    $installConfigFile = file_exists($installConfigFile) ? $installConfigFile : "{$installConfigFile}.dist";
+    $installConfig = require $installConfigFile;
+    $installOptions = isset($installConfig['install_options']) ? $installConfig['install_options'] : array();
+
+    /* Install application */
+    if ($installOptions) {
+        $installCmd = sprintf('php -f %s --', escapeshellarg(realpath($testsBaseDir . '/../../shell/install.php')));
+        foreach ($installOptions as $optionName => $optionValue) {
+            $installCmd .= sprintf(' --%s %s', $optionName, escapeshellarg($optionValue));
+        }
+        echo $installCmd . "\n";
+        passthru($installCmd, $exitCode);
+        if ($exitCode) {
+            exit($exitCode);
+        }
+    }
+}
+
+/* Unset declared global variables to release PHPUnit from maintaining their values between tests */
+unset($bootstrap, $installCmd, $installConfigFile, $installConfig, $installExitCode);
