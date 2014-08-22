2c410c82a74b834cde7c8ee462056f0d45879c11
Cari Spruiell <cspruiell@ebay.com>
2014-08-22 17:10:10 -0500

    MAGETWO-27272 Add BundleProductSaveProcessor
    Update formatting

--- app/code/Magento/Bundle/Service/V1/Product/BundleProductSaveProcessor.php
+++ app/code/Magento/Bundle/Service/V1/Product/BundleProductSaveProcessor.php
@@ -21,214 +21,204 @@ use Magento\Catalog\Model\Product\Type as ProductType;
  */
 class BundleProductSaveProcessor implements \Magento\Catalog\Service\V1\Product\ProductSaveProcessorInterface
 {
-    /**
-     * @var LinkWriteService
-     */
-    private $linkWriteService;
+	/**
+	 * @var LinkWriteService
+	 */
+	private $linkWriteService;
 
-    /**
-     * @var OptionWriteService
-     */
-    private $optionWriteService;
+	/**
+	 * @var OptionWriteService
+	 */
+	private $optionWriteService;
 
-    /**
-     * @var LinkReadService
-     */
-    private $linkReadService;
+	/**
+	 * @var LinkReadService
+	 */
+	private $linkReadService;
 
-    /**
-     * @var OptionReadService
-     */
-    private $optionReadService;
+	/**
+	 * @var OptionReadService
+	 */
+	private $optionReadService;
 
-    /**
-     * @var ProductRepository
-     */
-    private $productRepository;
+	/**
+	 * @var ProductRepository
+	 */
+	private $productRepository;
 
-    /**
-     * @param LinkWriteService $linkWriteService
-     * @param OptionWriteService $optionWriteService
-     * @param LinkReadService $linkReadService
-     * @param OptionReadService $optionReadService
-     * @param ProductRepository $productRepository
-     */
-    public function __construct(
-        LinkWriteService $linkWriteService,
-        OptionWriteService $optionWriteService,
-        LinkReadService $linkReadService,
-        OptionReadService $optionReadService,
-        ProductRepository $productRepository)
-    {
-        $this->linkWriteService = $linkWriteService;
-        $this->optionWriteService = $optionWriteService;
-        $this->linkReadService = $linkReadService;
-        $this->optionReadService = $optionReadService;
-        $this->productRepository = $productRepository;
-    }
+	/**
+	 * @param LinkWriteService $linkWriteService
+	 * @param OptionWriteService $optionWriteService
+	 * @param LinkReadService $linkReadService
+	 * @param OptionReadService $optionReadService
+	 * @param ProductRepository $productRepository
+	 */
+	public function __construct(
+		LinkWriteService $linkWriteService,
+		OptionWriteService $optionWriteService,
+		LinkReadService $linkReadService,
+		OptionReadService $optionReadService,
+		ProductRepository $productRepository)
+	{
+		$this->linkWriteService = $linkWriteService;
+		$this->optionWriteService = $optionWriteService;
+		$this->linkReadService = $linkReadService;
+		$this->optionReadService = $optionReadService;
+		$this->productRepository = $productRepository;
+	}
 
-    /**
-     * Create bundle.
-     *
-     * @param \Magento\Catalog\Model\Product $product
-     * @param \Magento\Catalog\Service\V1\Data\Product $productData
-     * @return bool
-     */
-    public function create(
-        \Magento\Catalog\Model\Product $product,
-        \Magento\Catalog\Service\V1\Data\Product $productData)
-    {
-        /**
-         * @var String $productSku
-         */
-        $productSku = $productData->getSku();
-        $optionId = 'dummy';
+	/**
+	 * Create bundle.
+	 *
+	 * @param \Magento\Catalog\Model\Product $product
+	 * @param \Magento\Catalog\Service\V1\Data\Product $productData
+	 * @return bool
+	 */
+	public function create(
+		\Magento\Catalog\Model\Product $product,
+		\Magento\Catalog\Service\V1\Data\Product $productData)
+	{
+		/**
+		 * @var String $productSku
+		 */
+		$productSku = $productData->getSku();
+		$optionId = 'dummy';
 
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $bundleProductLinks
-         */
-        $bundleProductLinks = $productData->getCustomAttribute('bundle_product_links');
-        foreach ($bundleProductLinks as $link)
-        {
-           $this->linkWriteService->addChild($productSku, $link);
-        }
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $bundleProductLinks
+		 */
+		$bundleProductLinks = $productData->getCustomAttribute('bundle_product_links');
+		foreach ($bundleProductLinks as $link) {
+			$this->linkWriteService->addChild($productSku, $link);
+		}
 
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $bundleProductOptions
-         */
-        $bundleProductOptions = $productData->getCustomAttribute('bundle_product_options');
-        foreach ($bundleProductOptions as $option)
-        {
-            $this->optionWriteService->add($productSku, $option);
-        }
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $bundleProductOptions
+		 */
+		$bundleProductOptions = $productData->getCustomAttribute('bundle_product_options');
+		foreach ($bundleProductOptions as $option) {
+			$this->optionWriteService->add($productSku, $option);
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Update bundle.
-     *
-     * @param string $id
-     * @param \Magento\Catalog\Service\V1\Data\Product $updatedProduct
-     * @return bool
-     */
-    public function update($id, \Magento\Catalog\Service\V1\Data\Product $updatedProduct)
-    {
-        $existingProduct = $this->productRepository->getById($id, true);
-        if ($existingProduct->getTypeId() != ProductType::TYPE_BUNDLE) {
-            return true;
-        }
+	/**
+	 * Update bundle.
+	 *
+	 * @param string $id
+	 * @param \Magento\Catalog\Service\V1\Data\Product $updatedProduct
+	 * @return bool
+	 */
+	public function update($id, \Magento\Catalog\Service\V1\Data\Product $updatedProduct)
+	{
+		$existingProduct = $this->productRepository->getById($id, true);
+		if ($existingProduct->getTypeId() != ProductType::TYPE_BUNDLE) {
+			return true;
+		}
 
-        $productSku = $existingProduct->getSku();
-        $optionId = 'dummy';
+		$productSku = $existingProduct->getSku();
+		$optionId = 'dummy';
 
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $existingProductLinks
-         */
-        $existingProductLinks = $this->linkReadService->getChildren($id);
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $newProductLinks
-         */
-        $newProductLinks = $updatedProduct->getCustomAttribute('bundle_product_links');
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $linksToDelete
-         */
-        $linksToDelete = array_udiff($existingProductLinks, $newProductLinks, array($this, 'compareLinks'));
-        foreach ($linksToDelete as $link)
-        {
-            $this->linkWriteService->removeChild($productSku, $optionId, $link->getSku());
-        }
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $linksToAdd
-         */
-        $linksToAdd = array_udiff($newProductLinks, $existingProductLinks, array($this, 'compareLinks'));
-        foreach ($linksToAdd as $link)
-        {
-            $this->linkWriteService->addChild($productSku, $link);
-        }
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $existingProductLinks
+		 */
+		$existingProductLinks = $this->linkReadService->getChildren($id);
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $newProductLinks
+		 */
+		$newProductLinks = $updatedProduct->getCustomAttribute('bundle_product_links');
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $linksToDelete
+		 */
+		$linksToDelete = array_udiff($existingProductLinks, $newProductLinks, array($this, 'compareLinks'));
+		foreach ($linksToDelete as $link) {
+			$this->linkWriteService->removeChild($productSku, $optionId, $link->getSku());
+		}
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $linksToAdd
+		 */
+		$linksToAdd = array_udiff($newProductLinks, $existingProductLinks, array($this, 'compareLinks'));
+		foreach ($linksToAdd as $link) {
+			$this->linkWriteService->addChild($productSku, $link);
+		}
 
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $existingProductOptions
-         */
-        $existingProductOptions = $this->optionReadService->getList($productSku);
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $newProductOptions
-         */
-        $newProductOptions = $updatedProduct->getCustomAttribute('bundle_product_options');
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $optionsToDelete
-         */
-        $optionsToDelete = array_udiff($existingProductOptions, $newProductOptions, array($this, 'compareOptions'));
-        foreach ($optionsToDelete as $option)
-        {
-            $this->optionWriteService->remove($productSku, $option->getId());
-        }
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $optionsToAdd
-         */
-        $optionsToAdd = array_udiff($newProductOptions, $existingProductOptions, array($this, 'compareOptions'));
-        foreach ($optionsToAdd as $option)
-        {
-            $this->optionWriteService->add($productSku, $option);
-        }
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $existingProductOptions
+		 */
+		$existingProductOptions = $this->optionReadService->getList($productSku);
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $newProductOptions
+		 */
+		$newProductOptions = $updatedProduct->getCustomAttribute('bundle_product_options');
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $optionsToDelete
+		 */
+		$optionsToDelete = array_udiff($existingProductOptions, $newProductOptions, array($this, 'compareOptions'));
+		foreach ($optionsToDelete as $option) {
+			$this->optionWriteService->remove($productSku, $option->getId());
+		}
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $optionsToAdd
+		 */
+		$optionsToAdd = array_udiff($newProductOptions, $existingProductOptions, array($this, 'compareOptions'));
+		foreach ($optionsToAdd as $option) {
+			$this->optionWriteService->add($productSku, $option);
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Delete bundle.
-     *
-     * @param \Magento\Catalog\Service\V1\Data\Product $product
-     * @return bool
-     */
-    public function delete(\Magento\Catalog\Service\V1\Data\Product $product)
-    {
-        /**
-         * @var String $productSku
-         */
-        $productSku = $product->getSku();
-        $optionId = 'dummy';
+	/**
+	 * Delete bundle.
+	 *
+	 * @param \Magento\Catalog\Service\V1\Data\Product $product
+	 * @return bool
+	 */
+	public function delete(\Magento\Catalog\Service\V1\Data\Product $product)
+	{
+		/**
+		 * @var String $productSku
+		 */
+		$productSku = $product->getSku();
+		$optionId = 'dummy';
 
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $bundleProductLinks
-         */
-        $bundleProductLinks = $product->getCustomAttribute('bundle_product_links');
-        foreach ($bundleProductLinks as $link)
-        {
-            $this->linkWriteService->removeChild($productSku, $optionId, $link->getSku());
-        }
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Link\Metadata[] $bundleProductLinks
+		 */
+		$bundleProductLinks = $product->getCustomAttribute('bundle_product_links');
+		foreach ($bundleProductLinks as $link) {
+			$this->linkWriteService->removeChild($productSku, $optionId, $link->getSku());
+		}
 
-        /**
-         * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $bundleProductOptions
-         */
-        $bundleProductOptions = $product->getCustomAttribute('bundle_product_options');
-        foreach ($bundleProductOptions as $option)
-        {
-            $this->optionWriteService->remove($productSku, $option->getId());
-        }
+		/**
+		 * @var \Magento\Bundle\Service\V1\Data\Product\Option[] $bundleProductOptions
+		 */
+		$bundleProductOptions = $product->getCustomAttribute('bundle_product_options');
+		foreach ($bundleProductOptions as $option) {
+			$this->optionWriteService->remove($productSku, $option->getId());
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    function compareLinks(\Magento\Bundle\Service\V1\Data\Product\Link\Metadata $link1,
-                                  \Magento\Bundle\Service\V1\Data\Product\Link\Metadata $link2)
-    {
-        if ($link1->getSku() == $link2->getSku())
-        {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
+	function compareLinks(\Magento\Bundle\Service\V1\Data\Product\Link\Metadata $link1,
+						  \Magento\Bundle\Service\V1\Data\Product\Link\Metadata $link2)
+	{
+		if ($link1->getSku() == $link2->getSku()) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
 
-    function compareOptions(\Magento\Bundle\Service\V1\Data\Product\Option $option1,
-                                    \Magento\Bundle\Service\V1\Data\Product\Option $option2)
-    {
-        if ($option1->getId() == $option2->getId())
-        {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
+	function compareOptions(\Magento\Bundle\Service\V1\Data\Product\Option $option1,
+							\Magento\Bundle\Service\V1\Data\Product\Option $option2)
+	{
+		if ($option1->getId() == $option2->getId()) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
 }
