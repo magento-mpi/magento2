1fcece7429c12f9fd3203c9d3bd1f70814f41397
Igor Kruchymskyi <ikruchynskyi@ebay.com>
2014-08-22 18:30:45 +0300

    MTA-39: Test Creation for OnePageCheckout within offline Payment Methods
    - fixed test case annotation

--- dev/tests/functional/tests/app/Magento/Checkout/Test/TestCase/OnePageCheckoutTest.php
+++ dev/tests/functional/tests/app/Magento/Checkout/Test/TestCase/OnePageCheckoutTest.php
@@ -16,8 +16,32 @@ use Mtf\TestCase\Scenario;
 use Magento\Reward\Test\Page\Adminhtml\RewardRateIndex;
 
 /**
- * Class OnePageCheckoutTest
- * OnePageCheckout Test
+ * OnePageCheckout within offline Payment Methods
+ *
+ * Test Flow:
+ * Preconditions:
+ * 1. Configure shipping method.
+ * 2. Configure payment method.
+ * 3. Create products.
+ * 4. Create and setup customer.
+ * 5. Create gift card account according to dataset.
+ * 6. Create sales rule according to dataset.
+ *
+ * Steps:
+ * 1. Go to Frontend.
+ * 2. Add products to the cart.
+ * 3. Apply discounts in shopping cart according to dataset.
+ * 4. Click the 'Proceed to Checkout' button.
+ * 5. Select checkout method according to dataset.
+ * 6. Fill billing information and select the 'Ship to this address' option.
+ * 7. Select shipping method.
+ * 8. Select payment method (use reward points and store credit if available).
+ * 9. Verify order total on review step.
+ * 10. Place order.
+ * 11. Perform assertions.
+ *
+ * @group One_Page_Checkout_(CS)
+ * @ZephyrId MAGETWO-27485
  */
 class OnePageCheckoutTest extends Scenario
 {
