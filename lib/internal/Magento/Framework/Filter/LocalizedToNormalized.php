28ed6a021287f03211343c451cba0198c4a95ee9
Andriy Nasinnyk <anasinnyk@ebay.com>
2014-08-26 17:53:11 +0300

    MAGETWO-26213: Unit and Integration Test Coverage [South Team]

---
+++ lib/internal/Magento/Framework/Filter/LocalizedToNormalized.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright  {copyright}
+ * @license    {license_link}
+ */
+namespace Magento\Framework\Filter;
+
+class LocalizedToNormalized extends \Zend_Filter_LocalizedToNormalized
+{
+
+}
