4396dfe5acaa2a81865d1651d265e20ea2838e6c
Ka Ho Lau <kahlau@ebay.com>
2014-08-26 18:01:35 -0500

    MAGETWO-27551: View Files Deployment Tool Broken After IsInstalled Refactoring -changing accessibility from private to protected

--- lib/internal/Magento/Framework/View/Asset/Source.php
+++ lib/internal/Magento/Framework/View/Asset/Source.php
@@ -56,7 +56,7 @@ class Source
     /**
      * @var \Magento\Framework\View\Design\Theme\ListInterface
      */
-    private $themeList;
+    protected $themeList;
 
     /**
      * @param PreProcessor\Cache $cache
