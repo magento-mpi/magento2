1e0c7fee80bd2916f8f182c9feb5e420e0ce7e06
Serhii Dzhepa <sdzhepa@ebay.com>
2014-08-26 15:43:11 +0300

    MAGETWO-27376: Cover new READ services with unit tests - fixed after code review

--- dev/tests/unit/testsuite/Magento/GiftMessage/Service/V1/ReadServiceTest.php
+++ dev/tests/unit/testsuite/Magento/GiftMessage/Service/V1/ReadServiceTest.php
@@ -51,11 +51,15 @@ class ReadServiceTest extends \PHPUnit_Framework_TestCase
      */
     protected $quoteItemMock;
 
+    /**
+     * @var string
+     */
+    protected $cardId;
 
     protected function setUp()
     {
         $objectManager =new \Magento\TestFramework\Helper\ObjectManager($this);
-
+        $this->cardId = 13;
         $this->quoteRepositoryMock = $this->getMock('\Magento\Sales\Model\QuoteRepository', [], [], '', false);
         $this->messageFactoryMock = $this->getMock(
             '\Magento\GiftMessage\Model\MessageFactory',
@@ -97,7 +101,6 @@ class ReadServiceTest extends \PHPUnit_Framework_TestCase
             '',
             false);
 
-
         $this->service = $objectManager->getObject(
             'Magento\GiftMessage\Service\V1\ReadService',
             [
@@ -107,39 +110,35 @@ class ReadServiceTest extends \PHPUnit_Framework_TestCase
                 'messageMapper' => $this->messageMapperMock,
             ]
         );
+
+        $this->quoteRepositoryMock->expects($this->once())
+            ->method('get')
+            ->with($this->cardId)
+            ->will($this->returnValue($this->quoteMock));
     }
 
     public function testGetWithOutMessageId()
     {
-        $cartId = 13;
         $messageId = 0;
 
-        $this->quoteRepositoryMock->expects($this->once())
-            ->method('get')
-            ->with($cartId)
-            ->will($this->returnValue($this->quoteMock));
         $this->quoteMock->expects($this->once())->method('getGiftMessageId')->will($this->returnValue($messageId));
 
-        $this->assertNull($this->service->get($cartId));
+        $this->assertNull($this->service->get($this->cardId));
     }
 
     public function testGet()
     {
-        $cartId = 13;
         $messageId = 156;
 
-        $this->quoteRepositoryMock->expects($this->once())
-            ->method('get')
-            ->with($cartId)
-            ->will($this->returnValue($this->quoteMock));
         $this->quoteMock->expects($this->once())->method('getGiftMessageId')->will($this->returnValue($messageId));
         $this->messageFactoryMock->expects($this->once())
             ->method('create')
             ->will($this->returnValue($this->messageMock));
         $this->messageMock->expects($this->once())->method('load')->will($this->returnValue($this->messageMock));
-        $this->messageMapperMock->expects($this->once())->method('extractDto')->with($this->messageMock);
+        $this->messageMapperMock->expects($this->once())
+            ->method('extractDto')->with($this->messageMock)->will($this->returnValue(['Expected value']));
 
-        $this->service->get($cartId);
+        $this->assertEquals(['Expected value'], $this->service->get($this->cardId));
     }
 
     /**
@@ -148,47 +147,32 @@ class ReadServiceTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetItemMessageWithNoSuchEntityException()
     {
-        $cartId = 13;
         $itemId = 2;
 
-        $this->quoteRepositoryMock->expects($this->once())
-            ->method('get')
-            ->with($cartId)
-            ->will($this->returnValue($this->quoteMock));
         $this->quoteMock->expects($this->once())->method('getItemById')->with($itemId)->will($this->returnValue(null));
 
-        $this->service->getItemMessage($cartId, $itemId);
+        $this->service->getItemMessage($this->cardId, $itemId);
     }
 
     public function testGetItemMessageWithoutMessageId()
     {
-        $cartId = 13;
         $messageId = 0;
         $itemId = 2;
 
-        $this->quoteRepositoryMock->expects($this->once())
-            ->method('get')
-            ->with($cartId)
-            ->will($this->returnValue($this->quoteMock));
         $this->quoteMock->expects($this->once())
             ->method('getItemById')
             ->with($itemId)
             ->will($this->returnValue($this->quoteItemMock));
         $this->quoteItemMock->expects($this->once())->method('getGiftMessageId')->will($this->returnValue($messageId));
 
-        $this->assertNull($this->service->getItemMessage($cartId, $itemId));
+        $this->assertNull($this->service->getItemMessage($this->cardId, $itemId));
     }
 
     public function testGetItemMessage()
     {
-        $cartId = 13;
         $messageId = 123;
         $itemId = 2;
 
-        $this->quoteRepositoryMock->expects($this->once())
-            ->method('get')
-            ->with($cartId)
-            ->will($this->returnValue($this->quoteMock));
         $this->quoteMock->expects($this->once())
             ->method('getItemById')
             ->with($itemId)
@@ -201,9 +185,12 @@ class ReadServiceTest extends \PHPUnit_Framework_TestCase
             ->method('load')
             ->with($messageId)
             ->will($this->returnValue($this->messageMock));
-        $this->messageMapperMock->expects($this->once())->method('extractDto')->with($this->messageMock);
+        $this->messageMapperMock->expects($this->once())
+            ->method('extractDto')
+            ->with($this->messageMock)
+            ->will($this->returnValue(['Expected value']));
 
-        $this->assertNull($this->service->getItemMessage($cartId, $itemId));
+        $this->assertEquals(['Expected value'], $this->service->getItemMessage($this->cardId, $itemId));
     }
 }
 
