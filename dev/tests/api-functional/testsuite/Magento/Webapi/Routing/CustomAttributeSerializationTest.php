de4abd2e41160198b0bcaea5f077f90298fc150d
Anup Dugar <anup@x.com>
2014-08-27 11:17:21 -0500

    MAGETWO-27314: Enhance Webapi deserialization to process mixed (any) type for custom attributes
    - Moved utility functions to DataObjectConverter. Similar functions already exist today for SOAP. This is now available for other client like api-functional tests to use
    - Removed unused Layout dependency from REST controller
    - Added test for one level of Data Object type in custom attributes

--- dev/tests/api-functional/testsuite/Magento/Webapi/Routing/CustomAttributeSerializationTest.php
+++ dev/tests/api-functional/testsuite/Magento/Webapi/Routing/CustomAttributeSerializationTest.php
@@ -11,6 +11,8 @@
  */
 namespace Magento\Webapi\Routing;
 
+use Magento\Framework\Service\DataObjectConverter;
+use Magento\Framework\Service\EavDataObjectConverter;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestModule1\Service\V1\Entity\ItemBuilder;
 use Magento\Webapi\Model\Rest\Config as RestConfig;
@@ -51,6 +53,11 @@ class CustomAttributeSerializationTest extends \Magento\Webapi\Routing\BaseServi
     protected $customAttributeDataObjectBuilder;
 
     /**
+     * @var DataObjectConverter $dataObjectConverter
+     */
+    protected $dataObjectConverter;
+
+    /**
      * Set up custom attribute related data objects
      */
     protected function setUp()
@@ -70,15 +77,52 @@ class CustomAttributeSerializationTest extends \Magento\Webapi\Routing\BaseServi
         $this->customAttributeNestedDataObjectBuilder = Bootstrap::getObjectManager()->create(
             'Magento\TestModule1\Service\V1\Entity\CustomAttributeNestedDataObjectBuilder'
         );
+
         $this->customAttributeDataObjectBuilder = Bootstrap::getObjectManager()->create(
             'Magento\TestModule1\Service\V1\Entity\CustomAttributeDataObjectBuilder'
+        );
+
+        $this->dataObjectConverter = Bootstrap::getObjectManager()->create(
+          'Magento\Framework\Service\DataObjectConverter'
         );
     }
 
     /**
      *  Test get item with any type
      */
-    public function testNestedCustomAttributes()
+    public function testDataObjectCustomAttributes()
+    {
+        $customAttributeDataObject = $this->customAttributeDataObjectBuilder
+            ->setName('nameValue')
+            ->setCustomAttribute('custom_attribute_int', 1)
+            ->create();
+
+        $item = $this->itemBuilder
+            ->setItemId(1)
+            ->setName('testProductAnyType')
+            ->setCustomAttribute('custom_attribute_data_object', $customAttributeDataObject)
+            ->setCustomAttribute('custom_attribute_string', 'someStringValue')
+            ->create();
+
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => $this->_restResourcePath . 'itemAnyType',
+                'httpMethod' => RestConfig::HTTP_METHOD_POST
+            ],
+            'soap' => ['service' => $this->_soapService, 'operation' => $this->_soapService . 'ItemAnyType']
+        ];
+        $requestData = $item->__toArray();
+        $result = $this->_webApiCall($serviceInfo, ['item' => $requestData]);
+
+        $expectedResponse = $this->dataObjectConverter->processServiceOutput($item);
+        //\Magento\TestModule1\Service\V1\AllSoapAndRest::itemAnyType just return the input data back as response
+        $this->assertEquals($expectedResponse, $result);
+    }
+
+    /**
+     *  Test get item with any type
+     */
+    public function testNestedDataObjectCustomAttributes()
     {
         $customAttributeNestedDataObject = $this->customAttributeNestedDataObjectBuilder
             ->setName('nestedNameValue')
@@ -107,8 +151,9 @@ class CustomAttributeSerializationTest extends \Magento\Webapi\Routing\BaseServi
         $requestData = $item->__toArray();
         $result = $this->_webApiCall($serviceInfo, ['item' => $requestData]);
 
+        $expectedResponse = $this->dataObjectConverter->processServiceOutput($item);
         //\Magento\TestModule1\Service\V1\AllSoapAndRest::itemAnyType just return the input data back as response
-        $this->assertEquals($requestData, $result);
+        $this->assertEquals($expectedResponse, $result);
     }
 
 } 
--- lib/internal/Magento/Framework/Service/DataObjectConverter.php
+++ lib/internal/Magento/Framework/Service/DataObjectConverter.php
@@ -95,7 +95,6 @@ class DataObjectConverter
         return $result;
     }
 
-
     /**
      * Converts an input string from snake_case to upper CamelCase.
      *
@@ -110,5 +109,58 @@ class DataObjectConverter
             $output .= ucfirst($segment);
         }
         return $output;
+    }
+
+    /**
+     * Converts the incoming data into scalar or an array of scalars format.
+     *
+     * If the data provided is null, then an empty array is returned.  Otherwise, if the data is an object, it is
+     * assumed to be a Data Object and converted to an associative array with keys representing the properties of the
+     * Data Object.
+     * Nested Data Objects are also converted.  If the data provided is itself an array, then we iterate through the
+     * contents and convert each piece individually.
+     *
+     * @param mixed $data
+     * @return array|int|string|bool|float Scalar or array of scalars
+     */
+    public function processServiceOutput($data)
+    {
+        if (is_array($data)) {
+            $result = [];
+            foreach ($data as $datum) {
+                if ($datum instanceof AbstractObject) {
+                    $datum = $this->processDataObject($datum->__toArray());
+                }
+                $result[] = $datum;
+            }
+            return $result;
+        } else if ($data instanceof AbstractObject) {
+            return $this->processDataObject($data->__toArray());
+        } else if (is_null($data)) {
+            return [];
+        } else {
+            /** No processing is required for scalar types */
+            return $data;
+        }
+    }
+
+    /**
+     * Convert data object to array and process available custom attributes
+     *
+     * @param array $dataObjectArray
+     * @return array
+     */
+    protected function processDataObject($dataObjectArray)
+    {
+        if (isset($dataObjectArray[EavAbstractObject::CUSTOM_ATTRIBUTES_KEY])) {
+            $dataObjectArray = EavDataObjectConverter::convertCustomAttributesToSequentialArray($dataObjectArray);
+        }
+        //Check for nested custom_attributes
+        foreach ($dataObjectArray as $key => $value) {
+            if (is_array($value)) {
+                $dataObjectArray[$key] = $this->processDataObject($value);
+            }
+        }
+        return $dataObjectArray;
     }
 }
