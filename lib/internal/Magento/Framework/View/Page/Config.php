617c414d602ae39462deb434de06f0eb233e4c83
Maxim Shikula <mshikula@ebay.com>
2014-08-27 13:00:05 +0300

    MAGETWO-27407: Implement parsing of the new head structurte and its rendering

--- lib/internal/Magento/Framework/View/Page/Config.php
+++ lib/internal/Magento/Framework/View/Page/Config.php
@@ -80,6 +80,26 @@ class Config
     protected $scopeConfig;
 
     /**
+     * @var string
+     */
+    protected $faviconFile;
+
+    /**
+     * @var \Magento\Core\Helper\File\Storage\Database
+     */
+    protected $fileStorageDatabase;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @var \Magento\Framework\Filesystem\Directory\ReadInterface
+     */
+    protected $mediaDirectory;
+
+    /**
      * @var array
      */
     protected $metadata = [
@@ -92,20 +112,27 @@ class Config
     ];
 
     /**
-     * Constructor
-     *
      * @param \Magento\Framework\View\Asset\Repository $assetRepo
      * @param \Magento\Framework\View\Asset\GroupedCollection $pageAssets
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Core\Helper\File\Storage\Database $fileStorageDatabase
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\App\Filesystem $filesystem
      */
     public function __construct(
         \Magento\Framework\View\Asset\Repository $assetRepo,
         \Magento\Framework\View\Asset\GroupedCollection $pageAssets,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Core\Helper\File\Storage\Database $fileStorageDatabase,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\App\Filesystem $filesystem
     ) {
         $this->assetRepo = $assetRepo;
         $this->pageAssets = $pageAssets;
         $this->scopeConfig = $scopeConfig;
+        $this->fileStorageDatabase = $fileStorageDatabase;
+        $this->storeManager = $storeManager;
+        $this->mediaDirectory = $filesystem->getDirectoryRead(\Magento\Framework\App\Filesystem::MEDIA_DIR);
     }
 
     /**
@@ -461,5 +488,59 @@ class Config
     public function getPageLayout()
     {
         return $this->pageLayout;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFaviconFile()
+    {
+        if (!$this->faviconFile) {
+            $this->faviconFile = $this->prepareFaviconFile();
+        }
+        return $this->faviconFile;
+    }
+
+    /**
+     * @return string
+     */
+    protected function prepareFaviconFile()
+    {
+        $folderName = \Magento\Backend\Model\Config\Backend\Image\Favicon::UPLOAD_DIR;
+        $scopeConfig = $this->scopeConfig->getValue(
+            'design/head/shortcut_icon',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        $path = $folderName . '/' . $scopeConfig;
+        $faviconUrl = $this->storeManager->getStore()
+            ->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . $path;
+
+        if (!is_null($scopeConfig) && $this->checkIsFile($path)) {
+            return $faviconUrl;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDefaultFavicon()
+    {
+        return 'Magento_Theme::favicon.ico';
+    }
+
+    /**
+     * If DB file storage is on - find there, otherwise - just file_exists
+     *
+     * @param string $filename relative file path
+     * @return bool
+     */
+    protected function checkIsFile($filename)
+    {
+        if ($this->fileStorageDatabase->checkDbUsage() && !$this->mediaDirectory->isFile($filename)) {
+            $this->fileStorageDatabase->saveFileToFilesystem($filename);
+        }
+        return $this->mediaDirectory->isFile($filename);
     }
 }
