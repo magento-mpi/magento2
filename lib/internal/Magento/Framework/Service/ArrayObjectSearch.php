a567d35e82b92a5c8ddb0d605f65addd0d8de80f
msavich <msavich@ebay.com>
2014-08-27 13:08:27 -0500

    MAGETWO-27137: Wrong validation rules responce in Customer Metadata service
    Changes after CR CR-MAGETWO-12475

--- lib/internal/Magento/Framework/Service/ArrayObjectSearch.php
+++ lib/internal/Magento/Framework/Service/ArrayObjectSearch.php
@@ -5,18 +5,46 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
+
 namespace Magento\Framework\Service;
 
+/**
+ * Class to provide possibility to search for any object's property value by the name and value of another property
+ */
 class ArrayObjectSearch
 {
     /**
      * Search for the value's value by specified key's name-value pair in the object
      *
+     * Example of usage:
+     * $data = array(
+     *     0 => ValidationRuleBuilderObject('name' => 'min_text_length', 'value'=>0)
+     *     1 => ValidationRuleBuilderObject('name' => 'max_text_length', 'value'=>255)
+     *     2 => ValidationRuleBuilderObject('anyOtherName' => 'customName', 'anyOtherValue'=>'customValue')
+     * );
+     *
+     * Call:
+     * $maxDateValue = ArrayObjectSearch::getArrayElementByName(
+     *     $data,
+     *     'max_text_length'
+     * );
+     * By default function looks for `value`'s value by the `name`'s value
+     * Result: 255
+     *
+     * Call:
+     * $customValue = ArrayObjectSearch::getArrayElementByName(
+     *     $data,
+     *     'customName',   //what key value to look for
+     *     'anyOtherName', //where to look for
+     *     'anyOtherValue' //where to return from
+     * );
+     * Result: 'customValue'
+     *
      * @param object $data Object to search in
      * @param string $keyValue Value of the key property to search for
      * @param string $keyName Name of the key property to search for
      * @param string $valueName Name of the value property name
-     * @return null | mixed
+     * @return null|mixed
      */
     public static function getArrayElementByName($data, $keyValue, $keyName = 'name', $valueName = 'value')
     {
