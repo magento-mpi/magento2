d5c646a2bef46d5dfd83bb96c4d5ee1f701d8551
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-08-29 16:47:40 +0300

    MAGETWO-26125: Create MySQL Adapter Library for Match Query Types MAGETWO-27572: Implement Match query builder      -  Fixed filtered query test

--- dev/tests/unit/testsuite/Magento/Framework/Search/Adapter/Mysql/MapperTest.php
+++ dev/tests/unit/testsuite/Magento/Framework/Search/Adapter/Mysql/MapperTest.php
@@ -9,6 +9,7 @@ namespace Magento\Framework\Search\Adapter\Mysql;
 
 use Magento\Framework\App\Resource;
 use Magento\Framework\App\Resource\Config;
+use Magento\Framework\Search\Request\Query\Filter;
 use Magento\Framework\Search\Request\QueryInterface;
 use Magento\TestFramework\Helper\ObjectManager;
 
@@ -43,6 +44,16 @@ class MapperTest extends \PHPUnit_Framework_TestCase
      * @var \Magento\Framework\Search\Adapter\Mysql\Query\Builder\Match|\PHPUnit_Framework_MockObject_MockObject
      */
     private $matchQueryBuilder;
+
+    /**
+     * @var \Magento\Framework\Search\Adapter\Mysql\Filter\Builder|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $filterBuilder;
+
+    /**
+     * @var \Magento\Framework\Search\Request\FilterInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $filter;
 
     /**
      * @var Mapper
@@ -93,12 +104,22 @@ class MapperTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
+        $this->filter = $this->getMockBuilder('Magento\Framework\Search\Request\FilterInterface')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $this->filterBuilder = $this->getMockBuilder('Magento\Framework\Search\Adapter\Mysql\Filter\Builder')
+            ->setMethods(['build'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
         $this->mapper = $helper->getObject(
             'Magento\Framework\Search\Adapter\Mysql\Mapper',
             [
                 'resource' => $this->resource,
                 'scoreBuilderFactory' => $this->scoreBuilderFactory,
-                'matchQueryBuilder' => $this->matchQueryBuilder
+                'matchQueryBuilder' => $this->matchQueryBuilder,
+                'filterBuilder' => $this->filterBuilder
             ]
         );
     }
@@ -126,7 +147,12 @@ class MapperTest extends \PHPUnit_Framework_TestCase
     public function testBuildFilterQuery()
     {
         $query = $this->createFilterQuery();
+        $query->expects($this->once())->method('getReferenceType')->will($this->returnValue(Filter::REFERENCE_FILTER));
+        $query->expects($this->once())->method('getReference')->will($this->returnValue($this->filter));
+
         $this->request->expects($this->once())->method('getQuery')->will($this->returnValue($query));
+
+        $this->filterBuilder->expects($this->once())->method('build')->will($this->returnValue('(1)'));
 
         $response = $this->mapper->buildQuery($this->request);
 
@@ -138,73 +164,56 @@ class MapperTest extends \PHPUnit_Framework_TestCase
         $query = $this->createBoolQuery();
         $this->request->expects($this->once())->method('getQuery')->will($this->returnValue($query));
 
-        $this->matchQueryBuilder->expects($this->exactly(3))->method('build')
+        $this->matchQueryBuilder->expects($this->exactly(4))->method('build')
             ->will($this->returnValue($this->select));
 
-        $query->expects($this->once())
-            ->method('getMust')
-            ->will(
-                $this->returnValue(
-                    [
-                        $this->createMatchQuery(),
-                        $this->createFilterQuery(),
-                    ]
-                )
-            );
+        $matchQuery = $this->createMatchQuery();
+        $filterMatchQuery = $this->createFilterQuery();
+        $filterMatchQuery->expects($this->once())->method('getReferenceType')
+            ->will($this->returnValue(Filter::REFERENCE_QUERY));
+        $filterMatchQuery->expects($this->once())->method('getReference')->will($this->returnValue($matchQuery));
 
-        $query->expects($this->once())
-            ->method('getShould')
-            ->will(
-                $this->returnValue(
-                    [
-                        $this->createMatchQuery(),
-                        $this->createFilterQuery(),
-                    ]
-                )
-            );
+        $filterQuery = $this->createFilterQuery();
+        $filterQuery->expects($this->once())->method('getReferenceType')
+            ->will($this->returnValue(Filter::REFERENCE_FILTER));
+        $filterQuery->expects($this->once())->method('getReference')->will($this->returnValue($this->filter));
 
-        $query->expects($this->once())
-            ->method('getMustNot')
-            ->will(
-                $this->returnValue(
-                    [
-                        $this->createMatchQuery(),
-                        $this->createFilterQuery(),
-                    ]
-                )
-            );
-
-        $response = $this->mapper->buildQuery($this->request);
-
-        $this->assertEquals($this->select, $response);
-    }
-
-    public function testBuildQueryConditionInheritance()
-    {
-        // @TODO: Finish this test after creating query builder
-        $this->markTestSkipped(
-            'We need to implement at least one query builder to check which condition was passed to it'
-        );
-        $query = $this->createBoolQuery();
         $this->request->expects($this->once())->method('getQuery')->will($this->returnValue($query));
 
-        $matchQuery = $this->createMatchQuery();
-        $nestedBoolQuery = $this->createBoolQuery();
-        $nestedBoolQuery->expects($this->once())->method('getMust')->will($this->returnValue([$matchQuery]));
-        $nestedBoolQuery->expects($this->once())->method('getShould')->will($this->returnValue([]));
-        $nestedBoolQuery->expects($this->once())->method('getMustNot')->will($this->returnValue([]));
+        $this->filterBuilder->expects($this->once())->method('build')->will($this->returnValue('(1)'));
 
         $query->expects($this->once())
             ->method('getMust')
-            ->will($this->returnValue([]));
+            ->will(
+                $this->returnValue(
+                    [
+                        $this->createMatchQuery(),
+                        $this->createFilterQuery(),
+                    ]
+                )
+            );
 
         $query->expects($this->once())
             ->method('getShould')
-            ->will($this->returnValue([]));
+            ->will(
+                $this->returnValue(
+                    [
+                        $this->createMatchQuery(),
+                        $filterMatchQuery,
+                    ]
+                )
+            );
 
         $query->expects($this->once())
             ->method('getMustNot')
-            ->will($this->returnValue([$nestedBoolQuery]));
+            ->will(
+                $this->returnValue(
+                    [
+                        $this->createMatchQuery(),
+                        $filterQuery,
+                    ]
+                )
+            );
 
         $response = $this->mapper->buildQuery($this->request);
 
@@ -244,7 +253,7 @@ class MapperTest extends \PHPUnit_Framework_TestCase
     private function createFilterQuery()
     {
         $query = $this->getMockBuilder('Magento\Framework\Search\Request\Query\Filter')
-            ->setMethods(['getType'])
+            ->setMethods(['getType', 'getReferenceType', 'getReference'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
         $query->expects($this->exactly(1))
