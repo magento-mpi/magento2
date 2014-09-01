84e28a204e3c88429a098ceacfc0a92ed0d6e8be
Yaroslav Onischenko <yonischenko@ebay.com>
2014-09-01 14:08:27 +0300

    MAGETWO-26106: Make Log Module Removable
    - MAGETWO-27646: Move Last Logged In infomation to module Log

--- app/code/Magento/Customer/Block/Adminhtml/Edit/Tab/View.php
+++ app/code/Magento/Customer/Block/Adminhtml/Edit/Tab/View.php
@@ -8,23 +8,12 @@
 namespace Magento\Customer\Block\Adminhtml\Edit\Tab;
 
 use Magento\Customer\Controller\RegistryConstants;
-use Magento\Customer\Service\V1\CustomerAccountServiceInterface;
-use Magento\Customer\Service\V1\Data\AddressConverter;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 
 /**
  * Customer account form block
- *
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class View extends \Magento\Backend\Block\Template implements \Magento\Backend\Block\Widget\Tab\TabInterface
 {
-    /**
-     * @var \Magento\Customer\Service\V1\Data\Customer
-     */
-    protected $_customer;
-
     /**
      * Core registry
      *
@@ -33,80 +22,17 @@ class View extends \Magento\Backend\Block\Template implements \Magento\Backend\B
     protected $_coreRegistry;
 
     /**
-     * @var CustomerAccountServiceInterface
-     */
-    protected $_accountService;
-
-    /**
-     * @var \Magento\Customer\Service\V1\CustomerAddressServiceInterface
-     */
-    protected $_addressService;
-
-    /**
-     * @var \Magento\Customer\Service\V1\CustomerGroupServiceInterface
-     */
-    protected $_groupService;
-
-    /**
-     * @var \Magento\Customer\Service\V1\Data\CustomerBuilder
-     */
-    protected $_customerBuilder;
-
-    /**
-     * @var \Magento\Customer\Helper\Address
-     */
-    protected $_addressHelper;
-
-    /**
-     * @var \Magento\Framework\Stdlib\DateTime
-     */
-    protected $dateTime;
-
-    /**
      * @param \Magento\Backend\Block\Template\Context $context
-     * @param CustomerAccountServiceInterface $accountService
-     * @param \Magento\Customer\Service\V1\CustomerAddressServiceInterface $addressService
-     * @param \Magento\Customer\Service\V1\CustomerGroupServiceInterface $groupService
-     * @param \Magento\Customer\Service\V1\Data\CustomerBuilder $customerBuilder
-     * @param \Magento\Customer\Helper\Address $addressHelper
      * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\Stdlib\DateTime $dateTime
      * @param array $data
-     *
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
      */
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
-        CustomerAccountServiceInterface $accountService,
-        \Magento\Customer\Service\V1\CustomerAddressServiceInterface $addressService,
-        \Magento\Customer\Service\V1\CustomerGroupServiceInterface $groupService,
-        \Magento\Customer\Service\V1\Data\CustomerBuilder $customerBuilder,
-        \Magento\Customer\Helper\Address $addressHelper,
         \Magento\Framework\Registry $registry,
-        \Magento\Framework\Stdlib\DateTime $dateTime,
         array $data = array()
     ) {
         $this->_coreRegistry = $registry;
-        $this->_accountService = $accountService;
-        $this->_addressService = $addressService;
-        $this->_groupService = $groupService;
-        $this->_customerBuilder = $customerBuilder;
-        $this->_addressHelper = $addressHelper;
-        $this->dateTime = $dateTime;
         parent::__construct($context, $data);
-    }
-
-    /**
-     * @return \Magento\Customer\Service\V1\Data\Customer
-     */
-    public function getCustomer()
-    {
-        if (!$this->_customer) {
-            $this->_customer = $this->_customerBuilder->populateWithArray(
-                $this->_backendSession->getCustomerData()['account']
-            )->create();
-        }
-        return $this->_customer;
     }
 
     /**
@@ -115,153 +41,6 @@ class View extends \Magento\Backend\Block\Template implements \Magento\Backend\B
     public function getCustomerId()
     {
         return $this->_coreRegistry->registry(RegistryConstants::CURRENT_CUSTOMER_ID);
-    }
-
-    /**
-     * @param int $groupId
-     * @return \Magento\Customer\Service\V1\Data\CustomerGroup|null
-     */
-    private function getGroup($groupId)
-    {
-        try {
-            $group = $this->_groupService->getGroup($groupId);
-        } catch (NoSuchEntityException $e) {
-            $group = null;
-        }
-        return $group;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getGroupName()
-    {
-        $customer = $this->getCustomer();
-
-        if ($groupId = $customer->getId() ? $customer->getGroupId() : null) {
-            if ($group = $this->getGroup($groupId)) {
-                return $group->getCode();
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Get customer creation date
-     *
-     * @return string
-     */
-    public function getCreateDate()
-    {
-        return $this->formatDate(
-            $this->getCustomer()->getCreatedAt(),
-            TimezoneInterface::FORMAT_TYPE_MEDIUM,
-            true
-        );
-    }
-
-    /**
-     * @return string
-     */
-    public function getStoreCreateDate()
-    {
-        $date = $this->_localeDate->scopeDate(
-            $this->getCustomer()->getStoreId(),
-            $this->dateTime->toTimestamp($this->getCustomer()->getCreatedAt()),
-            true
-        );
-        return $this->formatDate($date, TimezoneInterface::FORMAT_TYPE_MEDIUM, true);
-    }
-
-    /**
-     * @return string
-     */
-    public function getStoreCreateDateTimezone()
-    {
-        return $this->_scopeConfig->getValue(
-            $this->_localeDate->getDefaultTimezonePath(),
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-            $this->getCustomer()->getStoreId()
-        );
-    }
-
-    /**
-     * @return string
-     */
-    public function getStoreLastLoginDateTimezone()
-    {
-        return $this->_scopeConfig->getValue(
-            $this->_localeDate->getDefaultTimezonePath(),
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-            $this->getCustomer()->getStoreId()
-        );
-    }
-
-    /**
-     * @return string
-     */
-    public function getIsConfirmedStatus()
-    {
-        $id = $this->getCustomerId();
-        switch ($this->_accountService->getConfirmationStatus($id)) {
-            case CustomerAccountServiceInterface::ACCOUNT_CONFIRMED:
-                return __('Confirmed');
-            case CustomerAccountServiceInterface::ACCOUNT_CONFIRMATION_REQUIRED:
-                return __('Confirmation Required');
-            case CustomerAccountServiceInterface::ACCOUNT_CONFIRMATION_NOT_REQUIRED:
-                return __('Confirmation Not Required');
-        }
-        return __('Indeterminate');
-    }
-
-    /**
-     * @return null|string
-     */
-    public function getCreatedInStore()
-    {
-        return $this->_storeManager->getStore($this->getStoreId())->getName();
-    }
-
-    /**
-     * @return int
-     */
-    public function getStoreId()
-    {
-        return $this->getCustomer()->getStoreId();
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getBillingAddressHtml()
-    {
-        try {
-            $address = $this->_addressService->getAddress($this->getCustomer()->getDefaultBilling());
-        } catch (NoSuchEntityException $e) {
-            return __('The customer does not have default billing address.');
-        }
-        return $this->_addressHelper->getFormatTypeRenderer(
-            'html'
-        )->renderArray(
-            AddressConverter::toFlatArray($address)
-        );
-    }
-
-    /**
-     * @return string
-     */
-    public function getAccordionHtml()
-    {
-        return $this->getChildHtml('accordion');
-    }
-
-    /**
-     * @return string
-     */
-    public function getSalesHtml()
-    {
-        return $this->getChildHtml('sales');
     }
 
     /**
--- dev/tests/unit/testsuite/Magento/Customer/Block/Adminhtml/Edit/Tab/ViewTest.php
+++ dev/tests/unit/testsuite/Magento/Customer/Block/Adminhtml/Edit/Tab/ViewTest.php
@@ -5,11 +5,14 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-
 namespace Magento\Customer\Block\Adminhtml\Edit\Tab;
 
 use \Magento\TestFramework\Helper\ObjectManager as ObjectManagerHelper;
 
+/**
+ * Class ViewTest
+ * @package Magento\Customer\Block\Adminhtml\Edit\Tab
+ */
 class ViewTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -17,84 +20,27 @@ class ViewTest extends \PHPUnit_Framework_TestCase
      */
     protected $view;
 
-    /**
-     * @var ObjectManagerHelper
-     */
-    protected $objectManagerHelper;
-
-    /**
-     * @var \Magento\Backend\Block\Template\Context|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $context;
-
-    /**
-     * @var \Magento\Customer\Service\V1\CustomerAccountServiceInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $customerAccountService;
-
-    /**
-     * @var \Magento\Customer\Service\V1\CustomerAddressServiceInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $customerAddressService;
-
-    /**
-     * @var \Magento\Customer\Service\V1\CustomerGroupServiceInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $customerGroupService;
-
-    /**
-     * @var \Magento\Customer\Service\V1\Data\CustomerBuilder|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $customerBuilder;
-
-    /**
-     * @var \Magento\Customer\Helper\Address|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $customerAddressHelper;
-
-    /**
-     * @var \Magento\Framework\Registry|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $registry;
-
     protected function setUp()
     {
-        $this->context = $this->getMock('Magento\Backend\Block\Template\Context', [], [], '', false);
-        $this->customerAccountService = $this->getMock('Magento\Customer\Service\V1\CustomerAccountServiceInterface');
-        $this->customerAddressService = $this->getMock('Magento\Customer\Service\V1\CustomerAddressServiceInterface');
-        $this->customerGroupService = $this->getMock('Magento\Customer\Service\V1\CustomerGroupServiceInterface');
-        $this->customerBuilder = $this->getMock('Magento\Customer\Service\V1\Data\CustomerBuilder', [], [], '', false);
-        $this->customerAddressHelper = $this->getMock('Magento\Customer\Helper\Address', [], [], '', false);
-        $this->registry = $this->getMock('Magento\Framework\Registry');
+        $customerAccountService = $this->getMock('Magento\Customer\Service\V1\CustomerAccountServiceInterface');
+        $customerAddressService = $this->getMock('Magento\Customer\Service\V1\CustomerAddressServiceInterface');
+        $customerGroupService = $this->getMock('Magento\Customer\Service\V1\CustomerGroupServiceInterface');
+        $customerBuilder = $this->getMock('Magento\Customer\Service\V1\Data\CustomerBuilder', [], [], '', false);
+        $customerAddressHelper = $this->getMock('Magento\Customer\Helper\Address', [], [], '', false);
+        $registry = $this->getMock('Magento\Framework\Registry');
 
-        $this->objectManagerHelper = new ObjectManagerHelper($this);
-        $this->view = $this->objectManagerHelper->getObject(
+        $objectManagerHelper = new ObjectManagerHelper($this);
+        $this->view = $objectManagerHelper->getObject(
             'Magento\Customer\Block\Adminhtml\Edit\Tab\View',
             [
-                'context' => $this->context,
-                'accountService' => $this->customerAccountService,
-                'addressService' => $this->customerAddressService,
-                'groupService' => $this->customerGroupService,
-                'customerBuilder' => $this->customerBuilder,
-                'addressHelper' => $this->customerAddressHelper,
-                'registry' => $this->registry
+                'accountService' => $customerAccountService,
+                'addressService' => $customerAddressService,
+                'groupService' => $customerGroupService,
+                'customerBuilder' => $customerBuilder,
+                'addressHelper' => $customerAddressHelper,
+                'registry' => $registry
             ]
         );
-    }
-
-    public function testGetLastLoginDate()
-    {
-        $this->assertEquals('Never', $this->view->getLastLoginDate());
-    }
-
-    public function testGetStoreLastLoginDate()
-    {
-        $this->assertEquals('Never', $this->view->getStoreLastLoginDate());
-    }
-
-    public function testGetCurrentStatus()
-    {
-        $this->assertEquals('Offline', $this->view->getCurrentStatus());
     }
 
     public function testGetTabLabel()
