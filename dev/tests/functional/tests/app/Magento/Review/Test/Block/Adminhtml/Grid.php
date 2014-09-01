df376af3f8be8b654ba5195fb8805772e84d1107
Vitaliy Honcharenko <vgoncharenko@ebay.com>
2014-09-01 16:18:49 +0300

    MTA-372: Test Creation for UpdateProductReviewEntityFromProductPage
    - change css selector

--- dev/tests/functional/tests/app/Magento/Review/Test/Block/Adminhtml/Grid.php
+++ dev/tests/functional/tests/app/Magento/Review/Test/Block/Adminhtml/Grid.php
@@ -29,7 +29,7 @@ class Grid extends GridAbstract
             'selector' => 'input[name="title"]',
         ],
         'status' => [
-            'selector' => 'select[name="status"]',
+            'selector' => '#reviwGrid_filter_status',
             'input' => 'select',
         ],
         'nickname' => [
