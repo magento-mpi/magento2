5fc5792cee3cb58fc1ad5143ed1fe13b798ff804
Yuxing Zheng <yuxzheng@ebay.com>
2014-09-05 11:22:40 -0500

    MAGETWO-27186: Fix tests
    - Fix static test Magento\Test\Integrity\Di\CompilerTest::testConstructorIntegrity

--- app/code/Magento/Customer/Service/V1/Data/CustomerGroupSearchResultsBuilder.php
+++ app/code/Magento/Customer/Service/V1/Data/CustomerGroupSearchResultsBuilder.php
@@ -11,6 +11,8 @@ namespace Magento\Customer\Service\V1\Data;
 use Magento\Framework\Service\Data\ObjectFactory;
 use Magento\Framework\Service\V1\Data\SearchCriteriaBuilder;
 use Magento\Framework\Service\V1\Data\AbstractSearchResultsBuilder;
+use Magento\Framework\Service\Data\AttributeValueBuilder;
+use Magento\Framework\Service\Data\MetadataServiceInterface;
 
 /**
  * Builder for the SearchResults Service Data Object
@@ -23,15 +25,25 @@ class CustomerGroupSearchResultsBuilder extends AbstractSearchResultsBuilder
      * Constructor
      *
      * @param ObjectFactory $objectFactory
+     * @param AttributeValueBuilder $valueBuilder
+     * @param MetadataServiceInterface $metadataService
      * @param SearchCriteriaBuilder $searchCriteriaBuilder
      * @param CustomerDetailsBuilder $itemObjectBuilder
      */
     public function __construct(
         ObjectFactory $objectFactory,
+        AttributeValueBuilder $valueBuilder,
+        MetadataServiceInterface $metadataService,
         SearchCriteriaBuilder $searchCriteriaBuilder,
         CustomerDetailsBuilder $itemObjectBuilder
     ) {
-        parent::__construct($objectFactory, $searchCriteriaBuilder, $itemObjectBuilder);
+        parent::__construct(
+            $objectFactory,
+            $valueBuilder,
+            $metadataService,
+            $searchCriteriaBuilder,
+            $itemObjectBuilder
+        );
     }
 
     /**
