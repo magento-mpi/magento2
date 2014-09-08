c2c4c622a0f8a539aab9193869d62f26ae63f9fc
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-09-08 14:16:17 +0300

    MAGETWO-26082: Create MySQL Adapter Library for Bool Query Type MAGETWO-27946: Use Boolean Mode
     - Some fixes

--- lib/internal/Magento/Framework/Search/ABstractKeyValuePair.php
+++ lib/internal/Magento/Framework/Search/ABstractKeyValuePair.php
@@ -5,10 +5,51 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-
 namespace Magento\Framework\Search;
 
+class AbstractKeyValuePair
+{
+    /**
+     * Field name
+     *
+     * @var string
+     */
+    protected $name;
 
-class ABstractKeyValuePair {
+    /**
+     * Field values
+     *
+     * @var mixed
+     */
+    protected $value;
 
-} 
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public function __construct($name, $value)
+    {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    /**
+     * Get field name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get field values
+     *
+     * @return array
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
