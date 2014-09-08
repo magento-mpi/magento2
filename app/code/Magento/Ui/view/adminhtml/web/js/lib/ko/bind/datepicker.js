9ea78593880bc58d7e64af8248920ffdfd5d4c2f
Mykhailo Miroshnikov <mmiroshnikov@ebay.com>
2014-09-08 17:49:57 +0300

    MAGETWO-27834: Implement date picker binding
     - Implemented datepicker binding

--- app/code/Magento/Ui/view/adminhtml/web/js/lib/ko/bind/datepicker.js
+++ app/code/Magento/Ui/view/adminhtml/web/js/lib/ko/bind/datepicker.js
@@ -1,9 +1,11 @@
 define(['ko', 'jquery', 'mage/calendar'], function (ko, $) {
+    var DEFAULT_DATE_FORMAT = 'mm/dd/yyyy';
+
     ko.bindingHandlers.datepicker = {
         init: function (el, valueAccessor) {
             var observable = valueAccessor();
 
-            $(el).calendar({ dateFormat: 'mm/dd/yyyy' });
+            $(el).calendar({ dateFormat: DEFAULT_DATE_FORMAT });
 
             ko.utils.registerEventHandler(el, 'change', function (e) {
                 var value = $(this).val();
