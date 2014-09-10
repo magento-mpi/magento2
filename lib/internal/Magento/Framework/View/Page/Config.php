e92bf00ca6c129c34594921bd1f45eca8c157444
Maxim Shikula <mshikula@ebay.com>
2014-09-10 15:35:08 +0300

    MAGETWO-27882: Stabilize CI

--- lib/internal/Magento/Framework/View/Page/Config.php
+++ lib/internal/Magento/Framework/View/Page/Config.php
@@ -90,24 +90,9 @@ class Config
     protected $scopeConfig;
 
     /**
-     * @var string
+     * @var \Magento\Framework\View\Page\FaviconInterface
      */
-    protected $faviconFile;
-
-    /**
-     * @var \Magento\Core\Helper\File\Storage\Database
-     */
-    protected $fileStorageDatabase;
-
-    /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    protected $storeManager;
-
-    /**
-     * @var \Magento\Framework\Filesystem\Directory\ReadInterface
-     */
-    protected $mediaDirectory;
+    protected $favicon;
 
     /**
      * @var array
@@ -130,24 +115,18 @@ class Config
      * @param \Magento\Framework\View\Asset\Repository $assetRepo
      * @param \Magento\Framework\View\Asset\GroupedCollection $pageAssets
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-     * @param \Magento\Core\Helper\File\Storage\Database $fileStorageDatabase
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-     * @param \Magento\Framework\App\Filesystem $filesystem
+     * @param \Magento\Framework\View\Page\FaviconInterface $favicon
      */
     public function __construct(
         \Magento\Framework\View\Asset\Repository $assetRepo,
         \Magento\Framework\View\Asset\GroupedCollection $pageAssets,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Core\Helper\File\Storage\Database $fileStorageDatabase,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Framework\App\Filesystem $filesystem
+        \Magento\Framework\View\Page\FaviconInterface $favicon
     ) {
         $this->assetRepo = $assetRepo;
         $this->pageAssets = $pageAssets;
         $this->scopeConfig = $scopeConfig;
-        $this->fileStorageDatabase = $fileStorageDatabase;
-        $this->storeManager = $storeManager;
-        $this->mediaDirectory = $filesystem->getDirectoryRead(\Magento\Framework\App\Filesystem::MEDIA_DIR);
+        $this->favicon = $favicon;
     }
 
     /**
@@ -537,31 +516,7 @@ class Config
      */
     public function getFaviconFile()
     {
-        if (null === $this->faviconFile) {
-            $this->faviconFile = $this->prepareFaviconFile();
-        }
-        return $this->faviconFile;
-    }
-
-    /**
-     * @return string
-     */
-    protected function prepareFaviconFile()
-    {
-        $folderName = \Magento\Backend\Model\Config\Backend\Image\Favicon::UPLOAD_DIR;
-        $scopeConfig = $this->scopeConfig->getValue(
-            'design/head/shortcut_icon',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
-        $path = $folderName . '/' . $scopeConfig;
-        $faviconUrl = $this->storeManager->getStore()
-            ->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . $path;
-
-        if (!is_null($scopeConfig) && $this->checkIsFile($path)) {
-            return $faviconUrl;
-        }
-
-        return false;
+        return $this->favicon->getFaviconFile();
     }
 
     /**
@@ -569,21 +524,7 @@ class Config
      */
     public function getDefaultFavicon()
     {
-        return 'Magento_Theme::favicon.ico';
-    }
-
-    /**
-     * If DB file storage is on - find there, otherwise - just file_exists
-     *
-     * @param string $filename relative file path
-     * @return bool
-     */
-    protected function checkIsFile($filename)
-    {
-        if ($this->fileStorageDatabase->checkDbUsage() && !$this->mediaDirectory->isFile($filename)) {
-            $this->fileStorageDatabase->saveFileToFilesystem($filename);
-        }
-        return $this->mediaDirectory->isFile($filename);
+        return $this->favicon->getDefaultFavicon();
     }
 
     /**
