9a083c106bb2e173cec68440d8328b4b1fb0d405
Maddy Chellathurai <mchellathura@ebay.com>
2014-09-15 16:50:34 -0500

    MAGETWO-28081: Enable autoloading of Magento application classes from setup
    - since autoloading is working, removing require_once.

--- app/code/Magento/SalesArchive/sql/salesarchive_setup/recurring.php
+++ app/code/Magento/SalesArchive/sql/salesarchive_setup/recurring.php
@@ -8,8 +8,6 @@
 
 use Magento\SalesArchive\Model\Resource\Synchronizer;
 
-require_once __DIR__ . '/../../Model/Resource/Synchronizer.php';
-
 /* @var $this \Magento\Setup\Module\Setup */
 $synchronizer = new Synchronizer($this);
 $synchronizer->syncArchiveStructure();
