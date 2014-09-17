931bf648cab98e17acf7dbec5bfef1a1243e4c36
Denys Rudchenko <drudchenko@ebay.com>
2014-09-17 15:57:09 +0300

    MAGETWO-27513: Implement universal way of using RSS module
    - MAGETWO-28460: Create Unit Tests for Catalog RSS

--- dev/tests/unit/testsuite/Magento/Catalog/Model/Rss/Product/SpecialTest.php
+++ dev/tests/unit/testsuite/Magento/Catalog/Model/Rss/Product/SpecialTest.php
@@ -31,26 +31,57 @@ class SpecialTest extends \PHPUnit_Framework_TestCase
     protected $productFactory;
 
     /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|\Magento\Catalog\Model\Product
+     */
+    protected $product;
+
+    /**
      * @var \Magento\Store\Model\StoreManagerInterface|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected $storeManagerInterface;
+    protected $storeManager;
 
     protected function setUp()
     {
-        $this->productFactory = $this->getMock('Magento\Catalog\Model\ProductFactory');
-        $this->storeManagerInterface = $this->getMock('Magento\Store\Model\StoreManagerInterface');
+        $this->product = $this->getMock('Magento\Catalog\Model\Product', [], [], '', false);
+        $this->productFactory = $this->getMock('Magento\Catalog\Model\ProductFactory', ['create']);
+        $this->productFactory->expects($this->any())->method('create')->will($this->returnValue($this->product));
+        $this->storeManager = $this->getMock('Magento\Store\Model\StoreManager', [], [], '', false);
 
         $this->objectManagerHelper = new ObjectManagerHelper($this);
         $this->special = $this->objectManagerHelper->getObject(
             'Magento\Catalog\Model\Rss\Product\Special',
             [
                 'productFactory' => $this->productFactory,
-                'storeManager' => $this->storeManagerInterface
+                'storeManager' => $this->storeManager
             ]
         );
     }
 
     public function testGetProductsCollection()
     {
+        $storeId = 1;
+        $store = $this->getMock('Magento\Store\Model\Store', [], [], '', false);
+        $this->storeManager->expects($this->once())->method('getStore')->with($storeId)->will(
+            $this->returnValue($store)
+        );
+        $websiteId = 1;
+        $store->expects($this->once())->method('getWebsiteId')->will($this->returnValue($websiteId));
+
+        /** @var \Magento\Catalog\Model\Resource\Product\Collection $productCollection */
+        $productCollection = $this->getMock('Magento\Catalog\Model\Resource\Product\Collection', [], [], '', false);
+        $this->product->expects($this->once())->method('getResourceCollection')->will(
+            $this->returnValue($productCollection)
+        );
+        $customerGroupId = 1;
+        $productCollection->expects($this->once())->method('addPriceDataFieldFilter')->will($this->returnSelf());
+        $productCollection->expects($this->once())->method('addPriceData')->with($storeId, $customerGroupId)->will(
+            $this->returnSelf()
+        );
+        $productCollection->expects($this->once())->method('addAttributeToSelect')->will($this->returnSelf());
+        $productCollection->expects($this->once())->method('addAttributeToSort')->will($this->returnSelf());
+
+        $products = $this->special->getProductsCollection($storeId, $customerGroupId);
+        $this->assertEquals($productCollection, $products);
+
     }
 }
