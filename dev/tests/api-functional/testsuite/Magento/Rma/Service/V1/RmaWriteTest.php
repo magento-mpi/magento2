d23f55b0d6328aae99de314e57a003f61e9f243c
Iryna Savchenko <isavchenko@ebay.com>
2014-09-22 13:05:31 +0300

    MAGETWO-25672: Returns service & APIs
    - api functional tests

--- dev/tests/api-functional/testsuite/Magento/Rma/Service/V1/RmaWriteTest.php
+++ dev/tests/api-functional/testsuite/Magento/Rma/Service/V1/RmaWriteTest.php
@@ -34,18 +34,40 @@ class RmaWriteTest extends WebapiAbstract
         $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
     }
 
-    /**
-     * @magentoApiDataFixture Magento/Sales/_files/order.php
-     */
     protected function prepareRma()
     {
         $order = $this->objectManager->get('Magento\Sales\Model\Order')->load(1);
+        /** @var \Magento\Sales\Service\V1\Data\OrderItemBuilder $orderItemBuilder */
+        $rmaItemBuilder = $this->objectManager->get('Magento\Rma\Service\V1\Data\ItemBuilder');
+        $rmaItemBuilder->populateWithArray($this->getDataStructure('Magento\Rma\Service\V1\Data\Item'));
+
+        $rmaItemBuilder->setId(333);
+        $orderItem = current($order->getAllItems());
+        $rmaItemBuilder->setOrderItemId($orderItem->getId());
+        $rmaItemBuilder->setQtyRequested($orderItem->getQtyOrdered());
+        $rmaItemBuilder->setQtyAuthorized(0);
+        $rmaItemBuilder->setQtyReturned(0);
+        $rmaItemBuilder->setQtyApproved(1);
+        $rmaItemBuilder->setReason(1);
+        $rmaItemBuilder->setCondition(2);
+        $rmaItemBuilder->setResolution(3);
+        $rmaItemBuilder->setStatus('pending');
+        $rmaItem = $rmaItemBuilder->create()->__toArray();
         /** @var \Magento\Rma\Service\V1\Data\RmaBuilder $orderBuilder */
         $rmaBuilder = $this->objectManager->get('Magento\Rma\Service\V1\Data\RmaBuilder');
         $rmaBuilder->populateWithArray($this->getDataStructure('Magento\Rma\Service\V1\Data\Rma'));
+
+        $rmaBuilder->setEntityId(12);
         $rmaBuilder->setOrderId($order->getId());
+        $rmaBuilder->setOrderIncrementId($order->getIncrementId());
         $rmaBuilder->setIncrementId(28);
-        return $rmaBuilder->create()->__toArray();
+        $rmaBuilder->setStoreId(1);
+        $rmaBuilder->setCustomerCustomEmail('customer@custom.com');
+        $rmaBuilder->setCustomerId(1);
+        $rmaBuilder->setItems([$rmaItem]);
+        $rmaBuilder->setStatus('pending');
+        $rma = $rmaBuilder->create()->__toArray();
+        return $rma;
     }
 
     /**
@@ -62,13 +84,17 @@ class RmaWriteTest extends WebapiAbstract
         return $data;
     }
 
+    /**
+     * @magentoApiDataFixture Magento/Sales/_files/shipment.php
+     */
     public function testCreate()
     {
         $rma = $this->prepareRma();
 
+        $requestData = ['rmaDataObject' => $rma];
         $serviceInfo = [
             'rest' => [
-                'resourcePath' => '/V1/returns',
+                'resourcePath' => '/V1/returns' . '/?' . http_build_query($requestData),
                 'httpMethod' => RestConfig::HTTP_METHOD_POST
             ],
             'soap' => [
@@ -77,38 +103,37 @@ class RmaWriteTest extends WebapiAbstract
                 'operation' => self::SERVICE_NAME . 'create'
             ]
         ];
-        $result = $this->_webApiCall($serviceInfo, ['rmaDataObject' => $rma]);
+        $result = $this->_webApiCall($serviceInfo, $requestData);
         $this->assertTrue($result);
         $model = $this->objectManager->get('Magento\Rma\Model\Rma');
-        $model->load($rma['increment_id'], 'increment_id');
+        $model->load($rma->getIncrementId(), 'increment_id');
         $this->assertTrue((bool)$model->getId());
     }
 
-    /**
-     * @magentoApiDataFixture Magento/Rma/_files/rma.php
-     */
-    public function testUpdate()
-    {
-        $rma = [
-            'customer_custom_email' => 'email@example.com'
-        ];
-
-        $model = $this->objectManager->get('Magento\Rma\Model\Rma');
-        $model->load('1', 'increment_id');
-        $serviceInfo = [
-            'rest' => [
-                'resourcePath' => '/V1/returns/' . $model->getId(),
-                'httpMethod' => RestConfig::HTTP_METHOD_PUT
-            ],
-            'soap' => [
-                'service' => self::SERVICE_NAME,
-                'serviceVersion' => self::SERVICE_VERSION,
-                'operation' => self::SERVICE_NAME . 'update'
-            ]
-        ];
-        $this->_webApiCall($serviceInfo, ['rmaDataObject' => $rma]);
-        $actualRma = $this->objectManager->get('Magento\Rma\Model\Rma')->load($model->getId());
-        $customerCustomEmail = $actualRma->getCustomerCustomEmail();
-        $this->assertEquals('email@example.com', $customerCustomEmail->getData('customer_custom_email'));
-    }
+//    /**
+//     * @magentoApiDataFixture Magento/Rma/_files/rma.php
+//     */
+//    public function testUpdate()
+//    {
+//        $rma = $this->prepareRma();
+//        $rma->setCustomerCustomEmail('email@example.com');
+//        $rma->create();
+//        $model = $this->objectManager->get('Magento\Rma\Model\Rma');
+//        $model->load('1', 'increment_id');
+//        $serviceInfo = [
+//            'rest' => [
+//                'resourcePath' => '/V1/returns/' . $model->getId(),
+//                'httpMethod' => RestConfig::HTTP_METHOD_PUT
+//            ],
+//            'soap' => [
+//                'service' => self::SERVICE_NAME,
+//                'serviceVersion' => self::SERVICE_VERSION,
+//                'operation' => self::SERVICE_NAME . 'update'
+//            ]
+//        ];
+//        $this->_webApiCall($serviceInfo, ['rmaDataObject' => $rma]);
+//        $actualRma = $this->objectManager->get('Magento\Rma\Model\Rma')->load($model->getId());
+//        $customerCustomEmail = $actualRma->getCustomerCustomEmail();
+//        $this->assertEquals('email@example.com', $customerCustomEmail->getData('customer_custom_email'));
+//    }
 }
