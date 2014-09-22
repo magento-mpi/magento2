3a4bc9a649820377169e6413b51175e3cf9684d4
Dmytro Kvashnin <dkvashnin@ebay.com>
2014-09-22 15:43:21 +0300

    MAGETWO-25672: Returns service & APIs
    - RMA write service

--- dev/tests/api-functional/testsuite/Magento/Rma/Service/V1/RmaWriteTest.php
+++ dev/tests/api-functional/testsuite/Magento/Rma/Service/V1/RmaWriteTest.php
@@ -10,6 +10,8 @@ namespace Magento\Rma\Service\V1;
 
 use Magento\TestFramework\TestCase\WebapiAbstract;
 use Magento\Webapi\Model\Rest\Config as RestConfig;
+use Magento\Rma\Service\V1\Data\Rma;
+use Magento\Rma\Service\V1\Data\Item;
 
 /**
  * Class RmaWriteTest
@@ -25,76 +27,16 @@ class RmaWriteTest extends WebapiAbstract
     /**#@-*/
 
     /**
-     * @var \Magento\TestFramework\ObjectManager
-     */
-    protected $objectManager;
-
-    public function setUp()
-    {
-        $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
-    }
-
-    protected function prepareRma()
-    {
-        $order = $this->objectManager->get('Magento\Sales\Model\Order')->load(1);
-        /** @var \Magento\Sales\Service\V1\Data\OrderItemBuilder $orderItemBuilder */
-        $rmaItemBuilder = $this->objectManager->get('Magento\Rma\Service\V1\Data\ItemBuilder');
-        $rmaItemBuilder->populateWithArray($this->getDataStructure('Magento\Rma\Service\V1\Data\Item'));
-
-        $rmaItemBuilder->setId(333);
-        $orderItem = current($order->getAllItems());
-        $rmaItemBuilder->setOrderItemId($orderItem->getId());
-        $rmaItemBuilder->setQtyRequested($orderItem->getQtyOrdered());
-        $rmaItemBuilder->setQtyAuthorized(0);
-        $rmaItemBuilder->setQtyReturned(0);
-        $rmaItemBuilder->setQtyApproved(1);
-        $rmaItemBuilder->setReason(1);
-        $rmaItemBuilder->setCondition(2);
-        $rmaItemBuilder->setResolution(3);
-        $rmaItemBuilder->setStatus('pending');
-        $rmaItem = $rmaItemBuilder->create()->__toArray();
-        /** @var \Magento\Rma\Service\V1\Data\RmaBuilder $orderBuilder */
-        $rmaBuilder = $this->objectManager->get('Magento\Rma\Service\V1\Data\RmaBuilder');
-        $rmaBuilder->populateWithArray($this->getDataStructure('Magento\Rma\Service\V1\Data\Rma'));
-
-        $rmaBuilder->setEntityId(12);
-        $rmaBuilder->setOrderId($order->getId());
-        $rmaBuilder->setOrderIncrementId($order->getIncrementId());
-        $rmaBuilder->setIncrementId(28);
-        $rmaBuilder->setStoreId(1);
-        $rmaBuilder->setCustomerCustomEmail('customer@custom.com');
-        $rmaBuilder->setCustomerId(1);
-        $rmaBuilder->setItems([$rmaItem]);
-        $rmaBuilder->setStatus('pending');
-        $rma = $rmaBuilder->create()->__toArray();
-        return $rma;
-    }
-
-    /**
-     * @param string $className
-     *
-     * @return array
-     */
-    protected function getDataStructure($className)
-    {
-        $refClass = new \ReflectionClass ($className);
-        $constants = $refClass->getConstants();
-        $data = array_fill_keys($constants, null);
-        unset($data['custom_attributes']);
-        return $data;
-    }
-
-    /**
      * @magentoApiDataFixture Magento/Sales/_files/shipment.php
      */
     public function testCreate()
     {
-        $rma = $this->prepareRma();
+        $rma = $this->getNewRmaData();
 
         $requestData = ['rmaDataObject' => $rma];
         $serviceInfo = [
             'rest' => [
-                'resourcePath' => '/V1/returns' . '/?' . http_build_query($requestData),
+                'resourcePath' => '/V1/returns',
                 'httpMethod' => RestConfig::HTTP_METHOD_POST
             ],
             'soap' => [
@@ -105,35 +47,124 @@ class RmaWriteTest extends WebapiAbstract
         ];
         $result = $this->_webApiCall($serviceInfo, $requestData);
         $this->assertTrue($result);
-        $model = $this->objectManager->get('Magento\Rma\Model\Rma');
-        $model->load($rma->getIncrementId(), 'increment_id');
-        $this->assertTrue((bool)$model->getId());
     }
 
-//    /**
-//     * @magentoApiDataFixture Magento/Rma/_files/rma.php
-//     */
-//    public function testUpdate()
-//    {
-//        $rma = $this->prepareRma();
-//        $rma->setCustomerCustomEmail('email@example.com');
-//        $rma->create();
-//        $model = $this->objectManager->get('Magento\Rma\Model\Rma');
-//        $model->load('1', 'increment_id');
-//        $serviceInfo = [
-//            'rest' => [
-//                'resourcePath' => '/V1/returns/' . $model->getId(),
-//                'httpMethod' => RestConfig::HTTP_METHOD_PUT
-//            ],
-//            'soap' => [
-//                'service' => self::SERVICE_NAME,
-//                'serviceVersion' => self::SERVICE_VERSION,
-//                'operation' => self::SERVICE_NAME . 'update'
-//            ]
-//        ];
-//        $this->_webApiCall($serviceInfo, ['rmaDataObject' => $rma]);
-//        $actualRma = $this->objectManager->get('Magento\Rma\Model\Rma')->load($model->getId());
-//        $customerCustomEmail = $actualRma->getCustomerCustomEmail();
-//        $this->assertEquals('email@example.com', $customerCustomEmail->getData('customer_custom_email'));
-//    }
+    /**
+     * @magentoApiDataFixture Magento/Sales/_files/shipment.php
+     */
+    public function testUpdate()
+    {
+        $rmaData = $this->getNewRmaData();
+
+        $requestData = ['rmaDataObject' => $rmaData];
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => '/V1/returns',
+                'httpMethod' => RestConfig::HTTP_METHOD_POST
+            ],
+            'soap' => [
+                'service' => self::SERVICE_NAME,
+                'serviceVersion' => self::SERVICE_VERSION,
+                'operation' => self::SERVICE_NAME . 'create'
+            ]
+        ];
+        $this->_webApiCall($serviceInfo, $requestData);
+
+        $rma = $this->getRmaFixture();
+
+        $requestData = ['rmaDataObject' => $this->getRequestForUpdateRma($rma)];
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => '/V1/returns/' . $rma->getId(),
+                'httpMethod' => RestConfig::HTTP_METHOD_PUT
+            ],
+            'soap' => [
+                'service' => self::SERVICE_NAME,
+                'serviceVersion' => self::SERVICE_VERSION,
+                'operation' => self::SERVICE_NAME . 'update'
+            ]
+        ];
+        $result = $this->_webApiCall($serviceInfo, $requestData);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @return array
+     */
+    private function getNewRmaData()
+    {
+        $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $collection = $objectManager->create('Magento\Sales\Model\Resource\Order\Collection');
+        $collection->setOrder('entity_id')
+            ->setPageSize(1)
+            ->load();
+        /** @var \Magento\Sales\Model\Order $order */
+        $order = $collection->fetchItem();
+        $items = $order->getItemsCollection();
+
+        $request = [
+            Rma::ORDER_ID => $order->getId(),
+            Rma::ITEMS => []
+        ];
+
+        /** @var \Magento\Sales\Model\Order\Item $item */
+        foreach ($items as $item) {
+            $request[Rma::ITEMS][] = [
+                Item::ORDER_ITEM_ID => $item->getId(),
+                Item::QTY_REQUESTED => 1,
+                Item::CONDITION => 7,
+                Item::REASON => 9,
+                Item::RESOLUTION => 4,
+                Item::STATUS => 'pending'
+            ];
+            $item->setProductType('simple');
+            $item->setQtyShipped($item->getQtyOrdered());
+            $item->save();
+        }
+
+        $order->save();
+
+        return $request;
+    }
+
+    /**
+     * Return last created Rma fixture
+     *
+     * @return \Magento\Rma\Model\Rma
+     */
+    private function getRmaFixture()
+    {
+        $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $collection = $objectManager->create('Magento\Rma\Model\Resource\Rma\Collection');
+        $collection->setOrder('entity_id')
+            ->setPageSize(1)
+            ->load();
+        return $collection->fetchItem();
+    }
+
+    /**
+     * @param \Magento\Rma\Model\Rma $rma
+     * @return array
+     */
+    private function getRequestForUpdateRma(\Magento\Rma\Model\Rma $rma)
+    {
+        $request = [
+            Rma::ORDER_ID => $rma->getOrderId(),
+            Rma::ITEMS => []
+        ];
+
+        /** @var \Magento\Rma\Model\Item $item */
+        foreach ($rma->getItemsForDisplay()as $item) {
+            $request[Rma::ITEMS][] = [
+                Item::ID => $item->getId(),
+                Item::ORDER_ITEM_ID => $item->getOrderItemId(),
+                Item::QTY_AUTHORIZED => 1,
+                Item::CONDITION => 7,
+                Item::REASON => 9,
+                Item::RESOLUTION => 4,
+                Item::STATUS => 'authorized'
+            ];
+        }
+        return $request;
+    }
 }
