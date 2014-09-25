39b64d14667cdb77dbcf69b519fb032964976050
Michail Slabko <mslabko@ebay.com>
2014-09-25 15:03:43 +0300

    MAGETWO-26865: Compare products link is disappeared after changing page
    - add negative cases in unit test, add dependency

--- dev/tests/unit/testsuite/Magento/Catalog/Model/Layout/DepersonalizePluginTest.php
+++ dev/tests/unit/testsuite/Magento/Catalog/Model/Layout/DepersonalizePluginTest.php
@@ -42,6 +42,11 @@ class DepersonalizePluginTest extends \PHPUnit_Framework_TestCase
      */
     protected $cacheConfigMock;
 
+    /**
+     * @var \Magento\Framework\View\Layout|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $resultLayout;
+
     public function setUp()
     {
         $this->layoutMock = $this->getMock('Magento\Framework\View\Layout', [], [], '', false);
@@ -54,6 +59,7 @@ class DepersonalizePluginTest extends \PHPUnit_Framework_TestCase
         $this->requestMock = $this->getMock('Magento\Framework\App\Request\Http', [], [], '', false);
         $this->moduleManagerMock = $this->getMock('Magento\Framework\Module\Manager', [], [], '', false);
         $this->cacheConfigMock = $this->getMock('Magento\PageCache\Model\Config', [], [], '', false);
+        $this->resultLayout = $this->getMock('Magento\Framework\View\Layout', array(), array(), '', false);
 
         $this->plugin = (new ObjectManager($this))->getObject('Magento\Catalog\Model\Layout\DepersonalizePlugin', [
             'catalogSession' => $this->catalogSessionMock,
@@ -65,7 +71,6 @@ class DepersonalizePluginTest extends \PHPUnit_Framework_TestCase
 
     public function testAfterGenerateXml()
     {
-        $expectedResult = $this->getMock('Magento\Framework\View\Layout', [], [], '', false);
         $this->moduleManagerMock->expects($this->once())->method('isEnabled')->with('Magento_PageCache')
             ->willReturn(true);
         $this->cacheConfigMock->expects($this->once())->method('isEnabled')->willReturn(true);
@@ -73,7 +78,61 @@ class DepersonalizePluginTest extends \PHPUnit_Framework_TestCase
         $this->layoutMock->expects($this->once())->method('isCacheable')->willReturn(true);
         $this->catalogSessionMock->expects($this->once())->method('clearStorage');
 
-        $actualResult = $this->plugin->afterGenerateXml($this->layoutMock, $expectedResult);
-        $this->assertEquals($expectedResult, $actualResult);
+        $actualResult = $this->plugin->afterGenerateXml($this->layoutMock, $this->resultLayout);
+        $this->assertEquals($this->resultLayout, $actualResult);
+    }
+
+    public function testPageCacheModuleIsDisabled()
+    {
+        $this->moduleManagerMock->expects($this->once())
+            ->method('isEnabled')
+            ->with($this->equalTo('Magento_PageCache'))
+            ->willReturn(false);
+        $this->catalogSessionMock->expects($this->never())->method('clearStorage');
+
+        $actualResult = $this->plugin->afterGenerateXml($this->layoutMock, $this->resultLayout);
+        $this->assertEquals($this->resultLayout, $actualResult);
+    }
+
+    public function testCacheIsDisabledInConfig()
+    {
+        $this->moduleManagerMock->expects($this->once())
+            ->method('isEnabled')
+            ->with($this->equalTo('Magento_PageCache'))
+            ->willReturn(true);
+        $this->cacheConfigMock->expects($this->once())->method('isEnabled')->willReturn(false);
+        $this->catalogSessionMock->expects($this->never())->method('clearStorage');
+
+        $actualResult = $this->plugin->afterGenerateXml($this->layoutMock, $this->resultLayout);
+        $this->assertEquals($this->resultLayout, $actualResult);
+    }
+
+    public function testIsAjax()
+    {
+        $this->moduleManagerMock->expects($this->once())
+            ->method('isEnabled')
+            ->with($this->equalTo('Magento_PageCache'))
+            ->willReturn(true);
+        $this->cacheConfigMock->expects($this->once())->method('isEnabled')->willReturn(true);
+        $this->requestMock->expects($this->once($this->once()))->method('isAjax')->willReturn(true);
+        $this->catalogSessionMock->expects($this->never())->method('clearStorage');
+
+        $actualResult = $this->plugin->afterGenerateXml($this->layoutMock, $this->resultLayout);
+        $this->assertEquals($this->resultLayout, $actualResult);
+    }
+
+    public function testLayoutIsNotCacheable()
+    {
+        $this->moduleManagerMock->expects($this->once())
+            ->method('isEnabled')
+            ->with($this->equalTo('Magento_PageCache'))
+            ->willReturn(true);
+        $this->cacheConfigMock->expects($this->once())->method('isEnabled')->willReturn(true);
+        $this->requestMock->expects($this->once($this->once()))->method('isAjax')->willReturn(false);
+        $this->layoutMock->expects($this->once())->method('isCacheable')->willReturn(false);
+        $this->catalogSessionMock->expects($this->never())->method('clearStorage');
+
+        $actualResult = $this->plugin->afterGenerateXml($this->layoutMock, $this->resultLayout);
+        $this->assertEquals($this->resultLayout, $actualResult);
     }
 }
