5ab2d5fb109167a697df166850d460927d82b008
Igor Kruchymskyi <ikruchynskyi@ebay.com>
2014-09-25 17:26:39 +0300

    MTA-132: Test Creation for MoveToAnotherWishlist
    - fixes after code review

--- dev/tests/functional/tests/app/Magento/MultipleWishlist/Test/TestCase/MoveToAnotherWishlistTest.php
+++ dev/tests/functional/tests/app/Magento/MultipleWishlist/Test/TestCase/MoveToAnotherWishlistTest.php
@@ -8,11 +8,19 @@
 
 namespace Magento\MultipleWishlist\Test\TestCase;
 
-use Magento\Catalog\Test\Fixture\CatalogProductSimple;
+use Magento\Catalog\Test\Page\Product\CatalogProductView;
+use Mtf\Client\Driver\Selenium\Browser;
+use Mtf\Fixture\FixtureFactory;
+use Magento\Catalog\Test\Fixture\CatalogCategory;
+use Magento\Customer\Test\Fixture\CustomerInjectable;
 use Magento\MultipleWishlist\Test\Fixture\MultipleWishlist;
+use Magento\Wishlist\Test\Page\WishlistIndex;
+use Mtf\ObjectManager;
+use Mtf\TestCase\Injectable;
+use Mtf\Fixture\InjectableFixture;
 
 /**
- * Test Creation for MoveToAnotherWishlist
+ * Test Creation for MoveToAnotherWishlistTest
  *
  * Test Flow:
  *
@@ -31,18 +39,145 @@ use Magento\MultipleWishlist\Test\Fixture\MultipleWishlist;
  * @group Multiple_Wishlists_(CS)
  * @ZephyrId MAGETWO-28820
  */
-class MoveToAnotherWishlistTest extends AbstractMoveToAnotherWishlist
+class MoveToAnotherWishlistTest extends Injectable
 {
+    /**
+     * Fixture factory
+     *
+     * @var FixtureFactory
+     */
+    protected $fixtureFactory;
+
+    /**
+     * Catalog product view page
+     *
+     * @var CatalogProductView
+     */
+    protected $catalogProductView;
+
+    /**
+     * Multiple wish list index page
+     *
+     * @var WishlistIndex
+     */
+    protected $wishlistIndex;
+
+    /**
+     * Browser object
+     *
+     * @var Browser
+     */
+    protected $browser;
+
+    /**
+     * ObjectManager object
+     *
+     * @var ObjectManager
+     */
+    protected $objectManager;
+
+    /**
+     * Prepare data for test
+     *
+     * @param FixtureFactory $fixtureFactory
+     * @param Browser $browser
+     * @param ObjectManager $objectManager
+     * @return void
+     */
+    public function __prepare(FixtureFactory $fixtureFactory, Browser $browser, ObjectManager $objectManager)
+    {
+        $this->browser = $browser;
+        $this->objectManager = $objectManager;
+        $this->fixtureFactory = $fixtureFactory;
+        $config = $this->fixtureFactory->createByCode('configData', ['dataSet' => 'multiple_wishlist_default']);
+        $config->persist();
+    }
+
+    /**
+     * Injection data
+     *
+     * @param WishlistIndex $wishlistIndex
+     * @param CatalogProductView $catalogProductView
+     * @return void
+     */
+    public function __inject(
+        WishlistIndex $wishlistIndex,
+        CatalogProductView $catalogProductView
+    ) {
+        $this->wishlistIndex = $wishlistIndex;
+        $this->catalogProductView = $catalogProductView;
+    }
+
     /**
      * Run Move To Another Wishlist test
      *
      * @param MultipleWishlist $multipleWishlist
-     * @param CatalogProductSimple $product
+     * @param string $product
+     * @param string $qty
      * @param string $qtyToMove
      * @return array
      */
-    public function test(MultipleWishlist $multipleWishlist, CatalogProductSimple $product, $qtyToMove)
+    public function test(MultipleWishlist $multipleWishlist, $product, $qty, $qtyToMove)
     {
-        return parent::moveToCustomWishlist($multipleWishlist, $product, $qtyToMove);
+        // Precondition:
+        $multipleWishlist->persist();
+        $customer = $multipleWishlist->getDataFieldConfig('customer_id')['source']->getCustomer();
+        $product = $this->createProduct($product, $qty);
+        $this->loginCustomer($customer);
+        $this->addProductToWishlist($product);
+
+        // Steps:
+        $productBlock = $this->wishlistIndex->getMultipleItemsBlock()->getItemProduct($product);
+        $productBlock->fillProduct(['qty' => $qtyToMove]);
+        $productBlock->moveToWishlist($multipleWishlist);
+
+        return ['product' => $product];
+    }
+
+    /**
+     * Create product
+     *
+     * @param string $product
+     * @param string $qty
+     * @return InjectableFixture
+     */
+    protected function createProduct($product, $qty)
+    {
+        list($fixture, $dataSet) = explode('::', $product);
+        $product = $this->fixtureFactory->createByCode(
+            $fixture,
+            ['dataSet' => $dataSet, 'data' => ['checkout_data' => ['data' => ['qty' => $qty]]]]
+        );
+        $product->persist();
+        return $product;
+    }
+
+    /**
+     * Login customer
+     *
+     * @param CustomerInjectable $customer
+     * @return void
+     */
+    protected function loginCustomer(CustomerInjectable $customer)
+    {
+        $customerLogin = $this->objectManager->create(
+            'Magento\Customer\Test\TestStep\LoginCustomerOnFrontendStep',
+            ['customer' => $customer]
+        );
+        $customerLogin->run();
+    }
+
+    /**
+     * Add product to wishlist
+     *
+     * @param InjectableFixture $product
+     * @return void
+     */
+    protected function addProductToWishlist($product)
+    {
+        $this->browser->open($_ENV['app_frontend_url'] . $product->getUrlKey() . '.html');
+        $wishlistViewBlock = $this->catalogProductView->getMultipleWishlistViewBlock();
+        $wishlistViewBlock->fillOptions($product);
+        $wishlistViewBlock->addToWishlist();
     }
 }
