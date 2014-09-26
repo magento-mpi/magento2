8e999f9d234ce66db102e7645aaf261b6c412e75
Mykhailo Miroshnikov <mmiroshnikov@ebay.com>
2014-09-26 18:29:20 +0300

    MAGETWO-28734: Implement Select
     - Implemented select form element

---
+++ app/code/Magento/Ui/view/base/web/js/form/element/select.js
@@ -0,0 +1,53 @@
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+define([
+    './abstract',
+    'Magento_Ui/js/lib/component',
+    'underscore',
+    'i18n'
+], function (AbstractElement, Component, _, t) {
+    'use strict';
+
+    var SelectElement = AbstractElement.extend({
+
+        /**
+         * Invokes initialize method of parent class and initializes properties of instance.
+         * @param {Object} config - form element configuration
+         * @param {Number|String} value - initial value of form element
+         */
+        initialize: function (config, value) {
+            this._super();
+
+            this.caption = this.caption || t('Select...');
+            this.options = this.options ? this.formatOptions() : [];
+        },
+
+        /**
+         * Formats options property of instance.
+         * @param {Object} options - object representing options
+         * @returns {Array} - Options, converted to array
+         */
+        formatOptions: function () {
+            return _.map(this.options, function (value, key) {
+                return { value: key, label: value  };
+            });
+        },
+
+        /**
+         * Stores element's value to registry by element's path value
+         * @param  {String} path
+         * @param  {Object} value - selected object
+         */
+        store: function (path, selected) {
+            registry.set(selected.value, path);
+        },
+    });
+
+    return Component({
+        constr: SelectElement
+    });
+});
