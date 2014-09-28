88293ee56b8bc6adff45bb9cdbdb0f6b87abfefd
Sergey Ivashchenko <sivashchenko@ebay.com>
2014-09-28 23:21:43 +0300

    MAGETWO-28719: Implement form save and validate action

---
+++ app/code/Magento/Ui/Controller/Adminhtml/Form/Validate.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+namespace Magento\Ui\Controller\Adminhtml\Form;
+
+class Save extends \Magento\Backend\App\Action
+{
+    /**
+     * Action for AJAX request
+     *
+     * @return void
+     */
+    public function execute()
+    {
+        $this->_response->appendBody(
+            $this->_request->getParam('response')
+        );
+    }
+}
