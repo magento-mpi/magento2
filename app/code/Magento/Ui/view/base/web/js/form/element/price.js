a6f626bc656a38e764ca4347603a4865647140eb
Anton Guz <aguz@ebay.com>
2014-10-01 15:03:00 +0300

    MAGETWO-28740: Implement Currency data type container
     - Implemented 'Price' data type

---
+++ app/code/Magento/Ui/view/base/web/js/form/element/price.js
@@ -0,0 +1,21 @@
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+define([
+    './abstract'
+], function (AbstractElement) {
+    'use strict';
+
+    return AbstractElement.extend({
+
+        /**
+         * Invokes initialize method of parent class.
+         */
+        initialize: function (config) {
+            this.constructor.__super__.initialize.apply(this, arguments);
+        }
+    });
+});
