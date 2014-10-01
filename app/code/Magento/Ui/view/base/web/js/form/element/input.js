bf722bf4f4417ae5b0de736ce6f4667bd6613780
Mykhailo Miroshnikov <mmiroshnikov@ebay.com>
2014-10-01 19:08:39 +0300

    MAGETWO-28739: Implement Abstract data type container (DTC)
     - Fixed invocation of parent class

--- app/code/Magento/Ui/view/base/web/js/form/element/input.js
+++ app/code/Magento/Ui/view/base/web/js/form/element/input.js
@@ -15,7 +15,7 @@ define([
          * Invokes initialize method of parent class.
          */
         initialize: function () {
-            this.constructor.__super__.initialize.apply(this, arguments);
+            AbstractElement.prototype.initialize.apply(this, arguments);
         }
     });
 });
