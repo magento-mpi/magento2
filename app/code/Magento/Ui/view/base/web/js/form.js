1c71322f092dc20b607e32eb44468e9ca8a09bf1
Mykhailo Miroshnikov <mmiroshnikov@ebay.com>
2014-10-02 10:36:10 +0300

    MAGETWO-28749: Implement Form viewmodel (as a part of Form component)
     - Added jsDoc to form

--- app/code/Magento/Ui/view/base/web/js/form.js
+++ app/code/Magento/Ui/view/base/web/js/form.js
@@ -14,6 +14,13 @@ define(function (require) {
         utils       = require('mage/utils'),
         _           = require('underscore');
 
+    /**
+     * Concatenates path and subpath by '.' separator
+     * 
+     * @param  {String} path
+     * @param  {String} subpath
+     * @return {String} result path
+     */
     function incrementPath(path, subpath) {
         return path + '.' + subpath;
     }
@@ -21,23 +28,36 @@ define(function (require) {
     var Form = Scope.extend({
 
         /**
-         * Extends instance with defaults and config, initializes observable properties.
+         * Extends instance with config, initializes observable properties,
+         *     invokes initElements method.
+         *     
          * @param  {Object} config
          */
-        initialize: function (config, refs) {
+        initialize: function (config) {
             _.extend(this, config);
-
-            this.refs = refs;
 
             this.initElements = this.initElements.bind(
                 this, 
-                refs.provider.meta.get(),
-                refs.provider.data.get()
+                this.refs.provider.meta.get(),
+                this.refs.provider.data.get()
             );
 
             this.initElements();
         },
 
+        /**
+         * Recursively loops over initial object's properties,
+         *     checkes if property is meta descriptor, looks for "meta_ref"
+         *     property, merges found reference object, builds config for element
+         *     and invoces initElement with built config.
+         * If property is not meta descriptor, invoces itself with new path and initial
+         *     parameters.
+         *     
+         * @param  {Object} meta - reference to meta
+         * @param  {Object} data - reference to data
+         * @param  {Object} initial - target object to format
+         * @param  {String} basePath - path to initial (e.g. "customer.website")
+         */
         initElements: function (meta, data, initial, basePath) {
             var target,
                 isObject,
@@ -73,6 +93,13 @@ define(function (require) {
             }
         },
 
+        /**
+         * Looks up for corresponding constructor in elements object,
+         *     creates an instance of it passing config as a single parameted.
+         * Invokes register method passing instance to it.
+         * 
+         * @param  {Object} config - config for instance
+         */
         initElement: function (config) {
             var constr  = elements[config.type],
                 element = new constr(config);
@@ -80,22 +107,42 @@ define(function (require) {
             this.register(element);
         },
 
+        /**
+         * Registes element by it's name to registry
+         * 
+         * @param  {Object} element
+         */
         register: function (element) {
             registry.set(element.name, element);
         },
 
+        /**
+         * Defines if an object is meta descriptor by checking if one has
+         *     "meta_ref" or "input_type" properties defined
+         *     
+         * @param  {Object}  obj
+         * @return {Boolean} - true, if object is meta descriptor
+         */
         isMetaDescriptor: function (obj) {
             return ('meta_ref' in obj) || ('input_type' in obj);
         }
     });
-
+    
+    /**
+     * Fetches for provider and globalStorage, extends config with references
+     *     to those, creates instance of Form with config passed to constructor as
+     *     a single parameter, writes instance of form to registry by it's name.
+     *     
+     * @param  {Object} config
+     */
     return function (config) {
         registry.get([config.source, 'globalStorage'], function (provider, globalStorage) {
-
-            registry.set(config.name, new Form(config, {
+            config.refs = {
                 provider: provider,
                 globalStorage: globalStorage
-            }));
+            };
+
+            registry.set(config.name, new Form(config));
         });
     };
 });
