4938e2ae239396a7e1c6c442dc2c8d6fea0397cf
Alex Bomko <abomko@ebay.com>
2014-10-03 16:06:32 +0300

    MAGETWO-28336: Sample Data Script and Fixtures for Products
    - fix static tests

--- dev/tools/Magento/Tools/SampleData/config/config.php
+++ dev/tools/Magento/Tools/SampleData/config/config.php
@@ -8,8 +8,8 @@
 
 return [
     'setup_resources' => [
-//        'Magento_Catalog' => 'Magento\Tools\SampleData\Module\Catalog\Setup',
-//        'Magento_ConfigurableProduct' => 'Magento\Tools\SampleData\Module\ConfigurableProduct\Setup',
+        'Magento_Catalog' => 'Magento\Tools\SampleData\Module\Catalog\Setup',
+        'Magento_ConfigurableProduct' => 'Magento\Tools\SampleData\Module\ConfigurableProduct\Setup',
         'Magento_RecurringPayment' => 'Magento\Tools\SampleData\Module\RecurringPayment\Setup',
         'Magento_Downloadable' => 'Magento\Tools\SampleData\Module\Downloadable\Setup',
         'Magento_GroupedProduct' => 'Magento\Tools\SampleData\Module\GroupedProduct\Setup',
