55a3a6bb812319257ea587a594db022ce2e96409
Mykhailo Miroshnikov <mmiroshnikov@ebay.com>
2014-10-07 16:59:36 +0300

    MAGETWO-29202: Implement Validation Rule Component
     - Provided JSDoc for validator

--- app/code/Magento/Ui/view/base/web/js/lib/validation/validator.js
+++ app/code/Magento/Ui/view/base/web/js/lib/validation/validator.js
@@ -10,6 +10,14 @@ define([
     'use strict';    
 
     return {
+
+        /**
+         * Validates value by rule and it's params.
+         * @param  {String} rule - name of the rule
+         * @param  {*} value
+         * @param  {*} params - rule configuration
+         * @return {Boolean} - true, if value is valid, false otherwise
+         */
         validate: function (rule, value, params) {
             var rule      = rules[rule],
                 validator = rule[0];
@@ -17,8 +25,13 @@ define([
             return validator(value, params);
         },
 
+        /**
+         * Returns error message assigned to the rule
+         * @param  {String} rule - name of the rule
+         * @return {String} - error message
+         */
         messageFor: function (rule) {
-            var rule = rules[rule]
+            var rule = rules[rule];
 
             return rule[1];
         }
