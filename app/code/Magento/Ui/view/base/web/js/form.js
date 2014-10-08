9b1a9ba448d76513bb2a5242bf00206f8e2cb7e2
Mykhailo Miroshnikov <mmiroshnikov@ebay.com>
2014-10-08 14:32:00 +0300

    MAGETWO-29203: Validation component integration with Form component
     - Refactored form component

--- app/code/Magento/Ui/view/base/web/js/form.js
+++ app/code/Magento/Ui/view/base/web/js/form.js
@@ -14,117 +14,115 @@ define(function (require) {
         utils       = require('mage/utils'),
         _           = require('underscore');
 
+    var defaults = {
+        elements: {}
+    };
+
     /**
-     * Concatenates path and subpath by '.' separator
-     * 
-     * @param  {String} path
-     * @param  {String} subpath
-     * @return {String} result path
+     * Defines if an object is meta descriptor by checking if one has
+     *     "meta_ref" or "input_type" properties defined
+     *     
+     * @param  {Object}  obj
+     * @return {Boolean} - true, if object is meta descriptor
      */
-    function incrementPath(path, subpath) {
-        return path + '.' + subpath;
-    }
+    function isMetaDescriptor(obj) {
+        var isMetaReference = obj.meta_ref,
+            hasInputType    = obj.input_type;
+
+        return isMetaReference || hasInputType;
+    };
 
     var Form = Scope.extend({
 
         /**
-         * Extends instance with config, initializes observable properties,
-         *     invokes initElements method.
+         * Extends instance with config, initializes instances core properties;
          *     
          * @param  {Object} config
          */
         initialize: function (config) {
-            _.extend(this, config);
-
-            this.initElements = this.initElements.bind(
-                this, 
-                this.refs.provider.meta.get(),
-                this.refs.provider.data.get()
-            );
+            _.extend(this, defaults, config);
 
             this.initElements();
         },
 
-        /**
-         * Recursively loops over initial object's properties,
-         *     checkes if property is meta descriptor, looks for "meta_ref"
-         *     property, merges found reference object, builds config for element
-         *     and invoces initElement with built config.
-         * If property is not meta descriptor, invoces itself with new path and initial
-         *     parameters.
-         *     
-         * @param  {Object} meta - reference to meta
-         * @param  {Object} data - reference to data
-         * @param  {Object} initial - target object to format
-         * @param  {String} basePath - path to initial (e.g. "customer.website")
-         */
-        initElements: function (meta, data, initial, basePath) {
-            var target,
-                isObject,
-                reference,
-                value,
-                path = '';
+        initElements: function () {
+            var provider = this.refs.provider;
 
-            initial = initial  || meta;
+            this.data = provider.data.get();
+            this.meta = provider.meta.get();
 
-            for (var name in initial) {
-                target      = initial[name];
-                isObject    = typeof target === 'object';
-                path        = basePath ? basePath + '.' + name : name;
-
-                if (isObject && this.isMetaDescriptor(target)) {
-
-                    if (reference = target['meta_ref']) {
-                        _.extend(target, meta[reference]);
-                        delete target['meta_ref'];
-                    }
-                    
-                    _.extend(target, {
-                        name: path,
-                        type: target.input_type,
-                        refs: this.refs,
-                        value: utils.nested(data, path)
-                    });
-
-                    this.initElement(target);
-                } else if (isObject) {
-                    this.initElements(target, path);            
-                }
-            }
+            this.createElements(this.meta);
         },
 
         /**
-         * Looks up for corresponding constructor in elements object,
-         *     creates an instance of it passing config as a single parameted.
-         * Invokes register method passing instance to it.
+         * Recursively loops over 'obj' object's properties,
+         *     checkes if property is meta descriptor, looks for "meta_ref"
+         *     property, merges found reference object, builds config for element
+         *     and invokes initElement with config.
+         * If property is not meta descriptor, invokes itself with new obj(nested one) and path.
+         *     
+         * @param  {Object} obj - target object to format
+         * @param  {String} basePath - path to obj (e.g. "customer.website")
+         */
+        createElements: function (obj, basePath) {
+            var reference,
+                path = '';
+
+            _.each(obj, function (element, name) {
+                element     = obj[name];
+                path        = basePath ? basePath + '.' + name : name;
+
+                if (typeof element !== 'object') {
+                    return;
+                }
+
+                isMetaDescriptor(element) 
+                    ? this.createElement(path, element)
+                    : this.createElements(element, path);
+
+            }, this);
+        },
+
+        /**
+         * Formats config by merging corresponding meta data into it,
+         *     looks up for corresponding constructor in elements object,
+         *     creates an instance of it passing prepared config as a single parameter.
+         * Invokes registerElement method passing instance and it's name to it.
          * 
          * @param  {Object} config - config for instance
          */
-        initElement: function (config) {
-            var constr  = elements[config.type],
-                element = new constr(config);
+        createElement: function (name, config) {
+            var metaReference   = config.meta_ref,
+                type            = config.input_type,
+                constr          = elements[type],
+                element;
 
-            this.register(element);
+            if (metaReference) {
+                _.extend(config, this.meta[metaReference]);
+                delete config.meta_ref;
+            }
+
+            _.extend(config, {
+                name: name,
+                type: type,
+                refs: this.refs,
+                value: utils.nested(this.data, name)
+            });
+
+            delete config.input_type;
+
+            element = new constr(config);
+            this.registerElement(name, element);
         },
 
         /**
-         * Registes element by it's name to registry
-         * 
+         * Registers element by it's name.
+         * @param  {String} name
          * @param  {Object} element
          */
-        register: function (element) {
-            registry.set(element.name, element);
-        },
-
-        /**
-         * Defines if an object is meta descriptor by checking if one has
-         *     "meta_ref" or "input_type" properties defined
-         *     
-         * @param  {Object}  obj
-         * @return {Boolean} - true, if object is meta descriptor
-         */
-        isMetaDescriptor: function (obj) {
-            return ('meta_ref' in obj) || ('input_type' in obj);
+        registerElement: function (name, element) {
+            this.elements[name] = element;
+            registry.set(name, element);
         }
     });
     
