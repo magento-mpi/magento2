924f3fb8816a53209ad1e8237071450015720f48
Diane Fleming <difleming@ebay.com>
2014-10-08 11:52:41 -0500

    MAGEDOC-1817: [DOC] Shopping Cart Items Service

--- app/code/Magento/Checkout/Service/V1/Item/WriteService.php
+++ app/code/Magento/Checkout/Service/V1/Item/WriteService.php
@@ -5,28 +5,34 @@
  * @copyright   {copyright}
  * @license     {license_link}
  */
-
 namespace Magento\Checkout\Service\V1\Item;
 
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\CouldNotSaveException;
 use Magento\Framework\Exception\InputException;
 
+/** Write service object. */
 class WriteService implements WriteServiceInterface
 {
     /**
+     * Quote repository.
+     *
      * @var \Magento\Sales\Model\QuoteRepository
      */
     protected $quoteRepository;
 
     /**
+     * Product loader.
+     *
      * @var \Magento\Catalog\Service\V1\Product\ProductLoader
      */
     protected $productLoader;
 
     /**
-     * @param \Magento\Sales\Model\QuoteRepository $quoteRepository
-     * @param \Magento\Catalog\Service\V1\Product\ProductLoader $productLoader
+     * Constructs a write service object.
+     *
+     * @param \Magento\Sales\Model\QuoteRepository $quoteRepository Quote repository.
+     * @param \Magento\Catalog\Service\V1\Product\ProductLoader $productLoader Product loader.
      */
     public function __construct(
         \Magento\Sales\Model\QuoteRepository $quoteRepository,
@@ -37,7 +43,14 @@ class WriteService implements WriteServiceInterface
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
+     *
+     * @param int $cartId The cart ID.
+     * @param \Magento\Checkout\Service\V1\Data\Cart\Item $data The item.
+     * @return int Item ID.
+     * @throws \Magento\Framework\Exception\NoSuchEntityException The specified cart does not exist.
+     * @throws \Magento\Framework\Exception\CouldNotSaveException The specified item could not be saved to the cart.
+     * @throws \Magento\Framework\Exception\InputException The specified item or cart is not valid.
      */
     public function addItem($cartId, \Magento\Checkout\Service\V1\Data\Cart\Item $data)
     {
@@ -60,7 +73,15 @@ class WriteService implements WriteServiceInterface
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
+     *
+     * @param int $cartId The cart ID.
+     * @param int $itemId The item ID of the item to be updated.
+     * @param \Magento\Checkout\Service\V1\Data\Cart\Item $data The item.
+     * @return bool
+     * @throws \Magento\Framework\Exception\NoSuchEntityException The specified item or cart does not exist.
+     * @throws \Magento\Framework\Exception\CouldNotSaveException The item could not be updated.
+     * @throws \Magento\Framework\Exception\InputException The specified item or cart is not valid.
      */
     public function updateItem($cartId, $itemId, \Magento\Checkout\Service\V1\Data\Cart\Item $data)
     {
@@ -85,11 +106,21 @@ class WriteService implements WriteServiceInterface
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
+     *
+     * @param int $cartId The cart ID.
+     * @param int $itemId The item ID of the item to be removed.
+     * @return bool
+     * @throws \Magento\Framework\Exception\NoSuchEntityException The specified item or cart does not exist.
+     * @throws \Magento\Framework\Exception\CouldNotSaveException The item could not be removed.
      */
     public function removeItem($cartId, $itemId)
     {
-        /** @var \Magento\Sales\Model\Quote $quote */
+        /**
+         * Quote.
+         *
+         * @var \Magento\Sales\Model\Quote $quote
+         */
         $quote = $this->quoteRepository->get($cartId);
         $quoteItem = $quote->getItemById($itemId);
         if (!$quoteItem) {
