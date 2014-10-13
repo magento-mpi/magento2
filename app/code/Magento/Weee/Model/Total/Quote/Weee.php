65013ca547de987c8dbb9adf761708cd73359e43
Max Y <max@magento.com>
2014-10-13 14:20:01 -0500

    MAGETWO-28279: PHP fatal error occurs when logging in as a user on checkout page to order a product with FPT.
    - fixed

--- app/code/Magento/Weee/Model/Total/Quote/Weee.php
+++ app/code/Magento/Weee/Model/Total/Quote/Weee.php
@@ -154,7 +154,15 @@ class Weee extends AbstractTotal
         $associatedTaxables = $item->getAssociatedTaxables();
         if (!$associatedTaxables) {
             $associatedTaxables = [];
+        }else{
+            // remove existing wee associated taxables
+            foreach ($associatedTaxables as $iTaxable => $taxable) {
+                if ($taxable[CommonTaxCollector::KEY_ASSOCIATED_TAXABLE_TYPE] == self::ITEM_TYPE) {
+                    unset($associatedTaxables[$iTaxable]);
+                }
+            }
         }
+
         foreach ($attributes as $key => $attribute) {
             $title          = $attribute->getName();
 
