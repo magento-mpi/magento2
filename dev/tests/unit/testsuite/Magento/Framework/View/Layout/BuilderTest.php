640e399d4d550ff6b1cd490349df7df8563c8036
vpaladiychuk <vpaladiychuk@ebay.com>
2014-10-14 16:13:22 +0300

    MAGETWO-29204: Cover new code with unit tests
    - added \Magento\Framework\View\Layout\BuilderTest.php

---
+++ dev/tests/unit/testsuite/Magento/Framework/View/Layout/BuilderTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\Framework\View\Layout;
+
+use Magento\Framework;
+
+/**
+ * Class BuilderTest
+ * @covers \Magento\Framework\View\Layout\Builder
+ */
+class BuilderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \Magento\Framework\View\Layout\Builder::build()
+     */
+    public function testBuild()
+    {
+        $fullActionName = 'route_controller_action';
+
+        /** @var Framework\App\Request\Http|\PHPUnit_Framework_MockObject_MockObject */
+        $request = $this->getMock('Magento\Framework\App\Request\Http', [], [], '', false);
+        $request->expects($this->exactly(3))->method('getFullActionName')->will($this->returnValue($fullActionName));
+
+        /** @var Framework\View\Layout\ProcessorInterface|\PHPUnit_Framework_MockObject_MockObject $processor */
+        $processor = $this->getMock('Magento\Framework\View\Layout\ProcessorInterface', [], [], '', false);
+        $processor->expects($this->once())->method('load');
+
+        /** @var Framework\View\Layout|\PHPUnit_Framework_MockObject_MockObject */
+        $layout = $this->getMock(
+            'Magento\Framework\View\Layout',
+            ['setBuilder', 'getUpdate', 'generateXml', 'generateElements'],
+            [],
+            '',
+            false
+        );
+        $layout->expects($this->atLeastOnce())->method('getUpdate')->will($this->returnValue($processor));
+        $layout->expects($this->atLeastOnce())->method('generateXml')->will($this->returnValue($processor));
+        $layout->expects($this->atLeastOnce())->method('generateElements')->will($this->returnValue($processor));
+
+        $data = ['full_action_name' => $fullActionName, 'layout' => $layout];
+        $prefix = 'controller_action_layout_';
+        /** @var Framework\Event\ManagerInterface|\PHPUnit_Framework_MockObject_MockObject $eventManager */
+        $eventManager = $this->getMock('Magento\Framework\Event\ManagerInterface', [], [], '', false);
+        $eventManager->expects($this->at(0))->method('dispatch')->with($prefix. 'load_before', $data);
+        $eventManager->expects($this->at(1))->method('dispatch')->with($prefix . 'generate_blocks_before', $data);
+        $eventManager->expects($this->at(2))->method('dispatch')->with($prefix . 'generate_blocks_after', $data);
+
+        /** @var Framework\View\Layout\Builder $builder */
+        $builder = (new \Magento\TestFramework\Helper\ObjectManager($this))
+            ->getObject(
+                'Magento\Framework\View\Layout\Builder',
+                [
+                    'eventManager' => $eventManager,
+                    'request' => $request,
+                    'layout' => $layout
+                ]
+            );
+        $builder->build();
+    }
+} 
