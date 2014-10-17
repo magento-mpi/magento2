5528c76a0ac66e7e7a150625236934f4d7a2f2ee
Diane Fleming <difleming@ebay.com>
2014-10-17 15:54:21 -0500

    MAGEDOC-1827: [DOC] Shopping Cart Shipping Method Service

--- app/code/Magento/Checkout/Service/V1/ShippingMethod/ReadService.php
+++ app/code/Magento/Checkout/Service/V1/ShippingMethod/ReadService.php
@@ -61,8 +61,9 @@ class ReadService implements ReadServiceInterface
     /**
      * {@inheritDoc}
      *
-     * @param int $cartId The cart ID.
-     * @return ShippingMethod|null Shipping method.
+     * @param int $cartId The shopping cart ID.
+     * @return \Magento\Checkout\Service\V1\Data\Cart\ShippingMethod Shipping method.
+     * @throws \Magento\Framework\Exception\NoSuchEntityException The specified shopping cart does not exist.
      * @throws \Magento\Framework\Exception\StateException The shipping address is not set.
      */
     public function getMethod($cartId)
@@ -116,8 +117,9 @@ class ReadService implements ReadServiceInterface
     /**
      * {@inheritDoc}
      *
-     * @param int $cartId The cart ID.
-     * @return array|\Magento\Checkout\Service\V1\Data\Cart\ShippingMethod[] Array of shipping methods.
+     * @param int $cartId The shopping cart ID.
+     * @return \Magento\Checkout\Service\V1\Data\Cart\ShippingMethod[] An array of shipping methods.
+     * @throws \Magento\Framework\Exception\NoSuchEntityException The specified quote does not exist.
      * @throws \Magento\Framework\Exception\StateException The shipping address is not set.
      */
     public function getList($cartId)
