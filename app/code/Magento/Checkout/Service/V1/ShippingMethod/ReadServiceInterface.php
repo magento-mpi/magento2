1d2ec1b5ebbc6124116b7f4bd8ca4a42dc7f41da
Diane Fleming <difleming@ebay.com>
2014-10-17 14:18:01 -0500

    MAGEDOC-1827: [DOC] Shopping Cart Shipping Method Service

--- app/code/Magento/Checkout/Service/V1/ShippingMethod/ReadServiceInterface.php
+++ app/code/Magento/Checkout/Service/V1/ShippingMethod/ReadServiceInterface.php
@@ -16,6 +16,7 @@ interface ReadServiceInterface
      * Returns selected shipping method for a specified quote.
      *
      * @param int $cartId The shopping cart ID.
+     * @return \Magento\Checkout\Service\V1\Data\Cart\ShippingMethod Shipping method.
      * @throws \Magento\Framework\Exception\NoSuchEntityException The specified shopping cart does not exist.
      * @throws \Magento\Framework\Exception\StateException The shipping address is not set.
      */
