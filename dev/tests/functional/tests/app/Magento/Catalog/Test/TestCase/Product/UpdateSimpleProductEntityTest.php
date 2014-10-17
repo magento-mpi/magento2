ba15641d771bf188428c44d01ed87faf98620a56
Oleksandr Rykh <orykh@ebay.com>
2014-10-17 17:41:50 +0300

    MTA-426: Test Creation for UpdateURLRewrites (change category assignment  for product)
    - Fix CR Changes

--- dev/tests/functional/tests/app/Magento/Catalog/Test/TestCase/Product/UpdateSimpleProductEntityTest.php
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/TestCase/Product/UpdateSimpleProductEntityTest.php
@@ -42,7 +42,7 @@ class UpdateSimpleProductEntityTest extends Injectable
      *
      * @var CatalogProductSimple
      */
-    protected $product;
+    protected $initialProduct;
 
     /**
      * Product page with a grid
@@ -87,7 +87,7 @@ class UpdateSimpleProductEntityTest extends Injectable
         CatalogCategory $category,
         FixtureFactory $fixtureFactory
     ) {
-        $this->product = $fixtureFactory->createByCode(
+        $this->initialProduct = $fixtureFactory->createByCode(
             'catalogProductSimple',
             [
                 'dataSet' => 'default',
@@ -98,7 +98,7 @@ class UpdateSimpleProductEntityTest extends Injectable
                 ]
             ]
         );
-        $this->product->persist();
+        $this->initialProduct->persist();
 
         $this->productGrid = $productGrid;
         $this->editProductPage = $editProductPage;
@@ -112,11 +112,11 @@ class UpdateSimpleProductEntityTest extends Injectable
      */
     public function test(CatalogProductSimple $product)
     {
-        $filter = ['sku' => $this->product->getSku()];
+        $filter = ['sku' => $this->initialProduct->getSku()];
         $this->productGrid->open()->getProductGrid()->searchAndOpen($filter);
         $this->editProductPage->getProductForm()->fill($product);
         $this->editProductPage->getFormPageActions()->save();
 
-        return ['initialProduct' => $this->product];
+        return ['initialProduct' => $this->initialProduct];
     }
 }
---
+++ dev/tests/functional/tests/app/Magento/UrlRewrite/Test/Constraint/AssertUrlRewriteUpdatedProductInGrid.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+
+namespace Magento\UrlRewrite\Test\Constraint;
+
+use Mtf\Constraint\AbstractConstraint;
+use Magento\Catalog\Test\Fixture\CatalogProductSimple;
+use Magento\UrlRewrite\Test\Page\Adminhtml\UrlRewriteIndex;
+
+/**
+ * Class AssertUrlRewriteUpdatedProductInGrid
+ * Assert that url product in grid.
+ */
+class AssertUrlRewriteUpdatedProductInGrid extends AbstractConstraint
+{
+    /**
+     * Constraint severeness.
+     *
+     * @var string
+     */
+    protected $severeness = 'low';
+
+    /**
+     * Assert that product url in url rewrite grid.
+     *
+     * @param CatalogProductSimple $product
+     * @param CatalogProductSimple $initialProduct
+     * @param UrlRewriteIndex $urlRewriteIndex
+     * @return void
+     */
+    public function processAssert(
+        CatalogProductSimple $product,
+        CatalogProductSimple $initialProduct,
+        UrlRewriteIndex $urlRewriteIndex
+    ) {
+        $urlRewriteIndex->open();
+        $category = $product->getDataFieldConfig('category_ids')['source']->getCategories()[0];
+        $targetPath = "catalog/product/view/id/{$initialProduct->getId()}/category/{$category->getId()}";
+        $url = strtolower($product->getCategoryIds()[0] . '/' . $product->getUrlKey());
+        $filter = [
+            'request_path' => $url,
+            'target_path' => $targetPath,
+        ];
+        \PHPUnit_Framework_Assert::assertTrue(
+            $urlRewriteIndex->getUrlRedirectGrid()->isRowVisible($filter, true, false),
+            "URL Rewrite with request path '$url' is absent in grid."
+        );
+
+        $categoryInitial = $initialProduct->getDataFieldConfig('category_ids')['source']->getCategories()[0];
+        $targetPath = "catalog/product/view/id/{$initialProduct->getId()}/category/{$categoryInitial->getId()}";
+
+        \PHPUnit_Framework_Assert::assertFalse(
+            $urlRewriteIndex->getUrlRedirectGrid()->isRowVisible(['target_path' => $targetPath], true, false),
+            "URL Rewrite with target path '$targetPath' is present in grid."
+        );
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return 'URL Rewrite is present in grid.';
+    }
+}
