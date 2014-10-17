272b91d5517cd9134e18115edf7aeeb773abbc26
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-10-17 15:15:40 +0300

    MAGETWO-28834: Cannot create target rule with product attribute in condition

--- app/code/Magento/CatalogRule/sql/catalogrule_setup/upgrade-1.6.0.3-1.6.0.4.php
+++ app/code/Magento/CatalogRule/sql/catalogrule_setup/upgrade-1.6.0.3-1.6.0.4.php
@@ -23,4 +23,19 @@ $installer->getConnection()->dropForeignKey(
     $installer->getFkName('catalogrule_product_price', 'website_id', 'store_website', 'website_id')
 );
 
+$table = $installer->getTable('catalogrule_product');
+$installer->getConnection()->dropForeignKey(
+    $table,
+    $installer->getFkName('catalogrule_product', 'product_id', 'catalog_product_entity', 'entity_id')
+)->dropForeignKey(
+    $table,
+    $installer->getFkName('catalogrule_product', 'customer_group_id', 'customer_group', 'customer_group_id')
+)->dropForeignKey(
+    $table,
+    $installer->getFkName('catalogrule_product', 'website_id', 'store_website', 'website_id')
+)->dropForeignKey(
+    $table,
+    $installer->getFkName('catalogrule_product', 'rule_id', 'catalogrule', 'rule_id')
+);
+
 $installer->endSetup();
