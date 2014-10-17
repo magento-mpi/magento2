9c3df4d7ebfed1f181843823b28fc05f00fd5c78
Yuxing Zheng <yuxzheng@ebay.com>
2014-10-17 17:03:39 -0500

    MAGETWO-29276: Remove Data Object dependencies from Web API Framework
     - Get rid of empty array for custom_attributes

--- app/code/Magento/Webapi/Model/DataObjectProcessor.php
+++ app/code/Magento/Webapi/Model/DataObjectProcessor.php
@@ -82,6 +82,9 @@ class DataObjectProcessor
                 $methodName !== 'getCustomAttribute') {
                 $value = $dataObject->{$methodName}();
                 if ($value !== null) {
+                    if ($methodName === 'getCustomAttributes' && $value === []) {
+                        continue;
+                    }
                     $key = SimpleDataObjectConverter::camelCaseToSnakeCase(substr($methodName, 3));
                     if ($key === AbstractExtensibleModel::CUSTOM_ATTRIBUTES_KEY) {
                         $value = $this->convertCustomAttributes($value);
