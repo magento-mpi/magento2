338118ff44256dbbd9408541c8d670738160e8fc
Evgeniy Miskov <emiskov@ebay.com>
2014-10-23 16:56:53 +0300

    MAGETWO-28694: Stabilize CI

--- app/code/Magento/Catalog/Controller/Adminhtml/Product/AlertsPriceGrid.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/AlertsPriceGrid.php
@@ -11,31 +11,9 @@ namespace Magento\Catalog\Controller\Adminhtml\Product;
 use Magento\Backend\App\Action;
 use Magento\Catalog\Controller\Adminhtml\Product;
 use Magento\Framework\Controller\Result;
-use Magento\Framework\View\Result\LayoutFactory;
 
-class AlertsPriceGrid extends \Magento\Catalog\Controller\Adminhtml\Product
+class AlertsPriceGrid extends AbstractProductGrid
 {
-    /**
-     * @var LayoutFactory
-     */
-    protected $resultLayoutFactory;
-
-    /**
-     * Constructor alert price grid
-     *
-     * @param Action\Context $context
-     * @param Builder $productBuilder
-     * @param \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
-     */
-    public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        Product\Builder $productBuilder,
-        LayoutFactory $resultLayoutFactory
-    ) {
-        parent::__construct($context, $productBuilder);
-        $this->resultLayoutFactory = $resultLayoutFactory;
-    }
-
     /**
      * Get alerts price grid
      *
--- app/code/Magento/Catalog/Controller/Adminhtml/Product/OptionsImportGrid.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/OptionsImportGrid.php
@@ -8,27 +8,8 @@
  */
 namespace Magento\Catalog\Controller\Adminhtml\Product;
 
-class OptionsImportGrid extends \Magento\Catalog\Controller\Adminhtml\Product
+class OptionsImportGrid extends AbstractProductGrid
 {
-    /**
-     * @var \Magento\Framework\View\Result\LayoutFactory
-     */
-    protected $resultLayoutFactory;
-
-    /**
-     * @param \Magento\Backend\App\Action\Context $context
-     * @param \Magento\Catalog\Controller\Adminhtml\Product\Builder $productBuilder
-     * @param \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
-     */
-    public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        \Magento\Catalog\Controller\Adminhtml\Product\Builder $productBuilder,
-        \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
-    ) {
-        parent::__construct($context, $productBuilder);
-        $this->resultLayoutFactory = $resultLayoutFactory;
-    }
-
     /**
      * Show product grid for custom options import popup
      *
