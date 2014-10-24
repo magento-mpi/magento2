94f2789845f534c99cd957dde482d380b9acbb82
Mykhailo Miroshnikov <mmiroshnikov@ebay.com>
2014-10-24 11:33:05 +0300

    MAGETWO-29218: Implement "collection" data type container
     - Backeng adjustments (naming)

--- app/code/Magento/Ui/view/base/web/js/form/components/collection/item.js
+++ app/code/Magento/Ui/view/base/web/js/form/components/collection/item.js
@@ -15,7 +15,7 @@ define([
         active: false,
         template: 'ui/form/components/collection/item',
         defaultDisplayArea: 'body',
-        defaultTitle: '',
+        defaultLabel: '',
         separator: ' '
     };
 
@@ -52,28 +52,28 @@ define([
         },
 
         initTitle: function () {
-            this.titleConfig = this.title || {};
-            this.title       = ko.computed(this.compositeTitle.bind(this));
+            this.labelConfig = this.label || {};
+            this.label       = ko.computed(this.compositeLabel.bind(this));
 
             return this;
         },
 
-        compositeTitle: function () {
-            var config          = this.titleConfig,
-                defaultTitle    = config['default'] || this.defaultTitle,
-                separator       = config.separator  || this.separator,
-                elements,
-                parts           = config.composite_of,
+        compositeLabel: function () {
+            var config          = this.labelConfig,
+                defaultLabel    = config['default'] || this.defaultLabel,
+                separator       = this.separator,
+                parts           = config.compositeOf,
                 indexed         = this.elems.indexBy('index'),
-                title           = '',
-                getValues       = this.getValues.bind(this, separator);
+                getValues       = this.getValues.bind(this, separator),
+                label           = '',
+                elements;
 
             if (parts) {
                 elements    = parts.map(function (part) { return indexed[part] });
-                title       = _.compact(elements).map(getValues).join(separator).trim();
+                label       = _.compact(elements).map(getValues).join(separator).trim();
             }
 
-            return title || defaultTitle;
+            return label || defaultLabel;
         },
 
         getValues: function (separator, element) {
@@ -83,7 +83,7 @@ define([
         },
 
         updateDisplayed: function (element) {
-            var config              = this.display || [],
+            var config              = this.previewElements || [],
                 shouldBeDisplayed   = !!~config.indexOf(element.index);
 
             if (shouldBeDisplayed) {
