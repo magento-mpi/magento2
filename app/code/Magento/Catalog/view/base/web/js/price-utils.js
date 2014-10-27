b3bff334b278fef3ec9d3baed3cf10da12239777
Anton Guz <aguz@ebay.com>
2014-10-27 19:43:19 +0200

    MAGETWO-29825: Prepare basic architecture for Simple Product Price View component (SPPV)
     - Price utils

---
+++ app/code/Magento/Catalog/view/base/web/js/price-utils.js
@@ -0,0 +1,73 @@
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/*jshint browser:true jquery:true*/
+/*global Handlebars*/
+define([
+    'underscore'
+], function (_) {
+
+    var globalPriceFormat = {
+        requiredPrecision: 2,
+        integerRequired: 1,
+        decimalSymbol: ',',
+        groupSymbol: ',',
+        groupLength: ','
+    };
+
+    return {
+        formatPrice: formatPrice,
+        deepClone: objectDeepClone,
+        strPad: stringPad
+    };
+
+
+    function formatPrice(amount, format, isShowSign) {
+        format = _.extend(globalPriceFormat, format);
+
+        // copied from price-option.js | Could be refactored with varien/js.js
+
+        var precision = isNaN(format.requiredPrecision = Math.abs(format.requiredPrecision)) ? 2 : format.requiredPrecision,
+            integerRequired = isNaN(format.integerRequired = Math.abs(format.integerRequired)) ? 1 : format.integerRequired,
+            decimalSymbol = format.decimalSymbol === undefined ? ',' : format.decimalSymbol,
+            groupSymbol = format.groupSymbol === undefined ? '.' : format.groupSymbol,
+            groupLength = format.groupLength === undefined ? 3 : format.groupLength,
+            s = '';
+
+        if (isShowSign === undefined || isShowSign === true) {
+            s = amount < 0 ? '-' : ( isShowSign ? '+' : '');
+        } else if (isShowSign === false) {
+            s = '';
+        }
+        var i = parseInt(amount = Math.abs(+amount || 0).toFixed(precision), 10) + '',
+            pad = (i.length < integerRequired) ? (integerRequired - i.length) : 0;
+
+        i = stringPad('0', pad) + i;
+
+        var j = i.length > groupLength ? i.length % groupLength : 0,
+            re = new RegExp('(\\d{' + groupLength + '})(?=\\d)', 'g');
+
+
+        // replace(/-/, 0) is only for fixing Safari bug which appears
+        // when Math.abs(0).toFixed() executed on '0' number.
+        // Result is '0.-0' :(
+        var r = (j ? i.substr(0, j) + groupSymbol : '') +
+                i.substr(j).replace(re, '$1' + groupSymbol) +
+                (precision ? decimalSymbol + Math.abs(amount - i).toFixed(precision).replace(/-/, 0).slice(2) : ''),
+            pattern = format.pattern.indexOf('{sign}') < 0 ? s + format.pattern : format.pattern.replace('{sign}', s);
+        return pattern.replace('%s', r).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+
+    }
+
+    function stringPad(string, times) {
+        return (new Array(times + 1)).join(string);
+    }
+
+    function objectDeepClone(obj) {
+        return JSON.parse(JSON.stringify(obj));
+    }
+
+});
