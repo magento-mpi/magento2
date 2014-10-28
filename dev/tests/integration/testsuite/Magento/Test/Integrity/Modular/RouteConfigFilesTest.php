eb792c862534ed768650023efcc18bae0543cb9d
Maddy Chellathurai <mchellathura@ebay.com>
2014-10-28 14:26:15 -0500

    MAGETWO-29770: Use new setup application in API Functional tests
    - fixing build errors

--- dev/tests/integration/testsuite/Magento/Test/Integrity/Modular/RouteConfigFilesTest.php
+++ dev/tests/integration/testsuite/Magento/Test/Integrity/Modular/RouteConfigFilesTest.php
@@ -36,18 +36,15 @@ class RouteConfigFilesTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        global $magentoBaseDir;
-
-        $this->_schemaFile = $magentoBaseDir . '/lib/internal/Magento/Framework/App/etc/routes.xsd';
-        $this->_mergedSchemaFile = $magentoBaseDir . '/lib/internal/Magento/Framework/App/etc/routes_merged.xsd';
+        $this->_schemaFile = BP . '/lib/internal/Magento/Framework/App/etc/routes.xsd';
+        $this->_mergedSchemaFile = BP . '/lib/internal/Magento/Framework/App/etc/routes_merged.xsd';
     }
 
     public function testRouteConfigsValidation()
     {
-        global $magentoBaseDir;
         $invalidFiles = array();
 
-        $mask = $magentoBaseDir . '/app/code/*/*/etc/*/routes.xml';
+        $mask = BP . '/app/code/*/*/etc/*/routes.xml';
         $files = glob($mask);
         $mergedConfig = new \Magento\Framework\Config\Dom('<config></config>', $this->_idAttributes);
 
@@ -71,7 +68,7 @@ class RouteConfigFilesTest extends \PHPUnit_Framework_TestCase
             $errors = array();
             $mergedConfig->validate($this->_mergedSchemaFile, $errors);
         } catch (\Exception $e) {
-            $this->fail('Merged routes config is invalid: ' . "\n" . implode("\n", $errors) . $e.getMessage());
+            $this->fail('Merged routes config is invalid: ' . "\n" . implode("\n", $errors));
         }
     }
 }
