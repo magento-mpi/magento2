169e7ac929f8227ec2815e3e262637901ba5b1c6
Dale Sikkema <dsikkema@ebay.com>
2014-10-28 13:38:24 -0500

    MAGETWO-29017: Decouple code generation and IncludePath  - updated integration test per CR comments

--- dev/tests/integration/testsuite/Magento/Framework/Code/Generator/FileResolverTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/Code/Generator/FileResolverTest.php
@@ -28,16 +28,25 @@ class FileResolverTest extends \PHPUnit_Framework_TestCase {
      */
     protected $model;
 
+    /**
+     * @var string original include-path variable
+     */
+    protected $originalPath;
+
     public function setUp()
     {
         $this->model = Bootstrap::getObjectManager()->create('Magento\Framework\Code\Generator\FileResolver');
+        $this->originalPath = get_include_path();
+        set_include_path('/pre/existing/paths/');
+    }
+
+    public function tearDown()
+    {
+        set_include_path($this->originalPath);
     }
 
     public function testAddIncludePathPrepend()
     {
-        $originalIncludePath = get_include_path();
-        set_include_path('/pre/existing/paths/');
-
         $this->model->addIncludePath(self::FIRST_PATH);
         $this->model->addIncludePath(self::SECOND_PATH);
 
@@ -46,15 +55,10 @@ class FileResolverTest extends \PHPUnit_Framework_TestCase {
             self::SECOND_PATH,
             $postIncludePath
         );
-
-        set_include_path($originalIncludePath);
     }
 
     public function testAddIncludePathAppend()
     {
-        $originalIncludePath = get_include_path();
-        set_include_path('/pre/existing/paths/');
-
         $this->model->addIncludePath(self::FIRST_PATH, false);
         $this->model->addIncludePath(self::SECOND_PATH, false);
 
@@ -63,20 +67,14 @@ class FileResolverTest extends \PHPUnit_Framework_TestCase {
             self::SECOND_PATH,
             $postIncludePath
         );
-
-        set_include_path($originalIncludePath);
     }
 
     public function testGetFile()
     {
-        $originalIncludePath = get_include_path();
-        set_include_path('/pre/existing/paths/');
         $includePath = realpath(__DIR__ . '/../_files/');
         $className = '\ClassToFind';
 
         $this->model->addIncludePath($includePath);
         $this->assertFileExists($this->model->getFile($className));
-
-        set_include_path($originalIncludePath);
     }
 }
