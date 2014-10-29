c1b34e7052279d5a7c1ffb5bb08eba883a9dfbda
Dale Sikkema <dsikkema@ebay.com>
2014-10-28 23:28:42 -0500

    MAGETWO-29017: Decouple code generation and IncludePath  - add comments to fix sanity tests

--- dev/tests/integration/testsuite/Magento/Framework/Code/_files/ClassToFind.php
+++ dev/tests/integration/testsuite/Magento/Framework/Code/_files/ClassToFind.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * {license_notice}
+ *
+ * @copyright  {copyright}
+ * @license    {license_link}
+ * 
  * Exists for testing FileResolver.
  *
  * \Magento\Framework\Code\Generator\FileResolverTest
