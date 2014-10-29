d6b4c6a8a41631a506eca64a6a83aff523ffbffe
Ievgen Shakhsuvarov <ishakhsuvarov@ebay.com>
2014-10-29 17:26:25 +0200

    MAGETWO-29864: Cover Product Links Interfaces with unit tests

---
+++ dev/tests/unit/testsuite/Magento/Catalog/Model/Product/LinkTypeProviderTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ishakhsuvarov
+ * Date: 29.10.2014
+ * Time: 11:37
+ */ 
---
+++ dev/tests/unit/testsuite/Magento/Catalog/Model/ProductLink/RepositoryTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ishakhsuvarov
+ * Date: 28.10.2014
+ * Time: 16:24
+ */ 
