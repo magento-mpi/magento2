e165cf8ce13c3e2fe832d5dd71af22e5a8c418c4
Maksym Iakusha <miakusha@ebay.com>
2014-10-30 11:20:48 +0200

    MAGETWO-29974: Merge SQL and Data Upgrades - MX Domain

--- app/code/Magento/CatalogEvent/data/catalogevent_setup/data-install-2.0.0.php
+++ app/code/Magento/CatalogEvent/data/catalogevent_setup/data-install-2.0.0.php
@@ -7,16 +7,16 @@
  */
 /** @var \Magento\CatalogEvent\Model\Resource\Setup $this */
 
-$this->addAttribute('quote_item', 'event_id', array('type' => \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER));
-$this->addAttribute('order_item', 'event_id', array('type' => \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER));
+$this->addAttribute('quote_item', 'event_id', ['type' => \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER]);
+$this->addAttribute('order_item', 'event_id', ['type' => \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER]);
 
-$cmsBlock = array(
+$cmsBlock = [
     'title' => 'Catalog Events Lister',
     'identifier' => 'catalog_events_lister',
     'content' => '{{block class="Magento\\\\CatalogEvent\\\\Block\\\\Event\\\\Lister" name="catalog.event.lister" template="lister.phtml"}}',
     'is_active' => 1,
     'stores' => 0
-);
+];
 
 /** @var \Magento\Cms\Model\Block $block */
 $block = $this->getBlockFactory()->create();
