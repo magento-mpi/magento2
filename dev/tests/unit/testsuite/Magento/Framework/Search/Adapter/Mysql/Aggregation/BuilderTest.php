2a6df5f415c07d221fd3dfdc0583332b74976321
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-10-31 17:31:58 +0200

    MAGETWO-28305: Integrate Search library into Layered Navigation functionality
     - MAGETWO-29744: Integrate dynamic algorithms to Layered Navigation
    
     - Fixed BuilderTest

--- dev/tests/unit/testsuite/Magento/Framework/Search/Adapter/Mysql/Aggregation/BuilderTest.php
+++ dev/tests/unit/testsuite/Magento/Framework/Search/Adapter/Mysql/Aggregation/BuilderTest.php
@@ -128,7 +128,7 @@ class BuilderTest extends \PHPUnit_Framework_TestCase
             ->setMethods(['getConnection'])
             ->disableOriginalConstructor()
             ->getMock();
-        $this->resource->expects($this->once())->method('getConnection')->willReturn($this->adapter);
+        $this->resource->expects($this->any())->method('getConnection')->willReturn($this->adapter);
 
         $this->builder = $helper->getObject(
             'Magento\Framework\Search\Adapter\Mysql\Aggregation\Builder',
@@ -152,11 +152,10 @@ class BuilderTest extends \PHPUnit_Framework_TestCase
         ];
 
         $this->bucket->expects($this->once())->method('getName')->willReturn('name');
-        $this->dataProvider->expects($this->once())->method('getDataSet')->willReturn($this->select);
         $this->entityMetadata->expects($this->once())->method('getEntityId')->willReturn('product_id');
-        $this->adapter->expects($this->once())->method('fetchAssoc')->willReturn($fetchResult['name']);
         $this->request->expects($this->once())->method('getAggregation')->willReturn([$this->bucket]);
-        $this->bucketBuilder->expects($this->once())->method('build')->willReturn($this->select);
+        $this->request->expects($this->once())->method('getDimensions')->willReturn([]);
+        $this->bucketBuilder->expects($this->once())->method('build')->willReturn($fetchResult['name']);
 
         $result = $this->builder->build($this->request, $documents);
 
