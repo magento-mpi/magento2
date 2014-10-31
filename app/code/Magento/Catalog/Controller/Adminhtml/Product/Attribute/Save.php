20842fe09849df4e4f48057f50c921a7e1607c4c
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-10-31 15:14:38 +0200

    MAGETWO-28410: [GoInc] Unit and Integration Test Coverage
     - CR Changes

--- app/code/Magento/Catalog/Controller/Adminhtml/Product/Attribute/Save.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/Attribute/Save.php
@@ -13,28 +13,83 @@ use Magento\Catalog\Model\Product\AttributeSet\AlreadyExistsException;
 class Save extends \Magento\Catalog\Controller\Adminhtml\Product\Attribute
 {
     /**
+     * @var \Magento\Catalog\Model\Product\AttributeSet\BuildFactory
+     */
+    protected $buildFactory;
+
+    /**
+     * @var \Magento\Framework\Filter\FilterManager
+     */
+    protected $filterManager;
+
+    /**
+     * @var \Magento\Catalog\Helper\Product
+     */
+    protected $productHelper;
+
+    /**
+     * @var \Magento\Catalog\Model\Resource\Eav\AttributeFactory
+     */
+    protected $attributeFactory;
+
+    /**
+     * @var \Magento\Eav\Model\Adminhtml\System\Config\Source\Inputtype\ValidatorFactory
+     */
+    protected $validatorFactory;
+
+    /**
+     * @var \Magento\Eav\Model\Resource\Entity\Attribute\Group\CollectionFactory
+     */
+    protected $groupCollectionFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\Cache\FrontendInterface $attributeLabelCache
+     * @param \Magento\Framework\Registry $coreRegistry
+     * @param \Magento\Catalog\Model\Product\AttributeSet\BuildFactory $buildFactory
+     * @param \Magento\Catalog\Model\Resource\Eav\AttributeFactory $attributeFactory
+     * @param \Magento\Eav\Model\Adminhtml\System\Config\Source\Inputtype\ValidatorFactory $validatorFactory
+     * @param \Magento\Eav\Model\Resource\Entity\Attribute\Group\CollectionFactory $groupCollectionFactory
+     * @param \Magento\Framework\Filter\FilterManager $filterManager
+     * @param \Magento\Catalog\Helper\Product $productHelper
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Cache\FrontendInterface $attributeLabelCache,
+        \Magento\Framework\Registry $coreRegistry,
+        \Magento\Catalog\Model\Product\AttributeSet\BuildFactory $buildFactory,
+        \Magento\Catalog\Model\Resource\Eav\AttributeFactory $attributeFactory,
+        \Magento\Eav\Model\Adminhtml\System\Config\Source\Inputtype\ValidatorFactory $validatorFactory,
+        \Magento\Eav\Model\Resource\Entity\Attribute\Group\CollectionFactory $groupCollectionFactory,
+        \Magento\Framework\Filter\FilterManager $filterManager,
+        \Magento\Catalog\Helper\Product $productHelper
+    ) {
+        $this->buildFactory = $buildFactory;
+        $this->filterManager = $filterManager;
+        $this->productHelper = $productHelper;
+        $this->attributeFactory = $attributeFactory;
+        $this->validatorFactory = $validatorFactory;
+        $this->groupCollectionFactory = $groupCollectionFactory;
+        parent::__construct($context, $attributeLabelCache, $coreRegistry);
+    }
+
+    /**
      * @return void
      */
     public function execute()
     {
         $data = $this->getRequest()->getPost();
         if ($data) {
-            /** @var $session \Magento\Backend\Model\Auth\Session */
-            $session = $this->_objectManager->get('Magento\Backend\Model\Session');
             $setId = $this->getRequest()->getParam('set');
 
             $attributeSet = null;
             if (!empty($data['new_attribute_set_name'])) {
-                $name = $this->_objectManager->get(
-                    'Magento\Framework\Filter\FilterManager'
-                )->stripTags(
-                    $data['new_attribute_set_name']
-                );
+                $name = $this->filterManager->stripTags($data['new_attribute_set_name']);
                 $name = trim($name);
 
                 try {
                     /** @var $attributeSet \Magento\Eav\Model\Entity\Attribute\Set */
-                    $attributeSet = $this->_objectManager->create('Magento\Catalog\Model\Product\AttributeSet\Build')
+                    $attributeSet = $this->buildFactory->create()
                         ->setEntityTypeId($this->_entityTypeId)
                         ->setSkeletonId($setId)
                         ->setName($name)
@@ -53,16 +108,14 @@ class Save extends \Magento\Catalog\Controller\Adminhtml\Product\Attribute
 
             $redirectBack = $this->getRequest()->getParam('back', false);
             /* @var $model \Magento\Catalog\Model\Resource\Eav\Attribute */
-            $model = $this->_objectManager->create('Magento\Catalog\Model\Resource\Eav\Attribute');
-            /* @var $helper \Magento\Catalog\Helper\Product */
-            $helper = $this->_objectManager->get('Magento\Catalog\Helper\Product');
+            $model = $this->attributeFactory->create();
 
             $attributeId = $this->getRequest()->getParam('attribute_id');
 
             $attributeCode = $this->getRequest()->getParam('attribute_code');
             $frontendLabel = $this->getRequest()->getParam('frontend_label');
             $attributeCode = $attributeCode ?: $this->generateCode($frontendLabel[0]);
-            if (strlen($attributeCode) > 0) {
+            if (strlen($this->getRequest()->getParam('attribute_code')) > 0) {
                 $validatorAttrCode = new \Zend_Validate_Regex(array('pattern' => '/^[a-z][a-z_0-9]{0,30}$/'));
                 if (!$validatorAttrCode->isValid($attributeCode)) {
                     $this->messageManager->addError(
@@ -81,9 +134,7 @@ class Save extends \Magento\Catalog\Controller\Adminhtml\Product\Attribute
             //validate frontend_input
             if (isset($data['frontend_input'])) {
                 /** @var $inputType \Magento\Eav\Model\Adminhtml\System\Config\Source\Inputtype\Validator */
-                $inputType = $this->_objectManager->create(
-                    'Magento\Eav\Model\Adminhtml\System\Config\Source\Inputtype\Validator'
-                );
+                $inputType = $this->validatorFactory->create();
                 if (!$inputType->isValid($data['frontend_input'])) {
                     foreach ($inputType->getMessages() as $message) {
                         $this->messageManager->addError($message);
@@ -103,7 +154,7 @@ class Save extends \Magento\Catalog\Controller\Adminhtml\Product\Attribute
                 // entity type check
                 if ($model->getEntityTypeId() != $this->_entityTypeId) {
                     $this->messageManager->addError(__('You can\'t update your attribute.'));
-                    $session->setAttributeData($data);
+                    $this->_session->setAttributeData($data);
                     $this->_redirect('catalog/*/');
                     return;
                 }
@@ -115,8 +166,12 @@ class Save extends \Magento\Catalog\Controller\Adminhtml\Product\Attribute
                 /**
                  * @todo add to helper and specify all relations for properties
                  */
-                $data['source_model'] = $helper->getAttributeSourceModelByInputType($data['frontend_input']);
-                $data['backend_model'] = $helper->getAttributeBackendModelByInputType($data['frontend_input']);
+                $data['source_model'] = $this->productHelper->getAttributeSourceModelByInputType(
+                    $data['frontend_input']
+                );
+                $data['backend_model'] = $this->productHelper->getAttributeBackendModelByInputType(
+                    $data['frontend_input']
+                );
             }
 
             $data += array('is_filterable' => 0, 'is_filterable_in_search' => 0, 'apply_to' => array());
@@ -145,14 +200,10 @@ class Save extends \Magento\Catalog\Controller\Adminhtml\Product\Attribute
             $groupCode = $this->getRequest()->getParam('group');
             if ($setId && $groupCode) {
                 // For creating product attribute on product page we need specify attribute set and group
-                $attributeSetId = !is_null($attributeSet) ? $attributeSet->getId() : $setId;
-                $groupCollection = !is_null($attributeSet)
+                $attributeSetId = $attributeSet ? $attributeSet->getId() : $setId;
+                $groupCollection = $attributeSet
                     ? $attributeSet->getGroups()
-                    : $this->_objectManager->create(
-                        'Magento\Eav\Model\Resource\Entity\Attribute\Group\Collection'
-                    )->setAttributeSetFilter(
-                        $attributeSetId
-                    )->load();
+                    : $this->groupCollectionFactory->create()->setAttributeSetFilter($attributeSetId)->load();
                 foreach ($groupCollection as $group) {
                     if ($group->getAttributeGroupCode() == $groupCode) {
                         $attributeGroupId = $group->getAttributeGroupId();
@@ -168,7 +219,7 @@ class Save extends \Magento\Catalog\Controller\Adminhtml\Product\Attribute
                 $this->messageManager->addSuccess(__('You saved the product attribute.'));
 
                 $this->_attributeLabelCache->clean();
-                $session->setAttributeData(false);
+                $this->_session->setAttributeData(false);
                 if ($this->getRequest()->getParam('popup')) {
                     $requestParams = array(
                         'attributeId' => $this->getRequest()->getParam('product'),
@@ -188,7 +239,7 @@ class Save extends \Magento\Catalog\Controller\Adminhtml\Product\Attribute
                 return;
             } catch (\Exception $e) {
                 $this->messageManager->addError($e->getMessage());
-                $session->setAttributeData($data);
+                $this->_session->setAttributeData($data);
                 $this->_redirect('catalog/*/edit', array('attribute_id' => $attributeId, '_current' => true));
                 return;
             }
