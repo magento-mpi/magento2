f35c3996ab973e055c6f25372e78cabda849cfd0
Pavlo Cherniavskyi <pcherniavsky@ebay.com>
2014-10-31 15:42:42 +0200

    MAGETWO-28025: Save only payment method on savePayment step

--- dev/tests/unit/testsuite/Magento/Payment/Model/InfoTest.php
+++ dev/tests/unit/testsuite/Magento/Payment/Model/InfoTest.php
@@ -223,4 +223,14 @@ class InfoTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($this->info->hasAdditionalInformation('key2'));
         $this->assertTrue($this->info->hasAdditionalInformation());
     }
+
+    public function testInitAdditionalInformationWithUnserialize()
+    {
+        $this->assertFalse($this->info->hasAdditionalInformation());
+
+        $data = serialize(['key1' => 'data1', 'key2' => 'data2']);
+        $this->info->setData('additional_information', $data);
+
+        $this->assertEquals(unserialize($data), $this->info->getAdditionalInformation());
+    }
 }
