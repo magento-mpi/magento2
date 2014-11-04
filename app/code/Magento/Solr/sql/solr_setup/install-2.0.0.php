657ab806ae3222153d4d6b4a86f16f104d0319d5
Maksym Iakusha <miakusha@ebay.com>
2014-11-04 12:33:04 +0200

    MAGETWO-29974: Merge SQL and Data Upgrades - MX Domain

--- app/code/Magento/Solr/sql/solr_setup/install-2.0.0.php
+++ app/code/Magento/Solr/sql/solr_setup/install-2.0.0.php
@@ -14,23 +14,23 @@ $installer->startSetup();
 $installer->getConnection()->addColumn(
     $installer->getTable('catalog_eav_attribute'),
     'search_weight',
-    array(
+    [
         'type' => \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
         'unsigned' => true,
         'nullable' => false,
         'default' => '1',
         'comment' => 'Search Weight'
-    )
+    ]
 );
 $installer->getConnection()->addIndex(
     $installer->getTable('search_query'),
-    $installer->getIdxName('search_query', array('num_results')),
+    $installer->getIdxName('search_query', ['num_results']),
     'num_results'
 );
 $installer->getConnection()->addIndex(
     $installer->getTable('search_query'),
-    $installer->getIdxName('search_query', array('query_text', 'store_id', 'num_results')),
-    array('query_text', 'store_id', 'num_results')
+    $installer->getIdxName('search_query', ['query_text', 'store_id', 'num_results']),
+    ['query_text', 'store_id', 'num_results']
 );
 
 $table = $installer->getConnection()->newTable(
@@ -39,19 +39,19 @@ $table = $installer->getConnection()->newTable(
     'id',
     \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
     null,
-    array('identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true),
+    ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
     'Id'
 )->addColumn(
     'query_id',
     \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
     null,
-    array('unsigned' => true, 'nullable' => false, 'default' => '0'),
+    ['unsigned' => true, 'nullable' => false, 'default' => '0'],
     'Query Id'
 )->addColumn(
     'relation_id',
     \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
     null,
-    array('unsigned' => true, 'nullable' => false, 'default' => '0'),
+    ['unsigned' => true, 'nullable' => false, 'default' => '0'],
     'Relation Id'
 )->addForeignKey(
     $installer->getFkName('catalogsearch_recommendations', 'query_id', 'search_query', 'query_id'),
@@ -67,9 +67,7 @@ $table = $installer->getConnection()->newTable(
     'query_id',
     \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE,
     \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
-)->setComment(
-    'Enterprise Search Recommendations'
-);
+)->setComment('Enterprise Search Recommendations');
 $installer->getConnection()->createTable($table);
 
 $installer->endSetup();
