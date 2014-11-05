6980a5aff7882c7e1718f7db15a60193e8fec624
Dmytro Bursak <dbursak@ebay.com>
2014-11-05 12:41:45 +0200

    MTA-615:  Stabilize CustomerSegment module

--- dev/tests/functional/tests/app/Magento/CustomerSegment/Test/TestCase/UpdateCustomerSegmentEntityTest.php
+++ dev/tests/functional/tests/app/Magento/CustomerSegment/Test/TestCase/UpdateCustomerSegmentEntityTest.php
@@ -163,6 +163,7 @@ class UpdateCustomerSegmentEntityTest extends Injectable
         CustomerSegment $customerSegment,
         CustomerSegment $customerSegmentOriginal
     ) {
+//        $this->markTestIncomplete('MAGETWO-30226');
         //Preconditions
         $customer->persist();
         $this->customerIndexPage->open();
