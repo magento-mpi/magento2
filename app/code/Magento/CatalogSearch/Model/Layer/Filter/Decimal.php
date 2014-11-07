caa14f5daa2f01309d80cb02365b2fbfbc8df02c
Andrii Kasian <akasian@ebay.com>
2014-11-07 11:19:23 +0200

    MAGETWO-28305: Integrate Search library into Layered Navigation functionality
     - MAGETWO-30254:    Implement decimal filter

--- app/code/Magento/CatalogSearch/Model/Layer/Filter/Decimal.php
+++ app/code/Magento/CatalogSearch/Model/Layer/Filter/Decimal.php
@@ -12,4 +12,101 @@ namespace Magento\CatalogSearch\Model\Layer\Filter;
  */
 class Decimal extends \Magento\Catalog\Model\Layer\Filter\Decimal
 {
+    /**
+     * Get data array for building attribute filter items
+     *
+     * @return array
+     */
+    protected function _getItemsData()
+    {
+        $attribute = $this->getAttributeModel();
+        $this->_requestVar = $attribute->getAttributeCode();
+
+        $productCollection = $this->getLayer()->getProductCollection();
+        $facets = $productCollection->getFacetedData($attribute->getAttributeCode());
+
+        $data = [];
+        if (count($facets) > 1) { // two range minimum
+            foreach ($facets as $key => $aggregation) {
+                $count = $aggregation['count'];
+                list($from, $to) = explode('_', $key);
+                if ($from == '*') {
+                    $from = '';
+                }
+                if ($to== '*') {
+                    $to= '';
+                }
+                $label = $this->renderRangeLabel(
+                    empty($from) ? 0 : $from,
+                    empty($to) ? $to: $to
+                );
+                $value =  $from . '-' . $to;
+
+
+                $data[] = [
+                    'label' => $label,
+                    'value' => $value,
+                    'count' => $count,
+                    'from' => $from,
+                    'to' => $to
+                ];
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * Prepare text of range label
+     *
+     * @param float|string $fromPrice
+     * @param float|string $toPrice
+     * @return string
+     */
+    protected function renderRangeLabel($fromPrice, $toPrice)
+    {
+        $formattedFromPrice = $this->priceCurrency->format($fromPrice);
+        if ($toPrice === '') {
+            return __('%1 and above', $formattedFromPrice);
+        } else {
+            if ($fromPrice != $toPrice) {
+                $toPrice -= .01;
+            }
+            return __('%1 - %2', $formattedFromPrice, $this->priceCurrency->format($toPrice));
+        }
+    }
+
+    /**
+     * Apply price range filter
+     *
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @return $this
+     */
+    public function apply(\Magento\Framework\App\RequestInterface $request)
+    {
+        /**
+         * Filter must be string: $fromPrice-$toPrice
+         */
+        $filter = $request->getParam($this->getRequestVar());
+        if (!$filter || is_array($filter)) {
+            return $this;
+        }
+
+
+        list($from, $to) = explode('-', $filter);
+
+        $this->getLayer()
+            ->getProductCollection()
+            ->addFieldToFilter(
+                $this->getAttributeModel()->getAttributeCode(),
+                ['from' => $from, 'to' => $to]
+            );
+
+
+        $this->getLayer()->getState()->addFilter(
+            $this->_createItem($this->renderRangeLabel(empty($from) ? 0 : $from, $to), $filter)
+        );
+
+        return $this;
+    }
 }
