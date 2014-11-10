9414bd394f3e77105abd374bfdf60a44422d041e
Mike Weis <miweis@ebay.com>
2014-11-10 11:38:35 -0600

    MAGETWO-26320: Bundle Product price gets doubled during backend order creation
     - fixed

--- app/code/Magento/Catalog/view/adminhtml/web/catalog/product/composite/configure.js
+++ app/code/Magento/Catalog/view/adminhtml/web/catalog/product/composite/configure.js
@@ -246,6 +246,7 @@ ProductConfigure.prototype = {
      * Do submit configured data through iFrame if needed
      */
     onConfirmBtn: function() {
+        this.clean('current');
         if (jQuery(this.blockForm).valid()) {
             if (this.listTypes[this.current.listType].urlConfirm) {
                 this.submit();
--- app/code/Magento/Sales/Model/Quote.php
+++ app/code/Magento/Sales/Model/Quote.php
@@ -1338,7 +1338,7 @@ class Quote extends \Magento\Framework\Model\AbstractModel
             if (!$parentItem) {
                 $parentItem = $item;
             }
-            if ($parentItem && $candidate->getParentProductId()) {
+            if ($parentItem && $candidate->getParentProductId() && !$item->getId()) {
                 $item->setParentItem($parentItem);
             }
 
