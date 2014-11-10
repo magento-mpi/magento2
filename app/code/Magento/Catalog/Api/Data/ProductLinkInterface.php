24ebffc8399bc0f3de94038a0210d3836af5da4c
Stanislav Idolov <sidolov@ebay.com>
2014-11-10 17:04:25 +0200

    MAGETWO-29876: Build stabilization

--- app/code/Magento/Catalog/Api/Data/ProductLinkInterface.php
+++ app/code/Magento/Catalog/Api/Data/ProductLinkInterface.php
@@ -15,6 +15,7 @@ interface ProductLinkInterface extends \Magento\Framework\Api\ExtensibleDataInte
     const LINKED_PRODUCT_SKU = 'linked_product_sku';
     const LINKED_PRODUCT_TYPE = 'linked_product_type';
     const POSITION = 'position';
+    const CUSTOM_ATTRIBUTES = 'custom_attributes';
 
     /**
      * Get product SKU
