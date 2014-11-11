11168d49324a6fac65295f29a79292936d6a5ede
Ievgen Shakhsuvarov <ishakhsuvarov@ebay.com>
2014-11-11 12:25:23 +0200

    MAGETWO-29604: Notice that product with enabled MAP not displaying in require attention block in shopping cart Covered changes with unit test

--- dev/tests/unit/testsuite/Magento/Checkout/Block/Cart/Item/RendererTest.php
+++ dev/tests/unit/testsuite/Magento/Checkout/Block/Cart/Item/RendererTest.php
@@ -7,12 +7,10 @@
  */
 namespace Magento\Checkout\Block\Cart\Item;
 
-use Magento\Checkout\Block\Cart\Item\Renderer;
-
 class RendererTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var \Magento\Catalog\Helper\Image|\PHPUnit_Framework_MockObject_MockObject
+     * @var \PHPUnit_Framework_MockObject_MockObject
      */
     protected $_imageHelper;
 
@@ -22,17 +20,22 @@ class RendererTest extends \PHPUnit_Framework_TestCase
     protected $_renderer;
 
     /**
-     * @var \Magento\View\LayoutInterface|\PHPUnit_Framework_MockObject_MockObject
+     * @var \PHPUnit_Framework_MockObject_MockObject
      */
     protected $layout;
 
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $msrpHelper;
+
     protected function setUp()
     {
-        parent::setUp();
         $objectManagerHelper = new \Magento\TestFramework\Helper\ObjectManager($this);
 
         $this->_imageHelper = $this->getMock('Magento\Catalog\Helper\Image', array(), array(), '', false);
         $this->layout = $this->getMock('Magento\Framework\View\LayoutInterface');
+        $this->msrpHelper = $this->getMock('\Magento\Msrp\Helper\Data', [], [], '', false);
 
         $context = $this->getMockBuilder('Magento\Framework\View\Element\Template\Context')
             ->disableOriginalConstructor()
@@ -43,7 +46,11 @@ class RendererTest extends \PHPUnit_Framework_TestCase
 
         $this->_renderer = $objectManagerHelper->getObject(
             'Magento\Checkout\Block\Cart\Item\Renderer',
-            array('imageHelper' => $this->_imageHelper, 'context' => $context)
+            [
+                'imageHelper' => $this->_imageHelper,
+                'context' => $context,
+                'msrpHelper' => $this->msrpHelper
+            ]
         );
     }
 
@@ -146,5 +153,31 @@ class RendererTest extends \PHPUnit_Framework_TestCase
             )->will($this->returnValue($priceHtml));
 
         $this->assertEquals($priceHtml, $this->_renderer->getProductPriceHtml($product));
+    }
+
+    /**
+     * @dataProvider canApplyMsrpDataProvider
+     */
+    public function testCanApplyMsrp($showBeforeConfirm, $isMinPriceLess, $expected)
+    {
+        $itemMock = $this->getMock('\Magento\Sales\Model\Quote\Item\AbstractItem', [], [], '', false);
+        $productMock = $this->getMock('\Magento\Catalog\Model\Product', [], [], '', false);
+        $this->_renderer->setItem($itemMock);
+        $itemMock->expects($this->atLeastOnce())->method('getProduct')->willReturn($productMock);
+        $this->msrpHelper->expects($this->once())->method('isShowBeforeOrderConfirm')->with($productMock)
+            ->willReturn($showBeforeConfirm);
+        $this->msrpHelper->expects($this->any())->method('isMinimalPriceLessMsrp')->with($productMock)
+            ->willReturn($isMinPriceLess);
+        $this->assertEquals($expected, $this->_renderer->canApplyMsrp());
+    }
+
+    public function canApplyMsrpDataProvider()
+    {
+        return [
+            [true, false, false],
+            [false, true, false],
+            [false, false, false],
+            [true, true, true]
+        ];
     }
 }
