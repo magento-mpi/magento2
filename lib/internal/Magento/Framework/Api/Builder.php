2298bfc5be856e548f88a754db838ffb7da0c07c
Eugene Tulika <etulika@ebay.com>
2014-11-12 15:12:05 -0600

    MAGETWO-30275: Consolidate all the common logic for ExtensibleObjectBuilder and ExtensibleDataBuilder in one base class - inherited annotations from the interface

--- lib/internal/Magento/Framework/Api/Builder.php
+++ lib/internal/Magento/Framework/Api/Builder.php
@@ -106,11 +106,7 @@ class Builder implements BuilderInterface
     }
 
     /**
-     * Set custom attribute value.
-     *
-     * @param string $attributeCode
-     * @param mixed $attributeValue
-     * @return $this
+     * {@inheritdoc}
      */
     public function setCustomAttribute($attributeCode, $attributeValue)
     {
@@ -127,11 +123,7 @@ class Builder implements BuilderInterface
     }
 
     /**
-     * Set array of custom attributes
-     *
-     * @param \Magento\Framework\Api\AttributeInterface[] $attributes
-     * @return $this
-     * @throws \LogicException If array elements are not of AttributeValue type
+     * {@inheritdoc}
      */
     public function setCustomAttributes(array $attributes)
     {
@@ -150,9 +142,7 @@ class Builder implements BuilderInterface
     }
 
     /**
-     * Return created DataInterface object
-     *
-     * @return \Magento\Framework\Api\ExtensibleDataInterface
+     * {@inheritdoc}
      */
     public function create()
     {
@@ -173,12 +163,7 @@ class Builder implements BuilderInterface
     }
 
     /**
-     * Populates the fields with data from the array.
-     *
-     * Keys for the map are snake_case attribute/field names.
-     *
-     * @param array $data
-     * @return $this
+     * {@inheritdoc}
      */
     public function populateWithArray(array $data)
     {
@@ -188,11 +173,7 @@ class Builder implements BuilderInterface
     }
 
     /**
-     * Populates the fields with an existing entity.
-     *
-     * @param ExtensibleDataInterface $prototype the prototype to base on
-     * @return $this
-     * @throws \LogicException If $prototype object class is not the same type as object that is constructed
+     * {@inheritdoc}
      */
     public function populate(ExtensibleDataInterface $prototype)
     {
@@ -205,13 +186,7 @@ class Builder implements BuilderInterface
     }
 
     /**
-     * Merge second Data Object data with first Data Object data and create new Data Object object based on merge
-     * result.
-     *
-     * @param ExtensibleDataInterface $firstDataObject
-     * @param ExtensibleDataInterface $secondDataObject
-     * @return $this
-     * @throws \LogicException
+     * {@inheritdoc}
      */
     public function mergeDataObjects(
         ExtensibleDataInterface $firstDataObject,
@@ -229,13 +204,7 @@ class Builder implements BuilderInterface
     }
 
     /**
-     * Merged data provided in array format with Data Object data and create new Data Object object based on merge
-     * result.
-     *
-     * @param ExtensibleDataInterface $dataObject
-     * @param array $data
-     * @return $this
-     * @throws \LogicException
+     * {@inheritdoc}
      */
     public function mergeDataObjectWithArray(ExtensibleDataInterface $dataObject, array $data)
     {
@@ -249,11 +218,8 @@ class Builder implements BuilderInterface
         return $this;
     }
 
-
     /**
-     * Return data Object data.
-     *
-     * @return array
+     * {@inheritdoc}
      */
     public function getData()
     {
