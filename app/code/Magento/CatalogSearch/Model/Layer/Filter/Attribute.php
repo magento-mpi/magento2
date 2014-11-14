07f62e56f1fb08af42fa0f0c008d51537b91025b
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-11-14 19:33:20 +0200

    MAGETWO-28305: Integrate Search library into Layered Navigation functionality
     - MAGETWO-30529: Latest filter in LN work wrong

--- app/code/Magento/CatalogSearch/Model/Layer/Filter/Attribute.php
+++ app/code/Magento/CatalogSearch/Model/Layer/Filter/Attribute.php
@@ -61,10 +61,14 @@ class Attribute extends AbstractFilter
         }
         $attribute = $this->getAttributeModel();
         /** @var \Magento\CatalogSearch\Model\Resource\Fulltext\Collection $productCollection */
-        $productCollection = $this->getLayer()->getProductCollection();
+        $productCollection = $this->getLayer()
+            ->getProductCollection();
         $productCollection->addFieldToFilter($attribute->getAttributeCode(), $attributeValue);
         $label = $this->getOptionText($attributeValue);
-        $this->getLayer()->getState()->addFilter($this->_createItem($label, $attributeValue));
+        $this->getLayer()
+            ->getState()
+            ->addFilter($this->_createItem($label, $attributeValue));
+
         return $this;
     }
 
@@ -78,17 +82,22 @@ class Attribute extends AbstractFilter
     {
         $attribute = $this->getAttributeModel();
         /** @var \Magento\CatalogSearch\Model\Resource\Fulltext\Collection $productCollection */
-        $productCollection = $this->getLayer()->getProductCollection();
+        $productCollection = $this->getLayer()
+            ->getProductCollection();
         $optionsFacetedData = $productCollection->getFacetedData($attribute->getAttributeCode());
 
-        $options = $attribute->getFrontend()->getSelectOptions();
+        $productSize = $productCollection->getSize();
+
+        $options = $attribute->getFrontend()
+            ->getSelectOptions();
         foreach ($options as $option) {
             if (empty($option['value'])) {
                 continue;
             }
             // Check filter type
             if ($this->getAttributeIsFilterable($attribute) == static::ATTRIBUTE_OPTIONS_ONLY_WITH_RESULTS
-                && empty($optionsFacetedData[$option['value']]['count'])
+                && (empty($optionsFacetedData[$option['value']]['count'])
+                    || $productSize <= $optionsFacetedData[$option['value']]['count'])
             ) {
                 continue;
             }
@@ -98,6 +107,7 @@ class Attribute extends AbstractFilter
                 $optionsFacetedData[$option['value']]['count']
             );
         }
+
         return $this->itemDataBuilder->build();
     }
 }
