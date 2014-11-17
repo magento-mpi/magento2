5e14ff51f3e12b887bfaabff53f7b51543830737
Victor Rad <vrad@ebay.com>
2014-11-17 19:05:01 +0200

    MAGETWO-29805: Create customer's gift registry
    - CR fix

--- dev/tools/Magento/Tools/SampleData/Module/GiftRegistry/Setup/GiftRegistry.php
+++ dev/tools/Magento/Tools/SampleData/Module/GiftRegistry/Setup/GiftRegistry.php
@@ -64,6 +64,11 @@ class GiftRegistry implements SetupInterface
     protected $itemFactory;
 
     /**
+     * @var \Magento\Catalog\Model\Resource\Product\Indexer\Eav\Source
+     */
+    protected $productIndexer;
+
+    /**
      * @param FixtureHelper $fixtureHelper
      * @param CsvReaderFactory $csvReaderFactory,
      * @param \Magento\Directory\Model\CountryFactory $countryFactory,
@@ -73,6 +78,7 @@ class GiftRegistry implements SetupInterface
      * @param \Magento\Framework\Stdlib\DateTime\DateTimeFactory $dateFactory,
      * @param \Magento\Catalog\Model\ProductFactory $productFactory,
      * @param \Magento\GiftRegistry\Model\ItemFactory $itemFactory
+     * @param \Magento\Catalog\Model\Resource\Product\Indexer\Eav\Source $productIndexer
      */
     public function __construct(
         FixtureHelper $fixtureHelper,
@@ -83,7 +89,8 @@ class GiftRegistry implements SetupInterface
         \Magento\Customer\Model\CustomerFactory $customerFactory,
         \Magento\Framework\Stdlib\DateTime\DateTimeFactory $dateFactory,
         \Magento\Catalog\Model\ProductFactory $productFactory,
-        \Magento\GiftRegistry\Model\ItemFactory $itemFactory
+        \Magento\GiftRegistry\Model\ItemFactory $itemFactory,
+        \Magento\Catalog\Model\Resource\Product\Indexer\Eav\Source $productIndexer
     ) {
         $this->fixtureHelper = $fixtureHelper;
         $this->csvReaderFactory = $csvReaderFactory;
@@ -94,6 +101,7 @@ class GiftRegistry implements SetupInterface
         $this->dateFactory = $dateFactory;
         $this->productFactory = $productFactory;
         $this->itemFactory = $itemFactory;
+        $this->productIndexer = $productIndexer;
     }
 
     /**
@@ -102,95 +110,83 @@ class GiftRegistry implements SetupInterface
     public function run()
     {
         echo 'Installing Gift Registry' . PHP_EOL;
-        $data = $this->generateData();
-        $giftRegistry = $this->giftRegistryFactory->create();
-        $address = $this->addressFactory->create();
-        $address->setData($data['address']);
-        $giftRegistry->setTypeById($data['type_id']);
-        $giftRegistry->importData($data);
-        $giftRegistry->addData(
-            [
-                'customer_id' => $data['customer_id'],
-                'website_id' => 1,
-                'url_key' => $giftRegistry->getGenerateKeyId(),
-                'created_at' => $this->dateFactory->create()->date(),
-                'is_add_action' => true
-            ]
-        );
-        $giftRegistry->importAddress($address);
-        $validationPassed = $giftRegistry->validate();
-        if ($validationPassed) {
-            $giftRegistry->save();
-            foreach($data['items'] as $productId) {
-                $item = $this->itemFactory->create();
-                $item->setEntityId($giftRegistry->getId())
-                    ->setProductId($productId)
-                    ->setQty(1)
-                    ->save();
-            }
-        }
-        echo '.';
-        echo PHP_EOL;
-    }
-
-    /**
-     * @return array
-     */
-    private function generateData()
-    {
-        $fixtureFile = 'Customer/customer_profile.csv';
-        $fixtureFilePath = $this->fixtureHelper->getPath($fixtureFile);
-        /** @var \Magento\Tools\SampleData\Helper\Csv\Reader $csvReader */
-        $csvReader = $this->csvReaderFactory->create(array('fileName' => $fixtureFilePath, 'mode' => 'r'));
-        foreach ($csvReader as $customerData) {}
         $fixtureFile = 'GiftRegistry/gift_registry.csv';
         $fixtureFilePath = $this->fixtureHelper->getPath($fixtureFile);
         /** @var \Magento\Tools\SampleData\Helper\Csv\Reader $csvReader */
         $csvReader = $this->csvReaderFactory->create(array('fileName' => $fixtureFilePath, 'mode' => 'r'));
-        foreach ($csvReader as $giftRegistryData) {}
+        foreach ($csvReader as $giftRegistryData) {
+            $data = $this->generateData($giftRegistryData);
+            $giftRegistry = $this->giftRegistryFactory->create();
+            $address = $this->addressFactory->create();
+            $address->setData($data['address']);
+            $giftRegistry->setTypeById($data['type_id']);
+            $giftRegistry->importData($data);
+            $giftRegistry->addData(
+                [
+                    'customer_id' => $data['customer_id'],
+                    'website_id' => 1,
+                    'url_key' => $giftRegistry->getGenerateKeyId(),
+                    'created_at' => $this->dateFactory->create()->date(),
+                    'is_add_action' => true
+                ]
+            );
+            $giftRegistry->importAddress($address);
+            $validationPassed = $giftRegistry->validate();
+            if ($validationPassed) {
+                $giftRegistry->save();
+                foreach($data['items'] as $productId) {
+                    $parentId = $this->productIndexer->getRelationsByChild($productId);
+                    $itemProduct = $parentId ? $parentId[0] : $productId;
+                    $itemOptions = $this->formItemOptions($productId);
+                    $item = $this->itemFactory->create();
+                    $item->setEntityId($giftRegistry->getId())
+                        ->setProductId($itemProduct)
+                        ->setQty(1)
+                        ->setOptions($itemOptions)
+                        ->save();
+                }
+            }
+            echo '.';
+        }
+        echo PHP_EOL;
+    }
+
+    /**
+     * @param array $giftRegistryData
+     * @return array
+     */
+    private function generateData(array $giftRegistryData)
+    {
         $giftRegistryData['sku'] = explode("\n", $giftRegistryData['sku']);
-        $productIds = $this->productSkuToId($giftRegistryData['sku']);
-        $customerId = $this->customerFactory->create()
-            ->setWebsiteId(1)
-            ->loadByEmail($customerData['email'])
-            ->getId();
+        $customer = $this->customerFactory->create()->setWebsiteId(1)->loadByEmail($giftRegistryData['customer_email']);
+        $address = $customer->getDefaultBillingAddress()->getData();
         return [
-            'customer_id' => $customerId,
+            'customer_id' => $customer->getId(),
             'type_id' => 1,
             'title' => $giftRegistryData['title'],
             'message' =>  $giftRegistryData['message'],
             'is_public' => 1,
             'is_active' => 1,
-            'event_country' => $customerData['country_id'],
-            'event_country_region' => $this->getRegionId($customerData),
+            'event_country' => $address['country_id'],
+            'event_country_region' => $address['region_id'],
             'event_country_region_text' => '',
             'event_date' => date('Y-m-d'),
             'address' =>
                 [
-                    'firstname' => $customerData['firstname'],
-                    'lastname' => $customerData['lastname'],
+                    'firstname' => $address['firstname'],
+                    'lastname' => $address['lastname'],
                     'company' => '',
-                    'street' => $customerData['street'],
-                    'city' => $customerData['city'],
-                    'region_id' => $this->getRegionId($customerData),
-                    'region' => $customerData['region'],
-                    'postcode' => $customerData['postcode'],
-                    'country_id' => $customerData['country_id'],
-                    'telephone' => $customerData['telephone'],
+                    'street' => $address['street'],
+                    'city' => $address['city'],
+                    'region_id' => $address['region_id'],
+                    'region' => $address['region'],
+                    'postcode' => $address['postcode'],
+                    'country_id' => $address['country_id'],
+                    'telephone' => $address['telephone'],
                     'fax' => '',
                 ],
-            'items' => $productIds,
+            'items' => $this->productSkuToId($giftRegistryData['sku']),
         ];
-    }
-
-    /**
-     * @param array $address
-     * @return mixed
-     */
-    protected function getRegionId($address)
-    {
-        $country = $this->countryFactory->create()->loadByCode($address['country_id']);
-        return $country->getRegionCollection()->addFieldToFilter('name', $address['region'])->getFirstItem()->getId();
     }
 
     /**
@@ -207,5 +203,65 @@ class GiftRegistry implements SetupInterface
             }
         }
         return $ids;
+    }
+
+    /**
+     * @param int $productId
+     * @return array
+     */
+    protected function formItemOptions($productId)
+    {
+        $itemOptions = [];
+        $parentId = $this->productIndexer->getRelationsByChild($productId);
+        if (!$parentId) {
+            $itemOptions[] = [
+                'product_id' => $productId,
+                'code' => 'info_buyRequest',
+                'value' => serialize(['product' => $productId, 'qty' => 1, 'related_product' => ''])
+            ];
+            return $itemOptions;
+        }
+        $parentId = array_shift($parentId);
+        /** @var \Magento\Catalog\Model\Product $product */
+        $product = $this->productFactory->create();
+        $product->load($productId);
+        /** @var \Magento\Catalog\Model\Product $parentProduct */
+        $parentProduct = $this->productFactory->create();
+        $parentProduct->load($parentId);
+        $superAttribute = [];
+        if ($parentProduct->getTypeId() == \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE) {
+            $itemOptions[] = [
+                'product_id' => $productId,
+                'code' => 'simple_product',
+                'value' => $productId
+            ];
+            $itemOptions[] = [
+                'product_id' => $productId,
+                'code' => 'product_qty_' . $productId,
+                'value' => 1
+            ];
+            /** @var \Magento\ConfigurableProduct\Model\Product\Type\Configurable $productType */
+            $productType = $parentProduct->getTypeInstance();
+            foreach ($productType->getConfigurableAttributes($parentProduct) as $attribute) {
+                $attributeCode = $attribute->getProductAttribute()->getAttributeCode();
+                $superAttribute[$attribute->getAttributeId()] = $product->getData($attributeCode);
+            }
+            $itemOptions[] = [
+                'product_id' => $parentId,
+                'code' => 'info_buyRequest',
+                'value' => serialize([
+                    'product' => $productId,
+                    'qty' => 1,
+                    'related_product' => '',
+                    'super_attribute' => $superAttribute
+                ])
+            ];
+            $itemOptions[] = [
+                'product_id' => $parentId,
+                'code' => 'attributes',
+                'value' => serialize($superAttribute)
+            ];
+        }
+        return $itemOptions;
     }
 }
