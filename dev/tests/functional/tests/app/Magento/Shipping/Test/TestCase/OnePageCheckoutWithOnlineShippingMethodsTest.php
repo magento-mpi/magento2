ee3c4a63849c8d3a237a5e3528ff462feb7f026b
Roman Liukshyn <rliukshyn@ebay.com>
2014-11-19 13:45:23 +0200

    MAGETWO-30800: Test Creation for OnePageCheckout with online shipping methods - CR Changes

--- dev/tests/functional/tests/app/Magento/Shipping/Test/TestCase/OnePageCheckoutWithOnlineShippingMethodsTest.php
+++ dev/tests/functional/tests/app/Magento/Shipping/Test/TestCase/OnePageCheckoutWithOnlineShippingMethodsTest.php
@@ -9,57 +9,29 @@
 namespace Magento\Shipping\Test\TestCase;
 
 use Mtf\TestCase\Scenario;
-use Magento\Customer\Test\Page\CustomerAccountLogout;
 
 /**
- * Test Creation for OnePageCheckout with online shipping methods
- *
  * Test Flow:
- * 1.*Precondition:*
+ * 1. Precondition:
  * 2. Shipping method is configured (UPS, USPS, Fedex, DHL)
- * 3. Customer is registered according to "customer/dataSet"
- * 4.
- * 5.*Steps:*
- * 6. Go to Frontend
- * 7. Add Products to the cart
- * 8. click the *Proceed to Checkout* button
- * 9. if $checkoutMethod =  login
- * 10. login as customer
- * 11.    elseif $checkoutMethod =  register
- * 12. select the *Register* option button and click the *Continue* button
- * 13. elseif $checkoutMethod =  guest
- * 14. select the *Checkout as Guest* option button and click the *Continue* button
- * 15. fill billing information according to "customer/dataSet"
- * 16. select the *Ship to this address* option button,  click the *Continue* button
- * 17. select shipping method according to shipping/shipping_method, click the *Continue* button
- * 18. select payment method according to dataset, click the *Continue* button
- * 19. Call assertOrderTotal
- * 20. click the *Place Order* button
- * 21. Perform assertions
+ *
+ * 1. Steps:
+ * 2. Go to Frontend
+ * 3. Add Products to the cart
+ * 4. Click the 'Proceed to Checkout' button
+ * 5. Select checkout method according to dataset
+ * 6. Fill billing information according to "customer/dataSet"
+ * 7. Select the 'Ship to this address' option button,  click the 'Continue' button
+ * 8. Select shipping method according to shipping/shipping_method, click the 'Continue' button
+ * 9. Select payment method according to dataset, click the 'Continue' button
+ * 10. Click the 'Place Order' button
+ * 11. Perform assertions
  *
  * @group DHL_(CS), FedEx_(CS), One_Page_Checkout_(CS), UPS_(CS), USPS_(CS)
- * @ZephyrId MTA-522
+ * @ZephyrId MAGETWO-29901
  */
 class OnePageCheckoutWithOnlineShippingMethodsTest extends Scenario
 {
-    /**
-     * Customer logout page
-     *
-     * @var CustomerAccountLogout
-     */
-    protected $customerAccountLogout;
-
-    /**
-     * Preparing configuration for test
-     *
-     * @param CustomerAccountLogout $customerAccountLogout
-     */
-    public function __prepare(
-        CustomerAccountLogout $customerAccountLogout
-    ) {
-        $this->customerAccountLogout = $customerAccountLogout;
-    }
-
     /**
      * Runs one page checkout test
      *
@@ -81,6 +53,5 @@ class OnePageCheckoutWithOnlineShippingMethodsTest extends Scenario
             'Magento\Core\Test\TestStep\SetupConfigurationStep',
             ['configData' => $this->currentVariation['arguments']['configData'], 'rollback' => true]
         )->run();
-        $this->customerAccountLogout->open();
     }
 }
