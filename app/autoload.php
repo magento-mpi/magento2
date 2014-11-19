838249e455808d8fb4439ee3b46332bf326b85de
Safwan Khan <safwkhan@paypal.com>
2014-11-19 17:16:04 -0600

    MAGETWO-30208: Fatal Error When Missed "composer install"
    - Fixed build failure issue.

--- app/autoload.php
+++ app/autoload.php
@@ -15,21 +15,8 @@ define('BP', dirname(__DIR__));
 
 $vendorDir = require BP . '/app/etc/vendor_path.php';
 $vendorAutoload = BP . "/{$vendorDir}/autoload.php";
-
-/* 'composer install' validation */
 if (file_exists($vendorAutoload)) {
     require_once $vendorAutoload;
-} else {
-    echo <<<HTML
-<div style="font:12px/1.35em arial, helvetica, sans-serif;">
-    <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
-        <h3 style="margin:0;font-size:1.7em;font-weight:normal;text-transform:none;text-align:left;color:#2f2f2f;">
-        Whoops, it looks like Magento application dependencies are not installed.</h3>
-    </div>
-    <p>Please run 'composer install' under application root directory.</p>
-</div>
-HTML;
-    exit(1);
 }
 
 require_once BP . '/lib/internal/Magento/Framework/Autoload/IncludePath.php';
--- index.php
+++ index.php
@@ -20,6 +20,24 @@
  */
 
 require __DIR__ . '/app/bootstrap.php';
+
+$vendorDir = require __DIR__ . '/app/etc/vendor_path.php';
+$vendorAutoload = __DIR__ . "/{$vendorDir}/autoload.php";
+
+/* 'composer install' validation */
+if (!file_exists($vendorAutoload)) {
+    echo <<<HTML
+<div style="font:12px/1.35em arial, helvetica, sans-serif;">
+    <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
+        <h3 style="margin:0;font-size:1.7em;font-weight:normal;text-transform:none;text-align:left;color:#2f2f2f;">
+        Whoops, it looks like Magento application dependencies are not installed.</h3>
+    </div>
+    <p>Please run 'composer install' under application root directory.</p>
+</div>
+HTML;
+    exit(1);
+}
+
 $bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
 /** @var \Magento\Framework\App\Http $app */
 $app = $bootstrap->createApplication('Magento\Framework\App\Http');
