5415fe630967d9b7d2be93b1319fa6becee64635
Alex Akimov <aakimov@ebay.com>
2014-11-19 13:11:52 +0200

    MAGETWO-29876: Build Stabilization

--- app/code/Magento/Catalog/Api/Data/ProductAttributeDataBuilder.php
+++ app/code/Magento/Catalog/Api/Data/ProductAttributeDataBuilder.php
@@ -25,7 +25,7 @@ class ProductAttributeDataBuilder extends \Magento\Framework\Api\Builder
      * @param \Magento\Framework\Reflection\DataObjectProcessor $objectProcessor
      * @param \Magento\Framework\Reflection\TypeProcessor $typeProcessor
      * @param \Magento\Framework\Serialization\DataBuilderFactory $dataBuilderFactory
-     * @param \Magento\Framework\ObjectManager\Config $objectManagerConfig
+     * @param \Magento\Framework\ObjectManager\ConfigInterface $objectManagerConfig
      * @param string $modelClassInterface
      */
     public function __construct(
@@ -35,7 +35,7 @@ class ProductAttributeDataBuilder extends \Magento\Framework\Api\Builder
         \Magento\Framework\Reflection\DataObjectProcessor $objectProcessor,
         \Magento\Framework\Reflection\TypeProcessor $typeProcessor,
         \Magento\Framework\Serialization\DataBuilderFactory $dataBuilderFactory,
-        \Magento\Framework\ObjectManager\Config $objectManagerConfig,
+        \Magento\Framework\ObjectManager\ConfigInterface $objectManagerConfig,
         $modelClassInterface = 'Magento\Catalog\Api\Data\ProductAttributeInterface'
     ) {
         parent::__construct(
