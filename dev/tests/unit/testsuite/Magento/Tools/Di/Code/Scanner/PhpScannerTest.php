1e12d7e61a1ad9b71d0c53d8c6d91574a6375171
Roman Liukshyn <rliukshyn@ebay.com>
2014-11-20 19:04:57 +0200

    MAGETWO-30007: Object Manager optimization integration
    - added test for ClassReaderDecorator
    - unit tests Test/Tools folder moved due to namespace definition

---
+++ dev/tests/unit/testsuite/Magento/Tools/Di/Code/Scanner/PhpScannerTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright  {copyright}
+ * @license    {license_link}
+ */
+namespace Magento\Tools\Di\Code\Scanner;
+
+require_once __DIR__ . '/../../_files/app/code/Magento/SomeModule/Helper/Test.php';
+require_once __DIR__ . '/../../_files/app/code/Magento/SomeModule/ElementFactory.php';
+class PhpScannerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Tools\Di\Code\Scanner\PhpScanner
+     */
+    protected $_model;
+
+    /**
+     * @var string
+     */
+    protected $_testDir;
+
+    /**
+     * @var array
+     */
+    protected $_testFiles = array();
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_logMock;
+
+    protected function setUp()
+    {
+        $this->_model = new \Magento\Tools\Di\Code\Scanner\PhpScanner(
+            $this->_logMock = $this->getMock('\Magento\Tools\Di\Compiler\Log\Log', array(), array(), '', false)
+        );
+        $this->_testDir = str_replace('\\', '/', realpath(__DIR__ . '/../../') . '/_files');
+        $this->_testFiles = array($this->_testDir . '/app/code/Magento/SomeModule/Helper/Test.php');
+    }
+
+    public function testCollectEntities()
+    {
+        $this->_logMock->expects(
+            $this->at(0)
+        )->method(
+            'add'
+        )->with(
+            4,
+            'Magento\SomeModule\Module\Factory',
+            'Invalid Factory for nonexistent class Magento\SomeModule\Module in file ' . $this->_testFiles[0]
+        );
+        $this->_logMock->expects(
+            $this->at(1)
+        )->method(
+            'add'
+        )->with(
+            4,
+            'Magento\SomeModule\Element\Factory',
+            'Invalid Factory declaration for class Magento\SomeModule\Element in file ' . $this->_testFiles[0]
+        );
+
+        $this->assertEquals(array(), $this->_model->collectEntities($this->_testFiles));
+    }
+}
---
+++ dev/tests/unit/testsuite/Magento/Tools/I18n/Code/Dictionary/Options/ResolverTest.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * {license_notice}
+ *
+ * @copyright {copyright}
+ * @license   {license_link}
+ */
+namespace Magento\Tools\I18n\Code\Dictionary\Options;
+
+/**
+ * Class ResolverTest
+ */
+class ResolverTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param string $directory
+     * @param bool $withContext
+     * @param array $result
+     * @dataProvider getOptionsDataProvider
+     */
+    public function testGetOptions($directory, $withContext, $result)
+    {
+        $objectManagerHelper = new \Magento\TestFramework\Helper\ObjectManager($this);
+        /** @var \Magento\Tools\I18n\Code\Dictionary\Options\Resolver $resolver */
+        $resolver = $objectManagerHelper->getObject(
+            'Magento\Tools\I18n\Code\Dictionary\Options\Resolver',
+            [
+                'directory' => $directory,
+                'withContext' => $withContext,
+            ]
+        );
+        $this->assertSame($result, $resolver->getOptions());
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptionsDataProvider()
+    {
+        $sourceFirst = __DIR__ . '/_files/source';
+        $sourceSecond = __DIR__ . '/_files/source';
+        return [
+            [
+                $sourceFirst,
+                true,
+                [
+                    [
+                        'type' => 'php',
+                        'paths' => [$sourceFirst . '/app/code/', $sourceFirst . '/app/design/'],
+                        'fileMask' => '/\.(php|phtml)$/'
+                    ],
+                    [
+                        'type' => 'js',
+                        'paths' => [
+                            $sourceFirst . '/app/code/',
+                            $sourceFirst . '/app/design/',
+                            $sourceFirst . '/lib/web/mage/',
+                            $sourceFirst . '/lib/web/varien/'
+                        ],
+                        'fileMask' => '/\.(js|phtml)$/'
+                    ],
+                    [
+                        'type' => 'xml',
+                        'paths' => [$sourceFirst . '/app/code/', $sourceFirst . '/app/design/'],
+                        'fileMask' => '/\.xml$/'
+                    ]
+                ]
+            ],
+            [
+                $sourceSecond,
+                false,
+                [
+                    ['type' => 'php', 'paths' => array($sourceSecond), 'fileMask' => '/\.(php|phtml)$/'],
+                    ['type' => 'js', 'paths' => array($sourceSecond), 'fileMask' => '/\.(js|phtml)$/'],
+                    ['type' => 'xml', 'paths' => array($sourceSecond), 'fileMask' => '/\.xml$/']
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * @param string $directory
+     * @param bool $withContext
+     * @param string $message
+     * @dataProvider getOptionsWrongDirDataProvider
+     */
+    public function testGetOptionsWrongDir($directory, $withContext, $message)
+    {
+        $objectManagerHelper = new \Magento\TestFramework\Helper\ObjectManager($this);
+        /** @var \Magento\Tools\I18n\Code\Dictionary\Options\Resolver $resolver */
+        $resolver = $objectManagerHelper->getObject(
+            'Magento\Tools\I18n\Code\Dictionary\Options\Resolver',
+            [
+                'directory' => $directory,
+                'withContext' => $withContext,
+            ]
+        );
+        $this->setExpectedException('\InvalidArgumentException', $message);
+        $resolver->getOptions();
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptionsWrongDirDataProvider()
+    {
+        return [
+            ['not_exist', true, 'Specified path is not a Magento root directory'],
+            ['not_exist', false, 'Specified path doesn\'t exist'],
+        ];
+    }
+}
