18578637ac9883610a1713f492385c24b8521382
Max Pronko <mpronko@ebay.com>
2014-11-21 11:59:50 +0200

    MAGETWO-28725: Form UI Component pull request proceeding
    - fixed issue during merge

--- dev/tests/functional/bootstrap.php
+++ dev/tests/functional/bootstrap.php
@@ -2,14 +2,13 @@
 /**
  * {license_notice}
  *
- * @copyright   {copyright}
- * @license     {license_link}
+ * @copyright {copyright}
+ * @license {license_link}
  */
 
 session_start();
 defined('MTF_BOOT_FILE') || define('MTF_BOOT_FILE', __FILE__);
 require_once __DIR__ . '/../../../app/bootstrap.php';
-$includePath = new \Magento\Framework\Autoload\IncludePath();
-spl_autoload_register([$includePath, 'load']);
+date_default_timezone_set('America/Los_Angeles');
 restore_error_handler();
 require_once __DIR__ . '/vendor/autoload.php';
