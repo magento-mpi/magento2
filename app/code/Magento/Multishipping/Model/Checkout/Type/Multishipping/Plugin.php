b07e582136f5699aea54799331f30041d78825f6
Ievgen Shakhsuvarov <ishakhsuvarov@ebay.com>
2014-11-21 09:22:49 +0200

    MAGETWO-30241: Product price details missing in summary block on cart page when back to shopping cart link is clicked on multishipping page CS fixes

--- app/code/Magento/Multishipping/Model/Checkout/Type/Multishipping/Plugin.php
+++ app/code/Magento/Multishipping/Model/Checkout/Type/Multishipping/Plugin.php
@@ -19,6 +19,10 @@ class Plugin
      */
     protected $checkoutStateBegin;
 
+
+    /**
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     */
     public function __construct(\Magento\Checkout\Model\Session $checkoutSession)
     {
         $this->checkoutSession = $checkoutSession;
@@ -27,9 +31,10 @@ class Plugin
 
     /**
      * Map STEP_SELECT_ADDRESSES to Cart::CHECKOUT_STATE_BEGIN
+     * @param \Magento\Checkout\Model\Cart
      * @return void
      */
-    public function beforeInit()
+    public function beforeInit(\Magento\Checkout\Model\Cart $subject)
     {
         if ($this->checkoutSession->getCheckoutState() === State::STEP_SELECT_ADDRESSES) {
             $this->checkoutSession->setCheckoutState($this->checkoutStateBegin);
