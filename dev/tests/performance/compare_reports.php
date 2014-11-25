34e7c41a43b1028fcf50b1a4cfdc0c732acd56ee
Roman Ganin <rganin@ebay.com>
2014-11-25 20:45:32 +0200

    MAGETWO-30663: Configure build to run scenario on mainline and branch simultaneously
    - removed Zend_Console_Getopt usage

--- dev/tests/performance/compare_reports.php
+++ dev/tests/performance/compare_reports.php
@@ -6,25 +6,29 @@
  * @license     {license_link}
  */
 
-$magentoBaseDir = realpath(__DIR__ . '/../../../');
-require_once $magentoBaseDir . '/lib/internal/Zend/Console/Getopt.php';
+$usageMessage =
+'Usage:' . PHP_EOL
+. '   php -f ' . str_replace(dirname(__FILE__), __FILE__, '')
+. ' -- -m mainline_report.jtl -b branch_report.jtl -o output_file.xml ";"' . PHP_EOL
+. PHP_EOL
+. 'Parameters:' . PHP_EOL
+. '   -m   - mainline report file' . PHP_EOL
+. '   -b   - branch report file' . PHP_EOL
+. '   -o   - output xml file' . PHP_EOL;
+
+$args = getopt('m:b:o:');
+if (empty($args)) {
+    echo $usageMessage;
+    exit(0);
+}
+
+$mainlineFile = $args['m'];
+$branchFile = $args['b'];
+$outputFile = $args['o'];
 
 try {
-    $shell = new Zend_Console_Getopt(array(
-        'm|mainline=s' => 'Response time report for mainline',
-        'b|branch=s' => 'Response time report for branch',
-        'o|output=s' => 'Target output file'
-    ));
-
-    $args = $shell->getOptions();
-    if (empty($args)) {
-        echo $shell->getUsageMessage();
-        exit(1);
-    }
-
-
-    $mainlineResults = readResponseTimeReport($shell->getOption('mainline'));
-    $branchResults = readResponseTimeReport($shell->getOption('branch'));
+    $mainlineResults = readResponseTimeReport($mainlineFile);
+    $branchResults = readResponseTimeReport($branchFile);
 
     $result = new SimpleXMLElement('<testResults version="1.2" />');
     foreach (array_keys($mainlineResults) as $sampleName) {
@@ -48,11 +52,8 @@ try {
     $dom->preserveWhiteSpace = false;
     $dom->formatOutput = true;
     $dom->loadXML($result->asXML());
-    file_put_contents($shell->getOption('output'), $dom->saveXML());
+    file_put_contents($outputFile, $dom->saveXML());
 
-} catch (\Zend_Console_Getopt_Exception $e) {
-    fwrite(STDERR, $e->getMessage() . "\n\n" . $e->getUsageMessage() . "\n");
-    exit(1);
 } catch (\Exception $e) {
     fwrite(STDERR, $e->getMessage() . "\n");
     exit(1);
