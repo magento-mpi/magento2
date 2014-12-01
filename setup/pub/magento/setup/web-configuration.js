db4b38d5b05762ab0d273511bae708631e56eb50
Safwan Khan <safwkhan@paypal.com>
2014-12-01 11:30:19 -0600

    MAGETWO-30953: Add URL format validation
    - Removed some duplication.

--- setup/pub/magento/setup/web-configuration.js
+++ setup/pub/magento/setup/web-configuration.js
@@ -92,32 +92,4 @@ angular.module('web-configuration', ['ngStorage'])
                 $scope.webconfig.submitted = false;
             }
         });
-    }])
-    .directive('validateHttpurl', function() {
-        return{
-            require: "ngModel",
-            link: function(scope, elm, attrs, ctrl){
-                var validator = function(value){
-                    var isValid  = value.match(/^(http):\/\/(([a-zA-Z0-9$\-_.+!*'(),;:&=]|%[0-9a-fA-F]{2})+@)?(((25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])){3})|localhost|([a-zA-Z0-9\-\u00C0-\u017F]+\.)+([a-zA-Z]{2,}))(:[0-9]+)?(\/(([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*(\/([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*)*)?(\?([a-zA-Z0-9$\-_.+!*'(),;:@&=\/?]|%[0-9a-fA-F]{2})*)?(\#([a-zA-Z0-9$\-_.+!*'(),;:@&=\/?]|%[0-9a-fA-F]{2})*)?)?$/);
-                    ctrl.$setValidity('validateHttpurl', isValid);
-                    return value;
-                };
-                ctrl.$parsers.unshift(validator);
-                ctrl.$formatters.unshift(validator);
-            }
-        };
-    })
-    .directive('validateHttpsurl', function() {
-        return{
-            require: "ngModel",
-            link: function(scope, elm, attrs, ctrl){
-                var validator = function(value){
-                    var isValid  = angular.isUndefined(value) || (value === "") || value.match(/^(https):\/\/(([a-zA-Z0-9$\-_.+!*'(),;:&=]|%[0-9a-fA-F]{2})+@)?(((25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])){3})|localhost|([a-zA-Z0-9\-\u00C0-\u017F]+\.)+([a-zA-Z]{2,}))(:[0-9]+)?(\/(([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*(\/([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*)*)?(\?([a-zA-Z0-9$\-_.+!*'(),;:@&=\/?]|%[0-9a-fA-F]{2})*)?(\#([a-zA-Z0-9$\-_.+!*'(),;:@&=\/?]|%[0-9a-fA-F]{2})*)?)?$/);
-                    ctrl.$setValidity('validateHttpsurl', isValid);
-                    return value;
-                };
-                ctrl.$parsers.unshift(validator);
-                ctrl.$formatters.unshift(validator);
-            }
-        };
-    });
+    }]);
