900645d35c4f1a592e766e2d554f553cc561c789
Eddie Lau <kahlau@ebay.com>
2014-12-01 16:50:05 -0600

    MAGETWO-31004 Validation of CLI
    - removed use_secure and use_secure_admin flag

--- dev/build/core_dev/functional/config/install.php
+++ dev/build/core_dev/functional/config/install.php
@@ -16,8 +16,6 @@ return array(
         'db_name'                => '{{db_name}}',
         'db_user'                => '{{db_user}}',
         'db_pass'                => '{{db_password}}',
-        'use_secure'             => '1',
-        'use_secure_admin'       => '1',
         'admin_use_security_key' => '0',
         'use_rewrites'           => '1',
         'admin_lastname'         => 'Admin',
