c60804fd25b3c57437cefea6ad33887034add7d1
Anton Kaplya <akaplya@ebay.com>
2014-12-02 15:41:33 +0200

    MAGETWO-30458: Integration test Magento\SalesRule\Model\Resource\Report\CollectionTest::testPeriod CollectionTest::testPeriod randomly fails

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
@@ -2895,5 +2895,5 @@ return array(
     ['Magento\Framework\App\Arguments\Loader'],
     ['Magento\Framework\App\Arguments', 'Magento\Framework\App\DeploymentConfig'],
     ['Magento\Sales\Controller\Adminhtml\Order\InvoiceLoader'],
-    ['Magento\Sales\Model\Resource\AbstractResource'],
+    ['Magento\Sales\Model\Resource\AbstractResource', 'Magento\Framework\Model\Resource\Db\AbstractDb'],
 );
