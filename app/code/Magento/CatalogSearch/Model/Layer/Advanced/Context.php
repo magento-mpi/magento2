c95383bfc0e40c5fb821d1ad8a796216fe80a457
Joan He <joan@x.com>
2014-12-04 09:10:56 -0600

    MAGETWO-31095: Fixed compiler fails

--- app/code/Magento/CatalogSearch/Model/Layer/Advanced/Context.php
+++ app/code/Magento/CatalogSearch/Model/Layer/Advanced/Context.php
@@ -7,23 +7,6 @@
  */
 namespace Magento\CatalogSearch\Model\Layer\Advanced;
 
-use \Magento\CatalogSearch\Model\Layer\Advanced\ItemCollectionProvider;
-use \Magento\CatalogSearch\Model\Layer\Search\StateKey;
-use \Magento\CatalogSearch\Model\Layer\Advanced\CollectionFilter;
-
 class Context extends \Magento\Catalog\Model\Layer\Context
 {
-
-    /**
-     * @param ItemCollectionProvider $collectionProvider
-     * @param StateKey $stateKey
-     * @param CollectionFilter $collectionFilter
-     */
-    public function __construct(
-        ItemCollectionProvider $collectionProvider,
-        StateKey $stateKey,
-        CollectionFilter $collectionFilter
-    ) {
-        parent::__construct($collectionProvider, $stateKey, $collectionFilter);
-    }
 }
