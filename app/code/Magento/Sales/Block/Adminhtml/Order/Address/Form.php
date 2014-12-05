dd75aca66e5928bcf709e7dd2e974008c043894b
Alexander Paliarush <apaliarush@ebay.com>
2014-12-05 13:40:55 +0200

    MAGETWO-31046: Remove V1\Service code from Customer

--- app/code/Magento/Sales/Block/Adminhtml/Order/Address/Form.php
+++ app/code/Magento/Sales/Block/Adminhtml/Order/Address/Form.php
@@ -41,7 +41,6 @@ class Form extends \Magento\Sales\Block\Adminhtml\Order\Create\Form\Address
      * @param \Magento\Customer\Model\Options $options
      * @param \Magento\Customer\Helper\Address $addressHelper
      * @param \Magento\Customer\Api\AddressRepositoryInterface $addressService
-     * @param \Magento\Customer\Model\Address\Mapper $addressMapper
      * @param \Magento\Framework\Api\SearchCriteriaBuilder $criteriaBuilder
      * @param \Magento\Framework\Api\FilterBuilder $filterBuilder
      * @param \Magento\Customer\Model\Address\Mapper $addressMapper
@@ -62,7 +61,6 @@ class Form extends \Magento\Sales\Block\Adminhtml\Order\Create\Form\Address
         \Magento\Customer\Model\Options $options,
         \Magento\Customer\Helper\Address $addressHelper,
         \Magento\Customer\Api\AddressRepositoryInterface $addressService,
-        \Magento\Customer\Model\Address\Mapper $addressMapper,
         \Magento\Framework\Api\SearchCriteriaBuilder $criteriaBuilder,
         \Magento\Framework\Api\FilterBuilder $filterBuilder,
         \Magento\Customer\Model\Address\Mapper $addressMapper,
@@ -84,7 +82,6 @@ class Form extends \Magento\Sales\Block\Adminhtml\Order\Create\Form\Address
             $options,
             $addressHelper,
             $addressService,
-            $addressMapper,
             $criteriaBuilder,
             $filterBuilder,
             $addressMapper,
