82f4b49e1617cb79a87c0d503584f9e0e17e2bdb
Olga Kopylova <okopylova@ebay.com>
2014-12-05 17:15:06 -0600

    MAGETWO-29035: Installation fails if cli php is different than apache configured
    - handle exception when 'vendor' directory is absent

--- index.php
+++ index.php
@@ -27,9 +27,9 @@ try {
 <div style="font:12px/1.35em arial, helvetica, sans-serif;">
     <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
         <h3 style="margin:0;font-size:1.7em;font-weight:normal;text-transform:none;text-align:left;color:#2f2f2f;">
-        Whoops, it looks like Magento application dependencies are not installed.</h3>
+        Autoload error</h3>
     </div>
-    <p>Please run 'composer install' under application root directory.</p>
+    <p>{$e->getMessage()}</p>
 </div>
 HTML;
     exit(1);
