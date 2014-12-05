0259f60ddc195106e3a649942cc9e56de2f87aed
Yaroslav Onischenko <yonischenko@ebay.com>
2014-12-05 13:31:37 +0200

    MAGETWO-31212: Test coverage for Sample Data

--- dev/tests/integration/testsuite/Magento/Tools/SampleData/Module/Catalog/Setup/ProductTest.php
+++ dev/tests/integration/testsuite/Magento/Tools/SampleData/Module/Catalog/Setup/ProductTest.php
@@ -18,25 +18,58 @@ class ProductTest extends \PHPUnit_Framework_TestCase
      */
     public function testRun()
     {
+        $this->installAttributes();
+        $this->installCategories();
+        $this->installProducts();
+    }
+
+    public function installAttributes()
+    {
+        $fixtureHelper = $this->getMockBuilder('Magento\Tools\SampleData\Helper\Fixture')
+            ->disableOriginalConstructor()->setMethods(['getPath'])
+            ->getMock();
+
+        $fixtureHelper->expects($this->at(0))->method('getPath')->will($this->returnValue(
+            realpath(__DIR__ . '/../../../_files/catalog_attributes.csv')
+        ));
+
         /** @var \Magento\Tools\SampleData\Module\Catalog\Setup\Attribute $attributes */
         $attributes = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-            'Magento\Tools\SampleData\Module\Catalog\Setup\Attribute'
+            'Magento\Tools\SampleData\Module\Catalog\Setup\Attribute',
+            [
+                'fixtureHelper' => $fixtureHelper
+            ]
         );
+
+        ob_start();
+        $attributes->run();
+        $result = ob_get_clean();
+        $this->assertContains('Installing catalog attributes' . PHP_EOL, $result);
+        $this->assertContains('.................' . PHP_EOL, $result);
+    }
+
+    public function installCategories()
+    {
         /** @var \Magento\Tools\SampleData\Module\Catalog\Setup\Category $categories */
         $categories = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
             'Magento\Tools\SampleData\Module\Catalog\Setup\Category'
         );
 
+        ob_start();
+        $categories->run();
+        $result = ob_get_clean();
+        $this->assertContains('Installing categories', $result);
+        $this->assertContains('......................................', $result);
+    }
+
+    public function installProducts()
+    {
         $fixtureHelper = $this->getMockBuilder('Magento\Tools\SampleData\Helper\Fixture')
             ->disableOriginalConstructor()->setMethods(['getPath'])
             ->getMock();
-
         $fixtures = [realpath(__DIR__ . '/../../../_files/catalog_product.csv')];
-        foreach ($fixtures as $index => $fixture) {
-            $fixtureHelper->expects($this->at($index))->method('getPath')->with($fixture)
-                ->will($this->returnValue($fixture));
-        }
-
+        $fixtureHelper->expects($this->at(0))->method('getPath')->with($fixtures[0])
+            ->will($this->returnValue($fixtures[0]));
         /** @var \Magento\Tools\SampleData\Module\Catalog\Setup\Product $products */
         $products = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
             'Magento\Tools\SampleData\Module\Catalog\Setup\Product',
@@ -45,18 +78,6 @@ class ProductTest extends \PHPUnit_Framework_TestCase
                 'fixtures' => $fixtures
             ]
         );
-
-        ob_start();
-        $attributes->run();
-        $result = ob_get_clean();
-        $this->assertContains('Installing catalog attributes', $result);
-        $this->assertContains('................................', $result);
-
-        ob_start();
-        $categories->run();
-        $result = ob_get_clean();
-        $this->assertContains('Installing categories', $result);
-        $this->assertContains('......................................', $result);
 
         ob_start();
         $products->run();
