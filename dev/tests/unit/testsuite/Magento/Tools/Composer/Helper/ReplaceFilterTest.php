af0204f2b47d6dfa2743075cb847f3263c179b4d
Christopher O'Toole <otoolec@x.com>
2014-12-09 15:24:30 -0600

    MAGETWO-31437: Modularity Work (composer.json suggest)
    - Modify ReplaceFilter to not move modules that are listed in both replace and suggest to require

--- dev/tests/unit/testsuite/Magento/Tools/Composer/Helper/ReplaceFilterTest.php
+++ dev/tests/unit/testsuite/Magento/Tools/Composer/Helper/ReplaceFilterTest.php
@@ -42,4 +42,42 @@ class ReplaceFilterTest extends \PHPUnit_Framework_TestCase
             ['vendor/module-foo', false],
         ];
     }
+
+    public function testMoveMagentoComponentsToRequire()
+    {
+        $replaceMap = [
+            'magento/framework' => 'self.version',
+            'magento/module-store' => 'self.version',
+            'magento/module-fedex' => 'self.version',
+            'symfony/yaml' => 'self.version',
+        ];
+        $package = $this->getMockBuilder('Magento\Tools\Composer\Package\Package')
+            ->disableOriginalConstructor()->getMock();
+        $package->expects($this->any())
+            ->method('get')
+            ->willReturnMap([
+                    ['replace', null, $replaceMap],
+                    ['suggest', null, ['magento/module-fedex' => 'comment']],
+                    ['replace->magento/framework', null, 'self.version'],
+                    ['replace->magento/module-store', null, 'self.version'],
+                    ['replace->magento/module-fedex', null, 'self.version'],
+                    ['suggest->magento/module-fedex', null, '0.1.0-alpha1337'],
+                ]);
+        $package->expects($this->exactly(2))
+            ->method('unsetProperty')
+            ->withConsecutive(
+                [$this->equalTo('replace->magento/framework')],
+                [$this->equalTo('replace->magento/module-store')]
+            );
+        $package->expects($this->exactly(2))
+            ->method('set')
+            ->withConsecutive(
+                [$this->equalTo('require->magento/framework'), $this->anything()],
+                [$this->equalTo('require->magento/module-store'), $this->anything()]
+            );
+
+        $replaceFilter = new \Magento\Tools\Composer\Helper\ReplaceFilter(null);
+
+        $replaceFilter->moveMagentoComponentsToRequire($package, false);
+    }
 }
