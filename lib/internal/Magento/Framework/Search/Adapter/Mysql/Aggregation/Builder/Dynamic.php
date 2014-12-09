edb6037c729aacf23d4be233b42f2fa0d59390f1
Anton Ohorodnyk <aohorodnyk@ebay.com>
2014-12-09 16:48:21 +0200

    MAGETWO-30818: Incorrect interval in LN(equalize product counts) for small values

--- lib/internal/Magento/Framework/Search/Adapter/Mysql/Aggregation/Builder/Dynamic.php
+++ lib/internal/Magento/Framework/Search/Adapter/Mysql/Aggregation/Builder/Dynamic.php
@@ -55,8 +55,8 @@ class Dynamic implements BucketInterface
     {
         $resultData = [];
         foreach ($data as $value) {
-            $from = $value['from'] ?: '*';
-            $to = $value['to'] ?: '*';
+            $from = isset($value['from']) ? $value['from'] : '*';
+            $to = isset($value['to']) ? $value['to'] : '*';
             unset($value['from'], $value['to']);
 
             $rangeName = "{$from}_{$to}";
