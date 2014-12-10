5abd9199943b1c9c7f3bda573a655359118b87dc
Olga Kopylova <okopylova@ebay.com>
2014-12-09 20:23:08 -0600

    MAGETWO-31413: Mainline Contribution of Install Bug Fixes
    - return back Sample Data installation tool
    - commit updated composer.lock

--- setup/module/Magento/Setup/src/Model/SampleData.php
+++ setup/module/Magento/Setup/src/Model/SampleData.php
@@ -61,16 +61,6 @@ class SampleData
         $sampleDataLogger = $objectManager->get('Magento\Tools\SampleData\Logger');
         $sampleDataLogger->setSubject($logger);
 
-        /** @var \Magento\User\Model\UserFactory $userFactory */
-        $userFactory = $objectManager->get('Magento\User\Model\UserFactory');
-        $user = $userFactory->create()->loadByUsername($adminUserName);
-        if (!$user || !$user->getId()) {
-            throw new \Exception('Invalid username provided');
-        }
-        /** @var \Magento\Backend\Model\Auth\Session $session */
-        $session = $objectManager->get('Magento\Backend\Model\Auth\Session');
-        $session->setUser($user);
-
         $areaCode = 'adminhtml';
         /** @var \Magento\Framework\App\State $appState */
         $appState = $objectManager->get('Magento\Framework\App\State');
@@ -79,49 +69,11 @@ class SampleData
         $configLoader = $objectManager->get('Magento\Framework\App\ObjectManager\ConfigLoader');
         $objectManager->configure($configLoader->load($areaCode));
 
-        /** @var \Magento\Tools\SampleData\Helper\Deploy $helper */
-        $helper = $objectManager->get('Magento\Tools\SampleData\Helper\Deploy');
-        $helper->run();
+        /** @var \Magento\User\Model\UserFactory $userFactory */
+        $userFactory = $objectManager->get('Magento\User\Model\UserFactory');
+        $user = $userFactory->create()->loadByUsername($adminUserName);
 
-        /** @var \Magento\Framework\Module\ModuleListInterface $moduleList */
-        $moduleList = $objectManager->get('Magento\Framework\Module\ModuleListInterface');
-
-        /** @var \Magento\Tools\SampleData\SetupFactory $setupFactory */
-        $setupFactory = $objectManager->get('Magento\Tools\SampleData\SetupFactory');
-
-        /** @var \Magento\Tools\SampleData\Helper\PostInstaller $postInstaller */
-        $postInstaller = $objectManager->get('Magento\Tools\SampleData\Helper\PostInstaller');
-
-        $resources = $this->getSetupResourceModels();
-        foreach ($moduleList->getNames() as $moduleName) {
-            if (isset($resources[$moduleName])) {
-                $class = $resources[$moduleName];
-                $setupFactory->create($class)->run();
-                $postInstaller->addModule($moduleName);
-            }
-        }
-
-        $session->unsUser();
-
-        /** @var \Magento\Tools\SampleData\Helper\PostInstaller $postInstaller */
-        $postInstaller = $objectManager->get('Magento\Tools\SampleData\Helper\PostInstaller');
-        $postInstaller->run();
-    }
-
-    /**
-     * Determines which resource models to run during setup
-     *
-     * @return array
-     */
-    private function getSetupResourceModels()
-    {
-        $config = [];
-        foreach ($this->rootDir->search(self::PATH . '/config/*.php') as $filename) {
-            if ($this->rootDir->isFile($filename)) {
-                $configPart = include $this->rootDir->getAbsolutePath($filename);
-                $config = array_merge_recursive($config, $configPart);
-            }
-        }
-        return isset($config['setup_resources']) ? $config['setup_resources'] : [];
+        $installer = $objectManager->get('Magento\Tools\SampleData\Installer');
+        $installer->run($user);
     }
 }
