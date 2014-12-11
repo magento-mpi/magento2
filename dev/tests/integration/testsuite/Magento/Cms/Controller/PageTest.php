3bd094186fa9428698a862be7a64e319f8037040
Evgeniy Miskov <emiskov@ebay.com>
2014-12-11 12:59:48 +0200

    MAGETWO-30419: [GITHUB] CMS Router not routing correctly #683

--- dev/tests/integration/testsuite/Magento/Cms/Controller/PageTest.php
+++ dev/tests/integration/testsuite/Magento/Cms/Controller/PageTest.php
@@ -31,4 +31,27 @@ class PageTest extends \Magento\TestFramework\TestCase\AbstractController
         $breadcrumbsBlock = $layout->getBlock('breadcrumbs');
         $this->assertContains($breadcrumbsBlock->toHtml(), $this->getResponse()->getBody());
     }
+
+    /**
+     * @magentoDataFixture cmsPageWIthSystemRouteFixture
+     */
+    public function testCreatePageWithSameModuleName()
+    {
+        $this->dispatch('/shipping/');
+        $content = $this->getResponse()->getBody();
+        $this->assertContains('Shipping Test Page', $content);
+    }
+
+    public static function cmsPageWIthSystemRouteFixture()
+    {
+        /** @var $page \Magento\Cms\Model\Page */
+        $page = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Cms\Model\Page');
+        $page->setTitle('Test title')
+            ->setIdentifier('shipping')
+            ->setStores([0])
+            ->setIsActive(1)
+            ->setContent('<h1>Shipping Test Page</h1>')
+            ->setPageLayout('1column')
+            ->save();
+    }
 }
