d0ee5ac3894bea9629595d0d9e486e1a762d74ad
Eddie Lau <kahlau@ebay.com>
2014-12-12 17:51:10 -0600

    MAGETWO-31534: Ogres Bugfixes Contribution to Mainline
    - fix sanity check fail

--- dev/tests/unit/testsuite/Magento/Framework/Module/DbVersionInfoTest.php
+++ dev/tests/unit/testsuite/Magento/Framework/Module/DbVersionInfoTest.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * {license_notice}
- *
- * @copyright   {copyright}
- * @license     {license_link}
+ * @copyright Copyright (c) 2014 X.commerce, Inc. (http://www.magentocommerce.com)
  */
 namespace Magento\Framework\Module;
 
@@ -26,7 +23,7 @@ class DbVersionInfoTest extends \PHPUnit_Framework_TestCase
     private $moduleResource;
 
     /**
-     * @var Magento\Framework\Module\ResourceResolverInterface|\PHPUnit_Framework_MockObject_MockObject
+     * @var \Magento\Framework\Module\ResourceResolverInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     private $resourceResolver;
 
