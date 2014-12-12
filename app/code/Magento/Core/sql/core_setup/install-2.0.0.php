7a3c12186951dff9b429d55b95e8af3e73502357
Arkadii Chyzhov <achyzhov@ebay.com>
2014-12-12 11:57:08 +0200

    MAGETWO-31708: Merge Core sql scripts and rename file with suffix 2.0.0
    - Merged upgrade-1.6.0.10-1.6.0.11.php into install-2.0.0.php

--- app/code/Magento/Core/sql/core_setup/install-2.0.0.php
+++ app/code/Magento/Core/sql/core_setup/install-2.0.0.php
@@ -795,6 +795,74 @@ $table = $connection->newTable(
 $connection->createTable($table);
 
 /**
+ * Add and change columns in 'core_theme_files'
+ */
+$connection->addColumn(
+    $installer->getTable('core_theme_files'),
+    'is_temporary',
+    array(
+        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_BOOLEAN,
+        'nullable' => false,
+        'default' => 0,
+        'comment' => 'Is Temporary File'
+    )
+);
+
+$connection->changeColumn(
+    $installer->getTable('core_theme_files'),
+    'file_name',
+    'file_path',
+    array(
+        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        'length' => 255,
+        'nullable' => true,
+        'comment' => 'Relative path to file'
+    )
+);
+
+$connection->changeColumn(
+    $installer->getTable('core_theme_files'),
+    'order',
+    'sort_order',
+    array('type' => \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT)
+);
+
+/**
+ * Create table 'core_theme_files_link'
+ */
+$table = $connection->newTable(
+    $installer->getTable('core_theme_files_link')
+)->addColumn(
+    'files_link_id',
+    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+    null,
+    array('identity' => true, 'nullable' => false, 'unsigned' => true, 'primary' => true),
+    'Customization link id'
+)->addColumn(
+    'theme_id',
+    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+    null,
+    array('nullable' => false, 'unsigned' => true),
+    'Theme Id'
+)->addColumn(
+    'layout_link_id',
+    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+    null,
+    array('nullable' => false, 'unsigned' => true),
+    'Theme layout link id'
+)->addForeignKey(
+    $installer->getFkName('core_theme_files_link', 'theme_id', 'core_theme', 'theme_id'),
+    'theme_id',
+    $installer->getTable('core_theme'),
+    'theme_id',
+    \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE,
+    \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+)->setComment(
+    'Core theme link on layout update'
+);
+$installer->getConnection()->createTable($table);
+
+/**
  * Add column 'type' to 'core_theme'
  */
 $connection->addColumn(
