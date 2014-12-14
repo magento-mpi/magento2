3d3cac4f7f9c810addfe517cfa3690023092c2d9
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2014-12-14 20:03:58 +0200

    MAGETWO-31798: Product alerts are not working

--- app/code/Magento/Cron/Model/Schedule.php
+++ app/code/Magento/Cron/Model/Schedule.php
@@ -44,14 +44,14 @@ class Schedule extends \Magento\Framework\Model\AbstractModel
     const STATUS_ERROR = 'error';
 
     /**
-     * @var \Magento\Framework\Stdlib\DateTime\DateTime
+     * @var \Magento\Framework\Stdlib\DateTime\TimezoneInterface
      */
-    protected $_date;
+    protected $timezone;
 
     /**
      * @param \Magento\Framework\Model\Context $context
      * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\Stdlib\DateTime\DateTime $date
+     * @param \Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone
      * @param \Magento\Framework\Model\Resource\AbstractResource $resource
      * @param \Magento\Framework\Data\Collection\Db $resourceCollection
      * @param array $data
@@ -59,12 +59,12 @@ class Schedule extends \Magento\Framework\Model\AbstractModel
     public function __construct(
         \Magento\Framework\Model\Context $context,
         \Magento\Framework\Registry $registry,
-        \Magento\Framework\Stdlib\DateTime\DateTime $date,
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone,
         \Magento\Framework\Model\Resource\AbstractResource $resource = null,
         \Magento\Framework\Data\Collection\Db $resourceCollection = null,
         array $data = []
     ) {
-        $this->_date = $date;
+        $this->timezone = $timezone;
         parent::__construct($context, $registry, $resource, $resourceCollection, $data);
     }
 
@@ -111,18 +111,13 @@ class Schedule extends \Magento\Framework\Model\AbstractModel
             $time = strtotime($time);
         }
 
-        // set time zone to UTC as getdate() takes into account PHP time zone and gets as a parameter a timestamp
-        // which is shifted to time zone of Magento
-        $currentTimezone = @date_default_timezone_get();
-        @date_default_timezone_set('UTC');
-        $d = getdate($this->_date->timestamp($time));
-        @date_default_timezone_set($currentTimezone);
+        $dateWithTimezone = $this->timezone->date($time);
 
-        $match = $this->matchCronExpression($e[0], $d['minutes'])
-            && $this->matchCronExpression($e[1], $d['hours'])
-            && $this->matchCronExpression($e[2], $d['mday'])
-            && $this->matchCronExpression($e[3], $d['mon'])
-            && $this->matchCronExpression($e[4], $d['wday']);
+        $match = $this->matchCronExpression($e[0], $dateWithTimezone->get(\Zend_Date::MINUTE))
+            && $this->matchCronExpression($e[1], $dateWithTimezone->get(\Zend_Date::HOUR))
+            && $this->matchCronExpression($e[2], $dateWithTimezone->get(\Zend_Date::DAY))
+            && $this->matchCronExpression($e[3], $dateWithTimezone->get(\Zend_Date::MONTH))
+            && $this->matchCronExpression($e[4], $dateWithTimezone->get(\Zend_Date::WEEKDAY));
 
         return $match;
     }
