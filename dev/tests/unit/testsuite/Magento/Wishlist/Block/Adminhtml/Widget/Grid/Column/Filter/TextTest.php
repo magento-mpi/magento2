a1d90a54830807dec725e5f4f56692b689836940
Pavlo Cherniavskyi <pcherniavsky@ebay.com>
2014-12-15 10:29:17 +0200

    MAGETWO-31724: Simple and Virtual Product can't be found in customer wishlist grid by name.
    - test fix

--- dev/tests/unit/testsuite/Magento/Wishlist/Block/Adminhtml/Widget/Grid/Column/Filter/TextTest.php
+++ dev/tests/unit/testsuite/Magento/Wishlist/Block/Adminhtml/Widget/Grid/Column/Filter/TextTest.php
@@ -4,7 +4,7 @@
  */
 namespace testsuite\Magento\Wishlist\Block\Adminhtml\Widget\Grid\Column\Filter;
 
-use Magento\Wishlist\Block\Adminhtml\Widget\Grid\Column\Filter\Text;
+use Magento\Wishlist\Block\Adminhtml\Widget\Grid\Column\Filter;
 
 class TextTest extends \PHPUnit_Framework_TestCase
 {
