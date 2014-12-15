b54d982ff26739eec9689f546a9537b138b1d736
Vlad Veselov <vveselov@ebay.com>
2014-12-15 13:09:19 -0500

    MAGETWO-31744: Last 5 Orders show 0 items qty for orders after created invoice
    - corrected namespace of test

--- dev/tests/integration/testsuite/Magento/Sales/Model/Order/InvoiceTest.php
+++ dev/tests/integration/testsuite/Magento/Sales/Model/Order/InvoiceTest.php
@@ -2,7 +2,7 @@
 /**
  * @copyright Copyright (c) 2014 X.commerce, Inc. (http://www.magentocommerce.com)
  */
-namespace Magento\Sales\Model\Order\Invoice;
+namespace Magento\Sales\Model\Order;
 
 class InvoiceTest extends \PHPUnit_Framework_TestCase
 {
